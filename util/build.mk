bc4c1b4eb9ca3b30f07ba78d0c3f591e057874b1
Randall Spangler <rspangler@chromium.org>
2011-12-08 09:37:32 -0800

    Separate utility build for build and host utils
    Build is the system doing the build (e.g. 64-bit linux) and host is the
    target platform on top of the ec (e.g. 32-bit Chromium OS).
    
    Necessary to get ectool properly compiling for Chromium OS.
    
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    
    BUG=none
    TEST=make && file build/bds/util/ectool; ectool should be a 32-bit binary
    
    Change-Id: I50eba4c164ece236646a7c6087b1b86769beeb28

--- util/build.mk
+++ util/build.mk
@@ -1,5 +1,8 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 #
 # Host tools build
 #
 
-util-bin=ectool
+host-util-bin=ectool
