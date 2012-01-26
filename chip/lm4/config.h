028150a0591c899feae6ec08fcd7efd685ca80ee
Randall Spangler <rspangler@chromium.org>
2012-01-26 13:36:07 -0800

    Increase link image size to 40KB
    with x86 power module enabled, it no longer fits in 32KB.
    
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    
    BUG=none
    TEST=make
    
    Change-Id: I38c9054efd8a072cc5800cc87936e53e2df00e58

--- chip/lm4/config.h
+++ chip/lm4/config.h
@@ -11,7 +11,7 @@
 #define CONFIG_RAM_SIZE         0x00008000
 
 /* Size of one firmware image in flash */
-#define CONFIG_FW_IMAGE_SIZE    (32 * 1024)
+#define CONFIG_FW_IMAGE_SIZE    (40 * 1024)
 #define CONFIG_FW_RO_OFF        0
 #define CONFIG_FW_A_OFF         CONFIG_FW_IMAGE_SIZE
 #define CONFIG_FW_B_OFF         (2 * CONFIG_FW_IMAGE_SIZE)
