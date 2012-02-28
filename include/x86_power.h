a395a7272d860ab630ebf9e9c6a04dc1311d1b4c
Randall Spangler <rspangler@chromium.org>
2012-02-27 16:58:43 -0800

    Add APIs for thermal module to tell x86_power about overheating
    (implementation of APIs still todo)
    
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    
    BUG=chrome-os-partner:8242
    TEST=none
    
    Change-Id: Idbd38c4e873e95382ae815e1d5b827d95396be8f

--- include/x86_power.h
+++ include/x86_power.h
@@ -20,5 +20,12 @@ void x86_power_interrupt(enum gpio_signal signal);
 /* Returns true if the system is in S0. */
 int x86_power_in_S0(void);
 
+/* Informs the power module that the CPU has overheated (too_hot=1) or is
+ * no longer too hot (too_hot=0). */
+void x86_power_cpu_overheated(int too_hot);
+
+/* Immediately shuts down power to the main processor and chipset.  This is
+ * intended for use when the system is too hot or battery power is critical. */
+void x86_power_force_shutdown(void);
 
 #endif  /* __CROS_EC_X86_POWER_H */
