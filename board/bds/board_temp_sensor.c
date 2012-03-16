jackadam1981/ChromeEc

Temperature sensor grouping.
Group temperature sensors into different types so we only have to set
temperature threshold for each type instead of each sensor.

Signed-off-by: Vic Yang <victoryang@google.com>

BUG=chrome-os-partner:8466
TEST=Fan control still works.

Change-Id: I7acc714c32f282cec490b9e02d402ab91a53becf

board/bds/board_temp_sensor.c
@@ -20,9 +20,12 @@
  * temp_sensor_id.
  */
 const struct temp_sensor_t temp_sensors[TEMP_SENSOR_COUNT] = {
-	{"ECInternal", TEMP_SENSOR_POWER_NONE, chip_temp_sensor_get_val, 0},
-	{"CaseDie", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
-	{"Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
+	{"ECInternal", TEMP_SENSOR_POWER_NONE, TEMP_SENSOR_TYPE_EC,
+	 chip_temp_sensor_get_val, 0},
+	{"CaseDie", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 0},
+	{"Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 1},
 };
 
 const struct tmp006_t tmp006_sensors[TMP006_COUNT] = {

board/link/board_temp_sensor.c
@@ -27,16 +27,26 @@
  * temp_sensor_id.
  */
 const struct temp_sensor_t temp_sensors[TEMP_SENSOR_COUNT] = {
-	{"I2C_CPU-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
-	{"I2C_CPU-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 1},
-	{"I2C_PCH-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 2},
-	{"I2C_PCH-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 3},
-	{"I2C_DDR-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 4},
-	{"I2C_DDR-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 5},
-	{"I2C_Charger-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 6},
-	{"I2C_Charger-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 7},
-	{"ECInternal", TEMP_SENSOR_POWER_NONE, chip_temp_sensor_get_val, 0},
-	{"PECI", TEMP_SENSOR_POWER_CPU, peci_temp_sensor_get_val, 0},
+	{"I2C_CPU-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CPU,
+	 tmp006_get_val, 0},
+	{"I2C_CPU-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 1},
+	{"I2C_PCH-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_PCH,
+	 tmp006_get_val, 2},
+	{"I2C_PCH-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 3},
+	{"I2C_DDR-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_MEMORY,
+	 tmp006_get_val, 4},
+	{"I2C_DDR-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 5},
+	{"I2C_Charger-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 6},
+	{"I2C_Charger-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 7},
+	{"ECInternal", TEMP_SENSOR_POWER_NONE, TEMP_SENSOR_TYPE_EC,
+	 chip_temp_sensor_get_val, 0},
+	{"PECI", TEMP_SENSOR_POWER_CPU, TEMP_SENSOR_TYPE_CPU,
+	 peci_temp_sensor_get_val, 0},
 };
 
 const struct tmp006_t tmp006_sensors[TMP006_COUNT] = {

FrameworkComputer/EmbeddedController

board/bds/board_temp_sensor.c
@@ -20,9 +20,12 @@
  * temp_sensor_id.
  */
 const struct temp_sensor_t temp_sensors[TEMP_SENSOR_COUNT] = {
-	{"ECInternal", TEMP_SENSOR_POWER_NONE, chip_temp_sensor_get_val, 0},
-	{"CaseDie", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
-	{"Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
+	{"ECInternal", TEMP_SENSOR_POWER_NONE, TEMP_SENSOR_TYPE_BOARD,
+	 chip_temp_sensor_get_val, 0},
+	{"CaseDie", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 0},
+	{"Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 1},
 };
 
 const struct tmp006_t tmp006_sensors[TMP006_COUNT] = {

board/link/board_temp_sensor.c
@@ -27,16 +27,26 @@
  * temp_sensor_id.
  */
 const struct temp_sensor_t temp_sensors[TEMP_SENSOR_COUNT] = {
-	{"I2C_CPU-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 0},
-	{"I2C_CPU-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 1},
-	{"I2C_PCH-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 2},
-	{"I2C_PCH-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 3},
-	{"I2C_DDR-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 4},
-	{"I2C_DDR-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 5},
-	{"I2C_Charger-Die", TEMP_SENSOR_POWER_VS, tmp006_get_val, 6},
-	{"I2C_Charger-Object", TEMP_SENSOR_POWER_VS, tmp006_get_val, 7},
-	{"ECInternal", TEMP_SENSOR_POWER_NONE, chip_temp_sensor_get_val, 0},
-	{"PECI", TEMP_SENSOR_POWER_CPU, peci_temp_sensor_get_val, 0},
+	{"I2C_CPU-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CPU,
+	 tmp006_get_val, 0},
+	{"I2C_CPU-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 1},
+	{"I2C_PCH-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 2},
+	{"I2C_PCH-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 3},
+	{"I2C_DDR-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 4},
+	{"I2C_DDR-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 5},
+	{"I2C_Charger-Die", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_BOARD,
+	 tmp006_get_val, 6},
+	{"I2C_Charger-Object", TEMP_SENSOR_POWER_VS, TEMP_SENSOR_TYPE_CASE,
+	 tmp006_get_val, 7},
+	{"ECInternal", TEMP_SENSOR_POWER_NONE, TEMP_SENSOR_TYPE_BOARD,
+	 chip_temp_sensor_get_val, 0},
+	{"PECI", TEMP_SENSOR_POWER_CPU, TEMP_SENSOR_TYPE_CPU,
+	 peci_temp_sensor_get_val, 0},
 };
 
 const struct tmp006_t tmp006_sensors[TMP006_COUNT] = {