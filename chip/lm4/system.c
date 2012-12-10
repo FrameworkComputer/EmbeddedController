7a2e3372eef4d8eee8ee140f517a309dc7065887
Randall Spangler <rspangler@chromium.org>
2012-12-10 14:31:45 -0800

    link: Increase cold reset hibernate time to 1 second
    This gives VDDC more time to bleed out before the system reboots.
    
    This will require FAFT changes to compensate for the longer cold reset time.
    
    BUG=chrome-os-partner:16600
    BRANCH=link
    TEST=from ec console, 'reboot cold' should take a second.
    
    Change-Id: I7e0e901958593262868151642560296f0c5496a7
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/39515
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>

--- chip/lm4/system.c
+++ chip/lm4/system.c
@@ -37,7 +37,7 @@ enum hibdata_index {
  * EC itself, but we need a longer delay to ensure the rest of the components
  * on the same power rail are reset and 5VALW has dropped.
  */
-#define HIB_RESET_USEC 200000
+#define HIB_RESET_USEC 1000000
 
 /**
  * Wait for a write to commit to a hibernate register.
