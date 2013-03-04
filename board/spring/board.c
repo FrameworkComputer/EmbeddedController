f568b823219183af836567cc44912199ec24b0c2
Jeremy Thorpe <jeremyt@chromium.org>
2013-03-04 10:18:25 -0800

    Rename CODEC_INT to ID_MUX.
    Rename CODEC_INT to ID_MUX and let it be settable on the EC command line.  This
    GPIO line controls the ID line MUX in Spring.
    
    BRANCH=spring
    BUG=chrome-os-partner:16196
    TEST=manual
    
    Change-Id: Ib29f31cf5dd28f5166236b5f98560543298b083b
    Signed-off-by: Jeremy Thorpe <jeremyt@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/44531
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Tested-by: Vic Yang <victoryang@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- board/spring/board.c
+++ board/spring/board.c
@@ -99,7 +99,7 @@ const struct gpio_info gpio_list[GPIO_COUNT] = {
 	{"ENTERING_RW", GPIO_D, (1<<0),  GPIO_OUT_LOW, NULL},
 	{"CHARGER_EN",  GPIO_B, (1<<2),  GPIO_OUT_LOW, NULL},
 	{"EC_INT",      GPIO_B, (1<<9),  GPIO_HI_Z, NULL},
-	{"CODEC_INT",   GPIO_D, (1<<1),  GPIO_HI_Z, NULL},
+	{"ID_MUX",      GPIO_D, (1<<1),  GPIO_OUT_LOW, NULL},
 	{"KB_OUT00",    GPIO_B, (1<<0),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT01",    GPIO_B, (1<<8),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT02",    GPIO_B, (1<<12), GPIO_KB_OUTPUT, NULL},
@@ -203,13 +203,6 @@ void board_interrupt_host(int active)
 {
 	/* interrupt host by using active low EC_INT signal */
 	gpio_set_level(GPIO_EC_INT, !active);
-}
-
-void board_keyboard_suppress_noise(void)
-{
-	/* notify audio codec of keypress for noise suppression */
-	gpio_set_level(GPIO_CODEC_INT, 0);
-	gpio_set_level(GPIO_CODEC_INT, 1);
 }
 
 static void board_startup_hook(void)
