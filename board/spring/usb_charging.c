4ac742b4d511359687a00de1ab6c3ca785f06b0b
Vic Yang <victoryang@chromium.org>
2013-03-09 03:10:43 +0800

    spring: Fix ID_MUX polarity
    This reflects the change of the ID_MUX polarity.
    
    BUG=chrome-os-partner:18165
    TEST=none
    BRANCH=none
    
    Change-Id: Iea126149f8785a0f028221463d197c63b39a3bd7
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/44945
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- board/spring/usb_charging.c
+++ board/spring/usb_charging.c
@@ -188,12 +188,12 @@ static int board_apple_charger_current(void)
 static int board_probe_video(int device_type)
 {
 	tsu6721_disable_interrupts();
-	gpio_set_level(GPIO_ID_MUX, 0);
+	gpio_set_level(GPIO_ID_MUX, 1);
 	msleep(DELAY_ID_MUX_MS);
 
 	if (adc_read_channel(ADC_CH_USB_DP_SNS) > VIDEO_ID_THRESHOLD) {
 		/* Actually an USB host */
-		gpio_set_level(GPIO_ID_MUX, 1);
+		gpio_set_level(GPIO_ID_MUX, 0);
 		msleep(DELAY_ID_MUX_MS);
 		tsu6721_enable_interrupts();
 		return device_type;
@@ -434,7 +434,7 @@ static void board_usb_monitor_detach(void)
 
 	if (adc_read_channel(ADC_CH_USB_DP_SNS) > VIDEO_ID_THRESHOLD) {
 		pmu_enable_fet(FET_VIDEO, 0, NULL);
-		gpio_set_level(GPIO_ID_MUX, 1);
+		gpio_set_level(GPIO_ID_MUX, 0);
 		msleep(DELAY_ID_MUX_MS);
 		tsu6721_enable_interrupts();
 		usb_device_change(TSU6721_TYPE_NONE);
