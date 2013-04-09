64ae15f3b3c743a8b98af7754871c0ba7470becd
Vic Yang <victoryang@chromium.org>
2013-04-09 15:34:23 +0800

    Decouple gaia_power from keyscan task
    This makes gaia_power.c compile when keyscan task is not present.
    
    BUG=chrome-os-partner:18598
    TEST=Build spring without keyscan task.
    BRANCH=none
    
    Change-Id: I70823fb562a2542d92929b9219d034216f636938
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47630

--- common/gaia_power.c
+++ common/gaia_power.c
@@ -172,9 +172,11 @@ static int check_for_power_off_event(void)
 			pressed = 1;
 	}
 
+#ifdef CONFIG_TASK_KEYSCAN
 	/* Dis/Enable keyboard scanning when the power button state changes */
 	if (!pressed || pressed != power_button_was_pressed)
 		keyboard_scan_enable(!pressed);
+#endif
 
 
 	now = get_time();
