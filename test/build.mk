950d507366cf66fb7a48d39c0612ccecd890fc19
Vic Yang <victoryang@chromium.org>
2013-04-28 14:42:10 +0800

    Remove mock files
    We are moving to a per-function mock mechanism, which is less likely to
    break when someone adds a function to a file with a mock.
    
    BUG=chrome-os-partner:18598
    TEST=None
    BRANCH=None
    
    Change-Id: Ie625064730d2224accbaf1febbdc4750e793d047
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/49451

--- test/build.mk
+++ test/build.mk
@@ -20,29 +20,3 @@ stress-y=stress.o
 timer_calib-y=timer_calib.o
 timer_dos-y=timer_dos.o
 utils-y=utils.o
-
-# Mock modules for 'thermal'
-chip-mock-thermal-lpc.o=mock_lpc.o
-chip-mock-thermal-pwm.o=mock_pwm.o
-common-mock-thermal-x86_power.o=mock_x86_power.o
-common-mock-thermal-temp_sensor.o=mock_temp_sensor.o
-
-# Mock modules for 'power_button'
-chip-mock-power_button-gpio.o=mock_gpio.o
-chip-mock-power_button-pwm.o=mock_pwm.o
-common-mock-power_button-x86_power.o=mock_x86_power.o
-common-mock-power_button-i8042.o=mock_i8042.o
-
-# Mock modules for 'scancode'
-chip-mock-scancode-keyboard_scan_stub.o=mock_keyboard_scan_stub.o
-common-mock-scancode-i8042.o=mock_i8042.o
-
-# Mock modules for 'typematic'
-chip-mock-typematic-keyboard_scan_stub.o=mock_keyboard_scan_stub.o
-common-mock-typematic-i8042.o=mock_i8042.o
-
-# Mock modules for 'charging'
-chip-mock-charging-gpio.o=mock_gpio.o
-common-mock-charging-x86_power.o=mock_x86_power.o
-common-mock-charging-smart_battery_stub.o=mock_smart_battery_stub.o
-common-mock-charging-charger_bq24725.o=mock_charger.o
