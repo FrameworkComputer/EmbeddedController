b061f460eb5753da10a9680bfdb0b1eebbb0103d
Bill Richardson <wfrichar@chromium.org>
2013-05-02 13:03:59 -0700

    Define slippy GPIOs, make power sequencing task do nothing.
    This specifies the Slippy GPIOs. Because the power controls are completely
    different from Link, we have to gut the power sequencing task to do nothing.
    For bringup and test, we'll just manually set and get the GPIOs until we
    know exactly what we need to do.
    
    This is where the fun starts...
    
    BUG=chrome-os-partner:18825
    BRANCH=slippy
    TEST=manual
    
    Built everything, Link still works.
    
    Change-Id: Ic1ce1d4085298f49dd98d99e81e04835eca5f11c
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/50004

--- board/slippy/board.h
+++ board/slippy/board.h
@@ -65,71 +65,68 @@ enum gpio_signal {
 	/* Inputs with interrupt handlers are first for efficiency */
 	GPIO_POWER_BUTTON_L = 0,   /* Power button */
 	GPIO_LID_OPEN,             /* Lid switch */
-	GPIO_THERMAL_DATA_READY_L, /* Data ready from I2C thermal sensor */
-	/* Other inputs */
 	GPIO_AC_PRESENT,           /* AC power present */
-	GPIO_BOARD_VERSION1,       /* Board version stuffing resistor 1 */
-	GPIO_BOARD_VERSION2,       /* Board version stuffing resistor 2 */
-	GPIO_BOARD_VERSION3,       /* Board version stuffing resistor 3 */
 	GPIO_PCH_BKLTEN,           /* Backlight enable signal from PCH */
-	GPIO_PCH_SLP_A_L,          /* SLP_A# signal from PCH */
-	GPIO_PCH_SLP_ME_CSW_DEV_L, /* SLP_ME_CSW_DEV# signal from PCH */
+	GPIO_PCH_SLP_S0_L,         /* SLP_S0# signal from PCH */
 	GPIO_PCH_SLP_S3_L,         /* SLP_S3# signal from PCH */
-	GPIO_PCH_SLP_S4_L,         /* SLP_S4# signal from PCH */
 	GPIO_PCH_SLP_S5_L,         /* SLP_S5# signal from PCH */
 	GPIO_PCH_SLP_SUS_L,        /* SLP_SUS# signal from PCH */
 	GPIO_PCH_SUSWARN_L,        /* SUSWARN# signal from PCH */
-	GPIO_PGOOD_1_5V_DDR,       /* Power good on +1.5V_DDR */
-	GPIO_PGOOD_1_5V_PCH,       /* Power good on +1.5V_PCH */
-	GPIO_PGOOD_1_8VS,          /* Power good on +1.8VS */
-	GPIO_PGOOD_5VALW,          /* Power good on +5VALW */
-	GPIO_PGOOD_CPU_CORE,       /* Power good on +CPU_CORE */
-	GPIO_PGOOD_VCCP,           /* Power good on +VCCP */
-	GPIO_PGOOD_VCCSA,          /* Power good on +VCCSA */
-	GPIO_PGOOD_VGFX_CORE,      /* Power good on +VGFX_CORE */
+	GPIO_PP1050_PGOOD,         /* Power good on 1.05V */
+	GPIO_PP1350_PGOOD,         /* Power good on 1.35V (DRAM) */
+	GPIO_PP5000_PGOOD,         /* Power good on 5V */
+	GPIO_VCORE_PGOOD,          /* Power good on core VR */
+	GPIO_CPU_PGOOD,            /* Power good on CPU */
+	GPIO_PCH_EDP_VDD_EN,       /* PCH wants EDP enabled */
 	GPIO_RECOVERY_L,           /* Recovery signal from servo */
-	GPIO_USB1_STATUS_L,        /* USB charger port 1 status output */
-	GPIO_USB2_STATUS_L,        /* USB charger port 2 status output */
 	GPIO_WRITE_PROTECT,        /* Write protect input */
+
+	/* Other inputs */
+	GPIO_FAN_ALERT_L,          /* From thermal sensor */
+	GPIO_USB1_OC_L,            /* USB port overcurrent warning */
+	GPIO_USB2_OC_L,            /* USB port overcurrent warning */
+	GPIO_BOARD_VERSION1,       /* Board version stuffing resistor 1 */
+	GPIO_BOARD_VERSION2,       /* Board version stuffing resistor 2 */
+	GPIO_BOARD_VERSION3,       /* Board version stuffing resistor 3 */
+
 	/* Outputs */
 	GPIO_CPU_PROCHOT,          /* Force CPU to think it's overheated */
-	GPIO_ENABLE_1_5V_DDR,      /* Enable +1.5V_DDR supply */
-	GPIO_ENABLE_5VALW,         /* Enable +5V always on rail */
+	GPIO_PP1350_EN,            /* Enable 1.35V supply */
+	GPIO_PP3300_DSW_GATED_EN,  /* Enable DSW rails */
+	GPIO_PP3300_DX_EN,         /* Enable power to lots of peripherals */
+	GPIO_PP3300_LTE_EN,        /* Enable LTE radio */
+	GPIO_PP3300_WLAN_EN,       /* Enable WiFi radio */
+	GPIO_SUSP_VR_EN,           /* Enable 1.05V regulator */
+	GPIO_VCORE_EN,             /* Stuffing option - not connected */
+	GPIO_PP5000_EN,            /* Enable 5V supply */
+	GPIO_SYS_PWROK,            /* EC thinks everything is up and ready */
+	GPIO_WLAN_OFF_L,           /* Disable WiFi chip? Or just the radio? */
+	GPIO_CHARGE_L,             /* Allow battery to charge when on AC */
+
 	GPIO_ENABLE_BACKLIGHT,     /* Enable backlight power */
 	GPIO_ENABLE_TOUCHPAD,      /* Enable touchpad power */
-	GPIO_ENABLE_VCORE,         /* Enable +CPU_CORE and +VGFX_CORE */
-	GPIO_ENABLE_VS,            /* Enable VS power supplies */
-	GPIO_ENABLE_WLAN,          /* Enable WLAN module power (+3VS_WLAN) */
 	GPIO_ENTERING_RW,          /* Indicate when EC is entering RW code */
-	GPIO_LIGHTBAR_RESET_L,     /* Reset lightbar controllers */
-	GPIO_PCH_A20GATE,          /* A20GATE signal to PCH */
-	GPIO_PCH_DPWROK,           /* DPWROK signal to PCH */
+	GPIO_PCH_DPWROK,           /* Indicate when VccDSW is good */
+
 	GPIO_PCH_HDA_SDO,          /* HDA_SDO signal to PCH; when high, ME
 				    * ignores security descriptor */
-	GPIO_PCH_WAKE_L,           /* Wake signal output to PCH */
+	GPIO_PCH_WAKE_L,           /* Wake signal from EC to PCH */
 	GPIO_PCH_NMI_L,            /* Non-maskable interrupt pin to PCH */
 	GPIO_PCH_PWRBTN_L,         /* Power button output to PCH */
 	GPIO_PCH_PWROK,            /* PWROK / APWROK signals to PCH */
-	GPIO_PCH_RCIN_L,           /* RCIN# signal to PCH */
+	GPIO_PCH_RCIN_L,           /* RCIN# line to PCH (for 8042 emulation) */
 	GPIO_PCH_RSMRST_L,         /* Reset PCH resume power plane logic */
-	GPIO_PCH_RTCRST_L,         /* Reset PCH RTC well */
 	GPIO_PCH_SMI_L,            /* System management interrupt to PCH */
-	GPIO_PCH_SRTCRST_L,        /* Reset PCH ME RTC well */
-	GPIO_PCH_SUSACK_L,         /* Acknowledge PCH SUSWARN# signal */
-	GPIO_RADIO_ENABLE_WLAN,    /* Enable WLAN radio */
-	GPIO_RADIO_ENABLE_BT,      /* Enable bluetooth radio */
-	GPIO_SPI_CS_L,             /* SPI chip select */
 	GPIO_TOUCHSCREEN_RESET_L,  /* Reset touch screen */
-	GPIO_USB1_CTL1,            /* USB charger port 1 CTL1 output */
-	GPIO_USB1_CTL2,            /* USB charger port 1 CTL2 output */
-	GPIO_USB1_CTL3,            /* USB charger port 1 CTL3 output */
-	GPIO_USB1_ENABLE,          /* USB charger port 1 enable */
-	GPIO_USB1_ILIM_SEL,        /* USB charger port 1 ILIM_SEL output */
-	GPIO_USB2_CTL1,            /* USB charger port 2 CTL1 output */
-	GPIO_USB2_CTL2,            /* USB charger port 2 CTL2 output */
-	GPIO_USB2_CTL3,            /* USB charger port 2 CTL3 output */
-	GPIO_USB2_ENABLE,          /* USB charger port 2 enable */
-	GPIO_USB2_ILIM_SEL,        /* USB charger port 2 ILIM_SEL output */
+	GPIO_EC_EDP_VDD_EN,        /* Enable EDP (passthru from PCH) */
+	GPOI_LPC_CLKRUN_L,         /* Dunno. Probably important, though. */
+
+	GPIO_USB1_ENABLE,          /* USB port 1 output power enable */
+	GPIO_USB2_ENABLE,          /* USB port 2 output power enable */
+
+	GPIO_PCH_CATERR_L,         /* Wanted CATERR# from PCH, probably NC */
+	GPIO_PCH_RTCRST_L,         /* Not supposed to be here */
+	GPIO_PCH_SRTCRST_L,        /* Not supposed to be here */
 
 	/* Number of GPIOs; not an actual GPIO */
 	GPIO_COUNT
