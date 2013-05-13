c18ef5f4219fddf36052abb5052396808c4fca4a
Duncan Laurie <dlaurie@chromium.org>
2013-05-13 10:08:22 -0700

    slippy: Enable internal pullup for recovery pin
    The recovery pin input from servo is open drain and needs
    an internal pullup enabled so it is not always low.
    
    BUG=chrome-os-partner:19398
    BRANCH=none
    TEST=manual: ensure rec_mode can be turned off
    
    $ dut-control rec_mode:off rec_mode
    rec_mode:off
    
    Change-Id: I387a53eb5b64bb0bf3a87fc47b5cca9b2063f6a9
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/50993

--- board/slippy/board.c
+++ board/slippy/board.c
@@ -55,7 +55,7 @@ const struct gpio_info gpio_list[GPIO_COUNT] = {
 	 x86_power_interrupt},
 	{"PCH_EDP_VDD_EN",       LM4_GPIO_J, (1<<1), GPIO_INT_BOTH,
 	 x86_power_interrupt},
-	{"RECOVERY_L",           LM4_GPIO_A, (1<<5), GPIO_INT_BOTH,
+	{"RECOVERY_L",           LM4_GPIO_A, (1<<5), GPIO_PULL_UP|GPIO_INT_BOTH,
 	 switch_interrupt},
 	{"WRITE_PROTECT",        LM4_GPIO_A, (1<<4), GPIO_INT_BOTH,
 	 switch_interrupt},
