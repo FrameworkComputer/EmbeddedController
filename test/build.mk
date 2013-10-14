99472d5cb577681b6b711e001f5a50e6abc2e356
Vic Yang <victoryang@chromium.org>
2013-10-14 10:52:31 +0800

    Re-enable basic tests for most LM4-based boards
    Basic tests are actually working for most LM4-based boards. Let's
    re-enable them except for Bolt and Samus. These two boards have
    board-specific chipset code, which fails test compilation.
    
    BUG=chrome-os-partner:18598
    TEST=Run affected tests on Link
    TEST=util/make_all.sh
    TEST='make tests' for all board
    BRANCH=None
    
    Change-Id: I46bc39c14ec43ccc29e9b0c46f349cac2755e684
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/172982
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Jeremy Thorpe <jeremyt@chromium.org>

--- test/build.mk
+++ test/build.mk
@@ -18,12 +18,8 @@ test-list-$(BOARD_pit)+=kb_scan stress
 test-list-$(BOARD_snow)+=kb_scan stress
 test-list-$(BOARD_spring)+=kb_scan stress
 
-# Disable x86 boards until they compiles
-# TODO(victoryang): Fix them
-test-list-$(BOARD_link)=
-test-list-$(BOARD_slippy)=
-test-list-$(BOARD_falco)=
-test-list-$(BOARD_peppy)=
+# Bolt and Samus have board-specific chipset code, and the tests don't
+# compile with those. Disable them for now.
 test-list-$(BOARD_bolt)=
 test-list-$(BOARD_samus)=
 
