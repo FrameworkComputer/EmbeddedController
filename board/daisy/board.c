2d4ece9b69978c6b03af6f763c8d23ad62239948
Randall Spangler <rspangler@chromium.org>
2013-10-18 15:52:44 -0700

    daisy: Clean up pmu initialization
    CG_CTRL0 is already set to 2 by pmu_init_registers(), and the battery
    charge rate hack was temporary.
    
    BUG=chrome-os-partner:23425
    BRANCH=none
    TEST=build daisy
    
    Change-Id: I863c8d6d5d5f30375dce14b7d2a30e9e710ef969
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/173915
    Reviewed-by: Jeremy Thorpe <jeremyt@chromium.org>

--- board/daisy/board.c
+++ board/daisy/board.c
@@ -136,28 +136,7 @@ void keyboard_suppress_noise(void)
 /**
  * Board-specific PMU init.
  */
-
-#define CG_CTRL0 0x04
-#define CG_CTRL3 0x07
-
 int pmu_board_init(void)
 {
-	int failure = 0;
-
-	/* Init configuration
-	 *   Fast charge timer    : 2 hours
-	 *   Charger              : disable
-	 *   External pin control : enable
-	 *
-	 * TODO: move settings to battery pack specific init
-	 */
-	if (!failure)
-		failure = pmu_write(CG_CTRL0, 2);
-	/* Limit full charge current to 50%
-	 * TODO: remove this temporary hack.
-	 */
-	if (!failure)
-		failure = pmu_write(CG_CTRL3, 0xbb);
-
-	return failure ? EC_ERROR_UNKNOWN : EC_SUCCESS;
+	return EC_SUCCESS;
 }
