56dd81e39c578c15f09be358ab53ecd486d2bf22
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2013-12-23 11:24:52 +0800

    Merge make_all.sh to Makefile
    By merging make_all.sh to Makefile, parallel make can be made faster.
    Previously, if one does 'util/make_all.sh -j32', most of the time is
    spent on waiting for linking. Now that we invoke sub-make, linking an
    executable doesn't block the next board.
    
    With '-j32', the new 'make buildall' takes about 7 seconds, while the
    original 'util/make_all.sh' takes about 27 seconds.
    
    BUG=None
    TEST=make buildall -j32
    BRANCH=None
    
    Change-Id: I7c2f0d1e928a9b60a8a9070bdcb71b00a3d534cd
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/181091
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- util/make_all.sh
+++ util/make_all.sh
@@ -6,12 +6,4 @@
 #
 # Build all EC boards and run unit tests
 
-# Build all boards except host
-boards=$(ls -1 board | grep -v host)
-for b in $boards; do
-    echo ======== building $b
-    make BOARD=$b $*
-done
-
-# Run unit tests
-make BOARD=host runtests $*
+echo "$0 is deprecated. Please run 'make buildall' instead."
