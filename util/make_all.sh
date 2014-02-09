fe7194d29d955a193d429f88dfd532e6953eed77
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2014-02-09 10:50:17 +0800

    Remove deprecated make_all.sh
    This script has been deprecated for more than a month. It's time to
    remove it.
    
    BUG=None
    TEST=None
    BRANCH=None
    
    Change-Id: I6604d556e37ad90d9e9bec01664cbfaafcfb7a51
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/185541
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- util/make_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-#
-# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# Build all EC boards and run unit tests
-
-echo "$0 is deprecated. Please run 'make buildall' instead."
