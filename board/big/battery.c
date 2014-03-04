cb462f29c640a6913c1ad98fecf2132d720c2488
cloud_lin <cloud_lin@compal.com>
2014-03-04 17:03:43 +0800

    Big: add LGC 3s battery info
    BRANCH=big
    BUG=chrome-os-partner:26533
    TEST=build ec and flash to big board; verify battery works
    
    Signed-off-by: Cloud Lin <cloud_lin@compal.com>
    Signed-off-by: Katie Roberts-Hoffman <katierh@chromium.org>
    
    Change-Id: Ia005a549b8318b4f8df81e7b1341d50da28f4282
    Reviewed-on: https://chromium-review.googlesource.com/188632
    Reviewed-by: Katie Roberts-Hoffman <katierh@chromium.org>
    Commit-Queue: Katie Roberts-Hoffman <katierh@chromium.org>
    Tested-by: Katie Roberts-Hoffman <katierh@chromium.org>

--- board/big/battery.c
+++ board/big/battery.c
@@ -74,6 +74,23 @@ static struct battery_info info_3s = {
 	.discharging_max_c    = 50,
 };
 
+static struct battery_info info_3s_LGC = {
+
+        .voltage_max    = 13140,
+        .voltage_normal = 11400, /* Average of max & min */
+        .voltage_min    =  9000,
+
+        /* Pre-charge values. */
+        .precharge_current  = 256,      /* mA */
+
+        .start_charging_min_c = 0,
+        .start_charging_max_c = 50,
+        .charging_min_c       = 0,
+        .charging_max_c       = 60,
+        .discharging_min_c    = 0,
+        .discharging_max_c    = 75,
+};
+
 static struct battery_info info_4s = {
 
 	.voltage_max    = 17520,
@@ -119,6 +136,13 @@ static struct battery_device support_batteries[] = {
 		.design_mv		= 15200,
 		.battery_info		= &info_4s,
 		.support_cut_off	= 1,
+	},
+	{
+		.manuf                  = "LGC",
+		.device                 = "AC14B18J",
+		.design_mv              = 11400,
+		.battery_info           = &info_3s_LGC,
+		.support_cut_off        = 1,
 	},
 };
 
