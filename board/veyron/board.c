423c40c5a0c2b97df287e751a8ebfd326f2e6723
Alexandru Stan <amstan@chromium.org>
2014-07-09 16:45:41 -0700

    Created Veyron board folder based on Big
    The 2 boards are similar enough to test stuff on big for now, at least
    until the new hardware comes.
    
    Also added veyron to flash_ec.
    
    Also cleaned up the style: pre-upload.py was giving errors on files
    that were unmodified from big(spaces instead of tabs).
    I had to ignore this though:
    > ERROR: Macros with complex values should be enclosed in parenthesis
    > #471: FILE: board/veyron/board.h:35:
    > +#define KB_OUT_PORT_LIST GPIO_A, GPIO_B, GPIO_C
    
    BRANCH=none
    BUG=chrome-os-partner:30167
    TEST=~/trunk/src/platform/ec $ make BOARD=veyron clean &&
    make -j BOARD=veyron && util/flash_ec --board=veyron --ro
    verify ec is alive and version is reported as veyron
    
    Change-Id: I1f4bd562c0ab55360a2160a753ad8ad9b58f8c47
    Signed-off-by: Alexandru Stan <amstan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/207270
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>

---
+++ board/veyron/board.c
@@ -0,0 +1,66 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Veyron board-specific configuration */
+
+#include "battery.h"
+#include "chipset.h"
+#include "common.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "keyboard_raw.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "power.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "registers.h"
+#include "spi.h"
+#include "task.h"
+#include "util.h"
+#include "timer.h"
+#include "charger.h"
+
+#define GPIO_KB_INPUT  (GPIO_INPUT | GPIO_PULL_UP | GPIO_INT_BOTH)
+#define GPIO_KB_OUTPUT GPIO_ODR_HIGH
+
+#include "gpio_list.h"
+
+/* Pins with alternate functions */
+const struct gpio_alt_func gpio_alt_funcs[] = {
+	{GPIO_A, 0x00f0, GPIO_ALT_SPI,   MODULE_SPI, GPIO_DEFAULT},
+	{GPIO_A, 0x0600, GPIO_ALT_USART, MODULE_UART, GPIO_DEFAULT},
+	{GPIO_B, 0x00c0, GPIO_ALT_I2C,	 MODULE_I2C, GPIO_DEFAULT},
+};
+const int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
+
+/* power signal list.  Must match order of enum power_signal. */
+const struct power_signal_info power_signal_list[] = {
+	{GPIO_SOC1V8_XPSHOLD, 1, "XPSHOLD"},
+	{GPIO_SUSPEND_L,      0, "SUSPEND#_ASSERTED"},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"master", I2C_PORT_MASTER, 100},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
+const struct pwm_t pwm_channels[] = {
+	{STM32_TIM(2), STM32_TIM_CH(3),
+	 PWM_CONFIG_ACTIVE_LOW, GPIO_LED_POWER_L},
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+/**
+ * Discharge battery when on AC power for factory test.
+ */
+int board_discharge_on_ac(int enable)
+{
+	return charger_discharge_on_ac(enable);
+}
