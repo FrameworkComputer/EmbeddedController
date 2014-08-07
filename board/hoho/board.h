e1ff1a3caef94bef3c2ea9e8036aa784be26ef1b
Todd Broch <tbroch@chromium.org>
2014-08-07 14:06:43 -0700

    hoho: Initial board configuration.
    Create necessary boiler plate files for USB type-C to HDMI dongle.
    
    BRANCH=none
    BUG=chrome-os-partner:31192
    TEST=manual
    1. Compiles & can program via fruitpie.
    2. Can access uart (w/ rework for tx/rx).
    3. Can drive HDMI capable monitor.
    
    test details ...
    
    Programming:
    ------------
       # connect fruitpie + fruitpie std-adapter(red) with u-USB (CN3) & type-A
       # (CN1) both to host that will run servod.
       FPIE_PORT=9993
       sudo servod -p 0x5009 --port $FPIE_PORT
    
       dut-control --port $FPIE_PORT "ec_uart_cmd:gpioset USB_C_5V_EN 1"
       dut-control --port $FPIE_PORT "ec_uart_cmd:usbmux usb"
       util/flash_ec --board=hoho
    
    Configuring samus for DPout on both ports:
    ------------------------------------------
    
        sudo servod -b samus
    
        for port 0 1; do
            dut-control "usbpd_uart_cmd:pd ${PORT} dualrole off"
            dut-control "usbpd_uart_cmd:gpioset USB_C${PORT}_CHARGE_EN_L 1"
            dut-control "usbpd_uart_cmd:typec ${PORT} dp"
            dut-control "usbpd_uart_cmd:gpioset USB_C${PORT}_5V_EN 1"
            dut-control "usbpd_uart_cmd:typec ${PORT}"
        done
    Change-Id: I39bbe1e347d1cfd777b68f3fdac6c5c6dd22800d
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/212523
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

---
+++ board/hoho/board.h
@@ -0,0 +1,52 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Twinkie dongle configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
+/* the UART console is on USART1 (PA9/PA10) */
+#define CONFIG_UART_CONSOLE 1
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+#define CONFIG_ADC
+#define CONFIG_BOARD_PRE_INIT
+#define CONFIG_I2C
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_TASK_PROFILING
+
+/* I2C ports configuration */
+#define I2C_PORT_MASTER 0
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK_PD_RX 1
+#define TIM_CLOCK32 2
+#define TIM_ADC     3
+
+#include "gpio_signal.h"
+
+/* ADC signal */
+enum adc_channel {
+	ADC_CH_CC1_PD = 0,
+	/* Number of ADC channels */
+	ADC_CH_COUNT
+};
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
