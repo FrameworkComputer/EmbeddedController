adbd5a0c9a6749468b7c085029cf5e5a41595dbf
Alec Berg <alecaberg@chromium.org>
2014-08-11 12:06:03 -0700

    firefly: default to requesting 20V
    Change default to request 20V.
    
    BUG=none
    BRANCH=none
    TEST=Load onto a firefly. Plug in a zinger, make sure 20V LED
    lights up. Press other buttons to make sure we can change the
    requested voltage to 5V and 12V.
    
    Change-Id: I3a3e63a6d0e4f01b9fe7c730468da75ad6783f41
    Signed-off-by: Alec Berg <alecaberg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/211872
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- board/firefly/usb_pd_policy.c
+++ board/firefly/usb_pd_policy.c
@@ -35,7 +35,7 @@ const uint32_t pd_snk_pdo[] = {
 const int pd_snk_pdo_cnt = ARRAY_SIZE(pd_snk_pdo);
 
 /* Desired voltage requested as a sink (in millivolts) */
-static unsigned select_mv = 5000;
+static unsigned select_mv = 20000;
 
 int pd_choose_voltage(int cnt, uint32_t *src_caps, uint32_t *rdo)
 {
