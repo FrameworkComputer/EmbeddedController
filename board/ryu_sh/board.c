f5b9f2d64119ec1f56a7fc9d8a503ebe2a5af5d8
Gwendal Grignou <gwendal@chromium.org>
2014-08-21 10:03:33 -0700

    First drop of ryu sensor hub file
    For building a basic image for the Ryu Sensor Hub.
    
    BUG=chrome-os-partner:30801
    TEST=uart work, i2c master finds device, pin with EC works.
    BRANCH=ToT
    Change-Id: I6f8c6fa550da91eabf8b21452684d2de410611b9
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/210755
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Reviewed-by: Alec Berg <alecaberg@chromium.org>

---
+++ board/ryu_sh/board.c
@@ -0,0 +1,32 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* ryu sensor hub configuration */
+
+#include "common.h"
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "registers.h"
+#include "task.h"
+#include "util.h"
+
+#include "gpio_list.h"
+
+/* Initialize board. */
+static void board_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"master", I2C_PORT_MASTER, 100,
+		GPIO_MASTER_I2C_SCL, GPIO_MASTER_I2C_SDA},
+	{"slave",  I2C_PORT_SLAVE, 100,
+		GPIO_SLAVE_I2C_SCL, GPIO_SLAVE_I2C_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
---
+++ board/ryu_sh/board.h
@@ -0,0 +1,50 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* ryu sensor board configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
+/* the UART console is on USART2 (PA2/PA3) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 2
+
+/* By default, enable all console messages  */
+#define CC_DEFAULT     CC_ALL
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+#define CONFIG_I2C
+#undef  CONFIG_LID_SWITCH
+#define CONFIG_VBOOT_HASH
+#undef CONFIG_WATCHDOG_HELP
+
+/* I2C ports configuration */
+#define I2C_PORT_MASTER 1
+#define I2C_PORT_SLAVE  0
+#define I2C_PORT_EC I2C_PORT_SLAVE
+#define I2C_PORT_ACCEL I2C_PORT_MASTER
+#define I2C_PORT_COMPASS I2C_PORT_MASTER
+
+/* slave address for host commands */
+#ifdef HAS_TASK_HOSTCMD
+#define CONFIG_HOSTCMD_I2C_SLAVE_ADDR 0x3e
+#endif
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK32 2
+#define TIM_ADC     3
+
+#include "gpio_signal.h"
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
