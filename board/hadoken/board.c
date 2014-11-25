22f61c45275df8367735eec938863afcc5875791
Myles Watson <mylesgw@chromium.org>
2014-11-25 15:12:50 -0800

    hadoken: Enable I2C.
    BUG=chrome-os-partner:34477
    BRANCH=none
    TEST=Used console commands to test access to the fuel gauge.
    
    Signed-off-by: Myles Watson <mylesgw@chromium.org>
    
    Change-Id: Iefcbbfe073dcffa3bfe6224ea8210f39fee563e1
    Reviewed-on: https://chromium-review.googlesource.com/234287
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Tested-by: Myles Watson <mylesgw@chromium.org>
    Reviewed-by: Alec Berg <alecaberg@chromium.org>
    Commit-Queue: Myles Watson <mylesgw@chromium.org>

--- board/hadoken/board.c
+++ board/hadoken/board.c
@@ -4,9 +4,17 @@
  */
 #include "gpio.h"
 #include "registers.h"
+#include "i2c.h"
 #include "util.h"
 
 
 /* To define the gpio_list[] instance. */
 #include "gpio_list.h"
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"master", 0 /*Port*/, 100 /*Kbps*/, GPIO_MCU_SCL, GPIO_MCU_SDA},
+};
+
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
--- board/hadoken/board.h
+++ board/hadoken/board.h
@@ -15,12 +15,27 @@
 #undef CONFIG_WATCHDOG
 #undef CONFIG_LID_SWITCH
 
+#define CONFIG_I2C
+#define CONFIG_I2C_DEBUG
 
 /*
  *  nRF51 board specific configuration.
  */
 #define NRF51_UART_TX_PIN 25
 #define NRF51_UART_RX_PIN 29
+
+#define NRF51_TWI_PORT 0
+#define NRF51_TWI0_SCL_PIN 23
+#define NRF51_TWI0_SDA_PIN 22
+
+#define NRF51_TWI0_SCL_GPIO MCU_SCL
+#define NRF51_TWI0_SDA_GPIO MCU_SDA
+
+#define NRF51_TWI_SCL_PIN(port)   NRF51_TWI0_SCL_PIN
+#define NRF51_TWI_SDA_PIN(port)   NRF51_TWI0_SDA_PIN
+#define NRF51_TWI_FREQ(port)      NRF51_TWI_100KBPS
+#define NRF51_TWI_PPI_CHAN(port)  0
+
 
 #include "gpio_signal.h"
 
