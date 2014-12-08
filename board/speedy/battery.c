3c780a915c6294fe5b3e9bde227585c562a65c85
ZhengShunQian <zhengsq@rock-chips.com>
2014-12-08 14:54:29 +0800

    Speedy: add initial speedy support
    Copy from Jerry for now.
    
    BUG=chrome-os-partner:33269
    TEST=emerge-veyron_speedy chromeos-ec
    BRANCH=None
    
    Change-Id: I8375e1e23de5dbae0d1c579770d88e81223debce
    Signed-off-by: ZhengShunQian <zhengsq@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/233795
    Reviewed-by: Katie Roberts-Hoffman <katierh@chromium.org>

---
+++ board/speedy/battery.c
@@ -0,0 +1,54 @@
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Battery pack vendor provided charging profile
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+#include "console.h"
+#include "gpio.h"
+#include "host_command.h"
+#include "util.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHUTDOWN_DATA	0x0010
+
+static const struct battery_info info = {
+	.voltage_max    = 8400,		/* mV */
+	.voltage_normal = 7400,
+	.voltage_min    = 6000,
+	.precharge_current  = 256,	/* mA */
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 45,
+	.charging_min_c       = 0,
+	.charging_max_c       = 45,
+	.discharging_min_c    = 0,
+	.discharging_max_c    = 60,
+};
+
+const struct battery_info *battery_get_info(void)
+{
+	return &info;
+}
+
+static int cutoff(void)
+{
+	int rv;
+
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+
+	if (rv != EC_SUCCESS)
+		return rv;
+
+	return sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+}
+
+
+int board_cut_off_battery(void)
+{
+	return cutoff();
+}
+
