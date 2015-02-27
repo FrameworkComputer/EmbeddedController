858cba298caa7047a76d477691ef1fcf62a3ac68
Myles Watson <mylesgw@chromium.org>
2015-02-27 10:23:01 -0800

    hadoken: Remove I2C and add ADC
    As of EVT2, remove I2C (TWI).
    Use the pins for analog sensing of the battery level.
    
    BUG=None
    BRANCH=None
    TEST=Sense the battery level and use the serial port.
    
    Change-Id: I3d5d2401e61e6e7d203e933d2566f6f3cd65c054
    Signed-off-by: Myles Watson <mylesgw@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/361546
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Levi Oliver <levio@google.com>

--- board/hadoken/board.h
+++ board/hadoken/board.h
@@ -15,43 +15,15 @@
 #undef CONFIG_WATCHDOG
 #undef CONFIG_LID_SWITCH
 
-#define CONFIG_I2C
-#define CONFIG_I2C_MASTER
-#define CONFIG_I2C_DEBUG
-
 /*
  *  nRF51 board specific configuration.
  */
-#define NRF51_UART_TX_PIN 25
-#define NRF51_UART_RX_PIN 29
-
-#define NRF51_TWI_PORT 0
-#define NRF51_TWI0_SCL_PIN 23
-#define NRF51_TWI0_SDA_PIN 22
-
-#define NRF51_TWI0_SCL_GPIO MCU_SCL
-#define NRF51_TWI0_SDA_GPIO MCU_SDA
-
-#define NRF51_TWI_SCL_PIN(port)   NRF51_TWI0_SCL_PIN
-#define NRF51_TWI_SDA_PIN(port)   NRF51_TWI0_SDA_PIN
-#define NRF51_TWI_FREQ(port)      NRF51_TWI_100KBPS
-#define NRF51_TWI_PPI_CHAN(port)  0
+#define NRF51_UART_TX_PIN 24
+#define NRF51_UART_RX_PIN 28
 
 #define BATTERY_VOLTAGE_MAX         4425 /* mV */
 #define BATTERY_VOLTAGE_NORMAL      3800 /* mV */
 #define BATTERY_VOLTAGE_MIN         3000 /* mV */
-
-#define CONFIG_BATTERY_BQ27621
-#define BQ27621_CHEM_ID             0x0354 /* 4.35V maximum charge */
-#define BQ27621_TOGGLE_POWER_MIN    0
-
-#define BQ27621_DESIGN_CAPACITY     120 /* mAh */
-#define BQ27621_DESIGN_ENERGY       444 /* mWh */
-#define BQ27621_TERMINATE_VOLTAGE   BATTERY_VOLTAGE_MIN /* mV */
-#define BQ27621_TAPER_CURRENT       6   /* mA */
-
-#define I2C_PORT_BATTERY NRF51_TWI_PORT
-
 
 #include "gpio_signal.h"
 
