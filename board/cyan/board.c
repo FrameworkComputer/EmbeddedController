157543b287b0391502f82b943722f8770dabc356
Vijay Hiremath <vijay.p.hiremath@intel.com>
2015-05-27 11:19:27 -0700

    Cyan: Correct the accelerometer orientation matrices
    BUG=chrome-os-partner:40177
    BRANCH=none
    TEST=Verified the lid angle from accelinfo console command
         by rotating the lid.
    
    Change-Id: Ib91258285990942f0307ae8b29983f588e972747
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/274850
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/cyan/board.c
+++ board/cyan/board.c
@@ -97,15 +97,15 @@ struct kxcj9_data g_kxcj9_data[2];
 /* Four Motion sensors */
 /* Matrix to rotate accelrator into standard reference frame */
 const matrix_3x3_t base_standard_ref = {
-	{-1,  0,  0},
-	{ 0, -1,  0},
-	{ 0,  0, -1}
+	{ 0,  FLOAT_TO_FP(1),  0},
+	{FLOAT_TO_FP(-1),  0,  0},
+	{ 0,  0,  FLOAT_TO_FP(1)}
 };
 
 const matrix_3x3_t lid_standard_ref = {
-	{ 0,  1,  0},
-	{-1,  0,  0},
-	{ 0,  0, -1}
+	{FLOAT_TO_FP(-1),  0,  0},
+	{ 0, FLOAT_TO_FP(-1),  0},
+	{ 0,  0,  FLOAT_TO_FP(1)}
 };
 
 struct motion_sensor_t motion_sensors[] = {
@@ -144,18 +144,18 @@ const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 /* Define the accelerometer orientation matrices. */
 const struct accel_orientation acc_orient = {
-	/* Hinge aligns with y axis. */
+	/* Hinge aligns with x axis. */
 	.rot_hinge_90 = {
-		{ 1,  0,  0},
-		{ 0,  1,  0},
-		{ 0,  0,  1}
+		{ FLOAT_TO_FP(1),  0,  0},
+		{ 0,  0,  FLOAT_TO_FP(1)},
+		{ 0, FLOAT_TO_FP(-1),  0}
 	},
 	.rot_hinge_180 = {
-		{ 1,  0,  0},
-		{ 0,  1,  0},
-		{ 0,  0,  1}
+		{ FLOAT_TO_FP(1),  0,  0},
+		{ 0, FLOAT_TO_FP(-1),  0},
+		{ 0,  0, FLOAT_TO_FP(-1)}
 	},
-	.hinge_axis = {0, 1, 0},
+	.hinge_axis = {1, 0, 0},
 };
 
 /*
