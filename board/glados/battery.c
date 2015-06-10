9c35b4840067ba67e5314c67a160604abe758055
Alec Berg <alecaberg@chromium.org>
2015-06-10 10:04:14 -0700

    glados: add battery cutoff functionality
    Add battery cutoff functionality to glados.
    
    BUG=none
    BRANCH=none
    TEST=use console cutoff command to verify that it cuts off
    battery and plug in zinger to verify that it comes out of it
    when AC is applied.
    
    Change-Id: I71ae113c398278ad9f67f3a4cedf6885754a5a72
    Signed-off-by: Alec Berg <alecaberg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/276690
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- board/glados/battery.c
+++ board/glados/battery.c
@@ -6,6 +6,13 @@
  */
 
 #include "battery.h"
+#include "battery_smart.h"
+#include "i2c.h"
+#include "util.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define PARAM_CUT_OFF_LOW  0x10
+#define PARAM_CUT_OFF_HIGH 0x00
 
 /* Battery info for BQ40Z55 */
 static const struct battery_info info = {
@@ -24,4 +31,24 @@ static const struct battery_info info = {
 const struct battery_info *battery_get_info(void)
 {
 	return &info;
+}
+
+int board_cut_off_battery(void)
+{
+	int rv;
+	uint8_t buf[3];
+
+	/* Ship mode command must be sent twice to take effect */
+	buf[0] = SB_MANUFACTURER_ACCESS & 0xff;
+	buf[1] = PARAM_CUT_OFF_LOW;
+	buf[2] = PARAM_CUT_OFF_HIGH;
+
+	i2c_lock(I2C_PORT_BATTERY, 1);
+	rv = i2c_xfer(I2C_PORT_BATTERY, BATTERY_ADDR, buf, 3, NULL, 0,
+		      I2C_XFER_SINGLE);
+	rv |= i2c_xfer(I2C_PORT_BATTERY, BATTERY_ADDR, buf, 3, NULL, 0,
+		       I2C_XFER_SINGLE);
+	i2c_lock(I2C_PORT_BATTERY, 0);
+
+	return rv;
 }
