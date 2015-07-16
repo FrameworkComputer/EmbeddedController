a82b97962b014595915af01845d225f8e07e36f9
Gwendal Grignou <gwendal@chromium.org>
2015-07-16 13:39:53 -0700

    test: temporary disable motion_lid test.
    The test became flaky because of CL:284616.
    Disable it while it is being investigated.
    
    BRANCH=smaug
    BUG=chrome-os-partner:42855
    TEST=Check motion_lid test is not ran anymore.
    
    Change-Id: I2160dcda65e478817791859a19cdb51e88296259
    Reviewed-on: https://chromium-review.googlesource.com/286180
    Reviewed-by: Alec Berg <alecaberg@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- test/build.mk
+++ test/build.mk
@@ -38,7 +38,7 @@ test-list-host=mutex pingpong utils kb_scan kb_mkbp lid_sw power_button hooks
 test-list-host+=thermal flash queue kb_8042 extpwr_gpio console_edit system
 test-list-host+=sbs_charging host_command
 test-list-host+=bklight_lid bklight_passthru interrupt timer_dos button
-test-list-host+=motion_lid math_util sbs_charging_v2 battery_get_params_smart
+test-list-host+=math_util sbs_charging_v2 battery_get_params_smart
 test-list-host+=lightbar inductive_charging usb_pd fan charge_manager
 test-list-host+=charge_ramp
 
