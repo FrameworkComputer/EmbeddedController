51afeae605e7604559ef6f199d97ed8112d42e12
Vijay Hiremath <vijay.p.hiremath@intel.com>
2015-10-15 16:48:03 -0700

    kunimitsu: update accelerometer standard reference matrix
    This is based on kunimitsu fab 4 close chassis system.
    
    BUG=none
    BRANCH=none
    TEST=Using the "accelinfo" console command verified the
         lid angle by rotating the lid & base.
    
    Change-Id: Ia0491b52fda74b066120f3e2173b73fd1282c9cb
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/306185
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/kunimitsu/board.c
+++ board/kunimitsu/board.c
@@ -206,14 +206,14 @@ struct l3gd20_data g_l3gd20h_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 const matrix_3x3_t base_standard_ref = {
-	{ 0,  FLOAT_TO_FP(1),  0},
+	{ 0, FLOAT_TO_FP(-1),  0},
 	{FLOAT_TO_FP(-1),  0,  0},
-	{ 0,  0,  FLOAT_TO_FP(1)}
+	{ 0,  0,  FLOAT_TO_FP(-1)}
 };
 
 const matrix_3x3_t lid_standard_ref = {
-	{FLOAT_TO_FP(-1),  0,  0},
-	{ 0, FLOAT_TO_FP(-1),  0},
+	{FLOAT_TO_FP(-1), 0,  0},
+	{ 0, FLOAT_TO_FP(1),  0},
 	{ 0,  0, FLOAT_TO_FP(-1)}
 };
 
