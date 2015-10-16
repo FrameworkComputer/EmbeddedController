47ff569b68a0219c0eb8b0fd5e9796863abc97c3
Kevin K Wong <kevin.k.wong@intel.com>
2015-10-15 21:27:20 -0700

    kunimitsu: enable USB-A port power control
    dumb mode is used since kunimitsu ec only have control over
    the two usb-a ports' power enable/disable functionality.
    
    BUG=none
    BRANCH=none
    TEST=verified with "ectool usbchargemode <port 0-based> <0=dis, 4=en>"
         and check with DMM to see if +5V is toggled when enabled/disabled.
    
    Change-Id: I122c99b4067a873313b892715078188e800b979a
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/307476
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- board/kunimitsu/board.c
+++ board/kunimitsu/board.c
@@ -528,8 +528,6 @@ void board_set_charge_limit(int charge_ma)
 /* Enable touchpad on chipset startup so that it can wake the system */
 static void board_chipset_startup(void)
 {
-	gpio_set_level(GPIO_USB1_ENABLE, 1);
-	gpio_set_level(GPIO_USB2_ENABLE, 1);
 	gpio_set_level(GPIO_ENABLE_TOUCHPAD, 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup,
@@ -538,8 +536,6 @@ DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup,
 /* Disable touchpad on chipset shutdown as it is no longer useful */
 static void board_chipset_shutdown(void)
 {
-	gpio_set_level(GPIO_USB1_ENABLE, 0);
-	gpio_set_level(GPIO_USB2_ENABLE, 0);
 	gpio_set_level(GPIO_ENABLE_TOUCHPAD, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown,
