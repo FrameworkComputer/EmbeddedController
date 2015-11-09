d50fda3f4e3c507183aaf4d5768bd40239dd46f8
Ryan Zhang <Ryan.Zhang@quantatw.com>
2015-11-09 11:22:57 +0800

    Lars: increase RO image size
    Following kunimitsu setting of
    	https://chromium-review.googlesource.com/#/c/310581/
    
    BUG=None
    BRANCH=lars
    TEST=`make buildall -j`
    
    Change-Id: I4e58c581f7d3dfe4a6064a6d339806d543f41e23
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/311920
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/lars/board.h
+++ board/lars/board.h
@@ -62,7 +62,7 @@
 #define CONFIG_POWER_SHUTDOWN_PAUSE_IN_S5
 /* All data won't fit in data RAM.  So, moving boundary slightly. */
 #undef CONFIG_RO_SIZE
-#define CONFIG_RO_SIZE (100 * 1024)
+#define CONFIG_RO_SIZE (104 * 1024)
 #define CONFIG_SCI_GPIO GPIO_PCH_SCI_L
 /* We're space constrained on lars, so reduce the UART TX buffer size. */
 #undef CONFIG_UART_TX_BUF_SIZE
