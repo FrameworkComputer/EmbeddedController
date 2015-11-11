b60d19198aaf0af2cbe92e9327e1eb09c52573f1
Shawn Nematbakhsh <shawnn@google.com>
2015-11-11 13:10:00 -0800

    snoball: Initial board commit
    BUG=chrome-os-partner:47522
    BRANCH=None
    TEST=Compile only
    
    Change-Id: I588733c0f34239a2b3eb36a8810ccfddd8ee98ca
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/312250
    Commit-Ready: Shawn N <shawnn@chromium.org>
    Tested-by: Shawn N <shawnn@chromium.org>
    Reviewed-by: Alec Berg <alecaberg@chromium.org>

---
+++ board/snoball/board.c
@@ -0,0 +1,71 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Snoball board configuration */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "common.h"
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "registers.h"
+#include "task.h"
+#include "usb_pd.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+
+void tcpc_alert_event(enum gpio_signal signal)
+{
+	/* Exchange status with TCPCs */
+	host_command_pd_send_status(PD_CHARGE_NO_CHANGE);
+}
+
+#include "gpio_list.h"
+
+const struct i2c_port_t i2c_ports[] = {
+	{"tcpc-a", STM32_I2C1_PORT, 1000, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"tcpc-b", STM32_I2C2_PORT, 1000, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+#define FUSB302_I2C_SLAVE_ADDR 0x44
+
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{STM32_I2C1_PORT, FUSB302_I2C_SLAVE_ADDR},
+	{STM32_I2C2_PORT, FUSB302_I2C_SLAVE_ADDR},
+	/* TODO: Verify secondary slave addr, or use i2c mux */
+	{STM32_I2C2_PORT, FUSB302_I2C_SLAVE_ADDR + 2},
+};
+
+/* ADC channels */
+const struct adc_t adc_channels[] = {
+	/* Current sensing. Converted to mA (6600mV/4096). */
+	[ADC_C0_CS] = {"C0_CS", 6600, 4096, 0, STM32_AIN(0)},
+	[ADC_C1_CS] = {"C1_CS", 6600, 4096, 0, STM32_AIN(1)},
+	[ADC_C2_CS] = {"C2_CS", 6600, 4096, 0, STM32_AIN(2)},
+	/* Voltage sensing. Converted to mV (40000mV/4096). */
+	[ADC_C0_VS] = {"C0_VS", 40000, 4096, 0, STM32_AIN(3)},
+	[ADC_C1_VS] = {"C1_VS", 40000, 4096, 0, STM32_AIN(4)},
+	[ADC_C2_VS] = {"C2_VS", 40000, 4096, 0, STM32_AIN(5)},
+	[ADC_VBUCK] = {"VBUCK",  40000, 4096, 0, STM32_AIN(8)},
+	/* TODO: Check characteristics of thermistor circuit */
+	[ADC_TEMP] = {"TEMP", 3300, 4096, 0, STM32_AIN(9)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+static void board_init(void)
+{
+	gpio_enable_interrupt(GPIO_TCPC1_INT);
+	gpio_enable_interrupt(GPIO_TCPC2_INT);
+	gpio_enable_interrupt(GPIO_TCPC3_INT);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+void board_reset_pd_mcu(void)
+{
+}
---
+++ board/snoball/board.h
@@ -0,0 +1,88 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Snoball board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
+/* the UART console is on USART1 (PA9/PA10) */
+#define CONFIG_UART_CONSOLE 1
+
+#define CONFIG_USB_POWER_DELIVERY
+#define CONFIG_USB_PD_ALT_MODE
+/* TODO: Consider disabling PD communication in RO */
+#define CONFIG_USB_PD_COMM_ENABLED 1
+#define CONFIG_USB_PD_CUSTOM_VDM
+#define CONFIG_USB_PD_DYNAMIC_SRC_CAP
+#define CONFIG_USB_PD_LOGGING
+#define CONFIG_USB_PD_LOG_SIZE 1024
+#define CONFIG_USB_PD_PORT_COUNT 3
+#define CONFIG_USB_PD_TCPM_FUSB302
+
+#define CONFIG_ADC
+#define CONFIG_HW_CRC
+#define CONFIG_I2C
+#define CONFIG_I2C_MASTER
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_WATCHDOG_HELP
+
+/* USB configuration */
+#define CONFIG_USB_PID 0x5019
+#define CONFIG_USB_BCD_DEV 0x0001 /* v 0.01 */
+
+#define CONFIG_HIBERNATE
+#define CONFIG_HIBERNATE_WAKEUP_PINS STM32_PWR_CSR_EWUP6
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK_MSB 3
+#define TIM_CLOCK_LSB 14
+#define TIM_ADC 1
+
+#include "gpio_signal.h"
+
+/* ADC signals */
+enum adc_channel {
+	ADC_C0_CS,
+	ADC_C1_CS,
+	ADC_C2_CS,
+	ADC_C0_VS,
+	ADC_C1_VS,
+	ADC_C2_VS,
+	ADC_VBUCK,
+	ADC_TEMP,
+	/* Number of ADC channels */
+	ADC_CH_COUNT
+};
+
+enum board_src_cap {
+	SRC_CAP_5V = 0,
+	SRC_CAP_12V,
+	SRC_CAP_20V,
+};
+
+#define PD_DEFAULT_STATE PD_STATE_SRC_DISCONNECTED
+
+/* delay necessary for the voltage transition on the power supply */
+/* TODO: Tune these parameters appropriately for snoball */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  50000 /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 50000 /* us */
+
+void board_reset_pd_mcu(void);
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
