119916761c23c5d2c9c31689e0b1ee445bb57c37
Shawn Nematbakhsh <shawnn@chromium.org>
2016-01-11 10:33:38 -0800

    sb_firmware_update: Use any available host protocol
    Don't restrict host protocol to LPC since newer boards only support
    COMM_DEV.
    
    BUG=chrome-os-partner:44052
    TEST=Verify SB FW update is successful on Ultima.
    BRANCH=None
    
    Change-Id: I64f0c3ff278fe464cc71661454734c4cac5e88a9
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/321330
    Commit-Ready: Shawn N <shawnn@chromium.org>
    Tested-by: Shawn N <shawnn@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- util/ec_sb_firmware_update.c
+++ util/ec_sb_firmware_update.c
@@ -742,7 +742,7 @@ void usage(char *argv[])
 
 int main(int argc, char *argv[])
 {
-	int rv = 0, interfaces = COMM_LPC;
+	int rv = 0, interfaces = COMM_ALL;
 	int op = OP_UNKNOWN;
 	uint8_t val = 0;
 
