d0523f15d2c20ceb26c4b5c4c3e097d8df15a845
Shawn Nematbakhsh <shawnn@chromium.org>
2016-03-05 13:10:12 -0800

    kevin: Initial board commit
    Initial EC board support for kevin, a npcx5m5g part with SPI host
    interface.
    
    BUG=chrome-os-partner:50819
    BRANCH=None
    TEST=`make BOARD=kevin`
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I0d878edd7e8c7f59cfcb8e16637a5b589552eba9
    Reviewed-on: https://chromium-review.googlesource.com/330856
    Commit-Ready: Shawn N <shawnn@chromium.org>
    Tested-by: Shawn N <shawnn@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>

---
+++ board/kevin/board.c
@@ -0,0 +1,134 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "backlight.h"
+#include "button.h"
+#include "common.h"
+#include "driver/tcpm/fusb302.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "keyboard_scan.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "registers.h"
+#include "shi_chip.h"
+#include "switch.h"
+#include "timer.h"
+#include "thermal.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+
+static void tcpc_alert_event(enum gpio_signal signal)
+{
+	/* Exchange status with TCPCs */
+	host_command_pd_send_status(PD_CHARGE_NO_CHANGE);
+}
+
+#include "gpio_list.h"
+
+/******************************************************************************/
+/* ADC channels. Must be in the exactly same order as in enum adc_channel. */
+const struct adc_t adc_channels[] = {
+	[ADC_BOARD_ID] = {
+		"BOARD_ID", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0 },
+	[ADC_PP900_AP] = {
+		"PP900_AP", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0 },
+	[ADC_PP1200_LPDDR] = {
+		"PP1200_LPDDR", NPCX_ADC_CH2, ADC_MAX_VOLT, ADC_READ_MAX+1, 0 },
+	[ADC_PPVAR_CLOGIC] = {
+		"PPVAR_CLOGIC",
+		NPCX_ADC_CH3, ADC_MAX_VOLT, ADC_READ_MAX+1, 0 },
+	[ADC_PPVAR_LOGIC] = {
+		"PPVAR_LOGIC", NPCX_ADC_CH4, ADC_MAX_VOLT, ADC_READ_MAX+1, 0 },
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+/******************************************************************************/
+/* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
+const struct pwm_t pwm_channels[] = {
+	[PWM_CH_KBLIGHT] = { 2, 0, 10000 },
+	[PWM_CH_LED] =     { 3, PWM_CONFIG_DSLEEP_CLK, 100 },
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+/******************************************************************************/
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"tcpc0",   NPCX_I2C_PORT0_0, 1000, GPIO_I2C0_SCL0, GPIO_I2C0_SDA0},
+	{"tcpc1",   NPCX_I2C_PORT0_1, 1000, GPIO_I2C0_SCL1, GPIO_I2C0_SDA1},
+	{"sensors", NPCX_I2C_PORT1,   1000, GPIO_I2C1_SCL,  GPIO_I2C1_SDA},
+	{"charger", NPCX_I2C_PORT2,    400, GPIO_I2C2_SCL,  GPIO_I2C2_SDA},
+	{"battery", NPCX_I2C_PORT3,    400, GPIO_I2C3_SCL,  GPIO_I2C3_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/******************************************************************************/
+/* Wake-up pins for hibernate */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_POWER_BUTTON_L,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+/******************************************************************************/
+/* Keyboard scan setting */
+struct keyboard_scan_config keyscan_config = {
+	.output_settle_us = 40,
+	.debounce_down_us = 6 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 1500,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = SECOND,
+	.actual_key_mask = {
+		0x14, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0xa4, 0xff, 0xf6, 0x55, 0xfa, 0xc8  /* full set */
+	},
+};
+
+const struct button_config buttons[CONFIG_BUTTON_COUNT] = {
+	{"Volume Down", KEYBOARD_BUTTON_VOLUME_DOWN, GPIO_VOLUME_DOWN_L,
+	 30 * MSEC, 0},
+	{"Volume Up", KEYBOARD_BUTTON_VOLUME_UP, GPIO_VOLUME_UP_L,
+	 30 * MSEC, 0},
+};
+
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{I2C_PORT_TCPC0, FUSB302_I2C_SLAVE_ADDR},
+	{I2C_PORT_TCPC1, FUSB302_I2C_SLAVE_ADDR},
+};
+
+void board_reset_pd_mcu(void)
+{
+}
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_L))
+		status |= PD_STATUS_TCPC_ALERT_0;
+	if (!gpio_get_level(GPIO_USB_C1_PD_INT_L))
+		status |= PD_STATUS_TCPC_ALERT_1;
+
+	return status;
+}
+
+int board_set_active_charge_port(int charge_port)
+{
+	/* TODO: Select proper charge port through BD99955 regs. */
+	ASSERT(charge_port != 1);
+	return EC_SUCCESS;
+}
+
+void board_set_charge_limit(int charge_ma)
+{
+	/* TODO: Add support for BD99955 charger. */
+}
---
+++ board/kevin/board.h
@@ -0,0 +1,115 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Configuration for Nuvoton M4 EB */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Optional modules */
+#define CONFIG_ADC
+#define CONFIG_PWM
+#define CONFIG_HOSTCMD_SPS /* Used in ARM-based platform for host interface */
+
+#define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands for testing */
+
+/* Optional features */
+#define CONFIG_BUTTON_COUNT        2
+#define CONFIG_FLASH_SIZE          0x00080000 /* 512KB spi flash */
+#define CONFIG_I2C
+#define CONFIG_I2C_MASTER
+#define CONFIG_KEYBOARD_BOARD_CONFIG
+#define CONFIG_KEYBOARD_PROTOCOL_MKBP /* Instead of 8042 protocol of keyboard */
+#define CONFIG_POWER_BUTTON
+#define CONFIG_PWM_KBLIGHT
+/* TODO: Verify W25Q40 protect regs are compatible with W25X40 */
+#define CONFIG_SPI_FLASH_W25X40
+#define CONFIG_VBOOT_HASH
+
+/* USB PD config */
+#define CONFIG_CHARGE_MANAGER
+#define CONFIG_USB_POWER_DELIVERY
+#define CONFIG_USB_PD_ALT_MODE
+#define CONFIG_USB_PD_ALT_MODE_DFP
+#define CONFIG_USB_PD_CUSTOM_VDM
+#define CONFIG_USB_PD_DUAL_ROLE
+#define CONFIG_USB_PD_LOGGING
+#define CONFIG_USB_PD_LOG_SIZE 512
+#define CONFIG_USB_PD_PORT_COUNT 2
+#define CONFIG_USB_PD_TCPM_FUSB302
+#define CONFIG_USB_PD_TCPM_VBUS
+/* TODO: Enable TRY_SRC */
+#undef CONFIG_USB_PD_TRY_SRC
+
+#define CONFIG_BATTERY_SMART
+#define CONFIG_BATTERY_PRESENT_GPIO GPIO_EC_BATT_PRES_L
+
+/* TODO: Allow higher voltage charging */
+#define PD_OPERATING_POWER_MW 15000
+#define PD_MAX_POWER_MW       15000
+#define PD_MAX_CURRENT_MA     3000
+#define PD_MAX_VOLTAGE_MV     5000
+
+/* TODO: determine the following board specific type-C power constants */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  30000  /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
+
+/* start as a sink in case we have no other power supply/battery */
+#define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
+
+/* Optional features for test commands */
+#define CONFIG_CMD_TASKREADY
+#define CONFIG_CMD_STACKOVERFLOW
+#define CONFIG_CMD_JUMPTAGS
+#define CONFIG_CMD_FLASH
+#define CONFIG_CMD_SPI_FLASH
+#define CONFIG_CMD_SCRATCHPAD
+
+#define CONFIG_UART_HOST                0
+
+/* Optional feature - used by nuvoton */
+#define NPCX_UART_MODULE2    1 /* 0:GPIO10/11 1:GPIO64/65 as UART */
+#define NPCX_JTAG_MODULE2    0 /* 0:GPIO21/17/16/20 1:GPIOD5/E2/D4/E5 as JTAG*/
+#define NPCX_TACH_SEL2       0 /* 0:GPIO40/A4 1:GPIO93/D3 as TACH */
+
+/* Optional for testing */
+#undef  CONFIG_PECI
+#undef  CONFIG_PSTORE
+#undef  CONFIG_LOW_POWER_IDLE           /* Deep Sleep Support */
+
+#define I2C_PORT_TCPC0    NPCX_I2C_PORT0_0
+#define I2C_PORT_TCPC1    NPCX_I2C_PORT0_1
+#define I2C_PORT_CHARGER  NPCX_I2C_PORT2
+#define I2C_PORT_BATTERY  NPCX_I2C_PORT3
+
+#ifndef __ASSEMBLER__
+
+enum adc_channel {
+	/* No VBUS ADC channel on kevin */
+	ADC_VBUS = -1,
+	/* Real ADC channels begin here */
+	ADC_BOARD_ID = 0,
+	ADC_PP900_AP,
+	ADC_PP1200_LPDDR,
+	ADC_PPVAR_CLOGIC,
+	ADC_PPVAR_LOGIC,
+	ADC_CH_COUNT
+};
+
+enum pwm_channel {
+	PWM_CH_KBLIGHT,
+	PWM_CH_LED,
+	/* Number of PWM channels */
+	PWM_CH_COUNT
+};
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+void board_reset_pd_mcu(void);
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
