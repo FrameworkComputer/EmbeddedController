c65700730d07dfd6813b5c74c6bbece218d8c3d7
nagendra modadugu <ngm@google.com>
2016-04-20 14:09:52 -0700

    Add the memcmp() function prototype to builtin/string.h
    Add memcmp() to builtin/string.h so that portable third_party
    code may build without glibc.
    
    BRANCH=none
    BUG=chrome-os-partner:43025,chrome-os-partner:47524
    TEST=none
    Signed-off-by: nagendra modadugu <ngm@google.com>
    
    Change-Id: Id52c9c76fceac94bf1998958b43f42ad5d5298d3
    Reviewed-on: https://chromium-review.googlesource.com/339878
    Commit-Ready: Nagendra Modadugu <ngm@google.com>
    Tested-by: Nagendra Modadugu <ngm@google.com>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Bill Richardson <wfrichar@google.com>

--- builtin/string.h
+++ builtin/string.h
@@ -10,6 +10,7 @@
 
 #include <stddef.h>
 
+int memcmp(const void *s1, const void *s2, size_t len);
 void *memcpy(void *dest, const void *src, size_t len);
 void *memset(void *dest, int c, size_t len);
 
