28827163c57390278a54ee10153c064cd6c18133
nagendra modadugu <ngm@google.com>
2016-04-22 14:35:07 -0700

    Move include/byteorder.h -> builtin/endian.h
    Move endian routines to builtin/ so that portable third_party
    code may build without glibc.
    
    BRANCH=none
    BUG=chrome-os-partner:43025,chrome-os-partner:47524
    TEST=none
    
    Change-Id: Icb900d1e9c56dc68ec1ef4b536ebc9dcf6ebcd69
    Signed-off-by: nagendra modadugu <ngm@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/340432
    Commit-Ready: Nagendra Modadugu <ngm@google.com>
    Tested-by: Nagendra Modadugu <ngm@google.com>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>

---
+++ builtin/endian.h
@@ -0,0 +1,38 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EC_BUILTIN_ENDIAN_H
+#define __EC_BUILTIN_ENDIAN_H
+
+#include <stdint.h>
+
+/*
+ * Functions to convert byte order in various sized big endian integers to
+ * host byte order. Note that the code currently does not require functions
+ * for converting little endian integers.
+ */
+#if (__BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__)
+
+static inline uint16_t be16toh(uint16_t in)
+{
+	return __builtin_bswap16(in);
+}
+static inline uint32_t be32toh(uint32_t in)
+{
+	return __builtin_bswap32(in);
+}
+static inline uint64_t be64toh(uint64_t in)
+{
+	return __builtin_bswap64(in);
+}
+
+#define htobe16 be16toh
+#define htobe32 be32toh
+#define htobe64 be64toh
+
+#endif  /* __BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__ */
+
+
+#endif  /* __EC_BUILTIN_ENDIAN_H */
--- include/byteorder.h
+++ include/byteorder.h
@@ -6,33 +6,6 @@
 #ifndef __EC_INCLUDE_BYTEORDER_H
 #define __EC_INCLUDE_BYTEORDER_H
 
-#include <stdint.h>
-
-/*
- * Functions to convert byte order in various sized big endian integers to
- * host byte order. Note that the code currently does not require functions
- * for converting little endian integers.
- */
-#if (__BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__)
-
-static inline uint16_t be16toh(uint16_t in)
-{
-	return __builtin_bswap16(in);
-}
-static inline uint32_t be32toh(uint32_t in)
-{
-	return __builtin_bswap32(in);
-}
-static inline uint64_t be64toh(uint64_t in)
-{
-	return __builtin_bswap64(in);
-}
-
-#define htobe16 be16toh
-#define htobe32 be32toh
-#define htobe64 be64toh
-
-#endif  /* __BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__ */
-
+#include <endian.h>
 
 #endif  /* __EC_INCLUDE_BYTEORDER_H */
