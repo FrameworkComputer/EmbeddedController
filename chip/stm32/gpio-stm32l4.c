4e0c89c23a826f1d22bbd7f16bcd66536bf792f1
Daisuke Nojiri <dnojiri@chromium.org>
2016-07-25 16:11:06 -0700

    stm32l4: Enable extended interrupts (EXTI)
    BUG=none
    BRANCH=none
    TEST=Validated by CTS timer test (up-coming)
    
    Change-Id: I9c23e7dbfab779dc4e847fa5c9b93bee484e55e2
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/363007
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Chris Chen <twothreecc@google.com>

--- chip/stm32/gpio-stm32l4.c
+++ chip/stm32/gpio-stm32l4.c
@@ -7,7 +7,11 @@
 
 #include "clock.h"
 #include "common.h"
+#include "gpio.h"
+#include "hooks.h"
 #include "registers.h"
+#include "task.h"
+#include "util.h"
 
 void gpio_enable_clocks(void)
 {
@@ -22,5 +26,27 @@ void gpio_enable_clocks(void)
 	/* Delay 1 AHB clock cycle after the clock is enabled */
 	clock_wait_bus_cycles(BUS_AHB, 1);
 }
+
+static void gpio_init(void)
+{
+	/* Enable IRQs now that pins are set up */
+	task_enable_irq(STM32_IRQ_EXTI0);
+	task_enable_irq(STM32_IRQ_EXTI1);
+	task_enable_irq(STM32_IRQ_EXTI2);
+	task_enable_irq(STM32_IRQ_EXTI3);
+	task_enable_irq(STM32_IRQ_EXTI4);
+	task_enable_irq(STM32_IRQ_EXTI9_5);
+	task_enable_irq(STM32_IRQ_EXTI15_10);
+
+}
+DECLARE_HOOK(HOOK_INIT, gpio_init, HOOK_PRIO_DEFAULT);
+
+DECLARE_IRQ(STM32_IRQ_EXTI0, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI1, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI2, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI3, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI4, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI9_5, gpio_interrupt, 1);
+DECLARE_IRQ(STM32_IRQ_EXTI15_10, gpio_interrupt, 1);
 
 #include "gpio-f0-l.c"
