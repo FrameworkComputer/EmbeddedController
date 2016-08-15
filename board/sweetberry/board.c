65ba93af93d5f05cc00a1acaafc2f172f578a722
Nick Sanders <nsanders@chromium.org>
2016-08-15 13:43:12 -0700

    sweetberry: add build target for sweetberry
    sweetberry is an stm32f446 based power monitoring
    board, with 48 channels of INA current sense chips
    
    BUG=chromium:608039
    TEST=boots
    BRANCH=none
    
    Change-Id: If263bcee3a648ba3605f991999d481b7a0e2a1db
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/370718
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ board/sweetberry/board.c
@@ -0,0 +1,57 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Sweetberry board configuration */
+
+#include "common.h"
+#include "dma.h"
+#include "ec_version.h"
+#include "gpio.h"
+#include "gpio_list.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "registers.h"
+#include "stm32-dma.h"
+#include "task.h"
+#include "util.h"
+
+
+#define GPIO_SET_HS(bank, number)	\
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number) * 2)))
+
+void board_config_post_gpio_init(void)
+{
+	/* We use MCO2 clock passthrough to provide a clock to USB HS */
+	gpio_config_module(MODULE_MCO, 1);
+	/* GPIO PC9 to high speed */
+	GPIO_SET_HS(C,  9);
+
+	/* Set USB GPIO to high speed */
+	GPIO_SET_HS(A, 11);
+	GPIO_SET_HS(A, 12);
+
+	GPIO_SET_HS(C,  3);
+	GPIO_SET_HS(C,  2);
+	GPIO_SET_HS(C,  0);
+	GPIO_SET_HS(A,  5);
+
+	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 13);
+	GPIO_SET_HS(B, 12);
+	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 10);
+	GPIO_SET_HS(B,  1);
+	GPIO_SET_HS(B,  0);
+	GPIO_SET_HS(A,  3);
+
+	/* Set I2C GPIO to HS */
+	GPIO_SET_HS(B,  6);
+	GPIO_SET_HS(B,  7);
+	GPIO_SET_HS(F,  1);
+	GPIO_SET_HS(F,  0);
+	GPIO_SET_HS(A,  8);
+	GPIO_SET_HS(B,  4);
+	GPIO_SET_HS(C,  6);
+	GPIO_SET_HS(C,  7);
+}
---
+++ board/sweetberry/board.h
@@ -0,0 +1,54 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Sweetberry configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Use external clock */
+#define CONFIG_STM32_CLOCK_HSE_HZ 24000000
+
+#define CONFIG_BOARD_POST_GPIO_INIT
+
+/* Enable console recasting of GPIO type. */
+#define CONFIG_CMD_GPIO_EXTENDED
+
+/* The UART console can be on flax USART3 (PC10/PC11) */
+/* The UART console can be on header USART4 (PA0/PA1) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 4
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 4096
+/* Don't waste precious DMA channels on console. */
+#undef CONFIG_UART_TX_DMA
+#undef CONFIG_UART_RX_DMA
+
+#define CONFIG_UART_TX_REQ_CH 4
+#define CONFIG_UART_RX_REQ_CH 4
+
+/* This is not actually a Chromium EC so disable some features. */
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_WATCHDOG
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK32 5
+
+#include "gpio_signal.h"
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
