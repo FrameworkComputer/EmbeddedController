71c9cc6f3f9673b7aab927a915fb8ed15be520ac
Gwendal Grignou <gwendal@chromium.org>
2016-08-20 15:33:24 -0700

    driver: l3gd20h: remove unnecessary test.
    rotate by identity matrix is fast.
    
    BRANCH=none
    BUG=none
    TEST=compile
    
    Change-Id: Ia9fccb81ebc4c0be7c9d80823b3c2f76aa2275d0
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/373723
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- driver/gyro_l3gd20h.c
+++ driver/gyro_l3gd20h.c
@@ -366,8 +366,7 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 	for (i = X; i <= Z; i++)
 		v[i] = ((int16_t)((raw[i * 2 + 1] << 8) | raw[i * 2]));
 
-	if (s->rot_standard_ref)
-		rotate(v, *s->rot_standard_ref, v);
+	rotate(v, *s->rot_standard_ref, v);
 
 	/* apply offset in the device coordinates */
 	range = get_range(s);
