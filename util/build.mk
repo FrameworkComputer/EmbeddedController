107cb0df63efb527a470553b0a4e6f1f1ab17eb6
Nicolas Boichat <drinkcat@google.com>
2016-09-01 14:35:48 +0800

    util: Add ec_parse_panicinfo tool to parse binary panicinfo
    To be able to parse binary panicinfo from feedback reports, we need
    a host tool:
     - Move panicinfo generic parsing functions to a separate C file
     - Create a new host utility to parse panicinfo
    
    BRANCH=none
    BUG=chromium:643062
    TEST=base64 -d | ec_parse_panicinfo
    
    Change-Id: Idd8560a2894f270d0ab3a9f654c333135759e57f
    Reviewed-on: https://chromium-review.googlesource.com/379639
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- util/build.mk
+++ util/build.mk
@@ -6,7 +6,8 @@
 # Host tools build
 #
 
-host-util-bin=ectool lbplay stm32mon ec_sb_firmware_update lbcc
+host-util-bin=ectool lbplay stm32mon ec_sb_firmware_update lbcc \
+	ec_parse_panicinfo
 build-util-bin=ec_uartd iteflash
 ifeq ($(CHIP),npcx)
 build-util-bin+=ecst
@@ -15,7 +16,9 @@ endif
 comm-objs=$(util-lock-objs:%=lock/%) comm-host.o comm-dev.o
 comm-objs+=comm-lpc.o comm-i2c.o misc_util.o
 
-ectool-objs=ectool.o ectool_keyscan.o ec_flash.o $(comm-objs)
+ectool-objs=ectool.o ectool_keyscan.o ec_flash.o ec_panicinfo.o $(comm-objs)
 ec_sb_firmware_update-objs=ec_sb_firmware_update.o $(comm-objs) misc_util.o
 ec_sb_firmware_update-objs+=powerd_lock.o
 lbplay-objs=lbplay.o $(comm-objs)
+
+ec_parse_panicinfo-objs=ec_parse_panicinfo.o ec_panicinfo.o
---
+++ util/ec_panicinfo.h
@@ -0,0 +1,19 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EC_PANICINFO_H
+#define EC_PANICINFO_H
+
+#include "panic.h"
+
+/**
+ * Prints panic information to stdout.
+ *
+ * @param pdata  Panic information to print
+ * @return 0 if success or non-zero error code if error.
+ */
+int parse_panic_info(const struct panic_data *pdata);
+
+#endif /* EC_PANICINFO_H */
