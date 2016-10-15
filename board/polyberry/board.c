6e03484c018310539196ee3cfceeeae806592670
Nick Sanders <nsanders@chromium.org>
2016-10-14 21:12:14 -0700

    polyberry: add initial board build
    This supports gpio initialization only.
    
    BUG=None
    TEST=Successfully checked console and available GPIO on sweetberry
    BRANCH=None
    
    Change-Id: Id50f66652b05c25a8c79ce2938fa161a944d93b8
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/399643
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

---
+++ board/polyberry/board.c
@@ -0,0 +1,106 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Polyberry board configuration */
+
+#include "common.h"
+#include "dma.h"
+#include "ec_version.h"
+#include "gpio.h"
+#include "gpio_list.h"
+#include "hooks.h"
+#include "registers.h"
+#include "stm32-dma.h"
+#include "task.h"
+#include "update_fw.h"
+#include "usb_descriptor.h"
+#include "util.h"
+#include "usb_dwc_hw.h"
+#include "usb_dwc_console.h"
+#include "usb_dwc_update.h"
+
+/******************************************************************************
+ * Define the strings used in our USB descriptors.
+ */
+const void *const usb_strings[] = {
+	[USB_STR_DESC]		= usb_string_desc,
+	[USB_STR_VENDOR]	= USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT]	= USB_STRING_DESC("Polyberry"),
+	[USB_STR_SERIALNO]	= USB_STRING_DESC("1234-a"),
+	[USB_STR_VERSION]	= USB_STRING_DESC(CROS_EC_VERSION32),
+	[USB_STR_CONSOLE_NAME]	= USB_STRING_DESC("Polyberry EC Shell"),
+	[USB_STR_UPDATE_NAME]	= USB_STRING_DESC("Firmware update"),
+};
+
+BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
+
+struct dwc_usb usb_ctl = {
+	.ep = {
+		&ep0_ctl,
+		&ep_console_ctl,
+		&usb_update_ep_ctl,
+	},
+	.speed = USB_SPEED_FS,
+	.phy_type = USB_PHY_ULPI,
+	.dma_en = 1,
+	.irq = STM32_IRQ_OTG_HS,
+};
+
+/******************************************************************************
+ * Support firmware upgrade over USB. We can update whichever section is not
+ * the current section.
+ */
+
+/*
+ * This array defines possible sections available for the firmware update.
+ * The section which does not map the current executing code is picked as the
+ * valid update area. The values are offsets into the flash space.
+ */
+const struct section_descriptor board_rw_sections[] = {
+	{CONFIG_RO_MEM_OFF,
+	 CONFIG_RO_MEM_OFF + CONFIG_RO_SIZE},
+	{CONFIG_RW_MEM_OFF,
+	 CONFIG_RW_MEM_OFF + CONFIG_RW_SIZE},
+};
+const struct section_descriptor * const rw_sections = board_rw_sections;
+const int num_rw_sections = ARRAY_SIZE(board_rw_sections);
+
+#define GPIO_SET_HS(bank, number)	\
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number) * 2)))
+
+void board_config_post_gpio_init(void)
+{
+	/* We use MCO2 clock passthrough to provide a clock to USB HS */
+	gpio_config_module(MODULE_MCO, 1);
+	/* GPIO PC9 to high speed */
+	GPIO_SET_HS(C,  9);
+
+	if (usb_ctl.phy_type == USB_PHY_ULPI)
+		gpio_set_level(GPIO_USB_MUX_SEL, 0);
+	else
+		gpio_set_level(GPIO_USB_MUX_SEL, 1);
+
+	/* Set USB GPIO to high speed */
+	GPIO_SET_HS(A, 11);
+	GPIO_SET_HS(A, 12);
+
+	GPIO_SET_HS(C,  3);
+	GPIO_SET_HS(C,  2);
+	GPIO_SET_HS(C,  0);
+	GPIO_SET_HS(A,  5);
+
+	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 13);
+	GPIO_SET_HS(B, 12);
+	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 10);
+	GPIO_SET_HS(B,  1);
+	GPIO_SET_HS(B,  0);
+	GPIO_SET_HS(A,  3);
+}
+
+static void board_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
---
+++ board/polyberry/board.h
@@ -0,0 +1,89 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Polyberry configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Use external clock */
+#define CONFIG_STM32_CLOCK_HSE_HZ 24000000
+
+#define CONFIG_BOARD_POST_GPIO_INIT
+
+/* Enable console recasting of GPIO type. */
+#define CONFIG_CMD_GPIO_EXTENDED
+
+/* The UART console is on test points USART3 (PC10/PC11) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 3
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 4096
+/* Don't waste precious DMA channels on console. */
+#undef CONFIG_UART_TX_DMA
+#undef CONFIG_UART_RX_DMA
+
+#define CONFIG_UART_TX_REQ_CH 4
+#define CONFIG_UART_RX_REQ_CH 4
+
+/* USB Configuration */
+#define CONFIG_USB
+#define CONFIG_USB_PID 0x5020
+#define CONFIG_USB_CONSOLE
+#define CONFIG_STREAM_USB
+#define CONFIG_USB_UPDATE
+
+#undef CONFIG_USB_MAXPOWER_MA
+#define CONFIG_USB_MAXPOWER_MA 100
+
+#define CONFIG_USB_SERIALNO
+#define DEFAULT_SERIALNO "Uninitialized"
+
+/* USB interface indexes (use define rather than enum to expand them) */
+#define USB_IFACE_CONSOLE	0
+#define USB_IFACE_UPDATE	1
+#define USB_IFACE_COUNT		2
+
+/* USB endpoint indexes (use define rather than enum to expand them) */
+#define USB_EP_CONTROL		0
+#define USB_EP_CONSOLE		1
+#define USB_EP_UPDATE		2
+#define USB_EP_COUNT		3
+
+/* This is not actually a Chromium EC so disable some features. */
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_WATCHDOG
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK32 5
+
+#include "gpio_signal.h"
+
+/* USB string indexes */
+enum usb_strings {
+	USB_STR_DESC = 0,
+	USB_STR_VENDOR,
+	USB_STR_PRODUCT,
+	USB_STR_SERIALNO,
+	USB_STR_VERSION,
+	USB_STR_CONSOLE_NAME,
+	USB_STR_UPDATE_NAME,
+	USB_STR_COUNT
+};
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
