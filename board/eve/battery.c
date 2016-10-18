a5393caeb591dce6ad14fe3580ce182f644a51eb
Duncan Laurie <dlaurie@chromium.org>
2016-10-17 19:00:39 -0700

    eve: Add new board
    Add support for eve P0 board with:
    
    - chip: npcx
    - pmic: bd999992GW
    - charger: bd99956
    - tcpc: 2x anx3429
    
    BUG=chrome-os-partner:58666
    BRANCH=none
    TEST=build and boot on eve board
    
    Change-Id: I69ff246e9f8197d5d50241e6a8fa4796f4c9bfda
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/400638

---
+++ board/eve/battery.c
@@ -0,0 +1,139 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Placeholder values for temporary battery pack.
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+#include "charge_state.h"
+#include "console.h"
+#include "ec_commands.h"
+#include "extpower.h"
+#include "util.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHUTDOWN_DATA	0x0010
+
+/* Battery info for proto */
+static const struct battery_info info = {
+	.voltage_max		= 8800,	/* mV */
+	.voltage_normal		= 7700,
+	.voltage_min		= 6000,
+	.precharge_current	= 64,	/* mA */
+	.start_charging_min_c	= 0,
+	.start_charging_max_c	= 46,
+	.charging_min_c		= 0,
+	.charging_max_c		= 60,
+	.discharging_min_c	= 0,
+	.discharging_max_c	= 60,
+};
+
+const struct battery_info *battery_get_info(void)
+{
+	return &info;
+}
+
+int board_cut_off_battery(void)
+{
+	int rv;
+
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	if (rv != EC_SUCCESS)
+		return EC_RES_ERROR;
+
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	return rv ? EC_RES_ERROR : EC_RES_SUCCESS;
+}
+
+enum battery_disconnect_state battery_get_disconnect_state(void)
+{
+	uint8_t data[6];
+	int rv;
+
+	/*
+	 * Take note if we find that the battery isn't in disconnect state,
+	 * and always return NOT_DISCONNECTED without probing the battery.
+	 * This assumes the battery will not go to disconnect state during
+	 * runtime.
+	 */
+	static int not_disconnected;
+
+	if (not_disconnected)
+		return BATTERY_NOT_DISCONNECTED;
+
+	if (extpower_is_present()) {
+		/* Check if battery charging + discharging is disabled. */
+		rv = sb_write(SB_MANUFACTURER_ACCESS,
+			      PARAM_OPERATION_STATUS);
+		if (rv)
+			return BATTERY_DISCONNECT_ERROR;
+
+		rv = sb_read_string(I2C_PORT_BATTERY, BATTERY_ADDR,
+				    SB_ALT_MANUFACTURER_ACCESS, data, 6);
+
+		if (rv || (~data[3] & (BATTERY_DISCHARGING_DISABLED |
+				       BATTERY_CHARGING_DISABLED))) {
+			not_disconnected = 1;
+			return BATTERY_NOT_DISCONNECTED;
+		}
+
+		/*
+		 * Battery is neither charging nor discharging. Verify that
+		 * we didn't enter this state due to a safety fault.
+		 */
+		rv = sb_write(SB_MANUFACTURER_ACCESS, PARAM_SAFETY_STATUS);
+		if (rv)
+			return BATTERY_DISCONNECT_ERROR;
+
+		rv = sb_read_string(I2C_PORT_BATTERY, BATTERY_ADDR,
+				    SB_ALT_MANUFACTURER_ACCESS, data, 6);
+
+		if (rv || data[2] || data[3] || data[4] || data[5])
+			return BATTERY_DISCONNECT_ERROR;
+
+		/*
+		 * Battery is present and also the status is initialized and
+		 * no safety fault, battery is disconnected.
+		 */
+		if (battery_is_present() == BP_YES)
+			return BATTERY_DISCONNECTED;
+	}
+	not_disconnected = 1;
+	return BATTERY_NOT_DISCONNECTED;
+}
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	const struct battery_info *batt_info;
+	/* battery temp in 0.1 deg C */
+	int bat_temp_c = curr->batt.temperature - 2731;
+
+	batt_info = battery_get_info();
+	/* Don't charge if outside of allowable temperature range */
+	if (bat_temp_c >= batt_info->charging_max_c * 10 ||
+	    bat_temp_c < batt_info->charging_min_c * 10) {
+		curr->requested_current = 0;
+		curr->requested_voltage = 0;
+		curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
+		curr->state = ST_IDLE;
+	}
+	return 0;
+}
+
+/* Customs options controllable by host command. */
+#define PARAM_FASTCHARGE (CS_PARAM_CUSTOM_PROFILE_MIN + 0)
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
---
+++ board/eve/board.c
@@ -0,0 +1,574 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Eve board-specific configuration */
+
+#include "adc_chip.h"
+#include "als.h"
+#include "bd99992gw.h"
+#include "board_config.h"
+#include "button.h"
+#include "charge_manager.h"
+#include "charge_state.h"
+#include "charge_ramp.h"
+#include "charger.h"
+#include "chipset.h"
+#include "console.h"
+#include "driver/als_isl29035.h"
+#include "driver/charger/bd9995x.h"
+#include "driver/tcpm/anx74xx.h"
+#include "driver/tcpm/tcpci.h"
+#include "driver/tcpm/tcpm.h"
+#include "driver/temp_sensor/bd99992gw.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "keyboard_scan.h"
+#include "lid_switch.h"
+#include "math_util.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "spi.h"
+#include "switch.h"
+#include "system.h"
+#include "task.h"
+#include "temp_sensor.h"
+#include "timer.h"
+#include "uart.h"
+#include "usb_charge.h"
+#include "usb_mux.h"
+#include "usb_pd.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+#include "espi.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+static void tcpc_alert_event(enum gpio_signal signal)
+{
+	if ((signal == GPIO_USB_C0_PD_INT_ODL) &&
+	    !gpio_get_level(GPIO_USB_C0_PD_RST_L))
+		return;
+	else if ((signal == GPIO_USB_C1_PD_INT_ODL) &&
+		 !gpio_get_level(GPIO_USB_C1_PD_RST_L))
+		return;
+
+#ifdef HAS_TASK_PDCMD
+	/* Exchange status with TCPCs */
+	host_command_pd_send_status(PD_CHARGE_NO_CHANGE);
+#endif
+}
+
+/*
+ * enable_input_devices() is called by the tablet_mode ISR, but changes the
+ * state of GPIOs, so its definition must reside after including gpio_list.
+ */
+static void enable_input_devices(void);
+DECLARE_DEFERRED(enable_input_devices);
+
+void tablet_mode_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&enable_input_devices_data, 0);
+}
+
+#include "gpio_list.h"
+
+/* power signal list.  Must match order of enum power_signal. */
+const struct power_signal_info power_signal_list[] = {
+	{GPIO_PCH_SLP_S0_L,	1, "SLP_S0_DEASSERTED"},
+	{VW_SLP_S3_L,		1, "SLP_S3_DEASSERTED"},
+	{VW_SLP_S4_L,		1, "SLP_S4_DEASSERTED"},
+	{GPIO_PCH_SLP_SUS_L,	1, "SLP_SUS_DEASSERTED"},
+	{GPIO_RSMRST_L_PGOOD,	1, "RSMRST_L_PGOOD"},
+	{GPIO_PMIC_DPWROK,	1, "PMIC_DPWROK"},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+/* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
+const struct pwm_t pwm_channels[] = {
+	[PWM_CH_KBLIGHT] = { 5, PWM_CONFIG_DSLEEP, 100 },
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+/* Hibernate wake configuration */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_AC_PRESENT,
+	GPIO_LID_OPEN,
+	GPIO_POWER_BUTTON_L,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+/* I2C port map */
+const struct i2c_port_t i2c_ports[]  = {
+	{"tcpc0",     I2C_PORT_TCPC0,   400, GPIO_I2C0_0_SCL, GPIO_I2C0_0_SDA},
+	{"tcpc1",     I2C_PORT_TCPC1,   400, GPIO_I2C0_1_SCL, GPIO_I2C0_1_SDA},
+	{"accelgyro", I2C_PORT_GYRO,    400, GPIO_I2C1_SCL,   GPIO_I2C1_SDA},
+	{"sensors",   I2C_PORT_ACCEL,   400, GPIO_I2C2_SCL,   GPIO_I2C2_SDA},
+	{"batt",      I2C_PORT_BATTERY, 100, GPIO_I2C3_SCL,   GPIO_I2C3_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* TCPC mux configuration */
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{I2C_PORT_TCPC0, 0x50, &anx74xx_tcpm_drv, TCPC_ALERT_ACTIVE_LOW},
+	{I2C_PORT_TCPC1, 0x50, &anx74xx_tcpm_drv, TCPC_ALERT_ACTIVE_LOW},
+};
+
+struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
+	{
+		.port_addr = 0,
+		.driver = &anx74xx_tcpm_usb_mux_driver,
+		.hpd_update = &anx74xx_tcpc_update_hpd_status,
+	},
+	{
+		.port_addr = 1,
+		.driver = &anx74xx_tcpm_usb_mux_driver,
+		.hpd_update = &anx74xx_tcpc_update_hpd_status,
+	},
+};
+
+/* called from anx74xx_set_power_mode() */
+void board_set_tcpc_power_mode(int port, int mode)
+{
+	switch (port) {
+	case 0:
+		if (mode) {
+			gpio_set_level(GPIO_USB_C0_TCPC_PWR, 1);
+			msleep(10);
+			gpio_set_level(GPIO_USB_C0_PD_RST_L, 1);
+		} else {
+			gpio_set_level(GPIO_USB_C0_PD_RST_L, 0);
+			msleep(1);
+			gpio_set_level(GPIO_USB_C0_TCPC_PWR, 0);
+		}
+		break;
+	case 1:
+		if (mode) {
+			gpio_set_level(GPIO_USB_C1_TCPC_PWR, 1);
+			msleep(10);
+			gpio_set_level(GPIO_USB_C1_PD_RST_L, 1);
+		} else {
+			gpio_set_level(GPIO_USB_C1_PD_RST_L, 0);
+			msleep(1);
+			gpio_set_level(GPIO_USB_C1_TCPC_PWR, 0);
+		}
+		break;
+	}
+}
+
+#ifdef CONFIG_USB_PD_TCPC_FW_VERSION
+void board_print_tcpc_fw_version(int port)
+{
+	int version;
+
+	if (!anx74xx_tcpc_get_fw_version(port, &version))
+		CPRINTS("TCPC p%d FW VER: 0x%x", port, version);
+}
+#endif
+
+void board_reset_pd_mcu(void)
+{
+	/* Assert reset */
+	gpio_set_level(GPIO_USB_C0_PD_RST_L, 0);
+	gpio_set_level(GPIO_USB_C1_PD_RST_L, 0);
+	msleep(1);
+	/* Disable power */
+	gpio_set_level(GPIO_USB_C0_TCPC_PWR, 0);
+	gpio_set_level(GPIO_USB_C1_TCPC_PWR, 0);
+	msleep(10);
+	/* Enable power */
+	gpio_set_level(GPIO_USB_C0_TCPC_PWR, 1);
+	gpio_set_level(GPIO_USB_C1_TCPC_PWR, 1);
+	msleep(10);
+	/* Deassert reset */
+	gpio_set_level(GPIO_USB_C0_PD_RST_L, 1);
+	gpio_set_level(GPIO_USB_C1_PD_RST_L, 1);
+}
+
+void board_tcpc_init(void)
+{
+	/* Only reset TCPC if not sysjump */
+	if (!system_jumped_to_this_image())
+		board_reset_pd_mcu();
+
+	/* Enable TCPC interrupts */
+	gpio_enable_interrupt(GPIO_USB_C0_PD_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_PD_INT_ODL);
+}
+DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_I2C+1);
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_ODL)) {
+		if (gpio_get_level(GPIO_USB_C0_PD_RST_L))
+			status |= PD_STATUS_TCPC_ALERT_0;
+	}
+
+	if (!gpio_get_level(GPIO_USB_C1_PD_INT_ODL)) {
+		if (gpio_get_level(GPIO_USB_C1_PD_RST_L))
+			status |= PD_STATUS_TCPC_ALERT_1;
+	}
+
+	return status;
+}
+
+const struct temp_sensor_t temp_sensors[] = {
+	{"Battery", TEMP_SENSOR_TYPE_BATTERY, charge_get_battery_temp, 0, 4},
+
+	/* These BD99992GW temp sensors are only readable in S0 */
+	{"Ambient", TEMP_SENSOR_TYPE_BOARD, bd99992gw_get_val,
+	 BD99992GW_ADC_CHANNEL_SYSTHERM0, 4},
+	{"Charger", TEMP_SENSOR_TYPE_BOARD, bd99992gw_get_val,
+	 BD99992GW_ADC_CHANNEL_SYSTHERM1, 4},
+	{"DRAM", TEMP_SENSOR_TYPE_BOARD, bd99992gw_get_val,
+	 BD99992GW_ADC_CHANNEL_SYSTHERM2, 4},
+	{"eMMC", TEMP_SENSOR_TYPE_BOARD, bd99992gw_get_val,
+	 BD99992GW_ADC_CHANNEL_SYSTHERM3, 4},
+};
+BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
+
+/* ALS instances. Must be in same order as enum als_id. */
+struct als_t als[] = {
+	{"ISL", isl29035_init, isl29035_read_lux, 5},
+};
+BUILD_ASSERT(ARRAY_SIZE(als) == ALS_COUNT);
+
+const struct button_config buttons[CONFIG_BUTTON_COUNT] = {
+	{"Volume Down", KEYBOARD_BUTTON_VOLUME_DOWN, GPIO_VOLUME_DOWN_L,
+	 30 * MSEC, 0},
+	{"Volume Up", KEYBOARD_BUTTON_VOLUME_UP, GPIO_VOLUME_UP_L,
+	 30 * MSEC, 0},
+};
+
+static void board_pmic_init(void)
+{
+	if (system_jumped_to_this_image())
+		return;
+
+	/* DISCHGCNT3 - enable 100 ohm discharge on V1.00A */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3e, 0x04);
+
+	/* Set CSDECAYEN / VCCIO decays to 0V at assertion of SLP_S0# */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x30, 0x4a);
+
+	/*
+	 * Set V100ACNT / V1.00A Control Register:
+	 * Nominal output = 1.0V.
+	 */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x37, 0x1a);
+
+	/*
+	 * Set V085ACNT / V0.85A Control Register:
+	 * Lower power mode = 0.7V.
+	 * Nominal output = 1.0V.
+	 */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x38, 0x7a);
+
+	/* VRMODECTRL - disable low-power mode for all rails */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3b, 0x1f);
+}
+DECLARE_HOOK(HOOK_INIT, board_pmic_init, HOOK_PRIO_DEFAULT);
+
+/* Initialize board. */
+static void board_init(void)
+{
+	/* Enable tablet mode interrupt for input device enable */
+	gpio_enable_interrupt(GPIO_TABLET_MODE_L);
+
+	/* Enable charger interrupts */
+	gpio_enable_interrupt(GPIO_CHARGER_INT_L);
+
+	/* Provide AC status to the PCH */
+	gpio_set_level(GPIO_PCH_ACOK, extpower_is_present());
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+#define MP2949_PAGE_SELECT	0x00	/* Select rail/page */
+#define MP2949_STORE_USER_ALL	0x15	/* Write settings to EEPROM */
+#define MP2949_MFR_TRANS_FAST	0xfa	/* Slew rate control */
+#define MP2949_FIXED_SLEW_RATE	0x0ac5	/* 40mV/uS */
+
+/*
+ * Workaround for P0 boards:
+ * Set voltage slew rate to 40mV/uS for all rails
+ */
+void board_before_rsmrst(int rsmrst)
+{
+	int rail, rate;
+	int fixed = 0;
+
+	/* Only trigger on RSMRST# deassertion */
+	if (!rsmrst)
+		return;
+
+	/* Only apply workaround to P0 boards */
+	if (system_get_board_version() != BOARD_VERSION_P0)
+		return;
+
+	i2c_lock(I2C_PORT_MP2949, 1);
+
+	for (rail = 2; rail >= 0; rail--) {
+		uint8_t buf[3];
+
+		/* Select register page for this rail */
+		buf[0] = MP2949_PAGE_SELECT;
+		buf[1] = rail;
+		i2c_xfer(I2C_PORT_MP2949, I2C_ADDR_MP2949,
+			 buf, 2, NULL, 0, I2C_XFER_SINGLE);
+
+		/* Check for workaround already applied */
+		buf[0] = MP2949_MFR_TRANS_FAST;
+		i2c_xfer(I2C_PORT_MP2949, I2C_ADDR_MP2949,
+			 buf, 1, buf+1, 2, I2C_XFER_SINGLE);
+		rate = ((int)buf[2] << 8) | buf[1];
+
+		if (rate == MP2949_FIXED_SLEW_RATE)
+			continue;
+		fixed = 1;
+
+		/* Set slew rate */
+		buf[0] = MP2949_MFR_TRANS_FAST;
+		buf[1] = MP2949_FIXED_SLEW_RATE & 0xff;
+		buf[2] = (MP2949_FIXED_SLEW_RATE >> 8) & 0xff;
+		i2c_xfer(I2C_PORT_MP2949, I2C_ADDR_MP2949,
+			 buf, 3, NULL, 0, I2C_XFER_SINGLE);
+
+		/* Store new settings (1 byte write, no data) */
+		buf[0] = MP2949_STORE_USER_ALL;
+		i2c_xfer(I2C_PORT_MP2949, I2C_ADDR_MP2949,
+			 buf, 1, NULL, 0, I2C_XFER_SINGLE);
+	}
+
+	i2c_lock(I2C_PORT_MP2949, 0);
+
+	CPRINTS("P0 board - IMVP8 workaround %sapplied",
+		fixed ? "" : "already ");
+}
+
+/**
+ * Buffer the AC present GPIO to the PCH.
+ */
+static void board_extpower(void)
+{
+	gpio_set_level(GPIO_PCH_ACOK, extpower_is_present());
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, board_extpower, HOOK_PRIO_DEFAULT);
+
+int pd_snk_is_vbus_provided(int port)
+{
+	enum bd9995x_charge_port bd9995x_port;
+
+	switch (port) {
+	case 0:
+	case 1:
+		bd9995x_port = bd9995x_pd_port_to_chg_port(port);
+		break;
+	default:
+		panic("Invalid charge port\n");
+		break;
+	}
+
+	return bd9995x_is_vbus_provided(bd9995x_port);
+}
+
+/**
+ * Set active charge port -- only one port can be active at a time.
+ *
+ * @param charge_port   Charge port to enable.
+ *
+ * Returns EC_SUCCESS if charge port is accepted and made active,
+ * EC_ERROR_* otherwise.
+ */
+int board_set_active_charge_port(int charge_port)
+{
+	enum bd9995x_charge_port bd9995x_port;
+	static int initialized;
+
+	/* charge port is a physical port */
+	int is_real_port = (charge_port >= 0 &&
+			    charge_port < CONFIG_USB_PD_PORT_COUNT);
+	/* check if we are source VBUS on the port */
+	int source = gpio_get_level(charge_port == 0 ? GPIO_USB_C0_5V_EN :
+						       GPIO_USB_C1_5V_EN);
+
+	if (is_real_port && source) {
+		CPRINTF("Skip enable p%d", charge_port);
+		return EC_ERROR_INVAL;
+	}
+
+	/*
+	 * Reject charge port disable if our battery is critical and we
+	 * have yet to initialize a charge port - continue to charge using
+	 * charger ROM / POR settings.
+	 */
+	if (!initialized &&
+	    charge_port == CHARGE_PORT_NONE &&
+	    charge_get_percent() < 2)
+		return -1;
+
+	CPRINTS("New chg p%d", charge_port);
+
+	switch (charge_port) {
+	case 0:
+	case 1:
+		bd9995x_port = bd9995x_pd_port_to_chg_port(charge_port);
+		break;
+	case CHARGE_PORT_NONE:
+		bd9995x_port = BD9995X_CHARGE_PORT_NONE;
+		break;
+	default:
+		panic("Invalid charge port\n");
+		break;
+	}
+
+	initialized = 1;
+
+	return bd9995x_select_input_port(bd9995x_port);
+}
+
+/**
+ * Set the charge limit based upon desired maximum.
+ *
+ * @param port          Port number.
+ * @param supplier      Charge supplier type.
+ * @param charge_ma     Desired charge limit (mA).
+ */
+void board_set_charge_limit(int port, int supplier, int charge_ma, int max_ma)
+{
+	/* Enable charging trigger by BC1.2 detection */
+	int bc12_enable = (supplier == CHARGE_SUPPLIER_BC12_CDP ||
+			   supplier == CHARGE_SUPPLIER_BC12_DCP ||
+			   supplier == CHARGE_SUPPLIER_BC12_SDP ||
+			   supplier == CHARGE_SUPPLIER_OTHER);
+
+	if (bd9995x_bc12_enable_charging(port, bc12_enable))
+		return;
+
+	charge_set_input_current_limit(MAX(charge_ma,
+					   CONFIG_CHARGER_INPUT_CURRENT));
+}
+
+/**
+ * Return whether ramping is allowed for given supplier
+ */
+int board_is_ramp_allowed(int supplier)
+{
+	/* Don't allow ramping in RO when write protected */
+	if (system_get_image_copy() != SYSTEM_IMAGE_RW
+		&& system_is_locked())
+		return 0;
+	else
+		return (supplier == CHARGE_SUPPLIER_BC12_DCP ||
+			supplier == CHARGE_SUPPLIER_BC12_SDP ||
+			supplier == CHARGE_SUPPLIER_BC12_CDP ||
+			supplier == CHARGE_SUPPLIER_OTHER);
+}
+
+/**
+ * Return the maximum allowed input current
+ */
+int board_get_ramp_current_limit(int supplier, int sup_curr)
+{
+	return bd9995x_get_bc12_ilim(supplier);
+}
+
+/**
+ * Return if board is consuming full amount of input current
+ */
+int board_is_consuming_full_charge(void)
+{
+	int chg_perc = charge_get_percent();
+
+	return chg_perc > 2 && chg_perc < 95;
+}
+
+/**
+ * Return if VBUS is sagging too low
+ */
+int board_is_vbus_too_low(enum chg_ramp_vbus_state ramp_state)
+{
+	return charger_get_vbus_level() < BD9995X_BC12_MIN_VOLTAGE;
+}
+
+/* Enable or disable input devices, based upon chipset state and tablet mode */
+static void enable_input_devices(void)
+{
+	int kb_enable = 1;
+	int tp_enable = 1;
+
+	/* Disable both TP and KB in tablet mode */
+	if (!gpio_get_level(GPIO_TABLET_MODE_L))
+		kb_enable = tp_enable = 0;
+	/* Disable TP if chipset is off */
+	else if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		tp_enable = 0;
+
+	keyboard_scan_enable(kb_enable, KB_SCAN_DISABLE_LID_ANGLE);
+	gpio_set_level(GPIO_ENABLE_TOUCHPAD, tp_enable);
+}
+
+/* Called on AP S5 -> S3 transition */
+static void board_chipset_startup(void)
+{
+	hook_call_deferred(&enable_input_devices_data, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup, HOOK_PRIO_DEFAULT);
+
+/* Called on AP S3 -> S5 transition */
+static void board_chipset_shutdown(void)
+{
+	hook_call_deferred(&enable_input_devices_data, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
+
+void board_hibernate_late(void)
+{
+	int i;
+	const uint32_t hibernate_pins[][2] = {
+		{GPIO_LID_OPEN, GPIO_INT_RISING | GPIO_PULL_DOWN},
+		/* Turn off LEDs in hibernate */
+		{GPIO_CHARGE_LED_1, GPIO_OUTPUT | GPIO_LOW},
+		{GPIO_CHARGE_LED_2, GPIO_OUTPUT | GPIO_LOW},
+		{GPIO_CHARGE_LED_3, GPIO_OUTPUT | GPIO_LOW},
+		{GPIO_CHARGE_LED_4, GPIO_OUTPUT | GPIO_LOW},
+		{GPIO_CHARGE_LED_5, GPIO_OUTPUT | GPIO_LOW},
+		{GPIO_CHARGE_LED_6, GPIO_OUTPUT | GPIO_LOW},
+		/*
+		 * BD99956 handles charge input automatically. We'll disable
+		 * charge output in hibernate. Charger will assert ACOK_OD
+		 * when VBUS or VCC are plugged in.
+		 */
+		{GPIO_USB_C0_5V_EN, GPIO_INPUT | GPIO_PULL_DOWN},
+		{GPIO_USB_C1_5V_EN, GPIO_INPUT | GPIO_PULL_DOWN},
+	};
+
+	/* Change GPIOs' state in hibernate for better power consumption */
+	for (i = 0; i < ARRAY_SIZE(hibernate_pins); ++i)
+		gpio_set_flags(hibernate_pins[i][0], hibernate_pins[i][1]);
+
+	gpio_config_module(MODULE_KEYBOARD_SCAN, 0);
+
+	/*
+	 * Calling gpio_config_module sets disabled alternate function pins to
+	 * GPIO_INPUT.  But to prevent keypresses causing leakage currents
+	 * while hibernating we want to enable GPIO_PULL_UP as well.
+	 */
+	gpio_set_flags_by_mask(0x2, 0x03, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_set_flags_by_mask(0x1, 0xFF, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_set_flags_by_mask(0x0, 0xE0, GPIO_INPUT | GPIO_PULL_UP);
+}
+
+void board_hibernate(void)
+{
+	/* Enable both the VBUS & VCC ports before entering PG3 */
+	bd9995x_select_input_port(BD9995X_CHARGE_PORT_BOTH);
+}
