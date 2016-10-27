e8ab026bac8960280d1dd63c532ee4aa31e9246c
Bruce <Bruce.Wan@quantatw.com>
2016-10-27 16:36:14 +0800

    Snappy: support LED behavior
    Support LED behavior.
    
    BUG=chrome-os-partner:58771, chrome-os-partner:58772
    BRANCH=None
    TEST=make buildall
    
    Signed-off-by: Bruce.Wan <Bruce.Wan@quantatw.com>
    Change-Id: Ieb323b5ff5e1096febef9948cf39b1fb22ff094a
    Reviewed-on: https://chromium-review.googlesource.com/403674
    Commit-Ready: Bruce Wan <Bruce.Wan@quantatw.com>
    Tested-by: Bruce Wan <Bruce.Wan@quantatw.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/snappy/led.c
+++ board/snappy/led.c
@@ -19,23 +19,15 @@
 #define BAT_LED_ON 0
 #define BAT_LED_OFF 1
 
-#define CRITICAL_LOW_BATTERY_PERCENTAGE 3
-#define LOW_BATTERY_PERCENTAGE 10
-
-#define LED_TOTAL_4SECS_TICKS 4
-#define LED_TOTAL_2SECS_TICKS 2
-#define LED_ON_1SEC_TICKS 1
-#define LED_ON_2SECS_TICKS 2
-
 const enum ec_led_id supported_led_ids[] = {
-			EC_LED_ID_BATTERY_LED};
+	EC_LED_ID_POWER_LED, EC_LED_ID_BATTERY_LED};
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
 enum led_color {
 	LED_OFF = 0,
-	LED_BLUE,
 	LED_AMBER,
+	LED_WHITE,
 	LED_COLOR_COUNT  /* Number of colors, not a color itself */
 };
 
@@ -43,16 +35,31 @@ static int led_set_color_battery(enum led_color color)
 {
 	switch (color) {
 	case LED_OFF:
-		gpio_set_level(GPIO_BAT_LED_BLUE, BAT_LED_OFF);
+		gpio_set_level(GPIO_BAT_LED_WHITE, BAT_LED_OFF);
 		gpio_set_level(GPIO_BAT_LED_AMBER, BAT_LED_OFF);
 		break;
-	case LED_BLUE:
-		gpio_set_level(GPIO_BAT_LED_BLUE, BAT_LED_ON);
+	case LED_WHITE:
+		gpio_set_level(GPIO_BAT_LED_WHITE, BAT_LED_ON);
 		gpio_set_level(GPIO_BAT_LED_AMBER, BAT_LED_OFF);
 		break;
 	case LED_AMBER:
-		gpio_set_level(GPIO_BAT_LED_BLUE, BAT_LED_OFF);
+		gpio_set_level(GPIO_BAT_LED_WHITE, BAT_LED_OFF);
 		gpio_set_level(GPIO_BAT_LED_AMBER, BAT_LED_ON);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+	return EC_SUCCESS;
+}
+
+static int led_set_color_power(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_set_level(GPIO_POWER_LED_WHITE, BAT_LED_OFF);
+		break;
+	case LED_WHITE:
+		gpio_set_level(GPIO_POWER_LED_WHITE, BAT_LED_ON);
 		break;
 	default:
 		return EC_ERROR_UNKNOWN;
@@ -62,7 +69,7 @@ static int led_set_color_battery(enum led_color color)
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	brightness_range[EC_LED_COLOR_BLUE] = 1;
+	brightness_range[EC_LED_COLOR_WHITE] = 1;
 	brightness_range[EC_LED_COLOR_AMBER] = 1;
 }
 
@@ -74,6 +81,9 @@ static int led_set_color(enum ec_led_id led_id, enum led_color color)
 	case EC_LED_ID_BATTERY_LED:
 		rv = led_set_color_battery(color);
 		break;
+	case EC_LED_ID_POWER_LED:
+		rv = led_set_color_power(color);
+		break;
 	default:
 		return EC_ERROR_UNKNOWN;
 	}
@@ -82,8 +92,8 @@ static int led_set_color(enum ec_led_id led_id, enum led_color color)
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
-	if (brightness[EC_LED_COLOR_BLUE] != 0)
-		led_set_color(led_id, LED_BLUE);
+	if (brightness[EC_LED_COLOR_WHITE] != 0)
+		led_set_color(led_id, LED_WHITE);
 	else if (brightness[EC_LED_COLOR_AMBER] != 0)
 		led_set_color(led_id, LED_AMBER);
 	else
@@ -95,80 +105,76 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 static void led_set_battery(void)
 {
 	static int battery_ticks;
-	static int suspend_ticks;
-	static int previous_state_suspend;
+	static int power_ticks;
 	uint32_t chflags = charge_get_flags();
+	int remaining_capacity;
+	int full_charge_capacity;
+	int permillage;
 
 	battery_ticks++;
-	suspend_ticks++;
+	power_ticks++;
+
+	remaining_capacity = *(int *)host_get_memmap(EC_MEMMAP_BATT_CAP);
+	full_charge_capacity = *(int *)host_get_memmap(EC_MEMMAP_BATT_LFCC);
+	permillage = !full_charge_capacity ? 0 :
+		(1000 * remaining_capacity) / full_charge_capacity;
+
 	switch (charge_get_state()) {
 	case PWR_STATE_CHARGE:
 		led_set_color_battery(LED_AMBER);
 		break;
 	case PWR_STATE_DISCHARGE:
-		/* Less than 3%, blink one second every two second */
-		if (!chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
-			charge_get_percent() < CRITICAL_LOW_BATTERY_PERCENTAGE)
+		/*
+		 * Blink white light (1 sec on, 1 sec off)
+		 * when battery capacity is less than 10%
+		 */
+		if (permillage < 100)
 			led_set_color_battery(
-				(battery_ticks % LED_TOTAL_2SECS_TICKS <
-				 LED_ON_1SEC_TICKS) ? LED_AMBER : LED_OFF);
-		/* Less than 10%, blink one second every four seconds */
-		else if (!chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
-			charge_get_percent() < LOW_BATTERY_PERCENTAGE)
-			led_set_color_battery(
-				(battery_ticks % LED_TOTAL_4SECS_TICKS <
-				 LED_ON_1SEC_TICKS) ? LED_AMBER : LED_OFF);
-		else {
-			if (chipset_in_state(CHIPSET_STATE_SUSPEND
-				| CHIPSET_STATE_STANDBY)) {
-				if (!previous_state_suspend)
-					suspend_ticks = 0;
-				/* Blink once every four seconds. */
-				led_set_color_battery(
-					(suspend_ticks % LED_TOTAL_4SECS_TICKS)
-					< LED_ON_1SEC_TICKS ?
-					LED_AMBER : LED_OFF);
-				previous_state_suspend = 1;
-				return;
-			}
-
-			if (chipset_in_state(CHIPSET_STATE_ON))
-				led_set_color_battery(LED_BLUE);
-			else
-				led_set_color_battery(LED_OFF);
-		}
+				(battery_ticks & 0x4) ? LED_WHITE : LED_OFF);
+		else
+			led_set_color_battery(LED_OFF);
 		break;
 	case PWR_STATE_ERROR:
 		led_set_color_battery(
-			(battery_ticks % LED_TOTAL_2SECS_TICKS <
-			 LED_ON_1SEC_TICKS) ? LED_AMBER : LED_OFF);
+			(battery_ticks & 0x2) ? LED_WHITE : LED_OFF);
 		break;
 	case PWR_STATE_CHARGE_NEAR_FULL:
-		led_set_color_battery(LED_BLUE);
+		led_set_color_battery(LED_WHITE);
 		break;
 	case PWR_STATE_IDLE: /* External power connected in IDLE */
 		if (chflags & CHARGE_FLAG_FORCE_IDLE)
 			led_set_color_battery(
-				(battery_ticks % LED_TOTAL_4SECS_TICKS <
-				 LED_ON_2SECS_TICKS) ? LED_AMBER : LED_BLUE);
+				(battery_ticks & 0x4) ? LED_AMBER : LED_OFF);
 		else
-			led_set_color_battery(LED_BLUE);
+			led_set_color_battery(LED_WHITE);
 		break;
 	default:
 		/* Other states don't alter LED behavior */
 		break;
 	}
-	previous_state_suspend = 0;
 }
 
-/* Called by hook task every 1 sec */
-static void led_second(void)
+static void led_set_power(void)
 {
-	/*
-	 * Reference board only has one LED, so overload it to act as both
-	 * power LED and battery LED.
-	 */
+	static int power_tick;
+
+	power_tick++;
+
+	if (chipset_in_state(CHIPSET_STATE_ON))
+		led_set_color_power(LED_WHITE);
+	else if (chipset_in_state(CHIPSET_STATE_SUSPEND))
+		led_set_color_power(
+			(power_tick & 0x4) ? LED_WHITE : LED_OFF);
+	else
+		led_set_color_power(LED_OFF);
+}
+
+/* Called by hook task every TICK */
+static void led_tick(void)
+{
 	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
 		led_set_battery();
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		led_set_power();
 }
-DECLARE_HOOK(HOOK_SECOND, led_second, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
