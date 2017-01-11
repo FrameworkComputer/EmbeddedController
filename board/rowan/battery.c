c54346f7830f9ee3607147cf32f541c4e1878503
Yidi Lin <yidi.lin@mediatek.com>
2017-01-11 10:32:04 +0800

    rowan: Add initial support for rowan
    Copied from elm with string updated.
    
    BRANCH=master
    BUG=chrome-os-partner:62673
    TEST=EC_FIRMWARE=rowan emerge-rowan chromeos-ec
    
    Change-Id: I424e9ea1cb1520766222eff3156da5f6edbcc2fd
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/427560
    Commit-Ready: Patrick Berny <pberny@chromium.org>
    Tested-by: Patrick Berny <pberny@chromium.org>
    Reviewed-by: Patrick Berny <pberny@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>

---
+++ board/rowan/battery.c
@@ -0,0 +1,47 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Battery pack vendor provided charging profile
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+#include "util.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHIP_MODE_REG	0x3a
+#define SB_SHUTDOWN_DATA	0xC574
+
+static const struct battery_info info = {
+	.voltage_max = 13200,
+	.voltage_normal = 11550,
+	.voltage_min = 9100,
+	/* Pre-charge values. */
+	.precharge_current = 256, /* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 60,
+};
+
+const struct battery_info *battery_get_info(void)
+{
+	return &info;
+}
+
+int board_cut_off_battery(void)
+{
+	int rv;
+
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+
+	if (rv != EC_SUCCESS)
+		return rv;
+
+	return sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+}
