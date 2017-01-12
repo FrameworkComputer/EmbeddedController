f4c99dfd27c6d95a516f748b15318b0a95c5d543
Ryan Zhang <ryan.zhang.quanta@gmail.com>
2017-01-12 17:59:16 +0800

    Electro: limit max input current for safety
    + Max = Max * 95%
    
    BUG=chrome-os-partner:61852
    BRANCH=reef
    TEST=Run fish tank + youtube + USB 0.9A * 2 +
         TypeC 3A * 1 + empty battery, input current
         from 3.033A become 2.92A
    
    Change-Id: I34563a011915c29972835f7d878cad29045d42f0
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/427485
    Commit-Ready: Benson Leung <bleung@chromium.org>
    Tested-by: Benson Leung <bleung@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/reef/board.c
+++ board/reef/board.c
@@ -608,6 +608,7 @@ void board_set_charge_limit(int port, int supplier, int charge_ma,
 	if (bd9995x_bc12_enable_charging(port, bc12_enable))
 		return;
 
+	charge_ma = (charge_ma * 95) / 100;
 	charge_set_input_current_limit(MAX(charge_ma,
 				   CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
 }
