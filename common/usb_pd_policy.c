eb5ab132163c31c4ae7c79d279af8d1763da42f9
philipchen <philipchen@google.com>
2017-01-31 11:46:46 -0800

    usb_pd_policy: make pd_set_vbus_discharge work with devices with one USB PD port
    We see compile error in case that pd_set_vbus_discharge is called
    when GPIO_USB_C1_DISCHARGE is not defined.
    
    BUG=chrome-os-partner:62207
    BRANCH=gru
    TEST=make buildall -j
    
    Change-Id: I17c324d26ee6bf94c13ec7e0f92b7352de602329
    Reviewed-on: https://chromium-review.googlesource.com/435458
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Philip Chen <philipchen@chromium.org>
    Tested-by: Philip Chen <philipchen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/435540
    Commit-Ready: Philip Chen <philipchen@chromium.org>
    Reviewed-by: Philip Chen <philipchen@chromium.org>

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -1021,8 +1021,12 @@ void pd_set_vbus_discharge(int port, int enable)
 	mutex_lock(&discharge_lock[port]);
 	enable &= !board_vbus_source_enabled(port);
 #ifdef CONFIG_USB_PD_DISCHARGE_GPIO
-	gpio_set_level(port ? GPIO_USB_C1_DISCHARGE :
-			      GPIO_USB_C0_DISCHARGE, enable);
+	if (!port)
+		gpio_set_level(GPIO_USB_C0_DISCHARGE, enable);
+#if CONFIG_USB_PD_PORT_COUNT > 1
+	else
+		gpio_set_level(GPIO_USB_C1_DISCHARGE, enable);
+#endif /* CONFIG_USB_PD_PORT_COUNT */
 #elif defined(CONFIG_USB_PD_DISCHARGE_TCPC)
 	tcpc_discharge_vbus(port, enable);
 #else
