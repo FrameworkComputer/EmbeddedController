f51fdf223dd846341a489b00c8f43db92a37ce37
Duncan Laurie <dlaurie@google.com>
2017-02-09 09:54:04 -0800

    eve: Revert trackpad interrupt changes
    The trackpad interrupt is input only to the EC and should not ever
    be driven from here.
    
    BUG=chrome-os-partner:58666
    BRANCH=none
    TEST=build and boot on eve p1
    
    Change-Id: I3ffa2ddb4990550b57c9191b5d721ab0ba206aca
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/439829

--- board/eve/board.c
+++ board/eve/board.c
@@ -573,7 +573,6 @@ void lid_angle_peripheral_enable(int enable)
 	if (tablet_get_mode() || chipset_in_state(CHIPSET_STATE_ANY_OFF))
 		enable = 0;
 	keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
-	gpio_set_level(GPIO_TRACKPAD_INT_L, !enable);
 }
 #endif
 
