81596da3d0143dea4ea8818976f3c3003387f927
Daisuke Nojiri <dnojiri@chromium.org>
2017-03-25 17:39:10 -0700

    Fizz: Add Fizz to flash_ec
    This patch also fixes years and board names in the comments.
    
    BUG=b:37271713
    BRANCH=none
    TEST=none
    
    Change-Id: Ib9595a7e091c70680333a02ba2fdde3f24c0f4e6
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/475210
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/fizz/board.c
+++ board/fizz/board.c
@@ -1,9 +1,9 @@
-/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Poppy board-specific configuration */
+/* Fizz board-specific configuration */
 
 #include "adc.h"
 #include "adc_chip.h"
