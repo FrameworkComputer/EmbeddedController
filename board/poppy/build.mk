0b077ad67173f11f8816b01213bc09f622326799
Nicolas Boichat <drinkcat@google.com>
2017-05-08 15:46:57 +0800

    poppy/soraka: Basic LED support
    This applies the simple rule: Charging port led is on, other
    is off. When charging, LED is amber, otherwise it's white.
    
    Open questions:
     - Do we want blinking on low battery? On which side(s)? In
       which AP states?
     - No blinking in S3? That's ok?
     - Need to add led blinking support for special debug mode
     - Recovery mode blinking does not work as LED is powered from
       a rail that is not on when AP is in S5.
    
    BRANCH=none
    BUG=b:37970194
    TEST=Charge from one side, led is first amber, then white when
         battery is full. Switch side, led behaves the same way.
    
    Change-Id: I0531d72cd621148c0d0cce57a32b7310792d9936
    Reviewed-on: https://chromium-review.googlesource.com/497372
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- board/poppy/build.mk
+++ board/poppy/build.mk
@@ -11,4 +11,5 @@ CHIP_VARIANT:=npcx5m6g
 
 board-y=board.o
 board-$(CONFIG_BATTERY_SMART)+=battery.o
+board-$(CONFIG_LED_COMMON)+=led.o
 board-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
---
+++ board/soraka/led.c
@@ -0,0 +1,1 @@
+../poppy/led.c
