d7f7f6931766472fab94726e82dc9b172f9c10a5
Duncan Laurie <dlaurie@google.com>
2017-07-21 01:10:48 -0700

    eve: PROCHOT behavior fixes
    1) Don't assert prochot by default, this signal has an inverter
    and driving it high is asserting prochot until it gets deasserted
    during S3->S0 transition.
    
    2) Clear PMIC power source interrupts in pmic_init in case they
    were inadvertently set by misbehaving PMIC.  The BATTSHORT event
    that matters for PROCHOT assertion is masked so it should never
    be set anyway, but I found a system where it was set. This register
    does not seem to get cleared by an LDO reset so PROCHOT was stuck on
    until battery cutoff.
    
    BUG=b:63913242
    BRANCH=eve
    TEST=manual testing on Eve unit
    
    Change-Id: I5c7f7ca528160972f65bd714deea98357fa43a53
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/579904
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

--- board/eve/board.c
+++ board/eve/board.c
@@ -374,6 +374,9 @@ static void board_pmic_init(void)
 
 	/* VRMODECTRL - disable low-power mode for all rails */
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3b, 0x1f);
+
+	/* Clear power source events */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x04, 0xff);
 }
 DECLARE_HOOK(HOOK_INIT, board_pmic_init, HOOK_PRIO_DEFAULT);
 
