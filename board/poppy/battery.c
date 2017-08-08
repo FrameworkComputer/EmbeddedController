a4b536c0f3fef40a1e764477dc2edfb233321c55
Furquan Shaikh <furquan@chromium.org>
2017-08-08 12:27:23 -0700

    poppy: Add custom battery presence function
    Just checking for battery present gpio is not sufficient to determine
    the state of the battery. On the lines of the changes made by Eve, add
    a new battery_is_present function which:
    1. Checks hardware gpio signal to determine if battery is present
    2. If yes, then checks following if its status just changed to present:
       a. Battery is not in cut-off state
       b. Battery is not in disconnect state (charging and discharging
       disabled)
       c. Battery initialization is complete
    
    Only if all the above conditions are true, then battery is considered
    as present.
    
    BUG=b:64460667,b:64370648
    BRANCH=None
    TEST=Verified that with this change recovering system from battery
    cut-off is more consistent.
    
    Change-Id: I10abdf603e01f404c9b8e2094e36bc068adf5450
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/607035
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/poppy/battery.c
+++ board/poppy/battery.c
@@ -11,7 +11,10 @@
 #include "console.h"
 #include "ec_commands.h"
 #include "extpower.h"
+#include "gpio.h"
 #include "util.h"
+
+static enum battery_present batt_pres_prev = BP_NOT_SURE;
 
 #ifdef BOARD_SORAKA
 /* Shutdown mode parameter to write to manufacturer access register */
@@ -164,3 +167,79 @@ enum ec_status charger_profile_override_set_param(uint32_t param,
 {
 	return EC_RES_INVALID_PARAM;
 }
+
+static enum battery_present battery_hw_present(void)
+{
+	/* The GPIO is low when the battery is physically present */
+	return gpio_get_level(GPIO_BATTERY_PRESENT_L) ? BP_NO : BP_YES;
+}
+
+static int battery_init(void)
+{
+	int batt_status;
+
+	return battery_status(&batt_status) ? 0 :
+		!!(batt_status & STATUS_INITIALIZED);
+}
+
+/*
+ * Check for case where both XCHG and XDSG bits are set indicating that even
+ * though the FG can be read from the battery, the battery is not able to be
+ * charged or discharged. This situation will happen if a battery disconnect was
+ * intiaited via H1 setting the DISCONN signal to the battery. This will put the
+ * battery pack into a sleep state and when power is reconnected, the FG can be
+ * read, but the battery is still not able to provide power to the system. The
+ * calling function returns batt_pres = BP_NO, which instructs the charging
+ * state machine to prevent powering up the AP on battery alone which could lead
+ * to a brownout event when the battery isn't able yet to provide power to the
+ * system. .
+ */
+static int battery_check_disconnect(void)
+{
+	int rv;
+	uint8_t data[6];
+
+	/* Check if battery charging + discharging is disabled. */
+	rv = sb_read_mfgacc(PARAM_OPERATION_STATUS,
+			    SB_ALT_MANUFACTURER_ACCESS, data, sizeof(data));
+	if (rv)
+		return BATTERY_DISCONNECT_ERROR;
+
+	if ((data[3] & (BATTERY_DISCHARGING_DISABLED |
+			BATTERY_CHARGING_DISABLED)) ==
+	    (BATTERY_DISCHARGING_DISABLED | BATTERY_CHARGING_DISABLED))
+		return BATTERY_DISCONNECTED;
+
+	return BATTERY_NOT_DISCONNECTED;
+}
+
+enum battery_present battery_is_present(void)
+{
+	enum battery_present batt_pres;
+
+	/* Get the physical hardware status */
+	batt_pres = battery_hw_present();
+
+	/*
+	 * Make sure battery status is implemented, I2C transactions are
+	 * success & the battery status is Initialized to find out if it
+	 * is a working battery and it is not in the cut-off mode.
+	 *
+	 * If battery I2C fails but VBATT is high, battery is booting from
+	 * cut-off mode.
+	 *
+	 * FETs are turned off after Power Shutdown time.
+	 * The device will wake up when a voltage is applied to PACK.
+	 * Battery status will be inactive until it is initialized.
+	 */
+	if (batt_pres == BP_YES && batt_pres_prev != batt_pres &&
+	    (battery_is_cut_off() != BATTERY_CUTOFF_STATE_NORMAL ||
+	     battery_check_disconnect() != BATTERY_NOT_DISCONNECTED ||
+	     battery_init() == 0)) {
+		batt_pres = BP_NO;
+	}
+
+	batt_pres_prev = batt_pres;
+	return batt_pres;
+}
+
