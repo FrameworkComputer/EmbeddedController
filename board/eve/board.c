61ee794d31b4429b8991043ad35e983adf776701
Duncan Laurie <dlaurie@google.com>
2017-08-14 14:33:19 -0700

    eve: Disable PMIC power button shutdown timer
    Disable the PMIC shutdown timer for power button to prevent
    it from being able to shutdown the system.  Refresh+Power is
    still the best way to do a PMIC reset.
    
    BUG=none
    TESET=manual: Close lid and enter suspend, hold power button for
    >30 seconds and ensure EC stays up and system stays in suspend.
    
    Change-Id: I1e03c6acab57aeba10083eb7bb6a6141bfa56993
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/614600
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2320256
    Commit-Queue: Patryk Duda <pdk@semihalf.com>
    Tested-by: Patryk Duda <pdk@semihalf.com>
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- board/eve/board.c
+++ board/eve/board.c
@@ -446,6 +446,9 @@ static void board_pmic_init(void)
 	/* Clear power source events */
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x04, 0xff);
 
+	/* Disable power button shutdown timer */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x14, 0x00);
+
 	/* Disable VCCIO in ALL_SYS_PWRGD for early boards */
 	if (board_get_version() <= BOARD_VERSION_DVTB)
 		i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x18, 0x80);
