c478733f19b0de89364e996604a2ac73d1d95ea7
YH Lin <yueherngl@google.com>
2017-10-03 14:46:37 -0700

    grunt: initial setup for ec
    Initial files for grunt, to be revised later.
    
    BUG=b:67187149
    TEST=None
    BRANCH=None
    
    Change-Id: I392bb4982fea0c9374ba8d262f8f4288522207d5
    Signed-off-by: YH Lin <yueherngl@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/699554
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

---
+++ board/grunt/battery.c
@@ -0,0 +1,39 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Battery pack vendor provided charging profile
+ */
+#include "battery.h"
+#include "battery_smart.h"
+#include "extpower.h"
+#include "gpio.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHUTDOWN_DATA	0x0010
+
+static const struct battery_info info = {
+	.voltage_max = 13200,/* mV */
+	.voltage_normal = 11400,
+	.voltage_min = 9000,
+	.precharge_current = 256,/* mA */
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = -20,
+	.discharging_max_c = 70,
+};
+const struct battery_info *battery_get_info(void)
+{
+	return &info;
+}
+int board_cut_off_battery(void)
+{
+	int rv;
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	if (rv != EC_SUCCESS)
+		return rv;
+	return sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+}
