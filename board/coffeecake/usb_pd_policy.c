e78174a0b52fead626decfdcb0156e8ddbf21a52
Benson Leung <bleung@chromium.org>
2017-10-17 11:33:46 -0700

    coffeecake: Set Unconstrained Power (aka Ext) bit on 5V PDO
    This bit needs to be set to allow Chromebooks to determine that the dock
    is an externally powered DRP, so they'll try to charge from it instead of
    sitting there with 5V 0A.
    
    BRANCH=none
    BUG=b:69056645
    TEST=make buildall -j
    Check that Chromebooks successfully charge from Coffee Cake.
    
    Signed-off-by: Benson Leung <bleung@chromium.org>
    
    Change-Id: I9b1f207d976ef55b4a3016ba35c99522d4d1fce6
    Reviewed-on: https://chromium-review.googlesource.com/723979
    Commit-Ready: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- board/coffeecake/usb_pd_policy.c
+++ board/coffeecake/usb_pd_policy.c
@@ -23,6 +23,9 @@
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
+#define PDO_FIXED_FLAGS_EXT (PDO_FIXED_DUAL_ROLE | PDO_FIXED_DATA_SWAP |\
+			     PDO_FIXED_COMM_CAP | PDO_FIXED_EXTERNAL)
+
 #define PDO_FIXED_FLAGS (PDO_FIXED_DUAL_ROLE | PDO_FIXED_DATA_SWAP |\
 			 PDO_FIXED_COMM_CAP)
 
@@ -37,7 +40,7 @@ enum volt_idx {
 
 /* PDOs */
 const uint32_t pd_src_pdo[] = {
-	[PDO_IDX_5V]  = PDO_FIXED(5000,  3000, PDO_FIXED_FLAGS),
+	[PDO_IDX_5V]  = PDO_FIXED(5000,  3000, PDO_FIXED_FLAGS_EXT),
 	[PDO_IDX_9V]  = PDO_FIXED(9000,  3000, PDO_FIXED_FLAGS),
 };
 const int pd_src_pdo_cnt = ARRAY_SIZE(pd_src_pdo);
