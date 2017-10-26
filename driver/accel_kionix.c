c577880b797367eb4503243284c3341b7615a7e6
Gwendal Grignou <gwendal@chromium.org>
2017-10-26 09:28:50 -0700

    Revert "driver: kionix: Add checking whoami value at initialization."
    This reverts commit 1d8fcd4fabfed67d8304b3c5ad418cf6f5ae1878.
    It looks like KXCJ9 may not return proper WHOAMI after powerup
    but before reset.
    
    BUG=b:67865186
    BRANCH=eve
    TEST=compile
    
    Change-Id: Iceaaf3b2a45d920e08db587e1308cec0379e68cf
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/753967
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- driver/accel_kionix.c
+++ driver/accel_kionix.c
@@ -521,7 +521,7 @@ static int init(const struct motion_sensor_t *s)
 		msleep(1);
 		/* Read WHO_AM_I to be sure the device has booted */
 		ret = raw_read8(s->port, s->addr, reg, &val);
-		if (ret == EC_SUCCESS && val == KIONIX_WHO_AM_I_VAL(V(s)))
+		if (ret == EC_SUCCESS)
 			break;
 
 		/* Check for timeout. */
