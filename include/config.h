c5c061f9b5707205bb32abdac15dc14a9af14800
Gwendal Grignou <gwendal@chromium.org>
2017-11-03 17:20:30 -0700

    poppy: Lower sensor max ODR
    EC seems to miss sample while providing sensor data at 200Hz.
    Limit sensors ODR to 100Hz.
    
    BUG=b:67112751
    BRANCH=none
    TEST=compile, tbd
    
    Change-Id: Ic324c3d989854ae8b7f6b27bf6338266ce01ceda
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/753434
    Commit-Ready: Li1 Feng <li1.feng@intel.com>
    Tested-by: Li1 Feng <li1.feng@intel.com>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1015,8 +1015,11 @@
 /* Support EC to Internal bus bridge. */
 #undef CONFIG_EC2I
 
-/* EC capable of sensor speeds up to 200000 mHz */
-#define CONFIG_EC_MAX_SENSOR_FREQ_MILLIHZ 200000
+/* Usually, EC capable of sensor speeds up to 200000 mHz */
+#define CONFIG_EC_MAX_SENSOR_FREQ_DEFAULT_MILLIHZ 200000
+
+/* Maximal EC sampling rate */
+#undef CONFIG_EC_MAX_SENSOR_FREQ_MILLIHZ
 
 /*
  * Allow board to override the feature bitmap provided through host command
@@ -3135,4 +3138,8 @@
 #define CONFIG_SERIALNO_LEN 28
 #endif
 
+#ifndef CONFIG_EC_MAX_SENSOR_FREQ_MILLIHZ
+#define CONFIG_EC_MAX_SENSOR_FREQ_MILLIHZ \
+	CONFIG_EC_MAX_SENSOR_FREQ_DEFAULT_MILLIHZ
+#endif
 #endif  /* __CROS_EC_CONFIG_H */
