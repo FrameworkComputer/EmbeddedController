ad231bf1d1eb84dbaeeab06006c2b33a52a53874
Ege Mihmanli <egemih@google.com>
2017-11-06 11:08:22 -0800

    rainier: initial mainboard
    Copied board-related files from scarlet folder and made edits to
    fit rainier. Left in most battery related code and config since there
    is enough logic to detect absent battery
    
    BUG=chromium:776441
    TEST=Run "make -j BOARD=rainier"
    BRANCH=none
    Signed-off-by: egemih@google.com
    Change-Id: Ifd1201a9a44cebd9b433545f0ac7ee04741429c9
    Reviewed-on: https://chromium-review.googlesource.com/755949
    Commit-Ready: Shawn N <shawnn@chromium.org>
    Tested-by: Ege Mihmanli <egemih@google.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

---
+++ board/rainier/build.mk
@@ -0,0 +1,14 @@
+# -*- makefile -*-
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+#
+# STmicro STM32F098VC
+CHIP:=stm32
+CHIP_FAMILY:=stm32f0
+CHIP_VARIANT:=stm32f09x
+
+board-y=board.o usb_pd_policy.o
