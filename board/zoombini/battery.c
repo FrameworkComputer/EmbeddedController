2fa3e47d9c4ea2fe1c087d30d2e68f1fe8fbae90
Aseda Aboagye <aaboagye@google.com>
2017-11-10 14:09:23 -0800

    meowth: Add battery pack information.
    BUG=b:69138843
    BRANCH=None
    TEST=make -j buildall.
    
    Change-Id: Ia3aa76e03c7551dc34041631e8f8d1b16c1771e3
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/772911
    Commit-Ready: Aseda Aboagye <aaboagye@chromium.org>
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/zoombini/battery.c
+++ board/zoombini/battery.c
@@ -9,6 +9,7 @@
 
 /* Battery info for proto */
 static const struct battery_info info = {
+#ifdef BOARD_ZOOMBINI
 	.voltage_max		= 13200,
 	.voltage_normal		= 11250,
 	.voltage_min		= 9000,
@@ -19,6 +20,32 @@ static const struct battery_info info = {
 	.charging_max_c		= 60,
 	.discharging_min_c	= -20,
 	.discharging_max_c	= 60,
+#else /* !defined(BOARD_ZOOMBINI) */
+	/* Meowth battery info. */
+#if 0 /* planned pack settings */
+	.voltage_max		= 8780,
+	.voltage_normal		= 7700,
+	.voltage_min		= 6000,
+	.precharge_current	= 160,
+	.start_charging_min_c	= 0,
+	.start_charging_max_c	= 45,
+	.charging_min_c		= 0,
+	.charging_max_c		= 45,
+	.discharging_min_c	= -20,
+	.discharging_max_c	= 60,
+#endif /* 0 */
+	/* Borrowed eve batteries for the time being. */
+	.voltage_max		= TARGET_WITH_MARGIN(8800, 5), /* mV */
+	.voltage_normal		= 7700,
+	.voltage_min		= 6100, /* Add 100mV for charger accuracy */
+	.precharge_current	= 256,	/* mA */
+	.start_charging_min_c	= 0,
+	.start_charging_max_c	= 46,
+	.charging_min_c		= 10,
+	.charging_max_c		= 50,
+	.discharging_min_c	= 0,
+	.discharging_max_c	= 60,
+#endif /* defined(BOARD_ZOOMBINI) */
 };
 
 const struct battery_info *battery_get_info(void)
