bf80bbf769270e094f224560e23f32fa4ca77144
YH Lin <yueherngl@google.com>
2017-11-22 14:04:38 -0800

    nami: initial setup for ec
    Add initial files for nami from fizz, to be revised later.
    
    BUG=b:69628538
    TEST=None
    BRANCH=None
    CQ-DEPEND=CL:782239
    
    Change-Id: I910a8fc9603471d5de0b34ca5888fff147f9cd6f
    Signed-off-by: YH Lin <yueherngl@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/783557
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ board/nami/build.mk
@@ -0,0 +1,14 @@
+# -*- makefile -*-
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=npcx
+CHIP_VARIANT:=npcx5m6g
+
+board-y=board.o
+board-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
+board-y+=led.o
