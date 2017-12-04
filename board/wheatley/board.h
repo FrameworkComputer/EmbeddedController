bdfc4db7af776bf0248326ac91cf1e5bfd491bbc
Jenny TC <jenny.tc@intel.com>
2017-12-04 15:38:14 +0530

    wheatly: Disable console commands to free up flash space
    Disable following configs to free up some flash space
    CONFIG_CMD_I2C_SCAN
    CONFIG_CONSOLE_HISTORY
    
    BRANCH=none
    BUG=b:63969337
    TEST=make buildall -j
    
    Change-Id: I3b5db7012825c3785fb72e96f6e08456357335e2
    Signed-off-by: Jenny TC <jenny.tc@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/806436
    Commit-Ready: Jenny Tc <jenny.tc@intel.com>
    Tested-by: Jenny Tc <jenny.tc@intel.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/wheatley/board.h
+++ board/wheatley/board.h
@@ -146,6 +146,8 @@
 #undef CONFIG_CMD_TEMP_SENSOR
 #undef CONFIG_CMD_TIMERINFO
 #undef CONFIG_CONSOLE_CMDHELP
+#undef CONFIG_CMD_I2C_SCAN
+#undef CONFIG_CONSOLE_HISTORY
 
 /* Features of eSPI */
 #undef CONFIG_ESPI       /* Use eSPI protocol for host interface of x86 CPU */
