313ea684c37a5f21d46304ed9f6515362d003544
YB.Ha <ybha@samsung.com>
2017-12-11 11:40:27 +0900

    nautilus: Limit input current to fraction of negotiated limit
    Limit input current to 96% of negotiated limit
    
    BUG=b:70481533
    BRANCH=none
    TEST=build/flash nautilus, connect adapter and check input current
    
    Change-Id: I20e250d8a37f22b471a69329b2e74da2c7ff2763
    Reviewed-on: https://chromium-review.googlesource.com/818844
    Commit-Ready: YongBeum Ha <ybha@samsung.com>
    Tested-by: YongBeum Ha <ybha@samsung.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/nautilus/board.c
+++ board/nautilus/board.c
@@ -502,6 +502,11 @@ int board_set_active_charge_port(int charge_port)
 void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
 {
+        /*
+         * Limit the input current to 96% negotiated limit,
+         * to account for the charger chip margin.
+         */
+	charge_ma = charge_ma * 96 / 100;
 	charge_set_input_current_limit(MAX(charge_ma,
 				   CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
 }
