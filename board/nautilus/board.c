b167a193e66e3c05ad61d997994072e36cb73773
Jongpil Jung <jongpil19.jung@samsung.com>
2017-12-16 17:27:28 +0900

    nautilus: update sensor rotation matrix.
    From rev02, Lid rotation matrix is changed.
    So, we have to change rotation matrix as well.
    
    BRANCH=master
    BUG=b:69399214
    TEST=check lid_angle on rev2.
    
    Change-Id: I6718b607391aef068bee3c324e85138bac0e283e
    Signed-off-by: Jongpil Jung <jongpil19.jung@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/830990
    Tested-by: Grace Kao <grace.kao@intel.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- board/nautilus/board.c
+++ board/nautilus/board.c
@@ -576,15 +576,15 @@ static struct bma2x2_accel_data g_bma255_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 const matrix_3x3_t base_standard_ref = {
-    { FLOAT_TO_FP(-1), 0, 0},
-    { 0,  FLOAT_TO_FP(1), 0},
-    { 0, 0, FLOAT_TO_FP(-1)}
+    { FLOAT_TO_FP(-1), 0, 0 },
+    { 0,  FLOAT_TO_FP(1), 0 },
+    { 0, 0, FLOAT_TO_FP(-1) }
 };
 
 const matrix_3x3_t lid_standard_ref = {
-    { FLOAT_TO_FP(-1), 0, 0},
-    { 0, FLOAT_TO_FP(-1), 0},
-    { 0,  0, FLOAT_TO_FP(1)}
+    { FLOAT_TO_FP(-1), 0, 0 },
+    { 0,  FLOAT_TO_FP(1), 0 },
+    { 0, 0, FLOAT_TO_FP(-1) }
 };
 
 struct motion_sensor_t motion_sensors[] = {
