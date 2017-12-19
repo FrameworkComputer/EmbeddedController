d8f08ae1a07c2884a8048b50d058653bc7a0f1be
Aseda Aboagye <aaboagye@google.com>
2017-12-19 12:10:00 -0800

    meowth: zoombini: Add support for battery cutoff.
    BUG=None
    BRANCH=None
    TEST=Flash meowth; 'cutoff'; Verify battery is cutoff.
    
    Change-Id: I95c3d1c10512c49da4768824c4126a3755af2b1a
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/zoombini/battery.c
+++ board/zoombini/battery.c
@@ -6,6 +6,12 @@
  */
 
 #include "battery.h"
+#include "battery_smart.h"
+#include "common.h"
+#include "ec_commands.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHUTDOWN_DATA	0x0010
 
 /* Battery info for proto */
 static const struct battery_info info = {
@@ -47,6 +53,19 @@ static const struct battery_info info = {
 	.discharging_max_c	= 60,
 #endif /* defined(BOARD_ZOOMBINI) */
 };
+
+int board_cut_off_battery(void)
+{
+	int rv;
+
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	if (rv != EC_SUCCESS)
+		return EC_RES_ERROR;
+
+	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	return rv ? EC_RES_ERROR : EC_RES_SUCCESS;
+}
 
 const struct battery_info *battery_get_info(void)
 {
