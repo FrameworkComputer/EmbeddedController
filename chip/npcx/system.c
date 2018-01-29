87a1268ef5c516e5eef0fa2dfc3c35339ed86792
Elmo_Lan <elmo_lan@compal.corp-partner.google.com>
2018-01-29 06:14:21 -0500

    Nami: force to set RESET_FLAG_RESET_PIN
    Like other KBL designs (eve, poppy and followers).
    EC is not able to distinguish between power up and reset.
    
    BUG=b:71839731
    BRANCH=none
    TEST=Verify Nami power on flag by EC console.
    
    Change-Id: Ice8b80259b8405f28b508918d5b3cfe37a8b1eb9
    Signed-off-by: Elmo_Lan <elmo_lan@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/891042
    Commit-Ready: Raymond Chou <raymond_chou@compal.corp-partner.google.com>
    Tested-by: Elmo Lan <elmo_lan@compal.corp-partner.google.com>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Elmo Lan <elmo_lan@compal.corp-partner.google.com>

--- chip/npcx/system.c
+++ chip/npcx/system.c
@@ -368,7 +368,7 @@ void system_check_reset_cause(void)
 	/* Use scratch bit to check power on reset or VCC1_RST reset */
 	if (!IS_BIT_SET(NPCX_RSTCTL, NPCX_RSTCTL_VCC1_RST_SCRATCH)) {
 #if defined(BOARD_WHEATLEY) || defined(BOARD_EVE) || defined(BOARD_POPPY) || defined(BOARD_SORAKA)\
-	|| defined(BOARD_NAUTILUS)
+	|| defined(BOARD_NAUTILUS) || defined(BOARD_NAMI)
 
 		/* TODO(crosbug.com/p/61028): Remove workaround for Eve */
 		flags |= RESET_FLAG_RESET_PIN;
