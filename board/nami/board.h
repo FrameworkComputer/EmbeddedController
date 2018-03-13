0778c50859ff6cb35822b9176f101aab23aece99
Gwendal Grignou <gwendal@chromium.org>
2018-03-13 11:35:51 -0700

    board: nami: Enable sensors in forced mode
    Sensors without interrupt line needs to be in forced mode,
    otherwise the EC may not poll them.
    
    BUG=b:73205042
    BRANCH=master
    TEST=Check with accelrate the ec rate is 100ms by default:
    Before:
    > accelrate 0
    Data rate for sensor 0: 15620
    EC rate for sensor 0: 0
    Current EC rate: 1000000
    Current Interrupt rate: 0
    
    After:
    +accelrate 0
    Data rate for sensor 0: 15620
    EC rate for sensor 0: 100000
    Current EC rate: 100000
    Current Interrupt rate: 0
    
    Change-Id: Ib626e06f572d97efe4ccd80bf87c18958f940c5c
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/960940
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/nami/board.h
+++ board/nami/board.h
@@ -276,6 +276,9 @@ int board_get_version(void);
 void board_reset_pd_mcu(void);
 void board_set_tcpc_power_mode(int port, int mode);
 
+/* Sensors without hardware FIFO are in forced mode */
+#define CONFIG_ACCEL_FORCE_MODE_MASK ((1 << LID_ACCEL) | (1 << LID_ALS))
+
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BOARD_H */
