e260ea92719bd0462394a2aa4fe05e163ea02306
li feng <li1.feng@intel.com>
2018-04-09 15:42:31 -0700

    atlas_ish: initial board files for atlas_ish
    BUG=b:117807679
    BRANCH=none
    TEST=build with BOARD=atlas_ish is successful
    
    Change-Id: Iddb9a8a5ced24e9b99753a876ec52b0062b80344
    Signed-off-by: li feng <li1.feng@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1003393
    Commit-Ready: Li1 Feng <li1.feng@intel.com>
    Tested-by: Li1 Feng <li1.feng@intel.com>
    Reviewed-by: Caveh Jalali <caveh@google.com>
    Reviewed-by: Li1 Feng <li1.feng@intel.com>

---
+++ board/atlas_ish/build.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=ish
+
+board-y=board.o
