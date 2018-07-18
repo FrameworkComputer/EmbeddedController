5100b4c3d2059de5da41c75fc19347918eec87da
Edward Hill <ecgh@chromium.org>
2018-07-18 16:25:27 -0600

    aleena,liara: Add new grunt-family boards.
    Add Aleena and Liara boards. These are initially just a copy of
    Careena.
    
    BUG=b:111606874,b:111607004
    BRANCH=none
    TEST=build
    
    Change-Id: I2e2d0a375c4d0943e6c7506bcad639d45d5f1eb4
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1142534
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Jonathan Brandmeyer <jbrandmeyer@chromium.org>

---
+++ board/aleena/board.h
@@ -0,0 +1,43 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Aleena board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+#include "baseboard.h"
+
+/*
+ * By default, enable all console messages excepted HC, ACPI and event:
+ * The sensor stack is generating a lot of activity.
+ */
+#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#undef CONFIG_HOSTCMD_DEBUG_MODE
+#define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
+
+#define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands while in dev. */
+
+#define CONFIG_LED_COMMON
+#define CONFIG_CMD_LEDTEST
+
+#ifndef __ASSEMBLER__
+
+enum pwm_channel {
+	PWM_CH_KBLIGHT = 0,
+	PWM_CH_COUNT
+};
+
+enum battery_type {
+	BATTERY_DANAPACK_COS,
+	BATTERY_DANAPACK_ATL,
+	BATTERY_DANAPACK_SDI,
+	BATTERY_SAMSUNG_SDI,
+	BATTERY_TYPE_COUNT,
+};
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
