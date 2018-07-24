d64a0c69a99c494724db250c8af51ec93706c73e
Daisuke Nojiri <dnojiri@chromium.org>
2018-07-24 16:51:09 -0700

    Nami: Disable ACPI and host event log messages
    Currently, every single host event is logged to the console when
    it's set or cleared. This spews messages when running CTS tests
    for the sensors.
    
    This patch disables log messages for LPC and host event. They can
    be re-enabled from console by the cc command.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:80284952
    BRANCH=none
    TEST=Verify messages are suppressed:
    > hostevent always_report 0xffffffff
    > hostevent set 0x4000000
    (No prints)
    > hostevent set 0x4000001
    [110.581101 event set 0x0000000004000001]
    
    Change-Id: I84ca913b130cd90b4ff0007cc52880860a8af133
    Reviewed-on: https://chromium-review.googlesource.com/1149279
    Commit-Ready: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Alexandru M Stan <amstan@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- board/nami/board.h
+++ board/nami/board.h
@@ -8,6 +8,12 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
+/*
+ * By default, enable all console messages except ACPI and host event because
+ * the sensor stack is generating a lot of activity.
+ */
+#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+
 /* EC */
 #define CONFIG_ADC
 #define CONFIG_BACKLIGHT_LID
