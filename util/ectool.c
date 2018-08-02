2725fd03a8aedef7b6e77a3ae7e1fbc05b4af3c2
Jett Rink <jettrink@chromium.org>
2018-08-02 11:14:09 -0600

    ectool: set option flag to 0 when unset
    I noticed that subsequent runs of `ectool cbi get 0` after
    `ectool cbi get 0 1` would force the EC to read the EEPROM when the
    flag wasn't specified. It looks like the previous stack may have been
    used.
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: Iba1aae64fb690ee7f3affa632185d53ee63c3c89
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1161001
    Reviewed-by: Jonathan Brandmeyer <jbrandmeyer@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -6647,7 +6647,7 @@ static int cmd_cbi(int argc, char *argv[])
 	}
 
 	if (!strcasecmp(argv[1], "get")) {
-		struct ec_params_get_cbi p;
+		struct ec_params_get_cbi p = { 0 };
 		uint8_t *r;
 		int i;
 		p.tag = tag;
