572045e5974ab34515ca509fa94bfe0fae5d7d01
Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
2018-08-03 17:34:03 +0800

    Octopus: Add new battery configuration for Bobba
    bobba new battery: AP13J7K of SIMPLO
    
    BUG=b:110096478
    BRANCH=master
    TEST=recognize new battery AP13J7K okay, ship mode test okay, DSG FET
    status test okay
    
    Change-Id: Ibfcd9af05492593d99e8d0d433dbc9f59cfeee79
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1162081
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- board/bobba/battery.c
+++ board/bobba/battery.c
@@ -145,6 +145,36 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 60,
 		},
 	},
+
+	/* Simplo AP13J7K Battery Information */
+	[BATTERY_SMP_AP13J7K] = {
+		.fuel_gauge = {
+			.manuf_name = "SIMPLO",
+			.device_name = "AP13J7K",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0000,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13050,
+			.voltage_normal		= 11400, /* mV */
+			.voltage_min		= 9000, /* mV */
+			.precharge_current	= 256,	/* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 45,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= 0,
+			.discharging_max_c	= 60,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
