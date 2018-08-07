2819012608ed7338d736ccfae4d0ab73ea8b3543
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2018-08-07 10:39:29 +0800

    bobba: Modify the LED behavior to meet the spec.
    Battery Error: Amber on 1 sec off 1 sec
    Discharge in S3: Amber on 1 sec off 3 sec
    
    BUG=b:111775507
    BRANCH=master
    TEST=check the LED is amber on 1sec off 1sec while only plugging in AC
    adpater PASS
    
    Change-Id: I359cb8d5e4981d3dc5f509c701c5a67bc7371bf8
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1164158
    Commit-Ready: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    Tested-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- board/bobba/led.c
+++ board/bobba/led.c
@@ -23,11 +23,11 @@ const struct led_descriptor
 	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_AMBER, LED_INDEFINITE} },
 	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_BLUE,  LED_INDEFINITE} },
 	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_BLUE,  LED_INDEFINITE} },
-	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_AMBER, 4 * LED_ONE_SEC},
-					{LED_OFF,            1 * LED_ONE_SEC} },
+	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            3 * LED_ONE_SEC} },
 	[STATE_DISCHARGE_S5]         = {{LED_OFF,            LED_INDEFINITE} },
-	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_BLUE,  2 * LED_ONE_SEC},
-					{EC_LED_COLOR_AMBER, 2 * LED_ONE_SEC} },
+	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            1 * LED_ONE_SEC} },
 };
 
 const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED };
