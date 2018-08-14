0143da797b853660f1dc08fff46f8a6b89257bb7
Devin Lu <devin.lu@quantatw.com>
2018-08-14 14:45:04 +0800

    careena: add simplo batteries
    This patch add two simplo batteries for careena, one is coslight pack,
    another one is ATL pack.
    
    BUG=b:111288497,b:111287943
    BRANCH=none
    TEST=Check DUT can recognize this two batteries.
    Check software battery cutoff and hardware battery cutoff could be
    woke up after charger attached.
    
    Change-Id: If22be1e84c0dee28e4d12dc64c3102b7d80dacc4
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/1174145
    Commit-Ready: Devin Lu <devin.lu@quanta.corp-partner.google.com>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

--- board/careena/battery.c
+++ board/careena/battery.c
@@ -151,6 +151,66 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c = 60,
 		},
 	},
+
+	/* Simplo Coslight Battery Information */
+	[BATTERY_SIMPLO_COS] = {
+		.fuel_gauge = {
+			.manuf_name = "333-1C-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x6000,
+				.disconnect_val = 0x6000,
+			},
+			.imbalance_mv = &battery_default_imbalance_mv,
+		},
+		.batt_info = {
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7600,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = 0,
+			.discharging_max_c = 60,
+		},
+	},
+
+	/* Simplo ATL Battery Information */
+	[BATTERY_SIMPLO_ATL] = {
+		.fuel_gauge = {
+			.manuf_name = "333-17-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x6000,
+				.disconnect_val = 0x6000,
+			},
+			.imbalance_mv = &battery_default_imbalance_mv,
+		},
+		.batt_info = {
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7600,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = 0,
+			.discharging_max_c = 60,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
