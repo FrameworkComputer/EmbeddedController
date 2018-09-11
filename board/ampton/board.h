ca25c78f48ed3fb6b52945afa9b5d83136f07c08
Diana Z <dzigterman@chromium.org>
2018-09-11 15:52:03 -0600

    Ampton: create initial EC image
    This image is based on bip from current ToT and the most recent ampton
    schematics.  Bugs have been created and put into TODO statements for
    the future work needed on this EC image.
    
    BRANCH=None
    BUG=b:111498206
    TEST=builds
    
    Change-Id: Idff972b9ce5231524dd4865a87953ebcd5310c62
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1225350
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

---
+++ board/ampton/board.h
@@ -0,0 +1,65 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Ampton/Apel board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Select Baseboard features */
+#define VARIANT_OCTOPUS_EC_ITE8320
+#define VARIANT_OCTOPUS_CHARGER_BQ25703
+#include "baseboard.h"
+
+#define CONFIG_VOLUME_BUTTONS
+#define GPIO_VOLUME_UP_L GPIO_EC_VOLUP_BTN_ODL
+#define GPIO_VOLUME_DOWN_L GPIO_EC_VOLDN_BTN_ODL
+
+/* Optional features */
+#define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands while in dev. */
+
+/* Sensors */
+/* TODO(b/115502220): Ampton/Apel: confirm thermistor parts */
+#define CONFIG_TEMP_SENSOR
+#define CONFIG_THERMISTOR
+#define CONFIG_STEINHART_HART_3V3_51K1_47K_4050B
+#define CONFIG_STEINHART_HART_3V3_13K7_47K_4050B
+
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 4096
+
+/* Keyboard backlight is unimplemented in hardware */
+#undef CONFIG_PWM
+#undef CONFIG_PWM_KBLIGHT
+
+#ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+enum adc_channel {
+	ADC_VBUS_C0,
+	ADC_VBUS_C1,
+	ADC_TEMP_SENSOR_AMB,
+	ADC_TEMP_SENSOR_CHARGER,
+	ADC_CH_COUNT
+};
+
+enum temp_sensor_id {
+	TEMP_SENSOR_BATTERY,
+	TEMP_SENSOR_AMBIENT,
+	TEMP_SENSOR_CHARGER,
+	TEMP_SENSOR_COUNT
+};
+
+/* List of possible batteries */
+/* TODO(b/115502621): Ampton/Apel: need battery datasheets and specs */
+enum battery_type {
+	BATTERY_TYPE_COUNT,
+};
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
