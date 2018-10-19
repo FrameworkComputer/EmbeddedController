58e54330d90e06e7b1d6fa8534062292f07ad085
Roy Mingi Park <roy.mingi.park@intel.com>
2018-10-19 11:27:22 -0700

    CHERRY-PICK: Nocturne: Remove VR decay for V1.00A power rail
    This patches removes the VR decay for V1.00A power rail when system
    enters low power mode. According to PAG #543977, V1.00A power has
    tolerance from -6.7% to 5% but it doesn't support LPM like VccPRIM_CORE
    which could down to 0.75v in low power mode.
    
    BUG=b:116180071
    BRANCH=Nocturne
    TEST=Measure V1.00A power rail when system is in S0iX and ensure it
         keeps at 1.0v
    
    Signed-off-by: Roy Mingi Park <roy.mingi.park@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/1291950
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>
    Change-Id: Iefbf86c8d71794277ee6e7ae4bdca81617cd03fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2160871
    Tested-by: Patryk Duda <pdk@semihalf.com>
    Commit-Queue: Patryk Duda <pdk@semihalf.com>

--- board/nocturne/board.c
+++ board/nocturne/board.c
@@ -468,15 +468,6 @@ static void board_pmic_disable_slp_s0_vr_decay(void)
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x34, 0x2a);
 
 	/*
-	 * V100ACNT:
-	 * Bits 7:6 (00) - Disable low power mode on SLP_S0# assertion
-	 * Bits 5:4 (01) - Nominal voltage 1.0V
-	 * Bits 3:2 (10) - VR set to AUTO on SLP_S0# de-assertion
-	 * Bits 1:0 (10) - VR set to AUTO operating mode
-	 */
-	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x37, 0x1a);
-
-	/*
 	 * V085ACNT:
 	 * Bits 7:6 (00) - Disable low power mode on SLP_S0# assertion
 	 * Bits 5:4 (10) - Nominal voltage 0.85V
@@ -505,15 +496,6 @@ static void board_pmic_enable_slp_s0_vr_decay(void)
 	 * Bits 1:0 (10) - VR set to AUTO operating mode
 	 */
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x34, 0x6a);
-
-	/*
-	 * V100ACNT:
-	 * Bits 7:6 (01) - Enable low power mode on SLP_S0# assertion
-	 * Bits 5:4 (01) - Nominal voltage 1.0V
-	 * Bits 3:2 (10) - VR set to AUTO on SLP_S0# de-assertion
-	 * Bits 1:0 (10) - VR set to AUTO operating mode
-	 */
-	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992_FLAGS, 0x37, 0x5a);
 
 	/*
 	 * V085ACNT:
