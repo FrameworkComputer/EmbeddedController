00e3643672d4a3bf2edc47f81c2b9ab39fa0dd1e
Caveh Jalali <caveh@chromium.org>
2018-10-23 22:45:36 -0700

    core/minute-ia: clean up atomics
    this cleans up the definitions in atomic.h.  the coreboot toolchain
    wasn't very happy with the original declarations and definitions, and
    sure enough inline global functions don't make a lot of sense.  so,
    i'm just going to apply the same definition style used for other
    architectuers here.
    
    BUG=b:118355015
    BRANCH=none
    TEST=tested in combination of a bunch of other patches to get
    	atlas_ish to build with the coreboot toolchain
    
    Change-Id: I654d1dd059b07484f724727d8546d8e7665d1b6c
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1297038
    Commit-Ready: Caveh Jalali <caveh@google.com>
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- core/minute-ia/build.mk
+++ core/minute-ia/build.mk
@@ -26,7 +26,7 @@ CFLAGS_CPU+=-flto
 LDFLAGS_EXTRA+=-flto
 endif
 
-core-y=cpu.o init.o interrupts.o atomic.o
+core-y=cpu.o init.o interrupts.o
 core-$(CONFIG_COMMON_PANIC_OUTPUT)+=panic.o
 core-$(CONFIG_COMMON_RUNTIME)+=switch.o task.o
 core-$(CONFIG_MPU)+=mpu.o
