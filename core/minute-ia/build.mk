a56ab7d4a3c7e05477f7e489db08de00c30da662
Caveh Jalali <caveh@chromium.org>
2018-10-23 20:17:45 -0700

    core/minute-ia: default to coreboot toolchain
    this sets the default toolchain for minute-ia to be the coreboot
    toolchain.
    
    BUG=b:118355015
    BRANCH=none
    TEST=built using make from platform/ec, verified coreboot toolchain is
    	used.
    
    Change-Id: I4078be8c1fafeee1e64c2ed008bb9f946f637077
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1297041
    Commit-Ready: Caveh Jalali <caveh@google.com>
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- core/minute-ia/build.mk
+++ core/minute-ia/build.mk
@@ -7,7 +7,8 @@
 #
 
 # Select Minute-IA bare-metal toolchain
-$(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_i386),i686-pc-linux-gnu-)
+$(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_i386),\
+	/opt/coreboot-sdk/bin/i386-elf-)
 
 # FPU compilation flags
 CFLAGS_FPU-$(CONFIG_FPU)=
