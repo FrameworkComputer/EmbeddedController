5e30db82364922372d029407cdea53f9835e1b9c
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2018-11-20 17:09:58 +0800

    Nami: modify actual_key_mask to enable the "Search" key for the keyboard with keypad
    BUG=b:119798830
    BRANCH=firmware-nami-10775.B
    TEST=Use ksstate console to check it can show the right point
    for the "Search" key after pressing the key.
    
    Change-Id: I068b629d962a3f9ebf70ef9785610bc9fc424696
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1352064
    Commit-Ready: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/nami/board.c
+++ board/nami/board.c
@@ -926,7 +926,7 @@ struct keyboard_scan_config keyscan_config = {
 	.min_post_scan_delay_us = 1000,
 	.poll_timeout_us = 100 * MSEC,
 	.actual_key_mask = {
-		0x14, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0x1c, 0xfe, 0xff, 0xff, 0xff, 0xf5, 0xff,
 		0xa4, 0xff, 0xfe, 0x55, 0xfe, 0xff, 0xff, 0xff,  /* full set */
 	},
 };
@@ -971,6 +971,8 @@ static void board_init(void)
 	/* Disable scanning KSO13 & 14 if keypad isn't present. */
 	if (!(sku & SKU_ID_MASK_KEYPAD)) {
 		keyboard_raw_set_cols(KEYBOARD_COLS_NO_KEYPAD);
+		keyscan_config.actual_key_mask[0] = 0x14;
+		keyscan_config.actual_key_mask[1] = 0xff;
 		keyscan_config.actual_key_mask[11] = 0xfa;
 		keyscan_config.actual_key_mask[12] = 0xca;
 	}
