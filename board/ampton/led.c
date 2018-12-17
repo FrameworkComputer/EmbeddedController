e1d83c546f7e3dd0912a6fa9b3842a4d9ff6fc5d
James_Chao <james_chao@asus.corp-partner.google.com>
2018-12-17 15:36:19 +0800

    Ampton: modify the led behavior when battery error
    BUG=none
    BRANCH=octopus
    TEST=check the led behavior when battery error
    
    Change-Id: I9680b7011bb88e5e064f67ae3625534945b5dff7
    Signed-off-by: James_Chao <james_chao@asus.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1381597
    Commit-Ready: James Chao <james_chao@asus.corp-partner.google.com>
    Tested-by: James Chao <james_chao@asus.corp-partner.google.com>
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- board/ampton/led.c
+++ board/ampton/led.c
@@ -26,8 +26,8 @@ struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
 	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
 					{LED_OFF,            3 * LED_ONE_SEC} },
 	[STATE_DISCHARGE_S5]         = {{LED_OFF,            LED_INDEFINITE} },
-	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_WHITE, 1 * LED_ONE_SEC},
-					{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC} },
+	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            1 * LED_ONE_SEC} },
 	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_WHITE, 2 * LED_ONE_SEC},
 					{EC_LED_COLOR_AMBER, 2 * LED_ONE_SEC} },
 };
