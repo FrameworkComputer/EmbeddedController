e17cd2a21f2b2762597cb6346bd359f784c9b43b
Kaka Ni <nigang@huaqin.corp-partner.google.com>
2019-01-29 21:00:32 +0800

    flapjack/gpio: Modify GPIO configuration for Board Id 3
    Modify GPIO and ADC confiturations for flapjack board_id 3 (P0B & P0C).
    
    CQ-DEPEND=CL:1445956
    BUG=b:123498558
    BRANCH=none
    TEST=BOOTBLOCK=... make BOARD=flapjack -j flash_ec; and see AP boots.
    
    Change-Id: Iac353c1a4ea92bd028003b6c11647965528ac30a
    Signed-off-by: Kaka Ni <nigang@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1438957
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/flapjack/board.h
+++ board/flapjack/board.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -9,11 +9,6 @@
 #define __CROS_EC_BOARD_H
 
 /* board revision */
-#define BOARD_REV 1
-
-#if BOARD_REV < 1 || BOARD_REV > 2
-#error "Board revision out of range"
-#endif
 
 /* Optional modules */
 #define CONFIG_ADC
@@ -202,12 +197,9 @@
 enum adc_channel {
 	/* Real ADC channels begin here */
 	ADC_BOARD_ID = 0,
-#if BOARD_REV >= 2
 	ADC_EC_SKU_ID,
 	ADC_BATT_ID,
-	ADC_POGO_ADC_INT_L,
 	ADC_USBC_THERM,
-#endif
 	ADC_CH_COUNT
 };
 
