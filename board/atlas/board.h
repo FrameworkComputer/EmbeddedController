88c5b086b2722b6f959f2608de298b3ba8299888
Caveh Jalali <caveh@chromium.org>
2019-02-26 20:40:06 -0800

    atlas: raise charger power limit to 60w
    this raises the ingress/charger power limit to 60w.  the hardware team
    has verified atlas can take 60w.
    
    BUG=b:126377091
    BRANCH=none
    TEST=verified atlas negotiates PD to 20v and can draw > 45w using zinger
    
    Change-Id: If738cfc690893df39f223205bff9daecd332b3a3
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1496199
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- board/atlas/board.h
+++ board/atlas/board.h
@@ -240,7 +240,7 @@ enum adc_channel {
 
 /* Define typical operating power and max power */
 #define PD_OPERATING_POWER_MW		15000
-#define PD_MAX_POWER_MW			45000
+#define PD_MAX_POWER_MW			60000
 #define PD_MAX_CURRENT_MA		3000
 #define PD_MAX_VOLTAGE_MV		20000
 
