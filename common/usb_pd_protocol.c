jackadam1981/ChromeEc

pd_protocol: Allow charging @ 500mA from Rp-USB.
A USB Type-C ECN "Title: Default Current Clarification for non-USB
Devices", allows a sink to draw 500mA based off of a Default USB Rp
advertisement.  This is especially important for devices which lack the
hardware to detect legacy sources via BC 1.2.  This commit will now
allow those devices to charge at 5V/500mA.

BUG=b:117805314
BRANCH=firmware-nocturne-10984.B,master
TEST=Flash nocturne, plug in USB-A to USB-C cable, verify cable is
detected as a charger and that we charge from it.

Change-Id: I1f40cca7246c6d671c7775869da64cf30f649dc4
Signed-off-by: Aseda Aboagye <aaboagye@google.com>

common/usb_pd_protocol.c
@@ -2450,6 +2450,8 @@ static typec_current_t get_typec_current_limit(int polarity, int cc1, int cc2)
 		charge = 3000;
 	else if (cc == TYPEC_CC_VOLT_SNK_1_5)
 		charge = 1500;
+	else if (cc == TYPEC_CC_VOLT_SNK_DEF)
+		charge = 500;
 	else
 		charge = 0;
 

FrameworkComputer/EmbeddedController

BUG=b:117805314,b:124138725
Reviewed-on: https://chromium-review.googlesource.com/c/1431252
Tested-by: Aseda Aboagye <aaboagye@chromium.org>
Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>
Reviewed-by: Benson Leung <bleung@chromium.org>
(cherry picked from commit 9509f9aa3322fb180f51064309875589c8b90973)
Reviewed-on: https://chromium-review.googlesource.com/1565463
Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
Tested-by: Matthew Blecker <matthewb@chromium.org>
Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
Reviewed-by: Matthew Blecker <matthewb@chromium.org>

common/usb_pd_protocol.c
@@ -2508,6 +2508,8 @@ static typec_current_t get_typec_current_limit(int polarity, int cc1, int cc2)
 		charge = 3000;
 	else if (cc == TYPEC_CC_VOLT_RP_1_5)
 		charge = 1500;
+	else if (cc == TYPEC_CC_VOLT_RP_DEF)
+		charge = 500;
 	else
 		charge = 0;
 