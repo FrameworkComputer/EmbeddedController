7f3b39fec4a91170b7de1ac4e003e238e6cb4366
Brian J. Nemec <bnemec@google.com>
2019-05-08 11:51:40 -0700

    Reverts an update to the compiler toolchain when building the Sweetberry project to repair ToT.
    The change which updated the toolchain exposed a problem with the
    USB initialization which prevents powerlog from being able to
    communicate with the device. The root cause has not been
    identified so the follow up issue b/132204142 has been filed.
    
    BRANCH=Servo
    BUG=b:126223732
    TEST=Verified that the Sweetberry project is able to build, lsusb
    identifies the device, and powerlog is capable of communicating with
    the device and retrieving data.
    
    Change-Id: I0d1f7e79a8ea890dcf5da3c16427220cf9067fdf
    Signed-off-by: Brian Nemec <bnemec@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1601350
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Nick Sanders <nsanders@chromium.org>

--- board/sweetberry/build.mk
+++ board/sweetberry/build.mk
@@ -5,6 +5,11 @@
 #
 # Board specific files build
 
+
+# TODO(b/132204142): Sweetberry's USB connection fails to initialize properly
+# when built using the 8.2.0 tool chain leave it with the 4.9.x.
+CROSS_COMPILE_arm:=arm-none-eabi-
+
 CHIP:=stm32
 CHIP_FAMILY:=stm32f4
 CHIP_VARIANT:=stm32f446
