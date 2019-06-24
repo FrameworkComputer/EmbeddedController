c30486beadfa9c2e9458586c6ebd5e977b85b014
ben.chen2@quanta.corp-partner.google.com <ben.chen2@quanta.corp-partner.google.com>
2019-06-24 15:06:45 +0800

    Kindred: update Led behavior
    Support Kindred LED Behavior as below
    
    Charge:          Amber on (S0/S3/S5)
    Full:            Blue on (S0/S3/S5)
    Discharge in s0: Blue on
    Discharge in s3: Amber on 1 sec off 3 sec
    Discharge in s5: off
    Erro:		 Amber on 1 sec off 1 sec
    Factory mode:	 Blue on 2 sec Amber on 2 sec
    
    BUG=b:135720559
    BRANCH=master
    TEST=none
    
    Change-Id: I2ff503e0f4b874e2a3785d0c0cf6f0fd5c7f1e3c
    Signed-off-by: ben.chen2@quanta.corp-partner.google.com
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1695564
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Tested-by: David Huang <David.Huang@quantatw.com>
    Commit-Queue: Philip Chen <philipchen@chromium.org>

--- board/kindred/led.c
+++ board/kindred/led.c
@@ -21,13 +21,14 @@ const int led_charge_lvl_2 = 95;
 struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
 	[STATE_CHARGING_LVL_1]	     = {{EC_LED_COLOR_AMBER, LED_INDEFINITE} },
 	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_AMBER, LED_INDEFINITE} },
-	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_WHITE,  LED_INDEFINITE} },
-	[STATE_DISCHARGE_S0]	     = {{LED_OFF,  LED_INDEFINITE} },
-	[STATE_DISCHARGE_S3]	     = {{LED_OFF,  LED_INDEFINITE} },
+	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_BLUE,  LED_INDEFINITE} },
+	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_BLUE,  LED_INDEFINITE} },
+	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            3 * LED_ONE_SEC} },
 	[STATE_DISCHARGE_S5]         = {{LED_OFF,  LED_INDEFINITE} },
 	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_AMBER,  1 * LED_ONE_SEC},
 					{LED_OFF,	    1 * LED_ONE_SEC} },
-	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_WHITE,   2 * LED_ONE_SEC},
+	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_BLUE,   2 * LED_ONE_SEC},
 					{EC_LED_COLOR_AMBER, 2 * LED_ONE_SEC} },
 };
 
@@ -41,12 +42,12 @@ void led_set_color_battery(enum ec_led_colors color)
 {
 	switch (color) {
 	case EC_LED_COLOR_AMBER:
-		gpio_set_level(GPIO_LED_2_L, LED_OFF_LVL);
-		gpio_set_level(GPIO_LED_1_L, LED_ON_LVL);
-		break;
-	case EC_LED_COLOR_WHITE:
-		gpio_set_level(GPIO_LED_1_L, LED_OFF_LVL);
 		gpio_set_level(GPIO_LED_2_L, LED_ON_LVL);
+		gpio_set_level(GPIO_LED_1_L, LED_OFF_LVL);
+		break;
+	case EC_LED_COLOR_BLUE:
+		gpio_set_level(GPIO_LED_1_L, LED_ON_LVL);
+		gpio_set_level(GPIO_LED_2_L, LED_OFF_LVL);
 		break;
 	default: /* LED_OFF and other unsupported colors */
 		gpio_set_level(GPIO_LED_1_L, LED_OFF_LVL);
@@ -59,7 +60,7 @@ void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
 	if (led_id == EC_LED_ID_BATTERY_LED) {
 		brightness_range[EC_LED_COLOR_AMBER] = 1;
-		brightness_range[EC_LED_COLOR_WHITE] = 1;
+		brightness_range[EC_LED_COLOR_BLUE] = 1;
 	}
 }
 
@@ -68,8 +69,8 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 	if (led_id == EC_LED_ID_BATTERY_LED) {
 		if (brightness[EC_LED_COLOR_AMBER] != 0)
 			led_set_color_battery(EC_LED_COLOR_AMBER);
-		else if (brightness[EC_LED_COLOR_WHITE] != 0)
-			led_set_color_battery(EC_LED_COLOR_WHITE);
+		else if (brightness[EC_LED_COLOR_BLUE] != 0)
+			led_set_color_battery(EC_LED_COLOR_BLUE);
 		else
 			led_set_color_battery(LED_OFF);
 	}
