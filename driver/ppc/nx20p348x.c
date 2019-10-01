0df7fdbf07a7d64a13416dc28e1a7538907216c5
Denis Brockus <dbrockus@chromium.org>
2019-10-01 10:58:29 -0600

    ppc: driver changes for FRS
    BUG=b:138599955
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I84f54f4bef9f38bc194e2a45802fb6fcf335e643
    Signed-off-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1834023
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- driver/ppc/nx20p348x.c
+++ driver/ppc/nx20p348x.c
@@ -231,7 +231,7 @@ static int nx20p348x_init(int port)
 	/* Mask interrupts for interrupt 1 register */
 	mask = ~(NX20P348X_INT1_OC_5VSRC | NX20P348X_INT1_SC_5VSRC |
 		 NX20P348X_INT1_RCP_5VSRC | NX20P348X_INT1_DBEXIT_ERR);
-#ifdef CONFIG_USBC_PPC_NX20P3481
+#if defined(CONFIG_USBC_PPC_NX20P3481) || defined(CONFIG_USBC_PPC_NX20P3483)
 	/* Unmask Fast Role Swap detect interrupt */
 	mask &= ~NX20P348X_INT1_FRS_DET;
 #endif
