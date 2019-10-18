ca3fce0e10421e3b75ca40ee8a3fed0a80d44b1a
ben.chen2@quanta.corp-partner.google.com <ben.chen2@quanta.corp-partner.google.com>
2019-10-18 09:28:09 +0800

    dood: initial files commit
    Initial files for dood are copied from bobba,
    to be revised later.
    
    BRANCH=octopus
    BUG=b:141960652
    TEST=build
    
    Change-Id: Ie26c6dfd21f420a9d66902de39d2393b0cd95fb3
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1868129
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Henry Sun <henrysun@google.com>
    Tested-by: Henry Sun <henrysun@google.com>
    Commit-Queue: Justin TerAvest <teravest@chromium.org>

---
+++ board/dood/board.h
@@ -0,0 +1,104 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Bobba board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Select Baseboard features */
+#define VARIANT_OCTOPUS_EC_NPCX796FB
+#define VARIANT_OCTOPUS_CHARGER_ISL9238
+#include "baseboard.h"
+
+/* I2C bus configuraiton */
+#define I2C_PORT_ACCEL	I2C_PORT_SENSOR
+
+/* EC console commands  */
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+
+#define CONFIG_LED_COMMON
+
+/* Sensors */
+#define CONFIG_ACCEL_KX022	/* Lid accel */
+#define CONFIG_ACCELGYRO_BMI160	/* Base accel */
+
+#define CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
+/* Sensors without hardware FIFO are in forced mode */
+#define CONFIG_ACCEL_FORCE_MODE_MASK BIT(LID_ACCEL)
+
+/* Motion Sense Task Events */
+#define CONFIG_ACCELGYRO_BMI160_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
+
+#define CONFIG_SYNC_INT_EVENT	\
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(VSYNC)
+
+#define CONFIG_LID_ANGLE
+#define CONFIG_LID_ANGLE_UPDATE
+#define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
+#define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
+
+#define CONFIG_VOLUME_BUTTONS
+#define GPIO_VOLUME_UP_L GPIO_EC_VOLUP_BTN_ODL
+#define GPIO_VOLUME_DOWN_L GPIO_EC_VOLDN_BTN_ODL
+
+#define CONFIG_TEMP_SENSOR
+#define CONFIG_THERMISTOR
+#define CONFIG_STEINHART_HART_3V3_13K7_47K_4050B
+#define CONFIG_STEINHART_HART_3V3_51K1_47K_4050B
+
+#ifndef __ASSEMBLER__
+
+/* support factory keyboard test */
+#define CONFIG_KEYBOARD_FACTORY_TEST
+extern const int keyboard_factory_scan_pins[][2];
+extern const int keyboard_factory_scan_pins_used;
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+enum adc_channel {
+	ADC_TEMP_SENSOR_AMB,		/* ADC0 */
+	ADC_TEMP_SENSOR_CHARGER,	/* ADC1 */
+	ADC_VBUS_C0,			/* ADC9 */
+	ADC_VBUS_C1,			/* ADC4 */
+	ADC_CH_COUNT
+};
+
+enum temp_sensor_id {
+	TEMP_SENSOR_BATTERY,
+	TEMP_SENSOR_AMBIENT,
+	TEMP_SENSOR_CHARGER,
+	TEMP_SENSOR_COUNT
+};
+
+/* Motion sensors */
+enum sensor_id {
+	LID_ACCEL,
+	BASE_ACCEL,
+	BASE_GYRO,
+	SENSOR_COUNT
+};
+
+/* List of possible batteries */
+enum battery_type {
+	BATTERY_LGC15,
+	BATTERY_PANASONIC_AP15O5L,
+	BATTERY_SANYO,
+	BATTERY_SONY,
+	BATTERY_SMP_AP13J7K,
+	BATTERY_PANASONIC_AC15A3J,
+	BATTERY_LGC_AP18C8K,
+	BATTERY_MURATA_AP18C4K,
+	BATTERY_LGC_AP19A8K,
+	BATTERY_LGC_G023,
+	BATTERY_TYPE_COUNT,
+};
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
