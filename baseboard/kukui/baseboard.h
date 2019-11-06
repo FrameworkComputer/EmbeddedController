53d08442e3c9bedde86f1a7ec961efd21902d115
Yilun Lin <yllin@chromium.org>
2019-11-06 13:28:15 +0800

    baseboard/kukui: Increase UART TX buffer size to 8192
    To keep buffer overflow logs.
    
    TEST=make buildall
    BUG=none
    BRANCH=kukui
    
    Change-Id: I2db611f9296aa03cfb97932f3c816003a2610ecb
    Signed-off-by: Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1899654
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- baseboard/kukui/baseboard.h
+++ baseboard/kukui/baseboard.h
@@ -154,7 +154,7 @@
 
 /* Increase tx buffer size, as we'd like to stream EC log to AP. */
 #undef CONFIG_UART_TX_BUF_SIZE
-#define CONFIG_UART_TX_BUF_SIZE 4096
+#define CONFIG_UART_TX_BUF_SIZE 8192
 
 /* To be able to indicate the device is in tablet mode. */
 #define CONFIG_TABLET_MODE
