f916f248e096648b8ccef2c14b1dd0f43f0b0e41
Abe Levkoy <alevkoy@chromium.org>
2019-11-07 18:58:35 -0800

    volteer: Adjust LED colors
    Decrease the duty cycle for the green LED when used it combinations. It
    seems to be inherently brighter than the others and overwhelms them in
    secondary colors when used at the natural duty cycle.
    
    BUG=b:139554899
    TEST=Ran ledtest; observed LEDs
    BRANCH=none
    
    Change-Id: Id02ff011bc88dc1d6370d07e951236308fce8270
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1907592
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- baseboard/volteer/led.c
+++ baseboard/volteer/led.c
@@ -20,9 +20,12 @@ struct pwm_led led_color_map[] = {
 	[EC_LED_COLOR_RED] =    {  100,   0,     0 },
 	[EC_LED_COLOR_GREEN] =  {    0, 100,     0 },
 	[EC_LED_COLOR_BLUE] =   {    0,   0,   100 },
-	[EC_LED_COLOR_YELLOW] = {  100, 100,     0 },
-	[EC_LED_COLOR_WHITE] =  {  100, 100,   100 },
-	[EC_LED_COLOR_AMBER] =  {  100,  75,     0 },
+	/* The green LED seems to be brighter than the others, so turn down
+	 * green from its natural level for these secondary colors.
+	 */
+	[EC_LED_COLOR_YELLOW] = {  100,  70,     0 },
+	[EC_LED_COLOR_WHITE] =  {  100,  70,   100 },
+	[EC_LED_COLOR_AMBER] =  {  100,  20,     0 },
 };
 
 struct pwm_led pwm_leds[] = {
@@ -36,7 +39,6 @@ struct pwm_led pwm_leds[] = {
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	/* TODO(b/139554899): Consider letting these go up to 255. */
 	brightness_range[EC_LED_COLOR_RED] = 100;
 	brightness_range[EC_LED_COLOR_GREEN] = 100;
 	brightness_range[EC_LED_COLOR_BLUE] = 100;
