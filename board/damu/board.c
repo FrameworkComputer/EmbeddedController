697a07fa83fedb9109ff207685ef6eb32504de09
Paul Ma <magf@bitland.corp-partner.google.com>
2019-11-12 13:56:32 +0800

    damu: add initial content for the build
    This initial content of damu is taken after jacuzzi. It will need
    to be revised later.
    
    BUG=b:144321985
    BRANCH=none
    TEST=make -j BOARD=damu
    
    Change-Id: Ia92a3f8e316891ab3491049e8330608052082f5e
    Signed-off-by: Paul Ma <magf@bitland.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1911267
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

---
+++ board/damu/board.c
@@ -0,0 +1,296 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "backlight.h"
+#include "button.h"
+#include "charge_manager.h"
+#include "charge_ramp.h"
+#include "charge_state.h"
+#include "charger.h"
+#include "chipset.h"
+#include "common.h"
+#include "console.h"
+#include "driver/accelgyro_bmi160.h"
+#include "driver/battery/max17055.h"
+#include "driver/bc12/pi3usb9201.h"
+#include "driver/charger/isl923x.h"
+#include "driver/sync.h"
+#include "driver/tcpm/fusb302.h"
+#include "driver/usb_mux/it5205.h"
+#include "ec_commands.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "registers.h"
+#include "spi.h"
+#include "system.h"
+#include "task.h"
+#include "tcpm.h"
+#include "timer.h"
+#include "usb_charge.h"
+#include "usb_mux.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+static void tcpc_alert_event(enum gpio_signal signal)
+{
+	schedule_deferred_pd_interrupt(0 /* port */);
+}
+
+#include "gpio_list.h"
+
+/******************************************************************************/
+/* ADC channels. Must be in the exactly same order as in enum adc_channel. */
+const struct adc_t adc_channels[] = {
+	[ADC_BOARD_ID] =  {"BOARD_ID",  3300, 4096, 0, STM32_AIN(10)},
+	[ADC_EC_SKU_ID] = {"EC_SKU_ID", 3300, 4096, 0, STM32_AIN(8)},
+	[ADC_BATT_ID] =   {"BATT_ID",   3300, 4096, 0, STM32_AIN(7)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+/******************************************************************************/
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"typec", 0, 400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"other", 1, 100, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+#define BC12_I2C_ADDR PI3USB9201_I2C_ADDR_3
+
+/* power signal list.  Must match order of enum power_signal. */
+const struct power_signal_info power_signal_list[] = {
+	{GPIO_AP_IN_SLEEP_L,   POWER_SIGNAL_ACTIVE_LOW,  "AP_IN_S3_L"},
+	{GPIO_PMIC_EC_RESETB,  POWER_SIGNAL_ACTIVE_HIGH, "PMIC_PWR_GOOD"},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+/******************************************************************************/
+/* SPI devices */
+/* TODO: to be added once sensors land via CL:1714436 */
+const struct spi_device_t spi_devices[] = {
+};
+const unsigned int spi_devices_used = ARRAY_SIZE(spi_devices);
+
+const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
+	{
+		.i2c_port = I2C_PORT_BC12,
+		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
+	},
+};
+
+/******************************************************************************/
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+	{
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_TCPC0,
+			.addr_flags = FUSB302_I2C_SLAVE_ADDR_FLAGS,
+		},
+		.drv = &fusb302_tcpm_drv,
+	},
+};
+
+static void board_hpd_status(int port, int hpd_lvl, int hpd_irq)
+{
+	/*
+	 * svdm_dp_attention() did most of the work, we only need to notify
+	 * host here.
+	 */
+	host_set_single_event(EC_HOST_EVENT_USB_MUX);
+}
+
+struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+	{
+		/* Driver uses I2C_PORT_USB_MUX as I2C port */
+		.port_addr = IT5205_I2C_ADDR1_FLAGS,
+		.driver = &it5205_usb_mux_driver,
+		.hpd_update = &board_hpd_status,
+	},
+};
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_ODL))
+		status |= PD_STATUS_TCPC_ALERT_0;
+
+	return status;
+}
+
+static int force_discharge;
+
+int board_set_active_charge_port(int charge_port)
+{
+	CPRINTS("New chg p%d", charge_port);
+
+	/* ignore all request when discharge mode is on */
+	if (force_discharge)
+		return EC_SUCCESS;
+
+	switch (charge_port) {
+	case CHARGE_PORT_USB_C:
+		/* Don't charge from a source port */
+		if (board_vbus_source_enabled(charge_port))
+			return -1;
+		break;
+	case CHARGE_PORT_NONE:
+		/*
+		 * To ensure the fuel gauge (max17055) is always powered
+		 * even when battery is disconnected, keep VBAT rail on but
+		 * set the charging current to minimum.
+		 */
+		charger_set_current(0);
+		break;
+	default:
+		panic("Invalid charge port\n");
+		break;
+	}
+
+	return EC_SUCCESS;
+}
+
+void board_set_charge_limit(int port, int supplier, int charge_ma,
+			    int max_ma, int charge_mv)
+{
+	charge_ma = (charge_ma * 95) / 100;
+	charge_set_input_current_limit(MAX(charge_ma,
+			       CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
+}
+
+int board_discharge_on_ac(int enable)
+{
+	int ret, port;
+
+	if (enable) {
+		port = CHARGE_PORT_NONE;
+	} else {
+		/* restore the charge port state */
+		port = charge_manager_get_override();
+		if (port == OVERRIDE_OFF)
+			port = charge_manager_get_active_charge_port();
+	}
+
+	ret = board_set_active_charge_port(port);
+	if (ret)
+		return ret;
+	force_discharge = enable;
+
+	return charger_discharge_on_ac(enable);
+}
+
+int pd_snk_is_vbus_provided(int port)
+{
+	/* TODO(b:138352732): read IT8801 GPIO EN_USBC_CHARGE_L */
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+void bc12_interrupt(enum gpio_signal signal)
+{
+	task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12, 0);
+}
+
+static void board_init(void)
+{
+	/* If the reset cause is external, pulse PMIC force reset. */
+	if (system_get_reset_flags() == EC_RESET_FLAG_RESET_PIN) {
+		gpio_set_level(GPIO_PMIC_FORCE_RESET_ODL, 0);
+		msleep(100);
+		gpio_set_level(GPIO_PMIC_FORCE_RESET_ODL, 1);
+	}
+
+	/* Enable TCPC alert interrupts */
+	gpio_enable_interrupt(GPIO_USB_C0_PD_INT_ODL);
+
+#ifdef SECTION_IS_RW
+	/* Enable interrupts from BMI160 sensor. */
+	gpio_enable_interrupt(GPIO_ACCEL_INT_ODL);
+#endif /* SECTION_IS_RW */
+
+	/* Enable interrupt from PMIC. */
+	gpio_enable_interrupt(GPIO_PMIC_EC_RESETB);
+
+	/* Enable BC12 interrupt */
+	gpio_enable_interrupt(GPIO_BC12_EC_INT_ODL);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/* Motion sensors */
+/* Mutexes */
+#ifdef SECTION_IS_RW
+/* TODO: to be added once sensors land via CL:1714436 */
+struct motion_sensor_t motion_sensors[] = {
+};
+const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+#endif /* SECTION_IS_RW */
+
+/* TODO(b:138640167): config charger correctly */
+int charger_is_sourcing_otg_power(int port)
+{
+	return 0;
+}
+
+/* Called on AP S5 -> S3 transition */
+static void board_chipset_startup(void)
+{
+	gpio_set_level(GPIO_EN_USBA_5V, 1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup, HOOK_PRIO_DEFAULT);
+
+static void disable_pp1800_s5_deferred(void);
+DECLARE_DEFERRED(disable_pp1800_s5_deferred);
+
+static void disable_pp1800_s5_deferred(void)
+{
+	if (power_get_state() == POWER_G3)
+		gpio_set_level(GPIO_EN_PP1800_S5_L, 1);
+	else if (power_get_state() == POWER_S5G3 ||
+			power_get_state() == POWER_S3S5 ||
+			power_get_state() == POWER_S5)
+		/* pmic is still on, wait a few seconds and try again */
+		hook_call_deferred(&disable_pp1800_s5_deferred_data,
+			SECOND);
+}
+
+/* Called on AP S3 -> S5 transition */
+static void board_chipset_shutdown(void)
+{
+	gpio_set_level(GPIO_EN_USBA_5V, 0);
+	if (board_get_version() >= 1)
+		/*
+		 * use deferred to make sure pp1800_s5 is turned off after pmic
+		 * off.
+		 */
+		hook_call_deferred(&disable_pp1800_s5_deferred_data,
+			SECOND);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
+
+void board_chipset_pre_init(void)
+{
+	if (board_get_version() >= 1)
+		gpio_set_level(GPIO_EN_PP1800_S5_L, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_chipset_pre_init, HOOK_PRIO_DEFAULT);
+
+int board_get_charger_i2c(void)
+{
+	/* TODO(b:138415463): confirm the bus allocation for future builds */
+	return board_get_version() == 1 ? 2 : 1;
+}
---
+++ board/damu/board.h
@@ -0,0 +1,137 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Configuration for Kukui */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+#define VARIANT_KUKUI_BATTERY_SMART
+#define VARIANT_KUKUI_CHARGER_ISL9238
+
+#ifndef SECTION_IS_RW
+#define VARIANT_KUKUI_NO_SENSORS
+#endif /* SECTION_IS_RW */
+
+#include "baseboard.h"
+
+/* TODO(b:135086465) led implementation */
+#undef CONFIG_LED_COMMON
+
+#define CONFIG_BATTERY_HW_PRESENT_CUSTOM
+
+#define CONFIG_CHARGER_PSYS
+
+#define CONFIG_BC12_DETECT_PI3USB9201
+
+#define CONFIG_EXTPOWER_GPIO
+#undef CONFIG_EXTPOWER_DEBOUNCE_MS
+#define CONFIG_EXTPOWER_DEBOUNCE_MS 200
+
+#define CONFIG_USB_PD_TCPM_FUSB302
+#define CONFIG_USB_PD_DISCHARGE_GPIO
+#define CONFIG_USB_PD_VBUS_MEASURE_NOT_PRESENT
+#define CONFIG_USB_PD_TCPC_LOW_POWER
+
+#define CONFIG_USB_MUX_IT5205
+
+/* Motion Sensors */
+#ifdef SECTION_IS_RW
+#define CONFIG_MAG_BMI160_BMM150
+#define CONFIG_ACCELGYRO_SEC_ADDR_FLAGS BMM150_ADDR0_FLAGS
+#define CONFIG_MAG_CALIBRATE
+#define CONFIG_ACCELGYRO_BMI160
+#define CONFIG_ACCEL_INTERRUPTS
+#define CONFIG_ACCELGYRO_BMI160_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(LID_ACCEL)
+#define CONFIG_ALS
+
+#define ALS_COUNT 1
+#define CONFIG_ALS_TCS3400
+#define CONFIG_ALS_TCS3400_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(CLEAR_ALS)
+#define CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT
+#define CONFIG_ACCEL_FORCE_MODE_MASK BIT(CLEAR_ALS)
+
+#endif /* SECTION_IS_RW */
+
+/* I2C ports */
+#define I2C_PORT_BC12               0
+#define I2C_PORT_TCPC0              0
+#define I2C_PORT_USB_MUX            0
+#define I2C_PORT_BATTERY            1
+#define I2C_PORT_CHARGER            board_get_charger_i2c()
+#define I2C_PORT_IO_EXPANDER_IT8801 1
+
+/* Enable Accel over SPI */
+#define CONFIG_SPI_ACCEL_PORT    0  /* The first SPI master port (SPI2) */
+
+#define CONFIG_KEYBOARD_PROTOCOL_MKBP
+#define CONFIG_MKBP_EVENT
+#define CONFIG_MKBP_USE_GPIO
+/* Define the MKBP events which are allowed to wakeup AP in S3. */
+#define CONFIG_MKBP_HOST_EVENT_WAKEUP_MASK \
+		(EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) |\
+		 EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON))
+
+#ifndef __ASSEMBLER__
+
+enum adc_channel {
+	/* Real ADC channels begin here */
+	ADC_BOARD_ID = 0,
+	ADC_EC_SKU_ID,
+	ADC_BATT_ID,
+	ADC_CH_COUNT
+};
+
+/* power signal definitions */
+enum power_signal {
+	AP_IN_S3_L,
+	PMIC_PWR_GOOD,
+
+	/* Number of signals */
+	POWER_SIGNAL_COUNT,
+};
+
+/* Motion sensors */
+enum sensor_id {
+	LID_ACCEL = 0,
+	LID_GYRO,
+	LID_MAG,
+	CLEAR_ALS,
+	RGB_ALS,
+	VSYNC,
+	SENSOR_COUNT,
+};
+
+enum charge_port {
+	CHARGE_PORT_USB_C,
+};
+
+enum battery_type {
+	BATTERY_PANASONIC_AC15A3J,
+	BATTERY_PANASONIC_AC16L5J,
+	BATTERY_TYPE_COUNT,
+};
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+#ifdef SECTION_IS_RO
+/* Interrupt handler for emmc task */
+void emmc_cmd_interrupt(enum gpio_signal signal);
+#endif
+
+void bc12_interrupt(enum gpio_signal signal);
+void board_reset_pd_mcu(void);
+int board_get_version(void);
+int board_is_sourcing_vbus(int port);
+
+/* returns the i2c port number of charger */
+int board_get_charger_i2c(void);
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
