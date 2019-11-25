ff9d3afc95c9b69b5ad91cf9ad17d4ac2812d1ca
xiaoqiang.zhu <xiaoqiang.zhu@bitland.corp-partner.google.com>
2019-11-25 09:54:13 +0800

    treeya : Add new battery information
    treeya need support three new batteries
     --SMP:L19M3PG1
     --LGC:L19L3PG1
     --Celxpert:L19C3PG1
    The same manufacturer(SMP) has two kinds of
    batteries, manuf_name can't specify the unique
    battery, so need to check device_name.
    
    BUG=none
    BRANCH=none
    TEST=boot treeya board with new batteries,
    charging/discharging/cutoff work as expected.
    
    Change-Id: I09e2a68961e5df92c6b6d639963ac8894eb7ec20
    Signed-off-by: xiaoqiang.zhu <xiaoqiang.zhu@bitland.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1933788
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Reviewed-by: Peichao Li <peichao.wang@bitland.corp-partner.google.com>
    Tested-by: Peichao Li <peichao.wang@bitland.corp-partner.google.com>
    Commit-Queue: Edward Hill <ecgh@chromium.org>

--- board/treeya/battery.c
+++ board/treeya/battery.c
@@ -36,6 +36,7 @@ const struct board_batt_params board_battery_info[] = {
 	[BATTERY_SMP] = {
 		.fuel_gauge = {
 			.manuf_name = "SMP",
+			.device_name = "L17M3PB0",
 			.ship_mode = {
 				.reg_addr = 0x34,
 				.reg_data = { 0x0000, 0x1000 },
@@ -63,6 +64,7 @@ const struct board_batt_params board_battery_info[] = {
 	[BATTERY_LGC] = {
 		.fuel_gauge = {
 			.manuf_name = "LGC",
+			.device_name = "L17L3PB0",
 			.ship_mode = {
 				.reg_addr = 0x34,
 				.reg_data = { 0x0000, 0x1000 },
@@ -90,6 +92,7 @@ const struct board_batt_params board_battery_info[] = {
 	[BATTERY_SUNWODA] = {
 		.fuel_gauge = {
 			.manuf_name = "SUNWODA",
+			.device_name = "L18D3PG1",
 			.ship_mode = {
 				.reg_addr = 0x34,
 				.reg_data = { 0x0000, 0x1000 },
@@ -113,7 +116,94 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 60,
 		},
 	},
+
+	/* SMP L19M3PG1 */
+	[BATTERY_SMP_1] = {
+		.fuel_gauge = {
+			.manuf_name = "SMP",
+			.device_name = "L19M3PG1",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0100,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13200, /* mV */
+			.voltage_normal		= 11520, /* mV */
+			.voltage_min		= 9000,  /* mV */
+			.precharge_current	= 200,	 /* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 60,
+			.charging_min_c		= 0,
+			.charging_max_c		= 50,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 73,
+		},
+	},
+
+	/* LGC  L19L3PG1 */
+	[BATTERY_LGC_1] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC",
+			.device_name = "L19L3PG1",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0100,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13200, /* mV */
+			.voltage_normal		= 11550, /* mV */
+			.voltage_min		= 9000,  /* mV */
+			.precharge_current	= 200,	 /* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 60,
+			.charging_min_c		= 0,
+			.charging_max_c		= 50,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 73,
+		},
+	},
+
+	/* Celxpert  L19C3PG1 */
+	[BATTERY_CEL_1] = {
+		.fuel_gauge = {
+			.manuf_name = "Celxpert",
+			.device_name = "L19C3PG1",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0100,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13200, /* mV */
+			.voltage_normal		= 11520, /* mV */
+			.voltage_min		= 9000,  /* mV */
+			.precharge_current	= 200,	 /* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 60,
+			.charging_min_c		= 0,
+			.charging_max_c		= 50,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 70,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_SMP;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_SMP_1;
