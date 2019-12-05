40b6c5de590e6290e6b516c380f6b59efbff4fa3
Craig Hesling <hesling@chromium.org>
2019-12-04 20:04:27 -0800

    board: Add nucleo-h743zi
    This brings initial support for the Nucleo-H743ZI
    development board.
    
    This configuration is similar to nocturne_fp, but without
    fingerprint sensor support, without rollback secret
    support, and with an adjusted console USART selection.
    
    The included README.md shows some quick steps to
    get a Nucleo-H743ZI up and running EC.
    
    Note that the RSA key included is only for test builds.
    It should not be used for any production builds.
    
    BRANCH=none
    BUG=none
    TEST=make BOARD=nucleo-h743zi
         make BOARD=nucleo-h743zi flash
         minicom -D/dev/ttyACM0
    
    Change-Id: Ibe4b19e51467831f53365a0a7f52bec3d854f806
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1952290
    Reviewed-by: Jett Rink <jettrink@chromium.org>

---
+++ board/nucleo-h743zi/build.mk
@@ -0,0 +1,15 @@
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+# the IC is STmicro STM32H743
+CHIP:=stm32
+CHIP_FAMILY:=stm32h7
+CHIP_VARIANT:=stm32h7x3
+
+board-y=board.o
+
+# Enable on device tests
+test-list-y=aes sha256 sha256_unrolled
