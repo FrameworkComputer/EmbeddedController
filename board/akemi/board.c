64482f3c1cedeaa2c0cbf665f851cc85e5eeae5f
dnojiri <dnojiri@chromium.org>
2019-12-10 09:04:46 -0800

    akemi: Remove CONFIG_EC_FEATURE_BOARD_OVERRIDE
    CONFIG_EC_FEATURE_BOARD_OVERRIDE is deprecated by CL:1951810.
    
    Signed-off-by: dnojiri <dnojiri@chromium.org>
    
    BUG=none
    BRANCH=none
    TEST=buildall
    
    Change-Id: I2e8a66b47bb5a29184dae0d7ba56381f5b1f59e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1960490
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Auto-Submit: Daisuke Nojiri <dnojiri@chromium.org>

--- board/akemi/board.c
+++ board/akemi/board.c
@@ -375,7 +375,7 @@ void board_overcurrent_event(int port, int is_overcurrented)
 	gpio_set_level(GPIO_USB_C_OC_ODL, !is_overcurrented);
 }
 
-uint32_t board_override_feature_flags0(uint32_t flags0)
+__override uint32_t board_override_feature_flags0(uint32_t flags0)
 {
 	uint8_t sku = get_board_sku();
 	/*
@@ -388,9 +388,4 @@ uint32_t board_override_feature_flags0(uint32_t flags0)
 		return (flags0 & ~EC_FEATURE_MASK_0(EC_FEATURE_PWM_KEYB));
 	else
 		return flags0;
-}
-
-uint32_t board_override_feature_flags1(uint32_t flags1)
-{
-	return flags1;
 }
