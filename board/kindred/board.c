80f1924c632787670d17f57a55664cd52dc40292
ben.chen2@quanta.corp-partner.google.com <ben.chen2@quanta.corp-partner.google.com>
2019-12-25 14:27:28 +0800

    Kindred: fix rotation matrices of base accel config
    Fix the base accel config
    
    BUG=b:145706600
    BRANCH=hatch
    TEST=Using ec console 'accelinfo on' verified lid angle now goes
        from 0 to 360 and swtiches to tablet mode after crossing 180 threshold.
    
    Change-Id: I5fb33e9796a64bb1c4995b65fe002132afcb5e44
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1980402
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- board/kindred/board.c
+++ board/kindred/board.c
@@ -202,8 +202,8 @@ static struct accelgyro_saved_data_t g_bma255_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 static const mat33_fp_t base_standard_ref = {
-	{ 0, FLOAT_TO_FP(1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
+	{ 0, FLOAT_TO_FP(-1), 0},
 	{ 0, 0, FLOAT_TO_FP(1)}
 };
 
