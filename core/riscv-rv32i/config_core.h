5e2d7361a65ef43cb0465b1d7e17a1aadc8b8284
Dino Li <Dino.Li@ite.com.tw>
2020-01-16 16:23:18 +0800

    core/riscv-rv32i: enable software ctz
    Because we don't support risc-v bit manipulation extension
    (CLZ/CTZ instructions) yet, so we enable the software
    implementation to fix build error while __ctzsi2 is called.
    
    BUG=none
    BRANCH=none
    TEST=no build error.
    
    Change-Id: If916010b9004f95aa4ccfd533be9539aff4c3f50
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1982295
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- core/riscv-rv32i/config_core.h
+++ core/riscv-rv32i/config_core.h
@@ -10,7 +10,12 @@
 #define BFD_ARCH riscv
 #define BFD_FORMAT "elf32-littleriscv"
 
+/*
+ * The hardware doesn't support the risc-v bit manipulation
+ * extension (CLZ/CTZ instructions) so let's use the software implementation.
+ */
 #define CONFIG_SOFTWARE_CLZ
+#define CONFIG_SOFTWARE_CTZ
 #define CONFIG_SOFTWARE_PANIC
 
 #endif /* __CROS_EC_CONFIG_CORE_H */
