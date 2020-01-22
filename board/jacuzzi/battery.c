e600975e068dc2b2706eac2a11eb2d62564c1b4a
David Huang <david.huang@quanta.corp-partner.google.com>
2020-01-22 10:36:17 +0800

    Juniper: Add new LGC battery
    Add new battery: LGC KT0020G010.
    
    BUG=b:148625782
    BRANCH=master
    TEST=Make sure battery can cutoff by console "cutoff" or "ectool batterycutoff"
    and resume by plug in adapter.
    
    Change-Id: I24cb91642863ab881f4debd5b61de8045d973112
    Signed-off-by: David Huang <David.Huang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2032549
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: David Huang <david.huang@quanta.corp-partner.google.com>
    Tested-by: David Huang <david.huang@quanta.corp-partner.google.com>

--- board/jacuzzi/battery.c
+++ board/jacuzzi/battery.c
@@ -62,6 +62,34 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 75,
 		},
 	},
+        [BATTERY_LGC_AC16L8J] = {
+                .fuel_gauge = {
+                        .manuf_name = "LGC KT0020G010",
+                        .device_name = "AP16L8J",
+                        .ship_mode = {
+                                .reg_addr = 0x3A,
+                                .reg_data = { 0xC574, 0xC574 },
+                        },
+                        .fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0,
+                        }
+                },
+                .batt_info = {
+                        .voltage_max            = 8700,
+                        .voltage_normal         = 7500,
+                        .voltage_min            = 6000,
+                        .precharge_current      = 256,
+                        .start_charging_min_c   = 0,
+                        .start_charging_max_c   = 50,
+                        .charging_min_c         = 0,
+                        .charging_max_c         = 60,
+                        .discharging_min_c      = -20,
+                        .discharging_max_c      = 75,
+                },
+        },
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
