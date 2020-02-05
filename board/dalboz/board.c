2aeb8e67d59f3fa4c7d25d330d0fffef856de61c
Martin Roth <martinroth@chromium.org>
2020-02-04 20:56:00 -0700

    dalboz: Initial EC image
    Create the initial EC image for the dalboz variant of the
    zork baseboard by copying the trembyle EC files into a new
    directory named for the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.0.1).
    
    BUG=b:147297680
    BRANCH=none
    TEST=make BOARD=dalboz
    
    Change-Id: Ibe4e21ead12dc1c721484006d8e505cb4e23bf15
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2037927
    Commit-Queue: Martin Roth <martinroth@google.com>
    Tested-by: Martin Roth <martinroth@google.com>
    Auto-Submit: Martin Roth <martinroth@google.com>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

---
+++ board/dalboz/board.c
@@ -0,0 +1,43 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Trembyle board configuration */
+
+#include "button.h"
+#include "driver/accelgyro_bmi160.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "switch.h"
+#include "system.h"
+#include "usb_charge.h"
+
+#include "gpio_list.h"
+
+/* These GPIOs moved. Temporarily detect and support the V0 HW. */
+enum gpio_signal GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L;
+enum gpio_signal GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK;
+
+void board_update_sensor_config_from_sku(void)
+{
+	int data;
+
+	/*
+	 * If the CBI EEPROM is found on the battery I2C port then we are
+	 * running on V0 HW so re-map the GPIOs that moved.
+	 */
+	if ((system_get_sku_id() == 0x7fffffff)
+	    && (i2c_read8(I2C_PORT_BATTERY, I2C_ADDR_EEPROM_FLAGS, 0, &data)
+		== EC_SUCCESS)) {
+		ccprints("V0 HW detected");
+		GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L_V0;
+		GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK_V0;
+	}
+
+	/* Enable Gyro interrupts */
+	gpio_enable_interrupt(GPIO_6AXIS_INT_L);
+}
---
+++ board/dalboz/led.c
@@ -0,0 +1,70 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ec_commands.h"
+#include "gpio.h"
+#include "led_common.h"
+#include "led_onoff_states.h"
+
+#define LED_OFF_LVL	1
+#define LED_ON_LVL	0
+
+const int led_charge_lvl_1;
+const int led_charge_lvl_2 = 100;
+
+struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
+	[STATE_CHARGING_LVL_1]       = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
+					{EC_LED_COLOR_RED, 2 * LED_ONE_SEC} },
+	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_RED, LED_INDEFINITE} },
+	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
+	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
+	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_RED, 1 * LED_ONE_SEC},
+					{LED_OFF,            3 * LED_ONE_SEC} },
+	[STATE_DISCHARGE_S5]         = {{LED_OFF,            LED_INDEFINITE} },
+	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
+					{EC_LED_COLOR_RED, 2 * LED_ONE_SEC} },
+	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
+};
+BUILD_ASSERT(ARRAY_SIZE(led_bat_state_table) == LED_NUM_STATES);
+
+const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED };
+
+const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
+
+void led_set_color_battery(enum ec_led_colors color)
+{
+	switch (color) {
+	case EC_LED_COLOR_GREEN:
+		gpio_set_level(GPIO_LED_FULL_L, LED_ON_LVL);
+		gpio_set_level(GPIO_LED_CHRG_L, LED_OFF_LVL);
+		break;
+	case EC_LED_COLOR_RED:
+		gpio_set_level(GPIO_LED_FULL_L, LED_OFF_LVL);
+		gpio_set_level(GPIO_LED_CHRG_L, LED_ON_LVL);
+		break;
+	default: /* LED_OFF and other unsupported colors */
+		gpio_set_level(GPIO_LED_FULL_L, LED_OFF_LVL);
+		gpio_set_level(GPIO_LED_CHRG_L, LED_OFF_LVL);
+		break;
+	}
+}
+
+void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
+{
+	brightness_range[EC_LED_COLOR_GREEN] = 1;
+	brightness_range[EC_LED_COLOR_RED] = 1;
+}
+
+int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
+{
+	if (brightness[EC_LED_COLOR_GREEN] != 0)
+		led_set_color_battery(EC_LED_COLOR_GREEN);
+	else if (brightness[EC_LED_COLOR_RED] != 0)
+		led_set_color_battery(EC_LED_COLOR_RED);
+	else
+		led_set_color_battery(LED_OFF);
+
+	return EC_SUCCESS;
+}
