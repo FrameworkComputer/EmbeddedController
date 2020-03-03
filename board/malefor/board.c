016780f4a5642e151b8c9d3eec4e01a1c516ace8
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-03-03 14:38:40 +0800

    malefor: Initial EC image
    Create the initial EC image for the malefor variant of the
    volteer baseboard by copying the baseboard EC files into a new
    directory named for the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.0.1).
    
    BUG=b:150653745
    BRANCH=none
    TEST=make BOARD=malefor
    
    Signed-off-by: xiong.huang <xiong.huang@bitland.corp-partner.google.com>
    Change-Id: I58c679fb1150997c199dda23fd69d23480a0fb80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2082304
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ board/malefor/board.c
@@ -0,0 +1,60 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Volteer board-specific configuration */
+
+#include "button.h"
+#include "common.h"
+#include "accelgyro.h"
+#include "driver/accel_bma2x2.h"
+#include "driver/als_tcs3400.h"
+#include "driver/sync.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "switch.h"
+#include "system.h"
+#include "task.h"
+#include "tablet_mode.h"
+#include "uart.h"
+#include "usb_pd_tbt.h"
+#include "util.h"
+
+#include "gpio_list.h" /* Must come after other header files. */
+
+static void board_init(void)
+{
+	/* TODO */
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+__override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
+{
+	/* Routing length exceeds 205mm prior to connection to re-timer */
+	if (port == USBC_PORT_C1)
+		return TBT_SS_U32_GEN1_GEN2;
+
+	/*
+	 * Thunderbolt-compatible mode not supported
+	 *
+	 * TODO (b/147726366): All the USB-C ports need to support same speed.
+	 * Need to fix once USB-C feature set is known for Volteer.
+	 */
+	return TBT_SS_RES_0;
+}
+
+__override bool board_is_tbt_usb4_port(int port)
+{
+	/*
+	 * On Proto-1 only Port 1 supports TBT & USB4
+	 *
+	 * TODO (b/147732807): All the USB-C ports need to support same
+	 * features. Need to fix once USB-C feature set is known for Volteer.
+	 */
+	return port == USBC_PORT_C1;
+}
