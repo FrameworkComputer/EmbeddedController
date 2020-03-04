38652c1d26adfa1d604aaa284f67a4d3b99ef3ff
Craig Hesling <hesling@chromium.org>
2020-03-03 17:38:26 -0800

    getversion.sh: Generalize FP targets for nucleo-*
    The nucleo-dartmonkey and future nucleo-bloonchipper will
    use the fingerprint private blobs and are designed to align
    as closely to dartmonkey and bloonchiper as possible.
    This patch makes these version strings equal too.
    
    BRANCH=none
    BUG=none
    TEST=make BOARD=nucleo-dartmonkey && strings build/nucleo-dartmonkey/ec.bin | grep dart
    TEST=make BOARD=dartmonkey && strings build/dartmonkey/ec.bin | grep dart
    TEST=make BOARD=bloonchipper && strings build/bloonchipper/ec.bin | grep bloon
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I8a4bce559a8c94222558e37b51b6a302595f29c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2086733
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- util/getversion.sh
+++ util/getversion.sh
@@ -104,7 +104,7 @@ main() {
     (cr50)
       dir_list+=( ../../third_party/tpm2 ../../third_party/cryptoc )
       ;;
-    (*_fp|dartmonkey|bloonchipper)
+    (*_fp|*dartmonkey|*bloonchipper)
       if [[ -d ../../third_party/cryptoc ]]; then
         dir_list+=( ../../third_party/cryptoc )
       fi
