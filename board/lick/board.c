c3d1ca4a3f64260862601e2342a5973d997a848e
Julia Tsai <julia.tsai@lcfc.corp-partner.google.com>
2020-03-04 23:02:41 +0800

    lick: correct sku id setting of board_is_convertible
    Set board_is_convertible to false for sku_id 1 and 255.
    
    BUG=b:150753388
    BRANCH=octopus
    TEST=make -j BOARD=lick
    
    Signed-off-by: Julia Tsai <julia.tsai@lcfc.corp-partner.google.com>
    Change-Id: Ia5f9e90338d1a1a4eb811b7ec72bd9e8aa0b62f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2087434
    Reviewed-by: Henry Sun <henrysun@google.com>
    Commit-Queue: Henry Sun <henrysun@google.com>
    Tested-by: Henry Sun <henrysun@google.com>

--- board/lick/board.c
+++ board/lick/board.c
@@ -192,7 +192,7 @@ unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 static int board_is_convertible(void)
 {
-	return sku_id != 255 || sku_id != 1;
+	return sku_id != 255 && sku_id != 1;
 }
 
 static void board_update_sensor_config_from_sku(void)
