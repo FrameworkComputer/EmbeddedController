02ebf268d260b34a2a2b8f7937d2a3db0c2a3e86
xiaoqiang.zhu <xiaoqiang.zhu@bitland.corp-partner.google.com>
2020-03-05 15:24:49 +0800

    foob : Add new battery information
    foob need support new battery
     --AEC:bq40z50-R3
    
    This CL add to support the AEC battery
    
    BUG=b:150802756
    BRANCH=master
    TEST=boot foob board with new battery,
    charging/discharging/cutoff work as expected.
    
    Signed-off-by: xiaoqiang.zhu <xiaoqiang.zhu@bitland.corp-partner.google.com>
    Change-Id: I38b201c0464f4256236d8ce10e5643fd3059410a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2087437
    Reviewed-by: Henry Sun <henrysun@google.com>
    Commit-Queue: Henry Sun <henrysun@google.com>
    Tested-by: Henry Sun <henrysun@google.com>

--- board/foob/battery.c
+++ board/foob/battery.c
@@ -168,6 +168,34 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 60,
 		},
 	},
+	/* GuoGuang 50Wh/38Wh Battery Information */
+	[BATTERY_AEC] = {
+		.fuel_gauge = {
+			.manuf_name = "AEC",
+			.ship_mode = {
+				.reg_addr = 0x0,
+				.reg_data = { 0x10, 0x10 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x2000,
+				.disconnect_val = 0x2000,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13050,
+			.voltage_normal		= 11400, /* mV */
+			.voltage_min		= 9000, /* mV */
+			.precharge_current	= 200,	/* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 60,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
