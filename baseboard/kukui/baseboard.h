6eaecf15cb0f5855413c6af70c73980dfc891f6d
Ting Shen <phoenixshen@google.com>
2020-03-13 14:01:33 +0800

    kukui: increase PD_POWER_SUPPLY_TURN_OFF_DELAY
    The default value (50ms) is not enough for kukui platform, increase it
    to 250ms.
    
    BUG=b:149808890
    TEST=plug in usb hub, plug in power cable to the hub
         verify power role swap succeed.
    BRANCH=kukui
    
    Change-Id: I487f0aeea78f2fc5de9a7dcc4a9a38cb831944cf
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2102171
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>

--- baseboard/kukui/baseboard.h
+++ baseboard/kukui/baseboard.h
@@ -205,7 +205,7 @@
 #endif
 
 #define PD_POWER_SUPPLY_TURN_ON_DELAY  30000  /* us */
-#define PD_POWER_SUPPLY_TURN_OFF_DELAY 50000  /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
 #define PD_VCONN_SWAP_DELAY 5000 /* us */
 
 /* Timer selection */
