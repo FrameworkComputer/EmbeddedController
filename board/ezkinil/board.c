a88bd6dd2ce985f15cee44e95562639a4112f283
David Huang <david.huang@quanta.corp-partner.google.com>
2020-03-24 15:21:41 +0800

    Ezkinil: Add FSUSB42UMX driver
    Add FSUSB42UMX support. This chip is used as SBU mux of usb
    c0 port.
    
    BUG=none
    BRANCH=ezkinil
    TEST=make buildall -j
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I566723de6b996652dc611262b609c08fe0d4b124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2115906
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -168,6 +168,37 @@ const struct mft_t mft_channels[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(mft_channels) == MFT_CH_COUNT);
 
+/*
+ * USB C0 port SBU mux use standalone FSUSB42UMX
+ * chip and it need a board specific driver.
+ * Overall, it will use chained mux framework.
+ */
+static int fsusb42umx_set_mux(const struct usb_mux *me, mux_state_t mux_state)
+{
+	if (mux_state & USB_PD_MUX_POLARITY_INVERTED)
+		ioex_set_level(IOEX_USB_C0_SBU_FLIP, 1);
+	else
+		ioex_set_level(IOEX_USB_C0_SBU_FLIP, 0);
+	return EC_SUCCESS;
+}
+/*
+ * .init is not necessary here because it has nothing
+ * to do. Primary mux will handle mux state so .get is
+ * not needed as well. usb_mux.c can handle the situation
+ * properly.
+ */
+const struct usb_mux_driver usbc0_sbu_mux_driver = {
+	.set = fsusb42umx_set_mux,
+};
+/*
+ * Since FSUSB42UMX is not a i2c device, .i2c_port and
+ * .i2c_addr_flags are not required here.
+ */
+const struct usb_mux usbc0_sbu_mux = {
+	.usb_port = USBC_PORT_C0,
+	.driver = &usbc0_sbu_mux_driver,
+};
+
 /*****************************************************************************
  * USB-C MUX/Retimer dynamic configuration
  */
@@ -208,7 +239,11 @@ DECLARE_HOOK(HOOK_INIT, setup_mux, HOOK_PRIO_DEFAULT);
 
 struct usb_mux usb_muxes[] = {
 	[USBC_PORT_C0] = {
-		/* USB-C0 does not have a retimer/mux */
+		.usb_port = USBC_PORT_C0,
+		.i2c_port = I2C_PORT_USB_AP_MUX,
+		.i2c_addr_flags = AMD_FP5_MUX_I2C_ADDR_FLAGS,
+		.driver = &amd_fp5_usb_mux_driver,
+		.next_mux = &usbc0_sbu_mux,
 	},
 	[USBC_PORT_C1] = {
 		/* Filled in dynamically at startup */
