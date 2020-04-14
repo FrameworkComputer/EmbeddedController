c822930e96290fe8eff6e4ede10fe90693c70f4a
Keith Short <keithshort@chromium.org>
2020-04-14 10:18:36 -0600

    halvor: Update bug tracking USB-C features
    BUG=b:153995632
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I283049e74073799ac086a16e801bd282817b5e94
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2149271
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Commit-Queue: Abe Levkoy <alevkoy@chromium.org>

--- board/halvor/board.c
+++ board/halvor/board.c
@@ -42,8 +42,8 @@ __override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
 	/*
 	 * Thunderbolt-compatible mode not supported
 	 *
-	 * TODO (b/147726366): All the USB-C ports need to support same speed.
-	 * Need to fix once USB-C feature set is known for Volteer.
+	 * TODO (b/153995632): All the USB-C ports need to support same speed.
+	 * Need to fix once USB-C feature set is known for Halvor.
 	 */
 	return TBT_SS_RES_0;
 }
@@ -51,10 +51,10 @@ __override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
 __override bool board_is_tbt_usb4_port(int port)
 {
 	/*
-	 * On Proto-1 only Port 1 supports TBT & USB4
+	 * On the volteer reference board 1 only port 1 supports TBT & USB4
 	 *
-	 * TODO (b/147732807): All the USB-C ports need to support same
-	 * features. Need to fix once USB-C feature set is known for Volteer.
+	 * TODO (b/153995632): All the USB-C ports need to support same
+	 * features. Need to fix once USB-C feature set is known for Halvor.
 	 */
 	return port == USBC_PORT_C1;
 }
