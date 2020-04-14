fee5ee977403bfc4bbd35a712dbed1d00d66a163
Ting Shen <phoenixshen@google.com>
2020-04-14 16:50:57 +0800

    asurada: enable battery / charger
    BUG=b:150341271
    TEST=boot on asurada, verify `chgstate` and `battery` looks good
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Icf1bda085307280ffb4bd96d22ed5e6e760a5336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2148852
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>

--- board/asurada/battery.c
+++ board/asurada/battery.c
@@ -8,19 +8,34 @@
 #include "gpio.h"
 
 const struct board_batt_params board_battery_info[] = {
-	[BATTERY_DUMMY] = {
+	[BATTERY_C235] = {
+		.fuel_gauge = {
+			.manuf_name = "AS3GWRc3KA",
+			.device_name = "C235-41",
+			.ship_mode = {
+				.reg_addr = 0x10,
+				.reg_data = { 0x10, 0x10 },
+			},
+			.fet = {
+				.reg_addr = 0x99,
+				.reg_mask = 0x0c,
+				.disconnect_val = 0x0c,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 8800,
+			.voltage_normal		= 7700,
+			.voltage_min		= 6000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 45,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= 0,
+			.discharging_max_c	= 60,
+		},
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_DUMMY;
-
-enum battery_present battery_hw_present(void)
-{
-	return BP_NO;
-}
-
-enum battery_present battery_is_present(void)
-{
-	return BP_NO;
-}
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_C235;
