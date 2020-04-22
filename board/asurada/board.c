78b4b148a225e558b34fdff6098f05560ef6a206
Nicolas Boichat <drinkcat@chromium.org>
2020-04-22 16:37:51 +0800

    asurada: Enable backlight GPIO
    BRANCH=master
    BUG=none
    TEST=see display on
    
    Change-Id: I363fc20242dc27ac4fd20c746fd6a57979b7cb0d
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2160237

--- board/asurada/board.c
+++ board/asurada/board.c
@@ -375,6 +375,20 @@ void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
 {
 }
 
+/* Called on AP S3 -> S0 transition */
+static void board_chipset_resume(void)
+{
+	gpio_set_level(GPIO_EC_BL_EN_OD, 1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
+
+/* Called on AP S0 -> S3 transition */
+static void board_chipset_suspend(void)
+{
+	gpio_set_level(GPIO_EC_BL_EN_OD, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
+
 /* Sub-board */
 
 static enum board_sub_board board_get_sub_board(void)
