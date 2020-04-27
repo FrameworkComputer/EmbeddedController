951e216cb1cd4f68db5a312300d1a03091ac2733
Udaya Kiran Ammu <udaykiran@google.com>
2020-04-27 11:49:58 -0700

    Quiche: Initial skeleton EC image
    This CL contains a skeleton image. Baseboard is honeybuns.
    
    BUG=b:154667612
    BRANCH=none
    TEST=make BOARD=quiche and verify that image builds successfully
    
    Signed-off-by: Udaya Kiran Ammu <udaykiran@google.com>
    Change-Id: I9873579bcda3e89ba2fda930b5dd21e8fda64cfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2168894
    Tested-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Scott Collyer <scollyer@chromium.org>

---
+++ board/quiche/board.c
@@ -0,0 +1,17 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Quiche board-specific configuration */
+
+#include "hooks.h"
+#include "switch.h"
+
+#include "gpio_list.h" /* Must come after other header files. */
+
+static void board_init(void)
+{
+	/* TODO */
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
