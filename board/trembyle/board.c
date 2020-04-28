dc5f575bdd51a022eb8e6daac74ab348620c73ba
Edward Hill <ecgh@chromium.org>
2020-04-28 10:39:08 -0600

    Zork: Configure GPIO86 EC_H1_PACKET_MODE low
    GPIO86 is EC_H1_PACKET_MODE and should be initialized low.
    Drop support for V0 HW.
    
    BUG=b:155032841
    BRANCH=none
    TEST=EC console is not read only
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: I025a280b1f7392ce952b418afdcd96a4da29e25f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2170143
    Tested-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>

--- board/trembyle/board.c
+++ board/trembyle/board.c
@@ -124,10 +124,6 @@ unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 #endif /* HAS_TASK_MOTIONSENSE */
 
-/* These GPIOs moved. Temporarily detect and support the V0 HW. */
-enum gpio_signal GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L;
-enum gpio_signal GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK;
-
 const struct pwm_t pwm_channels[] = {
 	[PWM_CH_KBLIGHT] = {
 		.channel = 3,
@@ -308,20 +304,6 @@ BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == USBC_PORT_COUNT);
 
 void setup_fw_config(void)
 {
-	int data;
-
-	/*
-	 * If the CBI EEPROM is found on the battery I2C port then we are
-	 * running on V0 HW so re-map the GPIOs that moved.
-	 */
-	if ((system_get_sku_id() == 0x7fffffff)
-	    && (i2c_read8(I2C_PORT_BATTERY, I2C_ADDR_EEPROM_FLAGS, 0, &data)
-		== EC_SUCCESS)) {
-		ccprints("V0 HW detected");
-		GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L_V0;
-		GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK_V0;
-	}
-
 	/* Enable Gyro interrupts */
 	gpio_enable_interrupt(GPIO_6AXIS_INT_L);
 
