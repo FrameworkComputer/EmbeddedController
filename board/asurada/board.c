8f44962b5a5e65e05191a06061bc11871efc2212
Eric Yilun Lin <yllin@chromium.org>
2020-05-04 14:14:51 +0800

    asurada: configure hibernate wake source.
    We have three wake sources: power button, lid open, and ACOK.
    it81202 doesn't need EC_RST_ODL as a wake source.
    
    BRANCH=master
    BUG=b:150341271
    TEST=test power button, lid open event can wake the system up.
    
    Change-Id: I4f919de661e9b2ee6caaf172f008cb608ea02b64
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2178693
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- board/asurada/board.c
+++ board/asurada/board.c
@@ -51,7 +51,9 @@ BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
 /* Wake-up pins for hibernate */
 const enum gpio_signal hibernate_wake_pins[] = {
-	GPIO_POWER_BUTTON_L, GPIO_LID_OPEN
+	GPIO_AC_PRESENT,
+	GPIO_LID_OPEN,
+	GPIO_POWER_BUTTON_L,
 };
 const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
 
