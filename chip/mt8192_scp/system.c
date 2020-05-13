9cfae101b2e6959c5f32aee1293469e441c9413f
Tzung-Bi Shih <tzungbi@chromium.org>
2020-05-13 17:34:07 +0800

    chip/mt8192_scp: add basic system settings
    Settings include:
    - default memmap
    
    BRANCH=none
    BUG=b:146213943
    TEST=make BOARD=asurada_scp
    
    Change-Id: I581a22527df579a815f895c6ba370e2e8bc0397d
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2198819
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>

--- chip/mt8192_scp/system.c
+++ chip/mt8192_scp/system.c
@@ -5,10 +5,42 @@
 
 /* System : hardware specific implementation */
 
+#include "registers.h"
 #include "system.h"
+
+static void scp_remap_init(void)
+{
+	/*
+	 *  external address	SCP address
+	 *
+	 *  0x10000000		0x60000000
+	 *  0x20000000		0x70000000
+	 *  0x30000000
+	 *  0x40000000
+	 *  0x50000000
+	 *  0x60000000		0x10000000
+	 *  0x70000000		0xA0000000
+	 *  0x80000000
+	 *  0x90000000		0x80000000
+	 *  0xA0000000		0x90000000
+	 *  0xB0000000
+	 *  0xC0000000		0x80000000
+	 *  0xD0000000		0x20000000
+	 *  0xE0000000		0x30000000
+	 *  0xF0000000		0x50000000
+	 */
+	SCP_R_REMAP_0X0123 = 0x00070600;
+	SCP_R_REMAP_0X4567 = 0x0A010000;
+	SCP_R_REMAP_0X89AB = 0x00090800;
+	SCP_R_REMAP_0XCDEF = 0x05030208;
+}
 
 void system_pre_init(void)
 {
+	scp_remap_init();
+	/* Disable jump (it has only RW) and enable MPU. */
+	/* TODO: implement MPU */
+	system_disable_jump();
 }
 
 void system_reset(int flags)
