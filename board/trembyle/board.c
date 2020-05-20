3775fb30b240a411c49f30f61a00acbc86b141a7
Edward Hill <ecgh@chromium.org>
2020-05-20 17:54:46 -0600

    Trembyle: Adjust fan speeds and temps
    Reduce fan speeds and increase temperature limits.
    
    BUG=b:154938623
    BRANCH=none
    TEST=check fan speed
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: Ic0411eb621246ecc60afa77045d284a200128cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2210832
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- board/trembyle/board.c
+++ board/trembyle/board.c
@@ -329,9 +329,9 @@ const struct fan_conf fan_conf_0 = {
 	.enable_gpio = -1,
 };
 const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 3100,
-	.rpm_start = 3100,
-	.rpm_max = 6900,
+	.rpm_min = 3000,
+	.rpm_start = 3000,
+	.rpm_max = 4900,
 };
 const struct fan_t fans[] = {
 	[FAN_CH_0] = {
@@ -343,26 +343,26 @@ BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
 
 const static struct ec_thermal_config thermal_thermistor = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(92),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
 	},
 	.temp_fan_off = C_TO_K(25),
-	.temp_fan_max = C_TO_K(50),
+	.temp_fan_max = C_TO_K(58),
 };
 
 const static struct ec_thermal_config thermal_cpu = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(85),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(92),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
 	},
 	.temp_fan_off = C_TO_K(25),
-	.temp_fan_max = C_TO_K(50),
+	.temp_fan_max = C_TO_K(58),
 };
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT];
