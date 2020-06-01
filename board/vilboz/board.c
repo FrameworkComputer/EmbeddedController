cf228dfae6ae9106be7680a571155db454e41f85
Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
2020-06-01 15:43:52 +0800

    vilboz: Initial EC image
    Create the initial EC image for the vilboz variant by copying the
    dalboz reference board EC files into a new directory named for
    the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.0.2).
    
    BUG=b:157499341
    BRANCH=none
    TEST=make BOARD=vilboz
    
    Signed-off-by: Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
    Change-Id: I511548fb432a0eb318b5f0281205c2c28ee1c7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2224680
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

---
+++ board/vilboz/board.c
@@ -0,0 +1,265 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery_smart.h"
+#include "button.h"
+#include "cros_board_info.h"
+#include "driver/accel_lis2dw12.h"
+#include "driver/accelgyro_lsm6dsm.h"
+#include "driver/ioexpander/pcal6408.h"
+#include "driver/usb_mux/ps8740.h"
+#include "driver/usb_mux/ps8743.h"
+#include "extpower.h"
+#include "fan.h"
+#include "fan_chip.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "switch.h"
+#include "system.h"
+#include "tablet_mode.h"
+#include "task.h"
+#include "usb_charge.h"
+
+/* This I2C moved. Temporarily detect and support the V0 HW. */
+int I2C_PORT_BATTERY = I2C_PORT_BATTERY_V1;
+
+/* Interrupt handler varies with DB option. */
+void (*c1_tcpc_config_interrupt)(enum gpio_signal signal) = tcpc_alert_event;
+
+void c1_tcpc_interrupt(enum gpio_signal signal)
+{
+	c1_tcpc_config_interrupt(signal);
+}
+
+#include "gpio_list.h"
+
+#ifdef HAS_TASK_MOTIONSENSE
+
+/* Motion sensors */
+static struct mutex g_lid_mutex;
+static struct mutex g_base_mutex;
+
+/* sensor private data */
+static struct stprivate_data g_lis2dwl_data;
+static struct lsm6dsm_data g_lsm6dsm_data = LSM6DSM_DATA;
+
+/* Matrix to rotate accelrator into standard reference frame */
+static const mat33_fp_t base_standard_ref = {
+	{ FLOAT_TO_FP(-1), 0, 0},
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ 0, 0, FLOAT_TO_FP(1)}
+};
+
+/* TODO(gcc >= 5.0) Remove the casts to const pointer at rot_standard_ref */
+struct motion_sensor_t motion_sensors[] = {
+	[LID_ACCEL] = {
+	 .name = "Lid Accel",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_LIS2DWL,
+	 .type = MOTIONSENSE_TYPE_ACCEL,
+	 .location = MOTIONSENSE_LOC_LID,
+	 .drv = &lis2dw12_drv,
+	 .mutex = &g_lid_mutex,
+	 .drv_data = &g_lis2dwl_data,
+	 .port = I2C_PORT_SENSOR,
+	 .i2c_spi_addr_flags = LIS2DWL_ADDR1_FLAGS,
+	 .rot_standard_ref = NULL,
+	 .default_range = 2, /* g, enough for laptop. */
+	 .min_frequency = LIS2DW12_ODR_MIN_VAL,
+	 .max_frequency = LIS2DW12_ODR_MAX_VAL,
+	 .config = {
+		 /* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 12500 | ROUND_UP_FLAG,
+		},
+		 /* Sensor on for lid angle detection */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+		},
+	},
+	},
+
+	[BASE_ACCEL] = {
+	 .name = "Base Accel",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_LSM6DSM,
+	 .type = MOTIONSENSE_TYPE_ACCEL,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &lsm6dsm_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = LSM6DSM_ST_DATA(g_lsm6dsm_data,
+			MOTIONSENSE_TYPE_ACCEL),
+	 .int_signal = GPIO_6AXIS_INT_L,
+	 .flags = MOTIONSENSE_FLAG_INT_SIGNAL,
+	 .port = I2C_PORT_SENSOR,
+	 .i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
+	 .default_range = 4, /* g, enough for laptop */
+	 .rot_standard_ref = &base_standard_ref,
+	 .min_frequency = LSM6DSM_ODR_MIN_VAL,
+	 .max_frequency = LSM6DSM_ODR_MAX_VAL,
+	 .config = {
+		 /* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 13000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		},
+		/* Sensor on for angle detection */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		},
+	 },
+	},
+
+	[BASE_GYRO] = {
+	 .name = "Base Gyro",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_LSM6DSM,
+	 .type = MOTIONSENSE_TYPE_GYRO,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &lsm6dsm_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = LSM6DSM_ST_DATA(g_lsm6dsm_data,
+			MOTIONSENSE_TYPE_GYRO),
+	.int_signal = GPIO_6AXIS_INT_L,
+	.flags = MOTIONSENSE_FLAG_INT_SIGNAL,
+	 .port = I2C_PORT_SENSOR,
+	 .i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
+	 .default_range = 1000 | ROUND_UP_FLAG, /* dps */
+	 .rot_standard_ref = &base_standard_ref,
+	 .min_frequency = LSM6DSM_ODR_MIN_VAL,
+	 .max_frequency = LSM6DSM_ODR_MAX_VAL,
+	},
+};
+
+unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+#endif /* HAS_TASK_MOTIONSENSE */
+
+/* These IO expander GPIOs vary with DB option. */
+enum gpio_signal IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT1;
+enum gpio_signal IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT1;
+
+static void pcal6408_handler(void)
+{
+	pcal6408_ioex_event_handler(IOEX_HDMI_PCAL6408);
+}
+DECLARE_DEFERRED(pcal6408_handler);
+
+void pcal6408_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&pcal6408_handler_data, 0);
+}
+
+static int board_ps8743_mux_set(const struct usb_mux *me,
+				mux_state_t mux_state)
+{
+	int rv = EC_SUCCESS;
+	int reg = 0;
+
+	rv = ps8743_read(me, PS8743_REG_MODE, &reg);
+	if (rv)
+		return rv;
+
+	/* Disable FLIP pin, enable I2C control. */
+	reg |= PS8743_MODE_FLIP_REG_CONTROL;
+	/* Disable CE_USB pin, enable I2C control. */
+	reg |= PS8743_MODE_USB_REG_CONTROL;
+	/* Disable CE_DP pin, enable I2C control. */
+	reg |= PS8743_MODE_DP_REG_CONTROL;
+
+	return ps8743_write(me, PS8743_REG_MODE, reg);
+}
+
+static void setup_fw_config(void)
+{
+	uint32_t board_version = 0;
+
+	if (cbi_get_board_version(&board_version) == EC_SUCCESS
+	    && board_version >= 2) {
+		ccprints("PS8743 USB MUX");
+		usb_muxes[USBC_PORT_C1].i2c_addr_flags = PS8743_I2C_ADDR1_FLAG;
+		usb_muxes[USBC_PORT_C1].driver = &ps8743_usb_mux_driver;
+		usb_muxes[USBC_PORT_C1].board_set = &board_ps8743_mux_set;
+	} else {
+		ccprints("PS8740 USB MUX");
+		usb_muxes[USBC_PORT_C1].i2c_addr_flags = PS8740_I2C_ADDR0_FLAG;
+		usb_muxes[USBC_PORT_C1].driver = &ps8740_usb_mux_driver;
+	}
+
+	if (ec_config_get_usb_db() == DALBOZ_DB_D_OPT2_USBA_HDMI) {
+		ccprints("DB OPT2 HDMI");
+		ioex_config[IOEX_HDMI_PCAL6408].flags = 0;
+		ioex_init(IOEX_HDMI_PCAL6408);
+		IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT2;
+		IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT2;
+		usb_port_enable[USBA_PORT_A1] = IOEX_EN_USB_A1_5V_DB_OPT2;
+		c1_tcpc_config_interrupt = pcal6408_interrupt;
+		ioex_enable_interrupt(IOEX_HDMI_CONN_HPD_3V3_DB);
+	} else {
+		ccprints("DB OPT1 USBC");
+		ioex_config[IOEX_C1_NCT3807].flags = 0;
+		ioex_init(IOEX_C1_NCT3807);
+		IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT1;
+		IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT1;
+		usb_port_enable[USBA_PORT_A1] = IOEX_EN_USB_A1_5V_DB_OPT1;
+		c1_tcpc_config_interrupt = tcpc_alert_event;
+	}
+
+	/* Enable PPC interrupts. */
+	gpio_enable_interrupt(GPIO_USB_C0_PPC_FAULT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_PPC_INT_ODL);
+
+	/* Enable TCPC interrupts. */
+	gpio_enable_interrupt(GPIO_USB_C0_TCPC_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
+
+	/* Enable BC 1.2 interrupts */
+	gpio_enable_interrupt(GPIO_USB_C0_BC12_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_BC12_INT_ODL);
+
+	if (ec_config_has_lid_angle_tablet_mode()) {
+		/* Enable Gyro interrupts */
+		gpio_enable_interrupt(GPIO_6AXIS_INT_L);
+	} else {
+		motion_sensor_count = 0;
+		/* Device is clamshell only */
+		tablet_set_mode(0);
+		/* Gyro is not present, don't allow line to float */
+		gpio_set_flags(GPIO_6AXIS_INT_L, GPIO_INPUT | GPIO_PULL_DOWN);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, setup_fw_config, HOOK_PRIO_INIT_I2C + 2);
+
+const struct pwm_t pwm_channels[] = {
+	[PWM_CH_KBLIGHT] = {
+		.channel = 3,
+		.flags = PWM_CONFIG_DSLEEP,
+		.freq = 100,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+/*
+ * If the battery is found on the V0 I2C port then re-map the battery port.
+ * Use HOOK_PRIO_INIT_I2C so we re-map before init_battery_type() and
+ * charger_chips_init() want to talk to the battery.
+ */
+static void check_v0_battery(void)
+{
+	int status;
+
+	if (i2c_read16(I2C_PORT_BATTERY_V0, BATTERY_ADDR_FLAGS,
+			SB_BATTERY_STATUS, &status) == EC_SUCCESS) {
+		ccprints("V0 HW detected");
+		I2C_PORT_BATTERY = I2C_PORT_BATTERY_V0;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, check_v0_battery, HOOK_PRIO_INIT_I2C);
---
+++ board/vilboz/board.h
@@ -0,0 +1,167 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Trembyle board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+#define VARIANT_ZORK_DALBOZ
+
+#include <stdbool.h>
+#include "baseboard.h"
+
+/*
+ * Allow dangerous commands.
+ * TODO: Remove this config before production.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+#define CONFIG_I2C_DEBUG
+
+#define CONFIG_MKBP_USE_GPIO
+
+/* Power  LEDs */
+#define CONFIG_LED_POWER_LED
+
+/* Motion sensing drivers */
+#define CONFIG_ACCELGYRO_LSM6DSM
+#define CONFIG_ACCEL_LSM6DSM_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
+#define CONFIG_ACCEL_LIS2DWL
+#define CONFIG_ACCEL_INTERRUPTS
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+#define CONFIG_TABLET_MODE
+#define CONFIG_LID_ANGLE
+#define CONFIG_LID_ANGLE_UPDATE
+#define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
+#define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
+
+
+
+/* GPIO mapping from board specific name to EC common name. */
+#define CONFIG_BATTERY_PRESENT_GPIO	GPIO_EC_BATT_PRES_ODL
+#define CONFIG_SCI_GPIO			GPIO_EC_FCH_SCI_ODL
+#define GPIO_AC_PRESENT			GPIO_ACOK_OD
+#define GPIO_CPU_PROCHOT		GPIO_PROCHOT_ODL
+#define GPIO_EC_INT_L			GPIO_EC_AP_INT_ODL
+#define GPIO_ENABLE_BACKLIGHT_L		GPIO_EC_EDP_BL_DISABLE
+#define GPIO_ENTERING_RW		GPIO_EC_ENTERING_RW
+#define GPIO_KBD_KSO2			GPIO_EC_KSO_02_INV
+#define GPIO_PCH_PWRBTN_L		GPIO_EC_FCH_PWR_BTN_L
+#define GPIO_PCH_RSMRST_L		GPIO_EC_FCH_RSMRST_L
+#define GPIO_PCH_SLP_S3_L		GPIO_SLP_S3_L
+#define GPIO_PCH_SLP_S5_L		GPIO_SLP_S5_L
+#define GPIO_PCH_SYS_PWROK		GPIO_EC_FCH_PWROK
+#define GPIO_PCH_WAKE_L			GPIO_EC_FCH_WAKE_L
+#define GPIO_POWER_BUTTON_L		GPIO_EC_PWR_BTN_ODL
+#define GPIO_S0_PGOOD			GPIO_S0_PWROK_OD
+#define GPIO_S5_PGOOD			GPIO_EC_PWROK_OD
+#define GPIO_SYS_RESET_L		GPIO_EC_SYS_RST_L
+#define GPIO_VOLUME_DOWN_L		GPIO_VOLDN_BTN_ODL
+#define GPIO_VOLUME_UP_L		GPIO_VOLUP_BTN_ODL
+#define GPIO_WP_L			GPIO_EC_WP_L
+#define GPIO_PACKET_MODE_EN		GPIO_EC_H1_PACKET_MODE
+
+#ifndef __ASSEMBLER__
+
+/* This I2C moved. Temporarily detect and support the V0 HW. */
+extern int I2C_PORT_BATTERY;
+
+enum battery_type {
+	BATTERY_SMP,
+	BATTERY_LGC,
+	BATTERY_CEL,
+	BATTERY_TYPE_COUNT,
+};
+
+enum pwm_channel {
+	PWM_CH_KBLIGHT = 0,
+	PWM_CH_COUNT
+};
+
+
+/*****************************************************************************
+ * CBI EC FW Configuration
+ */
+#include "cbi_ec_fw_config.h"
+
+/**
+ * DALBOZ_MB_USBAC
+ *	USB-A0  Speed: 5 Gbps
+ *		Retimer: none
+ *	USB-C0  Speed: 5 Gbps
+ *		Retimer: none
+ *		TCPC: NCT3807
+ *		PPC: AOZ1380
+ *		IOEX: TCPC
+ */
+enum ec_cfg_usb_mb_type {
+	DALBOZ_MB_USBAC = 0,
+};
+
+/**
+ * DALBOZ_DB_D_OPT1_USBAC
+ *	USB-A1  Speed: 5 Gbps
+ *		Retimer: TUSB522
+ *	USB-C1  Speed: 5 Gbps
+ *		Retimer: PS8740
+ *		TCPC: NCT3807
+ *		PPC: NX20P3483
+ *		IOEX: TCPC
+ *	HDMI    Exists: no
+ *		Retimer: none
+ *		MST Hub: none
+ *
+ * DALBOZ_DB_D_OPT2_USBA_HDMI
+ *	USB-A1  Speed: 5 Gbps
+ *		Retimer: TUSB522
+ *	USB-C1  none
+ *		IOEX: PCAL6408
+ *	HDMI    Exists: yes
+ *		Retimer: PI3HDX1204
+ *		MST Hub: none
+ */
+enum ec_cfg_usb_db_type {
+	DALBOZ_DB_D_OPT1_USBAC = 0,
+	DALBOZ_DB_D_OPT2_USBA_HDMI = 1,
+};
+
+
+#define HAS_USBC1 \
+			(BIT(DALBOZ_DB_D_OPT1_USBAC))
+
+static inline bool ec_config_has_usbc1(void)
+{
+	return !!(BIT(ec_config_get_usb_db()) &
+		  HAS_USBC1);
+}
+
+
+#define HAS_USBC1_RETIMER_PS8740 \
+			(BIT(DALBOZ_DB_D_OPT1_USBAC))
+
+static inline bool ec_config_has_usbc1_retimer_ps8740(void)
+{
+	return !!(BIT(ec_config_get_usb_db()) &
+		  HAS_USBC1_RETIMER_PS8740);
+}
+
+#define HAS_HDMI_RETIMER_PI3HDX1204 \
+			(BIT(DALBOZ_DB_D_OPT2_USBA_HDMI))
+
+static inline bool ec_config_has_hdmi_retimer_pi3hdx1204(void)
+{
+	return !!(BIT(ec_config_get_usb_db()) &
+		  HAS_HDMI_RETIMER_PI3HDX1204);
+}
+
+/* These IO expander GPIOs vary with DB option. */
+extern enum gpio_signal IOEX_USB_A1_RETIMER_EN;
+extern enum gpio_signal IOEX_USB_A1_CHARGE_EN_DB_L;
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
