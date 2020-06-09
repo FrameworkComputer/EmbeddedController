f6392851299ee41e006febcf0029d00f1b31c5be
Scott Chao <scott.chao@bitland.corp-partner.google.com>
2020-06-09 13:36:03 +0800

    kakadu: turn off Vconn boost gpio pin when S5/G3
    It should be ~0V in G3, 5V in S3/S0. And this pin can solve apple HDMI dongle issue.
    
    BUG=b:156570690
    BRANCH=kukui
    TEST=make -j BOARD=kakadu
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Change-Id: I3bd23a0534d0d63fe22bf26ed3a24b34b73fa142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2235235
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- power/mt8183.c
+++ power/mt8183.c
@@ -109,6 +109,9 @@ static const struct power_seq_op s3s5_power_seq[] = {
 	{ GPIO_AP_SYS_RST_L, 0, 0 },
 	/* Assert watchdog to PMIC (there may be a 1.6ms debounce) */
 	{ GPIO_PMIC_WATCHDOG_L, 0, 3 },
+#ifdef BOARD_KAKADU
+        { GPIO_USB_C0_VCONN_EN_OD, 0, 0 },
+#endif
 };
 
 static int forcing_shutdown;
