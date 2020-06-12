f9645b2b4ccf45b91ebaff2a7395f1e8f2406797
Casper Chang <casper.chang@bitland.corp-partner.google.com>
2020-06-12 23:51:11 +0800

    Damu: fix lid sensor rotation matrix
    BUG=b:147689571
    BRANCH=kukui
    TEST=make -j BOARD=damu
    TEST=make buildall
    TEST=boot into OS and screen show normally.
    
    Change-Id: Ib163eb7edee9c79efbf650b9889c6bb16fd3cd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2241090
    Tested-by: Casper Chang <casper.chang@bitland.corp-partner.google.com>
    Reviewed-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Reviewed-by: Li-jen Chen <lijen@google.com>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Commit-Queue: Casper Chang <casper.chang@bitland.corp-partner.google.com>

--- board/damu/board.c
+++ board/damu/board.c
@@ -343,8 +343,8 @@ static const mat33_fp_t base_standard_ref = {
 };
 
 static const mat33_fp_t lid_standard_ref = {
-	{FLOAT_TO_FP(-1), 0, 0},
-	{0, FLOAT_TO_FP(-1), 0},
+	{FLOAT_TO_FP(1), 0, 0},
+	{0, FLOAT_TO_FP(1), 0},
 	{0, 0, FLOAT_TO_FP(1) }
 };
 
