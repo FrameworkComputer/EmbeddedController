9f3a2599ad983944bfe979e3509c70efd2663c45
Josh Tsai <josh_tsai@compal.com>
2020-07-13 14:47:16 +0800

    pompom: implement LED behavior
    Implement the EC LED code dependent on spec.
    
    BUG=b:160744331
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: Josh Tsai <josh_tsai@compal.corp-partner.google.com>
    Change-Id: I727981464c5df078351aa6df76a4040272566351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2297215
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>

--- board/pompom/led.c
+++ board/pompom/led.c
@@ -17,13 +17,12 @@
 #include "system.h"
 #include "util.h"
 
-#define LED_ONE_SEC (1000 / HOOK_TICK_INTERVAL_MS)
-
-#define BAT_LED_ON 1
-#define BAT_LED_OFF 0
+#define LED_ON_LVL 1
+#define LED_OFF_LVL 0
 
 const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_LEFT_LED,
+	EC_LED_ID_BATTERY_LED,
+	EC_LED_ID_POWER_LED,
 };
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
@@ -31,32 +30,49 @@ const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 enum led_color {
 	LED_OFF = 0,
 	LED_AMBER,
-	LED_BLUE,
+	LED_WHITE,
 	LED_COLOR_COUNT  /* Number of colors, not a color itself */
 };
 
-static void led_set_color(enum led_color color)
+static void led_set_color_battery(enum led_color color)
 {
-	gpio_set_level(GPIO_EC_CHG_LED_Y_C1,
-		(color == LED_AMBER) ? BAT_LED_ON : BAT_LED_OFF);
-	gpio_set_level(GPIO_EC_CHG_LED_B_C1,
-		(color == LED_BLUE) ? BAT_LED_ON : BAT_LED_OFF);
+	gpio_set_level(GPIO_EC_CHG_LED_Y_C0,
+		(color == LED_AMBER) ? LED_ON_LVL : LED_OFF_LVL);
+	gpio_set_level(GPIO_EC_CHG_LED_W_C0,
+		(color == LED_WHITE) ? LED_ON_LVL : LED_OFF_LVL);
+}
+
+void led_set_color_power(enum led_color color)
+{
+	gpio_set_level(GPIO_EC_PWR_LED_W,
+		(color == LED_WHITE) ? LED_ON_LVL : LED_OFF_LVL);
 }
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	brightness_range[EC_LED_COLOR_AMBER] = 1;
-	brightness_range[EC_LED_COLOR_BLUE] = 1;
+	if (led_id == EC_LED_ID_BATTERY_LED) {
+		brightness_range[EC_LED_COLOR_AMBER] = 1;
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+	} else if (led_id == EC_LED_ID_POWER_LED) {
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+	}
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
-	if (brightness[EC_LED_COLOR_BLUE] != 0)
-		led_set_color(LED_BLUE);
-	else if (brightness[EC_LED_COLOR_AMBER] != 0)
-		led_set_color(LED_AMBER);
-	else
-		led_set_color(LED_OFF);
+	if (led_id == EC_LED_ID_BATTERY_LED) {
+		if (brightness[EC_LED_COLOR_WHITE] != 0)
+			led_set_color_battery(LED_WHITE);
+		else if (brightness[EC_LED_COLOR_AMBER] != 0)
+			led_set_color_battery(LED_AMBER);
+		else
+			led_set_color_battery(LED_OFF);
+	} else if (led_id == EC_LED_ID_POWER_LED) {
+		if (brightness[EC_LED_COLOR_WHITE] != 0)
+			led_set_color_power(LED_WHITE);
+		else
+			led_set_color_power(LED_OFF);
+	}
 
 	return EC_SUCCESS;
 }
@@ -76,61 +92,76 @@ static void board_led_set_battery(void)
 		color = LED_AMBER;
 		break;
 	case PWR_STATE_DISCHARGE:
-		if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
-			/* Discharging in S3: Amber 1 sec, off 3 sec */
-			period = (1 + 3) * LED_ONE_SEC;
-			battery_ticks = battery_ticks % period;
-			if (battery_ticks < 1 * LED_ONE_SEC)
-				color = LED_AMBER;
-			else
-				color = LED_OFF;
-		} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
-			/* Discharging in S5: off */
-			color = LED_OFF;
-		} else if (chipset_in_state(CHIPSET_STATE_ON)) {
-			/* Discharging in S0: Blue on */
-			color = LED_BLUE;
-		}
+		/* Discharging : off */
+		color = LED_OFF;
 		break;
 	case PWR_STATE_ERROR:
 		/* Battery error: Amber 1 sec, off 1 sec */
-		period = (1 + 1) * LED_ONE_SEC;
+		period = (1 + 1);
 		battery_ticks = battery_ticks % period;
-		if (battery_ticks < 1 * LED_ONE_SEC)
+		if (battery_ticks < 1)
 			color = LED_AMBER;
 		else
 			color = LED_OFF;
 		break;
 	case PWR_STATE_CHARGE_NEAR_FULL:
-		/* Full Charged: Blue on */
-		color = LED_BLUE;
+		/* Full Charged: White on */
+		color = LED_WHITE;
 		break;
 	case PWR_STATE_IDLE: /* External power connected in IDLE */
 		if (chflags & CHARGE_FLAG_FORCE_IDLE) {
-			/* Factory mode: Blue 2 sec, Amber 2 sec */
-			period = (2 + 2) * LED_ONE_SEC;
+			/* Factory mode: White 2 sec, Amber 2 sec */
+			period = (2 + 2);
 			battery_ticks = battery_ticks % period;
-			if (battery_ticks < 2 * LED_ONE_SEC)
-				color = LED_BLUE;
+			if (battery_ticks < 2)
+				color = LED_WHITE;
 			else
 				color = LED_AMBER;
 		} else
-			color = LED_BLUE;
+			color = LED_WHITE;
 		break;
 	default:
 		/* Other states don't alter LED behavior */
 		break;
 	}
 
-	led_set_color(color);
+	led_set_color_battery(color);
+}
+
+static void board_led_set_power(void)
+{
+	static int power_ticks;
+	int color = LED_OFF;
+	int period = 0;
+
+	power_ticks++;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+		/* S3: On 1 sec, Off 3 sec */
+		period = (1 + 3);
+		power_ticks = power_ticks % period;
+		if (power_ticks < 1)
+			color = LED_WHITE;
+		else
+			color = LED_OFF;
+	} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		/* S5: LED off */
+		color = LED_OFF;
+	} else if (chipset_in_state(CHIPSET_STATE_ON)) {
+		/* S0: LED on */
+		color = LED_WHITE;
+	}
+
+	led_set_color_power(color);
 }
 
 /* Called by hook task every TICK */
 static void led_tick(void)
 {
 	board_led_set_battery();
+	board_led_set_power();
 }
-DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_SECOND, led_tick, HOOK_PRIO_DEFAULT);
 
 void led_control(enum ec_led_id led_id, enum ec_led_state state)
 {
@@ -141,14 +172,14 @@ void led_control(enum ec_led_id led_id, enum ec_led_state state)
 		return;
 
 	if (state == LED_STATE_RESET) {
-		led_auto_control(EC_LED_ID_LEFT_LED, 1);
+		led_auto_control(EC_LED_ID_BATTERY_LED, 1);
 		board_led_set_battery();
 		return;
 	}
 
-	color = state ? LED_BLUE : LED_OFF;
+	color = state ? LED_WHITE : LED_OFF;
 
-	led_auto_control(EC_LED_ID_LEFT_LED, 0);
+	led_auto_control(EC_LED_ID_BATTERY_LED, 0);
 
-	led_set_color(color);
+	led_set_color_battery(color);
 }
