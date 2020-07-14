29f5f79610663bafb208b7251488e00461470f1b
hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
2020-07-14 19:30:58 +0800

    lingcod: Initial EC image
    Create the initial EC image for the lingcod variant by copying the
    malefor reference board EC files into a new directory named for
    the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.0.3).
    
    BUG=b:161103776
    BRANCH=none
    TEST=make BOARD=lingcod
    
    Signed-off-by: hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
    Change-Id: I8b1d7f2deff74dfac5bcb8f32684c53448fff576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2297073
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ board/lingcod/build.mk
@@ -0,0 +1,33 @@
+# -*- makefile -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=npcx
+CHIP_FAMILY:=npcx7
+CHIP_VARIANT:=npcx7m6fc
+BASEBOARD:=volteer
+
+# TODO: b/143375057 - Remove this code after power on.
+#
+# Temporary for board power on.  Provide a Volteer specific make option
+# to enable the power signal GPIOs that are not stuffed by default.  This
+# is a backup if board logic power sequencing needs to be adjusted.
+#
+# Set the following variable to 'y' to enable the Volteer optional power signals
+VOLTEER_POWER_SEQUENCE=
+ifneq (,$(VOLTEER_POWER_SEQUENCE))
+CFLAGS_BASEBOARD+=-DVOLTEER_POWER_SEQUENCE
+endif
+
+# Force changes to VOLTEER_POWER_SEQUENCE variable to trigger a full build.
+ENV_VARS := VOLTEER_POWER_SEQUENCE
+
+
+board-y=board.o
+board-y+=battery.o
+board-y+=led.o
+board-$(VOLTEER_POWER_SEQUENCE)+=power_sequence.o
