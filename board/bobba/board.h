32d6184d3ceefedc86b4a1997c55453ef5fc52b7
David Huang <david.huang@quanta.corp-partner.google.com>
2020-07-23 17:04:46 +0800

    Bobba: set SYV682X HV_ILIM to 5.5A
    Set CONFIG_SYV682X_HV_ILIM to 5.5A on bobba.
    
    BUG=b:161762373
    BRANCH=master
    TEST=Use ppc_dump <port> to check the setting correct.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I87f38ddd65f1908e230110f3ba2bc0f4e1a73057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2315952
    Reviewed-by: Marco Chen <marcochen@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Marco Chen <marcochen@chromium.org>

--- board/bobba/board.h
+++ board/bobba/board.h
@@ -69,6 +69,8 @@
 /* Additional PPC second source */
 #define CONFIG_USBC_PPC_SYV682X
 #define CONFIG_USBC_PPC_DEDICATED_INT
+#undef CONFIG_SYV682X_HV_ILIM
+#define CONFIG_SYV682X_HV_ILIM SYV682X_HV_ILIM_5_50
 
 #ifndef __ASSEMBLER__
 
