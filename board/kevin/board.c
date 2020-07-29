bcc2c00d5628e70a4f5c8094df22daef96c9c6c2
Jack Rosenthal <jrosenth@chromium.org>
2020-07-29 09:31:41 -0600

    board: clear out disabled boards left for building utils
    These boards were removed from buildall as they ran out of flash
    space, but we kept the board directory around so that we could
    continue to build the ec-utils ebuild, which used to require the board
    directory to build tools like ectool (this was fixed in CL:2283999).
    
    Since we've detangled the host utils ebuilds, these directories can
    now be removed.
    
    BUG=chromium:1035536
    BRANCH=none
    TEST=buildall
    
    Change-Id: I736b323f9e977d74ca31788ed320befa2289a9dc
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2326304
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- board/kevin/board.c
+++
@@ -1,9 +0,0 @@
-/* Copyright 2016 The Chromium OS Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-/* Board is only valid for host tools */
-#ifndef HOST_TOOLS_BUILD
-#error "Can only build for host tools"
-#endif
