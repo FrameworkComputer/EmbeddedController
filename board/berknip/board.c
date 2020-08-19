9f36f26717844dfedb02aa4e50c8d199c9121f50
Edward Hill <ecgh@chromium.org>
2020-08-18 21:02:18 -0600

    Berknip: Fix HDMI retimer suspend / resume
    BUG=b:165191082
    BRANCH=none
    TEST=none
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: If0619e61faa36e67e4e73c1cc59b296f6a039075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2364115
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>

--- board/berknip/board.c
+++ board/berknip/board.c
@@ -14,6 +14,7 @@
 #include "driver/accelgyro_bmi_common.h"
 #include "driver/accel_kionix.h"
 #include "driver/accel_kx022.h"
+#include "driver/retimer/pi3hdx1204.h"
 #include "driver/retimer/tusb544.h"
 #include "driver/temp_sensor/sb_tsi.h"
 #include "driver/usb_mux/amd_fp5.h"
@@ -161,22 +162,31 @@ const int usb_port_enable[USBA_PORT_COUNT] = {
 };
 
 /*****************************************************************************
- * Retimers
+ * Board suspend / resume
  */
 
-static void retimers_on(void)
+static void board_chipset_resume(void)
 {
-	/* hdmi retimer power on */
-	ioex_set_level(IOEX_HDMI_POWER_EN_DB, 1);
+	if (ec_config_has_hdmi_retimer_pi3hdx1204()) {
+		ioex_set_level(IOEX_HDMI_POWER_EN_DB, 1);
+		msleep(PI3HDX1204_POWER_ON_DELAY_MS);
+		pi3hdx1204_enable(I2C_PORT_TCPC1,
+				  PI3HDX1204_I2C_ADDR_FLAGS,
+				  1);
+	}
 }
-DECLARE_HOOK(HOOK_CHIPSET_RESUME, retimers_on, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 
-static void retimers_off(void)
+static void board_chipset_suspend(void)
 {
-	/* hdmi retimer power off */
-	ioex_set_level(IOEX_HDMI_POWER_EN_DB, 0);
+	if (ec_config_has_hdmi_retimer_pi3hdx1204()) {
+		pi3hdx1204_enable(I2C_PORT_TCPC1,
+				  PI3HDX1204_I2C_ADDR_FLAGS,
+				  0);
+		ioex_set_level(IOEX_HDMI_POWER_EN_DB, 0);
+	}
 }
-DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, retimers_off, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
 
 /*
  * USB C0 port SBU mux use standalone PI3USB221
