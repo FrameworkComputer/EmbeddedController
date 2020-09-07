af64cc766b654edd8b3bbd71760f8cbd30a68a98
Devin Lu <devin.lu@quantatw.com>
2020-09-07 20:34:28 +0800

    Meep: Set SYV682x high voltage path ilimit to 5.5A
    BUG=b:162221222
    BRANCH=firmware-octopus-11297.B
    TEST=EC console ppc_dump <port> to make sure setting.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I3840e6a86563d2e7faa6eb3c12c51925718ef306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2396098
    Reviewed-by: Marco Chen <marcochen@chromium.org>

--- board/meep/board.h
+++ board/meep/board.h
@@ -54,6 +54,8 @@
 /* Additional PPC second source */
 #define CONFIG_USBC_PPC_SYV682X
 #define CONFIG_USBC_PPC_DEDICATED_INT
+#undef CONFIG_SYV682X_HV_ILIM
+#define CONFIG_SYV682X_HV_ILIM SYV682X_HV_ILIM_5_50
 
 /* Additional TCPC second source in Port 1 */
 #define CONFIG_USB_PD_TCPM_MULTI_PS8XXX
