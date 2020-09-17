be80d749b6bfbc28f19583ad7f6736f7ed76f1d4
Wai-Hong Tam <waihong@google.com>
2020-09-16 20:03:29 -0700

    Lazor: Support multiple PS8xxx TCPC and switch to PS8805 on rev3+
    Lazor rev3+ switches the TCPC to PS8805. This CL makes it support
    multiple TCPC on a single image.
    
    BRANCH=None
    BUG=b:168682309
    TEST=Built Lazor image correctly.
    
    Change-Id: I2fc93ae18051e8950c2a7975e43771c238c22aad
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2415631
    Reviewed-by: Alexandru M Stan <amstan@chromium.org>

--- board/lazor/board.c
+++ board/lazor/board.c
@@ -509,6 +509,16 @@ static void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
+__override uint16_t board_get_ps8xxx_product_id(int port)
+{
+	/* Lazor (SKU_ID: 0, 1, 2, 3) rev 3+ changes TCPC to PS8805 */
+	if ((sku_id == 0 || sku_id == 1 || sku_id == 2 || sku_id == 3) &&
+	    system_get_board_version() >= 3)
+		return PS8805_PRODUCT_ID;
+
+	return PS8751_PRODUCT_ID;
+}
+
 void board_tcpc_init(void)
 {
 	/* Only reset TCPC if not sysjump */
