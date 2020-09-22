bca9bc430210c287b23fc222188062843c7adada
Jett Rink <jettrink@chromium.org>
2020-09-22 17:07:42 -0600

    volteer: initial project for volteer
    This prints hello world. Make sure to flash zephyr_volteer.bin instead
    of zephyr.bin to the device with flash_ec script.
    
    BUG=b:169182500
    TEST=flash zephyr_volteer.bin with flash_ec using servo micro and view
    UART output on EC console.
    
    Change-Id: I5094cd06824d29a7abc5526e9dbc921253854f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/zephyr-chrome/+/2427408
    Commit-Queue: Jett Rink <jettrink@chromium.org>
    Tested-by: Jett Rink <jettrink@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2630127
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

---
+++ zephyr/projects/volteer/boards/arm/volteer/board.cmake
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# This is file path you actually flash to the target (e.g. zephyr_volteer.bin)
+set(TARGET_IMAGE_FILE  ${PROJECT_BINARY_DIR}/zephyr_volteer.bin)
