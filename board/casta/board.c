c0a778cef35a9b06be6014121c18cb26d3691184
YongBeum.Ha <ybha@samsung.corp-partner.google.com>
2020-09-25 18:58:49 +0900

    Casta : Write input current limit on AC removal
    Input current limit is set to default when AC is removed.
    It needs to change input current on AC removal.
    
    BUG=b:169259388
    BRANCH=firmware-octopus-11297.B
    TEST=None
    
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>
    Change-Id: Ia6d6a28bb418b475513bd541498646f2e1cc70dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2430905
    Tested-by: Patryk Duda <patrykd@google.com>
    Commit-Queue: Dossym Nurmukhanov <dossym@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/casta/board.c
+++ board/casta/board.c
@@ -166,6 +166,19 @@ static void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_INIT_I2C);
 
+static void set_input_limit_on_ac_removal(void)
+{
+        if(extpower_is_present())
+                return;
+
+        if(get_cbi_ssfc_charger() != SSFC_CHARGER_BQ25710)
+                return;
+
+        charger_set_input_current(0, CONFIG_CHARGER_INPUT_CURRENT);
+
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, set_input_limit_on_ac_removal, HOOK_PRIO_DEFAULT);
+
 void board_overcurrent_event(int port, int is_overcurrented)
 {
 	/* Check that port number is valid. */
