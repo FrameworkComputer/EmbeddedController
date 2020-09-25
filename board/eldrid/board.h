365d4312b09204699c53829185fadffd529fde3a
Abe Levkoy <alevkoy@chromium.org>
2020-09-25 16:20:50 +0000

    Revert "volteer: Move USB PID to board files"
    This reverts commit 6ed9b65ae38977118f0d5f02d6f5c61da23c5f24.
    
    Reason for revert: Historically, Octopus variants used the baseboard
    VID and PID.
    
    Original change's description:
    > volteer: Move USB PID to board files
    >
    > Add TODOs to update variants boards.
    >
    > BUG=none
    > TEST=make buildall
    > BRANCH=none
    >
    > Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    > Change-Id: If0d8d0dd544c06114f355dcf281b2841a8397aee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2417348
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    
    BUG=b:168826898,b:168827570,b:168827133,b:168827128,b:168827420
    BUG=b:168827418,b:168826886,b:168826868,b:168827029,b:168826914
    
    Change-Id: I5b9b1cee25e34a4b922fd604b49deabc9591b4e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2431687
    Tested-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Abe Levkoy <alevkoy@chromium.org>

--- board/eldrid/board.h
+++ board/eldrid/board.h
@@ -57,8 +57,6 @@
 #define CONFIG_LID_ANGLE_SENSOR_LID		LID_ACCEL
 
 /* USB Type C and USB PD defines */
-/* TODO(b/168826914): Don't use Volteer PID. */
-#define CONFIG_USB_PID 0x503E
 #define CONFIG_USB_PD_PORT_MAX_COUNT			2
 
 /*
