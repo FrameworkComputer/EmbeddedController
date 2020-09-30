6efb091912dd5ba1421e2717c56cf020b203687a
Scott Collyer <scollyer@google.com>
2020-09-30 10:45:34 -0700

    usb_mux: Remove ec host command dependency
    This CL removes EC host command dependency for the virtual type-c mux.
    
    BUG=None
    BRANCH=None
    TEST=make BOARD=quiche
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I7d5107b60310456cb23393a443fb4f303106954b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2441390
    Tested-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Scott Collyer <scollyer@chromium.org>

--- driver/usb_mux/virtual.c
+++ driver/usb_mux/virtual.c
@@ -27,8 +27,9 @@ static mux_state_t virtual_mux_state[CONFIG_USB_PD_PORT_MAX_COUNT];
 static inline void virtual_mux_update_state(int port, mux_state_t mux_state)
 {
 	virtual_mux_state[port] = mux_state;
-
+#ifdef CONFIG_HOSTCMD_EVENTS
 	host_set_single_event(EC_HOST_EVENT_USB_MUX);
+#endif
 }
 
 static int virtual_init(const struct usb_mux *me)
