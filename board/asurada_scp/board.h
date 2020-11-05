f898db15828f720c1911b70b8a2e9985a1d8ea52
Tzung-Bi Shih <tzungbi@chromium.org>
2020-11-05 16:44:46 +0800

    asurada_scp: re-layout memory regions
    Uses the bottom-most (288 + 8) * 2 bytes for IPI shared buffer.
    - IPI ID (4 bytes)
    - IPI payload length (4 bytes)
    - IPI payload (288 bytes)
    
    BRANCH=none
    BUG=b:156222508
    BUG=b:156222459
    TEST=make BOARD=asurada_scp
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Iaec5977c602e92ff66e416d67a3d1b78b729173d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2519244
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>

--- board/asurada_scp/board.h
+++ board/asurada_scp/board.h
@@ -10,7 +10,7 @@
 
 #define CC_DEFAULT (CC_ALL & ~(CC_MASK(CC_HOSTCMD) | CC_MASK(CC_IPI)))
 
-#define CONFIG_FLASH_SIZE 0x58000
+#define CONFIG_FLASH_SIZE CONFIG_RAM_BASE
 #define CONFIG_LTO
 #define CONFIG_UART_CONSOLE 0
 
@@ -20,27 +20,24 @@
  * | ROM vectortable, .text, .rodata, .data LMA
  * +-------------------- 0x58000
  * | RAM .bss, .data
- * +-------------------- 0x7BDB0
+ * +-------------------- 0x0ffc00
+ * | Reserved (padding for 1k-alignment)
+ * +-------------------- 0x0ffdb0
  * | IPI shared buffer with AP (288 + 8) * 2
- * +-------------------- 0x7C000
- * | 8KB I-CACHE
- * +-------------------- 0x7E000
- * | 8KB D-CACHE
- * +-------------------- 0x80000
+ * +-------------------- 0x100000
  */
-#define ICACHE_BASE 0x7C000
 #define CONFIG_ROM_BASE 0x0
 #define CONFIG_RAM_BASE 0x58000
 #define CONFIG_ROM_SIZE (CONFIG_RAM_BASE - CONFIG_ROM_BASE)
-#define CONFIG_RAM_SIZE (CONFIG_IPC_SHARED_OBJ_ADDR - CONFIG_RAM_BASE)
-#define CONFIG_CODE_RAM_SIZE CONFIG_RAM_BASE
-#define CONFIG_DATA_RAM_SIZE (ICACHE_BASE - CONFIG_RAM_BASE)
-#define CONFIG_RO_MEM_OFF 0
+#define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - \
+			 CONFIG_RAM_BASE)
+
+#define SCP_FW_END 0x100000
 
 /* IPI configs */
 #define CONFIG_IPC_SHARED_OBJ_BUF_SIZE 288
 #define CONFIG_IPC_SHARED_OBJ_ADDR                                             \
-	(ICACHE_BASE -                                                         \
+	(SCP_FW_END -                                                         \
 	 (CONFIG_IPC_SHARED_OBJ_BUF_SIZE + 2 * 4 /* int32_t */) * 2)
 #define CONFIG_IPI
 #define CONFIG_RPMSG_NAME_SERVICE
