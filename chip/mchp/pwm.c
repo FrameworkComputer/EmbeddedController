7036c8763f869235292380c1331150e36bbd2ab5
Kieran Levin <ktl@frame.work>
2020-11-06 01:07:43 -0800

    add bb led pwm control to the pwm api
    bbled uses pwm channels above the pwm channel max to extend the functionality
    
    Signed-off-by: Kieran Levin <ktl@frame.work>

--- chip/mchp/pwm.c
+++ chip/mchp/pwm.c
@@ -29,18 +29,38 @@
  * b[31] = PWM 9
  */
 static uint32_t pwm_keep_awake_mask;
+static uint32_t bbled_keep_awake_mask;
 
-const uint8_t pwm_slp_bitpos[12] = {
-	4, 20, 21, 22, 23, 24, 25, 26, 27, 31, 0, 1
+
+const uint8_t pwm_slp_bitpos[MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES] = {
+	4, 20, 21, 22, 23, 24, 25, 26, 27,
+#if defined(CHIP_FAMILY_MEC17XX)
+	31, 0, 1,
+#endif
+	/* BBLED instances*/
+	16, 17, 18,
+	#if defined(CHIP_FAMILY_MEC17XX)
+	25
+	#endif
+
 };
 
 static uint32_t pwm_get_sleep_mask(int id)
 {
 	uint32_t bitpos = 32;
 
-	if (id < 12)
+	if (id < MCHP_PWM_ID_MAX)
 		bitpos = (uint32_t)pwm_slp_bitpos[id];
 
+	return (1ul << bitpos);
+}
+
+static uint32_t pwm_get_bb_sleep_mask(int id)
+{
+	uint32_t bitpos = 32;
+
+	if (id >= MCHP_PWM_ID_MAX && id < MCHP_PWM_ID_MAX)
+		bitpos = (uint32_t)pwm_slp_bitpos[id];
 	return (1ul << bitpos);
 }
 
@@ -50,21 +70,44 @@ void pwm_enable(enum pwm_channel ch, int enabled)
 	int id = pwm_channels[ch].channel;
 	uint32_t pwm_slp_mask;
 
-	pwm_slp_mask = pwm_get_sleep_mask(id);
+	if (id < MCHP_PWM_ID_MAX) {
+		pwm_slp_mask = pwm_get_sleep_mask(id);
 
-	if (enabled) {
-		MCHP_PWM_CFG(id) |= 0x1;
-		if (pwm_channels[ch].flags & PWM_CONFIG_DSLEEP)
-			pwm_keep_awake_mask |= pwm_slp_mask;
-	} else {
-		MCHP_PWM_CFG(id) &= ~0x1;
-		pwm_keep_awake_mask &= ~pwm_slp_mask;
+		if (enabled) {
+			MCHP_PWM_CFG(id) |= 0x1;
+			if (pwm_channels[ch].flags & PWM_CONFIG_DSLEEP)
+				pwm_keep_awake_mask |= pwm_slp_mask;
+		} else {
+			MCHP_PWM_CFG(id) &= ~0x1;
+			pwm_keep_awake_mask &= ~pwm_slp_mask;
+		}
+	} else if (id < (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)) {
+		pwm_slp_mask = pwm_get_bb_sleep_mask(id);
+		id = id - MCHP_PWM_ID_MAX;
+		/* Handle BBLEDs here*/
+		if (enabled) {
+			/* Blink = PWM mode when clock=high*/
+			MCHP_BBLED_CONFIG(id) |= MCHP_BBLED_CTRL_BLINK;
+			if (pwm_channels[ch].flags & PWM_CONFIG_DSLEEP)
+				bbled_keep_awake_mask |= pwm_slp_mask;
+		} else {
+			MCHP_BBLED_CONFIG(id) &= ~MCHP_BBLED_CTRL_MASK;
+			bbled_keep_awake_mask &= ~pwm_slp_mask;
+		}
 	}
 }
 
 int pwm_get_enabled(enum pwm_channel ch)
 {
-	return MCHP_PWM_CFG(pwm_channels[ch].channel) & 0x1;
+	int id = pwm_channels[ch].channel;
+
+	if (id < MCHP_PWM_ID_MAX) {
+		return MCHP_PWM_CFG(id) & 0x1;
+	} else if (id < (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)) {
+		id = id - MCHP_PWM_ID_MAX;
+		return (MCHP_BBLED_CONFIG(id) & MCHP_BBLED_CTRL_MASK) != MCHP_BBLED_CTRL_OFF;
+	}
+	return 0;
 }
 
 void pwm_set_duty(enum pwm_channel ch, int percent)
@@ -75,14 +118,27 @@ void pwm_set_duty(enum pwm_channel ch, int percent)
 		percent = 0;
 	else if (percent > 100)
 		percent = 100;
-
-	MCHP_PWM_ON(id) = percent;
-	MCHP_PWM_OFF(id) = 100 - percent;
+	if (id < MCHP_PWM_ID_MAX) {
+		MCHP_PWM_ON(id) = percent;
+		MCHP_PWM_OFF(id) = 100 - percent;
+	} else if (id < (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)) {
+		id = id - MCHP_PWM_ID_MAX;
+		/* The BBLED peripheral does not have the ability to set reload value*/
+		MCHP_BBLED_LIMIT_MIN(id) = (percent * 0xFF) / 100;
+	}
 }
 
 int pwm_get_duty(enum pwm_channel ch)
 {
-	return MCHP_PWM_ON(pwm_channels[ch].channel);
+	int id = pwm_channels[ch].channel;
+
+	if (id < MCHP_PWM_ID_MAX) {
+		return MCHP_PWM_ON(pwm_channels[ch].channel);
+	} else if (id < (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)) {
+		id = id - MCHP_PWM_ID_MAX;
+		return DIV_ROUND_NEAREST(MCHP_BBLED_LIMIT_MIN(id) * 100, 0xFF);
+	}
+	return -1;
 }
 
 void pwm_keep_awake(void)
@@ -98,7 +154,12 @@ void pwm_keep_awake(void)
 		MCHP_PCR_SLP_EN4 &= ~(pwm_keep_awake_mask &
 					 (MCHP_PCR_SLP_EN4_PWM_ALL));
 	} else {
-		MCHP_PCR_SLOW_CLK_CTL &= 0xFFFFFC00;
+		/*Disable 100khz clock - this is shared with tach*/
+		MCHP_PCR_SLOW_CLK_CTL &= ~MCHP_PCR_SLOW_CLK_CTL_MASK;
+	}
+	if (bbled_keep_awake_mask) {
+		MCHP_PCR_SLP_EN3 &= ~(bbled_keep_awake_mask &
+					(MCHP_PCR_SLP_EN3_LED_ALL));
 	}
 }
 
@@ -109,13 +170,31 @@ void pwm_configure(int ch, int active_low, int clock_low)
 	 * clock_low=0 selects the 48MHz Ring Oscillator source
 	 * clock_low=1 selects the 100kHz_Clk source
 	 */
-	MCHP_PWM_CFG(ch) = (15 << 3) |    /* Pre-divider = 16 */
+	if (ch < MCHP_PWM_ID_MAX) {
+		MCHP_PWM_CFG(ch) = (15 << 3) |    /* Pre-divider = 16 */
 			      (active_low ? BIT(2) : 0) |
 			      (clock_low  ? BIT(1) : 0);
+	} else if (ch < (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)) {
+		ch = ch - MCHP_PWM_ID_MAX;
+		/** Clock low selects the 32.768 clock,
+		 * and Clock high selects the main system clock
+		 */
+		MCHP_BBLED_CONFIG(ch) = (clock_low ? 0 : MCHP_BBLED_CLK_48M) |
+								MCHP_BBLED_ASYMMETRIC;
+
+		/** in PWM mode the delay register sets the prescaler:
+		 * Fpwm = Fclock/(256*(DELAY+1))
+		 * with Fclock set to 48Mhz:
+		 * 8 =  20833Hz
+		 * 15 = 11718Hz
+		 * Lets set it above audio frequencies
+		 */
+		MCHP_BBLED_DELAY(ch) = 15;
+	}
 }
 
-static const uint16_t pwm_pcr[MCHP_PWM_ID_MAX] = {
-	MCHP_PCR_PWM0,
+static const uint16_t pwm_pcr[MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES] = {
+	MCHP_PCR_PWM0, /*Located in Sleep enable 1*/
 	MCHP_PCR_PWM1,
 	MCHP_PCR_PWM2,
 	MCHP_PCR_PWM3,
@@ -125,15 +204,21 @@ static const uint16_t pwm_pcr[MCHP_PWM_ID_MAX] = {
 	MCHP_PCR_PWM7,
 	MCHP_PCR_PWM8,
 #if defined(CHIP_FAMILY_MEC17XX)
-	MCHP_PCR_PWM9,
-	MCHP_PCR_PWM10,
-	MCHP_PCR_PWM11,
+	MCHP_PCR_PWM9, /*Located in Sleep enable 3*/
+	MCHP_PCR_PWM10,/*Located in Sleep enable 4*/
+	MCHP_PCR_PWM11,/*Located in Sleep enable 4*/
+#endif
+	MCHP_PCR_LED0, /*Located in Sleep enable 3*/
+	MCHP_PCR_LED1, /*Located in Sleep enable 3*/
+	MCHP_PCR_LED2, /*Located in Sleep enable 3*/
+#if defined(CHIP_FAMILY_MEC17XX)
+	MCHP_PCR_LED3, /*Located in Sleep enable 3*/
 #endif
 };
 
 void pwm_slp_en(int pwm_id, int sleep_en)
 {
-	if ((pwm_id < 0) || (pwm_id > MCHP_PWM_ID_MAX))
+	if ((pwm_id < 0) || (pwm_id > (MCHP_PWM_ID_MAX + MCHP_BBLEN_INSTANCES)))
 		return;
 
 	if (sleep_en)
