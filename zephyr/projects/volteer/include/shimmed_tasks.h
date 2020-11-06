9f7f2f5e7506136bc7f28ad793310f38cefa18de
Jack Rosenthal <jrosenth@chromium.org>
2020-11-06 15:54:40 -0700

    volteer: shim HOSTCMD task
    Enable so we can shim in some host command files.
    
    BUG=b:172678200
    TEST=run on volteer, nothing useful expected yet
    
    Change-Id: Ie049654140f7d65e2aa438086b93b5443ac4c51e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/zephyr-chrome/+/2523466
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2630142

--- zephyr/projects/volteer/include/shimmed_tasks.h
+++ zephyr/projects/volteer/include/shimmed_tasks.h
@@ -10,13 +10,15 @@
  * Manually define these HAS_TASK_* defines. There is a build time assert
  * to at least verify we have the minimum set defined correctly.
  */
+#define HAS_TASK_HOSTCMD 1
 #define HAS_TASK_KEYPROTO 1
 
 /*
  * Highest priority on bottom -- same as in platform/ec. List of CROS_EC_TASK
  * items. See CONFIG_TASK_LIST in platform/ec's config.h for more informaiton
  */
-#define CROS_EC_TASK_LIST  \
+#define CROS_EC_TASK_LIST                                \
+	CROS_EC_TASK(HOSTCMD, host_command_task, 0, 512) \
 	CROS_EC_TASK(KEYPROTO, keyboard_protocol_task, 0, 512)
 
 #endif /* __CROS_EC_SHIMMED_TASKS_H */
