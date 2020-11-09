22ee9a346718a9a07955fd81b0d03c4dde5b2ec8
Jett Rink <jettrink@chromium.org>
2020-11-09 14:42:53 -0700

    zephyr: forward cros_crc8 to zephyrs crc8 impl
    Shim in support for crc8 used in CBI, I2C, and other
    applications within platform/ec
    
    BRANCH=none
    BUG=b:168032589
    TEST=add unit test for platform/ec and zephyr based
         CRC8 approaches and verify they both pass.
    
    Change-Id: Ib609f98596c4edc6e6f0d01b6242e62a1f95ad72
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2527811
    Reviewed-by: Yuval Peress <peress@chromium.org>

---
+++ zephyr/test/crc/main.c
@@ -0,0 +1,27 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <kernel.h>
+#include <ztest.h>
+
+#include "crc8.h"
+
+/* Note this test makes the pure platform/ec test that uses the same value */
+static void test_crc8_known_data(void)
+{
+	uint8_t buffer[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 8 };
+
+	int crc = cros_crc8(buffer, 10);
+
+	/* Verifies polynomial values of 0x07 representing x^8 + x^2 + x + 1 */
+	zassert_equal(crc, 170, "CRC8 hash did not match");
+}
+
+void test_main(void)
+{
+	ztest_test_suite(test_task_shim,
+			 ztest_unit_test(test_crc8_known_data));
+	ztest_run_test_suite(test_task_shim);
+}
