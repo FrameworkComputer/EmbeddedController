4f32e20770141ed0d8ab4fab282039abb371abf4
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2020-11-10 09:42:46 +0800

    Metaknight: Reconfig LED GPIO, and modify Led behavior
    Config GPIOC3, GPIOC4 to control LED.
    Move GPIOC2 to NC pins.
    Modify Led behavior.
    
    BUG=b:172845491
    BRANCH=master
    TEST=make BOARD=metaknight PASS
    TEST=Led behavior is correct after change.
    
    Signed-off-by: yu-an.chen@quanta.corp-partner.google.com
    Change-Id: Icc86b6e6c7e6541c73d70c0903a609be419782ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2528730
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- board/metaknight/led.c
+++ board/metaknight/led.c
@@ -1,70 +1,80 @@
 /* Copyright 2020 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
+ *
+ * Power and battery LED control for Metaknight
  */
 
-/* Waddledoo specific PWM LED settings. */
-
-#include "common.h"
 #include "ec_commands.h"
-#include "led_pwm.h"
-#include "pwm.h"
-#include "util.h"
+#include "gpio.h"
+#include "led_common.h"
+#include "led_onoff_states.h"
+#include "chipset.h"
+
+#define LED_ON_LVL 0
+#define LED_OFF_LVL 1
+
+const int led_charge_lvl_1;
+
+const int led_charge_lvl_2 = 100;
+
+struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
+	[STATE_CHARGING_LVL_1]	     = {{EC_LED_COLOR_AMBER, LED_INDEFINITE} },
+	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_AMBER, LED_INDEFINITE} },
+	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_WHITE, LED_INDEFINITE} },
+	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_WHITE, LED_INDEFINITE} },
+	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            3 * LED_ONE_SEC} },
+	[STATE_DISCHARGE_S5]         = {{LED_OFF,            LED_INDEFINITE} },
+	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_AMBER, 1 * LED_ONE_SEC},
+					{LED_OFF,            1 * LED_ONE_SEC} },
+	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_WHITE, 2 * LED_ONE_SEC},
+					{EC_LED_COLOR_AMBER, 2 * LED_ONE_SEC} },
+};
+
 
 const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_POWER_LED,
+	EC_LED_ID_BATTERY_LED,
 };
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
-/*
- * We only have a white and an amber LED, so setting any other colour results in
- * both LEDs being off.
- */
-struct pwm_led led_color_map[EC_LED_COLOR_COUNT] = {
-				/* Amber, White */
-	[EC_LED_COLOR_RED]    = {   0,   0 },
-	[EC_LED_COLOR_GREEN]  = {   0,   0 },
-	[EC_LED_COLOR_BLUE]   = {   0,   0 },
-	[EC_LED_COLOR_YELLOW] = {   0,   0 },
-	[EC_LED_COLOR_WHITE]  = {   0, 100 },
-	[EC_LED_COLOR_AMBER]  = {  100,  0 },
-};
 
-/* One logical LED with amber and white channels. */
-struct pwm_led pwm_leds[CONFIG_LED_PWM_COUNT] = {
-	{
-		.ch0 = PWM_CH_LED1_AMBER,
-		.ch1 = PWM_CH_LED2_WHITE,
-		.ch2 = PWM_LED_NO_CHANNEL,
-		.enable = &pwm_enable,
-		.set_duty = &pwm_set_duty,
-	},
-};
+void led_set_color_battery(enum ec_led_colors color)
+{
+	switch (color) {
+	case EC_LED_COLOR_WHITE:
+		gpio_set_level(GPIO_LED_W_ODL, LED_ON_LVL);
+		gpio_set_level(GPIO_LED_Y_ODL, LED_OFF_LVL);
+		break;
+	case EC_LED_COLOR_AMBER:
+		gpio_set_level(GPIO_LED_W_ODL, LED_OFF_LVL);
+		gpio_set_level(GPIO_LED_Y_ODL, LED_ON_LVL);
+		break;
+	default: /* LED_OFF and other unsupported colors */
+		gpio_set_level(GPIO_LED_W_ODL, LED_OFF_LVL);
+		gpio_set_level(GPIO_LED_Y_ODL, LED_OFF_LVL);
+		break;
+	}
+}
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	memset(brightness_range, '\0',
-	       sizeof(*brightness_range) * EC_LED_COLOR_COUNT);
-	brightness_range[EC_LED_COLOR_AMBER] = 100;
-	brightness_range[EC_LED_COLOR_WHITE] = 100;
+	if (led_id == EC_LED_ID_BATTERY_LED) {
+		brightness_range[EC_LED_COLOR_AMBER] = 1;
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+	}
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
-	enum pwm_led_id pwm_id;
-
-	/* Convert ec_led_id to pwm_led_id. */
-	if (led_id == EC_LED_ID_POWER_LED)
-		pwm_id = PWM_LED0;
-	else
-		return EC_ERROR_UNKNOWN;
-
-	if (brightness[EC_LED_COLOR_WHITE])
-		set_pwm_led_color(pwm_id, EC_LED_COLOR_WHITE);
-	else if (brightness[EC_LED_COLOR_AMBER])
-		set_pwm_led_color(pwm_id, EC_LED_COLOR_AMBER);
-	else
-		/* Otherwise, the "color" is "off". */
-		set_pwm_led_color(pwm_id, -1);
-
+	if (led_id == EC_LED_ID_BATTERY_LED) {
+		if (brightness[EC_LED_COLOR_WHITE] != 0)
+			led_set_color_battery(EC_LED_COLOR_WHITE);
+		else if (brightness[EC_LED_COLOR_AMBER] != 0)
+			led_set_color_battery(EC_LED_COLOR_AMBER);
+		else
+			led_set_color_battery(LED_OFF);
+	}
 	return EC_SUCCESS;
 }
+
+
