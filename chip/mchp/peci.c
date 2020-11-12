89473beb0ca9fec38042366f08b5f2858cd3d1ac
Josh Tsai <josh_tsai@compal.com>
2020-11-12 15:13:05 +0800

    Modify the PECI function to match the chromium ec common/peci.c
    Signed-off-by: Josh Tsai <josh_tsai@compal.com>

--- chip/mchp/peci.c
+++ chip/mchp/peci.c
@@ -7,21 +7,49 @@
 #include "chipset.h"
 #include "console.h"
 #include "hooks.h"
-#include "peci_chip.h"
+#include "peci.h"
 #include "registers.h"
 #include "timer.h"
 #include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_I2C, format, ## args)
 
 #define PECI_MAX_RETRIES    3
 #define PECI_MAX_FIFO_SIZE  32
 #define WAIT_IDLE_TIMEOUT   60   /* ms */
 #define POLYNOMIAL          0x07
 
-#define CPRINTS(format, args...) cprints(CC_I2C, format, ## args)
+/*completion codes*/
+#define COMP_CODE       0
+#define CC_PASS_MASK    (0 << 7)
+#define CC_FAIL_MASK    (1 << 7)
+#define CC_PASSED       0x40        /* command passed */
+#define CC_TIMED_OUT    0x80        /* command timed-out */
+#define CC_BAD          0x90        /* unknown-invalid-illegal request */
+#define CPU_ADDR        0x30
 
 /* PECI 3.0 does not support multiple originators (hosts) */
 #define HOST_ID             0x00
 #define RETRY_BIT           0x01
+
+enum peci_status {
+	PECI_STATUS_NO_ERR        = 0x00,
+	PECI_STATUS_HOBY          = 0x01,
+	PECI_STATUS_FINISH        = 0x02,
+	PECI_STATUS_RD_FCS_ERR    = 0x04,
+	PECI_STATUS_WR_FCS_ERR    = 0x08,
+	PECI_STATUS_EXTERR        = 0x20,
+	PECI_STATUS_BUSERR        = 0x40,
+	PECI_STATUS_RCV_ERRCODE   = 0x80,
+	PECI_STATUS_ERR_NEED_RST  = (PECI_STATUS_BUSERR | PECI_STATUS_EXTERR),
+	PECI_STATUS_ANY_ERR       = (PECI_STATUS_RCV_ERRCODE |
+					PECI_STATUS_BUSERR |
+					PECI_STATUS_EXTERR |
+					PECI_STATUS_WR_FCS_ERR |
+					PECI_STATUS_RD_FCS_ERR),
+	PECI_STATUS_ANY_BIT       = 0xFE,
+	PECI_STATUS_TIMEOUT       = 0xFF,
+};
 
 struct peci_params_t {
 	uint8_t cmd_fifo[PECI_MAX_FIFO_SIZE];
@@ -82,7 +110,7 @@ void reset_PECI(uint8_t mask)
 /**
  * write specified number of bytes to write buffer while FIFO is not full,
  * check idle for IDLE ready
- * 
+ *
  * @param   cmd_fifo
  * @param   cmd_length
  */
@@ -110,9 +138,9 @@ void write_command(uint8_t cmd_fifo[], uint8_t cmd_length)
 }
 
 /**
- * Read the specified number of bytes while FIFO not empty, return bytes in 
+ * Read the specified number of bytes while FIFO not empty, return bytes in
  * read_data buffer
- * 
+ *
  * @param   read_ptr
  * @param   num_bytes
  */
@@ -120,8 +148,7 @@ void read_databytes(uint8_t *read_ptr, uint8_t num_bytes)
 {
 	uint8_t readdcnt;
 
-	for (readdcnt = 0; readdcnt < num_bytes; readdcnt++)
-	{
+	for (readdcnt = 0; readdcnt < num_bytes; readdcnt++) {
 		if (!(MCHP_PECI_STATUS2 & MCHP_PECI_STATUS2_RFE))
 			*read_ptr = MCHP_PECI_READ_DATA;
 
@@ -132,7 +159,7 @@ void read_databytes(uint8_t *read_ptr, uint8_t num_bytes)
 
 /**
  * do appropriate reset/retry after write/read of PECI commands
- * 
+ *
  * @param   done
  * @param   zero_err
  */
@@ -142,36 +169,26 @@ void cleanup(uint8_t done, uint8_t zero_err)
 
 	errval = MCHP_PECI_ERROR;
 
-	if (errval)
-	{
+	if (errval) {
 		trace1(0, PECI, 0, "ERROR val=0x%02x - resetPECI", errval);
 		MCHP_PECI_ERROR = errval;   /* RWC clear error by writing value back */
 		reset_PECI(MCHP_PECI_CONTROL_RST | MCHP_PECI_CONTROL_FRST);
-	}
-
-	else
-	{
-		if (done)
-		{
+	} else {
+		if (done) {
 			trace0(0, PECI, 0, "Issue FIFO Reset to cleanup");
 			reset_PECI(MCHP_PECI_CONTROL_FRST);
-		}
-		else if (zero_err)
-		{
+		} else if (zero_err) {
 			trace0(0, PECI, 0, "retry command");
 			reset_PECI(MCHP_PECI_CONTROL_RST | MCHP_PECI_CONTROL_FRST);
-		}
-		else
-		{
-			trace0(0, PECI, 0, "retry command")
-		}
-}
+		} else
+			trace0(0, PECI, 0, "retry command");
+	}
 }
 
 /**
  * calculate the Assured Write value based on the number of bytes in input
  * buffer
- * 
+ *
  * @param   data_blk_ptr
  * @param   length
  */
@@ -181,21 +198,17 @@ uint8_t calc_AWFCS(uint8_t *data_blk_ptr, unsigned int length)
 	uint8_t temp1, data_byte, bit0;
 	unsigned int i, j;
 
-	for(i = 0; i < length; i++)
-	{
+	for (i = 0; i < length; i++) {
 		data_byte = *data_blk_ptr;
 
-		for(j = 0; j < 8; j++)
-		{
+		for (j = 0; j < 8; j++) {
 			bit0 = (data_byte & 0x80) ? 0x80 : 0;
 			data_byte <<= 1;
 			crc ^= bit0;
 			temp1 = crc & 0x80;
 			crc <<= 1;
 			if (temp1)
-			{
 				crc ^= POLYNOMIAL;
-			}
 		}
 	}
 	crc ^= 0x80;
@@ -205,117 +218,94 @@ uint8_t calc_AWFCS(uint8_t *data_blk_ptr, unsigned int length)
 /**
  * main PECI function sends/recives PECI command bytes, check for FCS error,
  * retries commands based on parameters
- * 
- * @param   PECI_PARAMS 
+ *
+ * @param   PECI_PARAMS
  * @return  error status
  */
 static uint8_t peci_trans(struct peci_params_t *peci)
 {
 	uint8_t error = 0;
-	uint8_t done = 0, retry = 0, nzdata = 0, zero_error = 0, cc_error=0;
+	uint8_t done = 0, retry = 0, nzdata = 0, zero_error = 0, cc_error = 0;
 	uint8_t i;
 
 	peci->cmd_FCS = 0;
 
-	while (!done)
-	{
+	while (!done) {
 		if (!peci->retry_valid)
 			done = 1;
-		else
-		{
+		else {
 			if (peci->host_byte && retry)
 				*(peci->cmd_fifo + peci->host_byte) |= RETRY_BIT;
 		}
 
-		write_command( peci->cmd_fifo, peci->cmd_length);
+		write_command(peci->cmd_fifo, peci->cmd_length);
 
 		read_databytes(&peci->cmd_FCS, 1);
 
-		if (MCHP_PECI_ERROR & MCHP_PECI_ERROR_FERR)
-		{
+		if (MCHP_PECI_ERROR & MCHP_PECI_ERROR_FERR) {
 			MCHP_PECI_ERROR = 0xFF;
 
 			error++;
 			trace0(0, PECI, 0, "Command FCS error!");
-		}
-		else
-		{
-			if (peci->read_length)
-			{
-				read_databytes(peci->data_fifo, (peci->read_length+1));
+		} else {
+			if (peci->read_length) {
+				read_databytes(peci->data_fifo, (peci->read_length + 1));
 
-				if ((!error) && (peci->cmd_FCS != 0))
-				{
-					for(i = 0; i < (peci-> read_length+1); i++)
-					{
+				if ((!error) && (peci->cmd_FCS != 0)) {
+					for (i = 0; i < (peci->read_length + 1); i++) {
 						if (peci->data_fifo[i] != 0)
 							nzdata++;
 					}
 
-					if (!nzdata)
-					{
+					if (!nzdata) {
 						trace0(0, PECI, 0, "Data error: All data = 0");
 						error++;
 						zero_error = 1;
 					}
 				}
 
-				if (!error)
-				{
-					if (MCHP_PECI_ERROR & MCHP_PECI_ERROR_FERR)
-					{
+				if (!error) {
+					if (MCHP_PECI_ERROR & MCHP_PECI_ERROR_FERR) {
 						MCHP_PECI_ERROR = 0xFF;
 
 						error++;
 						trace0(0, PECI, 0, "Data FCS error!");
 					}
 
-					if (!error)
-					{
-						if ((peci->check_completion) && 
-						((peci->data_fifo[COMP_CODE] & CC_PASSED)!=CC_PASSED))
-						{
+					if (!error) {
+						if ((peci->check_completion) &&
+						((peci->data_fifo[COMP_CODE] & CC_PASSED) != CC_PASSED)) {
 							error++;
 							cc_error++;
 
-							if ((peci->data_fifo[COMP_CODE] & CC_BAD) == CC_BAD)
-							{
+							if ((peci->data_fifo[COMP_CODE] & CC_BAD) == CC_BAD) {
 								done = 1;
 								cc_error = 2;
 							}
-						}
-						else
-						{
+						} else {
 							done = 1;
 							trace0(0, PECI, 0, "Command success");
 						}
 					}
 				}
-			}
-			else
-			{
+			} else {
 				done = 1;
 				trace0(0, PECI, 0, "Command success");
 			}
 		}
 
-		if (error)
-		{
-			if (!done)
-			{
+		if (error) {
+			if (!done) {
 				retry++;
-				if ( retry > PECI_MAX_RETRIES)
+				if (retry > PECI_MAX_RETRIES)
 					done = 1;
 			}
 
-			if (done)
-			{
-				if (cc_error)
-				{
+			if (done) {
+				if (cc_error) {
 					if (cc_error > 1)
 						trace0(0, PECI, 0, "Illegal Request (no retry)");
-					else
-					{
+					else {
 						if (peci->data_fifo[COMP_CODE] == CC_TIMED_OUT)
 							trace0(0, PECI, 0, "Timeout");
 						else
@@ -328,9 +318,8 @@ static uint8_t peci_trans(struct peci_params_t *peci)
 
 		cleanup(done, zero_error);
 
-		if (!done) {
+		if (!done)
 			error = 0;
-		}
 
 		zero_error = 0;
 		cc_error = 0;
@@ -348,63 +337,38 @@ static void peci_init(void)
 DECLARE_HOOK(HOOK_INIT, peci_init, HOOK_PRIO_DEFAULT);
 
 /*****************************************************************************/
-/* PECI Command functions */
-
 /**
- * create the peci protocol and issue transfer
- * 
- * @param   peci_addr   intel CPU always 0x30
- * @param   cmd_code    peci command, refers peci_command_t
- * @param   domain      0/1 for CPU domain
- * @param   out         write data (index, parameter, data)
- * @param   out_size:   length for output data
- * @param   in          read data (w/o FC)
- * @param   in_size     length for read data (w/o FCS byte)
+ * Start a PECI transaction
+ *
+ * @param peci transaction data
+ *
+ * @return zero if successful, non-zero if error
  */
-void peci_protocol(uint8_t peci_addr, uint8_t cmd_code, uint8_t domain,
-		const uint8_t *out, int out_size, uint8_t *in, int in_size)
+int peci_transaction(struct peci_data *peci)
 {
 	struct peci_params_t peci_params;
-	uint8_t aw_FCS_calc;
 	int index;
-	int len = out_size - 3;
 	int dlen;
 
-	peci_params.cmd_fifo[0] = peci_addr;
-	peci_params.cmd_fifo[1] = len;
-	peci_params.cmd_fifo[2] = in_size;
-	peci_params.cmd_length = out_size;
-	peci_params.read_length = in_size;
+	peci_params.cmd_fifo[0] = peci->addr;
+	peci_params.cmd_fifo[1] = peci->w_len+1;
+	peci_params.cmd_fifo[2] = peci->r_len;
+	peci_params.cmd_length = peci->w_len+4;
+	peci_params.read_length = peci->r_len;
 	peci_params.check_completion = 0;
 	peci_params.retry_valid = 0;
 	peci_params.host_byte = 0;
 
-	if (cmd_code != PECI_COMMAND_PING)
-	{
-		peci_params.cmd_fifo[3] = (domain ? cmd_code + 1 : cmd_code);
+	if (peci->cmd_code != PECI_CMD_PING) {
+		peci_params.cmd_fifo[3] = peci->cmd_code;
 
 		/* GetDIB and GetTemp only command byte */
-		if (out_size - 3 > 1)
-		{
-			peci_params.cmd_fifo[4] = HOST_ID;
-
-			if (in_size == 1)
-			{
-				/*Write command has AW FCS byte */
-				len = out_size - 4;
-			}
-
-			for(index = 2; index < len; index++)
-			{
-				/* put data in cmd_fifo */
-				peci_params.cmd_fifo[index + 3] = out[index];
-			}
-
-			if (in_size == 1)
-			{
-				/* TODO: calculate AW FCS and put to latest write byte */
-				aw_FCS_calc = calc_AWFCS(peci_params.cmd_fifo, out_size - 1);
-				peci_params.cmd_fifo[out_size] = aw_FCS_calc;
+		if (!(peci->cmd_code == PECI_CMD_GET_DIB ||
+			peci->cmd_code == PECI_CMD_GET_TEMP)) {
+			if (peci->r_len == 1) {
+				/*Write command */
+				for (index = 0; index < peci->w_len; index++)
+					peci_params.cmd_fifo[index+4] = peci->w_buf[index];
 			}
 
 			peci_params.check_completion = 1;
@@ -416,7 +380,8 @@ void peci_protocol(uint8_t peci_addr, uint8_t cmd_code, uint8_t domain,
 	peci_trans(&peci_params);
 
 	/* TODO: check error message, if no error, pass the data fifo to *in*/
-	for (dlen = 0; dlen < peci_params.read_length; dlen++) {
-		in[dlen] = peci_params.data_fifo[dlen];
-	}
+	for (dlen = 0; dlen < peci_params.read_length; dlen++)
+		peci->r_buf[dlen] = peci_params.data_fifo[dlen];
+
+	return PECI_STATUS_NO_ERR;
 }
