eb76e68d291648c60b2ff9cde3d071785add90a6
Devin Lu <Devin.Lu@quantatw.com>
2020-11-13 17:04:45 +0800

    drawlat: Add CosMX B00C4473A9D0002 battery
    BUG=b:173174333
    BRANCH=none
    TEST=Test on charging/discharging/battery cut off pass.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: Ide3560ccbbbc0c65d5c3c72d8ed184d21271b860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2537523
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- board/drawcia/battery.c
+++ board/drawcia/battery.c
@@ -263,6 +263,35 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c = 60,
 		},
 	},
+
+	/* CosMX B00C4473A9D0002 Battery Information */
+	[BATTERY_COS_2] = {
+		.fuel_gauge = {
+			.manuf_name = "333-AC-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x0,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0006,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max = 8800,		/* mV */
+			.voltage_normal = 7700,		/* mV */
+			.voltage_min = 6000,		/* mV */
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
