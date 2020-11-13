8abeefdccbc98eec5e24da971e0bfe0cb2fbf279
Yuval Peress <peress@chromium.org>
2020-11-13 11:09:40 -0700

    zephyr: add i2c_ports enum
    This change adds an i2c_ports enum based on the devicetree
    phandle list 'named-i2c-ports'. Modeled after named-gpios.
    Currently, this is only enabled for ztests as it conflicts
    with the definitions in config_chip-npcx7.h. But once that
    dependency is removed, this enum will become publicly
    available.
    
    BUG=b:171302975
    BRANCH=none
    TEST=../zephyr-chrome/firmware_builder.py test
    
    Cq-Depend: chromium:2546328
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I8e763da0c867fed383eb333092f191079fb792ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2546997
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -6,6 +6,8 @@
 #ifndef __CROS_EC_CONFIG_CHIP_H
 #define __CROS_EC_CONFIG_CHIP_H
 
+#include <devicetree.h>
+
 /*
  * This file translates Kconfig options to platform/ec options.
  *
@@ -64,6 +66,21 @@
 
 #ifdef CONFIG_PLATFORM_EC_I2C
 #define CONFIG_I2C
+
+/*
+ * Define the i2c_ports enum for Ztests only right now. In full builds this
+ * will clash with the definitions in config_chip-npcx7.h. Once we've migrated
+ * away from platform/ec/chip/... files we can remove this guard.
+ */
+#if defined(CONFIG_ZTEST) && DT_NODE_EXISTS(DT_PATH(named_i2c_ports))
+#define I2C_PORT(id) DT_CAT(I2C_, id)
+#define I2C_PORT_WITH_COMMA(id) I2C_PORT(id),
+enum i2c_ports {
+DT_FOREACH_CHILD(DT_PATH(named_i2c_ports), I2C_PORT_WITH_COMMA)
+I2C_PORT_COUNT
+};
+#define NAMED_I2C(name) I2C_PORT(DT_PATH(named_i2c_ports, name))
+#endif /* CONFIG_ZTEST && named_i2c_ports */
 #endif /* CONFIG_PLATFORM_EC_I2C */
 
 #endif  /* __CROS_EC_CONFIG_CHIP_H */
---
+++ zephyr/test/i2c/src/main.c
@@ -0,0 +1,43 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <devicetree.h>
+#include <ztest.h>
+
+#include "common.h"
+#include "i2c/i2c.h"
+
+/* Unused: required for shimming i2c. */
+uint32_t sleep_mask;
+
+/* Unused: required for shimming i2c. */
+const struct device *i2c_get_device_for_port(int port)
+{
+	return NULL;
+}
+
+/* Unused: required for shimming i2c. */
+void watchdog_reload(void)
+{
+}
+
+static void test_i2c_port_count(void)
+{
+	zassert_equal(NAMED_I2C(accel_0), 0,
+		      "accel_0 expected to be 0 but was %d",
+		      NAMED_I2C(accel_0));
+	zassert_equal(I2C_PORT_COUNT, 1,
+		      "I2C_PORT_COUNT expected to be 1 but was %d",
+		      I2C_PORT_COUNT);
+}
+
+/* Test case main entry. */
+void test_main(void)
+{
+	ztest_test_suite(test_i2c,
+			 ztest_user_unit_test(test_i2c_port_count));
+	ztest_run_test_suite(test_i2c);
+}
+
