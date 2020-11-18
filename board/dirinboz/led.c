73b5043df48f5071131cd0424f9e6d992e89f4f5
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-11-18 11:11:30 +0800

    dirinboz: update led on/off level
    This patch update led on/off level by board version
    and USB-C port:
    1.board version >= 3 && USB-C right port
    led on: gpio set high
    led off: gpio set low
    2. board version < 3
    led on: gpio set low
    led off: gpio set high
    
    BUG=b:161210524
    BRANCH=zork
    TEST=verify on rework board, led behavior as intended.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    
    Change-Id: I6fec6968b2599702d61e9f2f1b7151ccd5c06a09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2545913
    Tested-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Zick Wei <zick.wei@quanta.corp-partner.google.com>

--- board/dirinboz/led.c
+++ board/dirinboz/led.c
@@ -7,17 +7,12 @@
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "chipset.h"
+#include "cros_board_info.h"
 #include "ec_commands.h"
 #include "gpio.h"
 #include "host_command.h"
 #include "led_common.h"
 #include "hooks.h"
-
-#define BAT_LED_ON 0
-#define BAT_LED_OFF 1
-
-#define POWER_LED_ON 0
-#define POWER_LED_OFF 1
 
 #define LED_TICKS_PER_CYCLE 10
 #define LED_ON_TICKS 5
@@ -44,24 +39,35 @@ enum led_port {
 static void led_set_color_battery(int port, enum led_color color)
 {
 	enum gpio_signal amber_led, white_led;
+	uint32_t board_ver = 0;
+	int led_batt_on_lvl, led_batt_off_lvl;
 
+	cbi_get_board_version(&board_ver);
 	amber_led = (port == LEFT_PORT ? GPIO_LED_CHRG_L :
 				 IOEX_C1_CHARGER_LED_AMBER_DB);
 	white_led = (port == LEFT_PORT ? GPIO_LED_FULL_L :
 				 IOEX_C1_CHARGER_LED_WHITE_DB);
 
+	if ((board_ver >= 3) && (port == RIGHT_PORT)) {
+		led_batt_on_lvl = 1;
+		led_batt_off_lvl = 0;
+	} else {
+		led_batt_on_lvl = 0;
+		led_batt_off_lvl = 1;
+	}
+
 	switch (color) {
 	case LED_WHITE:
-		gpio_or_ioex_set_level(white_led, BAT_LED_ON);
-		gpio_or_ioex_set_level(amber_led, BAT_LED_OFF);
+		gpio_or_ioex_set_level(white_led, led_batt_on_lvl);
+		gpio_or_ioex_set_level(amber_led, led_batt_off_lvl);
 		break;
 	case LED_AMBER:
-		gpio_or_ioex_set_level(white_led, BAT_LED_OFF);
-		gpio_or_ioex_set_level(amber_led, BAT_LED_ON);
+		gpio_or_ioex_set_level(white_led, led_batt_off_lvl);
+		gpio_or_ioex_set_level(amber_led, led_batt_on_lvl);
 		break;
 	case LED_OFF:
-		gpio_or_ioex_set_level(white_led, BAT_LED_OFF);
-		gpio_or_ioex_set_level(amber_led, BAT_LED_OFF);
+		gpio_or_ioex_set_level(white_led, led_batt_off_lvl);
+		gpio_or_ioex_set_level(amber_led, led_batt_off_lvl);
 		break;
 	default:
 		break;
