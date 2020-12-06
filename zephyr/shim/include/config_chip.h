6dc2111008af964fa1521b1259d3da45cb7dbe73
Keith Short <keithshort@chromium.org>
2020-12-06 11:42:54 -0700

    Reland "zephyr: Add battery and smart battery options"
    This is a reland of ac4f512e60af42efe693e6955c110a7908090e56
    
    Original change's description:
    > zephyr: Add battery and smart battery options
    >
    > Shim in battery and smart battery support.
    >
    > BUG=b:175248556
    > BRANCH=none
    > TEST=boot on volteer, run 'battery' command.
    > TEST=run 'ninja menuconfig' and check KConfig help text.
    > 20-12-10 20:08:12.778 battery
    > 20-12-10 20:08:13.509   Status:    0x02c0 DCHG INIT RC
    > 20-12-10 20:08:13.529   Param flags:00000003
    > 20-12-10 20:08:13.541   Temp:      0x0b63 = %.1d K (%.1d C)
    > 20-12-10 20:08:13.541   V:         0x2a1e = 10782 mV
    > 20-12-10 20:08:13.541   V-desired: 0x3390 = 13200 mV
    > 20-12-10 20:08:13.541   I:         0x0000 = 0 mA
    > 20-12-10 20:08:13.550   I-desired: 0x0a19 = 2585 mA
    > 20-12-10 20:08:13.550   Charging:  Allowed
    > 20-12-10 20:08:13.550   Charge:    0 %
    > 20-12-10 20:08:13.550   Manuf:     LG
    > 20-12-10 20:08:13.555   Device:    AC17A8
    > 20-12-10 20:08:13.573   Chem:      LIO
    > 20-12-10 20:08:13.573   Serial:    0xb754
    > 20-12-10 20:08:13.573   V-design:  0x2d1e = 11550 mV
    > 20-12-10 20:08:13.573   Mode:      0x6001
    > 20-12-10 20:08:13.573   Abs charge:0 %
    > 20-12-10 20:08:13.573   Remaining: 0 mAh
    > 20-12-10 20:08:13.577   Cap-full:  4932 mAh (4833 mAh with 98 % compensation)
    > 20-12-10 20:08:13.585     Design:  5360 mAh
    > 20-12-10 20:08:13.594   Time-full: 0h:0
    > 20-12-10 20:08:13.594     Empty:   0h:0
    >
    > Change-Id: Ie782e75ee4027ab2a5c6a0ae7f4ad81e9c360711
    > Signed-off-by: Simon Glass <sjg@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2575199
    
    Bug: b:175248556
    TEST=make buildall, which fails with:
    /opt/coreboot-sdk/lib/gcc/nds32le-elf/8.3.0/../../../../nds32le-elf/bin/ld: __flash_dma_size < IT83XX_ILM_BLOCK_SIZE
    BRANCH=none
    
    Change-Id: I318fb13ba91485cd7d876b2294f20d748f47e0fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2585931
    Tested-by: Simon Glass <sjg@chromium.org>
    Auto-Submit: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -26,6 +26,26 @@
 #define CONFIG_CHIPSET_TIGERLAKE
 #endif
 
+/* Battery configuration */
+#undef CONFIG_BATTERY
+#undef CONFIG_BATTERY_FUEL_GAUGE
+#ifdef CONFIG_PLATFORM_EC_BATTERY
+#define CONFIG_BATTERY
+#define CONFIG_BATTERY_FUEL_GAUGE
+#endif
+
+#undef CONFIG_CHARGER_ISL9241
+#ifdef CONFIG_PLATFORM_EC_CHARGER_ISL9241
+#define CONFIG_CHARGER_ISL9241
+/* Hardware based charge ramp is broken in the ISL9241 (b/169350714) */
+#define CONFIG_CHARGE_RAMP_SW
+#endif
+
+#undef CONFIG_BATTERY_SMART
+#ifdef CONFIG_PLATFORM_EC_BATTERY_SMART
+#define CONFIG_BATTERY_SMART
+#endif
+
 /* eSPI configuration */
 #ifdef CONFIG_PLATFORM_EC_ESPI
 
