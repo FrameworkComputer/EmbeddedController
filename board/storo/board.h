d6ff267da9db1fb51c456a3164f989db9ac137fa
Mike Lee <mike5@huaqin.corp-partner.google.com>
2020-12-10 20:15:38 +0800

    Storo: Initial EC image
    Initial EC image for Storo
    
    BUG=b:173340493
    BRANCH=dedede
    TEST=make BOARD=storo pass
    boot device successfully
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Change-Id: I1c82cc80f161cdbe8561b06606ec9f34ed094094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2584202
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- board/storo/board.h
+++ board/storo/board.h
@@ -1,9 +1,9 @@
-/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Waddledee board configuration */
+/* Storo board configuration */
 
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
@@ -15,6 +15,8 @@
 /* System unlocked in early development */
 #define CONFIG_SYSTEM_UNLOCKED
 
+/* EC console commands */
+#define CONFIG_CMD_TCPC_DUMP
 #define CONFIG_CMD_CHARGER_DUMP
 
 /* Battery */
@@ -24,40 +26,37 @@
 #define CONFIG_BC12_DETECT_PI3USB9201
 
 /* Charger */
-#define CONFIG_CHARGER_SM5803		/* C0 and C1: Charger */
-#define CONFIG_USB_PD_VBUS_DETECT_CHARGER
-#define CONFIG_USB_PD_5V_CHARGER_CTRL
-#define CONFIG_CHARGER_OTG
-#undef  CONFIG_CHARGER_SINGLE_CHIP
+#define CONFIG_CHARGER_RAA489000
+#define CONFIG_CHARGER_SENSE_RESISTOR_AC 10
+#define CONFIG_CHARGER_SENSE_RESISTOR 10
+#define CONFIG_OCPC_DEF_RBATT_MOHMS 22 /* R_DS(on) 11.6mOhm + 10mOhm sns rstr */
 #define CONFIG_OCPC
-#define CONFIG_OCPC_DEF_RBATT_MOHMS 21 /* R_DS(on) 10.7mOhm + 10mOhm sns rstr */
+#undef  CONFIG_CHARGER_SINGLE_CHIP
 
-/*
- * GPIO for C1 interrupts, for baseboard use
- *
- * Note this will only be valid for board revision 1
- */
-#define GPIO_USB_C1_INT_ODL GPIO_USB_C1_INT_V1_ODL
+#define GPIO_USB_C1_INT_ODL GPIO_SUB_USB_C1_INT_ODL
 
 /* LED */
-#define CONFIG_LED_PWM
-#define CONFIG_LED_PWM_COUNT 1
+#define CONFIG_LED_ONOFF_STATES
+#define CONFIG_LED_ONOFF_STATES_BAT_LOW	10
 
-/* PWM */
-#define CONFIG_PWM
+#define I2C_PORT_ACCEL	I2C_PORT_SENSOR
 
 /* Sensors */
-#define CONFIG_ACCEL_KX022		/* Lid accel */
-#define CONFIG_ACCELGYRO_LSM6DSM	/* Base accel */
-/* Sensors without hardware FIFO are in forced mode */
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+
+#define CONFIG_ACCEL_BMA255		/* Lid accel */
+#define CONFIG_ACCELGYRO_BMI160	/* Base accel */
+
+/* Lid operates in forced mode, base in FIFO */
 #define CONFIG_ACCEL_FORCE_MODE_MASK BIT(LID_ACCEL)
+#define CONFIG_ACCEL_FIFO
+#define CONFIG_ACCEL_FIFO_SIZE 256	/* Must be a power of 2 */
+#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
 
 #define CONFIG_ACCEL_INTERRUPTS
-/* Enable sensor fifo, must also define the _SIZE and _THRES */
-#define CONFIG_ACCEL_FIFO
-/* Power of 2 - Too large of a fifo causes too much timestamp jitter */
-#define CONFIG_ACCEL_FIFO_SIZE 256
-#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
+#define CONFIG_ACCELGYRO_BMI160_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
 
 #define CONFIG_LID_ANGLE
 #define CONFIG_LID_ANGLE_UPDATE
@@ -70,9 +69,14 @@
 
 /* TCPC */
 #define CONFIG_USB_PD_PORT_MAX_COUNT 2
-#define CONFIG_USB_PD_TCPM_ITE_ON_CHIP	/* C0: ITE EC TCPC */
-#define CONFIG_USB_PD_TCPM_ANX7447	/* C1: ANX TCPC + Mux */
-#define CONFIG_USB_PD_ITE_ACTIVE_PORT_COUNT 1
+#define CONFIG_USB_PD_TCPM_RAA489000
+
+/* USB defines specific to external TCPCs */
+#define CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+#define CONFIG_USB_PD_VBUS_DETECT_TCPC
+#define CONFIG_USB_PD_DISCHARGE_TCPC
+#define CONFIG_USB_PD_TCPC_LOW_POWER
+#define CONFIG_USB_PD_5V_EN_CUSTOM
 
 /* Thermistors */
 #define CONFIG_TEMP_SENSOR
@@ -86,6 +90,8 @@
 
 #define CONFIG_USBC_RETIMER_TUSB544		/* C1 Redriver: TUSB544 */
 
+
+
 #ifndef __ASSEMBLER__
 
 #include "gpio_signal.h"
@@ -97,13 +103,7 @@ enum chg_id {
 	CHARGER_NUM,
 };
 
-enum pwm_channel {
-	PWM_CH_KBLIGHT,
-	PWM_CH_LED_RED,
-	PWM_CH_LED_GREEN,
-	PWM_CH_LED_BLUE,
-	PWM_CH_COUNT,
-};
+
 
 /* Motion sensors */
 enum sensor_id {
@@ -118,28 +118,20 @@ enum adc_channel {
 	ADC_VSNS_PP3300_A,     /* ADC0 */
 	ADC_TEMP_SENSOR_1,     /* ADC2 */
 	ADC_TEMP_SENSOR_2,     /* ADC3 */
-	ADC_SUB_ANALOG,        /* ADC13 */
+	ADC_TEMP_SENSOR_3,     /* ADC15*/
 	ADC_CH_COUNT
 };
 
 enum temp_sensor_id {
 	TEMP_SENSOR_1,
 	TEMP_SENSOR_2,
+	TEMP_SENSOR_3,
 	TEMP_SENSOR_COUNT
 };
 
 /* List of possible batteries */
 enum battery_type {
-	BATTERY_LGC15,
-	BATTERY_PANASONIC_AP15O5L,
-	BATTERY_SANYO,
-	BATTERY_SONY,
-	BATTERY_SMP_AP13J7K,
-	BATTERY_PANASONIC_AC15A3J,
-	BATTERY_LGC_AP18C8K,
-	BATTERY_MURATA_AP18C4K,
-	BATTERY_LGC_AP19A8K,
-	BATTERY_LGC_G023,
+	BATTERY_C21N2018,
 	BATTERY_TYPE_COUNT,
 };
 
