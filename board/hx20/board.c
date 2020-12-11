1a293c9d29fdf6df177405b8f20a25d5f08580d6
Josh Tsai <Josh_Tsai@compal.corp-partner.google.com>
2020-12-11 08:56:21 +0800

    Move update_power_limit to sci_enable (acpi_driver_ready)
    In windows system, peci communicate need to wait ACPI driver ready
    
    Signed-off-by: Josh Tsai <Josh_Tsai@compal.corp-partner.google.com>

--- board/hx20/board.c
+++ board/hx20/board.c
@@ -770,6 +770,8 @@ static void sci_enable(void)
 	if (*host_get_customer_memmap(0x00) == 1) {
 	/* when host set EC driver ready flag, EC need to enable SCI */
 		lpc_set_host_event_mask(LPC_HOST_EVENT_SCI, 0x20AF92AFF);
+
+		update_power_limit();
 	} else
 		hook_call_deferred(&sci_enable_data, 250 * MSEC);
 }
