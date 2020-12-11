e66095d53b3b5b5c1951fa19f47ec15dce0bbae8
Craig Hesling <hesling@chromium.org>
2020-12-11 11:56:36 -0800

    bloonchipper: Comment cleanup
    No code change.
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I6f64cf7980a00093c8403e83309988e3c00ba5c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2587568
    Reviewed-by: Yicheng Li <yichengli@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- board/hatch_fp/board.c
+++ board/hatch_fp/board.c
@@ -2,7 +2,6 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-/* Meowth Fingerprint MCU configuration */
 
 #include "common.h"
 #include "console.h"
