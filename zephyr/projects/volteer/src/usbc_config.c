458ee6e3fef4b582f930a488e012d5bb549288ed
Simon Glass <sjg@chromium.org>
2020-12-11 17:42:20 -0700

    Add volteer USB-C support code
    This enables various options on volteer so that some of the USB-C code
    builds.
    
    BUG=b:175434113
    TEST=build and run on volteer
    
    Cq-Depend: chromium:2587225
    Change-Id: I48cc7a0e50a76338d86ba03f24f5fb169c907143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/zephyr-chrome/+/2585918
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Simon Glass <sjg@chromium.org>
    Tested-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2630165
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

---
+++ zephyr/projects/volteer/src/usbc_config.c
@@ -0,0 +1,148 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* USBC PPC chip list.  Copied from volteer board.c in platform/ec. */
+
+#include <stdbool.h>
+#include <zephyr.h>
+
+#include "charge_manager.h"
+#include "charge_state.h"
+#include "gpio_map.h"
+#include "gpio_signal.h"
+#include "usbc_ocp.h"
+#include "usbc_ppc.h"
+#include "../driver/ppc/sn5s330.h"
+#include "../driver/ppc/syv682x.h"
+#include "../driver/tcpm/tusb422.h"
+
+#define SN5S330_ADDR0_FLAGS 0x40
+#define SYV682X_ADDR0_FLAGS		0x40
+
+#define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+enum usbc_port {
+	USBC_PORT_C0 = 0,
+	USBC_PORT_C1,
+	USBC_PORT_COUNT
+};
+
+struct ppc_config_t ppc_chips[] = {
+	[USBC_PORT_C0] = {
+		.i2c_port = I2C_PORT_USB_C0,
+		.i2c_addr_flags = SN5S330_ADDR0_FLAGS,
+		.drv = &sn5s330_drv,
+	},
+	[USBC_PORT_C1] = {
+		.i2c_port = I2C_PORT_USB_C1,
+		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
+		.drv = &syv682x_drv,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(ppc_chips) == USBC_PORT_COUNT);
+unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
+
+/* FIXME: For some reason these don't get defined by gpio_signal.h */
+#define GPIO_USB_C0_OC_ODL 0
+#define GPIO_USB_C1_OC_ODL 1
+
+void board_overcurrent_event(int port, int is_overcurrented)
+{
+	/* Note that the level is inverted because the pin is active low. */
+	switch (port) {
+	case USBC_PORT_C0:
+		gpio_set_level(GPIO_USB_C0_OC_ODL, !is_overcurrented);
+		break;
+	case USBC_PORT_C1:
+		gpio_set_level(GPIO_USB_C1_OC_ODL, !is_overcurrented);
+		break;
+	}
+}
+
+/******************************************************************************/
+/* USBC TCPC configuration */
+const struct tcpc_config_t tcpc_config[] = {
+	[USBC_PORT_C0] = {
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_USB_C0,
+			.addr_flags = TUSB422_I2C_ADDR_FLAGS,
+		},
+		.drv = &tusb422_tcpm_drv,
+	},
+	[USBC_PORT_C1] = {
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_USB_C1,
+			.addr_flags = TUSB422_I2C_ADDR_FLAGS,
+		},
+		.drv = &tusb422_tcpm_drv,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(tcpc_config) == USBC_PORT_COUNT);
+BUILD_ASSERT(CONFIG_USB_PD_PORT_MAX_COUNT == USBC_PORT_COUNT);
+
+int board_set_active_charge_port(int port)
+{
+	int is_valid_port = (port >= 0 &&
+			     port < CONFIG_USB_PD_PORT_MAX_COUNT);
+	int i;
+
+	if (port == CHARGE_PORT_NONE) {
+		CPRINTSUSB("Disabling all charger ports");
+
+		/* Disable all ports. */
+		for (i = 0; i < ppc_cnt; i++) {
+			/*
+			 * Do not return early if one fails otherwise we can
+			 * get into a boot loop assertion failure.
+			 */
+			if (ppc_vbus_sink_enable(i, 0))
+				CPRINTSUSB("Disabling C%d as sink failed.", i);
+		}
+
+		return EC_SUCCESS;
+	} else if (!is_valid_port) {
+		return EC_ERROR_INVAL;
+	}
+
+
+	/* Check if the port is sourcing VBUS. */
+	if (ppc_is_sourcing_vbus(port)) {
+		CPRINTFUSB("Skip enable C%d", port);
+		return EC_ERROR_INVAL;
+	}
+
+	CPRINTSUSB("New charge port: C%d", port);
+
+	/*
+	 * Turn off the other ports' sink path FETs, before enabling the
+	 * requested charge port.
+	 */
+	for (i = 0; i < ppc_cnt; i++) {
+		if (i == port)
+			continue;
+
+		if (ppc_vbus_sink_enable(i, 0))
+			CPRINTSUSB("C%d: sink path disable failed.", i);
+	}
+
+	/* Enable requested charge port. */
+	if (ppc_vbus_sink_enable(port, 1)) {
+		CPRINTSUSB("C%d: sink path enable failed.", port);
+		return EC_ERROR_UNKNOWN;
+	}
+
+	return EC_SUCCESS;
+}
+
+__overridable void board_set_charge_limit(int port, int supplier, int charge_ma,
+			    int max_ma, int charge_mv)
+{
+	charge_set_input_current_limit(MAX(charge_ma,
+					CONFIG_CHARGER_INPUT_CURRENT),
+					charge_mv);
+}
