3a03483f801c5b21aa662d6a2da97fed042f60ee
Dossym Nurmukhanov <dossym@google.com>
2020-12-12 18:51:29 +0000

    EC: Removing an unused i2cm_init function signature
    BUG=none
    TEST=built several boards
    BRANCH=none
    
    Signed-off-by: dossym@chromium.org
    Change-Id: I4e9b52fb5c7ef6dab4db30f98cece9b5635f699c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2587805
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/i2c.h
+++ include/i2c.h
@@ -507,12 +507,6 @@ int i2c_set_response(int port, uint8_t *buf, int len);
 void i2c_init(void);
 
 /**
- * Initialize i2c controller ports. This function can be called for cases where
- * i2c ports are not initialized by default from main.c.
- */
-void i2cm_init(void);
-
-/**
  * Board-level function to determine whether i2c passthru should be allowed
  * on a given port.
  *
