11dda529f98acf1fdbbc1be0cf39ff8801ea25f8
Yicheng Li <yichengli@chromium.org>
2020-12-14 16:25:50 -0800

    fingerprint: Only link FPC libraries if there's a FPC sensor
    If a board doesn't have a FPC sensor, don't build FPC.
    
    BRANCH=none
    BUG=b:171812737, b:175158241
    TEST=Change the sensor config on nocturne_fp to ELAN and build with
         ELAN's driver and library.
    
    Signed-off-by: Yicheng Li <yichengli@chromium.org>
    Change-Id: I90eca478b6eadfbd373154c52e13f734473abfdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2591868
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- driver/fingerprint/fpc/build.mk
+++ driver/fingerprint/fpc/build.mk
@@ -7,6 +7,10 @@
 # Note that this variable includes the trailing "/"
 _fpc_cur_dir:=$(dir $(lastword $(MAKEFILE_LIST)))
 
+# Only build if FPC sensor is configured.
+ifneq (,$(filter rw,$(CONFIG_FP_SENSOR_FPC1145) $(CONFIG_FP_SENSOR_FPC1025) \
+	$(CONFIG_FP_SENSOR_FPC1035)))
+
 ifeq ($(CONFIG_FP_SENSOR_FPC1145),rw)
 include $(_fpc_cur_dir)libfp/build.mk
 else ifeq ($(CONFIG_FP_SENSOR_FPC1025),rw)
@@ -21,4 +25,6 @@ dirs-rw+="$(_fpc_cur_dir)"
 
 # Only build these objects for the RW image
 all-obj-rw+=$(_fpc_cur_dir)fpc_sensor.o
+endif
+
 endif
