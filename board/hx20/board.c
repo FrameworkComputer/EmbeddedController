3d8b62bd0c75b03e8a11d2b820c7eefaac429b0b
Kieran Levin <ktl@frame.work>
2020-12-17 19:34:32 -0800

    change LED names for EVT only
    Signed-off-by: Kieran Levin <ktl@frame.work>

--- board/hx20/board.c
+++ board/hx20/board.c
@@ -201,16 +201,16 @@ const struct pwm_t pwm_channels[] = {
 		.channel = 1,
 		.flags = PWM_CONFIG_DSLEEP,
 	},
-	[PWM_CH_DB1_LED_BLUE] = {
-		.channel = 8,
-		.flags = PWM_CONFIG_DSLEEP,
-	},
-	[PWM_CH_DB1_LED_GREEN] = {
+	[PWM_CH_DB1_LED_GREEN_EVT] = {
 		.channel = 4,
 		.flags = PWM_CONFIG_DSLEEP,
 	},
-	[PWM_CH_DB1_LED_GREEN_DVT] = {
+	[PWM_CH_DB1_LED_GREEN] = {
 		.channel = 3,
+		.flags = PWM_CONFIG_DSLEEP,
+	},
+	[PWM_CH_DB1_LED_BLUE] = {
+		.channel = 8,
 		.flags = PWM_CONFIG_DSLEEP,
 	},
 	[PWM_CH_FPR_LED_RED] = {
