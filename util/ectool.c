6815dbc5c1ff98d53fe483f3e336cc0de6c5275d
Prashant Malani <pmalani@chromium.org>
2020-12-22 14:56:54 -0800

    ectool: Add inventory feature string for mux ack
    Update the `ectool inventory` ec_feature_names array with an entry for
    the "Type C Mux requires AP ack" feature.
    
    BUG=b:161327513
    BRANCH=None
    TEST=Build and deploy ec-utils to volteer. Run 'ectool inventory' and
         verify that the mux ack feature shows the provided feature
         description string.
    
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Change-Id: I0642947991dece36eb8a5c3332b305ff24ab113e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2601169
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -862,6 +862,8 @@ static const char * const ec_feature_names[] = {
 	[EC_FEATURE_TYPEC_CMD] = "TCPMv2 Type-C commands",
 	[EC_FEATURE_TYPEC_REQUIRE_AP_MODE_ENTRY] =
 		"Host-controlled Type-C mode entry",
+	[EC_FEATURE_TYPEC_MUX_REQUIRE_AP_ACK] =
+		"AP ack for Type-C mux configuration",
 };
 
 int cmd_inventory(int argc, char *argv[])
