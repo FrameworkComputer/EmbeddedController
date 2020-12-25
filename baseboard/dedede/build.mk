251a1ee0ae893929f41c7afcd32741ea0f3805a2
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2020-12-25 18:14:26 +0800

    Dedede: Initiate the first version of parsing SSFC of CBI
    Initiate the helper functions to parse the SSFC of CBI.
    The first component define lid / base sensors in this version.
    
    BUG=b:176314158, b:176314157
    BRANCH=main
    TEST=Make buildall PASS.
    
    Change-Id: I1dbf06b7f2c8037a98e4d4c5c3d7beee556270ce
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2603092
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- baseboard/dedede/build.mk
+++ baseboard/dedede/build.mk
@@ -1,11 +1,11 @@
 # -*- makefile -*-
-# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
 # Baseboard specific files build
 #
 
-baseboard-y=baseboard.o cbi_fw_config.o
+baseboard-y=baseboard.o cbi_fw_config.o cbi_ssfc.o
 baseboard-$(VARIANT_DEDEDE_EC_NPCX796FC)+=variant_ec_npcx796fc.o
 baseboard-$(VARIANT_DEDEDE_EC_IT8320)+=variant_ec_it8320.o
