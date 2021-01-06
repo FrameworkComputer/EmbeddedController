c6baaa7c8201decf21362d55973c7176d300df95
Rob Barnes <robbarnes@google.com>
2021-01-06 09:03:59 -0700

    guybrush: implement usb_pd_policy stubs
    Implement pd stubs in usb_pd_policy. Based on zork implementation.
    
    BUG=b:175400400
    TEST=Build
    BRANCH=None
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: I4b87518bf758d3eb7245ad4eafd5a99dd824a150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2613225
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- baseboard/guybrush/usb_pd_policy.c
+++ baseboard/guybrush/usb_pd_policy.c
@@ -20,19 +20,55 @@
 
 int pd_check_vconn_swap(int port)
 {
-	/* TODO */
-	return 0;
+	/*
+	 * Do not allow vconn swap 5V rail is off
+	 * S5_PGOOD depends on PG_PP5000_S5 being asserted,
+	 * so GPIO_S5_PGOOD is a reasonable proxy for PP5000_S5
+	 */
+	return gpio_get_level(GPIO_S5_PGOOD);
 }
 
 void pd_power_supply_reset(int port)
 {
-	/* TODO */
+	/*
+	 * Don't need to shutoff VBus if we are not sourcing it
+	 * TODO: Ensure Vbus sourcing is being disabled appropriately to
+	 *       avoid invalid TC states
+	 */
+	if (ppc_is_sourcing_vbus(port)) {
+		/* Disable VBUS. */
+		ppc_vbus_source_enable(port, 0);
+
+		/* Enable discharge if we were previously sourcing 5V */
+		if (IS_ENABLED(CONFIG_USB_PD_DISCHARGE))
+			pd_set_vbus_discharge(port, 1);
+	}
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 }
 
 int pd_set_power_supply_ready(int port)
 {
-	/* TODO */
-	return 0;
+	int rv;
+
+	/* Disable charging. */
+	rv = ppc_vbus_sink_enable(port, 0);
+	if (rv)
+		return rv;
+
+	if (IS_ENABLED(CONFIG_USB_PD_DISCHARGE))
+		pd_set_vbus_discharge(port, 0);
+
+	/* Provide Vbus. */
+	rv = ppc_vbus_source_enable(port, 1);
+	if (rv)
+		return rv;
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+
+	return EC_SUCCESS;
 }
 
 int board_vbus_source_enabled(int port)
