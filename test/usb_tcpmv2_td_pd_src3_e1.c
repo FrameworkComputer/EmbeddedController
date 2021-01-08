a2d869ec640ac1bcbc661276d90804b50d717e79
Denis Brockus <dbrockus@google.com>
2021-01-08 12:58:13 -0700

    TCPMv2: Compliance Unit Test - TD.PD.SRC3.E1 cleanup
    cleanup message offsets to be more clear
    
    BUG=none
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I7874b299e5518bd51cf884a41e3cc3c5833c8149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2618567
    Tested-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Commit-Queue: Edward Hill <ecgh@chromium.org>
    Auto-Submit: Denis Brockus <dbrockus@chromium.org>

--- test/usb_tcpmv2_td_pd_src3_e1.c
+++ test/usb_tcpmv2_td_pd_src3_e1.c
@@ -12,6 +12,8 @@
 #include "usb_tc_sm.h"
 
 #define BUFFER_SIZE 100
+
+#define HEADER_BYTE_OFFSET 1
 #define HEADER_BYTE_CNT 2
 #define PDO_BYTE_CNT 4
 
@@ -76,10 +78,12 @@ int test_td_pd_src3_e1(void)
 		EC_SUCCESS, "%d");
 	TEST_GE(msg_len, HEADER_BYTE_CNT, "%d");
 
-	header = UINT16_FROM_BYTE_ARRAY_LE(data, 1);
+	header = UINT16_FROM_BYTE_ARRAY_LE(data, HEADER_BYTE_OFFSET);
 	pd_cnt = PD_HEADER_CNT(header);
 	TEST_NE(pd_cnt, 0, "%d");
-	TEST_EQ(msg_len, HEADER_BYTE_CNT + (pd_cnt * PDO_BYTE_CNT) + 1, "%d");
+	TEST_EQ(msg_len, HEADER_BYTE_OFFSET +
+			 HEADER_BYTE_CNT +
+			 (pd_cnt * PDO_BYTE_CNT), "%d");
 	TEST_EQ(PD_HEADER_PROLE(header), PD_ROLE_SOURCE, "%d");
 	TEST_EQ(PD_HEADER_REV(header), REVISION_3, "%d");
 	TEST_EQ(PD_HEADER_DROLE(header), PD_ROLE_DFP, "%d");
@@ -92,7 +96,8 @@ int test_td_pd_src3_e1(void)
 	 *    2. Voltage field = 100 (5 V)
 	 *    3. Bits 23..22 = 000b (Reserved)
 	 */
-	pdo = UINT32_FROM_BYTE_ARRAY_LE(&data[2], 1);
+	pdo = UINT32_FROM_BYTE_ARRAY_LE(data, HEADER_BYTE_OFFSET +
+					      HEADER_BYTE_CNT);
 
 	type = pdo & PDO_TYPE_MASK;
 	TEST_EQ(type, PDO_TYPE_FIXED, "%d");
@@ -130,8 +135,10 @@ int test_td_pd_src3_e1(void)
 		int offset;
 		uint32_t voltage;
 
-		offset = HEADER_BYTE_CNT + (i * PDO_BYTE_CNT);
-		pdo = UINT32_FROM_BYTE_ARRAY_LE(&data[offset], 1);
+		offset = HEADER_BYTE_OFFSET +
+			 HEADER_BYTE_CNT +
+			 (i * PDO_BYTE_CNT);
+		pdo = UINT32_FROM_BYTE_ARRAY_LE(data, offset);
 
 		type = pdo & PDO_TYPE_MASK;
 		if (type == PDO_TYPE_FIXED) {
