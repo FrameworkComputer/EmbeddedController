32b90cdae2520c2bb8e4f68da50575621dc270c2
Jack Rosenthal <jrosenth@chromium.org>
2021-01-11 15:56:27 -0700

    zephyr: copy zephyr-chrome/tests/dts/bindings/i2c to platform/ec
    Previous Git history can be found here:
    https://chromium.googlesource.com/chromiumos/platform/zephyr-chrome/+log/cc6e5d19939c03ac091b5e91d1a3b511ba99b68a/tests/dts/bindings/i2c
    
    BUG=b:177157241
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I6a0d774f8400a9fd2d7a8ce7f96943b3601debe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2622899
    Commit-Queue: Yuval Peress <peress@chromium.org>
    Reviewed-by: Yuval Peress <peress@chromium.org>

---
+++ zephyr/test/i2c_dts/src/main.c
@@ -0,0 +1,30 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <device.h>
+#include <devicetree.h>
+#include <ztest.h>
+
+static void test_i2c_get_device(void)
+{
+	const struct device *accel0 = device_get_binding(
+		DT_PROP_BY_PHANDLE(DT_PATH(named_i2c_ports, accel_0), i2c_port,
+				   label));
+	const struct device *bmi_i2c = device_get_binding(
+		DT_LABEL(DT_NODELABEL(bmi_i2c)));
+
+	zassert_not_null(accel0, "accel0 was NULL");
+	zassert_not_null(bmi_i2c, "bmi_i2c was NULL");
+	zassert_equal(accel0, bmi_i2c,
+		"named_i2c_ports/accel0 and bmi_i2c should resolve to the same device");
+}
+
+/* test case main entry */
+void test_main(void)
+{
+	ztest_test_suite(test_i2c_bindings,
+			 ztest_user_unit_test(test_i2c_get_device));
+	ztest_run_test_suite(test_i2c_bindings);
+}
