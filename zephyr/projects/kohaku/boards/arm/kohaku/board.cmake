0563daab68246e1e36de630356e9fe202dd29639
Jack Rosenthal <jrosenth@chromium.org>
2021-01-14 19:15:34 -0700

    zephyr: initial support for kohaku
    Kohaku uses the same EC chip as volteer.  Adding a second device will
    help give us a tiny bit more build diversity too, which is a good
    thing probably.
    
    This just adds a minimal build for kohaku with just UART support.  No
    power sequencing, keyboard, USB-C, charging, etc.
    
    BUG=b:177609422
    BRANCH=none
    TEST=flash onto kohaku, observe working shell and timers
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I277510081c9e06b516b6c29f790e16dd1dfe8028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2631361
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ zephyr/projects/kohaku/boards/arm/kohaku/board.cmake
@@ -0,0 +1,6 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is file path you actually flash to the target.
+set(TARGET_IMAGE_FILE  ${PROJECT_BINARY_DIR}/zephyr_kohaku.bin)
---
+++ zephyr/projects/kohaku/include/i2c_map.h
@@ -0,0 +1,11 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_CHROME_I2C_MAP_H
+#define __ZEPHYR_CHROME_I2C_MAP_H
+
+/* TODO(b/177609422): add kohaku i2c mappings */
+
+#endif /* __ZEPHYR_CHROME_I2C_MAP_H */
