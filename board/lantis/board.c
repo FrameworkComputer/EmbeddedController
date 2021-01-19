77b45ffae65428a26c6616ce3bdf0165da7a0f77
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-01-19 10:28:59 +0800

    lantis: vivaldi keyboard initialization
    Initialize vivaldi keyboard on lantis.
    
    BUG=b:175672478
    BRANCH=dedede
    TEST=Make sure that all top row keys work correctly.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: If6edd4f2638ec79dc444ce013ae11a6ed6844fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2635878
    Reviewed-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- board/lantis/board.c
+++ board/lantis/board.c
@@ -23,6 +23,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "intc.h"
+#include "keyboard_8042.h"
 #include "keyboard_scan.h"
 #include "lid_switch.h"
 #include "power.h"
@@ -353,6 +354,29 @@ struct motion_sensor_t motion_sensors[] = {
 };
 
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+static const struct ec_response_keybd_config keybd1 = {
+	.num_top_row_keys = 10,
+	.action_keys = {
+		TK_BACK,		/* T1 */
+		TK_FORWARD,		/* T2 */
+		TK_REFRESH,		/* T3 */
+		TK_FULLSCREEN,		/* T4 */
+		TK_OVERVIEW,		/* T5 */
+		TK_BRIGHTNESS_DOWN,	/* T6 */
+		TK_BRIGHTNESS_UP,	/* T7 */
+		TK_VOL_MUTE,		/* T8 */
+		TK_VOL_DOWN,		/* T9 */
+		TK_VOL_UP,		/* T10 */
+	},
+	/* No function keys, no numeric keypad and no screenlock key */
+};
+
+__override const struct ec_response_keybd_config
+*board_vivaldi_keybd_config(void)
+{
+	return &keybd1;
+}
 
 void board_init(void)
 {
