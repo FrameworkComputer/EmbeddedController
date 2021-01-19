4ad071cebdae5a9acf2c0c9ca21d56b94e04c24c
David Huang <david.huang@quanta.corp-partner.google.com>
2021-01-19 11:16:04 +0800

    voema: Add battery AP15O5L
    Add new battery AP15O5L
    
    BUG=b:176860886
    BRANCH=main
    TEST=1) See "[found batt:PANASONIC KT00305013]" on EC console
         2) battery readings looks reasonable.
         3) cutoff workable.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: Ie2522ab2a782def304ae2588b7f34d7422e97345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2639418
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- board/voema/battery.c
+++ board/voema/battery.c
@@ -62,7 +62,35 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 75,
 		},
 	},
+	/* Panasonic AP1505L Battery Information */
+	[BATTERY_PANASONIC_AP15O5L] = {
+		.fuel_gauge = {
+			.manuf_name = "PANASONIC KT00305013",
+			.device_name = "AP15O5L",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.reg_addr = 0x0,
+				.reg_mask = 0x4000,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 13200,
+			.voltage_normal		= 11550, /* mV */
+			.voltage_min		= 9000, /* mV */
+			.precharge_current	= 256,	/* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_LGC011;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_PANASONIC_AP15O5L;
