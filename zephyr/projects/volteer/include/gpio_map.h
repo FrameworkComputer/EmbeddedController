c924504eaebea3e45b99e5b836da8b51903897d8
Hyungwoo Yang <hyungwoo.yang@intel.corp-partner.google.com>
2021-01-19 23:50:17 -0800

    volteer: enable MKBP event for Zephyr
    Enable MKBP event for Zephyr on Volteer.
    
    BUG=b:173507858
    BRANCH=none
    TEST=make buildall -j8
    build volteer on zephyr
    
    Signed-off-by: Hyungwoo Yang <hyungwoo.yang@intel.corp-partner.google.com>
    Change-Id: I0c663f5a0bde8dece09c48a012de8f1903dd82d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2639856
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/projects/volteer/include/gpio_map.h
+++ zephyr/projects/volteer/include/gpio_map.h
@@ -24,6 +24,7 @@
 #define GPIO_AC_PRESENT            NAMED_GPIO(acok_od)
 #define GPIO_CPU_PROCHOT           NAMED_GPIO(ec_prochot_odl)
 #define GPIO_EC_BATT_PRES_ODL      NAMED_GPIO(ec_batt_pres_odl)
+#define GPIO_EC_INT_L              NAMED_GPIO(ec_pch_int_odl)
 #define GPIO_EC_PCH_SYS_PWROK      NAMED_GPIO(ec_pch_sys_pwrok)
 #define GPIO_EC_PCH_WAKE_ODL       NAMED_GPIO(ec_pch_wake_odl)
 #define GPIO_EN_PP3300_A           NAMED_GPIO(en_pp3300_a)
