3c5f55ace377e1c939be2397c4fe73a3b2ee4fb4
Jack Rosenthal <jrosenth@chromium.org>
2021-01-25 13:00:59 -0700

    zephyr: provide a header for cpu.h
    Provide a minimal header for cpu.h, which is used by both
    panic_output.c and system.c.
    
    BUG=b:174481378,b:178011288
    BRANCH=none
    TEST=compiles with panic_output.c
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I16ca7350d58bc392d87a6f0bbf9d6080e607998d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2647951
    Reviewed-by: Yuval Peress <peress@chromium.org>

---
+++ zephyr/shim/include/cpu.h
@@ -0,0 +1,14 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_CPU_H
+#define __CROS_EC_CPU_H
+
+/* Do nothing for Zephyr */
+static inline void cpu_init(void)
+{
+}
+
+#endif /* __CROS_EC_CPU_H */
