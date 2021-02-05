78c6af23aced2bd462056bd90b6885a3bb8318df
Eric Yilun Lin <yllin@chromium.org>
2021-02-05 15:43:44 +0800

    spherion: add new board
    Copy asurada board with some minor fix to build successfully.
    
    BUG=b:178440482
    TEST=make BOARD=spherion
    BRANCH=none
    
    Change-Id: Ia679a542460ace781eae0f9d60ad65a25d96eb3c
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2677608
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>

---
+++ board/spherion/build.mk
@@ -0,0 +1,14 @@
+# -*- makefile -*-
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+# the IC is ITE IT8xxx2
+CHIP:=it83xx
+CHIP_FAMILY:=it8xxx2
+CHIP_VARIANT:=it81202bx_1024
+BASEBOARD:=asurada
+
+board-y+=battery.o board.o led.o
