e1e049f2f5ff181574364cb25786eefa9f92b4e7
Jack Rosenthal <jrosenth@chromium.org>
2021-02-16 13:44:31 -0700

    zephyr: delbin: bring up eSPI
    Enable eSPI and host commands.
    
    BUG=b:180410072
    BRANCH=none
    TEST=run "hostevent" on delbin
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I2422b02dd8a2ece58793b10beed213ee00c8f418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2699527
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- zephyr/projects/delbin/include/gpio_map.h
+++ zephyr/projects/delbin/include/gpio_map.h
@@ -9,6 +9,7 @@
 #include <devicetree.h>
 #include <gpio_signal.h>
 
+#define GPIO_EC_PCH_WAKE_ODL	NAMED_GPIO(ec_pch_wake_odl)
 #define GPIO_ENTERING_RW	NAMED_GPIO(ec_entering_rw)
 #define GPIO_WP_L		NAMED_GPIO(ec_wp_l)
 
