5e607d6c41e92b2b15ee7c1310394ceaac1c49c7
David Huang <david.huang@quanta.corp-partner.google.com>
2021-02-18 10:07:48 +0800

    voema: fix base matrix
    Fix the x-axis of voema matrix.
    
    BUG=b:179001192
    BRANCH=main
    TEST=Check lid angle correct when DUT turn 90/180/270 degrees.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I6c1f009651dff2227480d21a44894f250f4aaeb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2702996
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- board/voema/sensors.c
+++ board/voema/sensors.c
@@ -87,7 +87,7 @@ static const mat33_fp_t lid_standard_ref = {
 };
 
 const mat33_fp_t base_standard_ref = {
-	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, FLOAT_TO_FP(-1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
 	{ 0, 0, FLOAT_TO_FP(-1)}
 };
