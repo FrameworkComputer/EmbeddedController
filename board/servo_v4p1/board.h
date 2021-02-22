118fa0ff5a6361066b213522798697ab399c1e84
Brian J. Nemec <bnemec@chromium.org>
2021-02-22 14:21:13 -0800

    servo_v4p1: Enable 5A and 20V charging
    Enables 5A and 20V charging on all Servo_v4p1's.
    
    There is a small population of servo_v4p1 devices with 3A cables
    but these represent about 100 total units between the early
    builds. The setting was not set as a board id configuration
    as stress tests show this process will still be safe and the
    Servo_v4p1's that are current limited are easy to keep within
    the 3A current limits. The 5A cables are visually distinctive
    and most chargers and DUTs are limited to 3A.
    
    BUG=b:176110980
    BRANCH=servo
    TEST=Validated the Servo_v4p1 can deliver power at 4.5A and 20V
        using a 90W charger and a resistive load. Verified current
        using meters and verified Servo_v4p1 does not heat excessively
        significantly after over 2 hours at the cable or PCB.
    TEST=Repeated the same test on older Servo_v4p1 devices with the
        smaller shunt resistors and 3A compliant cable and verified
        components remain at reasonable temperatures.
    
    Signed-off-by: Brian Nemec <bnemec@chromium.org>
    Change-Id: If9e67081fe4441ca0fe1039c3189e0673eb8f13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2714031
    Reviewed-by: Jan Dąbroś <jsd@semihalf.com>
    Reviewed-by: Matthew Blecker <matthewb@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- board/servo_v4p1/board.h
+++ board/servo_v4p1/board.h
@@ -235,8 +235,8 @@
 
 /* Define typical operating power and max power */
 #define PD_OPERATING_POWER_MW 15000
-#define PD_MAX_POWER_MW       60000
-#define PD_MAX_CURRENT_MA     3000
+#define PD_MAX_POWER_MW       100000
+#define PD_MAX_CURRENT_MA     5000
 #define PD_MAX_VOLTAGE_MV     20000
 
 /* Add the raw option to the i2c_xfer command */
