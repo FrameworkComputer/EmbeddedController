450fcadcb0e43d739b0de4d05528fee9d394388e
tim <tim2.lin@ite.corp-partner.google.com>
2021-02-26 14:12:27 +0800

    zephyr: initial bringup for it8xxx2_evb
    Initial bringup for it8xxx2_evb. The function includes
    UART console and basic commands.
    
    BUG=b:185202623
    BRANCH=none
    TEST=EC boots on it8xxx2_evb.
          Console commands test: version
    
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>
    Change-Id: I96839f96b8128460a123fe0344b4ae077b6e8761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2752568
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

---
+++ zephyr/projects/it8xxx2_evb/include/i2c_map.h
@@ -0,0 +1,16 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_CHROME_I2C_MAP_H
+#define __ZEPHYR_CHROME_I2C_MAP_H
+
+#include <devicetree.h>
+
+#include "config.h"
+
+/* We need registers.h to get the chip specific defines for now */
+#include "i2c/i2c.h"
+
+#endif /* __ZEPHYR_CHROME_I2C_MAP_H */
---
+++ zephyr/projects/it8xxx2_evb/include/pwm_map.h
@@ -0,0 +1,15 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_CHROME_PWM_MAP_H
+#define __ZEPHYR_CHROME_PWM_MAP_H
+
+#include <devicetree.h>
+
+#include "config.h"
+
+#include "pwm/pwm.h"
+
+#endif /* __ZEPHYR_CHROME_PWM_MAP_H */
