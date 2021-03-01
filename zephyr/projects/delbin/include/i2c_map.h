685b451618248477207c828275f82833403c05e8
Jack Rosenthal <jrosenth@chromium.org>
2021-03-01 16:38:36 -0700

    zephyr: delbin: bring-up named-i2cs
    Add required formalisms (named-i2cs and i2c_map.h) to get i2c working
    with Zephyr shim.
    
    BUG=b:180410072
    BRANCH=none
    TEST=compile with CBI code (follow-up CL)
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I3a50331ca5a42fa45840c24c381513ba4fb7c262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2727853
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/projects/delbin/include/i2c_map.h
+++ zephyr/projects/delbin/include/i2c_map.h
@@ -6,4 +6,20 @@
 #ifndef __ZEPHYR_I2C_MAP_H
 #define __ZEPHYR_I2C_MAP_H
 
+#include <devicetree.h>
+
+#include "i2c/i2c.h"
+
+#define I2C_PORT_ACCEL		I2C_PORT_SENSOR
+#define I2C_PORT_SENSOR		NAMED_I2C(sensor)
+#define I2C_PORT_USB_C0		NAMED_I2C(usb_c0)
+#define I2C_PORT_USB_C1		NAMED_I2C(usb_c1)
+#define I2C_PORT_USB_1_MIX	NAMED_I2C(usb1_mix)
+#define I2C_PORT_POWER		NAMED_I2C(power)
+#define I2C_PORT_EEPROM		NAMED_I2C(eeprom)
+
+#define I2C_ADDR_EEPROM_FLAGS	0x50
+#define I2C_PORT_BATTERY	I2C_PORT_POWER
+#define I2C_PORT_CHARGER	I2C_PORT_EEPROM
+
 #endif /* __ZEPHYR_I2C_MAP_H */
