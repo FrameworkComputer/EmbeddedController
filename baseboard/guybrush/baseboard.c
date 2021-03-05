2b3b4380a1f4c6caff99fba9ac8e64c406aae586
Rob Barnes <robbarnes@google.com>
2021-03-05 16:19:24 -0700

    guybrush: add current monitor adc signals
    Add the two current monitor signals from the PCORE. These will only be
    used for debugging.
    
    BUG=None
    TEST=Build
    BRANCH=None
    
    Change-Id: I9d25fa263cd4ef9526a8c493a703d020672338c5
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2740560
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -163,7 +163,20 @@ const struct adc_t adc_channels[] = {
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
 	},
-
+	[ADC_CORE_IMON1] = {
+		.name = "CORE_I",
+		.input_ch = NPCX_ADC_CH3,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
+	[ADC_SOC_IMON2] = {
+		.name = "SOC_I",
+		.input_ch = NPCX_ADC_CH4,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
