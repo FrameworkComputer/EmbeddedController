aa61475c2f7f682f822e4c6b2b3656b6806522f0
YongBeum.Ha <ybha@samsung.corp-partner.google.com>
2021-03-10 12:17:46 +0900

    sasuke : lower input voltage on S0iX
    Lower input voltage on S0iX to reduce power consumption
    
    BUG=b:182221344
    BRANCH=None
    TEST=make -j BOARD=sasuke
    
    Change-Id: I96d7a9d48cc0f52910e80528fc8fec09818cd40a
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2747532
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- board/sasuke/battery.c
+++ board/sasuke/battery.c
@@ -8,6 +8,8 @@
 #include "battery_fuel_gauge.h"
 #include "charge_state.h"
 #include "common.h"
+#include "hooks.h"
+#include "usb_pd.h"
 #include "util.h"
 
 #define CHARGING_CURRENT_REDUCE	4000
@@ -109,3 +111,23 @@ enum ec_status charger_profile_override_set_param(uint32_t param,
 {
 	return EC_RES_INVALID_PARAM;
 }
+
+/* Lower our input voltage to 5V in S0iX when battery is full. */
+static void reduce_input_voltage_when_full(void)
+{
+        int max_pd_voltage_mv;
+        int port;
+
+        if (charge_get_percent() == 100 &&
+           chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+                max_pd_voltage_mv = 5000;
+        else
+                max_pd_voltage_mv = PD_MAX_VOLTAGE_MV;
+
+        if (pd_get_max_voltage() != max_pd_voltage_mv) {
+                for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++)
+                        pd_set_external_voltage_limit(port, max_pd_voltage_mv);
+        }
+}
+DECLARE_HOOK(HOOK_SECOND, reduce_input_voltage_when_full,
+             HOOK_PRIO_DEFAULT);
