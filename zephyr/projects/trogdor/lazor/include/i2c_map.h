c8690c18c3f605523709990ec3a0dcb5549a2b3b
Wai-Hong Tam <waihong@google.com>
2021-03-16 15:15:09 -0700

    zephyr: lazor: enable I2C
    Enable I2C. Lazor uses the I2C buses 0, 1, 2, 3, 5, and 7.
    Bus 3 is for WLC but no WLC connected. So leave it disabled.
    
    BUG=b:182398910
    BRANCH=None
    TEST=Tested on Lazor:
    
    uart:~$ i2c scan I2C_0_PORT_0
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:             -- -- -- -- -- 09 -- 0b -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 5f
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    3 devices found on I2C_0_PORT_0
    
    uart:~$ i2c scan I2C_1_PORT_0
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:             -- -- -- -- 08 09 0a 0b 0c 0d 0e 0f
    10: 10 11 12 13 14 15 16 17 -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: 40 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    17 devices found on I2C_1_PORT_0
    
    uart:~$ i2c scan I2C_2_PORT_0
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:             -- -- -- -- 08 09 0a 0b 0c 0d 0e 0f
    10: 10 11 12 13 14 15 16 17 -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: 40 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    17 devices found on I2C_2_PORT_0
    
    uart:~$ i2c scan I2C_3_PORT_0
    I2C: Device driver I2C_3_PORT_0 not found.
    
    uart:~$ i2c scan I2C_5_PORT_0
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:             -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 5f
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    1 devices found on I2C_5_PORT_0
    
    uart:~$ i2c scan I2C_7_PORT_0
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:             -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- 68 -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    1 devices found on I2C_7_PORT_0
    
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Change-Id: Ia2159bbe040f5308661911cf0dd5236d71b50484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2765153
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/projects/trogdor/lazor/include/i2c_map.h
+++ zephyr/projects/trogdor/lazor/include/i2c_map.h
@@ -10,4 +10,16 @@
 
 #include "i2c/i2c.h"
 
+#define I2C_PORT_BATTERY	I2C_PORT_POWER
+#define I2C_PORT_VIRTUAL	I2C_PORT_BATTERY
+#define I2C_PORT_CHARGER	I2C_PORT_POWER
+#define I2C_PORT_ACCEL		I2C_PORT_SENSOR
+
+#define I2C_PORT_POWER		NAMED_I2C(power)
+#define I2C_PORT_TCPC0		NAMED_I2C(tcpc0)
+#define I2C_PORT_TCPC1		NAMED_I2C(tcpc1)
+#define I2C_PORT_WLC		NAMED_I2C(wlc)
+#define I2C_PORT_EEPROM		NAMED_I2C(eeprom)
+#define I2C_PORT_SENSOR		NAMED_I2C(sensor)
+
 #endif /* __ZEPHYR_I2C_MAP_H */
