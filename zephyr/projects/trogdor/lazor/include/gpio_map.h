54c452f2568059199b97b59b59adc58f3b6359d2
Jack Rosenthal <jrosenth@chromium.org>
2021-03-18 12:20:18 -0600

    zephyr: lazor: enable switchcap
    Enable switchcap and SKU code from board directory, which is a
    dependency of power sequencing code.
    
    BUG=b:183054226
    BRANCH=none
    TEST=observe uart prints from switchcap init and sku init
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I6cd0cff071410d45e1478da04c6422fa346f34c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2774367
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

--- zephyr/projects/trogdor/lazor/include/gpio_map.h
+++ zephyr/projects/trogdor/lazor/include/gpio_map.h
@@ -13,9 +13,16 @@
 #define GPIO_BOARD_VERSION1		NAMED_GPIO(brd_id0)
 #define GPIO_BOARD_VERSION2		NAMED_GPIO(brd_id1)
 #define GPIO_BOARD_VERSION3		NAMED_GPIO(brd_id2)
+#define GPIO_DA9313_GPIO0		NAMED_GPIO(da9313_gpio0)
 #define GPIO_ENTERING_RW		NAMED_GPIO(ec_entering_rw)
 #define GPIO_LID_OPEN			NAMED_GPIO(lid_open_ec)
 #define GPIO_POWER_BUTTON_L		NAMED_GPIO(ec_pwr_btn_odl)
+#define GPIO_SKU_ID0			NAMED_GPIO(sku_id0)
+#define GPIO_SKU_ID1			NAMED_GPIO(sku_id1)
+#define GPIO_SKU_ID2			NAMED_GPIO(sku_id2)
+#define GPIO_SWITCHCAP_ON		NAMED_GPIO(switchcap_on)
+#define GPIO_SWITCHCAP_ON_L		NAMED_GPIO(switchcap_on)
+#define GPIO_SWITCHCAP_PG_INT_L		NAMED_GPIO(da9313_gpio0)
 #define GPIO_WP_L			NAMED_GPIO(ec_wp_odl)
 
 /*
@@ -37,6 +44,7 @@
 	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt) \
 	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
 	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
-		 power_button_interrupt)
+		 power_button_interrupt)                                  \
+	GPIO_INT(GPIO_SWITCHCAP_PG_INT_L, GPIO_INT_FALLING, ln9310_interrupt)
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
