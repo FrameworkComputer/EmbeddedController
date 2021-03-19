9b3facc3143c345c666a4fd59815f1883119ec8a
Wai-Hong Tam <waihong@google.com>
2021-03-19 14:16:53 -0700

    zephyr: lazor: enable PWM
    Enable PWM. Lazor uses PWM 3 and 5 for keyboard backlight and
    display backlight.
    
    The display backlight can't be tested yet. The EN pin of the
    LED driver IC is connected to an AND gate. One of its input is
    an AP's GPIO. Since the AP power sequence is not ready yet, the
    IC is disabled.
    
    BUG=b:182398910
    BRANCH=None
    TEST=Tested on Lazor:
    uart:~$ gpioset EN_PP5000_A 1
    uart:~$ pwmduty
    PWM channels:
      0: disabled
      1: disabled
    uart:~$ pwmduty 0 50  # The keyboard backlight is on
    Setting channel 0 to 50
      0: 50%
    uart:~$ pwmduty 0 30
    Setting channel 0 to 30
      0: 30%
    uart:~$ pwmduty 0 100
    Setting channel 0 to 100
      0: 100%
    uart:~$ pwmduty 0 -1
      0: disabled
    
    Change-Id: I73dca658b54fd6d4bd7668e85d60dff915faf671
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2776215
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

---
+++ zephyr/projects/trogdor/lazor/include/pwm_map.h
@@ -0,0 +1,16 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_PWM_MAP_H
+#define __ZEPHYR_PWM_MAP_H
+
+#include <devicetree.h>
+
+#include "pwm/pwm.h"
+
+#define PWM_CH_KBLIGHT		NAMED_PWM(kblight)
+#define PWM_CH_DISPLIGHT	NAMED_PWM(displight)
+
+#endif /* __ZEPHYR_PWM_MAP_H */
