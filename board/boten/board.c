b505164da7edb881c87dd5381eb09fb41b18a05a
reno.wang <reno.wang@lcfc.corp-partner.google.com>
2021-03-23 14:01:54 +0800

    boten: To correct base sensor's X-axis and Y-axis setting
    The base sensor's rotation matrix is wrong and cause CTS verifier get
    failure result. After correct it, CTS verifier can pass.
    
    BRANCH=boten
    BUG=b:182320815
    TEST=Verify CTS-Verifier's GyroscopeMeasurementTestActivity and
    AccelerometerMeasurementTestActivity pass
    
    Signed-off-by: reno.wang <reno.wang@lcfc.corp-partner.google.com>
    Change-Id: Ic5e8070fb3e9a43ef4bd088977f98060b2147d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2780462
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- board/boten/board.c
+++ board/boten/board.c
@@ -369,8 +369,8 @@ static const mat33_fp_t lid_standard_ref = {
 };
 
 static const mat33_fp_t base_standard_ref = {
-	{ 0, FLOAT_TO_FP(-1), 0},
 	{ FLOAT_TO_FP(1), 0, 0},
+	{ 0, FLOAT_TO_FP(1), 0},
 	{ 0, 0, FLOAT_TO_FP(1)}
 };
 
