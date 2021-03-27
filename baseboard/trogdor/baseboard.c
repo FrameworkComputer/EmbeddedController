94c95b81eacc42ffd40e0274aec4c3dae59887fa
Simon Glass <sjg@chromium.org>
2021-03-27 16:34:35 +1300

    trogdor: Move charger profile code to usbc_config
    Move this code into the shared file so that Zephyr can use it.
    
    BUG=b:183296099
    BRANCH=none
    TEST=make BOARD=lazor -j4
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I0bb2c3803c88e487f6e0db74368387126e2ba1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2788834
    Reviewed-by: Yuval Peress <peress@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- baseboard/trogdor/baseboard.c
+++ baseboard/trogdor/baseboard.c
@@ -5,13 +5,7 @@
 
 /* Trogdor baseboard-specific configuration */
 
-#include "charge_state.h"
 #include "i2c.h"
-#include "power.h"
-#include "usb_pd.h"
-
-#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
-#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
 /* Wake-up pins for hibernate */
 const enum gpio_signal hibernate_wake_pins[] = {
@@ -31,42 +25,4 @@ void board_hibernate_late(void)
 int board_allow_i2c_passthru(int port)
 {
 	return (port == I2C_PORT_VIRTUAL_BATTERY);
-}
-
-int charger_profile_override(struct charge_state_data *curr)
-{
-	int usb_mv;
-	int port;
-
-	if (curr->state != ST_CHARGE)
-		return 0;
-
-	/* Lower the max requested voltage to 5V when battery is full. */
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
-	    !(curr->batt.flags & BATT_FLAG_BAD_STATUS) &&
-	    !(curr->batt.flags & BATT_FLAG_WANT_CHARGE) &&
-	    (curr->batt.status & STATUS_FULLY_CHARGED))
-		usb_mv = 5000;
-	else
-		usb_mv = PD_MAX_VOLTAGE_MV;
-
-	if (pd_get_max_voltage() != usb_mv) {
-		CPRINTS("VBUS limited to %dmV", usb_mv);
-		for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++)
-			pd_set_external_voltage_limit(port, usb_mv);
-	}
-
-	return 0;
-}
-
-enum ec_status charger_profile_override_get_param(uint32_t param,
-						  uint32_t *value)
-{
-	return EC_RES_INVALID_PARAM;
-}
-
-enum ec_status charger_profile_override_set_param(uint32_t param,
-						  uint32_t value)
-{
-	return EC_RES_INVALID_PARAM;
 }
--- baseboard/trogdor/usbc_config.c
+++ baseboard/trogdor/usbc_config.c
@@ -7,6 +7,11 @@
 
 #include "charger.h"
 #include "charger/isl923x_public.h"
+#include "charge_state.h"
+#include "usb_pd.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
 const struct charger_config_t chg_chips[] = {
 	{
@@ -15,3 +20,41 @@ const struct charger_config_t chg_chips[] = {
 		.drv = &isl923x_drv,
 	},
 };
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	int usb_mv;
+	int port;
+
+	if (curr->state != ST_CHARGE)
+		return 0;
+
+	/* Lower the max requested voltage to 5V when battery is full. */
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
+	    !(curr->batt.flags & BATT_FLAG_BAD_STATUS) &&
+	    !(curr->batt.flags & BATT_FLAG_WANT_CHARGE) &&
+	    (curr->batt.status & STATUS_FULLY_CHARGED))
+		usb_mv = 5000;
+	else
+		usb_mv = PD_MAX_VOLTAGE_MV;
+
+	if (pd_get_max_voltage() != usb_mv) {
+		CPRINTS("VBUS limited to %dmV", usb_mv);
+		for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++)
+			pd_set_external_voltage_limit(port, usb_mv);
+	}
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
