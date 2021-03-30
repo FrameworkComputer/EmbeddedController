9f01d098243c450ebe64bd1cdfc5d281293b8683
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2021-03-30 16:24:38 +0800

    spherion: remove motion sensor function
    Not supports tablet mode, disable motion sensor function.
    
    BUG=b:183064682
    BRANCH=asurada
    TEST=check ec console don't initialize motion sensors.
    
    Change-Id: Ibeadb05849bb7e4bdd8bb780be805ef1d057e945
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2794201
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>

--- board/spherion/board.c
+++ board/spherion/board.c
@@ -30,7 +30,6 @@
 #include "i2c.h"
 #include "keyboard_scan.h"
 #include "lid_switch.h"
-#include "motion_sense.h"
 #include "power.h"
 #include "power_button.h"
 #include "pwm.h"
@@ -50,122 +49,6 @@
 
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
-
-/* Initialize board. */
-static void board_init(void)
-{
-	/* Enable motion sensor interrupt */
-	gpio_enable_interrupt(GPIO_BASE_IMU_INT_L);
-	gpio_enable_interrupt(GPIO_LID_ACCEL_INT_L);
-}
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
-
-/* Sensor */
-static struct mutex g_base_mutex;
-static struct mutex g_lid_mutex;
-
-static struct bmi_drv_data_t g_bmi160_data;
-static struct stprivate_data g_lis2dwl_data;
-
-/* Matrix to rotate accelerometer into standard reference frame */
-/* for Spherion */
-static const mat33_fp_t base_standard_ref = {
-	{0, FLOAT_TO_FP(1), 0},
-	{FLOAT_TO_FP(-1), 0, 0},
-	{0, 0, FLOAT_TO_FP(1)},
-};
-
-static void update_rotation_matrix(void)
-{
-	if (board_get_version() >= 2) {
-		motion_sensors[BASE_ACCEL].rot_standard_ref =
-			&base_standard_ref;
-		motion_sensors[BASE_GYRO].rot_standard_ref =
-			&base_standard_ref;
-	}
-}
-DECLARE_HOOK(HOOK_INIT, update_rotation_matrix, HOOK_PRIO_INIT_ADC + 2);
-
-struct motion_sensor_t motion_sensors[] = {
-	/*
-	 * Note: bmi160: supports accelerometer and gyro sensor
-	 * Requirement: accelerometer sensor must init before gyro sensor
-	 * DO NOT change the order of the following table.
-	 */
-	[BASE_ACCEL] = {
-		.name = "Base Accel",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_BMI160,
-		.type = MOTIONSENSE_TYPE_ACCEL,
-		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &bmi160_drv,
-		.mutex = &g_base_mutex,
-		.drv_data = &g_bmi160_data,
-		.port = I2C_PORT_ACCEL,
-		.i2c_spi_addr_flags = BMI160_ADDR0_FLAGS,
-		.rot_standard_ref = NULL, /* identity matrix */
-		.default_range = 4,  /* g, to meet CDD 7.3.1/C-1-4 reqs */
-		.min_frequency = BMI_ACCEL_MIN_FREQ,
-		.max_frequency = BMI_ACCEL_MAX_FREQ,
-		.config = {
-			/* Sensor on for angle detection */
-			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 10000 | ROUND_UP_FLAG,
-				.ec_rate = 100 * MSEC,
-			},
-			/* Sensor on for angle detection */
-			[SENSOR_CONFIG_EC_S3] = {
-				.odr = 10000 | ROUND_UP_FLAG,
-				.ec_rate = 100 * MSEC,
-			},
-		},
-	},
-	[BASE_GYRO] = {
-		.name = "Base Gyro",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_BMI160,
-		.type = MOTIONSENSE_TYPE_GYRO,
-		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &bmi160_drv,
-		.mutex = &g_base_mutex,
-		.drv_data = &g_bmi160_data,
-		.port = I2C_PORT_ACCEL,
-		.i2c_spi_addr_flags = BMI160_ADDR0_FLAGS,
-		.default_range = 1000, /* dps */
-		.rot_standard_ref = NULL, /* identity matrix */
-		.min_frequency = BMI_GYRO_MIN_FREQ,
-		.max_frequency = BMI_GYRO_MAX_FREQ,
-	},
-	[LID_ACCEL] = {
-		.name = "Lid Accel",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LIS2DWL,
-		.type = MOTIONSENSE_TYPE_ACCEL,
-		.location = MOTIONSENSE_LOC_LID,
-		.drv = &lis2dw12_drv,
-		.mutex = &g_lid_mutex,
-		.drv_data = &g_lis2dwl_data,
-		.int_signal = GPIO_LID_ACCEL_INT_L,
-		.port = I2C_PORT_ACCEL,
-		.i2c_spi_addr_flags = LIS2DWL_ADDR1_FLAGS,
-		.flags = MOTIONSENSE_FLAG_INT_SIGNAL,
-		.rot_standard_ref = NULL, /* identity matrix */
-		.default_range = 2, /* g */
-		.min_frequency = LIS2DW12_ODR_MIN_VAL,
-		.max_frequency = LIS2DW12_ODR_MAX_VAL,
-		.config = {
-			/* EC use accel for angle detection */
-			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 12500 | ROUND_UP_FLAG,
-			},
-			/* Sensor on for lid angle detection */
-			[SENSOR_CONFIG_EC_S3] = {
-				.odr = 10000 | ROUND_UP_FLAG,
-			},
-		},
-	},
-};
-const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 /* PWM */
 
