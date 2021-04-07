d1924be854913acf1b42fc9fff9c547c947b72b7
Abe Levkoy <alevkoy@chromium.org>
2021-04-07 16:34:34 -0600

    TCPMv2: Set SOP before requesting chunk
    When requesting the next chunk in the RCH state machine, set the SOP
    type for the request to the type of the just-received chunk response. Do
    not just use the SOP type of the last transmitted message.
    
    BUG=b:179443762
    TEST=Pass TD.PD.SRC3.E32 with SOP' traffic immediately before chunk 0
    BRANCH=firmware-volteer-13672.B-main
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: Ifb520bb7ee439ea895b11938395a943d5ca32edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2812138
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Scott Collyer <scollyer@chromium.org>

--- common/usbc/usb_prl_sm.c
+++ common/usbc/usb_prl_sm.c
@@ -1633,6 +1633,7 @@ static void rch_requesting_chunk_entry(const int port)
 
 	pdmsg[port].data_objs = 1;
 	pdmsg[port].ext = 1;
+	pdmsg[port].xmit_type = prl_rx[port].sop;
 	PRL_TX_SET_FLAG(port, PRL_FLAGS_MSG_XMIT);
 	task_set_event(PD_PORT_TO_TASK_ID(port), PD_EVENT_TX);
 }
