2488b0595fca9f17cd31d61821512017d3536a0b
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2021-04-07 15:56:55 +0800

    zephyr: add clock to support it8xxx2 cros_kb_row driver
    Add clock to support it8xxx2 cros_kb_row driver.
    
    BUG=b:187192587
    BRANCH=none
    TEST=on hayato, read pll_reg_to_freq is same as setting.
    
    Cq-Depend: chromium:2909732
    Change-Id: I9fd9f6ca5c6796ad9aee22a5cf7dc23564d2a814
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2813880
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ zephyr/shim/chip/it8xxx2/clock.c
@@ -0,0 +1,41 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <device.h>
+#include <drivers/clock_control.h>
+#include <kernel.h>
+#include <logging/log.h>
+#include <soc.h>
+#include <zephyr.h>
+#include <soc/ite_it8xxx2/reg_def_cros.h>
+#include <sys/util.h>
+
+#include "module_id.h"
+
+LOG_MODULE_REGISTER(shim_clock, LOG_LEVEL_ERR);
+
+#define ECPM_NODE		DT_INST(0, ite_it8xxx2_ecpm)
+#define HAL_ECPM_REG_BASE_ADDR	\
+			((struct ecpm_reg *)DT_REG_ADDR_BY_IDX(ECPM_NODE, 0))
+#define PLLFREQ_MASK		0xf
+
+static const int pll_reg_to_freq[8] = {
+	MHZ(8),
+	MHZ(16),
+	MHZ(24),
+	MHZ(32),
+	MHZ(48),
+	MHZ(64),
+	MHZ(72),
+	MHZ(96)
+};
+
+int clock_get_freq(void)
+{
+	struct ecpm_reg *const ecpm_base = HAL_ECPM_REG_BASE_ADDR;
+	int reg_val = ecpm_base->ECPM_PLLFREQ & PLLFREQ_MASK;
+
+	return pll_reg_to_freq[reg_val];
+}
