ce5a951499fe9591054c8fa07df5f5fcf61dec59
Rob Barnes <robbarnes@google.com>
2021-04-06 19:54:07 -0600

    guybrush: Add header for ANX7491 retimer
    Adding a header to hold register values for ANX7491. ANX7491 should work
    without configuration. More registers can be added as needed for
    tuning.
    
    BUG=b:184592168
    TEST=Build
    BRANCH=None
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: If529bfdedff6b151147c8ef02840621b093be672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2809881
    Reviewed-by: Diana Z <dzigterman@chromium.org>

---
+++ driver/retimer/anx7491.h
@@ -0,0 +1,17 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * ANX7491:10G USB 3.2 Re-timer (1-Port)
+ */
+
+#ifndef __CROS_EC_USB_RETIMER_ANX7491_H
+#define __CROS_EC_USB_RETIMER_ANX7491_H
+
+/* I2C interface addresses */
+#define ANX7491_I2C_ADDR0_FLAGS		0x10
+#define ANX7491_I2C_ADDR1_FLAGS		0x14
+#define ANX7491_I2C_ADDR2_FLAGS		0x16
+#define ANX7491_I2C_ADDR3_FLAGS		0x11
+
+#endif /* __CROS_EC_USB_RETIMER_ANX7491_H */
