337b81f689f2f9a1045725d1a91a19be81b048bc
Tzung-Bi Shih <tzungbi@chromium.org>
2021-04-08 17:30:36 +0800

    cherry_scp: add initial cherry SCP board
    This is basically: cp -r board/asurada_scp board/cherry_scp.
    
    BRANCH=none
    BUG=b:184793035
    TEST=make BOARD=cherry_scp
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: I7a035962a5a371587b5bea6f1cd78cd777dcdc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2813399
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@chromium.org>

---
+++ board/cherry_scp/build.mk
@@ -0,0 +1,13 @@
+# -*- makefile -*-
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=mt8192_scp
+CHIP_VARIANT:=mt8192
+BASEBOARD:=mtscp-rv32i
+
+board-y=board.o
