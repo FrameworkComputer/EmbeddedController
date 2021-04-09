aa1ab37b8f18a9438e420531c295141728ce0ae2
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2021-04-09 10:52:54 +0800

    Cherry: Add battery information
    New battery: AP16L5J
    
    BUG=none
    BRANCH=none
    TEST=Check found battery info in console and cutoff work.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: Ife754ad23063b3471fff097a5c34f7cbc57c2a5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2812600
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- board/cherry/battery.c
+++ board/cherry/battery.c
@@ -14,9 +14,66 @@
 #include "usb_pd.h"
 
 const struct board_batt_params board_battery_info[] = {
+	/* Panasonic AP16L5J Battery Information */
+	[BATTERY_PANASONIC_AC16L5J] = {
+		.fuel_gauge = {
+			.manuf_name = "PANASONIC",
+			.device_name = "AP16L5J",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.reg_addr = 0x0,
+				.reg_mask = 0x4000,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 8800,
+			.voltage_normal		= 7700,
+			.voltage_min		= 6000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
+		},
+	},
+	[BATTERY_PANASONIC_AC16L5J_KT00205009] = {
+		.fuel_gauge = {
+			.manuf_name = "PANASONIC KT00205009",
+			.device_name = "AP16L5J",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.reg_addr = 0x0,
+				.reg_mask = 0x4000,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 8800,
+			.voltage_normal		= 7700,
+			.voltage_min		= 6000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
+		},
+	},
+	/* LGC AP18C8K Battery Information */
 	[BATTERY_LGC_AP18C8K] = {
 		.fuel_gauge = {
 			.manuf_name = "LGC KT0030G020",
+			.device_name = "AP18C8K",
 			.ship_mode = {
 				.reg_addr = 0x3A,
 				.reg_data = { 0xC574, 0xC574 },
@@ -25,24 +82,22 @@ const struct board_batt_params board_battery_info[] = {
 				.reg_addr = 0x43,
 				.reg_mask = 0x0001,
 				.disconnect_val = 0x0,
-				.cfet_mask = 0x0002,
-				.cfet_off_val = 0x0000,
 			},
 		},
 		.batt_info = {
-			.voltage_max            = 13050,
-			.voltage_normal         = 11250,
-			.voltage_min            = 9000,
-			.precharge_current      = 256,
-			.start_charging_min_c   = 0,
-			.start_charging_max_c   = 50,
-			.charging_min_c         = 0,
-			.charging_max_c         = 60,
-			.discharging_min_c      = -20,
-			.discharging_max_c      = 75,
+			.voltage_max		= 13050,
+			.voltage_normal		= 11250,
+			.voltage_min		= 9000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
 		},
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_LGC_AP18C8K;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_PANASONIC_AC16L5J;
