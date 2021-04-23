73886cddb0eb255072848e2d071707976729be03
Aseda Aboagye <aaboagye@google.com>
2021-04-23 15:07:25 -0700

    raa489000: Disable DVC in hibernate
    Leaving DVC enabled on the primary charger in hibernate will increase
    the power consumption of the system.  When we enter hibernate, DVC
    isn't needed so we can safely disable it.
    
    BUG=b:184219851
    BRANCH=dedede
    TEST=Build and flash madoo, verify that DUT is able to hibernate and
    can charge from sub board when woken up.
    
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Change-Id: I027cceac0cb7eff9ac08293449a06712e9d1daaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2848292
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>
    Auto-Submit: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Diana Z <dzigterman@chromium.org>

--- driver/charger/isl923x.c
+++ driver/charger/isl923x.c
@@ -887,6 +887,14 @@ void raa489000_hibernate(int chgnum, bool disable_adc)
 			CPRINTS("%s(%d):Failed to set Control8!", __func__,
 				chgnum);
 	}
+
+	/* Disable DVC on the main charger to reduce power consumption. */
+	if (chgnum == CHARGER_PRIMARY) {
+		rv = raw_write16(chgnum, RAA489000_REG_CONTROL10, 0);
+		if (rv)
+			CPRINTS("%s(%d):Failed to set Control10!", __func__,
+				chgnum);
+	}
 #endif
 
 	cflush();
