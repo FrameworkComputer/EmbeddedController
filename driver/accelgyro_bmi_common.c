a9545e92e0b588c6e21915248da6557b0eb071f2
Yuval Peress <peress@chromium.org>
2021-04-23 10:22:24 -0600

    driver: bmi*: Add check for BMI160/BMI260 config
    The common logic doesn't work if neither the BMI160/BMI260 configs are
    defined. Add a check for this so the build would fail.
    
    BRANCH=none
    BUG=b:185966444
    TEST=make buildall
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I3288c5481eba25c8c1858122c83018073e0eaab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2848429
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- driver/accelgyro_bmi_common.c
+++ driver/accelgyro_bmi_common.c
@@ -23,6 +23,10 @@
 #define CPRINTF(format, args...) cprintf(CC_ACCEL, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_ACCEL, format, ## args)
 
+#if !defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260)
+#error "Must use either BMI160 or BMI260"
+#endif
+
 #if defined(CONFIG_ACCELGYRO_BMI260) && !defined(CONFIG_ACCELGYRO_BMI160)
 #define V(s_) 1
 #elif defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260)
