8a7a059761a0cc4a09b1a0208a6d6ca8319b6a5a
elmo_lan <hplan1112@gmail.com>
2021-05-13 18:39:07 +0800

    modify power control when receive me cmd.
    Signed-off-by: elmo_lan <hplan1112@gmail.com>

--- board/hx20/board.h
+++ board/hx20/board.h
@@ -766,6 +766,8 @@ int pos_get_state(void);
 
 void me_gpio_change(uint32_t flags);
 
+void update_me_change(int change);
+
 #ifdef CONFIG_LOW_POWER_IDLE
 void board_prepare_for_deep_sleep(void);
 void board_resume_from_deep_sleep(void);
