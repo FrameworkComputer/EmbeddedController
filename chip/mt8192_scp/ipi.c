f78ab576aef15e293558de20340263f2acd0bf7e
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2021-05-14 16:28:09 +0800

    chip/mt8192_scp: using mm21 instead of mt21c
    BRANCH=asurada
    BUG=b:187782435
    TEST=make BOARD=asurada_scp
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Change-Id: I71d7dd7fa0764f7f8810553021b6a7d1b62d83f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2896761
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- chip/mt8192_scp/ipi.c
+++ chip/mt8192_scp/ipi.c
@@ -128,7 +128,7 @@ static void ipi_enable_deferred(void)
 	scp_run.signaled = 1;
 	strncpy(scp_run.fw_ver, system_get_version(EC_IMAGE_RW),
 		SCP_FW_VERSION_LEN);
-	scp_run.dec_capability = VDEC_CAP_MT21C | VDEC_CAP_H264_SLICE |
+	scp_run.dec_capability = VDEC_CAP_MM21 | VDEC_CAP_H264_SLICE |
 				 VDEC_CAP_VP8_FRAME | VDEC_CAP_VP9_FRAME;
 	scp_run.enc_capability = VENC_CAP_4K;
 
