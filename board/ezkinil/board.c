184955dc875f653ad94ef39a9b7bb89d4bd5c463
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2021-06-01 17:14:57 +0800

    Ezkinil: support factory keyboard test.
    connector-to-GPIO map:
      {-1, -1}, {0, 5}, {1, 1}, {1, 0}, {0, 6},
      {0, 7}, {-1, -1}, {-1, -1}, {1, 4}, {1, 3},
      {-1, -1}, {1, 6}, {1, 7}, {3, 1}, {2, 0},
      {1, 5}, {2, 6}, {2, 7}, {2, 1}, {2, 4},
      {2, 5}, {1, 2}, {2, 3},{2, 2}, {3, 0},
      {-1, -1}, {0, 4}, {-1, -1}, {8, 2}, {-1, -1},
      {-1, -1},
    
    BUG=b:191931746
    BRANCH=zork
    TEST=`ectool kbfactorytest` PASS.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I0354835b8b606fe735b1cef433e6e07bba658249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2929062
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -43,6 +43,24 @@
 
 static int board_ver;
 
+/*
+ * We have total 30 pins for keyboard connecter {-1, -1} mean
+ * the N/A pin that don't consider it and reserve index 0 area
+ * that we don't have pin 0.
+ */
+const int keyboard_factory_scan_pins[][2] = {
+	{-1, -1}, {0, 5}, {1, 1}, {1, 0}, {0, 6},
+	{0, 7}, {-1, -1}, {-1, -1}, {1, 4}, {1, 3},
+	{-1, -1}, {1, 6}, {1, 7}, {3, 1}, {2, 0},
+	{1, 5}, {2, 6}, {2, 7}, {2, 1}, {2, 4},
+	{2, 5}, {1, 2}, {2, 3}, {2, 2}, {3, 0},
+	{-1, -1}, {0, 4}, {-1, -1}, {8, 2}, {-1, -1},
+	{-1, -1},
+};
+
+const int keyboard_factory_scan_pins_used =
+		ARRAY_SIZE(keyboard_factory_scan_pins);
+
 /* Motion sensors */
 static struct mutex g_lid_mutex;
 static struct mutex g_base_mutex;
--- board/ezkinil/board.h
+++ board/ezkinil/board.h
@@ -13,6 +13,8 @@
 
 #define CONFIG_FAN_RPM_CUSTOM
 
+#define CONFIG_KEYBOARD_FACTORY_TEST
+
 /* Motion sensing drivers */
 #define CONFIG_ACCELGYRO_ICM426XX
 #define CONFIG_ACCELGYRO_ICM426XX_INT_EVENT \
@@ -201,6 +203,8 @@ static inline bool ec_config_has_hdmi_conn_hpd(void)
 		? GPIO_DP1_HPD \
 		: GPIO_DP2_HPD)
 
+extern const int keyboard_factory_scan_pins[][2];
+extern const int keyboard_factory_scan_pins_used;
 extern const struct usb_mux usbc1_tusb544;
 extern const struct usb_mux usbc1_ps8743;
 extern struct usb_mux usbc1_amd_fp5_usb_mux;
