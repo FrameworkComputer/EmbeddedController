41165549be19ddc085bffc7b7abb09bc20b7eac5
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2021-06-02 15:40:01 +0800

    zephyr: enable related configs for flash driver on it8xxx2
    These flash related configs have to be enabled for the flash driver
    of it8xxx2.
    
    This CL also distinguishes the flash configs into header file of
    it8xxx2 and npcx.
    
    BUG=b:187192628
    BRANCH=none
    TEST=zmake -lDEBUG configure -b -B zephyr/build_ite \
    zephyr/projects/it8xxx2_evb
         zmake -lDEBUG configure -b -B zephyr/build_volteer \
    zephyr/projects/volteer/volteer
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>
    Change-Id: Ic6a2e89a24676d6ac484a389c938ab0692971be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2952280
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ zephyr/shim/chip/it8xxx2/include/flash_chip.h
@@ -0,0 +1,38 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_FLASH_CHIP_H
+#define __CROS_EC_FLASH_CHIP_H
+
+/* Flash size */
+#define CONFIG_FLASH_SIZE_BYTES         DT_REG_SIZE(DT_NODELABEL(flash0))
+/* Program is run directly from storage */
+#define CONFIG_MAPPED_STORAGE_BASE      DT_REG_ADDR(DT_NODELABEL(flash0))
+/*
+ * One page program instruction allows maximum 256 bytes (a page) of data
+ * to be programmed.
+ */
+#define CONFIG_FLASH_WRITE_IDEAL_SIZE   256
+/* Minimum write size */
+#define CONFIG_FLASH_WRITE_SIZE         DT_PROP(DT_INST(0, soc_nv_flash), \
+						write_block_size)
+/* Erase bank size */
+#define CONFIG_FLASH_ERASE_SIZE         DT_PROP(DT_INST(0, soc_nv_flash), \
+						erase_block_size)
+/* Protect bank size */
+#define CONFIG_FLASH_BANK_SIZE          CONFIG_FLASH_ERASE_SIZE
+
+#define CONFIG_RO_STORAGE_OFF           0x0
+#define CONFIG_RW_STORAGE_OFF           0x0
+
+/*
+ * The EC uses the one bank of flash to emulate a SPI-like write protect
+ * register with persistent state.
+ */
+#define CONFIG_FW_PSTATE_SIZE           CONFIG_FLASH_BANK_SIZE
+#define CONFIG_FW_PSTATE_OFF            (CONFIG_FLASH_SIZE_BYTES / 2 - \
+						CONFIG_FW_PSTATE_SIZE)
+
+#endif /* __CROS_EC_FLASH_CHIP_H */
