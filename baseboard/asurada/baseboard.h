3842d28f91fa6ea45e989ad652ff6f70e9a743c4
Simon Glass <sjg@chromium.org>
2021-06-04 15:16:45 -0600

    zephyr: asurada: Create a common baseboard header
    Move some definitions to a common file so we can use them from both
    ECOS and Zephyr.
    
    BUG=b:189855648
    BRANCH=none
    TEST=with other CLs, build asurada for Zephyr
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I43d3d09e47d185d8980724e8f1ea100f15417455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2941815
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Yuval Peress <peress@chromium.org>
    Tested-by: Denis Brockus <dbrockus@chromium.org>
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>

--- baseboard/asurada/baseboard.h
+++ baseboard/asurada/baseboard.h
@@ -177,13 +177,7 @@
 		(BIT(EC_MKBP_EVENT_KEY_MATRIX) | \
 		 BIT(EC_MKBP_EVENT_HOST_EVENT))
 
-/* GPIO name remapping */
-#define GPIO_EN_HDMI_PWR        GPIO_EC_X_GPIO1
-#define GPIO_USB_C1_FRS_EN      GPIO_EC_X_GPIO1
-#define GPIO_USB_C1_PPC_INT_ODL GPIO_X_EC_GPIO2
-#define GPIO_PS185_EC_DP_HPD    GPIO_X_EC_GPIO2
-#define GPIO_USB_C1_DP_IN_HPD   GPIO_EC_X_GPIO3
-#define GPIO_PS185_PWRDN_ODL    GPIO_EC_X_GPIO3
+#include "baseboard_common.h"
 
 #ifndef __ASSEMBLER__
 
@@ -197,14 +191,6 @@ enum power_signal {
 	POWER_SIGNAL_COUNT,
 };
 
-enum board_sub_board {
-	SUB_BOARD_NONE = -1,
-	SUB_BOARD_TYPEC,
-	SUB_BOARD_HDMI,
-	SUB_BOARD_COUNT,
-};
-
-int board_get_version(void);
 void board_reset_pd_mcu(void);
 enum board_sub_board board_get_sub_board(void);
 void usb_a0_interrupt(enum gpio_signal signal);
