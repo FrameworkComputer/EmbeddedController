69b9b9d7e74e6aed462a3df154c8a0e6fd0ae128
Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
2021-06-08 09:18:19 +0800

    gimble: Initial EC image
    Create the initial EC image for the gimble variant by copying
    the brya reference board EC files into a new directory named for
    the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.5.0).
    
    BUG=b:190334274
    BRANCH=None
    TEST=make BOARD=gimble
    
    Signed-off-by: Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
    Change-Id: Ia740667582e0f53bfc6afb37d23edf8c2d1d543e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2944517
    Reviewed-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

---
+++ board/gimble/usbc_config.h
@@ -0,0 +1,22 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Brya board-specific USB-C configuration */
+
+#ifndef __CROS_EC_USBC_CONFIG_H
+#define __CROS_EC_USBC_CONFIG_H
+
+#define CONFIG_USB_PD_PORT_MAX_COUNT	3
+
+enum usbc_port {
+	USBC_PORT_C0 = 0,
+	USBC_PORT_C1,
+	USBC_PORT_C2,
+	USBC_PORT_COUNT
+};
+
+void config_usb_db_type(void);
+
+#endif /* __CROS_EC_USBC_CONFIG_H */
