748c59c5916fab15dca99121371e911a68460594
Diana Z <dzigterman@chromium.org>
2021-06-10 14:53:21 -0600

    Guybrush: Protect against simultanteous sinking
    When the NCT3807 boots in dead battery mode, it may neglect to drive its
    EN_SNK line until reset.  This commit prevents us from changing charge
    ports when we're in dead battery boot and there's insufficient power to
    withstand Vbus loss.  When there is enough power, we'll reset the dead
    battery TCPC before enabling sinking on the new charge port.  This will
    cause an interruption to the PD connection on the original port, which
    would manifest in loss of USB and DP connections.
    
    BRANCH=None
    BUG=b:183660105
    TEST=on guybrush, confirm we can switch ports with sufficient battery
    charge and do not pass through voltage from one port to the other
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: If4512c9a5dee162af41d03fef9979d760f4b1a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2953877
    Reviewed-by: Rob Barnes <robbarnes@google.com>

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -50,6 +50,8 @@
 #define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
+static void reset_nct38xx_port(int port);
+
 /* Wake Sources */
 const enum gpio_signal hibernate_wake_pins[] = {
 	GPIO_LID_OPEN,
@@ -571,6 +573,7 @@ int board_set_active_charge_port(int port)
 	int is_valid_port = (port >= 0 &&
 			     port < CONFIG_USB_PD_PORT_MAX_COUNT);
 	int i;
+	int cur_port = charge_manager_get_active_charge_port();
 
 	if (port == CHARGE_PORT_NONE) {
 		CPRINTSUSB("Disabling all charger ports");
@@ -593,8 +596,31 @@ int board_set_active_charge_port(int port)
 
 	/* Check if the port is sourcing VBUS. */
 	if (ppc_is_sourcing_vbus(port)) {
-		CPRINTFUSB("Skip enable C%d", port);
+		CPRINTSUSB("Skip enable C%d", port);
 		return EC_ERROR_INVAL;
+	}
+
+	/*
+	 * Disallow changing ports if we booted in dead battery mode and don't
+	 * have sufficient power to withstand Vbus loss.  The NCT3807 may
+	 * continue to keep EN_SNK low on the original port and allow a
+	 * dangerous level of voltage to pass through to the initial charge
+	 * port (see b/183660105)
+	 *
+	 * If we do have sufficient power, then reset the dead battery port and
+	 * set up Type-C error recovery on its connection.
+	 */
+	if (cur_port != CHARGE_PORT_NONE &&
+			port != cur_port &&
+			nct38xx_get_boot_type(cur_port) ==
+						NCT38XX_BOOT_DEAD_BATTERY) {
+		if (pd_is_battery_capable()) {
+			reset_nct38xx_port(cur_port);
+			pd_set_error_recovery(cur_port);
+		} else {
+			CPRINTSUSB("Battery too low for charge port change");
+			return EC_ERROR_INVAL;
+		}
 	}
 
 	CPRINTSUSB("New charge port: C%d", port);
