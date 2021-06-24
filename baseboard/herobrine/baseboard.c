bf3c4f24fd2e4d84fc35ee643fcaac489ef391d2
Wai-Hong Tam <waihong@google.com>
2021-06-23 17:28:37 -0700

    herobrine: Initial board commit for herobrine_npcx7
    This CL first copies:
     * baseboard/trogdor -> baseboard/herobrine
     * board/trogdor -> board/herobrine_npcx7
    
    And update the following:
     * Rename "Trogdor" to "Herobrine"
     * Update the copyright year
     * Use the Herobrine USB PID
    
    BRANCH=None
    BUG=b:191803008
    TEST=Built the herobrine_npcx7 image successfully.
    
    Change-Id: I604671b686fc0212bcb49865f59e23d46f6275cf
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2984400
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>

---
+++ baseboard/herobrine/baseboard.c
@@ -0,0 +1,22 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Herobrine baseboard-specific configuration */
+
+#include "i2c.h"
+
+/* Wake-up pins for hibernate */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_LID_OPEN,
+	GPIO_AC_PRESENT,
+	GPIO_POWER_BUTTON_L,
+	GPIO_EC_RST_ODL,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+int board_allow_i2c_passthru(int port)
+{
+	return (port == I2C_PORT_VIRTUAL_BATTERY);
+}
---
+++ baseboard/herobrine/build.mk
@@ -0,0 +1,12 @@
+# -*- makefile -*-
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Baseboard specific files build
+#
+
+baseboard-y+=baseboard.o
+baseboard-y+=hibernate.o
+baseboard-y+=usbc_config.o
+baseboard-y+=usb_pd_policy.o
