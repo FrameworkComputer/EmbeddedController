d729b26f22776e8bce5a5b485be9bc252e832e8d
Diana Z <dzigterman@chromium.org>
2021-06-25 11:27:58 -0600

    Guybrush: Set up custom Vbus source detection
    In the USB common code, PPCs will always be given priority over TCPCs
    for detecting whether sourcing is enabled.  However, with FRS enabled
    the PPC cached flags no longer accurately reflect whether we're
    sourcing.  Therefore, set up a custom board function which may query the
    most appropriate chip, though for now set it up for the same component
    used by the Vbus discharge code.
    
    BRANCH=None
    BUG=b:183586640
    TEST=make -j buildall
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I4acf65930e944990e7770252de4ff47c671382a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2987596
    Reviewed-by: Rob Barnes <robbarnes@google.com>

--- baseboard/guybrush/usb_pd_policy.c
+++ baseboard/guybrush/usb_pd_policy.c
@@ -71,7 +71,14 @@ int pd_set_power_supply_ready(int port)
 	return EC_SUCCESS;
 }
 
+/* Used by Vbus discharge common code with CONFIG_USB_PD_DISCHARGE */
 int board_vbus_source_enabled(int port)
 {
 	return ppc_is_sourcing_vbus(port);
+}
+
+/* Used by USB charger task with CONFIG_USB_PD_5V_EN_CUSTOM */
+int board_is_sourcing_vbus(int port)
+{
+	return board_vbus_source_enabled(port);
 }
