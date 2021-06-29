FrameworkComputer/EmbeddedController

commit 21b8075b6a800aaefbf606e04ec93cccfcf6d1e4

fix undesired power on on AC attach
Some docks are slow to finish contract negociation
and our system would power on with AC attach when
this feature was not enabled.

Switch from assuming AC_OK is up by the time we poll it on boot
to determine if the system should power on.
Instead use latched VCI register to identify power button
was the reason to boot the sytem

Signed-off-by: Kieran Levin <ktl@frame.work>

board/hx20/board.c
@@ -343,8 +343,23 @@ void board_reset_pd_mcu(void)
 
 }
 
+static int power_button_pressed_on_boot;
+int poweron_reason_powerbtn(void)
+{
+	int r = power_button_pressed_on_boot;
+
+	power_button_pressed_on_boot = 0;
+	return r;
+}
+
 static void vci_init(void)
 {
+	if (MCHP_VCI_NEGEDGE_DETECT & (BIT(0) | BIT(1))) {
+		MCHP_VCI_NEGEDGE_DETECT = BIT(0) |  BIT(1);
+		MCHP_VCI_POSEDGE_DETECT = BIT(0) |  BIT(1);
+		power_button_pressed_on_boot = 1;
+	}
+
 	/**
 	 * Switch VCI control from VCI_OUT to GPIO Pin Control
 	 * These have to be done in sequence to prevent glitching
@@ -357,7 +372,7 @@ static void vci_init(void)
 	 * enable BIT 2 for chassis open
 	 */
 	MCHP_VCI_INPUT_ENABLE = BIT(0) |  BIT(1);
-	MCHP_VCI_BUFFER_EN = BIT(2);
+	MCHP_VCI_BUFFER_EN = BIT(0) | BIT(1) | BIT(2);
 }
 DECLARE_HOOK(HOOK_INIT, vci_init, HOOK_PRIO_FIRST);
 
@@ -377,6 +392,9 @@ static void board_power_off_deferred(void)
 		task_disable_irq(i);
 		task_clear_pending_irq(i);
 	}
+
+	MCHP_VCI_NEGEDGE_DETECT = BIT(0) |  BIT(1);
+	MCHP_VCI_POSEDGE_DETECT = BIT(0) |  BIT(1);
 
 	gpio_set_level(GPIO_VS_ON, 0);
 	MCHP_VCI_REGISTER &= ~(MCHP_VCI_REGISTER_FW_CNTRL + MCHP_VCI_REGISTER_FW_EXT);

board/hx20/power_button_x86.c
@@ -374,9 +374,8 @@ static void state_machine(uint64_t tnow)
 			tnext_state = tnow + PWRBTN_DELAY_INITIAL;
 			initial_delay--;
 		} else {
-			if (!extpower_is_present() ||
-				(system_get_reset_flags() & EC_RESET_FLAG_HARD) ==
-				EC_RESET_FLAG_HARD) {
+			if (poweron_reason_powerbtn()) {
+
 				reset_diagnostics();
 
 				if (chipset_in_state(CHIPSET_STATE_HARD_OFF))

board/hx20/board.h
@@ -770,6 +770,8 @@ int ac_boot_status(void);
 
 void update_me_change(int change);
 
+int poweron_reason_powerbtn(void);
+
 #ifdef CONFIG_LOW_POWER_IDLE
 void board_prepare_for_deep_sleep(void);
 void board_resume_from_deep_sleep(void);

commit 795969f889c668e0004451dcf7fc6ab8b0ab3c52

Fix regression on power from G3 cold
Regression in
https://github.com/FrameworkComputer/ec/commit/21b8075b6a800aaefbf606e04ec93cccfcf6d1e4
caused you to need to double tap the power button when powering on from
G3 cold when the EC_ON rail is off while running on battery.

This also improves power on button press detection so you do not need to
hold the power button as long to power up the system from G3 on battery.

And keeps the improved behavior of not incorrectly powering on when
attaching to some docks

Signed-off-by: Kieran Levin <ktl@frame.work>

board/hx20/board.c
@@ -346,10 +346,7 @@ void board_reset_pd_mcu(void)
 static int power_button_pressed_on_boot;
 int poweron_reason_powerbtn(void)
 {
-	int r = power_button_pressed_on_boot;
-
-	power_button_pressed_on_boot = 0;
-	return r;
+	return power_button_pressed_on_boot;
 }
 
 static void vci_init(void)

board/hx20/power_button_x86.c
@@ -365,8 +365,9 @@ static void state_machine(uint64_t tnow)
 			}
 		}
 		*/
+
 		/* if trigger by power button don't need wait initial.*/
-		if (power_button_is_pressed()) {
+		if (power_button_is_pressed() || poweron_reason_powerbtn()) {
 			initial_delay = 0;
 		}
 