6707d5228e450607f18eb64ca77171a2bf29c00d
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2021-06-29 11:05:20 +0800

    mrbland: Initial EC image
    Create the initial EC image for the mrbland variant by copying the
    homestar reference board EC files into a new directory named for
    the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.5.0).
    
    BUG=b:192304588
    BRANCH=trogdor
    TEST=make BOARD=mrbland
    
    Signed-off-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Change-Id: Ibf5c02c3483e8768acfbfd889b038ef65071cb61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2993958
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

---
+++ board/mrbland/battery.c
@@ -0,0 +1,213 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Battery pack vendor provided charging profile
+ */
+
+#include "battery_fuel_gauge.h"
+#include "common.h"
+#include "util.h"
+
+/*
+ * Battery info for all homestar battery types. Note that the fields
+ * start_charging_min/max and charging_min/max are not used for the charger.
+ * The effective temperature limits are given by discharging_min/max_c.
+ *
+ * Fuel Gauge (FG) parameters which are used for determining if the battery
+ * is connected, the appropriate ship mode (battery cutoff) command, and the
+ * charge/discharge FETs status.
+ *
+ * Ship mode (battery cutoff) requires 2 writes to the appropriate smart battery
+ * register. For some batteries, the charge/discharge FET bits are set when
+ * charging/discharging is active, in other types, these bits set mean that
+ * charging/discharging is disabled. Therefore, in addition to the mask for
+ * these bits, a disconnect value must be specified. Note that for TI fuel
+ * gauge, the charge/discharge FET status is found in Operation Status (0x54),
+ * but a read of Manufacturer Access (0x00) will return the lower 16 bits of
+ * Operation status which contains the FET status bits.
+ *
+ * The assumption for battery types supported is that the charge/discharge FET
+ * status can be read with a sb_read() command and therefore, only the register
+ * address, mask, and disconnect value need to be provided.
+ */
+
+const struct board_batt_params board_battery_info[] = {
+	/* COSMX GH02047XL 333-1C-DA-A */
+	[BATTERY_GH02047XL_1C] = {
+		.fuel_gauge = {
+			.manuf_name = "333-1C-DA-A",
+			.device_name = "GH02047XL",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8800, /* mV */
+			.voltage_normal         = 7700, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 256,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 45,
+			.charging_min_c         = 0,
+			.charging_max_c         = 45,
+			.discharging_min_c      = -10,
+			.discharging_max_c      = 60,
+		},
+	},
+	/* COSMX GH02047XL */
+	[BATTERY_GH02047XL] = {
+		.fuel_gauge = {
+			.manuf_name = "333-AC-DA-A",
+			.device_name = "GH02047XL",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8800, /* mV */
+			.voltage_normal         = 7700, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 256,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 45,
+			.charging_min_c         = 0,
+			.charging_max_c         = 45,
+			.discharging_min_c      = -10,
+			.discharging_max_c      = 60,
+		},
+	},
+	/* COSMX DS02032XL */
+	[BATTERY_DS02032XL] = {
+		.fuel_gauge = {
+			.manuf_name = "333-AC-13-A",
+			.device_name = "DS02032XL",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8800, /* mV */
+			.voltage_normal         = 7700, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 256,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 45,
+			.charging_min_c         = 0,
+			.charging_max_c         = 45,
+			.discharging_min_c      = -10,
+			.discharging_max_c      = 60,
+		},
+	},
+	/* SMP DS02032XL */
+	[BATTERY_DS02032XL_1C] = {
+		.fuel_gauge = {
+			.manuf_name = "333-1C-13-A",
+			.device_name = "DS02032XL",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0002,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8800, /* mV */
+			.voltage_normal         = 7700, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 256,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 45,
+			.charging_min_c         = 0,
+			.charging_max_c         = 45,
+			.discharging_min_c      = -10,
+			.discharging_max_c      = 60,
+		},
+	},
+	/* Sunwoda L21D4PG0 */
+	[BATTERY_L21D4PG0] = {
+		.fuel_gauge = {
+			.manuf_name = "Sunwoda",
+			.device_name = "L21D4PG0",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8900, /* mV */
+			.voltage_normal         = 7720, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 274,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 50,
+			.charging_min_c         = 0,
+			.charging_max_c         = 60,
+			.discharging_min_c      = -20,
+			.discharging_max_c      = 60,
+		},
+	},
+	/* SMP L21M4PG0 */
+	[BATTERY_L21M4PG0] = {
+		.fuel_gauge = {
+			.manuf_name = "SMP",
+			.device_name = "L21M4PG0",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0,
+			}
+		},
+		.batt_info = {
+			.voltage_max            = 8900, /* mV */
+			.voltage_normal         = 7720, /* mV */
+			.voltage_min            = 6000, /* mV */
+			.precharge_current      = 274,  /* mA */
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 50,
+			.charging_min_c         = 0,
+			.charging_max_c         = 60,
+			.discharging_min_c      = -20,
+			.discharging_max_c      = 60,
+		},
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
+
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_L21D4PG0;
---
+++ board/mrbland/led.c
@@ -0,0 +1,177 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Power and battery LED control.
+ */
+
+#include "battery.h"
+#include "charge_manager.h"
+#include "charge_state.h"
+#include "chipset.h"
+#include "ec_commands.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "led_common.h"
+#include "system.h"
+#include "util.h"
+#include "extpower.h"
+
+#define LED_ONE_SEC (1000 / HOOK_TICK_INTERVAL_MS)
+/* Battery LED blinks every per 400ms */
+#define LED_HALF_ONE_SEC (500 / HOOK_TICK_INTERVAL_MS)
+
+#define BAT_LED_ON 1
+#define BAT_LED_OFF 0
+
+const enum ec_led_id supported_led_ids[] = {
+	EC_LED_ID_BATTERY_LED,
+};
+
+const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
+
+enum led_color {
+	LED_OFF = 0,
+	LED_RED,
+	LED_GREEN,
+	LED_AMBER,
+	LED_COLOR_COUNT  /* Number of colors, not a color itself */
+};
+
+static void led_set_color(enum led_color color)
+{
+	gpio_set_level(GPIO_EC_CHG_LED_R_C0,
+		(color == LED_RED) ? BAT_LED_ON : BAT_LED_OFF);
+	gpio_set_level(GPIO_EC_CHG_LED_G_C0,
+		(color == LED_GREEN) ? BAT_LED_ON : BAT_LED_OFF);
+	if (color == LED_AMBER) {
+		gpio_set_level(GPIO_EC_CHG_LED_R_C0, BAT_LED_ON);
+		gpio_set_level(GPIO_EC_CHG_LED_G_C0, BAT_LED_ON);
+	}
+}
+
+void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
+{
+	brightness_range[EC_LED_COLOR_RED] = 1;
+	brightness_range[EC_LED_COLOR_GREEN] = 1;
+	brightness_range[EC_LED_COLOR_AMBER] = 1;
+}
+
+int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
+{
+	if (brightness[EC_LED_COLOR_RED] != 0)
+		led_set_color(LED_RED);
+	else if (brightness[EC_LED_COLOR_GREEN] != 0)
+		led_set_color(LED_GREEN);
+	else if (brightness[EC_LED_COLOR_AMBER] != 0)
+		led_set_color(LED_AMBER);
+	else
+		led_set_color(LED_OFF);
+
+	return EC_SUCCESS;
+}
+
+static void board_led_set_battery(void)
+{
+	static int battery_ticks;
+	int color = LED_OFF;
+	int period = 0;
+	int percent = DIV_ROUND_NEAREST(charge_get_display_charge(), 10);
+	uint32_t chflags = charge_get_flags();
+
+	battery_ticks++;
+
+	switch (charge_get_state()) {
+	case PWR_STATE_CHARGE:
+	case PWR_STATE_CHARGE_NEAR_FULL:
+		if (chipset_in_state(CHIPSET_STATE_ON |
+					CHIPSET_STATE_ANY_SUSPEND |
+					CHIPSET_STATE_ANY_OFF)) {
+			if (percent <= BATTERY_LEVEL_CRITICAL) {
+				/* battery capa <= 5%, Red */
+				color = LED_RED;
+			} else if (percent > BATTERY_LEVEL_CRITICAL &&
+					percent < BATTERY_LEVEL_NEAR_FULL) {
+				/* 5% < battery capa < 97%, Orange */
+				color = LED_AMBER;
+			} else {
+				/* battery capa >= 97%, Green */
+				color = LED_GREEN;
+			}
+		}
+		break;
+	case PWR_STATE_DISCHARGE:
+		if (chipset_in_state(CHIPSET_STATE_ON)) {
+			/* S0, Green (soild on) */
+			color = LED_GREEN;
+		} else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+			/* S3, Orange (1s on 3s off) */
+			period = (2 + 2) * LED_ONE_SEC;
+			battery_ticks = battery_ticks % period;
+			if (battery_ticks < 1 * LED_ONE_SEC)
+				color = LED_AMBER;
+			else
+				color = LED_OFF;
+		} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+			/* S5, off */
+			color = LED_OFF;
+		}
+		break;
+	case PWR_STATE_ERROR:
+		/* Battery error, Red on 1sec off 1sec */
+		period = (1 + 1) * LED_ONE_SEC;
+		battery_ticks = battery_ticks % period;
+		if (battery_ticks < 1 * LED_ONE_SEC)
+			color = LED_RED;
+		else
+			color = LED_OFF;
+		break;
+	case PWR_STATE_IDLE: /* External power connected in IDLE */
+		if (chflags & CHARGE_FLAG_FORCE_IDLE) {
+			/* Factory mode, Red 2 sec, green 2 sec */
+			period = (2 + 2) * LED_ONE_SEC;
+			battery_ticks = battery_ticks % period;
+			if (battery_ticks < 2 * LED_ONE_SEC)
+				color = LED_RED;
+			else
+				color = LED_GREEN;
+		} else
+			color = LED_RED;
+		break;
+	default:
+		/* Other states don't alter LED behavior */
+		break;
+	}
+
+	led_set_color(color);
+}
+
+/* Called by hook task every TICK */
+static void led_tick(void)
+{
+	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
+		board_led_set_battery();
+}
+DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
+
+void led_control(enum ec_led_id led_id, enum ec_led_state state)
+{
+	enum led_color color;
+
+	if ((led_id != EC_LED_ID_RECOVERY_HW_REINIT_LED) &&
+		(led_id != EC_LED_ID_SYSRQ_DEBUG_LED))
+		return;
+
+	if (state == LED_STATE_RESET) {
+		led_auto_control(EC_LED_ID_BATTERY_LED, 1);
+		board_led_set_battery();
+		return;
+	}
+
+	color = state ? LED_RED : LED_OFF;
+
+	led_auto_control(EC_LED_ID_BATTERY_LED, 0);
+
+	led_set_color(color);
+}
