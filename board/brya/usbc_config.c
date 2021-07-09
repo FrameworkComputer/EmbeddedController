e9d2fb3cedb0408dbcc1cc5e4ade97b11c576fc8
Boris Mittelberg <bmbm@google.com>
2021-07-09 15:56:38 -0700

    brya: disable debug accessory control on port C0
    This will release Burnside Bridge from reset and fix the issue when
    Servo's USB hub disappears after Servo was reset.
    
    BRANCH=none
    BUG=b:191516281
    TEST=running deployment in the lab
    
    Signed-off-by: Boris Mittelberg <bmbm@google.com>
    Change-Id: I6a65ffa13903adf96686681d961818ffe0e68fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3018683
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/brya/usbc_config.c
+++ board/brya/usbc_config.c
@@ -45,7 +45,8 @@ const struct tcpc_config_t tcpc_config[] = {
 			.addr_flags = NCT38XX_I2C_ADDR1_1_FLAGS,
 		},
 		.drv = &nct38xx_tcpm_drv,
-		.flags = TCPC_FLAGS_TCPCI_REV2_0,
+		.flags = TCPC_FLAGS_TCPCI_REV2_0 |
+			TCPC_FLAGS_NO_DEBUG_ACC_CONTROL,
 	},
 	[USBC_PORT_C1] = {
 		.bus_type = EC_BUS_TYPE_I2C,
