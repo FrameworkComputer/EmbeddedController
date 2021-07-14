3bb84d97357895d567b7199d10ac63bf81747a2a
Bhanu Prakash Maiya <bhanumaiya@google.com>
2021-07-14 10:46:04 -0700

    guybrush: Fix LID_ACCEL rotation matrix
    After testing the LID_ACCEL on chassis the rotation matrix
    should be fixed for Y and Z axis.
    
    BRANCH=none
    BUG=b:178400750
    TEST=Accel implementation tested on Guybrush
         EC commands:
         > accelinfo
         > acceldata
    
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Signed-off-by: Latchiamaran Senram <latchiamaran.senram@bosch.corp-partner.google.com>
    Change-Id: Ieb1d4e654a7417d22bb307a6f1589162ca7625e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3027382
    Tested-by: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Auto-Submit: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Diana Z <dzigterman@chromium.org>

--- board/guybrush/board.c
+++ board/guybrush/board.c
@@ -47,9 +47,9 @@ const mat33_fp_t base_standard_ref = {
 };
 
 const mat33_fp_t lid_standard_ref = {
-	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, FLOAT_TO_FP(-1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, 0,  FLOAT_TO_FP(1)}
+	{ 0, 0,  FLOAT_TO_FP(-1)}
 };
 
 struct motion_sensor_t motion_sensors[] = {
