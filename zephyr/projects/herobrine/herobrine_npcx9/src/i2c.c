f5364070332169ea2733b750df3265f410ea8be6
Wai-Hong Tam <waihong@google.com>
2021-07-19 18:26:23 -0700

    zephyr: herobrine_npcx9: Initial image which uses NPCX9
    This CL copies the Zephyr board/trogdor to board/herobrine_npcx9.
    Modify the chip config to npcx9m3f and some related configs (simply
    rename) and dts (update the UART property). The board/herobrine_npcx9
    and other NPCX9 boards, like board/brya will be merged to a single
    board. But the merge needs more work to remove the not-common configs
    and dts to the project directories. Leave it as a future work.
    
    This CL also copies the project trogdor/herobrine_npcx7 to
    herobrine/herobrine_npcx9. Remove the dead files power.c and
    hibernate.c, in the EC-OS directory. Remove the Zephyr 2.5 support
    and board.cmake (needed only for 2.5).
    
    BRANCH=None
    BUG=b:192253134
    TEST=Built the herobrine_npcx9 image successfully.
    
    Change-Id: I9641768ee978920f6a8677f13ae14e0a26ad35f4
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2993220
    Reviewed-by: Yuval Peress <peress@chromium.org>

---
+++ zephyr/projects/herobrine/herobrine_npcx9/src/i2c.c
@@ -0,0 +1,17 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "i2c/i2c.h"
+#include "i2c.h"
+
+/* Herobrine-NPCX9 board specific i2c implementation */
+
+#ifdef CONFIG_PLATFORM_EC_I2C_PASSTHRU_RESTRICTED
+int board_allow_i2c_passthru(int port)
+{
+	return (i2c_get_device_for_port(port) ==
+		i2c_get_device_for_port(I2C_PORT_VIRTUAL_BATTERY));
+}
+#endif
