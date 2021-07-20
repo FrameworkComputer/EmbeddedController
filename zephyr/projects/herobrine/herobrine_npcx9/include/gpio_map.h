87657e7f7cc0a8b356dffb736ac35a076c3ade58
Wai-Hong Tam <waihong@google.com>
2021-07-19 19:20:02 -0700

    zephyr: herobrine_npcx9: Update GPIOs
    This CL is a Zephyr-equivalent of CL:2993217.
    
    Update the GPIOs to match the schematic. Also update the hibnerate
    wake sources. Remove the AP_RST_L and WARM_RESET_L from the low voltage
    dts as their new GPIOs already operate at 1.8V, not configurable.
    
    BRANCH=None
    BUG=b:192253134, b:193583152
    TEST=Built the Zephyr herobrine_npcx9 image successfully.
    
    Change-Id: I2fcb03acff7ac376a5627f66c800d2d8cade1998
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3039386
    Reviewed-by: Yuval Peress <peress@chromium.org>

--- zephyr/projects/herobrine/herobrine_npcx9/include/gpio_map.h
+++ zephyr/projects/herobrine/herobrine_npcx9/include/gpio_map.h
@@ -38,8 +38,6 @@
 	GPIO_INT(GPIO_VOLUME_UP_L, GPIO_INT_EDGE_BOTH, button_interrupt)      \
 	GPIO_INT(GPIO_AP_RST_L, GPIO_INT_EDGE_BOTH, chipset_ap_rst_interrupt) \
 	GPIO_INT(GPIO_AP_SUSPEND, GPIO_INT_EDGE_BOTH, power_signal_interrupt) \
-	GPIO_INT(GPIO_DEPRECATED_AP_RST_REQ, GPIO_INT_EDGE_BOTH,              \
-		 power_signal_interrupt)                                      \
 	GPIO_INT(GPIO_POWER_GOOD, GPIO_INT_EDGE_BOTH,                         \
 		 chipset_power_good_interrupt)                                \
 	GPIO_INT(GPIO_PS_HOLD, GPIO_INT_EDGE_BOTH, power_signal_interrupt)    \
