4d71713791792036b22a0d52865413148c350506
Denis Brockus <dbrockus@google.com>
2021-07-27 09:09:17 -0600

    zephyr: ite: add system_get_fw_reset_vector to shim
    ITE, can you verify using base for the reset vector
    lines up with where you expect the reset vector to be
    in the image
    
    BUG=b:194794622
    BRANCH=none
    TEST=zmake configure -b $PROJ_HAYATO
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: Id66c1613e4a89aad36f68a8d03d3079f9d035e94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3056513
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>
    Tested-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/shim/chip/it8xxx2/system.c
+++ zephyr/shim/chip/it8xxx2/system.c
@@ -83,3 +83,13 @@ static int chip_system_init(const struct device *unused)
 	return 0;
 }
 SYS_INIT(chip_system_init, PRE_KERNEL_1, 15);
+
+uintptr_t system_get_fw_reset_vector(uintptr_t base)
+{
+	/*
+	 * Because our reset vector is at the beginning of image copy
+	 * (see init.S). So I just need to return 'base' here and EC will jump
+	 * to the reset vector.
+	 */
+	return base;
+}
