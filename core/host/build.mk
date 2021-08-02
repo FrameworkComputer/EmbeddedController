62e8a15c4bc87f9c9c146527922b6435a9136e63
Evan Green <evgreen@chromium.org>
2021-08-02 20:21:45 +0000

    Revert "make: Compile host tests in 32-bit mode"
    This reverts commit 1821e9423af84c7a126a4a15303c6492dd44e94b.
    
    Reason for revert: This change broke the build.
    
    BUG=b:195332627
    TEST=None
    BRANCH=None
    
    Original change's description:
    > make: Compile host tests in 32-bit mode
    >
    > Compile the host tests in 32-bit mode instead of 64-bit mode as it's
    > more likely to match the actual environments our ECs run on. For
    > instance, in 32-bit mode the tests might actually have a prayer of
    > finding a torn read or write bug, whereas in 64-bit mode 64-bit integers
    > are always operated on atomically.
    >
    > BUG=b:179062230
    > BRANCH=none
    > TEST=make -j runhosttests BOARD=host on x86_64 and i686
    >
    > Signed-off-by: Evan Green <evgreen@chromium.org>
    > Change-Id: I02517066337ec08c311c1f7daae5d3de84b1f4c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3059231
    > Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    
    Bug: b:179062230
    Change-Id: I35bcac77deeb1d031a31b32d911781f5fed4ab13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3067826
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Tested-by: Evan Green <evgreen@chromium.org>

--- core/host/build.mk
+++ core/host/build.mk
@@ -6,8 +6,6 @@
 # emulator specific files build
 #
 
-# Build host board in 32-bit mode for a better chance of catching things like
-# 64-bit write tearing.
-CFLAGS_CPU=-fno-builtin -m32
+CFLAGS_CPU=-fno-builtin
 
 core-y=main.o task.o timer.o panic.o disabled.o stack_trace.o
