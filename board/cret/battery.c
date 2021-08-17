3db59f2c94f3d7b2cdef794877e178df9bb75c20
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2021-08-17 14:59:56 +0800

    cret: Add six battery parameter
    Add six battery type for cret.
    And add device name to identify.
    
    BUG=b:196943911
    BRANCH=dedede
    TEST=make BOARD=cret,battery can be found via EC log check and
    battery can be cutoff by ectool command.Charge and discharge
    normally.
    
    Signed-off-by: johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
    Change-Id: I32a73783f13627d2e8b0e0f3e3f778340dc1cb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3099287
    Reviewed-by: Elthan Huang <elthan_huang@compal.corp-partner.google.com>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Elmo Lan <elmo_lan@compal.corp-partner.google.com>
    Tested-by: Elthan Huang <elthan_huang@compal.corp-partner.google.com>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- board/cret/battery.c
+++ board/cret/battery.c
@@ -65,6 +65,38 @@ const struct board_batt_params board_battery_info[] = {
 	},
 
 	/* BYD Battery Information */
+	[BATTERY_BYD_YT39X] = {
+		.fuel_gauge = {
+			.manuf_name = "BYD",
+			.device_name = "DELL YT39X",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11400,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
+
+	/* BYD Battery Information */
 	[BATTERY_BYD_X0Y5M] = {
 		.fuel_gauge = {
 			.manuf_name = "BYD",
@@ -96,9 +128,10 @@ const struct board_batt_params board_battery_info[] = {
 	},
 
 	/* LGC Battery Information */
-	[BATTERY_LGC] = {
+	[BATTERY_LGC_FDRHM] = {
 		.fuel_gauge = {
 			.manuf_name = "LGC-LGC3.65",
+			.device_name = "DELL FDRHM",
 			.ship_mode = {
 				.wb_support = 1,
 				.reg_addr = 0x44,
@@ -126,10 +159,76 @@ const struct board_batt_params board_battery_info[] = {
 		},
 	},
 
+	/* LGC Battery Information */
+	[BATTERY_LGC_8GHCX] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC-LGC3.65",
+			.device_name = "DELL 8GHCX",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11460,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
+
+
 	/* SWD-ATL Battery Information */
-	[BATTERY_SWD_ATL] = {
+	[BATTERY_SWD_ATL_WJPC4] = {
 		.fuel_gauge = {
 			.manuf_name = "SWD-ATL3.618",
+			.device_name = "DELL WJPC4",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11400,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
+
+	/* SWD-ATL Battery Information */
+	[BATTERY_SWD_ATL_CTGKT] = {
+		.fuel_gauge = {
+			.manuf_name = "SWD-ATL3.618",
+			.device_name = "DELL CTGKT",
 			.ship_mode = {
 				.wb_support = 1,
 				.reg_addr = 0x44,
@@ -158,9 +257,42 @@ const struct board_batt_params board_battery_info[] = {
 	},
 
 	/* SWD-COS Battery Information */
-	[BATTERY_SWD_COS] = {
+	[BATTERY_SWD_COS_WJPC4] = {
 		.fuel_gauge = {
 			.manuf_name = "SWD-COS3.634",
+			.device_name = "DELL WJPC4",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11400,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
+
+	/* SWD-COS Battery Information */
+	[BATTERY_SWD_COS_CTGKT] = {
+		.fuel_gauge = {
+			.manuf_name = "SWD-COS3.634",
+			.device_name = "DELL CTGKT",
 			.ship_mode = {
 				.wb_support = 1,
 				.reg_addr = 0x44,
@@ -193,6 +325,38 @@ const struct board_batt_params board_battery_info[] = {
 		.fuel_gauge = {
 			.manuf_name = "SMP-ATL-3.61",
 			.device_name = "DELL VM732",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11400,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
+
+	/* SMP-ATL Battery Information */
+	[BATTERY_SMP_ATL_26JGK] = {
+		.fuel_gauge = {
+			.manuf_name = "SMP-ATL-3.61",
+			.device_name = "DELL 26JGK",
 			.ship_mode = {
 				.wb_support = 1,
 				.reg_addr = 0x44,
@@ -283,6 +447,37 @@ const struct board_batt_params board_battery_info[] = {
 		},
 	},
 
+	/* SMP-COS Battery Information */
+	[BATTERY_SMP_COS_26JGK] = {
+		.fuel_gauge = {
+			.manuf_name = "SMP-COS3.63",
+			.device_name = "DELL 26JGK",
+			.ship_mode = {
+				.wb_support = 1,
+				.reg_addr = 0x44,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr       = 0x0,
+				.reg_mask       = 0x2000,
+				.disconnect_val = 0x2000,
+				.cfet_mask = 0x4000,
+				.cfet_off_val = 0x4000,
+			}
+		},
+		.batt_info = {
+			.voltage_max          = 13200,    /* mV */
+			.voltage_normal       = 11400,
+			.voltage_min          = 9000,
+			.precharge_current    = 256,       /* mA */
+			.start_charging_min_c = -3,
+			.start_charging_max_c = 50,
+			.charging_min_c       = -3,
+			.charging_max_c       = 60,
+			.discharging_min_c    = -5,
+			.discharging_max_c    = 70,
+		},
+	},
 	/* SMP-COS Battery Information */
 	[BATTERY_SMP_COS_RF9H3] = {
 		.fuel_gauge = {
