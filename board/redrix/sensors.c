3197bb9fe754da45bacb0a2cca646064b4dfb58e
Devin Lu <Devin.Lu@quantatw.com>
2021-08-25 19:19:00 +0800

    redrix: Correct accelerometer orientation
    BUG=b:197804802
    BRANCH=none
    TEST=On Redrix mockup, check the screen rotation was normally.
    Make sure the x-y-z axis direction is indeed.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I7a9dffaea3a16e2beb5d6df2af605086a4e13b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3117527
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/redrix/sensors.c
+++ board/redrix/sensors.c
@@ -53,17 +53,15 @@ K_MUTEX_DEFINE(g_base_accel_mutex);
 static struct accelgyro_saved_data_t g_bma253_data;
 static struct lsm6dsm_data lsm6dsm_data = LSM6DSM_DATA;
 
-/* TODO(b/184779333): calibrate the orientation matrix on later board stage */
 static const mat33_fp_t lid_standard_ref = {
+	{ FLOAT_TO_FP(-1), 0, 0},
 	{ 0, FLOAT_TO_FP(1), 0},
-	{ FLOAT_TO_FP(1), 0, 0},
 	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 
-/* TODO(b/184779743): verify orientation matrix */
 static const mat33_fp_t base_standard_ref = {
-	{ FLOAT_TO_FP(1), 0, 0},
-	{ 0, FLOAT_TO_FP(-1), 0},
+	{ FLOAT_TO_FP(-1), 0, 0},
+	{ 0, FLOAT_TO_FP(1), 0},
 	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 
