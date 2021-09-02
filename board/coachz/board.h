a42b82561483d046b08bb177b02393104ce88c0b
pengjunhao5 <pengjunhao5@huaqin.corp-partner.google.com>
2021-09-02 15:10:37 +0800

    Coachz: MotionSensor: Save some flash space
    This commit is for second source motionsensor combine(BMI160 & BMI260),
    We use chip id to detect which MotionSensor is on the board but flash
    is not enough so we reduce some console cmd.
    
    BUG=b:195908820
    TEST=make -j BOARD=coachz
    	Verify build on SIT board
    BRANCH=Trogdor
    
    Signed-off-by: pengjunhao <pengjunhao5@huaqin.corp-partner.google.com>
    Reviewed-by: tongjian <tongjian@huaqin.corp-partner.google.com>
    Reviewed-by: yudengwu <yudengwu@huaqin.corp-partner.google.com>
    Change-Id: I396052cb5996858507864c5201c2d93f1a885618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3139717
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Jian Tong <tongjian@huaqin.corp-partner.google.com>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- board/coachz/board.h
+++ board/coachz/board.h
@@ -23,6 +23,15 @@
 /* Internal SPI flash on NPCX7 */
 #define CONFIG_FLASH_SIZE_BYTES (512 * 1024)  /* 512KB internal spi flash */
 
+/* Save some flash space */
+#define CONFIG_USB_PD_DEBUG_LEVEL 2
+#undef CONFIG_CMD_FLASHINFO
+#undef CONFIG_CMD_MMAPINFO
+#undef CONFIG_CMD_ACCELSPOOF
+#undef CONFIG_CMD_ACCEL_FIFO
+#undef CONFIG_CMD_ACCEL_INFO
+#undef CONFIG_CMD_TASK_RESET
+
 /* Battery */
 #define CONFIG_BATTERY_DEVICE_CHEMISTRY  "LION"
 #define CONFIG_BATTERY_REVIVE_DISCONNECT
