8f95bc2c19f88ce78146919ad1a08d4495ca66e0
Devin Lu <Devin.Lu@quantatw.com>
2021-09-10 13:12:18 +0800

    redrix: Update fan real rpm
    BUG=b:193487913
    BRANCH=none
    TEST=make BOARD=redrix
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I9d4491937d0dd2d679537aae3020f17cfb014a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3153120
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/redrix/fans.c
+++ board/redrix/fans.c
@@ -42,17 +42,16 @@ static const struct fan_conf fan_conf_1 = {
 	.enable_gpio = GPIO_EN_PP5000_FAN2,
 };
 
-/* TOOD(b/193487913): need to update for real fan */
 static const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 2200,
-	.rpm_start = 2200,
-	.rpm_max = 7200,
+	.rpm_min = 1900,
+	.rpm_start = 2900,
+	.rpm_max = 7300,
 };
 
 static const struct fan_rpm fan_rpm_1 = {
-	.rpm_min = 2200,
-	.rpm_start = 2200,
-	.rpm_max = 7200,
+	.rpm_min = 1900,
+	.rpm_start = 2900,
+	.rpm_max = 7300,
 };
 
 const struct fan_t fans[FAN_CH_COUNT] = {
