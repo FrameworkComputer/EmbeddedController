9b2954a312e2c9b0a13a8d14a2d98fe6a19d53a7
David Huang <david.huang@quanta.corp-partner.google.com>
2021-09-13 17:45:43 +0800

    kano: fix rt1715 c0 flag
    Set correct tcpc flags for c0 tcpc config.
    
    BUG=none
    BRANCH=none
    TEST=Insert USB device in C0 and get SRC_Discovery.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I9a945a53335d5212b9080cf880c80f70227be1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3156316
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/kano/usbc_config.c
+++ board/kano/usbc_config.c
@@ -43,7 +43,8 @@ const struct tcpc_config_t tcpc_config[] = {
 			.addr_flags = RT1715_I2C_ADDR_FLAGS,
 		},
 		.drv = &rt1715_tcpm_drv,
-		.flags = TCPC_FLAGS_TCPCI_REV2_0,
+		.flags = TCPC_FLAGS_TCPCI_REV2_0 |
+			 TCPC_FLAGS_TCPCI_REV2_0_NO_VSAFE0V,
 	},
 	[USBC_PORT_C1] = {
 		.bus_type = EC_BUS_TYPE_I2C,
