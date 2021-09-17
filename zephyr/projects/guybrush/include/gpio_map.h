09bb5e3659aa2802669e9504947175832069daf7
Jack Rosenthal <jrosenth@chromium.org>
2021-09-17 13:44:03 -0600

    zephyr: guybrush: Bringup power button
    Enable power button interrupt and module.
    
    Don't have the x86 state machine going yet.
    
    BUG=b:195137794
    BRANCH=none
    TEST=push the button, see the prints
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I5996a65fe40afd58027452e5b44a60becbbdfa0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3169199
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/projects/guybrush/include/gpio_map.h
+++ zephyr/projects/guybrush/include/gpio_map.h
@@ -24,8 +24,10 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
-#define EC_CROS_GPIO_INTERRUPTS                                    \
-	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt) \
-	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt)
+#define EC_CROS_GPIO_INTERRUPTS                                           \
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
+	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt) \
+	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
+		 power_button_interrupt)
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
