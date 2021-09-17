ef1a31ced60aba0e1b54c8ae2c28e6c3fdd6f4c1
Jack Rosenthal <jrosenth@chromium.org>
2021-09-17 16:02:42 -0600

    zephyr: guybrush: Bringup power sequencing
    Bringup power sequencing for AMD and enable for guybrush.
    
    Note that at the moment we had to copy-paste the power signals and
    related funcitons, the code was a bit too far from shimming without
    extensive editing.  We can try and merge together later.
    
    BUG=b:195137794
    BRANCH=none
    TEST=power sequence to S0, nothing useful on ap console (yet!)
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I9a577e24d80fe0ff992af4b847e14695b82f871d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3169568
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/projects/guybrush/include/gpio_map.h
+++ zephyr/projects/guybrush/include/gpio_map.h
@@ -9,6 +9,19 @@
 #include <devicetree.h>
 #include <gpio_signal.h>
 
+/* Power input signals */
+enum power_signal {
+	X86_SLP_S0_N, /* SOC  -> SLP_S3_S0I3_L */
+	X86_SLP_S3_N, /* SOC  -> SLP_S3_L */
+	X86_SLP_S5_N, /* SOC  -> SLP_S5_L */
+
+	X86_S0_PGOOD, /* PMIC -> S0_PWROK_OD */
+	X86_S5_PGOOD, /* PMIC -> S5_PWROK */
+
+	/* Number of X86 signals */
+	POWER_SIGNAL_COUNT,
+};
+
 /*
  * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
  *
@@ -24,10 +37,25 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
-#define EC_CROS_GPIO_INTERRUPTS                                           \
-	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
-	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt) \
-	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
-		 power_button_interrupt)
+#define EC_CROS_GPIO_INTERRUPTS                                              \
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)           \
+	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt)    \
+	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                    \
+		 power_button_interrupt)                                     \
+	GPIO_INT(GPIO_EC_PWR_BTN_ODL, GPIO_INT_EDGE_BOTH,                    \
+		 power_button_interrupt)                                     \
+	GPIO_INT(GPIO_PCH_SLP_S3_L, GPIO_INT_EDGE_BOTH, baseboard_en_pwr_s0) \
+	GPIO_INT(GPIO_PCH_SLP_S5_L, GPIO_INT_EDGE_BOTH,                      \
+		 power_signal_interrupt)                                     \
+	GPIO_INT(GPIO_PCH_SLP_S0_L, GPIO_INT_EDGE_BOTH,                      \
+		 power_signal_interrupt)                                     \
+	GPIO_INT(GPIO_S5_PGOOD, GPIO_INT_EDGE_BOTH, extpower_interrupt)      \
+	GPIO_INT(GPIO_S0_PGOOD, GPIO_INT_EDGE_BOTH, power_signal_interrupt)  \
+	GPIO_INT(GPIO_EC_PCORE_INT_ODL, GPIO_INT_EDGE_BOTH,                  \
+		 power_signal_interrupt)                                     \
+	GPIO_INT(GPIO_PG_GROUPC_S0_OD, GPIO_INT_EDGE_BOTH,                   \
+		 baseboard_en_pwr_pcore_s0)                                  \
+	GPIO_INT(GPIO_PG_LPDDR4X_S3_OD, GPIO_INT_EDGE_BOTH,                  \
+		 baseboard_en_pwr_pcore_s0)
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
