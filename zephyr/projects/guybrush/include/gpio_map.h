96a9ffe99bdaf600ae6f93ee97ee4534f3b3b170
Jack Rosenthal <jrosenth@chromium.org>
2021-09-17 13:32:02 -0600

    zephyr: guybrush: Enable extpower gpio detection
    Bring up GPIOs for ACOK, and wire the interrupts.
    
    Note we needed to set certain Kconfigs to n due to incorrectly
    specified defaults.
    
    BUG=b:195137794
    BRANCH=none
    TEST=compiles
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I64f935895a74a5df2dbcd1122623788f91ef89fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3169198
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/projects/guybrush/include/gpio_map.h
+++ zephyr/projects/guybrush/include/gpio_map.h
@@ -24,7 +24,8 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
-#define EC_CROS_GPIO_INTERRUPTS \
-	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)
+#define EC_CROS_GPIO_INTERRUPTS                                    \
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt) \
+	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt)
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
