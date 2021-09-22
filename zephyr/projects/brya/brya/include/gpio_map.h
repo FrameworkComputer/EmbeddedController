a2cdbd60e4de8b45054f1c032868d6c7232d3299
Divya Sasidharan <divya.s.sasidharan@intel.com>
2021-09-22 14:38:46 -0700

    zephyr: brya: Add power sequencing support using shim
    Add required configs for power sequencing brya.
    
    BUG=b:200975143
    BRANCH=main
    TEST=zmake testall; check AP power state transitions
    to S0, verify booting up to kernel.
    
    Change-Id: Ib02cc4dc1be11af024f485efd5b73c613a4d1ac2
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3176402
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/projects/brya/brya/include/gpio_map.h
+++ zephyr/projects/brya/brya/include/gpio_map.h
@@ -27,11 +27,25 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
+/* Helper macros for generating CROS_EC_GPIO_INTERRUPTS */
+#ifdef CONFIG_PLATFORM_EC_POWERSEQ
+#define POWER_SIGNAL_INT(gpio, edge) \
+	GPIO_INT(gpio, edge, power_signal_interrupt)
+#else
+#define POWER_SIGNAL_INT(gpio, edge)
+#endif
+
+
 #define EC_CROS_GPIO_INTERRUPTS                                           \
 	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
 	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
 		 power_button_interrupt)                                  \
 	GPIO_INT(GPIO_WP_L, GPIO_INT_EDGE_BOTH, switch_interrupt)         \
-	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt)
-
+	GPIO_INT(GPIO_AC_PRESENT, GPIO_INT_EDGE_BOTH, extpower_interrupt) \
+	POWER_SIGNAL_INT(GPIO_PCH_SLP_S0_L, GPIO_INT_EDGE_BOTH)           \
+	POWER_SIGNAL_INT(GPIO_PCH_SLP_S3_L, GPIO_INT_EDGE_BOTH)           \
+	POWER_SIGNAL_INT(GPIO_SLP_SUS_L, GPIO_INT_EDGE_BOTH)              \
+	POWER_SIGNAL_INT(GPIO_PG_EC_DSW_PWROK, GPIO_INT_EDGE_BOTH)        \
+	POWER_SIGNAL_INT(GPIO_PG_EC_RSMRST_ODL, GPIO_INT_EDGE_BOTH)       \
+	POWER_SIGNAL_INT(GPIO_PG_EC_ALL_SYS_PWRGD, GPIO_INT_EDGE_BOTH)
 #endif /* __ZEPHYR_GPIO_MAP_H */
