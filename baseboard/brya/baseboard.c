78911f24fdfebf1fa2faf1fcfe2df1aeac897993
Boris Mittelberg <bmbm@google.com>
2021-09-23 17:03:36 -0700

    brya family: fix keyboard wake in tablet mode
    Block keyscan in S0ix, when the device is in tablet mode
    
    BUG=b:199114579
    BRANCH=none
    TEST=manual, as described in b:199114579
    
    Signed-off-by: Boris Mittelberg <bmbm@google.com>
    Change-Id: I17c5fc55e5e07ae180275aba492ef491b2d116cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3180270
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- baseboard/brya/baseboard.c
+++ baseboard/brya/baseboard.c
@@ -3,10 +3,12 @@
  * found in the LICENSE file.
  */
 
+#include "chipset.h"
 #include "common.h"
 #include "compile_time_macros.h"
-
 #include "gpio_signal.h"
+#include "keyboard_scan.h"
+#include "tablet_mode.h"
 
 /* Wake up pins */
 const enum gpio_signal hibernate_wake_pins[] = {
@@ -15,3 +17,27 @@ const enum gpio_signal hibernate_wake_pins[] = {
 	GPIO_LID_OPEN,
 };
 const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+/* This callback disables keyboard when convertibles are fully open */
+__override void lid_angle_peripheral_enable(int enable)
+{
+	/*
+	 * If the lid is in tablet position via other sensors,
+	 * ignore the lid angle, which might be faulty then
+	 * disable keyboard.
+	 */
+	if (tablet_get_mode())
+		enable = 0;
+
+	if (enable) {
+		keyboard_scan_enable(1, KB_SCAN_DISABLE_LID_ANGLE);
+	} else {
+		/*
+		 * When the chipset is on, the EC keeps the keyboard enabled and
+		 * Ensure that the chipset is off before disabling the keyboard.
+		 * the AP decides whether to ignore input devices or not.
+		 */
+		if (!chipset_in_state(CHIPSET_STATE_ON))
+			keyboard_scan_enable(0, KB_SCAN_DISABLE_LID_ANGLE);
+		}
+}
