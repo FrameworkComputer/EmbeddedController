07e1969eed461d0130f29a81f0a0b3da5e238170
David Huang <david.huang@quanta.corp-partner.google.com>
2021-09-30 11:37:03 +0800

    kano: Fix lid accel address and matrix
    Fix lid accel address and matrix.
    
    BUG=b:197200940
    BRANCH=none
    TEST=Use "ectool motionsense lid_angle" to check lid angle correct.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I474999818d7eeca3214b609997035c72b48d0a29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3192748
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/kano/sensors.c
+++ board/kano/sensors.c
@@ -52,7 +52,7 @@ static struct icm_drv_data_t g_icm426xx_data;
  */
 static const mat33_fp_t lid_standard_ref = {
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, FLOAT_TO_FP(-1), 0},
+	{ 0, FLOAT_TO_FP(1), 0},
 	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 static const mat33_fp_t base_standard_ref = {
@@ -72,7 +72,7 @@ struct motion_sensor_t motion_sensors[] = {
 		.mutex = &g_lid_accel_mutex,
 		.drv_data = &g_kx022_data,
 		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = KX022_ADDR0_FLAGS,
+		.i2c_spi_addr_flags = KX022_ADDR1_FLAGS,
 		.flags = MOTIONSENSE_FLAG_INT_SIGNAL,
 		.rot_standard_ref = &lid_standard_ref, /* identity matrix */
 		.default_range = 2, /* g */
