9afe143e2ce0941590bc23e5924c92c9ac3f187d
Sam Hurst <shurst@google.com>
2021-10-06 11:06:54 -0700

    zephyr: add support for ppc in device tree
    This commit adds support for ppc definition in device tree.
    It will allow to remove board specific files which implemented
    the ppc functionalities. This will unify the logic between
    different board in zephyr.
    
    BRANCH=main
    BUG=b:194432779
    TEST=Verified that the DUT charges from Port0 and Port1
    C0: Req [3] 15000mV 3000mA
    C1: Req [3] 15000mV 3000mA
    
    Signed-off-by: Sam Hurst <shurst@google.org>
    Change-Id: I6492e9bd447d880e0c47f9178ddc3767e68ebdf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3208913
    Commit-Queue: Sam Hurst <shurst@google.com>
    Tested-by: Sam Hurst <shurst@google.com>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

---
+++ zephyr/shim/src/ppc_sn5s330.c
@@ -0,0 +1,33 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define DT_DRV_COMPAT ti_sn5s330
+
+#include <devicetree.h>
+#include "ppc/sn5s330_public.h"
+#include "usb_pd.h"
+#include "usbc_ocp.h"
+#include "usbc_ppc.h"
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
+		"No compatible PPC instance found");
+
+#define USBC_PORT_PPC(inst)                                                   \
+	{                                                                     \
+		.i2c_port = I2C_PORT(DT_PHANDLE(DT_DRV_INST(inst), port)),    \
+		.i2c_addr_flags = DT_STRING_UPPER_TOKEN(                      \
+					DT_DRV_INST(inst), i2c_addr_flags),   \
+		.drv = &sn5s330_drv                                           \
+	},
+
+/* Power Path Controller */
+struct ppc_config_t ppc_chips[] = {
+	DT_INST_FOREACH_STATUS_OKAY(USBC_PORT_PPC)
+};
+unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
