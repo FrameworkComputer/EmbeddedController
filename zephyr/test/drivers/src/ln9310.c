491779d5e873d4a056cc52df20dcdd1ba28b8502
Aaron Massey <aaronmassey@google.com>
2021-10-13 17:29:14 -0600

    zephyr: test: ln9310 verify system status reg failure path
    Test that the LN9310 driver appropriately handles a failure to read
    the system status register when handling interrupts.
    
    BRANCH=none
    BUG=b:184856083
    TEST=zmake configure --test zephyr/test/drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@chromium.org>
    Change-Id: Ib30674eee7d465707f71f6acb701f156c821fafc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3221778
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Tested-by: Aaron Massey <aaronmassey@google.com>
    Reviewed-by: Yuval Peress <peress@google.com>
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- zephyr/test/drivers/src/ln9310.c
+++ zephyr/test/drivers/src/ln9310.c
@@ -489,6 +489,46 @@ static void test_ln9310_interrupt_reg_fail(void)
 	i2c_common_emul_set_read_func(i2c_emul, NULL, NULL);
 }
 
+static void test_ln9310_sys_sts_reg_fail(void)
+{
+	const struct emul *emulator =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(ln9310)));
+	struct i2c_emul *i2c_emul = ln9310_emul_get_i2c_emul(emulator);
+	struct reg_to_fail_data test_data = {
+		.reg_access_to_fail = 0,
+		.reg_access_fail_countdown = 0,
+	};
+
+	zassert_not_null(emulator, NULL);
+	zassert_not_null(i2c_emul, NULL);
+
+	ln9310_emul_set_context(emulator);
+	ln9310_emul_reset(emulator);
+	/* Battery and chip rev won't matter here so only testing one pair */
+	ln9310_emul_set_battery_cell_type(emulator, BATTERY_CELL_TYPE_2S);
+	ln9310_emul_set_version(emulator, LN9310_BC_STS_C_CHIP_REV_FIXED);
+
+	zassert_ok(ln9310_init(), NULL);
+	zassert_true(ln9310_emul_is_init(emulator), NULL);
+
+	i2c_common_emul_set_read_func(
+		i2c_emul, &mock_read_intercept_reg_to_fail, &test_data);
+
+	/* Register only read once and in the interrupt handler */
+	test_data.reg_access_to_fail = LN9310_REG_SYS_STS;
+	test_data.reg_access_fail_countdown = 1;
+
+	ln9310_software_enable(1);
+
+	/* TODO(b/201420132) */
+	k_msleep(TEST_DELAY_MS);
+
+	zassert_false(ln9310_power_good(), NULL);
+	zassert_true(test_data.reg_access_fail_countdown <= 0, NULL);
+
+	i2c_common_emul_set_read_func(i2c_emul, NULL, NULL);
+}
+
 static void reset_ln9310_state(void)
 {
 	ln9310_reset_to_initial_state();
@@ -499,6 +539,10 @@ void test_suite_ln9310(void)
 {
 	ztest_test_suite(
 		ln9310,
+		ztest_unit_test_setup_teardown(
+			test_ln9310_sys_sts_reg_fail,
+			reset_ln9310_state,
+			reset_ln9310_state),
 		ztest_unit_test_setup_teardown(
 			test_ln9310_interrupt_reg_fail,
 			reset_ln9310_state,
