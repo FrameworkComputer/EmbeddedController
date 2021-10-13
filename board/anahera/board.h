41ee17ad17806fc742a9622341d34b0f0fe5c80d
Devin Lu <Devin.Lu@quantatw.com>
2021-10-13 15:58:10 +0800

    anahera: Enable keyboard factory scanning
    This patch adds the factory keyboard connector test.
    
    BUG=b:202914217
    BRANCH=none
    TEST=Short keyboard pins and make sure "ectool kbfactorytest" works.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I9d06b8a44e6274e27f6ce96e23b48de799eaad61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3220039
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/anahera/board.h
+++ board/anahera/board.h
@@ -142,6 +142,7 @@
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC	10
 
 /* Keyboard features */
+#define CONFIG_KEYBOARD_FACTORY_TEST
 #define CONFIG_KEYBOARD_VIVALDI
 #define CONFIG_KEYBOARD_REFRESH_ROW3
 
@@ -194,6 +195,11 @@ enum mft_channel {
 	MFT_CH_0 = 0,
 	MFT_CH_COUNT
 };
+
+#ifdef CONFIG_KEYBOARD_FACTORY_TEST
+extern const int keyboard_factory_scan_pins[][2];
+extern const int keyboard_factory_scan_pins_used;
+#endif
 
 #endif /* !__ASSEMBLER__ */
 
