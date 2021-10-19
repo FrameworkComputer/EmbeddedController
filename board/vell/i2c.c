25fc7677d40d9c789dc0cd2abd3434c9979b5a53
Devin Lu <Devin.Lu@quantatw.com>
2021-10-19 18:33:26 +0800

    vell: Configure USBC ports
    Vell has 4 USBC ports:
    C0: TCPC NCT3808 port 1 + SYV682B + BBR + BC1.2
    C1: TCPC NCT3808 port 2 + SYV682B + BBR + BC1.2
    C2: TCPC NCT3808 port 1 + SYV682B + BBR + BC1.2
    C3: TCPC NCT3808 port 2 + SYV682B + BBR + BC1.2
    
    BUG=b:186907625, b:208583786
    BRANCH=none
    TEST=1. Make sure all USBC ports is able to charge.
    2. Make sure all USBC ports is able to provide source to USB device.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: Ideb5fcb438d6d68d568ae6776c3fdab0c16f2103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3276019
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/vell/i2c.c
+++ board/vell/i2c.c
@@ -8,8 +8,6 @@
 #include "hooks.h"
 #include "i2c.h"
 
-#define BOARD_ID_FAST_PLUS_CAPABLE	2
-
 /* I2C port map configuration */
 const struct i2c_port_t i2c_ports[] = {
 	{
@@ -22,36 +20,35 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 	{
 		/* I2C1 */
-		.name = "tcpc0,2",
-		.port = I2C_PORT_USB_C0_C2_TCPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
+		.name = "tcpc0,1",
+		.port = I2C_PORT_USB_C0_C1_TCPC,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_C0_C1_TCPC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C1_TCPC_SDA,
 	},
 	{
 		/* I2C2 */
-		.name = "ppc0,2",
-		.port = I2C_PORT_USB_C0_C2_PPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
+		.name = "ppc0,1",
+		.port = I2C_PORT_USB_C0_C1_PPC,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_C0_C1_PPC_BC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C1_PPC_BC_SDA,
 	},
 	{
 		/* I2C3 */
-		.name = "retimer0,2",
-		.port = I2C_PORT_USB_C0_C2_MUX,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_RT_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
+		.name = "retimer0,1,2,3",
+		.port = I2C_PORT_USB_C0_C1_MUX,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_RT_SCL,
+		.sda = GPIO_EC_I2C_USB_RT_SDA,
 	},
 	{
-		/* I2C4 C1 TCPC */
-		.name = "tcpc1",
-		.port = I2C_PORT_USB_C1_TCPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
-		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
+		/* I2C4 */
+		.name = "tcpc2,3",
+		.port = I2C_PORT_USB_C2_C3_TCPC,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_C2_C3_TCPC_SCL,
+		.sda = GPIO_EC_I2C_USB_C2_C3_TCPC_SDA,
 	},
 	{
 		/* I2C5 */
@@ -63,12 +60,11 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 	{
 		/* I2C6 */
-		.name = "ppc1",
-		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
-		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
+		.name = "ppc2,3",
+		.port = I2C_PORT_USB_C2_C3_PPC,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_C2_C3_PPC_BC_RT_SCL,
+		.sda = GPIO_EC_I2C_USB_C2_C3_PPC_BC_RT_SDA,
 	},
 	{
 		/* I2C7 */
@@ -80,19 +76,3 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
-
-/*
- * I2C controllers are initialized in main.c. This sets the speed much
- * later, but before I2C peripherals are initialized.
- */
-static void set_board_legacy_i2c_speeds(void)
-{
-	if (get_board_id() >= BOARD_ID_FAST_PLUS_CAPABLE)
-		return;
-
-	ccprints("setting USB DB I2C buses to 400 kHz\n");
-
-	i2c_set_freq(I2C_PORT_USB_C1_TCPC, I2C_FREQ_400KHZ);
-	i2c_set_freq(I2C_PORT_USB_C1_PPC, I2C_FREQ_400KHZ);
-}
-DECLARE_HOOK(HOOK_INIT, set_board_legacy_i2c_speeds, HOOK_PRIO_INIT_I2C - 1);
--- board/vell/usbc_config.c
+++ board/vell/usbc_config.c
@@ -17,7 +17,6 @@
 #include "driver/ppc/syv682x_public.h"
 #include "driver/retimer/bb_retimer_public.h"
 #include "driver/tcpm/nct38xx.h"
-#include "driver/tcpm/ps8xxx_public.h"
 #include "driver/tcpm/tcpci.h"
 #include "ec_commands.h"
 #include "fw_config.h"
@@ -44,29 +43,35 @@ const struct tcpc_config_t tcpc_config[] = {
 	[USBC_PORT_C0] = {
 		.bus_type = EC_BUS_TYPE_I2C,
 		.i2c_info = {
-			.port = I2C_PORT_USB_C0_C2_TCPC,
+			.port = I2C_PORT_USB_C0_C1_TCPC,
 			.addr_flags = NCT38XX_I2C_ADDR1_1_FLAGS,
 		},
 		.drv = &nct38xx_tcpm_drv,
-		.flags = TCPC_FLAGS_TCPCI_REV2_0 |
-			TCPC_FLAGS_NO_DEBUG_ACC_CONTROL,
+		.flags = TCPC_FLAGS_TCPCI_REV2_0,
 	},
 	[USBC_PORT_C1] = {
 		.bus_type = EC_BUS_TYPE_I2C,
 		.i2c_info = {
-			.port = I2C_PORT_USB_C1_TCPC,
-			.addr_flags = PS8751_I2C_ADDR1_FLAGS,
+			.port = I2C_PORT_USB_C0_C1_TCPC,
+			.addr_flags = NCT38XX_I2C_ADDR2_1_FLAGS,
 		},
-		.drv = &ps8xxx_tcpm_drv,
-		.flags = TCPC_FLAGS_TCPCI_REV2_0 |
-			 TCPC_FLAGS_TCPCI_REV2_0_NO_VSAFE0V |
-			 TCPC_FLAGS_CONTROL_VCONN,
+		.drv = &nct38xx_tcpm_drv,
+		.flags = TCPC_FLAGS_TCPCI_REV2_0,
 	},
 	[USBC_PORT_C2] = {
 		.bus_type = EC_BUS_TYPE_I2C,
 		.i2c_info = {
-			.port = I2C_PORT_USB_C0_C2_TCPC,
-			.addr_flags = NCT38XX_I2C_ADDR2_1_FLAGS,
+			.port = I2C_PORT_USB_C2_C3_TCPC,
+			.addr_flags = NCT38XX_I2C_ADDR1_4_FLAGS,
+		},
+		.drv = &nct38xx_tcpm_drv,
+		.flags = TCPC_FLAGS_TCPCI_REV2_0,
+	},
+	[USBC_PORT_C3] = {
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_USB_C2_C3_TCPC,
+			.addr_flags = NCT38XX_I2C_ADDR2_4_FLAGS,
 		},
 		.drv = &nct38xx_tcpm_drv,
 		.flags = TCPC_FLAGS_TCPCI_REV2_0,
@@ -88,23 +93,23 @@ BUILD_ASSERT(ARRAY_SIZE(usb_port_enable) == USB_PORT_COUNT);
 /* USBC PPC configuration */
 struct ppc_config_t ppc_chips[] = {
 	[USBC_PORT_C0] = {
-		.i2c_port = I2C_PORT_USB_C0_C2_PPC,
+		.i2c_port = I2C_PORT_USB_C0_C1_PPC,
 		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
 		.drv = &syv682x_drv,
 	},
 	[USBC_PORT_C1] = {
-		/* Compatible with Silicon Mitus SM536A0 */
-		.i2c_port = I2C_PORT_USB_C1_PPC,
-		.i2c_addr_flags = NX20P3483_ADDR2_FLAGS,
-		.drv = &nx20p348x_drv,
+		.i2c_port = I2C_PORT_USB_C0_C1_PPC,
+		.i2c_addr_flags = SYV682X_ADDR1_FLAGS,
+		.drv = &syv682x_drv,
 	},
 	[USBC_PORT_C2] = {
-		.i2c_port = I2C_PORT_USB_C0_C2_PPC,
-		/*
-		 * b/179987870
-		 * schematics I2C map says ADDR3
-		 */
+		.i2c_port = I2C_PORT_USB_C2_C3_PPC,
 		.i2c_addr_flags = SYV682X_ADDR2_FLAGS,
+		.drv = &syv682x_drv,
+	},
+	[USBC_PORT_C3] = {
+		.i2c_port = I2C_PORT_USB_C2_C3_PPC,
+		.i2c_addr_flags = SYV682X_ADDR3_FLAGS,
 		.drv = &syv682x_drv,
 	},
 };
@@ -118,21 +123,20 @@ static const struct usb_mux usbc0_tcss_usb_mux = {
 	.driver = &virtual_usb_mux_driver,
 	.hpd_update = &virtual_hpd_update,
 };
+static const struct usb_mux usbc1_tcss_usb_mux = {
+	.usb_port = USBC_PORT_C1,
+	.driver = &virtual_usb_mux_driver,
+	.hpd_update = &virtual_hpd_update,
+};
 static const struct usb_mux usbc2_tcss_usb_mux = {
 	.usb_port = USBC_PORT_C2,
 	.driver = &virtual_usb_mux_driver,
 	.hpd_update = &virtual_hpd_update,
 };
-
-/*
- * USB3 DB mux configuration - the top level mux still needs to be set
- * to the virtual_usb_mux_driver so the AP gets notified of mux changes
- * and updates the TCSS configuration on state changes.
- */
-static const struct usb_mux usbc1_usb3_db_retimer = {
-	.usb_port = USBC_PORT_C1,
-	.driver = &tcpci_tcpm_usb_mux_driver,
-	.hpd_update = &ps8xxx_tcpc_update_hpd_status,
+static const struct usb_mux usbc3_tcss_usb_mux = {
+	.usb_port = USBC_PORT_C3,
+	.driver = &virtual_usb_mux_driver,
+	.hpd_update = &virtual_hpd_update,
 };
 
 const struct usb_mux usb_muxes[] = {
@@ -140,24 +144,33 @@ const struct usb_mux usb_muxes[] = {
 		.usb_port = USBC_PORT_C0,
 		.driver = &bb_usb_retimer,
 		.hpd_update = bb_retimer_hpd_update,
-		.i2c_port = I2C_PORT_USB_C0_C2_MUX,
+		.i2c_port = I2C_PORT_USB_C0_C1_MUX,
 		.i2c_addr_flags = USBC_PORT_C0_BB_RETIMER_I2C_ADDR,
 		.next_mux = &usbc0_tcss_usb_mux,
 	},
 	[USBC_PORT_C1] = {
-		/* PS8815 DB */
 		.usb_port = USBC_PORT_C1,
-		.driver = &virtual_usb_mux_driver,
-		.hpd_update = &virtual_hpd_update,
-		.next_mux = &usbc1_usb3_db_retimer,
+		.driver = &bb_usb_retimer,
+		.hpd_update = bb_retimer_hpd_update,
+		.i2c_port = I2C_PORT_USB_C0_C1_MUX,
+		.i2c_addr_flags = USBC_PORT_C1_BB_RETIMER_I2C_ADDR,
+		.next_mux = &usbc1_tcss_usb_mux,
 	},
 	[USBC_PORT_C2] = {
 		.usb_port = USBC_PORT_C2,
 		.driver = &bb_usb_retimer,
 		.hpd_update = bb_retimer_hpd_update,
-		.i2c_port = I2C_PORT_USB_C0_C2_MUX,
+		.i2c_port = I2C_PORT_USB_C2_C3_MUX,
 		.i2c_addr_flags = USBC_PORT_C2_BB_RETIMER_I2C_ADDR,
 		.next_mux = &usbc2_tcss_usb_mux,
+	},
+	[USBC_PORT_C3] = {
+		.usb_port = USBC_PORT_C3,
+		.driver = &bb_usb_retimer,
+		.hpd_update = bb_retimer_hpd_update,
+		.i2c_port = I2C_PORT_USB_C2_C3_MUX,
+		.i2c_addr_flags = USBC_PORT_C3_BB_RETIMER_I2C_ADDR,
+		.next_mux = &usbc3_tcss_usb_mux,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == USBC_PORT_COUNT);
@@ -165,16 +178,20 @@ BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == USBC_PORT_COUNT);
 /* BC1.2 charger detect configuration */
 const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
 	[USBC_PORT_C0] = {
-		.i2c_port = I2C_PORT_USB_C0_C2_BC12,
+		.i2c_port = I2C_PORT_USB_C0_C1_BC12,
 		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
 	},
 	[USBC_PORT_C1] = {
-		.i2c_port = I2C_PORT_USB_C1_BC12,
-		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
+		.i2c_port = I2C_PORT_USB_C0_C1_BC12,
+		.i2c_addr_flags = PI3USB9201_I2C_ADDR_2_FLAGS,
 	},
 	[USBC_PORT_C2] = {
-		.i2c_port = I2C_PORT_USB_C0_C2_BC12,
+		.i2c_port = I2C_PORT_USB_C2_C3_BC12,
 		.i2c_addr_flags = PI3USB9201_I2C_ADDR_1_FLAGS,
+	},
+	[USBC_PORT_C3] = {
+		.i2c_port = I2C_PORT_USB_C2_C3_BC12,
+		.i2c_addr_flags = PI3USB9201_I2C_ADDR_0_FLAGS,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(pi3usb9201_bc12_chips) == USBC_PORT_COUNT);
@@ -190,26 +207,26 @@ BUILD_ASSERT(ARRAY_SIZE(pi3usb9201_bc12_chips) == USBC_PORT_COUNT);
 
 struct ioexpander_config_t ioex_config[] = {
 	[IOEX_C0_NCT38XX] = {
-		.i2c_host_port = I2C_PORT_USB_C0_C2_TCPC,
+		.i2c_host_port = I2C_PORT_USB_C0_C1_TCPC,
 		.i2c_addr_flags = NCT38XX_I2C_ADDR1_1_FLAGS,
+		.drv = &nct38xx_ioexpander_drv,
+		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
+	},
+	[IOEX_C1_NCT38XX] = {
+		.i2c_host_port = I2C_PORT_USB_C0_C1_TCPC,
+		.i2c_addr_flags = NCT38XX_I2C_ADDR2_1_FLAGS,
 		.drv = &nct38xx_ioexpander_drv,
 		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
 	},
 	[IOEX_C2_NCT38XX] = {
-		.i2c_host_port = I2C_PORT_USB_C0_C2_TCPC,
-		.i2c_addr_flags = NCT38XX_I2C_ADDR2_1_FLAGS,
+		.i2c_host_port = I2C_PORT_USB_C2_C3_TCPC,
+		.i2c_addr_flags = NCT38XX_I2C_ADDR1_4_FLAGS,
 		.drv = &nct38xx_ioexpander_drv,
 		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
 	},
-	[IOEX_ID_1_C0_NCT38XX] = {
-		.i2c_host_port = I2C_PORT_USB_C0_C2_TCPC,
-		.i2c_addr_flags = NCT38XX_I2C_ADDR1_1_FLAGS,
-		.drv = &nct38xx_ioexpander_drv,
-		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
-	},
-	[IOEX_ID_1_C2_NCT38XX] = {
-		.i2c_host_port = I2C_PORT_USB_C0_C2_TCPC,
-		.i2c_addr_flags = NCT38XX_I2C_ADDR2_1_FLAGS,
+	[IOEX_C3_NCT38XX] = {
+		.i2c_host_port = I2C_PORT_USB_C2_C3_TCPC,
+		.i2c_addr_flags = NCT38XX_I2C_ADDR2_4_FLAGS,
 		.drv = &nct38xx_ioexpander_drv,
 		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
 	},
@@ -256,8 +273,12 @@ __override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 
 	if (me->usb_port == USBC_PORT_C0)
 		rst_signal = IOEX_USB_C0_RT_RST_ODL;
+	else if (me->usb_port == USBC_PORT_C1)
+		rst_signal = IOEX_USB_C1_RT_RST_ODL;
 	else if (me->usb_port == USBC_PORT_C2)
 		rst_signal = IOEX_USB_C2_RT_RST_ODL;
+	else if (me->usb_port == USBC_PORT_C3)
+		rst_signal = IOEX_USB_C3_RT_RST_ODL;
 	else
 		return EC_ERROR_INVAL;
 
@@ -292,9 +313,8 @@ void board_reset_pd_mcu(void)
 	 * TODO(b/203371200): figure out correct timing
 	 */
 
-	gpio_set_level(GPIO_USB_C0_C2_TCPC_RST_ODL, 0);
-	gpio_set_level(GPIO_USB_C1_RST_ODL, 0);
-	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 0);
+	gpio_set_level(GPIO_USB_C0_C1_TCPC_RST_ODL, 0);
+	gpio_set_level(GPIO_USB_C2_C3_TCPC_RST_ODL, 0);
 
 	/*
 	 * delay for power-on to reset-off and min. assertion time
@@ -302,9 +322,8 @@ void board_reset_pd_mcu(void)
 
 	msleep(20);
 
-	gpio_set_level(GPIO_USB_C0_C2_TCPC_RST_ODL, 1);
-	gpio_set_level(GPIO_USB_C1_RST_ODL, 1);
-	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 1);
+	gpio_set_level(GPIO_USB_C0_C1_TCPC_RST_ODL, 1);
+	gpio_set_level(GPIO_USB_C2_C3_TCPC_RST_ODL, 1);
 
 	/* wait for chips to come up */
 
@@ -324,27 +343,30 @@ static void board_tcpc_init(void)
 
 		/*
 		 * These IO expander pins are implemented using the
-		 * C0/C2 TCPC, so they must be set up after the TCPC has
-		 * been taken out of reset.
+		 * C0/C1/C2/C3 TCPCs, so they must be set up after the TCPC
+		 * has been taken out of reset.
 		 */
 		enable_ioex(IOEX_C0_NCT38XX);
+		enable_ioex(IOEX_C1_NCT38XX);
 		enable_ioex(IOEX_C2_NCT38XX);
+		enable_ioex(IOEX_C3_NCT38XX);
 	}
 
 	/* Enable PPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_PPC_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_PPC_INT_ODL);
 	gpio_enable_interrupt(GPIO_USB_C2_PPC_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C3_PPC_INT_ODL);
 
 	/* Enable TCPC interrupts. */
-	gpio_enable_interrupt(GPIO_USB_C0_C2_TCPC_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C0_C1_TCPC_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C2_C3_TCPC_INT_ODL);
 
 	/* Enable BC1.2 interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_BC12_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C2_BC12_INT_ODL);
-
-	gpio_enable_interrupt(GPIO_USB_C1_PPC_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
 	gpio_enable_interrupt(GPIO_USB_C1_BC12_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C2_BC12_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C3_BC12_INT_ODL);
 }
 DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_CHIPSET);
 
@@ -352,11 +374,11 @@ uint16_t tcpc_get_alert_status(void)
 {
 	uint16_t status = 0;
 
-	if (gpio_get_level(GPIO_USB_C0_C2_TCPC_INT_ODL) == 0)
-		status |= PD_STATUS_TCPC_ALERT_0 | PD_STATUS_TCPC_ALERT_2;
+	if (gpio_get_level(GPIO_USB_C0_C1_TCPC_INT_ODL) == 0)
+		status = PD_STATUS_TCPC_ALERT_0 | PD_STATUS_TCPC_ALERT_1;
 
-	if (gpio_get_level(GPIO_USB_C1_TCPC_INT_ODL) == 0)
-		status |= PD_STATUS_TCPC_ALERT_1;
+	if (gpio_get_level(GPIO_USB_C2_C3_TCPC_INT_ODL) == 0)
+		status = PD_STATUS_TCPC_ALERT_2 | PD_STATUS_TCPC_ALERT_3;
 
 	return status;
 }
@@ -369,17 +391,19 @@ int ppc_get_alert_status(int port)
 		return gpio_get_level(GPIO_USB_C1_PPC_INT_ODL) == 0;
 	else if (port == USBC_PORT_C2)
 		return gpio_get_level(GPIO_USB_C2_PPC_INT_ODL) == 0;
+	else if (port == USBC_PORT_C3)
+		return gpio_get_level(GPIO_USB_C3_PPC_INT_ODL) == 0;
 	return 0;
 }
 
 void tcpc_alert_event(enum gpio_signal signal)
 {
 	switch (signal) {
-	case GPIO_USB_C0_C2_TCPC_INT_ODL:
+	case GPIO_USB_C0_C1_TCPC_INT_ODL:
 		schedule_deferred_pd_interrupt(USBC_PORT_C0);
 		break;
-	case GPIO_USB_C1_TCPC_INT_ODL:
-		schedule_deferred_pd_interrupt(USBC_PORT_C1);
+	case GPIO_USB_C2_C3_TCPC_INT_ODL:
+		schedule_deferred_pd_interrupt(USBC_PORT_C2);
 		break;
 	default:
 		break;
@@ -398,6 +422,9 @@ void bc12_interrupt(enum gpio_signal signal)
 	case GPIO_USB_C2_BC12_INT_ODL:
 		task_set_event(TASK_ID_USB_CHG_P2, USB_CHG_EVENT_BC12);
 		break;
+	case GPIO_USB_C3_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P3, USB_CHG_EVENT_BC12);
+		break;
 	default:
 		break;
 	}
@@ -410,10 +437,13 @@ void ppc_interrupt(enum gpio_signal signal)
 		syv682x_interrupt(USBC_PORT_C0);
 		break;
 	case GPIO_USB_C1_PPC_INT_ODL:
-		nx20p348x_interrupt(USBC_PORT_C1);
+		syv682x_interrupt(USBC_PORT_C1);
 		break;
 	case GPIO_USB_C2_PPC_INT_ODL:
 		syv682x_interrupt(USBC_PORT_C2);
+		break;
+	case GPIO_USB_C3_PPC_INT_ODL:
+		syv682x_interrupt(USBC_PORT_C3);
 		break;
 	default:
 		break;
@@ -430,20 +460,4 @@ void retimer_interrupt(enum gpio_signal signal)
 __override bool board_is_dts_port(int port)
 {
 	return port == USBC_PORT_C0;
-}
-
-__override bool board_is_tbt_usb4_port(int port)
-{
-	if (port == USBC_PORT_C0 || port == USBC_PORT_C2)
-		return true;
-
-	return false;
-}
-
-__override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
-{
-	if (!board_is_tbt_usb4_port(port))
-		return TBT_SS_RES_0;
-
-	return TBT_SS_TBT_GEN3;
 }
