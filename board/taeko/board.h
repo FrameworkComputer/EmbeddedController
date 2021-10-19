9cb708d986a3c693ac97b8301f492497a89e4f13
reno.wang <reno.wang@lcfc.corp-partner.google.com>
2021-10-19 22:44:05 +0800

    taeko: change PD_MAX_POWER_MW from 60W to 45W
    BUG=b:196426265
    BRANCH=None
    TEST=make -j BOARD=taeko
    
    Signed-off-by: reno.wang <reno.wang@lcfc.corp-partner.google.com>
    Change-Id: Ie96eea42ca24647c8875ba8aaafc9a7a4498bd9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3231715
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- board/taeko/board.h
+++ board/taeko/board.h
@@ -100,7 +100,7 @@
  * Passive USB-C cables only support up to 60W.
  */
 #define PD_OPERATING_POWER_MW	15000
-#define PD_MAX_POWER_MW		60000
+#define PD_MAX_POWER_MW		45000
 #define PD_MAX_CURRENT_MA	3000
 #define PD_MAX_VOLTAGE_MV	20000
 
