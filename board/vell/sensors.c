559dc7b297b03c2b94ce118025aca1e65d0ef7a2
Devin Lu <Devin.Lu@quantatw.com>
2021-10-22 13:25:08 +0800

    vell: Configure thermal sensors
    There are 5 thermal sensors on Vell.
    The sensor map as following:
    
    ADC_TEMP_SENSOR_1_SOC: Close to SOC.
    ADC_TEMP_SENSOR_2_CHARGER: Close to charger.
    ADC_TEMP_SENSOR_3_WWAN: Close to wwan.
    ADC_TEMP_SENSOR_4_DDR: Close to DDR.
    ADC_TEMP_SENSOR_5_REGULATOR: Close to 3/5V regulator.
    
    BUG=b:203839956
    BRANCH=none
    TEST=EC console "temps".
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I54aeec5f6040a1592583e921d58dc39d148b7a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3276021
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/vell/sensors.c
+++ board/vell/sensors.c
@@ -15,30 +15,37 @@
 
 /* ADC configuration */
 struct adc_t adc_channels[] = {
-	[ADC_TEMP_SENSOR_1_DDR_SOC] = {
-		.name = "TEMP_DDR_SOC",
+	[ADC_TEMP_SENSOR_1_SOC] = {
+		.name = "TEMP_SOC",
 		.input_ch = NPCX_ADC_CH0,
 		.factor_mul = ADC_MAX_VOLT,
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
 	},
-	[ADC_TEMP_SENSOR_2_AMBIENT] = {
-		.name = "TEMP_AMBIENT",
+	[ADC_TEMP_SENSOR_2_CHARGER] = {
+		.name = "TEMP_CHARGER",
 		.input_ch = NPCX_ADC_CH1,
 		.factor_mul = ADC_MAX_VOLT,
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
 	},
-	[ADC_TEMP_SENSOR_3_CHARGER] = {
-		.name = "TEMP_CHARGER",
+	[ADC_TEMP_SENSOR_3_WWAN] = {
+		.name = "TEMP_WWAN",
 		.input_ch = NPCX_ADC_CH6,
 		.factor_mul = ADC_MAX_VOLT,
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
 	},
-	[ADC_TEMP_SENSOR_4_WWAN] = {
-		.name = "TEMP_WWAN",
+	[ADC_TEMP_SENSOR_4_DDR] = {
+		.name = "TEMP_DDR",
 		.input_ch = NPCX_ADC_CH7,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
+	[ADC_TEMP_SENSOR_5_REGULATOR] = {
+		.name = "TEMP_REGULATOR",
+		.input_ch = NPCX_ADC_CH4,
 		.factor_mul = ADC_MAX_VOLT,
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
@@ -156,35 +163,41 @@ DECLARE_HOOK(HOOK_INIT, baseboard_sensors_init, HOOK_PRIO_INIT_I2C + 1);
 
 /* Temperature sensor configuration */
 const struct temp_sensor_t temp_sensors[] = {
-	[TEMP_SENSOR_1_DDR_SOC] = {
-		.name = "DDR and SOC",
+	[TEMP_SENSOR_1_SOC] = {
+		.name = "SOC",
 		.type = TEMP_SENSOR_TYPE_BOARD,
 		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_1_DDR_SOC,
+		.idx = ADC_TEMP_SENSOR_1_SOC,
 	},
-	[TEMP_SENSOR_2_AMBIENT] = {
-		.name = "Ambient",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_2_AMBIENT,
-	},
-	[TEMP_SENSOR_3_CHARGER] = {
+	[TEMP_SENSOR_2_CHARGER] = {
 		.name = "Charger",
 		.type = TEMP_SENSOR_TYPE_BOARD,
 		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_3_CHARGER,
+		.idx = ADC_TEMP_SENSOR_2_CHARGER,
 	},
-	[TEMP_SENSOR_4_WWAN] = {
+	[TEMP_SENSOR_3_WWAN] = {
 		.name = "WWAN",
 		.type = TEMP_SENSOR_TYPE_BOARD,
 		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_4_WWAN,
+		.idx = ADC_TEMP_SENSOR_3_WWAN,
+	},
+	[TEMP_SENSOR_4_DDR] = {
+		.name = "DDR",
+		.type = TEMP_SENSOR_TYPE_BOARD,
+		.read = get_temp_3v3_30k9_47k_4050b,
+		.idx = ADC_TEMP_SENSOR_4_DDR,
+	},
+	[TEMP_SENSOR_5_REGULATOR] = {
+		.name = "Regulator",
+		.type = TEMP_SENSOR_TYPE_BOARD,
+		.read = get_temp_3v3_30k9_47k_4050b,
+		.idx = ADC_TEMP_SENSOR_5_REGULATOR,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
 /*
- * TODO(b/180681346): update for Alder Lake/brya
+ * TODO(b/203839956): update for Alder Lake/vell
  *
  * Alder Lake specifies 100 C as maximum TDP temperature.  THRMTRIP# occurs at
  * 130 C.  However, sensor is located next to DDR, so we need to use the lower
@@ -208,17 +221,7 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 
 /*
- * TODO(b/180681346): update for Alder Lake/brya
- *
- * Inductor limits - used for both charger and PP3300 regulator
- *
- * Need to use the lower of the charger IC, PP3300 regulator, and the inductors
- *
- * Charger max recommended temperature 100C, max absolute temperature 125C
- * PP3300 regulator: operating range -40 C to 145 C
- *
- * Inductors: limit of 125c
- * PCB: limit is 80c
+ * TODO(b/203839956): update for Alder Lake/vell
  */
 /*
  * TODO(b/202062363): Remove when clang is fixed.
@@ -239,15 +242,7 @@ __maybe_unused static const struct ec_thermal_config thermal_ambient =
 	THERMAL_AMBIENT;
 
 /*
- * Inductor limits - used for both charger and PP3300 regulator
- *
- * Need to use the lower of the charger IC, PP3300 regulator, and the inductors
- *
- * Charger max recommended temperature 125C, max absolute temperature 150C
- * PP3300 regulator: operating range -40 C to 125 C
- *
- * Inductors: limit of 125c
- * PCB: limit is 80c
+ * TODO(b/203839956): update for Alder Lake/vell
  */
 /*
  * TODO(b/202062363): Remove when clang is fixed.
@@ -268,7 +263,7 @@ __maybe_unused static const struct ec_thermal_config thermal_charger =
 	THERMAL_CHARGER;
 
 /*
- * TODO(b/180681346): update for brya WWAN module
+ * TODO(b/203839956): update for vell WWAN module
  */
 /*
  * TODO(b/202062363): Remove when clang is fixed.
@@ -288,27 +283,54 @@ __maybe_unused static const struct ec_thermal_config thermal_charger =
 __maybe_unused static const struct ec_thermal_config thermal_wwan =
 	THERMAL_WWAN;
 
-struct ec_thermal_config thermal_params[] = {
-	[TEMP_SENSOR_1_DDR_SOC] = THERMAL_CPU,
-	[TEMP_SENSOR_2_AMBIENT] = THERMAL_AMBIENT,
-	[TEMP_SENSOR_3_CHARGER] = THERMAL_CHARGER,
-	[TEMP_SENSOR_4_WWAN] = THERMAL_WWAN,
-};
-BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
+/*
+ * TODO(b/203839956): update for Alder Lake/vell
+ */
+/*
+ * TODO(b/202062363): Remove when clang is fixed.
+ */
 
-static void board_thermals_init(void)
-{
-	if (get_board_id() == 1) {
-		/*
-		 * Board ID 1 only has 3 sensors and the AMBIENT sensor
-		 * ADC pins have been reassigned, so we're down to 2
-		 * sensors that can easily be configured. So, alias the
-		 * AMBIENT sensor ADC channel to the unimplemented ADC
-		 * slots.
-		 */
-		adc_channels[ADC_TEMP_SENSOR_3_CHARGER].input_ch = NPCX_ADC_CH1;
-		adc_channels[ADC_TEMP_SENSOR_4_WWAN].input_ch = NPCX_ADC_CH1;
+#define THERMAL_DDR \
+	{ \
+		.temp_host = { \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(85), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(90), \
+		}, \
+		.temp_host_release = { \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(80), \
+		}, \
+		.temp_fan_off = C_TO_K(35), \
+		.temp_fan_max = C_TO_K(60), \
 	}
-}
+__maybe_unused static const struct ec_thermal_config thermal_ddr =
+	THERMAL_DDR;
 
-DECLARE_HOOK(HOOK_INIT, board_thermals_init, HOOK_PRIO_INIT_CHIPSET);
+/*
+ * TODO(b/203839956): update for Alder Lake/vell
+ */
+/*
+ * TODO(b/202062363): Remove when clang is fixed.
+ */
+
+#define THERMAL_REGULATOR \
+	{ \
+		.temp_host = { \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(120), \
+		}, \
+		.temp_host_release = { \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(90), \
+		}, \
+		.temp_fan_off = C_TO_K(35), \
+		.temp_fan_max = C_TO_K(65), \
+	}
+__maybe_unused static const struct ec_thermal_config thermal_regulator =
+	THERMAL_REGULATOR;
+
+struct ec_thermal_config thermal_params[] = {
+	[TEMP_SENSOR_1_SOC] = thermal_cpu,
+	[TEMP_SENSOR_2_CHARGER]	= thermal_charger,
+	[TEMP_SENSOR_3_WWAN] = thermal_wwan,
+	[TEMP_SENSOR_4_DDR] = thermal_ddr,
+	[TEMP_SENSOR_5_REGULATOR] = thermal_regulator,
+};
