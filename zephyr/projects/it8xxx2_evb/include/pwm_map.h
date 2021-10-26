2158fdc0a824f0a1ae13fe2053bc6f14c4f5758b
Fabio Baltieri <fabiobaltieri@google.com>
2021-10-26 16:56:06 +0000

    zephyr: shim: it8xxx2_evb: drop PWM_CH_WITH_DSLEEP_FLAG
    PWM_CH_WITH_DSLEEP_FLAG does not seem to be used right now in the Zephyr
    port. Dropping the define in the map file, if we need it in the future
    we can use a device tree label directly.
    
    BRANCH=none
    BUG=none
    TEST=zmake configure -b -B ~/build-it8xxx2_evb/ zephyr/projects/it8xxx2_evb
    
    Change-Id: Idadad0656aa172292574857ad59cc863007de836
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3245509
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- zephyr/projects/it8xxx2_evb/include/pwm_map.h
+++ zephyr/projects/it8xxx2_evb/include/pwm_map.h
@@ -12,10 +12,4 @@
 
 #include "pwm/pwm.h"
 
-/*
- * TODO(b/177452529): eliminate the dependency on enum pwm_channel
- * and configure this information directly from the device tree.
- */
-#define PWM_CH_WITH_DSLEEP_FLAG	NAMED_PWM(test1)
-
 #endif /* __ZEPHYR_CHROME_PWM_MAP_H */
