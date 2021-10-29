0172afcaa6be55bbff57fb0eeb98609e2ade2811
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2021-10-29 17:42:04 +0800

    gimble: tune PS8815 parameter for RX
    The parameters were tuned depending on type-c RX test
    
    BRANCH=none
    BUG=b:204719059
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: Iddda0a56317bee39ad7038dbab9b67dc0ecea7dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3251215
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>

--- board/gimble/board.c
+++ board/gimble/board.c
@@ -25,6 +25,7 @@
 #include "lid_switch.h"
 #include "power_button.h"
 #include "power.h"
+#include "ps8xxx.h"
 #include "registers.h"
 #include "switch.h"
 #include "tablet_mode.h"
@@ -135,6 +136,55 @@ static void board_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_SECOND, board_init, HOOK_PRIO_DEFAULT);
+
+__overridable void board_ps8xxx_tcpc_init(int port)
+{
+	int val;
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_10G, &val))
+		CPRINTS("ps8815: fail to read reg 0x%02x",
+			PS8815_REG_APTX_EQ_AT_10G);
+
+	/* APTX2 EQ 23dB, APTX1 EQ 23dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_10G, 0x99))
+		CPRINTS("ps8815: fail to write reg 0x%02x",
+			PS8815_REG_APTX_EQ_AT_10G);
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_10G, &val))
+		CPRINTS("ps8815: fail to read reg 0x%02x",
+			PS8815_REG_RX_EQ_AT_10G);
+
+	/* RX2 EQ 18dB, RX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_10G, 0x64))
+		CPRINTS("ps8815: fail to write reg 0x%02x",
+			PS8815_REG_RX_EQ_AT_10G);
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_5G, &val))
+		CPRINTS("ps8815: fail to read reg 0x%02x",
+			PS8815_REG_APTX_EQ_AT_5G);
+
+	/* APTX2 EQ 16dB, APTX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_5G, 0x44))
+		CPRINTS("ps8815: fail to write reg 0x%02x",
+			PS8815_REG_APTX_EQ_AT_5G);
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_5G, &val))
+		CPRINTS("ps8815: fail to read reg 0x%02x",
+			PS8815_REG_RX_EQ_AT_5G);
+
+	/* RX2 EQ 16dB, RX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_5G, 0x44))
+		CPRINTS("ps8815: fail to write reg 0x%02x",
+			PS8815_REG_RX_EQ_AT_5G);
+}
 
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
