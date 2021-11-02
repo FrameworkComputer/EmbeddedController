5b16d138e9bd2d01cef1f08f74c88923bb1770cf
Aaron Massey <aaronmassey@google.com>
2021-11-02 15:53:25 -0600

    zephyr: emul: sn5s330 add int trip fall registers
    Emulate int trip fall registers used by driver during initialization.
    
    BRANCH=none
    BUG=b:203364783
    TEST=zmake configure --test zephyr/test/drivers (check coverage change)
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I67f51c457f243c3255081c736459a940e440a033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3258522
    Reviewed-by: Yuval Peress <peress@google.com>

--- zephyr/emul/emul_sn5s330.c
+++ zephyr/emul/emul_sn5s330.c
@@ -81,6 +81,12 @@ struct sn5s330_emul_data {
 	uint8_t int_trip_rise_reg2;
 	/** Emulated INT_TRIP_RISE_REG3 register */
 	uint8_t int_trip_rise_reg3;
+	/** Emulated INT_TRIP_FALL_REG1 register */
+	uint8_t int_trip_fall_reg1;
+	/** Emulated INT_TRIP_FALL_REG2 register */
+	uint8_t int_trip_fall_reg2;
+	/** Emulated INT_TRIP_FALL_REG3 register */
+	uint8_t int_trip_fall_reg3;
 };
 
 struct sn5s330_emul_cfg {
@@ -215,6 +221,18 @@ static int sn5s330_emul_read_byte(struct i2c_emul *emul, int reg, uint8_t *val,
 		__ASSERT_NO_MSG(bytes == 0);
 		*val = data->int_trip_rise_reg3;
 		break;
+	case SN5S330_INT_TRIP_FALL_REG1:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg1;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG2:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg2;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG3:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg3;
+		break;
 	default:
 		__ASSERT(false, "Unimplemented Register Access Error on 0x%x",
 			 reg);
@@ -326,6 +344,18 @@ static int sn5s330_emul_write_byte(struct i2c_emul *emul, int reg, uint8_t val,
 	case SN5S330_INT_TRIP_RISE_REG3:
 		__ASSERT_NO_MSG(bytes == 1);
 		data->int_trip_rise_reg3 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG1:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg1 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG2:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg2 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG3:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg3 = val;
 		break;
 	default:
 		__ASSERT(false, "Unimplemented Register Access Error on 0x%x",
