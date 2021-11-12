eaa90ae90bd29ff1dd2365baa5e67a323b399d80
Shon Wang <shon.wang@quanta.corp-partner.google.com>
2021-11-12 14:27:17 +0800

    vell: Initial EC image
    Create the initial EC image for the vell variant by copying the brya
    reference board EC files into a new directory named for the variant.
    
    (Auto-Generated by create_initial_ec_image.sh version 1.5.0).
    
    BUG=b:205908918
    BRANCH=None
    TEST=make BOARD=vell, make buildall
    
    Signed-off-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Change-Id: I4e5bd91e643a8ef1a1ec6cf1dbb191d192433cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3277679
    Reviewed-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Tested-by: Devin Lu <Devin.Lu@quantatw.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>

---
+++ board/vell/build.mk
@@ -0,0 +1,26 @@
+# -*- makefile -*-
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Brya board specific files build
+#
+
+CHIP:=npcx
+CHIP_FAMILY:=npcx9
+CHIP_VARIANT:=npcx9m3f
+BASEBOARD:=brya
+
+board-y=
+board-y+=battery.o
+board-y+=board.o
+board-y+=charger.o
+board-y+=fans.o
+board-y+=fw_config.o
+board-y+=i2c.o
+board-y+=keyboard.o
+board-y+=led.o
+board-y+=pwm.o
+board-y+=sensors.o
+board-y+=tune_mp2964.o
+board-y+=usbc_config.o
---
+++ board/vell/pwm.c
@@ -0,0 +1,71 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+
+#include "compile_time_macros.h"
+#include "hooks.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+
+const struct pwm_t pwm_channels[] = {
+	[PWM_CH_LED2] = {
+		.channel = 0,
+		.flags = PWM_CONFIG_ACTIVE_LOW | PWM_CONFIG_DSLEEP,
+		.freq = 4800,
+	},
+	[PWM_CH_LED3] = {
+		.channel = 1,
+		.flags = PWM_CONFIG_ACTIVE_LOW | PWM_CONFIG_DSLEEP,
+		.freq = 4800,
+	},
+	[PWM_CH_LED1] = {
+		.channel = 2,
+		.flags = PWM_CONFIG_ACTIVE_LOW | PWM_CONFIG_DSLEEP,
+		.freq = 4800,
+	},
+	[PWM_CH_KBLIGHT] = {
+		.channel = 3,
+		.flags = 0,
+		/*
+		 * Set PWM frequency to multiple of 50 Hz and 60 Hz to prevent
+		 * flicker. Higher frequencies consume similar average power to
+		 * lower PWM frequencies, but higher frequencies record a much
+		 * lower maximum power.
+		 */
+		.freq = 2400,
+	},
+	[PWM_CH_FAN] = {
+		.channel = 5,
+		.flags = PWM_CONFIG_OPEN_DRAIN | PWM_CONFIG_DSLEEP,
+		.freq = 1000
+	},
+	[PWM_CH_LED4] = {
+		.channel = 7,
+		.flags = PWM_CONFIG_ACTIVE_LOW | PWM_CONFIG_DSLEEP,
+		.freq = 4800,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+static void board_pwm_init(void)
+{
+	/*
+	 * Turn on all the LED at 50%.
+	 * Turn on the fan at 100%.
+	 */
+	pwm_enable(PWM_CH_LED1, 1);
+	pwm_set_duty(PWM_CH_LED1, 50);
+	pwm_enable(PWM_CH_LED2, 1);
+	pwm_set_duty(PWM_CH_LED2, 50);
+	pwm_enable(PWM_CH_LED3, 1);
+	pwm_set_duty(PWM_CH_LED3, 50);
+	pwm_enable(PWM_CH_LED4, 1);
+	pwm_set_duty(PWM_CH_LED4, 50);
+
+	pwm_enable(PWM_CH_KBLIGHT, 1);
+	pwm_set_duty(PWM_CH_KBLIGHT, 50);
+}
+DECLARE_HOOK(HOOK_INIT, board_pwm_init, HOOK_PRIO_DEFAULT);
