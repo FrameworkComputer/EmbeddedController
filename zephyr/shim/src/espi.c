0b293e2fd74ea5aeccafcd310c077791e68af5e2
Dino Li <Dino.Li@ite.com.tw>
2021-11-16 13:46:55 +0800

    zephyr: espi: move common function to src/espi.c
    These six functions can be common. Because they use the event data
    struct of common espi driver header file to parse host's request.
    
    BRANCH=none
    BUG=none
    TEST=zmake testall
    On adlrvp, verify that the I/O port (60h/64h, 62h/66h) communication
    looks good.
    
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Change-Id: I4ccb264343a6cc3a0145ec1db60a7eec0bd79350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3282974
    Reviewed-by: Yuval Peress <peress@google.com>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/shim/src/espi.c
+++ zephyr/shim/src/espi.c
@@ -561,3 +561,45 @@ int zephyr_shim_setup_espi(void)
 
 	return 0;
 }
+
+bool is_acpi_command(uint32_t data)
+{
+	struct espi_evt_data_acpi *acpi = (struct espi_evt_data_acpi *)&data;
+
+	return acpi->type;
+}
+
+uint32_t get_acpi_value(uint32_t data)
+{
+	struct espi_evt_data_acpi *acpi = (struct espi_evt_data_acpi *)&data;
+
+	return acpi->data;
+}
+
+bool is_8042_ibf(uint32_t data)
+{
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->evt & HOST_KBC_EVT_IBF;
+}
+
+bool is_8042_obe(uint32_t data)
+{
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->evt & HOST_KBC_EVT_OBE;
+}
+
+uint32_t get_8042_type(uint32_t data)
+{
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->type;
+}
+
+uint32_t get_8042_data(uint32_t data)
+{
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->data;
+}
