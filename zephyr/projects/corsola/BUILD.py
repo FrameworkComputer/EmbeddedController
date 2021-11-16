5800bf43a1d741eae7bfc5c13c25be912008214a
Eric Yilun Lin <yllin@chromium.org>
2021-11-16 14:46:06 +0800

    zephyr: krabby: restructure folder layout
    To follow the guidelines in go/zephyr-variant.
    
    next steps:
     - restructure kingler layout
     - move common configs into prj.conf
     - move source files from cros-ec/krabby to zephyr
    
    BUG=b:206553789
    TEST=zmake -b krabby; zmake -b kingler
    BRANCH=main
    
    Change-Id: I145fcb90cca360f733ecad41d0fcc82283563634
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3289581
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>

---
+++ zephyr/projects/corsola/BUILD.py
@@ -0,0 +1,37 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Default chip is it8xxx2, some variants will use NPCX9X.
+
+
+def register_corsola_project(
+    project_name,
+    chip="it8xxx2",
+    extra_dts_overlays=(),
+    extra_kconfig_files=(),
+):
+    register_func = register_binman_project
+    if chip.startswith("npcx9"):
+        register_func = register_npcx_project
+
+    register_func(
+        project_name=project_name,
+        zephyr_board=chip,
+        dts_overlays=[*extra_dts_overlays],
+        kconfig_files=[here / "prj.conf", *extra_kconfig_files],
+    )
+
+
+register_corsola_project(
+    "krabby",
+    extra_dts_overlays=[
+        here / "adc_krabby.dts",
+        here / "battery_krabby.dts",
+        here / "gpio_krabby.dts",
+        here / "i2c_krabby.dts",
+        here / "motionsense_krabby.dts",
+        here / "pwm_krabby.dts",
+    ],
+    extra_kconfig_files=[here / "prj_krabby.conf"],
+)
