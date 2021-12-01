5dac04933a787dd0c7c2ade40459c7ade563cb32
Andrew McRae <amcrae@google.com>
2021-12-01 17:44:59 +1100

    nissa: Initial zephyr config for nivviks
    Initial EC Zephyr config for Nissa/Nivviks.
    Not all GPIOs have been defined yet.
    V2 - reworked to use a single project directory.
    V3 - renaming.
    
    BUG=b:201000681
    TEST=make -j buildall; cd zephyr; zmake configure -b nivviks
    BRANCH=none
    
    Cq-Depend: chromium:3312397
    Change-Id: I2b3f7ee77dc8365fac10b8fb7a4eec65d827bed1
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3310024
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ zephyr/projects/nissa/BUILD.py
@@ -0,0 +1,34 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Nivviks has NPCX993F, Nereid has ITE81302
+
+
+def register_nissa_project(
+    project_name,
+    chip="it8xxx2",
+    extra_dts_overlays=(),
+    extra_kconfig_files=(),
+):
+    register_func = register_binman_project
+    if chip.startswith("npcx9"):
+        register_func = register_npcx_project
+
+    register_func(
+        project_name=project_name,
+        zephyr_board=chip,
+        dts_overlays=[*extra_dts_overlays],
+        kconfig_files=[here / "prj.conf", *extra_kconfig_files],
+    )
+
+
+register_nissa_project(
+    project_name="nivviks",
+    chip="npcx9",
+    extra_dts_overlays=[
+        here / "gpio_nivviks.dts",
+        here / "i2c_nivviks.dts",
+    ],
+    extra_kconfig_files=[here / "prj_nivviks.conf"],
+)
---
+++ zephyr/projects/nissa/include/gpio_map.h
@@ -0,0 +1,48 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_GPIO_MAP_H
+#define __ZEPHYR_GPIO_MAP_H
+
+#include <devicetree.h>
+#include <gpio_signal.h>
+
+/*
+ * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
+ *
+ * Each GPIO_INT requires three parameters:
+ *   gpio_signal - The enum gpio_signal for the interrupt gpio
+ *   interrupt_flags - The interrupt-related flags (e.g. GPIO_INT_EDGE_BOTH)
+ *   handler - The platform/ec interrupt handler.
+ *
+ * Ensure that this files includes all necessary headers to declare all
+ * referenced handler functions.
+ *
+ * For example, one could use the follow definition:
+ * #define EC_CROS_GPIO_INTERRUPTS \
+ *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
+ */
+/* Helper macros for generating CROS_EC_GPIO_INTERRUPTS */
+#ifdef CONFIG_PLATFORM_EC_POWERSEQ
+#define POWER_SIGNAL_INT(gpio, edge) \
+	GPIO_INT(gpio, edge, power_signal_interrupt)
+#define AP_PROCHOT_INT(gpio, edge) \
+	GPIO_INT(gpio, edge, throttle_ap_prochot_input_interrupt)
+#else
+#define POWER_SIGNAL_INT(gpio, edge)
+#define AP_PROCHOT_INT(gpio, edge)
+#endif
+
+
+#define EC_CROS_GPIO_INTERRUPTS                                           \
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
+	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
+		 power_button_interrupt)                                  \
+	POWER_SIGNAL_INT(GPIO_SLP_SUS_L, GPIO_INT_EDGE_BOTH)              \
+	POWER_SIGNAL_INT(GPIO_PG_EC_DSW_PWROK, GPIO_INT_EDGE_BOTH)        \
+	POWER_SIGNAL_INT(GPIO_PG_EC_RSMRST_ODL, GPIO_INT_EDGE_BOTH)       \
+	POWER_SIGNAL_INT(GPIO_PG_EC_ALL_SYS_PWRGD, GPIO_INT_EDGE_BOTH)    \
+	AP_PROCHOT_INT(GPIO_EC_PROCHOT_ODL, GPIO_INT_EDGE_BOTH)
+#endif /* __ZEPHYR_GPIO_MAP_H */
