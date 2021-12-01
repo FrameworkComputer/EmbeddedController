2dd48d46985ad2ee76d5e6a324893c782a95e889
Jack Rosenthal <jrosenth@chromium.org>
2021-11-30 18:23:59 -0700

    zephyr: Split the guybrush build for skyrim
    Rename the guybrush project directory to skyrim.
    
    Guybrush becomes a variant of skyrim that we plan to remove once
    skyrim hardware is readily available.
    
    Initially I have it set up so that project-specific DTS customizations
    can be made in {project_name}.dts, and project-specific Kconfig
    customizations can be made in prj_{project_name}.conf.  Right now
    these files are both empty for both guybrush and skyrim, but the idea
    is that we start to move things into these files that should be
    project-specific in the coming weeks.
    
    BUG=b:208515128
    BRANCH=none
    TEST=compile for both skyrim and guybrush
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I3c08cf89b7788b4a56fff9fdfa40e8f8a1347cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3309827
    Commit-Queue: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>

---
+++ zephyr/projects/skyrim/BUILD.py
@@ -0,0 +1,29 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+def register_variant(project_name):
+    register_npcx_project(
+        project_name=project_name,
+        zephyr_board="npcx9",
+        dts_overlays=[
+            # Common to all projects.
+            here / "battery.dts",
+            here / "fan.dts",
+            here / "gpio.dts",
+            here / "i2c.dts",
+            here / "pwm.dts",
+            # Project-specific DTS customizations.
+            here / f"{project_name}.dts",
+        ],
+        kconfig_files=[
+            here / f"prj_{project_name}.conf",
+        ],
+    )
+
+
+register_variant(project_name="skyrim")
+
+# TODO: Deprecate guybrush build after skyrim hardware is readily available.
+register_variant(project_name="guybrush")
