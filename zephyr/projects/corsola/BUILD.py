828807a9ff3a879c0aa60a49c08ce1522ca726d6
Andrew McRae <amcrae@google.com>
2021-12-06 12:23:02 +1100

    it8xxx2: Move non-core config to per-project config
    Move non-base configuration from default it8xxx2 configuration to
    project level configuration:
     - keyboard enabling
     - keyboard scanning
     - wake mask
     - cros kb shim
    
    BUG=b:209324248
    TEST=zmake configure -b {krabby,kinger}
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I9db3aab59f68602afdf5274a7eb4c9a77e778bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3316164
    Reviewed-by: Sam Hurst <shurst@google.com>

--- zephyr/projects/corsola/BUILD.py
+++ zephyr/projects/corsola/BUILD.py
@@ -30,6 +30,7 @@ register_corsola_project(
         here / "battery_krabby.dts",
         here / "gpio_krabby.dts",
         here / "i2c_krabby.dts",
+        here / "kb_krabby.dts",
         here / "motionsense_krabby.dts",
         here / "pwm_krabby.dts",
     ],
