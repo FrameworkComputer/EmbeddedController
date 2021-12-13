aeecb768071a57d38a5e0c64670178ebb5eefeac
Kshitiz Godara <kgodara@qualcomm.corp-partner.google.com>
2021-12-13 18:25:19 +0530

    zephyr: hoglin: Board creation
    Created hoglin board using herobrine as reference board
    
    BRANCH=None
    BUG=None
    TEST=Tested image on hoglin board and board is booting up fine.
    
    Change-Id: I81a8b97d5d4bf7c1e40267dd399dc4a8060ce2b4
    Signed-off-by: Kshitiz Godara <kgodara@qualcomm.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3334736
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>

--- zephyr/projects/herobrine/BUILD.py
+++ zephyr/projects/herobrine/BUILD.py
@@ -11,14 +11,11 @@ def register_variant(project_name, extra_dts_overlays=(), extra_kconfig_files=()
             # Common to all projects.
             here / "adc.dts",
             here / "display.dts",
-            here / "gpio.dts",
             here / "common.dts",
             here / "i2c.dts",
             here / "interrupts.dts",
             here / "keyboard.dts",
-            here / "motionsense.dts",
             here / "pwm.dts",
-            here / "switchcap.dts",
             # Project-specific DTS customization.
             *extra_dts_overlays,
         ],
@@ -35,6 +32,9 @@ register_variant(
     project_name="herobrine",
     extra_dts_overlays=[
         here / "battery_herobrine.dts",
+        here / "gpio.dts",
+        here / "motionsense.dts",
+        here / "switchcap.dts",
         here / "usbc_herobrine.dts",
     ],
     extra_kconfig_files=[here / "prj_herobrine.conf"],
@@ -45,7 +45,10 @@ register_variant(
     project_name="hoglin",
     extra_dts_overlays=[
         here / "battery_herobrine.dts",
-        here / "usbc_herobrine.dts",
+        here / "gpio_hoglin.dts",
+        here / "motionsense_hoglin.dts",
+        here / "switchcap_hoglin.dts",
+        here / "usbc_hoglin.dts",
     ],
     extra_kconfig_files=[here / "prj_hoglin.conf"],
 )
@@ -55,6 +58,9 @@ register_variant(
     project_name="villager",
     extra_dts_overlays=[
         here / "battery_villager.dts",
+        here / "gpio.dts",
+        here / "motionsense.dts",
+        here / "switchcap.dts",
         here / "usbc_villager.dts",
     ],
     extra_kconfig_files=[here / "prj_villager.conf"],
