9cae34f1ccc1eb9796a0fb3acfd3c81d934c8a52
Devin Lu <Devin.Lu@quantatw.com>
2021-12-30 11:30:30 +0800

    anahera: Raise C1 port i2c speed to 1Mhz
    BUG=b:207802700
    BRANCH=none
    TEST=On Anahera, Make sure the C1 port is charging.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: Ie5c53c865b6dc8046e594721b5a1790b5fc7e507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3361068
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/anahera/i2c.c
+++ board/anahera/i2c.c
@@ -38,7 +38,7 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C4 C1 TCPC */
 		.name = "tcpc1",
 		.port = I2C_PORT_USB_C1_TCPC,
-		.kbps = 400,
+		.kbps = 1000,
 		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
 		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
 	},
@@ -54,7 +54,7 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C6 */
 		.name = "ppc1,retimer1",
 		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 400,
+		.kbps = 1000,
 		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
 		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
 	},
