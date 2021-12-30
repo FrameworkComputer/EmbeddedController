8eb09176c577ee2231b17d9c67c8cefe8b6f491d
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-12-30 13:24:47 +0800

    agah: update gpio
    Update EC gpio follow schematic.
    
    BUG=b:210970640
    BRANCH=none
    TEST=make BOARD=agah
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: If1c1ff66c8a16d9f395fb9662006768bc4cc0495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3361699
    Reviewed-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/agah/board.c
+++ board/agah/board.c
@@ -37,7 +37,7 @@ __override void board_cbi_init(void)
 static void board_chipset_resume(void)
 {
 	/* Allow keyboard backlight to be enabled */
-	gpio_set_level(GPIO_EC_KB_BL_EN_L, 0);
+	gpio_set_level(GPIO_EC_KB_BL_EN, 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 
@@ -45,6 +45,6 @@ DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 static void board_chipset_suspend(void)
 {
 	/* Turn off the keyboard backlight if it's on. */
-	gpio_set_level(GPIO_EC_KB_BL_EN_L, 1);
+	gpio_set_level(GPIO_EC_KB_BL_EN, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
