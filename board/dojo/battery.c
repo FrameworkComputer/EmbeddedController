7f399abb0bc6d4e05f8a76d07766ed3352287a1f
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2022-01-03 15:08:10 +0800

    dojo: Update battery, gpio configs and rename all comments
    Update battery, gpio configs and rename all comments for dojo.
    For battery config, the battery and gauge specs are attached in the
    bug tracker. For gpio config, since this is the new created board
    variant, the BOARD_CONFIGs should be removed.
    
    BUG=b:213000777
    BRANCH=none
    TEST=make sure all battery charging, discharging, cut-off work.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: I4c426c86740125520a9b163872386dfb9535607d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3364059
    Reviewed-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- board/dojo/battery.c
+++ board/dojo/battery.c
@@ -8,119 +8,151 @@
 #include "battery_smart.h"
 
 const struct board_batt_params board_battery_info[] = {
-	/* Panasonic AP16L5J Battery Information */
-	[BATTERY_PANASONIC_AC16L5J] = {
+	/* DynaPack CosMX Battery Information */
+	[BATTERY_DYNAPACK_COS] = {
 		.fuel_gauge = {
-			.manuf_name = "PANASONIC",
-			.device_name = "AP16L5J",
+			.manuf_name = "333-2C-DA-A",
 			.ship_mode = {
-				.reg_addr = 0x3A,
-				.reg_data = { 0xC574, 0xC574 },
-			},
-			.fet = {
-				.reg_addr = 0x0,
-				.reg_mask = 0x4000,
-				.disconnect_val = 0x0,
-			}
-		},
-		.batt_info = {
-			.voltage_max		= 8800,
-			.voltage_normal		= 7700,
-			.voltage_min		= 6000,
-			.precharge_current	= 256,
-			.start_charging_min_c	= 0,
-			.start_charging_max_c	= 50,
-			.charging_min_c		= 0,
-			.charging_max_c		= 60,
-			.discharging_min_c	= -20,
-			.discharging_max_c	= 75,
-		},
-	},
-	[BATTERY_PANASONIC_AC16L5J_KT00205009] = {
-		.fuel_gauge = {
-			.manuf_name = "PANASONIC KT00205009",
-			.device_name = "AP16L5J",
-			.ship_mode = {
-				.reg_addr = 0x3A,
-				.reg_data = { 0xC574, 0xC574 },
-			},
-			.fet = {
-				.reg_addr = 0x0,
-				.reg_mask = 0x4000,
-				.disconnect_val = 0x0,
-			}
-		},
-		.batt_info = {
-			.voltage_max		= 8800,
-			.voltage_normal		= 7700,
-			.voltage_min		= 6000,
-			.precharge_current	= 256,
-			.start_charging_min_c	= 0,
-			.start_charging_max_c	= 50,
-			.charging_min_c		= 0,
-			.charging_max_c		= 60,
-			.discharging_min_c	= -20,
-			.discharging_max_c	= 75,
-		},
-	},
-	/* AP16L8J */
-	[BATTERY_AP16L8J] = {
-		.fuel_gauge = {
-			.manuf_name = "LGC KT0020G010",
-			.device_name = "AP16L8J",
-			.ship_mode = {
-				.reg_addr = 0x3A,
-				.reg_data = { 0xC574, 0xC574 },
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
 			},
 			.fet = {
 				.mfgacc_support = 1,
 				.reg_addr = 0x0,
-				.reg_mask = 0x0002,
+				.reg_mask = 0x0006,
 				.disconnect_val = 0x0,
-			}
+			},
 		},
 		.batt_info = {
-			.voltage_max            = 8700,
-			.voltage_normal         = 7500, /* mV */
-			.voltage_min            = 6000, /* mV */
-			.precharge_current      = 256,  /* mA */
-			.start_charging_min_c   = 0,
-			.start_charging_max_c   = 50,
-			.charging_min_c         = 0,
-			.charging_max_c         = 60,
-			.discharging_min_c      = -20,
-			.discharging_max_c      = 75,
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
 		},
 	},
-	/* LGC AP18C8K Battery Information */
-	[BATTERY_LGC_AP18C8K] = {
+
+	/* DynaPack ATL Battery Information */
+	[BATTERY_DYNAPACK_ATL] = {
 		.fuel_gauge = {
-			.manuf_name = "LGC KT0030G020",
-			.device_name = "AP18C8K",
+			.manuf_name = "333-27-DA-A",
 			.ship_mode = {
-				.reg_addr = 0x3A,
-				.reg_data = { 0xC574, 0xC574 },
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
 			},
 			.fet = {
-				.reg_addr = 0x43,
-				.reg_mask = 0x0001,
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0006,
 				.disconnect_val = 0x0,
 			},
 		},
 		.batt_info = {
-			.voltage_max		= 13050,
-			.voltage_normal		= 11250,
-			.voltage_min		= 9000,
-			.precharge_current	= 256,
-			.start_charging_min_c	= 0,
-			.start_charging_max_c	= 50,
-			.charging_min_c		= 0,
-			.charging_max_c		= 60,
-			.discharging_min_c	= -20,
-			.discharging_max_c	= 75,
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
+		},
+	},
+
+	/* Simplo CosMX Battery Information */
+	[BATTERY_SIMPLO_COS] = {
+		.fuel_gauge = {
+			.manuf_name = "333-1C-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0006,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
+		},
+	},
+
+	/* Simplo HIGHPOWER Battery Information */
+	[BATTERY_SIMPLO_HIGHPOWER] = {
+		.fuel_gauge = {
+			.manuf_name = "333-1D-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0006,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
+		},
+	},
+
+	/* CosMX B00C4473A9D0002 Battery Information */
+	[BATTERY_COS] = {
+		.fuel_gauge = {
+			.manuf_name = "333-AC-DA-A",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.mfgacc_support = 1,
+				.reg_addr = 0x0,
+				.reg_mask = 0x0006,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max = 8800,	/* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,	/* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = -10,
+			.discharging_max_c = 60,
 		},
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_PANASONIC_AC16L5J;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_DYNAPACK_COS;
--- board/dojo/board.h
+++ board/dojo/board.h
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-/* Cherry board configuration */
+/* Dojo board configuration */
 
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
@@ -85,10 +85,11 @@
 #include "registers.h"
 
 enum battery_type {
-	BATTERY_PANASONIC_AC16L5J,
-	BATTERY_PANASONIC_AC16L5J_KT00205009,
-	BATTERY_AP16L8J,
-	BATTERY_LGC_AP18C8K,
+	BATTERY_DYNAPACK_COS,
+	BATTERY_DYNAPACK_ATL,
+	BATTERY_SIMPLO_COS,
+	BATTERY_SIMPLO_HIGHPOWER,
+	BATTERY_COS,
 	BATTERY_TYPE_COUNT,
 };
 
