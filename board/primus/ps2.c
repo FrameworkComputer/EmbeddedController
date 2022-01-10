ab207f02263624b765508a17a7fea36071770b8b
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-01-10 16:12:46 +0800

    primus: fix Goodix touchpad initialize failed
    Goodix touchpad AVDD need to pull low to 0V when poweroff.
    Setting PS2 module in GPIO.inc will let AVDD have 0.9V offset.
    So we need to enable PS2 module later than PLTRST# to avoid the 0.9V
    offset.
    
    BUG=b:214150759
    BRANCH=none
    TEST=make -j BOARD=primus
    TEST=verified by Goodix
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I49869d26262948f7f7242ebe494eef86e459a42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3376688
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- board/primus/ps2.c
+++ board/primus/ps2.c
@@ -4,11 +4,29 @@
  */
 
 #include "hooks.h"
+#include "i8042_protocol.h"
 #include "keyboard_8042.h"
 #include "ps2_chip.h"
 
 void send_aux_data_to_device(uint8_t data)
 {
+	if (data == I8042_CMD_RESET_DIS) {
+		/*
+		 * EC will receive I8042_CMD_RESET_DIS when warm reboot,
+		 * set GPIO62/ GPIO63 back to GPIO and pull low.
+		 */
+		gpio_set_flags(GPIO_EC_PS2_SCL_TPAD, GPIO_ODR_LOW);
+		gpio_set_flags(GPIO_EC_PS2_SDA_TPAD, GPIO_ODR_LOW);
+		gpio_set_alternate_function(GPIO_PORT_6,
+			BIT(2) | BIT(3), GPIO_ALT_FUNC_NONE);
+	} else if (data == I8042_CMD_GETID) {
+		/*
+		 * In normal boot, when we get I8042_CMD_GETID command,
+		 * enable the PS2 module.
+		 */
+		gpio_set_alternate_function(GPIO_PORT_6,
+			BIT(2) | BIT(3), GPIO_ALT_FUNC_DEFAULT);
+	}
 	ps2_transmit_byte(NPCX_PS2_CH1, data);
 }
 
