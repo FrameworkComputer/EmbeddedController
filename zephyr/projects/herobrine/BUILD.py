29d3413208972eac61e6dec33c9c4bf400f7e836
Wai-Hong Tam <waihong@google.com>
2022-01-14 15:36:50 -0800

    Reland "herobrine: Add a wrapper to create new variant projects"
    This is a reland of 7da11090fe6b612eba73cdd0fe4e0d6b0818f5a5
    
    Original change's description:
    > herobrine: Add a wrapper to create new variant projects
    >
    > Add a wrapper function to help creating new project easily. So far
    > it just builds the existing herobrine_npcx9 project.
    >
    > BRANCH=None
    > BUG=b:214613810
    > TEST=Built the herobrine_npcx9 project and checked it boot correctly
    > on the hardware.
    >
    > Change-Id: I28eef726108aa3f9de9fd266f6b843222ab42f8c
    > Signed-off-by: Wai-Hong Tam <waihong@google.com>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3390640
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    
    Bug: b:214613810
    Change-Id: I64aa4c93917ae3a2207e3ab53a8555b31adf7585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3417016
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>
    Tested-by: Wai-Hong Tam <waihong@google.com>
    Auto-Submit: Wai-Hong Tam <waihong@google.com>

--- zephyr/projects/herobrine/BUILD.py
+++ zephyr/projects/herobrine/BUILD.py
@@ -2,15 +2,32 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-register_npcx_project(
+
+def register_variant(project_name, extra_dts_overlays=(), extra_kconfig_files=()):
+    register_npcx_project(
+        project_name=project_name,
+        zephyr_board="herobrine_npcx9",
+        dts_overlays=[
+            # Common to all projects.
+            here / "battery.dts",
+            here / "gpio.dts",
+            here / "i2c.dts",
+            here / "motionsense.dts",
+            here / "switchcap.dts",
+            here / "usbc.dts",
+            # Project-specific DTS customization.
+            *extra_dts_overlays,
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "prj.conf",
+            # Project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
+
+
+register_variant(
     project_name="herobrine_npcx9",
-    zephyr_board="herobrine_npcx9",
-    dts_overlays=[
-        "gpio.dts",
-        "battery.dts",
-        "i2c.dts",
-        "motionsense.dts",
-        "switchcap.dts",
-        "usbc.dts",
-    ],
+    extra_kconfig_files=[here / "prj_herobrine_npcx9.conf"],
 )
