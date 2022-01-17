021f00a1b2dbedfb0d3c3565423cdb21276c4c7f
Teddy Shih <teddyshih@ami.corp-partner.google.com>
2022-01-17 15:35:24 +0800

    USB MUX: Search SPI address through anx7447_i2c_addrs_flags array
    In order to use anx7447 as a MUX only in Beadrix project, search
    corresponding anx7447 SPI address according to specified TCPC address.
    
    BRANCH=main
    BUG=b:204882915
    TEST=on beadrix, validate daughterboard PD C1 mux ANX3447QN-AC-R and
    retimer NB7VPQ904M are working good on C1 Data and C1 Display Port(DP).
    
    Signed-off-by: Teddy Shih <teddyshih@ami.corp-partner.google.com>
    Change-Id: Id3800508c52ee1dc0e9d1621ae25828afa115260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3392106
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- driver/tcpm/anx7447.c
+++ driver/tcpm/anx7447.c
@@ -508,7 +508,28 @@ void anx7447_tcpc_clear_hpd_status(int port)
 static int anx7447_mux_init(const struct usb_mux *me)
 {
 	int port = me->usb_port;
+	int i;
 	bool unused;
+
+	/*
+	 * find corresponding anx7447 SPI address according to
+	 * specified TCPC address
+	 */
+	for (i = 0; i < ARRAY_SIZE(anx7447_i2c_addrs_flags); i++) {
+		if (I2C_STRIP_FLAGS(tcpc_config[port].i2c_info.addr_flags) ==
+		    I2C_STRIP_FLAGS(
+			    anx7447_i2c_addrs_flags[i].tcpc_addr_flags)) {
+			anx[port].i2c_addr_flags =
+				anx7447_i2c_addrs_flags[i].spi_addr_flags;
+			break;
+		}
+	}
+	if (!I2C_STRIP_FLAGS(anx[port].i2c_addr_flags)) {
+		ccprintf("TCPC I2C addr 0x%x is invalid for ANX7447\n",
+			 I2C_STRIP_FLAGS(tcpc_config[port]
+				      .i2c_info.addr_flags));
+		return EC_ERROR_UNKNOWN;
+	}
 
 	ASSERT(port < CONFIG_USB_PD_PORT_MAX_COUNT);
 
