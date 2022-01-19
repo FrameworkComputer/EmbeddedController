3dc9ff4cff22b9d4375cd392242c61d5679795c4
Josh Tsai <josh_tsai@compal.com>
2022-01-19 13:22:54 +0800

    Modified the ALS driver
    Signed-off-by: Josh Tsai <josh_tsai@compal.com>

--- driver/als_cm32183.c
+++ driver/als_cm32183.c
@@ -2,66 +2,93 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * CAPELLA CM32181E light sensor driver
+ * CAPELLA CM32183 light sensor driver
  */
+
 #include "common.h"
 #include "driver/als_cm32183.h"
 #include "i2c.h"
 #include "accelgyro.h"
 #include "math_util.h"
-#define CM32183_GET_DATA(_s)	((struct cm32183_drv_data *)(_s)->drv_data)
+
 /*
- * Read data from CM32183 light sensor, and transfer unit into lux.
+ * Read CM32183 light sensor data.
  */
-static int cm32183_read_lux(const struct motion_sensor_t *s, intv3_t v)
+int cm32183_read_lux(int *lux)
 {
-	struct cm32183_drv_data *drv_data = CM32183_GET_DATA(s);
 	int ret;
 	int data;
 
-	ret = i2c_read16(s->port, s->i2c_spi_addr_flags,
+	ret = i2c_read16(I2C_PORT_SENSOR, CM32183_I2C_ADDR,
 		CM32183_REG_ALS_RESULT, &data);
+
 	if (ret)
 		return ret;
+
 	/*
 	 * lux = data * 0.016
 	 */
-	data = (data * 16)/1000;
-	data += drv_data->offset;
-	if (data < 0)
-		data = 1;
-	v[0] = data;
+	*lux = (data * 16)/1000;
+
+	return EC_SUCCESS;
+}
+
+/*
+ * Read data from CM32183 light sensor, and transfer unit into lux.
+ */
+static int cm32183_read(const struct motion_sensor_t *s, intv3_t v)
+{
+	struct cm32183_drv_data *drv_data = CM32183_GET_DATA(s);
+	int ret;
+	int lux_data;
+
+	ret = cm32183_read_lux(&lux_data);
+
+	if (ret)
+		return ret;
+
+	lux_data += drv_data->offset;
+
+	v[0] = lux_data;
 	v[1] = 0;
 	v[2] = 0;
+
 	/*
 	 * Return an error when nothing change to prevent filling the
 	 * fifo with useless data.
 	 */
 	if (v[0] == drv_data->last_value)
 		return EC_ERROR_UNCHANGED;
+
 	drv_data->last_value = v[0];
 	return EC_SUCCESS;
 }
+
 static int cm32183_set_range(const struct motion_sensor_t *s, int range,
 			     int rnd)
 {
 	return EC_SUCCESS;
 }
+
 static int cm32183_set_data_rate(const struct motion_sensor_t *s,
 				int rate, int roundup)
 {
 	CM32183_GET_DATA(s)->rate = rate;
 	return EC_SUCCESS;
 }
+
 static int cm32183_get_data_rate(const struct motion_sensor_t *s)
 {
 	return CM32183_GET_DATA(s)->rate;
 }
+
 static int cm32183_set_offset(const struct motion_sensor_t *s,
 			const int16_t *offset, int16_t temp)
 {
+	/* TODO: check calibration method */
 	return EC_SUCCESS;
 }
+
 static int cm32183_get_offset(const struct motion_sensor_t *s,
 			int16_t *offset, int16_t *temp)
 {
@@ -72,26 +99,32 @@ static int cm32183_get_offset(const struct motion_sensor_t *s,
 #ifdef CONFIG_ACCEL_INTERRUPTS
 static int cm32183_irq_handler(struct motion_sensor_t *s, uint32_t *event)
 {
+	struct cm32183_drv_data *drv_data = CM32183_GET_DATA(s);
 	int ret;
-	int data;
+	int lux_data;
 
-	ret = i2c_read16(s->port, s->i2c_spi_addr_flags,
-		CM32183_REG_ALS_RESULT, &data);
+	ret = cm32183_read_lux(&lux_data);
+
 	if (ret)
 		return ret;
-	/*
-	 * lux = data * 0.016
-	 */
-	data = (data * 16)/1000;
-	s->xyz[0] = data;
+
+	lux_data += drv_data->offset;
+
+	s->xyz[0] = lux_data;
 	s->xyz[1] = 0;
 	s->xyz[2] = 0;
+
+	if (lux_data == drv_data->last_value)
+		return EC_ERROR_UNCHANGED;
+
+	drv_data->last_value = lux_data;
+
 	return EC_SUCCESS;
 }
 #endif
 
 /**
- * Initialise CM32181E light sensor.
+ * Initialise CM32183 light sensor.
  */
 static int cm32183_init(const struct motion_sensor_t *s)
 {
@@ -99,24 +132,29 @@ static int cm32183_init(const struct motion_sensor_t *s)
 	int data;
 
 	ret = i2c_write16(s->port, s->i2c_spi_addr_flags,
-		CM32183_REG_CONFIGURE, 0x0004);
+		CM32183_REG_CONFIGURE, CM32183_REG_CONFIGURE_CH_EN);
+
 	if (ret)
 		return ret;
+
 	ret = i2c_read16(s->port, s->i2c_spi_addr_flags,
 		CM32183_REG_ALS_RESULT, &data);
+
 	if (ret)
 		return ret;
-	return EC_SUCCESS;
+
+	return sensor_init_done(s);
 }
+
 const struct accelgyro_drv cm32183_drv = {
 	.init = cm32183_init,
-	.read = cm32183_read_lux,
+	.read = cm32183_read,
 	.set_range = cm32183_set_range,
 	.set_offset = cm32183_set_offset,
 	.get_offset = cm32183_get_offset,
 	.set_data_rate = cm32183_set_data_rate,
 	.get_data_rate = cm32183_get_data_rate,
-#ifdef CONFIG_ACCEL_INTERRUPTS
+	#ifdef CONFIG_ACCEL_INTERRUPTS
 	.irq_handler = cm32183_irq_handler,
-#endif
+	#endif
 };
