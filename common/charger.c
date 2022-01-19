c11da2a0f9bb289077bcc42198ebe49a637cce9e
Andrew McRae <amcrae@google.com>
2022-01-20 10:40:54 +1100

    ocpc: Do not report invalid charger if CHARGE_PORT_NONE
    Do not report an invalid charger if there is no charger.
    
    BUG=b:215253447
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I23b1d7861065c32b438e43198c2fa1cc2a515f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3403358
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>

--- common/charger.c
+++ common/charger.c
@@ -515,7 +515,11 @@ enum ec_error_list charger_get_vbus_voltage(int port, int *voltage)
 enum ec_error_list charger_set_input_current_limit(int chgnum,
 						   int input_current)
 {
-	if ((chgnum < 0) || (chgnum >= board_get_charger_chip_count())) {
+	/* Note: may be called with CHARGE_PORT_NONE regularly */
+	if (chgnum < 0)
+		return EC_ERROR_INVAL;
+
+	if (chgnum >= board_get_charger_chip_count()) {
 		CPRINTS("%s(%d) Invalid charger!", __func__, chgnum);
 		return EC_ERROR_INVAL;
 	}
