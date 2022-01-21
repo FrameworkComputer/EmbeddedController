431b9efa172a759e0908b639dcc763ff22fa301a
Aaron Massey <aaronmassey@google.com>
2022-01-21 13:18:16 -0700

    zephyr: test: verify usb pd host info host command
    Verify the zephyr espi driver can receive host EC_CMD_USB_PD_POWER_INFO
    host command.
    
    BRANCH=none
    BUG=b:215389861
    TEST=zmake configure --test test-drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Ib947f5c2f335629f781ae63066110466640b661e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3408002
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- zephyr/test/drivers/src/espi.c
+++ zephyr/test/drivers/src/espi.c
@@ -9,6 +9,8 @@
 #include "ec_commands.h"
 #include "host_command.h"
 
+#define PORT 0
+
 static void test_host_command_get_protocol_info(void)
 {
 	struct ec_response_get_protocol_info response;
@@ -27,9 +29,25 @@ static void test_host_command_get_protocol_info(void)
 	zassert_equal(response.flags, 0, NULL);
 }
 
+static void test_host_command_usb_pd_power_info(void)
+{
+	/* Only test we've enabled the command */
+	struct ec_response_usb_pd_power_info response;
+	struct ec_params_usb_pd_power_info params = { .port = PORT };
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_RESPONSE(
+		EC_CMD_USB_PD_POWER_INFO, 0, response);
+
+	args.params = &params;
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+	zassert_equal(args.response_size, sizeof(response), NULL);
+}
+
 void test_suite_espi(void)
 {
 	ztest_test_suite(espi,
+			 ztest_user_unit_test(
+				 test_host_command_usb_pd_power_info),
 			 ztest_user_unit_test(
 				 test_host_command_get_protocol_info));
 	ztest_run_test_suite(espi);
