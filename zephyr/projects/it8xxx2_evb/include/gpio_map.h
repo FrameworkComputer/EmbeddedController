95047508ab609f66e3306aae77a0c33cd80101ac
Andrew McRae <amcrae@google.com>
2022-01-23 16:05:07 +1100

    it8xxx2_evb: Remove EC_CROS_GPIO_INTERRUPTS
    Remove EC_CROS_GPIO_INTERRUPTS
    
    BUG=b:215258914
    TEST=zmake configure -b it8xxx2_evb
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I8f4c6fcb867f9720522c391f5a23e8c808474d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3413595
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- zephyr/projects/it8xxx2_evb/include/gpio_map.h
+++ zephyr/projects/it8xxx2_evb/include/gpio_map.h
@@ -12,26 +12,4 @@
 #define GPIO_ENTERING_RW	GPIO_UNIMPLEMENTED
 #define GPIO_WP_L		GPIO_UNIMPLEMENTED
 
-/*
- * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
- *
- * Each GPIO_INT requires three parameters:
- *   gpio_signal - The enum gpio_signal for the interrupt gpio
- *   interrupt_flags - The interrupt-related flags (e.g. GPIO_INT_EDGE_BOTH)
- *   handler - The platform/ec interrupt handler.
- *
- * Ensure that this files includes all necessary headers to declare all
- * referenced handler functions.
- *
- * For example, one could use the follow definition:
- * #define EC_CROS_GPIO_INTERRUPTS \
- *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
- */
-#define EC_CROS_GPIO_INTERRUPTS						\
-	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)	\
-	GPIO_INT(GPIO_POWER_BUTTON_L,					\
-		 GPIO_INT_EDGE_BOTH, power_button_interrupt)		\
-	GPIO_INT(GPIO_SPI0_CS,						\
-		 GPIO_INT_EDGE_FALLING, spi_event)
-
 #endif /* __ZEPHYR_GPIO_MAP_H */
