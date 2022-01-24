3b170e47f5e29e561629393eaaf1e412612ac880
Wai-Hong Tam <waihong@google.com>
2022-01-24 22:44:57 +0000

    Revert "herobrine: Move the ADC dt nodes from board to project"
    This reverts commit 5340a3299dd6193c5821fbc57d3fb1852137cf01.
    
    Reason for revert: The herobrine build is broken.
    
    Original change's description:
    > herobrine: Move the ADC dt nodes from board to project
    >
    > Make the board independent from the hardware project. The ADC
    > dt nodes should be related to the project. Move them.
    >
    > BRANCH=None
    > BUG=b:214613810
    > TEST=Built the herobrine_npcx9 project and checked generated
    > source.dts unchanged.
    >
    > Change-Id: I09e921b42b29851a0a3ca955da64cdd1e72d2a2c
    > Signed-off-by: Wai-Hong Tam <waihong@google.com>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3400544
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    
    Bug: b:214613810
    Change-Id: I44048a359caa1e8891c304c7083fafbc8e994dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3413729
    Auto-Submit: Wai-Hong Tam <waihong@google.com>
    Tested-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>

--- zephyr/projects/herobrine/BUILD.py
+++ zephyr/projects/herobrine/BUILD.py
@@ -9,7 +9,6 @@ def register_variant(project_name, extra_dts_overlays=(), extra_kconfig_files=()
         zephyr_board="herobrine_npcx9",
         dts_overlays=[
             # Common to all projects.
-            here / "adc.dts",
             here / "battery.dts",
             here / "gpio.dts",
             here / "i2c.dts",
