cfa0dac6a46c1256d417f3d64cb9af55e18b5383
Devin Lu <Devin.Lu@quantatw.com>
2022-01-24 14:53:56 +0800

    vell: Update fan real rpm
    BUG=b:216026302
    BRANCH=none
    TEST=make BOARD=vell
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I0dfefa4754eb9647f46ff05b422dff2cc74119b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3411684
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/vell/fans.c
+++ board/vell/fans.c
@@ -30,17 +30,10 @@ static const struct fan_conf fan_conf_0 = {
 	.enable_gpio = GPIO_EN_PP5000_FAN,
 };
 
-/*
- * TOOD(b/181271666): thermistor placement and calibration
- *
- * Prototype fan spins at about 4200 RPM at 100% PWM, this
- * is specific to board ID 2 and might also apears in later
- * boards as well.
- */
 static const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 2200,
-	.rpm_start = 2200,
-	.rpm_max = 4200,
+	.rpm_min = 2500,
+	.rpm_start = 2500,
+	.rpm_max = 6800,
 };
 
 const struct fan_t fans[FAN_CH_COUNT] = {
