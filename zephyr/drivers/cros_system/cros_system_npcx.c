48813eba426326f9ee4a5b5f5e45310fe7597a70
Jun Lin <CHLin56@nuvoton.com>
2022-01-28 17:20:03 +0800

    zephyr: npcx: refactor the calculation of total RAM blcok
    The calculation of the total RAM blocks is determined by the size of
    the code RAM, data RAM, and the RAM used by the booter. In the current
    source, the RAM used by the booter is hardcoded. This CL replaces the
    hardcoding by fetching the size from the DT node bootloader_ram.
    
    BUG=none
    BRANCH=none
    TEST="zmake testall -clobber"
    TEST="check the total RAM block is correct for different npcx variants"
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Change-Id: Ia04710360ec4d3144860fb1b33763e2230747c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3443081
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Yuval Peress <peress@google.com>
    Commit-Queue: CH Lin <chlin56@nuvoton.com>
    Tested-by: CH Lin <chlin56@nuvoton.com>

--- zephyr/drivers/cros_system/cros_system_npcx.c
+++ zephyr/drivers/cros_system/cros_system_npcx.c
@@ -64,10 +64,15 @@ struct cros_system_npcx_data {
  * total RAM size = code ram + data ram + extra 2K for ROM functions
  * divided by the block size 32k.
  */
+#if DT_NODE_EXISTS(DT_NODELABEL(bootloader_ram))
+#define BT_RAM_SIZE DT_REG_SIZE(DT_NODELABEL(bootloader_ram))
+#else
+#define BT_RAM_SIZE 0
+#endif
 #define DATA_RAM_SIZE DT_REG_SIZE(DT_NODELABEL(sram0))
 #define CODE_RAM_SIZE DT_REG_SIZE(DT_NODELABEL(flash0))
 #define NPCX_RAM_BLOCK_COUNT \
-	((DATA_RAM_SIZE + CODE_RAM_SIZE + KB(2)) / NPCX_RAM_BLOCK_SIZE)
+	((DATA_RAM_SIZE + CODE_RAM_SIZE + BT_RAM_SIZE) / NPCX_RAM_BLOCK_SIZE)
 
 /* Valid bit-depth of RAM block Power-Down control (RAM_PD) registers. Use its
  * mask to power down all unnecessary RAM blocks before hibernating.
