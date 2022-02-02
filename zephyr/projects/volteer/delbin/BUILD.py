37311b8f646a97fd5fa5789fac69374a7f714359
Andrew McRae <amcrae@google.com>
2022-02-02 15:37:37 +1100

    delbin: Remove from Zephyr projects
    Clean up old unsupported Zephyr projects (volteer/delbin).
    
    BUG=b:217455790
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Cq-Depend: chrome-internal:4518690
    Change-Id: I6c6d46080e47fa4a9a67a1278d465cd7b27a4d16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3430641
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/projects/volteer/delbin/BUILD.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-register_npcx_project(
-    project_name="delbin",
-    zephyr_board="volteer",
-    dts_overlays=["gpio.dts", "interrupts.dts", "motionsense.dts"],
-)
