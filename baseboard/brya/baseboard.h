b259a4fe197fcc38e93330a31f3ad63b21440e62
Sugnan Prabhu S <sugnan.prabhu.s@intel.com>
2022-02-02 20:48:30 +0530

    brya: enable CONFIG_BOARD_HAS_RTC_RESET for brya baseboard
    This patch enables CONFIG_BOARD_HAS_RTC_RESET for brya baseboard by
    updating the S5 wait timeout value.
    
    This reverts commit 7aa6adaf53f99f3b3d03fbd935c930ce3f46cc66.
    
    BRANCH=None
    BUG=b:191742284
    TEST=Boot test on Brya with the DAM enabled coreboot
    
    Signed-off-by: Sugnan Prabhu S <sugnan.prabhu.s@intel.com>
    Change-Id: I6332418aad97048485ef895aa4b798101dc0c38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3432665
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Tested-by: Sugnan Prabhu S <sugnan.prabhu.s@intel.corp-partner.google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Sridhar Siricilla <sridhar.siricilla@intel.corp-partner.google.com>

--- baseboard/brya/baseboard.h
+++ baseboard/brya/baseboard.h
@@ -113,13 +113,9 @@
 
 #define CONFIG_HOSTCMD_ESPI_RESET_SLP_SX_VW_ON_ESPI_RST
 
-/*
- * TODO(b/191742284): When DAM enabled coreboot image is flashed on top of DAM
- * disabled coreboot, S5 exit is taking more than 4 seconds, then EC triggers
- * system shutdown. This WA deselects CONFIG_BOARD_HAS_RTC_RESET to prevent
- * EC from system shutdown.
- */
-/* #define CONFIG_BOARD_HAS_RTC_RESET */
+#define CONFIG_BOARD_HAS_RTC_RESET
+#undef CONFIG_S5_EXIT_WAIT
+#define CONFIG_S5_EXIT_WAIT 10
 
 #define CONFIG_CMD_AP_RESET_LOG
 #define CONFIG_HOSTCMD_AP_RESET
