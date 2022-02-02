45df03d5b00072f2289d2e61c854800c700ab115
Andrew McRae <amcrae@google.com>
2022-02-02 12:57:20 +1100

    zephyr: Add npcx7 as zephyr board target
    Make npcx7 support common so that lazor et. al
    can use it instead of having a trogdor board target.
    
    BUG=b:217460464
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Ia91173c74b0c4ae2fcc6a8931dd9e43f29ab3bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3430639
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

---
+++ zephyr/boards/arm/npcx7/board.cmake
@@ -0,0 +1,5 @@
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set(NPCX_IMAGE_FILE ${PROJECT_BINARY_DIR}/zephyr.npcx.bin)
