3c743670489e30a9843095bfc9463537c1c2f07f
Andrew McRae <amcrae@google.com>
2022-02-03 09:49:12 +1100

    zephyr: Remove unused zephyr board (trogdor)
    Remove unused zephyr board (trogdor).
    
    BUG=b:217460464
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I93539cc5756b538ed8ea5ea04780944313d6eca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3434185
    Reviewed-by: Yuval Peress <peress@google.com>

--- zephyr/boards/arm/trogdor/board.cmake
+++
@@ -1,5 +0,0 @@
-# Copyright 2021 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-set(NPCX_IMAGE_FILE ${PROJECT_BINARY_DIR}/zephyr.npcx.bin)
