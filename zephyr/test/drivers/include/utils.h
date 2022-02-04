94d9e596fbb577d9b37adfd37da50585cafebe3d
Aaron Massey <aaronmassey@google.com>
2022-02-03 17:32:28 -0700

    zephyr: test: Add temporary downstream zassume API
    We need a ztest assumption API to write hermetic assertions while
    describing a dependency relationship between values.
    
    Add zassume API downstream that are just zasserts under the hood
    until we land a zassume API upstream.
    
    BRANCH=none
    BUG=b:217755888
    TEST=zmake configure --test test-drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I9d4fb4bbd0ef340c608f6caa2887d31626997e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3438812
    Reviewed-by: Yuval Peress <peress@google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>

--- zephyr/test/drivers/include/utils.h
+++ zephyr/test/drivers/include/utils.h
@@ -14,4 +14,109 @@ void test_set_chipset_to_s0(void);
 /** @brief Set chipset to G3 state. Call all necessary hooks. */
 void test_set_chipset_to_g3(void);
 
+/*
+ * TODO(b/217755888): Implement ztest assume API upstream
+ */
+
+/**
+ * @brief Assume that this function call won't be reached
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_unreachable(msg, ...) zassert_unreachable(msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a cond is true
+ * @param cond Condition to check
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_true(cond, msg, ...) zassert_true(cond, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a cond is false
+ * @param cond Condition to check
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_false(cond, msg, ...) zassert_false(cond, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a cond is 0 (success)
+ * @param cond Condition to check
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_ok(cond, msg, ...) zassert_ok(cond, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a ptr is NULL
+ * @param ptr Pointer to compare
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_is_null(ptr, msg, ...) zassert_is_null(ptr, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a ptr is not NULL
+ * @param ptr Pointer to compare
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_not_null(ptr, msg, ...) \
+	zassert_not_null(ptr, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a a equals @a b
+ *
+ * @a a and @a b won't be converted and will be compared directly.
+ *
+ * @param a Value to compare
+ * @param b Value to compare
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_equal(a, b, msg, ...) zassert_equal(a, b, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a a does not equal @a b
+ *
+ * @a a and @a b won't be converted and will be compared directly.
+ *
+ * @param a Value to compare
+ * @param b Value to compare
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_not_equal(a, b, msg, ...) \
+	zassert_not_equal(a, b, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a a equals @a b
+ *
+ * @a a and @a b will be converted to `void *` before comparing.
+ *
+ * @param a Value to compare
+ * @param b Value to compare
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_equal_ptr(a, b, msg, ...) \
+	zassert_equal_ptr(a, b, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that @a a is within @a b with delta @a d
+ *
+ * @param a Value to compare
+ * @param b Value to compare
+ * @param d Delta
+ * @param msg Optional message to print if the assumption fails
+ */
+#define zassume_within(a, b, d, msg, ...) \
+	zassert_within(a, b, d, msg, ##__VA_ARGS__)
+
+/**
+ * @brief Assume that 2 memory buffers have the same contents
+ *
+ * This macro calls the final memory comparison assumption macro.
+ * Using double expansion allows providing some arguments by macros that
+ * would expand to more than one values (ANSI-C99 defines that all the macro
+ * arguments have to be expanded before macro call).
+ *
+ * @param ... Arguments, see @ref zassume_mem_equal__
+ *            for real arguments accepted.
+ */
+#define zassume_mem_equal(...) zassert_mem_equal(##__VA_ARGS__)
+
 #endif /* ZEPHYR_TEST_DRIVERS_INCLUDE_UTILS_H_ */
