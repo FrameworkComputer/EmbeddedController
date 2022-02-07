c917b2b10dc51b74fe3f42fdb2746aa1d48e2ab1
Al Semjonovs <asemjonovs@google.com>
2022-02-07 09:16:19 -0700

    zephyr: drivers: Improve code coverage of panic.c
    Add test cases to improve code coverage of panic.c
    
    BRANCH=none
    BUG=b:218310152
    TEST=zmake -D configure --coverage --test zephyr/test/drivers/
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I8d29fcf8628cbe550bb9060fad2fb7b83b1e4762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3443641
    Reviewed-by: Yuval Peress <peress@google.com>

---
+++ zephyr/test/drivers/src/panic.c
@@ -0,0 +1,60 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief Unit Tests for panic.
+ */
+
+#include <device.h>
+
+#include <logging/log.h>
+#include <zephyr.h>
+#include <ztest.h>
+
+#include "common.h"
+#include "ec_tasks.h"
+#include "panic.h"
+#include "stubs.h"
+#include "test_state.h"
+
+/**
+ * @brief Test Suite: Verifies panic functionality.
+ */
+ZTEST_SUITE(panic, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
+
+/**
+ * @brief TestPurpose: Verify panic set/get reason.
+ *
+ * @details
+ * Validate panic set/get reason.
+ *
+ * Expected Results
+ *  - Success
+ */
+ZTEST(panic, test_panic_reason)
+{
+	uint32_t reason;
+	uint32_t info;
+	uint8_t exception;
+	struct panic_data *pdata = panic_get_data();
+
+	zassert_is_null(pdata, NULL);
+	panic_set_reason(PANIC_SW_WATCHDOG, 0, 0);
+
+	panic_get_reason(&reason, &info, &exception);
+
+	zassert_equal(PANIC_SW_WATCHDOG, reason, NULL);
+	zassert_equal(0, info, NULL);
+	zassert_equal(0, exception, NULL);
+
+	pdata = panic_get_data();
+	zassert_not_null(pdata, NULL);
+	zassert_equal(pdata->struct_version, 2, NULL);
+	zassert_equal(pdata->magic, PANIC_DATA_MAGIC, NULL);
+	zassert_equal(pdata->struct_size, CONFIG_PANIC_DATA_SIZE, NULL);
+
+	panic_data_print(pdata);
+}
