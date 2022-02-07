c2359b2b4b9c197d20461b7c7032a7714be88afb
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2022-02-07 10:12:47 +0800

    dojo: Update keyboard scan config
    Update keyboard scan config to meet dojo's KB Mapping.
    
    BUG=b:217975844, b:214738575
    BRANCH=cherry
    TEST=make sure that keyboard scan works correctly by console command
    "ksstate on".
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: Ia8da863a1907bb289d759203bdcbe0111aa43f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3442902
    Reviewed-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- board/dojo/board.c
+++ board/dojo/board.c
@@ -13,6 +13,7 @@
 #include "driver/accelgyro_icm_common.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "keyboard_scan.h"
 #include "motion_sense.h"
 #include "pwm.h"
 #include "pwm_chip.h"
@@ -20,6 +21,21 @@
 
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+/* Keyboard scan setting */
+__override struct keyboard_scan_config keyscan_config = {
+	/* Increase from 50 us, because KSO_02 passes through the H1. */
+	.output_settle_us = 80,
+	.debounce_down_us = 9 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 3 * MSEC,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = 100 * MSEC,
+	.actual_key_mask = {
+		0x1c, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+		0xa4, 0xff, 0xfe, 0x55, 0xfa, 0xca  /* full set */
+	},
+};
 
 /* Sensor */
 static struct mutex g_base_mutex;
