c644c33a55561b571c3217c2e9c643d029dfbcab
Devin Lu <Devin.Lu@quantatw.com>
2022-02-09 10:22:47 +0800

    redrix: Configure FRS GPIOs for PPC
    BRANCH=firmware-brya-14505.B-main
    BUG=b:218754289
    TEST=buildall
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I8d29fa9af8fe58349962b6317e9550f4357474c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3449998
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/redrix/usbc_config.c
+++ board/redrix/usbc_config.c
@@ -63,11 +63,13 @@ struct ppc_config_t ppc_chips[] = {
 	[USBC_PORT_C0] = {
 		.i2c_port = I2C_PORT_USB_C0_PPC,
 		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
+		.frs_en = IOEX_USB_C0_FRS_EN,
 		.drv = &syv682x_drv,
 	},
 	[USBC_PORT_C1] = {
 		.i2c_port = I2C_PORT_USB_C1_PPC,
 		.i2c_addr_flags = SYV682X_ADDR2_FLAGS,
+		.frs_en = IOEX_USB_C1_FRS_EN,
 		.drv = &syv682x_drv,
 	},
 };
