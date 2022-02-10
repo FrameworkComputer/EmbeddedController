334577b2517348136bfe095f4e70099394614253
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-02-10 18:18:31 +0800

    gimble: turn off power led in s0ix
    BUG=b:220941729, b:218628427
    BRANCH=none
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: I8887398b10dc41530172ae10882ed24b539da952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3483137
    Reviewed-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Tested-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/gimble/led.c
+++ board/gimble/led.c
@@ -50,10 +50,8 @@ __override struct led_descriptor
 __override const struct led_descriptor
 		led_pwr_state_table[PWR_LED_NUM_STATES][LED_NUM_PHASES] = {
 	[PWR_LED_STATE_ON]           =  {{EC_LED_COLOR_WHITE, LED_INDEFINITE} },
-	[PWR_LED_STATE_SUSPEND_AC]   =  {{EC_LED_COLOR_WHITE,  1 * LED_ONE_SEC},
-		{LED_OFF,	           1 * LED_ONE_SEC} },
-	[PWR_LED_STATE_SUSPEND_NO_AC] = {{EC_LED_COLOR_WHITE,  1 * LED_ONE_SEC},
-		{LED_OFF,	           1 * LED_ONE_SEC} },
+	[PWR_LED_STATE_SUSPEND_AC]   =  {{LED_OFF,  LED_INDEFINITE} },
+	[PWR_LED_STATE_SUSPEND_NO_AC] =  {{LED_OFF,  LED_INDEFINITE} },
 	[PWR_LED_STATE_OFF]           = {
 		{LED_OFF,             LED_INDEFINITE} },
 };
