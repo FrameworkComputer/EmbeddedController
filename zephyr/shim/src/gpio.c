95ada813759dcde7157ea6f04a277ea8b4f30f7b
Andrew McRae <amcrae@google.com>
2022-02-10 14:31:21 +1100

    zephyr: Upon warm boot, maintain the value of output GPIOs
    Upon warm boot, maintain the current value of any
    output GPIOs.
    
    BUG=b:218773662
    TEST=zmake configure -b lazor; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I0864021d9f252433573d54101e6ac7b350d9d4db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3448053
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- zephyr/shim/src/gpio.c
+++ zephyr/shim/src/gpio.c
@@ -295,9 +295,17 @@ static int init_gpios(const struct device *unused)
 		 */
 		flags = configs[i].init_flags;
 
-		if (is_sys_jumped) {
+		/*
+		 * For warm boot, retrieve the current value of any
+		 * output pins so that no changes are made.
+		 */
+		if (is_sys_jumped && (flags & GPIO_OUTPUT)) {
+			int current = gpio_pin_get_dt(&configs[i].spec);
+
 			flags &=
-				~(GPIO_OUTPUT_INIT_LOW | GPIO_OUTPUT_INIT_HIGH);
+			    ~(GPIO_OUTPUT_INIT_LOW | GPIO_OUTPUT_INIT_HIGH);
+			flags |= current ? GPIO_OUTPUT_INIT_HIGH
+					 : GPIO_OUTPUT_INIT_LOW;
 		}
 
 		rv = gpio_pin_configure_dt(&configs[i].spec, flags);
