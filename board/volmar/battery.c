6918967a088006500d49ec1755d3f2d8bfdec35c
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-02-14 10:03:13 +0800

    volmar: Add battery AP18C8K
    Add battery AP18C8K
    
    BUG=b:218976941
    BRANCH=none
    TEST=Check battery charge/discharge/cutoff working
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Change-Id: I76bf826e1df7e0fdd837592324c5100df67ad3e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3460764
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- board/volmar/battery.c
+++ board/volmar/battery.c
@@ -153,6 +153,34 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c      = 75,
 		},
 	},
+	/* LGC AP18C8K Battery Information */
+	[BATTERY_LGC_AP18C8K] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC KT0030G020",
+			.device_name = "AP18C8K",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.reg_addr = 0x43,
+				.reg_mask = 0x0001,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max            = 13050,
+			.voltage_normal         = 11250,
+			.voltage_min            = 9000,
+			.precharge_current      = 256,
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 50,
+			.charging_min_c         = 0,
+			.charging_max_c         = 60,
+			.discharging_min_c      = -20,
+			.discharging_max_c      = 75,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
