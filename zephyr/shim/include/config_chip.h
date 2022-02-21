86295f979ec39b8b522e75bb507830a57bf17dcc
Dino Li <Dino.Li@ite.com.tw>
2022-02-21 10:51:36 +0800

    zephyr: it8xxx2: use 768KB space from flash
    This change was made to use more space from flash.
    Because Nereid image is almost out of space
    (On previous test, the configuration still able to pass
    software sync)
    
    BRANCH=none
    BUG=b:220273249
    TEST=Build Nereid with enable CONFIG_PLATFORM_EC_BATTERY_CUT_OFF.
         Verified sysjump command on Krabby.
    
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Change-Id: I15445caa9cbf974128b4413d430569e844ef059d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3476659
    Tested-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -537,10 +537,10 @@
 #ifdef CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES
 /*
  * Flash size of IT81202 is 1MB.
- * We use only half space of flash to save time of erasing RW image from flash.
+ * We use only 3/4 space of flash to save time of erasing RW image from flash.
  */
 #ifdef CONFIG_SOC_IT8XXX2
-#define CONFIG_FLASH_SIZE_BYTES (CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES / 2)
+#define CONFIG_FLASH_SIZE_BYTES (CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES * 3 / 4)
 #else
 #define CONFIG_FLASH_SIZE_BYTES CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES
 #endif
