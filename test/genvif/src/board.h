6b3b00d4b1caedbe3af1dc4bf35183acc7833a29
Keith Short <keithshort@chromium.org>
2022-02-23 17:15:37 -0700

    genvif: add stub gpio.inc
    Add a stub gpio.inc file for the genvif tool.  This is required once the
    next CL removes #include "gpio.h" from i2c.h
    
    BUG=b:218856245
    BRANCH=none
    TEST=test/genvif/genvif.h
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I8c50e1eb7de95044724b508fb52d3e8af35c6578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3489091
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- test/genvif/src/board.h
+++ test/genvif/src/board.h
@@ -7,3 +7,7 @@
 #define PD_MAX_POWER_MW		65000
 #define PD_MAX_CURRENT_MA	3250
 #define PD_MAX_VOLTAGE_MV	20000
+
+/* Stubs required by the shared code */
+#define GPIO_PIN(port, index) (GPIO_##port, BIT(index))
+#define GPIO_PIN_MASK(p, m) .port = GPIO_##p, .mask = (m)
