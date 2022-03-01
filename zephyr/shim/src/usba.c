f7323804473e8f85f2fc794047371e440ff8d53e
Sam Hurst <shurst@google.com>
2022-03-01 11:12:22 -0800

    zephyr: Fix USBA device driver
    Change the USBA device driver so that it correctly
    processes the usba-port-enable-list node
    
    BUG=b:220704797
    TEST=zmake configure -b herobrine
    
         Verified that I could charge a device from the USBA port.
    
         Modified gpio.dts usba-port-enable-list node to include 2
         port enable pins to test that all pins are processed.
    
         usba-port-enable-list {
               compatible = "cros-ec,usba-port-enable-pins";
               enable-pins = <&gpio_en_usb_a_5v &gpio_switchcap_on>;
         };
    
         Add debug print statement to usba.c
    
         #define XSTR(x) STR(x)
         #define STR(x) #x
    
         #define PIN(node_id, prop, idx) DT_STRING_TOKEN(DT_PHANDLE_BY_IDX(node_id, prop, idx), enum_name)
    
         #pragma message "PINS: " XSTR(DT_FOREACH_PROP_ELEM(DT_PATH(usba_port_enable_list), enable_pins, PIN))
    
         Output after build:
         note: '#pragma message: PINS: GPIO_EN_USB_A_5V GPIO_SWITCHCAP_ON'
    
    BRANCH=main
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I99595b1f9bfa35658879b9b26c54aa8e4ee43bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3498703
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

--- zephyr/shim/src/usba.c
+++ zephyr/shim/src/usba.c
@@ -10,11 +10,13 @@
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
+#define PIN(node_id, prop, idx) GPIO_SIGNAL(DT_PHANDLE_BY_IDX(node_id, prop, idx)),
+
 BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
 		"No compatible USBA Port Enable instance found");
 
 const int usb_port_enable[] = {
-	DT_FOREACH_CHILD(DT_PATH(usba_port_enable_list), GPIO_SIGNAL_WITH_COMMA)
+	DT_FOREACH_PROP_ELEM(DT_PATH(usba_port_enable_list), enable_pins, PIN)
 };
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
