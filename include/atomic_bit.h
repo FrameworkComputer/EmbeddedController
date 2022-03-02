4cd95ce28ba93a8f54ec60deb5b6584799a9cc53
Dawid Niedzwiecki <dn@semihalf.com>
2022-03-02 14:33:31 +0100

    atomic: wrap atomic.h file from Zephyr
    Create a wrapper file for atomic.h from Zephyr. It needs including
    atomic.h file from CrosEC and definding atomic_get function.
    
    The atomic_*_bit functions can be used in e.g. usb_pd_timer file.
    This approach should simplify checking flags when there are more than
    32 values.
    
    BUG=b:208435177
    TEST=zmake testall & make buildall
    BRANCH=none
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: I86a4e800ce3186f8635da50f7f93d81519f8b34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3500592
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Dawid Niedzwiecki <dawidn@google.com>

---
+++ include/atomic_bit.h
@@ -0,0 +1,20 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_ATOMIC_BIT_H
+#define __CROS_EC_ATOMIC_BIT_H
+
+#ifndef CONFIG_ZEPHYR
+#include "atomic.h"
+
+static inline atomic_val_t atomic_get(const atomic_t *target)
+{
+	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
+}
+
+#include "third_party/zephyr/atomic.h"
+
+#endif /* CONFIG_ZEPHYR */
+#endif /* __CROS_EC_ATOMIC_BIT_H */
