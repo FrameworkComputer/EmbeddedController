cb6f7f153352d83d52ee0c5c35ab11845b072512
Yuval Peress <peress@google.com>
2022-03-02 22:25:48 -0700

    zephyr: Add a test rule to reset the FFF fakes
    Add a test rule to reset all the fakes so that tests no longer
    have to be responsible for it.
    
    BRANCH=none
    BUG=none
    TEST=zmake configure --clobber --test test-drivers
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: Ifbb9107aec843b5b127e8842ac4fc5c0e4c8086b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3500299
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/test/drivers/src/test_mocks.c
+++ zephyr/test/drivers/src/test_mocks.c
@@ -3,6 +3,8 @@
  * found in the LICENSE file.
  */
 
+#include <ztest.h>
+
 #include "test_mocks.h"
 
 DEFINE_FFF_GLOBALS;
@@ -11,3 +13,19 @@ DEFINE_FFF_GLOBALS;
 DEFINE_FAKE_VALUE_FUNC(const void *, init_rom_map, const void *, int);
 DEFINE_FAKE_VOID_FUNC(init_rom_unmap, const void *, int);
 DEFINE_FAKE_VALUE_FUNC(int, init_rom_copy, int, int, int);
+
+/**
+ * @brief Reset all the fakes before each test.
+ */
+static void fff_reset_rule_before(const struct ztest_unit_test *test,
+				  void *data)
+{
+	ARG_UNUSED(test);
+	ARG_UNUSED(data);
+
+	RESET_FAKE(init_rom_map);
+	RESET_FAKE(init_rom_unmap);
+	RESET_FAKE(init_rom_copy);
+}
+
+ZTEST_RULE(fff_reset_rule, fff_reset_rule_before, NULL);
