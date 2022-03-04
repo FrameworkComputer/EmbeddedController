76d0398d8e90d4bbcb093d7a3a9dc9ae7b3c2de8
Eric Yilun Lin <yllin@chromium.org>
2022-03-04 14:13:11 +0800

    kingler: swap VOLUP/VOLDN on rev0
    The schematic is reversed
    
    BUG=b:219891339
    TEST=press volume buttons and the VOLDN/VOLUP are reversed
    BRANCH=none
    
    Change-Id: I6a54060b3075f604bae3ef2e8f47bb5263432f9d
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3499689
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

---
+++ zephyr/projects/corsola/src/kingler/button.c
@@ -0,0 +1,34 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* kingler button */
+
+#include "button.h"
+#include "cros_board_info.h"
+#include "gpio.h"
+#include "hooks.h"
+
+static void buttons_hook(void)
+{
+	int version;
+
+	if (cbi_get_board_version(&version))
+		return;
+
+	/* b:219891339: drop this workaround when we deprecate rev0 */
+	if (version == 0) {
+		/* swap VOLUP/VOLDN */
+		button_reassign_gpio(BUTTON_VOLUME_DOWN, GPIO_VOLUME_UP_L);
+		button_reassign_gpio(BUTTON_VOLUME_UP, GPIO_VOLUME_DOWN_L);
+		/*
+		 * button_reassign_gpio will disable the old button interrupt
+		 * and then enable the new button interrupt which cause the
+		 * GPIO_VOLUME_UP_L interrupt disabled after we reassign
+		 * BUTTON_VOLUME_UP, so we need to re-enable it here.
+		 */
+		gpio_enable_interrupt(GPIO_VOLUME_UP_L);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, buttons_hook, HOOK_PRIO_DEFAULT);
