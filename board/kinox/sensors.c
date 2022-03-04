aef3acb0159a9b88482daf0b7ac6eced4bc9a37c
felixtwchang <felixtw_chang@compal.corp-partner.google.com>
2022-03-04 16:49:10 +0800

    Kinox: Modify fan table
    Modify fan table and thermal protection.
    
    BUG=b:221180425
    BRANCH=none
    TEST=Make board passed
    
    Signed-off-by: felixtwchang <felixtw_chang@compal.corp-partner.google.com>
    Change-Id: I1d21fef099a5c5c3eae025df0a981e992bbd5911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3503076
    Reviewed-by: KevinSJ Yu <kevinsj_yu@compal.corp-partner.google.com>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/kinox/sensors.c
+++ board/kinox/sensors.c
@@ -91,14 +91,17 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 #define THERMAL_CPU \
 	{ \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(70), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(80), \
+			[EC_TEMP_THRESH_WARN] = 0, \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(65), \
+			[EC_TEMP_THRESH_WARN] = 0, \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(90), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(90), \
 		}, \
 		.temp_fan_off = C_TO_K(35), \
-		.temp_fan_max = C_TO_K(50), \
+		.temp_fan_max = C_TO_K(65), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 
