d9d2917bf49b0dc5c38e95dd257cb0ac9b80d9e5
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-03-04 19:20:09 -0800

    nereid: Enable ADC power signals
    This will bind ADC power signals with ITE VCMP driver instances.
    
    BUG=b:227108147
    TEST=Nereid sequences to S0 successfully
    BRANCH=none
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Ic35f36c7dcba92625f9e633c537e942cd8dd8e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3504427
    Tested-by: Andrew McRae <amcrae@google.com>
    Reviewed-by: Andrew McRae <amcrae@google.com>
    Commit-Queue: Andrew McRae <amcrae@google.com>

--- zephyr/projects/nissa/src/board_power.c
+++ zephyr/projects/nissa/src/board_power.c
@@ -116,19 +116,6 @@ int board_power_signal_get(enum power_signal signal)
 			return 0;
 		}
 		return 1;
-	/*
-	 * TODO: When upstreamed changes ready, remove.
-	 * Temporary workaround for ADC signal.
-	 * Assume always on.
-	 */
-
-#ifdef CONFIG_BOARD_NEREID
-	case PWR_DSW_PWROK:
-		return 1;
-
-	case PWR_PG_PP1P05:
-		return 1;
-#endif
 	}
 }
 
