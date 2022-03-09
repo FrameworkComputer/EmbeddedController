f9eac9a5654611b29e94a496284cb9c79a28d6fa
Zhuohao Lee <zhuohao@chromium.org>
2022-03-09 19:37:55 +0800

    brask: separate fw_config from the board.*
    Currently, we put the fw_config control in the board.* and
    it would be better to separate it from the board.* and put
    it into the fw_config.*. Besides, we redefine the fw_config
    assignment. The new assignment are:
    1. 3 bits for audio (0-2)
    2. 2 bits for barreljack (3-4)
    
    BUG=b:205646582
    TEST=run on the DUT and checked the fw_config value.
    
    Change-Id: I25a5fd8d25919b7a40f5ee4ace5d41f7df5fb757
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3513273
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- board/brask/board.c
+++ board/brask/board.c
@@ -22,6 +22,7 @@
 #include "usbc_config.h"
 #include "usbc_ppc.h"
 #include "driver/tcpm/tcpci.h"
+#include "fw_config.h"
 
 /* Console output macros */
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
@@ -202,36 +203,6 @@ static void port_ocp_interrupt(enum gpio_signal signal)
  * only do that if the system is off since it might still brown out.
  */
 
-/*
- * Barrel-jack power adapter ratings.
- */
-static const struct {
-	int voltage;
-	int current;
-} bj_power[] = {
-	{ /* 0 - 135W (also default) */
-	.voltage = 19500,
-	.current = 6920
-	},
-	{ /* 1 - 230W */
-	.voltage = 19500,
-	.current = 11800
-	},
-};
-
-static unsigned int ec_config_get_bj_power(void)
-{
-	uint32_t fw_config;
-	unsigned int bj;
-
-	cbi_get_fw_config(&fw_config);
-	bj = (fw_config & EC_CFG_BJ_POWER_MASK) >> EC_CFG_BJ_POWER_L;
-	/* Out of range value defaults to 0 */
-	if (bj >= ARRAY_SIZE(bj_power))
-		bj = 0;
-	return bj;
-}
-
 #define ADP_DEBOUNCE_MS		1000  /* Debounce time for BJ plug/unplug */
 /* Debounced connection state of the barrel jack */
 static int8_t adp_connected = -1;
@@ -243,12 +214,8 @@ static void adp_connect_deferred(void)
 	/* Debounce */
 	if (connected == adp_connected)
 		return;
-	if (connected) {
-		unsigned int bj = ec_config_get_bj_power();
-
-		pi.voltage = bj_power[bj].voltage;
-		pi.current = bj_power[bj].current;
-	}
+	if (connected)
+		ec_bj_power(&pi.voltage, &pi.current);
 	charge_manager_update_charge(CHARGE_SUPPLIER_DEDICATED,
 				     DEDICATED_CHARGE_PORT, &pi);
 	adp_connected = connected;
