433e704af69146f373053fa88f98ff1d95517536
Yuval Peress <peress@google.com>
2022-03-09 21:31:25 -0700

    zephyr: test chgoverride console command for sink port
    Add a test to execute the chgoverride console command. The test starts
    with the override in the off position, connects a sink and passes 0
    (the port number of the sink). It then verifies that the override is set
    to port 0.
    
    BRANCH=none
    BUG=b:222630005
    TEST=zmake test test-drivers
    
    Change-Id: I974347df68b632e5b7c04e9245a04cd3ff977ae8
    Signed-off-by: Yuval Peress <peress@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3516710
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- zephyr/test/drivers/src/console_cmd/charge_manager.c
+++ zephyr/test/drivers/src/console_cmd/charge_manager.c
@@ -3,19 +3,79 @@
  * found in the LICENSE file.
  */
 
+#include <shell/shell.h>
 #include <ztest.h>
 
 #include "charge_manager.h"
 #include "console.h"
+#include "emul/emul_isl923x.h"
+#include "emul/tcpc/emul_tcpci_partner_snk.h"
+#include "tcpm/tcpci.h"
 #include "test_state.h"
+#include "utils.h"
+
+static void connect_sink_to_port(const struct emul *charger_emul,
+				 const struct emul *tcpci_emul,
+				 struct tcpci_snk_emul *sink)
+{
+	isl923x_emul_set_adc_vbus(charger_emul, 0);
+	tcpci_emul_set_reg(tcpci_emul, TCPC_REG_POWER_STATUS,
+			   TCPC_REG_POWER_STATUS_VBUS_DET);
+	tcpci_emul_set_reg(tcpci_emul, TCPC_REG_EXT_STATUS,
+			   TCPC_REG_EXT_STATUS_SAFE0V);
+	tcpci_tcpc_alert(0);
+	zassume_ok(tcpci_snk_emul_connect_to_tcpci(&sink->data,
+						   &sink->common_data,
+						   &sink->ops, tcpci_emul),
+		   NULL);
+
+	/* Wait for PD negotiation and current ramp.
+	 * TODO(b/213906889): Check message timing and contents.
+	 */
+	k_sleep(K_SECONDS(10));
+}
+
+static inline void disconnect_sink_from_port(const struct emul *tcpci_emul)
+{
+	zassume_ok(tcpci_emul_disconnect_partner(tcpci_emul), NULL);
+	k_sleep(K_SECONDS(1));
+}
+
+struct console_cmd_charge_manager_fixture {
+	struct tcpci_snk_emul sink_5v_3a;
+	const struct emul *tcpci_emul;
+	const struct emul *charger_emul;
+};
+
+static void *console_cmd_charge_manager_setup(void)
+{
+	static struct console_cmd_charge_manager_fixture test_fixture;
+
+	/* Get references for the emulators */
+	test_fixture.tcpci_emul =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(tcpci_emul)));
+	test_fixture.charger_emul =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(isl923x_emul)));
+	tcpci_emul_set_rev(test_fixture.tcpci_emul, TCPCI_EMUL_REV2_0_VER1_1);
+
+	/* Initialized the sink to request 5V and 3A */
+	tcpci_snk_emul_init(&test_fixture.sink_5v_3a);
+	test_fixture.sink_5v_3a.data.pdo[1] =
+		PDO_FIXED(5000, 3000, PDO_FIXED_UNCONSTRAINED);
+
+	return &test_fixture;
+}
 
 static void console_cmd_charge_manager_after(void *state)
 {
-	ARG_UNUSED(state);
+	struct console_cmd_charge_manager_fixture *fixture = state;
+
 	shell_execute_cmd(get_ec_shell(), "chgoverride -1");
+	disconnect_sink_from_port(fixture->tcpci_emul);
 }
 
-ZTEST_SUITE(console_cmd_charge_manager, drivers_predicate_post_main, NULL, NULL,
+ZTEST_SUITE(console_cmd_charge_manager, drivers_predicate_post_main,
+	    console_cmd_charge_manager_setup, NULL,
 	    console_cmd_charge_manager_after, NULL);
 
 /**
@@ -48,4 +108,18 @@ ZTEST_USER(console_cmd_charge_manager, test_chgoverride_disable_from_off)
 	zassert_ok(shell_execute_cmd(get_ec_shell(), "chgoverride -2"), NULL);
 	zassert_equal(charge_manager_get_override(), OVERRIDE_DONT_CHARGE,
 		      NULL);
+}
+
+ZTEST_USER_F(console_cmd_charge_manager, test_chgoverride_0_from_sink)
+{
+	/* Set chipset to ON, this will set TCPM to DRP */
+	test_set_chipset_to_s0();
+
+	/* TODO(b/214401892): Check why need to give time TCPM to spin */
+	k_sleep(K_SECONDS(1));
+
+	connect_sink_to_port(this->charger_emul, this->tcpci_emul,
+			     &this->sink_5v_3a);
+	zassert_equal(shell_execute_cmd(get_ec_shell(), "chgoverride 0"),
+		      EC_ERROR_INVAL, NULL);
 }
