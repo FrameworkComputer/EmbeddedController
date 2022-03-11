0265f0de24ab791b173d7f58d9f1edc4d4b99b5f
Devin Lu <Devin.Lu@quantatw.com>
2022-03-11 11:13:18 +0800

    vell: Change FAN pwm to 25000 frequency
    BUG=b:223950148
    BRANCH=none
    TEST=On Vell. Fan works normally.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I4417113898573509909097c60640820bc596029b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3518805
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- board/vell/pwm.c
+++ board/vell/pwm.c
@@ -24,8 +24,8 @@ const struct pwm_t pwm_channels[] = {
 	},
 	[PWM_CH_FAN] = {
 		.channel = 5,
-		.flags = PWM_CONFIG_OPEN_DRAIN | PWM_CONFIG_DSLEEP,
-		.freq = 1000
+		.flags = PWM_CONFIG_OPEN_DRAIN,
+		.freq = 25000,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
