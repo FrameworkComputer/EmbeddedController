6f90c6543224155be1234de199c8b3c9775b72f3
Jack Rosenthal <jrosenth@chromium.org>
2022-03-16 13:37:40 -0600

    zephyr: Add "ghost" variant of brya
    Right now using brya hardware to develop ghost EC.  Initially it can
    be a simple rename of brya.  Later we will change the charger chip,
    which will require a couple of DTS customizations.  Eventually, this
    project will need to move to not be a brya variant at some point.
    
    BUG=b:222738712
    BRANCH=none
    TEST=builds (don't have hardware for testing yet)
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I3c713e12bd0685d08ed31c40bd103c2685416d78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3530958
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/projects/brya/brya/BUILD.py
+++ zephyr/projects/brya/brya/BUILD.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-register_npcx_project(
+brya = register_npcx_project(
     project_name="brya",
     zephyr_board="brya",
     dts_overlays=[
@@ -18,3 +18,5 @@ register_npcx_project(
         "usbc.dts",
     ],
 )
+
+ghost = brya.variant(project_name="ghost")
