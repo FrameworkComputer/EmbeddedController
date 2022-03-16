546bda790d162e75b34a1811046398b4df121b9d
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-03-16 13:56:30 -0700

    zephyr-mtlrvp: Add support to get BOARD ID
    Add support to get board ID for MTLRVP
    
    BUG=none
    BRANCH=none
    TEST= zmake mtlrvp and verify BOARD ID and system booting
    
    Cq-Depend: chromium:3462922
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I63167500eb485413e30a8872b530c3add544df3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3529812
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/projects/intelrvp/mtlrvp/src/mtlrvp.c
+++ zephyr/projects/intelrvp/mtlrvp/src/mtlrvp.c
@@ -3,8 +3,16 @@
  * found in the LICENSE file.
  */
 
+#include "common.h"
+#include "console.h"
 #include "gpio.h"
+#include "i2c.h"
+#include "intelrvp.h"
+#include "intel_rvp_board_id.h"
 #include "power/meteorlake.h"
+
+#define CPRINTF(format, args...) cprintf(CC_COMMAND, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_COMMAND, format, ## args)
 
 /******************************************************************************/
 /* PWROK signal configuration */
@@ -26,3 +34,69 @@ const struct intel_x86_pwrok_signal pwrok_signal_deassert_list[] = {
 	},
 };
 const int pwrok_signal_deassert_count = ARRAY_SIZE(pwrok_signal_deassert_list);
+
+/*
+ * Returns board information (board id[7:0] and Fab id[15:8]) on success
+ * -1 on error.
+ */
+__override int board_get_version(void)
+{
+	/* Cache the MTLRVP board ID */
+	static int mtlrvp_board_id;
+
+	int i;
+	int rv = EC_ERROR_UNKNOWN;
+	int fab_id, board_id, bom_id;
+
+	/* Board ID is already read */
+	if (mtlrvp_board_id)
+		return mtlrvp_board_id;
+
+	/*
+	 * IOExpander that has Board ID information is on DSW-VAL rail on
+	 * ADL RVP. On cold boot cycles, DSW-VAL rail is taking time to settle.
+	 * This loop retries to ensure rail is settled and read is successful
+	 */
+	for (i = 0; i < RVP_VERSION_READ_RETRY_CNT; i++) {
+
+		rv  = gpio_pin_get_dt(&bom_id_config[0]);
+
+		if (rv >= 0)
+			break;
+
+		k_msleep(1);
+	}
+
+	/* return -1 if failed to read board id */
+	if (rv)
+		return -1;
+
+	/*
+	 * BOM ID [2]   : IOEX[0]
+	 * BOM ID [1:0] : IOEX[15:14]
+	 */
+	bom_id  = gpio_pin_get_dt(&bom_id_config[0]) << 2;
+	bom_id |= gpio_pin_get_dt(&bom_id_config[1]) << 1;
+	bom_id |= gpio_pin_get_dt(&bom_id_config[2]);
+	/*
+	 * FAB ID [1:0] : IOEX[2:1] + 1
+	 */
+	fab_id  = gpio_pin_get_dt(&fab_id_config[0]) << 1;
+	fab_id |= gpio_pin_get_dt(&fab_id_config[1]);
+	fab_id += 1;
+
+	/*
+	 * BOARD ID[5:0] : IOEX[13:8]
+	 */
+	board_id  = gpio_pin_get_dt(&board_id_config[0]) << 5;
+	board_id |= gpio_pin_get_dt(&board_id_config[1]) << 4;
+	board_id |= gpio_pin_get_dt(&board_id_config[2]) << 3;
+	board_id |= gpio_pin_get_dt(&board_id_config[3]) << 2;
+	board_id |= gpio_pin_get_dt(&board_id_config[4]) << 1;
+	board_id |= gpio_pin_get_dt(&board_id_config[5]);
+
+	CPRINTF("BID:0x%x, FID:0x%x, BOM:0x%x", board_id, fab_id, bom_id);
+
+	mtlrvp_board_id = board_id | (fab_id << 8);
+	return mtlrvp_board_id;
+}
