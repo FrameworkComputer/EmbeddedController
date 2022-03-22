4ea3e478f43ccd3eb9efe3b565989ce969738ceb
Jeremy Bettis <jbettis@google.com>
2022-03-22 16:49:37 -0600

    zephyr: Add trivial test of vboot_hash
    This only tests the host command EC_VBOOT_HASH_START, which doesn't
    really do anything, but it is a stub for the next person to add more
    test cases.
    
    BRANCH=None
    BUG=b:214256453
    TEST=zmake test -a
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: Icdde811128b66c610eaa37b16d4626728273d287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3543484
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>

---
+++ zephyr/test/drivers/src/vboot_hash.c
@@ -0,0 +1,32 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr.h>
+#include <ztest.h>
+#include <sha256.h>
+
+#include "ec_commands.h"
+#include "host_command.h"
+#include "test_state.h"
+
+ZTEST_USER(vboot_hash, test_hostcmd)
+{
+	struct ec_params_vboot_hash params = {
+		.cmd = EC_VBOOT_HASH_START,
+		.offset = 0,
+		.size = 0,
+	};
+	struct ec_response_vboot_hash response;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND(EC_CMD_VBOOT_HASH, 0, response, params);
+
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+	zassert_equal(args.response_size, sizeof(response), NULL);
+	zassert_equal(response.status, EC_VBOOT_HASH_STATUS_BUSY,
+		      "response.status = %d", response.status);
+}
+
+ZTEST_SUITE(vboot_hash, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
