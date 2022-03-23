ed511ab9284517233b59b2f5b42ee1a0303cd6f9
Daisuke Nojiri <dnojiri@chromium.org>
2022-03-23 16:57:06 +0000

    FPMCU/stm32: Compile host_command_common only for FPMCU
    stm32/host_command_common.c is dedicated to FPMCU. This patch makes it
    compiled only for FPMCU boards.
    
    BUG=None
    BRANCH=None
    TEST=buildall
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I7553cc2ce027616abfe4e62afb95cf5793d9c4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3546566
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- chip/stm32/build.mk
+++ chip/stm32/build.mk
@@ -62,7 +62,7 @@ chip-$(CONFIG_STREAM_USART)+=usart_tx_interrupt.o
 chip-$(CONFIG_STREAM_USART)+=usart_rx_dma.o usart_tx_dma.o
 chip-$(CONFIG_USART_HOST_COMMAND)+=usart_host_command.o
 chip-$(CONFIG_CMD_USART_INFO)+=usart_info_command.o
-chip-$(HAS_TASK_CONSOLE)+=host_command_common.o
+chip-$(CONFIG_FINGERPRINT_MCU)+=host_command_common.o
 chip-$(CONFIG_WATCHDOG)+=watchdog.o
 chip-$(HAS_TASK_CONSOLE)+=uart.o
 ifndef CONFIG_KEYBOARD_NOT_RAW
