2a10d7626b3424152b23a3beb5a4748eeea732d5
Dino Li <Dino.Li@ite.com.tw>
2022-03-23 14:40:08 +0800

    krabby: disable i2c3 in G3 state
    This is workaround for i2c enhance channels will block chip
    to enter low power if clock and data pins aren't both at high
    level.
    
    BRANCH=none
    BUG=b:220647142
    TEST=power number is reduced in G3 on krabby.
         i2c3 works in S0
    
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Change-Id: I00590791c36885a58e868a6cc4bbd40545b2403f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3544756
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@google.com>

--- zephyr/projects/corsola/src/krabby/hooks.c
+++ zephyr/projects/corsola/src/krabby/hooks.c
@@ -8,6 +8,38 @@
 
 #include <ap_power/ap_power.h>
 #include "gpio.h"
+#include "hooks.h"
+
+static void board_i2c3_ctrl(bool enable)
+{
+	if (DEVICE_DT_GET(DT_PHANDLE(DT_NODELABEL(i2c3), gpio_dev)) ==
+	    DEVICE_DT_GET(DT_NODELABEL(gpiof))) {
+		/*
+		 * TODO(b/226296649):
+		 * Use pinctrl APIs to enable/disable an interface.
+		 */
+		struct gctrl_it8xxx2_regs *const gctrl_base =
+			(struct gctrl_it8xxx2_regs *)
+				DT_REG_ADDR(DT_NODELABEL(gctrl));
+
+		if (enable)
+			gctrl_base->GCTRL_PMER3 |= IT8XXX2_GCTRL_SMB3PSEL;
+		else
+			gctrl_base->GCTRL_PMER3 &= ~IT8XXX2_GCTRL_SMB3PSEL;
+	}
+}
+
+static void board_enable_i2c3(void)
+{
+	board_i2c3_ctrl(1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_enable_i2c3, HOOK_PRIO_FIRST);
+
+static void board_disable_i2c3(void)
+{
+	board_i2c3_ctrl(0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_HARD_OFF, board_disable_i2c3, HOOK_PRIO_LAST);
 
 static void board_suspend_handler(struct ap_power_ev_callback *cb,
 				  struct ap_power_ev_data data)
