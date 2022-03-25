555073594254d5be23e7bb4d8ff9267a9965c2b4
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-25 13:17:08 +0800

    primus: init touchpad interface after PLT_RST#
    this fixes Goodix touchpad initialization failures.
    
    BUG=b:214150759
    BRANCH=none
    TEST=make -j BOARD=primus
    TEST=verified by EE
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ie74f4e39cf04ab387c03e2c7b0b2267ad335ae80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3552396
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- board/primus/ps2.c
+++ board/primus/ps2.c
@@ -7,6 +7,7 @@
 #include "hooks.h"
 #include "keyboard_8042.h"
 #include "ps2_chip.h"
+#include "time.h"
 
 void send_aux_data_to_device(uint8_t data)
 {
@@ -18,3 +19,27 @@ static void board_init(void)
 	ps2_enable_channel(NPCX_PS2_CH1, 1, send_aux_data_to_host_interrupt);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/*
+ * Goodix touchpad AVDD need to pull low to 0V when poweroff.
+ * Setting PS2 module in GPIO.inc will let AVDD have 0.9V offset.
+ * So we need to enable PS2 module later than PLTRST# to avoid the 0.9V
+ * offset.
+ */
+static void enable_ps2(void)
+{
+	gpio_set_alternate_function(GPIO_PORT_6,
+		BIT(2) | BIT(3), GPIO_ALT_FUNC_DEFAULT);
+}
+DECLARE_DEFERRED(enable_ps2);
+
+static void disable_ps2(void)
+{
+	gpio_set_flags(GPIO_EC_PS2_SCL_TPAD, GPIO_ODR_LOW);
+	gpio_set_flags(GPIO_EC_PS2_SDA_TPAD, GPIO_ODR_LOW);
+	gpio_set_alternate_function(GPIO_PORT_6,
+		BIT(2) | BIT(3), GPIO_ALT_FUNC_NONE);
+	/* make sure PLTRST# goes high and re-enable ps2.*/
+	hook_call_deferred(&enable_ps2_data, 2 * SECOND);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESET, disable_ps2, HOOK_PRIO_DEFAULT);
