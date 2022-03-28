8be1e6b6b4ff78b62c7ffeab9c7cd77fb8ec8c54
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-28 19:32:17 +0800

    primus: read trackpoint device id
    Use PS2 command(0xE1) to read PS2 device id and
    determine the vendor instead of using SSFC.
    
    BUG=b:226215983
    BRANCH=none
    TEST=make -j BOARD=primus
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I85c9101851ad673f0588db174f20f0fc3fac734c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3552486
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/primus/ps2.c
+++ board/primus/ps2.c
@@ -4,6 +4,7 @@
  */
 
 #include <stddef.h>
+#include <string.h>
 
 #include "cbi_ssfc.h"
 #include "gpio.h"
@@ -11,10 +12,13 @@
 #include "keyboard_8042.h"
 #include "ps2.h"
 #include "ps2_chip.h"
-#include "time.h"
 #include "registers.h"
+#include "time.h"
 
 #define	PS2_TRANSMIT_DELAY_MS	10
+
+static uint8_t queue_data[3];
+static int data_count;
 
 void send_aux_data_to_device(uint8_t data)
 {
@@ -46,7 +50,7 @@ static void disable_ps2(void)
 	gpio_set_flags(GPIO_EC_PS2_SDA_TPAD, GPIO_ODR_LOW);
 	gpio_set_alternate_function(GPIO_PORT_6,
 		BIT(2) | BIT(3), GPIO_ALT_FUNC_NONE);
-	/* make sure PLTRST# goes high and re-enable ps2.*/
+	/* make sure PLTRST# goes high and re-enable PS2.*/
 	hook_call_deferred(&enable_ps2_data, 2 * SECOND);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESET, disable_ps2, HOOK_PRIO_DEFAULT);
@@ -55,6 +59,13 @@ static void ps2_transmit(uint8_t cmd)
 {
 	ps2_transmit_byte(PRIMUS_PS2_CH, cmd);
 	msleep(PS2_TRANSMIT_DELAY_MS);
+}
+
+/* Process the PS2 data at here */
+void get_ps2_data(uint8_t data)
+{
+	/* receive the PS2 data and save in PS2 queue */
+	queue_data[data_count++] = data;
 }
 
 static void send_command_to_trackpoint(uint8_t command1, uint8_t command2)
@@ -71,26 +82,47 @@ static void send_command_to_trackpoint(uint8_t command1, uint8_t command2)
 	ps2_transmit(command2);
 }
 
-int get_trackpoint_id(void)
+uint8_t get_trackpoint_id(void)
 {
-	if (get_cbi_ssfc_trackpoint() == SSFC_SENSOR_TRACKPOINT_ELAN)
-		return TP_VARIANT_ELAN;
+	/*
+	 * Three data will be received when asking PS2
+	 * device id.
+	 */
+	ps2_transmit(TP_READ_ID);
+
+	/*
+	 * When EC send TP_READ_ID, trackpoint will return ACK(0xFA),
+	 * and then return device ID. So return the second data.
+	 * Also make sure only return the trackpoint device ID.
+	 */
+	if (queue_data[1] == TP_VARIANT_ELAN ||
+		queue_data[1] == TP_VARIANT_SYNAPTICS)
+		return queue_data[1];
 	else
-		return TP_VARIANT_SYNAPTICS;
+		return 0;
 }
 
-/* Called on AP S0 -> S3 transition */
+/* Called on AP S0 -> S0ix transition */
 static void ps2_suspend(void)
 {
-	int trackpoint_id;
+	uint8_t trackpoint_id;
 	/*
 	 * When EC send PS2 command to PS2 device,
 	 * PS2 device will return ACK(0xFA).
 	 * EC will send it to host and cause host wake from suspend.
 	 * So disable EC send data to host to avoid it.
+	 *
+	 * In order to receive the PS2 data and also not to wake host,
+	 * use get_ps2_data to process PS2 data.
+	 */
+	ps2_enable_channel(PRIMUS_PS2_CH, 1, get_ps2_data);
+	trackpoint_id = get_trackpoint_id();
+	/*
+	 * Don't need to read any data from PS2 device now,
+	 * so disable it.
 	 */
 	ps2_enable_channel(PRIMUS_PS2_CH, 1, NULL);
-	trackpoint_id = get_trackpoint_id();
+
 	/*
 	 * Send suspend mode to trackpoint
 	 * Those commands was provide by Elan and Synaptics
@@ -101,14 +133,19 @@ static void ps2_suspend(void)
 	else if (trackpoint_id == TP_VARIANT_SYNAPTICS)
 		send_command_to_trackpoint(TP_TOGGLE_SOURCE_TAG,
 			TP_TOGGLE_SNAPTICS_SLEEP);
+
+	/* Clear the data in queue and the counter */
+	memset(queue_data, 0, ARRAY_SIZE(queue_data));
+	data_count = 0;
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, ps2_suspend, HOOK_PRIO_DEFAULT);
 
-/* Called on AP S3 -> S0 transition */
+/* Called on AP S0ix -> S0 transition */
 static void ps2_resume(void)
 {
-	int trackpoint_id;
+	uint8_t trackpoint_id;
 
+	ps2_enable_channel(PRIMUS_PS2_CH, 1, get_ps2_data);
 	trackpoint_id = get_trackpoint_id();
 	ps2_enable_channel(PRIMUS_PS2_CH, 1, send_aux_data_to_host_interrupt);
 	/*
@@ -118,5 +155,9 @@ static void ps2_resume(void)
 	if (trackpoint_id == TP_VARIANT_SYNAPTICS)
 		send_command_to_trackpoint(TP_TOGGLE_SOURCE_TAG,
 			TP_TOGGLE_SNAPTICS_SLEEP);
+
+	/* Clear the data in queue and the counter */
+	memset(queue_data, 0, ARRAY_SIZE(queue_data));
+	data_count = 0;
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, ps2_resume, HOOK_PRIO_DEFAULT);
