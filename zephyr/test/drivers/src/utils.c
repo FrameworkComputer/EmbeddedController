1ea4f38e820a1034a9870c4f3062dcaefd0ff5ef
Andrew McRae <amcrae@google.com>
2022-03-30 13:57:01 +1100

    leds: Add tests for LED GPIO handling.
    Add tests for LED GPIO processing.
    
    BUG=none
    TEST=zmake test test-drivers
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Ibecc3a3cf61b1bb5cb3b35b53485b4d36605d0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3559691
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>

--- zephyr/test/drivers/src/utils.c
+++ zephyr/test/drivers/src/utils.c
@@ -23,25 +23,16 @@
 #define GPIO_BATT_PRES_ODL_PATH DT_PATH(named_gpios, ec_batt_pres_odl)
 #define GPIO_BATT_PRES_ODL_PORT DT_GPIO_PIN(GPIO_BATT_PRES_ODL_PATH, gpios)
 
-void test_set_chipset_to_s0(void)
+void test_set_battery_level(int percent)
 {
 	struct sbat_emul_bat_data *bat;
 	struct i2c_emul *emul;
 	const struct device *battery_gpio_dev =
 		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_BATT_PRES_ODL_PATH, gpios));
 
-	printk("%s: Forcing power on\n", __func__);
 	emul = sbat_emul_get_ptr(BATTERY_ORD);
 	bat = sbat_emul_get_bat_data(emul);
-
-	/*
-	 * Make sure that battery is in good condition to
-	 * not trigger hibernate in charge_state_v2.c
-	 * Set battery voltage to expected value and capacity to 75%. Battery
-	 * will not be full and accepts charging, but will not trigger
-	 * hibernate. Charge level is chosen arbitrary.
-	 */
-	bat->cap = bat->full_cap * 3 / 4;
+	bat->cap = bat->full_cap * percent / 100;
 	bat->volt = battery_get_info()->voltage_normal;
 	bat->design_mv = bat->volt;
 
@@ -49,6 +40,19 @@ void test_set_chipset_to_s0(void)
 	zassert_ok(gpio_emul_input_set(battery_gpio_dev,
 				       GPIO_BATT_PRES_ODL_PORT, 0),
 		   NULL);
+}
+
+void test_set_chipset_to_s0(void)
+{
+	printk("%s: Forcing power on\n", __func__);
+	/*
+	 * Make sure that battery is in good condition to
+	 * not trigger hibernate in charge_state_v2.c
+	 * Set battery voltage to expected value and capacity to 75%. Battery
+	 * will not be full and accepts charging, but will not trigger
+	 * hibernate. Charge level is chosen arbitrary.
+	 */
+	test_set_battery_level(75);
 
 	/* The easiest way to power on seems to be the shell command. */
 	zassert_equal(EC_SUCCESS, shell_execute_cmd(get_ec_shell(), "power on"),
