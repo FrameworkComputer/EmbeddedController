73bd7f2839e1fbdca6f0b0975be648f961c39cdb
Al Semjonovs <asemjonovs@google.com>
2022-04-05 13:56:39 +0000

    brya: reorganize project files
    Move brya from boards/arm/brya to project/brya.
    
    BUG=b:226574243
    BRANCH=NONE
    TEST=zmake build brya
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I3506f081d995d09739a7bd54d5308dedbb8c5b09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3572182
    Reviewed-by: Sam Hurst <shurst@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/projects/brya/BUILD.py
+++ zephyr/projects/brya/BUILD.py
@@ -2,21 +2,42 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-brya = register_npcx_project(
+
+def register_npcx9_variant(project_name, extra_dts_overlays=(), extra_kconfig_files=()):
+    return register_npcx_project(
+        project_name=project_name,
+        zephyr_board="npcx9",
+        dts_overlays=[
+            "adc.dts",
+            "battery.dts",
+            "bb_retimer.dts",
+            "cbi_eeprom.dts",
+            "fan.dts",
+            "gpio.dts",
+            "i2c.dts",
+            "interrupts.dts",
+            "keyboard.dts",
+            "motionsense.dts",
+            "pwm_leds.dts",
+            "temp_sensors.dts",
+            "usbc.dts",
+            # Project-specific DTS customization.
+            *extra_dts_overlays,
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "prj.conf",
+            # Project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
+
+
+brya = register_npcx9_variant(
     project_name="brya",
-    zephyr_board="brya",
-    dts_overlays=[
-        "battery.dts",
-        "bb_retimer.dts",
-        "cbi_eeprom.dts",
-        "fan.dts",
-        "gpio.dts",
-        "interrupts.dts",
-        "keyboard.dts",
-        "motionsense.dts",
-        "pwm_leds.dts",
-        "usbc.dts",
-    ],
+    extra_dts_overlays=[here / "brya.dts"],
+    extra_kconfig_files=[here / "prj_brya.conf"],
 )
+
 
 ghost = brya.variant(project_name="ghost")
