dee87f10e7f7f6e8fc75d3d248c26ba0dc5de298
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2022-04-06 15:53:09 +0800

    mt_scp: set decoder capture buffer format to MM21
    In order to reduce latency, need to enable vp8/h264/vp9 default
    capture buffer format type to MM21.
    
    BRANCH=none
    BUG=b:227452337
    TEST=build cherry_scp pass
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: Ib0e0b90f04c8df93fb7eade3ac02f48f0e7d577b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3573430
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Tested-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>

--- chip/mt_scp/mt8195/video.c
+++ chip/mt_scp/mt8195/video.c
@@ -13,7 +13,6 @@ uint32_t video_get_enc_capability(void)
 
 uint32_t video_get_dec_capability(void)
 {
-	return VDEC_CAP_MT21C | VDEC_CAP_MM21 |
-		VDEC_CAP_H264_SLICE | VDEC_CAP_VP8_FRAME |
-		VDEC_CAP_VP9_FRAME;
+	return VDEC_CAP_MM21 | VDEC_CAP_H264_SLICE |
+		VDEC_CAP_VP8_FRAME | VDEC_CAP_VP9_FRAME;
 }
