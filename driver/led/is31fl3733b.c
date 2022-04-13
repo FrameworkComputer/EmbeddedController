840b4ecc5e1f4d77b044912ce2364eca4b254a53
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-04-13 15:59:57 +0800

    RGBKBD: Add IS31FL3733B driver
    IS31FL3733B is a LED driver which can control 4x16 RGB LEDs.
    
    BUG=b:205926011
    BRANCH=None
    TEST=build pass
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Change-Id: Ifc6d9f104782d2257e8b04dcd05487bbec54648f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3585264
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ driver/led/is31fl3733b.c
@@ -0,0 +1,222 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include <string.h>
+#include "common.h"
+#include "console.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "rgb_keyboard.h"
+#include "stddef.h"
+#include "timer.h"
+
+#define CPRINTF(fmt, args...) cprintf(CC_RGBKBD, "RGBKBD: " fmt, ##args)
+#define CPRINTS(fmt, args...) cprints(CC_RGBKBD, "RGBKBD: " fmt, ##args)
+
+
+/* This depends on ADDR1 and ADDR2. (GND, GND) = 0x50. */
+#define IS31FL3733B_ADDR_FLAGS		0x50
+
+#define IS31FL3733B_ROW_SIZE	16
+#define IS31FL3733B_COL_SIZE	4
+#define IS31FL3733B_GRID_SIZE	(IS31FL3733B_COL_SIZE * IS31FL3733B_ROW_SIZE)
+#define IS31FL3733B_BUF_SIZE	(SIZE_OF_RGB * IS31FL3733B_GRID_SIZE)
+
+/* IS31FL3733B registers */
+#define IS31FL3733B_REG_COMMAND			0xFD
+#define IS31FL3733B_REG_COMMAND_WRITE_LOCK	0xFE
+#define IS31FL3733B_REG_INT_MASK		0xF0
+#define IS31FL3733B_REG_INT_STATUS		0xF1
+
+#define IS31FL3733B_PAGE_CTRL			0x00
+#define IS31FL3733B_PAGE_PWM			0x01
+#define IS31FL3733B_PAGE_AUTO			0x02
+#define IS31FL3733B_PAGE_FUNC			0x03
+
+/* FEh Command Register Write Lock */
+#define IS31FL3733B_WRITE_DISABLE		0x00
+#define IS31FL3733B_WRITE_ENABLE		0xC5
+
+#define IS31FL3733B_INT_MASK_IAC		BIT(3)
+#define IS31FL3733B_INT_MASK_IAB		BIT(2)
+#define IS31FL3733B_INT_MASK_IS			BIT(1)
+#define IS31FL3733B_INT_MASK_IO			BIT(0)
+#define IS31FL3733B_INT_STATUS_ABM3		BIT(4)
+#define IS31FL3733B_INT_STATUS_ABM2		BIT(3)
+#define IS31FL3733B_INT_STATUS_ABM1		BIT(2)
+#define IS31FL3733B_INT_STATUS_SB		BIT(1)
+#define IS31FL3733B_INT_STATUS_OB		BIT(0)
+
+#define IS31FL3733B_FUNC_CFG			0x00
+#define IS31FL3733B_FUNC_GCC			0x01
+#define IS31FL3733B_FUNC_ABM1_1			0x02
+#define IS31FL3733B_FUNC_ABM1_2			0x03
+#define IS31FL3733B_FUNC_ABM1_3			0x04
+#define IS31FL3733B_FUNC_ABM1_4			0x05
+#define IS31FL3733B_FUNC_ABM2_1			0x06
+#define IS31FL3733B_FUNC_ABM2_2			0x07
+#define IS31FL3733B_FUNC_ABM2_3			0x08
+#define IS31FL3733B_FUNC_ABM2_4			0x09
+#define IS31FL3733B_FUNC_ABM3_1			0x0a
+#define IS31FL3733B_FUNC_ABM3_2			0x0b
+#define IS31FL3733B_FUNC_ABM3_3			0x0c
+#define IS31FL3733B_FUNC_ABM3_4			0x0d
+#define IS31FL3733B_FUNC_TUR			0x0e
+#define IS31FL3733B_FUNC_SW_PU			0x0f
+#define IS31FL3733B_FUNC_CS_PD			0x10
+#define IS31FL3733B_FUNC_RST			0x11
+
+static int is31fl3733b_read(struct rgbkbd *ctx, uint8_t addr, uint8_t *value)
+{
+	return i2c_xfer(ctx->cfg->i2c, IS31FL3733B_ADDR_FLAGS,
+			&addr, sizeof(addr), value, sizeof(*value));
+}
+
+static int is31fl3733b_write(struct rgbkbd *ctx, uint8_t addr, uint8_t value)
+{
+	uint8_t buf[2] = {
+		[0] = addr,
+		[1] = value,
+	};
+
+	return i2c_xfer(ctx->cfg->i2c, IS31FL3733B_ADDR_FLAGS,
+			buf, sizeof(buf), NULL, 0);
+}
+
+static int is31fl3733b_set_page(struct rgbkbd *ctx, uint8_t page)
+{
+	int rv;
+
+	/* unlock page select once */
+	rv = is31fl3733b_write(ctx, IS31FL3733B_REG_COMMAND_WRITE_LOCK,
+					IS31FL3733B_WRITE_ENABLE);
+	if (rv) {
+		return rv;
+	}
+
+	return is31fl3733b_write(ctx, IS31FL3733B_REG_COMMAND, page);
+}
+
+static int is31fl3733b_get_config(struct rgbkbd *ctx, uint8_t addr,
+							uint8_t *value)
+{
+	int rv;
+
+	rv = is31fl3733b_set_page(ctx, IS31FL3733B_PAGE_FUNC);
+	if (rv) {
+		return rv;
+	}
+
+	return is31fl3733b_read(ctx, addr, value);
+}
+
+static int is31fl3733b_set_config(struct rgbkbd *ctx, uint8_t addr,
+							uint8_t value)
+{
+	int rv;
+
+	rv = is31fl3733b_set_page(ctx, IS31FL3733B_PAGE_FUNC);
+	if (rv) {
+		return rv;
+	}
+
+	return is31fl3733b_write(ctx, addr, value);
+}
+
+static int is31fl3733b_reset(struct rgbkbd *ctx)
+{
+	uint8_t value;
+
+	return is31fl3733b_get_config(ctx, IS31FL3733B_FUNC_RST, &value);
+}
+
+static int is31fl3733b_enable(struct rgbkbd *ctx, bool enable)
+{
+	uint8_t u8;
+	int rv;
+
+	gpio_set_level(GPIO_RGBKBD_SDB_L, enable ? 1 : 0);
+
+	rv = is31fl3733b_get_config(ctx, IS31FL3733B_FUNC_CFG, &u8);
+	if (rv) {
+		return rv;
+	}
+
+	WRITE_BIT(u8, 4, 1);
+	WRITE_BIT(u8, 0, enable);
+
+	return is31fl3733b_write(ctx, IS31FL3733B_FUNC_CFG, u8);
+}
+
+static int is31fl3733b_set_color(struct rgbkbd *ctx, uint8_t offset,
+			     struct rgb_s *color, uint8_t len)
+{
+	uint8_t buf[sizeof(offset) + IS31FL3733B_BUF_SIZE];
+	const int frame_len = len * SIZE_OF_RGB + sizeof(offset);
+	const int frame_offset = offset * SIZE_OF_RGB;
+	int i, rv;
+
+	if (frame_offset + frame_len > sizeof(buf)) {
+		return EC_ERROR_OVERFLOW;
+	}
+
+	rv = is31fl3733b_set_page(ctx, IS31FL3733B_PAGE_PWM);
+	if (rv) {
+		return rv;
+	}
+
+	for (i = 0; i < len; i++) {
+		buf[i * SIZE_OF_RGB + 0] = color[i].r;
+		buf[i * SIZE_OF_RGB + 1] = color[i].g;
+		buf[i * SIZE_OF_RGB + 2] = color[i].b;
+	}
+
+	return i2c_xfer(ctx->cfg->i2c, IS31FL3733B_ADDR_FLAGS,
+			buf, frame_len, NULL, 0);
+}
+
+static int is31fl3733b_set_scale(struct rgbkbd *ctx, uint8_t offset,
+						uint8_t scale, uint8_t len)
+{
+	/* is31fl3733b not support scale function */
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+static int is31fl3733b_set_gcc(struct rgbkbd *ctx, uint8_t level)
+{
+	return is31fl3733b_set_config(ctx, IS31FL3733B_FUNC_GCC, level);
+}
+
+static int is31fl3733b_init(struct rgbkbd *ctx)
+{
+	int rv;
+
+	rv = is31fl3733b_reset(ctx);
+	msleep(3);
+
+	if (IS_ENABLED(CONFIG_RGB_KEYBOARD_DEBUG)) {
+
+		uint8_t val;
+		int ret;
+
+		ret = is31fl3733b_get_config(ctx,
+					IS31FL3733B_FUNC_SW_PU, &val);
+		CPRINTS("SW_PU: val=0x%02x (rv=%d)", val, ret);
+
+		ret = is31fl3733b_get_config(ctx,
+					IS31FL3733B_FUNC_CS_PD, &val);
+		CPRINTS("CS_PD: val=0x%02x (rv=%d)", val, ret);
+	}
+
+	return rv;
+}
+
+const struct rgbkbd_drv is31fl3733b_drv = {
+	.reset = is31fl3733b_reset,
+	.init = is31fl3733b_init,
+	.enable = is31fl3733b_enable,
+	.set_color = is31fl3733b_set_color,
+	.set_scale = is31fl3733b_set_scale,
+	.set_gcc = is31fl3733b_set_gcc,
+};
