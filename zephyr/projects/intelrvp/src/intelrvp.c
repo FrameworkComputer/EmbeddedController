8b30474f0c9f50c3cabae405fa08a347d3546fe1
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-04-15 17:13:34 -0700

    zephyr: intelrvp: Enable apreset host command
    This Enables apreset host command for intel RVPs.
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake testall
         Ec console command 'apreset'
         AP console command 'ectool apreset'
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I5a2f30ad331a69db1588a860d16ef042eb778afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3589193
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- zephyr/projects/intelrvp/src/intelrvp.c
+++ zephyr/projects/intelrvp/src/intelrvp.c
@@ -13,3 +13,12 @@ static void board_init(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(ec_spi_oe_mecc), 1);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_LAST);
+
+__override void intel_x86_sys_reset_delay(void)
+{
+	/*
+	 * From MAX6818 Data sheet, Range of 'Debounce Duaration' is
+	 * Minimum - 20 ms, Typical - 40 ms, Maximum - 80 ms.
+	 */
+	udelay(60 * MSEC);
+}
