e7d8d80a210af147565dc641f5c7c059f7c2de23
Gwendal Grignou <gwendal@chromium.org>
2022-04-26 11:19:34 -0700

    taeko: remove interrupt setup for lid accel
    taeko can be stuffed with a lis12dw or a bma422.
    Since the later does not have code to support interrupt, disable
    interrupt for both.
    
    BUG=b:230401133
    BRANCH=brya
    TEST=Check on taeko hardware.SensorIioserviceHard passes.
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: Id58b2d8474097947881bd821692a86f53a538c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3609358
    Reviewed-by: Yuval Peress <peress@google.com>

--- board/taeko/board.c
+++ board/taeko/board.c
@@ -16,6 +16,7 @@
 #include "gpio_signal.h"
 #include "hooks.h"
 #include "driver/accel_lis2dw12.h"
+#include "driver/accelgyro_lsm6dsm.h"
 #include "driver/accelgyro_lsm6dso.h"
 #include "driver/als_tcs3400.h"
 #include "fw_config.h"
