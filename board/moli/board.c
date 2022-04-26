55f8814eebbb4f2921bba1019b46e34e4b2f8e38
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-04-26 22:58:49 +0800

    moli: fix type-c cannot negotiation to 20V
    BUG=b:230464225
    BRANCH=none
    TEST=make -j BOARD=moli
    TEST=VBUS change from 5V to 20V
    
    Change-Id: I9f930254f21e646c355c140c3f12412955aa8356
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3608037
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/moli/board.c
+++ board/moli/board.c
@@ -84,6 +84,8 @@ int board_set_active_charge_port(int port)
 	switch (port) {
 	case CHARGE_PORT_TYPEC0:
 	case CHARGE_PORT_TYPEC1:
+		gpio_set_level(GPIO_EN_PPVAR_BJ_ADP_L, 1);
+		break;
 	case CHARGE_PORT_BARRELJACK:
 		/* Make sure BJ adapter is sourcing power */
 		if (gpio_get_level(GPIO_BJ_ADP_PRESENT_ODL))
