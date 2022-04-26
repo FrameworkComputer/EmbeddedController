9175f84a4c6e9f06e6a3bfdc8c49acb0ecdb4e29
Gwendal Grignou <gwendal@chromium.org>
2022-04-25 19:27:53 -0700

    beadrix/pirika/waddledee/wheelie: Add CONFIG_ACCEL_LSM6DSM_INT_EVENT
    board.h was missing this define. The error was hidden since
    CONFIG_ACCEL_LSM6DSM_INT_EVENT was defined to 0 only in LSM6DSM driver
    when not found.
    
    See commit 55dc3bb5577 ("driver: lsm6dsm: Allow building without CONFIG_ACCEL_FIFO")
    
    These boards are canceled projects, so it does not impact any devices.
    
    BUG=b:230401133
    BRANCH=asurada,dedede,brya
    TEST=Build all. Check Zephyr with herobrine. Check on bugzzy.
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: I402e2a14b7d44623f859092875085b9928ef9821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3609123
    Reviewed-by: Yuval Peress <peress@google.com>

--- board/waddledee/board.h
+++ board/waddledee/board.h
@@ -49,6 +49,8 @@
 /* Sensors */
 #define CONFIG_ACCEL_KX022		/* Lid accel */
 #define CONFIG_ACCELGYRO_LSM6DSM	/* Base accel */
+#define CONFIG_ACCEL_LSM6DSM_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK BIT(LID_ACCEL)
 
--- board/wheelie/board.h
+++ board/wheelie/board.h
@@ -40,6 +40,8 @@
 #define CONFIG_ACCEL_LIS2DE		/* Lid accel */
 #define CONFIG_ACCELGYRO_LSM6DSM	/* Base accel */
 #define CONFIG_SYNC			/* Camera VSYNC */
+#define CONFIG_ACCEL_LSM6DSM_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK BIT(LID_ACCEL)
 
