ba9a94643787558016786c215cb18ce6befbeaac
Matt Wang <Matt_Wang@compal.com>
2022-04-27 16:52:39 +0800

    [modify] update pd power state to S0ix for reducing power consumption
    PD needs 0.1.10 or the latest.
    EE teset passed.
    
    Signed-off-by: Matt Wang <Matt_Wang@compal.com>

--- board/hx30/cypress5525.c
+++ board/hx30/cypress5525.c
@@ -441,11 +441,9 @@ void update_system_power_state(void)
 	case POWER_S3S5:
 		cypd_set_power_state(CYP5525_POWERSTATE_S5);
 		break;
-	/* wait PD FW stable */
-	/*case POWER_S0S0ix:
+	case POWER_S0S0ix:
 		cypd_set_power_state(CYP5525_POWERSTATE_S3);
 		break;
-	*/
 	default:
 		cypd_set_error_recovery();
 		cypd_set_power_state(CYP5525_POWERSTATE_S0);
