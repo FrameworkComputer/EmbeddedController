168e48eb6a6ed3c49e21d9b61cb747f573991436
Leo_Tsai <LeoCX_Tsai@compal.com>
2022-04-29 14:31:16 +0800

    [modify] switch Legacy or ESPI PECI
    when Legacy PECI is read 3 times pass will force at
    Legacy PECI, but when to read 10 times fail will
    switch to ESPI PECI.
    
    Signed-off-by: Leo_Tsai <LeoCX_Tsai@compal.com>

--- board/hx30/peci_customization.c
+++ board/hx30/peci_customization.c
@@ -18,6 +18,8 @@
 #define CPRINTS(format, args...) cprints(CC_THERMAL, format, ## args)
 
 static int peci_temp;
+static int peci_select_count;
+static int peci_select_flags;
 
 /*****************************************************************************/
 /* Internal functions */
@@ -74,18 +76,27 @@ int peci_Wr_Pkg_Config(uint8_t index, uint16_t parameter, uint32_t data, int wle
 	for (clen = 4; clen < wlen - 1; clen++)
 		out[clen] = ((data >> ((clen - 4) * 8)) & 0xFF);
 
-/*	if (board_get_version() >= BOARD_VERSION_7)
-		espi_oob_peci_transaction(&peci);
-	else {
-		 DVT1 is HW PECI pin
-*/
-	rv = peci_transaction(&peci);
+
+	if (peci_select_count < 10 || peci_select_flags) {
+		rv = peci_transaction(&peci);
+
+		if (rv != 0)
+			peci_select_count++;
+		else if (!peci_select_flags) {
+			CPRINTS("FORCE GPIO PECI!");
+			peci_select_count = 0;
+			peci_select_flags = 1;
+		}
+	} else {
+		rv = espi_oob_peci_transaction(&peci);
+	}
+
 	if (rv)
 		return rv;
-	/*}*/
+
 	return EC_SUCCESS;
 }
-/*
+
 static int peci_over_espi_get_cpu_temp(int *cpu_temp)
 {
 	int rv;
@@ -101,32 +112,43 @@ static int peci_over_espi_get_cpu_temp(int *cpu_temp)
 		.timeout_us = PECI_GET_TEMP_TIMEOUT_US,
 	};
 
-	rv = espi_oob_peci_transaction(&peci);
+	if (peci_select_count < 10 || peci_select_flags) {
+		rv = peci_transaction(&peci);
 
-	if (rv == EC_ERROR_TIMEOUT) {
-		CPRINTS("ESPI GET VALUE TIMEOUT!");
-		espi_oob_retry_receive_date(r_buf);
+		if (rv != 0)
+			peci_select_count++;
+		else if (!peci_select_flags && peci_select_count > 3) {
+			CPRINTS("FORCE GPIO PECI!");
+			peci_select_count = 0;
+			peci_select_flags = 1;
+		}
+	} else {
+		rv = espi_oob_peci_transaction(&peci);
+
+		if (rv == EC_ERROR_TIMEOUT) {
+			CPRINTS("ESPI GET VALUE TIMEOUT!");
+			espi_oob_retry_receive_date(r_buf);
+		}
 	}
 
 	if (rv)
 		return rv;
 
-
-	 Get relative raw data of temperature.
+	/* Get relative raw data of temperature. */
 	*cpu_temp = (r_buf[1] << 8) | r_buf[0];
 
-	 Convert relative raw data to degrees C.
+	/* Convert relative raw data to degrees C. */
 	*cpu_temp = ((*cpu_temp ^ 0xFFFF) + 1) >> 6;
 
 	if (*cpu_temp >= CONFIG_PECI_TJMAX)
 		return EC_ERROR_INVAL;
 
-	 temperature in K
+	/* temperature in K */
 	*cpu_temp = CONFIG_PECI_TJMAX - *cpu_temp + 273;
 
 	return EC_SUCCESS;
 }
-*/
+
 int check_system_power(void)
 {
 	uint8_t host_power_state = *host_get_customer_memmap(EC_EMEMAP_ER1_POWER_STATE);
@@ -265,7 +287,7 @@ int peci_over_espi_temp_sensor_get_val(int idx, int *temp_ptr)
 	return EC_SUCCESS;
 }
 
-/*
+
 void read_peci_over_espi_gettemp(void)
 {
 	int rv;
@@ -288,4 +310,3 @@ void read_peci_over_espi_gettemp(void)
 	}
 }
 DECLARE_HOOK(HOOK_SECOND, read_peci_over_espi_gettemp, HOOK_PRIO_DEFAULT);
-*/
