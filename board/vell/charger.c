19517153849c9da5f9a41e3cb9b86ee651227fa9
Devin Lu <Devin.Lu@quantatw.com>
2022-05-04 10:54:59 +0800

    vell: Limit input current to fraction of negotiated limit
    Limit input current to 96% of negotiated limit
    
    BUG=none
    BRANCH=none
    TEST=Connect adapter then check input current.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I89cf7b1d9a8b8a022e1312dad5de853d53cf7308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3626851
    Reviewed-by: Boris Mittelberg <bmbm@google.com>

--- board/vell/charger.c
+++ board/vell/charger.c
@@ -82,9 +82,14 @@ int board_set_active_charge_port(int port)
 	return EC_SUCCESS;
 }
 
-__overridable void board_set_charge_limit(int port, int supplier, int charge_ma,
+void board_set_charge_limit(int port, int supplier, int charge_ma,
 					  int max_ma, int charge_mv)
 {
+	/*
+	 * Limit the input current to 96% negotiated limit,
+	 * to account for the charger chip margin.
+	 */
+	charge_ma = charge_ma * 96 / 100;
 	charge_set_input_current_limit(MAX(charge_ma,
 					   CONFIG_CHARGER_INPUT_CURRENT),
 				       charge_mv);
