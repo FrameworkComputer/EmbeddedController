d33d4b9183ea8e4cc3167c32ef7f4f08d629611c
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-05-05 19:28:31 +0800

    moli: modify rpm deviation
    Currently rpm deviation is 7%, It will cause [ectool pwmsetfanrpm 5000]
    and [ectool pwmgetfanrpm] not accurate.
    
    BUG=b:231542804
    TEST=make -j BOARD=moli
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: Ibba4b3bae04ceddd9952490ec11c5ede22b88a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3627615
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/moli/board.h
+++ board/moli/board.h
@@ -128,11 +128,9 @@
 /* ADC */
 #define CONFIG_ADC
 
-/*
- * TODO(b/197478860): Enable the fan control. We need
- * to check the sensor value and adjust the fan speed.
- */
- #define CONFIG_FANS			FAN_CH_COUNT
+/* Fan */
+#define CONFIG_FANS			FAN_CH_COUNT
+#define RPM_DEVIATION		1
 
 /* Include math_util for bitmask_uint64 used in pd_timers */
 #define CONFIG_MATH_UTIL
