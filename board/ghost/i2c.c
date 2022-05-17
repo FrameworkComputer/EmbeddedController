f4bc5fcbba1a73ee9b0f75822de3c4fef4ff7ed0
Caveh Jalali <caveh@chromium.org>
2022-05-17 02:12:52 -0700

    ghost: set 1 MHz I2C buses to 400 KHz
    This sets all I2C buses to run at no more than 400 KHz.
    1 MHz operation should only be enabled after validation.
    
    BRANCH=none
    BUG=b:230813256
    TEST=image boots on brya, PD charging works
    
    Change-Id: I926c2ae49aa2cf3cc410bba1ff940695ac94d29e
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3651011
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

--- board/ghost/i2c.c
+++ board/ghost/i2c.c
@@ -23,7 +23,8 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C1 */
 		.name = "tcpc0,2",
 		.port = I2C_PORT_USB_C0_C2_TCPC,
-		.kbps = 1000,
+		/* TODO(b/233013680): set to 1000 when validated */
+		.kbps = 400,
 		.scl = GPIO_EC_I2C_USB_C0_C2_TCPC_SCL,
 		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
 	},
@@ -31,7 +32,8 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C2 */
 		.name = "ppc0,2",
 		.port = I2C_PORT_USB_C0_C2_PPC,
-		.kbps = 1000,
+		/* TODO(b/233013680): set to 1000 when validated */
+		.kbps = 400,
 		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
 		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
 	},
@@ -39,7 +41,8 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C3 */
 		.name = "retimer0,2",
 		.port = I2C_PORT_USB_C0_C2_MUX,
-		.kbps = 1000,
+		/* TODO(b/233013680): set to 1000 when validated */
+		.kbps = 400,
 		.scl = GPIO_EC_I2C_USB_C0_C2_RT_SCL,
 		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
 	},
