857cafbe30936d5975ca9fe1bb6faf27dca32bc8
Parth Malkan <parthmalkan@google.com>
2022-05-17 09:11:40 -0700

    zephyr: skyrim: Enable skyrim to use common LED driver
    This patch adds led_policy and led_pins dts files for skyrim and
    enables common led_driver code.
    
    Also disables guybrush build for zephyr
    
    BUG=b:232104887
    BRANCH=none
    TEST=Tested manually on Skyrim proto
    
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: Ie9e7a7c20b1d6272707934a838c8acdfab7f6410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3651490
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/projects/skyrim/BUILD.py
+++ zephyr/projects/skyrim/BUILD.py
@@ -19,8 +19,9 @@ def register_variant(project_name):
             here / "i2c.dts",
             here / "interrupts.dts",
             here / "keyboard.dts",
+            here / "led_pins_skyrim.dts",
+            here / "led_policy_skyrim.dts",
             here / "motionsense.dts",
-            here / "pwm_leds.dts",
             here / "usbc.dts",
             # Project-specific DTS customizations.
             here / f"{project_name}.dts",
@@ -34,4 +35,4 @@ def register_variant(project_name):
 register_variant(project_name="skyrim")
 
 # TODO: Deprecate guybrush build after skyrim hardware is readily available.
-register_variant(project_name="guybrush")
+# register_variant(project_name="guybrush")
