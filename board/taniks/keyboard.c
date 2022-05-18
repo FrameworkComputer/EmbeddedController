6abe6202a5536caee467d7f665697d2bf23ac78e
arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
2022-05-18 17:49:03 +0800

    taniks: modify RGB mapping table for 4 zone
    Modify RGB led mapping table for 4 zone.
    
    BUG=b:227522597, b:231747095
    BRANCH=none
    TEST=make buildall -j
    ectool rgbkbd 1 0xffffff
    ectool rgbkbd 2 0xffffff
    ...
    ectool rgbkbd 127 0xffffff
    
    Signed-off-by: arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
    Change-Id: I1d7ae552a0b16be048598dc2b65bd9cc385a58b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3653757
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>

--- board/taniks/keyboard.c
+++ board/taniks/keyboard.c
@@ -70,48 +70,136 @@ const uint8_t rgbkbd_vsize = RGB_GRID0_ROW;
 #define LED(x, y)	RGBKBD_COORD((x), (y))
 #define DELM		RGBKBD_DELM
 const uint8_t rgbkbd_map[] = {
-	DELM,
-	LED( 0, 0), DELM,
-	LED( 1, 0), DELM,
-	LED( 2, 0), DELM,
-	LED( 3, 0), DELM,
-	LED( 4, 0), DELM,
-	LED( 5, 0), DELM,
-	LED( 6, 0), DELM,
-	LED( 7, 0), DELM,
-	LED( 0, 1), DELM,
-	LED( 1, 1), DELM,
-	LED( 2, 1), DELM,
-	LED( 3, 1), DELM,
-	LED( 4, 1), DELM,
-	LED( 5, 1), DELM,
-	LED( 6, 1), DELM,
-	LED( 7, 1), DELM,
-	LED( 0, 2), DELM,
-	LED( 1, 2), DELM,
-	LED( 2, 2), DELM,
-	LED( 3, 2), DELM,
-	LED( 4, 2), DELM,
-	LED( 5, 2), DELM,
-	LED( 6, 2), DELM,
-	LED( 7, 2), DELM,
-	LED( 0, 3), DELM,
-	LED( 1, 3), DELM,
-	LED( 2, 3), DELM,
-	LED( 3, 3), DELM,
-	LED( 4, 3), DELM,
-	LED( 5, 3), DELM,
-	LED( 6, 3), DELM,
-	LED( 7, 3), DELM,
-	LED( 0, 4), DELM,
-	LED( 1, 4), DELM,
-	LED( 2, 4), DELM,
-	LED( 3, 4), DELM,
-	LED( 4, 4), DELM,
-	LED( 5, 4), DELM,
-	LED( 6, 4), DELM,
-	LED( 7, 4), DELM,
-	DELM,
+	DELM,				/* 0: (null) */
+	LED( 0, 0), DELM,		/* 1: ~ ` */
+	LED( 2, 0), LED( 4, 0), DELM,	/* 2: ! 1 */
+	LED( 6, 0), DELM,		/* 3: @ 2 */
+	LED( 0, 1), DELM,		/* 4: # 3 */
+	LED( 2, 1), DELM,		/* 5: $ 4 */
+	LED( 4, 1), LED( 6, 1), DELM,	/* 6: % 5 */
+	LED( 0, 2), DELM,		/* 7: ^ 6 */
+	LED( 2, 2), DELM,		/* 8: & 7 */
+	LED( 4, 2), DELM,		/* 9: * 8 */
+	LED( 6, 2), DELM,		/* 10: ( 9 */
+	LED( 0, 3), DELM,		/* 11: ) 0 */
+	LED( 1, 3), DELM,		/* 12: _ - */
+	LED( 3, 3), DELM,		/* 13: + = */
+	DELM,				/* 14: (null) */
+	LED( 5, 3), LED( 6, 3), DELM,	/* 15: backspace */
+	LED( 0, 0), DELM,		/* 16: tab */
+	LED( 2, 0), LED( 4, 0), DELM,	/* 17: q */
+	LED( 6, 0), DELM,		/* 18: w */
+	LED( 0, 1), DELM,		/* 19: e */
+	LED( 2, 1), DELM,		/* 20: r */
+	LED( 4, 1), LED( 6, 1), DELM,	/* 21: t */
+	LED( 0, 2), DELM,		/* 22: y */
+	LED( 2, 2), DELM,		/* 23: u */
+	LED( 4, 2), DELM,		/* 24: i */
+	LED( 6, 2), DELM,		/* 25: o */
+	LED( 0, 3), LED( 1, 3), DELM,	/* 26: p */
+	LED( 3, 3), DELM,		/* 27: [ { */
+	LED( 5, 3), DELM,		/* 28: ] } */
+	LED( 6, 3), DELM,		/* 29: \ | */
+	LED( 0, 0), DELM,		/* 30: caps lock */
+	LED( 2, 0), LED( 4, 0), DELM,	/* 31: a */
+	LED( 6, 0), DELM,		/* 32: s */
+	LED( 0, 1), DELM,		/* 33: d */
+	LED( 2, 1), DELM,		/* 34: f */
+	LED( 4, 1), LED( 6, 1), DELM,	/* 35: g */
+	LED( 0, 2), DELM,		/* 36: h */
+	LED( 2, 2), DELM,		/* 37: j */
+	LED( 4, 2), DELM,		/* 38: k */
+	LED( 6, 4), DELM,		/* 39: l */
+	LED( 0, 3), LED( 1, 3), DELM,	/* 40: ; : */
+	LED( 3, 3), DELM,		/* 41: " ' */
+	DELM,				/* 42: (null) */
+	LED( 5, 3), LED( 6, 3), DELM,	/* 43: enter */
+	LED( 1, 0), LED( 3, 0), DELM,	/* 44: L-shift */
+	DELM,				/* 45: (null) */
+	LED( 5, 0), DELM,		/* 46: z */
+	LED( 7, 0), DELM,		/* 47: x */
+	LED( 1, 1), DELM,		/* 48: c */
+	LED( 3, 1), DELM,		/* 49: v */
+	LED( 5, 1), LED( 7, 1), DELM,	/* 50: b */
+	LED( 1, 2), DELM,		/* 51: n */
+	LED( 3, 2), DELM,		/* 52: m */
+	LED( 5, 2), DELM,		/* 53: , < */
+	LED( 7, 2), DELM,		/* 54: . > */
+	LED( 2, 3), DELM,		/* 55: / ? */
+	DELM,				/* 56: (null) */
+	LED( 4, 3), LED( 7, 3), DELM,	/* 57: R-shift */
+	LED( 1, 0), LED( 3, 0), DELM,	/* 58: L-ctrl */
+	LED( 5, 3), LED( 6, 3), DELM,	/* 59: power */
+	LED( 5, 0), LED( 7, 0), DELM,	/* 60: L-alt */
+	LED( 1, 1), LED( 3, 1),
+	LED( 5, 1), LED( 7, 1),
+	LED( 1, 2), LED( 3, 2), DELM,	/* 61: space */
+	LED( 5, 2), DELM,		/* 62: R-alt */
+	DELM,				/* 63: (null) */
+	LED( 7, 2), DELM,		/* 64: R-ctrl */
+	DELM,				/* 65: (null) */
+	DELM,				/* 66: (null) */
+	DELM,				/* 67: (null) */
+	DELM,				/* 68: (null) */
+	DELM,				/* 69: (null) */
+	DELM,				/* 70: (null) */
+	DELM,				/* 71: (null) */
+	DELM,				/* 72: (null) */
+	DELM,				/* 73: (null) */
+	DELM,				/* 74: (null) */
+	DELM,				/* 75: (null) */
+	LED( 0, 4), DELM,		/* 76: delete */
+	DELM,				/* 77: (null) */
+	DELM,				/* 78: (null) */
+	LED( 2, 3), DELM,		/* 79: left */
+	LED( 4, 4), DELM,		/* 80: home */
+	LED( 6, 4), DELM,		/* 81: end */
+	DELM,				/* 82: (null) */
+	LED( 4, 3), DELM,		/* 83: up */
+	LED( 4, 3), DELM,		/* 84: down */
+	LED( 0, 4), DELM,		/* 85: page up */
+	LED( 2, 4), DELM,		/* 86: page down */
+	DELM,				/* 87: (null) */
+	DELM,				/* 88: (null) */
+	LED( 7, 3), DELM,		/* 89: right */
+	DELM,				/* 90: (null) */
+	LED( 0, 4), DELM,		/* 91: numpad 7 */
+	LED( 0, 4), DELM,		/* 92: numpad 4 */
+	LED( 1, 4), DELM,		/* 93: numpad 1 */
+	DELM,				/* 94: (null) */
+	LED( 2, 4), DELM,		/* 95: numpad / */
+	LED( 2, 4), DELM,		/* 96: numpad 8 */
+	LED( 2, 4), DELM,		/* 97: numpad 5 */
+	LED( 3, 4), DELM,		/* 98: numpad 2 */
+	LED( 3, 4), DELM,		/* 99: numpad 0 */
+	LED( 4, 4), DELM,		/* 100: numpad * */
+	LED( 4, 4), DELM,		/* 101: numpad 9 */
+	LED( 4, 4), DELM,		/* 102: numpad 6 */
+	LED( 5, 4), DELM,		/* 103: numpad 3 */
+	LED( 5, 4), DELM,		/* 104: numpad . */
+	LED( 6, 4), DELM,		/* 105: numpad - */
+	LED( 6, 4), DELM,		/* 106: numpad + */
+	DELM,				/* 107: (null) */
+	LED( 7, 4), DELM,		/* 108: numpad enter */
+	DELM,				/* 109: (null) */
+	LED( 0, 0), DELM,		/* 110: esc */
+	LED( 2, 0), LED( 4, 0), DELM,	/* T1: back */
+	LED( 6, 0), DELM,		/* T2: refresh */
+	LED( 0, 1), DELM,		/* T3: full screen */
+	LED( 2, 1), DELM,		/* T4: overview */
+	LED( 4, 1), LED( 6, 1), DELM,	/* T5: snapshot */
+	LED( 0, 2), DELM,		/* T6: brightness down */
+	LED( 2, 2), DELM,		/* T7: brightness up */
+	DELM,				/* T8: (null) */
+	DELM,				/* T9: (null) */
+	DELM,				/* T10: (null) */
+	LED( 4, 2), DELM,		/* T11: mic mute */
+	LED( 6, 2), DELM,		/* T12: volume mute */
+	LED( 0, 3), LED( 1, 3), DELM,	/* T13: volume down */
+	LED( 3, 3), DELM,		/* T14: volume up */
+	DELM,				/* T15: (null) */
+	DELM,				/* 126: (null) */
+	DELM,				/* 127: (null) */
 };
 #undef LED
 #undef DELM
