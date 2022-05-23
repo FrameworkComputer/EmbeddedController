9fddea2eab424e2e0da6d7ce6cfd73b4e1a48ff3
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-05-23 14:07:13 +0800

    osiris: Update gpio power control function
    rgb kb power:
    board_kblight_init -> power on
    board_kblight_shutdown -> power off
    
    speaker power:
    S0 -> power on
    S3 -> power off
    
    BUG=b:233323599 b:224423318
    BRANCH=none
    TEST=make BOARD=osiris
    
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Change-Id: Ia0a47c21862097467e919872ec100fc6f0969a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3658673
    Reviewed-by: Parth Malkan <parthmalkan@google.com>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/osiris/board.c
+++ board/osiris/board.c
@@ -38,15 +38,27 @@ __override void board_cbi_init(void)
 /* Called on AP S3 -> S0 transition */
 static void board_chipset_resume(void)
 {
-	/* Allow keyboard backlight to be enabled */
-	gpio_set_level(GPIO_EC_KB_BL_EN_L, 0);
+	gpio_set_level(GPIO_AMP_PWR_EN, 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 
 /* Called on AP S0 -> S3 transition */
 static void board_chipset_suspend(void)
 {
-	/* Turn off the keyboard backlight if it's on. */
-	gpio_set_level(GPIO_EC_KB_BL_EN_L, 1);
+	gpio_set_level(GPIO_AMP_PWR_EN, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
+
+__override void board_kblight_shutdown(void)
+{
+	/* turn off keyboard backlight power */
+	gpio_set_level(GPIO_KYBL_EN, 0);
+}
+
+__override void board_kblight_init(void)
+{
+	/* turn on keyboard backlight power */
+	gpio_set_level(GPIO_KYBL_EN, 1);
+	gpio_set_level(GPIO_EC_KB_BL_EN_L, 1);
+	msleep(10);
+}
