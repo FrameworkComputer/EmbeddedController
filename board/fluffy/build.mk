74047bd2d19977c1cd0579dd60449f92bd21e78d
Tom Hughes <tomhughes@chromium.org>
2022-06-17 09:01:58 -0700

    board/fluffy: Remove CROSS_COMPILE from makefile
    The CROSS_COMPILE variable is set in core/cortex-m0/build.mk so it's
    unnecessary to set it again here.
    
    BRANCH=none
    BUG=b:234181908, b:234181908
    TEST=./util/compare_build.sh -b all -j 120
         => MATCH
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I5139b163fbee33a00c6da08461cce2c1fbfcd84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3712188
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- board/fluffy/build.mk
+++ board/fluffy/build.mk
@@ -10,9 +10,4 @@ CHIP:=stm32
 CHIP_FAMILY:=stm32f0
 CHIP_VARIANT:=stm32f07x
 
-# Use coreboot-sdk
-$(call set-option,CROSS_COMPILE_arm,\
-	$(CROSS_COMPILE_coreboot_sdk_arm),\
-	/opt/coreboot-sdk/bin/arm-eabi-)
-
 board-y=board.o
