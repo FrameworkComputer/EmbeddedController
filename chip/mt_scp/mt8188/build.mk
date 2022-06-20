d8ae8d302824455713907e2c19a5f7dd2f8e58d3
Tinghan Shen <tinghan.shen@mediatek.com>
2022-06-20 19:21:31 +0800

    chip/mt_scp: add chip dependent driver for mt8188
    Add chip dependent driver for mt8188.
    
    BRANCH=none
    BUG=b:236331462
    TEST=boot to scp console
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Signed-off-by: Jason Chen <jason-ch.chen@mediatek.corp-partner.google.com>
    Change-Id: If1503ad7fa6ee434f25bde50afd81202cd9b169c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3720829
    Tested-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ chip/mt_scp/mt8188/build.mk
@@ -0,0 +1,10 @@
+# -*- makefile -*-
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Required chip modules
+chip-y+=$(CHIP_VARIANT)/clock.o
+chip-y+=$(CHIP_VARIANT)/intc_group.o
+chip-y+=$(CHIP_VARIANT)/uart.o
+chip-y+=$(CHIP_VARIANT)/video.o
---
+++ chip/mt_scp/mt8188/clock_regs.h
@@ -0,0 +1,11 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* SCP clock module registers */
+
+#ifndef __CROS_EC_CLOCK_REGS_H
+#define __CROS_EC_CLOCK_REGS_H
+
+#endif /* __CROS_EC_CLOCK_REGS_H */
