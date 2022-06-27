ec42feaaa74b6d52b302fbc8c8d3b1e0e549612e
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:32:37 -0600

    chip/stm32/usb_gpio.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I38561b85db80d7fbbdc7a42dc67c1b041cef3f46
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729553
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/usb_gpio.c
+++ chip/stm32/usb_gpio.c
@@ -11,8 +11,8 @@
 
 void usb_gpio_tx(struct usb_gpio_config const *config)
 {
-	size_t   i;
-	uint32_t mask  = 1;
+	size_t i;
+	uint32_t mask = 1;
 	uint32_t value = 0;
 
 	for (i = 0; i < config->num_gpios; ++i, mask <<= 1)
@@ -31,12 +31,12 @@ void usb_gpio_tx(struct usb_gpio_config const *config)
 
 void usb_gpio_rx(struct usb_gpio_config const *config)
 {
-	size_t   i;
-	uint32_t mask        = 1;
-	uint32_t set_mask    = ((uint32_t)(config->rx_ram[0]) |
-			        (uint32_t)(config->rx_ram[1]) << 16);
-	uint32_t clear_mask  = ((uint32_t)(config->rx_ram[2]) |
-			        (uint32_t)(config->rx_ram[3]) << 16);
+	size_t i;
+	uint32_t mask = 1;
+	uint32_t set_mask = ((uint32_t)(config->rx_ram[0]) |
+			     (uint32_t)(config->rx_ram[1]) << 16);
+	uint32_t clear_mask = ((uint32_t)(config->rx_ram[2]) |
+			       (uint32_t)(config->rx_ram[3]) << 16);
 	uint32_t ignore_mask = set_mask & clear_mask;
 
 	config->state->set_mask = set_mask;
@@ -69,10 +69,10 @@ void usb_gpio_event(struct usb_gpio_config const *config, enum usb_ep_event evt)
 
 	i = config->endpoint;
 
-	btable_ep[i].tx_addr  = usb_sram_addr(config->tx_ram);
+	btable_ep[i].tx_addr = usb_sram_addr(config->tx_ram);
 	btable_ep[i].tx_count = USB_GPIO_TX_PACKET_SIZE;
 
-	btable_ep[i].rx_addr  = usb_sram_addr(config->rx_ram);
+	btable_ep[i].rx_addr = usb_sram_addr(config->rx_ram);
 	btable_ep[i].rx_count = ((USB_GPIO_RX_PACKET_SIZE / 2) << 10);
 
 	/*
@@ -82,8 +82,8 @@ void usb_gpio_event(struct usb_gpio_config const *config, enum usb_ep_event evt)
 	config->tx_ram[0] = 0;
 	config->tx_ram[1] = 0;
 
-	STM32_USB_EP(i) = ((i <<  0) | /* Endpoint Addr*/
-			   (3 <<  4) | /* TX Valid */
-			   (0 <<  9) | /* Bulk EP */
+	STM32_USB_EP(i) = ((i << 0) | /* Endpoint Addr*/
+			   (3 << 4) | /* TX Valid */
+			   (0 << 9) | /* Bulk EP */
 			   (3 << 12)); /* RX Valid */
 }
