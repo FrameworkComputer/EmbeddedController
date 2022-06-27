d7b77b62accbbe1d8086afffe288060a93cf7670
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:37:23 -0600

    zephyr/shim/src/battery.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I4cfdd84ca0fdb48d3d2ec950bb4ffad3e25b5493
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730861
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/src/battery.c
+++ zephyr/shim/src/battery.c
@@ -8,7 +8,7 @@
 #include "battery_fuel_gauge.h"
 
 #define NODE_FUEL_GAUGE(node) \
-{ \
+	{ \
 	.manuf_name = DT_PROP(node, manuf_name), \
 	.device_name = DT_PROP(node, device_name), \
 	.ship_mode = { \
@@ -34,32 +34,29 @@
 		(.imbalance_mv = DT_STRING_TOKEN(node, imbalance_mv),), ()) \
 },
 
-#define NODE_BATT_INFO(node) \
-{ \
-	.voltage_max = DT_PROP(node, voltage_max), \
-	.voltage_normal = DT_PROP(node, voltage_normal), \
-	.voltage_min = DT_PROP(node, voltage_min), \
-	.precharge_voltage = DT_PROP_OR(node, precharge_voltage, 0), \
-	.precharge_current = DT_PROP_OR(node, precharge_current, 0), \
-	.start_charging_min_c = DT_PROP(node, start_charging_min_c), \
-	.start_charging_max_c = DT_PROP(node, start_charging_max_c), \
-	.charging_min_c = DT_PROP(node, charging_min_c), \
-	.charging_max_c = DT_PROP(node, charging_max_c), \
-	.discharging_min_c = DT_PROP(node, discharging_min_c), \
-	.discharging_max_c = DT_PROP(node, discharging_max_c), \
-},
+#define NODE_BATT_INFO(node)                                                 \
+	{                                                                    \
+		.voltage_max = DT_PROP(node, voltage_max),                   \
+		.voltage_normal = DT_PROP(node, voltage_normal),             \
+		.voltage_min = DT_PROP(node, voltage_min),                   \
+		.precharge_voltage = DT_PROP_OR(node, precharge_voltage, 0), \
+		.precharge_current = DT_PROP_OR(node, precharge_current, 0), \
+		.start_charging_min_c = DT_PROP(node, start_charging_min_c), \
+		.start_charging_max_c = DT_PROP(node, start_charging_max_c), \
+		.charging_min_c = DT_PROP(node, charging_min_c),             \
+		.charging_max_c = DT_PROP(node, charging_max_c),             \
+		.discharging_min_c = DT_PROP(node, discharging_min_c),       \
+		.discharging_max_c = DT_PROP(node, discharging_max_c),       \
+	},
 
-#define NODE_BATT_PARAMS(node) \
-{ \
-	.fuel_gauge = NODE_FUEL_GAUGE(node) \
-	.batt_info = NODE_BATT_INFO(node) \
-},
+#define NODE_BATT_PARAMS(node)                            \
+	{ .fuel_gauge = NODE_FUEL_GAUGE(node).batt_info = \
+		  NODE_BATT_INFO(node) },
 
 #if DT_HAS_COMPAT_STATUS_OKAY(battery_smart)
 
-const struct board_batt_params board_battery_info[] = {
-	DT_FOREACH_STATUS_OKAY(battery_smart, NODE_BATT_PARAMS)
-};
+const struct board_batt_params board_battery_info[] = { DT_FOREACH_STATUS_OKAY(
+	battery_smart, NODE_BATT_PARAMS) };
 
 #if DT_NODE_EXISTS(DT_NODELABEL(default_battery))
 #define BAT_ENUM(node) DT_CAT(BATTERY_, node)
