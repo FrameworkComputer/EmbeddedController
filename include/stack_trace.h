5738e12ef4fa699c0b45a4bc310f934f35a3b5f2
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:16:05 -0600

    include/stack_trace.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I1cb296e34f00921c7e6539542891e51a7fde919f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730411
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/stack_trace.h
+++ include/stack_trace.h
@@ -18,8 +18,12 @@ void task_register_tracedump(void);
 /* Dump current stack trace */
 void task_dump_trace(void);
 #else
-static inline void task_register_tracedump(void) { }
-static inline void task_dump_trace(void) { }
+static inline void task_register_tracedump(void)
+{
+}
+static inline void task_dump_trace(void)
+{
+}
 #endif
 
-#endif  /* __CROS_EC_STACK_TRACE_H */
+#endif /* __CROS_EC_STACK_TRACE_H */
