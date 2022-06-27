723d875fd093f4392e73009cac3afaa54f85e0a3
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:33:47 -0600

    common/base32.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0ecfcd0b4a83b5feb010e80af70231807716899f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729607
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- common/base32.c
+++ common/base32.c
@@ -9,15 +9,13 @@
 #include "base32.h"
 #include "util.h"
 
-static const unsigned char crc5_table1[] = {
-	0x00, 0x0E, 0x1C, 0x12, 0x11, 0x1F, 0x0D, 0x03,
-	0x0B, 0x05, 0x17, 0x19, 0x1A, 0x14, 0x06, 0x08
-};
+static const unsigned char crc5_table1[] = { 0x00, 0x0E, 0x1C, 0x12, 0x11, 0x1F,
+					     0x0D, 0x03, 0x0B, 0x05, 0x17, 0x19,
+					     0x1A, 0x14, 0x06, 0x08 };
 
-static const unsigned char crc5_table0[] = {
-	0x00, 0x16, 0x05, 0x13, 0x0A, 0x1C, 0x0F, 0x19,
-	0x14, 0x02, 0x11, 0x07, 0x1E, 0x08, 0x1B, 0x0D
-};
+static const unsigned char crc5_table0[] = { 0x00, 0x16, 0x05, 0x13, 0x0A, 0x1C,
+					     0x0F, 0x19, 0x14, 0x02, 0x11, 0x07,
+					     0x1E, 0x08, 0x1B, 0x0D };
 
 uint8_t crc5_sym(uint8_t sym, uint8_t previous_crc)
 {
@@ -46,9 +44,8 @@ static int decode_sym(int sym)
 	return -1;
 }
 
-int base32_encode(char *dest, int destlen_chars,
-		  const void *srcbits, int srclen_bits,
-		  int add_crc_every)
+int base32_encode(char *dest, int destlen_chars, const void *srcbits,
+		  int srclen_bits, int add_crc_every)
 {
 	const uint8_t *src = srcbits;
 	int destlen_needed;
@@ -59,14 +56,14 @@ int base32_encode(char *dest, int destlen_chars,
 	*dest = 0;
 
 	/* Make sure destination is big enough */
-	destlen_needed = (srclen_bits + 4) / 5;  /* Symbols before adding CRC */
+	destlen_needed = (srclen_bits + 4) / 5; /* Symbols before adding CRC */
 	if (add_crc_every) {
 		/* Must be an exact number of groups to add CRC */
 		if (destlen_needed % add_crc_every)
 			return EC_ERROR_INVAL;
 		destlen_needed += destlen_needed / add_crc_every;
 	}
-	destlen_needed++;  /* For terminating null */
+	destlen_needed++; /* For terminating null */
 	if (destlen_chars < destlen_needed)
 		return EC_ERROR_INVAL;
 
@@ -124,7 +121,7 @@ int base32_decode(uint8_t *dest, int destlen_bits, const char *src,
 
 		sym = decode_sym(*src);
 		if (sym < 0)
-			return -1;  /* Bad input symbol */
+			return -1; /* Bad input symbol */
 
 		/* Check CRC if needed */
 		if (crc_after_every) {
@@ -155,7 +152,7 @@ int base32_decode(uint8_t *dest, int destlen_bits, const char *src,
 		dbits = 8 - (out_bits & 7);
 		b = MIN(dbits, sbits);
 		if (dbits == 8)
-			dest[out_bits / 8] = 0;  /* Starting a new byte */
+			dest[out_bits / 8] = 0; /* Starting a new byte */
 		dest[out_bits / 8] |= (sym << (dbits - b)) >> (sbits - b);
 		out_bits += b;
 		sbits -= b;
