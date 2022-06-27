eaab6467c060de80adf3bdb31d3a9528c898693a
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:29:07 -0600

    board/crota/fw_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I1c296efbeeae209106369f16d7a934a35a2e51dd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728193
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/crota/fw_config.h
+++ board/crota/fw_config.h
@@ -14,10 +14,7 @@
  * Source of truth is the project/brya/brya/config.star configuration file.
  */
 
-enum ec_cfg_usb_db_type {
-	DB_USB_ABSENT = 0,
-	DB_USB_ABSENT2 = 15
-};
+enum ec_cfg_usb_db_type { DB_USB_ABSENT = 0, DB_USB_ABSENT2 = 15 };
 
 enum ec_cfg_keyboard_backlight_type {
 	KEYBOARD_BACKLIGHT_DISABLED = 0,
@@ -26,12 +23,12 @@ enum ec_cfg_keyboard_backlight_type {
 
 union brya_cbi_fw_config {
 	struct {
-		enum ec_cfg_usb_db_type			usb_db : 4;
-		uint32_t				sd_db : 2;
-		uint32_t				lte_db : 1;
-		enum ec_cfg_keyboard_backlight_type	kb_bl : 1;
-		uint32_t				audio : 3;
-		uint32_t				reserved_1 : 21;
+		enum ec_cfg_usb_db_type usb_db : 4;
+		uint32_t sd_db : 2;
+		uint32_t lte_db : 1;
+		enum ec_cfg_keyboard_backlight_type kb_bl : 1;
+		uint32_t audio : 3;
+		uint32_t reserved_1 : 21;
 	};
 	uint32_t raw_value;
 };
