c97000ce0e82a0e4323fccba94eeed2770b61ade
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:11:17 -0600

    board/volmar/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If38addf5747cbe07cc62e17fc540aab7e179b73e
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729089
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/volmar/usbc_config.h
+++ board/volmar/usbc_config.h
@@ -8,13 +8,9 @@
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
 
-#define CONFIG_USB_PD_PORT_MAX_COUNT	2
+#define CONFIG_USB_PD_PORT_MAX_COUNT 2
 
-enum usbc_port {
-	USBC_PORT_C0 = 0,
-	USBC_PORT_C1,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
 void config_usb_db_type(void);
 
