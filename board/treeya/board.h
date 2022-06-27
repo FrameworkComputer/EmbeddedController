b31e84f13b1800091dd4a6975b78f5cce0b9ffe0
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:08:43 -0600

    board/treeya/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I2dd703c845efe326a8cd6e943e1d3d56be7a91d9
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729022
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/treeya/board.h
+++ board/treeya/board.h
@@ -30,7 +30,7 @@
  * By default, enable all console messages excepted HC, ACPI and event:
  * The sensor stack is generating a lot of activity.
  */
-#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#define CC_DEFAULT (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
 #undef CONFIG_HOSTCMD_DEBUG_MODE
 #define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
 
@@ -61,7 +61,7 @@
 /*
  * Slew rate on the PP1800_SENSOR load switch requires a short delay on startup.
  */
-#undef  CONFIG_MOTION_SENSE_RESUME_DELAY_US
+#undef CONFIG_MOTION_SENSE_RESUME_DELAY_US
 #define CONFIG_MOTION_SENSE_RESUME_DELAY_US (10 * MSEC)
 
 /* Second set of sensor drivers */
@@ -71,7 +71,6 @@
 #define CONFIG_ACCEL_LIS2DWL
 
 #ifndef __ASSEMBLER__
-
 
 enum battery_type {
 	BATTERY_SMP,
