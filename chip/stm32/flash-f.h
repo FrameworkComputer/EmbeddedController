d9e6e8846634437d469893ecc019a47e60a54862
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:28:43 -0600

    chip/stm32/flash-f.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Iee2bf280ff948727c76cbcf748538f26492b1e4a
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729495
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/flash-f.h
+++ chip/stm32/flash-f.h
@@ -9,16 +9,16 @@
 #include <stdbool.h>
 
 enum flash_rdp_level {
-	FLASH_RDP_LEVEL_INVALID = -1,	/**< Error occurred. */
-	FLASH_RDP_LEVEL_0,		/**< No read protection. */
-	FLASH_RDP_LEVEL_1,		/**< Reading flash is disabled while in
-					 *   bootloader mode or JTAG attached.
-					 *   Changing to Level 0 from this level
-					 *   triggers mass erase.
-					 */
-	FLASH_RDP_LEVEL_2,		/**< Same as Level 1, but is permanent
-					 *   and can never be disabled.
-					 */
+	FLASH_RDP_LEVEL_INVALID = -1, /**< Error occurred. */
+	FLASH_RDP_LEVEL_0, /**< No read protection. */
+	FLASH_RDP_LEVEL_1, /**< Reading flash is disabled while in
+			    *   bootloader mode or JTAG attached.
+			    *   Changing to Level 0 from this level
+			    *   triggers mass erase.
+			    */
+	FLASH_RDP_LEVEL_2, /**< Same as Level 1, but is permanent
+			    *   and can never be disabled.
+			    */
 };
 
 bool is_flash_rdp_enabled(void);
