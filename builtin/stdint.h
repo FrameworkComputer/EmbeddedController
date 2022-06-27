8d9eb8c579146abd46e9f9302122743bcbf21079
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:14:21 -0600

    builtin/stdint.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia466cfd84e1886e3cce6401b7656b00e4be91d0c
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729126
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- builtin/stdint.h
+++ builtin/stdint.h
@@ -6,43 +6,43 @@
 #ifndef __CROS_EC_STDINT_H__
 #define __CROS_EC_STDINT_H__
 
-typedef unsigned char      uint8_t;
-typedef signed char        int8_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
 
-typedef unsigned short     uint16_t;
-typedef signed short       int16_t;
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
 
-typedef unsigned int       uint32_t;
-typedef signed int         int32_t;
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
 
 typedef unsigned long long uint64_t;
-typedef signed long long   int64_t;
+typedef signed long long int64_t;
 
-typedef unsigned int       uintptr_t;
-typedef int                intptr_t;
+typedef unsigned int uintptr_t;
+typedef int intptr_t;
 
 /* uint_leastX_t represents the smallest type available with at least X bits.
  * uint_fastX_t represents the fastest type available with at least X bits.
  */
-typedef uint8_t            uint_least8_t;
-typedef uint16_t           uint_least16_t;
-typedef uint32_t           uint_least32_t;
-typedef uint64_t           uint_least64_t;
+typedef uint8_t uint_least8_t;
+typedef uint16_t uint_least16_t;
+typedef uint32_t uint_least32_t;
+typedef uint64_t uint_least64_t;
 
-typedef int8_t             int_least8_t;
-typedef int16_t            int_least16_t;
-typedef int32_t            int_least32_t;
-typedef int64_t            int_least64_t;
+typedef int8_t int_least8_t;
+typedef int16_t int_least16_t;
+typedef int32_t int_least32_t;
+typedef int64_t int_least64_t;
 
-typedef uint8_t            uint_fast8_t;
-typedef uint16_t           uint_fast16_t;
-typedef uint32_t           uint_fast32_t;
-typedef uint64_t           uint_fast64_t;
+typedef uint8_t uint_fast8_t;
+typedef uint16_t uint_fast16_t;
+typedef uint32_t uint_fast32_t;
+typedef uint64_t uint_fast64_t;
 
-typedef int8_t             int_fast8_t;
-typedef int16_t            int_fast16_t;
-typedef int32_t            int_fast32_t;
-typedef int64_t            int_fast64_t;
+typedef int8_t int_fast8_t;
+typedef int16_t int_fast16_t;
+typedef int32_t int_fast32_t;
+typedef int64_t int_fast64_t;
 
 #ifndef UINT8_MAX
 #define UINT8_MAX (255U)
@@ -69,10 +69,10 @@ typedef int64_t            int_fast64_t;
 #endif
 
 #ifndef UINT64_C
-#define UINT64_C(c)	c ## ULL
+#define UINT64_C(c) c##ULL
 #endif
 #ifndef INT64_C
-#define INT64_C(c)	c ## LL
+#define INT64_C(c) c##LL
 #endif
 
 #ifndef UINT64_MAX
