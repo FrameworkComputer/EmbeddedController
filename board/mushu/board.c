373f8b949c220dc11d97f166c501bb69de477a84
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:55:01 -0600

    board/mushu/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ieef035b2b8bf24f1b1403ac793d0bc29d23ee923
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728712
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/mushu/board.c
+++ board/mushu/board.c
@@ -44,8 +44,8 @@
 #include "usbc_ppc.h"
 #include "util.h"
 
-#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
-#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ##args)
 
 /* GPIO to enable/disable the USB Type-A port. */
 const int usb_port_enable[CONFIG_USB_PORT_POWER_SMART_PORT_COUNT] = {
@@ -111,18 +111,19 @@ static void bc12_interrupt(enum gpio_signal signal)
 
 /******************************************************************************/
 /* SPI devices */
-const struct spi_device_t spi_devices[] = {
-};
+const struct spi_device_t spi_devices[] = {};
 const unsigned int spi_devices_used = ARRAY_SIZE(spi_devices);
 
 /******************************************************************************/
 /* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
 const struct pwm_t pwm_channels[] = {
-	[PWM_CH_KBLIGHT]   = { .channel = 3, .flags = 0, .freq = 10000 },
-	[PWM_CH_FAN] = {.channel = 5, .flags = PWM_CONFIG_OPEN_DRAIN,
-			.freq = 25000},
-	[PWM_CH_FAN2] = {.channel = 6, .flags = PWM_CONFIG_OPEN_DRAIN,
-			.freq = 25000},
+	[PWM_CH_KBLIGHT] = { .channel = 3, .flags = 0, .freq = 10000 },
+	[PWM_CH_FAN] = { .channel = 5,
+			 .flags = PWM_CONFIG_OPEN_DRAIN,
+			 .freq = 25000 },
+	[PWM_CH_FAN2] = { .channel = 6,
+			  .flags = PWM_CONFIG_OPEN_DRAIN,
+			  .freq = 25000 },
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
@@ -224,22 +225,18 @@ static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
 };
 
 /* Matrix to rotate accelrator into standard reference frame */
-static const mat33_fp_t base_standard_ref = {
-	{ 0, FLOAT_TO_FP(1), 0},
-	{ FLOAT_TO_FP(1), 0, 0},
-	{ 0, 0, FLOAT_TO_FP(-1)}
-};
+static const mat33_fp_t base_standard_ref = { { 0, FLOAT_TO_FP(1), 0 },
+					      { FLOAT_TO_FP(1), 0, 0 },
+					      { 0, 0, FLOAT_TO_FP(-1) } };
 
 /*
  * TODO(b/124337208): P0 boards don't have this sensor mounted so the rotation
  * matrix can't be tested properly. This needs to be revisited after EVT to make
  * sure the rotation matrix for the lid sensor is correct.
  */
-static const mat33_fp_t lid_standard_ref = {
-	{ 0, FLOAT_TO_FP(-1), 0},
-	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, 0, FLOAT_TO_FP(-1)}
-};
+static const mat33_fp_t lid_standard_ref = { { 0, FLOAT_TO_FP(-1), 0 },
+					     { FLOAT_TO_FP(-1), 0, 0 },
+					     { 0, 0, FLOAT_TO_FP(-1) } };
 
 struct motion_sensor_t motion_sensors[] = {
 	[LID_ACCEL] = {
@@ -363,14 +360,14 @@ BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 
 const struct fan_conf fan_conf_0 = {
 	.flags = FAN_USE_RPM_MODE,
-	.ch = MFT_CH_0,	/* Use MFT id to control fan */
+	.ch = MFT_CH_0, /* Use MFT id to control fan */
 	.pgood_gpio = -1,
 	.enable_gpio = GPIO_EN_PP5000_FAN,
 };
 
 const struct fan_conf fan_conf_1 = {
 	.flags = FAN_USE_RPM_MODE,
-	.ch = MFT_CH_1,	/* Use MFT id to control fan */
+	.ch = MFT_CH_1, /* Use MFT id to control fan */
 	.pgood_gpio = -1,
 	.enable_gpio = GPIO_EN_PP5000_FAN,
 };
@@ -394,60 +391,47 @@ const struct fan_t fans[FAN_CH_COUNT] = {
 /******************************************************************************/
 /* MFT channels. These are logically separate from pwm_channels. */
 const struct mft_t mft_channels[] = {
-	[MFT_CH_0] = {NPCX_MFT_MODULE_1, TCKC_LFCLK, PWM_CH_FAN},
-	[MFT_CH_1] = {NPCX_MFT_MODULE_2, TCKC_LFCLK, PWM_CH_FAN2},
+	[MFT_CH_0] = { NPCX_MFT_MODULE_1, TCKC_LFCLK, PWM_CH_FAN },
+	[MFT_CH_1] = { NPCX_MFT_MODULE_2, TCKC_LFCLK, PWM_CH_FAN2 },
 };
 BUILD_ASSERT(ARRAY_SIZE(mft_channels) == MFT_CH_COUNT);
 
 /* ADC channels */
 const struct adc_t adc_channels[] = {
-	[ADC_TEMP_SENSOR_1] = {
-		"TEMP_CHARGER", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
-	[ADC_TEMP_SENSOR_2] = {
-		"TEMP_5V", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+	[ADC_TEMP_SENSOR_1] = { "TEMP_CHARGER", NPCX_ADC_CH0, ADC_MAX_VOLT,
+				ADC_READ_MAX + 1, 0 },
+	[ADC_TEMP_SENSOR_2] = { "TEMP_5V", NPCX_ADC_CH1, ADC_MAX_VOLT,
+				ADC_READ_MAX + 1, 0 },
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 const struct temp_sensor_t temp_sensors[] = {
-	[TEMP_CHARGER] = {
-		.name = "CHARGER",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_1
-	},
-	[TEMP_5V] = {
-		.name = "5V",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = get_temp_3v3_30k9_47k_4050b,
-		.idx = ADC_TEMP_SENSOR_2
-	},
-	[TEMP_GPU] = {
-		.name = "GPU",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = get_temp_R19ME4070,
-		.idx = R19ME4070_LOCAL
-	},
-	[TEMP_F75303_LOCAL] = {
-		.name = "F75303_Local",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = f75303_get_val,
-		.idx = F75303_IDX_LOCAL
-	},
-	[TEMP_F75303_GPU] = {
-		.name = "F75303_GPU",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = f75303_get_val,
-		.idx = F75303_IDX_REMOTE1
-	},
-	[TEMP_F75303_GPU_POWER] = {
-		.name = "F75303_GPU_Power",
-		.type = TEMP_SENSOR_TYPE_BOARD,
-		.read = f75303_get_val,
-		.idx = F75303_IDX_REMOTE2
-	},
+	[TEMP_CHARGER] = { .name = "CHARGER",
+			   .type = TEMP_SENSOR_TYPE_BOARD,
+			   .read = get_temp_3v3_30k9_47k_4050b,
+			   .idx = ADC_TEMP_SENSOR_1 },
+	[TEMP_5V] = { .name = "5V",
+		      .type = TEMP_SENSOR_TYPE_BOARD,
+		      .read = get_temp_3v3_30k9_47k_4050b,
+		      .idx = ADC_TEMP_SENSOR_2 },
+	[TEMP_GPU] = { .name = "GPU",
+		       .type = TEMP_SENSOR_TYPE_BOARD,
+		       .read = get_temp_R19ME4070,
+		       .idx = R19ME4070_LOCAL },
+	[TEMP_F75303_LOCAL] = { .name = "F75303_Local",
+				.type = TEMP_SENSOR_TYPE_BOARD,
+				.read = f75303_get_val,
+				.idx = F75303_IDX_LOCAL },
+	[TEMP_F75303_GPU] = { .name = "F75303_GPU",
+			      .type = TEMP_SENSOR_TYPE_BOARD,
+			      .read = f75303_get_val,
+			      .idx = F75303_IDX_REMOTE1 },
+	[TEMP_F75303_GPU_POWER] = { .name = "F75303_GPU_Power",
+				    .type = TEMP_SENSOR_TYPE_BOARD,
+				    .read = f75303_get_val,
+				    .idx = F75303_IDX_REMOTE2 },
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
-
 
 /* Hatch Temperature sensors */
 /*
@@ -458,8 +442,8 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 /*
  * TODO(b/202062363): Remove when clang is fixed.
  */
-#define THERMAL_A \
-	{ \
+#define THERMAL_A                \
+	{                        \
 		.temp_host = { \
 			[EC_TEMP_THRESH_WARN] = 0, \
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(75), \
@@ -478,8 +462,8 @@ __maybe_unused static const struct ec_thermal_config thermal_a = THERMAL_A;
 /*
  * TODO(b/202062363): Remove when clang is fixed.
  */
-#define THERMAL_B \
-	{ \
+#define THERMAL_B                \
+	{                        \
 		.temp_host = { \
 			[EC_TEMP_THRESH_WARN] = 0, \
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(75), \
@@ -494,7 +478,6 @@ __maybe_unused static const struct ec_thermal_config thermal_a = THERMAL_A;
 		.temp_fan_max = C_TO_K(70), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_b = THERMAL_B;
-
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT];
 
@@ -536,7 +519,6 @@ static void board_gpio_set_pp5000(void)
 	} else if (board_id >= 1) {
 		reset_gpio_flags(GPIO_EN_PP5000_A_V1, GPIO_OUT_LOW);
 	}
-
 }
 
 static void board_init(void)
