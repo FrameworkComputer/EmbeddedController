9edf64f19cb510f7505751586bef08c46969a6c0
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:14:15 -0600

    builtin/stdbool.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic40cf953745bb9064bf2f0b9b49374aab60462e9
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729145
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- builtin/stdbool.h
+++ builtin/stdbool.h
@@ -6,8 +6,8 @@
 #ifndef __CROS_EC_STDBOOL_H__
 #define __CROS_EC_STDBOOL_H__
 
-#define bool    _Bool
-#define true    1
-#define false   0
+#define bool _Bool
+#define true 1
+#define false 0
 
 #endif /* __CROS_EC_STDBOOL_H__ */
