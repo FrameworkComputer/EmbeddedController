294dfa9ca48d4044c0c137a129a7584c70223d39
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:27:28 -0600

    chip/stm32/config-stm32f07x.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8e34b4f14f42539a7e15a52c0ce83f011d46127b
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729471
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/config-stm32f07x.h
+++ chip/stm32/config-stm32f07x.h
@@ -5,15 +5,15 @@
 
 /* Memory mapping */
 #define CONFIG_FLASH_SIZE_BYTES (128 * 1024)
-#define CONFIG_FLASH_BANK_SIZE  0x1000
-#define CONFIG_FLASH_ERASE_SIZE 0x0800  /* erase bank size */
-#define CONFIG_FLASH_WRITE_SIZE 0x0002  /* minimum write size */
+#define CONFIG_FLASH_BANK_SIZE 0x1000
+#define CONFIG_FLASH_ERASE_SIZE 0x0800 /* erase bank size */
+#define CONFIG_FLASH_WRITE_SIZE 0x0002 /* minimum write size */
 
 /* No page mode on STM32F, so no benefit to larger write sizes */
 #define CONFIG_FLASH_WRITE_IDEAL_SIZE 0x0002
 
-#define CONFIG_RAM_BASE         0x20000000
-#define CONFIG_RAM_SIZE         0x00004000
+#define CONFIG_RAM_BASE 0x20000000
+#define CONFIG_RAM_SIZE 0x00004000
 
 /* Number of IRQ vectors on the NVIC */
 #define CONFIG_IRQ_COUNT 32
@@ -23,10 +23,10 @@
 #define CONFIG_CONSOLE_HISTORY 3
 
 /* USB packet ram config */
-#define CONFIG_USB_RAM_BASE        0x40006000
-#define CONFIG_USB_RAM_SIZE        1024
+#define CONFIG_USB_RAM_BASE 0x40006000
+#define CONFIG_USB_RAM_SIZE 1024
 #define CONFIG_USB_RAM_ACCESS_TYPE uint16_t
 #define CONFIG_USB_RAM_ACCESS_SIZE 2
 
 /* DFU Address */
-#define STM32_DFU_BASE              0x1fffC800
+#define STM32_DFU_BASE 0x1fffC800
