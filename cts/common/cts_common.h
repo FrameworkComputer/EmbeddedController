0cb7aa5b745930fa24d5554e3bfbcfb6db0575e9
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:45:05 -0600

    cts/common/cts_common.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic04639838f2be7b1942974e0224d069997011e08
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729740
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- cts/common/cts_common.h
+++ cts/common/cts_common.h
@@ -10,16 +10,16 @@
 
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_SYSTEM, outstr)
-#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
-#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
-#define CPRINTL(format, args...) CPRINTS("%s:%d: "format, \
-					 __func__, __LINE__, ## args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+#define CPRINTL(format, args...) \
+	CPRINTS("%s:%d: " format, __func__, __LINE__, ##args)
 
-#define READ_WAIT_TIME_MS		100
-#define CTS_INTERRUPT_TRIGGER_DELAY_US	(250 * MSEC)
+#define READ_WAIT_TIME_MS 100
+#define CTS_INTERRUPT_TRIGGER_DELAY_US (250 * MSEC)
 
 enum cts_rc {
-	#include "cts.rc"
+#include "cts.rc"
 };
 
 struct cts_test {
@@ -38,7 +38,7 @@ extern const int cts_test_count;
  * @test: List of tests to run
  * @name: Name of the test to be printed on EC console
  */
-void cts_main_loop(const struct cts_test* tests, const char *name);
+void cts_main_loop(const struct cts_test *tests, const char *name);
 
 /**
  * Callback function called at the beginning of the main loop
