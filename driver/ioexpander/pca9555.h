05c7e8365835b51b1a3e4a977c9b4ab2ae9e919f
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:50:43 -0600

    driver/ioexpander/pca9555.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I4763a4e4c151b7576aba307c69722e3bbcd1e735
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729991
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/ioexpander/pca9555.h
+++ driver/ioexpander/pca9555.h
@@ -10,33 +10,31 @@
 
 #include "i2c.h"
 
-#define PCA9555_CMD_INPUT_PORT_0		0
-#define PCA9555_CMD_INPUT_PORT_1		1
-#define PCA9555_CMD_OUTPUT_PORT_0		2
-#define PCA9555_CMD_OUTPUT_PORT_1		3
-#define PCA9555_CMD_POLARITY_INVERSION_PORT_0	4
-#define PCA9555_CMD_POLARITY_INVERSION_PORT_1	5
-#define PCA9555_CMD_CONFIGURATION_PORT_0	6
-#define PCA9555_CMD_CONFIGURATION_PORT_1	7
+#define PCA9555_CMD_INPUT_PORT_0 0
+#define PCA9555_CMD_INPUT_PORT_1 1
+#define PCA9555_CMD_OUTPUT_PORT_0 2
+#define PCA9555_CMD_OUTPUT_PORT_1 3
+#define PCA9555_CMD_POLARITY_INVERSION_PORT_0 4
+#define PCA9555_CMD_POLARITY_INVERSION_PORT_1 5
+#define PCA9555_CMD_CONFIGURATION_PORT_0 6
+#define PCA9555_CMD_CONFIGURATION_PORT_1 7
 
-#define PCA9555_IO_0	BIT(0)
-#define PCA9555_IO_1	BIT(1)
-#define PCA9555_IO_2	BIT(2)
-#define PCA9555_IO_3	BIT(3)
-#define PCA9555_IO_4	BIT(4)
-#define PCA9555_IO_5	BIT(5)
-#define PCA9555_IO_6	BIT(6)
-#define PCA9555_IO_7	BIT(7)
+#define PCA9555_IO_0 BIT(0)
+#define PCA9555_IO_1 BIT(1)
+#define PCA9555_IO_2 BIT(2)
+#define PCA9555_IO_3 BIT(3)
+#define PCA9555_IO_4 BIT(4)
+#define PCA9555_IO_5 BIT(5)
+#define PCA9555_IO_6 BIT(6)
+#define PCA9555_IO_7 BIT(7)
 
-static inline int pca9555_read(const int port,
-			       const uint16_t i2c_addr_flags,
+static inline int pca9555_read(const int port, const uint16_t i2c_addr_flags,
 			       int reg, int *data_ptr)
 {
 	return i2c_read8(port, i2c_addr_flags, reg, data_ptr);
 }
 
-static inline int pca9555_write(const int port,
-				const uint16_t i2c_addr_flags,
+static inline int pca9555_write(const int port, const uint16_t i2c_addr_flags,
 				int reg, int data)
 {
 	return i2c_write8(port, i2c_addr_flags, reg, data);
