b5c574353419ed888682df21ad09c50a6b15461c
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:00:42 -0600

    board/primus/ps2.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic56befd87b1a2158e41ed78e65d688e94aabc664
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728843
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/primus/ps2.h
+++ board/primus/ps2.h
@@ -12,20 +12,20 @@
  * 0x01 was the original IBM trackpoint, others implement very limited
  * subset of trackpoint features.
  */
-#define TP_READ_ID			0xE1	/* Sent for device identification */
+#define TP_READ_ID 0xE1 /* Sent for device identification */
 
-#define TP_COMMAND			0xE2	/* Commands start with this */
+#define TP_COMMAND 0xE2 /* Commands start with this */
 
 /*
  * Toggling Flag bits
  */
-#define TP_TOGGLE			0x47	/* Toggle command */
+#define TP_TOGGLE 0x47 /* Toggle command */
 
-#define TP_VARIANT_ELAN			0x03
-#define TP_VARIANT_SYNAPTICS		0x06
-#define TP_TOGGLE_SOURCE_TAG		0x20
-#define TP_TOGGLE_BURST			0x28
-#define TP_TOGGLE_SNAPTICS_SLEEP	0x10
-#define TP_TOGGLE_ELAN_SLEEP		0x8
+#define TP_VARIANT_ELAN 0x03
+#define TP_VARIANT_SYNAPTICS 0x06
+#define TP_TOGGLE_SOURCE_TAG 0x20
+#define TP_TOGGLE_BURST 0x28
+#define TP_TOGGLE_SNAPTICS_SLEEP 0x10
+#define TP_TOGGLE_ELAN_SLEEP 0x8
 
 #endif /* __CROS_EC_PRIMUS_PS2_H */
