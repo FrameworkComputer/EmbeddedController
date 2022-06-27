8ba4dce4b0a07a208081d37bfe88eaca661d811d
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:06:19 -0600

    board/stm32f446e-eval/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0437961704fe5ab8b0917b3b52ea7cc3f9144622
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728969
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/stm32f446e-eval/board.c
+++ board/stm32f446e-eval/board.c
@@ -20,12 +20,12 @@
  * Define the strings used in our USB descriptors.
  */
 const void *const usb_strings[] = {
-	[USB_STR_DESC]		= usb_string_desc,
-	[USB_STR_VENDOR]	= USB_STRING_DESC("Google Inc."),
-	[USB_STR_PRODUCT]	= USB_STRING_DESC("stm32f446-eval"),
-	[USB_STR_SERIALNO]	= USB_STRING_DESC("1234-a"),
-	[USB_STR_VERSION]	= USB_STRING_DESC(CROS_EC_VERSION32),
-	[USB_STR_CONSOLE_NAME]	= USB_STRING_DESC("EC Shell"),
+	[USB_STR_DESC] = usb_string_desc,
+	[USB_STR_VENDOR] = USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT] = USB_STRING_DESC("stm32f446-eval"),
+	[USB_STR_SERIALNO] = USB_STRING_DESC("1234-a"),
+	[USB_STR_VERSION] = USB_STRING_DESC(CROS_EC_VERSION32),
+	[USB_STR_CONSOLE_NAME] = USB_STRING_DESC("EC Shell"),
 };
 
 BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
@@ -43,25 +43,21 @@ struct dwc_usb usb_ctl = {
 
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
-	{
-		.name = "i2c1",
-		.port = I2C_PORT_0,
-		.kbps = 100,
-		.scl  = GPIO_I2C1_SCL,
-		.sda  = GPIO_I2C1_SDA
-	},
-	{
-		.name = "fmpi2c4",
-		.port = FMPI2C_PORT_3,
-		.kbps = 100,
-		.scl  = GPIO_FMPI2C_SCL,
-		.sda  = GPIO_FMPI2C_SDA
-	},
+	{ .name = "i2c1",
+	  .port = I2C_PORT_0,
+	  .kbps = 100,
+	  .scl = GPIO_I2C1_SCL,
+	  .sda = GPIO_I2C1_SDA },
+	{ .name = "fmpi2c4",
+	  .port = FMPI2C_PORT_3,
+	  .kbps = 100,
+	  .scl = GPIO_FMPI2C_SCL,
+	  .sda = GPIO_FMPI2C_SDA },
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
-#define GPIO_SET_HS(bank, number)	\
-	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number) * 2)))
+#define GPIO_SET_HS(bank, number) \
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number)*2)))
 
 void board_config_post_gpio_init(void)
 {
@@ -74,27 +70,27 @@ void board_config_post_gpio_init(void)
 	GPIO_SET_HS(A, 11);
 	GPIO_SET_HS(A, 12);
 
-	GPIO_SET_HS(C,  3);
-	GPIO_SET_HS(C,  2);
-	GPIO_SET_HS(C,  0);
-	GPIO_SET_HS(A,  5);
+	GPIO_SET_HS(C, 3);
+	GPIO_SET_HS(C, 2);
+	GPIO_SET_HS(C, 0);
+	GPIO_SET_HS(A, 5);
 
-	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 5);
 	GPIO_SET_HS(B, 13);
 	GPIO_SET_HS(B, 12);
-	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 2);
 	GPIO_SET_HS(B, 10);
-	GPIO_SET_HS(B,  1);
-	GPIO_SET_HS(B,  0);
-	GPIO_SET_HS(A,  3);
+	GPIO_SET_HS(B, 1);
+	GPIO_SET_HS(B, 0);
+	GPIO_SET_HS(A, 3);
 
 	/* Set I2C GPIO to HS */
-	GPIO_SET_HS(B,  6);
-	GPIO_SET_HS(B,  7);
-	GPIO_SET_HS(F,  1);
-	GPIO_SET_HS(F,  0);
-	GPIO_SET_HS(A,  8);
-	GPIO_SET_HS(B,  4);
-	GPIO_SET_HS(C,  6);
-	GPIO_SET_HS(C,  7);
+	GPIO_SET_HS(B, 6);
+	GPIO_SET_HS(B, 7);
+	GPIO_SET_HS(F, 1);
+	GPIO_SET_HS(F, 0);
+	GPIO_SET_HS(A, 8);
+	GPIO_SET_HS(B, 4);
+	GPIO_SET_HS(C, 6);
+	GPIO_SET_HS(C, 7);
 }
