400275d3547418a7cdea853c43ab4eb28ffd91e6
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:55:53 -0600

    board/nipperkin/board_fw_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8f73139b4643081d48709cf8f90ff33d9a71ea1b
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728753
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/nipperkin/board_fw_config.h
+++ board/nipperkin/board_fw_config.h
@@ -13,10 +13,10 @@
 /*
  * Keyboard Backlight (1 bit)
  */
-#define FW_CONFIG_KBLIGHT_OFFSET		0
-#define FW_CONFIG_KBLIGHT_WIDTH			1
-#define FW_CONFIG_KBLIGHT_NO			0
-#define FW_CONFIG_KBLIGHT_YES			1
+#define FW_CONFIG_KBLIGHT_OFFSET 0
+#define FW_CONFIG_KBLIGHT_WIDTH 1
+#define FW_CONFIG_KBLIGHT_NO 0
+#define FW_CONFIG_KBLIGHT_YES 1
 
 /*
  * Bit 1 ~ 6 not related to EC function
@@ -25,10 +25,10 @@
 /*
  * Keyboard (1 bit)
  */
-#define FW_CONFIG_KEYBOARD_OFFSET		7
-#define FW_CONFIG_KEYBOARD_WIDTH		1
-#define FW_CONFIG_KEYBOARD_PRIVACY_YES		0
-#define FW_CONFIG_KEYBOARD_PRIVACY_NO		1
+#define FW_CONFIG_KEYBOARD_OFFSET 7
+#define FW_CONFIG_KEYBOARD_WIDTH 1
+#define FW_CONFIG_KEYBOARD_PRIVACY_YES 0
+#define FW_CONFIG_KEYBOARD_PRIVACY_NO 1
 
 bool board_has_privacy_panel(void);
 
