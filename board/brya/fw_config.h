d77f639c74eb536d12f4482854ebd4b7e3f11ae7
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:24:27 -0600

    board/brya/fw_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If3ee5472402005393d502e05db72420f8e573f5f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728101
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/brya/fw_config.h
+++ board/brya/fw_config.h
@@ -27,12 +27,12 @@ enum ec_cfg_keyboard_backlight_type {
 
 union brya_cbi_fw_config {
 	struct {
-		enum ec_cfg_usb_db_type			usb_db : 4;
-		uint32_t				sd_db : 2;
-		uint32_t				lte_db : 1;
-		enum ec_cfg_keyboard_backlight_type	kb_bl : 1;
-		uint32_t				audio : 3;
-		uint32_t				reserved_1 : 21;
+		enum ec_cfg_usb_db_type usb_db : 4;
+		uint32_t sd_db : 2;
+		uint32_t lte_db : 1;
+		enum ec_cfg_keyboard_backlight_type kb_bl : 1;
+		uint32_t audio : 3;
+		uint32_t reserved_1 : 21;
 	};
 	uint32_t raw_value;
 };
