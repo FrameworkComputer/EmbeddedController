b6d2cf710ca9c84fc666eac2266d42246628a5cd
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:43:10 -0600

    board/kukui_scp/mdp_ipi_message.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If0cb81c2e91273eabb97cedab439a418093396fd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728597
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/kukui_scp/mdp_ipi_message.h
+++ board/kukui_scp/mdp_ipi_message.h
@@ -11,9 +11,10 @@ struct mdp_msg_service {
 	unsigned char msg[20];
 };
 
-BUILD_ASSERT(member_size(struct mdp_msg_service, msg) <= CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
+BUILD_ASSERT(member_size(struct mdp_msg_service, msg) <=
+	     CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
 
 void mdp_common_init(void);
 void mdp_ipi_task_handler(void *pvParameters);
 
-#endif  // _MDP_IPI_MESSAGE_H
+#endif // _MDP_IPI_MESSAGE_H
