16313ab6c4d6158650a9243784495060b61f6ee6
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:38:18 -0600

    zephyr/shim/src/led_driver/led.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie005916fb68efea716ab9c35636f9b61c3184ba9
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727424
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/src/led_driver/led.h
+++ zephyr/shim/src/led_driver/led.h
@@ -11,34 +11,32 @@
 #include <zephyr/drivers/pwm.h>
 
 #define COMPAT_GPIO_LED cros_ec_gpio_led_pins
-#define COMPAT_PWM_LED  cros_ec_pwm_led_pins
+#define COMPAT_PWM_LED cros_ec_pwm_led_pins
 
-#define PINS_NODE(id)	DT_CAT(PIN_NODE_, id)
-#define PINS_ARRAY(id)	DT_CAT(PINS_ARRAY_, id)
+#define PINS_NODE(id) DT_CAT(PIN_NODE_, id)
+#define PINS_ARRAY(id) DT_CAT(PINS_ARRAY_, id)
 
 /*
  * Return string-token if the property exists, otherwise return 0
  */
-#define GET_PROP(id, prop)						\
-	COND_CODE_1(DT_NODE_HAS_PROP(id, prop),				\
-		    (DT_STRING_UPPER_TOKEN(id, prop)),			\
-		    (0))
+#define GET_PROP(id, prop)                      \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, prop), \
+		    (DT_STRING_UPPER_TOKEN(id, prop)), (0))
 
 /*
  * Return string-token if the property exists, otherwise return -1
  */
-#define GET_PROP_NVE(id, prop)						\
-	COND_CODE_1(DT_NODE_HAS_PROP(id, prop),				\
-		    (DT_STRING_UPPER_TOKEN(id, prop)),			\
-		    (-1))
+#define GET_PROP_NVE(id, prop)                  \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, prop), \
+		    (DT_STRING_UPPER_TOKEN(id, prop)), (-1))
 
-#define LED_ENUM(id, enum_name)	 DT_STRING_TOKEN(id, enum_name)
-#define LED_ENUM_WITH_COMMA(id, enum_name)				\
-	COND_CODE_1(DT_NODE_HAS_PROP(id, enum_name),			\
-		    (LED_ENUM(id, enum_name),), ())
+#define LED_ENUM(id, enum_name) DT_STRING_TOKEN(id, enum_name)
+#define LED_ENUM_WITH_COMMA(id, enum_name)           \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, enum_name), \
+		    (LED_ENUM(id, enum_name), ), ())
 
 #define GPIO_LED_PINS_NODE DT_PATH(gpio_led_pins)
-#define PWM_LED_PINS_NODE  DT_PATH(pwm_led_pins)
+#define PWM_LED_PINS_NODE DT_PATH(pwm_led_pins)
 
 enum led_color {
 	LED_OFF,
@@ -48,7 +46,7 @@ enum led_color {
 	LED_YELLOW,
 	LED_WHITE,
 	LED_AMBER,
-	LED_COLOR_COUNT  /* Number of colors, not a color itself */
+	LED_COLOR_COUNT /* Number of colors, not a color itself */
 };
 
 /*
