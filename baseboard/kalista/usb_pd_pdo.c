d70cb7c8aeba56514ff084c9e3552acac508ad78
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:16:40 -0600

    baseboard/kalista/usb_pd_pdo.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I5d0f9745fe478c58a03bced13e04fd5e0040ed81
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727912
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/kalista/usb_pd_pdo.c
+++ baseboard/kalista/usb_pd_pdo.c
@@ -7,9 +7,8 @@
 #include "usb_pd.h"
 #include "usb_pd_pdo.h"
 
-#define PDO_FIXED_FLAGS (PDO_FIXED_UNCONSTRAINED | \
-			 PDO_FIXED_DATA_SWAP | \
-			 PDO_FIXED_COMM_CAP)
+#define PDO_FIXED_FLAGS \
+	(PDO_FIXED_UNCONSTRAINED | PDO_FIXED_DATA_SWAP | PDO_FIXED_COMM_CAP)
 
 const uint32_t pd_src_pdo[] = {
 	PDO_FIXED(5000, 3000, PDO_FIXED_FLAGS),
