1119032aa0c2c7c0ac3e36fe9e13a16a2df12ce6
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:17:53 -0600

    include/usb_tbt_alt_mode.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I015a8fe973704ecb6bcad6a58f8e2e01c0e9ccb1
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730449
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/usb_tbt_alt_mode.h
+++ include/usb_tbt_alt_mode.h
@@ -78,7 +78,7 @@ bool tbt_is_active(int port);
  * @param vdm       VDM from ACK
  */
 void intel_vdm_acked(int port, enum tcpci_msg_type type, int vdo_count,
-		uint32_t *vdm);
+		     uint32_t *vdm);
 
 /*
  * Handles NAKed (or Not Supported or timed out) Thunderbolt VDM requests.
