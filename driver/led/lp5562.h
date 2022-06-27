426799a19b873c1dc7020c4cf5ae857792500857
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:51:25 -0600

    driver/led/lp5562.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie72c5d767dd9be0fef205dde765bfd1173d0e062
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730016
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/led/lp5562.h
+++ driver/led/lp5562.h
@@ -8,40 +8,40 @@
 #ifndef __CROS_EC_LP5562_H
 #define __CROS_EC_LP5562_H
 
-#define LP5562_REG_ENABLE	0x00
-#define LP5562_REG_OP_MODE	0x01
-#define LP5562_REG_B_PWM	0x02
-#define LP5562_REG_G_PWM	0x03
-#define LP5562_REG_R_PWM	0x04
-#define LP5562_REG_B_CURRENT	0x05
-#define LP5562_REG_G_CURRENT	0x06
-#define LP5562_REG_R_CURRENT	0x07
-#define LP5562_REG_CONFIG	0x08
-#define LP5562_REG_ENG1_PC	0x09
-#define LP5562_REG_ENG2_PC	0x0a
-#define LP5562_REG_ENG3_PC	0x0b
-#define LP5562_REG_STATUS	0x0c
-#define LP5562_REG_RESET	0x0d
-#define LP5562_REG_W_PWM	0x0e
-#define LP5562_REG_W_CURRENT	0x0f
-#define LP5562_REG_LED_MAP	0x70
+#define LP5562_REG_ENABLE 0x00
+#define LP5562_REG_OP_MODE 0x01
+#define LP5562_REG_B_PWM 0x02
+#define LP5562_REG_G_PWM 0x03
+#define LP5562_REG_R_PWM 0x04
+#define LP5562_REG_B_CURRENT 0x05
+#define LP5562_REG_G_CURRENT 0x06
+#define LP5562_REG_R_CURRENT 0x07
+#define LP5562_REG_CONFIG 0x08
+#define LP5562_REG_ENG1_PC 0x09
+#define LP5562_REG_ENG2_PC 0x0a
+#define LP5562_REG_ENG3_PC 0x0b
+#define LP5562_REG_STATUS 0x0c
+#define LP5562_REG_RESET 0x0d
+#define LP5562_REG_W_PWM 0x0e
+#define LP5562_REG_W_CURRENT 0x0f
+#define LP5562_REG_LED_MAP 0x70
 
-#define LP5562_REG_ENG_PROG(n)	(0x10 + ((n)-1) * 0x20)
+#define LP5562_REG_ENG_PROG(n) (0x10 + ((n)-1) * 0x20)
 
 /* Brightness range: 0x00 - 0xff */
-#define LP5562_COLOR_NONE	0x000000
-#define LP5562_COLOR_RED(b)	(0x010000 * (b))
-#define LP5562_COLOR_GREEN(b)	(0x000100 * (b))
-#define LP5562_COLOR_BLUE(b)	(0x000001 * (b))
+#define LP5562_COLOR_NONE 0x000000
+#define LP5562_COLOR_RED(b) (0x010000 * (b))
+#define LP5562_COLOR_GREEN(b) (0x000100 * (b))
+#define LP5562_COLOR_BLUE(b) (0x000001 * (b))
 
-#define LP5562_ENG_SEL_NONE	0x0
-#define LP5562_ENG_SEL_1	0x1
-#define LP5562_ENG_SEL_2	0x2
-#define LP5562_ENG_SEL_3	0x3
+#define LP5562_ENG_SEL_NONE 0x0
+#define LP5562_ENG_SEL_1 0x1
+#define LP5562_ENG_SEL_2 0x2
+#define LP5562_ENG_SEL_3 0x3
 
-#define LP5562_ENG_HOLD		0x0
-#define LP5562_ENG_STEP		0x1
-#define LP5562_ENG_RUN		0x2
+#define LP5562_ENG_HOLD 0x0
+#define LP5562_ENG_STEP 0x1
+#define LP5562_ENG_RUN 0x2
 
 /* Power on and initialize LP5562. */
 int lp5562_poweron(void);
