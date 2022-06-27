aeda14dfdf7508f63be00dfcc97e16315e94af20
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:50:33 -0600

    driver/ioexpander/it8801.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ib0dbe9d2e9ccc1bd4ef6f40cb5625acf8c73d456
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729986
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/ioexpander/it8801.h
+++ driver/ioexpander/it8801.h
@@ -14,79 +14,81 @@
 #define IT8801_I2C_ADDR2 0x39
 
 /* Keyboard Matrix Scan control (KBS) */
-#define IT8801_REG_KSOMCR               0x40
-#define IT8801_REG_MASK_KSOSDIC         BIT(7)
-#define IT8801_REG_MASK_KSE             BIT(6)
-#define IT8801_REG_MASK_AKSOSC          BIT(5)
-#define IT8801_REG_KSIDR                0x41
-#define IT8801_REG_KSIEER               0x42
-#define IT8801_REG_KSIIER               0x43
-#define IT8801_REG_SMBCR                0xfa
-#define IT8801_REG_MASK_ARE             BIT(4)
-#define IT8801_REG_GIECR                0xfb
-#define IT8801_REG_MASK_GKSIIE          BIT(3)
-#define IT8801_REG_GPIO10               0x12
-#define IT8801_REG_GPIO00_KSO19         0x0a
-#define IT8801_REG_GPIO01_KSO18         0x0b
-#define IT8801_REG_GPIO22_KSO21         0x1c
-#define IT8801_REG_GPIO23_KSO20         0x1d
-#define IT8801_REG_MASK_GPIOAFS_PULLUP  BIT(7)
-#define IT8801_REG_MASK_GPIOAFS_FUNC2   BIT(6)
-#define IT8801_REG_MASK_GPIODIR         BIT(5)
-#define IT8801_REG_MASK_GPIOPUE         BIT(0)
-#define IT8801_REG_GPIO23SOV            BIT(3)
-#define IT8801_REG_MASK_SELKSO2         0x02
-#define IT8801_REG_GISR                 0xF9
-#define IT8801_REG_MASK_GISR_GKSIIS     BIT(6)
-#define IT8801_REG_MASK_GISR_GGPIOG2IS  BIT(2)
-#define IT8801_REG_MASK_GISR_GGPIOG1IS  BIT(1)
-#define IT8801_REG_MASK_GISR_GGPIOG0IS  BIT(0)
-#define IT8801_REG_MASK_GISR_GGPIOGXIS  (IT8801_REG_MASK_GISR_GGPIOG2IS | \
-		IT8801_REG_MASK_GISR_GGPIOG1IS | IT8801_REG_MASK_GISR_GGPIOG0IS)
-#define IT8801_REG_LBVIDR               0xFE
-#define IT8801_REG_HBVIDR               0xFF
-#define IT8801_KSO_COUNT                18
+#define IT8801_REG_KSOMCR 0x40
+#define IT8801_REG_MASK_KSOSDIC BIT(7)
+#define IT8801_REG_MASK_KSE BIT(6)
+#define IT8801_REG_MASK_AKSOSC BIT(5)
+#define IT8801_REG_KSIDR 0x41
+#define IT8801_REG_KSIEER 0x42
+#define IT8801_REG_KSIIER 0x43
+#define IT8801_REG_SMBCR 0xfa
+#define IT8801_REG_MASK_ARE BIT(4)
+#define IT8801_REG_GIECR 0xfb
+#define IT8801_REG_MASK_GKSIIE BIT(3)
+#define IT8801_REG_GPIO10 0x12
+#define IT8801_REG_GPIO00_KSO19 0x0a
+#define IT8801_REG_GPIO01_KSO18 0x0b
+#define IT8801_REG_GPIO22_KSO21 0x1c
+#define IT8801_REG_GPIO23_KSO20 0x1d
+#define IT8801_REG_MASK_GPIOAFS_PULLUP BIT(7)
+#define IT8801_REG_MASK_GPIOAFS_FUNC2 BIT(6)
+#define IT8801_REG_MASK_GPIODIR BIT(5)
+#define IT8801_REG_MASK_GPIOPUE BIT(0)
+#define IT8801_REG_GPIO23SOV BIT(3)
+#define IT8801_REG_MASK_SELKSO2 0x02
+#define IT8801_REG_GISR 0xF9
+#define IT8801_REG_MASK_GISR_GKSIIS BIT(6)
+#define IT8801_REG_MASK_GISR_GGPIOG2IS BIT(2)
+#define IT8801_REG_MASK_GISR_GGPIOG1IS BIT(1)
+#define IT8801_REG_MASK_GISR_GGPIOG0IS BIT(0)
+#define IT8801_REG_MASK_GISR_GGPIOGXIS                                     \
+	(IT8801_REG_MASK_GISR_GGPIOG2IS | IT8801_REG_MASK_GISR_GGPIOG1IS | \
+	 IT8801_REG_MASK_GISR_GGPIOG0IS)
+#define IT8801_REG_LBVIDR 0xFE
+#define IT8801_REG_HBVIDR 0xFF
+#define IT8801_KSO_COUNT 18
 
 /* General Purpose I/O Port (GPIO) */
-#define IT8801_SUPPORT_GPIO_FLAGS (GPIO_OPEN_DRAIN | GPIO_INPUT | \
-		GPIO_OUTPUT | GPIO_LOW | GPIO_HIGH | GPIO_INT_ANY)
+#define IT8801_SUPPORT_GPIO_FLAGS                                            \
+	(GPIO_OPEN_DRAIN | GPIO_INPUT | GPIO_OUTPUT | GPIO_LOW | GPIO_HIGH | \
+	 GPIO_INT_ANY)
 
-#define IT8801_REG_MASK_GPIOAFS_FUNC1   (0x00 << 7)
+#define IT8801_REG_MASK_GPIOAFS_FUNC1 (0x00 << 7)
 
 /* IT8801 only supports GPIO 0/1/2 */
-#define IT8801_VALID_GPIO_G0_MASK       0xD9
-#define IT8801_VALID_GPIO_G1_MASK       0x3F
-#define IT8801_VALID_GPIO_G2_MASK       0x0F
+#define IT8801_VALID_GPIO_G0_MASK 0xD9
+#define IT8801_VALID_GPIO_G1_MASK 0x3F
+#define IT8801_VALID_GPIO_G2_MASK 0x0F
 
 extern __override_proto const uint8_t it8801_kso_mapping[];
 extern const struct ioexpander_drv it8801_ioexpander_drv;
 
 /* GPIO Register map */
 /* Input pin status register */
-#define IT8801_REG_GPIO_IPSR(port)      (0x00 + (port))
+#define IT8801_REG_GPIO_IPSR(port) (0x00 + (port))
 /* Set output value register */
-#define IT8801_REG_GPIO_SOVR(port)      (0x05 + (port))
+#define IT8801_REG_GPIO_SOVR(port) (0x05 + (port))
 /* Control register */
-#define IT8801_REG_GPIO_CR(port, mask)  \
-	(0x0A + (port) * 8 + GPIO_MASK_TO_NUM(mask))
+#define IT8801_REG_GPIO_CR(port, mask) \
+	(0x0A + (port)*8 + GPIO_MASK_TO_NUM(mask))
 /* Interrupt status register */
-#define IT8801_REG_GPIO_ISR(port)       (0x32 + (port))
+#define IT8801_REG_GPIO_ISR(port) (0x32 + (port))
 /* Interrupt enable register */
-#define IT8801_REG_GPIO_IER(port)       (0x37 + (port))
+#define IT8801_REG_GPIO_IER(port) (0x37 + (port))
 
 /* Control register values */
-#define IT8801_GPIOAFS_SHIFT            6  /* bit 6~7 */
+#define IT8801_GPIOAFS_SHIFT 6 /* bit 6~7 */
 
-#define IT8801_GPIODIR                  BIT(5)  /* direction, output=1 */
+#define IT8801_GPIODIR BIT(5) /* direction, output=1 */
 /* input pin */
-#define IT8801_GPIOIOT_INT_RISING       BIT(3)
-#define IT8801_GPIOIOT_INT_FALLING      BIT(4)
+#define IT8801_GPIOIOT_INT_RISING BIT(3)
+#define IT8801_GPIOIOT_INT_FALLING BIT(4)
 
-#define IT8801_GPIODIR                  BIT(5)
-#define IT8801_GPIOIOT                  BIT(4)
-#define IT8801_GPIOPOL                  BIT(2)  /* polarity */
-#define IT8801_GPIOPDE                  BIT(1)  /* pull-down enable */
-#define IT8801_GPIOPUE                  BIT(0)  /* pull-up enable */
+#define IT8801_GPIODIR BIT(5)
+#define IT8801_GPIOIOT BIT(4)
+#define IT8801_GPIOPOL BIT(2) /* polarity */
+#define IT8801_GPIOPDE BIT(1) /* pull-down enable */
+#define IT8801_GPIOPUE BIT(0) /* pull-up enable */
 
 /* ISR for IT8801's SMB_INT# */
 void io_expander_it8801_interrupt(enum gpio_signal signal);
@@ -109,18 +111,18 @@ uint16_t it8801_pwm_get_raw_duty(enum pwm_channel ch);
 void it8801_pwm_set_duty(enum pwm_channel ch, int percent);
 int it8801_pwm_get_duty(enum pwm_channel ch);
 
-#define IT8801_REG_PWMODDSR                            0x5F
-#define IT8801_REG_PWMMCR(n)            (0x60 + ((n) - 1) * 8)
-#define IT8801_REG_PWMDCR(n)            (0x64 + ((n) - 1) * 8)
-#define IT8801_REG_PWMPRSL(n)           (0x66 + ((n) - 1) * 8)
-#define IT8801_REG_PWMPRSM(n)           (0x67 + ((n) - 1) * 8)
+#define IT8801_REG_PWMODDSR 0x5F
+#define IT8801_REG_PWMMCR(n) (0x60 + ((n)-1) * 8)
+#define IT8801_REG_PWMDCR(n) (0x64 + ((n)-1) * 8)
+#define IT8801_REG_PWMPRSL(n) (0x66 + ((n)-1) * 8)
+#define IT8801_REG_PWMPRSM(n) (0x67 + ((n)-1) * 8)
 
-#define IT8801_PWMMCR_MCR_MASK          0x3
-#define IT8801_PWMMCR_MCR_OFF           0
-#define IT8801_PWMMCR_MCR_BLINKING      1
-#define IT8801_PWMMCR_MCR_BREATHING     2
-#define IT8801_PWMMCR_MCR_ON            3
+#define IT8801_PWMMCR_MCR_MASK 0x3
+#define IT8801_PWMMCR_MCR_OFF 0
+#define IT8801_PWMMCR_MCR_BLINKING 1
+#define IT8801_PWMMCR_MCR_BREATHING 2
+#define IT8801_PWMMCR_MCR_ON 3
 
-#endif  /* CONFIG_IO_EXPANDER_IT8801_PWM */
+#endif /* CONFIG_IO_EXPANDER_IT8801_PWM */
 
 #endif /* __CROS_EC_KBEXPANDER_IT8801_H */
