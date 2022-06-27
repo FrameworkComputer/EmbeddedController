7fe50bf514da729f32acf21c5b6c30682f3092d3
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:44:14 -0600

    board/lantis/cbi_ssfc.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia706ad3587449c8f809af362b852ce4e923c472a
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728615
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/lantis/cbi_ssfc.h
+++ board/lantis/cbi_ssfc.h
@@ -36,18 +36,18 @@ enum ec_ssfc_lid_sensor {
  * Audio Codec Source(Bit 8-10)
  */
 enum ec_ssfc_audio_codec_source {
-        SSFC_AUDIO_CODEC_DEFAULT = 0,
-        SSFC_AUDIO_CODEC_VD = 1,
-        SSFC_ADUIO_CODEC_VS = 2,
+	SSFC_AUDIO_CODEC_DEFAULT = 0,
+	SSFC_AUDIO_CODEC_VD = 1,
+	SSFC_ADUIO_CODEC_VS = 2,
 };
 
 /*
  * Touchscreen Driver Source(Bit 11-13)
  */
 enum ec_ssfc_ts_driver_source {
-        SSFC_TS_DRIVER_DEFAULT = 0,
-        SSFC_TS_DRIVER_GENERIC = 1,
-        SSFC_TS_DRIVER_HID = 2,
+	SSFC_TS_DRIVER_DEFAULT = 0,
+	SSFC_TS_DRIVER_GENERIC = 1,
+	SSFC_TS_DRIVER_HID = 2,
 };
 
 union dedede_cbi_ssfc {
@@ -56,7 +56,7 @@ union dedede_cbi_ssfc {
 		uint32_t lid_sensor : 3;
 		uint32_t reserved : 2;
 		uint32_t audio_codec_source : 3;
-		uint32_t ts_driver_source: 3;
+		uint32_t ts_driver_source : 3;
 		uint32_t reserved_2 : 18;
 	};
 	uint32_t raw_value;
@@ -75,6 +75,5 @@ enum ec_ssfc_base_sensor get_cbi_ssfc_base_sensor(void);
  * @return the Lid sensor board type.
  */
 enum ec_ssfc_lid_sensor get_cbi_ssfc_lid_sensor(void);
-
 
 #endif /* _DEDEDE_CBI_SSFC__H_ */
