125d776fbd8f88b14bda0ceb4c1f15c150b87694
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:25:41 -0600

    chip/npcx/sib_chip.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I6ca6285c1df845f4a1c2a1f648c138934e4eff15
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729434
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/npcx/sib_chip.h
+++ chip/npcx/sib_chip.h
@@ -7,15 +7,14 @@
 /* NPCX-specific SIB module for Chrome EC */
 
 /* Super-IO index and register definitions */
-#define INDEX_SID       0x20
-#define INDEX_CHPREV    0x24
-#define INDEX_SRID      0x27
+#define INDEX_SID 0x20
+#define INDEX_CHPREV 0x24
+#define INDEX_SRID 0x27
 
-#define SIO_OFFSET      0x4E
+#define SIO_OFFSET 0x4E
 
 /* Super-IO register write function */
-void sib_write_reg(uint8_t io_offset, uint8_t index_value,
-		uint8_t io_data);
+void sib_write_reg(uint8_t io_offset, uint8_t index_value, uint8_t io_data);
 /* Super-IO register read function */
 uint8_t sib_read_reg(uint8_t io_offset, uint8_t index_value);
 /* Emulate host to read Keyboard I/O */
