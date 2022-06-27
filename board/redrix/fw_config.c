ad2709eef9236b186f7f86cd57cfc87ffd168a7a
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:02:02 -0600

    board/redrix/fw_config.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ied7d270d01c42a8e3f65aba69ab2f2cbe76b90ae
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728859
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/redrix/fw_config.c
+++ board/redrix/fw_config.c
@@ -9,7 +9,7 @@
 #include "cros_board_info.h"
 #include "fw_config.h"
 
-#define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
 
 static union redrix_cbi_fw_config fw_config;
 BUILD_ASSERT(sizeof(fw_config) == sizeof(uint32_t));
