d1b46d1ca7d2e921faa9376b75c92c09fb33a0c0
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:20:33 -0600

    test/genvif/src/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I01991cfe362c233fbef6815a2592fc017ff5c3e1
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730504
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- test/genvif/src/board.h
+++ test/genvif/src/board.h
@@ -3,10 +3,10 @@
  * found in the LICENSE file.
  */
 
-#define PD_OPERATING_POWER_MW	15000
-#define PD_MAX_POWER_MW		65000
-#define PD_MAX_CURRENT_MA	3250
-#define PD_MAX_VOLTAGE_MV	20000
+#define PD_OPERATING_POWER_MW 15000
+#define PD_MAX_POWER_MW 65000
+#define PD_MAX_CURRENT_MA 3250
+#define PD_MAX_VOLTAGE_MV 20000
 
 /* Stubs required by the shared code */
 #define GPIO_PIN(port, index) (GPIO_##port, BIT(index))
