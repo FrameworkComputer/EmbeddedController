5b55113f4808f84b74d3af3a3c766319ef3abc80
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:56:40 -0600

    board/npcx_evb_arm/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If33cc13e21b468d501cd340bc1cb6764edc950f5
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728771
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/npcx_evb_arm/board.c
+++ board/npcx_evb_arm/board.c
@@ -35,16 +35,19 @@
 /******************************************************************************/
 /* ADC channels. Must be in the exactly same order as in enum adc_channel. */
 const struct adc_t adc_channels[] = {
-	[ADC_CH_0] = {"ADC0", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
-	[ADC_CH_1] = {"ADC1", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
-	[ADC_CH_2] = {"ADC2", NPCX_ADC_CH2, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+	[ADC_CH_0] = { "ADC0", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX + 1,
+		       0 },
+	[ADC_CH_1] = { "ADC1", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX + 1,
+		       0 },
+	[ADC_CH_2] = { "ADC2", NPCX_ADC_CH2, ADC_MAX_VOLT, ADC_READ_MAX + 1,
+		       0 },
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 /******************************************************************************/
 /* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
 const struct pwm_t pwm_channels[] = {
-	[PWM_CH_FAN] = { 0, PWM_CONFIG_OPEN_DRAIN, 25000},
+	[PWM_CH_FAN] = { 0, PWM_CONFIG_OPEN_DRAIN, 25000 },
 	[PWM_CH_KBLIGHT] = { 1, 0, 10000 },
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
@@ -53,7 +56,7 @@ BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 /* Physical fans. These are logically separate from pwm_channels. */
 const struct fan_conf fan_conf_0 = {
 	.flags = FAN_USE_RPM_MODE,
-	.ch = 0,	/* Use MFT id to control fan */
+	.ch = 0, /* Use MFT id to control fan */
 	.pgood_gpio = GPIO_PGOOD_FAN,
 	.enable_gpio = -1,
 };
@@ -72,48 +75,38 @@ BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
 /******************************************************************************/
 /* MFT channels. These are logically separate from pwm_channels. */
 const struct mft_t mft_channels[] = {
-	[MFT_CH_0] = { NPCX_MFT_MODULE_1, TCKC_LFCLK, PWM_CH_FAN},
+	[MFT_CH_0] = { NPCX_MFT_MODULE_1, TCKC_LFCLK, PWM_CH_FAN },
 };
 BUILD_ASSERT(ARRAY_SIZE(mft_channels) == MFT_CH_COUNT);
 
 /******************************************************************************/
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
-	{
-		.name = "master0-0",
-		.port = NPCX_I2C_PORT0_0,
-		.kbps = 100,
-		.scl  = GPIO_I2C0_SCL0,
-		.sda  = GPIO_I2C0_SDA0
-	},
-	{
-		.name = "master0-1",
-		.port = NPCX_I2C_PORT0_1,
-		.kbps = 100,
-		.scl  = GPIO_I2C0_SCL1,
-		.sda  = GPIO_I2C0_SDA1
-	},
-	{
-		.name = "master1",
-		.port = NPCX_I2C_PORT1,
-		.kbps = 100,
-		.scl  = GPIO_I2C1_SCL,
-		.sda  = GPIO_I2C1_SDA
-	},
-	{
-		.name = "master2",
-		.port = NPCX_I2C_PORT2,
-		.kbps = 100,
-		.scl  = GPIO_I2C2_SCL,
-		.sda  = GPIO_I2C2_SDA
-	},
-	{
-		.name = "master3",
-		.port = NPCX_I2C_PORT3,
-		.kbps = 100,
-		.scl  = GPIO_I2C3_SCL,
-		.sda  = GPIO_I2C3_SDA
-	},
+	{ .name = "master0-0",
+	  .port = NPCX_I2C_PORT0_0,
+	  .kbps = 100,
+	  .scl = GPIO_I2C0_SCL0,
+	  .sda = GPIO_I2C0_SDA0 },
+	{ .name = "master0-1",
+	  .port = NPCX_I2C_PORT0_1,
+	  .kbps = 100,
+	  .scl = GPIO_I2C0_SCL1,
+	  .sda = GPIO_I2C0_SDA1 },
+	{ .name = "master1",
+	  .port = NPCX_I2C_PORT1,
+	  .kbps = 100,
+	  .scl = GPIO_I2C1_SCL,
+	  .sda = GPIO_I2C1_SDA },
+	{ .name = "master2",
+	  .port = NPCX_I2C_PORT2,
+	  .kbps = 100,
+	  .scl = GPIO_I2C2_SCL,
+	  .sda = GPIO_I2C2_SDA },
+	{ .name = "master3",
+	  .port = NPCX_I2C_PORT3,
+	  .kbps = 100,
+	  .scl = GPIO_I2C3_SCL,
+	  .sda = GPIO_I2C3_SDA },
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
