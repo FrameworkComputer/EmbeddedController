83a7c3bfbe28bb93d4a490799422932f980c63b8
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:22:03 -0600

    test/printf.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie10f826042f3fa01b6f59497e7c100fe924434fd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730531
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- test/printf.c
+++ test/printf.c
@@ -18,8 +18,7 @@
 static const char err_str[] = "ERROR";
 static char output[1024];
 
-int run(int expect_ret, const char *expect,
-	bool output_null, size_t size_limit,
+int run(int expect_ret, const char *expect, bool output_null, size_t size_limit,
 	const char *format, va_list args)
 {
 	size_t expect_size = expect ? strlen(expect) + 1 : 0;
@@ -34,10 +33,8 @@ int run(int expect_ret, const char *expect,
 	TEST_ASSERT(expect_size <= size_limit);
 	memset(output, INIT_VALUE, sizeof(output));
 
-	rv = vsnprintf(output_null ? NULL : output, size_limit,
-		       format, args);
-	ccprintf("received='%.*s'   | ret          =%d\n",
-		 30, output, rv);
+	rv = vsnprintf(output_null ? NULL : output, size_limit, format, args);
+	ccprintf("received='%.*s'   | ret          =%d\n", 30, output, rv);
 
 	TEST_ASSERT_ARRAY_EQ(output, expect, expect_size);
 	TEST_ASSERT_MEMSET(&output[expect_size], INIT_VALUE,
@@ -59,41 +56,36 @@ int expect_success(const char *expect, const char *format, ...)
 	int rv;
 
 	va_start(args, format);
-	rv = run(EC_SUCCESS, expect,
-		 false, sizeof(output),
-		 format, args);
+	rv = run(EC_SUCCESS, expect, false, sizeof(output), format, args);
 	va_end(args);
 
 	return rv;
 }
 
-int expect(int expect_ret, const char *expect,
-	   bool output_null, size_t size_limit,
-	   const char *format, ...)
+int expect(int expect_ret, const char *expect, bool output_null,
+	   size_t size_limit, const char *format, ...)
 {
 	va_list args;
 	int rv;
 
 	va_start(args, format);
-	rv = run(expect_ret, expect,
-		 output_null, size_limit,
-		 format, args);
+	rv = run(expect_ret, expect, output_null, size_limit, format, args);
 	va_end(args);
 
 	return rv;
 }
 
-#define T(n)                                                                  \
-	do {                                                                  \
-		int rv = (n);                                                 \
-		if (rv != EC_SUCCESS)                                         \
-			return rv;                                            \
+#define T(n)                          \
+	do {                          \
+		int rv = (n);         \
+		if (rv != EC_SUCCESS) \
+			return rv;    \
 	} while (0)
 
 test_static int test_vsnprintf_args(void)
 {
-	T(expect_success("",          ""));
-	T(expect_success("a",         "a"));
+	T(expect_success("", ""));
+	T(expect_success("a", "a"));
 
 	T(expect(/* expect an invalid args error */
 		 EC_ERROR_INVAL, NO_BYTES_TOUCHED,
@@ -122,17 +114,17 @@ test_static int test_vsnprintf_args(void)
 
 test_static int test_vsnprintf_int(void)
 {
-	T(expect_success("123",       "%d",      123));
-	T(expect_success("-123",      "%d",     -123));
-	T(expect_success("+123",      "%+d",     123));
-	T(expect_success("-123",      "%+d",    -123));
-	T(expect_success("123",       "%-d",     123));
-	T(expect_success("-123",      "%-d",    -123));
+	T(expect_success("123", "%d", 123));
+	T(expect_success("-123", "%d", -123));
+	T(expect_success("+123", "%+d", 123));
+	T(expect_success("-123", "%+d", -123));
+	T(expect_success("123", "%-d", 123));
+	T(expect_success("-123", "%-d", -123));
 
-	T(expect_success("  123",     "%5d",     123));
-	T(expect_success(" +123",     "%+5d",    123));
-	T(expect_success("00123",     "%05d",    123));
-	T(expect_success("00123",     "%005d",   123));
+	T(expect_success("  123", "%5d", 123));
+	T(expect_success(" +123", "%+5d", 123));
+	T(expect_success("00123", "%05d", 123));
+	T(expect_success("00123", "%005d", 123));
 	/*
 	 * TODO(crbug.com/974084): This odd behavior should be fixed.
 	 * T(expect_success("+0123",     "%+05d",   123));
@@ -141,56 +133,56 @@ test_static int test_vsnprintf_int(void)
 	 * Actual: "0+123"
 	 */
 
-	T(expect_success("  123",     "%*d",     5, 123));
-	T(expect_success(" +123",     "%+*d",    5, 123));
-	T(expect_success("00123",     "%0*d",    5, 123));
+	T(expect_success("  123", "%*d", 5, 123));
+	T(expect_success(" +123", "%+*d", 5, 123));
+	T(expect_success("00123", "%0*d", 5, 123));
 	/*
 	 * TODO(crbug.com/974084): This odd behavior should be fixed.
 	 * T(expect_success("00123",     "%00*d",   5, 123));
 	 * Actual: "ERROR"
 	 */
-	T(expect_success("0+123",     "%+0*d",   5, 123));
+	T(expect_success("0+123", "%+0*d", 5, 123));
 	/*
 	 * TODO(crbug.com/974084): This odd behavior should be fixed.
 	 * T(expect_success("0+123",     "%+00*d",  5, 123));
 	 * Actual: "ERROR"
 	 */
 
-	T(expect_success("123  ",     "%-5d",    123));
-	T(expect_success("+123 ",     "%-+5d",   123));
-	T(expect_success(err_str,     "%+-5d",   123));
-	T(expect_success("123  ",     "%-05d",   123));
-	T(expect_success("123  ",     "%-005d",  123));
-	T(expect_success("+123 ",     "%-+05d",  123));
-	T(expect_success("+123 ",     "%-+005d", 123));
+	T(expect_success("123  ", "%-5d", 123));
+	T(expect_success("+123 ", "%-+5d", 123));
+	T(expect_success(err_str, "%+-5d", 123));
+	T(expect_success("123  ", "%-05d", 123));
+	T(expect_success("123  ", "%-005d", 123));
+	T(expect_success("+123 ", "%-+05d", 123));
+	T(expect_success("+123 ", "%-+005d", 123));
 
-	T(expect_success("0.00123",   "%.5d",    123));
-	T(expect_success("+0.00123",  "%+.5d",   123));
-	T(expect_success("0.00123",   "%7.5d",   123));
-	T(expect_success("  0.00123", "%9.5d",   123));
-	T(expect_success(" +0.00123", "%+9.5d",  123));
+	T(expect_success("0.00123", "%.5d", 123));
+	T(expect_success("+0.00123", "%+.5d", 123));
+	T(expect_success("0.00123", "%7.5d", 123));
+	T(expect_success("  0.00123", "%9.5d", 123));
+	T(expect_success(" +0.00123", "%+9.5d", 123));
 
-	T(expect_success("123",        "%u",    123));
-	T(expect_success("4294967295", "%u",   -1));
+	T(expect_success("123", "%u", 123));
+	T(expect_success("4294967295", "%u", -1));
 	T(expect_success("18446744073709551615", "%llu", (uint64_t)-1));
 
-	T(expect_success("0",         "%x",     0));
-	T(expect_success("0",         "%X",     0));
-	T(expect_success("5e",        "%x",     0X5E));
-	T(expect_success("5E",        "%X",     0X5E));
+	T(expect_success("0", "%x", 0));
+	T(expect_success("0", "%X", 0));
+	T(expect_success("5e", "%x", 0X5E));
+	T(expect_success("5E", "%X", 0X5E));
 
 	/*
 	 * %l is deprecated on 32-bit systems (see crbug.com/984041), but is
 	 * is still functional on 64-bit systems.
 	 */
 	if (sizeof(long) == sizeof(uint32_t)) {
-		T(expect_success(err_str,     "%lx",    0x7b));
-		T(expect_success(err_str,     "%08lu",  0x7b));
-		T(expect_success("13ERROR",   "%d%lu", 13, 14));
+		T(expect_success(err_str, "%lx", 0x7b));
+		T(expect_success(err_str, "%08lu", 0x7b));
+		T(expect_success("13ERROR", "%d%lu", 13, 14));
 	} else {
-		T(expect_success("7b",        "%lx",    0x7b));
-		T(expect_success("00000123",  "%08lu",  123));
-		T(expect_success("131415",    "%d%lu%d", 13, 14L, 15));
+		T(expect_success("7b", "%lx", 0x7b));
+		T(expect_success("00000123", "%08lu", 123));
+		T(expect_success("131415", "%d%lu%d", 13, 14L, 15));
 	}
 
 	return EC_SUCCESS;
@@ -201,22 +193,19 @@ test_static int test_vsnprintf_pointers(void)
 	void *ptr = (void *)0x55005E00;
 	unsigned int val = 0;
 
-	T(expect_success("55005e00",  "%pP",     ptr));
-	T(expect_success(err_str,     "%P",      ptr));
+	T(expect_success("55005e00", "%pP", ptr));
+	T(expect_success(err_str, "%P", ptr));
 	/* %p by itself is invalid */
-	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED,
-		 false, 0, "%p"));
+	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED, false, 0, "%p"));
 	/* %p with an unknown suffix is invalid */
-	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED,
-		 false, 0, "%p "));
+	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED, false, 0, "%p "));
 	/* %p with an unknown suffix is invalid */
-	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED,
-		 false, 0, "%pQ"));
+	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED, false, 0, "%pQ"));
 
 	/* Test %pb, binary format */
-	T(expect_success("0",         "%pb",     BINARY_VALUE(val, 0)));
+	T(expect_success("0", "%pb", BINARY_VALUE(val, 0)));
 	val = 0x5E;
-	T(expect_success("1011110",   "%pb",     BINARY_VALUE(val, 0)));
+	T(expect_success("1011110", "%pb", BINARY_VALUE(val, 0)));
 	T(expect_success("0000000001011110", "%pb", BINARY_VALUE(val, 16)));
 	val = 0x12345678;
 	T(expect_success("10010001101000101011001111000", "%pb",
@@ -230,36 +219,36 @@ test_static int test_vsnprintf_pointers(void)
 
 test_static int test_vsnprintf_chars(void)
 {
-	T(expect_success("a",         "%c",      'a'));
-	T(expect_success("*",         "%c",      '*'));
+	T(expect_success("a", "%c", 'a'));
+	T(expect_success("*", "%c", '*'));
 	return EC_SUCCESS;
 }
 
 test_static int test_vsnprintf_strings(void)
 {
-	T(expect_success("abc",       "%s",      "abc"));
-	T(expect_success("  abc",     "%5s",     "abc"));
-	T(expect_success("abc",       "%0s",     "abc"));
-	T(expect_success("abc  ",     "%-5s",    "abc"));
-	T(expect_success("abc",       "%*s",     0, "abc"));
-	T(expect_success("a",         "%.1s",    "abc"));
-	T(expect_success("a",         "%.*s",    1, "abc"));
-	T(expect_success("",          "%.0s",    "abc"));
-	T(expect_success("",          "%.*s",    0, "abc"));
+	T(expect_success("abc", "%s", "abc"));
+	T(expect_success("  abc", "%5s", "abc"));
+	T(expect_success("abc", "%0s", "abc"));
+	T(expect_success("abc  ", "%-5s", "abc"));
+	T(expect_success("abc", "%*s", 0, "abc"));
+	T(expect_success("a", "%.1s", "abc"));
+	T(expect_success("a", "%.*s", 1, "abc"));
+	T(expect_success("", "%.0s", "abc"));
+	T(expect_success("", "%.*s", 0, "abc"));
 	/*
 	 * TODO(crbug.com/974084):
 	 * Ignoring the padding parameter is slightly
 	 * odd behavior and could use a review.
 	 */
-	T(expect_success("ab",        "%5.2s",   "abc"));
-	T(expect_success("abc",        "%.4s",   "abc"));
+	T(expect_success("ab", "%5.2s", "abc"));
+	T(expect_success("abc", "%.4s", "abc"));
 
 	/*
 	 * Given a malformed string (address 0x1 is a good example),
 	 * if we ask for zero precision, expect no bytes to be read
 	 * from the malformed address and a blank output string.
 	 */
-	T(expect_success("",          "%.0s",    (char *)1));
+	T(expect_success("", "%.0s", (char *)1));
 
 	return EC_SUCCESS;
 }
@@ -268,28 +257,28 @@ test_static int test_vsnprintf_timestamps(void)
 {
 	uint64_t ts = 0;
 
-	T(expect_success("0.000000",       "%pT",      &ts));
+	T(expect_success("0.000000", "%pT", &ts));
 	ts = 123456;
-	T(expect_success("0.123456",       "%pT",      &ts));
+	T(expect_success("0.123456", "%pT", &ts));
 	ts = 9999999000000;
-	T(expect_success("9999999.000000", "%pT",      &ts));
+	T(expect_success("9999999.000000", "%pT", &ts));
 	return EC_SUCCESS;
 }
 
 test_static int test_vsnprintf_hexdump(void)
 {
-	const char bytes[] = {0x00, 0x5E};
+	const char bytes[] = { 0x00, 0x5E };
 
-	T(expect_success("005e",      "%ph",      HEX_BUF(bytes, 2)));
-	T(expect_success("",          "%ph",      HEX_BUF(bytes, 0)));
-	T(expect_success("00",        "%ph",      HEX_BUF(bytes, 1)));
+	T(expect_success("005e", "%ph", HEX_BUF(bytes, 2)));
+	T(expect_success("", "%ph", HEX_BUF(bytes, 0)));
+	T(expect_success("00", "%ph", HEX_BUF(bytes, 1)));
 	return EC_SUCCESS;
 }
 
 test_static int test_vsnprintf_combined(void)
 {
-	T(expect_success("abc",       "%c%s",    'a', "bc"));
-	T(expect_success("12\tbc",    "%d\t%s",  12, "bc"));
+	T(expect_success("abc", "%c%s", 'a', "bc"));
+	T(expect_success("12\tbc", "%d\t%s", 12, "bc"));
 	return EC_SUCCESS;
 }
 
