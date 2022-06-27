e0ee86371faea71282e8b81f0435e4e1bf8352b4
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:39:42 -0600

    zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ifc5fd9788443640170afdf558d35f9816d7adb85
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730950
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
@@ -15,7 +15,7 @@
 #include <ap_power_host_sleep.h>
 #include <x86_common_pwrseq.h>
 
-#define DT_DRV_COMPAT	intel_ap_pwrseq
+#define DT_DRV_COMPAT intel_ap_pwrseq
 
 /* The wait time is ~150 msec, allow for safety margin. */
 #define IN_PCH_SLP_SUS_WAIT_TIME_MS 250
@@ -25,7 +25,7 @@ void init_chipset_pwr_seq_state(void);
 enum power_states_ndsx chipset_pwr_seq_get_state(void);
 void request_start_from_g3(void);
 enum power_states_ndsx pwr_sm_get_state(void);
-const char * const pwr_sm_get_state_name(enum power_states_ndsx state);
+const char *const pwr_sm_get_state_name(enum power_states_ndsx state);
 void apshutdown(void);
 void ap_pwrseq_handle_chipset_reset(void);
 void set_start_from_g3_delay_seconds(uint32_t d_time);
