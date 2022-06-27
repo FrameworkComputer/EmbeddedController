b5d6e60b5190630d08b653b8b5b79d85460e1f63
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:53:37 -0600

    board/mithrax/keyboard.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I9bc28ac7e7312a667442e8c97a0600ff5bca77b8
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728461
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/mithrax/keyboard.c
+++ board/mithrax/keyboard.c
@@ -61,145 +61,144 @@ const uint8_t rgbkbd_count = ARRAY_SIZE(rgbkbds);
 const uint8_t rgbkbd_hsize = RGB_GRID0_COL;
 const uint8_t rgbkbd_vsize = RGB_GRID0_ROW;
 
-#define LED(x, y)	RGBKBD_COORD((x), (y))
-#define DELM		RGBKBD_DELM
+#define LED(x, y) RGBKBD_COORD((x), (y))
+#define DELM RGBKBD_DELM
 const uint8_t rgbkbd_map[] = {
-	DELM,				/* 0: (null) */
-	LED(0, 0), DELM,		/* 1: ~ ` */
-	LED(0, 0), DELM,		/* 2: ! 1 */
-	LED(0, 0), DELM,		/* 3: @ 2 */
-	LED(0, 0), DELM,		/* 4: # 3 */
-	LED(1, 0), DELM,		/* 5: $ 4 */
-	LED(1, 0), DELM,		/* 6: % 5 */
-	LED(1, 0), DELM,		/* 7: ^ 6 */
-	LED(2, 0), DELM,		/* 8: & 7 */
-	LED(2, 0), DELM,		/* 9: * 8 */
-	LED(2, 0), DELM,		/* 10: ( 9 */
-	LED(2, 0), DELM,		/* 11: ) 0 */
-	LED(2, 0), DELM,		/* 12: _ - */
-	LED(3, 0), DELM,		/* 13: + = */
-	DELM,				/* 14: (null) */
-	LED(3, 0), DELM,		/* 15: backspace */
-	LED(0, 0), DELM,		/* 16: tab */
-	LED(0, 0), DELM,		/* 17: q */
-	LED(0, 0), DELM,		/* 18: w */
-	LED(0, 0), DELM,		/* 19: e */
-	LED(1, 0), DELM,		/* 20: r */
-	LED(1, 0), DELM,		/* 21: t */
-	LED(1, 0), DELM,		/* 22: y */
-	LED(2, 0), DELM,		/* 23: u */
-	LED(2, 0), DELM,		/* 24: i */
-	LED(2, 0), DELM,		/* 25: o */
-	LED(2, 0), DELM,		/* 26: p */
-	LED(2, 0), DELM,		/* 27: [ { */
-	LED(3, 0), DELM,		/* 28: ] } */
-	LED(3, 0), DELM,		/* 29: \ | */
-	LED(0, 0), DELM,		/* 30: caps lock */
-	LED(0, 0), DELM,		/* 31: a */
-	LED(0, 0), DELM,		/* 32: s */
-	LED(0, 0), DELM,		/* 33: d */
-	LED(1, 0), DELM,		/* 34: f */
-	LED(1, 0), DELM,		/* 35: g */
-	LED(1, 0), DELM,		/* 36: h */
-	LED(2, 0), DELM,		/* 37: j */
-	LED(2, 0), DELM,		/* 38: k */
-	LED(2, 0), DELM,		/* 39: l */
-	LED(3, 0), DELM,		/* 40: ; : */
-	LED(3, 0), DELM,		/* 41: " ' */
-	DELM,				/* 42: (null) */
-	LED(3, 0), DELM,		/* 43: enter */
-	LED(0, 0), DELM,		/* 44: L-shift */
-	DELM,				/* 45: (null) */
-	LED(0, 0), DELM,		/* 46: z */
-	LED(0, 0), DELM,		/* 47: x */
-	LED(0, 0), DELM,		/* 48: c */
-	LED(1, 0), DELM,		/* 49: v */
-	LED(1, 0), DELM,		/* 50: b */
-	LED(1, 0), DELM,		/* 51: n */
-	LED(2, 0), DELM,		/* 52: m */
-	LED(2, 0), DELM,		/* 53: , < */
-	LED(2, 0), DELM,		/* 54: . > */
-	LED(3, 0), DELM,		/* 55: / ? */
-	DELM,				/* 56: (null) */
-	LED(3, 0), DELM,		/* 57: R-shift */
-	LED(0, 0), DELM,		/* 58: L-ctrl */
-	LED(3, 0), DELM,		/* 59: power */
-	LED(0, 0), DELM,		/* 60: L-alt */
-	LED(0, 0), LED(1, 0),
-	LED(2, 0), DELM,		/* 61: space */
-	LED(2, 0), DELM,		/* 62: R-alt */
-	DELM,				/* 63: (null) */
-	LED(2, 0), DELM,		/* 64: R-ctrl */
-	DELM,				/* 65: (null) */
-	DELM,				/* 66: (null) */
-	DELM,				/* 67: (null) */
-	DELM,				/* 68: (null) */
-	DELM,				/* 69: (null) */
-	DELM,				/* 70: (null) */
-	DELM,				/* 71: (null) */
-	DELM,				/* 72: (null) */
-	DELM,				/* 73: (null) */
-	DELM,				/* 74: (null) */
-	DELM,				/* 75: (null) */
-	DELM,				/* 76: delete */
-	DELM,				/* 77: (null) */
-	DELM,				/* 78: (null) */
-	LED(3, 0), DELM,		/* 79: left */
-	DELM,				/* 80: home */
-	DELM,				/* 81: end */
-	DELM,				/* 82: (null) */
-	LED(3, 0), DELM,		/* 83: up */
-	LED(3, 0), DELM,		/* 84: down */
-	DELM,				/* 85: page up */
-	DELM,				/* 86: page down */
-	DELM,				/* 87: (null) */
-	DELM,				/* 88: (null) */
-	LED(3, 0), DELM,		/* 89: right */
-	DELM,				/* 90: (null) */
-	DELM,				/* 91: numpad 7 */
-	DELM,				/* 92: numpad 4 */
-	DELM,				/* 93: numpad 1 */
-	DELM,				/* 94: (null) */
-	DELM,				/* 95: numpad / */
-	DELM,				/* 96: numpad 8 */
-	DELM,				/* 97: numpad 5 */
-	DELM,				/* 98: numpad 2 */
-	DELM,				/* 99: numpad 0 */
-	DELM,				/* 100: numpad * */
-	DELM,				/* 101: numpad 9 */
-	DELM,				/* 102: numpad 6 */
-	DELM,				/* 103: numpad 3 */
-	DELM,				/* 104: numpad . */
-	DELM,				/* 105: numpad - */
-	DELM,				/* 106: numpad + */
-	DELM,				/* 107: (null) */
-	DELM,				/* 108: numpad enter */
-	DELM,				/* 109: (null) */
-	LED(0, 0), DELM,		/* 110: esc */
-	LED(0, 0), DELM,		/* T1: back */
-	LED(0, 0), DELM,		/* T2: refresh */
-	LED(1, 0), DELM,		/* T3: full screen */
-	LED(1, 0), DELM,		/* T4: overview */
-	LED(1, 0), DELM,		/* T5: snapshot */
-	LED(2, 0), DELM,		/* T6: brightness down */
-	LED(2, 0), DELM,		/* T7: brightness up */
-	LED(2, 0), DELM,		/* T8: mute */
-	LED(2, 0), DELM,		/* T9: volume down */
-	LED(3, 0), DELM,		/* T10: volume up */
-	DELM,				/* T11: (null) */
-	DELM,				/* T12: (null) */
-	DELM,				/* T13: (null) */
-	DELM,				/* T14: (null) */
-	DELM,				/* T15: (null) */
-	DELM,				/* 126: (null) */
-	DELM,				/* 127: (null) */
+	DELM, /* 0: (null) */
+	LED(0, 0), DELM, /* 1: ~ ` */
+	LED(0, 0), DELM, /* 2: ! 1 */
+	LED(0, 0), DELM, /* 3: @ 2 */
+	LED(0, 0), DELM, /* 4: # 3 */
+	LED(1, 0), DELM, /* 5: $ 4 */
+	LED(1, 0), DELM, /* 6: % 5 */
+	LED(1, 0), DELM, /* 7: ^ 6 */
+	LED(2, 0), DELM, /* 8: & 7 */
+	LED(2, 0), DELM, /* 9: * 8 */
+	LED(2, 0), DELM, /* 10: ( 9 */
+	LED(2, 0), DELM, /* 11: ) 0 */
+	LED(2, 0), DELM, /* 12: _ - */
+	LED(3, 0), DELM, /* 13: + = */
+	DELM, /* 14: (null) */
+	LED(3, 0), DELM, /* 15: backspace */
+	LED(0, 0), DELM, /* 16: tab */
+	LED(0, 0), DELM, /* 17: q */
+	LED(0, 0), DELM, /* 18: w */
+	LED(0, 0), DELM, /* 19: e */
+	LED(1, 0), DELM, /* 20: r */
+	LED(1, 0), DELM, /* 21: t */
+	LED(1, 0), DELM, /* 22: y */
+	LED(2, 0), DELM, /* 23: u */
+	LED(2, 0), DELM, /* 24: i */
+	LED(2, 0), DELM, /* 25: o */
+	LED(2, 0), DELM, /* 26: p */
+	LED(2, 0), DELM, /* 27: [ { */
+	LED(3, 0), DELM, /* 28: ] } */
+	LED(3, 0), DELM, /* 29: \ | */
+	LED(0, 0), DELM, /* 30: caps lock */
+	LED(0, 0), DELM, /* 31: a */
+	LED(0, 0), DELM, /* 32: s */
+	LED(0, 0), DELM, /* 33: d */
+	LED(1, 0), DELM, /* 34: f */
+	LED(1, 0), DELM, /* 35: g */
+	LED(1, 0), DELM, /* 36: h */
+	LED(2, 0), DELM, /* 37: j */
+	LED(2, 0), DELM, /* 38: k */
+	LED(2, 0), DELM, /* 39: l */
+	LED(3, 0), DELM, /* 40: ; : */
+	LED(3, 0), DELM, /* 41: " ' */
+	DELM, /* 42: (null) */
+	LED(3, 0), DELM, /* 43: enter */
+	LED(0, 0), DELM, /* 44: L-shift */
+	DELM, /* 45: (null) */
+	LED(0, 0), DELM, /* 46: z */
+	LED(0, 0), DELM, /* 47: x */
+	LED(0, 0), DELM, /* 48: c */
+	LED(1, 0), DELM, /* 49: v */
+	LED(1, 0), DELM, /* 50: b */
+	LED(1, 0), DELM, /* 51: n */
+	LED(2, 0), DELM, /* 52: m */
+	LED(2, 0), DELM, /* 53: , < */
+	LED(2, 0), DELM, /* 54: . > */
+	LED(3, 0), DELM, /* 55: / ? */
+	DELM, /* 56: (null) */
+	LED(3, 0), DELM, /* 57: R-shift */
+	LED(0, 0), DELM, /* 58: L-ctrl */
+	LED(3, 0), DELM, /* 59: power */
+	LED(0, 0), DELM, /* 60: L-alt */
+	LED(0, 0), LED(1, 0), LED(2, 0), DELM, /* 61: space */
+	LED(2, 0), DELM, /* 62: R-alt */
+	DELM, /* 63: (null) */
+	LED(2, 0), DELM, /* 64: R-ctrl */
+	DELM, /* 65: (null) */
+	DELM, /* 66: (null) */
+	DELM, /* 67: (null) */
+	DELM, /* 68: (null) */
+	DELM, /* 69: (null) */
+	DELM, /* 70: (null) */
+	DELM, /* 71: (null) */
+	DELM, /* 72: (null) */
+	DELM, /* 73: (null) */
+	DELM, /* 74: (null) */
+	DELM, /* 75: (null) */
+	DELM, /* 76: delete */
+	DELM, /* 77: (null) */
+	DELM, /* 78: (null) */
+	LED(3, 0), DELM, /* 79: left */
+	DELM, /* 80: home */
+	DELM, /* 81: end */
+	DELM, /* 82: (null) */
+	LED(3, 0), DELM, /* 83: up */
+	LED(3, 0), DELM, /* 84: down */
+	DELM, /* 85: page up */
+	DELM, /* 86: page down */
+	DELM, /* 87: (null) */
+	DELM, /* 88: (null) */
+	LED(3, 0), DELM, /* 89: right */
+	DELM, /* 90: (null) */
+	DELM, /* 91: numpad 7 */
+	DELM, /* 92: numpad 4 */
+	DELM, /* 93: numpad 1 */
+	DELM, /* 94: (null) */
+	DELM, /* 95: numpad / */
+	DELM, /* 96: numpad 8 */
+	DELM, /* 97: numpad 5 */
+	DELM, /* 98: numpad 2 */
+	DELM, /* 99: numpad 0 */
+	DELM, /* 100: numpad * */
+	DELM, /* 101: numpad 9 */
+	DELM, /* 102: numpad 6 */
+	DELM, /* 103: numpad 3 */
+	DELM, /* 104: numpad . */
+	DELM, /* 105: numpad - */
+	DELM, /* 106: numpad + */
+	DELM, /* 107: (null) */
+	DELM, /* 108: numpad enter */
+	DELM, /* 109: (null) */
+	LED(0, 0), DELM, /* 110: esc */
+	LED(0, 0), DELM, /* T1: back */
+	LED(0, 0), DELM, /* T2: refresh */
+	LED(1, 0), DELM, /* T3: full screen */
+	LED(1, 0), DELM, /* T4: overview */
+	LED(1, 0), DELM, /* T5: snapshot */
+	LED(2, 0), DELM, /* T6: brightness down */
+	LED(2, 0), DELM, /* T7: brightness up */
+	LED(2, 0), DELM, /* T8: mute */
+	LED(2, 0), DELM, /* T9: volume down */
+	LED(3, 0), DELM, /* T10: volume up */
+	DELM, /* T11: (null) */
+	DELM, /* T12: (null) */
+	DELM, /* T13: (null) */
+	DELM, /* T14: (null) */
+	DELM, /* T15: (null) */
+	DELM, /* 126: (null) */
+	DELM, /* 127: (null) */
 };
 #undef LED
 #undef DELM
 const size_t rgbkbd_map_size = ARRAY_SIZE(rgbkbd_map);
 
-__override const struct ec_response_keybd_config
-*board_vivaldi_keybd_config(void)
+__override const struct ec_response_keybd_config *
+board_vivaldi_keybd_config(void)
 {
 	return &mithrax_kb;
 }
@@ -213,20 +212,20 @@ __override const struct key {
 	uint8_t row;
 	uint8_t col;
 } vivaldi_keys[] = {
-	{.row = 4, .col = 2},	/* T1 */
-	{.row = 3, .col = 2},	/* T2 */
-	{.row = 2, .col = 2},	/* T3 */
-	{.row = 1, .col = 2},	/* T4 */
-	{.row = 4, .col = 4},	/* T5 */
-	{.row = 3, .col = 4},	/* T6 */
-	{.row = 2, .col = 4},	/* T7 */
-	{.row = 2, .col = 9},	/* T8 */
-	{.row = 1, .col = 9},	/* T9 */
-	{.row = 1, .col = 4},	/* T10 */
-	{.row = 0, .col = 4},	/* T11 */
-	{.row = 1, .col = 5},	/* T12 */
-	{.row = 3, .col = 5},	/* T13 */
-	{.row = 2, .col = 1},	/* T14 */
-	{.row = 0, .col = 1},	/* T15 */
+	{ .row = 4, .col = 2 }, /* T1 */
+	{ .row = 3, .col = 2 }, /* T2 */
+	{ .row = 2, .col = 2 }, /* T3 */
+	{ .row = 1, .col = 2 }, /* T4 */
+	{ .row = 4, .col = 4 }, /* T5 */
+	{ .row = 3, .col = 4 }, /* T6 */
+	{ .row = 2, .col = 4 }, /* T7 */
+	{ .row = 2, .col = 9 }, /* T8 */
+	{ .row = 1, .col = 9 }, /* T9 */
+	{ .row = 1, .col = 4 }, /* T10 */
+	{ .row = 0, .col = 4 }, /* T11 */
+	{ .row = 1, .col = 5 }, /* T12 */
+	{ .row = 3, .col = 5 }, /* T13 */
+	{ .row = 2, .col = 1 }, /* T14 */
+	{ .row = 0, .col = 1 }, /* T15 */
 };
 BUILD_ASSERT(ARRAY_SIZE(vivaldi_keys) == MAX_TOP_ROW_KEYS);
