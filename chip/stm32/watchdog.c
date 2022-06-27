59fc4017960634d5c12feb5c86ede424e4401704
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:33:14 -0600

    chip/stm32/watchdog.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I07c219b436e10365018dfb7b6e0c4cdfb6fed8ac
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729560
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/watchdog.c
+++ chip/stm32/watchdog.c
@@ -67,8 +67,9 @@ int watchdog_init(void)
 	STM32_IWDG_PR = IWDG_PRESCALER & 7;
 
 	/* Set the reload value of the watchdog counter */
-	STM32_IWDG_RLR = MIN(STM32_IWDG_RLR_MAX, CONFIG_WATCHDOG_PERIOD_MS *
-			     (LSI_CLOCK / IWDG_PRESCALER_DIV) / 1000);
+	STM32_IWDG_RLR = MIN(STM32_IWDG_RLR_MAX,
+			     CONFIG_WATCHDOG_PERIOD_MS *
+				     (LSI_CLOCK / IWDG_PRESCALER_DIV) / 1000);
 #ifdef CHIP_FAMILY_STM32L4
 	tickstart = get_time();
 	/* Wait for SR */
