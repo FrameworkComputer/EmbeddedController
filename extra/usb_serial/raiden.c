a8f99edf161a8cac86af77cf08063e6a06e3e884
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:06:46 -0600

    extra/usb_serial/raiden.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Icb5b66f4000902a76ebd4209af3008b2c83ca53f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730189
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- extra/usb_serial/raiden.c
+++ extra/usb_serial/raiden.c
@@ -19,28 +19,25 @@
 
 MODULE_LICENSE("GPL");
 
-#define USB_VENDOR_ID_GOOGLE		0x18d1
-#define USB_SUBCLASS_GOOGLE_SERIAL	0x50
-#define USB_PROTOCOL_GOOGLE_SERIAL	0x01
+#define USB_VENDOR_ID_GOOGLE 0x18d1
+#define USB_SUBCLASS_GOOGLE_SERIAL 0x50
+#define USB_PROTOCOL_GOOGLE_SERIAL 0x01
 
 static struct usb_device_id const ids[] = {
-	{ USB_VENDOR_AND_INTERFACE_INFO(USB_VENDOR_ID_GOOGLE,
-					USB_CLASS_VENDOR_SPEC,
-					USB_SUBCLASS_GOOGLE_SERIAL,
-					USB_PROTOCOL_GOOGLE_SERIAL) },
-        { 0 }
+	{ USB_VENDOR_AND_INTERFACE_INFO(
+		USB_VENDOR_ID_GOOGLE, USB_CLASS_VENDOR_SPEC,
+		USB_SUBCLASS_GOOGLE_SERIAL, USB_PROTOCOL_GOOGLE_SERIAL) },
+	{ 0 }
 };
 
 MODULE_DEVICE_TABLE(usb, ids);
 
-static struct usb_serial_driver device =
-{
-	.driver    = { .owner = THIS_MODULE,
-		       .name  = "Google" },
-	.id_table  = ids,
+static struct usb_serial_driver device = {
+	.driver = { .owner = THIS_MODULE, .name = "Google" },
+	.id_table = ids,
 	.num_ports = 1,
 };
 
-static struct usb_serial_driver * const drivers[] = { &device, NULL };
+static struct usb_serial_driver *const drivers[] = { &device, NULL };
 
 module_usb_serial_driver(drivers, ids);
