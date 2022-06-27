e78db01b7c1aba297afd232ba4c22f11da25420e
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:39:40 -0600

    board/homestar/usbc_config.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I2df28345a3d3934cf3233d14ccfc24c7c65ada24
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728325
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/homestar/usbc_config.c
+++ board/homestar/usbc_config.c
@@ -11,8 +11,8 @@
 #include "console.h"
 #include "usb_pd.h"
 
-#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
-#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ##args)
 
 const struct charger_config_t chg_chips[] = {
 	{
