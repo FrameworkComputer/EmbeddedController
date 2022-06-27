1eac3f5d031349dcf4ee7a9f72e90f97135d05ac
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:53:32 -0600

    driver/retimer/pi3hdx1204.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I883f420d7c1e2a507a38860d3fdd71d047059e3c
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730045
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/retimer/pi3hdx1204.c
+++ driver/retimer/pi3hdx1204.c
@@ -9,8 +9,7 @@
 #include "i2c.h"
 #include "pi3hdx1204.h"
 
-int pi3hdx1204_enable(const int i2c_port,
-		      const uint16_t i2c_addr_flags,
+int pi3hdx1204_enable(const int i2c_port, const uint16_t i2c_addr_flags,
 		      const int enable)
 {
 	const uint8_t buf[PI3HDX1204_DE_OFFSET + 1] = {
@@ -27,8 +26,7 @@ int pi3hdx1204_enable(const int i2c_port,
 	};
 	int rv;
 
-	rv = i2c_xfer(i2c_port, i2c_addr_flags,
-		      buf, PI3HDX1204_DE_OFFSET + 1,
+	rv = i2c_xfer(i2c_port, i2c_addr_flags, buf, PI3HDX1204_DE_OFFSET + 1,
 		      NULL, 0);
 
 	if (rv)
