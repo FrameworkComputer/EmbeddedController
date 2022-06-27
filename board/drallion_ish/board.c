4a1479d8c0b5b1fe38e0178f2c189e185c0e91b7
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:31:13 -0600

    board/drallion_ish/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8a2756815e2aa5da485ea720539af3a1a0311017
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3726420
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/drallion_ish/board.c
+++ board/drallion_ish/board.c
@@ -43,11 +43,9 @@ static struct stprivate_data g_lis2dh_data;
 static struct lis2mdl_private_data lis2mdl_a_data;
 
 /* Matrix to rotate base sensor into standard reference frame */
-const mat33_fp_t base_rot_ref = {
-	{ 0, FLOAT_TO_FP(1), 0},
-	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, 0,  FLOAT_TO_FP(1)}
-};
+const mat33_fp_t base_rot_ref = { { 0, FLOAT_TO_FP(1), 0 },
+				  { FLOAT_TO_FP(-1), 0, 0 },
+				  { 0, 0, FLOAT_TO_FP(1) } };
 
 /* Drivers */
 struct motion_sensor_t motion_sensors[] = {
