7a46aed15240d83409ff9435f23a2d5ef10831fa
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:24:00 -0600

    test/usb_tcpmv2_td_pd_ll_e3.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I51d1d412e90f8f82ec7baa288cf09d1c0a4a3e92
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730553
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- test/usb_tcpmv2_td_pd_ll_e3.c
+++ test/usb_tcpmv2_td_pd_ll_e3.c
@@ -29,8 +29,8 @@ static int td_pd_ll_e3(enum pd_data_role data_role)
 	/*
 	 * a) Run PROC.PD.E1 Bring-up according to the UUT role.
 	 */
-	TEST_EQ(proc_pd_e1(data_role, INITIAL_AND_ALREADY_ATTACHED),
-		EC_SUCCESS, "%d");
+	TEST_EQ(proc_pd_e1(data_role, INITIAL_AND_ALREADY_ATTACHED), EC_SUCCESS,
+		"%d");
 
 	/*
 	 * Make sure we are idle. Reject everything that is pending
@@ -42,13 +42,11 @@ static int td_pd_ll_e3(enum pd_data_role data_role)
 	 *    and do not send GoodCrc for nRetryCount + 1 times
 	 *    (nRetryCount equals 3 since PD 2.1).
 	 */
-	partner_send_msg(TCPCI_MSG_SOP,
-			 PD_CTRL_GET_SINK_CAP,
-			 0, 0, NULL);
+	partner_send_msg(TCPCI_MSG_SOP, PD_CTRL_GET_SINK_CAP, 0, 0, NULL);
 
 	retries = (partner_get_pd_rev() == PD_REV30) ? 2 : 3;
 	TEST_EQ(verify_tcpci_tx_retry_count(TCPCI_MSG_SOP, 0, PD_DATA_SINK_CAP,
-			retries),
+					    retries),
 		EC_SUCCESS, "%d");
 	mock_set_alert(TCPC_REG_ALERT_TX_FAILED);
 
