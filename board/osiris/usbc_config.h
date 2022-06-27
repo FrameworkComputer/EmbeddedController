95f6b262c8ec11798d353f19f0ddfe0cafa8fa04
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:58:11 -0600

    board/osiris/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Iead12406d5706f440f16d517c636c4b9d30f4953
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728805
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/osiris/usbc_config.h
+++ board/osiris/usbc_config.h
@@ -9,14 +9,10 @@
 #define __CROS_EC_USBC_CONFIG_H
 
 #ifndef CONFIG_ZEPHYR
-#define CONFIG_USB_PD_PORT_MAX_COUNT	2
+#define CONFIG_USB_PD_PORT_MAX_COUNT 2
 #endif
 
-enum usbc_port {
-	USBC_PORT_C0 = 0,
-	USBC_PORT_C1,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
 void config_usb_db_type(void);
 
