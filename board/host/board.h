713559c93ee533e019bff4f10fc3027c677d5ff9
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:39:47 -0600

    board/host/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0e176700b60b8d0daecfeae3c90a33825b8b5eb0
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728471
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/host/board.h
+++ board/host/board.h
@@ -65,25 +65,25 @@ enum {
 };
 
 /* Standard-current Rp */
-#define PD_SRC_VNC           PD_SRC_DEF_VNC_MV
-#define PD_SRC_RD_THRESHOLD  PD_SRC_DEF_RD_THRESH_MV
+#define PD_SRC_VNC PD_SRC_DEF_VNC_MV
+#define PD_SRC_RD_THRESHOLD PD_SRC_DEF_RD_THRESH_MV
 
 /* delay necessary for the voltage transition on the power supply */
-#define PD_POWER_SUPPLY_TURN_ON_DELAY  20000 /* us */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY 20000 /* us */
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY 20000 /* us */
 
 /* Define typical operating power and max power */
 #define PD_OPERATING_POWER_MW 15000
-#define PD_MAX_POWER_MW       60000
-#define PD_MAX_CURRENT_MA     3000
-#define PD_MAX_VOLTAGE_MV     20000
+#define PD_MAX_POWER_MW 60000
+#define PD_MAX_CURRENT_MA 3000
+#define PD_MAX_VOLTAGE_MV 20000
 
-#define PD_MIN_CURRENT_MA     500
-#define PD_MIN_POWER_MW       7500
+#define PD_MIN_CURRENT_MA 500
+#define PD_MIN_POWER_MW 7500
 
 /* Configuration for fake Fingerprint Sensor */
 #define CONFIG_SPI_CONTROLLER
-#define CONFIG_SPI_FP_PORT    1 /* SPI1: third master config */
+#define CONFIG_SPI_FP_PORT 1 /* SPI1: third master config */
 
 #define CONFIG_RNG
 void fps_event(enum gpio_signal signal);
@@ -92,7 +92,7 @@ void fps_event(enum gpio_signal signal);
 
 #define CONFIG_I2C
 #define CONFIG_I2C_CONTROLLER
-#define I2C_PORT_EEPROM		0
-#define I2C_ADDR_EEPROM_FLAGS	0x50
+#define I2C_PORT_EEPROM 0
+#define I2C_ADDR_EEPROM_FLAGS 0x50
 
 #endif /* __CROS_EC_BOARD_H */
