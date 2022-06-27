24ca5e8922fe0c273dad2e3af5658b5c7846ecc5
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:09:43 -0600

    include/driver/accel_bma2x2_public.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic5c6a58fdb3e9eb0b9fbdc9104f304410b2e18bd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730245
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/driver/accel_bma2x2_public.h
+++ include/driver/accel_bma2x2_public.h
@@ -14,27 +14,27 @@ extern const struct accelgyro_drv bma2x2_accel_drv;
 
 /* I2C ADDRESS DEFINITIONS    */
 /* The following definition of I2C address is used for the following sensors
-* BMA253
-* BMA255
-* BMA355
-* BMA280
-* BMA282
-* BMA223
-* BMA254
-* BMA284
-* BMA250E
-* BMA222E
-*/
-#define BMA2x2_I2C_ADDR1_FLAGS              0x18
-#define BMA2x2_I2C_ADDR2_FLAGS              0x19
+ * BMA253
+ * BMA255
+ * BMA355
+ * BMA280
+ * BMA282
+ * BMA223
+ * BMA254
+ * BMA284
+ * BMA250E
+ * BMA222E
+ */
+#define BMA2x2_I2C_ADDR1_FLAGS 0x18
+#define BMA2x2_I2C_ADDR2_FLAGS 0x19
 
 /* The following definition of I2C address is used for the following sensors
-* BMC150
-* BMC056
-* BMC156
-*/
-#define BMA2x2_I2C_ADDR3_FLAGS              0x10
-#define BMA2x2_I2C_ADDR4_FLAGS              0x11
+ * BMC150
+ * BMC056
+ * BMC156
+ */
+#define BMA2x2_I2C_ADDR3_FLAGS 0x10
+#define BMA2x2_I2C_ADDR4_FLAGS 0x11
 
 /*
  * Min and Max sampling frequency in mHz.
@@ -43,8 +43,7 @@ extern const struct accelgyro_drv bma2x2_accel_drv;
  * (see CONFIG_MOTION_MIN_SENSE_WAIT_TIME), we may read too early when
  * other sensors are active.
  */
-#define BMA255_ACCEL_MIN_FREQ           7810
-#define BMA255_ACCEL_MAX_FREQ \
-	MOTION_MAX_SENSOR_FREQUENCY(125000, 15625)
+#define BMA255_ACCEL_MIN_FREQ 7810
+#define BMA255_ACCEL_MAX_FREQ MOTION_MAX_SENSOR_FREQUENCY(125000, 15625)
 
 #endif /* CROS_EC_DRIVER_ACCEL_BMA2x2_PUBLIC_H */
