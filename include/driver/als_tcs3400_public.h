8f073a32a1aa526f04bd01c31f282897d8f78332
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:10:16 -0600

    include/driver/als_tcs3400_public.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I4c59ffad975a95cdd2e7143d5234b920f2636420
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730272
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/driver/als_tcs3400_public.h
+++ include/driver/als_tcs3400_public.h
@@ -11,34 +11,34 @@
 #include "accelgyro.h"
 
 /* I2C Interface */
-#define TCS3400_I2C_ADDR_FLAGS	0x39
+#define TCS3400_I2C_ADDR_FLAGS 0x39
 
 /* NOTE: The higher the ATIME value in reg, the shorter the accumulation time */
-#define TCS_MIN_ATIME           0x00            /* 712 ms */
-#define TCS_MAX_ATIME           0x70            /* 400 ms */
-#define TCS_ATIME_GRANULARITY   256             /* 256 atime settings */
-#define TCS_SATURATION_LEVEL    0xffff          /* for 0 < atime < 0x70 */
-#define TCS_DEFAULT_ATIME       TCS_MIN_ATIME   /* 712 ms */
-#define TCS_CALIBRATION_ATIME   TCS_MIN_ATIME
-#define TCS_GAIN_UPSHIFT_ATIME  TCS_MAX_ATIME
+#define TCS_MIN_ATIME 0x00 /* 712 ms */
+#define TCS_MAX_ATIME 0x70 /* 400 ms */
+#define TCS_ATIME_GRANULARITY 256 /* 256 atime settings */
+#define TCS_SATURATION_LEVEL 0xffff /* for 0 < atime < 0x70 */
+#define TCS_DEFAULT_ATIME TCS_MIN_ATIME /* 712 ms */
+#define TCS_CALIBRATION_ATIME TCS_MIN_ATIME
+#define TCS_GAIN_UPSHIFT_ATIME TCS_MAX_ATIME
 
 /* Number of different ranges supported for atime adjustment support */
-#define TCS_MAX_ATIME_RANGES    13
-#define TCS_GAIN_TABLE_MAX_LUX  12999
-#define TCS_ATIME_GAIN_FACTOR   100     /* table values are 100x actual value */
+#define TCS_MAX_ATIME_RANGES 13
+#define TCS_GAIN_TABLE_MAX_LUX 12999
+#define TCS_ATIME_GAIN_FACTOR 100 /* table values are 100x actual value */
 
-#define TCS_MIN_AGAIN           0x00            /* 1x gain */
-#define TCS_MAX_AGAIN           0x03            /* 64x gain */
-#define TCS_CALIBRATION_AGAIN   0x02            /* 16x gain */
-#define TCS_DEFAULT_AGAIN       TCS_CALIBRATION_AGAIN
+#define TCS_MIN_AGAIN 0x00 /* 1x gain */
+#define TCS_MAX_AGAIN 0x03 /* 64x gain */
+#define TCS_CALIBRATION_AGAIN 0x02 /* 16x gain */
+#define TCS_DEFAULT_AGAIN TCS_CALIBRATION_AGAIN
 
 #define TCS_MAX_INTEGRATION_TIME 2780 /* 2780us */
-#define TCS_ATIME_DEC_STEP      5
-#define TCS_ATIME_INC_STEP      TCS_GAIN_UPSHIFT_ATIME
+#define TCS_ATIME_DEC_STEP 5
+#define TCS_ATIME_INC_STEP TCS_GAIN_UPSHIFT_ATIME
 
 /* Min and Max sampling frequency in mHz */
-#define TCS3400_LIGHT_MIN_FREQ              149
-#define TCS3400_LIGHT_MAX_FREQ              1000
+#define TCS3400_LIGHT_MIN_FREQ 149
+#define TCS3400_LIGHT_MAX_FREQ 1000
 #if (CONFIG_EC_MAX_SENSOR_FREQ_MILLIHZ <= TCS3400_LIGHT_MAX_FREQ)
 #error "EC too slow for light sensor"
 #endif
@@ -55,15 +55,16 @@ struct tcs_saturation_t {
 	uint8_t again;
 
 	/* Acquisition Time, controlled by the ATIME register */
-	uint8_t atime;             /* ATIME register setting */
+	uint8_t atime; /* ATIME register setting */
 };
 
 /* tcs3400 rgb als driver data */
 struct tcs3400_rgb_drv_data_t {
-	uint8_t calibration_mode;/* 0 = normal run mode, 1 = calibration mode */
+	uint8_t calibration_mode; /* 0 = normal run mode, 1 = calibration mode
+				   */
 
 	struct rgb_calibration_t calibration;
-	struct tcs_saturation_t saturation;  /* saturation adjustment */
+	struct tcs_saturation_t saturation; /* saturation adjustment */
 };
 
 extern const struct accelgyro_drv tcs3400_drv;
