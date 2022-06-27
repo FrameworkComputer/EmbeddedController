b58a3852501da2d3147d20e14897267fcb9cae8a
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:37:13 -0600

    board/gimble/fw_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Id51ea67600d1ab35f904a39a98cc11072c529677
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728414
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/gimble/fw_config.h
+++ board/gimble/fw_config.h
@@ -25,22 +25,19 @@ enum ec_cfg_keyboard_backlight_type {
 	KEYBOARD_BACKLIGHT_ENABLED = 1
 };
 
-enum ec_cfg_keyboard_layout {
-	KB_LAYOUT_DEFAULT = 0,
-	KB_LAYOUT_1 = 1
-};
+enum ec_cfg_keyboard_layout { KB_LAYOUT_DEFAULT = 0, KB_LAYOUT_1 = 1 };
 
 union brya_cbi_fw_config {
 	struct {
-		enum ec_cfg_usb_db_type			usb_db : 4;
-		uint32_t				sd_db : 2;
-		uint32_t				reserved_0 : 1;
-		enum ec_cfg_keyboard_backlight_type	kb_bl : 1;
-		uint32_t				audio : 3;
-		uint32_t				cellular_db : 2;
-		uint32_t				wifi_sar_id : 1;
-		enum ec_cfg_keyboard_layout		kb_layout : 2;
-		uint32_t				reserved_1 : 16;
+		enum ec_cfg_usb_db_type usb_db : 4;
+		uint32_t sd_db : 2;
+		uint32_t reserved_0 : 1;
+		enum ec_cfg_keyboard_backlight_type kb_bl : 1;
+		uint32_t audio : 3;
+		uint32_t cellular_db : 2;
+		uint32_t wifi_sar_id : 1;
+		enum ec_cfg_keyboard_layout kb_layout : 2;
+		uint32_t reserved_1 : 16;
 	};
 	uint32_t raw_value;
 };
