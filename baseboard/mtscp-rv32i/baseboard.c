1918a62a634a6fe4f5c66a48af701bdda3205a2d
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:17:13 -0600

    baseboard/mtscp-rv32i/baseboard.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I19c1abd1bfb704933e978c2da8aea002a820577d
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727925
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/mtscp-rv32i/baseboard.c
+++ baseboard/mtscp-rv32i/baseboard.c
@@ -13,22 +13,24 @@
 
 struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
 	/* SRAM (for most code, data) */
-	{0, SCP_SRAM_END, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R},
+	{ 0, SCP_SRAM_END, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R },
 	/* SRAM (for IPI shared buffer) */
-	{SCP_SRAM_END, SCP_FW_END, MPU_ATTR_W | MPU_ATTR_R},
-	/* For AP domain */
+	{ SCP_SRAM_END, SCP_FW_END, MPU_ATTR_W | MPU_ATTR_R },
+/* For AP domain */
 #ifdef CHIP_VARIANT_MT8195
-	{0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R | MPU_ATTR_P},
+	{ 0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R | MPU_ATTR_P },
 #else
-	{0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R},
+	{ 0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R },
 #endif
 	/* For SCP sys */
-	{0x70000000, 0x80000000, MPU_ATTR_W | MPU_ATTR_R},
+	{ 0x70000000, 0x80000000, MPU_ATTR_W | MPU_ATTR_R },
 #ifdef CHIP_VARIANT_MT8195
-	{0x10000000, 0x11400000, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R},
-	{CONFIG_PANIC_DRAM_BASE, CONFIG_PANIC_DRAM_BASE + CONFIG_PANIC_DRAM_SIZE, MPU_ATTR_W | MPU_ATTR_R},
+	{ 0x10000000, 0x11400000, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R },
+	{ CONFIG_PANIC_DRAM_BASE,
+	  CONFIG_PANIC_DRAM_BASE + CONFIG_PANIC_DRAM_SIZE,
+	  MPU_ATTR_W | MPU_ATTR_R },
 #else
-	{0x10000000, 0x11400000, MPU_ATTR_W | MPU_ATTR_R},
+	{ 0x10000000, 0x11400000, MPU_ATTR_W | MPU_ATTR_R },
 #endif
 };
 
@@ -37,7 +39,7 @@ struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
 #ifdef CONFIG_PANIC_CONSOLE_OUTPUT
 static void report_previous_panic(void)
 {
-	struct panic_data * panic = panic_get_data();
+	struct panic_data *panic = panic_get_data();
 
 	if (panic == NULL && SCP_CORE0_MON_PC_LATCH == 0)
 		return;
@@ -48,11 +50,8 @@ static void report_previous_panic(void)
 	} else {
 		ccprintf("No panic data\n");
 	}
-	ccprintf("Latch PC:%x LR:%x SP:%x\n",
-		SCP_CORE0_MON_PC_LATCH,
-		SCP_CORE0_MON_LR_LATCH,
-		SCP_CORE0_MON_SP_LATCH);
-
+	ccprintf("Latch PC:%x LR:%x SP:%x\n", SCP_CORE0_MON_PC_LATCH,
+		 SCP_CORE0_MON_LR_LATCH, SCP_CORE0_MON_SP_LATCH);
 }
 DECLARE_HOOK(HOOK_INIT, report_previous_panic, HOOK_PRIO_DEFAULT);
 #endif
