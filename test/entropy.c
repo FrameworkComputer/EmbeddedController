9592fae7fb78201480845546798bc1c7faedf389
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:20:09 -0600

    test/entropy.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If0496251d6ad350168b5126092a82ae267a908ba
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730498
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- test/entropy.c
+++ test/entropy.c
@@ -26,7 +26,7 @@ uint32_t log2(int32_t val)
 	int val1 = 31 - __builtin_clz(val);
 	int val2 = 32 - __builtin_clz(val - 1);
 
-	return log2_mult * (val1 + val2)/2;
+	return log2_mult * (val1 + val2) / 2;
 }
 
 void run_test(int argc, char **argv)
@@ -52,7 +52,7 @@ void run_test(int argc, char **argv)
 		t1 = get_time();
 		if (i == 0)
 			ccprintf("Got %zd bytes in %" PRId64 " us\n",
-				sizeof(buffer), t1.val - t0.val);
+				 sizeof(buffer), t1.val - t0.val);
 
 		for (j = 0; j < sizeof(buffer); j++)
 			buckets[buffer[j]]++;
@@ -82,7 +82,7 @@ void run_test(int argc, char **argv)
 	ccprintf("\n");
 
 	ccprintf("Entropy: %u/1000 bits\n",
-		entropy * 1000 / (log2_mult * totalcount));
+		 entropy * 1000 / (log2_mult * totalcount));
 
 	/* We want at least 2 bits of entropy (out of a maximum of 8) */
 	if ((entropy / (log2_mult * totalcount)) >= 2)
