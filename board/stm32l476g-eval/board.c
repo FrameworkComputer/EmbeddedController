9859cec3c65ac54966fc49b4f9bfddc1ffb09dc7
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:06:24 -0600

    board/stm32l476g-eval/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ife6eae3ccf1361bcf00aaca3fb1a1afd44f67d37
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728970
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/stm32l476g-eval/board.c
+++ board/stm32l476g-eval/board.c
@@ -32,14 +32,12 @@ void tick_event(void)
 DECLARE_HOOK(HOOK_TICK, tick_event, HOOK_PRIO_DEFAULT);
 
 #ifdef CTS_MODULE_I2C
-const struct i2c_port_t i2c_ports[]  = {
-	{
-		.name = "test",
-		.port = STM32_I2C2_PORT,
-		.kbps = 100,
-		.scl  = GPIO_I2C2_SCL,
-		.sda  = GPIO_I2C2_SDA
-	},
+const struct i2c_port_t i2c_ports[] = {
+	{ .name = "test",
+	  .port = STM32_I2C2_PORT,
+	  .kbps = 100,
+	  .scl = GPIO_I2C2_SCL,
+	  .sda = GPIO_I2C2_SDA },
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 #endif
