d3db636b67218b3d95e27a9d015741073406a20d
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:34:28 -0600

    common/cbi_eeprom.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0891009f625db01245b20cb97ecf31c95204564f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729612
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- common/cbi_eeprom.c
+++ common/cbi_eeprom.c
@@ -14,20 +14,20 @@
 #include "util.h"
 #include "write_protect.h"
 
-#define CPRINTS(format, args...) cprints(CC_SYSTEM, "CBI " format, ## args)
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, "CBI " format, ##args)
 
 /*
  * We allow EEPROMs with page size of 8 or 16. Use 8 to be the most compatible.
  * This causes a little more overhead for writes, but we are not writing to the
  * EEPROM outside of the factory process.
  */
-#define EEPROM_PAGE_WRITE_SIZE	8
-#define EEPROM_PAGE_WRITE_MS	5
+#define EEPROM_PAGE_WRITE_SIZE 8
+#define EEPROM_PAGE_WRITE_MS 5
 
 static int eeprom_read(uint8_t offset, uint8_t *data, int len)
 {
-	return i2c_read_block(I2C_PORT_EEPROM, I2C_ADDR_EEPROM_FLAGS,
-			      offset, data, len);
+	return i2c_read_block(I2C_PORT_EEPROM, I2C_ADDR_EEPROM_FLAGS, offset,
+			      data, len);
 }
 
 static int eeprom_is_write_protected(void)
