2333b18a3c3929fd36c4111d3016bfd172f8d327
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:40:54 -0600

    board/kano/keyboard.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ib17108e32a9a97af0300682310b584a750006e36
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728483
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/kano/keyboard.c
+++ board/kano/keyboard.c
@@ -41,8 +41,8 @@ static const struct ec_response_keybd_config kano_kb = {
 	},
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
 };
-__override const struct ec_response_keybd_config
-*board_vivaldi_keybd_config(void)
+__override const struct ec_response_keybd_config *
+board_vivaldi_keybd_config(void)
 {
 	return &kano_kb;
 }
