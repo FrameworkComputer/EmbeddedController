c4f541c7276cebc74fe678e413453b0cf2e1dc42
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:31:38 -0600

    chip/stm32/usart_rx_interrupt-stm32f3.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I7a5c31e60c02dcfc6b16049d93a7cb1a423f9a4d
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729420
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/usart_rx_interrupt-stm32f3.c
+++ chip/stm32/usart_rx_interrupt-stm32f3.c
@@ -1,1 +1,49 @@
-usart_rx_interrupt.c
+/* Copyright 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Interrupt based USART RX driver for STM32F0 and STM32F3 */
+
+#include "usart.h"
+
+#include "atomic.h"
+#include "common.h"
+#include "queue.h"
+#include "registers.h"
+
+static void usart_rx_init(struct usart_config const *config)
+{
+	intptr_t base = config->hw->base;
+
+	STM32_USART_CR1(base) |= STM32_USART_CR1_RXNEIE;
+	STM32_USART_CR1(base) |= STM32_USART_CR1_RE;
+	STM32_USART_CR3(base) |= STM32_USART_CR3_OVRDIS;
+}
+
+static void usart_rx_interrupt_handler(struct usart_config const *config)
+{
+	intptr_t base = config->hw->base;
+	int32_t status = STM32_USART_SR(base);
+
+	if (status & STM32_USART_SR_RXNE) {
+		uint8_t byte = STM32_USART_RDR(base);
+
+		if (!queue_add_unit(config->producer.queue, &byte))
+			atomic_add((atomic_t *)&(config->state->rx_dropped), 1);
+	}
+}
+
+struct usart_rx const usart_rx_interrupt = {
+	.producer_ops = {
+		/*
+		 * Nothing to do here, we either had enough space in the queue
+		 * when a character came in or we dropped it already.
+		 */
+		.read = NULL,
+	},
+
+	.init      = usart_rx_init,
+	.interrupt = usart_rx_interrupt_handler,
+	.info      = NULL,
+};
