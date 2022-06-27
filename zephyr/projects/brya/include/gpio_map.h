ff60ea9d5493da9864bc77e06bc117346ecef4c4
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:31:18 -0600

    zephyr/projects/brya/include/gpio_map.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I70c689777e447548c3c8c20897f7d1a834898671
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730734
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/projects/brya/include/gpio_map.h
+++ zephyr/projects/brya/include/gpio_map.h
@@ -9,7 +9,7 @@
 #include <zephyr/devicetree.h>
 #include <gpio_signal.h>
 
-#define GPIO_ENTERING_RW	GPIO_UNIMPLEMENTED
+#define GPIO_ENTERING_RW GPIO_UNIMPLEMENTED
 
 #define GPIO_SEQ_EC_DSW_PWROK GPIO_PG_EC_DSW_PWROK
 
