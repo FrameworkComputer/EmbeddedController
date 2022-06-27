341ea5cbde577273d44c14b40f5719bd013267d3
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:59:33 -0600

    board/plankton/usb_pd_pdo.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I7f4454c1214717008b23f5e6086f95daa7ad3649
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728835
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/plankton/usb_pd_pdo.c
+++ board/plankton/usb_pd_pdo.c
@@ -7,28 +7,28 @@
 #include "usb_pd.h"
 #include "usb_pd_pdo.h"
 
-#define PDO_FIXED_FLAGS (PDO_FIXED_DATA_SWAP | PDO_FIXED_UNCONSTRAINED |\
-			 PDO_FIXED_COMM_CAP)
+#define PDO_FIXED_FLAGS \
+	(PDO_FIXED_DATA_SWAP | PDO_FIXED_UNCONSTRAINED | PDO_FIXED_COMM_CAP)
 
 /* Source PDOs */
 const uint32_t pd_src_pdo[] = {
-		PDO_FIXED(5000,  3000, PDO_FIXED_FLAGS),
-		PDO_FIXED(12000, 3000, PDO_FIXED_FLAGS),
-		PDO_FIXED(20000, 3000, PDO_FIXED_FLAGS),
+	PDO_FIXED(5000, 3000, PDO_FIXED_FLAGS),
+	PDO_FIXED(12000, 3000, PDO_FIXED_FLAGS),
+	PDO_FIXED(20000, 3000, PDO_FIXED_FLAGS),
 };
 
 /* Fake PDOs : we just want our pre-defined voltages */
 const uint32_t pd_snk_pdo[] = {
-		PDO_FIXED(5000,   500, PDO_FIXED_FLAGS),
-		PDO_FIXED(12000,  500, PDO_FIXED_FLAGS),
-		PDO_FIXED(20000,  500, PDO_FIXED_FLAGS),
+	PDO_FIXED(5000, 500, PDO_FIXED_FLAGS),
+	PDO_FIXED(12000, 500, PDO_FIXED_FLAGS),
+	PDO_FIXED(20000, 500, PDO_FIXED_FLAGS),
 };
 const int pd_snk_pdo_cnt = ARRAY_SIZE(pd_snk_pdo);
 
 static const int pd_src_pdo_cnts[] = {
-		[SRC_CAP_5V] = 1,
-		[SRC_CAP_12V] = 2,
-		[SRC_CAP_20V] = 3,
+	[SRC_CAP_5V] = 1,
+	[SRC_CAP_12V] = 2,
+	[SRC_CAP_20V] = 3,
 };
 
 static int pd_src_pdo_idx;
