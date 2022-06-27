5c2bcc3d5acccfe41fab53986c4baecd7af36f1e
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:01:34 -0600

    board/rainier/usb_pd_policy.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Iaac1ac5e5b9a310a119cd09b65eeaffbf83cd712
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728852
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/rainier/usb_pd_policy.c
+++ board/rainier/usb_pd_policy.c
@@ -18,8 +18,8 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 
-#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
-#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
 static uint8_t vbus_en;
 
