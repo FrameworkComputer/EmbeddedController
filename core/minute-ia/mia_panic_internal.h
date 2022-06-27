14e4d88e424d17ff2deefbd08853f5b4f041f9e3
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:44:13 -0600

    core/minute-ia/mia_panic_internal.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0e73535092a5c253c8e035d8492439e8b8d84adc
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729855
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- core/minute-ia/mia_panic_internal.h
+++ core/minute-ia/mia_panic_internal.h
@@ -8,10 +8,5 @@
  * convenientely in the same order as pushed by hardwared during a
  * processor exception.
  */
-noreturn
-void exception_panic(
-	uint32_t vector,
-	uint32_t errorcode,
-	uint32_t eip,
-	uint32_t cs,
-	uint32_t eflags);
+noreturn void exception_panic(uint32_t vector, uint32_t errorcode, uint32_t eip,
+			      uint32_t cs, uint32_t eflags);
