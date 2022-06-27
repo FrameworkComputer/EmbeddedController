27bad460e2c35132406e838ae78a0d1e38ac8605
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:37:17 -0600

    zephyr/shim/include/zephyr_mkbp_event.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I522ba0d925cdcf613cc791c73f83a00cf941ab86
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730860
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/zephyr_mkbp_event.h
+++ zephyr/shim/include/zephyr_mkbp_event.h
@@ -3,21 +3,20 @@
  * found in the LICENSE file.
  */
 
-#if !defined(__CROS_EC_MKBP_EVENT_H) || \
-	defined(__CROS_EC_ZEPHYR_MKBP_EVENT_H)
+#if !defined(__CROS_EC_MKBP_EVENT_H) || defined(__CROS_EC_ZEPHYR_MKBP_EVENT_H)
 #error "This file must only be included from mkbp_event.h. " \
 	"Include mkbp_event.h directly"
 #endif
 #define __CROS_EC_ZEPHYR_MKBP_EVENT_H
 
-const struct mkbp_event_source *zephyr_find_mkbp_event_source(
-		uint8_t event_type);
+const struct mkbp_event_source *
+zephyr_find_mkbp_event_source(uint8_t event_type);
 
 /**
  * See include/mkbp_event.h for documentation.
  */
-#define DECLARE_EVENT_SOURCE(_type, _func)                                     \
-	STRUCT_SECTION_ITERABLE(mkbp_event_source, _cros_evtsrc_##_func) = {   \
-		.event_type = _type,                                           \
-		.get_data = _func,                                             \
+#define DECLARE_EVENT_SOURCE(_type, _func)                                   \
+	STRUCT_SECTION_ITERABLE(mkbp_event_source, _cros_evtsrc_##_func) = { \
+		.event_type = _type,                                         \
+		.get_data = _func,                                           \
 	}
