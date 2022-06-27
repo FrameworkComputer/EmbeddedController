bd988c16ac75146ac714a761f05c493075c2a09b
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:34:05 -0600

    zephyr/shim/chip/it8xxx2/include/flash_chip.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I7869ed9263213158802a8118fb47df4b7b9bb775
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730813
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/chip/it8xxx2/include/flash_chip.h
+++ zephyr/shim/chip/it8xxx2/include/flash_chip.h
@@ -9,25 +9,25 @@
  * One page program instruction allows maximum 256 bytes (a page) of data
  * to be programmed.
  */
-#define CONFIG_FLASH_WRITE_IDEAL_SIZE   256
+#define CONFIG_FLASH_WRITE_IDEAL_SIZE 256
 /* Minimum write size */
-#define CONFIG_FLASH_WRITE_SIZE         DT_PROP(DT_INST(0, soc_nv_flash), \
-						write_block_size)
+#define CONFIG_FLASH_WRITE_SIZE \
+	DT_PROP(DT_INST(0, soc_nv_flash), write_block_size)
 /* Erase bank size */
-#define CONFIG_FLASH_ERASE_SIZE         DT_PROP(DT_INST(0, soc_nv_flash), \
-						erase_block_size)
+#define CONFIG_FLASH_ERASE_SIZE \
+	DT_PROP(DT_INST(0, soc_nv_flash), erase_block_size)
 /* Protect bank size */
-#define CONFIG_FLASH_BANK_SIZE          CONFIG_FLASH_ERASE_SIZE
+#define CONFIG_FLASH_BANK_SIZE CONFIG_FLASH_ERASE_SIZE
 
-#define CONFIG_RO_STORAGE_OFF           0x0
-#define CONFIG_RW_STORAGE_OFF           0x0
+#define CONFIG_RO_STORAGE_OFF 0x0
+#define CONFIG_RW_STORAGE_OFF 0x0
 
 /*
  * The EC uses the one bank of flash to emulate a SPI-like write protect
  * register with persistent state.
  */
-#define CONFIG_FW_PSTATE_SIZE           CONFIG_FLASH_BANK_SIZE
-#define CONFIG_FW_PSTATE_OFF            (CONFIG_FLASH_SIZE_BYTES / 2 - \
-						CONFIG_FW_PSTATE_SIZE)
+#define CONFIG_FW_PSTATE_SIZE CONFIG_FLASH_BANK_SIZE
+#define CONFIG_FW_PSTATE_OFF \
+	(CONFIG_FLASH_SIZE_BYTES / 2 - CONFIG_FW_PSTATE_SIZE)
 
 #endif /* __CROS_EC_FLASH_CHIP_H */
