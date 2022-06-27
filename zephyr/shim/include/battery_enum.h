5a23e14e7c2cb0561e24a1108fa09019cd64a164
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:35:03 -0600

    zephyr/shim/include/battery_enum.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I60f02c4fa9b35d7bd64a9a5f577814a6e341b3bb
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730565
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/battery_enum.h
+++ zephyr/shim/include/battery_enum.h
@@ -8,16 +8,16 @@
 	"included in all zephyr builds automatically"
 #endif
 
-#define BATTERY_ENUM(val)	DT_CAT(BATTERY_, val)
+#define BATTERY_ENUM(val) DT_CAT(BATTERY_, val)
 #define BATTERY_TYPE(id) BATTERY_ENUM(DT_STRING_UPPER_TOKEN(id, enum_name))
-#define BATTERY_TYPE_WITH_COMMA(id)	BATTERY_TYPE(id),
+#define BATTERY_TYPE_WITH_COMMA(id) BATTERY_TYPE(id),
 
 /* This produces a list of BATTERY_<ENUM_NAME> identifiers */
 enum battery_type {
 #if DT_HAS_COMPAT_STATUS_OKAY(battery_smart)
 	DT_FOREACH_STATUS_OKAY(battery_smart, BATTERY_TYPE_WITH_COMMA)
 #endif
-	BATTERY_TYPE_COUNT,
+		BATTERY_TYPE_COUNT,
 };
 
 #undef BATTERY_TYPE_WITH_COMMA
