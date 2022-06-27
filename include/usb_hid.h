726865bd036d235bbc35ad45872878c762d2b7cc
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:17:10 -0600

    include/usb_hid.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I92dd3e5666f0e951cb50a931a58fa3922eb07290
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730434
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/usb_hid.h
+++ include/usb_hid.h
@@ -8,39 +8,39 @@
 #ifndef __CROS_EC_USB_HID_H
 #define __CROS_EC_USB_HID_H
 
-#define USB_HID_SUBCLASS_BOOT     1
+#define USB_HID_SUBCLASS_BOOT 1
 #define USB_HID_PROTOCOL_KEYBOARD 1
-#define USB_HID_PROTOCOL_MOUSE    2
+#define USB_HID_PROTOCOL_MOUSE 2
 
 /* USB HID Class requests */
-#define USB_HID_REQ_GET_REPORT    0x01
-#define USB_HID_REQ_GET_IDLE      0x02
-#define USB_HID_REQ_GET_PROTOCOL  0x03
-#define USB_HID_REQ_SET_REPORT    0x09
-#define USB_HID_REQ_SET_IDLE      0x0A
-#define USB_HID_REQ_SET_PROTOCOL  0x0B
+#define USB_HID_REQ_GET_REPORT 0x01
+#define USB_HID_REQ_GET_IDLE 0x02
+#define USB_HID_REQ_GET_PROTOCOL 0x03
+#define USB_HID_REQ_SET_REPORT 0x09
+#define USB_HID_REQ_SET_IDLE 0x0A
+#define USB_HID_REQ_SET_PROTOCOL 0x0B
 
 /* USB HID class descriptor types */
-#define USB_HID_DT_HID            (USB_TYPE_CLASS | 0x01)
-#define USB_HID_DT_REPORT         (USB_TYPE_CLASS | 0x02)
-#define USB_HID_DT_PHYSICAL       (USB_TYPE_CLASS | 0x03)
+#define USB_HID_DT_HID (USB_TYPE_CLASS | 0x01)
+#define USB_HID_DT_REPORT (USB_TYPE_CLASS | 0x02)
+#define USB_HID_DT_PHYSICAL (USB_TYPE_CLASS | 0x03)
 
 /* Pre-defined report types */
-#define REPORT_TYPE_INPUT         0x01
-#define REPORT_TYPE_OUTPUT        0x02
-#define REPORT_TYPE_FEATURE       0x03
+#define REPORT_TYPE_INPUT 0x01
+#define REPORT_TYPE_OUTPUT 0x02
+#define REPORT_TYPE_FEATURE 0x03
 
 struct usb_hid_class_descriptor {
-	uint8_t  bDescriptorType;
+	uint8_t bDescriptorType;
 	uint16_t wDescriptorLength;
 } __packed;
 
 struct usb_hid_descriptor {
-	uint8_t  bLength;
-	uint8_t  bDescriptorType;
+	uint8_t bLength;
+	uint8_t bDescriptorType;
 	uint16_t bcdHID;
-	uint8_t  bCountryCode;
-	uint8_t  bNumDescriptors;
+	uint8_t bCountryCode;
+	uint8_t bNumDescriptors;
 	struct usb_hid_class_descriptor desc[1];
 } __packed;
 
