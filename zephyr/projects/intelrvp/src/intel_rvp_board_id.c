b92fbe0b0e1188a5b905f40271abd60cd37d8a40
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:32:27 -0600

    zephyr/projects/intelrvp/src/intel_rvp_board_id.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie35f8b6f2e436f6d48289c7401ede7eee819240f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730778
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/projects/intelrvp/src/intel_rvp_board_id.c
+++ zephyr/projects/intelrvp/src/intel_rvp_board_id.c
@@ -9,25 +9,22 @@
 #define DT_DRV_COMPAT intel_rvp_board_id
 
 BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) <= 1,
-	"Unsupported RVP Board ID instance");
+	     "Unsupported RVP Board ID instance");
 
 #define RVP_ID_GPIO_DT_SPEC_GET(idx, node_id, prop) \
 	GPIO_DT_SPEC_GET_BY_IDX(node_id, prop, idx),
 
-#define RVP_ID_CONFIG_LIST(node_id, prop)           \
-	LISTIFY(DT_PROP_LEN(node_id, prop),         \
-	RVP_ID_GPIO_DT_SPEC_GET, (), node_id, prop)
+#define RVP_ID_CONFIG_LIST(node_id, prop)                                \
+	LISTIFY(DT_PROP_LEN(node_id, prop), RVP_ID_GPIO_DT_SPEC_GET, (), \
+		node_id, prop)
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-const struct gpio_dt_spec bom_id_config[] = {
-	RVP_ID_CONFIG_LIST(DT_DRV_INST(0), bom_gpios)
-};
+const struct gpio_dt_spec bom_id_config[] = { RVP_ID_CONFIG_LIST(DT_DRV_INST(0),
+								 bom_gpios) };
 
-const struct gpio_dt_spec fab_id_config[] = {
-	RVP_ID_CONFIG_LIST(DT_DRV_INST(0), fab_gpios)
-};
+const struct gpio_dt_spec fab_id_config[] = { RVP_ID_CONFIG_LIST(DT_DRV_INST(0),
+								 fab_gpios) };
 
-const struct gpio_dt_spec board_id_config[] = {
-	RVP_ID_CONFIG_LIST(DT_DRV_INST(0), board_gpios)
-};
+const struct gpio_dt_spec board_id_config[] = { RVP_ID_CONFIG_LIST(
+	DT_DRV_INST(0), board_gpios) };
 #endif /* #if DT_HAS_COMPAT_STATUS_OKAY */
