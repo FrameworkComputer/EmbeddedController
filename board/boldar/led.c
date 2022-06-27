56cf21f601c688114d1ae9a5e3321ee6034fee6d
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:23:31 -0600

    board/boldar/led.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia5fbc53fda8d574b98219355b2fa3e8b7ae21377
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728065
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/boldar/led.c
+++ board/boldar/led.c
@@ -19,16 +19,16 @@ const enum ec_led_id supported_led_ids[] = {
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
 struct pwm_led_color_map led_color_map[] = {
-				/* Red, Green, Blue */
-	[EC_LED_COLOR_RED] =    {  100,   0,     0 },
-	[EC_LED_COLOR_GREEN] =  {    0, 100,     0 },
-	[EC_LED_COLOR_BLUE] =   {    0,   0,   100 },
+	/* Red, Green, Blue */
+	[EC_LED_COLOR_RED] = { 100, 0, 0 },
+	[EC_LED_COLOR_GREEN] = { 0, 100, 0 },
+	[EC_LED_COLOR_BLUE] = { 0, 0, 100 },
 	/* The green LED seems to be brighter than the others, so turn down
 	 * green from its natural level for these secondary colors.
 	 */
-	[EC_LED_COLOR_YELLOW] = {  100,  70,     0 },
-	[EC_LED_COLOR_WHITE] =  {  100,  70,   100 },
-	[EC_LED_COLOR_AMBER] =  {  100,  20,     0 },
+	[EC_LED_COLOR_YELLOW] = { 100, 70, 0 },
+	[EC_LED_COLOR_WHITE] = { 100, 70, 100 },
+	[EC_LED_COLOR_AMBER] = { 100, 20, 0 },
 };
 
 struct pwm_led pwm_leds[] = {
