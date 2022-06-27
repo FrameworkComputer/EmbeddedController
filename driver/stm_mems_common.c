3beb81da4d8e08221bb8910ebdf94c67a75b1e2f
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:54:07 -0600

    driver/stm_mems_common.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia3070653d6edb3f3a5f537ef1f2e4df167421abf
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730071
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/stm_mems_common.c
+++ driver/stm_mems_common.c
@@ -11,28 +11,24 @@
 /**
  * st_raw_read_n - Read n bytes for read
  */
-int st_raw_read_n(const int port,
-		  const uint16_t i2c_addr_flags,
+int st_raw_read_n(const int port, const uint16_t i2c_addr_flags,
 		  const uint8_t reg, uint8_t *data_ptr, const int len)
 {
 	/* TODO: Implement SPI interface support */
-	return i2c_read_block(port, i2c_addr_flags,
-			      reg | 0x80, data_ptr, len);
+	return i2c_read_block(port, i2c_addr_flags, reg | 0x80, data_ptr, len);
 }
 
 /**
  * st_raw_read_n_noinc - Read n bytes for read (no auto inc address)
  */
-int st_raw_read_n_noinc(const int port,
-			const uint16_t i2c_addr_flags,
+int st_raw_read_n_noinc(const int port, const uint16_t i2c_addr_flags,
 			const uint8_t reg, uint8_t *data_ptr, const int len)
 {
 	/* TODO: Implement SPI interface support */
-	return i2c_read_block(port, i2c_addr_flags,
-			      reg, data_ptr, len);
+	return i2c_read_block(port, i2c_addr_flags, reg, data_ptr, len);
 }
 
- /**
+/**
  * st_write_data_with_mask - Write register with mask
  * @s: Motion sensor pointer
  * @reg: Device register
@@ -40,24 +36,22 @@ int st_raw_read_n_noinc(const int port,
  * @data: Data pointer
  */
 int st_write_data_with_mask(const struct motion_sensor_t *s, int reg,
-			 uint8_t mask, uint8_t data)
+			    uint8_t mask, uint8_t data)
 {
 	int err;
 	int new_data = 0x00, old_data = 0x00;
 
-	err = st_raw_read8(s->port, s->i2c_spi_addr_flags,
-			   reg, &old_data);
+	err = st_raw_read8(s->port, s->i2c_spi_addr_flags, reg, &old_data);
 	if (err != EC_SUCCESS)
 		return err;
 
-	new_data = ((old_data & (~mask)) |
-		    ((data << __builtin_ctz(mask)) & mask));
+	new_data =
+		((old_data & (~mask)) | ((data << __builtin_ctz(mask)) & mask));
 
 	if (new_data == old_data)
 		return EC_SUCCESS;
 
-	return st_raw_write8(s->port, s->i2c_spi_addr_flags,
-			     reg, new_data);
+	return st_raw_write8(s->port, s->i2c_spi_addr_flags, reg, new_data);
 }
 
 /**
@@ -77,8 +71,8 @@ int st_get_resolution(const struct motion_sensor_t *s)
  * @offset: offset vector
  * @temp: Temp
  */
-int st_set_offset(const struct motion_sensor_t *s,
-	       const int16_t *offset, int16_t temp)
+int st_set_offset(const struct motion_sensor_t *s, const int16_t *offset,
+		  int16_t temp)
 {
 	struct stprivate_data *data = s->drv_data;
 
@@ -94,8 +88,8 @@ int st_set_offset(const struct motion_sensor_t *s,
  * @offset: offset vector
  * @temp: Temp
  */
-int st_get_offset(const struct motion_sensor_t *s,
-	       int16_t *offset, int16_t *temp)
+int st_get_offset(const struct motion_sensor_t *s, int16_t *offset,
+		  int16_t *temp)
 {
 	struct stprivate_data *data = s->drv_data;
 
