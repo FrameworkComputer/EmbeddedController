27d4a70b39cc4d11b02744c2aff3f1b81d6ff1ee
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:51:15 -0600

    driver/led/lm3509.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ide41ed5419bfda44ac10a28d7eeef884c03bca5a
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729997
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/led/lm3509.h
+++ driver/led/lm3509.h
@@ -8,7 +8,7 @@
 #ifndef __CROS_EC_LM3509_H
 #define __CROS_EC_LM3509_H
 
-#define LM3509_I2C_ADDR_FLAGS	0x36
+#define LM3509_I2C_ADDR_FLAGS 0x36
 
 /*
  * General purpose register
@@ -18,7 +18,7 @@
  * [1]= enable secondary current sink.
  * [0]= enable main current sink.
  */
-#define LM3509_REG_GP		0x10
+#define LM3509_REG_GP 0x10
 
 /*
  * Brightness register
@@ -27,10 +27,10 @@
  * 0x1F: 100%
  * Power-on-value: 0% (0xE0)
  */
-#define LM3509_REG_BMAIN	0xA0
-#define LM3509_REG_BSUB		0xB0
+#define LM3509_REG_BMAIN 0xA0
+#define LM3509_REG_BSUB 0xB0
 
-#define LM3509_BMAIN_MASK	0x1F
+#define LM3509_BMAIN_MASK 0x1F
 
 extern const struct kblight_drv kblight_lm3509;
 
