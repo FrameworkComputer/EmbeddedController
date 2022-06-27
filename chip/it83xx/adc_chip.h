08555f2711e95caa06d0f4fc482a853acaf0935e
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:16:37 -0600

    chip/it83xx/adc_chip.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic681124be58f7025b7450a385c30a49ac01718b0
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729192
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/it83xx/adc_chip.h
+++ chip/it83xx/adc_chip.h
@@ -61,13 +61,13 @@ enum vcmp_scan_period {
 	VCMP_SCAN_PERIOD_400US = 0x30,
 	VCMP_SCAN_PERIOD_600US = 0x40,
 	VCMP_SCAN_PERIOD_800US = 0x50,
-	VCMP_SCAN_PERIOD_1MS   = 0x60,
+	VCMP_SCAN_PERIOD_1MS = 0x60,
 	VCMP_SCAN_PERIOD_1_5MS = 0x70,
-	VCMP_SCAN_PERIOD_2MS   = 0x80,
+	VCMP_SCAN_PERIOD_2MS = 0x80,
 	VCMP_SCAN_PERIOD_2_5MS = 0x90,
-	VCMP_SCAN_PERIOD_3MS   = 0xA0,
-	VCMP_SCAN_PERIOD_4MS   = 0xB0,
-	VCMP_SCAN_PERIOD_5MS   = 0xC0,
+	VCMP_SCAN_PERIOD_3MS = 0xA0,
+	VCMP_SCAN_PERIOD_4MS = 0xB0,
+	VCMP_SCAN_PERIOD_5MS = 0xC0,
 };
 
 /* Data structure to define ADC channel control registers. */
@@ -95,8 +95,8 @@ struct vcmp_ctrl_t {
 };
 
 /* supported flags (member "flag" in struct vcmp_t) for voltage comparator */
-#define GREATER_THRESHOLD         BIT(0)
-#define LESS_EQUAL_THRESHOLD      BIT(1)
+#define GREATER_THRESHOLD BIT(0)
+#define LESS_EQUAL_THRESHOLD BIT(1)
 
 /* Data structure for board to define voltage comparator list. */
 struct vcmp_t {
