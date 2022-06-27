4d5c516b98022ecca57081e74473d6f262511bba
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:27:35 -0600

    chip/stm32/config-stm32f373.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Idd76e3d86c568d8696c59b94c0ec2b0540b7c6e0
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729473
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/stm32/config-stm32f373.h
+++ chip/stm32/config-stm32f373.h
@@ -5,24 +5,24 @@
 
 /* Memory mapping */
 #define CONFIG_FLASH_SIZE_BYTES 0x00040000
-#define CONFIG_FLASH_BANK_SIZE  0x2000
-#define CONFIG_FLASH_ERASE_SIZE 0x0800  /* erase bank size */
-#define CONFIG_FLASH_WRITE_SIZE 0x0002  /* minimum write size */
+#define CONFIG_FLASH_BANK_SIZE 0x2000
+#define CONFIG_FLASH_ERASE_SIZE 0x0800 /* erase bank size */
+#define CONFIG_FLASH_WRITE_SIZE 0x0002 /* minimum write size */
 
 /* No page mode on STM32F, so no benefit to larger write sizes */
 #define CONFIG_FLASH_WRITE_IDEAL_SIZE 0x0002
 
-#define CONFIG_RAM_BASE         0x20000000
-#define CONFIG_RAM_SIZE         0x00008000
+#define CONFIG_RAM_BASE 0x20000000
+#define CONFIG_RAM_SIZE 0x00008000
 
 /* Number of IRQ vectors on the NVIC */
 #define CONFIG_IRQ_COUNT 81
 
 /* STM32F3 uses the older 4 byte aligned access mechanism */
-#define CONFIG_USB_RAM_BASE        0x40006000
-#define CONFIG_USB_RAM_SIZE        512
+#define CONFIG_USB_RAM_BASE 0x40006000
+#define CONFIG_USB_RAM_SIZE 512
 #define CONFIG_USB_RAM_ACCESS_TYPE uint32_t
 #define CONFIG_USB_RAM_ACCESS_SIZE 4
 
 /* DFU Address */
-#define STM32_DFU_BASE              0x1fffd800
+#define STM32_DFU_BASE 0x1fffd800
