f3d9ac9ada7ecc01becc28c846f52ccfd143ae3f
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:10:30 -0600

    include/driver/ppc/syv682x_public.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8701aeb65e931b2567353c4aec627f82bbf21ba2
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730250
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/driver/ppc/syv682x_public.h
+++ include/driver/ppc/syv682x_public.h
@@ -9,10 +9,10 @@
 #define __CROS_EC_DRIVER_PPC_SYV682X_PUBLIC_H
 
 /* I2C addresses */
-#define SYV682X_ADDR0_FLAGS		0x40
-#define SYV682X_ADDR1_FLAGS		0x41
-#define SYV682X_ADDR2_FLAGS		0x42
-#define SYV682X_ADDR3_FLAGS		0x43
+#define SYV682X_ADDR0_FLAGS 0x40
+#define SYV682X_ADDR1_FLAGS 0x41
+#define SYV682X_ADDR2_FLAGS 0x42
+#define SYV682X_ADDR3_FLAGS 0x43
 
 extern const struct ppc_drv syv682x_drv;
 
