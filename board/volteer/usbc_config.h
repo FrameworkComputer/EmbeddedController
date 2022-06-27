99fbb2b502a28b0809400fb6d5364b77f283d4ed
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:11:32 -0600

    board/volteer/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I5147caa945e052cd1f6ff7c4653f7dcb25f4bd5d
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729093
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/volteer/usbc_config.h
+++ board/volteer/usbc_config.h
@@ -8,11 +8,7 @@
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
 
-enum usbc_port {
-	USBC_PORT_C0 = 0,
-	USBC_PORT_C1,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
 /* Configure the USB3 daughterboard type */
 void config_usb3_db_type(void);
