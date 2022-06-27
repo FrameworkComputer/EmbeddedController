f22c1a18ec5bc683d8ee0eca6bd0964860955450
Eric Yilun Lin <yllin@chromium.org>
2022-06-27 16:07:34 +0800

    kingler: migrate usb_mux config to dts
    BUG=b:227359727
    TEST=enable on kingler
    BRANCH=none
    
    Change-Id: I97e19ce15aebcd4fc8b17e67a0bc04c55127372e
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3725856
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Tested-by: Eric Yilun Lin <yllin@google.com>

--- zephyr/projects/corsola/src/kingler/usbc_config.c
+++ zephyr/projects/corsola/src/kingler/usbc_config.c
@@ -37,7 +37,7 @@
 /* USB Mux */
 
 /* USB Mux C1 : board_init of PS8743 */
-static int ps8743_tune_mux(const struct usb_mux *me)
+int ps8743_mux_1_board_init(const struct usb_mux *me)
 {
 	ps8743_tune_usb_eq(me, PS8743_USB_EQ_TX_3_6_DB,
 			   PS8743_USB_EQ_RX_16_0_DB);
@@ -55,35 +55,6 @@ void board_usb_mux_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, board_usb_mux_init, HOOK_PRIO_INIT_I2C + 1);
-
-const struct usb_mux usbc0_virtual_mux = {
-	.usb_port = USBC_PORT_C0,
-	.driver = &virtual_usb_mux_driver,
-	.hpd_update = &virtual_hpd_update,
-};
-
-const struct usb_mux usbc1_virtual_mux = {
-	.usb_port = USBC_PORT_C1,
-	.driver = &virtual_usb_mux_driver,
-	.hpd_update = &virtual_hpd_update,
-};
-
-struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
-	[USBC_PORT_C0] = {
-		.usb_port = USBC_PORT_C0,
-		.driver = &anx7447_usb_mux_driver,
-		.hpd_update = &anx7447_tcpc_update_hpd_status,
-		.next_mux = &usbc0_virtual_mux,
-	},
-	[USBC_PORT_C1] = {
-		.usb_port = USBC_PORT_C1,
-		.i2c_port = I2C_PORT_USB_C1,
-		.i2c_addr_flags = PS8743_I2C_ADDR0_FLAG,
-		.driver = &ps8743_usb_mux_driver,
-		.next_mux = &usbc1_virtual_mux,
-		.board_init = &ps8743_tune_mux,
-	},
-};
 
 void board_tcpc_init(void)
 {
