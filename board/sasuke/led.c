28064b54d556f3580f94a42ce61d7932264dd762
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:03:18 -0600

    board/sasuke/led.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I4e97e864294be0ca553ce97a4d15312df65c0a21
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728908
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/sasuke/led.c
+++ board/sasuke/led.c
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * Power and battery LED control for sasuke 
+ * Power and battery LED control for sasuke
  */
 
 #include "chipset.h"
@@ -12,8 +12,8 @@
 #include "led_common.h"
 #include "led_onoff_states.h"
 
-#define LED_OFF_LVL	1
-#define LED_ON_LVL	0
+#define LED_OFF_LVL 1
+#define LED_ON_LVL 0
 
 __override const int led_charge_lvl_1 = 1;
 
@@ -21,33 +21,36 @@ __override const int led_charge_lvl_2 = 100;
 
 /* sasuke : There are 3 leds for AC, Battery and Power */
 __override struct led_descriptor
-			led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
-	[STATE_CHARGING_LVL_1]	     = {{EC_LED_COLOR_RED, 1 * LED_ONE_SEC},
-       					{LED_OFF, 1 * LED_ONE_SEC} },
-	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_RED, LED_INDEFINITE} },
-	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_GREEN, LED_INDEFINITE} },
-	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_GREEN, LED_INDEFINITE} },
-	[STATE_DISCHARGE_S0_BAT_LOW] = {{LED_OFF, LED_INDEFINITE} },
-	[STATE_DISCHARGE_S3]	     = {{LED_OFF, LED_INDEFINITE} },
-	[STATE_DISCHARGE_S5]         = {{LED_OFF, LED_INDEFINITE} },
-	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_RED, 0.5 * LED_ONE_SEC},
-					{LED_OFF, 0.5 * LED_ONE_SEC} },
-	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_RED, 1 * LED_ONE_SEC},
-					{LED_OFF, 1 * LED_ONE_SEC} },
-};
+	led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
+		[STATE_CHARGING_LVL_1] = { { EC_LED_COLOR_RED,
+					     1 * LED_ONE_SEC },
+					   { LED_OFF, 1 * LED_ONE_SEC } },
+		[STATE_CHARGING_LVL_2] = { { EC_LED_COLOR_RED,
+					     LED_INDEFINITE } },
+		[STATE_CHARGING_FULL_CHARGE] = { { EC_LED_COLOR_GREEN,
+						   LED_INDEFINITE } },
+		[STATE_DISCHARGE_S0] = { { EC_LED_COLOR_GREEN,
+					   LED_INDEFINITE } },
+		[STATE_DISCHARGE_S0_BAT_LOW] = { { LED_OFF, LED_INDEFINITE } },
+		[STATE_DISCHARGE_S3] = { { LED_OFF, LED_INDEFINITE } },
+		[STATE_DISCHARGE_S5] = { { LED_OFF, LED_INDEFINITE } },
+		[STATE_BATTERY_ERROR] = { { EC_LED_COLOR_RED,
+					    0.5 * LED_ONE_SEC },
+					  { LED_OFF, 0.5 * LED_ONE_SEC } },
+		[STATE_FACTORY_TEST] = { { EC_LED_COLOR_RED, 1 * LED_ONE_SEC },
+					 { LED_OFF, 1 * LED_ONE_SEC } },
+	};
 
 __override const struct led_descriptor
-		led_pwr_state_table[PWR_LED_NUM_STATES][LED_NUM_PHASES] = {
-	[PWR_LED_STATE_ON]           = {{EC_LED_COLOR_BLUE, LED_INDEFINITE} },
-	[PWR_LED_STATE_SUSPEND_AC]   = {{LED_OFF, LED_INDEFINITE} },
-	[PWR_LED_STATE_SUSPEND_NO_AC] = {{LED_OFF, LED_INDEFINITE} },
-	[PWR_LED_STATE_OFF]           = {{LED_OFF, LED_INDEFINITE} },
-};
+	led_pwr_state_table[PWR_LED_NUM_STATES][LED_NUM_PHASES] = {
+		[PWR_LED_STATE_ON] = { { EC_LED_COLOR_BLUE, LED_INDEFINITE } },
+		[PWR_LED_STATE_SUSPEND_AC] = { { LED_OFF, LED_INDEFINITE } },
+		[PWR_LED_STATE_SUSPEND_NO_AC] = { { LED_OFF, LED_INDEFINITE } },
+		[PWR_LED_STATE_OFF] = { { LED_OFF, LED_INDEFINITE } },
+	};
 
-const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_BATTERY_LED,
-	EC_LED_ID_POWER_LED
-};
+const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED,
+					     EC_LED_ID_POWER_LED };
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
@@ -55,14 +58,13 @@ __override void led_set_color_power(enum ec_led_colors color)
 {
 	/* Don't set led if led_auto_control is disabled. */
 	if (!led_auto_control_is_enabled(EC_LED_ID_POWER_LED) ||
-                !led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
+	    !led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
 		return;
-        }
+	}
 
-	if (color == EC_LED_COLOR_BLUE)
-	{
-                gpio_set_level(GPIO_BAT_LED_RED_L, LED_OFF_LVL);
-                gpio_set_level(GPIO_BAT_LED_GREEN_L, LED_OFF_LVL);
+	if (color == EC_LED_COLOR_BLUE) {
+		gpio_set_level(GPIO_BAT_LED_RED_L, LED_OFF_LVL);
+		gpio_set_level(GPIO_BAT_LED_GREEN_L, LED_OFF_LVL);
 		gpio_set_level(GPIO_PWR_LED_BLUE_L, LED_ON_LVL);
 	} else {
 		/* LED_OFF and unsupported colors */
@@ -74,17 +76,16 @@ __override void led_set_color_battery(enum ec_led_colors color)
 {
 	/* Don't set led if led_auto_control is disabled. */
 	if (!led_auto_control_is_enabled(EC_LED_ID_POWER_LED) ||
-                !led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
+	    !led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
 		return;
-        }
+	}
 
 	/* Battery leds must be turn off when blue led is on
 	 * because casta has 3-in-1 led.
 	 */
-	if(!gpio_get_level(GPIO_PWR_LED_BLUE_L))
-	{
+	if (!gpio_get_level(GPIO_PWR_LED_BLUE_L)) {
 		gpio_set_level(GPIO_BAT_LED_RED_L, LED_OFF_LVL); /*red*/
-                gpio_set_level(GPIO_BAT_LED_GREEN_L, LED_OFF_LVL); /*green*/
+		gpio_set_level(GPIO_BAT_LED_GREEN_L, LED_OFF_LVL); /*green*/
 		return;
 	}
 
@@ -118,10 +119,13 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
 	if (led_id == EC_LED_ID_BATTERY_LED) {
 		gpio_set_level(GPIO_PWR_LED_BLUE_L, LED_OFF_LVL);
-		gpio_set_level(GPIO_BAT_LED_GREEN_L, !brightness[EC_LED_COLOR_GREEN]);
-		gpio_set_level(GPIO_BAT_LED_RED_L, !brightness[EC_LED_COLOR_RED]);
+		gpio_set_level(GPIO_BAT_LED_GREEN_L,
+			       !brightness[EC_LED_COLOR_GREEN]);
+		gpio_set_level(GPIO_BAT_LED_RED_L,
+			       !brightness[EC_LED_COLOR_RED]);
 	} else if (led_id == EC_LED_ID_POWER_LED) {
-		gpio_set_level(GPIO_PWR_LED_BLUE_L, !brightness[EC_LED_COLOR_BLUE]);
+		gpio_set_level(GPIO_PWR_LED_BLUE_L,
+			       !brightness[EC_LED_COLOR_BLUE]);
 		gpio_set_level(GPIO_BAT_LED_GREEN_L, LED_OFF_LVL);
 		gpio_set_level(GPIO_BAT_LED_RED_L, LED_OFF_LVL);
 	}
