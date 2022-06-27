3ee1036ee32d12f4b92ac2b2557483b9113bd705
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:38:00 -0600

    common/mock/tcpci_i2c_mock.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I447666741c8570a34f602ddf1f5c8dde23d032fd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729676
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- common/mock/tcpci_i2c_mock.c
+++ common/mock/tcpci_i2c_mock.c
@@ -18,15 +18,19 @@
 #define VERIFY_TIMEOUT (5 * SECOND)
 
 struct tcpci_reg {
-	uint8_t		offset;
-	uint8_t		size;
-	uint16_t	value;
-	const char	*name;
+	uint8_t offset;
+	uint8_t size;
+	uint16_t value;
+	const char *name;
 };
 
-#define TCPCI_REG(reg_name, reg_size)					\
-	[reg_name] = { .offset = (reg_name), .size = (reg_size),	\
-			.value = 0, .name = #reg_name, }
+#define TCPCI_REG(reg_name, reg_size) \
+	[reg_name] = {                \
+		.offset = (reg_name), \
+		.size = (reg_size),   \
+		.value = 0,           \
+		.name = #reg_name,    \
+	}
 
 static struct tcpci_reg tcpci_regs[] = {
 	TCPCI_REG(TCPC_REG_VENDOR_ID, 2),
@@ -76,111 +80,109 @@ static int tx_retry_cnt = -1;
 static uint8_t rx_buffer[BUFFER_SIZE];
 static int rx_pos = -1;
 
-static const char * const ctrl_msg_name[] = {
-	[0]				= "C-RSVD_0",
-	[PD_CTRL_GOOD_CRC]		= "C-GOODCRC",
-	[PD_CTRL_GOTO_MIN]		= "C-GOTOMIN",
-	[PD_CTRL_ACCEPT]		= "C-ACCEPT",
-	[PD_CTRL_REJECT]		= "C-REJECT",
-	[PD_CTRL_PING]			= "C-PING",
-	[PD_CTRL_PS_RDY]		= "C-PSRDY",
-	[PD_CTRL_GET_SOURCE_CAP]	= "C-GET_SRC_CAP",
-	[PD_CTRL_GET_SINK_CAP]		= "C-GET_SNK_CAP",
-	[PD_CTRL_DR_SWAP]		= "C-DR_SWAP",
-	[PD_CTRL_PR_SWAP]		= "C-PR_SWAP",
-	[PD_CTRL_VCONN_SWAP]		= "C-VCONN_SW",
-	[PD_CTRL_WAIT]			= "C-WAIT",
-	[PD_CTRL_SOFT_RESET]		= "C-SOFT-RESET",
-	[14]				= "C-RSVD_14",
-	[15]				= "C-RSVD_15",
-	[PD_CTRL_NOT_SUPPORTED]		= "C-NOT_SUPPORTED",
-	[PD_CTRL_GET_SOURCE_CAP_EXT]	= "C-GET_SRC_CAP-EXT",
-	[PD_CTRL_GET_STATUS]		= "C-GET-STATUS",
-	[PD_CTRL_FR_SWAP]		= "C-FR_SWAP",
-	[PD_CTRL_GET_PPS_STATUS]	= "C-GET_PPS_STATUS",
-	[PD_CTRL_GET_COUNTRY_CODES]	= "C-GET_COUNTRY_CODES",
-	[PD_CTRL_GET_SINK_CAP_EXT]	= "C-GET_SINK_CAP_EXT",
-	[PD_CTRL_GET_SOURCE_INFO]	= "C-GET_SOURCE_INFO",
-	[PD_CTRL_GET_REVISION]		= "C-GET_REVISION",
+static const char *const ctrl_msg_name[] = {
+	[0] = "C-RSVD_0",
+	[PD_CTRL_GOOD_CRC] = "C-GOODCRC",
+	[PD_CTRL_GOTO_MIN] = "C-GOTOMIN",
+	[PD_CTRL_ACCEPT] = "C-ACCEPT",
+	[PD_CTRL_REJECT] = "C-REJECT",
+	[PD_CTRL_PING] = "C-PING",
+	[PD_CTRL_PS_RDY] = "C-PSRDY",
+	[PD_CTRL_GET_SOURCE_CAP] = "C-GET_SRC_CAP",
+	[PD_CTRL_GET_SINK_CAP] = "C-GET_SNK_CAP",
+	[PD_CTRL_DR_SWAP] = "C-DR_SWAP",
+	[PD_CTRL_PR_SWAP] = "C-PR_SWAP",
+	[PD_CTRL_VCONN_SWAP] = "C-VCONN_SW",
+	[PD_CTRL_WAIT] = "C-WAIT",
+	[PD_CTRL_SOFT_RESET] = "C-SOFT-RESET",
+	[14] = "C-RSVD_14",
+	[15] = "C-RSVD_15",
+	[PD_CTRL_NOT_SUPPORTED] = "C-NOT_SUPPORTED",
+	[PD_CTRL_GET_SOURCE_CAP_EXT] = "C-GET_SRC_CAP-EXT",
+	[PD_CTRL_GET_STATUS] = "C-GET-STATUS",
+	[PD_CTRL_FR_SWAP] = "C-FR_SWAP",
+	[PD_CTRL_GET_PPS_STATUS] = "C-GET_PPS_STATUS",
+	[PD_CTRL_GET_COUNTRY_CODES] = "C-GET_COUNTRY_CODES",
+	[PD_CTRL_GET_SINK_CAP_EXT] = "C-GET_SINK_CAP_EXT",
+	[PD_CTRL_GET_SOURCE_INFO] = "C-GET_SOURCE_INFO",
+	[PD_CTRL_GET_REVISION] = "C-GET_REVISION",
 };
 
-static const char * const data_msg_name[] = {
-	[0]				= "D-RSVD_0",
-	[PD_DATA_SOURCE_CAP]		= "D-SRC_CAP",
-	[PD_DATA_REQUEST]		= "D-REQUEST",
-	[PD_DATA_BIST]			= "D-BIST",
-	[PD_DATA_SINK_CAP]		= "D-SNK_CAP",
+static const char *const data_msg_name[] = {
+	[0] = "D-RSVD_0",
+	[PD_DATA_SOURCE_CAP] = "D-SRC_CAP",
+	[PD_DATA_REQUEST] = "D-REQUEST",
+	[PD_DATA_BIST] = "D-BIST",
+	[PD_DATA_SINK_CAP] = "D-SNK_CAP",
 	/* 5-14 Reserved for REV 2.0 */
-	[PD_DATA_BATTERY_STATUS]	= "D-BATTERY_STATUS",
-	[PD_DATA_ALERT]			= "D-ALERT",
-	[PD_DATA_GET_COUNTRY_INFO]	= "D-GET_COUNTRY_CODES",
+	[PD_DATA_BATTERY_STATUS] = "D-BATTERY_STATUS",
+	[PD_DATA_ALERT] = "D-ALERT",
+	[PD_DATA_GET_COUNTRY_INFO] = "D-GET_COUNTRY_CODES",
 	/* 8-14 Reserved for REV 3.0 */
-	[PD_DATA_ENTER_USB]		= "D-ENTER_USB",
-	[PD_DATA_EPR_REQUEST]		= "D-EPR_REQUEST",
-	[PD_DATA_EPR_MODE]		= "D-EPR_MODE",
-	[PD_DATA_SOURCE_INFO]		= "D-EPR_SOURCE_INFO",
-	[PD_DATA_REVISION]		= "D-REVISION",
+	[PD_DATA_ENTER_USB] = "D-ENTER_USB",
+	[PD_DATA_EPR_REQUEST] = "D-EPR_REQUEST",
+	[PD_DATA_EPR_MODE] = "D-EPR_MODE",
+	[PD_DATA_SOURCE_INFO] = "D-EPR_SOURCE_INFO",
+	[PD_DATA_REVISION] = "D-REVISION",
 	/* 13-14 Reserved for REV 3.0 */
-	[PD_DATA_VENDOR_DEF]		= "D-VDM",
+	[PD_DATA_VENDOR_DEF] = "D-VDM",
 };
 
-static const char * const ext_msg_name[] = {
-	[0]				= "X-RSVD_0",
-	[PD_EXT_SOURCE_CAP]		= "X-SRC_CAP",
-	[PD_EXT_STATUS]			= "X-STATUS",
-	[PD_EXT_GET_BATTERY_CAP]	= "X-GET_BATTERY_CAP",
-	[PD_EXT_GET_BATTERY_STATUS]	= "X-GET_BATTERY_STATUS",
-	[PD_EXT_BATTERY_CAP]		= "X-BATTERY_CAP",
-	[PD_EXT_GET_MANUFACTURER_INFO]	= "X-GET_MFR_INFO",
-	[PD_EXT_MANUFACTURER_INFO]	= "X-MFR_INFO",
-	[PD_EXT_SECURITY_REQUEST]	= "X-SECURITY_REQ",
-	[PD_EXT_SECURITY_RESPONSE]	= "X-SECURITY_RESP",
+static const char *const ext_msg_name[] = {
+	[0] = "X-RSVD_0",
+	[PD_EXT_SOURCE_CAP] = "X-SRC_CAP",
+	[PD_EXT_STATUS] = "X-STATUS",
+	[PD_EXT_GET_BATTERY_CAP] = "X-GET_BATTERY_CAP",
+	[PD_EXT_GET_BATTERY_STATUS] = "X-GET_BATTERY_STATUS",
+	[PD_EXT_BATTERY_CAP] = "X-BATTERY_CAP",
+	[PD_EXT_GET_MANUFACTURER_INFO] = "X-GET_MFR_INFO",
+	[PD_EXT_MANUFACTURER_INFO] = "X-MFR_INFO",
+	[PD_EXT_SECURITY_REQUEST] = "X-SECURITY_REQ",
+	[PD_EXT_SECURITY_RESPONSE] = "X-SECURITY_RESP",
 	[PD_EXT_FIRMWARE_UPDATE_REQUEST] = "X-FW_UP_REQ",
 	[PD_EXT_FIRMWARE_UPDATE_RESPONSE] = "X-FW_UP_RESP",
-	[PD_EXT_PPS_STATUS]		= "X-PPS_STATUS",
-	[PD_EXT_COUNTRY_INFO]		= "X-COUNTRY_INFO",
-	[PD_EXT_COUNTRY_CODES]		= "X-COUNTRY_CODES",
-	[PD_EXT_SINK_CAP]		= "X-SNK_CAP",
-	[PD_EXT_CONTROL]		= "X-CONTROL",
-	[PD_EXT_EPR_SOURCE_CAP]		= "X-EPR_SRC_CAP",
-	[PD_EXT_EPR_SINK_CAP]		= "X-EPR_SNK_CAP",
+	[PD_EXT_PPS_STATUS] = "X-PPS_STATUS",
+	[PD_EXT_COUNTRY_INFO] = "X-COUNTRY_INFO",
+	[PD_EXT_COUNTRY_CODES] = "X-COUNTRY_CODES",
+	[PD_EXT_SINK_CAP] = "X-SNK_CAP",
+	[PD_EXT_CONTROL] = "X-CONTROL",
+	[PD_EXT_EPR_SOURCE_CAP] = "X-EPR_SRC_CAP",
+	[PD_EXT_EPR_SINK_CAP] = "X-EPR_SNK_CAP",
 };
 
-static const char * const rev_name[] = {
+static const char *const rev_name[] = {
 	[PD_REV10] = "1.0",
 	[PD_REV20] = "2.0",
 	[PD_REV30] = "3.0",
 	[3] = "RSVD",
 };
 
-static const char * const drole_name[] = {
+static const char *const drole_name[] = {
 	[PD_ROLE_UFP] = "UFP",
 	[PD_ROLE_DFP] = "DFP",
 };
 
-static const char * const prole_name[] = {
+static const char *const prole_name[] = {
 	[PD_ROLE_SINK] = "SNK",
 	[PD_ROLE_SOURCE] = "SRC",
 };
 
 static void print_header(const char *prefix, uint16_t header)
 {
-	int type  = PD_HEADER_TYPE(header);
+	int type = PD_HEADER_TYPE(header);
 	int drole = PD_HEADER_DROLE(header);
-	int rev   = PD_HEADER_REV(header);
+	int rev = PD_HEADER_REV(header);
 	int prole = PD_HEADER_PROLE(header);
-	int id    = PD_HEADER_ID(header);
-	int cnt   = PD_HEADER_CNT(header);
-	int ext   = PD_HEADER_EXT(header);
-	const char *name = ext	? ext_msg_name[type]
-				: cnt
-					? data_msg_name[type]
-					: ctrl_msg_name[type];
+	int id = PD_HEADER_ID(header);
+	int cnt = PD_HEADER_CNT(header);
+	int ext = PD_HEADER_EXT(header);
+	const char *name = ext ? ext_msg_name[type] :
+			   cnt ? data_msg_name[type] :
+				 ctrl_msg_name[type];
 
-	ccprints("%s header=0x%x [%s %s %s %s id=%d cnt=%d ext=%d]",
-		 prefix, header,
-		 name, drole_name[drole], rev_name[rev], prole_name[prole],
-		 id, cnt, ext);
+	ccprints("%s header=0x%x [%s %s %s %s id=%d cnt=%d ext=%d]", prefix,
+		 header, name, drole_name[drole], rev_name[rev],
+		 prole_name[prole], id, cnt, ext);
 }
 
 static bool dead_battery(void)
@@ -193,11 +195,9 @@ static bool debug_accessory_indicator_supported(void)
 	return true;
 }
 
-static int verify_transmit(enum tcpci_msg_type want_tx_type,
-			   int want_tx_retry,
+static int verify_transmit(enum tcpci_msg_type want_tx_type, int want_tx_retry,
 			   enum pd_ctrl_msg_type want_ctrl_msg,
-			   enum pd_data_msg_type want_data_msg,
-			   int timeout)
+			   enum pd_data_msg_type want_data_msg, int timeout)
 {
 	uint64_t end_time = get_time().val + timeout;
 
@@ -215,10 +215,10 @@ static int verify_transmit(enum tcpci_msg_type want_tx_type,
 				tcpci_regs[TCPC_REG_TRANSMIT].value);
 			int tx_retry = TCPC_REG_TRANSMIT_RETRY(
 				tcpci_regs[TCPC_REG_TRANSMIT].value);
-			uint16_t header = UINT16_FROM_BYTE_ARRAY_LE(
-						tx_buffer, 1);
-			int pd_type  = PD_HEADER_TYPE(header);
-			int pd_cnt   = PD_HEADER_CNT(header);
+			uint16_t header =
+				UINT16_FROM_BYTE_ARRAY_LE(tx_buffer, 1);
+			int pd_type = PD_HEADER_TYPE(header);
+			int pd_cnt = PD_HEADER_CNT(header);
 
 			TEST_EQ(tx_type, want_tx_type, "%d");
 			if (want_tx_retry >= 0)
@@ -246,46 +246,34 @@ int verify_tcpci_transmit(enum tcpci_msg_type tx_type,
 			  enum pd_ctrl_msg_type ctrl_msg,
 			  enum pd_data_msg_type data_msg)
 {
-	return verify_transmit(tx_type, -1,
-			       ctrl_msg, data_msg,
-			       VERIFY_TIMEOUT);
+	return verify_transmit(tx_type, -1, ctrl_msg, data_msg, VERIFY_TIMEOUT);
 }
 
 int verify_tcpci_tx_timeout(enum tcpci_msg_type tx_type,
 			    enum pd_ctrl_msg_type ctrl_msg,
-			    enum pd_data_msg_type data_msg,
-			    int timeout)
+			    enum pd_data_msg_type data_msg, int timeout)
 {
-	return verify_transmit(tx_type, -1,
-			       ctrl_msg, data_msg,
-			       timeout);
+	return verify_transmit(tx_type, -1, ctrl_msg, data_msg, timeout);
 }
 
 int verify_tcpci_tx_retry_count(enum tcpci_msg_type tx_type,
 				enum pd_ctrl_msg_type ctrl_msg,
-				enum pd_data_msg_type data_msg,
-				int retry_count)
+				enum pd_data_msg_type data_msg, int retry_count)
 {
-	return verify_transmit(tx_type, retry_count,
-			       ctrl_msg, data_msg,
+	return verify_transmit(tx_type, retry_count, ctrl_msg, data_msg,
 			       VERIFY_TIMEOUT);
 }
 
 int verify_tcpci_tx_with_data(enum tcpci_msg_type tx_type,
-			      enum pd_data_msg_type data_msg,
-			      uint8_t *data,
-			      int data_bytes,
-			      int *msg_len,
-			      int timeout)
+			      enum pd_data_msg_type data_msg, uint8_t *data,
+			      int data_bytes, int *msg_len, int timeout)
 {
 	int rv;
 
 	if (timeout <= 0)
 		timeout = VERIFY_TIMEOUT;
 
-	rv = verify_transmit(tx_type, -1,
-			     0, data_msg,
-			     timeout);
+	rv = verify_transmit(tx_type, -1, 0, data_msg, timeout);
 	if (!rv) {
 		TEST_NE(data, NULL, "%p");
 		TEST_GE(data_bytes, tx_msg_cnt, "%d");
@@ -296,13 +284,9 @@ int verify_tcpci_tx_with_data(enum tcpci_msg_type tx_type,
 	return rv;
 }
 
-int verify_tcpci_possible_tx(struct possible_tx possible[],
-			     int possible_cnt,
-			     int *found_index,
-			     uint8_t *data,
-			     int data_bytes,
-			     int *msg_len,
-			     int timeout)
+int verify_tcpci_possible_tx(struct possible_tx possible[], int possible_cnt,
+			     int *found_index, uint8_t *data, int data_bytes,
+			     int *msg_len, int timeout)
 {
 	bool assert_on_timeout = true;
 	uint64_t end_time;
@@ -328,10 +312,10 @@ int verify_tcpci_possible_tx(struct possible_tx possible[],
 			int i;
 			int tx_type = TCPC_REG_TRANSMIT_TYPE(
 				tcpci_regs[TCPC_REG_TRANSMIT].value);
-			uint16_t header = UINT16_FROM_BYTE_ARRAY_LE(
-						tx_buffer, 1);
-			int pd_type  = PD_HEADER_TYPE(header);
-			int pd_cnt   = PD_HEADER_CNT(header);
+			uint16_t header =
+				UINT16_FROM_BYTE_ARRAY_LE(tx_buffer, 1);
+			int pd_type = PD_HEADER_TYPE(header);
+			int pd_cnt = PD_HEADER_CNT(header);
 
 			for (i = 0; i < possible_cnt; ++i) {
 				int want_tx_type = possible[i].tx_type;
@@ -352,8 +336,8 @@ int verify_tcpci_possible_tx(struct possible_tx possible[],
 						continue;
 
 					if (data != NULL) {
-						TEST_GE(data_bytes,
-							tx_msg_cnt, "%d");
+						TEST_GE(data_bytes, tx_msg_cnt,
+							"%d");
 						memcpy(data, tx_buffer,
 						       tx_msg_cnt);
 					}
@@ -391,9 +375,9 @@ void mock_tcpci_receive(enum tcpci_msg_type sop, uint16_t header,
 
 	for (i = 4; i < rx_buffer[0]; i += 4) {
 		rx_buffer[i] = *payload & 0xFF;
-		rx_buffer[i+1] = (*payload >> 8) & 0xFF;
-		rx_buffer[i+2] = (*payload >> 16) & 0xFF;
-		rx_buffer[i+3] = (*payload >> 24) & 0xFF;
+		rx_buffer[i + 1] = (*payload >> 8) & 0xFF;
+		rx_buffer[i + 2] = (*payload >> 16) & 0xFF;
+		rx_buffer[i + 3] = (*payload >> 24) & 0xFF;
 		payload++;
 	}
 
@@ -411,11 +395,11 @@ static void tcpci_reset_register_masks(void)
 	/*
 	 * Using table 4-1 for default mask values
 	 */
-	tcpci_regs[TCPC_REG_ALERT_MASK].value =			0x7FFF;
-	tcpci_regs[TCPC_REG_POWER_STATUS_MASK].value =		0xFF;
-	tcpci_regs[TCPC_REG_FAULT_STATUS_MASK].value =		0xFF;
-	tcpci_regs[TCPC_REG_EXT_STATUS_MASK].value =		0x01;
-	tcpci_regs[TCPC_REG_ALERT_EXTENDED_MASK].value =	0x07;
+	tcpci_regs[TCPC_REG_ALERT_MASK].value = 0x7FFF;
+	tcpci_regs[TCPC_REG_POWER_STATUS_MASK].value = 0xFF;
+	tcpci_regs[TCPC_REG_FAULT_STATUS_MASK].value = 0xFF;
+	tcpci_regs[TCPC_REG_EXT_STATUS_MASK].value = 0x01;
+	tcpci_regs[TCPC_REG_ALERT_EXTENDED_MASK].value = 0x07;
 }
 
 static void tcpci_reset_register_defaults(void)
@@ -427,69 +411,68 @@ static void tcpci_reset_register_defaults(void)
 		tcpci_regs[i].value = 0;
 
 	/* Type-C Release 1,3 */
-	tcpci_regs[TCPC_REG_TC_REV].value =			0x0013;
+	tcpci_regs[TCPC_REG_TC_REV].value = 0x0013;
 	/* PD Revision 3.0 Version 1.2 */
-	tcpci_regs[TCPC_REG_PD_REV].value =			0x3012;
+	tcpci_regs[TCPC_REG_PD_REV].value = 0x3012;
 	/* PD Interface Revision 2.0, Version 1.1 */
-	tcpci_regs[TCPC_REG_PD_INT_REV].value =			0x2011;
+	tcpci_regs[TCPC_REG_PD_INT_REV].value = 0x2011;
 
 	tcpci_reset_register_masks();
 
 	tcpci_regs[TCPC_REG_CONFIG_STD_OUTPUT].value =
-			TCPC_REG_CONFIG_STD_OUTPUT_AUDIO_CONN_N |
-			TCPC_REG_CONFIG_STD_OUTPUT_DBG_ACC_CONN_N;
+		TCPC_REG_CONFIG_STD_OUTPUT_AUDIO_CONN_N |
+		TCPC_REG_CONFIG_STD_OUTPUT_DBG_ACC_CONN_N;
 
 	tcpci_regs[TCPC_REG_POWER_CTRL].value =
-			TCPC_REG_POWER_CTRL_VOLT_ALARM_DIS |
-			TCPC_REG_POWER_CTRL_VBUS_VOL_MONITOR_DIS;
+		TCPC_REG_POWER_CTRL_VOLT_ALARM_DIS |
+		TCPC_REG_POWER_CTRL_VBUS_VOL_MONITOR_DIS;
 
 	tcpci_regs[TCPC_REG_FAULT_STATUS].value =
-			TCPC_REG_FAULT_STATUS_ALL_REGS_RESET;
+		TCPC_REG_FAULT_STATUS_ALL_REGS_RESET;
 
 	tcpci_regs[TCPC_REG_DEV_CAP_1].value =
-			TCPC_REG_DEV_CAP_1_SOURCE_VBUS |
-			TCPC_REG_DEV_CAP_1_SINK_VBUS |
-			TCPC_REG_DEV_CAP_1_PWRROLE_SRC_SNK_DRP |
-			TCPC_REG_DEV_CAP_1_SRC_RESISTOR_RP_3P0_1P5_DEF;
+		TCPC_REG_DEV_CAP_1_SOURCE_VBUS | TCPC_REG_DEV_CAP_1_SINK_VBUS |
+		TCPC_REG_DEV_CAP_1_PWRROLE_SRC_SNK_DRP |
+		TCPC_REG_DEV_CAP_1_SRC_RESISTOR_RP_3P0_1P5_DEF;
 
 	/*
 	 * Using table 4-17 to get the default Role Control and
 	 * Message Header Info register values.
 	 */
 	switch (mock_tcpci_get_reg(TCPC_REG_DEV_CAP_1) &
-			TCPC_REG_DEV_CAP_1_PWRROLE_MASK) {
+		TCPC_REG_DEV_CAP_1_PWRROLE_MASK) {
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SRC_OR_SNK:
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SNK:
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SNK_ACC:
-		tcpci_regs[TCPC_REG_ROLE_CTRL].value =		0x0A;
-		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value =	0x04;
+		tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x0A;
+		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value = 0x04;
 		break;
 
 	case TCPC_REG_DEV_CAP_1_PWRROLE_DRP:
 		if (dead_battery())
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x0A;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x0A;
 		else if (debug_accessory_indicator_supported())
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x4A;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x4A;
 		else
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x0F;
-		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value =	0x04;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x0F;
+		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value = 0x04;
 		break;
 
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SRC:
 		if (!dead_battery())
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x05;
-		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value =	0x0D;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x05;
+		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value = 0x0D;
 		break;
 
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SRC_SNK_DRP_ADPT_CBL:
 	case TCPC_REG_DEV_CAP_1_PWRROLE_SRC_SNK_DRP:
 		if (dead_battery())
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x0A;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x0A;
 		else if (debug_accessory_indicator_supported())
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x4A;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x4A;
 		else
-			tcpci_regs[TCPC_REG_ROLE_CTRL].value =	0x0F;
-		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value =	0x04;
+			tcpci_regs[TCPC_REG_ROLE_CTRL].value = 0x0F;
+		tcpci_regs[TCPC_REG_MSG_HDR_INFO].value = 0x04;
 		break;
 	}
 }
@@ -505,7 +488,7 @@ void mock_tcpci_set_reg(int reg_offset, uint16_t value)
 	struct tcpci_reg *reg = tcpci_regs + reg_offset;
 
 	reg->value = value;
-	ccprints("TCPCI mock set %s = 0x%x",  reg->name, reg->value);
+	ccprints("TCPCI mock set %s = 0x%x", reg->name, reg->value);
 }
 
 void mock_tcpci_set_reg_bits(int reg_offset, uint16_t mask)
@@ -514,8 +497,8 @@ void mock_tcpci_set_reg_bits(int reg_offset, uint16_t mask)
 	uint16_t old_value = reg->value;
 
 	reg->value |= mask;
-	ccprints("TCPCI mock set bits %s (mask=0x%x) = 0x%x -> 0x%x",
-		 reg->name, mask, old_value, reg->value);
+	ccprints("TCPCI mock set bits %s (mask=0x%x) = 0x%x -> 0x%x", reg->name,
+		 mask, old_value, reg->value);
 }
 
 void mock_tcpci_clr_reg_bits(int reg_offset, uint16_t mask)
@@ -524,8 +507,8 @@ void mock_tcpci_clr_reg_bits(int reg_offset, uint16_t mask)
 	uint16_t old_value = reg->value;
 
 	reg->value &= ~mask;
-	ccprints("TCPCI mock clr bits %s (mask=0x%x) = 0x%x -> 0x%x",
-		 reg->name, mask, old_value, reg->value);
+	ccprints("TCPCI mock clr bits %s (mask=0x%x) = 0x%x -> 0x%x", reg->name,
+		 mask, old_value, reg->value);
 }
 
 uint16_t mock_tcpci_get_reg(int reg_offset)
@@ -533,9 +516,8 @@ uint16_t mock_tcpci_get_reg(int reg_offset)
 	return tcpci_regs[reg_offset].value;
 }
 
-int tcpci_i2c_xfer(int port, uint16_t addr_flags,
-		const uint8_t *out, int out_size,
-		uint8_t *in, int in_size, int flags)
+int tcpci_i2c_xfer(int port, uint16_t addr_flags, const uint8_t *out,
+		   int out_size, uint8_t *in, int in_size, int flags)
 {
 	struct tcpci_reg *reg;
 
@@ -554,10 +536,10 @@ int tcpci_i2c_xfer(int port, uint16_t addr_flags,
 			return EC_ERROR_UNKNOWN;
 		}
 		memcpy(in, rx_buffer + rx_pos, in_size);
-		rx_pos	+= in_size;
+		rx_pos += in_size;
 		if (rx_pos == rx_buffer[0] + 1) {
-			print_header("RX", UINT16_FROM_BYTE_ARRAY_LE(
-						rx_buffer, 2));
+			print_header("RX",
+				     UINT16_FROM_BYTE_ARRAY_LE(rx_buffer, 2));
 			rx_pos = -1;
 		}
 		return EC_SUCCESS;
@@ -576,8 +558,8 @@ int tcpci_i2c_xfer(int port, uint16_t addr_flags,
 		tx_pos += out_size;
 		tx_msg_cnt = tx_pos;
 		if (tx_pos > 0 && tx_pos == tx_buffer[0] + 1) {
-			print_header("TX", UINT16_FROM_BYTE_ARRAY_LE(
-						tx_buffer, 1));
+			print_header("TX",
+				     UINT16_FROM_BYTE_ARRAY_LE(tx_buffer, 1));
 			tx_pos = -1;
 			tx_retry_cnt = -1;
 		}
@@ -638,8 +620,7 @@ int tcpci_i2c_xfer(int port, uint16_t addr_flags,
 		else if (reg->size == 2)
 			value = out[1] + (out[2] << 8);
 		ccprints("%s TCPCI write %s = 0x%x",
-			 task_get_name(task_get_current()),
-			 reg->name, value);
+			 task_get_name(task_get_current()), reg->name, value);
 		if (reg->offset == TCPC_REG_ALERT)
 			reg->value &= ~value;
 		else
