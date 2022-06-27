212399eca7b769ce289b527566ad38faba67f00a
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:30:23 -0600

    zephyr/include/dt-bindings/motionsense/utils.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I08123c65a43f38b7cf30a494eabef12ece82d14f
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730715
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/include/dt-bindings/motionsense/utils.h
+++ zephyr/include/dt-bindings/motionsense/utils.h
@@ -7,8 +7,8 @@
 #ifndef DT_BINDINGS_UTILS_H
 #define DT_BINDINGS_UTILS_H
 
-#define BIT(x)			(1U << (x))
-#define ROUND_UP_FLAG		BIT(31)
-#define USEC_PER_MSEC		1000
+#define BIT(x) (1U << (x))
+#define ROUND_UP_FLAG BIT(31)
+#define USEC_PER_MSEC 1000
 
 #endif /* DT_BINDINGS_UTILS_H */
