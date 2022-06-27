63c04b5be090a8333484703551387029cd5f1174
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:37:20 -0600

    zephyr/shim/src/adc.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Iebfd3ac2e93edb46663737cc57f47f8252f8e3ce
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730846
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/src/adc.c
+++ zephyr/shim/src/adc.c
@@ -18,7 +18,7 @@ LOG_MODULE_REGISTER(shim_adc, LOG_LEVEL_ERR);
 #define HAS_NAMED_ADC_CHANNELS DT_NODE_EXISTS(DT_INST(0, named_adc_channels))
 
 #if HAS_NAMED_ADC_CHANNELS
-#define ADC_CHANNEL_COMMA(node_id)                                        \
+#define ADC_CHANNEL_COMMA(node_id) \
 	[ZSHIM_ADC_ID(node_id)] = {                                       \
 		.name = DT_LABEL(node_id),                                \
 		.dev = DEVICE_DT_GET(DT_IO_CHANNELS_CTLR(node_id)),        \
@@ -35,8 +35,8 @@ LOG_MODULE_REGISTER(shim_adc, LOG_LEVEL_ERR);
 		},                                                        \
 	},
 #ifdef CONFIG_ADC_CHANNELS_RUNTIME_CONFIG
-struct adc_t adc_channels[] = { DT_FOREACH_CHILD(
-	DT_INST(0, named_adc_channels), ADC_CHANNEL_COMMA) };
+struct adc_t adc_channels[] = { DT_FOREACH_CHILD(DT_INST(0, named_adc_channels),
+						 ADC_CHANNEL_COMMA) };
 #else
 const struct adc_t adc_channels[] = { DT_FOREACH_CHILD(
 	DT_INST(0, named_adc_channels), ADC_CHANNEL_COMMA) };
