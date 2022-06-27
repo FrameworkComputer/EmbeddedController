99511274de8f1fcd50b76e576a7b47fed2f79ce1
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:40:12 -0600

    board/jacuzzi/battery.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I7267a683de78a94e76fe121f15acc83aa9fa706b
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728474
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/jacuzzi/battery.c
+++ board/jacuzzi/battery.c
@@ -120,7 +120,8 @@ const struct board_batt_params board_battery_info[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_PANASONIC_AC16L5J_KT00205009;
+const enum battery_type DEFAULT_BATTERY_TYPE =
+	BATTERY_PANASONIC_AC16L5J_KT00205009;
 
 enum battery_present battery_hw_present(void)
 {
