230149fcf3ed30a3e5e32a8c16e168775271042b
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:13:46 -0600

    board/zinger/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I82884b5dbf0227a57cb34bac6fb8c321c4ba71dc
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729139
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/zinger/board.c
+++ board/zinger/board.c
@@ -21,8 +21,8 @@
 static uint32_t rsa_workbuf[3 * RSANUMWORDS];
 
 /* RW firmware reset vector */
-static uint32_t * const rw_rst =
-	(uint32_t *)(CONFIG_PROGRAM_MEMORY_BASE+CONFIG_RW_MEM_OFF+4);
+static uint32_t *const rw_rst =
+	(uint32_t *)(CONFIG_PROGRAM_MEMORY_BASE + CONFIG_RW_MEM_OFF + 4);
 
 /* External interrupt EXTINT7 for external comparator on PA7 */
 static void pd_rx_interrupt(void)
@@ -57,8 +57,8 @@ static int check_rw_valid(void *rw_hash)
 		return 0;
 
 	good = rsa_verify((const struct rsa_public_key *)CONFIG_RO_PUBKEY_ADDR,
-			  (const uint8_t *)CONFIG_RW_SIG_ADDR,
-			  rw_hash, rsa_workbuf);
+			  (const uint8_t *)CONFIG_RW_SIG_ADDR, rw_hash,
+			  rsa_workbuf);
 	if (!good) {
 		debug_printf("RSA FAILED\n");
 		pd_log_event(PD_EVENT_ACC_RW_FAIL, 0, 0, NULL);
@@ -75,8 +75,7 @@ int main(void)
 	void *rw_hash;
 
 	hardware_init();
-	debug_printf("%s started\n",
-		is_ro_mode() ? "RO" : "RW");
+	debug_printf("%s started\n", is_ro_mode() ? "RO" : "RW");
 
 	/* the RO partition protection is not enabled : do it */
 	if (!flash_physical_is_permanently_protected())
