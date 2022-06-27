830e568a898d521d11f121e3f722c6d486d9f067
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:30:37 -0600

    board/discovery-stm32f072/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I5289a919e0d84007cd798287a0c76548119b6393
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728228
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/discovery-stm32f072/board.h
+++ board/discovery-stm32f072/board.h
@@ -32,27 +32,26 @@
 #define CONFIG_USB_CONSOLE
 
 /* USB interface indexes (use define rather than enum to expand them) */
-#define USB_IFACE_STREAM  0
-#define USB_IFACE_GPIO    1
-#define USB_IFACE_SPI     2
+#define USB_IFACE_STREAM 0
+#define USB_IFACE_GPIO 1
+#define USB_IFACE_SPI 2
 #define USB_IFACE_CONSOLE 3
-#define USB_IFACE_COUNT   4
+#define USB_IFACE_COUNT 4
 
 /* USB endpoint indexes (use define rather than enum to expand them) */
 #define USB_EP_CONTROL 0
-#define USB_EP_STREAM  1
-#define USB_EP_GPIO    2
-#define USB_EP_SPI     3
+#define USB_EP_STREAM 1
+#define USB_EP_GPIO 2
+#define USB_EP_SPI 3
 #define USB_EP_CONSOLE 4
-#define USB_EP_COUNT   5
+#define USB_EP_COUNT 5
 
 /* Enable control of GPIOs over USB */
 #define CONFIG_USB_GPIO
 
 /* Enable control of SPI over USB */
 #define CONFIG_SPI_CONTROLLER
-#define CONFIG_SPI_FLASH_PORT    0  /* First SPI controller port */
-
+#define CONFIG_SPI_FLASH_PORT 0 /* First SPI controller port */
 
 #define CONFIG_USB_SPI
 
