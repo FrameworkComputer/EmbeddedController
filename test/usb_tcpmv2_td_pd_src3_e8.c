d473753c1b0a88e1371c1185a17f9552be82a64c
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:24:27 -0600

    test/usb_tcpmv2_td_pd_src3_e8.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I275008147b1f48a44a9a81d23707e4bb7b061e58
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730559
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- test/usb_tcpmv2_td_pd_src3_e8.c
+++ test/usb_tcpmv2_td_pd_src3_e8.c
@@ -24,10 +24,10 @@
 #define SCEDB_NUM_BYTES 24
 
 #define BSDO_NUM_BYTES 4
-#define BSDO_INV_BATTERY_REF(bsdo)	(((bsdo) >> 8) & 1)
-#define BSDO_BATTERY_PRESENT(bsdo)	(((bsdo) >> 9) & 1)
-#define BSDO_BATTERY_CHRG_STS(bsdo)	(((bsdo) >> 10) & 3)
-#define BSDO_BATTERY_INFO(bsdo)		(((bsdo) >> 8) & 0xFF)
+#define BSDO_INV_BATTERY_REF(bsdo) (((bsdo) >> 8) & 1)
+#define BSDO_BATTERY_PRESENT(bsdo) (((bsdo) >> 9) & 1)
+#define BSDO_BATTERY_CHRG_STS(bsdo) (((bsdo) >> 10) & 3)
+#define BSDO_BATTERY_INFO(bsdo) (((bsdo) >> 8) & 0xFF)
 
 static int number_of_fixed_batteries(void)
 {
@@ -88,33 +88,26 @@ int test_td_pd_src3_e8(void)
 	possible[1].ctrl_msg = 0;
 	possible[1].data_msg = PD_DATA_SOURCE_CAP;
 
-	TEST_EQ(verify_tcpci_possible_tx(possible,
-					 2,
-					 &found_index,
-					 data,
-					 sizeof(data),
-					 &msg_len,
-					 0),
+	TEST_EQ(verify_tcpci_possible_tx(possible, 2, &found_index, data,
+					 sizeof(data), &msg_len, 0),
 		EC_SUCCESS, "%d");
 	if (found_index == 1) {
 		mock_set_alert(TCPC_REG_ALERT_TX_SUCCESS);
 		task_wait_event(10 * MSEC);
 
-		TEST_EQ(msg_len, HEADER_BYTE_OFFSET +
-				 HEADER_NUM_BYTES +
-				 SCEDB_NUM_BYTES,
+		TEST_EQ(msg_len,
+			HEADER_BYTE_OFFSET + HEADER_NUM_BYTES + SCEDB_NUM_BYTES,
 			"%d");
 
 		num_fixed_batteries =
-				data[HEADER_BYTE_OFFSET +
-				     HEADER_NUM_BYTES +
-				     SCEDB_NUM_BATTERY_OFFSET] &
-				0x0F;
+			data[HEADER_BYTE_OFFSET + HEADER_NUM_BYTES +
+			     SCEDB_NUM_BATTERY_OFFSET] &
+			0x0F;
 		num_swappable_battery_slots =
-				(data[HEADER_BYTE_OFFSET +
-				      HEADER_NUM_BYTES +
-				      SCEDB_NUM_BATTERY_OFFSET] >> 4) &
-				0x0F;
+			(data[HEADER_BYTE_OFFSET + HEADER_NUM_BYTES +
+			      SCEDB_NUM_BATTERY_OFFSET] >>
+			 4) &
+			0x0F;
 	}
 	/*
 	 *    If a Not_Supported message is received, the Tester reads the
@@ -141,8 +134,7 @@ int test_td_pd_src3_e8(void)
 		uint16_t header;
 		uint32_t bsdo;
 
-		ext_msg = EXT_MSG_CHUNKED | EXT_MSG_DATA_SIZE_1 |
-			  (ref << 16);
+		ext_msg = EXT_MSG_CHUNKED | EXT_MSG_DATA_SIZE_1 | (ref << 16);
 		partner_send_msg(TCPCI_MSG_SOP, PD_EXT_GET_BATTERY_STATUS, 1, 1,
 				 &ext_msg);
 
@@ -151,17 +143,13 @@ int test_td_pd_src3_e8(void)
 		 *    verifies:
 		 */
 		TEST_EQ(verify_tcpci_tx_with_data(TCPCI_MSG_SOP,
-						  PD_DATA_BATTERY_STATUS,
-						  data,
-						  sizeof(data),
-						  &msg_len,
-						  0),
+						  PD_DATA_BATTERY_STATUS, data,
+						  sizeof(data), &msg_len, 0),
 			EC_SUCCESS, "%d");
 		mock_set_alert(TCPC_REG_ALERT_TX_SUCCESS);
 		task_wait_event(10 * MSEC);
-		TEST_EQ(msg_len, HEADER_BYTE_OFFSET +
-				 HEADER_NUM_BYTES +
-				 BSDO_NUM_BYTES,
+		TEST_EQ(msg_len,
+			HEADER_BYTE_OFFSET + HEADER_NUM_BYTES + BSDO_NUM_BYTES,
 			"%d");
 
 		/*
@@ -200,33 +188,26 @@ int test_td_pd_src3_e8(void)
 		 *    8. If Invalid Battery Reference field is 1, Battery is
 		 *       present field shall be 0
 		 */
-		bsdo = UINT32_FROM_BYTE_ARRAY_LE(data, HEADER_BYTE_OFFSET +
-						       HEADER_NUM_BYTES);
+		bsdo = UINT32_FROM_BYTE_ARRAY_LE(
+			data, HEADER_BYTE_OFFSET + HEADER_NUM_BYTES);
 
 		/* FIXED BATTERY */
 		if (ref < 4) {
 			if (ref < num_fixed_batteries) {
-				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo),
-					0, "%d");
-				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo),
-					1, "%d");
+				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo), 0, "%d");
+				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo), 1, "%d");
 			} else {
-				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo),
-					1, "%d");
-				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo),
-					0, "%d");
+				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo), 1, "%d");
+				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo), 0, "%d");
 			}
 		}
 		/* BATTERY SLOT */
 		else {
 			if ((ref - 4) < num_swappable_battery_slots) {
-				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo),
-					0, "%d");
+				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo), 0, "%d");
 			} else {
-				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo),
-					1, "%d");
-				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo),
-					0, "%d");
+				TEST_EQ(BSDO_INV_BATTERY_REF(bsdo), 1, "%d");
+				TEST_EQ(BSDO_BATTERY_PRESENT(bsdo), 0, "%d");
 			}
 		}
 
