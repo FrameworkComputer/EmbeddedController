09f3a06909e40ac5ee0786fae6434ff0fa98e7dd
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:15:49 -0600

    include/software_panic.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Id68a1cc62812a8ca9957cdaafcffb91bdfc629da
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730406
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/software_panic.h
+++ include/software_panic.h
@@ -9,23 +9,23 @@
 #define __CROS_EC_SOFTWARE_PANIC_H
 
 /* Holds software panic reason PANIC_SW_* */
-#define SOFTWARE_PANIC_REASON_REG	r4
-#define SOFTWARE_PANIC_INFO_REG		r5
+#define SOFTWARE_PANIC_REASON_REG r4
+#define SOFTWARE_PANIC_INFO_REG r5
 
-#define PANIC_SW_BASE		0xDEAD6660
+#define PANIC_SW_BASE 0xDEAD6660
 
 /* Software panic reasons */
-#define PANIC_SW_DIV_ZERO		(PANIC_SW_BASE + 0)
-#define PANIC_SW_STACK_OVERFLOW		(PANIC_SW_BASE + 1)
-#define PANIC_SW_PD_CRASH		(PANIC_SW_BASE + 2)
-#define PANIC_SW_ASSERT			(PANIC_SW_BASE + 3)
-#define PANIC_SW_WATCHDOG		(PANIC_SW_BASE + 4)
-#define PANIC_SW_BAD_RNG		(PANIC_SW_BASE + 5)
-#define PANIC_SW_PMIC_FAULT		(PANIC_SW_BASE + 6)
+#define PANIC_SW_DIV_ZERO (PANIC_SW_BASE + 0)
+#define PANIC_SW_STACK_OVERFLOW (PANIC_SW_BASE + 1)
+#define PANIC_SW_PD_CRASH (PANIC_SW_BASE + 2)
+#define PANIC_SW_ASSERT (PANIC_SW_BASE + 3)
+#define PANIC_SW_WATCHDOG (PANIC_SW_BASE + 4)
+#define PANIC_SW_BAD_RNG (PANIC_SW_BASE + 5)
+#define PANIC_SW_PMIC_FAULT (PANIC_SW_BASE + 6)
 
 #ifndef __ASSEMBLER__
-extern const char * const panic_sw_reasons[];
+extern const char *const panic_sw_reasons[];
 extern int panic_sw_reason_is_valid(uint32_t vec);
 #endif
 
-#endif  /* __CROS_EC_SOFTWARE_PANIC_H */
+#endif /* __CROS_EC_SOFTWARE_PANIC_H */
