93399d3c6ac27c675d33ff46b4696b5cf0575595
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:15:33 -0600

    include/rtc.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie4657df4b5d194787cc07bbe9683a8df7cc710bd
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730400
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/rtc.h
+++ include/rtc.h
@@ -10,14 +10,14 @@
 
 #include "common.h"
 
-#define SECS_PER_MINUTE     60
-#define SECS_PER_HOUR       (60 * SECS_PER_MINUTE)
-#define SECS_PER_DAY        (24 * SECS_PER_HOUR)
-#define SECS_PER_WEEK       (7 * SECS_PER_DAY)
-#define SECS_PER_YEAR       (365 * SECS_PER_DAY)
+#define SECS_PER_MINUTE 60
+#define SECS_PER_HOUR (60 * SECS_PER_MINUTE)
+#define SECS_PER_DAY (24 * SECS_PER_HOUR)
+#define SECS_PER_WEEK (7 * SECS_PER_DAY)
+#define SECS_PER_YEAR (365 * SECS_PER_DAY)
 /* The seconds elapsed from 01-01-1970 to 01-01-2000 */
-#define SECS_TILL_YEAR_2K   (946684800)
-#define IS_LEAP_YEAR(x)     \
+#define SECS_TILL_YEAR_2K (946684800)
+#define IS_LEAP_YEAR(x) \
 	(((x) % 4 == 0) && (((x) % 100 != 0) || ((x) % 400 == 0)))
 
 struct calendar_date {
