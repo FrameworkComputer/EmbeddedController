d0a753cb655b1e8e01b7ad9b2e8422d004127750
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:32:14 -0600

    zephyr/projects/intelrvp/adlrvp/src/adlrvp.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia05d4dc992be2081bb6d7a9980927a98910718fb
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730773
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/projects/intelrvp/adlrvp/src/adlrvp.c
+++ zephyr/projects/intelrvp/adlrvp/src/adlrvp.c
@@ -29,9 +29,8 @@
 #include "usbc_ppc.h"
 #include "util.h"
 
-
-#define CPRINTF(format, args...) cprintf(CC_COMMAND, format, ## args)
-#define CPRINTS(format, args...) cprints(CC_COMMAND, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_COMMAND, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_COMMAND, format, ##args)
 
 /* TCPC AIC GPIO Configuration */
 const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[] = {
@@ -124,8 +123,8 @@ void board_overcurrent_event(int port, int is_overcurrented)
 {
 	/* Port 0 & 1 and 2 & 3 share same line for over current indication */
 #if defined(HAS_TASK_PD_C2)
-	enum ioex_signal oc_signal = port < TYPE_C_PORT_2 ?
-				IOEX_USB_C0_C1_OC : IOEX_USB_C2_C3_OC;
+	enum ioex_signal oc_signal = port < TYPE_C_PORT_2 ? IOEX_USB_C0_C1_OC :
+							    IOEX_USB_C2_C3_OC;
 #else
 	enum ioex_signal oc_signal = IOEX_USB_C0_C1_OC;
 #endif
@@ -211,11 +210,11 @@ void set_charger_system_voltage(void)
 		 * on AC or AC+battery
 		 */
 		if (extpower_is_present() && battery_is_present()) {
-			bq25710_set_min_system_voltage(CHARGER_SOLO,
-				battery_get_info()->voltage_min);
+			bq25710_set_min_system_voltage(
+				CHARGER_SOLO, battery_get_info()->voltage_min);
 		} else {
-			bq25710_set_min_system_voltage(CHARGER_SOLO,
-				battery_get_info()->voltage_max);
+			bq25710_set_min_system_voltage(
+				CHARGER_SOLO, battery_get_info()->voltage_max);
 		}
 		break;
 
@@ -224,8 +223,7 @@ void set_charger_system_voltage(void)
 		break;
 	}
 }
-DECLARE_HOOK(HOOK_AC_CHANGE, set_charger_system_voltage,
-		HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_AC_CHANGE, set_charger_system_voltage, HOOK_PRIO_DEFAULT);
 
 static void configure_charger(void)
 {
@@ -251,9 +249,8 @@ static void configure_retimer_usbmux(void)
 	case ADLN_LP5_RVP_SKU_BOARD_ID:
 		/* enable TUSB1044RNQR redriver on Port0  */
 		usb_muxes[TYPE_C_PORT_0].i2c_addr_flags =
-					TUSB1064_I2C_ADDR14_FLAGS;
-		usb_muxes[TYPE_C_PORT_0].driver =
-					&tusb1064_usb_mux_driver;
+			TUSB1064_I2C_ADDR14_FLAGS;
+		usb_muxes[TYPE_C_PORT_0].driver = &tusb1064_usb_mux_driver;
 		usb_muxes[TYPE_C_PORT_0].hpd_update = tusb1044_hpd_update;
 
 #if defined(HAS_TASK_PD_C1)
@@ -275,15 +272,15 @@ static void configure_retimer_usbmux(void)
 		 * Change the default usb mux config on runtime to support
 		 * dual retimer topology.
 		 */
-		usb_muxes[TYPE_C_PORT_0].next_mux
-			= &soc_side_bb_retimer0_usb_mux;
+		usb_muxes[TYPE_C_PORT_0].next_mux =
+			&soc_side_bb_retimer0_usb_mux;
 #if defined(HAS_TASK_PD_C1)
-		usb_muxes[TYPE_C_PORT_1].next_mux
-			= &soc_side_bb_retimer1_usb_mux;
+		usb_muxes[TYPE_C_PORT_1].next_mux =
+			&soc_side_bb_retimer1_usb_mux;
 #endif
 		break;
 
-	/* Add additional board SKUs */
+		/* Add additional board SKUs */
 
 	default:
 		break;
@@ -357,8 +354,7 @@ __override int board_get_version(void)
 	 * This loop retries to ensure rail is settled and read is successful
 	 */
 	for (i = 0; i < RVP_VERSION_READ_RETRY_CNT; i++) {
-
-		rv  = gpio_pin_get_dt(&bom_id_config[0]);
+		rv = gpio_pin_get_dt(&bom_id_config[0]);
 
 		if (rv >= 0)
 			break;
@@ -374,21 +370,21 @@ __override int board_get_version(void)
 	 * BOM ID [2]   : IOEX[0]
 	 * BOM ID [1:0] : IOEX[15:14]
 	 */
-	bom_id  = gpio_pin_get_dt(&bom_id_config[0]) << 2;
+	bom_id = gpio_pin_get_dt(&bom_id_config[0]) << 2;
 	bom_id |= gpio_pin_get_dt(&bom_id_config[1]) << 1;
 	bom_id |= gpio_pin_get_dt(&bom_id_config[2]);
 
 	/*
 	 * FAB ID [1:0] : IOEX[2:1] + 1
 	 */
-	fab_id  = gpio_pin_get_dt(&fab_id_config[0]) << 1;
+	fab_id = gpio_pin_get_dt(&fab_id_config[0]) << 1;
 	fab_id |= gpio_pin_get_dt(&fab_id_config[1]);
 	fab_id += 1;
 
 	/*
 	 * BOARD ID[5:0] : IOEX[13:8]
 	 */
-	board_id  = gpio_pin_get_dt(&board_id_config[0]) << 5;
+	board_id = gpio_pin_get_dt(&board_id_config[0]) << 5;
 	board_id |= gpio_pin_get_dt(&board_id_config[1]) << 4;
 	board_id |= gpio_pin_get_dt(&board_id_config[2]) << 3;
 	board_id |= gpio_pin_get_dt(&board_id_config[3]) << 2;
@@ -450,4 +446,4 @@ static int board_pre_task_peripheral_init(const struct device *unused)
 	return 0;
 }
 SYS_INIT(board_pre_task_peripheral_init, APPLICATION,
-			CONFIG_APPLICATION_INIT_PRIORITY);
+	 CONFIG_APPLICATION_INIT_PRIORITY);
