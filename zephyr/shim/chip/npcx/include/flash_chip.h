4f052806c9c76b589ac2ce270a3985ab0069a1ae
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:34:32 -0600

    zephyr/shim/chip/npcx/include/flash_chip.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ib3832b01526560a7dd717bd1120779f78b8877a7
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728329
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/chip/npcx/include/flash_chip.h
+++ zephyr/shim/chip/npcx/include/flash_chip.h
@@ -8,17 +8,17 @@
 
 #define CONFIG_SPI_FLASH_W25Q80 /* Internal SPI flash type. */
 
-#define CONFIG_FLASH_WRITE_SIZE		0x1  /* minimum write size */
-#define CONFIG_FLASH_WRITE_IDEAL_SIZE	256   /* one page size for write */
-#define CONFIG_FLASH_ERASE_SIZE	0x10000
-#define CONFIG_FLASH_BANK_SIZE		CONFIG_FLASH_ERASE_SIZE
+#define CONFIG_FLASH_WRITE_SIZE 0x1 /* minimum write size */
+#define CONFIG_FLASH_WRITE_IDEAL_SIZE 256 /* one page size for write */
+#define CONFIG_FLASH_ERASE_SIZE 0x10000
+#define CONFIG_FLASH_BANK_SIZE CONFIG_FLASH_ERASE_SIZE
 
 /* RO image resides at start of protected region, right after header */
-#define CONFIG_RO_STORAGE_OFF	CONFIG_RO_HDR_SIZE
+#define CONFIG_RO_STORAGE_OFF CONFIG_RO_HDR_SIZE
 
 #define CONFIG_RW_STORAGE_OFF 0
 
 /* Use 4k sector erase for NPCX monitor flash erase operations. */
-#define NPCX_MONITOR_FLASH_ERASE_SIZE	0x1000
+#define NPCX_MONITOR_FLASH_ERASE_SIZE 0x1000
 
 #endif /* __CROS_EC_FLASH_CHIP_H */
