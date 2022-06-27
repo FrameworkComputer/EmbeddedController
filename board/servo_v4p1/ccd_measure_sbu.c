276c3d446a7e9f6e13a3a229ce32948fcc223d45
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:04:40 -0600

    board/servo_v4p1/ccd_measure_sbu.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I1f20e589b944fc5450e782b5b500d8a60ffa129e
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728929
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/servo_v4p1/ccd_measure_sbu.c
+++ board/servo_v4p1/ccd_measure_sbu.c
@@ -11,8 +11,8 @@
 #include "ioexpanders.h"
 #include "timer.h"
 
-#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
-#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ##args)
 
 /*
  * Define voltage thresholds for SBU USB detection.
@@ -20,15 +20,15 @@
  * Max observed USB low across sampled systems: 666mV
  * Min observed USB high across sampled systems: 3026mV
  */
-#define GND_MAX_MV      700
-#define USB_HIGH_MV     2500
-#define SBU_DIRECT      0
-#define SBU_FLIP        1
+#define GND_MAX_MV 700
+#define USB_HIGH_MV 2500
+#define SBU_DIRECT 0
+#define SBU_FLIP 1
 
-#define MODE_SBU_DISCONNECT     0
-#define MODE_SBU_CONNECT        1
-#define MODE_SBU_FLIP           2
-#define MODE_SBU_OTHER          3
+#define MODE_SBU_DISCONNECT 0
+#define MODE_SBU_CONNECT 1
+#define MODE_SBU_FLIP 2
+#define MODE_SBU_OTHER 3
 
 static void ccd_measure_sbu(void);
 DECLARE_DEFERRED(ccd_measure_sbu);
