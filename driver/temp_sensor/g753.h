87d1d22991b5652d79b83219800746757804d721
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:56:13 -0600

    driver/temp_sensor/g753.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ic56222d78765ab1afba94587a15767c09697e121
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730104
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/temp_sensor/g753.h
+++ driver/temp_sensor/g753.h
@@ -8,36 +8,35 @@
 #ifndef __CROS_EC_G753_H
 #define __CROS_EC_G753_H
 
+#define G753_I2C_ADDR_FLAGS 0x48
 
-#define G753_I2C_ADDR_FLAGS			0x48
-
-#define G753_IDX_INTERNAL	0
+#define G753_IDX_INTERNAL 0
 
 /* G753 register */
-#define G753_TEMP_LOCAL				0x00
-#define G753_STATUS				0x02
-#define G753_CONFIGURATION_R			0x03
-#define G753_CONVERSION_RATE_R			0x04
-#define G753_LOCAL_TEMP_HIGH_LIMIT_R		0x05
-#define G753_CONFIGURATION_W			0x09
-#define G753_CONVERSION_RATE_W			0x0A
-#define G753_LOCAL_TEMP_HIGH_LIMIT_W		0x0B
-#define G753_ONESHOT				0x0F
-#define G753_Customer_Data_Log_Register_1	0x2D
-#define G753_Customer_Data_Log_Register_2	0x2E
-#define G753_Customer_Data_Log_Register_3	0x2F
-#define G753_ALERT_MODE				0xBF
-#define G753_CHIP_ID				0xFD
-#define G753_VENDOR_ID				0xFE
-#define G753_DEVICE_ID				0xFF
+#define G753_TEMP_LOCAL 0x00
+#define G753_STATUS 0x02
+#define G753_CONFIGURATION_R 0x03
+#define G753_CONVERSION_RATE_R 0x04
+#define G753_LOCAL_TEMP_HIGH_LIMIT_R 0x05
+#define G753_CONFIGURATION_W 0x09
+#define G753_CONVERSION_RATE_W 0x0A
+#define G753_LOCAL_TEMP_HIGH_LIMIT_W 0x0B
+#define G753_ONESHOT 0x0F
+#define G753_Customer_Data_Log_Register_1 0x2D
+#define G753_Customer_Data_Log_Register_2 0x2E
+#define G753_Customer_Data_Log_Register_3 0x2F
+#define G753_ALERT_MODE 0xBF
+#define G753_CHIP_ID 0xFD
+#define G753_VENDOR_ID 0xFE
+#define G753_DEVICE_ID 0xFF
 
 /* Config register bits */
-#define G753_CONFIGURATION_STANDBY		BIT(6)
-#define G753_CONFIGURATION_ALERT_MASK		BIT(7)
+#define G753_CONFIGURATION_STANDBY BIT(6)
+#define G753_CONFIGURATION_ALERT_MASK BIT(7)
 
 /* Status register bits */
-#define G753_STATUS_LOCAL_TEMP_HIGH_ALARM	BIT(6)
-#define G753_STATUS_BUSY			BIT(7)
+#define G753_STATUS_LOCAL_TEMP_HIGH_ALARM BIT(6)
+#define G753_STATUS_BUSY BIT(7)
 
 /**
  * Get the last polled value of a sensor.
@@ -50,4 +49,4 @@
  */
 int g753_get_val(int idx, int *temp_ptr);
 
-#endif  /* __CROS_EC_G753_H */
+#endif /* __CROS_EC_G753_H */
