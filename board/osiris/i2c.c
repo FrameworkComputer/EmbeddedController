2e57ad3b830d4e866d4309e19da560a8887bacc0
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:57:58 -0600

    board/osiris/i2c.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I6e40aafb0e449e74c24d25a6a8a35a2111948ae1
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728800
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/osiris/i2c.c
+++ board/osiris/i2c.c
@@ -9,7 +9,7 @@
 #include "hooks.h"
 #include "i2c.h"
 
-#define BOARD_ID_FAST_PLUS_CAPABLE	2
+#define BOARD_ID_FAST_PLUS_CAPABLE 2
 
 /* I2C port map configuration */
 const struct i2c_port_t i2c_ports[] = {
