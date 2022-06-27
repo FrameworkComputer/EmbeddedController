5aa417fb82a26c153a6f6097fbb85202b64dc1af
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:20:49 -0600

    board/anahera/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Id9e7b9c5de85116e27abf4665563b118bc55c358
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727991
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/anahera/usbc_config.h
+++ board/anahera/usbc_config.h
@@ -8,21 +8,13 @@
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
 
-#define CONFIG_USB_PD_PORT_MAX_COUNT	2
+#define CONFIG_USB_PD_PORT_MAX_COUNT 2
 
 /* USB-A ports */
-enum usba_port {
-	USBA_PORT_A0 = 0,
-	USBA_PORT_A1,
-	USBA_PORT_COUNT
-};
+enum usba_port { USBA_PORT_A0 = 0, USBA_PORT_A1, USBA_PORT_COUNT };
 
 /* USB-C ports */
-enum usbc_port {
-	USBC_PORT_C0 = 0,
-	USBC_PORT_C1,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
 struct ps8811_reg_val {
 	uint8_t reg;
