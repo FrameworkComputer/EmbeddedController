4684dffb4fea86ed67df58555ece8d2114f402ce
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:24:22 -0600

    chip/npcx/gpio_chip.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ib302a155b6228f2e7f1141f70ab2433e501fdc2c
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729397
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- chip/npcx/gpio_chip.h
+++ chip/npcx/gpio_chip.h
@@ -9,37 +9,40 @@
 struct npcx_wui {
 	uint8_t table : 2;
 	uint8_t group : 3;
-	uint8_t bit   : 3;
+	uint8_t bit : 3;
 };
 
 /* Macros to initialize the MIWU mapping table. */
 #define NPCX_WUI_GPIO_PIN(port, index) NPCX_WUI_GPIO_##port##_##index
-#define WUI(tbl, grp, idx) ((struct npcx_wui) { .table = tbl, .group = grp, \
-						.bit = idx })
-#define WUI_INT(tbl, grp)  WUI(tbl, grp, 0)
-#define WUI_NONE ((struct npcx_wui) { .table = MIWU_TABLE_COUNT, .group = 0, \
-				      .bit = 0 })
+#define WUI(tbl, grp, idx) \
+	((struct npcx_wui){ .table = tbl, .group = grp, .bit = idx })
+#define WUI_INT(tbl, grp) WUI(tbl, grp, 0)
+#define WUI_NONE \
+	((struct npcx_wui){ .table = MIWU_TABLE_COUNT, .group = 0, .bit = 0 })
 
 /* Macros to initialize the alternative and low voltage mapping table. */
-#define NPCX_GPIO_NONE ((struct npcx_gpio) {.port = 0, .bit = 0, .valid = 0})
-#define NPCX_GPIO(grp, pin) ((struct npcx_gpio) {.port = GPIO_PORT_##grp, \
-							.bit = pin, .valid = 1})
+#define NPCX_GPIO_NONE ((struct npcx_gpio){ .port = 0, .bit = 0, .valid = 0 })
+#define NPCX_GPIO(grp, pin) \
+	((struct npcx_gpio){ .port = GPIO_PORT_##grp, .bit = pin, .valid = 1 })
 
-#define NPCX_ALT(grp, pin) ((struct npcx_alt) {.group = ALT_GROUP_##grp, \
-			.bit = NPCX_DEVALT##grp##_##pin, .inverted = 0 })
-#define NPCX_ALT_INV(grp, pin) ((struct npcx_alt) {.group = ALT_GROUP_##grp, \
-			.bit = NPCX_DEVALT##grp##_##pin, .inverted = 1 })
-#define ALT(port, index, _alt) { .gpio = NPCX_GPIO(port, index), \
-			.alt = (_alt) },
+#define NPCX_ALT(grp, pin)                                   \
+	((struct npcx_alt){ .group = ALT_GROUP_##grp,        \
+			    .bit = NPCX_DEVALT##grp##_##pin, \
+			    .inverted = 0 })
+#define NPCX_ALT_INV(grp, pin)                               \
+	((struct npcx_alt){ .group = ALT_GROUP_##grp,        \
+			    .bit = NPCX_DEVALT##grp##_##pin, \
+			    .inverted = 1 })
+#define ALT(port, index, _alt) \
+	{ .gpio = NPCX_GPIO(port, index), .alt = (_alt) },
 
-#define NPCX_LVOL_CTRL_ITEMS(ctrl) { NPCX_LVOL_CTRL_##ctrl##_0, \
-				     NPCX_LVOL_CTRL_##ctrl##_1, \
-				     NPCX_LVOL_CTRL_##ctrl##_2, \
-				     NPCX_LVOL_CTRL_##ctrl##_3, \
-				     NPCX_LVOL_CTRL_##ctrl##_4, \
-				     NPCX_LVOL_CTRL_##ctrl##_5, \
-				     NPCX_LVOL_CTRL_##ctrl##_6, \
-				     NPCX_LVOL_CTRL_##ctrl##_7, }
+#define NPCX_LVOL_CTRL_ITEMS(ctrl)                                            \
+	{                                                                     \
+		NPCX_LVOL_CTRL_##ctrl##_0, NPCX_LVOL_CTRL_##ctrl##_1,         \
+			NPCX_LVOL_CTRL_##ctrl##_2, NPCX_LVOL_CTRL_##ctrl##_3, \
+			NPCX_LVOL_CTRL_##ctrl##_4, NPCX_LVOL_CTRL_##ctrl##_5, \
+			NPCX_LVOL_CTRL_##ctrl##_6, NPCX_LVOL_CTRL_##ctrl##_7, \
+	}
 
 /**
  * Switch NPCX UART pins back to normal GPIOs.
