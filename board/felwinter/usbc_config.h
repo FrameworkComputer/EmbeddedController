dd4b4ecf4f66ed476281002b16dc2fbd9c4ce491
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:34:30 -0600

    board/felwinter/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I937f48cb6e685055b49d4f7698d889fd4ce86d49
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728362
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/felwinter/usbc_config.h
+++ board/felwinter/usbc_config.h
@@ -8,15 +8,11 @@
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
 
-#define CONFIG_USB_PD_PORT_MAX_COUNT	2
+#define CONFIG_USB_PD_PORT_MAX_COUNT 2
 #define CONFIG_USB_PD_TCPC_RUNTIME_CONFIG
 #define CONFIG_USB_MUX_RUNTIME_CONFIG
 
-enum usbc_port {
-	USBC_PORT_C2 = 0,
-	USBC_PORT_C1,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C2 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
 void config_usb_db_type(void);
 void db_update_usb4_config_from_config(void);
