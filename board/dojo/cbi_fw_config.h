f0e659bffcf5c7d5657ec675b60dd33799a4f8bc
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:30:50 -0600

    board/dojo/cbi_fw_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ie678a557808e0d54c8df69b0ff595e5fcc76c480
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728231
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/dojo/cbi_fw_config.h
+++ board/dojo/cbi_fw_config.h
@@ -17,8 +17,8 @@ enum fw_config_kblight_type {
 	KB_BL_ABSENT = 0,
 	KB_BL_PRESENT = 1,
 };
-#define FW_CONFIG_KB_BL_OFFSET			0
-#define FW_CONFIG_KB_BL_MASK			GENMASK(0, 0)
+#define FW_CONFIG_KB_BL_OFFSET 0
+#define FW_CONFIG_KB_BL_MASK GENMASK(0, 0)
 
 /*
  * Keyboard layout (bit 4-5)
@@ -27,8 +27,8 @@ enum fw_config_kblayout_type {
 	KB_BL_TOGGLE_KEY_ABSENT = 0, /* Vol-up key on T12 */
 	KB_BL_TOGGLE_KEY_PRESENT = 1, /* Vol-up key on T13 */
 };
-#define FW_CONFIG_KB_LAYOUT_OFFSET		4
-#define FW_CONFIG_KB_LAYOUT_MASK		GENMASK(5, 4)
+#define FW_CONFIG_KB_LAYOUT_OFFSET 4
+#define FW_CONFIG_KB_LAYOUT_MASK GENMASK(5, 4)
 
 enum fw_config_kblight_type get_cbi_fw_config_kblight(void);
 enum fw_config_kblayout_type get_cbi_fw_config_kblayout(void);
