ca172e69a251dc41e56588376c4159b671621447
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:51:33 -0600

    driver/led/max695x.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: If36a8f7553f9e3751277772a4b0b840c46ef6c38
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730000
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- driver/led/max695x.h
+++ driver/led/max695x.h
@@ -9,36 +9,36 @@
 #define __CROS_EC_MAX656X_H
 
 /* I2C interface */
-#define MAX695X_I2C_ADDR1_FLAGS	0x38
-#define MAX695X_I2C_ADDR2_FLAGS	0x39
+#define MAX695X_I2C_ADDR1_FLAGS 0x38
+#define MAX695X_I2C_ADDR2_FLAGS 0x39
 
 /* Decode mode register */
-#define MAX695X_REG_DECODE_MODE		0x01
+#define MAX695X_REG_DECODE_MODE 0x01
 /* Hexadecimal decode for digits 3–0 */
-#define MAX695X_DECODE_MODE_HEX_DECODE	0x0f
+#define MAX695X_DECODE_MODE_HEX_DECODE 0x0f
 
 /* Intensity register */
-#define MAX695X_REG_INTENSITY		0x02
+#define MAX695X_REG_INTENSITY 0x02
 /* Setting meduim intensity */
-#define MAX695X_INTENSITY_MEDIUM	0x20
+#define MAX695X_INTENSITY_MEDIUM 0x20
 
 /* Scan limit register value */
-#define MAX695X_REG_SCAN_LIMIT		0x03
+#define MAX695X_REG_SCAN_LIMIT 0x03
 
 /* Scanning digits 0-3 */
-#define MAX695X_SCAN_LIMIT_4		0x03
+#define MAX695X_SCAN_LIMIT_4 0x03
 
 /* Configuration register */
-#define MAX695X_REG_CONFIG		0x04
+#define MAX695X_REG_CONFIG 0x04
 /* Shutdown seven segment display */
-#define MAX695X_CONFIG_OPR_SHUTDOWN	0x00
+#define MAX695X_CONFIG_OPR_SHUTDOWN 0x00
 /* Start seven segment display */
-#define MAX695X_CONFIG_OPR_NORMAL	0x01
+#define MAX695X_CONFIG_OPR_NORMAL 0x01
 
 /* Digit addresses */
-#define MAX695X_DIGIT0_ADDR	0x20
-#define MAX695X_DIGIT1_ADDR	0x21
-#define MAX695X_DIGIT2_ADDR	0x22
-#define MAX695X_DIGIT3_ADDR	0x23
+#define MAX695X_DIGIT0_ADDR 0x20
+#define MAX695X_DIGIT1_ADDR 0x21
+#define MAX695X_DIGIT2_ADDR 0x22
+#define MAX695X_DIGIT3_ADDR 0x23
 
 #endif /* __CROS_EC_MAX656X_H */
