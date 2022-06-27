b3164588e53812aed6bfa5740f76dfb5bdbdb962
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:14:06 -0600

    baseboard/brask/usb_pd_policy.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I154ae4b571a4ac3bc9f4378eff137391fbb84fde
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727859
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/brask/usb_pd_policy.c
+++ baseboard/brask/usb_pd_policy.c
@@ -24,8 +24,8 @@
 #include "usb_pd_vdo.h"
 #include "util.h"
 
-#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
-#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
 int pd_check_vconn_swap(int port)
 {
@@ -85,49 +85,38 @@ int board_vbus_source_enabled(int port)
 
 #define OPOS_TBT 1
 
-static const union tbt_mode_resp_device vdo_tbt_modes[1] = {
-		{
-			.tbt_alt_mode = 0x0001,
-			.tbt_adapter = TBT_ADAPTER_TBT3,
-			.intel_spec_b0 = 0,
-			.vendor_spec_b0 = 0,
-			.vendor_spec_b1 = 0,
-		}
-};
+static const union tbt_mode_resp_device vdo_tbt_modes[1] = { {
+	.tbt_alt_mode = 0x0001,
+	.tbt_adapter = TBT_ADAPTER_TBT3,
+	.intel_spec_b0 = 0,
+	.vendor_spec_b0 = 0,
+	.vendor_spec_b1 = 0,
+} };
 
-static const uint32_t vdo_idh = VDO_IDH(
-				1, /* Data caps as USB host     */
-				0, /* Not a USB device   */
-				IDH_PTYPE_PERIPH,
-				1, /* Supports alt modes */
-				USB_VID_GOOGLE);
+static const uint32_t vdo_idh = VDO_IDH(1, /* Data caps as USB host     */
+					0, /* Not a USB device   */
+					IDH_PTYPE_PERIPH, 1, /* Supports alt
+								modes */
+					USB_VID_GOOGLE);
 
-static const uint32_t vdo_idh_rev30 = VDO_IDH_REV30(
-				 1, /* Data caps as USB host     */
-				 0, /* Not a USB device   */
-				 IDH_PTYPE_PERIPH,
-				 1, /* Supports alt modes */
-				 IDH_PTYPE_DFP_HOST,
-				 USB_TYPEC_RECEPTACLE,
-				 USB_VID_GOOGLE);
+static const uint32_t vdo_idh_rev30 =
+	VDO_IDH_REV30(1, /* Data caps as USB host     */
+		      0, /* Not a USB device   */
+		      IDH_PTYPE_PERIPH, 1, /* Supports alt modes */
+		      IDH_PTYPE_DFP_HOST, USB_TYPEC_RECEPTACLE, USB_VID_GOOGLE);
 
-static const uint32_t vdo_product = VDO_PRODUCT(CONFIG_USB_PID,
-						CONFIG_USB_BCD_DEV);
+static const uint32_t vdo_product =
+	VDO_PRODUCT(CONFIG_USB_PID, CONFIG_USB_BCD_DEV);
 
 /* TODO(b/168890624): add USB4 to capability once USB4 response implemented */
 static const uint32_t vdo_ufp1 = VDO_UFP1(
-				   (VDO_UFP1_CAPABILITY_USB20
-				   | VDO_UFP1_CAPABILITY_USB32),
-				   USB_TYPEC_RECEPTACLE,
-				   VDO_UFP1_ALT_MODE_TBT3,
-				   USB_R30_SS_U40_GEN3);
+	(VDO_UFP1_CAPABILITY_USB20 | VDO_UFP1_CAPABILITY_USB32),
+	USB_TYPEC_RECEPTACLE, VDO_UFP1_ALT_MODE_TBT3, USB_R30_SS_U40_GEN3);
 
-static const uint32_t vdo_dfp = VDO_DFP(
-				 (VDO_DFP_HOST_CAPABILITY_USB20
-				 | VDO_DFP_HOST_CAPABILITY_USB32
-				 | VDO_DFP_HOST_CAPABILITY_USB4),
-				 USB_TYPEC_RECEPTACLE,
-				 1  /* Port 1 */);
+static const uint32_t vdo_dfp =
+	VDO_DFP((VDO_DFP_HOST_CAPABILITY_USB20 | VDO_DFP_HOST_CAPABILITY_USB32 |
+		 VDO_DFP_HOST_CAPABILITY_USB4),
+		USB_TYPEC_RECEPTACLE, 1 /* Port 1 */);
 
 static int svdm_tbt_compat_response_identity(int port, uint32_t *payload)
 {
@@ -166,8 +155,7 @@ static int svdm_tbt_compat_response_modes(int port, uint32_t *payload)
 	}
 }
 
-static int svdm_tbt_compat_response_enter_mode(
-	int port, uint32_t *payload)
+static int svdm_tbt_compat_response_enter_mode(int port, uint32_t *payload)
 {
 	mux_state_t mux_state = 0;
 
@@ -176,7 +164,7 @@ static int svdm_tbt_compat_response_enter_mode(
 		return 0; /* NAK */
 
 	if ((PD_VDO_VID(payload[0]) != USB_VID_INTEL) ||
-		(PD_VDO_OPOS(payload[0]) != OPOS_TBT))
+	    (PD_VDO_OPOS(payload[0]) != OPOS_TBT))
 		return 0; /* NAK */
 
 	mux_state = usb_mux_get(port);
@@ -186,7 +174,7 @@ static int svdm_tbt_compat_response_enter_mode(
 	 * Enter Mode Command response.
 	 */
 	if ((mux_state & USB_PD_MUX_USB_ENABLED) ||
-		(mux_state & USB_PD_MUX_SAFE_MODE)) {
+	    (mux_state & USB_PD_MUX_SAFE_MODE)) {
 		pd_ufp_set_enter_mode(port, payload);
 		set_tbt_compat_mode_ready(port);
 		CPRINTS("UFP Enter TBT mode");
