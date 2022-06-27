8baeaf9011772079714c0b26f55ba8300ef01254
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:37:03 -0600

    zephyr/shim/include/usbc/virtual_usb_mux.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I17c3ed33c7fe1e4e2e7a846ec44ef229c81e459c
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730844
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/usbc/virtual_usb_mux.h
+++ zephyr/shim/include/usbc/virtual_usb_mux.h
@@ -8,13 +8,13 @@
 
 #include "usb_mux.h"
 
-#define VIRTUAL_USB_MUX_COMPAT	cros_ec_usbc_mux_virtual
+#define VIRTUAL_USB_MUX_COMPAT cros_ec_usbc_mux_virtual
 
-#define USB_MUX_CONFIG_VIRTUAL(mux_id, port_id, idx)			\
-	{								\
-		USB_MUX_COMMON_FIELDS(mux_id, port_id, idx),		\
-		.driver = &virtual_usb_mux_driver,			\
-		.hpd_update = &virtual_hpd_update,			\
+#define USB_MUX_CONFIG_VIRTUAL(mux_id, port_id, idx)         \
+	{                                                    \
+		USB_MUX_COMMON_FIELDS(mux_id, port_id, idx), \
+			.driver = &virtual_usb_mux_driver,   \
+			.hpd_update = &virtual_hpd_update,   \
 	}
 
 #endif /* __ZEPHYR_SHIM_VIRTUAL_USB_MUX_H */
