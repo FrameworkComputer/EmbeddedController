f6c11e50401a03cd3f82f1b4123dbabcc1cfa2b8
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:01:44 -0600

    board/rammus/cbi_ssfc.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8e3d8cac16e8b4d402934629dc0bc3eb893545a5
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728854
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/rammus/cbi_ssfc.c
+++ board/rammus/cbi_ssfc.c
@@ -23,7 +23,7 @@ static void cbi_ssfc_init(void)
 
 	CPRINTS("Read CBI SSFC : 0x%04X", cached_ssfc.raw_value);
 }
-DECLARE_HOOK(HOOK_INIT, cbi_ssfc_init, HOOK_PRIO_INIT_I2C+1);
+DECLARE_HOOK(HOOK_INIT, cbi_ssfc_init, HOOK_PRIO_INIT_I2C + 1);
 
 enum ec_ssfc_lid_sensor get_cbi_ssfc_lid_sensor(void)
 {
