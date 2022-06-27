c4d874233483c5c700869ca51a84eea1cd31010c
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 14:14:10 -0600

    builtin/endian.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I694fb45a5eebb5049d0c7709d84615d1627e0bf0
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3729144
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- builtin/endian.h
+++ builtin/endian.h
@@ -17,7 +17,7 @@ extern "C" {
  * host byte order. Note that the code currently does not require functions
  * for converting little endian integers.
  */
-#if (__BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__)
+#if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
 
 static inline uint16_t be16toh(uint16_t in)
 {
@@ -40,10 +40,10 @@ static inline uint64_t be64toh(uint64_t in)
 #define htole32(x) (uint32_t)(x)
 #define htole64(x) (uint64_t)(x)
 
-#endif  /* __BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__ */
+#endif /* __BYTE_ORDER__  == __ORDER_LITTLE_ENDIAN__ */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  /* __EC_BUILTIN_ENDIAN_H */
+#endif /* __EC_BUILTIN_ENDIAN_H */
