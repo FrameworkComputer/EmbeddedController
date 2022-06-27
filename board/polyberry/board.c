422f892a20276a24935ca2ad11a3fdc6f8384028
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:59:40 -0600

    board/polyberry/board.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I09e08ed76c30f3fba2cec1c52a8badc779b4ac14
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728837
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/polyberry/board.c
+++ board/polyberry/board.c
@@ -24,13 +24,13 @@
  * Define the strings used in our USB descriptors.
  */
 const void *const usb_strings[] = {
-	[USB_STR_DESC]		= usb_string_desc,
-	[USB_STR_VENDOR]	= USB_STRING_DESC("Google Inc."),
-	[USB_STR_PRODUCT]	= USB_STRING_DESC("Polyberry"),
-	[USB_STR_SERIALNO]	= USB_STRING_DESC("1234-a"),
-	[USB_STR_VERSION]	= USB_STRING_DESC(CROS_EC_VERSION32),
-	[USB_STR_CONSOLE_NAME]	= USB_STRING_DESC("Polyberry EC Shell"),
-	[USB_STR_UPDATE_NAME]	= USB_STRING_DESC("Firmware update"),
+	[USB_STR_DESC] = usb_string_desc,
+	[USB_STR_VENDOR] = USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT] = USB_STRING_DESC("Polyberry"),
+	[USB_STR_SERIALNO] = USB_STRING_DESC("1234-a"),
+	[USB_STR_VERSION] = USB_STRING_DESC(CROS_EC_VERSION32),
+	[USB_STR_CONSOLE_NAME] = USB_STRING_DESC("Polyberry EC Shell"),
+	[USB_STR_UPDATE_NAME] = USB_STRING_DESC("Firmware update"),
 };
 
 BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
@@ -47,15 +47,15 @@ struct dwc_usb usb_ctl = {
 	.irq = STM32_IRQ_OTG_HS,
 };
 
-#define GPIO_SET_HS(bank, number)	\
-	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number) * 2)))
+#define GPIO_SET_HS(bank, number) \
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number)*2)))
 
 void board_config_post_gpio_init(void)
 {
 	/* We use MCO2 clock passthrough to provide a clock to USB HS */
 	gpio_config_module(MODULE_MCO, 1);
 	/* GPIO PC9 to high speed */
-	GPIO_SET_HS(C,  9);
+	GPIO_SET_HS(C, 9);
 
 	if (usb_ctl.phy_type == USB_PHY_ULPI)
 		gpio_set_level(GPIO_USB_MUX_SEL, 0);
@@ -66,19 +66,19 @@ void board_config_post_gpio_init(void)
 	GPIO_SET_HS(A, 11);
 	GPIO_SET_HS(A, 12);
 
-	GPIO_SET_HS(C,  3);
-	GPIO_SET_HS(C,  2);
-	GPIO_SET_HS(C,  0);
-	GPIO_SET_HS(A,  5);
+	GPIO_SET_HS(C, 3);
+	GPIO_SET_HS(C, 2);
+	GPIO_SET_HS(C, 0);
+	GPIO_SET_HS(A, 5);
 
-	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 5);
 	GPIO_SET_HS(B, 13);
 	GPIO_SET_HS(B, 12);
-	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 2);
 	GPIO_SET_HS(B, 10);
-	GPIO_SET_HS(B,  1);
-	GPIO_SET_HS(B,  0);
-	GPIO_SET_HS(A,  3);
+	GPIO_SET_HS(B, 1);
+	GPIO_SET_HS(B, 0);
+	GPIO_SET_HS(A, 3);
 }
 
 static void board_init(void)
