890b1bf862029bce52f4bac151a6c51e384b264e
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:14:13 -0600

    baseboard/brya/baseboard.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I9c573b92eb0a59c1660f591a3bd097347b832980
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727861
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/brya/baseboard.h
+++ baseboard/brya/baseboard.h
@@ -12,18 +12,18 @@
  * By default, enable all console messages excepted HC, ACPI and event:
  * The sensor stack is generating a lot of activity.
  */
-#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#define CC_DEFAULT (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
 #undef CONFIG_HOSTCMD_DEBUG_MODE
 #define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
 
 /* NPCX9 config */
-#define NPCX9_PWM1_SEL    1  /* GPIO C2 is used as PWM1. */
+#define NPCX9_PWM1_SEL 1 /* GPIO C2 is used as PWM1. */
 /*
  * This defines which pads (GPIO10/11 or GPIO64/65) are connected to
  * the "UART1" (NPCX_UART_PORT0) controller when used for
  * CONSOLE_UART.
  */
-#define NPCX_UART_MODULE2	1 /* 1:GPIO64/65 for UART1 */
+#define NPCX_UART_MODULE2 1 /* 1:GPIO64/65 for UART1 */
 
 /* EC Defines */
 #define CONFIG_LTO
@@ -68,7 +68,7 @@
 #define CONFIG_CHARGE_MANAGER
 #define CONFIG_CHARGER
 #define CONFIG_CHARGER_DISCHARGE_ON_AC
-#define CONFIG_CHARGER_INPUT_CURRENT		512
+#define CONFIG_CHARGER_INPUT_CURRENT 512
 
 #define CONFIG_CMD_CHARGER_DUMP
 
@@ -79,8 +79,8 @@
  * Don't allow the system to boot to S0 when the battery is low and unable to
  * communicate on locked systems (which haven't PD negotiated)
  */
-#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON_WITH_BATT	15000
-#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON		15001
+#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON_WITH_BATT 15000
+#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON 15001
 
 /* Common battery defines */
 #define CONFIG_BATTERY_SMART
@@ -181,7 +181,7 @@
 #define CONFIG_USB_PD_TCPM_NCT38XX
 
 #define CONFIG_USB_PD_TCPM_MUX
-#define CONFIG_HOSTCMD_PD_CONTROL		/* Needed for TCPC FW update */
+#define CONFIG_HOSTCMD_PD_CONTROL /* Needed for TCPC FW update */
 #define CONFIG_CMD_USB_PD_PE
 
 /*
@@ -189,7 +189,7 @@
  * with non-PD chargers.  Override the default low-power mode exit delay.
  */
 #undef CONFIG_USB_PD_TCPC_LPM_EXIT_DEBOUNCE
-#define CONFIG_USB_PD_TCPC_LPM_EXIT_DEBOUNCE	(50*MSEC)
+#define CONFIG_USB_PD_TCPC_LPM_EXIT_DEBOUNCE (50 * MSEC)
 
 /* Enable USB3.2 DRD */
 #define CONFIG_USB_PD_USB32_DRD
@@ -230,13 +230,13 @@
  * bytes. Task stack sizes not listed here use more generic values (see
  * ec.tasklist).
  */
-#define BASEBOARD_CHARGER_TASK_STACK_SIZE	1088
-#define BASEBOARD_CHG_RAMP_TASK_STACK_SIZE	1088
-#define BASEBOARD_CHIPSET_TASK_STACK_SIZE	1152
-#define BASEBOARD_PD_INT_TASK_STACK_SIZE	 800
-#define BASEBOARD_PD_TASK_STACK_SIZE		1216
-#define BASEBOARD_POWERBTN_TASK_STACK_SIZE	1088
-#define BASEBOARD_RGBKBD_TASK_STACK_SIZE	2048
+#define BASEBOARD_CHARGER_TASK_STACK_SIZE 1088
+#define BASEBOARD_CHG_RAMP_TASK_STACK_SIZE 1088
+#define BASEBOARD_CHIPSET_TASK_STACK_SIZE 1152
+#define BASEBOARD_PD_INT_TASK_STACK_SIZE 800
+#define BASEBOARD_PD_TASK_STACK_SIZE 1216
+#define BASEBOARD_POWERBTN_TASK_STACK_SIZE 1088
+#define BASEBOARD_RGBKBD_TASK_STACK_SIZE 2048
 
 #ifndef __ASSEMBLER__
 
@@ -247,7 +247,6 @@
 #include "common.h"
 #include "baseboard_usbc_config.h"
 #include "extpower.h"
-
 
 /*
  * Check battery disconnect state.
