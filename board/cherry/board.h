9314bea0e45825cb68e03393f1421fbdce9a2859
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:26:04 -0600

    board/cherry/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia179103c4edd1c0d958ada620d06b2b7fd8e34cb
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728143
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/cherry/board.h
+++ board/cherry/board.h
@@ -26,14 +26,15 @@
 #define CONFIG_LED_ONOFF_STATES_BAT_LOW 10
 
 /* PD / USB-C / PPC */
-#undef CONFIG_USB_PD_DEBUG_LEVEL /* default to 1, configurable in ec console */
+#undef CONFIG_USB_PD_DEBUG_LEVEL /* default to 1, configurable in ec console \
+				  */
 
 /* Optional console commands */
 #define CONFIG_CMD_FLASH
 #define CONFIG_CMD_SCRATCHPAD
 #define CONFIG_CMD_STACKOVERFLOW
 
-#define CONFIG_BATT_FULL_CHIPSET_OFF_INPUT_LIMIT_MV	9000
+#define CONFIG_BATT_FULL_CHIPSET_OFF_INPUT_LIMIT_MV 9000
 
 /* Keyboard */
 #define CONFIG_KEYBOARD_REFRESH_ROW3
