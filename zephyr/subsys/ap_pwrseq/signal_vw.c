c7b0bca6e1f50f7f14beda9b10ab854ebcf74bcc
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:39:59 -0600

    zephyr/subsys/ap_pwrseq/signal_vw.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Icdde12cc19ff4546962a418ba7048d771e044f5e
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730933
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/subsys/ap_pwrseq/signal_vw.c
+++ zephyr/subsys/ap_pwrseq/signal_vw.c
@@ -9,31 +9,30 @@
 
 #include "signal_vw.h"
 
-#define MY_COMPAT	intel_ap_pwrseq_vw
+#define MY_COMPAT intel_ap_pwrseq_vw
 
 #if HAS_VW_SIGNALS
 
 LOG_MODULE_DECLARE(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 
-#define INIT_ESPI_SIGNAL(id)					\
-{								\
-	.espi_signal = DT_STRING_UPPER_TOKEN(id, virtual_wire),	\
-	.signal = PWR_SIGNAL_ENUM(id),				\
-	.invert = DT_PROP(id, vw_invert),			\
-},
+#define INIT_ESPI_SIGNAL(id)                                            \
+	{                                                               \
+		.espi_signal = DT_STRING_UPPER_TOKEN(id, virtual_wire), \
+		.signal = PWR_SIGNAL_ENUM(id),                          \
+		.invert = DT_PROP(id, vw_invert),                       \
+	},
 
 /*
  * Struct containing the eSPI virtual wire config.
  */
 struct vw_config {
-	uint8_t espi_signal;	/* associated VW signal */
-	uint8_t signal;		/* power signal */
-	bool invert;		/* Invert the signal value */
+	uint8_t espi_signal; /* associated VW signal */
+	uint8_t signal; /* power signal */
+	bool invert; /* Invert the signal value */
 };
 
-const static struct vw_config vw_config[] = {
-DT_FOREACH_STATUS_OKAY(MY_COMPAT, INIT_ESPI_SIGNAL)
-};
+const static struct vw_config vw_config[] = { DT_FOREACH_STATUS_OKAY(
+	MY_COMPAT, INIT_ESPI_SIGNAL) };
 
 /*
  * Current signal value.
@@ -49,16 +48,14 @@ static atomic_t signal_valid;
 
 BUILD_ASSERT(ARRAY_SIZE(vw_config) <= (sizeof(atomic_t) * 8));
 
-static void espi_handler(const struct device *dev,
-			 struct espi_callback *cb,
+static void espi_handler(const struct device *dev, struct espi_callback *cb,
 			 struct espi_event event)
 {
-	LOG_DBG("ESPI event type 0x%x %d:%d", event.evt_type,
-		event.evt_details, event.evt_data);
+	LOG_DBG("ESPI event type 0x%x %d:%d", event.evt_type, event.evt_details,
+		event.evt_data);
 	switch (event.evt_type) {
 	default:
-		__ASSERT(0, "ESPI unknown event type: %d",
-			 event.evt_type);
+		__ASSERT(0, "ESPI unknown event type: %d", event.evt_type);
 		break;
 
 	case ESPI_BUS_EVENT_CHANNEL_READY:
@@ -72,9 +69,9 @@ static void espi_handler(const struct device *dev,
 	case ESPI_BUS_EVENT_VWIRE_RECEIVED:
 		for (int i = 0; i < ARRAY_SIZE(vw_config); i++) {
 			if (event.evt_details == vw_config[i].espi_signal) {
-				bool value = vw_config[i].invert
-						? !event.evt_data
-						: !!event.evt_data;
+				bool value = vw_config[i].invert ?
+						     !event.evt_data :
+						     !!event.evt_data;
 
 				atomic_set_bit_to(&signal_data, i, value);
 				atomic_set_bit(&signal_valid, i);
@@ -104,7 +101,7 @@ void power_signal_vw_init(void)
 	/* Configure handler for eSPI events */
 	espi_init_callback(&espi_cb, espi_handler,
 			   ESPI_BUS_EVENT_CHANNEL_READY |
-			   ESPI_BUS_EVENT_VWIRE_RECEIVED);
+				   ESPI_BUS_EVENT_VWIRE_RECEIVED);
 	espi_add_callback(espi_dev, &espi_cb);
 	/*
 	 * Check whether the bus is ready, and if so,
@@ -115,14 +112,13 @@ void power_signal_vw_init(void)
 			uint8_t vw_value;
 
 			if (espi_receive_vwire(espi_dev,
-					   vw_config[i].espi_signal,
-					   &vw_value) == 0) {
+					       vw_config[i].espi_signal,
+					       &vw_value) == 0) {
 				atomic_set_bit_to(&signal_data, i,
-					vw_config[i].invert
-						? !vw_value
-						: !!vw_value);
+						  vw_config[i].invert ?
+							  !vw_value :
+							  !!vw_value);
 				atomic_set_bit(&signal_valid, i);
-
 			}
 		}
 	}
