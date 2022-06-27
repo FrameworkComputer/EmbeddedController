a7cb9e83d2b7bce928c6af2bbb09753be9d43916
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:08:19 -0600

    include/charge_state.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ia3c4df26feb745c64c9890858de232088b61f1e8
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730222
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- include/charge_state.h
+++ include/charge_state.h
@@ -14,14 +14,14 @@
 #define PRECHARGE_TIMEOUT CONFIG_BATTERY_PRECHARGE_TIMEOUT
 
 /* Power state task polling periods in usec */
-#define CHARGE_POLL_PERIOD_VERY_LONG   MINUTE
-#define CHARGE_POLL_PERIOD_LONG        (MSEC * 500)
-#define CHARGE_POLL_PERIOD_CHARGE      (MSEC * 250)
-#define CHARGE_POLL_PERIOD_SHORT       (MSEC * 100)
-#define CHARGE_MIN_SLEEP_USEC          (MSEC * 50)
+#define CHARGE_POLL_PERIOD_VERY_LONG MINUTE
+#define CHARGE_POLL_PERIOD_LONG (MSEC * 500)
+#define CHARGE_POLL_PERIOD_CHARGE (MSEC * 250)
+#define CHARGE_POLL_PERIOD_SHORT (MSEC * 100)
+#define CHARGE_MIN_SLEEP_USEC (MSEC * 50)
 /* If a board hasn't provided a max sleep, use 1 minute as default */
 #ifndef CHARGE_MAX_SLEEP_USEC
-#define CHARGE_MAX_SLEEP_USEC          MINUTE
+#define CHARGE_MAX_SLEEP_USEC MINUTE
 #endif
 
 /* Power states */
@@ -61,20 +61,13 @@ enum charge_state {
 /* Debugging constants, in the same order as enum charge_state. This string
  * table was moved here to sync with enum above.
  */
-#define CHARGE_STATE_NAME_TABLE { \
-		"unchange",	\
-		"init",		\
-		"reinit",	\
-		"idle0",	\
-		"idle",		\
-		"discharge",	\
-		"discharge_full",	\
-		"charge",	\
-		"charge_near_full",      \
-		"error"		\
+#define CHARGE_STATE_NAME_TABLE                                             \
+	{                                                                   \
+		"unchange", "init", "reinit", "idle0", "idle", "discharge", \
+			"discharge_full", "charge", "charge_near_full",     \
+			"error"                                             \
 	}
-	/* End of CHARGE_STATE_NAME_TABLE macro */
-
+/* End of CHARGE_STATE_NAME_TABLE macro */
 
 /**
  * Return current charge state.
@@ -159,8 +152,7 @@ int charge_get_battery_temp(int idx, int *temp_ptr);
  */
 const struct batt_params *charger_current_battery_params(void);
 
-
 /* Config Charger */
 #include "charge_state_v2.h"
 
-#endif	/* __CROS_EC_CHARGE_STATE_H */
+#endif /* __CROS_EC_CHARGE_STATE_H */
