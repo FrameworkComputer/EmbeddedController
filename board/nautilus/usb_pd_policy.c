f6bb860c31b7a4ed4aef73333137186f35451bb3
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:55:33 -0600

    board/nautilus/usb_pd_policy.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ibfa62ad45169851f18de1fb8e667d85f02fff137
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728725
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/nautilus/usb_pd_policy.c
+++ board/nautilus/usb_pd_policy.c
@@ -21,12 +21,12 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 
-#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
-#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
 static uint8_t vbus_en[CONFIG_USB_PD_PORT_MAX_COUNT];
-static uint8_t vbus_rp[CONFIG_USB_PD_PORT_MAX_COUNT] = {TYPEC_RP_1A5,
-							TYPEC_RP_1A5};
+static uint8_t vbus_rp[CONFIG_USB_PD_PORT_MAX_COUNT] = { TYPEC_RP_1A5,
+							 TYPEC_RP_1A5 };
 
 int board_vbus_source_enabled(int port)
 {
@@ -47,8 +47,8 @@ static void board_vbus_update_source_current(int port)
 		 * is controlled by GPIO_USB_C0/1_5V_EN. Both of these signals
 		 * can remain outputs.
 		 */
-		gpio_set_level(gpio_3a_en, vbus_rp[port] == TYPEC_RP_3A0 ?
-				1 : 0);
+		gpio_set_level(gpio_3a_en,
+			       vbus_rp[port] == TYPEC_RP_3A0 ? 1 : 0);
 		gpio_set_level(gpio_5v_en, vbus_en[port]);
 	} else {
 		/*
@@ -60,8 +60,8 @@ static void board_vbus_update_source_current(int port)
 		 * 1505 mA.
 		 */
 		int flags = (vbus_rp[port] == TYPEC_RP_1A5 && vbus_en[port]) ?
-			(GPIO_INPUT | GPIO_PULL_UP) :
-			(GPIO_OUTPUT | GPIO_PULL_UP);
+				    (GPIO_INPUT | GPIO_PULL_UP) :
+				    (GPIO_OUTPUT | GPIO_PULL_UP);
 		gpio_set_level(gpio_5v_en, vbus_en[port]);
 		gpio_set_flags(gpio_5v_en, flags);
 	}
@@ -78,8 +78,7 @@ __override void typec_set_source_current_limit(int port, enum tcpc_rp_value rp)
 int pd_set_power_supply_ready(int port)
 {
 	/* Disable charging */
-	gpio_set_level(port ? GPIO_USB_C1_CHARGE_L :
-			      GPIO_USB_C0_CHARGE_L, 1);
+	gpio_set_level(port ? GPIO_USB_C1_CHARGE_L : GPIO_USB_C0_CHARGE_L, 1);
 
 	/* Ensure we advertise the proper available current quota */
 	charge_manager_source_port(port, 1);
@@ -124,22 +123,19 @@ int pd_snk_is_vbus_provided(int port)
 				      GPIO_USB_C0_VBUS_WAKE_L);
 }
 
-
 int pd_check_vconn_swap(int port)
 {
 	/* in G3, do not allow vconn swap since pp5000_A rail is off */
 	return gpio_get_level(GPIO_PMIC_SLP_SUS_L);
 }
 
-__override void pd_execute_data_swap(int port,
-				     enum pd_data_role data_role)
+__override void pd_execute_data_swap(int port, enum pd_data_role data_role)
 {
 	/* Only port 0 supports device mode. */
 	if (port != 0)
 		return;
 
-	gpio_set_level(GPIO_USB2_OTG_ID,
-		      (data_role == PD_ROLE_UFP) ? 1 : 0);
+	gpio_set_level(GPIO_USB2_OTG_ID, (data_role == PD_ROLE_UFP) ? 1 : 0);
 	gpio_set_level(GPIO_USB2_OTG_VBUSSENSE,
-		      (data_role == PD_ROLE_UFP) ? 1 : 0);
+		       (data_role == PD_ROLE_UFP) ? 1 : 0);
 }
