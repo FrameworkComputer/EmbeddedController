4d2e3ee910972044cd1c37c966ed02ecaa281472
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:33:55 -0600

    zephyr/projects/trogdor/lazor/src/switchcap.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: Ib8f4d074e96ea12f3027ff70b42c03ba69adb7b4
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730809
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/projects/trogdor/lazor/src/switchcap.c
+++ zephyr/projects/trogdor/lazor/src/switchcap.c
@@ -16,8 +16,8 @@
 #include "system.h"
 #include "sku.h"
 
-#define CPRINTS(format, args...) cprints(CC_I2C, format, ## args)
-#define CPRINTF(format, args...) cprintf(CC_I2C, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_I2C, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_I2C, format, ##args)
 
 /* LN9310 switchcap */
 const struct ln9310_config_t ln9310_config = {
@@ -34,18 +34,16 @@ static void switchcap_init(void)
 		 * When the chip in power down mode, it outputs high-Z.
 		 * Set pull-down to avoid floating.
 		 */
-		gpio_pin_configure_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_da9313_gpio0),
-			GPIO_INPUT | GPIO_PULL_DOWN);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_da9313_gpio0),
+				      GPIO_INPUT | GPIO_PULL_DOWN);
 
 		/*
 		 * Configure DA9313 enable, push-pull output. Don't set the
 		 * level here; otherwise, it will override its value and
 		 * shutdown the switchcap when sysjump to RW.
 		 */
-		gpio_pin_configure_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
-			GPIO_OUTPUT);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
+				      GPIO_OUTPUT);
 	} else if (board_has_ln9310()) {
 		CPRINTS("Use switchcap: LN9310");
 
@@ -71,9 +69,8 @@ static void switchcap_init(void)
 		 * (6) GPIO init according to gpio.inc -> push-pull LOW
 		 * (7) This function configures it -> open-drain LOW
 		 */
-		gpio_pin_configure_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
-			GPIO_OUTPUT | GPIO_OPEN_DRAIN);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
+				      GPIO_OUTPUT | GPIO_OPEN_DRAIN);
 
 		/* Only configure the switchcap if not sysjump */
 		if (!system_jumped_late()) {
@@ -83,8 +80,7 @@ static void switchcap_init(void)
 			 * configured from standby mode to switching mode.
 			 */
 			gpio_pin_set_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
-				0);
+				GPIO_DT_FROM_NODELABEL(gpio_switchcap_on), 0);
 			ln9310_init();
 		}
 	} else if (board_has_buck_ic()) {
@@ -98,18 +94,14 @@ DECLARE_HOOK(HOOK_INIT, switchcap_init, HOOK_PRIO_DEFAULT);
 void board_set_switchcap_power(int enable)
 {
 	if (board_has_da9313()) {
-		gpio_pin_set_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
-			enable);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
+				enable);
 	} else if (board_has_ln9310()) {
-		gpio_pin_set_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
-			enable);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_switchcap_on),
+				enable);
 		ln9310_software_enable(enable);
 	} else if (board_has_buck_ic()) {
-		gpio_pin_set_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_vbob_en),
-			enable);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vbob_en), enable);
 	}
 }
 
@@ -117,18 +109,17 @@ int board_is_switchcap_enabled(void)
 {
 	if (board_has_da9313() || board_has_ln9310())
 		return gpio_pin_get_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_switchcap_on));
+			GPIO_DT_FROM_NODELABEL(gpio_switchcap_on));
 
 	/* Board has buck ic*/
-	return gpio_pin_get_dt(
-			GPIO_DT_FROM_NODELABEL(gpio_vbob_en));
+	return gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_vbob_en));
 }
 
 int board_is_switchcap_power_good(void)
 {
 	if (board_has_da9313())
 		return gpio_pin_get_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_da9313_gpio0));
+			GPIO_DT_FROM_NODELABEL(gpio_da9313_gpio0));
 	else if (board_has_ln9310())
 		return ln9310_power_good();
 
