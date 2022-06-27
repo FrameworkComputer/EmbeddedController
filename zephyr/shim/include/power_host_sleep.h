297a16450a9b79c5fdee2c3ba0742f2973c46bc4
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:36:01 -0600

    zephyr/shim/include/power_host_sleep.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I8ae73f8cde5a247f55fe933ba2b8d7f03dbf802e
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730847
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/power_host_sleep.h
+++ zephyr/shim/include/power_host_sleep.h
@@ -24,31 +24,31 @@
 /* power.h */
 enum power_state {
 	/* Steady states */
-	POWER_G3 = 0,	/*
-			 * System is off (not technically all the way into G3,
-			 * which means totally unpowered...)
-			 */
-	POWER_S5,		/* System is soft-off */
-	POWER_S4,		/* System is suspended to disk */
-	POWER_S3,		/* Suspend; RAM on, processor is asleep */
-	POWER_S0,		/* System is on */
+	POWER_G3 = 0, /*
+		       * System is off (not technically all the way into G3,
+		       * which means totally unpowered...)
+		       */
+	POWER_S5, /* System is soft-off */
+	POWER_S4, /* System is suspended to disk */
+	POWER_S3, /* Suspend; RAM on, processor is asleep */
+	POWER_S0, /* System is on */
 #if CONFIG_AP_PWRSEQ_S0IX
 	POWER_S0ix,
 #endif
 	/* Transitions */
-	POWER_G3S5,	/* G3 -> S5 (at system init time) */
-	POWER_S5S3,	/* S5 -> S3 (skips S4 on non-Intel systems) */
-	POWER_S3S0,	/* S3 -> S0 */
-	POWER_S0S3,	/* S0 -> S3 */
-	POWER_S3S5,	/* S3 -> S5 (skips S4 on non-Intel systems) */
-	POWER_S5G3,	/* S5 -> G3 */
-	POWER_S3S4,	/* S3 -> S4 */
-	POWER_S4S3,	/* S4 -> S3 */
-	POWER_S4S5,	/* S4 -> S5 */
-	POWER_S5S4,	/* S5 -> S4 */
+	POWER_G3S5, /* G3 -> S5 (at system init time) */
+	POWER_S5S3, /* S5 -> S3 (skips S4 on non-Intel systems) */
+	POWER_S3S0, /* S3 -> S0 */
+	POWER_S0S3, /* S0 -> S3 */
+	POWER_S3S5, /* S3 -> S5 (skips S4 on non-Intel systems) */
+	POWER_S5G3, /* S5 -> G3 */
+	POWER_S3S4, /* S3 -> S4 */
+	POWER_S4S3, /* S4 -> S3 */
+	POWER_S4S5, /* S4 -> S5 */
+	POWER_S5S4, /* S5 -> S4 */
 #if CONFIG_AP_PWRSEQ_S0IX
-	POWER_S0ixS0,   /* S0ix -> S0 */
-	POWER_S0S0ix,   /* S0 -> S0ix */
+	POWER_S0ixS0, /* S0ix -> S0 */
+	POWER_S0S0ix, /* S0 -> S0ix */
 #endif
 };
 
@@ -56,12 +56,11 @@ enum power_state {
 /* Context to pass to a host sleep command handler. */
 struct host_sleep_event_context {
 	uint32_t sleep_transitions; /* Number of sleep transitions observed */
-	uint16_t sleep_timeout_ms;  /* Timeout in milliseconds */
+	uint16_t sleep_timeout_ms; /* Timeout in milliseconds */
 };
 
 void ap_power_chipset_handle_host_sleep_event(
-		enum host_sleep_event state,
-		struct host_sleep_event_context *ctx);
+	enum host_sleep_event state, struct host_sleep_event_context *ctx);
 void power_set_host_sleep_state(enum host_sleep_event state);
 #endif /* CONFIG_AP_PWRSEQ_HOST_SLEEP */
 
