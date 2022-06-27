091c999194ea502780df83d0dd8a23fe88025745
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:37:00 -0600

    zephyr/shim/include/usbc/utils.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I4ccfd5f2bd19270ee079bdb120b1774994e4a30b
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730855
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/usbc/utils.h
+++ zephyr/shim/include/usbc/utils.h
@@ -5,18 +5,15 @@
 
 #ifndef __CROS_EC_ZEPHYR_SHIM_USBC_UTIL
 
-
 /*
  * Enable interrupt from the `irq` property of an instance's node.
  *
  * @param inst: instance number
  */
-#define BC12_GPIO_ENABLE_INTERRUPT(inst)				\
-	IF_ENABLED(DT_INST_NODE_HAS_PROP(inst, irq),			\
-		   (gpio_enable_dt_interrupt(				\
-			GPIO_INT_FROM_NODE(DT_INST_PHANDLE(inst, irq)));\
-		   ) \
-		  )
+#define BC12_GPIO_ENABLE_INTERRUPT(inst)             \
+	IF_ENABLED(DT_INST_NODE_HAS_PROP(inst, irq), \
+		   (gpio_enable_dt_interrupt(        \
+			    GPIO_INT_FROM_NODE(DT_INST_PHANDLE(inst, irq)));))
 
 /*
  * Get the port number from a child of `named-usbc-port` node.
@@ -31,6 +28,5 @@
  * @param inst: instance number of the node
  */
 #define USBC_PORT_FROM_INST(inst) USBC_PORT(DT_DRV_INST(inst))
-
 
 #endif /* __CROS_EC_ZEPHYR_SHIM_USBC_UTIL */
