ccedb2090f49fd2d244b892df086a2b179dd59b4
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:17:40 -0600

    baseboard/nucleo-h743zi/base-board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I83ab497c8158447e704862893b69cf282ecdb94a
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727934
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/nucleo-h743zi/base-board.h
+++ baseboard/nucleo-h743zi/base-board.h
@@ -53,25 +53,25 @@
  *
  * We need 2 independently erasable blocks, at a minimum.
  */
-#define CONFIG_ROLLBACK_SIZE   (2 * CONFIG_FLASH_BANK_SIZE)
-#define CONFIG_ROLLBACK_OFF    ((CONFIG_FLASH_SIZE_BYTES / 2) - \
-				CONFIG_ROLLBACK_SIZE)
+#define CONFIG_ROLLBACK_SIZE (2 * CONFIG_FLASH_BANK_SIZE)
+#define CONFIG_ROLLBACK_OFF \
+	((CONFIG_FLASH_SIZE_BYTES / 2) - CONFIG_ROLLBACK_SIZE)
 
-#define CONFIG_RO_MEM_OFF      0
-#define CONFIG_RO_SIZE         CONFIG_ROLLBACK_OFF
-#define CONFIG_RW_MEM_OFF      (CONFIG_FLASH_SIZE_BYTES / 2)
-#define CONFIG_RW_SIZE         (CONFIG_FLASH_SIZE_BYTES / 2)
+#define CONFIG_RO_MEM_OFF 0
+#define CONFIG_RO_SIZE CONFIG_ROLLBACK_OFF
+#define CONFIG_RW_MEM_OFF (CONFIG_FLASH_SIZE_BYTES / 2)
+#define CONFIG_RW_SIZE (CONFIG_FLASH_SIZE_BYTES / 2)
 
-#define CONFIG_RO_STORAGE_OFF  0
-#define CONFIG_RW_STORAGE_OFF  0
+#define CONFIG_RO_STORAGE_OFF 0
+#define CONFIG_RW_STORAGE_OFF 0
 
-#define CONFIG_EC_PROTECTED_STORAGE_OFF   CONFIG_RO_MEM_OFF
-#define CONFIG_EC_PROTECTED_STORAGE_SIZE  CONFIG_RO_SIZE
-#define CONFIG_EC_WRITABLE_STORAGE_OFF    CONFIG_RW_MEM_OFF
-#define CONFIG_EC_WRITABLE_STORAGE_SIZE   CONFIG_RW_SIZE
+#define CONFIG_EC_PROTECTED_STORAGE_OFF CONFIG_RO_MEM_OFF
+#define CONFIG_EC_PROTECTED_STORAGE_SIZE CONFIG_RO_SIZE
+#define CONFIG_EC_WRITABLE_STORAGE_OFF CONFIG_RW_MEM_OFF
+#define CONFIG_EC_WRITABLE_STORAGE_SIZE CONFIG_RW_SIZE
 
-#define CONFIG_WP_STORAGE_OFF             CONFIG_EC_PROTECTED_STORAGE_OFF
-#define CONFIG_WP_STORAGE_SIZE            CONFIG_EC_PROTECTED_STORAGE_SIZE
+#define CONFIG_WP_STORAGE_OFF CONFIG_EC_PROTECTED_STORAGE_OFF
+#define CONFIG_WP_STORAGE_SIZE CONFIG_EC_PROTECTED_STORAGE_SIZE
 
 /* Disabled features */
 
@@ -98,25 +98,25 @@
 #define CONFIG_SHA256
 #define CONFIG_SHA256_UNROLLED
 #undef CONFIG_SHAREDLIB_SIZE
-#define CONFIG_SHAREDLIB_SIZE   0
+#define CONFIG_SHAREDLIB_SIZE 0
 #define CONFIG_STM_HWTIMER32
 #define CONFIG_WATCHDOG_HELP
 #define CONFIG_WP_ACTIVE_HIGH
 
 #ifndef TEST_BUILD
-	/* TODO(hesling): Fix the illogical dependency between spi.c
-	 *                and host_command.c
-	 *
-	 * Currently, the chip/stm32/spi.c depends on functions defined in
-	 * common/host_command.c. When unit test builds use their own tasklist
-	 * without the HOSTCMD task, host_command.c is excluded from the build,
-	 * but chip/stm32/spi.c remains (because of CONFIG_SPI).
-	 * This triggers an undefined reference linker error.
-	 * The reproduce case:
-	 * - Allow CONFIG_SPI in TEST_BUILDs
-	 * - make BOARD=nucleo-h743zi tests
-	 */
-#	define CONFIG_SPI
+/* TODO(hesling): Fix the illogical dependency between spi.c
+ *                and host_command.c
+ *
+ * Currently, the chip/stm32/spi.c depends on functions defined in
+ * common/host_command.c. When unit test builds use their own tasklist
+ * without the HOSTCMD task, host_command.c is excluded from the build,
+ * but chip/stm32/spi.c remains (because of CONFIG_SPI).
+ * This triggers an undefined reference linker error.
+ * The reproduce case:
+ * - Allow CONFIG_SPI in TEST_BUILDs
+ * - make BOARD=nucleo-h743zi tests
+ */
+#define CONFIG_SPI
 #endif
 
 /*
@@ -146,10 +146,10 @@
 #define CONFIG_CMD_IDLE_STATS
 
 #ifdef SECTION_IS_RO
-	/* RO verifies the RW partition signature */
-#	define CONFIG_RSA
-#	define CONFIG_RWSIG
-#endif  /* SECTION_IS_RO */
+/* RO verifies the RW partition signature */
+#define CONFIG_RSA
+#define CONFIG_RWSIG
+#endif /* SECTION_IS_RO */
 
 #define CONFIG_RSA_KEY_SIZE 3072
 #define CONFIG_RSA_EXPONENT_3
@@ -160,7 +160,7 @@
  */
 #undef CONFIG_ROLLBACK_SECRET_LOCAL_ENTROPY_SIZE
 #ifdef SECTION_IS_RW
-#	undef CONFIG_ROLLBACK_UPDATE
+#undef CONFIG_ROLLBACK_UPDATE
 #endif
 /*
  * Add rollback protection
@@ -169,11 +169,11 @@
 #define CONFIG_ROLLBACK_MPU_PROTECT
 
 #ifndef __ASSEMBLER__
-	/* Timer selection */
-#	define TIM_CLOCK32 2
-#	define TIM_WATCHDOG 16
-#	include "gpio_signal.h"
-	void button_event(enum gpio_signal signal);
+/* Timer selection */
+#define TIM_CLOCK32 2
+#define TIM_WATCHDOG 16
+#include "gpio_signal.h"
+void button_event(enum gpio_signal signal);
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BASE_BOARD_H */
