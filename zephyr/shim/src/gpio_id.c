a4472e0a6b188fe4ee2f1c52ca979552057e2959
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:38:00 -0600

    zephyr/shim/src/gpio_id.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I3cb1c7b3013f42ae594f0b94621e8967aae765a3
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730893
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/src/gpio_id.c
+++ zephyr/shim/src/gpio_id.c
@@ -11,10 +11,8 @@
 #include "gpio.h"
 #include "util.h"
 
-#define IS_BOARD_COMPATIBLE \
-	DT_NODE_HAS_COMPAT(DT_PATH(board), cros_ec_gpio_id)
-#define IS_SKU_COMPATIBLE \
-	DT_NODE_HAS_COMPAT(DT_PATH(sku), cros_ec_gpio_id)
+#define IS_BOARD_COMPATIBLE DT_NODE_HAS_COMPAT(DT_PATH(board), cros_ec_gpio_id)
+#define IS_SKU_COMPATIBLE DT_NODE_HAS_COMPAT(DT_PATH(sku), cros_ec_gpio_id)
 
 #define CONVERT_NUMERAL_SYSTEM_EVAL(system, bits, nbits) \
 	system##_from_bits(bits, nbits)
@@ -32,11 +30,8 @@ __override uint32_t board_get_sku_id(void)
 	static uint32_t sku_id = (uint32_t)-1;
 
 	if (sku_id == (uint32_t)-1) {
-		int bits[] = {
-			DT_FOREACH_PROP_ELEM(DT_PATH(sku),
-					bits,
-					READ_PIN_FROM_PHANDLE)
-		};
+		int bits[] = { DT_FOREACH_PROP_ELEM(DT_PATH(sku), bits,
+						    READ_PIN_FROM_PHANDLE) };
 
 		if (sizeof(bits) == 0)
 			return (uint32_t)-1;
@@ -58,11 +53,8 @@ __override int board_get_version(void)
 	static int board_version = -1;
 
 	if (board_version == -1) {
-		int bits[] = {
-			DT_FOREACH_PROP_ELEM(DT_PATH(board),
-					bits,
-					READ_PIN_FROM_PHANDLE)
-		};
+		int bits[] = { DT_FOREACH_PROP_ELEM(DT_PATH(board), bits,
+						    READ_PIN_FROM_PHANDLE) };
 
 		if (sizeof(bits) == 0)
 			return -1;
