6a797c3e410f3786c07ab8742eeef2494830f05a
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:36:25 -0600

    zephyr/shim/include/usbc/it5205_usb_mux.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I190fa45dae52e00fa0ed2c404e906888e001bfed
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3730851
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/include/usbc/it5205_usb_mux.h
+++ zephyr/shim/include/usbc/it5205_usb_mux.h
@@ -8,15 +8,15 @@
 
 #include "usb_mux/it5205_public.h"
 
-#define IT5205_USB_MUX_COMPAT	ite_it5205
+#define IT5205_USB_MUX_COMPAT ite_it5205
 
-#define USB_MUX_CONFIG_IT5205(mux_id, port_id, idx)			\
-	{								\
-		USB_MUX_COMMON_FIELDS(mux_id, port_id, idx),		\
-		.driver = &it5205_usb_mux_driver,			\
-		.i2c_port = I2C_PORT(DT_PHANDLE(mux_id, port)),		\
-		.i2c_addr_flags =					\
-			DT_STRING_UPPER_TOKEN(mux_id, i2c_addr_flags),	\
+#define USB_MUX_CONFIG_IT5205(mux_id, port_id, idx)                            \
+	{                                                                      \
+		USB_MUX_COMMON_FIELDS(mux_id, port_id, idx),                   \
+			.driver = &it5205_usb_mux_driver,                      \
+			.i2c_port = I2C_PORT(DT_PHANDLE(mux_id, port)),        \
+			.i2c_addr_flags =                                      \
+				DT_STRING_UPPER_TOKEN(mux_id, i2c_addr_flags), \
 	}
 
 #endif /* __ZEPHYR_SHIM_IT5205_USB_MUX_H */
