ef6a1acbdba6a586a5a38bbf54d1be7a4cc48897
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:38:11 -0600

    board/guybrush/board_fw_config.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I994369af065796b5ff4b7914dc29029c8e5bfdb6
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728438
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/guybrush/board_fw_config.c
+++ board/guybrush/board_fw_config.c
@@ -9,14 +9,15 @@
 bool board_is_convertible(void)
 {
 	return (get_fw_config_field(FW_CONFIG_FORM_FACTOR_OFFSET,
-			FW_CONFIG_FORM_FACTOR_WIDTH)
-			== FW_CONFIG_FORM_FACTOR_CONVERTIBLE);
+				    FW_CONFIG_FORM_FACTOR_WIDTH) ==
+		FW_CONFIG_FORM_FACTOR_CONVERTIBLE);
 }
 
 bool board_has_kblight(void)
 {
 	return (get_fw_config_field(FW_CONFIG_KBLIGHT_OFFSET,
-			FW_CONFIG_KBLIGHT_WIDTH) == FW_CONFIG_KBLIGHT_YES);
+				    FW_CONFIG_KBLIGHT_WIDTH) ==
+		FW_CONFIG_KBLIGHT_YES);
 }
 
 enum board_usb_c1_mux board_get_usb_c1_mux(void)
