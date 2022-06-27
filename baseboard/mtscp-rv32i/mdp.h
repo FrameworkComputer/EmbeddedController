aef5eb50254dc5c88dc5e92d1fdb4393b5ec7ab6
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 13:17:20 -0600

    baseboard/mtscp-rv32i/mdp.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I70e9198c0f6fd65bb829a3448bbc4af56c731167
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3727928
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- baseboard/mtscp-rv32i/mdp.h
+++ baseboard/mtscp-rv32i/mdp.h
@@ -11,10 +11,10 @@ struct mdp_msg_service {
 	unsigned char msg[20];
 };
 BUILD_ASSERT(member_size(struct mdp_msg_service, msg) <=
-		CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
+	     CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
 
 /* Functions provided by private overlay. */
 void mdp_common_init(void);
 void mdp_ipi_task_handler(void *pvParameters);
 
-#endif  /* __CROS_EC_SCP_MDP_H */
+#endif /* __CROS_EC_SCP_MDP_H */
