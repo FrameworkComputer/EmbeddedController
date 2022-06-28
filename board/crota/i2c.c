bbf87229f20e06b14e3ed3b98cfcd5bbc3652d82
Peter Chi <peter_chi@wistron.corp-partner.google.com>
2022-06-28 17:53:12 +0800

    crota: remove set_board_legacy_i2c_speeds
    Because crota have not PS8815, so we remove it.
    
    BUG=b:237357634
    BRANCH=none
    TEST=make -j BOARD=crota
    
    Signed-off-by: Peter Chi <peter_chi@wistron.corp-partner.google.com>
    Change-Id: I652bd83907aea05cef3dc043ae9aa09524356f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3731771
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- board/crota/i2c.c
+++ board/crota/i2c.c
@@ -63,19 +63,3 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
-
-/*
- * I2C controllers are initialized in main.c. This sets the speed much
- * later, but before I2C peripherals are initialized.
- */
-static void set_board_legacy_i2c_speeds(void)
-{
-	if (get_board_id() >= BOARD_ID_FAST_PLUS_CAPABLE)
-		return;
-
-	ccprints("setting USB DB I2C buses to 400 kHz\n");
-
-	i2c_set_freq(I2C_PORT_USB_C0_C1_TCPC, I2C_FREQ_400KHZ);
-	i2c_set_freq(I2C_PORT_USB_C0_C1_PPC, I2C_FREQ_400KHZ);
-}
-DECLARE_HOOK(HOOK_INIT, set_board_legacy_i2c_speeds, HOOK_PRIO_INIT_I2C - 1);
