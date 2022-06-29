ee550514d7d113191d0f3848772fd94d0d2ec0c0
Bobby Casey <bobbycasey@google.com>
2022-06-29 09:21:55 -0400

    chip/stm32/debug_printf.h: Fix header guard
    The include guard on debug_printf.h was __CROS_EC_DEBUG_H rather than
    __CROS_EC_DEBUG_PRINTF_H. Unfortunately, debug.h also used
    __CROS_EC_DEBUG_H which resulted in errors if both were included by a
    single compilation unit.
    
    BRANCH=none
    BUG=b:180144572
    TEST=./util/compare_build.sh -b all -j90
    
    Signed-off-by: Bobby Casey <bobbycasey@google.com>
    Change-Id: I2354181644e1d50bf06a20ec51d432f4d583b50a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3735349
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- chip/stm32/debug_printf.h
+++ chip/stm32/debug_printf.h
@@ -4,8 +4,8 @@
  */
 /* Synchronous UART debug printf */
 
-#ifndef __CROS_EC_DEBUG_H
-#define __CROS_EC_DEBUG_H
+#ifndef __CROS_EC_DEBUG_PRINTF_H
+#define __CROS_EC_DEBUG_PRINTF_H
 
 #ifdef CONFIG_DEBUG_PRINTF
 __attribute__((__format__(__printf__, 1, 2))) void
@@ -14,4 +14,4 @@ debug_printf(const char *format, ...);
 #define debug_printf(...)
 #endif
 
-#endif /* __CROS_EC_DEBUG_H */
+#endif /* __CROS_EC_DEBUG_PRINTF_H */
