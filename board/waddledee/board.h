e68a546a56c0c21c44c058200522fc74c6ed0b6a
Jack Rosenthal <jrosenth@chromium.org>
2022-07-06 11:42:16 -0600

    board/waddledee/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Tricium: disable
    Change-Id: I5e871c061251cf7fc633576ac76c976862f40f90
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3749425
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/waddledee/board.h
+++ board/waddledee/board.h
@@ -30,8 +30,9 @@
 #define CONFIG_CHARGER_OTG
 #undef CONFIG_CHARGER_SINGLE_CHIP
 #define CONFIG_OCPC
-#define CONFIG_OCPC_DEF_RBATT_MOHMS 21 /* R_DS(on) 10.7mOhm + 10mOhm sns rstr \
-					*/
+#define CONFIG_OCPC_DEF_RBATT_MOHMS               \
+	21 /* R_DS(on) 10.7mOhm + 10mOhm sns rstr \
+	    */
 
 /*
  * GPIO for C1 interrupts, for baseboard use
