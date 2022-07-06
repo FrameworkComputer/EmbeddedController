2b8e6c8a813f0e5314da1dd940f06078e9850b05
Jack Rosenthal <jrosenth@chromium.org>
2022-07-06 11:41:02 -0600

    board/kuldax/usbc_config.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Tricium: disable
    Change-Id: I4ed878874550a2c11f6d86fd82730e46e5297d90
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3728604
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/kuldax/usbc_config.h
+++ board/kuldax/usbc_config.h
@@ -8,11 +8,8 @@
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
 
-#define CONFIG_USB_PD_PORT_MAX_COUNT	1
+#define CONFIG_USB_PD_PORT_MAX_COUNT 1
 
-enum usbc_port {
-	USBC_PORT_C0 = 0,
-	USBC_PORT_COUNT
-};
+enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_COUNT };
 
 #endif /* __CROS_EC_USBC_CONFIG_H */
