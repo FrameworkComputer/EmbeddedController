d6190d3bb049ab4ebae9a6a5255117a366a743bf
Jack Rosenthal <jrosenth@chromium.org>
2022-07-06 11:41:50 -0600

    board/sasuke/board.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Tricium: disable
    Change-Id: I65855a13c9043d68164ecacd174813c2ea8f3412
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3749418
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/sasuke/board.h
+++ board/sasuke/board.h
@@ -25,8 +25,9 @@
 #define CONFIG_CHARGER_RAA489000
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC 10
 #define CONFIG_CHARGER_SENSE_RESISTOR 10
-#define CONFIG_OCPC_DEF_RBATT_MOHMS 22 /* R_DS(on) 11.6mOhm + 10mOhm sns rstr \
-					*/
+#define CONFIG_OCPC_DEF_RBATT_MOHMS               \
+	22 /* R_DS(on) 11.6mOhm + 10mOhm sns rstr \
+	    */
 #define CONFIG_OCPC
 #define CONFIG_CHARGER_PROFILE_OVERRIDE
 #define CONFIG_CHARGE_RAMP_HW
