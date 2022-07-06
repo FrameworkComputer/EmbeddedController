fa82a2ed84de0701bf7bb15cb58983ae7a292222
Jack Rosenthal <jrosenth@chromium.org>
2022-07-06 11:41:42 -0600

    board/osiris/pwm.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Tricium: disable
    Change-Id: Icace7ddb3c635b14b8b0c241dfa920b97cbd3a3e
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3749416
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- board/osiris/pwm.c
+++ board/osiris/pwm.c
@@ -11,15 +11,11 @@
 #include "pwm_chip.h"
 
 const struct pwm_t pwm_channels[] = {
-	[PWM_CH_FAN] = {
-		.channel = 5,
-		.flags = PWM_CONFIG_OPEN_DRAIN,
-		.freq = 25000
-	},
-	[PWM_CH_FAN2] = {
-		.channel = 3,
-		.flags = PWM_CONFIG_OPEN_DRAIN,
-		.freq = 25000
-	},
+	[PWM_CH_FAN] = { .channel = 5,
+			 .flags = PWM_CONFIG_OPEN_DRAIN,
+			 .freq = 25000 },
+	[PWM_CH_FAN2] = { .channel = 3,
+			  .flags = PWM_CONFIG_OPEN_DRAIN,
+			  .freq = 25000 },
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
