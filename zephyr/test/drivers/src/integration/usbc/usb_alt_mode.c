e6074b1eedc154f3bf93fbca9675e36dca51b0bc
Jack Rosenthal <jrosenth@chromium.org>
2022-07-06 11:46:02 -0600

    zephyr/test/drivers/src/integration/usbc/usb_alt_mode.c: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Tricium: disable
    Change-Id: I40311932a571aa0ec81f9314e3b6f050f0f1df23
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3749528
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/test/drivers/src/integration/usbc/usb_alt_mode.c
+++ zephyr/test/drivers/src/integration/usbc/usb_alt_mode.c
@@ -243,8 +243,9 @@ ZTEST_F(usbc_alt_mode, verify_displayport_mode_entry)
 	/* TODO(b/237553647): Test EC-driven mode entry (requires a separate
 	 * config).
 	 */
-	host_cmd_typec_control(TEST_PORT,
-	TYPEC_CONTROL_COMMAND_ENTER_MODE, TYPEC_MODE_DP); k_sleep(K_SECONDS(1));
+	host_cmd_typec_control(TEST_PORT, TYPEC_CONTROL_COMMAND_ENTER_MODE,
+			       TYPEC_MODE_DP);
+	k_sleep(K_SECONDS(1));
 
 	/* Verify host command when VDOs are present. */
 	struct ec_params_usb_pd_get_mode_request params = {
@@ -306,7 +307,6 @@ ZTEST_F(usbc_alt_mode, verify_displayport_mode_reentry)
 	zassert_equal(response.svid, USB_SID_DISPLAYPORT, NULL);
 	zassert_equal(response.vdo[0],
 		      fixture->partner.modes_vdm[response.opos], NULL);
-
 }
 
 ZTEST_SUITE(usbc_alt_mode, drivers_predicate_post_main, usbc_alt_mode_setup,
