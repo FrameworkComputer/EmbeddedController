ac352c63e5a8d4329f6d12e2beaf1c40cd6c0f59
Andrew McRae <amcrae@google.com>
2022-07-07 12:59:14 +1000

    zephyr: Remove gpio_map.h legacy include file
    Remove gpio_map.h and update Kconfig references to it.
    
    BUG=b:237716584
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I4b21b53a4a63c6a934f9ab3bef3707e225bd01b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3750267
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/shim/include/board.h
+++ zephyr/shim/include/board.h
@@ -15,11 +15,6 @@
 /* Include shimmed version of power signal */
 #include "power/power.h"
 
-/* Include board specific gpio mapping/aliases if named_pgios node exists */
-#if !defined(TEST_BUILD) && DT_NODE_EXISTS(DT_PATH(named_gpios))
-#include "gpio_map.h"
-#endif
-
 /* Include board specific i2c mapping if I2C is enabled. */
 #if defined(CONFIG_I2C)
 #include "i2c/i2c.h"
