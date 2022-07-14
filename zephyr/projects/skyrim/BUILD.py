fa0c23cc9be08fdb6b7d11d0ca6c19621fb37816
Tim Van Patten <timvp@google.com>
2022-07-14 12:12:10 -0600

    Skyrim: Remove guybrush support
    Skyrim hardware is available now, so remove guybrush as a skyrim
    "variant".
    
    BUG=b:231996904
    TEST=emerge-skyrim chromeos-zephyr
    TEST=zmake build skyrim
    BRANCH=None
    
    Signed-off-by: Tim Van Patten <timvp@google.com>
    Change-Id: I3da3b7a8b67664f3e076a90c570c74302b22d528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3765445
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/projects/skyrim/BUILD.py
+++ zephyr/projects/skyrim/BUILD.py
@@ -33,6 +33,3 @@ def register_variant(project_name):
 
 
 register_variant(project_name="skyrim")
-
-# TODO: Deprecate guybrush build after skyrim hardware is readily available.
-# register_variant(project_name="guybrush")
