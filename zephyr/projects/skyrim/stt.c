b05bf8a1c165139b64aea7279320c9bca3b1b9fa
Diana Z <dzigterman@chromium.org>
2022-07-14 14:01:08 -0600

    Skyrim: Enable STT reporting
    Enable the EC to send temperatures to the AP to support the STT feature.
    
    BRANCH=None
    BUG=b:231994602
    TEST=on skyrim A2, boot up and ensure transactions with the AP succeed
    once the AP has booted enough
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I42b631b55e0ab295501a3cea3e5336008dfef681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3764070
    Reviewed-by: Robert Zieba <robertzieba@google.com>
    Commit-Queue: Robert Zieba <robertzieba@google.com>

---
+++ zephyr/projects/skyrim/stt.c
@@ -0,0 +1,26 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Support code for STT temperature reporting */
+
+#include "chipset.h"
+#include "temp_sensor/pct2075.h"
+#include "temp_sensor/temp_sensor.h"
+
+int board_get_soc_temp_mk(int *temp_mk)
+{
+	if (chipset_in_state(CHIPSET_STATE_HARD_OFF))
+		return EC_ERROR_NOT_POWERED;
+
+	return pct2075_get_val_mk(PCT2075_SOC, temp_mk);
+}
+
+int board_get_ambient_temp_mk(int *temp_mk)
+{
+	if (chipset_in_state(CHIPSET_STATE_HARD_OFF))
+		return EC_ERROR_NOT_POWERED;
+
+	return pct2075_get_val_mk(PCT2075_AMB, temp_mk);
+}
