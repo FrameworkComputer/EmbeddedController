55450dc91a6f3cde3cd425435a62b965077f1bdc
Fabio Baltieri <fabiobaltieri@google.com>
2022-07-15 14:41:17 +0000

    zephyr: motionsense: drop few label properties
    Drop label property from motionsensor bindings, replace it with the node
    name where it was used.
    
    BRANCH=none
    BUG=b:239165779
    TEST=zmake testall
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: Ie28a25feafd4e063e0c9bd90a4a2be1b815e181d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3763914
    Reviewed-by: Aaron Massey <aaronmassey@google.com>

--- zephyr/shim/src/motionsense_sensors.c
+++ zephyr/shim/src/motionsense_sensors.c
@@ -206,12 +206,13 @@ DT_FOREACH_CHILD(SENSOR_ROT_REF_NODE, DECLARE_SENSOR_ROT_REF)
 		   (CREATE_SENSOR_CONFIG(DT_CHILD(id, configs)), ))
 
 /* Get and assign the basic information for a motion sensor */
-#define SENSOR_BASIC_INFO(id)                                                  \
-	.name = DT_LABEL(id), .active_mask = DT_STRING_TOKEN(id, active_mask), \
-	.location = DT_STRING_TOKEN(id, location),                             \
-	.default_range = DT_PROP(id, default_range),                           \
-	SENSOR_I2C_SPI_ADDR_FLAGS(id) SENSOR_MUTEX(id) SENSOR_I2C_PORT(id)     \
-		SENSOR_ROT_STD_REF(id) SENSOR_DRV_DATA(id) SENSOR_CONFIG(id)   \
+#define SENSOR_BASIC_INFO(id)                                                \
+	.name = DT_NODE_FULL_NAME(id),                                       \
+	.active_mask = DT_STRING_TOKEN(id, active_mask),                     \
+	.location = DT_STRING_TOKEN(id, location),                           \
+	.default_range = DT_PROP(id, default_range),                         \
+	SENSOR_I2C_SPI_ADDR_FLAGS(id) SENSOR_MUTEX(id) SENSOR_I2C_PORT(id)   \
+		SENSOR_ROT_STD_REF(id) SENSOR_DRV_DATA(id) SENSOR_CONFIG(id) \
 			SENSOR_INT_SIGNAL(id) SENSOR_FLAGS(id)
 
 /* Create motion sensor node with node ID */
