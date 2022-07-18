2ef2b298e78d9c483ad35dbaf0f2227df2c1931a
Scott Collyer <scollyer@google.com>
2022-07-18 12:29:46 -0700

    rex: Add auto-generated gpio device tree nodes
    This CL adds GPIO device tree file which was autogenerated via
    ec/util/pinmap/ using the input .csv file rex_gpio.csv. The utility
    does not include the pinmap key/value pairs required for alternate
    functions. Those values are added in rex.dts.
    
    This CL also enables CONFIG_ADC and CONFIG_I2C as both I2C and ADC
    nodes are contained in generated.dts.
    
    Note: The input spreadsheet is missing information for enum-names for
    some of the I2C ports and those were manually edited. The input
    spreadsheet will be fixed in a subsequent CL.
    
    BRANCH=none
    BUG=b:240434243
    TEST=zmake build rex
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I84c71acd622a51e28499eec96a5cf166b97175a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3767859
    Tested-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Madhu P <mparuchuri@google.com>
    Commit-Queue: Scott Collyer <scollyer@chromium.org>

--- zephyr/projects/rex/BUILD.py
+++ zephyr/projects/rex/BUILD.py
@@ -5,15 +5,18 @@
 """Rex Projects."""
 
 
-def register_variant(project_name, extra_kconfig_files=()):
+def register_variant(
+    project_name, extra_dts_overlays=(), extra_kconfig_files=()
+):
     """Register a variant of rex."""
     register_npcx_project(
         project_name=project_name,
         zephyr_board="npcx9m3f",
         dts_overlays=[
             # Common to all projects.
-            here
-            / "rex.dts"
+            here / "rex.dts",
+            # Project-specific DTS customization.
+            *extra_dts_overlays,
         ],
         kconfig_files=[
             # Common to all projects.
@@ -26,5 +29,8 @@ def register_variant(project_name, extra_kconfig_files=()):
 
 register_variant(
     project_name="rex",
+    extra_dts_overlays=[
+        here / "generated.dts",
+    ],
     extra_kconfig_files=[here / "prj_rex.conf"],
 )
