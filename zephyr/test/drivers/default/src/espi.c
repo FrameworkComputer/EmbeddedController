b62394fb4444883dd7bb2ff9a162ebff55d3ce8f
Aaron Massey <aaronmassey@google.com>
2022-07-19 15:05:02 -0600

    test: EC_CMD_GPIO_GET version 0 host command
    Test happy-path coverage on the EC_CMD_GPIO_GET version 0 host command.
    
    BRANCH=none
    BUG=b:239608056
    TEST=zmake test test-drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Ia0be87104e70988107c0310d3d1ac69a96d35b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3780764
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/test/drivers/default/src/espi.c
+++ zephyr/test/drivers/default/src/espi.c
@@ -4,11 +4,12 @@
  */
 
 #include <zephyr/zephyr.h>
-#include <ztest.h>
+#include <zephyr/ztest.h>
 
 #include "ec_commands.h"
 #include "host_command.h"
 #include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
 
 #define PORT 0
 
@@ -73,6 +74,34 @@ ZTEST_USER(espi, test_host_command_usb_pd_get_amode)
 	 * See the usb alt mode test for verifying larger struct sizes
 	 */
 	zassert_equal(args.response_size, sizeof(response.svid), NULL);
+}
+
+ZTEST_USER(espi, test_host_command_gpio_get_v0)
+{
+	struct ec_params_gpio_get p = {
+		/* Checking for AC enabled */
+		.name = "acok_od",
+	};
+	struct ec_response_gpio_get response;
+
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND(EC_CMD_GPIO_GET, 0, response, p);
+
+	/* Test true */
+	set_ac_enabled(true);
+
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+	zassert_equal(args.response_size, sizeof(response), NULL);
+	zassert_true(response.val, NULL);
+
+	/* Test false */
+	set_ac_enabled(false);
+
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+	zassert_equal(args.response_size, sizeof(response), NULL);
+	zassert_false(response.val, NULL);
 }
 
 ZTEST_SUITE(espi, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
