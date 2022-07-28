0298bfc009cc659e0f8c2d92b7a121cbcede158e
Robert Zieba <robertzieba@google.com>
2022-07-28 15:19:00 -0600

    drivers/usb_mux: Update magic value used to target all muxes
    This commit changes the value used for `USB_MUX_ALL_CHIPS` from -1 to
    0xff. This value is used as an `uint8_t` and this change makes that more
    explicit.
    
    BRANCH=none
    BUG=b:228223774
    TEST=built chromeos-ec and chromeos-zephyr
    
    Change-Id: Icfbac4e870743ec2b146c296fe69a1adacc4d3e7
    Signed-off-by: Robert Zieba <robertzieba@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3791655
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Diana Z <dzigterman@chromium.org>

--- driver/usb_mux/usb_mux.c
+++ driver/usb_mux/usb_mux.c
@@ -62,7 +62,7 @@ enum mux_config_type {
 };
 
 /* Set all muxes for this board's port */
-#define USB_MUX_ALL_CHIPS -1
+#define USB_MUX_ALL_CHIPS 0xff
 
 /* Define a USB mux task ID for the purpose of linking */
 #ifndef HAS_TASK_USB_MUX
