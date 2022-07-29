411f4ced63c2375ecb4482fe0237a571209d46c1
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2022-07-29 16:31:32 +0800

    joxer: Configure top row keys
    T1:TK_BACK, T2:TK_REFRESH, T3:TK_FULLSCREEN, T4:TK_OVERVIEW,
    T5:TK_SNAPSHOT, T6:TK_BRIGHTNESS_DOWN, T7:TK_BRIGHTNESS_UP,
    T8:TK_KBD_BKLIGHT_TOGGLE, T9:TK_PLAY_PAUSE, T10:TK_MICMUTE,
    T11:TK_VOL_MUTE, T12:TK_VOL_DOWN, T13:TK_VOL_UP
    
    BUG=b:240635192
    BRANCH=none
    TEST=zmake build joxer
    
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: I0f80630883ef34c7e5f4587188f2613252372ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3792709
    Reviewed-by: Andrew McRae <amcrae@google.com>

--- zephyr/projects/nissa/joxer/src/keyboard.c
+++ zephyr/projects/nissa/joxer/src/keyboard.c
@@ -6,18 +6,21 @@
 #include "ec_commands.h"
 
 static const struct ec_response_keybd_config joxer_kb_legacy = {
-	.num_top_row_keys = 10,
+	.num_top_row_keys = 13,
 	.action_keys = {
 		TK_BACK,		/* T1 */
-		TK_FORWARD,		/* T2 */
-		TK_REFRESH,		/* T3 */
-		TK_FULLSCREEN,		/* T4 */
-		TK_OVERVIEW,		/* T5 */
+		TK_REFRESH,		/* T2 */
+		TK_FULLSCREEN,		/* T3 */
+		TK_OVERVIEW,		/* T4 */
+		TK_SNAPSHOT,		/* T5 */
 		TK_BRIGHTNESS_DOWN,	/* T6 */
 		TK_BRIGHTNESS_UP,	/* T7 */
-		TK_VOL_MUTE,		/* T8 */
-		TK_VOL_DOWN,		/* T9 */
-		TK_VOL_UP,		/* T10 */
+		TK_KBD_BKLIGHT_TOGGLE,	/* T8 */
+		TK_PLAY_PAUSE,		/* T9 */
+		TK_MICMUTE,		/* T10 */
+		TK_VOL_MUTE,		/* T11 */
+		TK_VOL_DOWN,		/* T12 */
+		TK_VOL_UP,		/* T13 */
 	},
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
 };
