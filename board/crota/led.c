773dc7f6b04873874befec1c54f7fe95ce4eddb7
Peter Chi <peter_chi@wistron.corp-partner.google.com>
2022-08-10 14:11:29 +0800

    crota: modify charge led behavior
    - battery RSOC >= 96%, turn off LED
    - battery error, blink led
    
    According to CL:3815864, we need add function when battery is
    pre-charging, the LED will blinking, otherwise it will wait
    30 seconds then blinking.
    
    BUG=b:242815300
    BRANCH=none
    TEST=make -j BOARD=crota
    
    Signed-off-by: Peter Chi <peter_chi@wistron.corp-partner.google.com>
    Change-Id: I6f2930b5ad94fb88480b4f1a369a0c5f33166673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3820867
    Commit-Queue: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>

--- board/crota/led.c
+++ board/crota/led.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "charge_manager.h"
+#include "charge_state.h"
 #include "common.h"
 #include "compile_time_macros.h"
 #include "ec_commands.h"
@@ -21,7 +22,7 @@
 #define BAT_LED_OFF_LVL 0
 
 __override const int led_charge_lvl_1 = 5;
-__override const int led_charge_lvl_2 = 100;
+__override const int led_charge_lvl_2 = 96;
 
 __override struct led_descriptor
 	led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
@@ -35,7 +36,9 @@ __override struct led_descriptor
 						   LED_INDEFINITE } },
 		[STATE_DISCHARGE_S3] = { { LED_OFF, LED_INDEFINITE } },
 		[STATE_DISCHARGE_S5] = { { LED_OFF, LED_INDEFINITE } },
-		[STATE_BATTERY_ERROR] = { { LED_OFF, LED_INDEFINITE } },
+		[STATE_BATTERY_ERROR] = { { EC_LED_COLOR_AMBER,
+					    1 * LED_ONE_SEC },
+					  { LED_OFF, 1 * LED_ONE_SEC } },
 		[STATE_FACTORY_TEST] = { { EC_LED_COLOR_WHITE,
 					   1 * LED_ONE_SEC },
 					 { LED_OFF, 1 * LED_ONE_SEC } },
@@ -88,4 +91,16 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 		led_set_color_battery(LED_OFF);
 
 	return EC_SUCCESS;
+}
+
+__override enum led_states board_led_get_state(enum led_states desired_state)
+{
+	/* Make sure when battery is pre-charging, the LED will blinking.
+	 * Otherwise it will wait 30 seconds then blinking.
+	 */
+	if (charge_get_state() == PWR_STATE_IDLE) {
+		if (charge_get_flags() & CHARGE_FLAG_EXTERNAL_POWER)
+			desired_state = STATE_BATTERY_ERROR;
+	}
+	return desired_state;
 }
