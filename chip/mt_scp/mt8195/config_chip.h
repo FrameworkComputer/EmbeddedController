a81bb9fd9e2f5fbb0e91f6eaf4bf80c5c79f7f3a
Tinghan Shen <tinghan.shen@mediatek.com>
2022-08-12 13:23:44 +0800

    chip/mt_scp: enable CONFIG_CHIP_MEMORY_REGIONS for mt8195
    Add non-cacheable dram region, panic data region, and kernel region for
    mt8195. The non-cacheable dram region allows read/write, the others are
    only reserved and should not load/clear by SCP firmware.
    
    BRANCH=none
    BUG=b:193814857
    TEST=make BOARD=cherry_scp
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I2c9d0099892502c9b6610d9ec35e371b7d15d968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3592418
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>

--- chip/mt_scp/mt8195/config_chip.h
+++ chip/mt_scp/mt8195/config_chip.h
@@ -6,6 +6,8 @@
 #ifndef __CROS_EC_CONFIG_CHIP_MT8195_H
 #define __CROS_EC_CONFIG_CHIP_MT8195_H
 
+#define CONFIG_CHIP_MEMORY_REGIONS
+
 #define CONFIG_PANIC_CONSOLE_OUTPUT
 
 /* Add some space (0x100) before panic for jump data */
