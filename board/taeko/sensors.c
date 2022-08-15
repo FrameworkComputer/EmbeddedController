98641ff3ea8faf65316ca04da245d0c343abc490
amber.chen <amber.chen@lcfc.corp-partner.google.com>
2022-08-15 10:30:40 +0800

    taeko: increase the max of temperature setting to 70 degrees
    increase the max of temperature setting to 70 degrees
    
    BUG=b:215033683
    BRANCH=none
    TEST=make -j BOARD=taeko
    
    Signed-off-by: amber.chen <amber.chen@lcfc.corp-partner.google.com>
    Change-Id: I34d752ebf4b9e88ee0ad12460a9c68b8a4e76f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3829327
    Reviewed-by: Boris Mittelberg <bmbm@google.com>

--- board/taeko/sensors.c
+++ board/taeko/sensors.c
@@ -367,7 +367,7 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(85), \
 		}, \
 		.temp_fan_off = C_TO_K(35), \
-		.temp_fan_max = C_TO_K(60), \
+		.temp_fan_max = C_TO_K(70), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 
@@ -397,7 +397,7 @@ __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(85), \
 		}, \
 		.temp_fan_off = C_TO_K(35), \
-		.temp_fan_max = C_TO_K(60), \
+		.temp_fan_max = C_TO_K(70), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_fan = THERMAL_FAN;
 
