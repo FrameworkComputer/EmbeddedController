1c04790c58e66e301c759531b901956e0f5fe95f
Wai-Hong Tam <waihong@google.com>
2022-08-16 14:54:15 -0700

    zephyr: test: Add sysinfo host command test case
    Add the test case which simply issues the sysinfo host command and get
    the results, e.g. reset_flags, current_image, and flags.
    
    BRANCH=None
    BUG=b:236075252
    TEST=./twister -s zephyr/test/drivers/drivers.default
    
    Change-Id: I2357188503e77bc1b5f040c98686c204366a927d
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3833558
    Reviewed-by: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/test/drivers/default/src/system.c
@@ -0,0 +1,30 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/zephyr.h>
+#include <zephyr/ztest.h>
+
+#include "ec_commands.h"
+#include "host_command.h"
+#include "test/drivers/test_state.h"
+
+ZTEST_USER(system, test_hostcmd_sysinfo)
+{
+	struct ec_response_sysinfo response;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_SYSINFO, 0, response);
+
+	/* Simply issue the command and get the results */
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+	zassert_equal(args.response_size, sizeof(response), NULL);
+	zassert_equal(response.reset_flags, 0, "response.reset_flags = %d",
+		      response.reset_flags);
+	zassert_equal(response.current_image, EC_IMAGE_RO,
+		      "response.current_image = %d", response.current_image);
+	zassert_equal(response.flags, 0, "response.flags = %d", response.flags);
+}
+
+ZTEST_SUITE(system, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
