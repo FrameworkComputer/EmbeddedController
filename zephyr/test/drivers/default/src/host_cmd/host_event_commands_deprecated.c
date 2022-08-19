5540ef68843f9a7b5d1b80a89b30a2966bbd3724
Keith Short <keithshort@chromium.org>
2022-08-19 11:14:32 -0600

    zephyr test: Add test for EC_CMD_HOST_EVENT_SET_SCI_MASK
    Add a test for the success path of EC_CMD_HOST_EVENT_SET_SCI_MASK.
    
    BUG=b:236161287
    BRANCH=none
    TEST=./twister -T zephyr/test/drivers/ -s drivers.default
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I9cbce646c69d16a6f234dcedd58162b948b1a6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3846278
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/default/src/host_cmd/host_event_commands_deprecated.c
+++ zephyr/test/drivers/default/src/host_cmd/host_event_commands_deprecated.c
@@ -197,6 +197,20 @@ host_event_get_sci_mask_helper(struct ec_response_host_event_mask *r)
 		      EC_RES_SUCCESS, ret_val);
 }
 
+static void host_event_set_sci_mask_helper(uint32_t mask)
+{
+	enum ec_status ret_val;
+	struct ec_params_host_event_mask params = { .mask = mask };
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_PARAMS(
+		EC_CMD_HOST_EVENT_SET_SCI_MASK, 0, params);
+
+	ret_val = host_command_process(&args);
+
+	/* EC_CMD_HOST_EVENT_SET_SCI_MASK always returns success */
+	zassert_equal(ret_val, EC_RES_SUCCESS, "Expected %d, returned %d",
+		      EC_RES_SUCCESS, ret_val);
+}
+
 /**
  * @brief TestPurpose: Verify EC_CMD_HOST_EVENT_GET_SCI_MASK host command.
  */
@@ -206,6 +220,36 @@ ZTEST_USER(host_cmd_host_event_commands, test_host_event_get_sci_mask)
 	struct ec_response_host_event_mask result = { 0 };
 
 	host_event_get_sci_mask_helper(&result);
+#else
+	ztest_test_skip();
+#endif
+}
+
+/**
+ * @brief TestPurpose: Verify EC_CMD_HOST_EVENT_SET_SCI_MASK host command.
+ */
+ZTEST_USER(host_cmd_host_event_commands, test_host_event_set_sci_mask)
+{
+#ifdef CONFIG_HOSTCMD_X86
+	struct ec_response_host_event_mask result = { 0 };
+
+	/* Read the current mask */
+	host_event_get_sci_mask_helper(&result);
+
+	/* Default mask is expected to be clear */
+	zassert_false(result.mask, "Default host event SCI mask is not clear");
+
+	host_event_set_sci_mask_helper(HOST_EVENT_TEST_MASK_VAL);
+
+	/* Verify the mask changed */
+	host_event_get_sci_mask_helper(&result);
+
+	zassert_equal(result.mask, HOST_EVENT_TEST_MASK_VAL,
+		      "Expected SCI mask 0x%08x, returned mask 0x%08x",
+		      HOST_EVENT_TEST_MASK_VAL, result.mask);
+
+	/* Clean up the mask */
+	host_event_set_sci_mask_helper(0);
 #else
 	ztest_test_skip();
 #endif
