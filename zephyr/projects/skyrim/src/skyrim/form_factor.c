a8e3d9918b69964b5a4e145d945cf7e37c78ac8f
Leila Lin <leilacy_lin@compal.corp-partner.google.com>
2022-08-19 14:15:05 +0800

    skyrim: Add rotation parameters for lid accel
    Lid accel sensor rotate after board ver4.
    
    BUG=b:242934665
    BRANCH=none
    TEST=Check the lid accel data is correct.
    
    Signed-off-by: Leila Lin <leilacy_lin@compal.corp-partner.google.com>
    Change-Id: I16e5736634a96c9abd0603e4b32170986e0866e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3841532
    Tested-by: LeilaCY Lin <leilacy_lin@compal.corp-partner.google.com.test-google-a.com>
    Commit-Queue: LeilaCY Lin <leilacy_lin@compal.corp-partner.google.com.test-google-a.com>
    Reviewed-by: Diana Z <dzigterman@chromium.org>

---
+++ zephyr/projects/skyrim/src/skyrim/form_factor.c
@@ -0,0 +1,37 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+#include "common.h"
+#include "accelgyro.h"
+#include "cros_board_info.h"
+#include "hooks.h"
+#include "motionsense_sensors.h"
+
+LOG_MODULE_DECLARE(skyrim, CONFIG_SKYRIM_LOG_LEVEL);
+
+/*
+ * Mainboard orientation support.
+ */
+
+#define ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(lid_rot_ref1))
+#define LID_ACCEL SENSOR_ID(DT_NODELABEL(lid_accel))
+
+static void form_factor_init(void)
+{
+	int ret;
+	uint32_t val;
+	/*
+	 * If the board version >=4
+	 * use ver1 rotation matrix.
+	 */
+	ret = cbi_get_board_version(&val);
+	if (ret == EC_SUCCESS && val >= 4) {
+		LOG_INF("Switching to ver1 lid");
+		motion_sensors[LID_ACCEL].rot_standard_ref = &ALT_MAT;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, form_factor_init, HOOK_PRIO_POST_I2C);
