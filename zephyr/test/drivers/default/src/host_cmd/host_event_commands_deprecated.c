97ce93e4b11564b44350f9dd411d8b3c1d1f426f
Keith Short <keithshort@chromium.org>
2022-08-19 11:14:32 -0600

    zephyr test: Add test for EC_CMD_HOST_EVENT_GET_SCI_MASK
    Add a test for the success path of EC_CMD_HOST_EVENT_GET_SCI_MASK.
    
    BUG=b:236161212
    BRANCH=none
    TEST=./twister -T zephyr/test/drivers/ -s drivers.default
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: If77cb618239fae87e9e00f091ba25c64eb4b519b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3846277
    Reviewed-by: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/default/src/host_cmd/host_event_commands_deprecated.c
+++ zephyr/test/drivers/default/src/host_cmd/host_event_commands_deprecated.c
@@ -182,3 +182,31 @@ ZTEST_USER(host_cmd_host_event_commands, test_host_event_get_b)
 	ztest_test_skip();
 #endif
 }
+
+static void
+host_event_get_sci_mask_helper(struct ec_response_host_event_mask *r)
+{
+	enum ec_status ret_val;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_RESPONSE(
+		EC_CMD_HOST_EVENT_GET_SCI_MASK, 0, *r);
+
+	ret_val = host_command_process(&args);
+
+	/* EC_CMD_HOST_EVENT_GET_SCI_MASK always returns success */
+	zassert_equal(ret_val, EC_RES_SUCCESS, "Expected %d, returned %d",
+		      EC_RES_SUCCESS, ret_val);
+}
+
+/**
+ * @brief TestPurpose: Verify EC_CMD_HOST_EVENT_GET_SCI_MASK host command.
+ */
+ZTEST_USER(host_cmd_host_event_commands, test_host_event_get_sci_mask)
+{
+#ifdef CONFIG_HOSTCMD_X86
+	struct ec_response_host_event_mask result = { 0 };
+
+	host_event_get_sci_mask_helper(&result);
+#else
+	ztest_test_skip();
+#endif
+}
