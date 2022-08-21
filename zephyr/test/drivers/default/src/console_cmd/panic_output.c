995f23ef8ad8c0d3d2bb6815d47226e5dacea31b
Adam Mills <adamjmills@google.com>
2022-08-22 00:49:49 +1000

    zephyr test: Add panicinfo command test
    Added more tests for `common/panic_output.c` console command `panicinfo`
    that test for when a panic has actually occurred.
    
    BRANCH=None
    BUG=b:236074674
    TEST=./twister -T zephyr/test/drivers
    
    Signed-off-by: Adam Mills <adamjmills@google.com>
    Change-Id: Ia1679818292df7775172a9932fedf2417a68565d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3844093
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/default/src/console_cmd/panic_output.c
+++ zephyr/test/drivers/default/src/console_cmd/panic_output.c
@@ -8,6 +8,7 @@
 
 #include "console.h"
 #include "ec_commands.h"
+#include "panic.h"
 #include "test/drivers/test_state.h"
 #include "test/drivers/utils.h"
 
@@ -18,4 +19,53 @@ ZTEST_USER(console_cmd_panic_output, test_panicinfo)
 		   "Failed default print");
 }
 
-ZTEST_SUITE(console_cmd_panic_output, NULL, NULL, NULL, NULL, NULL);
+/* Test panicinfo when a panic hasn't occurred with an extra arg. */
+/* Should return successfully. */
+ZTEST_USER(console_cmd_panic_output, test_panicinfo_bad_arg)
+{
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "panicinfo fish"),
+		   "Failed default print with a bad argument");
+}
+
+/* Fixture needed to save panic data state */
+struct console_cmd_panic_output_fixture {
+	struct panic_data *p_data;
+	struct panic_data cpy_data;
+};
+
+static void *console_cmd_panic_setup(void)
+{
+	static struct console_cmd_panic_output_fixture fixture;
+
+	return &fixture;
+}
+
+static void console_cmd_panic_before(void *data)
+{
+	struct console_cmd_panic_output_fixture *fixture = data;
+
+	fixture->p_data = get_panic_data_write();
+	fixture->cpy_data = *fixture->p_data;
+}
+
+static void console_cmd_panic_after(void *data)
+{
+	struct console_cmd_panic_output_fixture *fixture = data;
+
+	struct panic_data *p_data = fixture->p_data;
+
+	*p_data = fixture->cpy_data;
+}
+
+/* Tests that need the fixture */
+ZTEST_USER_F(console_cmd_panic_output, test_panicinfo_with_panic)
+{
+	fixture->p_data->flags = 0x1;
+	fixture->p_data->struct_size = CONFIG_PANIC_DATA_SIZE;
+	fixture->p_data->magic = PANIC_DATA_MAGIC;
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "panicinfo"),
+		   "Failed to print details about panic.");
+}
+
+ZTEST_SUITE(console_cmd_panic_output, NULL, console_cmd_panic_setup,
+	    console_cmd_panic_before, console_cmd_panic_after, NULL);
