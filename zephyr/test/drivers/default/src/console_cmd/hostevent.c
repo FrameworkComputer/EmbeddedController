88bdad26f24e36477df3fa6bb8255b54fcb60b4e
Keith Short <keithshort@chromium.org>
2022-08-22 11:48:49 -0600

    zephyr test: hostevent console with subcommands
    Test the "hostevent" command with sub-commands.
    
    BUG=b:236074810
    BRANCH=none
    TEST=./twister -T zephyr/test/drivers
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: Iab70d6a9a1ddfe491dcb6ec5215311b3cb730961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3871742
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>

--- zephyr/test/drivers/default/src/console_cmd/hostevent.c
+++ zephyr/test/drivers/default/src/console_cmd/hostevent.c
@@ -82,6 +82,112 @@ ZTEST_USER(console_cmd_hostevent, test_hostevent_invalid)
 		      EC_ERROR_PARAM2, rv);
 }
 
+/* hostevent with sub-commands and verification */
+ZTEST_USER(console_cmd_hostevent, test_hostevent_sub_commands)
+{
+	int rv;
+	enum ec_status ret_val;
+	host_event_t event_mask;
+	host_event_t all_events = 0;
+	host_event_t set_events;
+	struct ec_response_host_event result = { 0 };
+	struct {
+		enum lpc_host_event_type type;
+		const char *name;
+		host_event_t mask;
+	} subcommand[] = {
+		{
+			.type = LPC_HOST_EVENT_SMI,
+			.name = "SMI",
+			.mask = EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_CLOSED),
+		},
+		{
+			.type = LPC_HOST_EVENT_SCI,
+			.name = "SCI",
+			.mask = EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN),
+		},
+		{
+			.type = LPC_HOST_EVENT_WAKE,
+			.name = "WAKE",
+			.mask = EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON),
+		},
+		{
+			.type = LPC_HOST_EVENT_ALWAYS_REPORT,
+			.name = "ALWAYS_REPORT",
+			.mask = EC_HOST_EVENT_MASK(
+				EC_HOST_EVENT_AC_DISCONNECTED),
+		},
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(subcommand); i++) {
+		event_mask = lpc_get_host_event_mask(subcommand[i].type);
+		zassert_false(event_mask & subcommand[i].mask,
+			      "%s mask is set before test started",
+			      subcommand[i].name);
+		/*
+		 * Setting mask value overwrites existing setting, so OR in
+		 * the test bit.
+		 */
+		event_mask |= subcommand[i].mask;
+		rv = console_cmd_hostevent(subcommand[i].name, event_mask);
+		zassert_ok(rv, "Subcommand %s failed", subcommand[i].name);
+		zassert_true(lpc_get_host_event_mask(subcommand[i].type) &
+				     subcommand[i].mask,
+			     "Failed to set %s event mask", subcommand[i].name);
+
+		/*
+		 * It is only valid to set host events, once at least one mask
+		 * value includes the event.  Setting host events preserves
+		 * existing events.
+		 */
+		zassert_false(host_get_events() & subcommand[i].mask,
+			      "Host event is set before test started");
+		rv = console_cmd_hostevent("set", subcommand[i].mask);
+		zassert_ok(rv, "Subcommand SET failed");
+
+		all_events |= subcommand[i].mask;
+	}
+
+	/* Verify all host events were set, and none were lost */
+	zassert_true((host_get_events() & all_events) == all_events,
+		     "Failed to set host events");
+
+	/* Test clearing of host events */
+	set_events = all_events;
+	for (int i = 0; i < ARRAY_SIZE(subcommand); i++) {
+		set_events &= ~subcommand[i].mask;
+		rv = console_cmd_hostevent("clear", subcommand[i].mask);
+		zassert_ok(rv, "Subcommand CLEAR failed");
+
+		zassert_true((host_get_events() & set_events) == set_events,
+			     "Failed to clear host event");
+	}
+
+	/* Verify the backup host events were set, and none were cleared */
+	ret_val = host_cmd_host_event(EC_HOST_EVENT_GET, EC_HOST_EVENT_B,
+				      &result);
+	zassert_equal(ret_val, EC_RES_SUCCESS, "Expected=%d, returned=%d",
+		      EC_RES_SUCCESS, ret_val);
+	zassert_true((result.value & all_events) == all_events,
+		     "Failed to set host events backup");
+
+	/* Test clearing of backup host events */
+	set_events = all_events;
+	for (int i = 0; i < ARRAY_SIZE(subcommand); i++) {
+		set_events &= ~subcommand[i].mask;
+		rv = console_cmd_hostevent("clearb", subcommand[i].mask);
+		zassert_ok(rv, "Subcommand CLEAR failed");
+
+		ret_val = host_cmd_host_event(EC_HOST_EVENT_GET,
+					      EC_HOST_EVENT_B, &result);
+		zassert_equal(ret_val, EC_RES_SUCCESS,
+			      "Expected=%d, returned=%d", EC_RES_SUCCESS,
+			      ret_val);
+		zassert_true((result.value & set_events) == set_events,
+			     "Failed to clear host events backup");
+	}
+}
+
 ZTEST_SUITE(console_cmd_hostevent, drivers_predicate_post_main,
 	    console_cmd_hostevent_setup, console_cmd_hostevent_before,
 	    console_cmd_hostevent_after, NULL);
