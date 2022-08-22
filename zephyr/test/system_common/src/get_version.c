fd2fd3c38f911e9001a31c3035229b8d9b1e9f81
Fabio Baltieri <fabiobaltieri@google.com>
2022-08-22 16:23:01 +0000

    zephyr: test: add tests for EC_CMD_GET_VERSION host command
    Add two tests for the EC_CMD_GET_VERSION host command, v0 and v1
    versions.
    
    BRANCH=none
    BUG=b:236074978
    TEST=./twister -v -T zephyr/test/system_common
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: I00edc688420def3f4246df89eece47fc6b15cc77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3846270
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Yuval Peress <peress@google.com>

---
+++ zephyr/test/system_common/src/get_version.c
@@ -0,0 +1,72 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/fff.h>
+#include <zephyr/ztest_assert.h>
+#include <zephyr/ztest_test_new.h>
+
+#include "host_command.h"
+#include "system.h"
+
+ZTEST_SUITE(host_cmd_get_version, NULL, NULL, NULL, NULL, NULL);
+
+__override const char *system_get_version(enum ec_image copy)
+{
+	switch (copy) {
+	case EC_IMAGE_RO:
+		return "version-ro";
+	case EC_IMAGE_RW:
+		return "version-rw";
+	default:
+		return "unknown";
+	}
+}
+
+ZTEST(host_cmd_get_version, test_get_version_v1)
+{
+	int ret;
+	struct ec_response_get_version_v1 r;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_GET_VERSION, 1, r);
+
+	ret = host_command_process(&args);
+
+	zassert_equal(ret, EC_SUCCESS, "Unexpected return value: %d", ret);
+
+	zassert_equal(strcmp(r.version_string_ro, "version-ro"), 0,
+		      "version_string_ro: %s", r.version_string_ro);
+	zassert_equal(args.response_size, sizeof(r), "response_size: %d",
+		      args.response_size);
+	zassert_equal(strcmp(r.version_string_rw, "version-rw"), 0,
+		      "version_string_rw: %s", r.version_string_rw);
+	zassert_equal(strcmp(r.cros_fwid_ro, "CROS_FWID_MISSING"), 0,
+		      "cros_fwid_ro: %s", r.cros_fwid_ro);
+	zassert_equal(strcmp(r.cros_fwid_rw, "CROS_FWID_MISSING"), 0,
+		      "cros_fwid_ro: %s", r.cros_fwid_rw);
+	zassert_equal(r.current_image, EC_IMAGE_UNKNOWN, "current_image: %s",
+		      r.current_image);
+}
+
+ZTEST(host_cmd_get_version, test_get_version_v0)
+{
+	int ret;
+	struct ec_response_get_version r;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_GET_VERSION, 0, r);
+
+	ret = host_command_process(&args);
+
+	zassert_equal(ret, EC_SUCCESS, "Unexpected return value: %d", ret);
+
+	zassert_equal(strcmp(r.version_string_ro, "version-ro"), 0,
+		      "version_string_ro: %s", r.version_string_ro);
+	zassert_equal(args.response_size, sizeof(r), "response_size: %d",
+		      args.response_size);
+	zassert_equal(strcmp(r.version_string_rw, "version-rw"), 0,
+		      "version_string_rw: %s", r.version_string_rw);
+	zassert_equal(r.current_image, EC_IMAGE_UNKNOWN, "current_image: %s",
+		      r.current_image);
+}
