6bb19830b681f2b61df4b627b21f18ef40fc3d89
Jeremy Bettis <jbettis@google.com>
2022-08-24 14:18:40 -0600

    zephyr: Remove skips from ec_app test
    With the ztest new api, there is no reason for all these skips.
    
    BRANCH=None
    BUG=None
    TEST=./twister --clobber -v -i -T zephyr/test/ec_app
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: Id98c69002bafb7e0b6d5e1a544cc3d2e1fea6997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3855161
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- zephyr/test/ec_app/src/main.c
+++ zephyr/test/ec_app/src/main.c
@@ -8,68 +8,54 @@
 #include "ec_app_main.h"
 #include "hooks.h"
 
+#ifdef CONFIG_CMD_AP_RESET_LOG
 ZTEST(ec_app_tests, test_init_reset_log)
 {
-#ifdef CONFIG_CMD_AP_RESET_LOG
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#ifdef CONFIG_HOSTCMD_X86
 ZTEST(ec_app_tests, test_lpc_init_mask)
 {
-#ifdef CONFIG_HOSTCMD_X86
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#ifdef HAS_TASK_KEYSCAN
 ZTEST(ec_app_tests, test_keyboard_scan_init)
 {
-#ifdef HAS_TASK_KEYSCAN
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#if defined(CONFIG_DEDICATED_RECOVERY_BUTTON) || defined(CONFIG_VOLUME_BUTTONS)
 ZTEST(ec_app_tests, test_button_init)
 {
-#if defined(CONFIG_DEDICATED_RECOVERY_BUTTON) || defined(CONFIG_VOLUME_BUTTONS)
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#ifdef CONFIG_PLATFORM_EC_HOST_INTERFACE_ESPI
 ZTEST(ec_app_tests, test_setup_espi)
 {
-#ifdef CONFIG_PLATFORM_EC_HOST_INTERFACE_ESPI
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#ifdef CONFIG_PLATFORM_EC_WATCHDOG
 ZTEST(ec_app_tests, test_watchdog_init)
 {
-#ifdef CONFIG_PLATFORM_EC_WATCHDOG
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
+#ifdef CONFIG_PLATFORM_EC_VBOOT_EFS2
 ZTEST(ec_app_tests, test_vboot_main)
 {
-#ifdef CONFIG_PLATFORM_EC_VBOOT_EFS2
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
 #ifdef CONFIG_PLATFORM_EC_HOOKS
 static int sample_init_hook_count;
@@ -94,21 +80,14 @@ ZTEST(ec_app_tests, test_hook_notify_init)
 	zassert_equal(1, sample_init_hook_count,
 		      "Expected sample_init_hook to run once.");
 }
-#else
-ZTEST(ec_app_tests, test_hook_notify_init)
-{
-	ztest_test_skip();
-}
 #endif
 
+#ifdef CONFIG_SHIMMED_TASKS
 ZTEST(ec_app_tests, test_start_ec_tasks)
 {
-#ifdef CONFIG_SHIMMED_TASKS
 	zassert_unreachable("TODO: Implement this test.");
-#else
-	ztest_test_skip();
-#endif
 }
+#endif
 
 /* Does setup for all of the test cases. */
 void *ec_app_setup(void)
