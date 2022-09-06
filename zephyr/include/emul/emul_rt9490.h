6493af0aeb66c95fb3204ae68b46aa62ec85262f
Ting Shen <phoenixshen@google.com>
2022-09-06 18:01:37 +0800

    zephyr/test: add rt9490 charger emulator
    Implemented a rt9490 charger emulator with basic i2c read/write, and
    extra peek/reset reg functions
    
    BUG=b:244233556,b:243841599
    TEST=tested with CL:3873241
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I87b77cf2b0bc0a0d89f2f76353e16b9296dc5596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3873240
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

---
+++ zephyr/include/emul/emul_rt9490.h
@@ -0,0 +1,15 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EMUL_RT9490_H
+#define EMUL_RT9490_H
+
+#include <zephyr/drivers/emul.h>
+
+void rt9490_emul_reset_regs(const struct emul *emul);
+
+int rt9490_emul_peek_reg(const struct emul *emul, int reg);
+
+#endif
