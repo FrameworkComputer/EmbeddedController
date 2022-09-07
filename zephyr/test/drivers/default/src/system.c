3a21889edd456028bb2a82c0f041212b260e5b75
Aaron Massey <aaronmassey@google.com>
2022-09-07 16:49:38 -0600

    test: Verify system_encode_save_flags function
    Verify that the system_encode_save_flags function can save all its flags
    and that it picks the correct reset flag based on system reset reason.
    
    BRANCH=none
    BUG=b:245615051
    TEST=twister -s zephyr/test/drivers/drivers.default
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I0011f811d0a56817df8b4a5695e6bdd4c39ceb1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3880832
    Reviewed-by: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/default/src/system.c
+++ zephyr/test/drivers/default/src/system.c
@@ -8,7 +8,10 @@
 
 #include "ec_commands.h"
 #include "host_command.h"
+#include "system.h"
 #include "test/drivers/test_state.h"
+
+/* System Host Commands */
 
 ZTEST_USER(system, test_hostcmd_sysinfo)
 {
@@ -41,4 +44,70 @@ ZTEST_USER(system, test_hostcmd_board_version)
 		      response.board_version);
 }
 
+/* System Function Testing */
+
+static void system_flags_before_after(void *data)
+{
+	ARG_UNUSED(data);
+	system_clear_reset_flags(-1);
+}
+
+ZTEST(system_save_flags, test_system_encode_save_flags)
+{
+	int flags_to_save = 0;
+	uint32_t saved_flags = 0;
+	int arbitrary_reset_flags = 1;
+
+	/* Save all possible flags */
+	flags_to_save = -1;
+
+	/* Clear all reset flags and set them arbitrarily */
+	system_set_reset_flags(arbitrary_reset_flags);
+
+	system_encode_save_flags(flags_to_save, &saved_flags);
+
+	/* Verify all non-mutually exclusive flags */
+	zassert_equal(1, saved_flags & system_get_reset_flags(), NULL);
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_AP_OFF, NULL);
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_STAY_IN_RO, NULL);
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_AP_WATCHDOG, NULL);
+}
+
+ZTEST(system_save_flags,
+      test_system_encode_save_flags_mutually_exclusive_reset_flags)
+{
+	int flags_to_save = 0;
+	uint32_t saved_flags = 0;
+
+	/* Verify reset hard takes precedence over hibernate/soft */
+	flags_to_save = SYSTEM_RESET_HARD | SYSTEM_RESET_HIBERNATE;
+
+	system_encode_save_flags(flags_to_save, &saved_flags);
+
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_HARD, NULL);
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_HIBERNATE, NULL);
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_SOFT, NULL);
+
+	/* Verify reset hibernate takes precedence over soft */
+	flags_to_save = SYSTEM_RESET_HIBERNATE;
+
+	system_encode_save_flags(flags_to_save, &saved_flags);
+
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_HARD, NULL);
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_HIBERNATE, NULL);
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_SOFT, NULL);
+
+	/* Verify reset soft is always saved given no other flags */
+	flags_to_save = 0;
+
+	system_encode_save_flags(flags_to_save, &saved_flags);
+
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_HARD, NULL);
+	zassert_equal(0, saved_flags & EC_RESET_FLAG_HIBERNATE, NULL);
+	zassert_not_equal(0, saved_flags & EC_RESET_FLAG_SOFT, NULL);
+}
+
 ZTEST_SUITE(system, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
+
+ZTEST_SUITE(system_save_flags, drivers_predicate_post_main, NULL,
+	    system_flags_before_after, system_flags_before_after, NULL);
