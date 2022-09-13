6315e187bdb783569447799018b5cbcdd800e2fe
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-09-13 11:17:59 +0800

    Craask: Fix lid rotation parameters for Craaskbowl
    The placement of the LID sensor is spin 180' on xy plane for Craaskbowl.
    Use fw_config bit8 to describe Lid sensor orientation:
    0: original placement
    1: rotate 180' on xy plane
    
    BUG=none
    BRANCH=none
    TEST=table mode and rotation work fine on Craaskbowl
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: Ifa3d22ca2e1c90dfebb8ad8a7eca676b84c07191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3891565
    Code-Coverage: Andrew McRae <amcrae@google.com>
    Reviewed-by: Andrew McRae <amcrae@google.com>

--- zephyr/projects/nissa/craask/src/form_factor.c
+++ zephyr/projects/nissa/craask/src/form_factor.c
@@ -22,9 +22,13 @@ LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
  * Mainboard orientation support.
  */
 
+#define LIS_ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(lid_rot_bma422))
+#define BMA_ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(lid_rot_ref))
 #define ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(base_rot_ver1))
+#define LID_SENSOR SENSOR_ID(DT_NODELABEL(lid_accel))
 #define BASE_SENSOR SENSOR_ID(DT_NODELABEL(base_accel))
 #define BASE_GYRO SENSOR_ID(DT_NODELABEL(base_gyro))
+#define ALT_LID_S SENSOR_ID(DT_NODELABEL(alt_lid_accel))
 
 static bool use_alt_sensor;
 
@@ -49,6 +53,23 @@ static void form_factor_init(void)
 		LOG_INF("Switching to ver1 base");
 		motion_sensors[BASE_SENSOR].rot_standard_ref = &ALT_MAT;
 		motion_sensors[BASE_GYRO].rot_standard_ref = &ALT_MAT;
+	}
+
+	/*
+	 * If the firmware config indicates
+	 * an craaskbowl form factor, use the alternative
+	 * rotation matrix.
+	 */
+	ret = cros_cbi_get_fw_config(FW_LID_INVERSION, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d",
+			FW_LID_INVERSION);
+		return;
+	}
+	if (val == FW_LID_XY_ROT_180) {
+		LOG_INF("Lid sensor placement rotate 180 on xy plane");
+		motion_sensors[LID_SENSOR].rot_standard_ref = &LIS_ALT_MAT;
+		motion_sensors_alt[ALT_LID_S].rot_standard_ref = &BMA_ALT_MAT;
 	}
 
 	/* check which base sensor is used for motion_interrupt */
