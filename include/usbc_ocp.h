8bd196d17fe6224183defa231c5910fc0bd3e906
Aaron Massey <aaronmassey@google.com>
2022-09-19 18:19:00 -0600

    test: Verify uncovered common OCP routines
    Verify adding/clearing over-current events flag the port latched off
    state appropriately and that usbc_ocp common code routines with bad port
    arguments return appropriately.
    
    BRANCH=none
    BUG=b:247635955
    TEST=twister --clobber -i -s zephyr/test/drivers/drivers.usbc_ocp
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I7519867aebf24eb28965b32b3b78b313427a1af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3908152
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>

--- include/usbc_ocp.h
+++ include/usbc_ocp.h
@@ -6,7 +6,32 @@
 #ifndef __CROS_EC_USBC_OCP_H
 #define __CROS_EC_USBC_OCP_H
 
+#include "common.h"
+
 /* Common APIs for USB Type-C Overcurrent Protection (OCP) Module */
+
+/*
+ * PD 3.1 Ver 1.3 7.1.7.1 Output Over Current Protection
+ *
+ * "After three consecutive over current events Source Shall go to
+ * ErrorRecovery.
+ *
+ * Sources Should attempt to send a Hard Reset message when over
+ * current protection engages followed by an Alert Message indicating
+ * an OCP event once an Explicit Contract has been established.
+ *
+ * The Source Shall prevent continual system or port cycling if over
+ * current protection continues to engage after initially resuming
+ * either default operation or renegotiation. Latching off the port or
+ * system is an acceptable response to recurring over current."
+ *
+ * Our policy will be first two OCPs -> hard reset
+ * 3rd -> ErrorRecovery
+ * 4th -> port latched off
+ */
+#define OCP_HR_CNT 2
+
+#define OCP_MAX_CNT 4
 
 /**
  * Increment the overcurrent event counter.
