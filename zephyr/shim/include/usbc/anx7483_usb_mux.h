60fca7c2143af0f15eb843a6fc57185102b5a7fb
Diana Z <dzigterman@chromium.org>
2022-09-19 18:28:10 -0600

    Zephyr: Remove default anx7483 board set
    Remove the default declaration of board_set, as boards may need their
    own tuning in addition to the default tuning.
    
    BRANCH=None
    BUG=b:244457125
    TEST=zmake build affected boards
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: Iedacdfb92417076841fd3c1c0178653c0a58cc60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3905900
    Reviewed-by: Andrew McRae <amcrae@google.com>

--- zephyr/shim/include/usbc/anx7483_usb_mux.h
+++ zephyr/shim/include/usbc/anx7483_usb_mux.h
@@ -10,13 +10,12 @@
 
 #define ANX7483_USB_MUX_COMPAT analogix_anx7483
 
-#define USB_MUX_CONFIG_ANX7483(mux_id)                            \
-	{                                                         \
-		USB_MUX_COMMON_FIELDS(mux_id),                    \
-			.driver = &anx7483_usb_retimer_driver,    \
-			.board_set = &anx7483_set_default_tuning, \
-			.i2c_port = I2C_PORT_BY_DEV(mux_id),      \
-			.i2c_addr_flags = DT_REG_ADDR(mux_id),    \
+#define USB_MUX_CONFIG_ANX7483(mux_id)                         \
+	{                                                      \
+		USB_MUX_COMMON_FIELDS(mux_id),                 \
+			.driver = &anx7483_usb_retimer_driver, \
+			.i2c_port = I2C_PORT_BY_DEV(mux_id),   \
+			.i2c_addr_flags = DT_REG_ADDR(mux_id), \
 	}
 
 #endif /* __ZEPHYR_SHIM_ANX7483_USB_MUX_H */
