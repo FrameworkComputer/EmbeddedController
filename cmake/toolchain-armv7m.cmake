72d07d9ef2018cbb88757ad3b708a136d36c8a1a
Tom Hughes <tomhughes@chromium.org>
2022-09-29 10:05:53 -0700

    cmake: Add ARMv7-M CMake toolchain file
    BRANCH=none
    BUG=b:248508087, b:254530679
    TEST=Build googletest with the ARMv7-M toolchain:
         cmake -GNinja -Dgtest_disable_pthreads=ON \
           -DCMAKE_TOOLCHAIN_FILE=~/chromiumos/src/platform/ec/cmake/toolchain-armv7m.cmake \
           -DCMAKE_INSTALL_PREFIX=~/chromiumos/src/third_party/tmp ..
         ninja
         ninja install
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: If31e1937f444f44f153fde9c8aadcb1824e12ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3929249
    Reviewed-by: Andrea Grandi <agrandi@google.com>
    Reviewed-by: Bobby Casey <bobbycasey@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ cmake/toolchain-armv7m.cmake
@@ -0,0 +1,38 @@
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(PREFIX armv7m-cros-eabi-)
+set(CMAKE_SYSROOT /usr/armv7m-cros-eabi/)
+
+set(CMAKE_C_COMPILER ${PREFIX}clang)
+set(CMAKE_CXX_COMPILER ${PREFIX}clang++)
+set(CMAKE_LINKER ${PREFIX}ld.lld)
+set(CMAKE_AR ${PREFIX}ar)
+set(CMAKE_NM ${PREFIX}nm)
+set(CMAKE_OBJCOPY ${PREFIX}objcopy)
+set(CMAKE_OBJDUMP ${PREFIX}objdump)
+set(CMAKE_RANLIB ${PREFIX}ranlib)
+set(CMAKE_READELF ${PREFIX}readelf)
+
+add_compile_options(-Oz)
+
+add_compile_options(-flto)
+add_link_options(-flto)
+
+# TODO(b/234507656): Remove this include when clang is fixed.
+add_compile_options(-I/usr/armv7m-cros-eabi/usr/include/c++/v1)
+# TODO(b/254916723): Remove this define when clang is fixed.
+add_compile_options(-D_GNU_SOURCE)
+add_link_options(-lclang_rt.builtins-armv7m)
+add_link_options(-lnosys)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE OFF)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
