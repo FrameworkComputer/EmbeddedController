8e96b22fccbd2515af092b1180ddc6fef2c7ba02
Yuval Peress <peress@google.com>
2022-09-30 13:02:34 -0600

    test: add missing include to avoid warning
    This emulator was using memset without including string.h
    
    BRANCH=none
    BUG=none
    TEST=./twister -T zephyr/test/drivers
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: I1d853ed5514d238265bc426961cba525af4d78f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3930859
    Commit-Queue: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/i2c_controller/src/basic_i2c_device_emul.c
+++ zephyr/test/drivers/i2c_controller/src/basic_i2c_device_emul.c
@@ -2,6 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#include <string.h>
 #include <zephyr/device.h>
 
 #include "basic_i2c_device_emul.h"
