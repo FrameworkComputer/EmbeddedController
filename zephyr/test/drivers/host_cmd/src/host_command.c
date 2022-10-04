ee5af48cec4ef0be294d9b18a463dc563f52f099
Tristan Honscheid <honscheid@google.com>
2022-10-04 14:38:15 -0600

    zephyr: tests: Test host_command.c Host Commands
    Test several remaining HCs in `common/host_command.c`. These are mainly
    for testing the protocol and EC.
    
    BRANCH=None
    BUG=b:236074823, b:236074905, b:236074901, b:236074900
    TEST=./twister -T zephyr/test/drivers
    
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Change-Id: I1a5034682ce56903e2b8778cf967e66b2caad561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3934413
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/test/drivers/host_cmd/src/host_command.c
@@ -0,0 +1,97 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include "ec_commands.h"
+#include "host_command.h"
+#include "test/drivers/test_state.h"
+
+ZTEST(host_cmd_host_commands, get_command_versions__v1)
+{
+	struct ec_response_get_cmd_versions response;
+	struct ec_params_get_cmd_versions_v1 params = {
+		.cmd = EC_CMD_GET_CMD_VERSIONS
+	};
+	int rv;
+
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND(
+		EC_CMD_GET_CMD_VERSIONS, 1, response, params);
+
+	rv = host_command_process(&args);
+
+	zassert_ok(rv, "Got %d", rv);
+	zassert_equal(EC_VER_MASK(0) | EC_VER_MASK(1), response.version_mask);
+}
+
+ZTEST(host_cmd_host_commands, get_command_versions__invalid_cmd)
+{
+	struct ec_response_get_cmd_versions response;
+	struct ec_params_get_cmd_versions_v1 params = {
+		/* Host command doesn't exist */
+		.cmd = UINT16_MAX,
+	};
+	int rv;
+
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND(
+		EC_CMD_GET_CMD_VERSIONS, 1, response, params);
+
+	rv = host_command_process(&args);
+
+	zassert_equal(EC_RES_INVALID_PARAM, rv, "Got %d", rv);
+}
+
+ZTEST(host_cmd_host_commands, get_comms_status)
+{
+	struct ec_response_get_comms_status response;
+	int rv;
+
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_RESPONSE(
+		EC_CMD_GET_COMMS_STATUS, 0, response);
+
+	rv = host_command_process(&args);
+
+	zassert_ok(rv, "Got %d", rv);
+
+	/* Unit test host commands are processed synchronously, so always expect
+	 * the EC to be not busy processing another.
+	 */
+	zassert_false(response.flags);
+}
+
+ZTEST(host_cmd_host_commands, resend_response)
+{
+	struct host_cmd_handler_args args =
+		(struct host_cmd_handler_args)BUILD_HOST_COMMAND_SIMPLE(
+			EC_CMD_RESEND_RESPONSE, 0);
+	int rv;
+
+	rv = host_command_process(&args);
+	zassert_ok(rv);
+
+	/* The way we trigger host commands in tests doesn't cause results to
+	 * get saved (it happens outside of host_command_process), so we cannot
+	 * verify the resent response itself.
+	 *
+	 * TODO: test at least one host command through the ESPI interface.
+	 */
+}
+
+ZTEST(host_cmd_host_commands, get_proto_version)
+{
+	struct ec_response_proto_version response;
+	int rv;
+
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_PROTO_VERSION, 0, response);
+
+	rv = host_command_process(&args);
+
+	zassert_ok(rv, "Got %d", rv);
+	zassert_equal(EC_PROTO_VERSION, response.version);
+}
+
+ZTEST_SUITE(host_cmd_host_commands, drivers_predicate_post_main, NULL, NULL,
+	    NULL, NULL);
