9b112272813c9e87440cece62e31ef9bab984b2f
Al Semjonovs <asemjonovs@google.com>
2022-10-11 15:22:50 -0600

    zephyr: Test ec_feature 'feat' console command
    Validate 'feat' console command output
    
    BUG=None
    BRANCH=NONE
    TEST=./twister -T zephyr/test
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: If8c5c102e3e1da6b8f0ce284a674e61ea88fe16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3947528
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Queue: Simon Glass <sjg@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/test/drivers/default/src/console_cmd/ec_features.c
@@ -0,0 +1,25 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/shell/shell.h>
+#include <zephyr/ztest.h>
+
+#include "console.h"
+#include "host_command.h"
+#include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
+
+ZTEST_SUITE(console_cmd_ec_features, drivers_predicate_post_main, NULL, NULL,
+	    NULL, NULL);
+
+ZTEST_USER(console_cmd_ec_features, test_feat)
+{
+	char expected[32];
+
+	snprintf(expected, sizeof(expected), " 0-31: 0x%08x\r\n32-63: 0x%08x",
+		 get_feature_flags0(), get_feature_flags1());
+
+	CHECK_CONSOLE_CMD("feat", expected, EC_SUCCESS);
+}
