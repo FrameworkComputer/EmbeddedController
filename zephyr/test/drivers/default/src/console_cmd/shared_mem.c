065186ef2ce243f12c729ff55c244b666aa44532
Al Semjonovs <asemjonovs@google.com>
2022-10-12 09:08:44 -0600

    zephyr: Test shmem console command
    Validate shmem console command
    
    BUG=None
    BRANCH=NONE
    TEST=./twister -T zephyr/test
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: Ic17cd5bd2448a45795b6525502565a7fceb06b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3949472
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>

---
+++ zephyr/test/drivers/default/src/console_cmd/shared_mem.c
@@ -0,0 +1,24 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/shell/shell.h>
+#include <zephyr/ztest.h>
+
+#include "console.h"
+#include "shared_mem.h"
+#include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
+
+ZTEST_SUITE(console_cmd_shared_mem, drivers_predicate_post_main, NULL, NULL,
+	    NULL, NULL);
+
+ZTEST_USER(console_cmd_shared_mem, test_shmem)
+{
+	char expected[32];
+
+	snprintf(expected, sizeof(expected), "Size:%6d", shared_mem_size());
+
+	CHECK_CONSOLE_CMD("shmem", expected, EC_SUCCESS);
+}
