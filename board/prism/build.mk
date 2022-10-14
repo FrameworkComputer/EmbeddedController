41aad246b8f96603440de30dd5b084428b6c7828
Daisuke Nojiri <dnojiri@chromium.org>
2022-10-14 14:30:33 -0700

    Prism: Correct comments in bulid.mk
    This patch corrects comments in build.mk. There is no functionality
    change.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Ia1919e0d7eb0e38860759f8792f5e7fa02c0dabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3957857
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- board/prism/build.mk
+++ board/prism/build.mk
@@ -1,16 +1,13 @@
 # -*- makefile -*-
-# Copyright 2016 The ChromiumOS Authors
+# Copyright 2022 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# Board specific files build
 
-# the IC is STmicro STM32F072RBT6
 CHIP:=stm32
 CHIP_FAMILY:=stm32f0
 CHIP_VARIANT:=stm32f07x
 
-# Build tests that we care about for hammer
+# Build tests that we care about for Prism.
 test-list-y=entropy rsa3 sha256 sha256_unrolled x25519
 
 board-y=board.o
