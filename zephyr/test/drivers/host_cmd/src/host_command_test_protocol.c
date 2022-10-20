97be2786bf6cfed2847d950a9c1d622e6606019b
Aaron Massey <aaronmassey@google.com>
2022-10-20 11:27:52 -0600

    test: EC_CMD_TEST_PROTOCOL
    Add tests that verifies the EC_CMD_TEST_PROTOCOL host command simply
    echoes back the host's commands parameter information in the form of a
    response struct.
    
    Scenarios:
    
    * Successful echo of max data from response.
    * Unsuccessful echo of 0 data from response.
    
    BRANCH=none
    BUG=b:236074825
    TEST=twister -i -s zephyr/test/drivers/drivers.host_cmd --coverage'
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Ia4c04f95fb5d471005195cb8b0be69b2c5ecd25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3967700
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>

---
+++ zephyr/test/drivers/host_cmd/src/host_command_test_protocol.c
@@ -0,0 +1,63 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include "host_command.h"
+#include "test/drivers/test_state.h"
+
+ZTEST_USER(hc_test_protocol, test_echo_max_buffer_size)
+{
+	struct ec_params_test_protocol params = {
+		/* Actual return length will only be sizeof(response) */
+		.ret_len = UINT32_MAX,
+		.buf = { 0 },
+		.ec_result = EC_SUCCESS,
+	};
+	struct ec_response_test_protocol response;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND(EC_CMD_TEST_PROTOCOL, 0, response, params);
+
+	/* Set first and last bytes of input array */
+	params.buf[0] = 0x1;
+	params.buf[ARRAY_SIZE(params.buf) - 1] = 0x2;
+
+	zassert_ok(host_command_process(&args));
+	zassert_equal(args.response_size, sizeof(response));
+
+	/* Check contents are echoed back in response */
+	zassert_mem_equal(params.buf, response.buf, sizeof(params.buf));
+}
+
+ZTEST_USER(hc_test_protocol, test_echo_min_buffer_size_failing_command)
+{
+	struct ec_params_test_protocol params = {
+		/* Don't want to have anything echoed back to us */
+		.ret_len = 0,
+		.buf = { 0 },
+		.ec_result = EC_ERROR_TRY_AGAIN,
+	};
+	struct ec_response_test_protocol response;
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND(EC_CMD_TEST_PROTOCOL, 0, response, params);
+
+	/*
+	 * Set first and last bytes of input array, neither will be written to
+	 * response
+	 */
+	params.buf[0] = 0x1;
+	params.buf[ARRAY_SIZE(params.buf) - 1] = 0x2;
+
+	zassert_equal(host_command_process(&args), params.ec_result);
+	zassert_equal(args.response_size, params.ret_len);
+
+	/* Check contents were never echoed back as intended */
+	zassert_not_equal(response.buf[0], params.buf[0]);
+	zassert_not_equal(response.buf[ARRAY_SIZE(response.buf) - 1],
+			  params.buf[ARRAY_SIZE(params.buf) - 1]);
+}
+
+ZTEST_SUITE(hc_test_protocol, drivers_predicate_post_main, NULL, NULL, NULL,
+	    NULL);
