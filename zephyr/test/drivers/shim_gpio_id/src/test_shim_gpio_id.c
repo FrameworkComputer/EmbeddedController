8997564e125d21e5554e2380bda1d503b6b54ee9
Aaron Massey <aaronmassey@google.com>
2022-10-21 14:56:29 -0600

    test: shim/gpio_id.c default values
    Verify that the board_get_version() and board_get_sku_id() functions
    return the default values obtained from the sku/version GPIOs having a
    Hi-Z default value.
    
    BRANCH=none
    BUG=b:254903833
    TEST=twister -i -s zephyr/test/drivers/drivers.shim_gpio_id
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I2e60c3066bf6c0142dac76f8a2f7c2bc08fd513c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3972361
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/test/drivers/shim_gpio_id/src/test_shim_gpio_id.c
@@ -0,0 +1,39 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include "system.h"
+#include "test/drivers/test_state.h"
+#include "util.h"
+
+/*
+ * TODO(b/254924012): Test alternative GPIO when emulated gpio tristate is
+ * supported upstream.
+ */
+
+ZTEST(gpio_id, test_board_get_sku_id__default_value)
+{
+	int bits[] = {
+		/* Default Hi-Z value */
+		2,
+	};
+
+	zassert_equal(board_get_sku_id(),
+		      ternary_from_bits(bits, ARRAY_SIZE(bits)));
+}
+
+ZTEST(gpio_id, test_board_get_version__default_value)
+{
+	int bits[] = {
+		/* Default Hi-Z value */
+		2,
+	};
+
+	zassert_equal(board_get_version(),
+		      ternary_from_bits(bits, ARRAY_SIZE(bits)));
+}
+
+ZTEST_SUITE(gpio_id, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
