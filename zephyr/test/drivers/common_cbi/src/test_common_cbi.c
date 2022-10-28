3304e05703d170812bb7e842157030284f16f531
Aaron Massey <aaronmassey@google.com>
2022-10-28 17:20:44 -0600

    test: Fix stack smashing in cbi_set_string()
    The cbi_set_string() didn't allocate extra space when using the cbi_data
    struct to write values to. This resulted in the stack getting
    overwritten and breaking gitlab due to its stack protection.
    
    Allocate extra space for the cbi data "arbitrary_string" to be
    written. In follow-up CL add -fstack-protector-all to compiler options
    under native posix to prevent future occurrences.
    
    BRANCH=none
    BUG=b:256030656
    TEST=twister -i -s zephyr/test/drivers/drivers.common_cbi_gpio
    TEST=Above test but with -fstack-protector-all compiler option
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I3815ad567904475c23ddeb75b5370551cb482be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3990555
    Reviewed-by: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/common_cbi/src/test_common_cbi.c
+++ zephyr/test/drivers/common_cbi/src/test_common_cbi.c
@@ -26,19 +26,30 @@ ZTEST(common_cbi, test_cbi_set_string__null_str)
 
 ZTEST(common_cbi, test_cbi_set_string)
 {
-	struct cbi_data data = { 0 };
+	const char arbitrary_str[] = "hello cbi";
 	enum cbi_data_tag arbitrary_valid_tag = CBI_TAG_SKU_ID;
-	const char *arbitrary_str = "hello cbi";
 
+	struct cbi_data_wrapper {
+		struct cbi_data data;
+		uint8_t value_arr[ARRAY_SIZE(arbitrary_str)];
+	};
+	struct cbi_data_wrapper cbi_data = { 0 };
+
+	/* Set some provided memory then check values */
 	uint8_t *addr_byte_after_store = cbi_set_string(
-		(uint8_t *)&data, arbitrary_valid_tag, arbitrary_str);
+		(uint8_t *)&cbi_data, arbitrary_valid_tag, arbitrary_str);
 
-	zassert_equal(data.tag, arbitrary_valid_tag);
-	zassert_equal(data.size, strlen(arbitrary_str) + 1);
-	zassert_mem_equal(data.value, arbitrary_str, data.size);
-	zassert_equal(addr_byte_after_store -
-			      (strlen(arbitrary_str) + 1 + sizeof(data)),
-		      (uint8_t *)&data);
+	zassert_equal(cbi_data.data.tag, arbitrary_valid_tag);
+	zassert_equal(cbi_data.data.size, ARRAY_SIZE(arbitrary_str));
+	zassert_mem_equal(cbi_data.data.value, arbitrary_str,
+			  cbi_data.data.size);
+
+	uint32_t expected_added_memory =
+		(ARRAY_SIZE(arbitrary_str) + sizeof(cbi_data.data));
+
+	/* Validate that next address for write was set appropriately */
+	zassert_equal_ptr(addr_byte_after_store - expected_added_memory,
+			  &cbi_data.data);
 }
 
 ZTEST_SUITE(common_cbi, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
