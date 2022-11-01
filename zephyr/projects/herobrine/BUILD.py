118bcdb316ae1d652d22af6425dfc6c3c5eda1d9
Jason Yuan <jasonyuan@google.com>
2022-11-01 11:20:35 -0700

    zephyr EC: Project organization - herobrine
    Organized the herobrin program into folders for projects.
    
    Devicetree files have been changed to use the .dtsi extension following
    the zephyr convention. Formerly prj.conf file have been separated into
    programs and projects for disambiguation.
    
    BUG=b:254097836
    TEST=Ran zmake compare-builds
    BRANCH=none
    
    Change-Id: Id398eccde2b243f8aa521e898e6c09e6cd175236
    Signed-off-by: Jason Yuan <jasonyuan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3995330
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Tested-by: zhi cheng yuan <jasonyuan@chromium.org>
    Commit-Queue: zhi cheng yuan <jasonyuan@chromium.org>

--- zephyr/projects/herobrine/BUILD.py
+++ zephyr/projects/herobrine/BUILD.py
@@ -6,119 +6,44 @@
 
 
 def register_variant(
-    project_name, extra_dts_overlays=(), extra_kconfig_files=()
+    project_name,
 ):
     """Register a variant of herobrine."""
     register_npcx_project(
         project_name=project_name,
         zephyr_board="npcx9m3f",
         dts_overlays=[
-            # Common to all projects.
-            here / "adc.dts",
-            here / "common.dts",
-            here / "interrupts.dts",
-            here / "keyboard.dts",
-            here / "default_gpio_pinctrl.dts",
-            # Project-specific DTS customization.
-            *extra_dts_overlays,
+            here / project_name / "project.overlay",
         ],
         kconfig_files=[
             # Common to all projects.
-            here / "prj.conf",
+            here / "program.conf",
             # Project-specific KConfig customization.
-            *extra_kconfig_files,
+            here / project_name / "project.conf",
         ],
     )
 
 
 register_variant(
     project_name="evoker",
-    extra_dts_overlays=[
-        here / "display.dts",
-        here / "battery_evoker.dts",
-        here / "gpio_evoker.dts",
-        here / "i2c_evoker.dts",
-        here / "led_pins_evoker.dts",
-        here / "led_policy_evoker.dts",
-        here / "motionsense_evoker.dts",
-        here / "switchcap.dts",
-        here / "usbc_evoker.dts",
-    ],
-    extra_kconfig_files=[here / "prj_evoker.conf"],
 )
 
 register_variant(
     project_name="herobrine",
-    extra_dts_overlays=[
-        here / "display.dts",
-        here / "battery_herobrine.dts",
-        here / "gpio.dts",
-        here / "i2c_herobrine.dts",
-        here / "led_pins_herobrine.dts",
-        here / "led_policy_herobrine.dts",
-        here / "motionsense.dts",
-        here / "switchcap.dts",
-        here / "usbc_herobrine.dts",
-    ],
-    extra_kconfig_files=[here / "prj_herobrine.conf"],
 )
 
 register_variant(
     project_name="hoglin",
-    extra_dts_overlays=[
-        here / "battery_hoglin.dts",
-        here / "gpio_hoglin.dts",
-        here / "i2c_hoglin.dts",
-        here / "led_pins_hoglin.dts",
-        here / "led_policy_hoglin.dts",
-        here / "motionsense_hoglin.dts",
-        here / "switchcap_hoglin.dts",
-        here / "usbc_hoglin.dts",
-    ],
-    extra_kconfig_files=[here / "prj_hoglin.conf"],
 )
 
 register_variant(
     project_name="villager",
-    extra_dts_overlays=[
-        here / "battery_villager.dts",
-        here / "gpio_villager.dts",
-        here / "i2c_villager.dts",
-        here / "led_pins_villager.dts",
-        here / "led_policy_villager.dts",
-        here / "motionsense_villager.dts",
-        here / "switchcap.dts",
-        here / "usbc_villager.dts",
-    ],
-    extra_kconfig_files=[here / "prj_villager.conf"],
 )
 
 register_variant(
     project_name="zoglin",
-    extra_dts_overlays=[
-        here / "battery_hoglin.dts",
-        here / "gpio_hoglin.dts",
-        here / "i2c_hoglin.dts",
-        here / "led_pins_hoglin.dts",
-        here / "led_policy_hoglin.dts",
-        here / "motionsense_hoglin.dts",
-        here / "switchcap_hoglin.dts",
-        here / "usbc_hoglin.dts",
-    ],
-    extra_kconfig_files=[here / "prj_zoglin.conf"],
 )
 
 register_variant(
     project_name="zombie",
-    extra_dts_overlays=[
-        here / "battery_zombie.dts",
-        here / "gpio_zombie.dts",
-        here / "i2c_zombie.dts",
-        here / "led_pins_zombie.dts",
-        here / "led_policy_zombie.dts",
-        here / "motionsense_zombie.dts",
-        here / "switchcap.dts",
-        here / "usbc_zombie.dts",
-    ],
-    extra_kconfig_files=[here / "prj_zombie.conf"],
 )
