621d0ee7ce950e99763175cca796f51df29785a6
Fabio Baltieri <fabiobaltieri@google.com>
2022-11-03 12:09:49 +0000

    zephyr: shim: use a Kconfig option for building power.c
    Use an automatic Kconfig option to include power.c in the build, rather
    than including it unconditionally.
    
    Removes some precompiler guards from the source file and also move some
    macro that are only used in power.c out of the header file.
    
    BRANCH=none
    BUG=none
    TEST=cq dry run
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: I021ec1384ef374f1c1dc841ea861e848ee9e65f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4003277
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/shim/src/power.c
+++ zephyr/shim/src/power.c
@@ -9,10 +9,32 @@
 #include "power.h"
 #include "power/power.h"
 
-#if (SYSTEM_DT_POWER_SIGNAL_CONFIG)
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_GPIO(cid) \
+	DT_STRING_UPPER_TOKEN(DT_PROP(cid, power_gpio_pin), enum_name)
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_FLAGS(cid)              \
+	(DT_GPIO_FLAGS(DT_PROP(cid, power_gpio_pin), gpios) & \
+			 GPIO_ACTIVE_LOW ?                    \
+		 POWER_SIGNAL_ACTIVE_LOW :                    \
+		 POWER_SIGNAL_ACTIVE_HIGH)
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_NAME(cid) DT_PROP(cid, power_enum_name)
+
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY(cid)                         \
+	{                                                          \
+		.gpio = GEN_POWER_SIGNAL_STRUCT_ENTRY_GPIO(cid),   \
+		.flags = GEN_POWER_SIGNAL_STRUCT_ENTRY_FLAGS(cid), \
+		.name = GEN_POWER_SIGNAL_STRUCT_ENTRY_NAME(cid)    \
+	}
+#define GEN_POWER_SIGNAL_STRUCT(cid) \
+	[GEN_POWER_SIGNAL_ENUM_ENTRY(cid)] = GEN_POWER_SIGNAL_STRUCT_ENTRY(cid),
 
 const struct power_signal_info power_signal_list[] = { DT_FOREACH_CHILD(
 	POWER_SIGNAL_LIST_NODE, GEN_POWER_SIGNAL_STRUCT) };
 BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
-#endif /* SYSTEM_DT_POWER_SIGNAL_CONFIG */
+/*
+ * Verify the number of required power-signals are specified in
+ * the DeviceTree
+ */
+#define POWER_SIGNALS_REQUIRED \
+	DT_PROP(POWER_SIGNAL_LIST_NODE, power_signals_required)
+BUILD_ASSERT(POWER_SIGNALS_REQUIRED == POWER_SIGNAL_COUNT);
