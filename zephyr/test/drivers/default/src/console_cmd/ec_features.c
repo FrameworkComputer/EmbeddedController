d889135ff840ec5055023a7c68522d4481286392
Jeremy Bettis <jbettis@google.com>
2022-11-03 13:32:55 -0600

    zephyr/test: Increase buffer size
    The expected buffer is too small for the format string. Increase it and
    add header for snprintf.
    
    BRANCH=None
    BUG=None
    TEST=./twister with gcc
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I27310ae77db004e2b1aafa0aa95eefd40a4011d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4004758
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Yuval Peress <peress@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Yuval Peress <peress@google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/test/drivers/default/src/console_cmd/ec_features.c
+++ zephyr/test/drivers/default/src/console_cmd/ec_features.c
@@ -6,6 +6,7 @@
 #include <zephyr/shell/shell.h>
 #include <zephyr/ztest.h>
 
+#include "builtin/stdio.h"
 #include "console.h"
 #include "host_command.h"
 #include "test/drivers/test_state.h"
@@ -16,10 +17,13 @@ ZTEST_SUITE(console_cmd_ec_features, drivers_predicate_post_main, NULL, NULL,
 
 ZTEST_USER(console_cmd_ec_features, test_feat)
 {
-	char expected[32];
+	char expected[50];
+	int ret;
 
-	snprintf(expected, sizeof(expected), " 0-31: 0x%08x\r\n32-63: 0x%08x",
-		 get_feature_flags0(), get_feature_flags1());
+	ret = snprintf(expected, sizeof(expected),
+		       " 0-31: 0x%08x\r\n32-63: 0x%08x", get_feature_flags0(),
+		       get_feature_flags1());
+	zassert_true(ret >= 0 && ret < sizeof(expected));
 
 	CHECK_CONSOLE_CMD("feat", expected, EC_SUCCESS);
 }
