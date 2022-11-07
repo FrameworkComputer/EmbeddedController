b191de95537ad2617f21d83f282d33a5b803f31b
Jeremy Bettis <jbettis@google.com>
2022-11-07 15:24:57 -0700

    zephyr: Add cmake files for host toolchain
    In order to allow building with the host toolchain inside the chroot,
    which should use  /usr/bin/x86_64-pc-linux-gnu-gcc, add cmake files for
    the host toolchain, that uses the same cross compiler gcc configs with
    the cross compile target set to x86_64-pc-linux-gnu.
    
    BRANCH=None
    BUG=None
    TEST=ZEPHYR_TOOLCHAIN_VARIANT=host ./twister \
      -x=CMAKE_VERBOSE_MAKEFILE=ON
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I9148f9fc58ccb0cbbf278c467c21d7ae78d099d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4011098
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/cmake/toolchain/host/generic.cmake
@@ -0,0 +1,17 @@
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set(CROSS_COMPILE_TARGET_posix    x86_64-pc-linux-gnu)
+set(CROSS_COMPILE_TARGET          ${CROSS_COMPILE_TARGET_${ARCH}})
+
+set(CC gcc)
+set(CROSS_COMPILE "/usr/bin/${CROSS_COMPILE_TARGET}-")
+
+set(COMPILER gcc)
+set(LINKER ld)
+set(BINTOOLS gnu)
+
+set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")
+
+message(STATUS "Found toolchain: host ${ARCH} (gcc/ld)")
