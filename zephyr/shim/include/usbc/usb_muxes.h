6acfadf0df547a86b1dfddb3f5487d606774be6a
Keith Short <keithshort@chromium.org>
2022-11-10 15:19:52 -0700

    usb_mux shim: Set a default flags value
    The flags property is marked as not required, set the default flag field
    to 0 if not specified in the devicetree.
    
    BUG=none
    BRANCH=none
    TEST=twister
    TEST=zmake compare-builds
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: Ibb4590645aa901bc874acb721fd0bbbb7671f409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4023766
    Reviewed-by: Yuval Peress <peress@google.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/shim/include/usbc/usb_muxes.h
+++ zephyr/shim/include/usbc/usb_muxes.h
@@ -177,14 +177,14 @@
  * @brief Set struct usb_mux fields common for all USB muxes and alter flags
  *
  * @param mux_id USB mux node ID
- * @param flags_mask Mask for bits that should be igonred in flags property
+ * @param flags_mask Mask for bits that should be ignored in flags property
  * @param flags_val Value that should be used instead for masked bits
  */
 #define USB_MUX_COMMON_FIELDS_WITH_FLAGS(mux_id, flags_mask, flags_val) \
 	.usb_port = USB_MUX_PORT(mux_id),                               \
 	.board_init = USB_MUX_CALLBACK_OR_NULL(mux_id, board_init),     \
 	.board_set = USB_MUX_CALLBACK_OR_NULL(mux_id, board_set),       \
-	.flags = (DT_PROP(mux_id, flags) & ~(flags_mask)) | (flags_val)
+	.flags = (DT_PROP_OR(mux_id, flags, 0) & ~(flags_mask)) | (flags_val)
 
 /**
  * @brief Set struct usb_mux fields common for all USB muxes
