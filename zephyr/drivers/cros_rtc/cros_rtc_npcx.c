d504c8cf98d76c501b0979b98954540ef4ae581b
Jeremy Bettis <jbettis@google.com>
2022-11-14 14:53:57 -0700

    zephyr/drivers: Fix copyright header
    BRANCH=None
    BUG=None
    TEST=None
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I7b59c1a4322786ff09df37932b4d38dc92a457d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4024020
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>

--- zephyr/drivers/cros_rtc/cros_rtc_npcx.c
+++ zephyr/drivers/cros_rtc/cros_rtc_npcx.c
@@ -1,7 +1,6 @@
-/*
- * Copyright 2021 Google LLC
- *
- * SPDX-License-Identifier: Apache-2.0
+/* Copyright 2021 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #define DT_DRV_COMPAT nuvoton_npcx_cros_mtc
