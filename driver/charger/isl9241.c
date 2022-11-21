c850ee4a7f85845fea95ddbe1cac359697a16938
Robert Zieba <robertzieba@google.com>
2022-11-21 17:45:28 +0000

    driver/isl9241: Add CONTROL3 mutex
    The CONTROL3 register does not currently seem to be accessed from
    different threads. However, the recently added PROCHOT info dumping
    function could access/modify this register from a separate thread. This
    commit protects CONTROL3 with a mutex.
    
    BRANCH=none
    BUG=b:227768313
    TEST=Ran 1000 cycles of charger factory test
    LOW_COVERAGE_REASON=No isl9241 emulator
    
    Change-Id: I9e4b446c7466f28d7d6eb504d9a5606ae158b287
    Signed-off-by: Robert Zieba <robertzieba@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4064419
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- driver/charger/isl9241.c
+++ driver/charger/isl9241.c
@@ -49,6 +49,9 @@ static int learn_mode;
 /* Mutex for CONTROL1 register, that can be updated from multiple tasks. */
 K_MUTEX_DEFINE(control1_mutex_isl9241);
 
+/* Mutex for CONTROL3 register, that can be updated from multiple tasks. */
+K_MUTEX_DEFINE(control3_mutex_isl9241);
+
 /* Charger parameters */
 static const struct charger_info isl9241_charger_info = {
 	.name = CHARGER_NAME,
@@ -253,8 +256,10 @@ static enum ec_error_list isl9241_set_mode(int chgnum, int mode)
 
 	/* POR reset */
 	if (mode & CHARGE_FLAG_POR_RESET) {
+		mutex_lock(&control3_mutex_isl9241);
 		rv = isl9241_write(chgnum, ISL9241_REG_CONTROL3,
 				   ISL9241_CONTROL3_DIGITAL_RESET);
+		mutex_unlock(&control3_mutex_isl9241);
 	}
 
 	return rv;
@@ -296,6 +301,7 @@ static enum ec_error_list isl9241_get_vbus_voltage(int chgnum, int port,
 	int rv;
 
 	/* Get current Control3 value */
+	mutex_lock(&control3_mutex_isl9241);
 	rv = isl9241_read(chgnum, ISL9241_REG_CONTROL3, &ctl3_val);
 	if (rv)
 		goto error;
@@ -330,6 +336,7 @@ error_restore_ctl3:
 		(void)isl9241_write(chgnum, ISL9241_REG_CONTROL3, ctl3_val);
 
 error:
+	mutex_unlock(&control3_mutex_isl9241);
 	if (rv)
 		CPRINTS("Could not read VBUS ADC! Error: %d", rv);
 
@@ -340,13 +347,14 @@ static enum ec_error_list isl9241_get_vsys_voltage(int chgnum, int port,
 						   int *voltage)
 {
 	int val = 0;
-	int rv;
+	int rv = EC_SUCCESS;
 
+	mutex_lock(&control3_mutex_isl9241);
 	rv = isl9241_update(chgnum, ISL9241_REG_CONTROL3,
 			    ISL9241_CONTROL3_ENABLE_ADC, MASK_SET);
 	if (rv) {
 		CPRINTS("Could not enable ADC for Vsys. (rv=%d)", rv);
-		return rv;
+		goto unlock_control3;
 	}
 
 	usleep(ISL9241_ADC_POLLING_TIME_US);
@@ -357,7 +365,7 @@ static enum ec_error_list isl9241_get_vsys_voltage(int chgnum, int port,
 		CPRINTS("Could not read Vsys. (rv=%d)", rv);
 		isl9241_update(chgnum, ISL9241_REG_CONTROL3,
 			       ISL9241_CONTROL3_ENABLE_ADC, MASK_CLR);
-		return rv;
+		goto unlock_control3;
 	}
 
 	/* Adjust adc_val. Same as Vin. */
@@ -365,7 +373,10 @@ static enum ec_error_list isl9241_get_vsys_voltage(int chgnum, int port,
 	val *= ISL9241_VIN_ADC_STEP_MV;
 	*voltage = val;
 
-	return EC_SUCCESS;
+unlock_control3:
+	mutex_unlock(&control3_mutex_isl9241);
+
+	return rv;
 }
 
 static enum ec_error_list isl9241_post_init(int chgnum)
@@ -491,10 +502,11 @@ static int isl9241_get_prochot_status(int chgnum, bool *out_low_vsys,
 	*out_acprochot = val & ISL9241_REG_INFORMATION1_AC_PROCHOT;
 
 	/* Get current Control3 value */
+	mutex_lock(&control3_mutex_isl9241);
 	rv = isl9241_read(chgnum, ISL9241_REG_CONTROL3, &val);
 	if (rv) {
 		CPRINTS("%s: failed to read control3 (%d)", __func__, rv);
-		return rv;
+		goto restore_control3;
 	}
 
 	/* Enable ADC */
@@ -503,15 +515,9 @@ static int isl9241_get_prochot_status(int chgnum, bool *out_low_vsys,
 				   val | ISL9241_CONTROL3_ENABLE_ADC);
 		if (rv) {
 			CPRINTS("%s: failed to enable ADC (%d)", __func__, rv);
-			return rv;
+			goto restore_control3;
 		}
 
-		/*
-		 * Since this function will generally run in an interrupt
-		 * handler using a mutex for control3 could cause a deadlock.
-		 * Instead we keep track of if the ADC was enabled and restore
-		 * it accordingly.
-		 */
 		restore_adc = true;
 	}
 
@@ -585,6 +591,7 @@ restore_control3:
 		if (rv)
 			CPRINTS("%s: failed to disable ADC (%d)", __func__, rv);
 	}
+	mutex_unlock(&control3_mutex_isl9241);
 
 	return rv;
 }
@@ -681,6 +688,7 @@ static enum ec_error_list isl9241_bypass_to_bat(int chgnum)
 
 	CPRINTS("bypass -> bat");
 
+	mutex_lock(&control3_mutex_isl9241);
 	/* 1: Disable force forward buck/reverse boost. */
 	isl9241_update(chgnum, ISL9241_REG_CONTROL4,
 		       ISL9241_CONTROL4_FORCE_BUCK_MODE, MASK_CLR);
@@ -707,6 +715,7 @@ static enum ec_error_list isl9241_bypass_to_bat(int chgnum)
 		      ISL9241_MV_TO_ACOK_REFERENCE(
 			      ISL9241_ACOK_REF_LOW_VOLTAGE_ADAPTER_MV));
 
+	mutex_unlock(&control3_mutex_isl9241);
 	return EC_SUCCESS;
 }
 
@@ -717,6 +726,7 @@ static enum ec_error_list isl9241_bypass_chrg_to_bat(int chgnum)
 {
 	CPRINTS("bypass_chrg -> bat");
 
+	mutex_lock(&control3_mutex_isl9241);
 	/* 1: Disable force forward buck/reverse boost. */
 	isl9241_update(chgnum, ISL9241_REG_CONTROL4,
 		       ISL9241_CONTROL4_FORCE_BUCK_MODE, MASK_CLR);
@@ -739,6 +749,7 @@ static enum ec_error_list isl9241_bypass_chrg_to_bat(int chgnum)
 	isl9241_write(chgnum, ISL9241_REG_ACOK_REFERENCE,
 		      ISL9241_MV_TO_ACOK_REFERENCE(3600));
 
+	mutex_unlock(&control3_mutex_isl9241);
 	return EC_SUCCESS;
 }
 
@@ -776,10 +787,12 @@ static enum ec_error_list isl9241_nvdc_to_bypass(int chgnum)
 	const int charge_current = charge_manager_get_charger_current();
 	const int charge_voltage = charge_manager_get_charger_voltage();
 	int vsys, vsys_target;
+	int rv = EC_SUCCESS;
 	timestamp_t deadline;
 
 	CPRINTS("nvdc -> bypass");
 
+	mutex_lock(&control3_mutex_isl9241);
 	/* 1: Set adapter current limit. */
 	isl9241_set_input_current_limit(chgnum, charge_current);
 
@@ -814,12 +827,14 @@ static enum ec_error_list isl9241_nvdc_to_bypass(int chgnum)
 		msleep(ISL9241_BYPASS_VSYS_TIMEOUT_MS / 10);
 		if (isl9241_get_vsys_voltage(chgnum, 0, &vsys)) {
 			CPRINTS("Aborting bypass mode. Vsys is unknown.");
-			return EC_ERROR_UNKNOWN;
+			rv = EC_ERROR_UNKNOWN;
+			goto unlock_control3;
 		}
 		if (timestamp_expired(deadline, NULL)) {
 			CPRINTS("Aborting bypass mode. Vsys too low (%d < %d)",
 				vsys, vsys_target);
-			return EC_ERROR_TIMEOUT;
+			rv = EC_ERROR_TIMEOUT;
+			goto unlock_control3;
 		}
 	} while (vsys < vsys_target - 256);
 
@@ -841,12 +856,14 @@ static enum ec_error_list isl9241_nvdc_to_bypass(int chgnum)
 	isl9241_update(chgnum, ISL9241_REG_CONTROL1, ISL9241_CONTROL1_BGATE_OFF,
 		       MASK_CLR);
 
-	if (!isl9241_is_ac_present(chgnum))
+	if (!isl9241_is_ac_present(chgnum)) {
 		/*
 		 * Suggestion: If ACOK goes low before step A16, stop
 		 * executing commands and complete steps for Bypass to BAT.
 		 */
-		return EC_ERROR_PARAM1;
+		rv = EC_ERROR_PARAM1;
+		goto unlock_control3;
+	}
 
 	/* 16: Enable 10 mA discharge on CSOP. */
 	/* 17: Read diode emulation active bit. */
@@ -855,14 +872,18 @@ static enum ec_error_list isl9241_nvdc_to_bypass(int chgnum)
 	isl9241_update(chgnum, ISL9241_REG_CONTROL4,
 		       ISL9241_CONTROL4_FORCE_BUCK_MODE, MASK_SET);
 
-	if (!isl9241_is_ac_present(chgnum))
+	if (!isl9241_is_ac_present(chgnum)) {
 		/*
 		 * Suggestion: If AC is removed on or after A16, complete all
 		 * 19 steps then execute Bypass to BAT.
 		 */
-		return EC_ERROR_PARAM2;
+		rv = EC_ERROR_PARAM2;
+	}
 
-	return EC_SUCCESS;
+unlock_control3:
+	mutex_unlock(&control3_mutex_isl9241);
+
+	return rv;
 }
 
 /*
@@ -1006,6 +1027,7 @@ static void isl9241_init(int chgnum)
 
 	const struct battery_info *bi = battery_get_info();
 
+	mutex_lock(&control3_mutex_isl9241);
 	/*
 	 * Set the MaxSystemVoltage to battery maximum,
 	 * 0x00=disables switching charger states
@@ -1066,6 +1088,8 @@ static void isl9241_init(int chgnum)
 		goto init_fail;
 #endif
 
+	mutex_unlock(&control3_mutex_isl9241);
+
 	/*
 	 * No need to proceed with the rest of init if we sysjump'd to this
 	 * image as the input current limit has already been set.
@@ -1081,6 +1105,7 @@ static void isl9241_init(int chgnum)
 	return;
 
 init_fail:
+	mutex_unlock(&control3_mutex_isl9241);
 	CPRINTS("Init failed!");
 }
 
