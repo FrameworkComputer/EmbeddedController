f366b8c97b6bc2bc3dcf4b2e69c2a3c43ea6efcb
Wai-Hong Tam <waihong@google.com>
2022-11-21 09:28:04 -0800

    zephyr: herobrine: Handle HPD High embedded in DP-Status VDM
    The HPD state is a field of the DP-Status VDO. The DP-Status VDO is
    embedded in either the DP-Status VDM or the Attention VDM.
    
    The DP-Status happens before the DP-Config. The pin assignment of the
    PD partner hasn't been set up yet. Passing through the HPD to the DP
    PHY is too early. Racing it may fail the link-training. Should wait
    until the DP-Config is done.
    
    BRANCH=None
    BUG=b:247005613
    LOW_COVERAGE_REASON=No existing test for the board-specific code
    TEST=Connect the device through a Type-C dongle to a monitor and verify
    the screen. Try the combinations:
     * Plug C0 monitor -> plug C0 dongle -> screen on C0 monitor
     * Plug C0 dongle -> plug C0 monitor -> screen on C0 monitor
       -> unplug C0 monitor -> plug C0 monitor -> screen on C0 monitor
     * Plug C0 dongle -> plug C0 monitor -> screen on C0 monitor
       -> plug C1 dongle -> -> plug C1 monitor -> no screen on C1 monitor
       -> unplug C0 monitor -> replug C1 monitor -> screen on C0 monitor
     * Plug C0 dongle -> plug C1 dongle -> plug C1 monitor
       -> screen on C1 monitor -> unplug C1 dongle -> plug C0 monitor
       -> screen on C0 monitor
    
    Change-Id: I9387568e2a36f3d44ba2efc78f9fcc2b64bd03fd
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4043350
    Reviewed-by: Eric Yilun Lin <yllin@google.com>

--- zephyr/program/herobrine/src/usb_pd_policy.c
+++ zephyr/program/herobrine/src/usb_pd_policy.c
@@ -104,8 +104,6 @@ __override int svdm_dp_config(int port, uint32_t *payload)
 
 __override void svdm_dp_post_config(int port)
 {
-	dp_flags[port] |= DP_FLAGS_DP_ON;
-
 	/*
 	 * Connect the SBU lines in PPC chip such that the AUX termination
 	 * can be passed through.
@@ -122,6 +120,19 @@ __override void svdm_dp_post_config(int port)
 	 */
 	usb_mux_set(port, USB_PD_MUX_DOCK, USB_SWITCH_CONNECT,
 		    polarity_rm_dts(pd_get_polarity(port)));
+
+	dp_flags[port] |= DP_FLAGS_DP_ON;
+	if (!(dp_flags[port] & DP_FLAGS_HPD_HI_PENDING))
+		return;
+
+	CPRINTS("C%d: Pending HPD. HPD->1", port);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_hot_plug_det_r), 1);
+
+	/* set the minimum time delay (2ms) for the next HPD IRQ */
+	svdm_hpd_deadline[port] = get_time().val + HPD_USTREAM_DEBOUNCE_LVL;
+
+	usb_mux_hpd_update(port,
+			   USB_PD_MUX_HPD_LVL | USB_PD_MUX_HPD_IRQ_DEASSERTED);
 }
 
 /**
@@ -193,6 +204,13 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 		 */
 		pd_notify_dp_alt_mode_entry(port);
 
+	/* Its initial DP status message prior to config */
+	if (!(dp_flags[port] & DP_FLAGS_DP_ON)) {
+		if (lvl)
+			dp_flags[port] |= DP_FLAGS_HPD_HI_PENDING;
+		return 1;
+	}
+
 	/* Configure TCPC for the HPD event, for proper muxing */
 	mux_state = (lvl ? USB_PD_MUX_HPD_LVL : USB_PD_MUX_HPD_LVL_DEASSERTED) |
 		    (irq ? USB_PD_MUX_HPD_IRQ : USB_PD_MUX_HPD_IRQ_DEASSERTED);
@@ -232,6 +250,8 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 __override void svdm_exit_dp_mode(int port)
 {
 	CPRINTS("%s(%d)", __func__, port);
+	dp_flags[port] = 0;
+	dp_status[port] = 0;
 	if (is_dp_muxable(port)) {
 		/* Disconnect the DP port selection mux. */
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_mux_oe_l), 1);
