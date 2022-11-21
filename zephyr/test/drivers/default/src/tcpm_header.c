e5d38683d436b29ce8ff7434dafae7dd6c3bb301
Clayton Whitelaw <cawhitelaw@google.com>
2022-11-21 14:10:35 -0700

    tcpm_header: add tests for tcpm_reset_bist_type_2
    This function's call-through to the driver's implementation was
    uncovered by current testing.
    
    Added tests for both cases where the driver does and does not implement
    this function.
    
    BUG=none
    TEST=twister, verify line is now covered
    BRANCH=none
    
    Signed-off-by: Clayton Whitelaw <cawhitelaw@google.com>
    Change-Id: Ib38b70e076938a8c139cf4b785967ec546faa7d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4043730
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/default/src/tcpm_header.c
+++ zephyr/test/drivers/default/src/tcpm_header.c
@@ -14,6 +14,7 @@
 #define TCPM_TEST_PORT USBC_PORT_C0
 
 FAKE_VALUE_FUNC(int, set_vconn, int, int);
+FAKE_VALUE_FUNC(int, reset_bist_type_2, int);
 
 struct tcpm_header_fixture {
 	/* The original driver pointer that gets restored after the tests */
@@ -39,6 +40,29 @@ ZTEST_F(tcpm_header, test_tcpm_header_drv_set_vconn_failure)
 	zassert_equal(-1, res);
 }
 
+ZTEST_F(tcpm_header, test_tcpm_header_reset_bist_type_2__unimplemented)
+{
+	int res;
+
+	res = tcpm_reset_bist_type_2(TCPM_TEST_PORT);
+
+	zassert_equal(EC_SUCCESS, res);
+}
+
+ZTEST_F(tcpm_header, test_tcpm_header_reset_bist_type_2__implemented)
+{
+	int res;
+	const int driver_return_code = 7458; /* arbitrary */
+
+	fixture->mock_driver.reset_bist_type_2 = reset_bist_type_2;
+	reset_bist_type_2_fake.return_val = driver_return_code;
+	res = tcpm_reset_bist_type_2(TCPM_TEST_PORT);
+
+	zassert_equal(1, reset_bist_type_2_fake.call_count);
+	zassert_equal(TCPM_TEST_PORT, reset_bist_type_2_fake.arg0_history[0]);
+	zassert_equal(driver_return_code, res);
+}
+
 static void *tcpm_header_setup(void)
 {
 	static struct tcpm_header_fixture fixture;
@@ -51,6 +75,7 @@ static void tcpm_header_before(void *state)
 	struct tcpm_header_fixture *fixture = state;
 
 	RESET_FAKE(set_vconn);
+	RESET_FAKE(reset_bist_type_2);
 
 	fixture->mock_driver = (struct tcpm_drv){ 0 };
 	fixture->saved_driver_ptr = tcpc_config[TCPM_TEST_PORT].drv;
