20ccd7a68f442514df5c7d1221ea733994ed39e0
jeffrey <jeffrey_lin@pegatron.corp-partner.google.com>
2022-12-12 16:01:07 +0800

    tentacruel: add lid sensor 2nd source BMA422.
    add BMA422 lid sensor and modify FW config conditions.
    
    BUG=b:255501962
    TEST=ectool motionsense info to check sensor are initial success.
    confirm lid angle.
    BRANCH=firmware-corsola-15194.B
    
    Signed-off-by: jeffrey <jeffrey_lin@pegatron.corp-partner.google.com>
    Change-Id: Iaf4eb89586854d5accc0643caca52875793e2152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4087970
    Reviewed-by: Eric Yilun Lin <yllin@google.com>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/corsola/tentacruel/src/sensor.c
+++ zephyr/program/corsola/tentacruel/src/sensor.c
@@ -5,36 +5,89 @@
 
 #include "accelgyro.h"
 #include "cros_cbi.h"
+#include "driver/accel_bma422.h"
+#include "driver/accel_lis2dw12.h"
 #include "driver/accelgyro_bmi323.h"
 #include "driver/accelgyro_icm42607.h"
+#include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "motion_sense.h"
 #include "motionsense_sensors.h"
+#include "tablet_mode.h"
 
-void motion_interrupt(enum gpio_signal signal)
+static bool base_is_none;
+static bool lid_is_none;
+
+void base_sensor_interrupt(enum gpio_signal signal)
 {
 	uint32_t val;
 
-	cros_cbi_get_fw_config(FW_BASE_GYRO, &val);
-	if (val == FW_BASE_ICM42607) {
+	cros_cbi_get_fw_config(BASE_SENSOR, &val);
+	if (val == BASE_ICM42607) {
 		icm42607_interrupt(signal);
-	} else if (val == FW_BASE_BMI323) {
+	} else if (val == BASE_BMI323) {
 		bmi3xx_interrupt(signal);
+	} else if (val == BASE_NONE) {
+		base_is_none = true;
 	}
 }
 
-static void motionsense_init(void)
+void lid_sensor_interrupt(enum gpio_signal signal)
 {
 	uint32_t val;
 
-	cros_cbi_get_fw_config(FW_BASE_GYRO, &val);
-	if (val == FW_BASE_ICM42607) {
-		ccprints("BASE ACCEL is ICM42607");
-	} else if (val == FW_BASE_BMI323) {
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
-		ccprints("BASE ACCEL IS BMI323");
-	} else {
-		ccprints("no motionsense");
+	cros_cbi_get_fw_config(LID_SENSOR, &val);
+	if (val == LID_LIS2DWLTR) {
+		lis2dw12_interrupt(signal);
+	} else if (val == LID_BMA422) {
+		bma4xx_interrupt(signal);
+	} else if (val == LID_NONE) {
+		lid_is_none = true;
 	}
 }
-DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
+
+static void disable_base_lid_irq(void)
+{
+	if (base_is_none && lid_is_none) {
+		gpio_disable_dt_interrupt(
+			GPIO_INT_FROM_NODELABEL(int_base_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(base_imu_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(lid_accel_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, disable_base_lid_irq, HOOK_PRIO_POST_DEFAULT);
+
+static void board_sensor_init(void)
+{
+	uint32_t val;
+
+	cros_cbi_get_fw_config(FORM_FACTOR, &val);
+	if (val == CLAMSHELL) {
+		motion_sensor_count = 0;
+		gmr_tablet_switch_disable();
+		ccprints("Board is Clamshell");
+	} else if (val == CONVERTIBLE) {
+		ccprints("Board is Convertible");
+
+		cros_cbi_get_fw_config(BASE_SENSOR, &val);
+		if (val == BASE_ICM42607) {
+			ccprints("Base sensor is ICM42607");
+		} else if (val == BASE_BMI323) {
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
+			ccprints("Base sensor is BMI323");
+		}
+
+		cros_cbi_get_fw_config(LID_SENSOR, &val);
+		if (val == LID_LIS2DWLTR) {
+			ccprints("Lid sensnor is LIS2DWLTR");
+		} else if (val == LID_BMA422) {
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
+			ccprints("Lid sensnor is BMA422");
+		}
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_sensor_init, HOOK_PRIO_DEFAULT);
---
+++ zephyr/test/krabby/src/fake.c
@@ -0,0 +1,23 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gpio_signal.h"
+
+#include <zephyr/fff.h>
+
+FAKE_VOID_FUNC(power_button_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(button_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(lid_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(chipset_reset_request_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(power_signal_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(chipset_watchdog_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(extpower_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(usb_a0_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(switch_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(spi_event, enum gpio_signal);
+FAKE_VOID_FUNC(ppc_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(bc12_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(ccd_interrupt, enum gpio_signal);
+FAKE_VOID_FUNC(x_ec_interrupt, enum gpio_signal);
