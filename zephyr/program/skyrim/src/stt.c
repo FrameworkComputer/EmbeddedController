f22f03e698fb89988c7b576a9d794cf04fdd2393
matt_wang <matt_wang@compal.corp-partner.google.com>
2023-01-09 17:00:02 +0800

    markarth: modify thermal sensor
    Markarth change the thermal sensor from PCT2075 to F75303.
    
    LOW_COVERAGE_REASON=no unit tests for Markarth yet, b/247151116
    BUG=b:262409595
    BRANCH=none
    TEST=zmake build markarth success
    
    Change-Id: I6fd0bb506d1daa78b2f729f62d96c09375583ceb
    Signed-off-by: matt_wang <matt_wang@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4145105
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- zephyr/program/skyrim/src/stt.c
+++ zephyr/program/skyrim/src/stt.c
@@ -6,6 +6,7 @@
 /* Support code for STT temperature reporting */
 
 #include "chipset.h"
+#include "driver/temp_sensor/f75303.h"
 #include "temp_sensor/pct2075.h"
 #include "temp_sensor/temp_sensor.h"
 
@@ -14,8 +15,13 @@ int board_get_soc_temp_mk(int *temp_mk)
 	if (chipset_in_state(CHIPSET_STATE_HARD_OFF))
 		return EC_ERROR_NOT_POWERED;
 
+#ifdef CONFIG_TEMP_SENSOR_PCT2075
 	return pct2075_get_val_mk(PCT2075_SENSOR_ID(DT_NODELABEL(soc_pct2075)),
 				  temp_mk);
+#else
+	return f75303_get_val_mk(F75303_SENSOR_ID(DT_NODELABEL(soc_f75303)),
+				 temp_mk);
+#endif
 }
 
 int board_get_ambient_temp_mk(int *temp_mk)
@@ -23,6 +29,11 @@ int board_get_ambient_temp_mk(int *temp_mk)
 	if (chipset_in_state(CHIPSET_STATE_HARD_OFF))
 		return EC_ERROR_NOT_POWERED;
 
+#ifdef CONFIG_TEMP_SENSOR_PCT2075
 	return pct2075_get_val_mk(PCT2075_SENSOR_ID(DT_NODELABEL(amb_pct2075)),
 				  temp_mk);
+#else
+	return f75303_get_val_mk(F75303_SENSOR_ID(DT_NODELABEL(amb_f75303)),
+				 temp_mk);
+#endif
 }
