cefcacb3742b589a412c0b72e9bea468269b5c3a
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-01-13 20:15:54 -0800

    ap_pwrseq: Add S3 state action handler for MTL chipset
    Following AP power sequence driver framework, this CL will add S3 state
    action handlers.
    
    BUG=b:217952699
    TEST=zmake build
    LOW_COVERAGE_REASON=Tests will be added in b:295173553
    
    Change-Id: Iab5e1e763a881b6024755acda9342a5d9e9bb3d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4169238
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_mtl_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_mtl_pwrseq_sm.c
@@ -79,4 +79,47 @@ static int x86_non_dsx_mtl_g3_run(void *data)
 
 AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_G3, NULL, x86_non_dsx_mtl_g3_run,
 			      NULL);
+
+static int x86_non_dsx_mtl_s3_entry(void *data)
+{
+	power_signal_set(PWR_PCH_PWROK, 0);
+	power_signal_set(PWR_EC_PCH_SYS_PWROK, 0);
+
+	return 0;
+}
+
+static int x86_non_dsx_mtl_s3_run(void *data)
+{
+	int all_sys_pwrgd_in = power_signal_get(PWR_ALL_SYS_PWRGD);
+
+	if (power_signal_get(PWR_RSMRST) == 0) {
+		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_G3);
+	}
+
+	if (power_signal_get(PWR_SLP_S4)) {
+		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S4);
+	}
+
+	if (all_sys_pwrgd_in && !power_signal_get(PWR_EC_PCH_SYS_PWROK)) {
+		k_msleep(AP_PWRSEQ_DT_VALUE(sys_pwrok_delay));
+	}
+
+	power_signal_set(PWR_EC_PCH_SYS_PWROK, all_sys_pwrgd_in);
+	/* PCH_PWROK is set to combined result of ALL_SYS_PWRGD and SLP_S3 */
+	power_signal_set(PWR_PCH_PWROK,
+			 all_sys_pwrgd_in && !power_signal_get(PWR_SLP_S3));
+
+	if ((power_signal_get(PWR_ALL_SYS_PWRGD) !=
+	     power_signal_get(PWR_PCH_PWROK)) ||
+	    (power_signal_get(PWR_ALL_SYS_PWRGD) !=
+	     power_signal_get(PWR_EC_PCH_SYS_PWROK))) {
+		/* Make sure these signals levels are stable */
+		return 1;
+	}
+
+	return 0;
+}
+
+AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S3, x86_non_dsx_mtl_s3_entry,
+			      x86_non_dsx_mtl_s3_run, NULL);
 #endif /* CONFIG_AP_PWRSEQ_DRIVER */
