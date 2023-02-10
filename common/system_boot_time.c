ed9896847fda73e6c12a17b93c47ff413511e7be
Rajesh Kumar <rajesh3.kumar@intel.com>
2023-02-10 11:12:49 -0800

    system_boot_time: Fix warm reboot counter update for native power seq
    Warm reboot counter is being reset to 0 on hook shutdown and increments
    on PLTRST. In coldboot scenario, warm reboot counter is incrementing on
    PLTRST and hook_shutdown was executed after PLTRST resetting the counter
    to 0 resulting the expected value of counter, but with native power
    sequencing, hook shutdown is being executed before PLTRST and counter is
    not being reset to 0.
    This change handles the PLTRST transition appropriately to identify cold
    and warm reboot scenario and updates the warm reboot counter as
    expected.
    
    BUG=b:249163956
    BRANCH=none
    TEST='ectool boottime' AP console command is used to fetch data
         'reboot' on AP console to trigger warm reboot
         'apshutdown' on EC console to trigger cold reboot
    
    Change-Id: I969bd9480bbfade481370a5a35f47cbbc53bc4bc
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4240334
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Reviewed-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- common/system_boot_time.c
+++ common/system_boot_time.c
@@ -9,6 +9,8 @@
 #include "system.h"
 #include "util.h"
 
+#include <stdbool.h>
+
 #ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
 /* Content of ap_boot_time will be lost on sysjump */
 static struct ec_response_get_boot_time ap_boot_time;
@@ -18,6 +20,9 @@ static struct ec_response_get_boot_time ap_boot_time;
 void update_ap_boot_time(enum boot_time_param param)
 {
 #ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
+	static bool ap_booted; /* tracks AP booted after #PLTRST */
+	static bool pltrst_transition; /* tracks #PLTRST transtion */
+
 	if (param > RESET_CNT) {
 		ccprintf("invalid boot_time_param: %d\n", param);
 		return;
@@ -28,12 +33,35 @@ void update_ap_boot_time(enum boot_time_param param)
 			 ap_boot_time.timestamp[param]);
 	}
 
+	/* warm reboot counter set to 1 only when #PLTRST transtion is complete
+	 * and AP is already booted (ap_booted = 1).
+	 * In initial boot or cold reboot scenario (ap_booted = 0) warm reboot
+	 * counter will be set to 0.
+	 */
 	switch (param) {
+	case PLTRST_HIGH:
+		/* warm reboot happened */
+		if (ap_booted && pltrst_transition)
+			ap_boot_time.cnt = 1;
+		else /* cold reboot happened */
+			ap_boot_time.cnt = 0;
+
+		if (pltrst_transition) {
+			ap_booted = true;
+			pltrst_transition = false;
+		}
+		break;
 	case PLTRST_LOW:
-		ap_boot_time.cnt++;
+		/* #PLTRST is in transition state */
+		pltrst_transition = true;
 		break;
 	case RESET_CNT:
 		ap_boot_time.cnt = 0;
+
+		if (ap_booted)
+			pltrst_transition = false;
+
+		ap_booted = false;
 		break;
 	default:
 		break;
