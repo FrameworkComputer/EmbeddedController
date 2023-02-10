8ca233c5c35e956cb9a9a1ea47de81a427582a27
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2023-02-10 11:29:12 +0800

    CPS8100: create header file
    This CL created the header file and also add the driver declaration.
    
    BUG=b:268581418
    BRANCH=none
    TEST=make buildall
    
    Change-Id: I08df700f4aff4c5cbe5ccd1345652263feab36f3
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4238095
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ driver/wpc/cps8100.h
@@ -0,0 +1,15 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* cps8100 Qi wireless power transmitter module for Chrome EC */
+
+#ifndef __CROS_EC_WPC_CPS8100_H
+#define __CROS_EC_WPC_CPS8100_H
+
+#include "peripheral_charger.h"
+
+extern struct pchg_drv cps8100_drv;
+
+#endif /* __CROS_EC_WPC_CPS8100_H */
