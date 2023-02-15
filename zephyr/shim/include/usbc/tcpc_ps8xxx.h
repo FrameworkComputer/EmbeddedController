0f0f0b289bc9f47c7a19db9693a77e754080af14
Peter Marheine <pmarheine@chromium.org>
2023-02-15 12:26:24 +1100

    zephyr/ps8xxx: fix build with TCPC_INTERRUPT disabled
    The irq_gpio field only exists if CONFIG_PLATFORM_EC_TCPC_INTERRUPT is
    enabled, otherwise the equivalent is alert_signal.
    
    BUG=b:269212593,b:254148652
    TEST=zmake build yaviks with CONFIG_PLATFORM_EC_TCPC_INTERRUPT=n
    BRANCH=nissa
    
    Change-Id: Ib2219e39eb81dcdb9f780b46a263b85adfbb6b15
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4252925
    Reviewed-by: zhi cheng yuan <jasonyuan@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- zephyr/shim/include/usbc/tcpc_ps8xxx.h
+++ zephyr/shim/include/usbc/tcpc_ps8xxx.h
@@ -19,7 +19,13 @@
 		},                                                             \
 		.drv = &ps8xxx_tcpm_drv,                                       \
 		.flags = DT_PROP(id, tcpc_flags),                              \
-		.irq_gpio = GPIO_DT_SPEC_GET_OR(id, irq_gpios, {}),            \
+		COND_CODE_1(CONFIG_PLATFORM_EC_TCPC_INTERRUPT,                 \
+			(.irq_gpio = GPIO_DT_SPEC_GET_OR(id, irq_gpios, {})),  \
+			(.alert_signal = COND_CODE_1(                          \
+				DT_NODE_HAS_PROP(id, int_pin),                 \
+				(GPIO_SIGNAL(DT_PHANDLE(id, int_pin))),        \
+				(GPIO_LIMIT)))                                 \
+		),                                                             \
 	},
 
 DT_FOREACH_STATUS_OKAY(PS8XXX_COMPAT, TCPC_VERIFY_NO_FLAGS_ACTIVE_ALERT_HIGH)
