1fbdcd9f057bc34d9b15b6a0d5eafee19df33152
Derek Huang <derekhuang@google.com>
2023-02-20 02:55:19 +0000

    zephyr: forward cros_crc16 to zephyr crc16 implementation
    Shim in support for crc16 used in CPS8100 drive within platform/ec.
    Add test for platform/ec and zephyr based CRC16 approaches.
    
    BUG=b:195708351
    BRANCH=none
    TEST=zmake build -a; ./twister -T zephyr/test/crc/
    
    Change-Id: I52e2c5d6e4568eae5c42ca0b68b6d27731b69f06
    Signed-off-by: Derek Huang <derekhuang@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4269953
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/shim/src/crc.c
+++ zephyr/shim/src/crc.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "crc.h"
 #include "crc8.h"
 
 #include <zephyr/sys/crc.h>
@@ -18,4 +19,9 @@ inline uint8_t cros_crc8(const uint8_t *data, int len)
 uint8_t cros_crc8_arg(const uint8_t *data, int len, uint8_t previous_crc)
 {
 	return crc8(data, len, SMBUS_POLYNOMIAL, previous_crc, false);
+}
+
+uint16_t cros_crc16(const uint8_t *data, int len, uint16_t previous_crc)
+{
+	return crc16_itu_t(previous_crc, data, len);
 }
--- zephyr/test/crc/main.c
+++ zephyr/test/crc/main.c
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-
+#include "crc.h"
 #include "crc8.h"
 
 #include <zephyr/ztest_assert.h>
@@ -18,4 +18,17 @@ ZTEST(crc_driver, test_crc8_known_data)
 
 	/* Verifies polynomial values of 0x07 representing x^8 + x^2 + x + 1 */
 	zassert_equal(crc, 170, "CRC8 hash did not match");
+}
+
+ZTEST(crc_driver, test_crc16_known_data)
+{
+	uint8_t buffer[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 8 };
+
+	int crc = cros_crc16(buffer, 10, 0);
+
+	/*
+	 * Verifies polynomial values of 0x1021 representing X^16 + X^15 + X^2 +
+	 * 1
+	 */
+	zassert_equal(crc, 60681, "CRC16 hash did not match");
 }
