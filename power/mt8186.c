2e8a4cfaae3a5bce52cf3a84ad4541affec37d35
Eric Yilun Lin <yllin@chromium.org>
2023-02-24 17:29:58 +0800

    mt8188: add power signal PMIC_EC_RESETB
    Add PMIC_EC_RESETB signal, which is used by PMIC to reset the AP.
    After the PMIC hard off triggered, we should wait for the PMIC reset
    the AP then we can safely remove the PMIC rail EN_PP4200_S5.
    
    BUG=b:267268982
    TEST=scope the power signals changing is expected when
         1. PMIC hard off
         2. powerkey forcing shutdown
         3. AP issue poweroff
    BRANCH=none
    
    Change-Id: I945d9f4ebbf6d53288c6503df7254ce448073565
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4290680
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

--- power/mt8186.c
+++ power/mt8186.c
@@ -47,6 +47,7 @@
 #define IN_SUSPEND_ASSERTED POWER_SIGNAL_MASK(AP_IN_S3)
 #define IN_AP_RST POWER_SIGNAL_MASK(AP_IN_RST)
 #define IN_PG_PP4200_S5 POWER_SIGNAL_MASK(PG_PP4200_S5)
+#define IN_PMIC_AP_RST POWER_SIGNAL_MASK(PMIC_AP_RST)
 
 /* Long power key press to force shutdown in S0. go/crosdebug */
 #define FORCED_SHUTDOWN_DELAY (8 * SECOND)
@@ -59,6 +60,8 @@
 #define PMIC_EN_PULSE_MS 50
 /* PMIC hard off delay with 20% tolerance. */
 #define PMIC_HARD_OFF_DELAY (8 * SECOND / 100 * 120)
+/* Timeout for PMIC resetting AP after hard off. */
+#define PMIC_AP_RESET_TIMEOUT (1 * SECOND)
 
 /* 30 ms for hard reset, we hold it longer to prevent TPM false alarm. */
 #define SYS_RST_PULSE_LENGTH (50 * MSEC)
@@ -327,6 +330,7 @@ enum power_state power_handle_state(enum power_state state)
 
 	case POWER_G3S5:
 #if DT_NODE_EXISTS(DT_NODELABEL(en_pp4200_s5))
+		power_signal_enable_interrupt(GPIO_PMIC_EC_RESETB);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(en_pp4200_s5), 1);
 		if (power_wait_mask_signals_timeout(IN_PG_PP4200_S5,
 						    IN_PG_PP4200_S5,
@@ -429,7 +433,12 @@ enum power_state power_handle_state(enum power_state state)
 
 	case POWER_S5G3:
 #if DT_NODE_EXISTS(DT_NODELABEL(en_pp4200_s5))
+		if (power_wait_mask_signals_timeout(IN_PMIC_AP_RST,
+						    IN_PMIC_AP_RST,
+						    PMIC_AP_RESET_TIMEOUT))
+			CPRINTS("PMIC reset AP timeout. Forcing PMIC off");
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(en_pp4200_s5), 0);
+		power_signal_disable_interrupt(GPIO_PMIC_EC_RESETB);
 #endif
 		return POWER_G3;
 	default:
