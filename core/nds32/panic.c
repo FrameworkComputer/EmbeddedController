255c4a92012467ce40ab15389ef0a0f3b516f97c
Rob Barnes <robbarnes@google.com>
2023-03-14 10:30:00 -0600

    nds32: Support for system safe mode
    Add support for entering system safe mode on nds32 based EC. This is
    accomplished by returning from exception handler with iret.
    
    BUG=b:269606355
    BRANCH=dedede
    TEST=Pass system safe mode tast test (with modified Kernel and AP).
    
    Change-Id: Iac87b5ebd55ce0224b277257e137dffb7d764263
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4334213
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- core/nds32/panic.c
+++ core/nds32/panic.c
@@ -10,6 +10,7 @@
 #include "printf.h"
 #include "software_panic.h"
 #include "system.h"
+#include "system_safe_mode.h"
 #include "task.h"
 #include "timer.h"
 #include "util.h"
@@ -190,6 +191,20 @@ void report_panic(uint32_t *regs, uint32_t itype)
 	pdata->nds_n8.ipsw = regs[17];
 
 	print_panic_information(regs, itype, regs[16], regs[17]);
+
+	if (IS_ENABLED(CONFIG_SYSTEM_SAFE_MODE)) {
+		if (start_system_safe_mode() == EC_SUCCESS) {
+			pdata->flags |= PANIC_DATA_FLAG_SAFE_MODE_STARTED;
+			/* Current task has been disabled.
+			 * Returning from the exception here should cause the
+			 * highest priority task that wasn't disabled to run.
+			 */
+			asm("iret");
+			__builtin_unreachable();
+		}
+		pdata->flags |= PANIC_DATA_FLAG_SAFE_MODE_FAIL_PRECONDITIONS;
+	}
+
 	panic_reboot();
 }
 
