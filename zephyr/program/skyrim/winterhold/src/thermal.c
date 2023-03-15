579c5e199bdd21c85097d70f9ab5c76c800bce60
Leila Lin <leilacy_lin@compal.corp-partner.google.com>
2023-03-15 15:18:18 +0800

    winterhold: Fine-tune thermal config
    Fine-tune thermal config for winterhold project.
    
    LOW_COVERAGE_REASON=no unit tests for skyrim yet, b/247151116
    BRANCH=none
    BUG=b:255732503
    TEST=verify the thermal config on winterhold is correct
    
    Change-Id: Ieccf5b2ffbd664f73bc14738e846ded58fa9e6c2
    Signed-off-by: Leila Lin <leilacy_lin@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4338762
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Tested-by: LeilaCY Lin <leilacy_lin@compal.corp-partner.google.com.test-google-a.com>
    Commit-Queue: Isaac Lee <isaaclee@google.com>
    Reviewed-by: Elthan Huang <elthan_huang@compal.corp-partner.google.com>

--- zephyr/program/skyrim/winterhold/src/thermal.c
+++ zephyr/program/skyrim/winterhold/src/thermal.c
@@ -26,13 +26,13 @@
 	{                        \
 		.temp_host = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(43), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(39), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		}, \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_desktop_lid_open =
@@ -45,13 +45,13 @@ __maybe_unused static const struct ec_thermal_config thermal_desktop_lid_open =
 	{                         \
 		.temp_host = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(43), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(39), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		},  \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_desktop_lid_close =
@@ -64,13 +64,13 @@ __maybe_unused static const struct ec_thermal_config thermal_desktop_lid_close =
 	{                        \
 		.temp_host = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(42), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
 			[EC_TEMP_THRESH_WARN] = C_TO_K(38), \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		}, \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_laptop =
@@ -82,12 +82,12 @@ __maybe_unused static const struct ec_thermal_config thermal_laptop =
 #define FAN_SOC_DESKTOP_LID_OPEN \
 	{                        \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		}, \
 		.temp_fan_off = C_TO_K(55), \
 		.temp_fan_max = C_TO_K(72), \
@@ -101,12 +101,12 @@ __maybe_unused static const struct ec_thermal_config fan_soc_desktop_lid_open =
 #define FAN_SOC_DESKTOP_LID_CLOSE \
 	{                         \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		}, \
 		.temp_fan_off = C_TO_K(55), \
 		.temp_fan_max = C_TO_K(72),  \
@@ -120,12 +120,12 @@ __maybe_unused static const struct ec_thermal_config fan_soc_desktop_lid_close =
 #define FAN_SOC_LAPTOP           \
 	{                        \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(97), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(98), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(87), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(88), \
 		}, \
 		.temp_fan_off = C_TO_K(51), \
 		.temp_fan_max = C_TO_K(68), \
@@ -158,7 +158,7 @@ __maybe_unused static const struct ec_thermal_config fan_cpu_desktop_lid_close =
  */
 #define FAN_CPU_LAPTOP                                                  \
 	{                                                               \
-		.temp_fan_off = C_TO_K(76), .temp_fan_max = C_TO_K(82), \
+		.temp_fan_off = C_TO_K(72), .temp_fan_max = C_TO_K(78), \
 	}
 __maybe_unused static const struct ec_thermal_config fan_cpu_laptop =
 	FAN_CPU_LAPTOP;
