0f16592f7f7e1313236738eae10a9de62015a204
Yuval Peress <peress@google.com>
2023-03-14 21:57:45 -0600

    rex: add rex-sans-sensors variant
    Add a new variant on top of Rex that disables the motionsense task,
    sensor configs, and respective DTS nodes.
    
    BRANCH=none
    BUG=b:272529568,b:273308069
    TEST=zmake build rex rex-sans-sensors
    
    Change-Id: I4df6cbc3e5e92b32232d24d8ce1d0f8a53dd7da6
    Signed-off-by: Yuval Peress <peress@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4339257
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Li1 Feng <li1.feng@intel.com>

--- zephyr/program/rex/BUILD.py
+++ zephyr/program/rex/BUILD.py
@@ -7,23 +7,39 @@
 
 def register_variant(
     project_name,
+    kconfig_files=None,
 ):
     """Register a variant of Rex."""
+    if kconfig_files is None:
+        kconfig_files = [
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+        ]
+
     register_npcx_project(
         project_name=project_name,
         zephyr_board="npcx9m7f",
         dts_overlays=[
             here / project_name / "project.overlay",
         ],
-        kconfig_files=[
-            # Common to all projects.
-            here / "program.conf",
-            # Project-specific KConfig customization.
-            here / project_name / "project.conf",
-        ],
+        kconfig_files=kconfig_files,
     )
 
 
 register_variant(
     project_name="rex",
+)
+
+register_variant(
+    project_name="rex-sans-sensors",
+    kconfig_files=[
+        # Common to all projects.
+        here / "program.conf",
+        # Parent project's config
+        here / "rex" / "project.conf",
+        # Project-specific KConfig customization.
+        here / "rex-sans-sensors" / "project.conf",
+    ],
 )
