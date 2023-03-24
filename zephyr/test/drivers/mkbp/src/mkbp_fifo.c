853d0b00bad965342334e28b5182cba83f538d13
Ting Shen <phoenixshen@google.com>
2023-03-24 14:45:29 +0800

    mkbp_fifo: fix buffer overflow bug
    The element type we used in mkbp fifo (ec_response_get_next_event,
    13 bytes) is smaller than the maximum data we put into the queue
    (KEYBOARD_COLS_WITH_KEYPAD, 15 bytes). Change the element type to
    ec_response_get_next_event_v1 to fix this bug.
    
    Also added a compile time check and updated the unittest to detect
    similar bugs in the future.
    
    BUG=none
    TEST=pass the updated drivers.mkbp test
    BRANCH=none
    
    Change-Id: Ifb6d89a81cbbb1bbcf9fa20164b6b133bad3d0a2
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4369577
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>

--- zephyr/test/drivers/mkbp/src/mkbp_fifo.c
+++ zephyr/test/drivers/mkbp/src/mkbp_fifo.c
@@ -64,7 +64,12 @@ ZTEST_F(mkbp_fifo, test_fifo_add_keyboard_key_matrix_event)
 	fill_array_with_incrementing_numbers(fixture->input_event_data,
 					     KEY_MATRIX_EVENT_DATA_SIZE);
 
-	/* Keyboard Key Matrix Event */
+	/* Keyboard Key Matrix Event
+	 * Push twice to verify their buffers do not overlap.
+	 */
+	zassert_ok(mkbp_fifo_add(EC_MKBP_EVENT_KEY_MATRIX,
+				 fixture->input_event_data),
+		   NULL);
 	zassert_ok(mkbp_fifo_add(EC_MKBP_EVENT_KEY_MATRIX,
 				 fixture->input_event_data),
 		   NULL);
