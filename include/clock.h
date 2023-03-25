404836eff4ac0f0251e3fbd0b1a2b477fb6543ca
Yi Chou <yich@google.com>
2023-03-25 19:40:20 +0800

    clock: Add C++ include support
    Add 'extern "C"' when building with C++, this would prevent some linker
    errors in the future.
    
    BUG=b:248508087
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Change-Id: I500d4543b003bc02e278d9e1e7d3c160f90950e1
    Signed-off-by: Yi Chou <yich@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4371820
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- include/clock.h
+++ include/clock.h
@@ -10,6 +10,10 @@
 
 #include "common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * Set the CPU clocks and PLLs.
  */
@@ -107,5 +111,9 @@ void clock_disable_peripheral(uint32_t offset, uint32_t mask, uint32_t mode);
  * Notify the clock module that the UART for the console is in use.
  */
 void clock_refresh_console_in_use(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __CROS_EC_CLOCK_H */
