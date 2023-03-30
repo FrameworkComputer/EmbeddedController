4f561a604d77fb53fb6eb1865627d265ff742584
Keith Short <keithshort@chromium.org>
2023-03-30 15:07:56 -0600

    zephyr: add checks for driver GPIOs
    The named-gpios node is used to configure the initial state of all
    GPIOs. However, as drivers are moved upstream the GPIO configuration
    will be done by the driver.
    
    Add a configure time check that validates if a GPIO is specified in
    named-gpios and as a property to a driver, that the flags match. This
    will help prevent regressions in the future.
    
    BUG=b:271135255
    BRANCH=none
    TEST=zmake configure -a
    TEST=./twister -b
    
    Change-Id: I45aa14bf93e74877082a2a674b391cfab56379e4
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4387260
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/scripts/named_gpios.py
+++ zephyr/scripts/named_gpios.py
@@ -9,6 +9,7 @@ import inspect
 import logging
 from pathlib import Path
 import pickle
+import re
 import site
 import sys
 from typing import List, Optional
@@ -45,7 +46,9 @@ def _load_edt(zephyr_base, edt_pickle):
         # UnpicklingErrors will raise an exception and fail the build.
         return None, None, None
 
-    if "twister-out" in edt_pickle.parts:
+    is_test = re.compile(r"twister-out")
+
+    if is_test.search(edt_pickle.as_posix()):
         # For tests built with twister, the edt.pickle file is located in a
         # path ending <test_name>/zephyr/.
         project_name = edt_pickle.parents[1].name
@@ -57,6 +60,135 @@ def _load_edt(zephyr_base, edt_pickle):
     edtlib = inspect.getmodule(edt)
 
     return edtlib, edt, project_name
+
+
+def _detect_gpios_mismatches(node_name, prop_name, prop_gpios, board_gpios):
+    """Verify that all GPIO entries in a -gpios style property match the flags
+    specified in the named-gpios node.
+
+    Args:
+        node_name   - The name of the node containing the -gpios style property.
+        prop_name   - The name of the -gpios style property.
+        prop_gpios  - An edtlib.ControllerAndData class object containing
+                      the GPIO tuples found in the property.
+        board_gpios - A dictionary that maps gpio port/pin tuples to the GPIO
+                      flags. This dictionary is initialized from the children
+                      found in the named-gpios devicetree node.
+
+    Returns:
+        A tuple indicating how many GPIOs were checked, and how many GPIOs
+        had a mismatch.
+    """
+    errors = 0
+    count = 0
+
+    # The -gpios property may be an array of GPIO tuples
+    for gpio in prop_gpios:
+        count += 1
+        gpio_pin = gpio.data["pin"]
+
+        # The "flags" cell should be only 16-bits
+        dt_flags = gpio.data["flags"] & 0xFFFF
+
+        if gpio.controller.labels[0] is not None:
+            nodelabel = gpio.controller.labels[0]
+        else:
+            nodelabel = gpio.controller.name
+
+        if (nodelabel, gpio_pin) not in board_gpios.keys():
+            # GPIO not specified in named-gpios
+            logging.debug(
+                "Warning: property %s/%s = <%s %s 0x%x> not found in named-gpios",
+                node_name,
+                prop_name,
+                nodelabel,
+                gpio_pin,
+                dt_flags,
+            )
+            continue
+
+        if dt_flags != board_gpios[(nodelabel, gpio_pin)]:
+            errors += 1
+            logging.error(
+                "ERROR: property %s/%s = <%s %s 0x%x>. Flags don't match named-gpios 0x%x",
+                node_name,
+                prop_name,
+                nodelabel,
+                gpio_pin,
+                dt_flags,
+                board_gpios[(nodelabel, gpio_pin)],
+            )
+
+    return count, errors
+
+
+def verify_gpios_flags_match(edtlib, edt, project_name):
+    """Check that GPIO flags used across devices matches.
+
+    Until all drivers are upstream, the Zephyr EC devicetrees specify GPIOs
+    in multiple locations: the "named-gpios" node performs the configuration
+    of all GPIOs on the board, and individual drivers may also configure GPIOs.
+
+    This routine finds all "*-gpios" style properties in the devicetree and
+    cross-checks the flags set by "named-gpios" and ensures they match.
+
+    Args:
+        edtlib: Module object for the edtlib library.
+        edt: EDT object representation of a devicetree
+        project_name: A string containing the project/test name
+
+    Returns:
+        True if no GPIO flag mismatches found.  Returns False otherwise.
+    """
+    try:
+        named_gpios = edt.get_node("/named-gpios")
+    except edtlib.EDTError:
+        # If the named-gpios node doesn't exist, return success.
+        return True
+
+    # Dictionary using the gpio,pin tuple as the key. Value set to the flags
+    board_gpios = dict()
+    for node in named_gpios.children.values():
+        if "gpios" not in node.props:
+            continue
+
+        gpios = node.props["gpios"].val
+
+        # edtlib converts a "-gpios" style property to a list of of
+        # ControllerAndData objects.  However, the named-gpios node only
+        # supports a single GPIO per child, so no need to iterate over
+        # the list.
+        gpio = gpios[0]
+        gpio_pin = gpio.data["pin"]
+
+        if gpio.controller.labels[0] is not None:
+            nodelabel = gpio.controller.labels[0]
+        else:
+            nodelabel = gpio.controller.name
+
+        # The named-gpios stores a 32-bit flags, but Zephyr GPIO flags
+        # are limited to the lower 16-bits
+        board_gpios[(nodelabel, gpio_pin)] = gpio.data["flags"] & 0xFFFF
+
+    errors = 0
+    count = 0
+    for node in edt.nodes:
+        for prop_name in node.props.keys():
+            if prop_name.endswith("-gpios"):
+                gpios = node.props[prop_name].val
+
+                prop_gpio_count, prop_gpio_errors = _detect_gpios_mismatches(
+                    node.name, prop_name, gpios, board_gpios
+                )
+                count += prop_gpio_count
+                errors += prop_gpio_errors
+
+    if errors:
+        logging.error("%d GPIO mismatches found in %s.", errors, project_name)
+        return False
+
+    logging.info("Verified %d '*-gpios' properties, all flags match", count)
+    return True
 
 
 def verify_no_duplicates(edtlib, edt, project_name):
@@ -193,6 +325,9 @@ def main(argv: Optional[List[str]] = None) -> Optional[int]:
         return 0
 
     if not verify_no_duplicates(edtlib, edt, project_name):
+        return 1
+
+    if not verify_gpios_flags_match(edtlib, edt, project_name):
         return 1
 
     return 0
