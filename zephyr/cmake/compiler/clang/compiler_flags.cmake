cc77132214314039ac2c14db1e73b8859d26e690
Keith Short <keithshort@chromium.org>
2023-04-19 16:17:47 -0600

    zephyr: Honor warnings as errors config for tests
    The clang compiler flags were unconditionally enabling errors for
    several warnings classes. Only enable these errors when the
    CONFIG_COMPILER_WARNINGS_AS_ERRORS option is enabled.
    
    BUG=none
    BRANCH=none
    TEST=./twister -W
    
    Change-Id: I2530a8af9aa51e12b5ae6d0e3d68b4963ecf82a1
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4451651
    Reviewed-by: Yuval Peress <peress@google.com>

--- zephyr/cmake/compiler/clang/compiler_flags.cmake
+++ zephyr/cmake/compiler/clang/compiler_flags.cmake
@@ -9,18 +9,20 @@ set_compiler_property(PROPERTY hosted)
 
 # Disable position independent code.
 if ("${ARCH}" STREQUAL "posix" OR "${ARCH}" STREQUAL "unit_testing")
-	add_compile_options(-fPIC)
+  add_compile_options(-fPIC)
 else()
-	add_compile_options(-fno-PIC)
+  add_compile_options(-fno-PIC)
 endif()
 
 # When testing, look for stack smashing
 add_compile_option_ifdef(CONFIG_ZTEST -fstack-protector-all)
 
-check_set_compiler_property(APPEND PROPERTY warning_extended -Wunused-variable
-	-Werror=unused-variable -Werror=missing-braces
-	-Werror=sometimes-uninitialized -Werror=unused-function
-	-Werror=array-bounds)
+if(DEFINED CONFIG_COMPILER_WARNINGS_AS_ERRORS)
+  check_set_compiler_property(APPEND PROPERTY warning_extended -Wunused-variable
+    -Werror=unused-variable -Werror=missing-braces
+    -Werror=sometimes-uninitialized -Werror=unused-function
+    -Werror=array-bounds)
+endif()
 
 # clang flags for coverage generation
 set_property(TARGET compiler PROPERTY coverage --coverage -fno-inline)
