ae77e271cec97465733805c55551c3e9fe56e63f
Jeremy Bettis <jbettis@google.com>
2023-05-01 21:48:32 -0600

    ec: Replace presubmit_check.sh
    Replace presubmit_check.sh and zephyr_check_testcase_yaml.py with
    python scripts which handle PRESUBMIT_COMMIT, PRESUBMIT_FILES, and also
    work outside of the chroot.
    
    BUG=None
    TEST=on various paths
    
    Change-Id: If7ff5e5d238a5846c19894bb288e96052f99aae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4497260
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>

---
+++ util/check_cprints.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env vpython3
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Checks files for invalid CPRINTS calls."""
+
+import re
+import sys
+from typing import List, Optional
+
+import preupload.lib
+
+
+CPRINTS_RE = re.compile(r'(CPRINTS|cprints)[^"]*"[^"]*\\n"')
+
+
+def main(argv: Optional[List[str]] = None) -> Optional[int]:
+    """Look at all files passed in on commandline for invalid CPRINTS calls."""
+    return_code = 0
+    parser = preupload.lib.argument_parser()
+
+    args = parser.parse_args(argv)
+    for filename in args.filename:
+        lines = preupload.lib.cat_file(args, filename).splitlines()
+        for linenum, line in enumerate(lines, start=1):
+            if CPRINTS_RE.search(line):
+                print(
+                    "error: CPRINTS strings should not include newline "
+                    f"characters\n{filename}:{linenum}: {line}",
+                    file=sys.stderr,
+                )
+                return_code = 1
+
+    return return_code
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
--- util/zephyr_check_testcase_yaml.py
+++ util/zephyr_check_testcase_yaml.py
@@ -13,99 +13,53 @@
 # >
 # [VPYTHON:END]
 
-import argparse
-import glob
-from pathlib import Path
 import sys
-from typing import List
+from typing import List, Optional
 
-from twister_launcher import EC_TEST_PATHS
+import preupload.lib
 import yaml  # pylint: disable=import-error
 
 
-def check_extra_args(filepath: Path):
-    """Check if extra_args references blocked fields"""
-    errors = []
-
-    blocked_fields = {
-        "CONF_FILE": "extra_conf_files",
-        "OVERLAY_CONFIG": "extra_overlay_confs",
-        "DTC_OVERLAY_FILE": "extra_dtc_overlay_files",
-    }
-
-    with open(filepath, "r") as file:
-        data = yaml.load(file, Loader=yaml.SafeLoader)
-
-    def scan_section(section):
-        if "extra_args" in section:
-            for field in blocked_fields:
-                if field in section["extra_args"]:
-                    errors.append(
-                        f" * Don't specify {field} in `extra_args`. "
-                        f"Use `{blocked_fields[field]}` ({filepath})"
-                    )
-
-    if "common" in data:
-        scan_section(data["common"])
-    if "tests" in data:
-        for section in data["tests"]:
-            scan_section(data["tests"][section])
-
-    return errors
+BLOCKED_FIELDS = {
+    "CONF_FILE": "extra_conf_files",
+    "OVERLAY_CONFIG": "extra_overlay_confs",
+    "DTC_OVERLAY_FILE": "extra_dtc_overlay_files",
+}
 
 
-def validate_files(files: List[Path]) -> List[str]:
-    """Run checks on a list of file paths."""
-    errors = []
+def main(argv: Optional[List[str]] = None) -> Optional[int]:
+    """Look at all testcase.yaml files passed in on commandline for invalid fields."""
+    return_code = 0
+    parser = preupload.lib.argument_parser()
 
-    checkers = [check_extra_args]
+    args = parser.parse_args(argv)
+    for filename in args.filename:
+        if filename.name == "/testcase.yaml":
+            lines = preupload.lib.cat_file(args, filename)
+            data = yaml.load(lines, Loader=yaml.SafeLoader)
 
-    for file in files:
-        if not file.name == "testcase.yaml":
+            def scan_section(section, filename):
+                nonlocal return_code
+                if "extra_args" in section:
+                    for field, replacement in BLOCKED_FIELDS.items():
+                        if field in section["extra_args"]:
+                            print(
+                                f"error: Don't specify {field} in "
+                                f"`extra_args`. Use `{replacement}`: "
+                                f"{filename}",
+                                file=sys.stderr,
+                            )
+                            return_code = 1
+
+            if "common" in data:
+                scan_section(data["common"], filename)
+            if "tests" in data:
+                for section in data["tests"]:
+                    scan_section(data["tests"][section], filename)
             continue
-        for check in checkers:
-            errors.extend(check(file))
 
-    return errors
+    return return_code
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "files",
-        nargs="*",
-        type=Path,
-        help="List of files to validate. If blank, scan entire EC repo. "
-        "If '-', read a newline-separated list of files from stdin",
-    )
-
-    args = parser.parse_args()
-
-    if len(args.files) == 0:
-        ec_dir = Path(__file__).resolve().parent.parent
-        file_list = []
-
-        for p in EC_TEST_PATHS:
-            file_list.extend(
-                [
-                    Path(f)
-                    for f in glob.glob(
-                        str(ec_dir / p / "**/testcase.yaml"),
-                    )
-                ]
-            )
-
-    elif args.files[0] == Path("-"):
-        # Read from stdin
-        file_list = [Path(line.strip()) for line in sys.stdin.readlines()]
-        file_list.extend(args.files[1:])
-    else:
-        file_list = args.files
-
-    all_errors = validate_files(file_list)
-
-    if all_errors:
-        for error in all_errors:
-            sys.stderr.write(error)
-            sys.stderr.write("\n")
-        sys.exit(1)
+    sys.exit(main(sys.argv[1:]))
