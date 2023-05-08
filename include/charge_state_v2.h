5cc8ef46cfbca7aeff4023df27ebd4af02ea81d2
Simon Glass <sjg@chromium.org>
2023-05-08 15:52:14 -0600

    charger: Delete charge_state_v2.h
    This is not used any more. Delete it.
    
    This makes no functional change.
    
    BUG=b:218332694
    TEST=make buildall
    
    Change-Id: I982d9ac1e593232518850ad061480cd9746cfcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4516192
    Tested-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Commit-Queue: Eric Peers <epeers@google.com>

--- include/charge_state_v2.h
+++
@@ -1,9 +0,0 @@
-/* Copyright 2014 The ChromiumOS Authors
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-#ifndef __CROS_EC_CHARGE_STATE_V2_H
-#define __CROS_EC_CHARGE_STATE_V2_H
-
-#endif /* __CROS_EC_CHARGE_STATE_V2_H */
