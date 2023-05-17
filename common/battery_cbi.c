b6217a55670fb6f08ba0e0f93f27d6afc3b41f4c
Daisuke Nojiri <dnojiri@chromium.org>
2023-05-17 14:59:03 -0700

    Battery: Add console command
    This CL adds a console command for debugging battery-info-in-CBI.
    
    > biic reset
    > cbi set 26 18000 4
    > biic read
    > biic dump
    
    BUG=b:283131235
    TEST=Use cbi and biic command on Agah.
    
    Change-Id: I98319f1e811c791e51841f5e6a1c58fedbd820c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4545454
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Auto-Submit: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- common/battery_cbi.c
+++ common/battery_cbi.c
@@ -20,6 +20,7 @@
 #include "console.h"
 #include "cros_board_info.h"
 #include "hooks.h"
+#include "util.h"
 
 #define CPRINTF(format, args...) cprintf(CC_SYSTEM, "CBI " format, ##args)
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, "CBI " format, ##args)
@@ -172,3 +173,104 @@ test_export_static void batt_cbi_main(void)
 	CPRINTS("%s done", __func__);
 }
 DECLARE_HOOK(HOOK_INIT, batt_cbi_main, HOOK_PRIO_DEFAULT);
+
+#ifdef CONFIG_CMD_BATTERY_INFO_IN_CBI
+static struct board_batt_params scratch_battery_info;
+
+static void batt_cbi_dump(const struct board_batt_params *info)
+{
+	const struct fuel_gauge_info *fg = &info->fuel_gauge;
+	const struct ship_mode_info *ship = &info->fuel_gauge.ship_mode;
+	const struct sleep_mode_info *sleep = &info->fuel_gauge.sleep_mode;
+	const struct fet_info *fet = &info->fuel_gauge.fet;
+	const struct battery_info *batt = &info->batt_info;
+
+	ccprintf("   .fuel_gauge = {\n");
+
+	ccprintf("%02x:\t.manuf_name = \"%s\",\n",
+		 CBI_TAG_FUEL_GAUGE_MANUF_NAME, fg->manuf_name);
+	ccprintf("%02x:\t.device_name= \"%s\",\n",
+		 CBI_TAG_FUEL_GAUGE_DEVICE_NAME, fg->device_name);
+	ccprintf("%02x:\t.override_nil = %d,\n", CBI_TAG_FUEL_GAUGE_FLAGS,
+		 fg->override_nil & BIT(0));
+
+	ccprintf("   \t.ship_mode = {\n");
+	ccprintf("%02x:\t\t.reg_addr = 0x%02x,\n",
+		 CBI_TAG_BATT_SHIP_MODE_REG_ADDR, ship->reg_addr);
+	ccprintf("%02x:\t\t.reg_data = { 0x%04x, 0x%04x },\n",
+		 CBI_TAG_BATT_SHIP_MODE_REG_DATA, ship->reg_data[0],
+		 ship->reg_data[1]);
+	ccprintf("%02x:\t\t.wb_support = %d,\n", CBI_TAG_BATT_SHIP_MODE_FLAGS,
+		 ship->wb_support & BIT(0));
+	ccprintf("   \t},\n");
+
+	ccprintf("   \t.sleep_mode = {\n");
+	ccprintf("%02x:\t\t.reg_addr = 0x%02x,\n",
+		 CBI_TAG_BATT_SLEEP_MODE_REG_ADDR, sleep->reg_addr);
+	ccprintf("%02x:\t\t.reg_data = 0x%04x,\n",
+		 CBI_TAG_BATT_SLEEP_MODE_REG_DATA, sleep->reg_data);
+	ccprintf("%02x:\t\t.sleep_supported = %d,\n",
+		 CBI_TAG_BATT_SLEEP_MODE_FLAGS,
+		 sleep->sleep_supported & BIT(0));
+	ccprintf("   \t},\n");
+
+	ccprintf("   \t.fet = {\n");
+	ccprintf("%02x:\t\t.reg_addr = 0x%02x,\n", CBI_TAG_BATT_FET_REG_ADDR,
+		 fet->reg_addr);
+	ccprintf("%02x:\t\t.reg_mask = 0x%04x,\n", CBI_TAG_BATT_FET_REG_MASK,
+		 fet->reg_mask);
+	ccprintf("%02x:\t\t.disconnect_val = 0x%x,\n",
+		 CBI_TAG_BATT_FET_DISCONNECT_VAL, fet->disconnect_val);
+	ccprintf("%02x:\t\t.cfet_mask = 0x%04x,\n", CBI_TAG_BATT_FET_CFET_MASK,
+		 fet->cfet_mask);
+	ccprintf("%02x:\t\t.cfet_off_val = 0x%04x,\n",
+		 CBI_TAG_BATT_FET_CFET_OFF_VAL, fet->cfet_off_val);
+	ccprintf("%02x:\t\t.mfgacc_support = %d,\n", CBI_TAG_BATT_FET_FLAGS,
+		 fet->mfgacc_support & BIT(0));
+	ccprintf("   \t},\n");
+
+	ccprintf("   },\n"); /* end of fuel_gauge */
+
+	ccprintf("   .batt_info = {\n");
+	ccprintf("%02x:\t.voltage_max = %d,\n", CBI_TAG_BATT_VOLTAGE_MAX,
+		 batt->voltage_max);
+	ccprintf("%02x:\t.voltage_normal = %d,\n", CBI_TAG_BATT_VOLTAGE_NORMAL,
+		 batt->voltage_normal);
+	ccprintf("%02x:\t.voltage_min = %d,\n", CBI_TAG_BATT_VOLTAGE_MIN,
+		 batt->voltage_min);
+	ccprintf("%02x:\t.precharge_voltage= %d,\n",
+		 CBI_TAG_BATT_PRECHARGE_VOLTAGE, batt->precharge_voltage);
+	ccprintf("%02x:\t.precharge_current = %d,\n",
+		 CBI_TAG_BATT_PRECHARGE_CURRENT, batt->precharge_current);
+	ccprintf("%02x:\t.start_charging_min_c = %d,\n",
+		 CBI_TAG_BATT_START_CHARGING_MIN_C, batt->start_charging_min_c);
+	ccprintf("%02x:\t.start_charging_max_c = %d,\n",
+		 CBI_TAG_BATT_START_CHARGING_MAX_C, batt->start_charging_max_c);
+	ccprintf("%02x:\t.charging_min_c = %d,\n", CBI_TAG_BATT_CHARGING_MIN_C,
+		 batt->charging_min_c);
+	ccprintf("%02x:\t.charging_max_c = %d,\n", CBI_TAG_BATT_CHARGING_MAX_C,
+		 batt->charging_max_c);
+	ccprintf("%02x:\t.discharging_min_c = %d,\n",
+		 CBI_TAG_BATT_DISCHARGING_MIN_C, batt->discharging_min_c);
+	ccprintf("%02x:\t.discharging_max_c = %d,\n",
+		 CBI_TAG_BATT_DISCHARGING_MAX_C, batt->discharging_max_c);
+	ccprintf("   },\n"); /* end of batt_info */
+}
+
+static int cc_batt_cbi(int argc, const char *argv[])
+{
+	if (argc == 2 && strcasecmp(argv[1], "read") == 0) {
+		batt_cbi_read_fuel_gauge_info(&scratch_battery_info);
+		batt_cbi_read_battery_info(&scratch_battery_info);
+	} else if (argc == 2 && strcasecmp(argv[1], "dump") == 0) {
+		batt_cbi_dump(&scratch_battery_info);
+	} else if (argc == 2 && strcasecmp(argv[1], "reset") == 0) {
+		memcpy(&scratch_battery_info, &default_battery_info,
+		       sizeof(scratch_battery_info));
+	} else {
+		return EC_ERROR_PARAM_COUNT;
+	}
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(biic, cc_batt_cbi, "read | dump | reset", NULL);
+#endif /* CONFIG_CMD_BATTERY_INFO_IN_CBI */
