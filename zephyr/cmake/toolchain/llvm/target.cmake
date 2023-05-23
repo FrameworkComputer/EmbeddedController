dd7fec3381b7e78cce1d26dfbb358b2a9f1b79ed
Patryk Duda <pdk@semihalf.com>
2023-05-23 12:22:52 +0200

    toolchain/llvm: Add support for ARMv7 and ARMv6 toolchains
    Introduce support for armv7m-cros-eabi and arm-none-eabi LLVM toolchains
    in Zephyr EC. These toolchains are present in chroot.
    
    BUG=b:239712345
    TEST=zmake build bloonchipper (with 'llvm' toolchain)
    
    Change-Id: I87afec365dc5a60eb1540ec2ab3d53d2c1c0c7cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4556449
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Patryk Duda <patrykd@google.com>
    Commit-Queue: Patryk Duda <patrykd@google.com>

--- zephyr/cmake/toolchain/llvm/target.cmake
+++ zephyr/cmake/toolchain/llvm/target.cmake
@@ -12,5 +12,17 @@ set(CROSS_COMPILE_TARGET_unit_testing x86_64-pc-linux-gnu)
 
 set(CROSS_COMPILE_TARGET          ${CROSS_COMPILE_TARGET_${ARCH}})
 
+if("${ARCH}" STREQUAL "arm")
+  if(DEFINED CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
+    # ARMV7_M_ARMV8_M_MAINLINE means that ARMv7-M or backward compatible ARMv8-M
+    # processor is used.
+    set(CROSS_COMPILE_TARGET armv7m-cros-eabi)
+  elseif(DEFINED CONFIG_ARMV6_M_ARMV8_M_BASELINE)
+    # ARMV6_M_ARMV8_M_BASELINE means that ARMv6-M or ARMv8-M supporting the
+    # Baseline implementation processor is used.
+    set(CROSS_COMPILE_TARGET arm-none-eabi)
+  endif()
+endif()
+
 set(CC clang)
 set(CROSS_COMPILE "/usr/bin/${CROSS_COMPILE_TARGET}-")
