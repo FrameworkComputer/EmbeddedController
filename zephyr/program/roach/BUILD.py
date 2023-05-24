67be1ab5b3c717647d2c6d6eb3986aad0a2a0def
Ting Shen <phoenixshen@google.com>
2023-05-24 17:51:08 +0800

    roach: initial EC
    This CL adds basic configs and device tree settings for a new keyboard
    EC (codename: roach). With minimal code to make the EC compilable.
    
    BUG=b:269989432
    TEST=zmake build roach --clobber
         EC bootable on roach.
    LOW_COVERAGE_REASON=early bringup (b:287012522)
    
    Change-Id: I8dc87138c2754363c164d7ea091d10d8518f6d7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4269914
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>

---
+++ zephyr/program/roach/BUILD.py
@@ -0,0 +1,21 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for roach."""
+
+
+def register_variant(project_name):
+    """Register a variant of Roach."""
+    register_binman_project(
+        project_name=project_name,
+        zephyr_board="it82202ax-512",
+        dts_overlays=[here / project_name / "project.overlay"],
+        kconfig_files=[
+            here / "program.conf",
+            here / project_name / "project.conf",
+        ],
+    )
+
+
+register_variant("roach")
