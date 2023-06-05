cde62d8e818dfb22e96faa57d67099ff9d11461d
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2023-06-05 16:32:56 +0800

    yavilla: Pen power control
    Control pen power depend on pen detect INT when AP is on.
    
    BUG=b:283021231
    TEST=make sure pen power can be truned on/off correctly depend on
    pen detect INT when AP is on, and can be truned off when AP is off.
    
    Change-Id: I34a7e6e58d7ea9fdd4471dc97e71e4aaaaf2d6ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4583173
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Tested-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>

--- zephyr/program/nissa/yavilla/src/board.c
+++ zephyr/program/nissa/yavilla/src/board.c
@@ -7,6 +7,7 @@
 #include "gpio/gpio_int.h"
 #include "hooks.h"
 #include "motion_sense.h"
+#include "nissa_common.h"
 #include "tablet_mode.h"
 #include "task.h"
 
@@ -17,7 +18,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
+
+#include <ap_power/ap_power.h>
+
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
+
 __override uint8_t board_get_usb_pd_port_count(void)
 {
 	return 2;
@@ -63,3 +68,45 @@ static void board_init(void)
 		LOG_INF("Convertible!!!");
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_POST_I2C);
+
+void pen_detect_interrupt(enum gpio_signal s)
+{
+	int pen_detect =
+		!gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_pen_detect_odl));
+
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_pen_x),
+			pen_detect);
+}
+
+__override void board_power_change(struct ap_power_ev_callback *cb,
+				   struct ap_power_ev_data data)
+{
+	const struct gpio_dt_spec *const pen_power_gpio =
+		GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_pen_x);
+	const struct gpio_dt_spec *const pen_detect_gpio =
+		GPIO_DT_FROM_NODELABEL(gpio_pen_detect_odl);
+	const struct gpio_int_config *const pen_detect_int =
+		GPIO_INT_FROM_NODELABEL(int_pen_det_l);
+
+	switch (data.event) {
+	case AP_POWER_STARTUP:
+		/* Enable Pen Detect interrupt */
+		gpio_enable_dt_interrupt(pen_detect_int);
+		/*
+		 * Make sure pen detection is triggered or not when AP power on
+		 */
+		if (!gpio_pin_get_dt(pen_detect_gpio))
+			gpio_pin_set_dt(pen_power_gpio, 1);
+		break;
+	case AP_POWER_SHUTDOWN:
+		/*
+		 * Disable pen detect INT and turn off pen power when AP
+		 * shutdown
+		 */
+		gpio_disable_dt_interrupt(pen_detect_int);
+		gpio_pin_set_dt(pen_power_gpio, 0);
+		break;
+	default:
+		break;
+	}
+}
