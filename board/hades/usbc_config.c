0d4a34fa4bb4e93d29cab1ae872c618bf7e97253
Daisuke Nojiri <dnojiri@chromium.org>
2023-06-06 10:55:14 -0700

    Hades: Fix delay after resetting NCT3808
    This CL makes Hades' EC wait for a correct period after resetting
    NCT3808.
    
    BUG=b:282849589
    TEST=Hades
    
    Change-Id: I19c9855a1b5898dddcfd6f4fc31b66e961b5ebb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4608452
    Reviewed-by: Tarun Tuli <taruntuli@google.com>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Auto-Submit: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/hades/usbc_config.c
+++ board/hades/usbc_config.c
@@ -234,8 +234,8 @@ void board_reset_pd_mcu(void)
 	nct38xx_reset_notify(USBC_PORT_C0);
 
 	/* wait for chips to come up */
-	if (NCT3807_RESET_POST_DELAY_MS != 0)
-		msleep(NCT3807_RESET_POST_DELAY_MS);
+	if (NCT3808_RESET_POST_DELAY_MS != 0)
+		msleep(NCT3808_RESET_POST_DELAY_MS);
 }
 
 static void board_tcpc_init(void)
