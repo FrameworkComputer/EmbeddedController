a5171b272b59c859ed130160d5d77ea816b0511b
Tom Hughes <tomhughes@chromium.org>
2023-06-13 13:00:55 -0700

    cmake/toolchain-common.cmake: Fix passing of compile flags
    The previous method of passing flags wasn't actually resulting in the
    flags being set when building googletest.
    
    This also fixes the issue for boringssl where it wasn't passing
    the architecture-specific flags (-mcpu=cortex-m4 and -mfloat-abi=hard)
    resulting in the "Linking two modules of different target triples"
    warnings.
    
    Verified with a verbose build to observe the compile flags:
    make V=1 BOARD=bloonchipper
    
    BUG=b:254530679, b:287672777
    TEST=make V=1 BOARD=bloonchipper
    TEST=./test/run_device_tests.py --board helipilot => PASS
    
    Change-Id: Icea40af729081c8d68fda5769154dda3a057db9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4610318
    Reviewed-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- third_party/boringssl/boringssl-toolchain.cmake
+++ third_party/boringssl/boringssl-toolchain.cmake
@@ -10,12 +10,6 @@ include("${CROS_EC_REPO}/cmake/toolchain-common.cmake")
 # Specify our platform, which disables filesystem, threads, etc.
 add_definitions(-DCROS_EC)
 
-# TODO(b/287661706): This can be removed once https://crrev.com/c/4610318 lands.
-if (CMAKE_SYSTEM_PROCESSOR STREQUAL armv7)
-    add_compile_options(-mcpu=cortex-m4)
-    add_compile_options(-mfloat-abi=hard)
-endif ()
-
 # When compiling the code with the portage build system, it will generate very
 # long file path strings. This compile options will strip the source path, and
 # recude the final code size.
