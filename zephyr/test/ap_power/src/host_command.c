ec5b20369a6b8eeb107d832ab11ed818299c74f3
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-06-27 12:55:35 -0700

    ap_pwrseq: test: Dissect power signals definition
    This will divide power signals definition into corresponding chipset and
    board file.
    
    BUG=b:222933615
    TEST=./twister --coverage -T zephyr/test/ap_power
    
    Change-Id: I11378974f3db03947be655e9cf5d3601c2525e27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4650357
    Tested-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Commit-Queue: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/ap_power/src/host_command.c
+++ zephyr/test/ap_power/src/host_command.c
@@ -4,6 +4,7 @@
  */
 
 #include "ec_commands.h"
+#include "emul/emul_stub_device.h"
 #include "host_command.h"
 #include "test_state.h"
 
@@ -23,3 +24,7 @@ ZTEST(host_cmd, test_hibernate_get)
 }
 
 ZTEST_SUITE(host_cmd, ap_power_predicate_post_main, NULL, NULL, NULL, NULL);
+
+/* These 2 lines are needed because we don't define an espi host driver */
+#define DT_DRV_COMPAT zephyr_espi_emul_espi_host
+DT_INST_FOREACH_STATUS_OKAY(EMUL_STUB_DEVICE);
