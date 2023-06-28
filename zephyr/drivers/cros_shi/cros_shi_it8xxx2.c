aeb400f8b3e4f357b279218b21d2a86da44c3c7b
Dawid Niedzwiecki <dawidn@google.com>
2023-06-28 10:51:26 +0000

    zephyr: ite shi: move common code to separate file
    Move ite shi code common for the Host Command upstream and CrosEC.
    
    BUG=b:265450629
    TEST=zmake build -a
    LOW_COVERAGE_REASON=No new code, moved to another file. Most of the
    upstream code will be used b/265450629
    
    Change-Id: I096415062198f7a0d034ef5e90bdaead61e04748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4652195
    Tested-by: Dawid Niedzwiecki <dawidn@google.com>
    Commit-Queue: Dawid Niedzwiecki <dawidn@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/drivers/cros_shi/cros_shi_it8xxx2.c
+++ zephyr/drivers/cros_shi/cros_shi_it8xxx2.c
@@ -378,39 +378,6 @@ static int cros_shi_ite_init(const struct device *dev)
 	return 0;
 }
 
-static void shi_disable(void)
-{
-	/* Enable sleep mask of SHI to enter deep sleep of power plicy. */
-	enable_sleep(SLEEP_MASK_SPI);
-}
-
-static void shi_power_shutdown_handler(struct ap_power_ev_callback *cb,
-				       struct ap_power_ev_data data)
-{
-	switch (data.event) {
-	case AP_POWER_SHUTDOWN_COMPLETE:
-		/* Disable SHI bus */
-		shi_disable();
-		break;
-	default:
-		__ASSERT(false, "%s: unhandled event: %d", __func__,
-			 data.event);
-		break;
-	}
-}
-
-static void install_power_change_handler(void)
-{
-	static struct ap_power_ev_callback cb;
-
-	/* Add a callback of power shutdown complete to enable sleep mask. */
-	ap_power_ev_init_callback(&cb, shi_power_shutdown_handler,
-				  AP_POWER_SHUTDOWN_COMPLETE);
-	ap_power_ev_add_callback(&cb);
-}
-/* Call hook after chipset sets initial power state */
-DECLARE_HOOK(HOOK_INIT, install_power_change_handler, HOOK_PRIO_POST_CHIPSET);
-
 PINCTRL_DT_INST_DEFINE(0);
 
 static const struct cros_shi_it8xxx2_cfg cros_shi_cfg = {
@@ -423,21 +390,3 @@ static const struct cros_shi_it8xxx2_cfg cros_shi_cfg = {
 #endif
 DEVICE_DT_INST_DEFINE(0, cros_shi_ite_init, NULL, NULL, &cros_shi_cfg,
 		      POST_KERNEL, CONFIG_CROS_SHI_IT8XXX2_INIT_PRIORITY, NULL);
-
-/* Get protocol information */
-enum ec_status spi_get_protocol_info(struct host_cmd_handler_args *args)
-{
-	struct ec_response_get_protocol_info *r = args->response;
-
-	memset(r, 0, sizeof(*r));
-	r->protocol_versions = BIT(3);
-	r->max_request_packet_size = SPI_MAX_REQUEST_SIZE;
-	r->max_response_packet_size = SPI_MAX_RESPONSE_SIZE;
-	r->flags = EC_PROTOCOL_INFO_IN_PROGRESS_SUPPORTED;
-
-	args->response_size = sizeof(*r);
-
-	return EC_SUCCESS;
-}
-DECLARE_HOST_COMMAND(EC_CMD_GET_PROTOCOL_INFO, spi_get_protocol_info,
-		     EC_VER_MASK(0));
