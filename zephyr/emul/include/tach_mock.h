0def138298ad6c9ee0dc0e28b98b5c936a69631a
Diana Z <dzigterman@chromium.org>
2023-07-06 14:36:38 -0600

    Zephyr emul: Add tachometer mock
    Add a mock tachometer for use in the fan tests
    
    BUG=None
    TEST=CQ+1
    
    Change-Id: I249111d21a7df9d181c275977797dcb09e4742c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4669753
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Diana Z <dzigterman@chromium.org>
    Tested-by: Diana Z <dzigterman@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/emul/include/tach_mock.h
@@ -0,0 +1,11 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EMUL_INCLUDE_TACH_MOCK_H
+#define __EMUL_INCLUDE_TACH_MOCK_H
+
+#include <zephyr/device.h>
+
+#endif /*__EMUL_INCLUDE_TACH_MOCK_H */
