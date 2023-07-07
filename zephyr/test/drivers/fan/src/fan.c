405417533fb9d4314d25548b8a02dc38f85feeb5
Diana Z <dzigterman@chromium.org>
2023-07-07 09:32:41 -0600

    Zephyr test: Add fan cases for hysterisis and host commands
    Expand the common fan testing to include the fan table hysteresis and
    host commands.
    
    BUG=None
    TEST=CQ+1
    
    Change-Id: I8852f1c734a35d3fdd875d020e7aa3def7388f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4670862
    Reviewed-by: Yuval Peress <peress@google.com>
    Tested-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Diana Z <dzigterman@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/drivers/fan/src/fan.c
+++ zephyr/test/drivers/fan/src/fan.c
@@ -124,3 +124,195 @@ ZTEST(fan_common, test_fanduty_valid_1_arg)
 {
 	CHECK_CONSOLE_CMD("fanduty 80", "Setting fan", EC_SUCCESS);
 }
+
+static const int temp_fan_off = C_TO_K(20);
+static const int temp_fan_max = C_TO_K(40);
+static const struct fan_step_1_1 test_table[] = {
+	{ .decreasing_temp_ratio_threshold = TEMP_TO_RATIO(20),
+	  .increasing_temp_ratio_threshold = TEMP_TO_RATIO(34),
+	  .rpm = 1000 },
+	{ .decreasing_temp_ratio_threshold = TEMP_TO_RATIO(39),
+	  .increasing_temp_ratio_threshold = TEMP_TO_RATIO(40),
+	  .rpm = 2000 },
+};
+
+static bool change;
+
+static void test_on_change(void)
+{
+	change = true;
+}
+
+ZTEST(fan_common, test_fan_hysteresis)
+{
+	/* Start the fan off */
+	change = false;
+	fan_set_rpm_target(0, 0);
+
+	/* Start the temperature below off threshold */
+	zassert_equal(temp_ratio_to_rpm_hysteresis(test_table, 2, 0,
+						   TEMP_TO_RATIO(10),
+						   &test_on_change),
+		      0);
+	zassert_false(change);
+
+	/* Increase, but stay below level 0 increase threshold */
+	zassert_equal(temp_ratio_to_rpm_hysteresis(test_table, 2, 0,
+						   TEMP_TO_RATIO(30),
+						   &test_on_change),
+		      0);
+	zassert_false(change);
+
+	/* Increase to level 0 */
+	zassert_equal(temp_ratio_to_rpm_hysteresis(test_table, 2, 0,
+						   TEMP_TO_RATIO(35),
+						   &test_on_change),
+		      1000);
+	zassert_true(change);
+
+	/* Increase to level 1 */
+	change = false;
+	zassert_equal(temp_ratio_to_rpm_hysteresis(test_table, 2, 0,
+						   TEMP_TO_RATIO(45),
+						   &test_on_change),
+		      2000);
+	zassert_true(change);
+
+	/* Decrease back to level 0 */
+	change = false;
+	zassert_equal(temp_ratio_to_rpm_hysteresis(test_table, 2, 0,
+						   TEMP_TO_RATIO(38),
+						   &test_on_change),
+		      1000);
+	zassert_true(change);
+}
+
+ZTEST(fan_common, test_fan_hc_get_target_rpm)
+{
+	struct ec_response_pwm_get_fan_rpm r;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_RESPONSE(
+		EC_CMD_PWM_GET_FAN_TARGET_RPM, 0, r);
+
+	zassert_ok(host_command_process(&args));
+	zassert_equal(r.rpm, fan_get_rpm_target(0));
+}
+
+ZTEST(fan_common, test_fan_hc_get_target_rpm_no_fans)
+{
+	struct ec_response_pwm_get_fan_rpm r;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND_RESPONSE(
+		EC_CMD_PWM_GET_FAN_TARGET_RPM, 0, r);
+
+	/* Pretend we have no fans */
+	fan_set_count(0);
+
+	zassert_equal(host_command_process(&args), EC_RES_ERROR);
+}
+
+ZTEST(fan_common, test_fan_hc_set_target_rpm_v0)
+{
+	struct ec_params_pwm_set_fan_target_rpm_v0 p = {
+		.rpm = 4000,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_TARGET_RPM, 0, p);
+
+	zassert_ok(host_command_process(&args));
+	zassert_equal(p.rpm, fan_get_rpm_target(0));
+}
+
+ZTEST(fan_common, test_fan_hc_set_target_rpm_v1)
+{
+	struct ec_params_pwm_set_fan_target_rpm_v1 p = {
+		.rpm = 4000,
+		.fan_idx = 0,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_TARGET_RPM, 1, p);
+
+	zassert_ok(host_command_process(&args));
+	zassert_equal(p.rpm, fan_get_rpm_target(0));
+}
+
+ZTEST(fan_common, test_fan_hc_set_target_rpm_v1_bad_fan)
+{
+	struct ec_params_pwm_set_fan_target_rpm_v1 p = {
+		.rpm = 4000,
+		.fan_idx = 80,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_TARGET_RPM, 1, p);
+
+	zassert_equal(host_command_process(&args), EC_RES_ERROR);
+}
+
+ZTEST(fan_common, test_fan_hc_set_duty_v0)
+{
+	struct ec_params_pwm_set_fan_duty_v0 p = {
+		.percent = 50,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_DUTY, 0, p);
+
+	zassert_ok(host_command_process(&args));
+}
+
+ZTEST(fan_common, test_fan_hc_set_duty_v1)
+{
+	struct ec_params_pwm_set_fan_duty_v1 p = {
+		.percent = 50,
+		.fan_idx = 0,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_DUTY, 1, p);
+
+	zassert_ok(host_command_process(&args));
+}
+
+ZTEST(fan_common, test_fan_hc_set_duty_v1_bad_fan)
+{
+	struct ec_params_pwm_set_fan_duty_v1 p = {
+		.percent = 50,
+		.fan_idx = 20,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_PWM_SET_FAN_DUTY, 1, p);
+
+	zassert_equal(host_command_process(&args), EC_RES_ERROR);
+}
+
+void set_thermal_control_enabled(int fan, int enable);
+
+ZTEST(fan_common, test_fan_hc_set_auto_fan_v0)
+{
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_SIMPLE(EC_CMD_THERMAL_AUTO_FAN_CTRL, 0);
+
+	set_thermal_control_enabled(0, 0);
+	zassert_ok(host_command_process(&args));
+	zassert_true(is_thermal_control_enabled(0));
+}
+
+ZTEST(fan_common, test_fan_hc_set_auto_fan_v1)
+{
+	struct ec_params_auto_fan_ctrl_v1 p = {
+		.fan_idx = 0,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_THERMAL_AUTO_FAN_CTRL, 1, p);
+
+	set_thermal_control_enabled(0, 0);
+	zassert_ok(host_command_process(&args));
+	zassert_true(is_thermal_control_enabled(0));
+}
+
+ZTEST(fan_common, test_fan_hc_set_auto_fan_v1_bad_fan)
+{
+	struct ec_params_auto_fan_ctrl_v1 p = {
+		.fan_idx = 20,
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_THERMAL_AUTO_FAN_CTRL, 1, p);
+
+	zassert_equal(host_command_process(&args), EC_RES_ERROR);
+}
