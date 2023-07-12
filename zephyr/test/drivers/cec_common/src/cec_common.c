7429a539f156d8a9d78595672db43686d7cc46e8
Reka Norman <rekanorman@google.com>
2023-07-12 12:58:56 +1000

    cec: Add more tests for cec_msg_transfer and cec_rx_queue
    BUG=b:270507438
    TEST=./twister -s drivers/drivers.cec_common
    
    Change-Id: I7a4c35a6848bb54d26c161606188c41ba1e80ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4736654
    Commit-Queue: Reka Norman <rekanorman@chromium.org>
    Tested-by: Reka Norman <rekanorman@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- zephyr/test/drivers/cec_common/src/cec_common.c
+++ zephyr/test/drivers/cec_common/src/cec_common.c
@@ -8,6 +8,59 @@
 
 #include <zephyr/ztest.h>
 
+/* Test basic get_bit/set_bit/inc_bit behaviour */
+ZTEST_USER(cec_common, test_cec_transfer)
+{
+	struct cec_msg_transfer transfer = {};
+	int i;
+
+	/* Set first byte to 1 */
+	for (i = 0; i < 8; i++) {
+		cec_transfer_set_bit(&transfer, 1);
+		zassert_true(cec_transfer_get_bit(&transfer));
+		cec_transfer_inc_bit(&transfer);
+		zassert_false(cec_transfer_get_bit(&transfer));
+	}
+	zassert_equal(transfer.buf[0], 0xff);
+	zassert_equal(transfer.buf[1], 0);
+
+	/* Set half of second byte to 1 */
+	for (i = 0; i < 4; i++) {
+		cec_transfer_set_bit(&transfer, 1);
+		zassert_true(cec_transfer_get_bit(&transfer));
+		cec_transfer_inc_bit(&transfer);
+		zassert_false(cec_transfer_get_bit(&transfer));
+	}
+	zassert_equal(transfer.buf[0], 0xff);
+	zassert_equal(transfer.buf[1], 0xf0);
+	zassert_equal(transfer.buf[2], 0);
+}
+
+static void test_transfer_is_eom(int len)
+{
+	struct cec_msg_transfer transfer = {};
+	int i;
+
+	/* Write one bit fewer than len bytes and check EOM is false */
+	zassert_false(cec_transfer_is_eom(&transfer, len));
+	for (i = 0; i < 8 * len - 1; i++) {
+		cec_transfer_inc_bit(&transfer);
+		zassert_false(cec_transfer_is_eom(&transfer, len));
+	}
+
+	/* Write one more bit and check EOM is true */
+	cec_transfer_inc_bit(&transfer);
+	zassert_true(cec_transfer_is_eom(&transfer, len));
+}
+
+ZTEST_USER(cec_common, test_cec_transfer_is_eom)
+{
+	test_transfer_is_eom(1);
+	test_transfer_is_eom(2);
+	test_transfer_is_eom(3);
+	test_transfer_is_eom(MAX_CEC_MSG_LEN);
+}
+
 struct overflow_msg {
 	struct cec_msg_transfer transfer;
 	uint8_t overflow_detector;
@@ -43,6 +96,78 @@ ZTEST_USER(cec_common, test_cec_transfer_overflow)
 		cec_transfer_inc_bit(&overflow_msg.transfer);
 	}
 	zassert_equal(overflow_msg.transfer.byte, MAX_CEC_MSG_LEN);
+
+	/* Check that cec_transfer_get_bit does not read past the transfer */
+	overflow_msg.overflow_detector = 0xff;
+	overflow_msg.transfer.bit = 0;
+	zassert_equal(cec_transfer_get_bit(&overflow_msg.transfer), 0);
+}
+
+static bool msg_is_equal(const uint8_t *msg1, const uint8_t msg1_len,
+			 const uint8_t *msg2, const uint8_t msg2_len)
+{
+	int i;
+
+	if (msg1_len != msg2_len)
+		return false;
+
+	for (i = 0; i < msg1_len; i++)
+		if (msg1[i] != msg2[i])
+			return false;
+
+	return true;
+}
+
+/* Test basic push/pop/flush behaviour */
+ZTEST_USER(cec_common, test_cec_rx_queue)
+{
+	struct cec_rx_queue queue = {};
+	const uint8_t msg1[] = { 0x0f, 0x87, 0x00, 0xe0, 0x91 };
+	const uint8_t msg1_len = ARRAY_SIZE(msg1);
+	const uint8_t msg2[] = { 0x04, 0x46 };
+	const uint8_t msg2_len = ARRAY_SIZE(msg2);
+	uint8_t msg[MAX_CEC_MSG_LEN];
+	uint8_t msg_len;
+	int i;
+
+	/* Queue is empty so pop returns an error */
+	zassert_not_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+
+	/* Push two messages */
+	zassert_equal(cec_rx_queue_push(&queue, msg1, msg1_len), EC_SUCCESS);
+	zassert_equal(cec_rx_queue_push(&queue, msg2, msg2_len), EC_SUCCESS);
+
+	/* Pop the messages and check they're correct */
+	zassert_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+	zassert_true(msg_is_equal(msg, msg_len, msg1, msg1_len));
+	zassert_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+	zassert_true(msg_is_equal(msg, msg_len, msg2, msg2_len));
+
+	/* Check queue is empty */
+	zassert_not_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+
+	/* Push and pop multiple times to check offsets wrap around correctly */
+	for (i = 0; i < (CEC_RX_BUFFER_SIZE * 2) / msg1_len; i++) {
+		zassert_equal(cec_rx_queue_push(&queue, msg1, msg1_len),
+			      EC_SUCCESS);
+		zassert_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+		zassert_true(msg_is_equal(msg, msg_len, msg1, msg1_len));
+	}
+
+	/* Check queue is empty */
+	zassert_not_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+
+	/* Check flush works */
+	zassert_equal(cec_rx_queue_push(&queue, msg1, msg1_len), EC_SUCCESS);
+	zassert_equal(cec_rx_queue_push(&queue, msg2, msg2_len), EC_SUCCESS);
+	cec_rx_queue_flush(&queue);
+	zassert_not_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
+
+	/* Push a message then corrupt the message length in the queue */
+	zassert_equal(cec_rx_queue_push(&queue, msg1, msg1_len), EC_SUCCESS);
+	queue.buf[queue.read_offset] = MAX_CEC_MSG_LEN + 1;
+	/* Check pop returns an error */
+	zassert_not_equal(cec_rx_queue_pop(&queue, msg, &msg_len), 0);
 }
 
 struct overflow_queue {
