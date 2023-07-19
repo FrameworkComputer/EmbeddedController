6b53fd5dbfdc0ef1b4a3cf1ad608d75a987853fe
mingjia.zhang <mingjia.zhang@mediatek.corp-partner.google.com>
2023-07-19 16:52:11 +0800

    chip/mt_scp: Add capability to support vdec 10bit
    add VDEC_CAP_IS_SUPPORT_10BIT to support 10bit
    
    BRANCH=none
    BUG=b:250698011
    TEST=make BOARD=geralt_scp
    
    Signed-off-by: mingjia zhang <mingjia.zhang@mediatek.com>
    Signed-off-by: Jason Chen <jason-ch.chen@mediatek.corp-partner.google.com>
    Change-Id:Iee90097c292cb056bbd6de490223eeb5609dce44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4700246
    Tested-by: Fei Shao <fshao@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>

--- chip/mt_scp/mt8188/video.c
+++ chip/mt_scp/mt8188/video.c
@@ -13,6 +13,7 @@ uint32_t video_get_enc_capability(void)
 
 uint32_t video_get_dec_capability(void)
 {
-	return VDEC_CAP_MM21 | VDEC_CAP_H264_SLICE | VDEC_CAP_VP8_FRAME |
-	       VDEC_CAP_VP9_FRAME | VDEC_CAP_AV1_FRAME | VDEC_CAP_HEVC_SLICE;
+	return VDEC_CAP_MM21 | VDEC_CAP_IS_SUPPORT_10BIT | VDEC_CAP_H264_SLICE |
+	       VDEC_CAP_VP8_FRAME | VDEC_CAP_VP9_FRAME | VDEC_CAP_AV1_FRAME |
+	       VDEC_CAP_HEVC_SLICE;
 }
