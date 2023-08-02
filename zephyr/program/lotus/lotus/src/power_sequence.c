cdbaa5ad9ea09631ccaf288b816d178afec6ff96
Josh-Tsai <josh_tsai@compal.com>
2023-08-02 09:32:14 +0800

    Lotus: Follow the SLP_S3 to disable the ssd2 power
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- zephyr/program/lotus/lotus/src/power_sequence.c
+++ zephyr/program/lotus/lotus/src/power_sequence.c
@@ -577,6 +577,7 @@ static void peripheral_power_suspend(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_mute_l), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_invpwr), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_sleep_l), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ssd2_pwr_en), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, peripheral_power_suspend, HOOK_PRIO_DEFAULT);
 
@@ -590,7 +591,6 @@ void system_check_d3cold(void)
 
 	if (enter_d3cold_flag && !d3cold_is_entry) {
 		d3cold_is_entry = 1;
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ssd2_pwr_en), 0);
 		*host_get_memmap(EC_CUSTOMIZED_MEMMAP_WAKE_EVENT) &= ~ENTER_D3COLD;
 	} else if (exit_d3cold_flag && d3cold_is_entry) {
 		d3cold_is_entry = 0;
