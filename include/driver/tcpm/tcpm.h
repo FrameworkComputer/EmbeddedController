efb698134b3aa66b56a34a06e89ab8703d88df85
Jeremy Bettis <jbettis@google.com>
2023-08-02 18:25:26 +0000

    ec: Add #line to include/driver/tcpm/tcpm.h
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E common/usbc/usb_pe_drp_sm.c
    
    Change-Id: I83204b3c00de0b9dcccee84efd17fc30cc5bf884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4742235
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>

--- include/driver/tcpm/tcpm.h
+++ include/driver/tcpm/tcpm.h
@@ -26,6 +26,12 @@
 #error "Please upgrade your board configuration"
 #endif
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 34
+
 #ifndef CONFIG_USB_PD_TCPC
 
 /* I2C wrapper functions - get I2C port / peripheral addr from config struct. */
