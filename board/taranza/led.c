af0b4c42e9c46c104d1906bfe5ae927cf610cf72
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2023-08-08 16:58:01 +0800

    taranza: Fixed LED init state
    Fixed LED init state
    
    BUG=b:290553700
    TEST=LED behavior as expected
    
    Change-Id: I849c895527ce9b217cc6e93a751e417b7a4f4ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4758146
    Auto-Submit: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Ricky Chang <rickytlchang@chromium.org>
    Commit-Queue: Ricky Chang <rickytlchang@chromium.org>

--- board/taranza/led.c
+++ board/taranza/led.c
@@ -164,6 +164,12 @@ static void led_resume(void)
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, led_resume, HOOK_PRIO_DEFAULT);
 
+static void led_init(void)
+{
+	set_color(EC_LED_ID_POWER_LED, LED_GREEN, 0);
+}
+DECLARE_HOOK(HOOK_INIT, led_init, HOOK_PRIO_INIT_PWM);
+
 void board_led_auto_control(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_ON))
