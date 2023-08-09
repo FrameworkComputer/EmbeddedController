54997b20b9d23e79f0dd3a5d814ac2547313cb3c
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-08-09 14:51:59 +0800

    Karis: Modify GPIOs definition and USBC retimer setting
    Move usb_c1_rst_odl from GPIO37 to GPIOC7 and rename to
    usb_c1_rt_3p3_sx_en which is the ls-en-pin in C1 HB retimer.
    For there is no NCT3807 on Karis, usb_c0_frs_en is moved to
    GPIO42 and usb_c0_rt_rst_odl is moved to GPIO37.
    
    Move usb_c1_rt_rst_odl from GPIO72 to GPIOA4 for the RST PIN
    needs to use 1.8V pin.
    
    The FW_CONFIG bit0-1 is for USB DB field which can switch
    the C1 retimer for future used. Karis only use USB4 HB for
    C1 now so the other retimers are removed.
    
    Add tests for karis usb_config
    
    BUG=b:296145621
    TEST=zmake build karis
         ./twister --coverage -v -i -s rex/rex.karis.usb_config
         ./twister -T zephyr/test/rex
    
    Change-Id: I8c4a24a8b201fc1acb7de94dab0de627a6b1eeab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4764525
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Code-Coverage: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/rex/karis/src/usb_mux_config.c
+++ zephyr/program/rex/karis/src/usb_mux_config.c
@@ -22,19 +22,6 @@
 
 #include <dt-bindings/gpio_defines.h>
 
-#ifdef CONFIG_ZTEST
-
-#undef USB_MUX_ENABLE_ALTERNATIVE
-#define USB_MUX_ENABLE_ALTERNATIVE(x)
-
-#undef TCPC_ENABLE_ALTERNATE_BY_NODELABEL
-#define TCPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
-
-#undef PPC_ENABLE_ALTERNATE_BY_NODELABEL
-#define PPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
-
-#endif /* CONFIG_ZTEST */
-
 LOG_MODULE_DECLARE(rex, CONFIG_REX_LOG_LEVEL);
 
 uint32_t usb_db_type;
@@ -55,33 +42,14 @@ static void setup_runtime_gpios(void)
 	}
 
 	switch (usb_db_type) {
-	case FW_USB_DB_USB3:
-		gpio_unused(GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rst_odl));
-		gpio_unused(GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_int_odl));
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(ps_usb_c1_rt_rst_odl),
-				      GPIO_ODR_HIGH | GPIO_ACTIVE_LOW);
-		gpio_unused(GPIO_DT_FROM_NODELABEL(gpio_usb_c1_frs_en));
-		break;
-
-	case FW_USB_DB_USB4_KB8010:
-		gpio_unused(GPIO_DT_FROM_ALIAS(kb_usb_c1_rst_odl));
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(kb_usb_c1_dp_mode),
-				      GPIO_ODR_HIGH);
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(kb_usb_c1_rt_rst_odl),
-				      GPIO_ODR_HIGH);
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(kb_usb_c1_frs_alert),
+	case FW_USB_DB_USB4_HB:
+		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(hbr_usb_c1_rt_pwr_en),
+				      GPIO_ODR_LOW);
+		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(hbr_usb_c1_rt_int_odl),
 				      GPIO_INPUT);
-		break;
-
-	case FW_USB_DB_USB4_ANX7452:
-	case FW_USB_DB_USB4_ANX7452_V2:
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(anx_usb_c1_rt_hpd_in),
+		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(hbr_usb_c1_rt_rst_odl),
 				      GPIO_OUTPUT_LOW);
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(anx_usb_c1_rt_dp_en),
-				      GPIO_OUTPUT_LOW);
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(anx_usb_c1_rt_usb_en),
-				      GPIO_OUTPUT_LOW);
-		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(anx_usb_c1_frs_en),
+		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(syv_usb_c1_frs_en),
 				      GPIO_OUTPUT_LOW);
 		break;
 
@@ -105,29 +73,12 @@ static void setup_usb_db(void)
 	case FW_USB_DB_NOT_CONNECTED:
 		LOG_INF("USB DB: not connected");
 		break;
-	case FW_USB_DB_USB3:
-		LOG_INF("USB DB: Setting USB3 mux");
-		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_ps8815_port1);
-		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
-						   tcpc_ps8815_port1);
-		PPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
-						  ppc_nx20p_port1);
-		break;
-	case FW_USB_DB_USB4_ANX7452:
-	case FW_USB_DB_USB4_ANX7452_V2:
-		LOG_INF("USB DB: Setting ANX7452 mux");
-		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_anx7452_port1);
+	case FW_USB_DB_USB4_HB:
+		LOG_INF("USB DB: Setting HBR mux");
+		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_hbr_port1);
 		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
 						   tcpc_rt1716_port1);
 		PPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1, ppc_syv_port1);
-		break;
-	case FW_USB_DB_USB4_KB8010:
-		LOG_INF("USB DB: Setting KB8010 mux");
-		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_kb8010_port1);
-		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
-						   tcpc_rt1716_port1);
-		PPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
-						  ppc_ktu1125_port1);
 		break;
 	default:
 		LOG_INF("USB DB: No known USB DB found");
@@ -138,10 +89,7 @@ DECLARE_HOOK(HOOK_INIT, setup_usb_db, HOOK_PRIO_POST_I2C);
 __override uint8_t board_get_usb_pd_port_count(void)
 {
 	switch (usb_db_type) {
-	case FW_USB_DB_USB3:
-	case FW_USB_DB_USB4_ANX7452:
-	case FW_USB_DB_USB4_ANX7452_V2:
-	case FW_USB_DB_USB4_KB8010:
+	case FW_USB_DB_USB4_HB:
 		return 2;
 	default:
 		return 1;
