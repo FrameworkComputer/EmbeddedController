504f6cd3c13bc00e5898c47afaeb5399f0681ad0
Dawid Niedzwiecki <dawidn@google.com>
2023-08-25 09:42:05 +0200

    zephyr: bloonchipper: add support for host commands
    Bloonchipper uses can use two interfaces to communicate with host - UART
    and SPI. It is detected in runtime which interface should be used, based
    on the GPIO state.
    
    Add initialization of a proper host command backend in runtime. Also add
    handler for the EC_CMD_GET_PROTOCOL_INFO, which also returns values
    appropriate for a present interface.
    
    BUG=b:259667397
    TEST=zmake build bloonchipper && enable commands and run 'ectool
    --name=cros_fp stress'
    LOW_COVERAGE_REASON=limitation of the GPIO emulator - no possibility of
    returning an error
    
    Change-Id: I93713e893b60228e73d2cd35ba7d894412faf13f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4813637
    Tested-by: Dawid Niedzwiecki <dawidn@google.com>
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Commit-Queue: Patryk Duda <patrykd@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/fpmcu/bloonchipper/src/fp_transport.c
+++ zephyr/program/fpmcu/bloonchipper/src/fp_transport.c
@@ -3,9 +3,14 @@
  * found in the LICENSE file.
  */
 
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
 #include <zephyr/kernel.h>
+#include <zephyr/mgmt/ec_host_cmd/ec_host_cmd.h>
 
+#include <ec_commands.h>
 #include <fpsensor_detect.h>
 #include <gpio_signal.h>
 
@@ -32,3 +37,99 @@ enum fp_transport_type get_fp_transport_type(void)
 
 	return ret;
 }
+
+#if !defined(CONFIG_EC_HOST_CMD_BACKEND_SPI) && \
+	!defined(CONFIG_EC_HOST_CMD_BACKEND_UART)
+BUILD_ASSERT(0, "Both backends are not enabled");
+#endif
+#define SPI_PREAMBLE_LENGTH 4
+#define EC_SPI_PAST_END_LENGTH 1
+/*
+ * Max data size for a version 3 request/response packet.  This is big enough
+ * to handle a request/response header, flash write offset/size and 512 bytes
+ * of request payload or 224 bytes of response payload.
+ */
+#define UART_MAX_REQUEST_SIZE 0x220
+#define SPI_MAX_REQUEST_SIZE 0x220
+
+#define UART_MAX_RESPONSE_SIZE 0x100
+#define SPI_MAX_RESPONSE_SIZE 0x220
+
+BUILD_ASSERT(CONFIG_EC_HOST_CMD_HANDLER_RX_BUFFER_SIZE >= UART_MAX_REQUEST_SIZE,
+	     "The Host Command RX buffer is too small for UART");
+BUILD_ASSERT(CONFIG_EC_HOST_CMD_HANDLER_RX_BUFFER_SIZE >= SPI_MAX_REQUEST_SIZE,
+	     "The Host Command RX buffer is too small for SPI");
+BUILD_ASSERT(CONFIG_EC_HOST_CMD_HANDLER_TX_BUFFER_SIZE >=
+		     UART_MAX_RESPONSE_SIZE,
+	     "The Host Command TX buffer is too small for UART");
+BUILD_ASSERT(CONFIG_EC_HOST_CMD_HANDLER_TX_BUFFER_SIZE >=
+		     (UART_MAX_RESPONSE_SIZE - SPI_PREAMBLE_LENGTH -
+		      EC_SPI_PAST_END_LENGTH),
+	     "The Host Command TX buffer is too small for SPI");
+
+/**
+ * Get protocol information
+ */
+test_export_static enum ec_host_cmd_status
+host_command_protocol_info(struct ec_host_cmd_handler_args *args)
+{
+	struct ec_response_get_protocol_info *r = args->output_buf;
+
+	r->protocol_versions = BIT(3);
+
+	switch (get_fp_transport_type()) {
+	case FP_TRANSPORT_TYPE_UART:
+		r->max_request_packet_size = UART_MAX_REQUEST_SIZE;
+		r->max_response_packet_size = UART_MAX_RESPONSE_SIZE;
+
+		break;
+	case FP_TRANSPORT_TYPE_SPI:
+		r->max_request_packet_size = SPI_MAX_REQUEST_SIZE;
+		r->max_response_packet_size = SPI_MAX_RESPONSE_SIZE;
+
+		break;
+	default:
+		r->max_request_packet_size = 0;
+		r->max_response_packet_size = 0;
+		break;
+	}
+
+	r->flags = EC_PROTOCOL_INFO_IN_PROGRESS_SUPPORTED;
+	args->output_buf_size = sizeof(*r);
+
+	return EC_HOST_CMD_SUCCESS;
+}
+EC_HOST_CMD_HANDLER_UNBOUND(EC_CMD_GET_PROTOCOL_INFO,
+			    host_command_protocol_info, EC_VER_MASK(0));
+
+test_export_static int fp_transport_init(void)
+{
+#ifdef CONFIG_EC_HOST_CMD_BACKEND_UART
+	const struct device *const dev_uart =
+		DEVICE_DT_GET(DT_CHOSEN(zephyr_host_cmd_uart_backend));
+#endif /* CONFIG_EC_HOST_CMD_BACKEND_UART */
+#ifdef CONFIG_EC_HOST_CMD_BACKEND_SPI
+	struct gpio_dt_spec cs = GPIO_DT_SPEC_GET(
+		DT_CHOSEN(zephyr_host_cmd_spi_backend), cs_gpios);
+#endif /* CONFIG_EC_HOST_CMD_BACKEND_SPI */
+
+	switch (get_fp_transport_type()) {
+	case FP_TRANSPORT_TYPE_UART:
+#ifdef CONFIG_EC_HOST_CMD_BACKEND_UART
+		ec_host_cmd_init(ec_host_cmd_backend_get_uart(dev_uart));
+#endif /* CONFIG_EC_HOST_CMD_BACKEND_UART */
+
+		break;
+	case FP_TRANSPORT_TYPE_SPI:
+#ifdef CONFIG_EC_HOST_CMD_BACKEND_SPI
+		ec_host_cmd_init(ec_host_cmd_backend_get_spi(&cs));
+#endif /* CONFIG_EC_HOST_CMD_BACKEND_SPI */
+
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+SYS_INIT(fp_transport_init, POST_KERNEL, CONFIG_EC_HOST_CMD_INIT_PRIORITY);
