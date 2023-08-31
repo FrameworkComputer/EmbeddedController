b3d1fb38e4759e6b36acf245067ff877750c4ba1
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2023-08-31 14:33:12 +0800

    uldren: add board version condition for motionsense
    When board version>=2
    Follow the fw config to setup the motionsensor
    
    BUG=b:292476009
    TEST=zmake build uldren
    LOW_COVERAGE_REASON=Uldren zephyr test code is not ready. b/291835976
    
    Change-Id: Iec731e38a4a2eecc8230b4b1d21b40107490c443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4829387
    Reviewed-by: Elthan Huang <elthan_huang@compal.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: JohnWC Yeh <johnwc_yeh@compal.corp-partner.google.com>

--- zephyr/program/nissa/uldren/src/sensor.c
+++ zephyr/program/nissa/uldren/src/sensor.c
@@ -16,47 +16,91 @@
 #include "motionsense_sensors.h"
 #include "tablet_mode.h"
 
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
+
+static int cbi_boardversion = -1;
 static int sensor_fwconfig;
 
 void motion_interrupt(enum gpio_signal signal)
 {
-	if (sensor_fwconfig == BMA422_BMI323 ||
-	    sensor_fwconfig == LIS2DW12_BMI323)
+	if (cbi_boardversion < 2)
 		bmi3xx_interrupt(signal);
-	else if (sensor_fwconfig == BMA422_LSM6DSO ||
-		 sensor_fwconfig == LIS2DW12_LSM6DSO)
-		lsm6dso_interrupt(signal);
+	else {
+		if (sensor_fwconfig == BMA422_BMI323 ||
+		    sensor_fwconfig == LIS2DW12_BMI323)
+			bmi3xx_interrupt(signal);
+		else if (sensor_fwconfig == BMA422_LSM6DSO ||
+			 sensor_fwconfig == LIS2DW12_LSM6DSO)
+			lsm6dso_interrupt(signal);
+	}
 }
 
 void lid_accel_interrupt(enum gpio_signal signal)
 {
-	if (sensor_fwconfig == BMA422_LSM6DSO ||
-	    sensor_fwconfig == BMA422_BMI323)
+	if (cbi_boardversion < 2)
 		bma4xx_interrupt(signal);
-	else if (sensor_fwconfig == LIS2DW12_BMI323 ||
-		 sensor_fwconfig == LIS2DW12_LSM6DSO)
-		lis2dw12_interrupt(signal);
+	else {
+		if (sensor_fwconfig == BMA422_LSM6DSO ||
+		    sensor_fwconfig == BMA422_BMI323)
+			bma4xx_interrupt(signal);
+		else if (sensor_fwconfig == LIS2DW12_BMI323 ||
+			 sensor_fwconfig == LIS2DW12_LSM6DSO)
+			lis2dw12_interrupt(signal);
+	}
 }
 
 static void motionsense_init(void)
 {
+	int ret;
+	int motion_none = 0;
+	uint32_t tablet_present;
+
+	ret = cbi_get_board_version(&cbi_boardversion);
+	if (ret != EC_SUCCESS) {
+		LOG_ERR("Error retrieving CBI BOARD_VER.");
+		return;
+	}
+
+	cros_cbi_get_fw_config(FW_TABLET, &tablet_present);
 	cros_cbi_get_fw_config(MOTIONSENSE_SENSOR, &sensor_fwconfig);
 
-	if (sensor_fwconfig == BMA422_LSM6DSO) {
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
-		ccprints("LID ACCEL:BMA422, BASE ACCEL:LSM6DSO");
-	} else if (sensor_fwconfig == BMA422_BMI323) {
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
-		ccprints("LID ACCEL:BMA422, BASE ACCEL:BMI323");
-	} else if (sensor_fwconfig == LIS2DW12_BMI323) {
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
-		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
-		ccprints("LID ACCEL:LIS2DW12, BASE ACCEL:BMI323");
-	} else if (sensor_fwconfig == LIS2DW12_LSM6DSO) {
-		ccprints("LID ACCEL:LIS2DW12, BASE ACCEL:LSM6DSO");
+	if (cbi_boardversion < 2) {
+		if (tablet_present == FW_TABLET_PRESENT) {
+			if (sensor_fwconfig == LIS2DW12_LSM6DSO) {
+				ccprints(
+					"LID SENSOR:LIS2DW12, BASE SENSOR:LSM6DSO");
+			} else {
+				MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
+				MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
+				MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
+				ccprints(
+					"LID SENSOR:BMA422, BASE SENSOR:BMI323");
+			}
+		} else
+			motion_none = 1;
 	} else {
+		if (sensor_fwconfig == BMA422_LSM6DSO) {
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
+			ccprints("LID ACCEL:BMA422, BASE ACCEL:LSM6DSO");
+		} else if (sensor_fwconfig == BMA422_BMI323) {
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_lid_accel);
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
+			ccprints("LID ACCEL:BMA422, BASE ACCEL:BMI323");
+		} else if (sensor_fwconfig == LIS2DW12_BMI323) {
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
+			MOTIONSENSE_ENABLE_ALTERNATE(alt_base_gyro);
+			ccprints("LID ACCEL:LIS2DW12, BASE ACCEL:BMI323");
+		} else if (sensor_fwconfig == LIS2DW12_LSM6DSO) {
+			ccprints("LID ACCEL:LIS2DW12, BASE ACCEL:LSM6DSO");
+		} else {
+			motion_none = 1;
+		}
+	}
+
+	if (motion_none) {
 		motion_sensor_count = 0;
 		gmr_tablet_switch_disable();
 		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
