bf76d4497ab4b7586d4e6fc8b47a321f8b034522
Dino Li <Dino.Li@ite.com.tw>
2023-09-11 16:58:21 +0800

    corsola: add wrapper function to register new variants
    With this CL we are able to create an initial Zephyr EC image based
    on the kingler or krabby reference board using the create_zephyr_ec_image
    script.
    
    BUG=none
    TEST=create new projects using the create_zephyr_ec_image script:
    - create_zephyr_ec_image.sh corsola kingler foo_npcx b:xxxxxxxxx
    - create_zephyr_ec_image.sh corsola krabby foo_ite b:xxxxxxxxx
    
    Change-Id: I4063f612de72f083a5cdb061d9578e928e32bd01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4853032
    Commit-Queue: Dino Li <Dino.Li@ite.com.tw>
    Tested-by: Dino Li <Dino.Li@ite.com.tw>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Eric Yilun Lin <yllin@google.com>

--- zephyr/program/corsola/BUILD.py
+++ zephyr/program/corsola/BUILD.py
@@ -31,6 +31,26 @@ def register_corsola_project(
     )
 
 
+def register_kingler_project(
+    project_name,
+):
+    """Wrapper function for registering a variant of kingler."""
+    return register_corsola_project(
+        project_name=project_name,
+        chip="npcx9m3f",
+    )
+
+
+def register_krabby_project(
+    project_name,
+):
+    """Wrapper function for registering a variant of krabby."""
+    return register_corsola_project(
+        project_name=project_name,
+        chip="it81202bx",
+    )
+
+
 register_corsola_project("krabby")
 
 register_corsola_project(
