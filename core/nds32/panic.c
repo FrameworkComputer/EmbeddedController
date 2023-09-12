74739ffbe3b113f5187d860eea7139fc13db3326
Rob Barnes <robbarnes@google.com>
2023-09-12 16:40:07 -0600

    nds32/riscv: Set WATCHDOG_WARN panic reason on watchdog warning
    When a watchdog warning occurs, fully populate the panic data using the
    PANIC_SW_WATCHDOG_WARN reason. Include the program counter and current
    task. Remove special case for preserving the program counter since
    this can result in stale program counter on hard hangs.
    
    The program counter field in the panic info will no longer be populated
    on watchdogs. Instead the program counter will be stored int the `info`
    register. Also the task id will be stored in the `exception` register.
    This aligns with how cortex-m watchdogs are handled on CrOS EC.
    
    BUG=b:300164756
    TEST=Tested watchdog and hang on kracko(nds32) and tomato(riscv)
    
    Change-Id: I0b67032bb12564fb636eafdb47f537256fcf6040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4859537
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Rob Barnes <robbarnes@google.com>

--- core/nds32/panic.c
+++ core/nds32/panic.c
@@ -95,19 +95,12 @@ void panic_set_reason(uint32_t reason, uint32_t info, uint8_t exception)
 	 * once again won't remove any data
 	 */
 	struct panic_data *const pdata = get_panic_data_write();
-	uint32_t warning_ipc;
 	uint32_t *regs;
 
 	regs = pdata->nds_n8.regs;
 
 	/* Setup panic data structure */
-	if (reason != PANIC_SW_WATCHDOG) {
-		memset(pdata, 0, CONFIG_PANIC_DATA_SIZE);
-	} else {
-		warning_ipc = pdata->nds_n8.ipc;
-		memset(pdata, 0, CONFIG_PANIC_DATA_SIZE);
-		pdata->nds_n8.ipc = warning_ipc;
-	}
+	memset(pdata, 0, CONFIG_PANIC_DATA_SIZE);
 	pdata->magic = PANIC_DATA_MAGIC;
 	pdata->struct_size = CONFIG_PANIC_DATA_SIZE;
 	pdata->struct_version = 2;
