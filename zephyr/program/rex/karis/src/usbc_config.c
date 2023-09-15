d9abc3da2bf8105f24cdbb1cc78663292378473d
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-09-15 10:24:08 +0800

    Karis: remove rex usbc_config for karis
    Add new config CONFIG_BOARD_REX_USBC_CONFIG for
    program/rex/src/usbc_config.c and setting the config default
    yes.
    
    Remove usb_c0_sbu_fault_odl interrupt for unused.
    Remove nct38xx@70 node for Karis use rt1716 as port0 TCPC.
    Undefine CONFIG_PLATFORM_EC_USB_CHARGER for there is no
    BC1.2 on Karis
    
    BUG=b:296145621
    TEST=power on without MPU fault on Karis
         ./twister -T zephyr/test/rex/ -v -i -s rex.karis.usb_config
    LOW_COVERAGE_REASON=the test will be updated in b/296820495
    
    Change-Id: Ia754e71ea390174ecce06ac091a2a624a3457c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4867951
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>

--- zephyr/program/rex/karis/src/usbc_config.c
+++ zephyr/program/rex/karis/src/usbc_config.c
@@ -7,10 +7,21 @@
 #include "driver/ppc/ktu1125_public.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/ps8xxx_public.h"
+#include "hooks.h"
 #include "ppc/syv682x_public.h"
 #include "system.h"
 #include "usb_mux_config.h"
 #include "usbc_config.h"
+
+#include <stdbool.h>
+
+#include <zephyr/drivers/espi.h>
+
+#define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ##args)
+#define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ##args)
+
+/* eSPI device */
+#define espi_dev DEVICE_DT_GET(DT_CHOSEN(cros_ec_espi))
 
 void board_reset_pd_mcu(void)
 {
@@ -20,4 +31,83 @@ void board_reset_pd_mcu(void)
 __override bool board_is_tbt_usb4_port(int port)
 {
 	return true;
+}
+
+static void usbc_interrupt_init(void)
+{
+	/* Only reset TCPC if not sysjump */
+	if (!system_jumped_late()) {
+		board_reset_pd_mcu();
+	}
+}
+DECLARE_HOOK(HOOK_INIT, usbc_interrupt_init, HOOK_PRIO_POST_I2C);
+
+__override void board_overcurrent_event(int port, int is_overcurrented)
+{
+	/*
+	 * Meteorlake PCH uses Virtual Wire for over current error,
+	 * hence Send Over Current Virtual Wire' eSPI signal.
+	 */
+	espi_send_vwire(espi_dev, port + ESPI_VWIRE_SIGNAL_SLV_GPIO_0,
+			!is_overcurrented);
+}
+
+static void board_disable_charger_ports(void)
+{
+	int i;
+
+	CPRINTSUSB("Disabling all charger ports");
+
+	/* Disable all ports. */
+	for (i = 0; i < board_get_usb_pd_port_count(); i++) {
+		/*
+		 * Do not return early if one fails otherwise we can
+		 * get into a boot loop assertion failure.
+		 */
+		if (ppc_vbus_sink_enable(i, 0)) {
+			CPRINTSUSB("Disabling C%d as sink failed.", i);
+		}
+	}
+}
+
+int board_set_active_charge_port(int port)
+{
+	bool is_valid_port = (port >= 0 && port < CONFIG_USB_PD_PORT_MAX_COUNT);
+	int i;
+
+	if (port == CHARGE_PORT_NONE) {
+		board_disable_charger_ports();
+		return EC_SUCCESS;
+	} else if (!is_valid_port) {
+		return EC_ERROR_INVAL;
+	}
+
+	/* Check if the port is sourcing VBUS. */
+	if (ppc_is_sourcing_vbus(port)) {
+		CPRINTSUSB("Skip enable C%d", port);
+		return EC_ERROR_INVAL;
+	}
+
+	CPRINTSUSB("New charge port: C%d", port);
+
+	/*
+	 * Turn off the other ports' sink path FETs, before enabling the
+	 * requested charge port.
+	 */
+	for (i = 0; i < board_get_usb_pd_port_count(); i++) {
+		if (i == port) {
+			continue;
+		}
+		if (ppc_vbus_sink_enable(i, 0)) {
+			CPRINTSUSB("C%d: sink path disable failed.", i);
+		}
+	}
+
+	/* Enable requested charge port. */
+	if (ppc_vbus_sink_enable(port, 1)) {
+		CPRINTSUSB("C%d: sink path enable failed.", port);
+		return EC_ERROR_UNKNOWN;
+	}
+
+	return EC_SUCCESS;
 }
--- zephyr/test/rex/tests/karis/src/usb_config.c
+++ zephyr/test/rex/tests/karis/src/usb_config.c
@@ -25,6 +25,8 @@ FAKE_VOID_FUNC(pd_set_input_current_limit, int, uint32_t, uint32_t);
 FAKE_VOID_FUNC(pd_power_supply_reset, int);
 FAKE_VALUE_FUNC(int, pd_check_vconn_swap, int);
 FAKE_VALUE_FUNC(int, pd_set_power_supply_ready, int);
+FAKE_VALUE_FUNC(int, ppc_vbus_sink_enable, int, int);
+FAKE_VALUE_FUNC(int, ppc_is_sourcing_vbus, int);
 
 int mock_cros_cbi_get_fw_config_hb(enum cbi_fw_config_field_id field_id,
 				   uint32_t *value)
@@ -57,7 +59,11 @@ static void usb_mux_config_before(void *fixture)
 	ARG_UNUSED(fixture);
 
 	RESET_FAKE(cros_cbi_get_fw_config);
+	RESET_FAKE(ppc_vbus_sink_enable);
+	RESET_FAKE(ppc_is_sourcing_vbus);
 }
+
+ZTEST_SUITE(usb_mux_config, NULL, NULL, usb_mux_config_before, NULL, NULL);
 
 ZTEST_USER(usb_mux_config, test_setup_usb_db_hb)
 {
@@ -103,4 +109,69 @@ ZTEST_USER(usb_mux_config, test_setup_usb_db_error_reading_cbi)
 	zassert_equal(1, board_get_usb_pd_port_count());
 }
 
-ZTEST_SUITE(usb_mux_config, NULL, NULL, usb_mux_config_before, NULL, NULL);
+ZTEST_USER(usb_mux_config, test_board_set_active_charge_port_invalid)
+{
+	/* Initial number of usbc port (non-USB-A sku) */
+	cros_cbi_get_fw_config_fake.custom_fake =
+		mock_cros_cbi_get_fw_config_hb;
+
+	hook_notify(HOOK_INIT);
+
+	zassert_equal(EC_ERROR_INVAL, board_set_active_charge_port(5));
+	zassert_equal(0, ppc_vbus_sink_enable_fake.call_count);
+}
+
+ZTEST_USER(usb_mux_config, test_board_set_active_charge_port_none)
+{
+	/* Initial number of usbc port (non-USB-A sku) */
+	cros_cbi_get_fw_config_fake.custom_fake =
+		mock_cros_cbi_get_fw_config_hb;
+	ppc_vbus_sink_enable_fake.return_val = 0;
+
+	hook_notify(HOOK_INIT);
+
+	zassert_equal(EC_SUCCESS,
+		      board_set_active_charge_port(CHARGE_PORT_NONE));
+	zassert_equal(2, ppc_vbus_sink_enable_fake.call_count);
+	/* C0 */
+	zassert_equal(0, ppc_vbus_sink_enable_fake.arg0_history[0]);
+	zassert_equal(0, ppc_vbus_sink_enable_fake.arg1_history[0]);
+	/* C1 */
+	zassert_equal(1, ppc_vbus_sink_enable_fake.arg0_history[1]);
+	zassert_equal(0, ppc_vbus_sink_enable_fake.arg1_history[1]);
+}
+
+ZTEST_USER(usb_mux_config, test_board_set_active_charge_port_normal)
+{
+	/* Initial number of usbc port (non-USB-A sku) */
+	cros_cbi_get_fw_config_fake.custom_fake =
+		mock_cros_cbi_get_fw_config_hb;
+	ppc_vbus_sink_enable_fake.return_val = 0;
+	ppc_is_sourcing_vbus_fake.return_val = 0;
+
+	hook_notify(HOOK_INIT);
+
+	/* set charge port C0 */
+	zassert_equal(EC_SUCCESS, board_set_active_charge_port(0));
+	zassert_equal(2, ppc_vbus_sink_enable_fake.call_count);
+
+	/* disable sink on other ports */
+	zassert_equal(ppc_vbus_sink_enable_fake.arg0_history[0], 1);
+	zassert_equal(ppc_vbus_sink_enable_fake.arg1_history[0], 0);
+
+	/* Enable requested charge port. */
+	zassert_equal(ppc_vbus_sink_enable_fake.arg0_history[1], 0);
+	zassert_equal(ppc_vbus_sink_enable_fake.arg1_history[1], 1);
+
+	/* set charge port C1 */
+	zassert_equal(EC_SUCCESS, board_set_active_charge_port(1));
+	zassert_equal(4, ppc_vbus_sink_enable_fake.call_count);
+
+	/* disable sink on other ports */
+	zassert_equal(ppc_vbus_sink_enable_fake.arg0_history[2], 0);
+	zassert_equal(ppc_vbus_sink_enable_fake.arg1_history[2], 0);
+
+	/* Enable requested charge port. */
+	zassert_equal(ppc_vbus_sink_enable_fake.arg0_history[3], 1);
+	zassert_equal(ppc_vbus_sink_enable_fake.arg1_history[3], 1);
+}
