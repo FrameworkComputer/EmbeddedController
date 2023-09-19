9eafdf3abc846b65f12916defdfeaa1102c8381a
Jeremy Bettis <jbettis@google.com>
2023-09-19 21:39:24 +0000

    ec: Add #line to driver/ppc/ktu1125.h
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E driver/ppc/ktu1125.c
    
    Change-Id: Iafc48ae307622cac9e8ad1751650868d7efd0eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4875710
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>

--- driver/ppc/ktu1125.h
+++ driver/ppc/ktu1125.h
@@ -5,6 +5,12 @@
 
 /* Kinetic KTU1125 Type-C Power Path Controller */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 13
+
 #ifndef __CROS_EC_KTU1125_H
 #define __CROS_EC_KTU1125_H
 
