f16aba0da133068aec7e95ed5d75f45720407b57
Patrick Georgi <pgeorgi@google.com>
2023-09-20 20:50:05 +0200

    ecusb: Prepare for copying to hdctools
    hdctools is pretty strict on pylint enforcement, so clean things up
    before copying: pylint complained about an ordering issue between
    OSError and USBError, but since they do the same, collate them.
    
    BUG=none
    TEST=Visual inspection of the diff.
    
    Change-Id: I92fa6676c99783f5f1aec4cda0cbc400b9e55f77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4874341
    Commit-Queue: Patrick Georgi <pgeorgi@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-by: Keith Haddow <haddowk@chromium.org>

--- extra/tigertool/ecusb/stm32uart.py
+++ extra/tigertool/ecusb/stm32uart.py
@@ -106,9 +106,7 @@ class Suart:
 
                     # If we miss some characters on pty disconnect, that's fine.
                     # ep.read() also throws USBError on timeout, which we discard.
-                    except OSError:
-                        pass
-                    except usb.core.USBError:
+                    except (OSError, usb.core.USBError):
                         pass
                 else:
                     time.sleep(0.1)
@@ -132,9 +130,7 @@ class Suart:
                         if r:
                             self._susb._write_ep.write(r, self._susb.TIMEOUT_MS)
 
-                    except OSError:
-                        pass
-                    except usb.core.USBError:
+                    except (OSError, usb.core.USBError):
                         pass
                 else:
                     time.sleep(0.1)
