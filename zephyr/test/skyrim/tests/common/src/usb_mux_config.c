963d0ef47627668748584a43b94b1c502f5ce4bb
Robert Zieba <robertzieba@google.com>
2023-09-20 11:17:11 -0600

    skyrim: Add test coverage for wake-on-DP code
    BUG=b:294307786
    TEST=./twister -T zephyr/test/skyrim
    
    Change-Id: I9f96fc5e9d2733b9759e9000b6f17285509dcb30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4879520
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Forest Mittelberg <bmbm@google.com>
    Commit-Queue: Robert Zieba <robertzieba@google.com>
    Tested-by: Robert Zieba <robertzieba@google.com>

--- zephyr/test/skyrim/tests/common/src/usb_mux_config.c
+++ zephyr/test/skyrim/tests/common/src/usb_mux_config.c
@@ -4,6 +4,8 @@
  */
 #include "chipset.h"
 #include "emul/retimer/emul_anx7483.h"
+#include "host_command.h"
+#include "power.h"
 #include "usbc/usb_muxes.h"
 #include "ztest/usb_mux_config.h"
 
@@ -25,6 +27,7 @@ extern const size_t anx7483_dock_flip_count;
 
 int ioex_set_flip(int port, mux_state_t mux_state);
 int board_c1_ps8818_mux_set(const struct usb_mux *me, mux_state_t mux_state);
+int board_mux_set(const struct usb_mux *mux, mux_state_t state);
 
 DEFINE_FAKE_VOID_FUNC(usb_mux_enable_alternative);
 
@@ -40,13 +43,28 @@ enum tcpc_cc_polarity pd_get_polarity(int port)
 
 FAKE_VOID_FUNC(chipset_force_shutdown, enum chipset_shutdown_reason);
 FAKE_VALUE_FUNC(int, extpower_is_present);
+FAKE_VALUE_FUNC(enum power_state, power_get_state);
+FAKE_VOID_FUNC(host_set_single_event, enum host_event_code);
 
 test_mockable int chipset_in_state(int mask)
 {
 	return mask & CHIPSET_STATE_ON;
 }
 
-ZTEST_SUITE(usb_mux_config_common, NULL, NULL, NULL, NULL, NULL);
+static void test_reset(void)
+{
+	RESET_FAKE(power_get_state);
+	RESET_FAKE(host_set_single_event);
+}
+
+static void usb_mux_config_before(void *fixture)
+{
+	ARG_UNUSED(fixture);
+	test_reset();
+}
+
+ZTEST_SUITE(usb_mux_config_common, NULL, NULL, usb_mux_config_before, NULL,
+	    NULL);
 
 /*
  * Winterhold is the only board that this test doesn't have
@@ -123,4 +141,54 @@ ZTEST(usb_mux_config_common, test_ioex_set_flip)
 	rv = ioex_set_flip(1, USB_PD_MUX_POLARITY_INVERTED);
 	zassert_ok(rv);
 	zassert_equal(gpio_emul_output_get(c1->port, c1->pin), 1);
+}
+
+static int board_mux_set_test(int port)
+{
+	const struct usb_mux me = { .usb_port = port };
+
+	/* Validate behavior in S0, no host event should be set. */
+	power_get_state_fake.return_val = POWER_S0;
+	board_mux_set(&me, USB_PD_MUX_NONE);
+	zassert_equal(host_set_single_event_fake.call_count, 0);
+
+	board_mux_set(&me, USB_PD_MUX_DP_ENABLED);
+	zassert_equal(host_set_single_event_fake.call_count, 0);
+
+	board_mux_set(&me, USB_PD_MUX_DP_ENABLED);
+	zassert_equal(host_set_single_event_fake.call_count, 0);
+
+	board_mux_set(&me, USB_PD_MUX_NONE);
+	zassert_equal(host_set_single_event_fake.call_count, 0);
+
+	/*
+	 * Validate behavior in S0ix, host events should only be set if there
+	 * was a change in DP status.
+	 */
+	power_get_state_fake.return_val = POWER_S0ix;
+	board_mux_set(&me, USB_PD_MUX_NONE);
+	zassert_equal(host_set_single_event_fake.call_count, 0);
+
+	board_mux_set(&me, USB_PD_MUX_DP_ENABLED);
+	zassert_equal(host_set_single_event_fake.call_count, 1);
+	zassert_true(host_set_single_event_fake.arg0_val &
+		     EC_HOST_EVENT_USB_MUX);
+
+	board_mux_set(&me, USB_PD_MUX_DP_ENABLED);
+	zassert_equal(host_set_single_event_fake.call_count, 1);
+
+	board_mux_set(&me, USB_PD_MUX_NONE);
+	zassert_equal(host_set_single_event_fake.call_count, 2);
+	zassert_true(host_set_single_event_fake.arg0_val &
+		     EC_HOST_EVENT_USB_MUX);
+
+	return 0;
+}
+
+ZTEST(usb_mux_config_common, test_board_mux_set)
+{
+	zassert_ok(board_mux_set_test(0));
+	test_reset();
+
+	zassert_ok(board_mux_set_test(1));
 }
