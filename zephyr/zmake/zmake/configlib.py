00540f9e4b7e3e18d5720406c503a02209ad648b
Keith Short <keithshort@chromium.org>
2023-09-26 13:38:48 -0600

    zmake: Ensure EC module is configured last
    Zephyr's Cmake processes modules in the order provided.  Modules
    typically add to the include path so that subsequent modules or the
    Zephyr application can access the module APIs.
    
    The EC application is configured and built as a module, so ensure the EC
    module is always specified last.
    
    BUG=none
    TEST=zmake compare-builds -a
    
    Change-Id: Idc576c405f1a9dca302faa3380d2d7fc625272e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4893900
    Tested-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Keith Short <keithshort@chromium.org>
    Auto-Submit: Keith Short <keithshort@chromium.org>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>

--- zephyr/zmake/zmake/configlib.py
+++ zephyr/zmake/zmake/configlib.py
@@ -17,6 +17,12 @@ def _register_project(**kwargs):
     )
 
 
+#
+# Note - the "ec" module must always be last in the list of modules.
+# This permits the EC code to access any public APIs from the included modules.
+#
+
+
 def register_host_project(**kwargs):
     """Register a project that runs on a posix host."""
     kwargs.setdefault("zephyr_board", "native_posix")
@@ -41,14 +47,14 @@ def register_binman_project(**kwargs):
 def register_npcx_project(**kwargs):
     """Register a project that uses NpcxPacker."""
     kwargs.setdefault("output_packer", zmake.output_packers.NpcxPacker)
-    kwargs.setdefault("modules", ["ec", "cmsis"])
+    kwargs.setdefault("modules", ["cmsis", "ec"])
     return register_binman_project(**kwargs)
 
 
 def register_mchp_project(**kwargs):
     """Register a project that uses MchpPacker."""
     kwargs.setdefault("output_packer", zmake.output_packers.MchpPacker)
-    kwargs.setdefault("modules", ["ec", "cmsis"])
+    kwargs.setdefault("modules", ["cmsis", "ec"])
     return register_binman_project(**kwargs)
 
 
@@ -56,5 +62,5 @@ def register_ish_project(**kwargs):
     """Register a project that uses IshBinPacker."""
     kwargs.setdefault("supported_toolchains", ["coreboot-sdk", "zephyr"])
     kwargs.setdefault("output_packer", zmake.output_packers.IshBinPacker)
-    kwargs.setdefault("modules", ["ec", "ish", "cmsis", "hal_intel_public"])
+    kwargs.setdefault("modules", ["ish", "cmsis", "hal_intel_public", "ec"])
     return _register_project(**kwargs)
