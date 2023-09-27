e8dbb35ec7859d05d87ed899b7f38d9d7922e562
Jay Patel <jay2.patel@intel.com>
2023-09-27 00:30:25 -0700

    intel altmode: Add support for TBT/USB4 mode mux
    Added code to support MUX for TBT/USB4 mode to configure the MUXes
    on Intel SOC.
    
    BUG=b:295917619
    TEST=Able to negotiate TBT/USB4 using CCG8-PD & MTL-RVP
    LOW_COVERAGE_REASON=Likely PMC to PD communication is POR, b:295924125
    
    Change-Id: I28361c225789afa00104254e9bcd3db840a9c2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4898924
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jay Patel <jay2.patel@intel.com>
    Reviewed-by: Sam Hurst <shurst@google.com>
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Commit-Queue: Jay Patel <jay2.patel@intel.com>

--- zephyr/subsys/usb_pd/pd_task_intel_altmode.c
+++ zephyr/subsys/usb_pd/pd_task_intel_altmode.c
@@ -10,6 +10,7 @@
 #include "i2c.h"
 #include "i2c/i2c.h"
 #include "usb_mux.h"
+#include "usb_pd.h"
 #include "usbc/utils.h"
 
 #include <stdlib.h>
@@ -167,6 +168,17 @@ static void process_altmode_pd_data(int port)
 
 	if (status.dp_irq)
 		mux |= USB_PD_MUX_HPD_IRQ;
+#endif
+
+#ifdef CONFIG_PLATFORM_EC_USB_PD_TBT_COMPAT_MODE
+	/* TBT status */
+	if (status.tbt)
+		mux |= USB_PD_MUX_TBT_COMPAT_ENABLED;
+#endif
+
+#ifdef CONFIG_PLATFORM_EC_USB_PD_USB4
+	if (status.usb4)
+		mux |= USB_PD_MUX_USB4_ENABLED;
 #endif
 
 	LOG_INF("Set p%d mux=0x%x", port, mux);
@@ -347,6 +359,18 @@ int pd_is_connected(int port)
 __override uint8_t get_dp_pin_mode(int port)
 {
 	return intel_altmode_task_data.data_status[port].dp_pin << 2;
+}
+#endif
+
+#ifdef CONFIG_PLATFORM_EC_USB_PD_TBT_COMPAT_MODE
+enum tbt_compat_cable_speed get_tbt_cable_speed(int port)
+{
+	return intel_altmode_task_data.data_status[port].cable_speed;
+}
+
+enum tbt_compat_rounded_support get_tbt_rounded_support(int port)
+{
+	return intel_altmode_task_data.data_status[port].cable_gen;
 }
 #endif
 
