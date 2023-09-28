cc1b138ec65546076303ecc3088fda04b3a3ef11
Bobby Casey <bobbycasey@google.com>
2023-09-28 13:45:32 -0400

    timer: Add tests for timestamp_expired
    BUG=none
    TEST=make BOARD=host run-timer
    TEST=run_device_tests.py --board helipilot -t timer
    
    Change-Id: I379ef86a46c6a28dc2c54d12a9f9217e8411c494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4902504
    Commit-Queue: Bobby Casey <bobbycasey@google.com>
    Reviewed-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Bobby Casey <bobbycasey@google.com>

--- test/timer.cc
+++ test/timer.cc
@@ -47,12 +47,57 @@ test_static int test_usleep(void)
 	return EC_SUCCESS;
 }
 
+/* When timestamp_expired is called with NULL for the second parameter,
+ * get_time() should be used for the "now" value.
+ */
+test_static int test_timestamp_expired(void)
+{
+	/* Set an arbitrary time for "now", all times will be relative to now */
+	timestamp_t now = { .val = 2 * HOUR };
+	timestamp_t deadline;
+
+	/* set the deadline in the past, verify expired*/
+	deadline.val = now.val - 1;
+	TEST_ASSERT(timestamp_expired(deadline, &now));
+
+	/* set the deadline in the now, verify expired*/
+	deadline.val = now.val;
+	TEST_ASSERT(timestamp_expired(deadline, &now));
+
+	/* set the deadline in the future, verify not expired*/
+	deadline.val = now.val + 1;
+	TEST_ASSERT(!timestamp_expired(deadline, &now));
+
+	return EC_SUCCESS;
+}
+
+/* When timestamp_expired is called with NULL for the second parameter,
+ * get_time() should be used for the "now" value.
+ */
+test_static int test_timestamp_expired_null(void)
+{
+	timestamp_t deadline;
+
+	/* set the deadline in the past, verify expired */
+	deadline.val = get_time().val - 1;
+	TEST_ASSERT(timestamp_expired(deadline, NULL));
+
+	/* set the deadline to far enough in the future that it will not expire,
+	 * verify not expired */
+	deadline.val = get_time().val + SECOND;
+	TEST_ASSERT(!timestamp_expired(deadline, NULL));
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	test_reset();
 	watchdog_reload();
 
 	RUN_TEST(test_usleep);
+	RUN_TEST(test_timestamp_expired);
+	RUN_TEST(test_timestamp_expired_null);
 
 	test_print_result();
 }
