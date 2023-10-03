893d32f0cdcd75b8aa8ecc26e29cdfe31a7fe9ef
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2023-10-03 11:09:24 +0800

    Craaskov: Support Canadian French keyboard (ANSI type)
    This patch change key scancode if KEYBOARD field of fw_config is value 1.
    
    BUG=b:298722285
    TEST=zmake build craaskov
    LOW_COVERAGE_REASON=keyboard test success, use b/316993425 to tracking test.
    
    Change-Id: I662b19c2831aa1dce4b8d974e132ffc5631d24d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4907852
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Tested-by: Ridden Liu <ridden_liu@compal.corp-partner.google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>

--- zephyr/program/nissa/craaskov/src/keyboard.c
+++ zephyr/program/nissa/craaskov/src/keyboard.c
@@ -3,9 +3,15 @@
  * found in the LICENSE file.
  */
 
+#include "cros_cbi.h"
 #include "ec_commands.h"
+#include "hooks.h"
+#include "keyboard_8042_sharedlib.h"
+#include "keyboard_customization.h"
 #include "keyboard_scan.h"
 #include "timer.h"
+
+LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 
 static const struct ec_response_keybd_config craaskov_kb = {
 	.num_top_row_keys = 10,
@@ -56,3 +62,27 @@ __override const struct key {
 	{ .row = 0, .col = 11 }, /* T15 */
 };
 BUILD_ASSERT(ARRAY_SIZE(vivaldi_keys) == MAX_TOP_ROW_KEYS);
+
+/*
+ * Keyboard layout decided by FW config.
+ */
+static void kb_layout_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_TYPE, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_TYPE);
+		return;
+	}
+	/*
+	 * If keyboard is ANSI(KEYBOARD_ANSI), we need translate make code 64
+	 * to 45.And translate 29 to 42
+	 */
+	if (val == FW_KB_TYPE_ANSI_CANADIAN) {
+		set_scancode_set2(4, 0, get_scancode_set2(2, 7));
+		set_scancode_set2(3, 11, get_scancode_set2(4, 10));
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_layout_init, HOOK_PRIO_POST_FIRST);
