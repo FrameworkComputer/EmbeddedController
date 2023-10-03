e7ea84035dfe59c1ff5765be09174a4ac7d80971
Fabio Baltieri <fabiobaltieri@google.com>
2023-10-03 11:34:25 +0000

    zephyr: tasks: disable task creation division-by-zero warning on LLVM
    LLVM trigger a harmless division-by-zero on SYS_TIMEOUT_MS, due to an
    element of z_tmcvt_64 resulting in a "/ ((1000) / (10000))" in some
    specific tick and clock configuration on some platforms.
    
    The resulting value is verfied to be calculated correctly:
    
    (gdb) p _k_thread_data_RWSIG.init_delay
    $1 = {ticks = -1}
    
    So the warning is harmless here as the condition never kicks in.
    
    BUG=b:303207178
    TEST=verified the output structure with gdb
    
    Cq-Depend: chromium:4905538
    Change-Id: I47f7d19c2edb49bbc77af32ace854da72efd2098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4907933
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

--- zephyr/subsys/ap_pwrseq/ap_pwrseq.c
+++ zephyr/subsys/ap_pwrseq/ap_pwrseq.c
@@ -206,9 +206,23 @@ DEVICE_DEFINE(ap_pwrseq_dev, "ap_pwrseq_drv", ap_pwrseq_driver_init, NULL,
 	      &ap_pwrseq_task_data, NULL, APPLICATION,
 	      CONFIG_APPLICATION_INIT_PRIORITY, NULL);
 
+#if defined(__llvm__)
+/*
+ * b/303207178 LLVM trigger a harmless division-by-zero on SYS_TIMEOUT_MS, due
+ * to an element of z_tmcvt_64 resulting in a "/ ((1000) / (10000))" in some
+ * specific tick and clock configuration on some platforms.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdivision-by-zero"
+#endif
+
 K_THREAD_DEFINE(ap_pwrseq_tid, CONFIG_AP_PWRSEQ_STACK_SIZE, ap_pwrseq_thread,
 		DEVICE_GET(ap_pwrseq_dev), NULL, NULL,
 		CONFIG_AP_PWRSEQ_THREAD_PRIORITY, 0, K_TICKS_FOREVER);
+
+#if defined(__llvm__)
+#pragma GCC diagnostic pop
+#endif
 
 static int ap_pwrseq_driver_init(const struct device *dev)
 {
