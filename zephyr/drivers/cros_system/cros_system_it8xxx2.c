48e9ab12b539c69768c8a7933696bcbe1f326563
Peter Marheine <pmarheine@chromium.org>
2023-10-19 15:29:26 +1100

    it8xxx2: report both reset causes if undifferentiable
    Always returning power-up reset cause is problematic for some use cases
    like handling entry to recovery (which only accepts a reset-pin reset).
    It's incorrect to report both causes for any single reset, but this at
    least allows recovery to work and generally returns the reset cause
    behavior on this chip to what it was prior to
    https://crrev.com/c/3606030.
    
    BUG=b:306057929,b:306270762
    TEST=builds
    LOW_COVERAGE_REASON=Chip-specific drivers are not currently testable
                        via emulation.
    
    Change-Id: If0afbd4ea665f675990c702192ec7b15ffdcfa1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4954181
    Tested-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- zephyr/drivers/cros_system/cros_system_it8xxx2.c
+++ zephyr/drivers/cros_system/cros_system_it8xxx2.c
@@ -99,9 +99,17 @@ static int cros_system_it8xxx2_get_reset_cause(const struct device *dev)
 		/*
 		 * We can't differentiate between power-on and reset pin because
 		 * LRSIWR is set on both ~WRST assertion and power-on, and LRS
-		 * is either 0 or 1 in both cases. Conservatively treat both as
-		 * power-on.
+		 * is either 0 or 1 in both cases.
+		 *
+		 * Some EC code paths care about only one of these options,
+		 * so we force both causes to be reported (via
+		 * system_set_reset_flags() behind our caller's back) even
+		 * though in reality it had to be only one of them because
+		 * being unable to report a hard reset breaks some
+		 * functionality, as would being unable to report power-on
+		 * reset.
 		 */
+		system_set_reset_flags(EC_RESET_FLAG_RESET_PIN);
 		return POWERUP;
 	}
 	return UNKNOWN_RST;
