716ba18877ed5dcc3a9c34a6740bff3ffb6d95b7
Al Semjonovs <asemjonovs@google.com>
2023-10-23 17:25:03 -0600

    zephyr: Update delimiters used for tokenizing
    Change delimiter to rarely used characters in EC logging.
    
    BUG=b:287264165
    TEST=Validated via servod and timberslide output
    
    Change-Id: I082669fccbddf98306908288a3a36ae833a2dad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4968882
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Al Semjonovs <asemjonovs@google.com>
    Tested-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>

--- zephyr/include/pigweed/pw_zephyr_tokenized_custom_log.h
+++ zephyr/include/pigweed/pw_zephyr_tokenized_custom_log.h
@@ -11,4 +11,11 @@
  */
 #define PW_LOG_TOKENIZED_FORMAT_STRING(string) string
 
+/* Use a rarely used char in EC logging as tokenizer prefix
+ * If prefix changes, make sure to update the following files to match
+ *  -- src/third_party/hdctools/servo/ec3po/console.py
+ *  -- src/platform2/timberslide/token_config.h
+ */
+#define PW_TOKENIZER_NESTED_PREFIX_STR "`"
+
 #endif /* __PW_ZEPHYR_TOKENIZED_CUSTOM_LOG_H */
--- zephyr/shim/src/pw_log_zephyr_tokenized.cc
+++ zephyr/shim/src/pw_log_zephyr_tokenized.cc
@@ -22,7 +22,10 @@ namespace
 	// The Zephyr console may output raw text along with Base64 tokenized
 	// messages, which could interfere with detokenization. Output a
 	// character to mark the end of a Base64 message.
-	constexpr char kEndDelimiter = '\n';
+	// If delimiter changes, make sure to update the following files to
+	// match
+	//  -- src/third_party/hdctools/servo/ec3po/console.py
+	constexpr char kEndDelimiter = '~';
 
 	struct k_spinlock lock;
 	k_spinlock_key_t key;
@@ -42,17 +45,17 @@ extern "C" void pw_log_tokenized_HandleLog(uint32_t metadata,
 		return;
 	}
 
-	base64_string += kEndDelimiter;
+	// On DUT, timberslide doesn't receive console raw text, okay to send
+	// base64 message without end delimiter
 	console_buf_notify_chars(base64_string.c_str(), base64_string.size());
+	base64_string += kEndDelimiter;
 
 	// TODO(asemjonovs):
 	// https://github.com/zephyrproject-rtos/zephyr/issues/59454 Zephyr
 	// frontend should protect messages from getting corrupted from multiple
 	// threads.
 	key = k_spin_lock(&lock);
-
 	LOG_PRINTK("%s", base64_string.c_str());
-
 	k_spin_unlock(&lock, key);
 }
 
