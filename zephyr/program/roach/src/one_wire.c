4d7f5362efdd766c27d2e64a9daecce52e02edb7
Ting Shen <phoenixshen@google.com>
2023-10-24 14:41:52 +0800

    geralt/roach: remove uart register hack
    These register change is now implemented in the driver.
    
    BUG=b:300403990
    TEST=1) manually check register value
         2) test uart communication between new fw and old fw to ensure
            compatibility
    
    Change-Id: Icd49430a8e1658e9ec2e796810d1c3f72e7578ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4971195
    Reviewed-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/roach/src/one_wire.c
+++ zephyr/program/roach/src/one_wire.c
@@ -3,23 +3,8 @@
  * found in the LICENSE file.
  */
 
-#include "hooks.h"
-
 /* LCOV_EXCL_START empty function stub required for build */
 void board_touchpad_reset(void)
 {
 }
 /* LCOV_EXCL_END */
-
-static void ec_ec_comm_init(void)
-{
-	/* b/300403990: put these here before driver ready */
-	uint8_t lcr_cache = *(volatile uint8_t *)0xf02803;
-
-	*(volatile uint8_t *)0xf02803 |= 0x80; /* access divisor latches */
-	*(volatile uint8_t *)0xf02800 = 0x01; /* set divisor = 0x8001 */
-	*(volatile uint8_t *)0xf02801 = 0x80;
-	*(volatile uint8_t *)0xf02803 = lcr_cache;
-	*(volatile uint8_t *)0xf02808 = 2; /* high speed select */
-}
-DECLARE_HOOK(HOOK_INIT, ec_ec_comm_init, HOOK_PRIO_DEFAULT);
