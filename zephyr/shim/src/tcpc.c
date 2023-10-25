37d69d078ed64f9cd0dc586f47380f56a6416520
Jeremy Bettis <jbettis@google.com>
2023-10-25 21:00:03 +0000

    ec: Add #line to zephyr/shim/src/tcpc.c
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E zephyr/shim/src/tcpc.c
    
    Change-Id: I10fa6422c8ee235f0d73acdcebf382fd4e923167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4977011
    Commit-Queue: Simon Glass <sjg@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/shim/src/tcpc.c
+++ zephyr/shim/src/tcpc.c
@@ -3,6 +3,12 @@
  * found in the LICENSE file.
  */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 11
+
 #include "hooks.h"
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
