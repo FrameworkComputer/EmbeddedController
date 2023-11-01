b05df555dc195952dc14ca205519e8ccefb0e413
Keith Short <keithshort@chromium.org>
2023-11-01 17:37:01 -0600

    it8xxx2_evb: Reorganize project files
    Reorganize the it8xxx2_evb project files to follow the convention
    detailed in Zephyr project configuration documentation.
    
    BUG=none
    TEST=zmake compare-builds it8xxx2_evb
    
    Change-Id: I021ca51457d07144b00f3d14c08639d68e443180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5002568
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Tested-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/it8xxx2_evb/BUILD.py
+++ zephyr/program/it8xxx2_evb/BUILD.py
@@ -4,16 +4,23 @@
 
 """Define zmake projects for it8xxx2_evb."""
 
-register_raw_project(
-    project_name="it8xxx2_evb",
-    zephyr_board="it81302bx",
-    dts_overlays=[
-        "adc.dts",
-        "fan.dts",
-        "gpio.dts",
-        "i2c.dts",
-        "interrupts.dts",
-        "pwm.dts",
-        "shi.dts",
-    ],
+
+def register_it8xxx2_evb_project(project_name, zephyr_board):
+    """Register a variant of the it8xxx2_evb"""
+    register_raw_project(
+        project_name=project_name,
+        zephyr_board=zephyr_board,
+        # Project-specific devicetree overlay
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        # Project-specific KConfig customization.
+        kconfig_files=[
+            here / project_name / "project.conf",
+        ],
+    )
+
+
+register_it8xxx2_evb_project(
+    project_name="it8xxx2_evb", zephyr_board="it81302bx"
 )
