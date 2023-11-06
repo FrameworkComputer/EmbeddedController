f921fa40d578e4296e68fb901245739f1e8da5a4
Jes B. Klinke <jbk@chromium.org>
2023-11-06 12:07:24 -0800

    board/hyperdebug: Support slow I2C clock speeds
    When using HyperDebug on some development boards that lack proper I2C
    pullup, we may want to set the clock speed slower than 100kbps, which is
    currently the lowest setting.
    
    This CL makes use of I2C clock prescaling 1-16x using the high 4 bits of
    the TIMINGR register to be able to scale the pre-calculated high/low
    timing values for 100kbps down to 50kbps, ..., 6.26kbps.
    
    In order to achieve the above, the logic of chip_i2c_set_freq() has been
    copied into board/hyperdebug/i2c.c, as this functionality is probably
    not something we want to support in the general EC codebase.  (It would
    require replacing `enum i2c_freq` with an integer, which would be
    non-trivial.)
    
    BUG=b:309485648
    TEST=observe I2C signal on logic analyzer
    
    Change-Id: I86c58efd31acaaace6a04e15b5962079233529b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5008169
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Commit-Queue: Jes Klinke <jbk@chromium.org>
    Tested-by: Jes Klinke <jbk@chromium.org>

--- board/hyperdebug/i2c.c
+++ board/hyperdebug/i2c.c
@@ -9,6 +9,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "registers.h"
 #include "util.h"
 
 /* I2C ports */
@@ -40,6 +41,8 @@ const struct i2c_port_t i2c_ports[] = {
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
+static uint32_t i2c_ports_bits_per_second[ARRAY_SIZE(i2c_ports)];
+
 int usb_i2c_board_is_enabled(void)
 {
 	return 1;
@@ -69,10 +72,8 @@ static int find_i2c_by_name(const char *name)
 
 static void print_i2c_info(int index)
 {
-	uint32_t bits_per_second = 1000 * i2c_freq_to_khz(i2c_get_freq(index));
-
 	ccprintf("  %d %s %d bps\n", index, i2c_ports[index].name,
-		 bits_per_second);
+		 i2c_ports_bits_per_second[index]);
 
 	/* Flush console to avoid truncating output */
 	cflush();
@@ -105,6 +106,55 @@ static int command_i2c_info(int argc, const char **argv)
 	return EC_SUCCESS;
 }
 
+/*
+ * Constants copied from i2c-stm32l4.c, for 16MHz base frequency.
+ */
+static const uint32_t TIMINGR_I2C_FREQ_1000KHZ = 0x00000107;
+static const uint32_t TIMINGR_I2C_FREQ_400KHZ = 0x00100B15;
+static const uint32_t TIMINGR_I2C_FREQ_100KHZ = 0x00303D5B;
+
+/*
+ * This function builds on a similar function in i2c-stm32l4.c, adding support
+ * for non-standard speeds slower than 100kbps.
+ */
+static void board_i2c_set_speed(int port, uint32_t desired_speed)
+{
+	i2c_lock(port, 1);
+
+	/* Disable port */
+	STM32_I2C_CR1(port) = 0;
+
+	if (desired_speed >= 1000000) {
+		/* Set clock frequency */
+		STM32_I2C_TIMINGR(port) = TIMINGR_I2C_FREQ_1000KHZ;
+		i2c_ports_bits_per_second[port] = 1000000;
+	} else if (desired_speed >= 400000) {
+		STM32_I2C_TIMINGR(port) = TIMINGR_I2C_FREQ_400KHZ;
+		i2c_ports_bits_per_second[port] = 400000;
+	} else {
+		/*
+		 * The code below uses the above constant meant for 100kbps I2C
+		 * clock, and possibly modifies the prescaling value, to divide
+		 * the frequency with an integer in the range 1-16.  It will
+		 * find the closest I2C frequency in the range 6.25kbps -
+		 * 100kbps which is not faster than the requested speed, except
+		 * if the requested speed is slower than the slowest supported
+		 * value.
+		 */
+		int divisor = 100000 / (desired_speed + 1);
+		if (divisor > 15)
+			divisor = 15;
+		STM32_I2C_TIMINGR(port) = TIMINGR_I2C_FREQ_100KHZ |
+					  (divisor << 28);
+		i2c_ports_bits_per_second[port] = 100000 / (divisor + 1);
+	}
+
+	/* Enable port */
+	STM32_I2C_CR1(port) = STM32_I2C_CR1_PE;
+
+	i2c_lock(port, 0);
+}
+
 static int command_i2c_set_speed(int argc, const char **argv)
 {
 	int index;
@@ -121,13 +171,8 @@ static int command_i2c_set_speed(int argc, const char **argv)
 	if (*e)
 		return EC_ERROR_PARAM4;
 
-	if (desired_speed >= 1000000) {
-		i2c_set_freq(index, I2C_FREQ_1000KHZ);
-	} else if (desired_speed >= 400000) {
-		i2c_set_freq(index, I2C_FREQ_400KHZ);
-	} else {
-		i2c_set_freq(index, I2C_FREQ_100KHZ);
-	}
+	board_i2c_set_speed(index, desired_speed);
+
 	return EC_SUCCESS;
 }
 
@@ -159,7 +204,16 @@ DECLARE_CONSOLE_COMMAND_FLAGS(i2c, command_i2c,
 static void i2c_reinit(void)
 {
 	for (unsigned int i = 0; i < i2c_ports_used; i++) {
-		i2c_set_freq(i, I2C_FREQ_100KHZ);
+		board_i2c_set_speed(i, i2c_ports[i].kbps * 1000);
+		i2c_ports_bits_per_second[i] = i2c_ports[i].kbps * 1000;
 	}
 }
 DECLARE_HOOK(HOOK_REINIT, i2c_reinit, HOOK_PRIO_DEFAULT);
+
+static void board_i2c_init(void)
+{
+	for (unsigned int i = 0; i < i2c_ports_used; i++) {
+		i2c_ports_bits_per_second[i] = i2c_ports[i].kbps * 1000;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_i2c_init, HOOK_PRIO_DEFAULT + 2);
