8a5eb998279fd831c6909cdbc96cb029f9e125f3
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2023-11-07 10:33:55 +0800

    craask: Add touch panel power sequence control
    To ensure that EC can quickly resolve touchscreen power sequence issues
    such as b/301372546, the touchscreen enable pin is routed to the EC.
    The EC will add a delay time when enabling the touchscreen.
    In the future, adjusting this delay may become an option for resolving
    touchscreen power sequence issues.
    
    BUG=b:313494799
    TEST=./twister -T zephyr/test/nissa
    
    Change-Id: I36778142b6dc2420a9599b83d19227424387602c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5004163
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Auto-Submit: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>

--- zephyr/test/nissa/src/craask.c
+++ zephyr/test/nissa/src/craask.c
@@ -1175,3 +1175,74 @@ ZTEST(craask, test_15w_thermal_solution)
 	zassert_equal(fan_get_rpm_mode(0), 1);
 	zassert_equal(fan_get_rpm_target(0), 0);
 }
+
+static bool cbi_touch_en;
+static bool cbi_read_fail;
+
+static int cbi_get_touch_en_config(enum cbi_fw_config_field_id field,
+				   uint32_t *value)
+{
+	if (field != FW_TOUCH_EN)
+		return -EINVAL;
+
+	if (cbi_read_fail)
+		return -1;
+
+	*value = cbi_touch_en ? FW_TOUCH_EN_ENABLE : FW_TOUCH_EN_DISABLE;
+	return 0;
+}
+
+#define TEST_DELAY_MS 1
+#define TOUCH_ENABLE_DELAY_MS (500 + TEST_DELAY_MS)
+#define TOUCH_DISABLE_DELAY_MS (0 + TEST_DELAY_MS)
+
+ZTEST(craask, test_touch_enable)
+{
+	const struct gpio_dt_spec *bl_en =
+		GPIO_DT_FROM_NODELABEL(gpio_soc_edp_bl_en);
+	const struct gpio_dt_spec *touch_en =
+		GPIO_DT_FROM_NODELABEL(gpio_ec_touch_en);
+
+	cbi_touch_en = true;
+	cbi_read_fail = false;
+	cros_cbi_get_fw_config_fake.custom_fake = cbi_get_touch_en_config;
+
+	hook_notify(HOOK_INIT);
+
+	/* touch_en become high after TOUCH_ENABLE_DELAY_MS delay */
+	zassert_ok(gpio_emul_input_set(bl_en->port, bl_en->pin, 1), NULL);
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	k_sleep(K_MSEC(TOUCH_ENABLE_DELAY_MS));
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 1);
+
+	/* touch_en become low after TOUCH_DISABLE_DELAY_MS delay */
+	zassert_ok(gpio_emul_input_set(bl_en->port, bl_en->pin, 0), NULL);
+	k_sleep(K_MSEC(TOUCH_DISABLE_DELAY_MS));
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	/* touch_en keep low if fw_config is not enabled */
+	cbi_touch_en = false;
+	gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_soc_edp_bl_en));
+	hook_notify(HOOK_INIT);
+
+	zassert_ok(gpio_emul_input_set(bl_en->port, bl_en->pin, 1), NULL);
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	k_sleep(K_MSEC(TOUCH_ENABLE_DELAY_MS));
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	/* touch_en keep low if fw_config read fail */
+	cbi_read_fail = true;
+	gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_soc_edp_bl_en));
+	hook_notify(HOOK_INIT);
+
+	zassert_ok(gpio_emul_input_set(bl_en->port, bl_en->pin, 0), NULL);
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	zassert_ok(gpio_emul_input_set(bl_en->port, bl_en->pin, 1), NULL);
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+
+	k_sleep(K_MSEC(TOUCH_ENABLE_DELAY_MS));
+	zassert_equal(gpio_emul_output_get(touch_en->port, touch_en->pin), 0);
+}
