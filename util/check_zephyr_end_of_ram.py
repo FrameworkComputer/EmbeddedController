52efe97d28c4555354ef401027dd03021cf133e0
Tristan Honscheid <honscheid@google.com>
2023-11-09 12:07:45 -0700

    util: Remove nereid, gothrax, pirrha from end-of-ram check
    These boards are no longer showing the end of ram size check
    issue.
    
    BUG=b:289320553
    BUG=b:289320515
    BUG=b:309980756
    TEST=zmake build nereid; zmake build gothrax; zmake build pirrha
    
    Change-Id: I3334c984e9c55a2f5f5cce33c49b557130b48853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5018636
    Auto-Submit: Tristan Honscheid <honscheid@google.com>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>

--- util/check_zephyr_end_of_ram.py
+++ util/check_zephyr_end_of_ram.py
@@ -25,14 +25,9 @@ from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
 
-EXCLUDED_BOARDS = [
-    # Only 116 bytes of free RAM (b/289320553)
-    "gothrax",
-    # Only 800 bytes of free RAM (b/289320515)
-    "nereid",
-    # Only 948 bytes of free RAM
-    "pirrha",
-]
+# Additions to this list MUST contain a reference to a bug for tracking
+# the fix for that board.
+EXCLUDED_BOARDS = []
 
 
 class BuildInfo:
