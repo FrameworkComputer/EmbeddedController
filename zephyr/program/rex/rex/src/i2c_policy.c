85e36e7e940b79c2de938d42c11a71303e4f1cd5
Caveh Jalali <caveh@chromium.org>
2023-11-10 19:10:16 -0800

    rex: Enable I2C_PASSTHRU_RESTRICTED
    Enable the I2C pass-through policy for all rex boards. The initial
    policy is to allow all access which is the same behavior as before.
    Each board will need to specify its own custom policy since I2C devices
    vary significantly between boards.
    
    BUG=b:306684154
    TEST='zmake build -a' passes
    TEST='./twister -T zephyr/test/rex' passes
    
    Change-Id: I3c122bfba57a721f8465af6ed10cc7ac031ddf6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5021021
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: caveh jalali <caveh@chromium.org>
    Auto-Submit: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>

---
+++ zephyr/program/rex/rex/src/i2c_policy.c
@@ -0,0 +1,16 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "i2c.h"
+#include "stdbool.h"
+
+int board_allow_i2c_passthru(const struct i2c_cmd_desc_t *cmd_desc)
+{
+	/*
+	 * TODO: Add permissions check
+	 */
+
+	return true;
+}
