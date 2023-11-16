e277285784f9d1d2e0c4e6696ec0c4b333e303a9
Caveh Jalali <caveh@chromium.org>
2023-11-15 17:53:07 -0800

    rex/rex: Update I2C pass-through policy
    This makes the I2C pass-through policy more restrictive. In this round,
    we lock down at the I2C bus level. I2C busses are locked down if they do
    not connect a target that is known to be firmware upgradable.
    
    BUG=b:311285192
    TEST='zmake build rex' passes
    TEST='./twister -T zephyr/test/rex' passes
    TEST=boots on rex
    
    Change-Id: I9588fe11f60bb33ab2ab256d723d838abefbcae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5035543
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Auto-Submit: caveh jalali <caveh@chromium.org>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: caveh jalali <caveh@chromium.org>

--- zephyr/program/rex/rex/src/i2c_policy.c
+++ zephyr/program/rex/rex/src/i2c_policy.c
@@ -3,14 +3,60 @@
  * found in the LICENSE file.
  */
 
+#include "cros_cbi.h"
 #include "i2c.h"
 #include "stdbool.h"
 
+#include <dt-bindings/gpio_defines.h>
+
 int board_allow_i2c_passthru(const struct i2c_cmd_desc_t *cmd_desc)
 {
-	/*
-	 * TODO: Add permissions check
-	 */
+	const uint8_t req_port = cmd_desc->port;
 
-	return true;
+	switch (req_port) {
+	case I2C_PORT_BY_DEV(DT_NODELABEL(tcpc_ps8815_port1)): {
+		/*
+		 * i2c4_1:
+		 * The PS8815 TCPC at C1 is FW upgradable from the AP.
+		 * Other TCPCs are not upgradable.
+		 */
+
+		uint32_t usb_db_type;
+
+		if (cros_cbi_get_fw_config(FW_USB_DB, &usb_db_type) !=
+		    EC_SUCCESS) {
+			return false;
+		}
+
+		if (usb_db_type == FW_USB_DB_USB3)
+			return true;
+
+		break;
+	}
+
+	case I2C_PORT_BY_DEV(DT_NODELABEL(usb_c1_anx7452_retimer)): {
+		/*
+		 * i2c6_1:
+		 * The ANX7452 retimer at C1 is FW upgradable from the AP.
+		 * Other targets are not upgradable.
+		 */
+
+		uint32_t usb_db_type;
+
+		if (cros_cbi_get_fw_config(FW_USB_DB, &usb_db_type) !=
+		    EC_SUCCESS) {
+			return false;
+		}
+
+		switch (usb_db_type) {
+		case FW_USB_DB_USB4_ANX7452_V2:
+			return true;
+		}
+
+		break;
+	}
+	}
+
+	/* All remaining targets are not allowed */
+	return false;
 }
