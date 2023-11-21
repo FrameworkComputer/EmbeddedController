52cdb67801a7674987deabfce0dbb778f4e98764
Fabio Baltieri <fabiobaltieri@google.com>
2023-11-21 15:51:53 +0000

    zephyr: keyboard_input: add col gpio handling support
    Add a binding and option to implement the
    input_kbd_matrix_drive_column_hook function, this can be used for
    handling the KSO_02_INV signal, or any other inverted column drive
    signal.
    
    BUG=b:274443029
    TEST=zmake build rex -DCONFIG_INPUT=y \
    	-DCONFIG_CROS_EC_KEYBOARD_INPUT=y \
    	-DCONFIG_PLATFORM_EC_KEYBOARD_VIVALDI=n
    
    Change-Id: Id59bf115af24883f0fc4dd4b3d467da8f7ec28a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5049863
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/test/input_keyboard/src/main.c
+++ zephyr/test/input_keyboard/src/main.c
@@ -6,8 +6,11 @@
 #include "keyboard_scan.h"
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_emul.h>
 #include <zephyr/fff.h>
 #include <zephyr/input/input.h>
+#include <zephyr/input/input_kbd_matrix.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
 
@@ -81,6 +84,33 @@ ZTEST(keyboard_input, test_keyboard_input_enable_disable)
 	input_report_key(fake_dev, INPUT_BTN_TOUCH, 1, true, K_FOREVER);
 
 	zassert_equal(keyboard_state_changed_fake.call_count, 2);
+}
+
+ZTEST(keyboard_input, test_kso_gpio)
+{
+	const struct device *gpio_dev =
+		DEVICE_DT_GET(DT_GPIO_CTLR(DT_NODELABEL(kso_gpio), col_gpios));
+	gpio_pin_t pin = DT_GPIO_PIN(DT_NODELABEL(kso_gpio), col_gpios);
+	int col_num = DT_PROP(DT_NODELABEL(kso_gpio), col_num);
+
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 0);
+
+	input_kbd_matrix_drive_column_hook(NULL, col_num);
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 0);
+
+	input_kbd_matrix_drive_column_hook(fake_dev, col_num);
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 1);
+
+	input_kbd_matrix_drive_column_hook(fake_dev, col_num + 1);
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 0);
+
+	input_kbd_matrix_drive_column_hook(fake_dev,
+					   INPUT_KBD_MATRIX_COLUMN_DRIVE_ALL);
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 1);
+
+	input_kbd_matrix_drive_column_hook(fake_dev,
+					   INPUT_KBD_MATRIX_COLUMN_DRIVE_NONE);
+	zassert_equal(gpio_emul_output_get(gpio_dev, pin), 0);
 }
 
 static void reset(void *fixture)
