2552f125ca2f95d76ebde63c0c385b44b69f3706
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2023-11-21 14:20:32 +0800

    rex/screebo: Update I2C pass-through policy
    This makes the I2C pass-through policy more restrictive. In this round,
    we lock down at the I2C bus level. I2C busses are locked down if they do
    not connect a target that is known to be firmware upgradable.
    
    BUG=b:311283246
    TEST='zmake build screebo' passes
    TEST='./twister -T zephyr/test/rex' passes
    TEST=boots on screebo
    
    Change-Id: Ice4953660abb7df3ab0e7edbfa03dabc7628c805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5047615
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Tested-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: wen zhang <zhangwen6@huaqin.corp-partner.google.com>

--- zephyr/program/rex/screebo/src/i2c_policy.c
+++ zephyr/program/rex/screebo/src/i2c_policy.c
@@ -3,14 +3,26 @@
  * found in the LICENSE file.
  */
 
+#include "cros_cbi.h"
 #include "i2c.h"
 #include "stdbool.h"
 
 int board_allow_i2c_passthru(const struct i2c_cmd_desc_t *cmd_desc)
 {
-	/*
-	 * TODO(b/311283246): Add permissions check
-	 */
+	const uint8_t req_port = cmd_desc->port;
+	uint32_t usb_db_type;
 
-	return true;
+	/*
+	 * See i2c map in b:311283246.
+	 * Only USB3_DB with PS8815 TCPC is FW upgradable from the AP.
+	 */
+	if (req_port == I2C_PORT_BY_DEV(DT_NODELABEL(tcpc_ps8815_port1))) {
+		if (cros_cbi_get_fw_config(FW_USB_DB, &usb_db_type)) {
+			return false;
+		} else {
+			return usb_db_type == FW_USB_DB_USB3;
+		}
+	}
+
+	return false;
 }
