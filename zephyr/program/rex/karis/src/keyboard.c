79f9898dadba1c439daefe98313df37582a2765d
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-11-28 14:09:18 +0800

    Karis: enable CONFIG_KEYBOARD_FACTORY_TEST
    Support factory keyboard test through command 'ectool kbfactorytest'.
    
    BUG=b:313555339
    TEST='ectool kbfactorytest' can detect shorted pins.
    
    Change-Id: If8f9e159ede82f9f2fde441919a605900688c329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5065111
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>

--- zephyr/program/rex/karis/src/keyboard.c
+++ zephyr/program/rex/karis/src/keyboard.c
@@ -64,3 +64,19 @@ test_export_static void kb_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
+
+/*
+ * We have total 30 pins for keyboard connecter {-1, -1} mean
+ * the N/A pin that don't consider it and reserve index 0 area
+ * that we don't have pin 0.
+ */
+const int keyboard_factory_scan_pins[][2] = {
+	{ -1, -1 }, { 0, 5 },	{ 1, 1 }, { 1, 0 },   { 0, 6 },	  { 0, 7 },
+	{ -1, -1 }, { -1, -1 }, { 1, 4 }, { 1, 3 },   { -1, -1 }, { 1, 6 },
+	{ 1, 7 },   { 3, 1 },	{ 2, 0 }, { 1, 5 },   { 2, 6 },	  { 2, 7 },
+	{ 2, 1 },   { 2, 4 },	{ 2, 5 }, { 1, 2 },   { 2, 3 },	  { 2, 2 },
+	{ 3, 0 },   { -1, -1 }, { 0, 4 }, { -1, -1 }, { 8, 2 },	  { -1, -1 },
+	{ -1, -1 },
+};
+const int keyboard_factory_scan_pins_used =
+	ARRAY_SIZE(keyboard_factory_scan_pins);
