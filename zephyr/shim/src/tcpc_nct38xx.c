729d95ef153424d2219d1754da2551b65c0fa7b8
Robert Zieba <robertzieba@google.com>
2023-12-08 09:56:29 -0700

    zephyr/shim/tcpc_nct38xx: Add compile time check around gpio-dev
    There are currently no compile-time checks around the `gpio-dev`
    property of `nuvoton,nct38xx-tcpc` compatible devices. This can cause a
    crash or other undefined behavior if the wrong device is assigned. Add
    a compile time check to validate this property.
    
    BUG=b:312519939
    TEST=Skyrim build with incorrect device fails, normal build completes
    
    Change-Id: Ie965261dfae257c89995101d7f052c43bb25ba32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5106089
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Robert Zieba <robertzieba@google.com>
    Commit-Queue: Robert Zieba <robertzieba@google.com>
    Reviewed-by: Forest Mittelberg <bmbm@google.com>

--- zephyr/shim/src/tcpc_nct38xx.c
+++ zephyr/shim/src/tcpc_nct38xx.c
@@ -10,10 +10,12 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 
-#define GPIO_DEV_WITH_COMMA(id) DEVICE_DT_GET(DT_PHANDLE(id, gpio_dev)),
+#define GPIO_DEV_PROP gpio_dev
+
+#define GPIO_DEV_WITH_COMMA(id) DEVICE_DT_GET(DT_PHANDLE(id, GPIO_DEV_PROP)),
 
 #define GPIO_DEV_BINDING(usbc_id, tcpc_id)                                     \
-	COND_CODE_1(DT_NODE_HAS_PROP(tcpc_id, gpio_dev),                       \
+	COND_CODE_1(DT_NODE_HAS_PROP(tcpc_id, GPIO_DEV_PROP),                  \
 		    ([USBC_PORT_NEW(usbc_id)] = GPIO_DEV_WITH_COMMA(tcpc_id)), \
 		    ())
 
@@ -25,10 +27,37 @@
 	COND_CODE_1(DT_NODE_HAS_PROP(usbc_id, tcpc), \
 		    (NCT38XX_CHECK(usbc_id, DT_PHANDLE(usbc_id, tcpc))), ())
 
+/* Compile-time checks. */
+/* Validate the GPIO device referenced by the TCPC matches our GPIO compat. */
+/* clang-format off */
+#define NCT38XX_TCPC_GPIO_VALIDATE(usbc_id, tcpc_id, gpio_id)                  \
+	BUILD_ASSERT(DT_NODE_HAS_COMPAT(gpio_id, NCT38XX_GPIO_COMPAT),         \
+		DT_NODE_PATH(gpio_id) " referenced by property "               \
+		STRINGIFY(GPIO_DEV_PROP) " of " DT_NODE_PATH(tcpc_id)          \
+		" is not compatible with " STRINGIFY(NCT38XX_GPIO_COMPAT));
+/* clang-format on */
+
+/* TCPC device validation. */
+#define NCT38XX_TCPC_VALIDATE(usbc_id, tcpc_id)                              \
+	COND_CODE_1(DT_NODE_HAS_PROP(tcpc_id, GPIO_DEV_PROP),                \
+		    (NCT38XX_TCPC_GPIO_VALIDATE(usbc_id, tcpc_id,            \
+						DT_PHANDLE(tcpc_id,          \
+							   GPIO_DEV_PROP))), \
+		    ())
+
+/* Base validation macro. */
+#define NCT38XX_VALIDATE(usbc_id)                                            \
+	COND_CODE_1(                                                         \
+		DT_NODE_HAS_PROP(usbc_id, tcpc),                             \
+		(NCT38XX_TCPC_VALIDATE(usbc_id, DT_PHANDLE(usbc_id, tcpc))), \
+		())
+
 /* NCT38XX GPIO device pool for binding the TCPC port and NCT38XX GPIO device */
 static const struct device *nct38xx_gpio_devices[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	DT_FOREACH_STATUS_OKAY(named_usbc_port, NCT38XX_GPIO)
 };
+
+DT_FOREACH_STATUS_OKAY(named_usbc_port, NCT38XX_VALIDATE)
 
 const struct device *nct38xx_get_gpio_device_from_port(const int port)
 {
