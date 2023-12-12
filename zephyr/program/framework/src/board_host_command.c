009a24fe2aecc7dbc289c00113c5c2ae6777a8d4
Josh-Tsai <josh_tsai@compal.com>
2023-12-12 14:51:45 +0800

    marigold: add the bb retimer host command (no function)
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- zephyr/program/framework/src/board_host_command.c
+++ zephyr/program/framework/src/board_host_command.c
@@ -474,7 +474,47 @@ static enum ec_status disable_ps2_mouse_emulation(struct host_cmd_handler_args *
 }
 DECLARE_HOST_COMMAND(EC_CMD_DISABLE_PS2_EMULATION, disable_ps2_mouse_emulation, EC_VER_MASK(0));
 
-#endif
+#ifdef PD_CHIP_CCG6
+static enum ec_status bb_retimer_control(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_bb_retimer_control_mode *p = args->params;
+	struct ec_response_bb_retimer_control_mode *r = args->response;
+
+	CPRINTS("TODO: bb-retimer interface :%d", p->controller);
+	r->status = 0;
+	args->response_size = sizeof(*r);
+
+	/**
+	 * TODO: wait CCG6 interface
+	 * Note: retimer control register is multi declared in CCG8 and CCG6
+	 */
+
+	/*switch (p->modes) {
+	case BB_ENTRY_FW_UPDATE_MODE:
+		entry_tbt_mode(p->controller);
+		break;
+	case BB_EXIT_FW_UPDATE_MODE:
+		exit_tbt_mode(p->controller);
+		break;
+	case BB_ENABLE_COMPLIANCE_MODE:
+		enable_compliance_mode(p->controller);
+		break;
+	case BB_DISABLE_COMPLIANCE_MODE:
+		disable_compliance_mode(p->controller);
+		break;
+	case BB_CHECK_STATUS:
+		r->status = check_tbt_mode(p->controller);
+		args->response_size = sizeof(*r);
+		break;
+	default:
+		return EC_RES_INVALID_PARAM;
+	}*/
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_BB_RETIMER_CONTROL, bb_retimer_control, EC_VER_MASK(0));
+#endif /* PD_CHIP_CCG6 */
+#endif /* CONFIG_CHIPSET_INTEL */
 /*******************************************************************************/
 /*                       EC console command for Project                        */
 /*******************************************************************************/
