ee983a643b32a5cc6fe79fdc3fc8fdab413b6dd3
Jeremy Bettis <jbettis@google.com>
2023-12-13 12:23:28 -0700

    ec: Fix bad copyright header
    BUG=b:313704200
    TEST=repo upload
    
    Change-Id: I44c8c662723449d695074f7962253432bf3f17cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5118515
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/include/drivers/ucsi_v3.h
+++ zephyr/include/drivers/ucsi_v3.h
@@ -1,5 +1,6 @@
 /* Copyright 2023 The ChromiumOS Authors
- * SPDX-License-Identifier: Apache-2.0
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 /**
