9c4a42d1b4cee35624000d6495ba16f87c4d1ffd
Jeremy Bettis <jbettis@google.com>
2023-12-15 17:15:04 +0000

    ec: Add #line to zephyr/shim/src/ppc.c
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E zephyr/shim/src/ppc.c
    
    Change-Id: I256bb0c8fa31296b657dc18fcd7c14c0aab2cc2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5126263
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/shim/src/ppc.c
+++ zephyr/shim/src/ppc.c
@@ -3,6 +3,12 @@
  * found in the LICENSE file.
  */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 11
+
 #include "hooks.h"
 #include "usbc/ppc.h"
 #include "usbc/ppc_aoz1380.h"
