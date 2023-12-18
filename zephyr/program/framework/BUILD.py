39722d9a1e294dfa0966a223a0ae8394c7795e19
Dustin L. Howett <dustin@howett.net>
2023-12-17 22:07:03 -0600

    fwk: lotus,azalea: officially reserve the top 4096b of flash for SPI_FLAGS
    Right now, the configuration storage support in
    lotus/src/flash_storage.c claims overship of the top 0x1000 bytes of
    flash and stores non-volatile configuration data in it.
    
    However, the last 0x20 bytes of flash contain the RW_FWID (RW version)
    section. In addition, RW_FW is specified as covering the entire top
    0x40000 bytes of flash.
    
    In short, flash looks roughly like this (not to scale)
    
    +----------------------------------+-40000------------~----------------+
    | RO_FW                            | RW_FW                             |
    |                          RO_FRID |                           RW_FWID |
    +----------------------------------+------------------~--------^-7FFE0-+
    
    flash_storage can, if it grows large enough, corrupt the RW version
    ID. If the RW firmware _also_ grows large enough, flash_storage can
    overwrite part of it (!).
    
    +----------------------------------+-40000------------~--+-7F000-------+
    | RO_FW                            | RW_FW               |spi flag spi |
    |                          RO_FRID |                     |flag Rp_ WlD |
    +----------------------------------+------------------~--+-~---^-7FFE0-+
    
    This change moves RW_FWID down to 0x7EFE0 and indicates in the FMAP that
    the RW section can only occupy the top 0x3F000 bytes of flash.
    
    The final layout looks more like this:
    
    +----------------------------------+-40000----~----------+-7F000-------+
    | RO_FW                            | RW_FW               |spi flag spi |
    |                          RO_FRID |             RW_FWID |flag spi fla |
    +----------------------------------+----------~--^-7EFE0-+-------------+
    
    If the RW section grows too large for SPI flags, that will result in a
    hard build break. That's probably better than runtime corruption of its
    .text. :)
    
    (cherry picked from commit 817e22093126d1506969518f32b3d88b1086644e)
        * Also adjusted assert_rw_fwid_DO_NOT_EDIT to match number (7EFE0)
        from the release branch and this commit message
    BRANCH=lotus
    TEST=Builds
    BUG=https://frameworkmarketplace.atlassian.net/browse/HW-5739 (cherry-picking)

--- zephyr/program/framework/BUILD.py
+++ zephyr/program/framework/BUILD.py
@@ -33,6 +33,6 @@ marigold = register_framework_project(
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
-assert_rw_fwid_DO_NOT_EDIT(project_name="lotus", addr=0X7FFE0)
-assert_rw_fwid_DO_NOT_EDIT(project_name="azalea", addr=0X7FFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="lotus", addr=0X7EFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="azalea", addr=0X7EFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="marigold", addr=0X7FFE0)
