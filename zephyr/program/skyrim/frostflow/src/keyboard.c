9715ef383c221201f758501cfab8b98d7a5800a9
Fabio Baltieri <fabiobaltieri@google.com>
2023-12-19 15:47:03 +0000

    keyboard: use cros-ec,keyscan on skyrim platforms
    Use the generic cros-ec,keyscan instead of overriding keyscan_config
    directly.
    
    BUG=b:274443029
    TEST=zmake compare-builds -a
    
    Change-Id: I4f9f4e4b7869b8f292bce259b76a7642801f95a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5135998
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

--- zephyr/program/skyrim/frostflow/src/keyboard.c
+++ zephyr/program/skyrim/frostflow/src/keyboard.c
@@ -7,22 +7,6 @@
 #include "keyboard_scan.h"
 #include "timer.h"
 
-/* Keyboard scan setting */
-__override struct keyboard_scan_config keyscan_config = {
-	/* Increase from 50 us, because KSO_02 passes through the H1. */
-	.output_settle_us = 80,
-	/* Other values should be the same as the default configuration. */
-	.debounce_down_us = 9 * MSEC,
-	.debounce_up_us = 30 * MSEC,
-	.scan_period_us = 3 * MSEC,
-	.min_post_scan_delay_us = 1000,
-	.poll_timeout_us = 100 * MSEC,
-	.actual_key_mask = {
-		0x1c, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-		0x86, 0xff, 0xff, 0x55, 0xff, 0xff, 0xff, 0xff,  /* full set */
-	},
-};
-
 static const struct ec_response_keybd_config frostflow_kb = {
 	.num_top_row_keys = 10,
 	.action_keys = {
