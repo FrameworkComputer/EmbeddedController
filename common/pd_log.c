dc62a0e16ecbd9d145902eed504148808dc1faaa
Jeremy Bettis <jbettis@google.com>
2023-12-20 17:42:51 +0000

    ec: Add #line to common/pd_log.c
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E common/pd_log.c
    
    Change-Id: Ic972909b39af3f96acb8720ada264dd7c46afc70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5141446
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>

--- common/pd_log.c
+++ common/pd_log.c
@@ -3,6 +3,12 @@
  * found in the LICENSE file.
  */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 11
+
 #include "charge_manager.h"
 #include "console.h"
 #include "event_log.h"
