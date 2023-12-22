d70c0a1ee77e0517569b461699717afad10352f9
LeoCX-Tsai <62692700+LeoCX-Tsai@users.noreply.github.com>
2023-12-22 08:51:33 +0800

    fwk: lotus: customize fan smart control (#912)
    * lotus: customize fan smart control
    
    from vendor suggestion we need force keep 15%
    for start up when fan response zero until fan start turn.
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * fwk: revert slow response at common code
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: fix style error
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: define the starup duty and remove delay
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: clear non use code
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: clear more not use code
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: change the zero turn early
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: use temp MK for fan tuning
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * fix style error
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus add message for GPU boco exit
    
    Signed-off-by: Kieran Levin <ktl@frame.work>
    
    * lotus: before fan off let it turn on a while
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: change delay time to 10s
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: clear flags and hook_call when fan startup
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: use timestamp for delay
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: add debug log
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * Lotus add temperature off histeresis of 0.5C
    
    This will keep the fan running until the system drops
    0.5C below the turn on temperature to prevent the fan
    from turning on/off/on/off too much.
    Reduce the timer to turn off the fan as the physical
    properties of the system make temperature changes slower
    than the fan off timer
    
    Signed-off-by: Kieran Levin <ktl@frame.work>
    
    * fix over line length
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    
    * lotus: modified thermal rep deviation from 3 to 7
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>
    
    * adjust fan tolerance and step
    
    this is based on our 16in fan behavior
    which is much bigger and slower than 13in fans.
    so our control needs to match the fan physical performance
    
    This will step the duty up/down correctly to match the fan rpm
    to speed gain
    
    Signed-off-by: Kieran Levin <ktl@frame.work>
    
    ---------
    
    Signed-off-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    Signed-off-by: Kieran Levin <ktl@frame.work>
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>
    Co-authored-by: LeoCX_Tsai <leocx_tsai@compal.corp-partner.google.com>
    Co-authored-by: Kieran Levin <ktl@frame.work>
    Co-authored-by: Josh-Tsai <josh_tsai@compal.com>
    
    (cherry picked from commit bd8871d940a1f53975c5d89108f9f10cd4f255c0)
    BRANCH=lotus
    TEST=Builds
    BUG=https://frameworkmarketplace.atlassian.net/browse/HW-5739 (cherry-picking)

---
+++ zephyr/program/framework/lotus/src/fan.c
@@ -0,0 +1,102 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "console.h"
+#include "fan.h"
+#include "math_util.h"
+#include "thermal.h"
+#include "amd_stt.h"
+
+#include <zephyr/kernel.h>
+
+#define CONFIG_FAN_START_DUTY 15
+
+#define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_THERMAL, format, ##args)
+
+
+enum fan_status board_override_fan_control_duty(int ch)
+{
+	struct fan_data *data = &fan_data[ch];
+	int duty, new_duty, rpm_diff, duty_step;
+	int rpm_actual = data->rpm_actual;
+	int rpm_target = data->rpm_target;
+	int deviation = fans[ch].rpm->rpm_deviation;
+
+	duty = fan_get_duty(ch);
+	if (duty == 0 && rpm_target == 0) {
+		return FAN_STATUS_STOPPED;
+	}
+
+	if (duty > 0) {
+		if (duty < 20) {
+			deviation = 10;
+		} else if (duty < 35) {
+			deviation = 7;
+		}
+	}
+
+	/* wait rpm is stable */
+	if (ABS(rpm_actual - data->rpm_pre) > (rpm_target * deviation / 100)) {
+		data->rpm_pre = rpm_actual;
+		return FAN_STATUS_CHANGING;
+	}
+
+	/* Record previous rpm */
+	data->rpm_pre = rpm_actual;
+
+	/*
+	 * A specific type of fan needs a longer time to output the TACH
+	 * signal to EC after EC outputs the PWM signal to the fan.
+	 * During this period, the driver will read two consecutive RPM = 0.
+	 * In this case, don't step the PWM duty too aggressively.
+	 */
+	if (data->rpm_pre < 1000 && rpm_actual < 1000) {
+		fan_set_duty(ch, CONFIG_FAN_START_DUTY);
+		return FAN_STATUS_CHANGING;
+	}
+
+	rpm_diff = rpm_target - rpm_actual;
+
+	if (rpm_diff > (rpm_target * deviation / 100)) {
+		/* Can't set duty higher than 100%... */
+		if (duty == 100)
+			return FAN_STATUS_FRUSTRATED;
+	} else if (rpm_diff < -(rpm_target * deviation / 100)) {
+		/* Can't set duty lower than 1%... */
+		if (duty == 1 && rpm_target != 0)
+			return FAN_STATUS_FRUSTRATED;
+	} else {
+		return FAN_STATUS_LOCKED;
+	}
+
+	/*
+	 * The rpm_diff -> duty_step conversion is specific to a specific
+	 * whiterun fan.
+	 * It has been determined empirically.
+	 */
+	if (ABS(rpm_diff) >= 2100) {
+		duty_step = 28;
+	} else if (ABS(rpm_diff) >= 1100) {
+		duty_step = 14;
+	} else if (ABS(rpm_diff) >= 550) {
+		duty_step = 6;
+	} else if (ABS(rpm_diff) >= 300) {
+		duty_step = 3;
+	} else if (ABS(rpm_diff) >= 150) {
+		duty_step = 2;
+	} else {
+		duty_step = 1;
+	}
+
+	if (rpm_diff > 0)
+		new_duty = MIN(duty + duty_step, 100);
+	else
+		new_duty = MAX(duty - duty_step, 1);
+
+	fan_set_duty(ch, new_duty);
+
+	return FAN_STATUS_CHANGING;
+}
--- zephyr/program/framework/lotus/src/thermal.c
+++ zephyr/program/framework/lotus/src/thermal.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "amd_stt.h"
 #include "chipset.h"
 #include "common.h"
 #include "console.h"
@@ -11,11 +12,17 @@
 #include "hooks.h"
 #include "system.h"
 #include "thermal.h"
+#include "temp_sensor/temp_sensor.h"
 #include "util.h"
 #include "gpu_configuration.h"
 
+#define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_THERMAL, format, ##args)
+
 uint16_t board_fan_max[2];
 uint16_t board_fan_min[2];
+
+#define FAN_STOP_DELAY_S (5 * SECOND)
 
 void fan_configure_gpu(struct gpu_cfg_fan *fan) {
 	if (fan == NULL) {
@@ -30,13 +37,14 @@ void fan_configure_gpu(struct gpu_cfg_fan *fan) {
 		}
 	}
 }
+
 void fan_set_rpm_target(int ch, int rpm)
 {
 	int board_rpm_max = fans[ch].rpm->rpm_max;
 	int board_rpm_min = fans[ch].rpm->rpm_min;
 
 	if (rpm == 0) {
-		/* If rpm = 0, disable PWM immediately. Why?*/
+		/* If rpm = 0, wait 10s for make sure temp is cold-down */
 		fan_set_duty(ch, 0);
 	} else {
 		/* This is the counterpart of disabling PWM above. */
@@ -82,4 +90,99 @@ int fan_percent_to_rpm(int fan_index, int temp_ratio)
 	}
 
 	return rpm;
+}
+
+#define TEMP_APU TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_apu))
+#define TEMP_GPU TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_gpu))
+
+void board_override_fan_control(int fan, int *temp)
+{
+	int actual_rpm, new_rpm;
+	int temp_mk = 0;
+#ifdef CONFIG_GPU
+	int gpu_pct = 0;
+#endif
+	int apu_pct = 0;
+	int pct = 0;
+	int current_max_temp_idx = TEMP_APU;
+	static timestamp_t deadline;
+	timestamp_t now = get_time();
+
+	if (!is_thermal_control_enabled(fan))
+		return;
+
+	/*
+	 * In common/fan.c pwm_fan_stop() will turn off fan
+	 * when chipset suspend or shutdown.
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ON)) {
+
+#ifdef CONFIG_GPU
+		board_get_gpu_temp_mk(&temp_mk);
+		if (thermal_params[TEMP_GPU].temp_fan_off &&
+			thermal_params[TEMP_GPU].temp_fan_max) {
+			gpu_pct = thermal_fan_percent(thermal_params[TEMP_GPU].temp_fan_off * 1000,
+						thermal_params[TEMP_GPU].temp_fan_max * 1000,
+						temp_mk);
+		}
+
+		board_get_soc_temp_mk(&temp_mk);
+		if (thermal_params[TEMP_APU].temp_fan_off &&
+			thermal_params[TEMP_APU].temp_fan_max) {
+			apu_pct = thermal_fan_percent(thermal_params[TEMP_APU].temp_fan_off * 1000,
+						thermal_params[TEMP_APU].temp_fan_max * 1000,
+						temp_mk);
+		}
+
+		if (apu_pct > gpu_pct || !gpu_power_enable())
+			pct = apu_pct;
+		else {
+			current_max_temp_idx = TEMP_GPU;
+			pct = gpu_pct;
+		}
+#else
+		board_get_soc_temp_mk(&temp_mk);
+
+		if (thermal_params[TEMP_APU].temp_fan_off &&
+			thermal_params[TEMP_APU].temp_fan_max) {
+			pct = thermal_fan_percent(thermal_params[TEMP_APU].temp_fan_off * 1000,
+						thermal_params[TEMP_APU].temp_fan_max * 1000,
+						temp_mk);
+		}
+#endif
+
+		new_rpm = fan_percent_to_rpm(fan, pct);
+		actual_rpm = fan_get_rpm_actual(FAN_CH(fan));
+
+		/*
+		 * If we want to turn and the fans are currently significantly below
+		 * the minimum turning speed, we should turn at least as fast as the
+		 * necessary start speed instead.
+		 */
+		if (new_rpm && actual_rpm < fans[fan].rpm->rpm_min * 9 / 10 &&
+			new_rpm < fans[fan].rpm->rpm_start)
+			new_rpm = fans[fan].rpm->rpm_start;
+
+		if (!new_rpm) {
+			/* add temperature histeresis so the fan does not turn off
+			 * unless the system has cooled 0.5C below the fan turn on temperature
+			 */
+			if (thermal_params[current_max_temp_idx].temp_fan_off &&
+				temp_mk > (thermal_params[current_max_temp_idx].temp_fan_off
+					* 1000 - 500)) {
+				deadline.val = get_time().val + FAN_STOP_DELAY_S;
+			}
+			if (!timestamp_expired(deadline, &now))
+				return;
+		}	else
+			deadline.val = get_time().val + FAN_STOP_DELAY_S;
+
+
+		fan_set_rpm_mode(fan, 1);
+		fan_set_rpm_target(FAN_CH(fan), new_rpm);
+	} else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+		/* Stop fan when enter S0ix */
+		fan_set_rpm_mode(fan, 1);
+		fan_set_rpm_target(fan, 0);
+	}
 }
