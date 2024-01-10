3710ea33f242d0a78a353b75229c5a292a8c4527
Scott Collyer <scollyer@google.com>
2024-01-10 14:59:49 -0800

    deku: Move usbc_config.h to own include directory
    The number of usbc ports is different between Ovis and Deku. Move
    usbc_config.h to the project specific include directories so enum
    usbc_port can be different for Deku relative to Ovis
    
    BUG=b:320204787
    TEST='zmake build deku' and 'zmake build ovis'
    LOW_COVERAGE_REASON=Initial deku/ovis configuration b/321848286, b/321848421
    
    Change-Id: I707ef0af8d8dc79066980c9a9850d1153b71ec76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5186144
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Code-Coverage: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Zhuohao Lee <zhuohao@chromium.org>

---
+++ zephyr/program/ovis/deku/include/usbc_config.h
@@ -0,0 +1,22 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_USBC_CONFIG_H
+#define __ZEPHYR_USBC_CONFIG_H
+
+#include "usb_mux.h"
+
+/* USB-C ports */
+enum usbc_port {
+	USBC_PORT_C0 = 0,
+	USBC_PORT_C1,
+	USBC_PORT_C2,
+	USBC_PORT_COUNT
+};
+BUILD_ASSERT(USBC_PORT_COUNT == CONFIG_USB_PD_PORT_MAX_COUNT);
+
+void reset_nct38xx_port(int port);
+
+#endif /* __ZEPHYR_USBC_CONFIG_H */
