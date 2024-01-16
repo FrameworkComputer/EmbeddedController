40dd11e8c68a1e8d6968e96aced60dcee784181f
Tristan Honscheid <honscheid@google.com>
2024-01-16 14:15:37 -0700

    pdc: Add host command implementations
    Add implementations for EC_CMD_LOCATE_CHIP, EC_CMD_PD_CHIP_INFO, and
    EC_CMD_USB_PD_PORTS to enable PDC firmware update from Depthcharge
    
    Also add a stub implementation of EC_CMD_USB_PD_MUX_INFO
    
    BUG=b:304301475
    TEST=zmake build brya_pdc
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: Ia72161589952690ba0a6ab7180292d4be5853e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5202124
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>
    Tested-by: Tristan Honscheid <honscheid@google.com>

--- common/peripheral.c
+++ common/peripheral.c
@@ -10,6 +10,10 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 
+#if defined(CONFIG_USB_PD_CONTROLLER)
+#include "usbc/pdc_power_mgmt.h"
+#endif
+
 #ifdef CONFIG_HOSTCMD_LOCATE_CHIP
 static enum ec_status hc_locate_chip(struct host_cmd_handler_args *args)
 {
@@ -49,7 +53,33 @@ static enum ec_status hc_locate_chip(struct host_cmd_handler_args *args)
 		break;
 	case EC_CHIP_TYPE_PDC:
 #if defined(CONFIG_USB_PD_CONTROLLER)
-		/* TODO: Add support for PDC Chip Type */
+		if (params->index >= pdc_power_mgmt_get_usb_pd_port_count())
+			return EC_RES_OVERFLOW;
+
+		struct pdc_bus_info_t bus_info;
+		int i2c_port;
+
+		if (pdc_power_mgmt_get_bus_info(params->index, &bus_info)) {
+			/* Cannot obtain I2C info for PDC */
+			return EC_RES_ERROR;
+		}
+
+		/* Only I2C PDCs are supported at this time. */
+		if (bus_info.bus_type != PDC_BUS_TYPE_I2C) {
+			return EC_RES_UNAVAILABLE;
+		}
+		resp->bus_type = EC_BUS_TYPE_I2C;
+
+		resp->i2c_info.addr_flags = bus_info.i2c.addr;
+
+		i2c_port = i2c_get_port_from_device(bus_info.i2c.bus);
+		if (i2c_port < 0) {
+			return EC_RES_DUP_UNAVAILABLE;
+		}
+
+		resp->i2c_info.port = (uint16_t)i2c_port;
+
+		break;
 #else
 		/* Only available on boards that use PD_CONTROLLER */
 		return EC_RES_UNAVAILABLE; /* LCOV_EXCL_LINE */
---
+++ zephyr/subsys/pd_controller/pdc_host_cmd.c
@@ -0,0 +1,83 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ec_commands.h"
+#include "usb_pd.h"
+#include "usbc/pdc_power_mgmt.h"
+
+#include <string.h>
+
+#include <zephyr/device.h>
+
+#include <drivers/pdc.h>
+
+#ifdef CONFIG_PLATFORM_EC_HOSTCMD_PD_CHIP_INFO
+/* EC_CMD_PD_CHIP_INFO implementation when a PDC is used. */
+
+static enum ec_status hc_remote_pd_chip_info(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_pd_chip_info *p = args->params;
+	struct ec_response_pd_chip_info_v1 resp = { 0 };
+	struct pdc_info_t pdc_info;
+
+	if (pdc_power_mgmt_get_info(p->port, &pdc_info)) {
+		return EC_RES_ERROR;
+	}
+
+	resp.vendor_id = PDC_VIDPID_GET_VID(pdc_info.vid_pid);
+	resp.product_id = PDC_VIDPID_GET_PID(pdc_info.vid_pid);
+
+	/* Ver output is 3 bytes right-aligned in a 32-bit container. Map into
+	 * the first three bytes of fw_version_string.
+	 */
+
+	resp.fw_version_string[2] = PDC_FWVER_GET_MAJOR(pdc_info.fw_version);
+	resp.fw_version_string[1] = PDC_FWVER_GET_MINOR(pdc_info.fw_version);
+	resp.fw_version_string[0] = PDC_FWVER_GET_PATCH(pdc_info.fw_version);
+
+	/*
+	 * Take advantage of the fact that v0 and v1 structs have the
+	 * same layout for v0 data. (v1 just appends data)
+	 */
+	args->response_size =
+		args->version ? sizeof(struct ec_response_pd_chip_info_v1) :
+				sizeof(struct ec_response_pd_chip_info);
+
+	memcpy(args->response, &resp, args->response_size);
+
+	return EC_RES_SUCCESS;
+}
+
+DECLARE_HOST_COMMAND(EC_CMD_PD_CHIP_INFO, hc_remote_pd_chip_info,
+		     EC_VER_MASK(0) | EC_VER_MASK(1));
+#endif /* CONFIG_HOSTCMD_PD_CHIP_INFO */
+
+static enum ec_status hc_pd_ports(struct host_cmd_handler_args *args)
+{
+	struct ec_response_usb_pd_ports *r = args->response;
+
+	r->num_ports = pdc_power_mgmt_get_usb_pd_port_count();
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_USB_PD_PORTS, hc_pd_ports, EC_VER_MASK(0));
+
+/**
+ * @brief This is a placeholder implementation of EC_CMD_USB_PD_MUX_INFO that
+ *        always indicates the muxes are disconnected.
+ */
+static enum ec_status hc_usb_pd_mux_info(struct host_cmd_handler_args *args)
+{
+	struct ec_response_usb_pd_mux_info *r = args->response;
+
+	/* TODO: Add range check on port # and query actual mux state */
+	r->flags = USB_PD_MUX_NONE;
+
+	args->response_size = sizeof(*r);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_USB_PD_MUX_INFO, hc_usb_pd_mux_info,
+		     EC_VER_MASK(0));
