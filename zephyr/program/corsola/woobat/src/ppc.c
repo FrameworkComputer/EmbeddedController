5a76e67210b15fcf67d8a6f90439993598949ae4
Devin Lu <Devin.Lu@quantatw.com>
2024-01-18 15:03:18 +0800

    woobat: Configure USB-C
    1. Remove old board code.
    2. Update TCPPC Configuration that we only use SYV682x.
    3. Remove daughter-board for C1 port.
    
    BUG=b:320347312
    TEST="zmake build woobat" without errors.
    ./twister -T zephyr/test/krabby
    
    Change-Id: I4bef00517e86c14110604fe98c2f54bc16bdff46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5209334
    Commit-Queue: Devin Lu <devin.lu@quantatw.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Devin Lu <devin.lu@quantatw.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- zephyr/program/corsola/woobat/src/ppc.c
+++ zephyr/program/corsola/woobat/src/ppc.c
@@ -3,14 +3,11 @@
  * found in the LICENSE file.
  */
 
-/* Woobat PPC/BC12 (mixed RT1739 or PI3USB9201+SYV682X) configuration */
+/* Woobat PPC/BC12 SYV682X+PI3USB9201 configuration */
 
 #include "baseboard_usbc_config.h"
-#include "cros_board_info.h"
-#include "driver/usb_mux/ps8743_public.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
-#include "usb_mux.h"
 #include "usbc/ppc.h"
 
 #include <zephyr/logging/log.h>
@@ -21,32 +18,6 @@
 
 LOG_MODULE_REGISTER(alt_dev_replacement);
 
-#define BOARD_VERSION_UNKNOWN 0xffffffff
-
-/* Check board version to decide which ppc/bc12 is used. */
-static bool board_has_syv_ppc(void)
-{
-	static uint32_t board_version = BOARD_VERSION_UNKNOWN;
-
-	if (board_version == BOARD_VERSION_UNKNOWN || IS_ENABLED(CONFIG_TEST)) {
-		if (cbi_get_board_version(&board_version) != EC_SUCCESS) {
-			LOG_ERR("Failed to get board version.");
-			board_version = 0;
-		}
-	}
-
-	return (board_version >= 3);
-}
-
-static void check_alternate_devices(void)
-{
-	/* Configure the PPC driver */
-	if (board_has_syv_ppc())
-		/* Arg is the USB port number */
-		PPC_ENABLE_ALTERNATE(0);
-}
-DECLARE_HOOK(HOOK_INIT, check_alternate_devices, HOOK_PRIO_DEFAULT);
-
 #ifdef CONFIG_PLATFORM_EC_USB_CHARGER
 void bc12_interrupt(enum gpio_signal signal)
 {
@@ -54,29 +25,15 @@ void bc12_interrupt(enum gpio_signal signal)
 }
 #endif
 
-/* USB Mux C1 : board_init of PS8743 */
-int ps8743_eq_c1_setting(void)
-{
-	ps8743_write(usb_muxes[1].mux, PS8743_REG_USB_EQ_RX, 0x90);
-	return EC_SUCCESS;
-}
-
 static void board_usbc_init(void)
 {
-	if (board_has_syv_ppc()) {
-		/* Enable PPC interrupts. */
-		gpio_enable_dt_interrupt(
-			GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc));
+	/* Enable PPC interrupts. */
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc));
 
 #ifdef CONFIG_PLATFORM_EC_USB_CHARGER
-		/* Enable BC1.2 interrupts. */
-		gpio_enable_dt_interrupt(
-			GPIO_INT_FROM_NODELABEL(int_usb_c0_bc12));
+	/* Enable BC1.2 interrupts. */
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_bc12));
 #endif
-	} else {
-		gpio_enable_dt_interrupt(
-			GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc));
-	}
 }
 DECLARE_HOOK(HOOK_INIT, board_usbc_init, HOOK_PRIO_POST_DEFAULT);
 
@@ -84,8 +41,5 @@ void ppc_interrupt(enum gpio_signal signal)
 {
 	if (signal == GPIO_SIGNAL(DT_NODELABEL(usb_c0_ppc_int_odl))) {
 		ppc_chips[0].drv->interrupt(0);
-	}
-	if (signal == GPIO_SIGNAL(DT_ALIAS(gpio_usb_c1_ppc_int_odl))) {
-		ppc_chips[1].drv->interrupt(1);
 	}
 }
--- zephyr/test/krabby/src/ppc_woobat.c
+++ zephyr/test/krabby/src/ppc_woobat.c
@@ -14,19 +14,7 @@
 #include <zephyr/fff.h>
 #include <zephyr/ztest.h>
 
-FAKE_VALUE_FUNC(int, cbi_get_board_version, uint32_t *);
-
-static int fake_board_version;
-
-static int fake_cbi_get_board_version(uint32_t *ver)
-{
-	*ver = fake_board_version;
-	return 0;
-}
-
 FAKE_VOID_FUNC(ppc_chip_0_interrupt, int);
-FAKE_VOID_FUNC(ppc_chip_alt_interrupt, int);
-FAKE_VOID_FUNC(ppc_chip_1_interrupt, int);
 
 ZTEST(ppc_woobat, test_ppc_init)
 {
@@ -35,10 +23,6 @@ ZTEST(ppc_woobat, test_ppc_init)
 	const gpio_port_pins_t ppc_int_pin =
 		DT_GPIO_PIN(DT_NODELABEL(usb_c0_ppc_int_odl), gpios);
 
-	/* Board version 0, expect that main ppc is enabled. */
-	RESET_FAKE(cbi_get_board_version);
-	cbi_get_board_version_fake.custom_fake = fake_cbi_get_board_version;
-	fake_board_version = 0;
 	hook_notify(HOOK_INIT);
 	zassert_ok(gpio_emul_input_set(ppc_int_gpio, ppc_int_pin, 1), NULL);
 	k_sleep(K_MSEC(100));
@@ -46,92 +30,24 @@ ZTEST(ppc_woobat, test_ppc_init)
 	k_sleep(K_MSEC(100));
 
 	zassert_equal(ppc_chip_0_interrupt_fake.call_count, 1);
-	zassert_equal(ppc_chip_alt_interrupt_fake.call_count, 0);
-	zassert_equal(ppc_chip_1_interrupt_fake.call_count, 0);
-
-	/* CBI access fail, fallback to board version 0 */
-	RESET_FAKE(cbi_get_board_version);
-	cbi_get_board_version_fake.return_val = -1;
-	fake_board_version = 0;
-	hook_notify(HOOK_INIT);
-	zassert_ok(gpio_emul_input_set(ppc_int_gpio, ppc_int_pin, 1), NULL);
-	k_sleep(K_MSEC(100));
-	zassert_ok(gpio_emul_input_set(ppc_int_gpio, ppc_int_pin, 0), NULL);
-	k_sleep(K_MSEC(100));
-
-	zassert_equal(ppc_chip_0_interrupt_fake.call_count, 2);
-	zassert_equal(ppc_chip_alt_interrupt_fake.call_count, 0);
-	zassert_equal(ppc_chip_1_interrupt_fake.call_count, 0);
-
-	/* Board version 3, expect that alt ppc is enabled.
-	 * Since PPC_ENABLE_ALTERNATE() is not reversible, we must test this
-	 * after the board version 0 test.
-	 */
-	RESET_FAKE(cbi_get_board_version);
-	cbi_get_board_version_fake.custom_fake = fake_cbi_get_board_version;
-	fake_board_version = 3;
-	hook_notify(HOOK_INIT);
-	zassert_ok(gpio_emul_input_set(ppc_int_gpio, ppc_int_pin, 1), NULL);
-	k_sleep(K_MSEC(100));
-	zassert_ok(gpio_emul_input_set(ppc_int_gpio, ppc_int_pin, 0), NULL);
-	k_sleep(K_MSEC(100));
-
-	zassert_equal(ppc_chip_0_interrupt_fake.call_count, 2);
-	zassert_equal(ppc_chip_alt_interrupt_fake.call_count, 1);
-	zassert_equal(ppc_chip_1_interrupt_fake.call_count, 0);
-}
-
-ZTEST(ppc_woobat, test_ppc_1_int)
-{
-	const struct device *x_ec_gpio2 = DEVICE_DT_GET(
-		DT_GPIO_CTLR(DT_NODELABEL(gpio_x_ec_gpio2), gpios));
-	const gpio_port_pins_t x_ec_gpio2_pin =
-		DT_GPIO_PIN(DT_NODELABEL(gpio_x_ec_gpio2), gpios);
-
-	zassert_ok(gpio_emul_input_set(x_ec_gpio2, x_ec_gpio2_pin, 1), NULL);
-	k_sleep(K_MSEC(100));
-	zassert_ok(gpio_emul_input_set(x_ec_gpio2, x_ec_gpio2_pin, 0), NULL);
-	k_sleep(K_MSEC(100));
-
-	zassert_equal(ppc_chip_0_interrupt_fake.call_count, 0);
-	zassert_equal(ppc_chip_alt_interrupt_fake.call_count, 0);
-	zassert_equal(ppc_chip_1_interrupt_fake.call_count, 1);
 }
 
 static void *ppc_woobat_init(void)
 {
 	static struct ppc_drv fake_ppc_drv_0;
-	static struct ppc_drv fake_ppc_drv_1;
-	static struct ppc_drv fake_ppc_drv_alt;
 
-	zassert_equal(ppc_cnt, 2);
+	zassert_equal(ppc_cnt, 1);
 
-	/* inject mocked interrupt handlers into ppc_drv and ppc_drv_alt */
+	/* inject mocked interrupt handlers into ppc_drv */
 	fake_ppc_drv_0 = *ppc_chips[0].drv;
 	fake_ppc_drv_0.interrupt = ppc_chip_0_interrupt;
 	ppc_chips[0].drv = &fake_ppc_drv_0;
-
-	fake_ppc_drv_alt = *ppc_chips_alt[0].drv;
-	fake_ppc_drv_alt.interrupt = ppc_chip_alt_interrupt;
-	ppc_chips_alt[0].drv = &fake_ppc_drv_alt;
-
-	fake_ppc_drv_1 = *ppc_chips[1].drv;
-	fake_ppc_drv_1.interrupt = ppc_chip_1_interrupt;
-	ppc_chips[1].drv = &fake_ppc_drv_1;
 
 	return NULL;
 }
 
 static void ppc_woobat_before(void *fixture)
 {
-	RESET_FAKE(cbi_get_board_version);
 	RESET_FAKE(ppc_chip_0_interrupt);
-	RESET_FAKE(ppc_chip_alt_interrupt);
-	RESET_FAKE(ppc_chip_1_interrupt);
-
-	/* We have bypassed the db detection, so we force enabling the
-	 * int_x_ec_gpio2.
-	 */
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_x_ec_gpio2));
 }
 ZTEST_SUITE(ppc_woobat, NULL, ppc_woobat_init, ppc_woobat_before, NULL, NULL);
