5f09abe5b11fb19e3b80ded096e6d0192208a8c8
Caveh Jalali <caveh@chromium.org>
2024-01-19 14:18:48 -0800

    zephyr/emul/ktu1125: Add ktu1125 emulator
    Add a minimal ktu1125 emulator to support driver testing. Note that this
    is not a functional emulator of the chip, but it does enable us to get
    coverage data for the ktu1125 PPC driver.
    
    BUG=b:304682189
    TEST=emulator exercised by follow-up patch
    	'zmake build -a' passes
    	'./twister -T zephyr/test' passes
    
    Change-Id: I33cb59c4229e2f3ddb75d7e82afaad11468220b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5219791
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

---
+++ zephyr/include/emul/emul_ktu1125.h
@@ -0,0 +1,28 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EMUL_KTU1125_H
+#define EMUL_KTU1125_H
+
+#include <zephyr/drivers/emul.h>
+#include <zephyr/sys/slist.h>
+
+/**
+ * @brief Function called on reset
+ *
+ * @param emul Pointer to ktu1125 emulator
+ */
+void ktu1125_emul_reset(const struct emul *emul);
+
+/**
+ * @brief Set value of given register on the ktu1125 PPC.
+ *
+ * @param emul Pointer to ktu1125 emulator
+ * @param reg Register to change
+ * @param val New register value
+ */
+int ktu1125_emul_set_reg(const struct emul *emul, int reg, int val);
+
+#endif /* EMUL_KTU1125_H */
