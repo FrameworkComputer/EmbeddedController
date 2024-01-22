5d2e3767cec79a631381842b6b3e84c4153476e2
Daisuke Nojiri <dnojiri@chromium.org>
2024-01-22 09:55:47 -0800

    PPM: Add EC_CMD_UCSI_PPM_SET and _GET
    These commands are used for the OPM to communicate with the PPM using
    UCSI protocol.
    
    $ ectool ppm notify 0xff
    Successfully executed SET_NOTIFICATION_ENABLE.
    
    $ ectool ppm dump cci
    CCI:
            Command Completed
            Connector Change = None
            Data Length = 0
    
    $ ectool ppm ack 2
    Successfully executed ACK_CC_CI. Acknowledged: CC.
    
    $ ectool ppm dump cci
    CCI:
            Acknowledge Command
            Connector Change = None
            Data Length = 0
    
    BUG=b:319124515
    TEST=See the description above.
    
    Change-Id: I8475077634b9089392842b674818f4bec9d48ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5280910
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Pavan Holla <pholla@google.com>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/subsys/ucsi/ucsi_host_cmd.c
+++ zephyr/subsys/ucsi/ucsi_host_cmd.c
@@ -5,7 +5,9 @@
 
 /* UCSI host command */
 
+#include "ec_commands.h"
 #include "hooks.h"
+#include "host_command.h"
 #include "include/pd_driver.h"
 #include "include/platform.h"
 #include "include/ppm.h"
@@ -20,6 +22,11 @@ LOG_MODULE_REGISTER(ucsi, LOG_LEVEL_INF);
 #define DEV_CAST_FROM(v) (struct ppm_common_device *)(v)
 
 static struct ucsi_ppm_driver *ppm_drv;
+
+static void opm_notify(void *context)
+{
+	pd_send_host_event(PD_EVENT_PPM);
+}
 
 /* Sort of main */
 static int eppm_init(void)
@@ -44,7 +51,41 @@ static int eppm_init(void)
 	ppm_drv = drv->get_ppm(pdc_dev);
 	ppm_dev = DEV_CAST_FROM(ppm_drv->dev);
 	LOG_INF("Initialized PPM num_ports=%u", ppm_dev->num_ports);
+	ppm_drv->register_notify(ppm_drv->dev, opm_notify, NULL);
 
 	return 0;
 }
 SYS_INIT(eppm_init, APPLICATION, 99);
+
+static enum ec_status hc_ucsi_ppm_set(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_ucsi_ppm_set *p = args->params;
+
+	if (!ppm_drv)
+		return EC_RES_UNAVAILABLE;
+
+	if (ppm_drv->write(ppm_drv->dev, p->offset, p->data,
+			   args->params_size - sizeof(p->offset)))
+		return EC_RES_ERROR;
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_UCSI_PPM_SET, hc_ucsi_ppm_set, EC_VER_MASK(0));
+
+static enum ec_status hc_ucsi_ppm_get(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_ucsi_ppm_get *p = args->params;
+	int len;
+
+	if (!ppm_drv)
+		return EC_RES_UNAVAILABLE;
+
+	len = ppm_drv->read(ppm_drv->dev, p->offset, args->response, p->size);
+	if (len < 0)
+		return EC_RES_ERROR;
+
+	args->response_size = len;
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_UCSI_PPM_GET, hc_ucsi_ppm_get, EC_VER_MASK(0));
