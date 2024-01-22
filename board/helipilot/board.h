5f64aa162d280e311314d5af43ecf3baf5d41681
Yi Chou <yich@google.com>
2024-01-22 13:23:07 +0800

    board: disable CONFIG_MALLOC for fpsensors
    The underlying issue is that both `shmalloc` and `malloc/free` are
    both set up to use the same memory region for allocation. If they
    are both enabled, they will overwrite each other.
    
    The change in https://crrev.com/c/5166769 triggered this behavior
    since it makes `CONFIG_MALLOC` take effect when
    `USE_BUILTIN_STDLIB` is `0`.
    
    BUG=b:321176438
    TEST=ectool --name=cros_fp fpmode enroll
    
    Change-Id: Id01f92c65ba3c22aeff13757752626fc117db1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5222240
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>
    Reviewed-by: Craig Hesling <hesling@chromium.org>
    Tested-by: Yi Chou <yich@google.com>

--- board/helipilot/board.h
+++ board/helipilot/board.h
@@ -225,12 +225,6 @@
 #ifdef SECTION_IS_RW
 #define CONFIG_FP_SENSOR_FPC1025
 /*
- * Use the malloc code only in the RW section (for the private library),
- * we cannot enable it in RO since it is not compatible with the RW
- * verification  (shared_mem_init done too late).
- */
-#define CONFIG_MALLOC
-/*
  * FP buffers are allocated in regular SRAM
  * TODO(b/124773209): Instead of defining to empty, #undef once all CLs that
  * depend on FP_*_SECTION have landed. Also rename the variables to CONFIG_*.
