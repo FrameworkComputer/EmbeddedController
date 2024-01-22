f3416f517a5b078bf9e599b2f9171dc3d120704a
Al Semjonovs <asemjonovs@google.com>
2024-01-22 13:41:25 -0700

    pdc: Add get_data_role test
    Validate PDC Power Management get_data_role API
    
    BUG=b:317065172
    TEST=./twister -c  -p native_posix -T zephyr/test/pdc/
    LOW_COVERAGE_REASON=Incremental change for coverage b:317065172
    
    Change-Id: I415f834545e3ead5d985a7192315df1248108cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5226122
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Al Semjonovs <asemjonovs@google.com>

--- zephyr/test/pdc/src/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/pdc_power_mgmt_api.c
@@ -86,3 +86,29 @@ ZTEST_USER(pdc_power_mgmt_api, test_pd_get_polarity)
 	zassert_equal(POLARITY_CC2, pdc_power_mgmt_pd_get_polarity(TEST_PORT));
 #endif /* TODO_B_321749548 */
 }
+
+ZTEST_USER(pdc_power_mgmt_api, test_pd_get_data_role)
+{
+	zassert_equal(
+		PD_ROLE_DISCONNECTED,
+		pdc_power_mgmt_pd_get_data_role(CONFIG_USB_PD_PORT_MAX_COUNT));
+
+/* TODO(b/321749548) - Read connector status after its read from I2C bus
+ * not after reading PING status.
+ */
+#ifdef TODO_B_321749548
+	struct connector_status_t connector_status;
+
+	connector_status.conn_partner_type = DFP_ATTACHED;
+	emul_pdc_set_connector_status(emul, &connector_status);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_equal(PD_ROLE_UFP, pdc_power_mgmt_pd_get_data_role(TEST_PORT));
+
+	connector_status.conn_partner_type = UFP_ATTACHED;
+	emul_pdc_set_connector_status(emul, &connector_status);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_equal(PD_ROLE_DFP, pdc_power_mgmt_pd_get_data_role(TEST_PORT));
+#endif /* TODO_B_321749548 */
+}
