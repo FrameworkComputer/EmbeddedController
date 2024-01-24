249997b615545328c1a8c75e68610375d2634be8
Al Semjonovs <asemjonovs@google.com>
2024-01-24 14:07:14 -0700

    pdc: Add get_partner_usb_comm_capable test
    Validate PDC Power Management get_partner_usb_comm_capable API
    
    BUG=b:317065172
    TEST=./twister -c  -p native_posix -T zephyr/test/pdc/
    LOW_COVERAGE_REASON=Incremental change for coverage b:317065172
    
    Change-Id: Ia5cbc834b2b110f153a7c5136b682dcad3beb368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5234944
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Sam Hurst <shurst@google.com>
    Tested-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>

--- zephyr/test/pdc/src/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/pdc_power_mgmt_api.c
@@ -205,3 +205,61 @@ ZTEST_USER(pdc_power_mgmt_api, test_pd_capable)
 	zassert_equal(true, pdc_power_mgmt_pd_capable(TEST_PORT));
 #endif /* TODO_B_321749548 */
 }
+
+ZTEST_USER(pdc_power_mgmt_api, test_get_partner_usb_comm_capable)
+{
+	zassert_false(pdc_power_mgmt_get_partner_usb_comm_capable(
+		CONFIG_USB_PD_PORT_MAX_COUNT));
+
+/* TODO(b/321749548) - Read connector status after its read from I2C bus
+ * not after reading PING status.
+ */
+#ifdef TODO_B_321749548
+	struct connector_status_t connector_status;
+	union connector_capability_t ccaps;
+
+	connector_status.connect_status = 1;
+	emul_pdc_set_connector_status(emul, &connector_status);
+
+	ccaps.raw_value = 0;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_false(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+
+	ccaps.raw_value = 0;
+	ccaps.op_mode_usb2 = 1;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_true(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+
+	ccaps.raw_value = 0;
+	ccaps.op_mode_usb3 = 1;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_true(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+
+	ccaps.raw_value = 0;
+	ccaps.ext_op_mode_usb4_gen2 = 1;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_true(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+
+	ccaps.raw_value = 0;
+	ccaps.ext_op_mode_usb4_gen3 = 1;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_true(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+
+	ccaps.raw_value = 0;
+	ccaps.ext_op_mode_usb4_gen4 = 1;
+	emul_pdc_set_connector_capability(emul, &ccaps);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(500));
+	zassert_true(pdc_power_mgmt_get_partner_usb_comm_capable(TEST_PORT));
+#endif /* TODO_B_321749548 */
+}
