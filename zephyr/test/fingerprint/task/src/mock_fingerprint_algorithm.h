2b8d437213890a96adaba24451470940fb0afa7e
Patryk Duda <patrykd@google.com>
2024-01-25 11:02:49 +0100

    zephyr/test: Introduce finger capture unit tests
    BUG=b:299924239
    TEST=twister -v -i -T zephyr/test/fingerprint/task --toolchain host
    TEST=twister -v -i -T zephyr/test/fingerprint/task --toolchain llvm
    TEST=Run Gitlab CI locally
    
    Change-Id: I807f8fb376c2cf6b32ca054ddc3a5ae60e539c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5093883
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Patryk Duda <patrykd@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Patryk Duda <patrykd@google.com>

---
+++ zephyr/test/fingerprint/task/src/mock_fingerprint_algorithm.h
@@ -0,0 +1,23 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stdint.h>
+
+#include <zephyr/fff.h>
+
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_init,
+			const struct fingerprint_algorithm *const);
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_exit,
+			const struct fingerprint_algorithm *const);
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_enroll_start,
+			const struct fingerprint_algorithm *const);
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_enroll_step,
+			const struct fingerprint_algorithm *const,
+			const uint8_t *const, int *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_enroll_finish,
+			const struct fingerprint_algorithm *const, void *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_alg_match,
+			const struct fingerprint_algorithm *const, void *,
+			uint32_t, const uint8_t *const, int32_t *, uint32_t *);
