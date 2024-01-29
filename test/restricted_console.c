e8ba87e86eccb1c0eaa3bd10d230564a604d6dea
Firas Sammoura <fsammoura@google.com>
2024-01-29 14:26:51 -0800

    test: add unit test for restricted console command_read_word
    BUG=b:142944002
    TEST=run_device_tests.py -b bloonchipper -t restricted_console
    TEST=run_device_tests.py -b dartmonkey -t restricted_console
    TEST=run_device_tests.py -b helipilot -t restricted_console
    
    Change-Id: I7772c4f2c0d9e85899759e0d427e1a42f469bf3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5246501
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>

--- test/restricted_console.c
+++ test/restricted_console.c
@@ -35,11 +35,42 @@ test_static int test_command_mem_dump(void)
 	return EC_SUCCESS;
 }
 
+test_static int test_command_read_write_word(void)
+{
+	enum ec_error_list res;
+	const uint32_t old_value = 0x1badd00d;
+	/* This word will be read/written by the rw command. */
+	volatile uint32_t valid_word = old_value;
+	/* Compose the rw console command to write |valid_word| with a value
+	 * of 5.
+	 */
+	char console_input[] = "rw 0x01234567 0x05";
+	const uint32_t new_value = 0x05;
+
+	snprintf(console_input, sizeof(console_input), "rw %p 0x%02x",
+		 &valid_word, new_value);
+
+	is_locked = 0;
+	res = test_send_console_command(console_input);
+	TEST_EQ(res, EC_SUCCESS, "%d");
+	TEST_EQ(new_value, valid_word, "%" PRIu32);
+
+	is_locked = 1;
+	/* Reset valid word */
+	valid_word = old_value;
+	res = test_send_console_command(console_input);
+	TEST_EQ(res, EC_ERROR_ACCESS_DENIED, "%d");
+	TEST_EQ(old_value, valid_word, "%" PRIu32);
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	test_reset();
 
 	RUN_TEST(test_command_mem_dump);
+	RUN_TEST(test_command_read_write_word);
 
 	test_print_result();
 }
