a5fe120d882a59ffee52486587b07a154dd20ddc
Eric Yilun Lin <yllin@chromium.org>
2024-01-30 13:31:49 -0800

    rauru: use it82202ax config instead
    512KB flash is not enough for rauru, use it82202ax config instead,
    which increase flash size to 768KB.
    
    BUG=b:308941437
    TEST=zmake build rauru
    
    Change-Id: I7753ef22e73cc69ed767a1aa28764b3979ba1f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5251038
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

--- zephyr/program/rauru/BUILD.py
+++ zephyr/program/rauru/BUILD.py
@@ -11,7 +11,7 @@ def register_rauru_project(project_name):
 
     return register_func(
         project_name=project_name,
-        zephyr_board="it82202ax-512",
+        zephyr_board="it82202ax",
         dts_overlays=[here / project_name / "project.overlay"],
         kconfig_files=[
             here / "program.conf",
@@ -25,4 +25,4 @@ register_rauru_project(project_name="rauru")
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
-assert_rw_fwid_DO_NOT_EDIT(project_name="rauru", addr=0x40098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="rauru", addr=0x60098)
