1abc171bb8fbf9a66de67cc7adf2918ff059fa9e
Eric Yilun Lin <yllin@chromium.org>
2024-02-02 14:13:02 -0800

    rauru: enable USBPD sink/source
    BUG=b:308941437
    TEST=zmake build rauru
    LOW_COVERAGE_REASON=b:308941437 Rauru bringup
    
    Change-Id: Ia672bdec9caf8dbae1f4b294d8d7a1df93da72ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5278854
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/rauru/src/usbc.c
+++ zephyr/program/rauru/src/usbc.c
@@ -4,27 +4,117 @@
  */
 
 #include "charge_manager.h"
+#include "chipset.h"
 #include "console.h"
 #include "usb_pd.h"
+#include "usbc_ppc.h"
 #include "zephyr_adc.h"
 
-#define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ##args)
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ##args)
 
 int board_set_active_charge_port(int port)
 {
-	/* TODO(b:308941437): implement me */
-	return 0;
+	int i;
+	int is_valid_port = (port >= 0 && port < board_get_usb_pd_port_count());
+	/* adjust the actual port count when not the type-c db connected. */
+
+	if (!is_valid_port && port != CHARGE_PORT_NONE) {
+		return EC_ERROR_INVAL;
+	}
+
+	if (port == CHARGE_PORT_NONE) {
+		CPRINTS("Disabling all charger ports");
+
+		/* Disable all ports. */
+		for (i = 0; i < board_get_usb_pd_port_count(); i++) {
+			/*
+			 * Do not return early if one fails otherwise we can
+			 * get into a boot loop assertion failure.
+			 */
+			if (ppc_vbus_sink_enable(i, 0)) {
+				CPRINTS("Disabling C%d as sink failed.", i);
+			}
+		}
+
+		return EC_SUCCESS;
+	}
+
+	/* Check if the port is sourcing VBUS. */
+	if (ppc_is_sourcing_vbus(port)) {
+		CPRINTS("Skip enable C%d", port);
+		return EC_ERROR_INVAL;
+	}
+
+	CPRINTS("New charge port: C%d", port);
+
+	/*
+	 * Turn off the other ports' sink path FETs, before enabling the
+	 * requested charge port.
+	 */
+	for (i = 0; i < board_get_usb_pd_port_count(); i++) {
+		if (i == port) {
+			continue;
+		}
+
+		if (ppc_vbus_sink_enable(i, 0)) {
+			CPRINTS("C%d: sink path disable failed.", i);
+		}
+	}
+
+	/* Enable requested charge port. */
+	if (ppc_vbus_sink_enable(port, 1)) {
+		CPRINTS("C%d: sink path enable failed.", port);
+		return EC_ERROR_UNKNOWN;
+	}
+
+	return EC_SUCCESS;
+}
+
+int board_vbus_source_enabled(int port)
+{
+	return ppc_is_sourcing_vbus(port);
 }
 
 int pd_set_power_supply_ready(int port)
 {
-	/* TODO(b:308941437): implement me */
-	return 0;
+	int rv;
+
+	/* Disable charging. */
+	rv = ppc_vbus_sink_enable(port, 0);
+	if (rv) {
+		return rv;
+	}
+
+	pd_set_vbus_discharge(port, 0);
+
+	/* Provide Vbus. */
+	rv = ppc_vbus_source_enable(port, 1);
+	if (rv) {
+		return rv;
+	}
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+
+	return EC_SUCCESS;
 }
 
 void pd_power_supply_reset(int port)
 {
-	/* TODO(b:308941437): implement me */
+	int prev_en;
+
+	prev_en = ppc_is_sourcing_vbus(port);
+
+	/* Disable VBUS. */
+	ppc_vbus_source_enable(port, 0);
+
+	/* Enable discharge if we were previously sourcing 5V */
+	if (prev_en) {
+		pd_set_vbus_discharge(port, 1);
+	}
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 }
 
 void board_reset_pd_mcu(void)
@@ -46,8 +136,8 @@ void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
 
 int pd_check_vconn_swap(int port)
 {
-	/* TODO(b:308941437): implement me */
-	return 0;
+	/* Allow Vconn swap if AP is on. */
+	return chipset_in_state(CHIPSET_STATE_SUSPEND | CHIPSET_STATE_ON);
 }
 
 #ifdef CONFIG_USB_PD_VBUS_MEASURE_ADC_EACH_PORT
@@ -58,7 +148,7 @@ enum adc_channel board_get_vbus_adc(int port)
 	} else if (port == 1) {
 		return ADC_VBUS_C1;
 	}
-	CPRINTSUSB("Unknown vbus adc port id: %d", port);
+	CPRINTS("Unknown vbus adc port id: %d", port);
 	return ADC_VBUS_C0;
 }
 #endif /* CONFIG_USB_PD_VBUS_MEASURE_ADC_EACH_PORT */
