c806bb80ebbf28ffbe8161e678e7a4511e27810d
Yuval Peress <peress@google.com>
2024-02-05 14:10:28 -0700

    lsm6dso: Add emulator
    Add emulator for lsm6dso. This is tested later with a new integration
    test motion_sense.rex.
    
    BUG=b:323908896
    TEST=twister -T zephyr/test/motion_sense
    LOW_COVERAGE_REASON=Coverage is added in chromium:5273875
    
    Change-Id: Icd63c15d48e595a07c9231f96f8bbaa1432ac0a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5273865
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Yuval Peress <peress@google.com>
    Tested-by: Yuval Peress <peress@google.com>

--- include/driver/accelgyro_lsm6dso_public.h
+++ include/driver/accelgyro_lsm6dso_public.h
@@ -8,8 +8,6 @@
 #ifndef __CROS_EC_ACCELGYRO_LSM6DSO_PUBLIC_H
 #define __CROS_EC_ACCELGYRO_LSM6DSO_PUBLIC_H
 
-#include "driver/accelgyro_lsm6dso_public.h"
-
 /*
  * 7-bit address is 110101xb. Where 'x' is determined
  * by the voltage on the ADDR pin
@@ -20,5 +18,20 @@
 /* Absolute maximum rate for Acc and Gyro sensors */
 #define LSM6DSO_ODR_MIN_VAL 13000
 #define LSM6DSO_ODR_MAX_VAL MOTION_MAX_SENSOR_FREQUENCY(416000, 13000)
+
+/* Who Am I */
+#define LSM6DSO_WHO_AM_I_REG 0x0f
+#define LSM6DSO_WHO_AM_I 0x6c
+
+/* INT1 control register */
+#define LSM6DSO_INT1_CTRL 0x0d
+
+/* CTRL3 register */
+#define LSM6DSO_CTRL3_ADDR 0x12
+#define LSM6DSO_SW_RESET 0x01
+#define LSM6DSO_IF_INC 0x04
+#define LSM6DSO_PP_OD 0x10
+#define LSM6DSO_H_L_ACTIVE 0x20
+#define LSM6DSO_BDU 0x40
 
 #endif /* __CROS_EC_ACCELGYRO_LSM6DSO_PUBLIC_H */
