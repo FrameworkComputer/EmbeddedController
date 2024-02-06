ac4408078c723ea429e98cb79b7e72e68f4f2fe8
Josh-Tsai <josh_tsai@compal.com>
2024-02-06 10:12:38 +0800

    Marigold: implement PECI over eSPI to update PECI power limit value
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- zephyr/program/framework/marigold/src/cpu_power.c
+++ zephyr/program/framework/marigold/src/cpu_power.c
@@ -18,17 +18,13 @@
 
 void update_soc_power_limit(bool force_update, bool force_no_adapter)
 {
-	/*
-	 * power limit is related to AC state, battery percentage, and power budget
-	 */
-
 	int active_power;
 	int pps_power_budget = 0;
 	int battery_percent;
 
 	static int old_pl2_watt = -1;
 	static int old_pl4_watt = -1;
-	static int old_psys_watt = -1;
+	static int old_pl3_watt = -1;
 
 	/* TODO: get the power and pps_power_budget */
 	/* pps_power_budget = cypd_get_pps_power_budget(); */
@@ -41,27 +37,29 @@ void update_soc_power_limit(bool force_update, bool force_no_adapter)
 
 	if (!extpower_is_present() || (active_power < 55)) {
 		/* Battery only or ADP < 55W */
-		pl2_watt = POWER_LIMIT_1_W;
-		pl4_watt = 70 - pps_power_budget;
-		psys_watt = 52 - pps_power_budget;
+		pl2_watt = 35;
+		pl3_watt = 35;
+		pl4_watt = 80;
 	} else if (battery_percent < 30) {
 		/* ADP > 55W and Battery percentage < 30% */
 		pl4_watt = active_power - 15 - pps_power_budget;
 		pl2_watt = MIN((pl4_watt * 90) / 100, 64);
-		psys_watt = ((active_power * 95) / 100) - pps_power_budget;
+		pl3_watt = ((active_power * 95) / 100) - pps_power_budget;
 	} else {
 		/* ADP > 55W and Battery percentage >= 30% */
 		pl2_watt = 64;
 		pl4_watt = 140;
 		/* psys watt = adp watt * 0.95 + battery watt(55 W) * 0.7 - pps power budget */
-		psys_watt = ((active_power * 95) / 100) + 39 - pps_power_budget;
+		pl3_watt = ((active_power * 95) / 100) + 39 - pps_power_budget;
 	}
+
 	if (pl2_watt != old_pl2_watt || pl4_watt != old_pl4_watt ||
-			psys_watt != old_psys_watt || force_update) {
-		old_psys_watt = psys_watt;
+			pl3_watt != old_pl3_watt || force_update) {
+		old_pl3_watt = pl3_watt;
 		old_pl4_watt = pl4_watt;
 		old_pl2_watt = pl2_watt;
 
 		pl1_watt = POWER_LIMIT_1_W;
+		set_pl_limits(pl1_watt, pl2_watt, pl3_watt, pl4_watt);
 	}
 }
