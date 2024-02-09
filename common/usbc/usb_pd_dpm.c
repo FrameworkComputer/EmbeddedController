541051e019d9a87220427a02c34dd4c7f9df3858
Jeremy Bettis <jbettis@google.com>
2024-02-09 16:43:06 +0000

    Fix #line directive
    Commit e5604be39624763b81fad9911f06192467d669a8 added a new header, so
    fix the #line directive.
    
    BUG=b:272518464
    TEST=./util/find_non_exec_lines.py
    
    Change-Id: I1b72f783e8e80695abe962d4e2ef6f604356e8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5281643
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Yuval Peress <peress@google.com>

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -40,7 +40,7 @@
  * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
  * #line marks the *next* line, so it is off by one.
  */
-#line 43
+#line 44
 
 #ifdef CONFIG_ZEPHYR
 #include "temp_sensor/temp_sensor.h"
