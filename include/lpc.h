82402122ef2ad32332728a561a80ab486e86016e
Jeremy Bettis <jbettis@google.com>
2024-04-01 15:10:48 +0000

    ec: Add #line to include/lpc.h
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=BOARD=npcx9
    TEST=zmake build --coverage $BOARD && ./util/find_non_exec_lines.py \
    build/zephyr/$BOARD/output/zephyr.info
    
    Change-Id: Ia5036cfa0c035ea0dc3202755630fb52c8b086bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5408909
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Yuval Peress <peress@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Yuval Peress <peress@google.com>

--- include/lpc.h
+++ include/lpc.h
@@ -5,6 +5,12 @@
 
 /* LPC module for Chrome EC */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 13
+
 #ifndef __CROS_EC_LPC_H
 #define __CROS_EC_LPC_H
 
