987778f185eedf365dca36c13ca63b60da0b9e81
Jason Chen <jason-ch.chen@mediatek.corp-partner.google.com>
2024-04-02 16:30:03 +0800

    mtk scp: Update license headers
    BRANCH=none
    BUG=b:317163840
    TEST=make BOARD=geralt_scp
    
    Change-Id: I44f1d0533b06ca806df2ea11f5564486948c53cf
    Signed-off-by: Jason Chen <jason-ch.chen@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5411971
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>

--- board/geralt_scp/build.mk
+++ board/geralt_scp/build.mk
@@ -1,5 +1,5 @@
 # -*- makefile -*-
-# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Copyright 2024 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
