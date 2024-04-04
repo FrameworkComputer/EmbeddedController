5dec155228e58b56bf64ca8dc335f305adaa5541
Jeremy Bettis <jbettis@google.com>
2024-04-04 13:00:40 -0600

    Enable kb_scan test for coverage
    It seems to be stable now. I ran it 50 times.
    
    FIXED=b:331651887
    TEST=Ran test 50 times with coverage enabled
    
    Change-Id: Ie92196eecc7157837511499ea98628771d285683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5425374
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- test/build.mk
+++ test/build.mk
@@ -170,7 +170,7 @@ cov-dont-test += version
 cov-dont-test += interrupt
 # Flaky tests. The number of covered lines changes from run to run
 # b/213374060
-cov-dont-test += accel_cal entropy flash float kb_mkbp kb_scan kb_scan_strict
+cov-dont-test += accel_cal entropy flash float kb_mkbp kb_scan_strict
 cov-dont-test += rsa
 
 cov-test-list-host = $(filter-out $(cov-dont-test), $(test-list-host))
