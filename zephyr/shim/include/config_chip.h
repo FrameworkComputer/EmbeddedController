ed9981ec24c54163f806b19aaca8ab651a1879a9
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2024-04-09 16:06:20 +0800

    zephyr: shim: config_chip: define CONFIG for variant PD PHY
    Variant chip has different USBPD HW PHY, so based on the
    PHY version to apply different CONFIG settings.
    
    BUG=b:333580634
    TEST=test on Rauru c-port, no disconnection anymore
    
    Change-Id: I12989e97adc5f324af62429b980d2134a1694229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5439954
    Tested-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -1744,7 +1744,10 @@ extern char mock_jump_data[CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE];
 #undef CONFIG_USB_PD_TCPM_DRIVER_IT8XXX2
 #ifdef CONFIG_PLATFORM_EC_USB_PD_TCPM_DRIVER_IT8XXX2
 #define CONFIG_USB_PD_TCPM_DRIVER_IT8XXX2
-#if defined(CONFIG_SOC_IT81202_CX) || defined(CONFIG_SOC_IT81302_CX)
+#ifdef CONFIG_SOC_IT8XXX2_USBPD_PHY_V1
+/* Individual setting CC1 and CC2 resistance. */
+#define IT83XX_USBPD_CC1_CC2_RESISTANCE_SEPARATE
+#elif defined(CONFIG_SOC_IT8XXX2_USBPD_PHY_V2)
 /* CCGCR 04h bit[3,2,1] Rp 3A value is changed to 000b. */
 #define IT8XXX2_USBPD_RP_3A0_VALUE_IS_ZERO
 /*
@@ -1752,9 +1755,6 @@ extern char mock_jump_data[CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE];
  * by CCCSR 05h bit[7,3].
  */
 #define IT8XXX2_USBPD_CCGCR_BIT7_RESERVED
-#else
-/* Individual setting CC1 and CC2 resistance. */
-#define IT83XX_USBPD_CC1_CC2_RESISTANCE_SEPARATE
 #endif
 #endif
 
