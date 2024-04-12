10197449073209a85814b52b4f02a60f6cbf4ce4
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2024-04-12 17:29:53 +0800

    anraggar: Change lid sensor from forced mode to interrupt mode
    Change lid sensor from forced mode to interrupt mode for CTS
    test.
    
    BUG=b:333834135, b:333285756
    TEST=1. zmake build anraggar
         2. ectool motionsense
    LOW_COVERAGE_REASON=Tests will be added in b:333834135
    
    Change-Id: I7b379eea7e18b7403db3a6e903f834bd1b6bb93e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5446496
    Reviewed-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>
    Tested-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/nissa/anraggar/src/board.c
+++ zephyr/program/nissa/anraggar/src/board.c
@@ -10,8 +10,11 @@
  */
 #include "accelgyro.h"
 #include "battery.h"
+#include "chipset.h"
 #include "common.h"
 #include "cros_cbi.h"
+#include "driver/accel_bma4xx.h"
+#include "driver/accel_lis2dw12_public.h"
 #include "driver/accelgyro_bmi323.h"
 #include "driver/accelgyro_lsm6dsm.h"
 #include "gpio/gpio_int.h"
@@ -25,8 +28,6 @@
 
 LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
 
-static bool board_is_clamshell;
-
 static void board_setup_init(void)
 {
 	int ret;
@@ -38,17 +39,54 @@ static void board_setup_init(void)
 		return;
 	}
 	if (val == CLAMSHELL) {
-		board_is_clamshell = true;
 		motion_sensor_count = 0;
 		gmr_tablet_switch_disable();
 		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
 		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
 				      GPIO_INPUT | GPIO_PULL_UP);
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
 	}
 }
 DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_PRE_DEFAULT);
 
+static void motionsense_suspend(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FORM_FACTOR);
+		return;
+	}
+	if (val != CLAMSHELL) {
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, motionsense_suspend, HOOK_PRIO_DEFAULT);
+
+static void motionsense_resume(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FORM_FACTOR);
+		return;
+	}
+	if (val != CLAMSHELL) {
+		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, motionsense_resume, HOOK_PRIO_DEFAULT);
+
 static bool base_use_alt_sensor;
+static bool lid_use_alt_sensor;
 
 void motion_interrupt(enum gpio_signal signal)
 {
@@ -59,10 +97,21 @@ void motion_interrupt(enum gpio_signal signal)
 	}
 }
 
+void lid_accel_interrupt(enum gpio_signal signal)
+{
+	if (lid_use_alt_sensor) {
+		lis2dw12_interrupt(signal);
+	} else {
+		bma4xx_interrupt(signal);
+	}
+}
+
 static void alt_sensor_init(void)
 {
 	base_use_alt_sensor = cros_cbi_ssfc_check_match(
 		CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_lsm6dsm)));
+	lid_use_alt_sensor = cros_cbi_ssfc_check_match(
+		CBI_SSFC_VALUE_ID(DT_NODELABEL(lid_sensor_lis2dw12)));
 
 	motion_sensors_check_ssfc();
 }
