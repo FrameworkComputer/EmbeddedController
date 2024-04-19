ac7b7e9733e2e0599b4592d5f3f0ed726fc6c106
Jeremy Bettis <jbettis@google.com>
2024-04-19 21:51:51 +0000

    Add #line to common/timer.c
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=BOARD=mtlrvpp_npcx
    TEST=zmake build --coverage $BOARD && ./util/find_non_exec_lines.py \
    build/zephyr/$BOARD/output/zephyr.info
    
    Change-Id: Ibbd7691f8bd9f880583759bd172f61293aa7ad08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5468774
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- common/timer.c
+++ common/timer.c
@@ -5,6 +5,12 @@
 
 /* Timer module for Chrome EC operating system */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 13
+
 #include "atomic.h"
 #include "builtin/assert.h"
 #include "common.h"
