d6ad51b27f8be343f672b13c8694a4656cc7a134
Jeremy Bettis <jbettis@google.com>
2024-05-06 21:21:27 +0000

    Add #line to zephyr/emul/retimer/emul_ps8811.c
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=testcase=drivers/drivers.led_driver ; ./twister -c -C --toolchain
    host --coverage-formats lcov -s "${testcase}" &&
    ./util/find_non_exec_lines.py twister-out/coverage.info
    
    Change-Id: I52a3295162d86dee69b2b5273cb705328fd67f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5518888
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>

--- zephyr/emul/retimer/emul_ps8811.c
+++ zephyr/emul/retimer/emul_ps8811.c
@@ -3,6 +3,12 @@
  * found in the LICENSE file.
  */
 
+/*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 11
+
 #include "driver/retimer/ps8811.h"
 #include "emul/emul_common_i2c.h"
 #include "emul/emul_stub_device.h"
