3159d2f18edbc579ea07a0be0ce35285e61566c8
Tom Hughes <tomhughes@chromium.org>
2024-05-06 14:24:33 -0700

    fpsensor: Remove global state from fpsensor_auth_crypto_stateful test
    BUG=b:286119221
    TEST=make run-fpsensor_auth_crypto_stateful
    
    Change-Id: Ief52fcd01b62dc2c13ba1b602d0208d73a688d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5519699
    Reviewed-by: Howard Yang <hcyang@google.com>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- test/fpsensor_auth_crypto_stateful.cc
+++ test/fpsensor_auth_crypto_stateful.cc
@@ -24,21 +24,19 @@
 namespace
 {
 
-void init_tpm_seed(void)
-{
-	std::array<uint8_t, 32> fake_tpm_seed = {
-		0xd9, 0x71, 0xaf, 0xc4, 0xcd, 0x36, 0xe3, 0x60,
-		0xf8, 0x5a, 0xa0, 0xa6, 0x2c, 0xb3, 0xf5, 0xe2,
-		0xeb, 0xb9, 0xd8, 0x2f, 0xb5, 0x78, 0x5c, 0x79,
-		0x82, 0xce, 0x06, 0x3f, 0xcc, 0x23, 0xb9, 0xe7,
-	};
+constexpr std::array<uint8_t, 32> kFakeTpmSeed = {
+	0xd9, 0x71, 0xaf, 0xc4, 0xcd, 0x36, 0xe3, 0x60, 0xf8, 0x5a, 0xa0,
+	0xa6, 0x2c, 0xb3, 0xf5, 0xe2, 0xeb, 0xb9, 0xd8, 0x2f, 0xb5, 0x78,
+	0x5c, 0x79, 0x82, 0xce, 0x06, 0x3f, 0xcc, 0x23, 0xb9, 0xe7,
+};
+static_assert(kFakeTpmSeed.size() == sizeof(global_context.tpm_seed));
 
-	static_assert(fake_tpm_seed.size() == sizeof(global_context.tpm_seed));
-
-	std::ranges::copy(fake_tpm_seed, global_context.tpm_seed);
-
-	global_context.fp_encryption_status |= FP_ENC_STATUS_SEED_SET;
-}
+constexpr std::array<uint8_t, 32> kFakeUserId = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05,  0x06, 0x07,  0x08, 0x09, 0x0a,
+	0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x010, 0x11, 0x012, 0x13, 0x14, 0x15,
+	0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,  0x1c, 0x1d,  0x1e, 0x1f
+};
+static_assert(kFakeUserId.size() == sizeof(global_context.user_id));
 
 test_static enum ec_error_list test_fp_encrypt_decrypt_data(void)
 {
@@ -51,8 +49,8 @@ test_static enum ec_error_list test_fp_encrypt_decrypt_data(void)
 
 	std::copy(input.begin(), input.end(), data.begin());
 
-	TEST_EQ(encrypt_data_in_place(version, info, global_context.user_id,
-				      global_context.tpm_seed, data),
+	TEST_EQ(encrypt_data_in_place(version, info, kFakeUserId, kFakeTpmSeed,
+				      data),
 		EC_SUCCESS, "%d");
 
 	TEST_EQ(info.struct_version, version, "%d");
@@ -61,8 +59,7 @@ test_static enum ec_error_list test_fp_encrypt_decrypt_data(void)
 	TEST_ASSERT_ARRAY_NE(data, input, data.size());
 
 	std::array<uint8_t, 32> output;
-	TEST_EQ(decrypt_data(info, global_context.user_id,
-			     global_context.tpm_seed, data, output),
+	TEST_EQ(decrypt_data(info, kFakeUserId, kFakeTpmSeed, data, output),
 		EC_SUCCESS, "%d");
 
 	TEST_ASSERT_ARRAY_EQ(input, output, sizeof(input));
@@ -82,14 +79,14 @@ test_static enum ec_error_list test_fp_encrypt_decrypt_key(void)
 
 	TEST_NE(key.get(), nullptr, "%p");
 
-	auto enc_key = create_encrypted_private_key(
-		*key, version, global_context.user_id, global_context.tpm_seed);
+	auto enc_key = create_encrypted_private_key(*key, version, kFakeUserId,
+						    kFakeTpmSeed);
 	TEST_ASSERT(enc_key.has_value());
 
 	TEST_EQ(enc_key->info.struct_version, version, "%d");
 
-	bssl::UniquePtr<EC_KEY> out_key = decrypt_private_key(
-		*enc_key, global_context.user_id, global_context.tpm_seed);
+	bssl::UniquePtr<EC_KEY> out_key =
+		decrypt_private_key(*enc_key, kFakeUserId, kFakeTpmSeed);
 
 	TEST_NE(key.get(), nullptr, "%p");
 
@@ -286,8 +283,6 @@ test_static enum ec_error_list test_fp_encrypt_data_with_ecdh_key_in_place(void)
 
 extern "C" void run_test(int argc, const char **argv)
 {
-	init_tpm_seed();
-
 	RUN_TEST(test_fp_encrypt_decrypt_data);
 	RUN_TEST(test_fp_encrypt_decrypt_key);
 	RUN_TEST(test_fp_generate_gsc_session_key);
