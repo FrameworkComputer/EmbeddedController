e9837708cb311af4baf990662339cdc52197ab26
Eric Yilun Lin <yllin@chromium.org>
2024-05-10 11:31:13 -0700

    rauru: enable virtual battery
    BUG=b:308941437
    TEST=virtual battery enabled
    LOW_COVERAGE_REASON=bringup b:308941437
    
    Change-Id: Ib8c18ba13fa565150a5c7005a796cdcf4548c777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5536239
    Code-Coverage: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

---
+++ zephyr/program/rauru/src/i2c.c
@@ -0,0 +1,12 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "i2c.h"
+
+__overridable int
+board_allow_i2c_passthru(const struct i2c_cmd_desc_t *cmd_desc)
+{
+	return cmd_desc->port == I2C_PORT_BATTERY;
+}
