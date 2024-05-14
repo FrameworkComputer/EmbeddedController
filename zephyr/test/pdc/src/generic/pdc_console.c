ae842498db119d232fe8154dc8db265accdd680d
Tristan Honscheid <honscheid@google.com>
2024-05-14 14:01:51 -0600

    pdc: test: Improve `pdc cable_prop` test
    Use the new FFF fakes to test an error code path in this console
    command, and use the shell backend to inspect its output for
    correctness.
    
    BUG=b:339844293
    TEST=./twister -civ -s pdc/pdc.console
    
    Change-Id: Ib232197256b51977428dec9f75b9c1e0035ae69d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5537775
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Tested-by: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>

--- zephyr/test/pdc/src/generic/pdc_console.c
+++ zephyr/test/pdc/src/generic/pdc_console.c
@@ -5,9 +5,11 @@
 
 #include "common.h"
 #include "console.h"
+#include "drivers/ucsi_v3.h"
 #include "mock_pdc_power_mgmt.h"
 
 #include <zephyr/shell/shell.h>
+#include <zephyr/shell/shell_dummy.h>
 #include <zephyr/ztest.h>
 
 #define TEST_PORT 0
@@ -21,6 +23,8 @@ static void console_cmd_pdc_setup(void)
 
 static void console_cmd_pdc_reset(void *fixture)
 {
+	shell_backend_dummy_clear_output(get_ec_shell());
+
 	helper_reset_pdc_power_mgmt_fakes();
 }
 
@@ -36,16 +40,98 @@ ZTEST_USER(console_cmd_pdc, test_no_args)
 		      SHELL_CMD_HELP_PRINTED, rv);
 }
 
+/**
+ * @brief Custom fake for pdc_power_mgmt_get_cable_prop that outputs some test
+ *        cable property info.
+ */
+static int
+custom_fake_pdc_power_mgmt_get_cable_prop(int port, union cable_property_t *out)
+{
+	zassert_not_null(out);
+
+	*out = (union cable_property_t){
+		.bm_speed_supported = 0xabcd,
+		/* 50mA units. This should represent 500mA */
+		.b_current_capablilty = 10,
+		.vbus_in_cable = 1,
+		.cable_type = 1,
+		.directionality = 1,
+		.plug_end_type = USB_TYPE_C,
+		.mode_support = 1,
+		.cable_pd_revision = 3,
+		.latency = 0xf,
+	};
+
+	return 0;
+}
+
 ZTEST_USER(console_cmd_pdc, test_cable_prop)
 {
 	int rv;
+	const char *outbuffer;
+	size_t buffer_size;
 
+	/* Invalid port number */
 	rv = shell_execute_cmd(get_ec_shell(), "pdc cable_prop 99");
 	zassert_equal(rv, -EINVAL, "Expected %d, but got %d", -EINVAL, rv);
+
+	/* Internal pdc_power_mgmt_get_cable_prop() failure */
+	pdc_power_mgmt_get_cable_prop_fake.return_val = 1;
+
+	rv = shell_execute_cmd(get_ec_shell(), "pdc cable_prop 0");
+	zassert_equal(rv, pdc_power_mgmt_get_cable_prop_fake.return_val,
+		      "Expected %d, but got %d",
+		      pdc_power_mgmt_get_cable_prop_fake.return_val, rv);
+
+	RESET_FAKE(pdc_power_mgmt_get_cable_prop);
+
+	/* Happy case */
+	pdc_power_mgmt_get_cable_prop_fake.custom_fake =
+		custom_fake_pdc_power_mgmt_get_cable_prop;
 
 	rv = shell_execute_cmd(get_ec_shell(), "pdc cable_prop 0");
 	zassert_equal(rv, EC_SUCCESS, "Expected %d, but got %d", EC_SUCCESS,
 		      rv);
+
+	/* Sample command output:
+	 *
+	 * ec:> pdc cable_prop 0
+	 * Port 0 GET_CABLE_PROP:
+	 *    bm_speed_supported               : 0x0000
+	 *    b_current_capablilty             : 0 mA
+	 *    vbus_in_cables                   : 0
+	 *    cable_type                       : 0
+	 *    directionality                   : 0
+	 *    plug_end_type                    : 0
+	 *    mode_support                     : 0
+	 *    cable_pd_revision                : 0
+	 *    latency                          : 0
+	 */
+
+	outbuffer =
+		shell_backend_dummy_get_output(get_ec_shell(), &buffer_size);
+
+	zassert_true(buffer_size > 0, NULL);
+
+	zassert_not_null(strstr(outbuffer, "Port 0 GET_CABLE_PROP:"));
+	zassert_not_null(
+		strstr(outbuffer, "bm_speed_supported               : 0xabcd"));
+	zassert_not_null(
+		strstr(outbuffer, "b_current_capablilty             : 500 mA"));
+	zassert_not_null(
+		strstr(outbuffer, "vbus_in_cables                   : 1"));
+	zassert_not_null(
+		strstr(outbuffer, "cable_type                       : 1"));
+	zassert_not_null(
+		strstr(outbuffer, "directionality                   : 1"));
+	zassert_not_null(
+		strstr(outbuffer, "plug_end_type                    : 2"));
+	zassert_not_null(
+		strstr(outbuffer, "mode_support                     : 1"));
+	zassert_not_null(
+		strstr(outbuffer, "cable_pd_revision                : 3"));
+	zassert_not_null(
+		strstr(outbuffer, "latency                          : 15"));
 }
 
 ZTEST_USER(console_cmd_pdc, test_trysrc)
