bea7089ee811ac5b41385d182861983f1d452ce2
Eric Yilun Lin <yllin@chromium.org>
2024-05-15 13:32:10 -0700

    rauru: enable CONFIG_LOG
    Enable Zephyr CONFIG_LOG
    
    BUG=b:308941437
    TEST=zmake build
    LOW_COVERAGE_REASON=b:308941437
    
    Change-Id: If32b2b52d9ae2767c8cd08f07a569c76e1b2a752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5541937
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

---
+++ zephyr/program/rauru/src/common.c
@@ -0,0 +1,8 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(rauru, CONFIG_RAURU_LOG_LEVEL);
