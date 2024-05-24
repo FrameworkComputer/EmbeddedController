9853d8a4648d25ce67052ba36cedb1d07faa3dde
Eric Yilun Lin <yllin@chromium.org>
2024-05-24 12:16:31 -0700

    battery: remove duplicated AC on/off message
    There are duplicated AC on/off messages in the console log.
    These messages are both invoked by HOOK_AC_CHANGE, which makes them redundant.
    
    power/common.c: power_ac_change
    common/battery.c: ac_change
    
    BUG=b:342654637
    TEST=plug/unplug AC and no duplicated AC on/off message
    
    Change-Id: I91f742b9d8173d51bc38dcf7d330d3615e29bd6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5568499
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Tested-by: Eric Yilun Lin <yllin@google.com>

--- common/battery.c
+++ common/battery.c
@@ -436,7 +436,6 @@ static void ac_change(void)
 	static bool was_ac_on;
 	bool key = false;
 
-	CPRINTS("AC %s", extpower_is_present() ? "on" : "off");
 	if (IS_ENABLED(HAS_TASK_KEYSCAN))
 		key = keyboard_scan_get_boot_keys() & BIT(BOOT_KEY_REFRESH);
 
