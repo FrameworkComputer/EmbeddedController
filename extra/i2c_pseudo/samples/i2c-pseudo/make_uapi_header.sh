464814de3c664a66fd5fc9a6c0f0cbce8b7ad53b
Jeremy Bettis <jbettis@google.com>
2024-06-04 22:10:46 +0000

    Remove i2c_pseudo kernel driver
    It is no longer needed and should not be used.
    
    BUG=b:316229747
    TEST=None
    
    Change-Id: I1ae9ff3399c0d8d6455bee94fbe31a6d920a72d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5597923
    Commit-Queue: Alexandru Stan <amstan@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Alexandru Stan <amstan@chromium.org>

--- extra/i2c_pseudo/samples/i2c-pseudo/make_uapi_header.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Copyright 2023 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# TODO: upstream patch: remove this file
-
-grep -v -E '^\s*#include\s+<linux/compiler\.h>\s*$' | sed 's/ __user / /g'
