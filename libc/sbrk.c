8d1ced3710be669f3f16c7c3a802073b30337a47
Dawid Niedzwiecki <dawidn@google.com>
2024-06-18 07:20:28 +0200

    libc: move sbrk to a separate file
    Move the CrosEC version of sbrk function to a separate file.
    
    It shouldn't change anything in the build.
    
    BUG=b:324574796
    TEST=make buildall && zmake build -a
    LOW_COVERAGE_REASON=Tests added in CL:5639700 as a part of b:324574796
    
    Change-Id: Ifdb5a3cd5fe352e382f412173aa27acb72d7a6ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5639698
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ libc/sbrk.c
@@ -0,0 +1,38 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "link_defs.h"
+#include "shared_mem.h"
+#include "system.h"
+
+#include <errno.h>
+
+/**
+ * Change program's data space by increment bytes.
+ *
+ * This function is called from the libc sbrk() function (which is in turn
+ * called from malloc() when memory needs to be allocated or released).
+ *
+ * @param incr[in] amount to increment or decrement. 0 means return current
+ * program break.
+ * @return the previous program break (address) on success
+ * @return (void*)-1 on error and errno is set to ENOMEM.
+ */
+void *_sbrk(intptr_t incr)
+{
+	static char *heap_end = __shared_mem_buf;
+	char *prev_heap_end;
+
+	if ((heap_end + incr < __shared_mem_buf) ||
+	    (heap_end + incr > (__shared_mem_buf + shared_mem_size()))) {
+		errno = ENOMEM;
+		return (void *)-1;
+	}
+
+	prev_heap_end = heap_end;
+	heap_end += incr;
+
+	return (void *)prev_heap_end;
+}
