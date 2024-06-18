d981d6a09124397e7a828b9145aaa8ad2a4fbbb3
Patryk Duda <patrykd@google.com>
2024-06-18 13:43:54 +0200

    cmake/toolchain/llvm: Enable FPU when compiling with armv7m-cros-eabi
    The armv7m-cros-eabi compiler-rt was compiled with assumption that it
    will run on Cortex-M4 MCU with hardware FPU. It means that even with
    '-mfpu=none' option the floating point instructions will be used
    (because they are used in compiler-rt).
    
    Let's enable FPU always when compiling for ARMv7-M.
    
    BUG=b:324574796
    TEST=zmake build bloonchipper
    TEST=Disable FPU in config and make sure that 'zmake build bloonchipper'
         fails.
    
    Change-Id: Ic2781502edeb5c7b878655cc117a8853b979266b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5636295
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Patryk Duda <patrykd@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Patryk Duda <patrykd@google.com>

--- zephyr/cmake/toolchain/llvm/target.cmake
+++ zephyr/cmake/toolchain/llvm/target.cmake
@@ -17,6 +17,14 @@ if("${ARCH}" STREQUAL "arm")
   if(DEFINED CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
     # ARMV7_M_ARMV8_M_MAINLINE means that ARMv7-M or backward compatible ARMv8-M
     # processor is used.
+    #
+    # The armv7m-cros-eabi compiler-rt was compiled with assumption that it will
+    # run on Cortex-M4 MCU with enabled FPU. Calling builtin functions that use
+    # floating-point instructions (e.g. __aeabi_ul2d) with FPU disabled leads to
+    # Usage Fault. Let's check if FPU is enabled.
+    if(NOT DEFINED CONFIG_FPU)
+      message(FATAL_ERROR "The armv7m-cros-eabi toolchain requires enabled FPU")
+    endif()
     set(CROSS_COMPILE_TARGET armv7m-cros-eabi)
   elseif(DEFINED CONFIG_ARMV6_M_ARMV8_M_BASELINE)
     # ARMV6_M_ARMV8_M_BASELINE means that ARMv6-M or ARMv8-M supporting the
