2e423834a2eb0a5e9d107387db0cf2a73bfae6d4
Firas Sammoura <fsammoura@google.com>
2024-06-18 17:51:37 +0000

    util: Change ZEPHYR_BASE to absolute path
    ZEPHYR_BASE is currently defined relative to the working directory. This
    will be wrong for the fingerprint board release branches, where the
    relative path is different. Initialize ZEPHYR_BASE using the absolute
    path.
    
    BUG=b:314208406
    TEST=make buildall -j100
    
    Change-Id: Iac95562b4d23498f6c3e95d639aef2363982fe57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5638936
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- util/linux_ec_commands_h_check.sh
+++ util/linux_ec_commands_h_check.sh
@@ -6,7 +6,21 @@
 
 set -e
 
-: "${ZEPHYR_BASE:=$(realpath ../../../src/third_party/zephyr/main)}"
+# Get the script's absolute directory path.
+chromiumos_src_dir=$(dirname "$(realpath "$0")")
+
+# Loop until we reach the root directory.
+while [[ ${chromiumos_src_dir} != "/" ]]; do
+  # Check if .repo directory exists.
+  if [[ -d ${chromiumos_src_dir}/.repo ]]; then
+    echo "${chromiumos_src_dir}"
+    break
+  fi
+  # Go up one level in the directory tree.
+  chromiumos_src_dir=$(dirname "${chromiumos_src_dir}")
+done
+
+: "${ZEPHYR_BASE:=${chromiumos_src_dir}/src/third_party/zephyr/main}"
 TMP="$(mktemp -d)"
 ec_commands_file_out="${TMP}/cros_ec_commands.h"
 
