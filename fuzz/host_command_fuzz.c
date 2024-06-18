5fcebeae698a5095233c1db09f853100ede9f3f1
Tom Hughes <tomhughes@chromium.org>
2024-06-18 08:53:34 -0700

    fuzz: Increase request/response buffer size
    The request/response buffer size should be large enough to hold the
    maximum size host command.
    
    BUG=b:343430512
    TEST=none
    
    Change-Id: Ife7a8c13149d606b49f691dad9a7326e52fd3fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5640335
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Yi Chou <yich@google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- fuzz/host_command_fuzz.c
+++ fuzz/host_command_fuzz.c
@@ -25,8 +25,12 @@
 #define TASK_EVENT_FUZZ TASK_EVENT_CUSTOM_BIT(0)
 #define TASK_EVENT_HOSTCMD_DONE TASK_EVENT_CUSTOM_BIT(1)
 
-/* Request/response buffer size (and maximum command length) */
-#define BUFFER_SIZE 128
+/*
+ * Request/response buffer size (and maximum command length).
+ * See comments in libec/ec_command.h:
+ * https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform2/libec/ec_command.h;l=43-58;drc=2d8cc86c157e60b412f82bc536e542fd1c164ccb
+ */
+#define BUFFER_SIZE 544
 
 struct host_packet pkt;
 static uint8_t resp_buf[BUFFER_SIZE];
