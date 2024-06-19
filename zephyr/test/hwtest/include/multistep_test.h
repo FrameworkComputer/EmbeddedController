b3dc06d14a867a044545b925cfd79d3d83066c0a
Dawid Niedzwiecki <dawidn@google.com>
2024-06-19 09:28:40 +0200

    zephyr: test: update multistep framework
    Run only testsuite defined with the MULTISTEP_TEST macro after reboot,
    not all suites.
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests ftrapv --zephyr'
    
    Change-Id: I40276b3217188a5ffeae0142a794944aeb6003a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5676796
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Reviewed-by: Patryk Duda <patrykd@google.com>

--- zephyr/test/hwtest/include/multistep_test.h
+++ zephyr/test/hwtest/include/multistep_test.h
@@ -11,6 +11,7 @@
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
+#include <zephyr/ztest_test.h>
 
 /**
  * @brief Register a multi-step test.
@@ -21,52 +22,52 @@
  * @param steps Array of steps which cause reboot/crash. Each step is a pointer
  * to a void function that takes no arguments.
  */
-#define MULTISTEP_TEST(name, steps)                                           \
-	static void multisetep_test_teardown(void *fixture)                   \
-	{                                                                     \
-		system_set_scratchpad(0);                                     \
-	}                                                                     \
-	ZTEST_SUITE(name, NULL, NULL, NULL, NULL, multisetep_test_teardown);  \
-	ZTEST(name, test_##name)                                              \
-	{                                                                     \
-		uint32_t step = 0;                                            \
-		int ret = 0;                                                  \
-                                                                              \
-		ret = system_get_scratchpad(&step);                           \
-		zassert_equal(ret, 0);                                        \
-		ret = system_set_scratchpad(step + 1);                        \
-                                                                              \
-		steps[step]();                                                \
-	}                                                                     \
-                                                                              \
-	/* If the test shell is enabled, the test will be run once by a test  \
-	 * runner. The steps call cause reboot/crash, which means we need to  \
-	 * run it again.                                                      \
-	 */                                                                   \
-	IF_ENABLED(CONFIG_ZTEST_SHELL,                                        \
-		   (                                                          \
-			   static struct k_work multistep_test_work;          \
-			   static void multistep_test_handler(                \
-				   struct k_work *work) {                     \
-				   uint32_t step = 0;                         \
-                                                                              \
-				   system_get_scratchpad(&step);              \
-				   if (step != 0) {                           \
-					   ztest_run_test_suites(NULL, false, \
-								 1, 1);       \
-				   }                                          \
-			   }                                                  \
-                                                                              \
-			   static int multistep_test_init(void) {             \
-				   k_work_init(&multistep_test_work,          \
-					       multistep_test_handler);       \
-                                                                              \
-				   /* Check if the test has to be run after   \
-				    * reboot */                               \
-				   k_work_submit(&multistep_test_work);       \
-                                                                              \
-				   return 0;                                  \
-			   } SYS_INIT(multistep_test_init, POST_KERNEL,       \
+#define MULTISTEP_TEST(name, steps)                                          \
+	static void multisetep_test_teardown(void *fixture)                  \
+	{                                                                    \
+		system_set_scratchpad(0);                                    \
+	}                                                                    \
+	ZTEST_SUITE(name, NULL, NULL, NULL, NULL, multisetep_test_teardown); \
+	ZTEST(name, test_##name)                                             \
+	{                                                                    \
+		uint32_t step = 0;                                           \
+		int ret = 0;                                                 \
+                                                                             \
+		ret = system_get_scratchpad(&step);                          \
+		zassert_equal(ret, 0);                                       \
+		ret = system_set_scratchpad(step + 1);                       \
+                                                                             \
+		steps[step]();                                               \
+	}                                                                    \
+                                                                             \
+	/* If the test shell is enabled, the test will be run once by a test \
+	 * runner. The steps call cause reboot/crash, which means we need to \
+	 * run it again.                                                     \
+	 */                                                                  \
+	IF_ENABLED(CONFIG_ZTEST_SHELL,                                       \
+		   (                                                         \
+			   static struct k_work multistep_test_work;         \
+			   static void multistep_test_handler(               \
+				   struct k_work *work) {                    \
+				   uint32_t step = 0;                        \
+                                                                             \
+				   system_get_scratchpad(&step);             \
+				   if (step != 0) {                          \
+					   ztest_run_test_suite(name, false, \
+								1, 1);       \
+				   }                                         \
+			   }                                                 \
+                                                                             \
+			   static int multistep_test_init(void) {            \
+				   k_work_init(&multistep_test_work,         \
+					       multistep_test_handler);      \
+                                                                             \
+				   /* Check if the test has to be run after  \
+				    * reboot */                              \
+				   k_work_submit(&multistep_test_work);      \
+                                                                             \
+				   return 0;                                 \
+			   } SYS_INIT(multistep_test_init, POST_KERNEL,      \
 				      CONFIG_APPLICATION_INIT_PRIORITY);))
 
 #endif /* _HWTEST_MULTISTEP_TEST_ */
