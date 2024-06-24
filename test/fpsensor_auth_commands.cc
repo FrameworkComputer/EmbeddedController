ca05f1ea213ce4dc6e175cd4bcece7f85fc01088
Tom Hughes <tomhughes@chromium.org>
2024-06-24 13:53:19 -0700

    test/fpsensor_auth_commands_otp: Add OTP test variant
    Since CONFIG_OTP_KEY is a compile-time option, we need a separate test
    that is the same as the fpsensor_auth_commands test, but with
    CONFIG_OTP_KEY enabled.
    
    BUG=b:323094297
    TEST=make run-fpsensor_auth_commands_otp
    
    Change-Id: Ie372281379d3a543bc70ab9ef3877c8764f8f84f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5651567
    Reviewed-by: David Cross <davidmcross@google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Howard Yang <hcyang@google.com>
    Tested-by: Tom Hughes <tomhughes@chromium.org>

--- test/fpsensor_auth_commands.cc
+++ test/fpsensor_auth_commands.cc
@@ -11,6 +11,7 @@
 #include "fpsensor/fpsensor_state.h"
 #include "fpsensor/fpsensor_template_state.h"
 #include "mock/fpsensor_state_mock.h"
+#include "mock/otpi_mock.h"
 #include "openssl/aes.h"
 #include "openssl/bn.h"
 #include "openssl/ec.h"
@@ -672,6 +673,15 @@ test_fp_command_read_match_secret_with_pubkey_succeed(void)
 	};
 
 	/* Expected positive_match_secret same as  in test/fpsensor_crypto.c */
+#ifdef CONFIG_OTP_KEY
+	static const uint8_t
+		expected_positive_match_secret_for_empty_user_id[] = {
+			0x2f, 0x78, 0x2d, 0xd2, 0x0a, 0xa9, 0xa2, 0x17,
+			0xc6, 0x4d, 0xa3, 0x1a, 0x02, 0xef, 0x4e, 0x2c,
+			0xf9, 0x23, 0xe1, 0x2d, 0x12, 0x3e, 0xa9, 0xe3,
+			0xc9, 0x16, 0x6f, 0x98, 0x39, 0x8b, 0x0e, 0xc5,
+		};
+#else
 	static const uint8_t
 		expected_positive_match_secret_for_empty_user_id[] = {
 			0x8d, 0xc4, 0x5b, 0xdf, 0x55, 0x1e, 0xa8, 0x72,
@@ -679,6 +689,7 @@ test_fp_command_read_match_secret_with_pubkey_succeed(void)
 			0xde, 0x38, 0xd5, 0x03, 0xce, 0xe4, 0x74, 0x51,
 			0x63, 0x6c, 0x6a, 0x26, 0xa9, 0xb7, 0xfa, 0x68,
 		};
+#endif
 
 	/* Create positive secret match state with valid deadline value,
 	 * readable state, and correct template matched
@@ -1583,6 +1594,14 @@ void run_test(int argc, const char **argv)
 	RUN_TEST(test_fp_command_check_context_cleared);
 	RUN_TEST(test_fp_command_generate_nonce);
 	RUN_TEST(test_fp_command_commit_without_seed);
+
+	/*
+	 * Set the OTP key here since the following tests require it.
+	 */
+	if (IS_ENABLED(CONFIG_OTP_KEY)) {
+		std::ranges::copy(default_fake_otp_key,
+				  mock_otp.otp_key_buffer);
+	}
 
 	// All tests after this require the TPM seed to be set.
 	RUN_TEST(test_set_fp_tpm_seed);
