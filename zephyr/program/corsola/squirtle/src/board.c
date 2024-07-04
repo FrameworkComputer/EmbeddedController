101f0eac68daf006b99e5f5f7bf528b2088ed138
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-07-04 16:09:43 +0800

    squirtle: Modify battery detection mechanism
    Battery manufacturers have revised the PF judgment
    logic based on customer needs. The PF judgment has
    been moved to bit 12 of the 0x00 register.
    
    BUG=b:345163724
    TEST=zmake build squirtle
    LOW_COVERAGE_REASON=Tests will be added in b:345163724
    
    Change-Id: Icaeaf4794d4e5becdca5c55348a113ce0fd29ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5678256
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>

--- zephyr/program/corsola/squirtle/src/board.c
+++ zephyr/program/corsola/squirtle/src/board.c
@@ -10,7 +10,6 @@
 #include "gpio.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
-#include "i2c.h"
 #include "math_util.h"
 #include "util.h"
 
@@ -51,44 +50,29 @@ __override struct dps_config_t dps_config = {
 
 enum battery_present battery_is_present(void)
 {
+	int state;
 	struct battery_static_info *bs = &battery_static[BATT_IDX_MAIN];
+
+	/*
+	 * When the battery information is not ready, it is determined that
+	 * the battery is not present.
+	 */
+	if (!strcasecmp(bs->model_ext, ""))
+		return BP_NO;
 
 	if (gpio_get_level(GPIO_BATT_PRES_ODL))
 		return BP_NO;
 
-	if (!strcasecmp(bs->model_ext, "AP23A7L")) {
-		uint8_t state[4];
-		uint8_t page = 0x54;
+	/*
+	 *  According to the battery manufacturer's reply:
+	 *  To detect a bad battery, need to read the 0x00 register.
+	 *  If the 12th bit(Permanently Failure) is 1, it means a bad battery.
+	 */
+	if (sb_read(SB_MANUFACTURER_ACCESS, &state))
+		return BP_NO;
 
-		/*
-		 * According to the battery manufacturer's reply:
-		 * To detect a bad battery, need to write 0x54 to the
-		 * 0x00 register and then read the 0x23. If the 12th
-		 * bit(Permanently Failure) is 1, it means a bad battery.
-		 */
-		sb_write_block(SB_AP23A7L, &page, 1);
-
-		if (sb_read_sized_block(SB_MANUFACTURER_DATA, state, 4))
-			return BP_NO;
-
-		/* Detect the 12th bit value */
-		if (state[1] & BIT(4))
-			return BP_NO;
-	} else if (!strcasecmp(bs->model_ext, "AP23A8L")) {
-		int value;
-
-		/*
-		 * According to the battery manufacturer's reply:
-		 * To detect a bad battery, need to read the 0x43 register.
-		 * If the second bit(Permanently Failure) is 1, it means a bad
-		 * battery.
-		 */
-		if (sb_read(SB_PACK_STATUS, &value))
-			return BP_NO;
-
-		if (value & BIT(2))
-			return BP_NO;
-	} else if (!strcasecmp(bs->model_ext, ""))
+	/* Detect the 12th bit value */
+	if (state & BIT(12))
 		return BP_NO;
 
 	return BP_YES;
