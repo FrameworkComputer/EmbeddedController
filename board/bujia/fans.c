ab81bf69e6e37d00522f5edeb446263e683b5a90
Devin Lu <Devin.Lu@quantatw.com>
2024-07-08 18:05:20 +0800

    bujia: Update fan real rpm
    BUG=b:351917517
    TEST=make BOARD=bujia
    
    Change-Id: Ia607575d1b4761adce7d218f6ccf453bdac39d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5682642
    Tested-by: Devin Lu <devin.lu@quantatw.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Devin Lu <devin.lu@quantatw.com>

--- board/bujia/fans.c
+++ board/bujia/fans.c
@@ -31,15 +31,13 @@ static const struct fan_conf fan_conf_0 = {
 };
 
 /*
- * TOOD(b/197478860): need to update for real fan
- *
- * Prototype fan spins at about 7200 RPM at 100% PWM.
+ * The fan spins at about 5000 RPM at 100% PWM.
  * Set minimum at around 30% PWM.
  */
 static const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 2000,
-	.rpm_start = 2000,
-	.rpm_max = 4700,
+	.rpm_min = 2100,
+	.rpm_start = 2100,
+	.rpm_max = 5000,
 };
 
 const struct fan_t fans[FAN_CH_COUNT] = {
