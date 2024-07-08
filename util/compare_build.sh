c110ac010c47f3230e48991b087f10fb14ebcf6e
Firas Sammoura <fsammoura@google.com>
2024-07-08 18:27:31 +0000

    util: Enable compare build with the unnested fingerprint repos
    The fingerprint private repos have been unnested. The private files are
    at ../ec-private and ../fingerprint relative to the ec directory. Make
    the necessary modifications to ensure that the private repos are
    included.
    
    BUG=b:349187604, b:314208406
    TEST=run ./util/compare_build.sh -b bloonchipper --private
         modify sensor_owner = 0xFC from, 0xFF inside fp_sensor_unlock
         function inside fpc_private.c file. Make sure the compare_build.sh
         fails.
    TEST=run ./util/compare_build.sh -b bloonchipper --private
         make sure the private libraries are linked and remaining RAM is the
         same as when makefile is run.
    
    Change-Id: I350aef2b6c51eac68c6b15d1649564258c828fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5685168
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- util/compare_build.sh
+++ util/compare_build.sh
@@ -40,7 +40,7 @@
 . "${SHFLAGS:-/usr/share/misc/shflags}" || exit 1
 
 FLAGS_PRIVATE_DEFAULT="${FLAGS_FALSE}"
-if [[ -d private ]]; then
+if [[ -d ../ec-private && -d ../fingerprint ]]; then
   FLAGS_PRIVATE_DEFAULT="${FLAGS_TRUE}"
 fi
 
@@ -218,8 +218,9 @@ printf "%s\n" "${BOARDS[@]}" | sort | column
 # Symbolically linked directories
 LINKS=( )
 if [[ "${FLAGS_private}" == "${FLAGS_TRUE}" ]]; then
-  echo "# Requesting private directory link"
-  LINKS+=( private )
+  echo "# Requesting ec-private and fingerprint directory links"
+  LINKS+=( ec-private )
+  LINKS+=( fingerprint )
 fi
 
 ##########################################################################
@@ -233,6 +234,7 @@ fi
 echo "# Preparing Makefile"
 cat > "${TMP_DIR}/Makefile" <<HEREDOC
 ORIGIN ?= $(realpath .)
+PARENT_DIR ?= $(realpath ../)
 BORINGSSL_DIR ?= $(realpath ../../third_party/boringssl)
 CRYPTOC_DIR ?= $(realpath ../../third_party/cryptoc)
 ZEPHYR_BASE ?= $(realpath ../../../src/third_party/zephyr/main)
@@ -243,14 +245,14 @@ LINKS ?= ${LINKS[*]}
 all: build-${OLD_REF} build-${NEW_REF}
 
 ec-%:
-	git clone --quiet --no-checkout --shared \$(ORIGIN) \$@
-	git -C \$@ checkout --quiet \$(@:ec-%=%)
+	git clone --quiet --no-checkout --shared \$(ORIGIN) \$(addprefix \$@/, ec)
+	git -C \$(addprefix \$@/, ec) checkout --quiet \$(@:ec-%=%)
 ifneq (\$(LINKS),)
-	ln -s \$(addprefix \$(ORIGIN)/,\$(LINKS)) \$@
+	ln -s \$(addprefix \$(PARENT_DIR)/,\$(LINKS)) \$@
 endif
 
 build-%: ec-%
-	\$(MAKE) --no-print-directory -C \$(@:build-%=ec-%)                   \\
+	\$(MAKE) --no-print-directory -C \$(@:build-%=\$(addprefix ec-%/, ec))  \\
 		STATIC_VERSION=1                                              \\
 		BORINGSSL_DIR=\$(BORINGSSL_DIR)                               \\
 		CRYPTOC_DIR=\$(CRYPTOC_DIR)                                   \\
@@ -260,9 +262,9 @@ build-%: ec-%
 	@mkdir -p \$@
 	@for b in \$(BOARDS); do	                                      \\
 		printf "  CP -l   '%s' to '%s'\n"                             \\
-			"\$(@:build-%=ec-%)/build/\$\$b/ec.bin"               \\
+			"\$(@:build-%=ec-%)/ec/build/\$\$b/ec.bin"               \\
                         "\$@/\$\$b-ec.bin";                                   \\
-		cp -l \$(@:build-%=ec-%)/build/\$\$b/ec.bin \$@/\$\$b-ec.bin; \\
+		cp -l \$(@:build-%=ec-%)/ec/build/\$\$b/ec.bin \$@/\$\$b-ec.bin; \\
 	done
 
 # So that make doesn't try to remove them
