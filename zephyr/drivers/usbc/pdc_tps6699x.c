8890ee73f6bbf8b04c2f5556d20d134d8bbc9cd8
Ivan Chen <yulunchen@google.com>
2024-07-08 16:51:26 +0800

    pdc: ti: adjust interrupt mask
    power_swap_complete and fr_swap_complete should be handled to update the connector status after a PRS or FRS to reflect the correct PE state
    
    BUG=b:349868822
    TEST=trigger PRS and FRS from external device
    LOW_COVERAGE_REASON=Will be covered once tests can run; see b/345292002
    
    Change-Id: I9d2e4724a03bd14e06c2f8e09842a49941fc811d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5681627
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Tested-by: Ivan Chen <yulunchen@google.com>

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -346,7 +346,9 @@ static void st_irq_run(void *o)
 	if (interrupt_pending) {
 		/* Set CCI EVENT for connector change */
 		data->cci_event.connector_change =
-			pdc_interrupt.plug_insert_or_removal;
+			(pdc_interrupt.plug_insert_or_removal |
+			 pdc_interrupt.power_swap_complete |
+			 pdc_interrupt.fr_swap_complete);
 		/* Set CCI EVENT for not supported */
 		data->cci_event.not_supported =
 			pdc_interrupt.not_supported_received;
@@ -1624,6 +1626,8 @@ static int pdc_interrupt_mask_init(struct pdc_data_t *data)
 	union reg_interrupt irq_mask = {
 		.pd_hardreset = 1,
 		.plug_insert_or_removal = 1,
+		.power_swap_complete = 1,
+		.fr_swap_complete = 1,
 		.status_updated = 1,
 		.power_event_occurred_error = 1,
 		.externl_dcdc_event_received = 1,
