a5db8197ac01f843c5dde6f203ad1b7a869b4465
Eric Yilun Lin <yllin@chromium.org>
2024-07-14 19:02:58 -0700

    rauru: update CBI config
    1. Add CBI fields HDMI, and fingerprint
    2. Handle the HDMI interrupt based on the CBI HDMI value.
    
    BUG=b:341659416
    TEST=cbi set 6 0x8806000 4; and check HDMI presented
    LOW_COVERAGE_REASON=early bringup b:341659416
    
    Change-Id: I73e63c623d3904c32aba6aab0d7fcb175acd21d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5714397
    Reviewed-by: Knox Chiou <knoxchiou@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/rauru/include/rauru_dp.h
+++ zephyr/program/rauru/include/rauru_dp.h
@@ -18,6 +18,7 @@ int rauru_is_dp_muxable(enum rauru_dp_port port);
 void rauru_set_dp_path(int port);
 enum rauru_dp_port rauru_get_dp_path(void);
 bool rauru_is_hpd_high(enum rauru_dp_port port);
+bool rauru_has_hdmi_port(void);
 /* Detach and rotate the DP path */
 void rauru_detach_dp_path(enum rauru_dp_port port);
 #endif /* ZEPHYR_RAURU_INCLUDE_RAURU_DP_H_ */
