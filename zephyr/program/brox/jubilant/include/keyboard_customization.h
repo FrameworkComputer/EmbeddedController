57b132fd66faf14eb7497f0c864f476f6ef59fef
ben chen <ben.chen2@quanta.corp-partner.google.com>
2024-07-16 20:00:35 +0800

    jubilant: implement keyboard matrix
    Implement the customization matrix for jubilant
    
    BUG=b:353110928
    TEST="zmake build jubilant --clobber"
         "./twister -ci -T zephyr/test/brox/ -s brox.jubilant.keyboard"
         "./twister -T zephyr/test/brox/"
         success
    
    Change-Id: I30b11067ad94459d80e07d7e12dbc148f771eefa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5710812
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Commit-Queue: Bob Moragues <moragues@chromium.org>
    Auto-Submit: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Tested-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/program/brox/jubilant/include/keyboard_customization.h
@@ -0,0 +1,23 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Keyboard configuration */
+
+#ifndef __KEYBOARD_CUSTOMIZATION_H
+#define __KEYBOARD_CUSTOMIZATION_H
+
+/*
+ * KEYBOARD_COLS_MAX has the build time column size. It's used to allocate
+ * exact spaces for arrays. Actual keyboard scanning is done using
+ * keyboard_cols, which holds a runtime column size.
+ */
+#ifdef CONFIG_KEYBOARD_CUSTOMIZATION
+#undef KEYBOARD_COLS_MAX
+#undef KEYBOARD_ROWS
+
+#define KEYBOARD_COLS_MAX 18
+#define KEYBOARD_ROWS 8
+#endif
+#endif /* __KEYBOARD_CUSTOMIZATION_H */
