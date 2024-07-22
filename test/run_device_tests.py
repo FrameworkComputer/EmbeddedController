67d40c531c064beffe63fe6c0a506aee408c1eac
Firas Sammoura <fsammoura@google.com>
2024-07-22 01:25:52 +0000

    run_device_tests: run hw_write_protect before power toggle
    If the HW WP GPIO is changed after the boot process has completed,
    changes system_is_locked won't take effect until the next reboot. We should set the HW WP GPIO before toggling power to ensure that the
    system is locked.
    
    BUG=b:324677118,b:349664212
    TEST=run_device_tests.py -b helipilot -t sram_mpu_protection
    TEST=run_device_tests.py -b helipilot -t system_is_locked.*
    
    Change-Id: I6ba2447682201ce1fcef982167258caf72178db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5727982
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -1277,13 +1277,13 @@ def flash_and_run_test(
             console_file = open(get_console(board_config), "wb+", buffering=0)
             console = stack.enter_context(console_file)
 
+        hw_write_protect(test.enable_hw_write_protect)
+
         if test.toggle_power:
             power_cycle(board_config)
         else:
             # In some cases flash_ec leaves the board off, so just ensure it is on
             power(board_config, power_on=True)
-
-        hw_write_protect(test.enable_hw_write_protect)
 
         # run the test
         logging.info('Running test: "%s"', test.config_name)
