801e206c96668e16aa23844af81687ee5ae8ff88
Dawid Niedzwiecki <dawidn@google.com>
2024-07-25 11:45:43 +0200

    zephyr: fpmcu: add fpsensor_auth_crypto_stateless test
    Add the fpsensor_auth_crypto_stateless test. It verifies fp encryption
    functions.
    
    This code was migrated from the following EC test:
    https://chromium.googlesource.com/chromiumos/platform/ec/+/5aa9baf19b6b8e1c49276c8c37418d0a2d96f91a/test/fpsensor_auth_crypto_stateless.cc
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests fpsensor_auth_crypto_stateless --zephyr'
    
    Change-Id: I83d53c148788a3b9580b206fc1d2cbf7a2f5027f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5741693
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- util/migrated_files.sh
+++ util/migrated_files.sh
@@ -22,6 +22,7 @@ test/exception.cc
 test/flash_write_protect.c
 test/fp_transport.c
 test/fpsensor_auth_crypto_stateful.cc
+test/fpsensor_auth_crypto_stateless.cc
 test/ftrapv.c
 test/libc_printf.c
 test/malloc.c
