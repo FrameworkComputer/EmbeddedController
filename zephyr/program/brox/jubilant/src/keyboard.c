45d3249398c57d9b51dab69d11a2074298d80b02
ben chen <ben.chen2@quanta.corp-partner.google.com>
2024-08-02 17:13:34 +0800

    jubilant: support strauss keyboard
    Implement the customization matrix for jubilant
    
    BUG=b:353110928  b:355735466
    TEST="zmake build jubilant --clobber"
         "./twister -ci -T zephyr/test/brox/ -s brox.jubilant.keyboard"
         "./twister -T zephyr/test/brox/"
         success
    
    Change-Id: I567a65a20622ab893fee7db9ef3bde8ecda2b3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5757968
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: Ben Chen <ben.chen2@quanta.corp-partner.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Auto-Submit: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- zephyr/program/brox/jubilant/src/keyboard.c
+++ zephyr/program/brox/jubilant/src/keyboard.c
@@ -4,9 +4,9 @@
  */
 
 #include "ec_commands.h"
+#include "gpio_it8xxx2.h"
 #include "hooks.h"
 #include "keyboard_8042_sharedlib.h"
-#include "keyboard_raw.h"
 #include "keyboard_scan.h"
 
 #include <drivers/vivaldi_kbd.h>
@@ -17,12 +17,15 @@
  * that we don't have pin 0.
  */
 const int keyboard_factory_scan_pins[][2] = {
-	{ -1, -1 }, { -1, -1 }, { -1, -1 }, { 3, 1 },  { 3, 0 }, { 2, 7 },
-	{ 2, 6 },   { 2, 5 },	{ 2, 4 },   { 2, 3 },  { 2, 2 }, { 8, 3 },
-	{ 8, 2 },   { 0, 4 },	{ 0, 5 },   { 0, 6 },  { 0, 7 }, { 1, 0 },
-	{ 1, 1 },   { 1, 2 },	{ 1, 3 },   { 1, 4 },  { 1, 5 }, { 1, 6 },
-	{ 1, 7 },   { 2, 0 },	{ 2, 1 },   { 11, 1 }, { 0, 3 }, { -1, -1 },
-	{ -1, -1 }, { -1, -1 }, { -1, -1 },
+	{ -1, -1 },	  { -1, -1 },	    { -1, -1 },	      { GPIO_KSI, 0 },
+	{ GPIO_KSI, 1 },  { GPIO_KSI, 2 },  { GPIO_KSI, 3 },  { GPIO_KSI, 4 },
+	{ GPIO_KSI, 5 },  { GPIO_KSI, 6 },  { GPIO_KSI, 7 },  { GPIO_KSOH, 7 },
+	{ GPIO_KSOH, 6 }, { GPIO_KSOH, 5 }, { GPIO_KSOH, 4 }, { GPIO_KSOH, 3 },
+	{ GPIO_KSOH, 2 }, { GPIO_KSOH, 1 }, { GPIO_KSOH, 0 }, { GPIO_KSOL, 7 },
+	{ GPIO_KSOL, 6 }, { GPIO_KSOL, 5 }, { GPIO_KSOL, 4 }, { GPIO_KSOL, 3 },
+	{ GPIO_KSOL, 2 }, { GPIO_KSOL, 1 }, { GPIO_KSOL, 0 }, { GPIO_C, 5 },
+	{ GPIO_C, 3 },	  { -1, -1 },	    { -1, -1 },	      { -1, -1 },
+	{ -1, -1 },
 };
 const int keyboard_factory_scan_pins_used =
 	ARRAY_SIZE(keyboard_factory_scan_pins);
