ec4d82f55b93c6009d7eef68b1c824c28509f1e1
Jeremy Bettis <jbettis@google.com>
2024-08-05 14:06:44 -0600

    Remove myst
    BUG=None
    TEST=None
    
    Change-Id: Iff5feb6891be583441d18ada4b66073ddb9af815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5763418
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Jonathon Murphy <jpmurphy@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>

--- zephyr/program/myst/src/common.c
+++
@@ -1,8 +0,0 @@
-/* Copyright 2023 The ChromiumOS Authors
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_REGISTER(myst, CONFIG_MYST_LOG_LEVEL);
