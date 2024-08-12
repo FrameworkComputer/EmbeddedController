ae830496d4e6f7e612f058692b3e0b887ee3cc69
Jay Patel <jay2.patel@intel.com>
2024-08-12 13:23:36 -0700

    zephyr/test/rex: Add link to common unit tests for board_power.c
    Instead of having separate unit tests file for board_power.c source
    file for each board, use common unit tests file in
    zephyr/test/board_power/ based on AP.
    
    BUG=b:336751447
    TEST=./twister -T zephyr/test/rex --coverage. Coverage 100% for
         board_power.c
    
    Change-Id: If9eb75e1d2917a21b3ce00567e1b1144b94c4fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5784080
    Reviewed-by: Vijay P Hiremath <vijay.p.hiremath@intel.com>
    Tested-by: Jay Patel <jay2.patel@intel.com>
    Commit-Queue: Jay Patel <jay2.patel@intel.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/test/rex/tests/program/src/fakes.h
@@ -0,0 +1,11 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_TEST_REX_SRC_FAKES_H
+#define ZEPHYR_TEST_REX_SRC_FAKES_H
+
+/* Board specific fake functions to be declared here */
+
+#endif /* ZEPHYR_TEST_INTELRVP_SRC_FAKES_H */
