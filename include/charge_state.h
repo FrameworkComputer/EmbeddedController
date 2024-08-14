6e716b9c0922e0dec9b9112f71bbb2b4f656981e
Josh-Tsai <josh_tsai@compal.com>
2024-08-14 10:04:28 +0800

    fwk: implement the battery extender feature
    The purpose of this function is to automatically drop the battery max
    charge voltage if the system is left plugged in to AC for a long time
    to preserve battery life.
    
    When the timer is exceeded the battery charge voltage should be reduced
    to 4.35V/cell. Two additional days after the timer has exceeded, the
    battery charge voltage shall be reduced to 4.3V/cell.
    
    The timer should reset to 0 days any time the system is in S0/S0ix and
    not attached to AC for 30 minutes or longer.
    
    BRANCH=marigold
    BUG=https://app.clickup.com/t/86eq06zen
    TEST=when timer expired, the charge voltage will reduce to 97% and
    96% battery maximum charge voltage
    TEST=charge voltage will recover when the user unplugs the adapter
    for 30 minutes when the system in S0/S0ix.
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- include/charge_state.h
+++ include/charge_state.h
@@ -411,6 +411,19 @@ bool battery_sustainer_enabled(void);
 #include "charge_state.h"
 
 #ifdef CONFIG_CUSTOMIZED_DESIGN
+/**
+ * Callback for boards to return voltage value.
+ *
+ * @param voltage	battery maximum voltage
+ * @return battery extender stage voltage (stage1: 97% * voltage; stage2: 96% * voltage).
+ */
+int battery_extender_stage_voltage(uint16_t voltage);
+
+/**
+ * Callback for boards to count the battery extender timer.
+ */
+void battery_extender(void);
+
 void battery_customize(struct charge_state_data *curr_batt);
 #endif
 
