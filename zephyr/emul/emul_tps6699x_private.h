3cb2859b6fde33a0c79f8ebcdf2d6d50d572ff4e
Al Semjonovs <asemjonovs@google.com>
2024-08-14 11:20:30 -0600

    emul_tps6699x: Add helper function to convert TI task names
    Create helper function to convert TI task names to uint32
    
    BUG=b:339686045
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: I3719c3d1924d0e9339410dd46af450a405b7f12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5788448
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Sam Hurst <shurst@google.com>
    Tested-by: Al Semjonovs <asemjonovs@google.com>

--- zephyr/emul/emul_tps6699x_private.h
+++ zephyr/emul/emul_tps6699x_private.h
@@ -20,80 +20,86 @@ enum tps6699x_reg_offset {
 	TPS6699X_NUM_REG = 0xa4,
 };
 
-/* This recapitulates enum command_task from tps6699x_reg.h, but in so doing, it
- * avoids sharing implementation between the driver and the emulator used to
- * test it.
- */
+/* Helper function to convert TI task names to UINT32*/
+#define TASK_TO_UINT32(a, b, c, d) \
+	((uint32_t)(a | (b << 8) | (c << 16) | (d << 24)))
+
 enum tps6699x_command_task {
 	/* Command complete: Not a real command. The TPS6699x clears the command
 	 * register when a command completes.
 	 */
 	COMMAND_TASK_COMPLETE = 0,
 	/* Invalid command */
-	COMMAND_TASK_NO_COMMAND = 0x444d4321,
+	COMMAND_TASK_NO_COMMAND = TASK_TO_UINT32('!', 'C', 'M', 'D'),
 	/* Cold reset request */
-	COMMAND_TASK_GAID = 0x44494147,
+	COMMAND_TASK_GAID = TASK_TO_UINT32('G', 'A', 'I', 'D'),
 	/* Simulate port disconnect */
-	COMMAND_TASK_DISC = 0x43534944,
+	COMMAND_TASK_DISC = TASK_TO_UINT32('D', 'I', 'S', 'C'),
 	/* PD PR_Swap to Sink */
-	COMMAND_TASK_SWSK = 0x6b535753,
+	COMMAND_TASK_SWSK = TASK_TO_UINT32('S', 'W', 'S', 'k'),
 	/* PD PR_Swap to Source */
-	COMMAND_TASK_SWSR = 0x72535753,
+	COMMAND_TASK_SWSR = TASK_TO_UINT32('S', 'W', 'S', 'r'),
 	/* PD DR_Swap to DFP */
-	COMMAND_TASK_SWDF,
+	COMMAND_TASK_SWDF = TASK_TO_UINT32('S', 'W', 'D', 'F'),
 	/* PD DR_Swap to UFP */
-	COMMAND_TASK_SWUF,
+	COMMAND_TASK_SWUF = TASK_TO_UINT32('S', 'W', 'U', 'F'),
 	/* PD Get Sink Capabilities */
-	COMMAND_TASK_GSKC,
+	COMMAND_TASK_GSKC = TASK_TO_UINT32('G', 'S', 'k', 'C'),
 	/* PD Get Source Capabilities */
-	COMMAND_TASK_GSRC,
+	COMMAND_TASK_GSRC = TASK_TO_UINT32('G', 'S', 'r', 'C'),
 	/* PD Get Port Partner Information */
-	COMMAND_TASK_GPPI,
+	COMMAND_TASK_GPPI = TASK_TO_UINT32('G', 'P', 'P', 'I'),
 	/* PD Send Source Capabilities */
-	COMMAND_TASK_SSRC,
+	COMMAND_TASK_SSRC = TASK_TO_UINT32('S', 'S', 'r', 'C'),
 	/* PD Data Reset */
-	COMMAND_TASK_DRST,
+	COMMAND_TASK_DRST = TASK_TO_UINT32('D', 'R', 'S', 'T'),
 	/* Message Buffer Read */
-	COMMAND_TASK_MBRD,
+	COMMAND_TASK_MBRD = TASK_TO_UINT32('M', 'B', 'R', 'd'),
 	/* Send Alert Message */
-	COMMAND_TASK_ALRT,
+	COMMAND_TASK_ALRT = TASK_TO_UINT32('A', 'L', 'R', 'T'),
+	/* Send EPR Mode Message */
+	COMMAND_TASK_EPRM = TASK_TO_UINT32('E', 'P', 'R', 'm'),
 	/* PD Send Enter Mode */
-	COMMAND_TASK_AMEN,
+	COMMAND_TASK_AMEN = TASK_TO_UINT32('A', 'M', 'E', 'n'),
 	/* PD Send Exit Mode */
-	COMMAND_TASK_AMEX,
+	COMMAND_TASK_AMEX = TASK_TO_UINT32('A', 'M', 'E', 'x'),
 	/* PD Start Alternate Mode Discovery */
-	COMMAND_TASK_AMDS,
+	COMMAND_TASK_AMDS = TASK_TO_UINT32('A', 'M', 'D', 's'),
 	/* Get Custom Discovered Modes */
-	COMMAND_TASK_GCDM,
+	COMMAND_TASK_GCDM = TASK_TO_UINT32('G', 'C', 'd', 'm'),
 	/* PD Send VDM */
-	COMMAND_TASK_VDMS,
+	COMMAND_TASK_VDMS = TASK_TO_UINT32('V', 'D', 'M', 's'),
 	/* System ready to enter sink power */
-	COMMAND_TASK_SRDY = 0x59445253,
+	COMMAND_TASK_SRDY = TASK_TO_UINT32('S', 'R', 'D', 'Y'),
 	/* SRDY reset */
-	COMMAND_TASK_SRYR = 0x52595253,
+	COMMAND_TASK_SRYR = TASK_TO_UINT32('S', 'R', 'Y', 'R'),
+	/* Power Register Read */
+	COMMAND_TASK_PPRD = TASK_TO_UINT32('P', 'P', 'R', 'd'),
+	/* Power Register Write */
+	COMMAND_TASK_PPWR = TASK_TO_UINT32('P', 'P', 'W', 'r'),
 	/* Firmware update tasks */
-	COMMAND_TASK_TFUS = 0x73554654,
-	COMMAND_TASK_TFUC = 0x63554654,
-	COMMAND_TASK_TFUD = 0x64554654,
-	COMMAND_TASK_TFUE = 0x65554654,
-	COMMAND_TASK_TFUI = 0x69554654,
-	COMMAND_TASK_TFUQ = 0x71554654,
+	COMMAND_TASK_TFUS = TASK_TO_UINT32('T', 'F', 'U', 's'),
+	COMMAND_TASK_TFUC = TASK_TO_UINT32('T', 'F', 'U', 'c'),
+	COMMAND_TASK_TFUD = TASK_TO_UINT32('T', 'F', 'U', 'd'),
+	COMMAND_TASK_TFUE = TASK_TO_UINT32('T', 'F', 'U', 'e'),
+	COMMAND_TASK_TFUI = TASK_TO_UINT32('T', 'F', 'U', 'i'),
+	COMMAND_TASK_TFUQ = TASK_TO_UINT32('T', 'F', 'U', 'q'),
 	/* Abort current task */
-	COMMAND_TASK_ABRT,
+	COMMAND_TASK_ABRT = TASK_TO_UINT32('A', 'B', 'R', 'T'),
 	/*Auto Negotiate Sink Update */
-	COMMAND_TASK_ANEG,
+	COMMAND_TASK_ANEG = TASK_TO_UINT32('A', 'N', 'e', 'g'),
 	/* Clear Dead Battery Flag */
-	COMMAND_TASK_DBFG,
+	COMMAND_TASK_DBFG = TASK_TO_UINT32('D', 'B', 'f', 'g'),
 	/* Error handling for I2C3m transactions */
-	COMMAND_TASK_MUXR,
+	COMMAND_TASK_MUXR = TASK_TO_UINT32('M', 'u', 'x', 'R'),
 	/* Trigger an Input GPIO Event */
-	COMMAND_TASK_TRIG,
+	COMMAND_TASK_TRIG = TASK_TO_UINT32('T', 'r', 'i', 'g'),
 	/* I2C read transaction */
-	COMMAND_TASK_I2CR,
+	COMMAND_TASK_I2CR = TASK_TO_UINT32('I', '2', 'C', 'r'),
 	/* I2C write transaction */
-	COMMAND_TASK_I2CW,
+	COMMAND_TASK_I2CW = TASK_TO_UINT32('I', '2', 'C', 'w'),
 	/* UCSI tasks */
-	COMMAND_TASK_UCSI = 0x49534355,
+	COMMAND_TASK_UCSI = TASK_TO_UINT32('U', 'C', 'S', 'I')
 };
 
 /* Results of a task, indicated by the PDC in byte 1 of the relevant DATAX
