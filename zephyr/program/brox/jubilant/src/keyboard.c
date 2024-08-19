5ffd1f4fbd0d41d8905b4e43afd36ae446c85da2
Fabio Baltieri <fabiobaltieri@google.com>
2024-08-19 16:19:11 +0100

    brox,rex: setup a keyboard factory test node
    Setup Brox and Rex for using the keyboard factory test functionality.
    
    BUG=b:360631986
    TEST=kbfactorytest on the ec console on a board with a short
    
    Cq-Depend: chromium:5814367
    Change-Id: I5e08340993296ecd325744c987e7f490987d39dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5803787
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

--- zephyr/program/brox/jubilant/src/keyboard.c
+++ zephyr/program/brox/jubilant/src/keyboard.c
@@ -49,22 +49,3 @@ test_export_static void kb_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
-
-/*
- * We have total 32 pins for keyboard connecter {-1, -1} mean
- * the N/A pin that don't consider it and reserve index 0 area
- * that we don't have pin 0.
- */
-const int keyboard_factory_scan_pins[][2] = {
-	{ -1, -1 },	  { -1, -1 },	    { -1, -1 },	      { GPIO_KSI, 0 },
-	{ GPIO_KSI, 1 },  { GPIO_KSI, 2 },  { GPIO_KSI, 3 },  { GPIO_KSI, 4 },
-	{ GPIO_KSI, 5 },  { GPIO_KSI, 6 },  { GPIO_KSI, 7 },  { GPIO_KSOH, 7 },
-	{ GPIO_KSOH, 6 }, { GPIO_KSOH, 5 }, { GPIO_KSOH, 4 }, { GPIO_KSOH, 3 },
-	{ GPIO_KSOH, 2 }, { GPIO_KSOH, 1 }, { GPIO_KSOH, 0 }, { GPIO_KSOL, 7 },
-	{ GPIO_KSOL, 6 }, { GPIO_KSOL, 5 }, { GPIO_KSOL, 4 }, { GPIO_KSOL, 3 },
-	{ GPIO_KSOL, 2 }, { GPIO_KSOL, 1 }, { GPIO_KSOL, 0 }, { GPIO_C, 5 },
-	{ GPIO_C, 3 },	  { -1, -1 },	    { -1, -1 },	      { -1, -1 },
-	{ -1, -1 },
-};
-const int keyboard_factory_scan_pins_used =
-	ARRAY_SIZE(keyboard_factory_scan_pins);
