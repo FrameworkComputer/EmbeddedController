9e882180439b4ebb697b320704809df4851d61e8
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2024-08-20 11:04:32 +0800

    deku: Increase delay before RT-RST de-assertion in HBR power en
    Deku need 8ms delay before RT-RST de-assertion in HB retimer power
    enable. See the bug for more details.
    
    BUG=b:346883913
    TEST=make sure the power on sequence meets HBR spec.
    
    Change-Id: I988cba77735e7058c09ec37d603f2a1565973791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5796024
    Code-Coverage: Newbie Chromium <newbiechromium@gmail.com>
    Commit-Queue: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Code-Coverage: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>

--- zephyr/program/ovis/deku/src/usbc_config.c
+++ zephyr/program/ovis/deku/src/usbc_config.c
@@ -3,9 +3,41 @@
  * found in the LICENSE file.
  */
 
+#include "driver/retimer/bb_retimer_public.h"
+#include "gpio.h"
+#include "timer.h"
 #include "usbc_config.h"
 
 void board_reset_pd_mcu(void)
 {
 	/* Required for build */
+}
+
+__override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
+{
+	const struct bb_usb_control *control = &bb_controls[me->usb_port];
+	/* handle retimer's power domain */
+	if (enable) {
+		gpio_set_level(control->usb_ls_en_gpio, 1);
+		/*
+		 * Tpw, minimum time from VCC to RESET_N de-assertion is 100us.
+		 * For boards that don't provide a load switch control, the
+		 * retimer_init() function ensures power is up before calling
+		 * this function.
+		 * For deku, we need 8ms delay before RT_RST de-assertion, see
+		 * b:346883913.
+		 */
+		crec_msleep(8);
+		gpio_set_level(control->retimer_rst_gpio, 1);
+		/*
+		 * Allow 1ms time for the retimer to power up lc_domain
+		 * which powers I2C controller within retimer
+		 */
+		crec_msleep(1);
+	} else {
+		gpio_set_level(control->retimer_rst_gpio, 0);
+		crec_msleep(1);
+		gpio_set_level(control->usb_ls_en_gpio, 0);
+	}
+	return EC_SUCCESS;
 }
