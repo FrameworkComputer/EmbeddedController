39ad8dda50fc6c661519a8a1dbc3e084843c6c1b
Craig Hesling <hesling@chromium.org>
2024-08-21 19:11:48 -0700

    test: Make fetching sensor hwid more generic for fpsensor_hw test
    We can't easily use if(IS_ENABLED()) statement blocks with all
    code logic, since there is still use of undefined symbols. We could
    change this if we allowed including of all driver functions, since
    the function names should be scoped as elan_ or fpc_.
    
    Do note that the CONFIG_FP_SENSOR_FPC1025 configs are only enabled
    in the RW section, so the assert on the RW is simply for clarity.
    
    BUG=b:347111619
    TEST=./test/run_device_tests.py --board=helipilot --with_private=no \
         --flasher=servo_micro --tests fpsensor_hw
    
    Change-Id: I07618832e09b8a9e59076f49524cb9c2323894f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5809268
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Firas Sammoura <fsammoura@google.com>
    Tested-by: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -6,34 +6,35 @@
 #include "common.h"
 #include "config.h"
 #include "fpc_private.h"
+#include "fpsensor_driver.h"
 #include "test_util.h"
-
-#ifdef SECTION_IS_RW
-#include "fpc/fpc_sensor.h"
-static const uint32_t fp_sensor_hwid = FP_SENSOR_HWID_FPC;
-#else
-static const uint32_t fp_sensor_hwid = UINT32_MAX;
-#endif
 
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
  */
 test_static int test_fp_check_hwid(void)
 {
-	uint16_t id = 0;
+	/* All fingerprint sensor support exists exclusively in RW. */
+	TEST_ASSERT(IS_ENABLED(SECTION_IS_RW));
 
-	if (IS_ENABLED(SECTION_IS_RW)) {
-		TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
-		/* The lower 4-bits of the sensor hardware id are a
-		 * manufacturing ID that is ok to vary.
-		 */
-		TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
-	};
+#if defined(CONFIG_FP_SENSOR_FPC1025) || defined(CONFIG_FP_SENSOR_FPC1145)
+	uint16_t id = 0;
+	TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
+	/* The lower 4-bits of the sensor hardware id are a
+	 * manufacturing ID that is ok to vary.
+	 */
+	TEST_EQ(FP_SENSOR_HWID, id >> 4, "0x%04x");
 	return EC_SUCCESS;
+#endif
+
+	return EC_ERROR_UNKNOWN;
 }
 
 void run_test(int argc, const char **argv)
 {
+	test_reset();
+
 	RUN_TEST(test_fp_check_hwid);
+
 	test_print_result();
 }
