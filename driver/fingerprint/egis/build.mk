7a6e606e88bdb2587b016afd7d514d01e48df045
Firas Sammoura <fsammoura@google.com>
2024-09-04 00:01:58 +0000

    driver/fingerprint: Create Egis folder and blank build file
    BUG=b:356679724
    TEST=CQ
    
    Change-Id: I815f041b9ffa57b823203da1f0acf92b03129de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5834782
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ driver/fingerprint/egis/build.mk
@@ -0,0 +1,5 @@
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Build for EGIS fingerprint drivers
