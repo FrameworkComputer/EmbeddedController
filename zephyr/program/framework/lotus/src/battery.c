c0b661b05d27cbd4a23cedeea0d7cac1580aa221
LeoCX_Tsai <LeoCX_Tsai@compal.com>
2024-09-04 11:09:24 +0800

    fwk: set charge limit control and percentage control to common
    all project use same code common it for future project
    
    TEST=zmake build marigold pass
    BRANCH=marigold
    
    Signed-off-by: LeoCX_Tsai <LeoCX_Tsai@compal.com>

--- zephyr/program/framework/lotus/src/battery.c
+++ zephyr/program/framework/lotus/src/battery.c
@@ -29,7 +29,6 @@
 #define CACHE_INVALIDATION_TIME_US (3 * SECOND)
 
 static int battery_current_array[4];
-static uint8_t charging_maximum_level = EC_CHARGE_LIMIT_RESTORE;
 static int old_btp;
 static int power_on_check_batt;
 
@@ -106,50 +105,6 @@ uint32_t get_system_percentage(void)
 	} else
 		return pre_os_percentage;
 }
-
-static void battery_percentage_control(void)
-{
-	enum ec_charge_control_mode new_mode;
-	static int in_percentage_control;
-	uint32_t batt_os_percentage = get_system_percentage();
-	int rv;
-
-	/**
-	 * If the host command EC_CMD_CHARGE_CONTROL set control mode to CHARGE_CONTROL_DISCHARGE
-	 * or CHARGE_CONTROL_IDLE, ignore the battery_percentage_control();
-	 */
-	if (!in_percentage_control && get_chg_ctrl_mode() != CHARGE_CONTROL_NORMAL)
-		return;
-
-	if (charging_maximum_level == EC_CHARGE_LIMIT_RESTORE)
-		system_get_bbram(SYSTEM_BBRAM_IDX_CHARGE_LIMIT_MAX, &charging_maximum_level);
-
-	if (charging_maximum_level & CHG_LIMIT_OVERRIDE) {
-		new_mode = CHARGE_CONTROL_NORMAL;
-		if (batt_os_percentage == 1000)
-			charging_maximum_level = charging_maximum_level | 0x64;
-	} else if (charging_maximum_level < 20)
-		new_mode = CHARGE_CONTROL_NORMAL;
-	else if (batt_os_percentage > charging_maximum_level * 10) {
-		new_mode = CHARGE_CONTROL_DISCHARGE;
-		in_percentage_control = 1;
-	} else if (batt_os_percentage == charging_maximum_level * 10) {
-		new_mode = CHARGE_CONTROL_IDLE;
-		in_percentage_control = 1;
-	} else {
-		new_mode = CHARGE_CONTROL_NORMAL;
-		in_percentage_control = 0;
-	}
-
-	set_chg_ctrl_mode(new_mode);
-#ifdef CONFIG_PLATFORM_EC_CHARGER_DISCHARGE_ON_AC
-	rv = charger_discharge_on_ac(new_mode == CHARGE_CONTROL_DISCHARGE);
-#endif
-	if (rv != EC_SUCCESS)
-		CPRINTS("Failed to discharge.");
-}
-DECLARE_HOOK(HOOK_AC_CHANGE, battery_percentage_control, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE, battery_percentage_control, HOOK_PRIO_DEFAULT);
 
 int get_average_battery_current(void)
 {
@@ -396,53 +351,3 @@ __override int board_cut_off_battery(void)
 
 	return EC_RES_ERROR;
 }
-
-/*****************************************************************************/
-/* Host command */
-
-static enum ec_status cmd_charging_limit_control(struct host_cmd_handler_args *args)
-{
-
-	const struct ec_params_ec_chg_limit_control *p = args->params;
-	struct ec_response_chg_limit_control *r = args->response;
-
-	if (p->modes & CHG_LIMIT_DISABLE) {
-		charging_maximum_level = 0;
-		system_set_bbram(SYSTEM_BBRAM_IDX_CHARGE_LIMIT_MAX, 0);
-	}
-
-	if (p->modes & CHG_LIMIT_SET_LIMIT) {
-		if (p->max_percentage < 20)
-			return EC_RES_ERROR;
-
-		charging_maximum_level = p->max_percentage;
-		system_set_bbram(SYSTEM_BBRAM_IDX_CHARGE_LIMIT_MAX, charging_maximum_level);
-	}
-
-	if (p->modes & CHG_LIMIT_OVERRIDE)
-		charging_maximum_level = charging_maximum_level | CHG_LIMIT_OVERRIDE;
-
-	if (p->modes & CHG_LIMIT_GET_LIMIT) {
-		system_get_bbram(SYSTEM_BBRAM_IDX_CHARGE_LIMIT_MAX, &charging_maximum_level);
-		r->max_percentage = charging_maximum_level;
-		args->response_size = sizeof(*r);
-	}
-
-	battery_percentage_control();
-
-	return EC_RES_SUCCESS;
-}
-DECLARE_HOST_COMMAND(EC_CMD_CHARGE_LIMIT_CONTROL, cmd_charging_limit_control,
-			EC_VER_MASK(0));
-
-static enum ec_status cmd_get_cutoff_status(struct host_cmd_handler_args *args)
-{
-	struct ec_response_get_cutoff_status *r = args->response;
-
-	r->status = battery_is_cut_off();
-	args->response_size = sizeof(*r);
-
-	return EC_RES_SUCCESS;
-}
-DECLARE_HOST_COMMAND(EC_CMD_GET_CUTOFF_STATUS, cmd_get_cutoff_status,
-			EC_VER_MASK(0));
