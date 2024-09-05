cac31a8f47b576d867788eaf0614845881cd43aa
Kieran Levin <ktl@frame.work>
2024-09-04 17:21:18 -0700

    fwk: move battery extender to use common
    rewrite our timer based extender functionality to use common battery
    extender functionality as upstream.
    
    BRANCH=marigold
    BUG=https://app.clickup.com/t/86eq06zen
    TEST=When the timer expired the battery SOC will be lowered to 90-95%
    for stage 1, and 85-87 for stage 2.
    The battery extender will be reset after going off AC for 30 minutes.
    toggling enable/disable will reactivate the timers
    
    -get the charge state and battery extender setting
    chgstate
    
    battextender
    
    - set the deadline timer values sometime in the future.
    battextender timeext 15 s
    battextender timeext2 20 s
    
    battextender timerst 20 s
    
    - set the reload values for trigger and reset
    battextender trigger 60 s
    battextender reset  30 s
    
    battextender enable
    battextender disable
    
    Signed-off-by: Kieran Levin <ktl@frame.work>
    (cherry picked from commit 8c8dc055abf7eaf38dd37bf07713c8c753b54ac4)

--- include/charge_state.h
+++ include/charge_state.h
@@ -389,20 +389,20 @@ int board_should_charger_bypass(void);
 int board_want_charger_change_mode(void);
 #endif
 
-#ifdef CONFIG_CUSTOMIZED_DESIGN
 /**
- * Callback for boards to return voltage value.
+ * Set the battery sustainer mode.
  *
- * @param voltage	battery maximum voltage
- * @return battery extender stage voltage (stage1: 97% * voltage; stage2: 96% * voltage).
+ * @param lower: lower battery soc charge limit [1-100%] or -1 to disable
+ * @param upper: upper battery soc charge limit [1-100%] or -1 to disable
+ * 
+ * @return EC_SUCCESS or EC_ERROR_INVAL
  */
-int battery_extender_stage_voltage(uint16_t voltage);
+int battery_sustainer_set(int8_t lower, int8_t upper);
 
-/**
- * Callback for boards to count the battery extender timer.
- */
-void battery_extender(void);
+/* Config Charger */
+#include "charge_state.h"
 
+#ifdef CONFIG_CUSTOMIZED_DESIGN
 void battery_customize(struct charge_state_data *curr_batt);
 #endif
 
