cae7d9f711ae2021bce7f52afc27e2ea0f6557fa
Keith Short <keithshort@chromium.org>
2024-09-10 12:12:30 -0600

    zephyr: Include correct linker flags for lld
    The bloonchipper build uses lld as the linker.  Pull in the correct
    Zephyr linker options for lld. This fixes an issue where bloonchipper
    pulled in libstdc++ instead of libc++.
    
    BUG=b:320527549
    TEST=zmake build bloonchipper
    
    Change-Id: I259700c8e84175fb2531f78c7aa2ae9a8aec5abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5851936
    Reviewed-by: Sam Hurst <shurst@google.com>
    Tested-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- zephyr/cmake/linker/lld/linker_flags.cmake
+++ zephyr/cmake/linker/lld/linker_flags.cmake
@@ -2,10 +2,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Include definitions for bfd as a base.
-include("${ZEPHYR_BASE}/cmake/linker/ld/linker_flags.cmake")
+# Zephyr cmake system finds this file via ${TOOLCHAIN_ROOT}, but we just send
+# this out to the copy in ${ZEPHYR_BASE}.
+include("${ZEPHYR_BASE}/cmake/linker/lld/linker_flags.cmake")
+
 # ld/linker_flags.cmake includes ${LINKER}/${COMPILER}/linker_flags.cmake but
-# that doesn't exist for ldd, so import the path that actually exists.
+# that doesn't exist for lld, so import the path that actually exists.
 include("${ZEPHYR_BASE}/cmake/linker/ld/${COMPILER}/linker_flags.cmake" OPTIONAL)
 
 # Add the -nopie option to the linker flags to match the compilation options
