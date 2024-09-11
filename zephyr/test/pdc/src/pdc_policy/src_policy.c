5e6f33a5f1bf580884a1ba98a8b59ba335396180
Keith Short <keithshort@chromium.org>
2024-09-10 18:07:43 -0600

    test: Add 1.5A FRS test
    FRS devices that need 1.5A or less should always have FRS enabled.
    
    BUG=b:360122732
    TEST=./twister -ivc -T zephyr/test/pdc
    
    Change-Id: I194c1eaf54b6c852c670d20c9e2f4ca1dec6744f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5855859
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Keith Short <keithshort@chromium.org>

--- zephyr/test/pdc/src/pdc_policy/src_policy.c
+++ zephyr/test/pdc/src/pdc_policy/src_policy.c
@@ -364,6 +364,84 @@ ZTEST_USER_F(src_policy, test_src_policy_non_pd)
 	zassert_equal(typec_current, TC_CURRENT_1_5A);
 }
 
+/* Verify operation with an FRS partner that requires 1.5A. */
+ZTEST_USER_F(src_policy, test_src_policy_frs_1a5)
+{
+	union connector_status_t snk_partner_connector_status;
+	union connector_status_t frs_partner_connector_status;
+	uint32_t snk_partner_snk_pdo =
+		PDO_FIXED(5000, 3000, PDO_FIXED_DUAL_ROLE);
+	uint32_t frs_partner_src_pdo =
+		PDO_FIXED(5000, 3000, PDO_FIXED_DUAL_ROLE);
+	uint32_t frs_partner_snk_pdo = PDO_FIXED(
+		5000, 3000, PDO_FIXED_DUAL_ROLE | PDO_FIXED_FRS_CURR_1A5_AT_5V);
+	uint32_t lpm_src_pdo_actual_port0;
+	uint32_t lpm_src_pdo_actual_port1;
+	union connector_capability_t frs_ccaps = {
+		.partner_pd_revision = 3,
+	};
+	bool frs_enabled;
+
+	if (!IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS)) {
+		ztest_test_skip();
+	}
+
+	/* Connect a PD sink at 3.0A */
+	emul_pdc_configure_src(fixture->emul_pdc[TEST_USBC_PORT0],
+			       &snk_partner_connector_status);
+	zassert_ok(emul_pdc_set_pdos(fixture->emul_pdc[TEST_USBC_PORT0],
+				     SINK_PDO, PDO_OFFSET_0, 1, PARTNER_PDO,
+				     &snk_partner_snk_pdo));
+	zassert_ok(emul_pdc_connect_partner(fixture->emul_pdc[TEST_USBC_PORT0],
+					    &snk_partner_connector_status));
+
+	/* Wait for connection to settle and source policies to run. */
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_USBC_PORT0));
+
+	zassert_ok(emul_pdc_get_pdos(fixture->emul_pdc[TEST_USBC_PORT0],
+				     SOURCE_PDO, PDO_OFFSET_0, 1, LPM_PDO,
+				     &lpm_src_pdo_actual_port0));
+	zassert_equal(PDO_FIXED_GET_VOLT(lpm_src_pdo_actual_port0), 5000,
+		      "LPM SOURCE_PDO voltage %d, but expected %d",
+		      PDO_FIXED_GET_VOLT(lpm_src_pdo_actual_port0), 5000);
+	zassert_equal(PDO_FIXED_GET_CURR(lpm_src_pdo_actual_port0), 3000,
+		      "LPM SOURCE_PDO current %d, but expected %d",
+		      PDO_FIXED_GET_CURR(lpm_src_pdo_actual_port0), 3000);
+
+	/* Connect an FRS source that needs 1.5A. */
+	zassert_ok(emul_pdc_set_connector_capability(
+		fixture->emul_pdc[TEST_USBC_PORT1], &frs_ccaps));
+	emul_pdc_configure_snk(fixture->emul_pdc[TEST_USBC_PORT1],
+			       &frs_partner_connector_status);
+	zassert_ok(emul_pdc_set_pdos(fixture->emul_pdc[TEST_USBC_PORT1],
+				     SOURCE_PDO, PDO_OFFSET_0, 1, PARTNER_PDO,
+				     &frs_partner_src_pdo));
+	zassert_ok(emul_pdc_set_pdos(fixture->emul_pdc[TEST_USBC_PORT1],
+				     SINK_PDO, PDO_OFFSET_0, 1, PARTNER_PDO,
+				     &frs_partner_snk_pdo));
+	zassert_ok(emul_pdc_connect_partner(fixture->emul_pdc[TEST_USBC_PORT1],
+					    &frs_partner_connector_status));
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_USBC_PORT1));
+
+	/* FRS should be enabled, even while providing 3A on another port. */
+	zassert_ok(emul_pdc_get_frs(fixture->emul_pdc[TEST_USBC_PORT1],
+				    &frs_enabled));
+	zassert_true(frs_enabled);
+
+	/* The source PDO should also be configured for 1.5A prior to
+	 * the swap.
+	 */
+	zassert_ok(emul_pdc_get_pdos(fixture->emul_pdc[TEST_USBC_PORT1],
+				     SOURCE_PDO, PDO_OFFSET_0, 1, LPM_PDO,
+				     &lpm_src_pdo_actual_port1));
+	zassert_equal(PDO_FIXED_GET_VOLT(lpm_src_pdo_actual_port1), 5000,
+		      "LPM SOURCE_PDO voltage %d, but expected %d",
+		      PDO_FIXED_GET_VOLT(lpm_src_pdo_actual_port1), 5000);
+	zassert_equal(PDO_FIXED_GET_CURR(lpm_src_pdo_actual_port1), 1500,
+		      "LPM SOURCE_PDO current %d, but expected %d",
+		      PDO_FIXED_GET_CURR(lpm_src_pdo_actual_port1), 1500);
+}
+
 /* Verify operation with an FRS partner that requires 3A. */
 ZTEST_USER_F(src_policy, test_src_policy_frs_3a)
 {
