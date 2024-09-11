6f613f95206ee96634fe8933fcacbc7b8425bec4
Josh-Tsai <josh_tsai@compal.com>
2024-09-11 17:12:00 +0800

    fwk: add the short hibernate timer for chassis open behavior
    The mainboard will wait 30s to enter the hibernate mode without adatper,
    and will wait 2s to enter the hibernate mode once chassis is opened.
    
    Change the timer to the Kconfig for diffenet projects to configure.
    
    BRANCH=sunflower
    BUG=Noen
    TEST=hibernate function is workable on sunflower mainboard
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- zephyr/program/framework/src/board_function.c
+++ zephyr/program/framework/src/board_function.c
@@ -42,6 +42,8 @@ static uint8_t chassis_press_counter;
 static uint8_t chassis_once_flag;
 
 static uint64_t chassis_open_hibernate_time;
+static uint64_t short_hibernate =
+	CONFIG_PLATFORM_EC_HIBERNATE_DELAY_SEC - CONFIG_SHORT_HIBERNATE_TIMER;
 
 static uint8_t init = 1;
 
@@ -170,11 +172,15 @@ __override enum critical_shutdown
 board_system_is_idle(uint64_t last_shutdown_time, uint64_t *target,
 		     uint64_t now)
 {
-	/* update the chassis open target time = 30s - 28s*/
-	chassis_open_hibernate_time = *target - 28000000;
+	/* update the chassis open target time */
+	chassis_open_hibernate_time = *target - (short_hibernate * SECOND);
 
-	/* After setting the chassis open hibernate timer, delay 2.5s to check the chassis status */
-	hook_call_deferred(&chassis_open_hibernate_data, 2500 * MSEC);
+	/**
+	 * After setting the chassis open hibernate timer,
+	 * delay (short timer + 500ms) to check the chassis status.
+	 */
+	hook_call_deferred(&chassis_open_hibernate_data,
+					(CONFIG_SHORT_HIBERNATE_TIMER * SECOND) + (500 * MSEC));
 
 	if (now < *target)
 		return CRITICAL_SHUTDOWN_IGNORE;
