f8ea14fa104b75e4f961d16ab5d61878e413060e
johnwc_yeh <JohnWC_Yeh@compal.com>
2024-09-25 17:08:18 +0800

    fwk: lilac: modify power down sequence
    Follow power-down sequencing requirements for common FP8 Platform.
    Setting gpiod4(PCH_PWR_EN) and gpiod7(0.75_1.8VALW_PWREN) power
    down sequence more delay 20ms.
    
    BRANCH=fwk-lilac-22648
    BUG=https://app.clickup.com/t/86eqd04vt
    TEST=EE team measured +0.75VALWS and lost power 20ms after +VDDIO_AUDIO
    
    Signed-off-by: johnwc_yeh <JohnWC_Yeh@compal.com>
    (cherry picked from commit 205c452335aae7920384b9f784d4a95774ed96b4)

--- zephyr/program/framework/azalea/src/power_sequence.c
+++ zephyr/program/framework/azalea/src/power_sequence.c
@@ -593,6 +593,7 @@ enum power_state power_handle_state(enum power_state state)
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pbtn_out), 0);
 		k_msleep(5);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_apu_aud_pwr_en), 0);
+		k_msleep(20);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pch_pwr_en), 0);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_0p75_1p8valw_pwren), 0);
 
