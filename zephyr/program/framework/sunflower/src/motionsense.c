90c847e3585819ae205d3506942d421a2b637e24
Josh-Tsai <josh_tsai@compal.com>
2024-09-27 14:47:28 +0800

    fwk: sunflower: calculate lid angle
    Modified the motionsense devicetree to enable ec to calculate the lid angle.
    Also add a host command to read the lid angle
    
    BRANCH=fwk-sunflower-26784
    BUG=None
    TEST=Use `accelinfo on` command to read lidangle. See a= value
    TEST=Use EC memmap from OS and read lidangle
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

---
+++ zephyr/program/framework/sunflower/src/motionsense.c
@@ -0,0 +1,30 @@
+/* Copyright 2024 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "console.h"
+#include "ec_commands.h"
+#include "keyboard_scan.h"
+#include "lid_angle.h"
+#include "motion_lid.h"
+#include "tablet_mode.h"
+
+#define CPRINTS(format, args...) cprints(CC_MOTION_SENSE, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_MOTION_SENSE, format, ##args)
+
+/* This callback disables keyboard when convertibles are fully open */
+__override void lid_angle_peripheral_enable(int enable)
+{
+	/*
+	 * If the lid is in tablet position via other sensors,
+	 * ignore the lid angle, which might be faulty then
+	 * disable keyboard.
+	 */
+	if (tablet_get_mode())
+		enable = 0;
+
+	/* EC needs to control the keyboard scan with non-ChromeOS systems */
+	keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
+}
