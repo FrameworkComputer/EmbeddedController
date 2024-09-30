e892b8d686d69cdae2753098f27658b5bd8b3b0d
Daniel Schaefer <dhs@frame.work>
2024-10-01 02:27:59 +0800

    fwk/sunflower: Expose table mode to SOC via GPIO
    BRANCH=fwk-sunflower-26784
    BUG=none
    TEST=ckeck gpio_ec_pad_mode_l status change when enter/exit tablet mode
    TEST=Use BIOS with PNP0C60 and observer that Intel HID filter driver gets loaded on Windows and tablet mode is toggled in windows when lid is folded
    
    Co-authored-by: LeilaCY-Lin <LeilaCY_Lin@compal.com>
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

---
+++ zephyr/program/framework/sunflower/src/board_tablet_mode.c
@@ -0,0 +1,26 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "hooks.h"
+#include "tablet_mode.h"
+
+static void board_tablet_mode_change(void)
+{
+	/*
+	 * gpio_ec_pad_mode is connected to the SoC and in ACPI there's a
+	 * device PNP0C60/INT33D3 with GPIO config to read it.
+	 * OS is notified of changes and state of this GPIO.
+	 *
+	 * Set this gpio to low if changing to notebook (clamshell) mode.
+	 * Set this gpio to high if changing to tablet mode.
+	 *
+	 * At EC boot it's in laptop mode by GPIO configuration.
+	 */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_pad_mode),
+			tablet_get_mode());
+}
+DECLARE_HOOK(HOOK_TABLET_MODE_CHANGE, board_tablet_mode_change, HOOK_PRIO_DEFAULT);
+/* Run after gmr_tablet_switch_init to initialize GPIO after debounce. */
+DECLARE_HOOK(HOOK_INIT, board_tablet_mode_change, HOOK_PRIO_DEFAULT + 1);
