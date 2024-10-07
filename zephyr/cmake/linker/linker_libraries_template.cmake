029bab32b01a8b031935cbd5072295504e1a155b
Jeremy Bettis <jbettis@google.com>
2024-10-07 14:59:30 -0600

    Add new cmake files
    Zephyr added some new cmake files, and since we override TOOLCHAIN_ROOT,
    make some files that will include the Zephyr ones.
    
    See crrev/c/5908564
    
    BUG=None
    TEST=CQ
    
    Change-Id: I9e07c6cb1ef55d4e403de7626ea00fc54881b3e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5914452
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>

---
+++ zephyr/cmake/linker/linker_libraries_template.cmake
@@ -0,0 +1,7 @@
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Zephyr cmake system looks into ${TOOLCHAIN_ROOT}, but we just send
+# this out to the copy in ${ZEPHYR_BASE}.
+include("${ZEPHYR_BASE}/cmake/linker/linker_libraries_template.cmake")
