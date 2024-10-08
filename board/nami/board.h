362b0b7e134bf080ca37c3897fe18d2dbcd27512
Gwendal Grignou <gwendal@chromium.org>
2024-10-07 20:38:25 -0700

    nami: Remove host command logging
    Will saturate the console, since every sensor read will trigger a HC.
    
    BUG=b:372076592
    TEST=compile
    
    Change-Id: I1c43654723d2f92e09f9baf8ad481385d0accd2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5915037
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Ashley Shen <shenashley@google.com>

--- board/nami/board.h
+++ board/nami/board.h
@@ -9,10 +9,12 @@
 #define __CROS_EC_BOARD_H
 
 /*
- * By default, enable all console messages except ACPI and host event because
+ * By default, enable all console messages excepted HC, ACPI and event:
  * the sensor stack is generating a lot of activity.
  */
 #define CC_DEFAULT (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#undef CONFIG_HOSTCMD_DEBUG_MODE
+#define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
 
 /* EC */
 #define CONFIG_ADC
