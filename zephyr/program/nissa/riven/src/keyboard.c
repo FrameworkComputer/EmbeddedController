f7cfab2c2f2f1ba8d88dd30acc44d1299e707a03
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-10-18 15:33:20 +0800

    Riven: Increase keyscan min_post_scan_delay_us to 2ms
    Increase min_post_scan_delay_us to 2ms to make task
    threads not too busy when pressing and holding KB key.
    
    BUG=b:374236308
    TEST=tast firmware.EcStress.all pass
    
    Change-Id: If9bb31da7bc48f1250d83ebbec1f66c8a00f65ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5937024
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Ashley Shen <shenashley@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>

--- zephyr/program/nissa/riven/src/keyboard.c
+++ zephyr/program/nissa/riven/src/keyboard.c
@@ -37,6 +37,12 @@ test_export_static void kb_init(void)
 		 */
 		set_scancode_set2(3, 14, get_scancode_set2(7, 17));
 	}
+
+	/*
+	 * Increase delay before the next key scan to prevent from WDT reset
+	 * under some specific condition.
+	 */
+	keyscan_config.min_post_scan_delay_us = 2 * MSEC;
 }
 DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
 
