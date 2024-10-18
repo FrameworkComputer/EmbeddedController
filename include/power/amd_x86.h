4a3c53f8f179b23fa090c05c5e349a87900310e2
Josh-Tsai <josh_tsai@compal.com>
2024-10-18 15:29:30 +0800

    fwk: don't use the common amd power signal if enable CONFIG_CROS_EC_POWER_SIGNAL_LIST
    We may use the non-common AMD power signal.
    In Zephyr, we can use the power signal device tree to define the customized signal,
    it will enable the CONFIG_CROS_EC_POWER_SIGNAL_LIST automatically.
    
    Add the condition to avoid EC build the AMD common power signal if enable
    CONFIG_CROS_EC_POWER_SIGNAL_LIST.
    
    BUG=None
    BRANCH=dogwood
    TEST=zmake build lotus pass
    TEST=can power on the system on lotus unit
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>

--- include/power/amd_x86.h
+++ include/power/amd_x86.h
@@ -10,7 +10,8 @@
  * In legacy EC-OS, the power signals are defined as part of
  * the board include headers, but with Zephyr, this is common.
  */
-#if defined(CONFIG_ZEPHYR) && defined(CONFIG_AP_X86_AMD)
+#if defined(CONFIG_ZEPHYR) && defined(CONFIG_AP_X86_AMD) && \
+	!defined(CONFIG_CROS_EC_POWER_SIGNAL_LIST)
 
 /* Power input signals */
 enum power_signal {
