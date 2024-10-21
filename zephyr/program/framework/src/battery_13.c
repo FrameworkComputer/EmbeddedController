e79252b49c4c40766a377c83d1a501bebf8f0404
Daniel Schaefer <dhs@frame.work>
2024-10-21 19:50:45 +0800

    framework: Extract battery presence detection
    Prepare for different battery detection in the future.
    
    BUG=Refactoring
    BRANCH=
    TEST=EC can boot from drained battery when AC is connected. And drained battery can be charged
    
    Signed-off-by: Josh-Tsai <josh_tsai@compal.com>
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- zephyr/program/framework/src/battery_13.c
+++ zephyr/program/framework/src/battery_13.c
@@ -30,31 +30,6 @@ enum battery_trip_point_state_t {
 	BATTERY_TRIP_POINT_WAIT_EVENT = BIT(3),
 };
 
-#ifndef CONFIG_PLATFORM_EC_BATTERY_PRESENT_GPIO
-enum battery_present battery_is_present(void)
-{
-	enum battery_present batt_pres;
-	int mv;
-
-	mv = adc_read_channel(ADC_VCIN1_BATT_TEMP);
-	batt_pres = (mv < 2200 ? BP_YES : BP_NO);
-
-	if (mv == ADC_READ_ERROR)
-		return BP_NO;
-
-	/*
-	 * If the battery is present now and was present last time we checked,
-	 * return early.
-	 */
-	if (batt_pres == BP_YES && batt_pres_prev == batt_pres)
-		return batt_pres;
-
-	batt_pres_prev = batt_pres;
-
-	return batt_pres;
-}
-#endif
-
 uint32_t get_system_percentage(void)
 {
 	static uint32_t pre_os_percentage;
@@ -68,15 +43,6 @@ uint32_t get_system_percentage(void)
 		return os_percentage;
 	} else
 		return pre_os_percentage;
-}
-
-enum battery_present board_batt_is_present(void)
-{
-	/*
-	 * Due to adc_read_channel() will clear the task event,
-	 * we should get the battery status without read adc channel again.
-	 */
-	return batt_pres_prev;
 }
 
 void battery_trip_point(struct charge_state_data *curr_batt)
