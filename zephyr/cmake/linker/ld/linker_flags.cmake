359824314e1d1fef046be5e2de8ef5cbddb5f810
Jeremy Bettis <jbettis@google.com>
2024-10-22 11:13:26 -0600

    Suppress RWX segment warning
    This is probably not the right fix, but it will let us build with GCC
    14.2.0.
    
    BUG=b:374997019
    TEST=zmake build -a
    
    Change-Id: I1478d1cb8dc3e802642358247df61e49db2549d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5954012
    Reviewed-by: Jonathon Murphy <jpmurphy@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Jonathon Murphy <jpmurphy@google.com>

--- zephyr/cmake/linker/ld/linker_flags.cmake
+++ zephyr/cmake/linker/ld/linker_flags.cmake
@@ -9,3 +9,6 @@ include("${ZEPHYR_BASE}/cmake/linker/ld/linker_flags.cmake")
 # There can also be compiler specific linker options, so try to include
 # our version of that also.
 include("${TOOLCHAIN_ROOT}/cmake/linker/${LINKER}/${COMPILER}/linker_flags.cmake" OPTIONAL)
+
+# TODO(b/374997019): Remove this and make the sections not RWX.
+add_link_options("${LINKERFLAGPREFIX},--no-warn-rwx-segments")
