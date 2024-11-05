ff181d34f94c6ea51f4a2c18ef62c77133a4a6c9
johnwc_yeh <JohnWC_Yeh@compal.com>
2024-11-05 10:31:26 +0800

    fwk: allow fan in S0ix
    Since we have some reports of the system getting hot along with
    many forum posts of the same issue with other vendors, keep the
    fan on for safety.
    
    BUG=https://app.clickup.com/t/86eqf3m36
    BRANCH=fwk-hx20-hx30
    TEST=keep the fan on in S0ix
    
    (cherry picked from commit 2a2313b6eab2961c1bed054a332e3fb7cc5734de)
    
    Signed-off-by: johnwc_yeh <JohnWC_Yeh@compal.com>
    (cherry picked from commit 948d41b0d406167b1ece39a5829581b66d3b4989)

--- common/fan.c
+++ common/fan.c
@@ -13,6 +13,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
+#include "power.h"
 #include "printf.h"
 #include "system.h"
 #include "util.h"
@@ -645,6 +646,17 @@ static void pwm_fan_stop(void)
 	 *
 	 * Thermal control may be already disabled if DPTF is used.
 	 */
+
+	/*
+	 * Allow the fan to run in S0ix as we have unknown user reports
+	 * of systems getting warm in this state. Possibly due to windows
+	 * continuing to run CPU heavy tasks for value added features
+	 * causing excessive heat.
+	 */
+	if (power_get_state() == POWER_S0ix ||
+		power_get_state() == POWER_S0S0ix)
+		return;
+
 	pwm_fan_control(0); /* crosbug.com/p/8097 */
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, pwm_fan_stop, HOOK_PRIO_DEFAULT);
