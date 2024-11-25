60afc1b8628320c812b65225f0acdef47ecf8223
LeoCX_Tsai <LeoCX_Tsai@compal.com>
2024-11-25 16:23:54 +0800

    fwk: lotus: clear rtc flag when force shutdown
    When we force shutdown, the unit should be forced to G3.
    so clearing RTC flags will ensure we do not get stuck at keep_pch_power.
    
    When system hangs before into s4, RTC wake flags will be set up, when
    this moment forces shutdown Unit will stuck at S4, and EC will push the
    solution for force shutdown can't power on.
    
    BRANCH=fwk-main
    BUG=[86equnzvk](https://app.clickup.com/t/86equnzvk)
    TEST=test on lotus, setup RTC flags by EC cmd (memmap set 0x102 1),
    and do force shutdown(8s), press power button make sure unit can power up
    
    Signed-off-by: LeoCX_Tsai <LeoCX_Tsai@compal.com>
    (cherry picked from commit 273d62f6c6c6c313a108e14667b7f52c238be771)

--- zephyr/program/framework/lotus/src/power_sequence.c
+++ zephyr/program/framework/lotus/src/power_sequence.c
@@ -313,6 +313,7 @@ void chipset_force_shutdown(enum chipset_shutdown_reason reason)
 		report_ap_reset(reason);
 		force_shoutdown_flags = 1;
 		task_wake(TASK_ID_CHIPSET);
+		clear_rtcwake();
 	}
 }
 
