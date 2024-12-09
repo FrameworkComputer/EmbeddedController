e66f3006765b85de9dd65d786d46673829598954
Josh Tsai <Josh_Tsai@compal.com>
2024-12-09 09:45:36 +0800

    fwk: ccg8: implement host command for retimer update mode control
    PD firmware add the vendor specific command 0x0043 to control the
    retimer update mode.
    
    EC implements a host command for host to control the retimer update
    mode during firmware update.
    
    BRANCH=fwk-lilac-27116
    BUG=None
    TEST=use i2ctrace and hcdebug to check the process is correct
    [188.065100 HC 0x3e0a.0:0101]
    i2c: 2:0x40 wr 0x43 0x00 0x01
    i2c: 2:0x40 wr 0x06 0x00   rd 0x01
    i2c: 2:0x40 wr 0x7E 0x00   rd 0x02 0x00
    i2c: 2:0x40 wr 0x06 0x00 0x01
    
    Signed-off-by: Josh Tsai <Josh_Tsai@compal.com>
    (cherry picked from commit 7ac1ca50bfff7bcabb60d7510183377ce71c8093)

--- zephyr/program/framework/src/cypd_ccg8.c
+++ zephyr/program/framework/src/cypd_ccg8.c
@@ -4,6 +4,7 @@
 
 #include "battery.h"
 #include "board_function.h"
+#include "board_host_command.h"
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "console.h"
@@ -13,6 +14,7 @@
 #include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "host_command.h"
 #include "i2c.h"
 #include "power.h"
 #include "task.h"
@@ -430,3 +432,56 @@ void cypd_update_epr_state(int controller, int port, int response_len)
 	pd_epr_in_progress &= ~BIT((controller << 1) + port);
 }
 #endif
+
+static int cypd_enable_retimer_update_mode(int controller, uint8_t data)
+{
+	int rv;
+
+	/* Wrong controller index */
+	if (controller >= PD_CHIP_COUNT)
+		return EC_ERROR_PARAM1;
+
+	/**
+	 * On the AMD platform, EC needs to send the HIP vendor specific command
+	 * 0x0043 data 0x01 to enable retimer update mode.
+	 * 0x0043 data 0x00 to disable retimer update mode.
+	 */
+	rv = cypd_write_reg8(controller, CCG_RETIMER_UPDATE_MODE_CONTROL, data);
+
+	if (rv != EC_SUCCESS)
+		CPRINTS("CCG8 Retimer update mode control failed:%d", rv);
+
+	return rv;
+}
+
+static enum ec_status bb_retimer_control(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_bb_retimer_control_mode *p = args->params;
+	int rv;
+
+	switch (p->modes) {
+	case BB_ENTRY_FW_UPDATE_MODE:
+		rv = cypd_enable_retimer_update_mode(p->controller, 0x01);
+		if (rv != EC_SUCCESS) {
+			if (rv == EC_ERROR_PARAM1)
+				return EC_RES_INVALID_PARAM;
+			else
+				return EC_RES_BUS_ERROR;
+		}
+		break;
+	case BB_EXIT_FW_UPDATE_MODE:
+		rv = cypd_enable_retimer_update_mode(p->controller, 0x00);
+		if (rv != EC_SUCCESS) {
+			if (rv == EC_ERROR_PARAM1)
+				return EC_RES_INVALID_PARAM;
+			else
+				return EC_RES_BUS_ERROR;
+		}
+		break;
+	default:
+		return EC_RES_INVALID_PARAM;
+	}
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_BB_RETIMER_CONTROL, bb_retimer_control, EC_VER_MASK(0));
