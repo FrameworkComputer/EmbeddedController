da2f4e7ce7ca2841ce59b73a9069004241f787ad
Daniel Schaefer <dhs@frame.work>
2024-12-10 17:33:39 +0800

    util/ectool: Add simpleversion command
    BRANCH=fwk-main
    BUG=Adding support for Framework specific host commands in ectool
    TEST=`ectool simpleversion` prints an abbreviated EC version
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- util/ectool.cc
+++ util/ectool.cc
@@ -6410,6 +6410,24 @@ int cmd_chassis_open_check(int argc, char *argv[])
 	return rv;
 }
 
+int cmd_simpleversion(int argc, char *argv[])
+{
+	struct ec_response_get_custom_version r;
+
+	int rv = ec_command(EC_CMD_GET_SIMPLE_VERSION, 0, NULL, 0, &r,
+			sizeof(r));
+
+	if (rv > 0) {
+		/* Make sure that we print from a NULL-terminated string */
+		char ver[sizeof(r.simple_version) + 1] = { '\0' };
+		strncpy(ver, r.simple_version, sizeof(r.simple_version));
+
+		printf("%s\n", ver);
+	}
+
+	return rv;
+}
+
 static void print_pd_power_info(struct ec_response_usb_pd_power_info *r)
 {
 	switch (r->role) {
@@ -12678,6 +12696,7 @@ const struct command commands[] = {
 	  "[enable | disable]\n"
 	  "\tEnable or disable PS2 mouse emulation." },
 	{ "chassisopen", cmd_chassis_open_check, "\n\tPrints whether or not the chassis is currently open." },
+	{ "simpleversion", cmd_simpleversion, "\n\tPrints a simple version string of the EC firmware." },
 	{ NULL, NULL }
 };
 
