8faddeff787fcf754c3a514a35626180bd087170
LeilaCY-Lin <LeilaCY_Lin@compal.com>
2024-12-17 14:46:56 +0800

    fwk: sunflower: reduce typec port to 1.5A for safety test
    During the safety test in DC mode, the system's wattage exceeds
    the battery discharge wattage, resulting in an inability to limit
    current. Reduce the type-c port from 3A down to 1.5A when the
    battery current limit is exceeded, the test result could pass the
    safety test criteria.
    
    BRANCH=sunflower
    BUG=https://app.clickup.com/t/86eqyrp4x
    TEST=Check typec port is 1.5A when battery current limit is
    exceeded setting.
    
    Signed-off-by: LeilaCY-Lin <LeilaCY_Lin@compal.com>

--- zephyr/program/framework/sunflower/src/cpu_power.c
+++ zephyr/program/framework/sunflower/src/cpu_power.c
@@ -4,6 +4,7 @@
  */
 
 #include "battery_fuel_gauge.h"
+#include "board_battery.h"
 #include "charge_state.h"
 #include "charger.h"
 #include "charge_manager.h"
@@ -11,6 +12,7 @@
 #include "common_cpu_power.h"
 #include "customized_shared_memory.h"
 #include "console.h"
+#include "cypress_pd_common.h"
 #include "extpower.h"
 #include "hooks.h"
 #include "math_util.h"
@@ -21,6 +23,57 @@
 
 #define ROP 15   /*Intel Rest Of Platform(ROP)*/
 #define batt_rating 50
+
+/*
+ * During the safety test in DC mode, the system's wattage exceeds the battery
+ * discharge wattage, resulting in an inability to limit current. Reduce the
+ * type-c port from 3A down to 1.5A when the battery current limit is exceeded,
+ * the test result could pass the safety test criteria.
+ */
+static void reduce_typec_1_5A(void)
+{
+	int average_current = get_average_battery_current();
+	static int battery_current_limit_mA = -4265;
+	static timestamp_t wait_stable_time;
+	static timestamp_t update_safety_timer;
+
+	static int pd_3a_controller;
+	static int pd_3a_port;
+	static int set_typec_1_5a_flag;
+	int rv;
+
+	timestamp_t now = get_time();
+
+	if (!timestamp_expired(wait_stable_time, &now) ||
+		!timestamp_expired(update_safety_timer, &now))
+		return;
+
+	/* discharge, value compare based on negative */
+	if (average_current < battery_current_limit_mA) {
+		if (set_typec_1_5a_flag == 0) {
+			for (int controller = 0; controller < PD_CHIP_COUNT; controller++) {
+				for (int port_idx = 0; port_idx < 2; port_idx++) {
+					if (cypd_port_3a_status(controller, port_idx)) {
+						pd_3a_controller = controller;
+						pd_3a_port = port_idx;
+						rv = cypd_modify_safety_power(pd_3a_controller,
+							pd_3a_port, CCG_PD_CMD_SET_TYPEC_1_5A);
+						set_typec_1_5a_flag = 1;
+					}
+				}
+			}
+		}
+	} else if (average_current > (battery_current_limit_mA * 80 / 100)) {
+		if (set_typec_1_5a_flag) {
+			rv = cypd_modify_safety_power(pd_3a_controller,
+					pd_3a_port, CCG_PD_CMD_SET_TYPEC_3A);
+			set_typec_1_5a_flag = 0;
+		}
+	}
+
+	/* only check safety function per 2 second */
+	update_safety_timer.val = get_time().val + (2 * SECOND);
+}
 
 void update_soc_power_limit(bool force_update, bool force_no_adapter)
 {
@@ -46,6 +99,7 @@ void update_soc_power_limit(bool force_update, bool force_no_adapter)
 		pl2_watt = batt_rating - ROP;
 		pl4_watt = (batt_rating * 13) / 10;
 		psyspl2_watt = (batt_rating * 95) / 100;
+		reduce_typec_1_5A();
 	} else if (!battery_is_present() && active_power >= 60) {
 		/*Standalone mode AC only and AC >= 60W*/
 		pl1_watt = 15;
