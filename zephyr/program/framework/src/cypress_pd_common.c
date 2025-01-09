b7be072ad2d6f8c39ef0160baaccaf8857190926
Josh Tsai <Josh_Tsai@compal.com>
2025-01-09 14:19:40 +0800

    fwk: add two adapters vid/pid to turn off the retimer power
    Add the chicony adapter vid (0x32AC) and pid (0x0023) and phihong
    adapter vid (0x32AC) and pid (0x000B) in the EC ode to turn off
    the retimer power
    
    BRANCH=fwk-main
    BUG=https://app.clickup.com/t/86er6kera
    TEST=when plugin the chicony adapter on port 1
        EC log shows "vdm vidpid match" and "P1: Safe"
    
    Signed-off-by: Josh Tsai <Josh_Tsai@compal.com>
    (cherry picked from commit 68b7e0c1aa046c48d587c94c7e3dee421f2138f9)

--- zephyr/program/framework/src/cypress_pd_common.c
+++ zephyr/program/framework/src/cypress_pd_common.c
@@ -1492,9 +1492,11 @@ struct framework_dp_ids {
 	uint16_t vid;
 	uint16_t pid;
 } const cypd_altmode_ids[] = {
-	{0x32AC, 0x0002},
-	{0x32AC, 0x0003},
-	{0x32AC, 0x000E},
+	{0x32AC, 0x0002}, /* HDMI expension card vid and pid */
+	{0x32AC, 0x0003}, /* DP expension card vid and pid */
+	{0x32AC, 0x000E}, /* 180w adapter vid and pid */
+	{0x32AC, 0x0023}, /* Chicony 60w adapter vid and pid */
+	{0x32AC, 0x000B}, /* Phihong 60w adapter vid and pid */
 };
 struct match_vdm_header {
 	uint8_t idx;
@@ -1525,13 +1527,18 @@ void cypd_handle_vdm(int controller, int port, uint8_t *data, int len)
 	 *
 	 *                       ID HDR            ProductVDO
 	 *   hdr  SOP R VDMHDR   VDO      VDO      VDO
+	 *   0 1  2  3  4        8        12       16
 	 * HDMI
 	 * 0x8f52 00 00 41a000ff ac32006c 00000000 00000200 18000000
+	 *
 	 * DP
 	 * 0x8f52 00 00 41a000ff ac32006c 00000000 00000300 18000000
-	 *   0 1  2  3  4        8        12       16
+	 *
 	 * 180W Power Adapter
 	 * 0x8f59 00 00 41a800ff ac32c001 00000000 00000e00 01008020
+	 *
+	 * 60W Power Adapter
+	 * 0x8f5b 00 00 41a800ff ac32c001 00000000 01012300 01008020
 	 */
 	int i;
 	uint16_t vid, pid;
