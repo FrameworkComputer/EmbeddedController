7a1066019a3b80fb94b151c4f836833ce784e721
LeoCX_Tsai <LeoCX_Tsai@compal.com>
2025-02-07 17:25:42 +0800

    fwk: lilac: fix ucsi get_error_status response
    only response get_error_status when msg have error data
    
    BRANCH=fwk-lilac-27116
    BUG=https://app.clickup.com/t/86erf8x1m
    TEST=test on lilac, open cypdctl ucsi 1,
    manual setup ucsi cmd by memmap set, and check EC can response
    correct controller
    
    Signed-off-by: LeoCX_Tsai <LeoCX_Tsai@compal.com>
    (cherry picked from commit 0fe6d0dfde59e81d9187fda3a54287012c07d863)

--- zephyr/program/framework/src/ucsi.c
+++ zephyr/program/framework/src/ucsi.c
@@ -126,6 +126,7 @@ int ucsi_write_tunnel(void)
 	case UCSI_CMD_GET_CABLE_PROPERTY:
 	case UCSI_CMD_GET_ALTERNATE_MODES:
 	case UCSI_CMD_GET_CURRENT_CAM:
+	case UCSI_CMD_GET_ERROR_STATUS:
 
 		if (*command == UCSI_CMD_GET_ALTERNATE_MODES) {
 			/**
@@ -355,13 +356,20 @@ int ucsi_read_tunnel(int controller)
 	case UCSI_CMD_CANCEL:
 	case UCSI_CMD_SET_NOTIFICATION_ENABLE:
 	case UCSI_CMD_GET_CAPABILITY:
-	case UCSI_CMD_GET_ERROR_STATUS:
 		/* Those command need to wait two pd chip to response completed */
 		if (pd_chip_ucsi_info[0].read_tunnel_complete &&
 		    pd_chip_ucsi_info[1].read_tunnel_complete)
 			read_complete = 1;
 		else
 			read_complete = 0;
+		break;
+	case UCSI_CMD_GET_ERROR_STATUS:
+		/* If data length is non zero, then read data */
+		if (pd_chip_ucsi_info[controller].cci & 0xFF00)
+			read_complete = 1;
+		else
+			pd_chip_ucsi_info[controller].read_tunnel_complete = 0;
+
 		break;
 	case UCSI_CMD_ACK_CC_CI:
 		if (pd_chip_ucsi_info[0].read_tunnel_complete &&
