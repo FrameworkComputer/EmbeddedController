a4cf4dff3f39437b96e503121288c4aa7d902747
Firas Sammoura <fsammoura@google.com>
2025-02-11 06:06:50 +0000

    zephyr: tests: add exit stdlib test
    Add exit stdlib test. The test verify if exit() call works correctly
    and validates panic data.
    
    BUG=b:384102168
    TEST=./twister -v -i -T zephyr/test/lib
    
    Change-Id: I82cf9418d56b46fcb78c572dbf044cf9d95568ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6252122
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Dawid Niedźwiecki <dawidn@google.com>
    (cherry picked from commit 1c12c3e09be0b93bfd497e01d904e0d6373cb968)

--- zephyr/libc/exit.c
+++ zephyr/libc/exit.c
@@ -9,11 +9,19 @@
 /**
  * Terminates the thread, which could lead to system reboot.
  *
- * This function is called from libc functions such as abort() or exit().
+ * CONFIG_ARCH_POSIX enables Zephyr to use the host OS's standard C library.
+ * Thus, it uses `exit()` for cleanup, unlike embedded targets that might use
+ * `_exit()`. This ensures proper process termination in a POSIX environment.
+ * The `_exit()` is a weak Stanadard C library function that can be overridden
+ * similar to other functions such as abort().
  *
  * @param rc exit code
  */
+#ifndef CONFIG_ARCH_POSIX
 void _exit(int rc)
+#else
+void exit(int rc)
+#endif
 {
 	k_tid_t thread = k_current_get();
 
