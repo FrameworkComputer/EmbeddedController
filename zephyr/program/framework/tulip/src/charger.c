502cccddc8d10e9df2513e4fea87658a5f55ed98
johnwc_yeh <JohnWC_Yeh@compal.com>
2025-02-21 10:25:20 +0800

    fwk: tulip: enable raa489300 kconfig
    Enable Raa489300 Kconfig.
    Modify charger current limit setting.
    
    BUG=none
    BRANCH=fwk-main
    TEST=zmake build tulip success
    
    Signed-off-by: johnwc_yeh <JohnWC_Yeh@compal.com>

--- zephyr/program/framework/tulip/src/charger.c
+++ zephyr/program/framework/tulip/src/charger.c
@@ -25,6 +25,7 @@
 #include "i2c.h"
 #include "math_util.h"
 #include "power.h"
+#include "raa489300.h"
 #include "system.h"
 #include "throttle_ap.h"
 #include "util.h"
@@ -32,10 +33,7 @@
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
 
-#define RAA489300_ADDR_FLAGS 0x4a
-
 static int last_extpower_present;
-static int raa489300_charge_mv;
 
 #ifdef CONFIG_PLATFORM_EC_CHARGER_INIT_CUSTOM
 static void charger_chips_init(void);
@@ -92,6 +90,11 @@ static void charger_chips_init(void)
 		BQ25770_REG_AUTO_CHARGE, 0x1DC3))
 		goto init_fail;
 
+	/* 0x31 */
+	if (i2c_write16(I2C_PORT_CHARGER, BQ25710_SMBUS_ADDR1_FLAGS,
+		BQ25710_REG_CHARGE_OPTION_2, 0x0037))
+		goto init_fail;
+
 	/* 0x33 */
 	if (i2c_write16(I2C_PORT_CHARGER, BQ25710_SMBUS_ADDR1_FLAGS,
 		BQ25710_REG_PROCHOT_OPTION_0, 0x4A38))
@@ -143,158 +146,6 @@ init_fail:
 DECLARE_HOOK(HOOK_INIT, charger_chips_init, HOOK_PRIO_POST_I2C + 1);
 #endif
 
-static void charger_spr(void);
-DECLARE_DEFERRED(charger_spr);
-
-static void charger_spr(void)
-{
-	int val = 0x0000;
-
-	if ((!extpower_is_present()) || (raa489300_charge_mv > 36000)) {
-		return;
-	}
-
-	if (i2c_read16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS,
-		0x3A, &val) != EC_SUCCESS) {
-		CPRINTS("3Level-Buck not ready");
-		hook_call_deferred(&charger_spr_data, 500 * MSEC);
-		return;
-	}
-
-	if (((val >> 8) & 0xF) == 0)
-		crec_msleep(150);
-	/* TODO: Need to be replaced with 3level-buck function and macro */
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x14, 0x1580);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3F, 0x157C);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4F, 0x0801);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3D, 0x0B00);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4E, 0x0140);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4C, 0x1001);
-	crec_msleep(10);
-
-	/* TODO: Need to be replaced with 3level-buck function and macro */
-	if (raa489300_charge_mv <= 20000) {
-		i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3C, 0x80A0);
-		crec_msleep(10);
-		i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x15, 0x3410);
-	} else if (raa489300_charge_mv <= 36000) {
-		i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3C, 0x80A8);
-		crec_msleep(10);
-		i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x15, 0x5DC0);
-	}
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x39, 0x1003);
-	crec_msleep(10);
-
-	if (i2c_read16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3A, &val)) {
-		CPRINTS("read raa489300 info1 reg fail");
-	}
-
-	if (((val >> 8) & 0x3F) == 0x26) {
-		CPRINTS("3-lv buck Success SPR----");
-		return;
-	}
-
-	hook_call_deferred(&charger_spr_data, 100 * MSEC);
-}
-
-static void charger_epr(void);
-DECLARE_DEFERRED(charger_epr);
-
-static void charger_epr(void)
-{
-	int val = 0x0000;
-
-	if ((!extpower_is_present()) || (raa489300_charge_mv != 48000)) {
-		return;
-	}
-
-	if (i2c_read16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS,
-		0x3A, &val) != EC_SUCCESS) {
-		CPRINTS("3Level-Buck not ready");
-		hook_call_deferred(&charger_epr_data, 500 * MSEC);
-		return;
-	}
-
-	if (((val >> 8) & 0xF) == 0)
-		crec_msleep(150);
-	/* TODO: Need to be replaced with 3level-buck function and macro */
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x14, 0x1B58);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3F, 0x157C);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x15, 0x2EE0);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4F, 0x0001);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3D, 0x0B00);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3C, 0x80A4);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4E, 0x0140);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4C, 0x1001);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x39, 0x1001);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3F, 0x1B58);
-	crec_msleep(10);
-	i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x15, 0x5358);
-	crec_msleep(10);
-
-	if (i2c_read16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x3A, &val)) {
-		CPRINTS("read raa489300 info1 reg fail");
-	}
-
-	if (((val >> 8) & 0x3F) == 0x35) {
-		CPRINTS("3-lv buck Success EPR----");
-		return;
-	}
-
-	hook_call_deferred(&charger_epr_data, 100 * MSEC);
-}
-
-void charger_update(void)
-{
-	static int pre_power_uw;
-	int power_uw = cypd_get_ac_power();
-
-	raa489300_charge_mv = cypd_get_active_port_voltage();
-
-	if (extpower_is_present()) {
-		if (pre_power_uw != power_uw) {
-			CPRINTS("3lv-buck update ! V:%dmV,W:%dmW", raa489300_charge_mv, power_uw);
-			if (raa489300_charge_mv <= 36000) {
-				charger_spr();
-			} else if (raa489300_charge_mv == 48000) {
-				charger_epr();
-			}
-			/* TODO: Need to be replaced with 3level-buck function and macro */
-			if (power_uw == 65000 || power_uw == 100000 ||
-				power_uw == 130000 || power_uw == 210000) {
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x40, 0x3800);
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4B, 0x3800);
-			} else if (power_uw == 165000) {
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x40, 0x4F00);
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4B, 0x4F00);
-			} else if (power_uw >= 180000) {
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x40, 0x6500);
-				i2c_write16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS, 0x4B, 0x6500);
-			}
-
-			pre_power_uw = power_uw;
-		}
-	}
-}
-DECLARE_HOOK(HOOK_POWER_SUPPLY_CHANGE, charger_update, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_AC_CHANGE, charger_update, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_INIT, charger_update, HOOK_PRIO_POST_I2C + 1);
-
 int board_discharge_on_ac(int enable)
 {
 	int chgnum;
@@ -313,6 +164,7 @@ int board_discharge_on_ac(int enable)
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
 {
+	int level_buck_ma;
 	int64_t calculate_ma;
 
 	if (charge_ma < CONFIG_PLATFORM_EC_CHARGER_DEFAULT_CURRENT_LIMIT) {
@@ -322,14 +174,18 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	/* Handle EPR converstion through the buck switcher */
 	if (charge_mv > 20000) {
 		/**
-		 * (charge_ma * charge_mv / 20000 ) * 0.9 * 0.94
+		 * (charge_ma * charge_mv / 24000 ) * 0.95 * 0.95
 		 */
-		calculate_ma = (int64_t)charge_ma * (int64_t)charge_mv * 90 * 95 / 200000000;
+		calculate_ma = (int64_t)charge_ma * (int64_t)charge_mv * 95 * 95 / 240000000;
 	} else {
-		calculate_ma = (int64_t)charge_ma * 88 / 100;
+		calculate_ma = (int64_t)charge_ma * 98 / 100;
 	}
 
+	level_buck_ma = charge_ma * 98 / 100;
+
 	CPRINTS("Updating charger with EPR correction: ma %d", (int16_t)calculate_ma);
+
+	level_buck_set_input_current_limit(level_buck_ma);
 
 	charge_set_input_current_limit((int)calculate_ma, charge_mv);
 }
@@ -392,29 +248,3 @@ __override void board_check_extpower(void)
 
 	pre_active_port = pd_active_port;
 }
-
-static int raa489300_cmd(int argc, const char **argv)
-{
-	if (argc >= 2) {
-		if (!strncmp(argv[1], "get", 3)) {
-			int i;
-			int val;
-
-			/* Dump all readable registers*/
-			static const uint8_t regs[] = {
-				0x14, 0x15, 0x39, 0x3a, 0x3c, 0x3d, 0x3f, 0x40, 0x43,
-				0x49, 0x4b, 0x4c, 0x4e, 0x4f,
-			};
-
-			for (i = 0; i < ARRAY_SIZE(regs); ++i) {
-				if (i2c_read16(I2C_PORT_CHARGER, RAA489300_ADDR_FLAGS,
-					regs[i], &val))
-					continue;
-				ccprintf("raa489300 REG 0x%02x:  0x%04x\n", regs[i], val);
-			}
-		}
-	}
-	return EC_SUCCESS;
-}
-DECLARE_CONSOLE_COMMAND(raa489300, raa489300_cmd, "[get]",
-			"Get raa489300 register");
