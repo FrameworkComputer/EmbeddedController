d15cf55329043c9c927dec68c3e5e87fb7c97993
Tom Hughes <tomhughes@chromium.org>
2025-02-25 15:15:17 -0800

    core/riscv-rv32i: Add support for using clang RISC-V toolchain
    The default is still the coreboot gcc toolchain. clang can be enabled
    with CROSS_COMPILE_CC_NAME=clang.
    
    The changes in this commit are analogous to the ones in
    core/cortex-m[0]/toolchain.mk and core/cortex-m[0]/build.mk.
    
    BUG=b:407560601, b:172020503
    TEST=make V=1 CROSS_COMPILE_CC_NAME=clang BOARD=it8xxx2_evb -j
    TEST=make V=1 BOARD=it8xxx2_evb -j
    TEST=./util/compare_build.sh -b all
    
    Change-Id: Iaad13dc5b9a729ff3e836c86083091c566bf2e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6303162
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Eric Yilun Lin <yllin@google.com>
    (cherry picked from commit 2ba085089a0b3ed96570d7bc33ba50f2f843a29f)

--- core/riscv-rv32i/build.mk
+++ core/riscv-rv32i/build.mk
@@ -21,11 +21,26 @@ CFLAGS_CPU+=-mabi=ilp32$(_FPU_EXTENSION) -Os
 # exact issue, but the added code is as small as it gets.
 CFLAGS_CPU+=-fsanitize=integer-divide-by-zero -fsanitize-undefined-trap-on-error
 LDFLAGS_EXTRA+=-mrelax
-LDFLAGS_EXTRA+=-static-libgcc -lgcc
 
 ifneq ($(CONFIG_LTO),)
 CFLAGS_CPU+=-flto
 LDFLAGS_EXTRA+=-flto
+endif
+
+ifeq ($(CROSS_COMPILE_CC_NAME),clang)
+CFLAGS_CPU+=-Oz		# Like -Os (and thus -O2), but reduces code size further.
+# b/256193799: Reduce inline threshold to decrease code size.
+CFLAGS_CPU+=-Wl,-mllvm -Wl,-inline-threshold=-10
+# Explicitly specify libclang_rt.builtins so that its symbols are preferred
+# over libc's. This avoids duplicate symbol errors. See b/346309204 for details.
+clang_resource_dir:="$(shell $(CC) --print-resource-dir)"
+ifneq ($(.SHELLSTATUS),0)
+$(error Could not determine path to libclang_rt.builtins)
+endif
+LDFLAGS_EXTRA+=\
+	"$(clang_resource_dir)/lib/baremetal/libclang_rt.builtins-riscv32.a"
+else
+LDFLAGS_EXTRA+=-static-libgcc -lgcc
 endif
 
 core-y=cpu.o init.o panic.o task.o switch.o __builtin.o math.o
--- core/riscv-rv32i/toolchain.mk
+++ core/riscv-rv32i/toolchain.mk
@@ -2,8 +2,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-USE_COREBOOT_SDK:=1
+ifeq ($(CROSS_COMPILE_CC_NAME),clang)
+# llvm sdk
+CROSS_COMPILE_RISCV_DEFAULT:=riscv32-cros-elf-
+else
+# coreboot sdk
+CROSS_COMPILE_RISCV_DEFAULT:=riscv64-elf
 COREBOOT_TOOLCHAIN:=riscv
+USE_COREBOOT_SDK:=1
+endif
 
 # Select RISC-V bare-metal toolchain
-$(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_riscv),riscv64-elf)
+$(call set-option,CROSS_COMPILE,\
+	$(CROSS_COMPILE_riscv),\
+	$(CROSS_COMPILE_RISCV_DEFAULT))
