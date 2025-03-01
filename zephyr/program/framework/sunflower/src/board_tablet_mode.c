1f6ae2df8e5f6c5a6055f8bc69b72fbfca4475fb
Daniel Schaefer <dhs@frame.work>
2025-03-02 01:16:42 +0800

    fwk/sunflower: Disable keyboard in tablet mode
    Instead of lidangle, let's go by tablet mode. We do the same for the
    touchpad disable.
    Doing that has the advantage that we can force tablet or laptop mode
    with a host command from Windows or Linux equally.
    
    BUG=https://frameworkmarketplace.atlassian.net/browse/HW-6394
    BRANCH=fwk-sunflower-26784
    TEST=Use framework_tool --tablet-mode laptop to force laptop mode. Then
    check EC console and fold the screen past 180deg. Keyboard should stay
    enabled and no related print should show on the console.
    
    Can also use `ectool tabletmode off` instead of framework_tool.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- zephyr/program/framework/sunflower/src/board_tablet_mode.c
+++ zephyr/program/framework/sunflower/src/board_tablet_mode.c
@@ -5,6 +5,7 @@
 
 #include "hooks.h"
 #include "tablet_mode.h"
+#include "keyboard_scan.h"
 
 static void board_tablet_mode_change(void)
 {
@@ -20,6 +21,9 @@ static void board_tablet_mode_change(void)
 	 */
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_pad_mode),
 			tablet_get_mode());
+
+	/* EC needs to control the keyboard scan with non-ChromeOS systems */
+	keyboard_scan_enable(!tablet_get_mode(), KB_SCAN_DISABLE_LID_ANGLE);
 }
 DECLARE_HOOK(HOOK_TABLET_MODE_CHANGE, board_tablet_mode_change, HOOK_PRIO_DEFAULT);
 /* Run after gmr_tablet_switch_init to initialize GPIO after debounce. */
