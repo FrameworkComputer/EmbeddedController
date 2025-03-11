a77d9148696a4f4944e3eee846e2c9c7b69600b4
Firas Sammoura <fsammoura@google.com>
2025-03-11 05:25:21 +0000

    ec/util: Generate 8-bit or 16-bit PGM ASCII output
    Use libec static method FrameToPgm from FpFrameCommand class to generate
    PGM ASCII output from both 8-bit and 16-bit frames.
    
    BUG=b:400472408
    TEST=emerge-brya ec-utils
    TEST=make BOARD=host utils-host
    LOW_COVERAGE_REASON=b:400472408, tested on device
    
    Force-Relevant-Builds: all
    Change-Id: I71e9402cdadf71c233c74f78ca8363a900bb78c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6331752
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    (cherry picked from commit 50e7fc0cb99cf21a548f3f25ebafb169e3544ab4)

--- util/ectool.cc
+++ util/ectool.cc
@@ -2180,16 +2180,15 @@ int cmd_fp_frame(int argc, char *argv[])
 		return 0;
 	}
 
-	/* Print 8-bpp PGM ASCII header */
-	printf("P2\n%d %d\n%d\n", r.width, r.height, (1 << r.bpp) - 1);
+	auto frame_to_pgm = ec::FpFrameCommand::FrameToPgm(
+		*fp_frame,
+		{ .bpp = r.bpp, .width = r.width, .height = r.height });
 
-	uint8_t *ptr = fp_frame->data();
-	for (int y = 0; y < r.height; y++) {
-		for (int x = 0; x < r.width; x++, ptr++)
-			printf("%d ", *ptr);
-		printf("\n");
+	if (!frame_to_pgm.has_value()) {
+		fprintf(stderr, "Error: Failed to convert frame to PGM.\n");
+		return -1;
 	}
-	printf("# END OF FILE\n");
+	fprintf(stdout, "%s", frame_to_pgm->c_str());
 	return 0;
 }
 
