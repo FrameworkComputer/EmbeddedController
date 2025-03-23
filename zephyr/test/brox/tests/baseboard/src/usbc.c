4816afeed69f23fafed7f48c1e32f1985fc964b0
Tristan Honscheid <honscheid@google.com>
2025-03-23 15:37:49 -0600

    test: brox: Add unit tests for Brox runtime USB-C config board code
    Add unit testing for Brox's board_get_pdc_for_port() implementation mo
    ensure the correct PDC configuration is returned.
    
    BUG=b:372910804
    TEST=./twister -s brox/brox.baseboard.usbc
    
    Change-Id: I204b3d0c135de1d46fdf66a2d878e3564590b6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6387775
    Commit-Queue: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Tristan Honscheid <honscheid@google.com>
    (cherry picked from commit a96bb6f6475a2c216027f2239b6009bde24c0890)

--- zephyr/test/brox/tests/baseboard/src/usbc.c
+++ zephyr/test/brox/tests/baseboard/src/usbc.c
@@ -5,6 +5,8 @@
 
 #include "usb_pd.h"
 
+#include <zephyr/device.h>
+#include <zephyr/fff.h>
 #include <zephyr/ztest.h>
 
 ZTEST_SUITE(usbc, NULL, NULL, NULL, NULL, NULL);
@@ -17,4 +19,130 @@ ZTEST(usbc, test_board_get_pd_port_location)
 		      EC_PD_PORT_LOCATION_LEFT_BACK);
 	zassert_equal(board_get_pd_port_location(2),
 		      EC_PD_PORT_LOCATION_UNKNOWN);
+}
+
+/*
+ * Runtime USB-C port config tests
+ */
+
+FAKE_VALUE_FUNC(int, cbi_get_sku_id, uint32_t *);
+
+/** Used to configure the custom fake below */
+static uint32_t fake_sku = 0;
+
+/** Custom fake to output controlled SKU IDs from CBI */
+static int cbi_get_sku_id_custom_fake(uint32_t *sku)
+{
+	*sku = fake_sku;
+
+	return 0;
+}
+
+static void usbc_runtime_reset(void *f)
+{
+	ARG_UNUSED(f);
+
+	fake_sku = 0;
+	RESET_FAKE(cbi_get_sku_id);
+}
+
+ZTEST_SUITE(usbc_runtime, NULL, NULL, usbc_runtime_reset, usbc_runtime_reset,
+	    NULL);
+
+/** Brox-supplied port information function */
+int board_get_pdc_for_port(int port, const struct device **dev);
+
+/** Declare fake device structs to return */
+const struct device DEVICE_DT_NAME_GET(DT_NODELABEL(pdc_power_p0));
+const struct device DEVICE_DT_NAME_GET(DT_NODELABEL(pdc_power_p1));
+const struct device DEVICE_DT_NAME_GET(DT_NODELABEL(pdc_power_p0_ti));
+const struct device DEVICE_DT_NAME_GET(DT_NODELABEL(pdc_power_p1_ti));
+
+ZTEST(usbc_runtime, test_board_get_pdc_for_port__null)
+{
+	int rv = board_get_pdc_for_port(0, NULL);
+
+	zassert_equal(-EINVAL, rv, "Expected -EINVAL, got %d", rv);
+}
+
+ZTEST(usbc_runtime, test_board_get_pdc_for_port__bad_cbi)
+{
+	const struct device *dev;
+	int rv;
+
+	cbi_get_sku_id_fake.return_val = 1;
+
+	rv = board_get_pdc_for_port(0, &dev);
+
+	zassert_equal(-EIO, rv, "Expected -EIO, got %d", rv);
+}
+
+ZTEST(usbc_runtime, test_board_get_pdc_for_port__bad_sku)
+{
+	const struct device *dev;
+	int rv;
+
+	cbi_get_sku_id_fake.custom_fake = cbi_get_sku_id_custom_fake;
+	fake_sku = 0xAABBCCDD;
+
+	rv = board_get_pdc_for_port(0, &dev);
+
+	zassert_equal(-ENOENT, rv, "Expected -ENOENT, got %d", rv);
+}
+
+ZTEST(usbc_runtime, test_board_get_pdc_for_port__rtk_skus)
+{
+	const struct device *dev;
+	const uint32_t rtk_skus[] = { 0x01, 0x02, 0x03, 0x21, 0x22, 0x23 };
+	int rv;
+
+	cbi_get_sku_id_fake.custom_fake = cbi_get_sku_id_custom_fake;
+
+	for (int i = 0; i < ARRAY_SIZE(rtk_skus); i++) {
+		fake_sku = rtk_skus[i];
+
+		rv = board_get_pdc_for_port(0, &dev);
+		zassert_ok(rv, "Got non-zero result for port 0, sku 0x%0x: %d",
+			   fake_sku, rv);
+		zassert_equal(dev, DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0)),
+			      "Got %p, expected %d", dev,
+			      DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0)));
+
+		rv = board_get_pdc_for_port(1, &dev);
+		zassert_ok(rv, "Got non-zero result for port 1, sku 0x%0x: %d",
+			   fake_sku, rv);
+		zassert_equal(dev, DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1)),
+			      "Got %p, expected %d", dev,
+			      DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1)));
+
+		rv = board_get_pdc_for_port(2, &dev);
+		zassert_equal(-ENOENT, rv, "Expected -ENOENT, got %d", rv);
+	}
+}
+
+ZTEST(usbc_runtime, test_board_get_pdc_for_port__ti_skus)
+{
+	const struct device *dev;
+	int rv;
+
+	cbi_get_sku_id_fake.custom_fake = cbi_get_sku_id_custom_fake;
+
+	fake_sku = 0x24;
+
+	rv = board_get_pdc_for_port(0, &dev);
+	zassert_ok(rv, "Got non-zero result for port 0, sku 0x%0x: %d",
+		   fake_sku, rv);
+	zassert_equal(dev, DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0_ti)),
+		      "Got %p, expected %d", dev,
+		      DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0_ti)));
+
+	rv = board_get_pdc_for_port(1, &dev);
+	zassert_ok(rv, "Got non-zero result for port 1, sku 0x%0x: %d",
+		   fake_sku, rv);
+	zassert_equal(dev, DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1_ti)),
+		      "Got %p, expected %d", dev,
+		      DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1_ti)));
+
+	rv = board_get_pdc_for_port(2, &dev);
+	zassert_equal(-ENOENT, rv, "Expected -ENOENT, got %d", rv);
 }
