d6d3673a856d3d97c8d3edde70e32359f6e7f1d0
Boris Mittelberg <bmbm@google.com>
2025-03-26 13:35:26 -0700

    brya: enable prochot_in monitoring
    Monitor EC_PROCHOT_IN_L on brya boards without custom prochot logic
    and set fan to 100% where applicable.
    
    BUG=b:399429151
    TEST=manually tested on felwinter
    
    Change-Id: I4a10868ed76fb390f539f6de2c8375337b4ff2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6397765
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Tested-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>
    (cherry picked from commit 4843547133cc0a544a49e331fda6b7dae80360e6)

--- baseboard/brya/baseboard.c
+++ baseboard/brya/baseboard.c
@@ -6,9 +6,12 @@
 #include "chipset.h"
 #include "common.h"
 #include "compile_time_macros.h"
+#include "gpio.h"
 #include "gpio_signal.h"
+#include "hooks.h"
 #include "keyboard_scan.h"
 #include "tablet_mode.h"
+#include "throttle_ap.h"
 
 /* Wake up pins */
 const enum gpio_signal hibernate_wake_pins[] = {
@@ -17,3 +20,17 @@ const enum gpio_signal hibernate_wake_pins[] = {
 	GPIO_LID_OPEN,
 };
 const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+#ifndef HAS_TASK_PROCHOT
+static const struct prochot_cfg brya_prochot_cfg = {
+	.gpio_prochot_in = GPIO_EC_PROCHOT_IN_L,
+};
+
+static void prochot_monitoring_init(void)
+{
+	/* Enable monitoring of the PROCHOT input to the EC */
+	throttle_ap_config_prochot(&brya_prochot_cfg);
+	gpio_enable_interrupt(GPIO_EC_PROCHOT_IN_L);
+}
+DECLARE_HOOK(HOOK_INIT, prochot_monitoring_init, HOOK_PRIO_DEFAULT);
+#endif /* !HAS_TASK_PROCHOT */
