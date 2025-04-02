796d046fcbc8e0ed9965fd28992f6911ba0e4949
Firas Sammoura <fsammoura@google.com>
2025-04-02 18:35:04 +0000

    ec_commands: Add fp_capture_type member to fp_image_frame_params struct
    Use the fp_capture_type member to store the capture_type in the ec
    domain.
    
    BUG=b:398899644
    TEST=None
    
    Change-Id: Id739b094280cffd0b5028a45300ae2e903eca024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6426320
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    (cherry picked from commit 1a659d39f4324797af424d5d63198e239dd3b1f4)

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -8440,7 +8440,9 @@ struct fp_image_frame_params {
 	uint32_t pixel_format; /* using V4L2_PIX_FMT_ */
 	uint16_t width;
 	uint16_t height;
-	uint16_t reserved; /**< padding for alignment */
+	/** Type of image capture from enum fp_capture_type. */
+	uint8_t fp_capture_type;
+	uint8_t reserved; /**< padding for alignment */
 } __ec_align4;
 BUILD_ASSERT(sizeof(struct fp_image_frame_params) == 16);
 
