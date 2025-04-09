e406e2e54d8c5fb96060665764aafe94ec10534e
Keith Short <keithshort@chromium.org>
2025-04-09 14:04:18 -0600

    npcx_monitor: Skip the hash check for the llvm toolchain
    We deploy the npcx_monitor binary built from the coreboot-sdk toolchain.
    However the CQ also builds the npcx_monitor using llvm.  Skip the
    hash check for llvm builds to prevent CQ failures when the llvm
    toolchain or dependent libraries change.
    
    BUG=b:409029097
    TEST="zmake build npcx_monitor --clobber", confirm hash check runs
    TEST="zmake build npcx_monitor --clobber --toolchain llvm", confirm hash
    check skipped
    
    Change-Id: I28f5bc6549fdb902844af3c1e8fcee16a72c4916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6443725
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Commit-Queue: zhi cheng yuan <jasonyuan@chromium.org>
    Reviewed-by: zhi cheng yuan <jasonyuan@chromium.org>
    Tested-by: Keith Short <keithshort@chromium.org>
    (cherry picked from commit 248525df88c510ecca6770579a8f7e2348511975)

--- zephyr/shim/chip/npcx/npcx_monitor/check_hash.cmake
+++ zephyr/shim/chip/npcx/npcx_monitor/check_hash.cmake
@@ -11,8 +11,10 @@ elseif ("${ZEPHYR_TOOLCHAIN_VARIANT}" STREQUAL "zephyr")
   # Zephyr builds
   set(expected_hash "2a4abad66294d175ad82542f5be35af3802d9df5e12c58c9835a15e0886f78dd")
 elseif ("${ZEPHYR_TOOLCHAIN_VARIANT}" STREQUAL "llvm")
-  # llvm builds
-  set(expected_hash "67c406243757ad29776beaf2f90183774fad1f3dc1348a9f5047cd6711377a90")
+  # llvm builds - skip the hash check.  We currently deploy the version
+  # built with the coreboot-sdk toolchain.
+  message(STATUS "NPCX Monitor hash check skipped")
+  return()
 endif()
 
 if("${npcx_monitor_hash}" STREQUAL "${expected_hash}")
