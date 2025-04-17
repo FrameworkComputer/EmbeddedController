62d86f871a60410bc69e08759258de94c09fe38d
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-04-17 18:50:10 +0800

    isl9241: Fix trickle charging current over battery spec
    Ensure the correct sense resistor config is applied when
    filling in the Trickle Charging Current.
    
    BUG=b:411524285
    TEST=Manually check the charge current on Kanix
    
    Change-Id: I4bbb03acc3f3dce3586af2e15a8b25b878b4011d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6467621
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Auto-Submit: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    (cherry picked from commit 16e79d480ae42fe56d026d982e544a4e845ef7ab)

--- driver/charger/isl9241.c
+++ driver/charger/isl9241.c
@@ -1255,11 +1255,11 @@ static void isl9241_init(int chgnum)
 	 * [15:13]: Trickle Charging Current (battery pre-charge current)
 	 * [10:9] : Prochot# Debounce time (1000us)
 	 */
-	if (isl9241_update(
-		    chgnum, ISL9241_REG_CONTROL2,
-		    (ISL9241_CONTROL2_TRICKLE_CHG_CURR(bi->precharge_current) |
-		     ISL9241_CONTROL2_PROCHOT_DEBOUNCE_1000),
-		    MASK_SET))
+	if (isl9241_update(chgnum, ISL9241_REG_CONTROL2,
+			   (ISL9241_CONTROL2_TRICKLE_CHG_CURR(
+				    BC_CURRENT_TO_REG(bi->precharge_current)) |
+			    ISL9241_CONTROL2_PROCHOT_DEBOUNCE_1000),
+			   MASK_SET))
 		goto init_fail;
 
 	/*
