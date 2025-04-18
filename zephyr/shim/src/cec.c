be4ff31d3d5da9cef7992d024bc91e86b559fb3b
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2025-04-18 10:47:46 +0800

    zephyr: cec: add CEC policy DT bindings
    Add default_cec_policy to cec_config[port].offline_policy by
    defined a DT node with compatible cros_ec_cec_policy.
    
    BUG=b:411181188
    TEST=check the command and action in cec_config[port].offline_policy
         by a console command on dirks.
    
    Change-Id: I3030ce30691b964a83118ffcbfdaf38174c38441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6471531
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    (cherry picked from commit 57e8d0cbdbf341d0c17d36652eee544ecf1980c8)

--- zephyr/shim/src/cec.c
+++ zephyr/shim/src/cec.c
@@ -11,6 +11,20 @@
 
 #define CEC_BITBANG_COMPAT cros_ec_cec_bitbang
 #define CEC_IT83XX_COMPAT cros_ec_cec_it83xx
+#define CEC_POLICY_COMPAT cros_ec_cec_policy
+
+#define CEC_POLICY_NODE DT_INST(0, CEC_POLICY_COMPAT)
+#define CEC_POLICY_CONFIG(node_id)                            \
+	{                                                     \
+		.command = DT_STRING_TOKEN(node_id, command), \
+		.action = DT_STRING_TOKEN(node_id, action),   \
+	},
+
+static struct cec_offline_policy default_cec_policy[] = {
+#if DT_NODE_EXISTS(CEC_POLICY_NODE)
+	DT_FOREACH_CHILD(CEC_POLICY_NODE, CEC_POLICY_CONFIG){ 0 },
+#endif
+};
 
 #define CEC_DRV_CONFIG_BITBANG(node_id)                                     \
 	static const struct bitbang_cec_config node_id##_bitbang_config = { \
@@ -23,17 +37,21 @@
 
 DT_FOREACH_STATUS_OKAY(CEC_BITBANG_COMPAT, CEC_DRV_CONFIG_BITBANG)
 
-#define CEC_CONFIG_BITBANG(node_id)                      \
-	{                                                \
-		.drv = &bitbang_cec_drv,                 \
-		.drv_config = &node_id##_bitbang_config, \
-		.offline_policy = NULL,                  \
+#define CEC_CONFIG_BITBANG(node_id)                                 \
+	{                                                           \
+		.drv = &bitbang_cec_drv,                            \
+		.drv_config = &node_id##_bitbang_config,            \
+		.offline_policy = DT_NODE_EXISTS(CEC_POLICY_NODE) ? \
+					  default_cec_policy :      \
+					  NULL,                     \
 	}
 
-#define CEC_CONFIG_IT83XX(node_id)                          \
-	{                                                   \
-		.drv = &it83xx_cec_drv, .drv_config = NULL, \
-		.offline_policy = NULL,                     \
+#define CEC_CONFIG_IT83XX(node_id)                                  \
+	{                                                           \
+		.drv = &it83xx_cec_drv, .drv_config = NULL,         \
+		.offline_policy = DT_NODE_EXISTS(CEC_POLICY_NODE) ? \
+					  default_cec_policy :      \
+					  NULL,                     \
 	}
 
 #define CHECK_COMPAT(node_id, compat, config) \
