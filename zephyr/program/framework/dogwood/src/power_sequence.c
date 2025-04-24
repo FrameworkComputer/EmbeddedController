1101eb364abc873d086d58a7940769ee02e177f0
Josh Tsai <Josh_Tsai@compal.com>
2025-04-24 14:00:30 +0800

    fwk: dogwood: create a hc and console command to force on the PSU in standby mode
    The latest UI ERS (x11) adds a new item that needs to control the PSU power with
    the host command in standby mode.
    
    BRANCH=fwk-dogwood-27111
    BUG=HW-6519
    TEST=Use console command "powerindebug" to check the X86_3VALW_PG bit with the
         below condition.
         1. Use EC console command "psu_s3_keep 1", when the system enters S0i3 state
            X86_3VALW_PG is 1.
         2. Use host command [939.328300 HC 0x3e28.0:01] to enable the force_on_psu,
            when the system enters S0i3 state X86_3VALW_PG is 1.
         3. Disable force_on_psu, when the system enters S0i3 state X86_3VALW_PG is 0.
    
    Signed-off-by: Josh Tsai <Josh_Tsai@compal.com>

--- zephyr/program/framework/dogwood/src/power_sequence.c
+++ zephyr/program/framework/dogwood/src/power_sequence.c
@@ -46,6 +46,7 @@
 static bool power_s5_up;		/* Chipset is sequencing up or down */
 static int force_shutdown_flags;
 static int d3cold_is_entry;	/* check the d3cold status */
+static int force_enable_psu = 0;
 
 static void system_check_ssd_status(void);
 
@@ -615,14 +616,20 @@ enum power_state power_handle_state(enum power_state state)
 				return POWER_S0;
 			}
 
-			k_msleep(10);
-			gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_s0ix), 1);
+			/**
+			 * Don't convert the 5VALW to 5VSB if the user enables to force
+			 * on PSU in standby mode.
+			 */
+			if (!force_enable_psu) {
+				k_msleep(10);
+				gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_s0ix), 1);
 
-			k_msleep(20);
-			gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_rvsp_l), 0);
+				k_msleep(20);
+				gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_rvsp_l), 0);
 
-			k_msleep(10);
-			power_enable_psu(0);
+				k_msleep(10);
+				power_enable_psu(0);
+			}
 		}
 
 		return POWER_S3;
@@ -869,3 +876,38 @@ __override int chipset_in_low_power_mode(void)
 
 	return in_low_power_mode;
 }
+
+static int cmd_psu_s3_keep(int argc, const char **argv)
+{
+	char *e;
+
+	if (argc > 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (argc == 2) {
+		force_enable_psu = strtoi(argv[1], &e, 10);
+		if (*e)
+			return EC_ERROR_PARAM1;
+	};
+
+	CPRINTS("PSU is forced on in S3: %d", force_enable_psu);
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(psu_s3_keep, cmd_psu_s3_keep,
+			"[1/0]",
+			"Keep the PSU on when the system is in S3.");
+
+static enum ec_status hc_psu_control(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_psu_control *p = args->params;
+	struct ec_response_psu_control *r = args->response;
+
+	force_enable_psu = p->force_enable_in_standby > 0 ? true : false;
+
+	r->force_enable_in_standby = force_enable_psu;
+	args->response_size = sizeof(*r);
+
+	return EC_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PSU_CONTROL, hc_psu_control, EC_VER_MASK(0));
