7853b66ec92817d74a3c83cb28b732a7832805ad
Daniel Schaefer <dhs@frame.work>
2025-05-17 15:03:58 +0800

    Revert "fwk: Fix ignoring powerbutton if lid closed from G3"
    It's buggy:
    
    ```
    /mnt/host/source/src/platform/ec/zephyr/program/framework/src/power_button_x86.c: In function 'set_initial_pwrbtn_state':
    /mnt/host/source/src/platform/ec/zephyr/program/framework/src/power_button_x86.c:257:17: error: this 'if' clause does not guard... [-Werror=misleading-indentation]
      257 |                 if (!lid_is_open())
          |                 ^~
    /mnt/host/source/src/platform/ec/zephyr/program/framework/src/power_button_x86.c:259:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
      259 |                         return;
          |                         ^~~~~~
    ```
    
    This reverts commit 637d4d5bc00756eb02c206dd529168d3758e5648.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- zephyr/program/framework/src/power_button_x86.c
+++ zephyr/program/framework/src/power_button_x86.c
@@ -210,7 +210,7 @@ static void power_button_debounce_deferred(void)
 		 * Power button already release ignore PB signal
 		 */
 		pwrbtn_state = PWRBTN_STATE_IDLE;
-		CPRINTS("PB debounce ignore signal - debounce");
+		CPRINTS("PB debounce ignore signal");
 	} else {
 		/**
 		 * Power button is still pressed, power on the system
@@ -249,15 +249,9 @@ static void set_initial_pwrbtn_state(void)
 			(gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_on_off_btn_l)) == 1) &&
 			!get_standalone_mode()) {
 			pwrbtn_state = PWRBTN_STATE_IDLE;
-			CPRINTS("PB ignore signal - chassis open");
+			CPRINTS("PB ignore signal");
 			return;
 		}
-
-#ifndef CONFIG_POWER_BUTTON_IGNORE_LID
-		if (!lid_is_open())
-			CPRINTS("PB ignore signal - lid closed");
-			return;
-#endif
 
 		/**
 		 * Check the power button debounce time when the system presses
