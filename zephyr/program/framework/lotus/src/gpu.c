c6a7e1e377b597a0d745cf0c50a1fd38ece0aceb
Josh Tsai <Josh_Tsai@compal.com>
2025-05-21 09:14:47 +0800

    Revert "fwk: workaround: gpu_power_enable() always return true with NV GPU"
    BIOS version v1.00 enabled the NV GPU D3Cold feature, the GPIO
    dgpu_pwr_en is controlled correctly, so revert the workaround.
    
    BRANCH=fwk-main
    BUG=Support GN22 D3Cold in EC code
    TEST=Used EC console "temps" to check the gpu sensor can be read
         when GPU is working
    TEST=Used EC console "temps" to check the gpu sensor not power
         when GPU in d3cold.
    
    This reverts commit 74aa9dc2d16ceab1ebecae71a0a196b0e7ed288e.

--- zephyr/program/framework/lotus/src/gpu.c
+++ zephyr/program/framework/lotus/src/gpu.c
@@ -46,14 +46,8 @@ bool gpu_power_enable(void)
 	/* dgpu pwr enable pin will be high at s5 state*/
 	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
 		return 0;
-	else {
-		uint8_t gpu_type = *host_get_memmap(EC_CUSTOMIZED_MEMMAP_GPU_TYPE);
-
-		if (gpu_type == GPU_NV_GN22)
-			return true;
-
+	else
 		return gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_dgpu_pwr_en));
-	}
 }
 
 bool gpu_is_working(void)
