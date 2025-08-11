b00a6006f1826c1d6621dfb69bcbedf2746e25e8
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2025-08-11 15:06:33 +0800

    lapis: Use CONFIG_AP_PWRSEQ_DRIVER for power sequence
    Lapis uses a Windows co-lay design, where certain timing pins
    require active control by the EC. Therefore, using
    AP_PWRSEQ_DRIVER mode provides more convenient control.
    
    BUG=b:438785495
    TEST=1.zmake build lapis
         2.EC can boot to S0
    LOW_COVERAGE_REASON=b:438785495 track ztest code
    
    Change-Id: Ic236c7f5aabc3e27a4f61bc419e7986b679aa412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6839678
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Tested-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Commit-Queue: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/fatcat/lapis/src/board_power.c
+++ zephyr/program/fatcat/lapis/src/board_power.c
@@ -3,160 +3,124 @@
  * found in the LICENSE file.
  */
 
-#include "gpio/gpio.h"
+#include "gpio.h"
 #include "gpio_signal.h"
 #include "system_boot_time.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/atomic.h>
 
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
 #include <ap_power/ap_power.h>
-#include <ap_power/ap_power_events.h>
-#else
-#include "ap_power/ap_pwrseq_sm.h"
-#endif
-#include <ap_power/ap_power_interface.h>
-#include <ap_power/ap_pwrseq.h>
+#include <ap_power/ap_pwrseq_sm.h>
 #include <ap_power_override_functions.h>
 #include <power_signals.h>
-#include <x86_power_signals.h>
 
 LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
-test_export_static bool s0_stable;
-#endif
+#define X86_NON_DSX_FORCE_SHUTDOWN_TO_MS 50
+
+/* Power cycling primary rail requires at least 30 ms 'off' time */
+#define BOARD_PTL_RVP_MINIMUM_POWER_DOWN_DELAY_MS 30
 
 void board_ap_power_force_shutdown(void)
 {
+	int timeout_ms = X86_NON_DSX_FORCE_SHUTDOWN_TO_MS;
+
+	/* Turn off PCH_RMSRST to meet tPCH12 */
 	power_signal_set(PWR_EC_PCH_RSMRST, 1);
 
-	power_signal_set(PWR_EN_PP3300_A, 0);
+	/* Disable all control gpio here */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_disable), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_lcd_backoff), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ssd_pln), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vr_on), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_susb_ec), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vsus_on_ec), 0);
 
 	power_signal_set(PWR_EN_PP5000_A, 0);
-
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
-	s0_stable = false;
-#endif
-}
-
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
-void board_ap_power_action_g3_s5(void)
-{
-	LOG_DBG("Turning on PWR_EN_PP5000_A and PWR_EN_PP3300_A");
-	power_signal_set(PWR_EN_PP5000_A, 1);
-	power_signal_set(PWR_EN_PP3300_A, 1);
-
-	update_ap_boot_time(ARAIL);
-	power_wait_signals_on_timeout(IN_PGOOD_ALL_CORE,
-				      AP_PWRSEQ_DT_VALUE(wait_signal_timeout));
-
-	s0_stable = false;
-}
-
-void board_ap_power_action_s3_s0(void)
-{
-	s0_stable = false;
-}
-
-void board_ap_power_action_s0_s3(void)
-{
-	s0_stable = false;
-}
-
-void board_ap_power_action_s0(void)
-{
-	if (s0_stable) {
-		return;
-	}
-	LOG_INF("Reaching S0");
-	s0_stable = true;
-}
-
-int board_ap_power_assert_pch_power_ok(void)
-{
-	/* Pass though PCH_PWROK */
-	if (power_signal_get(PWR_PCH_PWROK) == 0) {
-		k_msleep(AP_PWRSEQ_DT_VALUE(pch_pwrok_delay));
-		power_signal_set(PWR_PCH_PWROK, 1);
-	}
-
-	return 0;
-}
-
-bool board_ap_power_check_power_rails_enabled(void)
-{
-	return power_signal_get(PWR_EN_PP3300_A) &&
-	       power_signal_get(PWR_EN_PP5000_A);
-}
-#else
-#ifndef CONFIG_EMUL_AP_PWRSEQ_DRIVER
-/* This is called by AP Power Sequence driver only when AP exits S0 or S0IX */
-static void board_ap_power_cb(const struct device *dev,
-			      const enum ap_pwrseq_state entry,
-			      const enum ap_pwrseq_state exit)
-{
-	if (entry == AP_POWER_STATE_S0ix) {
-		/* Avoid enabling signals when entering S0IX */
-		return;
-	}
-}
-
-static int board_ap_power_init(void)
-{
-	const struct device *ap_pwrseq_dev = ap_pwrseq_get_instance();
-	static struct ap_pwrseq_state_callback exit_cb = {
-		.cb = board_ap_power_cb,
-		.states_bit_mask =
-			(BIT(AP_POWER_STATE_S0) | BIT(AP_POWER_STATE_S0ix)),
+	/* Wait RSMRST to be off. */
+	while (power_signal_get(PWR_RSMRST_PWRGD) && (timeout_ms > 0)) {
+		k_msleep(1);
+		timeout_ms--;
 	};
 
-	ap_pwrseq_register_state_exit_callback(ap_pwrseq_dev, &exit_cb);
+	if (power_signal_get(PWR_RSMRST_PWRGD))
+		LOG_WRN("RSMRST_PWRGD didn't go low!  Assuming G3.");
 
-	return 0;
+	k_msleep(BOARD_PTL_RVP_MINIMUM_POWER_DOWN_DELAY_MS);
 }
-SYS_INIT(board_ap_power_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
-#endif /* CONFIG_EMUL_AP_PWRSEQ_DRIVER */
 
-static int board_ap_power_g3_entry(void *data)
+int board_ap_power_action_g3_entry(void *data)
 {
 	board_ap_power_force_shutdown();
 
 	return 0;
 }
 
-static int board_ap_power_g3_run(void *data)
+static int board_ap_power_action_g3_run(void *data)
 {
 	if (ap_pwrseq_sm_is_event_set(data, AP_PWRSEQ_EVENT_POWER_STARTUP)) {
-		LOG_INF("Turning on PWR_EN_PP5000_A and PWR_EN_PP3300_A");
-
 		power_signal_set(PWR_EN_PP5000_A, 1);
-		power_signal_set(PWR_EN_PP3300_A, 1);
-
-		power_wait_signals_on_timeout(
-			AP_PWRSEQ_DT_VALUE(wait_signal_timeout));
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vsus_on_ec), 1);
+		update_ap_boot_time(ARAIL);
 	}
 
-	if (power_signal_get(PWR_EN_PP5000_A) &&
-	    power_signal_get(PWR_EN_PP3300_A)) {
+	/* Return 0 only if power rails have been enabled  */
+	return !power_signal_get(PWR_EN_PP5000_A);
+}
+
+AP_POWER_APP_STATE_DEFINE(G3, board_ap_power_action_g3_entry,
+			  board_ap_power_action_g3_run, NULL);
+
+static int board_ap_power_action_s4_run(void *data)
+{
+	/* Disable SUSB_EC# before goto S5 */
+	if (power_signal_get(PWR_RSMRST_PWRGD) == 0 ||
+	    power_signal_get(PWR_SLP_S5)) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_susb_ec), 0);
 		return 0;
 	}
 
-	return 1;
-}
+	/* Enable SUSB_EC# before goto S3 */
+	if (!power_signal_get(PWR_SLP_S4)) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_susb_ec), 1);
+		return 0;
+	}
 
-AP_POWER_APP_STATE_DEFINE(G3, board_ap_power_g3_entry, board_ap_power_g3_run,
-			  NULL);
-
-static int board_ap_power_s0_run(void *data)
-{
 	return 0;
 }
 
-AP_POWER_APP_STATE_DEFINE(S0, NULL, board_ap_power_s0_run, NULL);
-#endif /* CONFIG_AP_PWRSEQ_DRIVER */
+AP_POWER_APP_STATE_DEFINE(S4, NULL, board_ap_power_action_s4_run, NULL);
+
+static int board_ap_power_action_s3_run(void *data)
+{
+	/* Disable VR before goto S4 */
+	if (power_signal_get(PWR_RSMRST_PWRGD) == 0 ||
+	    power_signal_get(PWR_SLP_S4)) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_disable), 0);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_lcd_backoff), 0);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ssd_pln), 0);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vr_on), 0);
+
+		return 0;
+	}
+
+	/* Enable VR before goto S0 */
+	if (!power_wait_signals_on_timeout(
+		    POWER_SIGNAL_MASK(PWR_ALL_SYS_PWRGD),
+		    AP_PWRSEQ_DT_VALUE(all_sys_pwrgd_timeout))) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_vr_on), 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ssd_pln), 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_lcd_backoff), 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_disable), 1);
+
+		return 0;
+	}
+
+	return 0;
+}
+
+AP_POWER_APP_STATE_DEFINE(S3, NULL, board_ap_power_action_s3_run, NULL);
 
 int power_signal_external_init(void)
 {
@@ -165,68 +129,10 @@ int power_signal_external_init(void)
 
 int board_power_signal_get(enum power_signal signal)
 {
-	switch (signal) {
-	default:
-		LOG_ERR("Unknown signal for board get: %d", signal);
-		return -EINVAL;
-
-	case PWR_ALL_SYS_PWRGD:
-		/*
-		 * All system power is good.
-		 * Checks that PWR_SLP_S3 is off, and
-		 * the GPIO signal for all power good is set,
-		 * and that the 1.05 volt line is ready.
-		 */
-		if (power_signal_get(PWR_SLP_S3)) {
-			return 0;
-		}
-		if (!gpio_pin_get_dt(
-			    GPIO_DT_FROM_NODELABEL(gpio_all_sys_pwrgd))) {
-			return 0;
-		}
-		return 1;
-	}
+	return -EINVAL;
 }
 
 int board_power_signal_set(enum power_signal signal, int value)
 {
 	return -EINVAL;
 }
-
-/*
- * As a soft power signal, PWR_ALL_SYS_PWRGD will never wake the power state
- * machine on its own. Since its value depends on the state of
- * gpio_all_sys_pwrgd, wake the state machine to re-evaluate ALL_SYS_PWRGD
- * anytime the input changes.
- */
-void board_all_sys_pwrgd_interrupt(const struct device *unused_device,
-				   struct gpio_callback *unused_callback,
-				   gpio_port_pins_t unused_pin)
-{
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
-	ap_pwrseq_wake();
-#else
-	ap_pwrseq_post_event(ap_pwrseq_get_instance(),
-			     AP_PWRSEQ_EVENT_POWER_SIGNAL);
-#endif
-}
-
-static int board_config_pwrgd_interrupt(void)
-{
-	const struct gpio_dt_spec *const pwrgd_gpio =
-		GPIO_DT_FROM_NODELABEL(gpio_all_sys_pwrgd);
-	static struct gpio_callback cb;
-	int rv;
-
-	gpio_init_callback(&cb, board_all_sys_pwrgd_interrupt,
-			   BIT(pwrgd_gpio->pin));
-	gpio_add_callback(pwrgd_gpio->port, &cb);
-
-	rv = gpio_pin_interrupt_configure_dt(pwrgd_gpio, GPIO_INT_EDGE_BOTH);
-	__ASSERT(rv == 0,
-		 "all_sys_pwrgd interrupt configuration returned error %d", rv);
-
-	return 0;
-}
-SYS_INIT(board_config_pwrgd_interrupt, APPLICATION,
-	 CONFIG_APPLICATION_INIT_PRIORITY);
