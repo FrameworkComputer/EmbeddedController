ab291ad5c89b9b26e9759d7491f37a025262711c
Megha Verma <megha.verma@intel.corp-partner.google.com>
2025-09-04 15:35:53 +0530

    ocelot-ish: Enable DSP comms based sensor support
    Enable the DSP comms based ISH sensor support. This change
    is needed to establish EC and ISH connection through
    dsp comms protocol.
    
    BUG=b:428084925
    TEST=zmake build ocelotrvp-ish, append manifest
        Flash kernel and coreboot which support ISH, copy ISH main
        fw image to /lib/firmware/intel/ish/ish_wcl.bin and reboot.
        Verified ISH fw load in cpu console using below command.
        ~ # dmesg | grep ish
        output:
        intel_ish_ipc 0000:00:12.0: ISH loader: firmware loaded. size:170496
        cros_ec_ishtp {7B7154D0-56F4-4BDC-B0D8-9E7CDAE0D6A0}: Chrome EC
        device registered
    
    Change-Id: Ia610be711578d8dcab0a1c2a7f596e1fc5589ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6916894
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Tested-by: Megha Verma <megha.verma@intel.corp-partner.google.com>

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -156,7 +156,9 @@ register_ish_project(
     kconfig_files=[
         here / "ocelot-ish" / "prj.conf",
         here / "ocelot-ish" / "motionsense.conf",
+        here / "dsp_comms.conf",
     ],
+    modules=["ec", "cmsis", "cmsis_6", "hal_intel_public", "pigweed", "nanopb"],
 )
 
 # For realtek
