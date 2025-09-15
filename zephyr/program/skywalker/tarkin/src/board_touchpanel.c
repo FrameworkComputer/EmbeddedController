d1bf1d5acc69aefdbdfa9171a270b636c77c082c
zengyuxiang <zengyuxiang@huaqin.corp-partner.google.com>
2025-09-15 20:49:49 +0800

    tarkin: Controlling the timing of the touchpanel using EC
    Based on the timing diagram provided by the hardware team, use the EC's GPIO to control the power on/off and suspend timing of the touchpanel.
    
    BUG=b:445883866
    TEST=1.zmake build tarkin
    2.timing test meets spec requirements
    LOW_COVERAGE_REASON=Ztest CL will be submitted later:b:445883866
    
    Change-Id: I44a583473d98cdcbf511fd8a444ced37955ff47d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6949305
    Code-Coverage: Tony Chang <tonytcc@google.com>
    Tested-by: Yuxiang Zeng <zengyuxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Yuxiang Zeng <zengyuxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Tony Chang <tonytcc@google.com>
    Reviewed-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/program/skywalker/tarkin/src/board_touchpanel.c
@@ -0,0 +1,70 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "gpio/gpio_int.h"
+#include "gpio_signal.h"
+#include "hooks.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
+
+#include <ap_power/ap_power.h>
+
+static bool value_en;
+
+static void set_tp_vtsp_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_vtsp_en), value_en);
+}
+DECLARE_DEFERRED(set_tp_vtsp_en_pin);
+
+static void set_tp_rst_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_rst), value_en);
+}
+DECLARE_DEFERRED(set_tp_rst_pin);
+
+static void set_tp_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_en), value_en);
+}
+DECLARE_DEFERRED(set_tp_en_pin);
+
+/* Power-on */
+static void board_tp_startup(void)
+{
+	value_en = 1;
+	hook_call_deferred(&set_tp_vtsp_en_pin_data, 0);
+	hook_call_deferred(&set_tp_rst_pin_data, 20 * USEC_PER_MSEC);
+	hook_call_deferred(&set_tp_en_pin_data, 200 * USEC_PER_MSEC);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_tp_startup, HOOK_PRIO_DEFAULT);
+
+/* Power-off */
+static void board_tp_shutdown(void)
+{
+	value_en = 0;
+	hook_call_deferred(&set_tp_en_pin_data, 0);
+	hook_call_deferred(&set_tp_rst_pin_data, 35 * USEC_PER_MSEC);
+	hook_call_deferred(&set_tp_vtsp_en_pin_data, 70 * USEC_PER_MSEC);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_tp_shutdown, HOOK_PRIO_DEFAULT);
+
+/* Exit suspend */
+static void board_tp_resume(void)
+{
+	value_en = 1;
+	hook_call_deferred(&set_tp_en_pin_data, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_tp_resume, HOOK_PRIO_DEFAULT);
+
+/* Go to suspend */
+static void board_tp_suspend(void)
+{
+	value_en = 0;
+	hook_call_deferred(&set_tp_en_pin_data, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_tp_suspend, HOOK_PRIO_DEFAULT);
