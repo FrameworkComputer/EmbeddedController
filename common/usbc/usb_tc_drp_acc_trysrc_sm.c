56d3e701a476ef7b297a6d449db54ce3028daf4d
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2025-09-16 16:18:57 +0800

    TCPMv2: we shall not drive Vconn when partner is DebugAccessory
    According to Type-C spec v2.3,
    4.5.2.2.17.1 UnorientedDebugAccessory.SRC State Requirements &
    4.5.2.2.18.1 OrientedDebugAccessory.SRC State Requirements &
    4.5.2.2.19.1 DebugAccessory.SNK State Requirements,
    indicate that the port shall not drive VCONN.
    
    As SRC, we should also follow this rule when doing Hard Reset,
    so I add the condition before turning on Vconn.
    
    As SNK, tc_perform_snk_hard_reset() follows spec,
    which doesn't turn on Vconn when doing Hard Reset.
    
    BUG=b:406373457
    TEST=can pass firmware.PDResetHard.dts on Rull
    
    Change-Id: I155c131269a0ef36a8316b52bd09d0261dbb1420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6486330
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Code-Coverage: Keith Short <keithshort@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1361,8 +1361,19 @@ static bool tc_perform_src_hard_reset(int port)
 		/* Update the Rp Value */
 		typec_update_cc(port);
 
-		/* Turn off VCONN */
-		set_vconn(port, 1);
+		/*
+		 * Turn on VCONN
+		 *
+		 * USB Type-C spec v2.3,
+		 * 4.5.2.2.17.1 UnorientedDebugAccessory.SRC State Requirements
+		 * & 4.5.2.2.18.1 OrientedDebugAccessory.SRC State Requirements
+		 * & 4.5.2.2.19.1 DebugAccessory.SNK State Requirements,
+		 * indicate that the port shall not drive VCONN.
+		 */
+		if (IS_ENABLED(CONFIG_USBC_VCONN) &&
+		    !TC_CHK_FLAG(port, TC_FLAGS_TS_DTS_PARTNER)) {
+			set_vconn(port, 1);
+		}
 
 		tc[port].ps_reset_state = PS_STATE2;
 		pd_timer_enable(port, TC_TIMER_TIMEOUT,
