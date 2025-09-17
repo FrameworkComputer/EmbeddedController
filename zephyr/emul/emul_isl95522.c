128d22d1310e572d0c2451b54a9adf09db3520ad
Al Semjonovs <asemjonovs@google.com>
2025-09-17 10:19:40 -0600

    charge_state: Add test for is_adapter_sufficient
    Add tests for is_adapter_sufficient
    
    BUG=b:447287812
    TEST=./twister -cT zephyr/test/charger
    
    Change-Id: I15364b85d434c8993af797ff1b43b000d4bf072a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6961834
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/emul/emul_isl95522.c
@@ -0,0 +1,17 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charger/chg_isl95522.h"
+#include "emul/emul_common_i2c.h"
+#include "emul/emul_stub_device.h"
+#include "util.h"
+
+#include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+
+#define DT_DRV_COMPAT ISL95522_CHG_COMPAT
+
+DT_INST_FOREACH_STATUS_OKAY(EMUL_STUB_DEVICE);
---
+++ zephyr/test/charger/charge/src/charge_state.c
@@ -0,0 +1,71 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_manager.h"
+#include "charge_state.h"
+#include "charger.h"
+#include "charger_test.h"
+#include "drivers/ucsi_v3.h"
+#include "ec_commands.h"
+#include "emul/emul_pdc.h"
+#include "usbc/pdc_power_mgmt.h"
+
+#include <zephyr/fff.h>
+#include <zephyr/ztest.h>
+
+BUILD_ASSERT(!IS_ENABLED(CONFIG_PLATFORM_EC_OCPC),
+	     "This test suite does not support OCPC");
+
+test_export_static bool charge_is_adapter_sufficient(int chgnum);
+
+#define RTS5453P_NODE DT_NODELABEL(pdc_emul1)
+static const struct emul *emul = EMUL_DT_GET(RTS5453P_NODE);
+
+static void test_before(void *fixture)
+{
+	emul_pdc_disconnect(emul);
+	pdc_power_mgmt_wait_for_sync(0, -1);
+}
+
+ZTEST_SUITE(charge_state, charger_predicate_post_main, NULL, test_before, NULL,
+	    NULL);
+
+ZTEST(charge_state, test_sufficient_adapter)
+{
+	union connector_status_t connector_status = {};
+	int chgnum = 0;
+
+	charge_manager_leave_safe_mode();
+
+	/* Default best PDO is 20 volts */
+	emul_pdc_configure_snk(emul, &connector_status);
+	emul_pdc_connect_partner(emul, &connector_status);
+	pdc_power_mgmt_wait_for_sync(0, -1);
+
+	chgnum = charge_get_active_chg_chip();
+	zassert_true(charge_is_adapter_sufficient(chgnum));
+}
+
+ZTEST(charge_state, test_insufficient_adapter)
+{
+	union connector_status_t connector_status = {};
+	int chgnum = 0;
+	uint32_t partner_pdos[] = {
+		PDO_FIXED(5000, 3000, 0),
+		PDO_FIXED(7200, 3000, 0),
+		PDO_FIXED(12000, 3000, 0),
+	};
+
+	charge_manager_leave_safe_mode();
+
+	emul_pdc_configure_snk(emul, &connector_status);
+	emul_pdc_set_pdos(emul, SOURCE_PDO, PDO_OFFSET_0,
+			  ARRAY_SIZE(partner_pdos), PARTNER_PDO, partner_pdos);
+	emul_pdc_connect_partner(emul, &connector_status);
+	pdc_power_mgmt_wait_for_sync(0, -1);
+
+	chgnum = charge_get_active_chg_chip();
+	zassert_false(charge_is_adapter_sufficient(chgnum));
+}
