f066c056afb5d5b2c1a1c5c2ff09b5be2f6d951d
mike <mike5@huaqin.corp-partner.google.com>
2025-09-17 09:37:42 +0800

    sapphire: init zephyr board
    copy tanjiro for init sapphire
    
    BUG=b:452155620
    TEST=zmake build sapphire
    
    Change-Id: Ibd6a2eb6a1a0c2443b3d9a16026579b964a5dfae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6965164
    Reviewed-by: Knox Chiou <knoxchiou@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Tested-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/tanjiro/BUILD.py
+++ zephyr/program/tanjiro/BUILD.py
@@ -21,7 +21,9 @@ def register_tanjiro_project(project_name):
 
 
 register_tanjiro_project(project_name="tanjiro")
+register_tanjiro_project(project_name="sapphire")
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="tanjiro", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="sapphire", addr=0x60098)
