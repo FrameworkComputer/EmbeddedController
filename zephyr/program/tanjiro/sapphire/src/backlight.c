75c8d29e72a069271579eebc7f1516ec07858979
mike <mike5@huaqin.corp-partner.google.com>
2025-09-18 21:21:11 +0800

    sapphire: enable backlight control logic
    The backlight control logic follows Hylia for bring-up.
    
    BUG=b:452155620
    TEST=zmake build sapphire
    LOW_COVERAGE_REASON=Tests will be added in b:452155620
    
    Change-Id: I14498b18439a2f8438a37781016876213040ebaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6965166
    Commit-Queue: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Code-Coverage: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Tested-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

---
+++ zephyr/program/tanjiro/sapphire/src/backlight.c
@@ -0,0 +1,50 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "gpio/gpio_int.h"
+#include "gpio_signal.h"
+#include "hooks.h"
+#include "timer.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
+
+#include <ap_power/ap_power.h>
+
+static bool value_en;
+
+static void set_tp_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_en_od), value_en);
+}
+DECLARE_DEFERRED(set_tp_en_pin);
+
+static void set_bl_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_en_od), value_en);
+}
+DECLARE_DEFERRED(set_bl_en_pin);
+
+void ap_bl_en_interrupt(enum gpio_signal signal)
+{
+	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ap_bl_en_odl))) {
+		value_en = true;
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_en_od),
+				value_en);
+		hook_call_deferred(&set_tp_en_pin_data, 3 * USEC_PER_MSEC);
+	} else {
+		value_en = false;
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_en_od),
+				value_en);
+		hook_call_deferred(&set_bl_en_pin_data, 3 * USEC_PER_MSEC);
+	}
+}
+
+static void ap_bl_en_init(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_ap_bl_en));
+}
+DECLARE_HOOK(HOOK_INIT, ap_bl_en_init, HOOK_PRIO_DEFAULT);
