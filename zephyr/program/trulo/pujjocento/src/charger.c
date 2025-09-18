b52cfc052221d40fb47c1a82929bd3cf6c987667
guojun <guojun@huaqin.corp-partner.google.com>
2025-09-18 17:21:45 +0800

    pujjocento: Modify charge IDPM via batt_pres_interrupt
    1. Disable charger IDPM function when AC only to prevent boot fail.
    2. Enable charger IDPM when insert battery, disable charger IDPM when removal battery.
    
    BUG=b:435604546
    TEST=1. check the IDPM bit is 0 when AC only
         2. check the IDPM bit is 1 when insert battery
         3. check the IDPM bit is 0 when removal battery
    LOW_COVERAGE_REASON=Tests will be added in b:435604546
    
    Change-Id: I85a2cba61d69ca232421495202bf903e38f9d7db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6964653
    Commit-Queue: guojun guojun <guojun@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: guojun guojun <guojun@huaqin.corp-partner.google.com>
    Reviewed-by: David Cross <davidmcross@google.com>
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- zephyr/program/trulo/pujjocento/src/charger.c
+++ zephyr/program/trulo/pujjocento/src/charger.c
@@ -4,6 +4,9 @@
  */
 
 #include "charger.h"
+#include "common.h"
+#include "gpio/gpio_int.h"
+#include "gpio_signal.h"
 #include "hooks.h"
 #include "i2c.h"
 #include "system.h"
@@ -37,11 +40,31 @@ static void set_bq25710_charge_option(void)
 
 	rv = i2c_read16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
 			BQ25710_REG_CHARGE_OPTION_0, &reg);
-	/* Disable IDPM when AC only */
-	if (rv == EC_SUCCESS && !battery_is_present()) {
-		reg = SET_BQ_FIELD(BQ257X0, CHARGE_OPTION_0, EN_IDPM, 0, reg);
+	if (rv == EC_SUCCESS) {
+		/* if AC only, disable IDPM,
+		 * because it will cause charger keep asserting PROCHOT
+		 */
+		if (gpio_pin_get_dt(
+			    GPIO_DT_FROM_NODELABEL(gpio_ec_batt_pres_odl)))
+			reg = SET_BQ_FIELD(BQ257X0, CHARGE_OPTION_0, EN_IDPM, 0,
+					   reg);
+		else
+			reg = SET_BQ_FIELD(BQ257X0, CHARGE_OPTION_0, EN_IDPM, 1,
+					   reg);
 		i2c_write16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
 			    BQ25710_REG_CHARGE_OPTION_0, reg);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, set_bq25710_charge_option, HOOK_PRIO_DEFAULT);
+DECLARE_DEFERRED(set_bq25710_charge_option);
+
+void batt_pres_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&set_bq25710_charge_option_data, 0);
+}
+
+static void batt_pres_en_init(void)
+{
+	hook_call_deferred(&set_bq25710_charge_option_data, 0);
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_batt_pres_en));
+}
+DECLARE_HOOK(HOOK_INIT, batt_pres_en_init, HOOK_PRIO_DEFAULT);
