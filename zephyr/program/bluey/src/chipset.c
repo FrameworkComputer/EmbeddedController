ff3a3ef05fb176dacb484668a93786b5dc74e635
Jayvik Desai <jayvik@google.com>
2025-09-22 16:48:41 +0530

    program/bluey: enable 5v FAN power-rail for bluey and quartz
    This patch enables FAN power-rail on chipset startup and shutdown for
    bluey and quartz project.
    
    BUG=b:443679498
    TEST=zmake build %bluey
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    
    Change-Id: I5981c2b26fb5b6034a04ac3b37e1e76bd97cea3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6973397
    Commit-Queue: Wai-Hong Tam <waihong@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>

--- zephyr/program/bluey/src/chipset.c
+++ zephyr/program/bluey/src/chipset.c
@@ -28,12 +28,14 @@ void board_chipset_startup(void)
 {
 #if defined(CONFIG_BOARD_BLUEY)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_off_odl), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 1);
 #endif
 
 #if defined(CONFIG_BOARD_QUARTZ)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_haptic_en_ec), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 1);
 #else
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_usb_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 1);
@@ -50,12 +52,14 @@ void board_chipset_shutdown(void)
 {
 #if defined(CONFIG_BOARD_BLUEY)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_off_odl), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 0);
 #endif
 
 #if defined(CONFIG_BOARD_QUARTZ)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_haptic_en_ec), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 0);
 #else
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_usb_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 0);
