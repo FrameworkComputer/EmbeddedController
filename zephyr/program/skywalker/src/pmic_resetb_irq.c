d104850a5b3870c8912c05865f5655534b5951e8
Ting Shen <phoenixshen@google.com>
2025-09-25 14:44:19 +0800

    skywalker: support pmic_resetb forwarding
    On Skywalker hardware v1.4 and newer, the PMIC_RESETB signal is wired
    to the EC. The EC must now forward this signal to EN_PWR_S3 to ensure
    correct power sequencing.
    
    This change adds a Kconfig option (`CONFIG_BOARD_SKYWALKER_PMIC_RESETB_IRQ`)
    to enable an interrupt handler that mirrors the PMIC_RESETB signal to
    the EN_PWR_S3 GPIO. This is enabled by default on boards that have the
    `gpio_en_pwr_s3` node defined in their device tree.
    
    BUG=b:373807923
    TEST=Build skywalker with following DT change:
    	named-gpios {
    		gpio_en_pwr_s3: en_pwr_s3 {
    			gpios = <&gpioc 7 GPIO_OUTPUT>;
    		};
    	};
    
    	gpio-interrupts {
    		pmic-ec-resetb {
    			handler = "pmic_ec_resetb_interrupt";
    		};
    	};
    
    Change-Id: I92dc15052171b659c98d8aa0be88a6b050bd915f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6982557
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

---
+++ zephyr/program/skywalker/src/pmic_resetb_irq.c
@@ -0,0 +1,16 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gpio.h"
+#include "power.h"
+
+#include <zephyr/drivers/gpio.h>
+
+void pmic_ec_resetb_interrupt(enum gpio_signal signal)
+{
+	power_signal_interrupt(signal);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pwr_s3),
+			gpio_get_level(signal));
+}
