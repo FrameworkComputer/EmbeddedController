44074a391ffdf7b1d8ab95b071911334174e9bbc
Jayvik Desai <jayvik@google.com>
2025-09-27 01:50:06 +0530

    bluey: Decouple 5V rail control from the fan driver for quenbi project
    The fan driver was incorrectly configured to control `ec_en_pp5000`, the
    GPIO that enables the board's +5V power rail. This rail is shared with
    the USB-A port.
    
    As a result, commands like `fanduty 0 0` would disable the fan and
    inadvertently cut power to the USB-A port, causing it to fail.
    
    This change fixes the issue by removing the `enable-gpio` property from
    the fan's device tree node.
    
    Additionally, this patch refactors the rail's startup control. The
    previous cros ec shimmed implementation for enabling the 5V rail was
    not being triggered. That legacy method is now removed, and the rail
    is instead enabled manually during chipset power hooks to guarantee its
    availability.
    
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    BUG=none
    TEST=zmake build %bluey --clobber
    
    w/o this patch:
    
     ec:> fanduty 0 0
          setting fan 0 duty cycle to 0%
     ec:> fanduty 1 0
          setting fan 1 duty cycle to 0%
     ec:> gpioget ec_en_pp5000
          0  ec_en_pp5000
    
    w/ this patch:
     ec:> fanduty 0 0
          setting fan 0 duty cycle to 0%
     ec:> fanduty 1 0
          setting fan 1 duty cycle to 0%
     ec:> gpioget ec_en_pp5000
          1*  ec_en_pp5000
    
    Change-Id: Ibfd5967a394d702c8b643bee70d3e5d58b50573b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6991224
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-by: Kornel Dulęba <korneld@google.com>
    Commit-Queue: Jayvik Desai <jayvik@google.com>

--- zephyr/program/bluey/src/chipset.c
+++ zephyr/program/bluey/src/chipset.c
@@ -42,7 +42,7 @@ void board_chipset_startup(void)
 #endif
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 1);
-
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 1);
 	/* Update the AC event during boot */
 	extpower_handle_update(gpio_get_level(GPIO_AC_PRESENT));
 }
@@ -66,6 +66,7 @@ void board_chipset_shutdown(void)
 #endif
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
 
