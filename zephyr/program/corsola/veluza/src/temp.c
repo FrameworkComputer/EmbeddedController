f4131714773f1fa7e5c5174909e648cb0b3a5ede
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2025-10-01 14:20:30 +0800

    veluza: Add read temperature condition
    Make sure the ADC is ready before executing get_temperature.
    
    BUG=b:441058563
    TEST=zmake build success
    LOW_COVERAGE_REASON=use b:344720576 tracking test code
    
    Change-Id: Ib94951e2cac88586fc5e15f1d2626309542d0726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6999118
    Tested-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/corsola/veluza/src/temp.c
+++ zephyr/program/corsola/veluza/src/temp.c
@@ -12,6 +12,7 @@
 
 #define TEMP_BUFF_SIZE 60
 #define KEEP_TIME 5
+#define TEMP_NOT_READY -1
 
 BUILD_ASSERT(IS_ENABLED(CONFIG_BOARD_VELUZA) || IS_ENABLED(CONFIG_TEST));
 
@@ -24,8 +25,12 @@ static int average_tempature(void)
 	static int past_temp;
 	static int avg_temp;
 	int cur_temp, t;
+	int rv;
 
-	temp_sensor_read(TEMP_SENSOR_ID(DT_NODELABEL(temp_charger)), &t);
+	rv = temp_sensor_read(TEMP_SENSOR_ID(DT_NODELABEL(temp_charger)), &t);
+	if (rv)
+		return TEMP_NOT_READY;
+
 	cur_temp = K_TO_C(t);
 	past_temp = temp_history_buffer[buff_ptr];
 	temp_history_buffer[buff_ptr] = cur_temp;
@@ -70,6 +75,8 @@ static void current_update(void)
 	static uint8_t dntime;
 
 	temp = average_tempature();
+	if (temp == TEMP_NOT_READY)
+		return;
 #ifndef CONFIG_TEST
 	if (led_pwr_get_state() == LED_PWRS_DISCHARGE) {
 		current_level = 0;
