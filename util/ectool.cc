fc849ddb25f7b0822709030160d4c07fe5e06f15
Firas Sammoura <fsammoura@google.com>
2025-10-02 18:05:04 +0000

    ectool: Add capture type argument to fpframe
    The `fpframe` command requires knowledge of the capture type to
    correctly interpret the frame data, as different types (e.g., vendor,
    pattern0, qual) can have different sensor image metadata, including
    frame sizes.
    
    Previously, `ectool` attempted to query the current mode from the EC.
    However, the EC firmware clears the transient capture mode immediately
    after image acquisition. This makes it unreliable for a subsequent
    `fpframe` invocation to determine the capture type used for the stored
    frame.
    
    This change enables the user to explicitly specify the expected capture
    type as an argument to the `fpframe` command. This ensures the correct
    metadata is used when downloading the frame, regardless of the EC's
    current mode state.
    
    The following capture type arguments are now supported:
      - vendor
      - pattern0
      - pattern1
      - qual
      - test_reset
      - test_defect_pixel
      - test_abnormal
      - test_noise
    
    If no type is specified, 'vendor' is assumed. The 'raw' argument can
    still be used in conjunction with a capture type.
    
    BUG=b:398899644
    TEST=emerge-rex ec-utils
    TEST=fpframe --> idx = FP_FRAME_INDEX_SIMPLE_IMAGE &&
            capture_type = FP_CAPTURE_VENDOR_FORMAT
    TEST=fpframe raw --> idx = FP_FRAME_INDEX_RAW_IMAGE
                capture_type = FP_CAPTURE_VENDOR_FORMAT
    TEST=fpframe pattern1 --> idx = FP_FRAME_INDEX_SIMPLE_IMAGE
                     capture_type = FP_CAPTURE_PATTERN1
    TEST=fpframe raw test_reset --> idx = FP_FRAME_INDEX_RAW_IMAGE
                           capture_type = FP_CAPTURE_RESET_TEST
    TEST=Emerge the `ec-utils` with the current change and cros deploy on a
         device. Capture a `test_reset` image by running:
         ```
         ectool --name= cros_fp waitevent FINGERPRINT 500 fpmode capture
         test_reset.
         ```
         Download the image using:
         `ectool --name=cros_fp fpframe > /tmp/test_reset.pnm`
         Check that the capture and download are successful. Repeat for
         `pattern0`, `pattern1`, and `qual`. For `qual` download the image
         as `raw`.
    
    Change-Id: I550937b037c9c437684f7cdf7bbf875b78d041f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7006302
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: ChromeOS Auto Runner <chromeos-auto-runner@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- util/ectool.cc
+++ util/ectool.cc
@@ -57,6 +57,7 @@
 #include <libec/mkbp_event.h>
 #include <libec/rand_num_command.h>
 #include <libec/versions_command.h>
+#include <map>
 #include <memory>
 #include <optional>
 #include <string>
@@ -1913,7 +1914,8 @@ int cmd_apreset(int argc, char *argv[])
  */
 static std::unique_ptr<std::vector<uint8_t> >
 fp_download_frame(struct SensorImage &sensor_image,
-		  struct TemplateInfo &template_info, int index)
+		  struct TemplateInfo &template_info, int index,
+		  uint8_t capture_type = FP_CAPTURE_VENDOR_FORMAT)
 {
 	ec::EcCommandFactory ec_command_factory;
 	ec::EcCommandVersionSupported ec_cmd_ver_supported;
@@ -1930,19 +1932,10 @@ fp_download_frame(struct SensorImage &sensor_image,
 	if (images.size() == 1) {
 		sensor_image = images[0];
 	} else {
-		ec::FpModeCommand fp_mode_command(
-			(ec::FpMode(ec::FpMode::Mode::kDontChange)));
-		if (!fp_mode_command.Run(comm_get_fd())) {
-			fprintf(stderr, "Failed to Run FpModeCommand.\n");
-			return nullptr;
-		}
-
 		bool found = false;
-		uint8_t current_fp_capture_type =
-			FP_CAPTURE_TYPE(fp_mode_command.Mode().RawVal());
 		for (const auto &image : fp_info_command->sensor_image())
 			if (image.fp_capture_type.has_value() &&
-			    *image.fp_capture_type == current_fp_capture_type) {
+			    *image.fp_capture_type == capture_type) {
 				sensor_image = image;
 				found = true;
 				break;
@@ -1954,6 +1947,10 @@ fp_download_frame(struct SensorImage &sensor_image,
 		}
 	}
 
+	/*
+	 * TODO(b/450381837): Remove raw/simple parameters and use fp_info
+	 * version 2 for frame size.
+	 */
 	size_t size;
 	if (index == FP_FRAME_INDEX_SIMPLE_IMAGE) {
 		size = (size_t)sensor_image.width * sensor_image.bpp / 8 *
@@ -2222,10 +2219,48 @@ int cmd_fp_frame(int argc, char *argv[])
 {
 	struct SensorImage sensor_image{};
 	struct TemplateInfo template_info{};
-	int idx = (argc == 2 && !strcasecmp(argv[1], "raw")) ?
-			  FP_FRAME_INDEX_RAW_IMAGE :
-			  FP_FRAME_INDEX_SIMPLE_IMAGE;
-	auto fp_frame = fp_download_frame(sensor_image, template_info, idx);
+
+	int idx = FP_FRAME_INDEX_SIMPLE_IMAGE;
+	uint8_t capture_type = FP_CAPTURE_VENDOR_FORMAT;
+	bool capture_type_arg_seen = false;
+
+	static const std::map<std::string, uint8_t> capture_type_map = {
+		{ "vendor", FP_CAPTURE_VENDOR_FORMAT },
+		{ "pattern0", FP_CAPTURE_PATTERN0 },
+		{ "pattern1", FP_CAPTURE_PATTERN1 },
+		{ "qual", FP_CAPTURE_QUALITY_TEST },
+		{ "test_reset", FP_CAPTURE_RESET_TEST },
+		{ "test_defect_pixel", FP_CAPTURE_DEFECT_PXL_TEST },
+		{ "test_abnormal", FP_CAPTURE_ABNORMAL_TEST },
+		{ "test_noise", FP_CAPTURE_NOISE_TEST }
+	};
+
+	for (int i = 1; i < argc; i++) {
+		std::string arg = argv[i];
+		if (arg == "raw") {
+			idx = FP_FRAME_INDEX_RAW_IMAGE;
+		} else {
+			auto it = capture_type_map.find(arg);
+			if (it != capture_type_map.end()) {
+				if (capture_type_arg_seen) {
+					fprintf(stderr,
+						"Error: Multiple capture types specified ('%s'). Please specify"
+						" only one.\n ",
+						arg.c_str());
+					return -1;
+				}
+				capture_type = it->second;
+				capture_type_arg_seen = true;
+			} else {
+				fprintf(stderr,
+					"Warning: Ignoring unknown argument '%s'\n",
+					arg.c_str());
+			}
+		}
+	}
+
+	auto fp_frame = fp_download_frame(sensor_image, template_info, idx,
+					  capture_type);
 	if (!fp_frame) {
 		fprintf(stderr, "Failed to get FP sensor frame\n");
 		return -1;
