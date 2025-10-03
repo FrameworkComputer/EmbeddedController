d04df9dbdeb0d487fc644ce26239df8a106cc01b
Tom Hughes <tomhughes@chromium.org>
2025-10-03 12:16:51 -0700

    zephyr: cmake: compiler: clang: Fix rt lib override for RISC-V
    Upstream Zephyr was changed to support using a custom function for
    overriding linker properties.
    
    BUG=b:407786163, b:407560601
    TEST=zmake build --clobber et171
    
    Change-Id: I411648b108fbdb5969717c43a79161a41befbab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7007076
    Auto-Submit: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Reviewed-by: Dawid Niedźwiecki <dawidn@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>

--- zephyr/cmake/compiler/clang/target.cmake
+++ zephyr/cmake/compiler/clang/target.cmake
@@ -7,9 +7,24 @@ include("${ZEPHYR_BASE}/cmake/compiler/clang/target.cmake")
 # TODO(b/407786163): Fix the path reported by "--print-libgcc-file-name" so that
 # this is not necessary.
 if("${ARCH}" STREQUAL "riscv")
-  cmake_path(SET RTLIB_DIR NORMALIZE "${RTLIB_DIR}/../baremetal")
-  set_property(TARGET linker PROPERTY lib_include_dir "-L${RTLIB_DIR}")
-  set_property(TARGET linker PROPERTY rt_library "-lclang_rt.builtins-riscv32")
+  # Provide a custom "compiler_set_linker_properties" that overrides the
+  # default in upstream Zephyr (cmake/compiler/target_template.cmake).
+  function(compiler_set_linker_properties)
+    execute_process(
+      COMMAND ${CMAKE_C_COMPILER}
+      ${target_flag}
+      --print-libgcc-file-name
+      OUTPUT_VARIABLE libgcc_file_name
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+
+    # libgcc_file_name is something like:
+    # /usr/lib64/clang/21/lib/riscv32-cros-unknown-elf/libclang_rt.builtins.a
+    cmake_path(SET rtlib_dir NORMALIZE "${libgcc_file_name}/../../baremetal")
+
+    set_linker_property(PROPERTY lib_include_dir "-L${rtlib_dir}")
+    set_linker_property(PROPERTY rt_library "-lclang_rt.builtins-riscv32")
+  endfunction()
 endif()
 
 set(CMAKE_C_COMPILER "${CROSS_COMPILE}clang")
