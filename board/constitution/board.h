b32bfa5a1202d78ef7e395e259c603f44741362c
Marek Maslanka <mmaslanka@google.com>
2025-10-06 03:29:57 +0000

    Constitution: Enable EC-based watchdog
    BUG=b:422991116
    TEST=run: make build BOARD=constitution
              tast run <ip> crash.WatchdogCrash
    
    Change-Id: Ia42c940affb5ec6ca87aeb3f0b7aaef58ea555f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7008331
    Auto-Submit: Marek Maślanka <mmaslanka@google.com>
    Tested-by: Marek Maślanka <mmaslanka@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Marek Maślanka <mmaslanka@google.com>
    Reviewed-by: Jan Dąbroś <dabros@google.com>

--- board/constitution/board.h
+++ board/constitution/board.h
@@ -140,6 +140,9 @@
 /* Include math_util for bitmask_uint64 used in pd_timers */
 #define CONFIG_MATH_UTIL
 
+/* Enable watchdog */
+#define CONFIG_AP_HANG_DETECT
+
 #ifndef __ASSEMBLER__
 
 #include "gpio_signal.h" /* needed by registers.h */
