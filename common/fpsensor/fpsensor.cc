b1cdf70e38f9d49f936c552f24b3ad62bc46f1cb
Firas Sammoura <fsammoura@google.com>
2025-10-10 18:20:27 +0000

    common/fpsensor: Bound FP_FRAME read by current frame size
    The FP_FRAME host command allows reading fingerprint frame data.
    Previously, the size and offset were validated against the total
    buffer size. However, different capture modes can result in frames
    smaller than the maximum buffer capacity.
    
    This change modifies the validation to check the requested size and
    offset against the *actual* size of the last captured frame, stored
    in `global_context.current_frame_size`. This prevents the command
    from returning uninitialized data from the buffer beyond the valid
    frame.
    
    An additional check ensures `current_frame_size` does not exceed
    the total buffer size.
    
    BUG=b:450383638
    TEST=Manually test on device by requesting frame data after different
         capture modes. Verify correct sizes are returned and invalid size
         requests are rejected.
    LOW_COVERAGE_REASON=b:450383638, test will be added later
    
    Change-Id: I2115e3ce8bf84a21450a9aa0e7c53c88c2b621d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7032753
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -466,8 +466,12 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		if (skip_image_offset(global_context.sensor_mode))
 			offset += FP_SENSOR_IMAGE_OFFSET;
 
-		ret = validate_fp_buffer_offset(sizeof(fp_buffer), offset,
-						size);
+		if (global_context.current_frame_size > sizeof(fp_buffer)) {
+			return EC_RES_INVALID_PARAM;
+		}
+
+		ret = validate_fp_buffer_offset(
+			global_context.current_frame_size, offset, size);
 		if (ret != EC_SUCCESS)
 			return EC_RES_INVALID_PARAM;
 
