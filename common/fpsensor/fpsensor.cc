2f79e53add28e81d0d93f01a465a02c99141e599
Firas Sammoura <fsammoura@google.com>
2025-10-13 19:15:39 +0000

    fpsensor: Use fixed size for FP_INFO v2 response
    Modify the EC_CMD_FP_INFO v2 handler to return a response with a
    fixed size. The size is calculated based on the maximum possible
    number of capture types (FP_MAX_CAPTURE_TYPES), rather than the
    actual number present for a given sensor.
    
    This change ensures a predictable response size for the host, which
    is expected by host-side libraries such as libec.
    
    The response buffer is now zero-initialized to avoid returning any
    uninitialized memory. A check is also added to ensure the host-provided
    buffer is large enough, returning EC_RES_OVERFLOW if it's not.
    
    BUG=b:445178465
    TEST=ectool --name=cros_fp fpinfo -> succeeds with fpinfo v2 firmware
    LOW_COVERAGE_REASON=b:398899644, test is done on hardware
    
    Change-Id: I3364e2a87eb81ee6f2b15ef48db1fc22fb374bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7038551
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -391,9 +391,23 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 {
 	struct ec_response_fp_info_v2 *r =
 		static_cast<ec_response_fp_info_v2 *>(args->response);
+	const size_t response_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		FP_MAX_CAPTURE_TYPES * sizeof(struct fp_image_frame_params);
+
+	if (response_size > args->response_max) {
+		return EC_RES_OVERFLOW;
+	}
+
+	/*
+	 * The sensor may have fewer than FP_MAX_CAPTURE_TYPES number of
+	 * captures, in which case the buffer will only be partially written. We
+	 * want to make sure the rest of the buffer does not leak any data.
+	 */
+	memset(r, 0, response_size);
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
-	if (fp_sensor_get_info_v2(r, args->response_max) < 0)
+	if (fp_sensor_get_info_v2(r, response_size) < 0)
 #endif
 		return EC_RES_UNAVAILABLE;
 
@@ -403,9 +417,7 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 	r->template_info.template_dirty = global_context.templ_dirty;
 	r->template_info.template_version = FP_TEMPLATE_FORMAT_VERSION;
 
-	args->response_size = sizeof(struct ec_response_fp_info_v2) +
-			      (r->sensor_info.num_capture_types) *
-				      sizeof(struct fp_image_frame_params);
+	args->response_size = response_size;
 
 	return EC_RES_SUCCESS;
 }
