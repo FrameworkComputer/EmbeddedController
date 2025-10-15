804c49b2a4e20839e302f8f98ebf5e14963d8231
Ting Shen <phoenixshen@google.com>
2025-10-15 18:20:08 +0800

    mt8186: disable WDT
    On Skywalker, the WDT is managed by the PMIC, not the EC.
    
    This patch changes the behavior of EC's watchdog interrupt handler
    to simply log that the interrupt occurred for debugging purposes
    instead of taking any restorative action.
    
    BUG=b:433636690
    TEST=none
    
    Change-Id: Ic0d47a1d0afe426ec247ceb3641179b3e05a87e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7043127
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Tested-by: Ting Shen <phoenixshen@chromium.org>

--- zephyr/program/skywalker/src/hooks.c
+++ zephyr/program/skywalker/src/hooks.c
@@ -18,6 +18,10 @@
 #include "driver/charger/bq257x0_regs.h"
 #endif
 
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(skywalker_hook, LOG_LEVEL_INF);
+
 static void skywalker_common_init(void)
 {
 	gpio_enable_dt_interrupt(
@@ -78,3 +82,8 @@ void update_bq25720_input_voltage(void)
 DECLARE_HOOK(HOOK_AC_CHANGE, update_bq25720_input_voltage, HOOK_PRIO_DEFAULT);
 DECLARE_HOOK(HOOK_INIT, update_bq25720_input_voltage, HOOK_PRIO_DEFAULT);
 #endif
+
+void null_watchdog_interrupt(enum gpio_signal signal)
+{
+	LOG_INF("watchdog interrupt");
+}
