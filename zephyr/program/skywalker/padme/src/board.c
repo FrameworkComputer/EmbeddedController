30e12cfc7f5aae193c2c3b3ec9071e65eba542b1
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-10-20 14:23:16 +0800

    padme: Add rt9490 ADC control
    Based on power requirements, we will add rt9490 ADC control.
    
    BUG=b:443883932
    TEST=zmake build padme
    LOW_COVERAGE_REASON=Tests will be added in b:443883932
    
    Change-Id: Ia7bc316edacee9aa980075921dc0d09d87f33e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7061570
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Commit-Queue: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

--- zephyr/program/skywalker/padme/src/board.c
+++ zephyr/program/skywalker/padme/src/board.c
@@ -6,6 +6,7 @@
 #include "charger.h"
 #include "chipset.h"
 #include "common.h"
+#include "driver/charger/rt9490.h"
 #include "extpower.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
@@ -76,6 +77,18 @@ static int install_backlight_handler(void)
 }
 
 SYS_INIT(install_backlight_handler, APPLICATION, 1);
+
+__overridable void board_rt9490_adc_control(void)
+{
+	rt9490_enable_adc(CHARGER_SOLO, extpower_is_present());
+}
+
+static void board_hook_ac_change(void)
+{
+	board_rt9490_adc_control();
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, board_hook_ac_change, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_hook_ac_change, HOOK_PRIO_LAST);
 
 static void check_audio_jack(void)
 {
