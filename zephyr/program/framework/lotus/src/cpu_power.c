592ce6baa431ef52be07cdaa256292c72149a7d1
johnwc_yeh <JohnWC_Yeh@compal.com>
2025-10-20 17:29:04 +0800

    fwk: tulip: assert GPU_PWR_LEVEL low only for AC-only mode
    Previously, the EC always pulled it low on force_update,
    causing NV Diag to fail in shell mode.
    Now GPIO is pulled low only under AC-only condition.
    0x3E07 (pull low)-> enter shell mode -> run NV diag (keep low).
    
    BRANCH=fwk-tulip-29169
    BUG=NVDiag Test Fail_Error Code = 000000000818
    TEST= 1. AC only(100w/180w/240) can power on
    2. Run the NVDiag Test not show the error code
    
    Signed-off-by: johnwc_yeh <JohnWC_Yeh@compal.com>
    (cherry picked from commit 283a54955b34ba1c7c27602ca7e3d08ab849ee1e)

--- zephyr/program/framework/lotus/src/cpu_power.c
+++ zephyr/program/framework/lotus/src/cpu_power.c
@@ -475,7 +475,7 @@ void update_soc_power_limit(bool force_update, bool force_no_adapter)
 		set_gpu_gpio(GPIO_FUNC_ACDC, 1);
 #endif
 
-	if (force_update)
+	if (force_update && !with_dc)
 		set_nv_gpu_throttle(THROTTLE_ON, 0);
 
 	if (mode_ctl)
