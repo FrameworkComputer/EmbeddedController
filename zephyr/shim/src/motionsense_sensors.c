24da026f1a65b22fa4aae45255117eddcb90c90e
Gwendal Grignou <gwendal@google.com>
2025-10-22 16:12:55 -0700

    motion_sense: Set Sensor IRQ even when sensors are disabled
    When some sensors in motion_sensors are disabled, still set the
    interrupt for all of them:
    For instance for uldrenite180: only the lid accel remains.
    Without this change the ISH would assert.
    After the sensor is initialized.
    
    Fixes: f73ddfea6418 ("motion_sense: Skip IRQ init if no sensors")
    
    BUG=b:410646711,b:435407060
    TEST=flash uldrenite and uldrenite-ish, check that sensors are working
    
    Change-Id: I1c158c18d305c10cb18228cb5d76234778c278d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7076901
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/shim/src/motionsense_sensors.c
+++ zephyr/shim/src/motionsense_sensors.c
@@ -400,16 +400,15 @@ BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 static void sensor_enable_irqs(void)
 {
 	if (motion_sensor_count == 0) {
-		/* Don't enable interrupts if there are no sensors. This is not
-		 * an optimal solution, it's possible that some sensors will be
-		 * disabled while others aren't. For now, we will use this
-		 * all-or-nothing approach which will be replaced eventually by
-		 * the Zephyr upstream drivers.
-		 */
+		/* Don't enable interrupts if there are no sensors. */
 		return;
 	}
-	__ASSERT_NO_MSG(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs) <=
-			motion_sensor_count);
+	/*
+	 * Note we may enable interrupt for sensors that are not present,
+	 * if the number of sensor is reduced.
+	 * This is not ideal but the related GPIO line is forced to HIGH
+	 * later in sensors.c, preventing it from floating.
+	 */
 	LISTIFY(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs),
 		SENSOR_GPIO_ENABLE_INTERRUPT, (), SENSOR_INFO_NODE)
 }
