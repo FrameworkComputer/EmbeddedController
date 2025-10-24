1638f56a7b4490748c14d898b881fa0650692619
Fabio Baltieri <fabiobaltieri@google.com>
2025-10-24 16:16:30 +0000

    common: queue: drop the MIN -> min Zephyr override
    The driver now uses min() in the code itself and this is no longer
    needed.
    
    BUG=none
    TEST=cq dry run
    
    Change-Id: I1c3463de1db9513531cf442503e39e6d0480b8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7080811
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>

--- common/queue.c
+++ common/queue.c
@@ -10,14 +10,6 @@
 #include "queue.h"
 #include "util.h"
 
-#ifdef CONFIG_ZEPHYR
-
-/* Use the single call variant for MIN in this file. */
-#undef MIN
-#define MIN min
-
-#endif
-
 #define CPRINTS(format, args...) cprints(CC_MOTION_SENSE, format, ##args)
 
 static void queue_action_null(struct queue_policy const *policy, size_t count)
