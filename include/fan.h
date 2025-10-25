f86309446f0d7441cf1de36579dc6b60e6a97d70
Jayvik Desai <jayvik@google.com>
2025-10-25 21:07:01 +0530

    fan: Expose fan_smart_control in header
    Add the function prototype for fan_smart_control() to fan.h.
    
    This allows platform-specific code using
    CONFIG_PLATFORM_EC_CUSTOM_FAN_DUTY_CONTROL to explicitly
    call fan_smart_control(), for example, to return to
    default smart fan behavior after custom control is finished.
    
    BUG=b:440120697
    TEST=none
    
    Change-Id: I5e3870c89e93ae1ebd3478066809da7c6ab01745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7079137
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- include/fan.h
+++ include/fan.h
@@ -262,6 +262,16 @@ extern struct fan_data fan_data[];
  * Return       Fan status (see fan_status enum definition)
  */
 enum fan_status board_override_fan_control_duty(int ch);
+
+/**
+ * Smart fan control function.
+ *
+ * The function sets the pwm duty to reach the target rpm
+ *
+ * @param ch    Fan number (index into fan_data[] and fans[])
+ * Return       Fan status (see fan_status enum definition)
+ */
+enum fan_status fan_smart_control(int ch);
 #endif
 
 #ifdef __cplusplus
