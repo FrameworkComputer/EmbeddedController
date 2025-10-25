6acec9e922435f9e8d1c6b9fb075f918bf0da9cb
Jayvik Desai <jayvik@google.com>
2025-10-25 15:02:50 +0530

    quartz: Relocate project-specific power-rail configuration
    The settings for the 'quartz' project are being moved from the common
    'bluey' program to the 'quartz' project itself. This clean-up ensures
    that project-specific configurations are not co-located in common
    modules, improving modularity and code readability.
    
    BUG=none
    TEST=zmake build quartz
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    
    Change-Id: I0b3712c4847eb175415f2a4c5df7aa017fde76cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7079564
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>

---
+++ zephyr/program/bluey/quartz/src/chipset.c
@@ -0,0 +1,36 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Quartz chipset-specific configuration */
+
+#include "common.h"
+#include "gpio.h"
+#include "hooks.h"
+
+void board_chipset_startup_quartz(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_haptic_en_ec), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_enavdd_oled), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_s5), 1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup_quartz,
+	     HOOK_PRIO_DEFAULT);
+
+void board_chipset_shutdown_quartz(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_haptic_en_ec), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_enavdd_oled), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_s5), 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown_quartz,
+	     HOOK_PRIO_DEFAULT);
