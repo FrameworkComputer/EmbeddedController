3ae8fe07c29fc4da64d38e40e211a20f6a10508e
Jayvik Desai <jayvik@google.com>
2025-10-25 16:10:01 +0530

    quenbi: Defer HDMI enable and PP5000 disable
    This patch modifies the chipset startup and shutdown routines to
    introduce deferred calls for enabling HDMI power and disabling PP5000.
    
    On CHIPSET_STARTUP:
    - The `gpio_en_hdmi_pwr` signal is now enabled via a 10ms deferred
      call instead of immediately. This is done to ensure a proper
      rising timing requirements.
    
    On CHIPSET_SHUTDOWN:
    - The `gpio_ec_en_pp5000` signal is now disabled via a 5ms deferred
      call.
    - This delay is critical because the Quenbi HDMI power loadswitch
      uses PP5000 for its active discharge functionality. Disabling
      PP5000 immediately would prevent the HDMI power rails from
      discharging correctly. This 5ms delay gives the loadswitch
      sufficient time to complete its discharge sequence.
    
    BUG=b:454149279
    TEST=zmake build quenbi
    TEST=ensure no power sequence violation
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    
    Change-Id: I9d2b3bd80b0bb826fd61f565a42837defcbbb37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7084487
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Commit-Queue: Jayvik Desai <jayvik@google.com>

--- zephyr/program/bluey/quenbi/src/chipset.c
+++ zephyr/program/bluey/quenbi/src/chipset.c
@@ -9,10 +9,39 @@
 #include "gpio.h"
 #include "hooks.h"
 
+/* Define delays for deferred power sequencing */
+#define HDMI_PWR_ENABLE_DELAY (10 * USEC_PER_MSEC)
+#define PP5000_PWR_DISABLE_DELAY (10 * USEC_PER_MSEC)
+
+/*
+ * Deferred function to assert gpio_en_hdmi_pwr.
+ * This is called on startup to delay enabling HDMI power.
+ */
+static void hdmi_pwr_enable_deferred(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 1);
+}
+DECLARE_DEFERRED(hdmi_pwr_enable_deferred);
+
+/*
+ * Deferred function to deassert gpio_ec_en_pp5000.
+ * This is called on shutdown to delay disabling PP5000.
+ */
+static void pp5000_pwr_disable_deferred(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 0);
+}
+DECLARE_DEFERRED(pp5000_pwr_disable_deferred);
+
 void board_chipset_startup_quenbi(void)
 {
+	/*
+	 * Schedule HDMI power to be enabled after a delay,
+	 * rather than enabling it immediately, for rising timing requirements.
+	 */
+	hook_call_deferred(&hdmi_pwr_enable_deferred_data,
+			   HDMI_PWR_ENABLE_DELAY);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_usb_en), 1);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 1);
@@ -22,11 +51,17 @@ DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup_quenbi,
 
 void board_chipset_shutdown_quenbi(void)
 {
+	/*
+	 * Quenbi HDMI power loadswitch has active discharge powered by pp5000.
+	 * Delay disabling pp5000 to give the loadswitch time to discharge
+	 * HDMI Power rails on Quenbi.
+	 */
+	hook_call_deferred(&pp5000_pwr_disable_deferred_data,
+			   PP5000_PWR_DISABLE_DELAY);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_usb_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 0);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown_quenbi,
 	     HOOK_PRIO_DEFAULT);
