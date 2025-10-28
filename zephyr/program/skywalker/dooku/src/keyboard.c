927d471eabfbccb885cf42084925e77b87f9ecc2
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2025-10-28 19:11:49 +0800

    Dooku: update battery, keyboard and temp sensor setting
    This CL updates supported batteries in battery table. It also
    update keyboard setting and temp sensor 2 to monitor 5V DC/DC.
    
    BUG=b:457605256
    TEST=zmake build dooku success
    LOW_COVERAGE_REASON=Tests will be added in b/457605256
    
    Change-Id: I4c89e5947b4877f7b0910f0ffbad1867803d27c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7089497
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tony Chang <tonytcc@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>

--- zephyr/program/skywalker/dooku/src/keyboard.c
+++ zephyr/program/skywalker/dooku/src/keyboard.c
@@ -8,20 +8,19 @@
 #include "keyboard_scan.h"
 
 /*
- * We have total 32 pins for keyboard connecter {-1, -1} mean
- * the N/A pin that don't consider it and reserve index 0 area
- * that we don't have pin 0.
+ * Map keyboard connector pins to EC GPIO pins for factory test.
+ * Pins mapped to {-1, -1} are skipped.
+ * The connector has 30 pins total, and there is no pin 0.
  */
 const int keyboard_factory_scan_pins[][2] = {
-	{ -1, -1 },	  { -1, -1 },	    { -1, -1 },	      { GPIO_KSI, 0 },
-	{ GPIO_KSI, 1 },  { GPIO_KSI, 2 },  { GPIO_KSI, 3 },  { GPIO_KSI, 4 },
-	{ GPIO_KSI, 5 },  { GPIO_KSI, 6 },  { GPIO_KSI, 7 },  { GPIO_KSOH, 7 },
-	{ GPIO_KSOH, 6 }, { GPIO_KSOH, 5 }, { GPIO_KSOH, 4 }, { GPIO_KSOH, 3 },
-	{ GPIO_KSOH, 2 }, { GPIO_KSOH, 1 }, { GPIO_KSOH, 0 }, { GPIO_KSOL, 7 },
-	{ GPIO_KSOL, 6 }, { GPIO_KSOL, 5 }, { GPIO_KSOL, 4 }, { GPIO_KSOL, 3 },
-	{ GPIO_KSOL, 2 }, { GPIO_KSOL, 1 }, { GPIO_KSOL, 0 }, { GPIO_C, 5 },
-	{ GPIO_C, 3 },	  { -1, -1 },	    { -1, -1 },	      { -1, -1 },
-	{ -1, -1 },
+	{ -1, -1 },	  { GPIO_KSOH, 4 }, { GPIO_KSOH, 0 }, { GPIO_KSOH, 1 },
+	{ GPIO_KSOH, 3 }, { GPIO_KSOH, 2 }, { -1, -1 },	      { -1, -1 },
+	{ GPIO_KSOL, 5 }, { GPIO_KSOL, 6 }, { -1, -1 },	      { GPIO_KSOL, 3 },
+	{ GPIO_KSOL, 2 }, { GPIO_KSI, 0 },  { GPIO_KSOL, 1 }, { GPIO_KSOL, 4 },
+	{ GPIO_KSI, 3 },  { GPIO_KSI, 2 },  { GPIO_KSOL, 0 }, { GPIO_KSI, 5 },
+	{ GPIO_KSI, 4 },  { GPIO_KSOL, 7 }, { GPIO_KSI, 6 },  { GPIO_KSI, 7 },
+	{ GPIO_KSI, 1 },  { -1, -1 },	    { GPIO_KSOH, 5 }, { -1, -1 },
+	{ GPIO_KSOH, 6 }, { -1, -1 },	    { -1, -1 },
 };
 const int keyboard_factory_scan_pins_used =
 	ARRAY_SIZE(keyboard_factory_scan_pins);
