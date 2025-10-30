d8fd5705b07e02f7c32e554391d74170a5ac6d9e
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2025-10-30 18:27:22 +0800

    Dooku: Add keyboard backlight control
    Use PWM channel 3 to control keyboard backlight.
    FW_CONFIG bit 0 to identify if the kblight is present.
    
    BUG=b:457605256
    TEST=zmake build dooku success
    LOW_COVERAGE_REASON=Tests will be added in b/457605256
    
    Change-Id: Iadf4f95d281c29405ed37c117e0a8476bfc732b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7100141
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tony Chang <tonytcc@google.com>

--- zephyr/program/skywalker/dooku/src/keyboard.c
+++ zephyr/program/skywalker/dooku/src/keyboard.c
@@ -3,9 +3,38 @@
  * found in the LICENSE file.
  */
 
+#include "board_config.h"
+#include "common.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
 #include "ec_commands.h"
 #include "gpio_it8xxx2.h"
 #include "keyboard_scan.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_keyboard, LOG_LEVEL_INF);
+
+__override uint32_t board_override_feature_flags0(uint32_t flags0)
+{
+	int ret;
+	uint32_t val;
+
+	/*
+	 * Remove keyboard backlight feature for devices that don't support it.
+	 */
+	ret = cros_cbi_get_fw_config(FW_KB_BACKLIGHT, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d",
+			FW_KB_BACKLIGHT);
+		return flags0;
+	}
+
+	if (val == FW_KB_BACKLIGHT_OFF)
+		return (flags0 & ~EC_FEATURE_MASK_0(EC_FEATURE_PWM_KEYB));
+	else
+		return flags0;
+}
 
 /*
  * Map keyboard connector pins to EC GPIO pins for factory test.
