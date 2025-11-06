14021704ca44bcebf22c6aebaed264ce981c89c6
johnwc_yeh <JohnWC_Yeh@compal.com>
2025-11-06 15:15:55 +0800

    fwk: fix power on AC attach requiring OS boot to take effect
    Previously, you needed to boot an OS first for the settings to kick in.
    So if you remove SSDs, the setting will not kick in.
    
    Add a host command EC_CMD_POWER_ON_AC_ATTACH(0x3E2A) only for power
    on AC attach, use for BIOS to notify EC to save it.
    
    BRANCH=fwk-dogwood-27111
    BUG=https://app.clickup.com/t/86evbbru9
    TEST=1.unplug all SSDs and other storage
    2. enable "power on ac attach"  f10, save, reboot
    3. unplug AC power and wait a few seconds
    4. plug AC power can power on
    
    Signed-off-by: johnwc_yeh <JohnWC_Yeh@compal.com>

--- zephyr/program/framework/include/board_host_command.h
+++ zephyr/program/framework/include/board_host_command.h
@@ -641,5 +641,14 @@ struct ec_response_psu_control {
 	uint8_t force_enable_in_standby;
 } __ec_align1;
 
+/*****************************************************************************/
+/*
+ * This command uses to control the Power on AC attach enable/disable
+ */
+#define EC_CMD_POWER_ON_AC_ATTACH 0x3E2A
+
+struct ec_params_power_on_ac_attach {
+	uint8_t enable;
+} __ec_align1;
 
 #endif /* __BOARD_HOST_COMMAND_H */
