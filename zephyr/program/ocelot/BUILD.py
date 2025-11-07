a128a723b7bc43e7627dd5d1bc8ba1718dbee80c
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-11-07 11:11:24 -0800

    ocelotrvp-rtk: Add initial board files
    Add minimal configuration for ocelotrvp with Realtek EC.
    
    BUG=b:458637544
    TEST=zmake build ocelotrvp-rtk
    
    Change-Id: Id0cd073b26de632242ff1029c10c146aa8800ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7132614
    Tested-by: Perez Priego, Bernardo <bernardo.perez.priego@intel.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Breitenstein, Brandon <brandon.breitenstein@intel.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -222,6 +222,13 @@ register_ish_project(
     ],
 )
 
+register_rtk59_project(
+    project_name="ocelotrvp-rtk",
+    extra_kconfig_base_files=[
+        here / "rvp_program.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="kodkod", addr=0x80404)
@@ -232,3 +239,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-ite", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-mchp", addr=0x40318)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ojal", addr=0x80404)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocicat", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-rtk", addr=0x80404)
