49a980b50d64c7b85aaa1772b9e5745f51949119
Jon Murphy <jpmurphy@google.com>
2025-11-08 08:03:32 -0700

    ec: Update source for dropped ec-legacy
    BUG=b:456553702
    TEST=CQ
    
    Change-Id: Ia9aee0a49ac9187834effa72faefadea876016db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7134918
    Tested-by: Jon Murphy <jpmurphy@google.com>
    Auto-Submit: Jon Murphy <jpmurphy@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Jon Murphy <jpmurphy@google.com>

--- zephyr/firmware_builder.py
+++ zephyr/firmware_builder.py
@@ -318,11 +318,11 @@ def build(opts):
         build_compare_builds()
 
     # Start with a clean build environment
-    cmd = ["make", "clobber"]
+    cmd = ["make", "-f", "Makefile.cq", "clobber"]
     log_cmd(cmd)
     subprocess.run(
         cmd,
-        cwd=platform_ec,
+        cwd=ZEPHYR_DIR,
         check=True,
         stdin=subprocess.DEVNULL,
         env=env,
