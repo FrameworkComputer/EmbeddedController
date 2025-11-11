7765d4b58e357196d999fcf8f37dbf219bbed44e
Firas Sammoura <fsammoura@google.com>
2025-11-11 17:54:48 +0000

    test: Test the get_image_frame_params utility function
    Add an EC test to validate the behavior of the `get_image_frame_params`
    utility function. This function is responsible for populating the
    `fp_image_frame_params` structure based on the provided `fp_capture_type`.
    
    The test suite iterates through all defined capture types, including an
    invalid one (FP_CAPTURE_TYPE_MAX). It asserts the following:
    
    -   For valid capture types, the function should return success, and the
        output parameters must match the expected values obtained via
        `fp_sensor_get_info`.
    -   For the invalid capture type, the function should return
        `EC_ERROR_INVAL`, and the output parameter structure must remain
        unmodified.
    
    BUG=b:457818548
    TEST=./test/run_device_tests.py -b bloonchipper -t fpsensor_debug --renode
    
    Change-Id: I8951866287dde8d68d1b747de78289b6417e7a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7141612
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- test/fpsensor_debug.cc
+++ test/fpsensor_debug.cc
@@ -7,6 +7,7 @@
 #include "test_util.h"
 
 #if defined(SECTION_IS_RW)
+#include "fpsensor/fpsensor.h"
 #include "fpsensor/fpsensor_state_driver.h"
 #include "fpsensor_driver.h"
 #endif
@@ -15,6 +16,8 @@
 
 #include <algorithm>
 #include <array>
+#include <cstdint>
+#include <vector>
 
 static int is_locked;
 
@@ -22,6 +25,9 @@ int system_is_locked(void)
 {
 	return is_locked;
 }
+
+int get_image_frame_params(struct fp_image_frame_params &image_frame_params,
+			   enum fp_capture_type capture_type);
 
 // Smoke test the "fpinfo" console command and its underlying version retrieval.
 test_static int test_console_fpinfo()
@@ -238,6 +244,57 @@ test_static int test_command_fpenroll(void)
 }
 
 #if defined(SECTION_IS_RW)
+test_static int test_get_image_frame_params(void)
+{
+	constexpr auto kCaptureTypesArray = std::to_array(
+		{ FP_CAPTURE_VENDOR_FORMAT, FP_CAPTURE_DEFECT_PXL_TEST,
+		  FP_CAPTURE_ABNORMAL_TEST, FP_CAPTURE_NOISE_TEST,
+		  FP_CAPTURE_SIMPLE_IMAGE, FP_CAPTURE_PATTERN0,
+		  FP_CAPTURE_PATTERN1, FP_CAPTURE_QUALITY_TEST,
+		  FP_CAPTURE_RESET_TEST, FP_CAPTURE_TYPE_MAX });
+	constexpr struct fp_image_frame_params zero_params{};
+
+	size_t fp_sensor_get_info_v2_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		sizeof(struct fp_image_frame_params) * FP_MAX_CAPTURE_TYPES;
+	std::vector<uint8_t> buffer(fp_sensor_get_info_v2_size);
+	auto *info = reinterpret_cast<ec_response_fp_info_v2 *>(buffer.data());
+
+	TEST_EQ(fp_sensor_get_info(info, buffer.size()), EC_SUCCESS, "%d");
+
+	struct fp_image_frame_params *image_frame_params_ptr =
+		info->image_frame_params;
+
+	for (enum fp_capture_type current_capture_type : kCaptureTypesArray) {
+		const struct fp_image_frame_params *expected_params = nullptr;
+		for (size_t j = 0; j < info->sensor_info.num_capture_types;
+		     ++j) {
+			if (image_frame_params_ptr[j].fp_capture_type ==
+			    current_capture_type) {
+				expected_params = &image_frame_params_ptr[j];
+				break;
+			}
+		}
+
+		struct fp_image_frame_params image_frame_params{};
+		int rv = get_image_frame_params(image_frame_params,
+						current_capture_type);
+
+		if (expected_params) {
+			TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+			TEST_EQ(memcmp(&image_frame_params, expected_params,
+				       sizeof(struct fp_image_frame_params)),
+				0, "%d");
+		} else {
+			TEST_EQ(rv, EC_ERROR_INVAL, "%d");
+			TEST_EQ(memcmp(&image_frame_params, &zero_params,
+				       sizeof(struct fp_image_frame_params)),
+				0, "%d");
+		}
+	}
+	return EC_SUCCESS;
+}
+
 enum ec_error_list
 upload_pgm_image(uint8_t *frame,
 		 const struct fp_image_frame_params &image_frame_params);
@@ -280,6 +337,7 @@ void run_test(int argc, const char **argv)
 		RUN_TEST(test_command_fpcapture_mode_is_too_large);
 		RUN_TEST(test_command_fpenroll);
 #if defined(SECTION_IS_RW)
+		RUN_TEST(test_get_image_frame_params);
 		RUN_TEST(test_upload_pgm_image_wrong_bpp);
 #endif
 	}
