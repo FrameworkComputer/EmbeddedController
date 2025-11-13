64cb0dfcb38d261cd0accd5e050a8be7a7c474b1
mike <mike5@huaqin.corp-partner.google.com>
2025-11-13 20:45:55 +0800

    sapphire: Modify WLC power control
    1.Keep pp5000_wlc always on to prevent power leakage.
    2.Drive gpio_pen_rst_l low when the pen is removed to prevent CTN730 execution.
    
    BUG=b:460057223, b:459717221
    TEST=Pen detection and charging function correctly.
    TEST=DC S5 power consumption remains within normal limits.
    LOW_COVERAGE_REASON=Tests will be added in b:460057223
    
    Change-Id: I47da8e64c31bf625539e95bae8c606cdb8def481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7149320
    Reviewed-by: Knox Chiou <knoxchiou@chromium.org>
    Tested-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Knox Chiou <knoxchiou@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- zephyr/program/tanjiro/sapphire/src/pen_control.c
+++ zephyr/program/tanjiro/sapphire/src/pen_control.c
@@ -32,8 +32,8 @@ static inline void update_pen_status(void)
 static void set_wlc_power(void)
 {
 	/* Enable or disable WLC power based on pen presence */
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pp5000_wlc_en),
-			pen_status_flag);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pen_rst_l),
+			!pen_status_flag);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pp1800_wlc_en),
 			pen_status_flag);
 }
@@ -71,5 +71,8 @@ __override void board_pchg_power_on(int port, bool on)
 static void pen_status_init(void)
 {
 	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_pen_pres));
+
+	// In order to prevent leakage current, always enable WLC power on init
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pp5000_wlc_en), true);
 }
 DECLARE_HOOK(HOOK_INIT, pen_status_init, HOOK_PRIO_DEFAULT);
