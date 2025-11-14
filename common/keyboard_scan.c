dc4fcee57cc37c089f59479b5546967e91f62bad
Cheng Yueh <cyueh@chromium.org>
2025-11-14 15:35:36 +0800

    keyboard_scan: Force poll if scanning status changes
    If a pressed key is released during the scanning is disabled, then we
    should force polling the status after enabling scanning.
    
    BUG=b:457551385
    TEST=1. press R
    TEST=2. press powerbutton
    TEST=3. release R
    TEST=4. release powerbutton
    TEST=5. ksstate in ec console
    TEST=6. Without this change, the R key is in KB debounced list and
    keyboard in UI keeps typing R.
    TEST=7. With this change, the R key goes to debouncing and then
    not pressed and keyboard in UI stops typing R.
    TEST=./twister -T zephyr/test/drivers -s drivers.keyboard_scan \
           --coverage
    
    Change-Id: I2a61e1314045ee1ace388b34b9439302e747ab31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7156187
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -1125,6 +1125,7 @@ void keyboard_scan_task(void *u)
 				keyboard_raw_drive_column(KEYBOARD_COLUMN_ALL);
 				udelay(keyscan_config.output_settle_us +
 				       COL2_DELAY_US);
+				force_poll |= local_disable_scanning;
 			} else if (!local_disable_scanning) {
 				/*
 				 * Scanning isn't enabled but it was last time
@@ -1175,9 +1176,6 @@ void keyboard_scan_task(void *u)
 			}
 		}
 
-		/* We're about to poll, so any existing forces are fulfilled */
-		force_poll = 0;
-
 		/* Enter polling mode */
 		CPRINTS5("poll");
 		keyboard_raw_enable_interrupt(0);
@@ -1187,8 +1185,12 @@ void keyboard_scan_task(void *u)
 		while (keyboard_scan_is_enabled()) {
 			start = get_time();
 
-			/* Check for keys down */
-			if (check_keys_changed(debounced_state)) {
+			/*
+			 * Check for keys down or force poll to debounce key
+			 * press during keyboard scan disablement.
+			 */
+			if (check_keys_changed(debounced_state) || force_poll) {
+				force_poll = 0;
 				poll_deadline.val =
 					start.val +
 					keyscan_config.poll_timeout_us;
@@ -1215,6 +1217,12 @@ void keyboard_scan_task(void *u)
 
 			crec_usleep(wait_time);
 		}
+
+		/*
+		 * Set force_poll in case that keyboard_scan_is_enabled is false
+		 * in the first iteration.
+		 */
+		force_poll = 0;
 	}
 }
 
