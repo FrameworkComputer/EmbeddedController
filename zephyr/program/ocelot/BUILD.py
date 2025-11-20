jackadam1981/ChromeEc

ocelot: add initial microchip EC support
Add a minimal config for the ocelot board based on microchip EC.

BUG=b:396318819
TEST=zmake build ocelot_mchp

Change-Id: Ia31cf189148d89f0c98d5dc250de011de88bd4f1

zephyr/program/ocelot/BUILD.py
@@ -49,6 +49,29 @@ def register_it8xxx2_project(
     )
 
 
+def register_mchp_project(
+    project_name,
+    extra_kconfig_files=(),
+):
+    """Register an microchip based variant of ocelot."""
+    register_binman_project(
+        project_name=project_name,
+        zephyr_board="mec172x/mec172x_nsz/mec1727",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+        modules=["cmsis", "ec"],
+    )
+
+
 # For use on SKU1 and SKU2
 register_npcx9_project(
     project_name="ocelot_nuvoton",
@@ -65,7 +88,17 @@ register_it8xxx2_project(
     ],
 )
 
+# For use on SKU4
+register_mchp_project(
+    project_name="ocelot_mchp",
+    extra_kconfig_files=[
+        here / ".." / "intelrvp" / "zephyr_ap_pwrseq.conf",
+    ],
+)
+
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_nuvoton", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_ite", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_mchp", addr=0x40318)

FrameworkComputer/EmbeddedController

TEST=zmake build ocelot_microchip
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6381518
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Reviewed-by: Boris Mittelberg <bmbm@google.com>
Commit-Queue: Keith Short <keithshort@chromium.org>
Tested-by: Megha Verma <megha.verma@intel.corp-partner.google.com>
Reviewed-by: Keith Short <keithshort@chromium.org>
(cherry picked from commit 361c811d82847a34a94f52664c530844838e821c)

zephyr/program/ocelot/BUILD.py
@@ -49,6 +49,29 @@ def register_it8xxx2_project(
     )
 
 
+def register_mchp_project(
+    project_name,
+    extra_kconfig_files=(),
+):
+    """Register an microchip based variant of ocelot."""
+    register_binman_project(
+        project_name=project_name,
+        zephyr_board="mec172x/mec172x_nsz/mec1727",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+        modules=["cmsis", "ec"],
+    )
+
+
 # For use on SKU1 and SKU2
 register_npcx9_project(
     project_name="ocelot_nuvoton",
@@ -65,7 +88,17 @@ register_it8xxx2_project(
     ],
 )
 
+# For use on SKU4
+register_mchp_project(
+    project_name="ocelot_microchip",
+    extra_kconfig_files=[
+        here / ".." / "intelrvp" / "zephyr_ap_pwrseq.conf",
+    ],
+)
+
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_nuvoton", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_ite", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_microchip", addr=0x40318)