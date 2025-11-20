jackadam1981/ChromeEc

config: Resolve name conflict with Zephyr
The legacy EC and Zephyr projects both define CONFIG_I2C_BITBANG.
Rename the legacy EC option to CONFIG_I2C_BITBANG_LEGACY so that the
Zephyr version can be used when supported.

BUG=b:410023128
TEST=zmake compare-builds -a
TEST=./util/compare_builds.sh -b all

Change-Id: Ie49e237ed2fd87cdf60aacd58b84620aa52b2414

include/config.h
@@ -2918,9 +2918,8 @@
  */
 #undef CONFIG_I2C_MULTI_PORT_CONTROLLER
 
-#ifndef CONFIG_ZEPHYR
 /*
- * Enable I2C bitbang driver.
+ * Enable the legacy I2C bitbang driver.
  *
  * If defined, the board must define array i2c_bitbang_ports[] and
  * i2c_bitbang_ports_count (same as i2c_ports/i2c_ports_count), but with
@@ -2928,9 +2927,10 @@
  *
  * For example:
  * {"battery", 2, 100, GPIO_I2C3_SCL, GPIO_I2C3_SDA, .drv = &bitbang_drv},
+ *
+ * This option cannot be used by Zephyr EC projects.
  */
-#undef CONFIG_I2C_BITBANG
-#endif /* CONFIG_ZEPHYR */
+#undef CONFIG_I2C_BITBANG_LEGACY
 
 /*
  * If defined, reduce I2C traffic from update functions (i2c_update8/16

include/i2c.h
@@ -73,7 +73,7 @@ extern "C" {
 /* This port allows changing speed at runtime */
 #define I2C_PORT_FLAG_DYNAMIC_SPEED BIT(0)
 
-#ifndef CONFIG_I2C_BITBANG
+#ifndef CONFIG_I2C_BITBANG_LEGACY
 #define I2C_BITBANG_PORT_COUNT 0
 #endif
 

FrameworkComputer/EmbeddedController

Rename the legacy EC option to CONFIG_I2C_BITBANG_CROS_EC so that the
Zephyr version can be used when supported by the Zephyr I2C driver.
LOW_COVERAGE_REASON=rename only, no code changed
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6455863
Commit-Queue: Keith Short <keithshort@chromium.org>
Reviewed-by: caveh jalali <caveh@chromium.org>
Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
Code-Coverage: Keith Short <keithshort@chromium.org>
Tested-by: Keith Short <keithshort@chromium.org>
(cherry picked from commit df292c97f3f6e975fdf243e1243a05d943696a57)

include/config.h
@@ -2947,9 +2947,8 @@
  */
 #undef CONFIG_I2C_MULTI_PORT_CONTROLLER
 
-#ifndef CONFIG_ZEPHYR
 /*
- * Enable I2C bitbang driver.
+ * Enable the legacy I2C bitbang driver.
  *
  * If defined, the board must define array i2c_bitbang_ports[] and
  * i2c_bitbang_ports_count (same as i2c_ports/i2c_ports_count), but with
@@ -2957,9 +2956,10 @@
  *
  * For example:
  * {"battery", 2, 100, GPIO_I2C3_SCL, GPIO_I2C3_SDA, .drv = &bitbang_drv},
+ *
+ * This option cannot be used by Zephyr EC projects.
  */
-#undef CONFIG_I2C_BITBANG
-#endif /* CONFIG_ZEPHYR */
+#undef CONFIG_I2C_BITBANG_CROS_EC
 
 /*
  * If defined, reduce I2C traffic from update functions (i2c_update8/16

include/i2c.h
@@ -75,7 +75,7 @@ extern "C" {
 /* This port allows changing speed at runtime */
 #define I2C_PORT_FLAG_DYNAMIC_SPEED BIT(0)
 
-#ifndef CONFIG_I2C_BITBANG
+#ifndef CONFIG_I2C_BITBANG_CROS_EC
 #define I2C_BITBANG_PORT_COUNT 0
 #endif
 