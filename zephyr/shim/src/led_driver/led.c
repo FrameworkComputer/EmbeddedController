a8f37c2c52108146a2440f45365d55c8530e6b18
Wai-Hong Tam <waihong@google.com>
2025-11-19 18:29:42 -0800

    led: Assert policy and pin LED IDs match
    This change adds a BUILD_ASSERT to ensure that the led-id property of
    the policy node matches the led-id property of the referenced pin node.
    
    BUG=b:462151527
    TEST=zmake build -a; manually modify dts to mismatch IDs and verify
    build fails.
    
    Change-Id: I46c3c0b52101465fb60510c8f7b52ff2fffd75dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7173475
    Tested-by: Wai-Hong Tam <waihong@google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Wai-Hong Tam <waihong@google.com>

--- zephyr/shim/src/led_driver/led.c
+++ zephyr/shim/src/led_driver/led.c
@@ -50,6 +50,14 @@ DT_FOREACH_CHILD_STATUS_OKAY_VARGS(PINS2_PARENT_NODE, DT_FOREACH_CHILD,
 				   DECLARE_PINS_NODE)
 #endif
 
+#define ASSERT_LEDS_ID_MATCH(id)                                              \
+	BUILD_ASSERT(                                                         \
+		DT_STRING_TOKEN(DT_PARENT(id), led_id) ==                     \
+			DT_STRING_TOKEN(DT_PARENT(DT_PHANDLE(id, led_color)), \
+					led_id),                              \
+		"The led-color node (" #id                                    \
+		") must belong to the same led-id defined in the policy.");
+
 #define SET_PATTERN_COLOR_ARRAY(id)                                      \
 	{                                                                \
 		.led_color_node = &PINS_NODE(DT_PHANDLE(id, led_color)), \
@@ -57,9 +65,10 @@ DT_FOREACH_CHILD_STATUS_OKAY_VARGS(PINS2_PARENT_NODE, DT_FOREACH_CHILD,
 	},
 
 #define PATTERN_COLOR_ARRAY(id) DT_CAT(PATTERN_COLOR_, id)
-#define GEN_PATTERN_COLOR_ARRAY(id, fn)                  \
-	struct pattern_color_node_t PATTERN_COLOR_ARRAY( \
-		id)[] = { fn(id, SET_PATTERN_COLOR_ARRAY) };
+#define GEN_PATTERN_COLOR_ARRAY(id, fn)                      \
+	struct pattern_color_node_t PATTERN_COLOR_ARRAY(     \
+		id)[] = { fn(id, SET_PATTERN_COLOR_ARRAY) }; \
+	fn(id, ASSERT_LEDS_ID_MATCH)
 DT_INST_FOREACH_CHILD_STATUS_OKAY_VARGS(0, DT_FOREACH_CHILD_VARGS,
 					GEN_PATTERN_COLOR_ARRAY,
 					DT_FOREACH_CHILD)
