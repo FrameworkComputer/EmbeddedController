jackadam1981/ChromeEc

mchp: Remove bbram access for SW sync
When EC does a FW jump, it uses VBAT RAM section `ec_img_load` to set
the next flash bank to use. This CL replaces the usage of bbram access
of section `ec_img_load` with a local variable that holds the next
section to execute, and invalidates this right after jumping to let
constant variable `mchp_image_type` determine current image running.

BUG=b:391403915
TEST=Run command on EC terminal `sysinfo`
     Result of `Copy` must be correct

Change-Id: I7c39bd1cf16207143879cfa3395899c8e18b20e6

zephyr/shim/chip/mchp/system_external_storage.c
@@ -32,6 +32,8 @@ const uint32_t mchp_image_type = MCHP_ECRW_WORD;
 #error "Unsupported image type!"
 #endif
 
+/* Holds next image to jump */
+static enum ec_image next_image_copy = EC_IMAGE_UNKNOWN;
 /*
  * Make sure CONFIG_XXX flash offsets are correct for MEC172x 512KB SPI flash.
  */
@@ -67,6 +69,10 @@ void system_jump_to_booter(void)
 	 */
 	clock_turbo();
 
+	/* Before jumping, invalidate `next_image_copy` to let `mchp_image_type`
+	 * figure out current image type.  */
+	system_set_image_copy(EC_IMAGE_UNKNOWN);
+
 	/* MCHP Read selected image from SPI flash into SRAM
 	 * Need a jump to little-fw (LFW).
 	 */
@@ -84,23 +90,13 @@ uint32_t system_get_lfw_address(void)
 
 enum ec_image system_get_shrspi_image_copy(void)
 {
-	enum ec_image img = EC_IMAGE_UNKNOWN;
-	uint32_t value = 0u;
+	enum ec_image img = next_image_copy;
 
-	if (bbram_dev) {
-		if (!bbram_read(bbram_dev, BBRAM_REGION_OFFSET(ec_img_load),
-				BBRAM_REGION_SIZE(ec_img_load),
-				(uint8_t *)&value)) {
-			img = (enum ec_image)(value & 0x7fu);
-		}
-	}
-
-	if (img == EC_IMAGE_UNKNOWN) {
+	if (next_image_copy == EC_IMAGE_UNKNOWN) {
 		img = EC_IMAGE_RO;
 		if (mchp_image_type == MCHP_ECRW_WORD) {
 			img = EC_IMAGE_RW;
 		}
-		system_set_image_copy(img);
 	}
 
 	return img;
@@ -113,7 +109,7 @@ enum ec_image system_get_shrspi_image_copy(void)
  */
 void system_set_image_copy(enum ec_image copy)
 {
-	uint32_t value = (uint32_t)copy;
+	uint8_t value = (uint8_t)copy;
 
 	if (!bbram_dev) {
 		return;
@@ -125,11 +121,11 @@ void system_set_image_copy(enum ec_image copy)
 		value = EC_IMAGE_RW;
 		break;
 	case EC_IMAGE_RO:
-	default:
 		value = EC_IMAGE_RO;
 		break;
+	default:
+		value = EC_IMAGE_UNKNOWN;
 	}
 
-	bbram_write(bbram_dev, BBRAM_REGION_OFFSET(ec_img_load),
-		    BBRAM_REGION_SIZE(ec_img_load), (uint8_t *)&value);
+	next_image_copy = copy;
 }

FrameworkComputer/EmbeddedController

LOW_COVERAGE_REASON=b:370072796 No test coverage for memory mapped devices
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6422989
Feels: Brandon Breitenstein <brandon.breitenstein@intel.com>
Feels: caveh jalali <caveh@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Tested-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
Commit-Queue: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
Reviewed-by: Keith Short <keithshort@chromium.org>
Reviewed-by: caveh jalali <caveh@chromium.org>
(cherry picked from commit e766bfb835285e5d4e873aab30f287a94b2cc812)

zephyr/shim/chip/mchp/system_external_storage.c
@@ -19,10 +19,6 @@
 #define MCHP_ECRW_WORD 0x57524345u /* ASCII ECRW */
 #define MCHP_PCR_NODE DT_INST(0, microchip_xec_pcr)
 
-static const struct device *const bbram_dev =
-	COND_CODE_1(DT_HAS_CHOSEN(cros_ec_bbram),
-		    DEVICE_DT_GET(DT_CHOSEN(cros_ec_bbram)), NULL);
-
 /* Build image type string in RO/RW image */
 #ifdef CONFIG_CROS_EC_RO
 const uint32_t mchp_image_type = MCHP_ECRO_WORD;
@@ -32,6 +28,8 @@ const uint32_t mchp_image_type = MCHP_ECRW_WORD;
 #error "Unsupported image type!"
 #endif
 
+/* Holds next image to jump */
+static enum ec_image next_image_copy = EC_IMAGE_UNKNOWN;
 /*
  * Make sure CONFIG_XXX flash offsets are correct for MEC172x 512KB SPI flash.
  */
@@ -67,6 +65,10 @@ void system_jump_to_booter(void)
 	 */
 	clock_turbo();
 
+	/* Before jumping, invalidate `next_image_copy` to let `mchp_image_type`
+	 * figure out current image type.  */
+	system_set_image_copy(EC_IMAGE_UNKNOWN);
+
 	/* MCHP Read selected image from SPI flash into SRAM
 	 * Need a jump to little-fw (LFW).
 	 */
@@ -84,23 +86,13 @@ uint32_t system_get_lfw_address(void)
 
 enum ec_image system_get_shrspi_image_copy(void)
 {
-	enum ec_image img = EC_IMAGE_UNKNOWN;
-	uint32_t value = 0u;
+	enum ec_image img = next_image_copy;
 
-	if (bbram_dev) {
-		if (!bbram_read(bbram_dev, BBRAM_REGION_OFFSET(ec_img_load),
-				BBRAM_REGION_SIZE(ec_img_load),
-				(uint8_t *)&value)) {
-			img = (enum ec_image)(value & 0x7fu);
-		}
-	}
-
-	if (img == EC_IMAGE_UNKNOWN) {
+	if (next_image_copy == EC_IMAGE_UNKNOWN) {
 		img = EC_IMAGE_RO;
 		if (mchp_image_type == MCHP_ECRW_WORD) {
 			img = EC_IMAGE_RW;
 		}
-		system_set_image_copy(img);
 	}
 
 	return img;
@@ -113,23 +105,15 @@ enum ec_image system_get_shrspi_image_copy(void)
  */
 void system_set_image_copy(enum ec_image copy)
 {
-	uint32_t value = (uint32_t)copy;
-
-	if (!bbram_dev) {
-		return;
-	}
-
 	switch (copy) {
 	case EC_IMAGE_RW:
 	case EC_IMAGE_RW_B:
-		value = EC_IMAGE_RW;
+		next_image_copy = EC_IMAGE_RW;
 		break;
 	case EC_IMAGE_RO:
-	default:
-		value = EC_IMAGE_RO;
+		next_image_copy = EC_IMAGE_RO;
 		break;
+	default:
+		next_image_copy = EC_IMAGE_UNKNOWN;
 	}
-
-	bbram_write(bbram_dev, BBRAM_REGION_OFFSET(ec_img_load),
-		    BBRAM_REGION_SIZE(ec_img_load), (uint8_t *)&value);
 }