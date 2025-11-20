jackadam1981/ChromeEc

zephyr: Add support for LLVM RISC-V toolchain
BUG=b:407560601, b:172020503, b:407786163
TEST=rm -rf build && zmake build --clobber minimal-it8xxx2

Change-Id: I8531c3bc83d78e4d0aa70c32344dd92d33bfe210

zephyr/cmake/toolchain/llvm/target.cmake
@@ -20,6 +20,8 @@ set(CROSS_COMPILE_TARGET_x86          x86_64-pc-linux-gnu)
 
 set(CROSS_COMPILE_TARGET          ${CROSS_COMPILE_TARGET_${ARCH}})
 
+set(target_uses_newlib FALSE)
+
 if("${ARCH}" STREQUAL "arm")
   if(DEFINED CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
     # ARMV7_M_ARMV8_M_MAINLINE means that ARMv7-M or backward compatible ARMv8-M
@@ -38,8 +40,14 @@ if("${ARCH}" STREQUAL "arm")
     # Baseline implementation processor is used.
     set(CROSS_COMPILE_TARGET arm-none-eabi)
   endif()
+  set(toolchain_uses_newlib TRUE)
+elseif("${ARCH}" STREQUAL "riscv")
+  set(CROSS_COMPILE_TARGET riscv32-cros-elf)
+  set(toolchain_uses_newlib TRUE)
+endif()
 
-  # LLVM based toolchains for ARM use newlib as a libc.
+if(toolchain_uses_newlib)
+  # LLVM based toolchains for ARM and RISC-V use newlib as a libc.
   # This variable is set AFTER all Kconfig files were processed, so it doesn't
   # affect them, but it's still useful for filtering tests.
   set(TOOLCHAIN_HAS_NEWLIB ON CACHE BOOL "True if toolchain supports newlib")

FrameworkComputer/EmbeddedController

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6303165
Reviewed-by: Eric Yilun Lin <yllin@google.com>
Reviewed-by: Patryk Duda <patrykd@google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Tested-by: Tom Hughes <tomhughes@chromium.org>
Commit-Queue: Tom Hughes <tomhughes@chromium.org>
(cherry picked from commit fa6e33da703740a70ae0d9141346d4d96d80dfde)

zephyr/cmake/toolchain/llvm/target.cmake
@@ -20,6 +20,8 @@ set(CROSS_COMPILE_TARGET_x86          x86_64-pc-linux-gnu)
 
 set(CROSS_COMPILE_TARGET          ${CROSS_COMPILE_TARGET_${ARCH}})
 
+set(toolchain_uses_newlib FALSE)
+
 if("${ARCH}" STREQUAL "arm")
   if(DEFINED CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
     # ARMV7_M_ARMV8_M_MAINLINE means that ARMv7-M or backward compatible ARMv8-M
@@ -38,8 +40,14 @@ if("${ARCH}" STREQUAL "arm")
     # Baseline implementation processor is used.
     set(CROSS_COMPILE_TARGET arm-none-eabi)
   endif()
+  set(toolchain_uses_newlib TRUE)
+elseif("${ARCH}" STREQUAL "riscv")
+  set(CROSS_COMPILE_TARGET riscv32-cros-elf)
+  set(toolchain_uses_newlib TRUE)
+endif()
 
-  # LLVM based toolchains for ARM use newlib as a libc.
+if(toolchain_uses_newlib)
+  # LLVM based toolchains for ARM and RISC-V use newlib as a libc.
   # This variable is set AFTER all Kconfig files were processed, so it doesn't
   # affect them, but it's still useful for filtering tests.
   set(TOOLCHAIN_HAS_NEWLIB ON CACHE BOOL "True if toolchain supports newlib")