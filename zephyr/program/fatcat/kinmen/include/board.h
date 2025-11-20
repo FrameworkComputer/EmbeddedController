jackadam1981/ChromeEc

kinmen: Initial Zephyr EC image
Create the initial Zephyr EC image for the kinmen variant, this is
a skeleton to the start point. Subsequent CLs will contain changes
that reflect the schematic and component.

BUG=none
TEST=zmake build kinmen

Change-Id: I7e5f621db63c6ee77e77995a06d1f07da3bad7d3

zephyr/program/fatcat/kinmen/include/board.h
@@ -1,0 +1,18 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef KINMEN_BOARD_H_
+#define KINMEN_BOARD_H_
+
+/* Power Signals */
+#define PWR_EN_PP3300_S5 &gpioi 7
+#define PWR_EN_PP5000_S5 &gpiog 1
+#define PWR_RSMRST_PWRGD &gpiof 0
+#define PWR_EC_PCH_RSMRST &gpioj 0
+#define PWR_SLP_S0 &gpioj 5
+#define PWR_PCH_PWROK &gpioj 1
+#define PWR_ALL_SYS_PWRGD &gpiod 5
+
+#endif /* KINMEN_BOARD_H_ */

FrameworkComputer/EmbeddedController

Create the initial Zephyr EC image for the kinmen variant which
is based on the brox and fatcat_it82002aw. Subsequent CLs will
contain changes that reflect the schematic and component.
BUG=b:409148565
LOW_COVERAGE_REASON=b:410431240 will add test code after bring up.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6450603
Tested-by: Devin Lu <devin.lu@quantatw.com>
Reviewed-by: caveh jalali <caveh@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Reviewed-by: Bob Moragues <moragues@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
(cherry picked from commit 8355961e4d6da98812fbe422c2b930acf10a75a1)

zephyr/program/fatcat/kinmen/include/board.h
@@ -1,0 +1,17 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef KINMEN_BOARD_H_
+#define KINMEN_BOARD_H_
+
+/* Power Signals */
+#define PWR_EN_PP5000_S5 &gpioi 7
+#define PWR_RSMRST_PWRGD &gpiof 0
+#define PWR_EC_PCH_RSMRST &gpioj 0
+#define PWR_SLP_S0 &gpioj 5
+#define PWR_PCH_PWROK &gpioj 1
+#define PWR_ALL_SYS_PWRGD &gpiod 5
+
+#endif /* KINMEN_BOARD_H_ */