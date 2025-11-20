jackadam1981/ChromeEc

uldrenite: Disable sensors if ISH
If the ISH is enabled, disable the sensor stack. There are no devices
that set this FW_CONFIG flag right now until we get the next boards,
but this flag is used on current boards to validate ISH sensors.

BUG=b:410646823
TEST=zmake build uldrenite

Change-Id: I751832021045400dd52c82a29b3a99b3fe0c5061

zephyr/program/trulo/uldrenite/src/sensor.c
@@ -41,7 +41,25 @@ void lid_accel_interrupt(enum gpio_signal signal)
 
 static void motionsense_init(void)
 {
-	int ret;
+	int ish_enabled;
+	int ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
+
+	if (ret < 0) {
+		LOG_ERR("Failed to load ISH config: %d", ret);
+		return;
+	}
+
+	if (ish_enabled == ISH_DISABLED) {
+		motion_sensor_count = 0;
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
+				      GPIO_DISCONNECTED);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_DISCONNECTED);
+		LOG_INF("No motionsense");
+		return;
+	}
 
 	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
 	if (ret < 0) {

FrameworkComputer/EmbeddedController

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6466263
Tested-by: Yuval Peress <peress@google.com>
Commit-Queue: Yuval Peress <peress@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Code-Coverage: Yuval Peress <peress@google.com>
Reviewed-by: Al Semjonovs <asemjonovs@google.com>
(cherry picked from commit 810bf2c814f214c869ccd240c37a3f14424299cb)

zephyr/program/trulo/uldrenite/src/sensor.c
@@ -41,7 +41,25 @@ void lid_accel_interrupt(enum gpio_signal signal)
 
 static void motionsense_init(void)
 {
-	int ret;
+	int ish_enabled;
+	int ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
+
+	if (ret < 0) {
+		LOG_ERR("Failed to load ISH config: %d", ret);
+		return;
+	}
+
+	if (ish_enabled == ISH_ENABLED) {
+		motion_sensor_count = 0;
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
+				      GPIO_DISCONNECTED);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_DISCONNECTED);
+		LOG_INF("No motionsense");
+		return;
+	}
 
 	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
 	if (ret < 0) {