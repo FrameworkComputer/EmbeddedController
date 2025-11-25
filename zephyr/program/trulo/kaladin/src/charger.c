92d8d550c5b958b456c2dbf81f9c4e8cdafb68de
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-11-25 18:54:59 +0800

    kaladin: Disable charger PSYS in S5 and re-enable in S0
    Disable charger PSYS in S5 to reduce power consumption, and enable it
    again after the system transitions to S0.
    
    BUG=b:463543106
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: Ifbb2a4edda4ce373aaca071f49b83ffd958f6ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7202329
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    Tested-by: Arthur Lin <lin.arthur@inventec.corp-partner.google.com>
    Reviewed-by: Arthur Lin <lin.arthur@inventec.corp-partner.google.com>
    Reviewed-by: Minho Park <minhopark@google.com>

--- zephyr/program/trulo/kaladin/src/charger.c
+++ zephyr/program/trulo/kaladin/src/charger.c
@@ -73,11 +73,11 @@ static void set_prochot_duration(void)
 	rv = isl95522_write(0, ISL95522_REG_PROCHOT_DURATION, ctl_val);
 }
 
-static void set_reg_control_1(void)
+static void set_psys_enable(int enable)
 {
-	/* Set bit for PSYS */
+	/* Set or clear PSYS bit */
 	isl95522_update(0, ISL95522_REG_CONTROL1, ISL95522_REG_CONTROL1_PSYS,
-			MASK_SET);
+			(enable) ? MASK_SET : MASK_CLR);
 }
 
 static void set_reg_control_2(void)
@@ -97,8 +97,24 @@ static void set_chg_custom_setting(void)
 	isl95522_set_dc_prochot(0, 4352);
 	/* Set ISL95522 data sheet 0x47H default value */
 	isl95522_set_ac_prochot(0, 6144);
-	set_reg_control_1();
+	set_psys_enable(0);
 	set_reg_control_2();
 }
 DECLARE_HOOK(HOOK_INIT, set_chg_custom_setting,
 	     HOOK_PRIO_POST_BATTERY_INIT + 1);
+
+static void charger_power_off(void)
+{
+	LOG_INF("kaladin: disable_chg_psys");
+
+	set_psys_enable(0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, charger_power_off, HOOK_PRIO_DEFAULT);
+
+static void charger_power_on(void)
+{
+	LOG_INF("kaladin: enable_chg_psys");
+
+	set_psys_enable(1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, charger_power_on, HOOK_PRIO_DEFAULT);
