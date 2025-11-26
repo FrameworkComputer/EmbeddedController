35d5c5ec7b7fc732a0de686b03c279c03b50184e
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-11-26 18:51:17 +0800

    matsu: Enable soc_edp_bl_en interrupt
    Enable soc_edp_bl_en interrupt
    
    BUG=b:463193164
    TEST=gpioget ec_touch_en
    LOW_COVERAGE_REASON=no unit test for ocelot board yet
    
    Change-Id: I7ad7b0e4809bc7f9bf2ea94d68c45c345fabc128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7206973
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Tested-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Commit-Queue: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/ocelot/matsu/src/touch_panel.c
+++ zephyr/program/ocelot/matsu/src/touch_panel.c
@@ -64,3 +64,9 @@ static void touch_lid_change(void)
 	}
 }
 DECLARE_HOOK(HOOK_LID_CHANGE, touch_lid_change, HOOK_PRIO_DEFAULT);
+
+static void touch_enable_init(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_soc_edp_bl_en));
+}
+DECLARE_HOOK(HOOK_INIT, touch_enable_init, HOOK_PRIO_DEFAULT);
