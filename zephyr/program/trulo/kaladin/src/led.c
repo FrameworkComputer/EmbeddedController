6fb7cb2a041ed23fb9792d2f7dcb07624d9cc852
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-12-04 16:42:53 +0800

    kaladin: Fix the charge LED flash once when Power Off over one hour then Power On system.
    Select PWM-LED init level high. And add counter to avoid LED flicker while EC is being initialized.
    
    BUG=b:460032780
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: I8061ad094335fa7395a69a89a4f61b0b9ba1a617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7226656
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Minho Park <minhopark@google.com>
    Commit-Queue: Johnson Wu <wu.johnson@inventec.corp-partner.google.com>
    Tested-by: Johnson Wu <wu.johnson@inventec.corp-partner.google.com>

--- zephyr/program/trulo/kaladin/src/led.c
+++ zephyr/program/trulo/kaladin/src/led.c
@@ -44,8 +44,10 @@ LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 #define LOW_ADP_BLINK_END_STEP 60
 
-static int blink_cnt;
+#define MINIMUM_LED_READY_DELAY 2
 
+static int blink_cnt;
+static int led_ready_counter;
 const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED,
 					     EC_LED_ID_POWER_LED };
 
@@ -231,6 +233,13 @@ static void led_set_battery(void)
 	static bool battery_low_triggeied = 0;
 	static bool battery_critical_triggeied = 0;
 	battery_ticks++;
+	/* Avoid LED flicker during EC init. curr.state defaults to ST_IDLE,
+	 * causing led_pwr_get_state() to return LED_PWRS_IDLE before init
+	 * completes. MINIMUM_LED_READY_DELAY * 500ms, Delay 1s */
+	if (led_ready_counter < MINIMUM_LED_READY_DELAY) {
+		led_ready_counter++;
+		return;
+	}
 
 	switch (led_pwr_get_state()) {
 	case LED_PWRS_CHARGE:
@@ -527,6 +536,7 @@ static void pwr_led_init(void)
 		pwr_led_shutdown_hook();
 
 	low_adp_blink_init();
+	led_ready_counter = 0;
 }
 DECLARE_HOOK(HOOK_INIT, pwr_led_init, HOOK_PRIO_DEFAULT);
 
