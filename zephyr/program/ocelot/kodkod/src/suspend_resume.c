2763a8f174b406abdf6bcbed13b478b6e62495a0
matt_wang <matt_wang@compal.corp-partner.google.com>
2025-12-08 11:47:36 +0800

    kodkod: Configure AMP_MUTE_L signal
    Drive the AMP_MUTE_L signal based on the AP power state.
    
    BUG=b:466136593
    TEST=have a beep voice when power on
    LOW_COVERAGE_REASON=b:452537396 will add test code after bring up.
    
    Change-Id: I9c8d0925563cb555fb03ae13349eb1c794e3d3c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7235293
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Tested-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Reviewed-by: Oscar Liu <oscarliu@google.com>

---
+++ zephyr/program/ocelot/kodkod/src/suspend_resume.c
@@ -0,0 +1,56 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ap_power/ap_power.h"
+#include "gpio_signal.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_INF);
+
+#define AMP_MUTE_L_DT_SPEC GPIO_DT_FROM_NODELABEL(gpio_ec_amp_mute_l)
+
+static void kodkod_power_event_handler(struct ap_power_ev_callback *callback,
+				       struct ap_power_ev_data data)
+{
+	switch (data.event) {
+	case AP_POWER_PRE_INIT:
+		/* fall-through */
+	case AP_POWER_STARTUP:
+
+		/* Deassert AMP_MUTE_L when AP is on. */
+		gpio_pin_set_dt(AMP_MUTE_L_DT_SPEC, 0);
+		break;
+	case AP_POWER_HARD_OFF:
+
+		/* Assert AMP_MUTE_L when powered off. */
+		gpio_pin_set_dt(AMP_MUTE_L_DT_SPEC, 1);
+		break;
+	default:
+		/* Other events ignored */
+		break;
+	}
+}
+
+static int init_suspend_resume(void)
+{
+	static struct ap_power_ev_callback cb;
+	const struct gpio_dt_spec *amp_mute_l =
+		GPIO_DT_FROM_NODELABEL(gpio_ec_amp_mute_l);
+
+	if (!gpio_is_ready_dt(amp_mute_l)) {
+		LOG_ERR("device %s not ready", amp_mute_l->port->name);
+		return -EINVAL;
+	}
+
+	ap_power_ev_init_callback(&cb, kodkod_power_event_handler,
+				  AP_POWER_PRE_INIT | AP_POWER_STARTUP |
+					  AP_POWER_HARD_OFF);
+	ap_power_ev_add_callback(&cb);
+
+	return 0;
+}
+SYS_INIT(init_suspend_resume, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
