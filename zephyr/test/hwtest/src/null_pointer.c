0c6dbad40670893fab6cec32392f63e1845964e5
Firas Sammoura <fsammoura@google.com>
2025-12-16 16:42:20 -0800

    zephyr: test: Fix null pointer test failure
    The `null_pointer` test was failing because the method used to verify
    the crash was unreliable. The test attempted to check the program
    counter value from panic data after an intentional null pointer
    dereference. This check, `(dereference_addr <= pc) && (dereference_end
    >= pc)`, failed, causing the test suite to report a failure, even though
    the null pointer dereference itself triggered the expected CPU exception.
    
    This commit refactors the test to use the standard Zephyr test (ztest)
    framework's fatal error handling mechanism, which is designed for
    verifying expected crashes.
    
    This revised approach provides a more robust and standard way to
    confirm that the null pointer dereference results in the correct
    fatal error, resolving the test instability.
    
    BUG=b:469460005
    TEST=./test/run_device_tests.py -b helipilot -f servo_micro -t
         null_pointer --zephyr
    
    Change-Id: Ia9a4450e8b8674c1b276580d67d38149c95433f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7266377
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- zephyr/test/hwtest/src/null_pointer.c
+++ zephyr/test/hwtest/src/null_pointer.c
@@ -2,44 +2,37 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-#include "multistep_test.h"
-#include "panic.h"
-#include "system.h"
-#include "util.h"
 
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/__assert.h>
 #include <zephyr/ztest.h>
+#include <zephyr/ztest_error_hook.h>
 
 LOG_MODULE_REGISTER(null_pointer, LOG_LEVEL_INF);
 
-static void test_null_pointer_dereference(void)
+ZTEST_SUITE(null_pointer, NULL, NULL, NULL, NULL, NULL);
+
+void ztest_post_fatal_error_hook(unsigned int reason,
+				 const struct arch_esf *pEsf)
+{
+	zassert_equal(reason, K_ERR_CPU_EXCEPTION);
+}
+
+void null_pointer_dereference(void)
 {
 	volatile uint32_t *null_ptr = NULL;
+
+	ztest_set_fault_valid(true);
 	LOG_INF("The value of null_ptr after dereferencing is: %d", *null_ptr);
 
 	/* Should never reach this. */
 	zassert_unreachable();
 }
 
-static void test_panic_data(void)
+ZTEST(null_pointer, test_null_pointer_dereference)
 {
-#ifdef CONFIG_ARM
-	struct panic_data *const pdata = panic_get_data();
-	uint32_t dereference_addr = (uint32_t)test_null_pointer_dereference;
-	/* Estimated end of the test_null_pointer_dereference() function */
-	uint32_t dereference_end =
-		(uint32_t)test_null_pointer_dereference + 0x40;
-	uint32_t pc = pdata->cm.frame[CORTEX_PANIC_FRAME_REGISTER_PC];
+	null_pointer_dereference();
 
-	/* Make sure Program Counter is stored correctly and points at the abort
-	 * function.
-	 */
-	zassert_true((dereference_addr <= pc) && (dereference_end >= pc));
-#endif
+	/* Should never reach this. */
+	zassert_unreachable();
 }
-
-static void (*test_steps[])(void) = { test_null_pointer_dereference,
-				      test_panic_data };
-
-MULTISTEP_TEST(null_pointer_dereference, test_steps)
