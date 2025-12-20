1de126b4e18a629eb6201b7307ec9d396560f338
Firas Sammoura <fsammoura@google.com>
2025-12-20 02:08:54 +0000

    driver/fingerprint/fpc: Use distinct parameters for raw/real images
    Introduce separate image frame parameter definitions for raw and real
    image captures on the FPC1145 sensor. This distinction is necessary
    because the required frame size can differ between these capture types.
    
    Raw image parameters, using FP_SENSOR_IMAGE_SIZE_FPC, are now applied
    to:
    *   FPC_CAPTURE_VENDOR_FORMAT
    *   FPC_CAPTURE_QUALITY_TEST
    
    Real image parameters, using the new FP_SENSOR_REAL_IMAGE_SIZE_FPC
    macro, are now applied to:
    *   FPC_CAPTURE_SIMPLE_IMAGE
    *   FPC_CAPTURE_PATTERN0
    *   FPC_CAPTURE_PATTERN1
    *   FPC_CAPTURE_RESET_TEST
    
    BUG=b:450381837
    TEST=CQ
    
    Change-Id: I01bf1d40e39616408b238ebb1344d29e0896ea46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7282040
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- driver/fingerprint/fpc/libfp/fpc1145_private.h
+++ driver/fingerprint/fpc/libfp/fpc1145_private.h
@@ -28,6 +28,8 @@
 /* Acquired finger frame definitions */
 #define FP_SENSOR_IMAGE_SIZE_MODE_VENDOR_FPC (35460)
 #define FP_SENSOR_IMAGE_SIZE_MODE_SIMPLE_FPC (13356)
+#define FP_SENSOR_REAL_IMAGE_SIZE_FPC \
+	(FP_SENSOR_RES_X_FPC * FP_SENSOR_RES_Y_FPC)
 /*
  * Size of the captured image in MQT mode. If you this is modified the
  * corresponding value in the MQT tool fputils.py must be changed too.
--- driver/fingerprint/fpc/libfp/fpc_private.c
+++ driver/fingerprint/fpc/libfp/fpc_private.c
@@ -60,40 +60,46 @@ static struct fp_sensor_info fpc1145_sensor_info = {
 	.version = 1,
 };
 
-#define FPC1145_DEFAULT_IMAGE_PARAMS                                          \
+#define FPC1145_DEFAULT_RAW_IMAGE_PARAMS                                      \
 	.bpp = FP_SENSOR_RES_BPP_FPC, .frame_size = FP_SENSOR_IMAGE_SIZE_FPC, \
 	.pixel_format = V4L2_PIX_FMT_GREY, .width = FP_SENSOR_RES_X_FPC,      \
+	.height = FP_SENSOR_RES_Y_FPC
+
+#define FPC1145_DEFAULT_REAL_IMAGE_PARAMS                                \
+	.bpp = FP_SENSOR_RES_BPP_FPC,                                    \
+	.frame_size = FP_SENSOR_REAL_IMAGE_SIZE_FPC,                     \
+	.pixel_format = V4L2_PIX_FMT_GREY, .width = FP_SENSOR_RES_X_FPC, \
 	.height = FP_SENSOR_RES_Y_FPC
 
 static const struct fp_image_frame_params fpc1145_image_frame_params[] = {
 	[FPC_CAPTURE_VENDOR_FORMAT] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_RAW_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_VENDOR_FORMAT,
 	},
 	[FPC_CAPTURE_SIMPLE_IMAGE] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_REAL_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_SIMPLE_IMAGE,
 	},
 	[FPC_CAPTURE_PATTERN0] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_REAL_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_PATTERN0,
 	},
 	[FPC_CAPTURE_PATTERN1] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_REAL_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_PATTERN1,
 	},
 	[FPC_CAPTURE_QUALITY_TEST] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_RAW_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_QUALITY_TEST,
 	},
 	[FPC_CAPTURE_RESET_TEST] =
 	{
-		FPC1145_DEFAULT_IMAGE_PARAMS,
+		FPC1145_DEFAULT_REAL_IMAGE_PARAMS,
 		.fp_capture_type = FP_CAPTURE_RESET_TEST,
 	},
 };
