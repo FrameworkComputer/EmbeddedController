4e3523ef921898c81c5cbe97ad857ad5dd862b1f
Firas Sammoura <fsammoura@google.com>
2025-12-20 02:22:17 +0000

    driver/fingerprint/fpc: Correct image height parameter for FPC1025
    Update the default image parameter macros to use the Y-axis resolution
    (FP_SENSOR_RES_Y_FPC) for the height field, instead of incorrectly
    using the X-axis resolution (FP_SENSOR_RES_X_FPC). This ensures the
    image dimensions are set correctly for both raw and real image
    captures.
    
    BUG=b:450381837
    TEST=CQ
    
    Change-Id: Icbe071e4a3ed6d2a10faadc8445c0dedd6ed46cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7284345
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- driver/fingerprint/fpc/bep/fpc_private.c
+++ driver/fingerprint/fpc/bep/fpc_private.c
@@ -53,13 +53,13 @@ static struct fp_sensor_info fpc1025_sensor_info = {
 #define FPC1025_DEFAULT_RAW_IMAGE_PARAMS                                      \
 	.bpp = FP_SENSOR_RES_BPP_FPC, .frame_size = FP_SENSOR_IMAGE_SIZE_FPC, \
 	.pixel_format = V4L2_PIX_FMT_GREY, .width = FP_SENSOR_RES_X_FPC,      \
-	.height = FP_SENSOR_RES_X_FPC
+	.height = FP_SENSOR_RES_Y_FPC
 
 #define FPC1025_DEFAULT_REAL_IMAGE_PARAMS                                \
 	.bpp = FP_SENSOR_RES_BPP_FPC,                                    \
 	.frame_size = FP_SENSOR_REAL_IMAGE_SIZE_FPC,                     \
 	.pixel_format = V4L2_PIX_FMT_GREY, .width = FP_SENSOR_RES_X_FPC, \
-	.height = FP_SENSOR_RES_X_FPC
+	.height = FP_SENSOR_RES_Y_FPC
 
 static const struct fp_image_frame_params fpc1025_image_frame_params[] = {
 	[FPC_CAPTURE_VENDOR_FORMAT] =
