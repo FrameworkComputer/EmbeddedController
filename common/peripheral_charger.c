bd6f5065f77101e3d1ecd78f98245f3dabde252c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-12-29 21:49:18 +0800

    pchg: Fix PCHG state not updating after stylus remove
    Fix PCHG state not updating when the stylus is removed.
    
    BUG=b:461992582
    TEST=1.zmake build padme
         2.pchg function normal.
    LOW_COVERAGE_REASON=Tests will be added in b:461992582
    
    Change-Id: I6bb41891eb3b949b4eb50a6971353cfb18bb2a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7320763
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

--- common/peripheral_charger.c
+++ common/peripheral_charger.c
@@ -845,7 +845,7 @@ static void pchg_shutdown(void)
 	CPRINTS("%s", __func__);
 
 	for (p = 0; p < pchg_count; p++) {
-		ctx = &pchgs[0];
+		ctx = &pchgs[p];
 		gpio_disable_interrupt(ctx->cfg->irq_pin);
 		board_pchg_power_on(p, 0);
 	}
@@ -859,8 +859,18 @@ void wpc_hall_handler(void)
 {
 	if (!gpio_get_level(GPIO_HALL_CTL_PCHG))
 		pchg_startup();
-	else
+	else {
+		struct pchg *ctx;
+		int p;
+
 		pchg_shutdown();
+		for (p = 0; p < pchg_count; p++) {
+			ctx = &pchgs[p];
+			ctx->battery_percent = 0;
+			pchg_queue_event(ctx, PCHG_EVENT_DEVICE_LOST);
+		}
+		task_wake(TASK_ID_PCHG);
+	}
 }
 DECLARE_DEFERRED(wpc_hall_handler);
 
