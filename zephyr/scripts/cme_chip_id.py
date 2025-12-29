228122f546b101f87b64795257b025b5adf4d38b
Ivan Chen <yulunchen@google.com>
2025-12-29 08:25:01 +0000

    cme: add more PDC and redriver chips
    GOOG0P00: RTS5452P-VB + JHL9040
    GOOG0S00: RTS5453P + JHL9040
    GOOG0W00: RTS5452P-VB + IT5205
    
    BUG=None
    TEST=probe on DUT
    
    Change-Id: Ic3b01f5db5970f0c97a85d9dd625cdb5d637fcda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7316927
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/scripts/cme_chip_id.py
+++ zephyr/scripts/cme_chip_id.py
@@ -1119,9 +1119,47 @@ DISAMBIGUATION_DICTIONARY = {
             },
             {
                 "reg": "0x80",
+                # the ascii characters "GOOG0P" on the 27-32 bytes
+                "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
+                "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730500000000000",
+                "bytes": 38,
+            },
+            None,
+            None,
+        ),
+        CompatibleInfo(
+            "realtek,rts5452p-vb",
+            None,
+            {
+                # the combined command string is 0x3A 0x3 0x0 0x0 0x26
+                "reg": "0x3A",
+                "write_data": "0x03000026",
+                "bytes": 0,
+            },
+            {
+                "reg": "0x80",
                 # the ascii characters "GOOG0R" on the 27-32 bytes
                 "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
                 "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730520000000000",
+                "bytes": 38,
+            },
+            None,
+            None,
+        ),
+        CompatibleInfo(
+            "realtek,rts5453p",
+            None,
+            {
+                # the combined command string is 0x3A 0x3 0x0 0x0 0x26
+                "reg": "0x3A",
+                "write_data": "0x03000026",
+                "bytes": 0,
+            },
+            {
+                "reg": "0x80",
+                # the ascii characters "GOOG0S" on the 27-32 bytes
+                "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
+                "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730530000000000",
                 "bytes": 38,
             },
             None,
@@ -1160,6 +1198,25 @@ DISAMBIGUATION_DICTIONARY = {
                 # the ascii characters "GOOG0U" on the 27-32 bytes
                 "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
                 "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730550000000000",
+                "bytes": 38,
+            },
+            None,
+            None,
+        ),
+        CompatibleInfo(
+            "realtek,rts5452p-vb",
+            None,
+            {
+                # the combined command string is 0x3A 0x3 0x0 0x0 0x26
+                "reg": "0x3A",
+                "write_data": "0x03000026",
+                "bytes": 0,
+            },
+            {
+                "reg": "0x80",
+                # the ascii characters "GOOG0W" on the 27-32 bytes
+                "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
+                "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730570000000000",
                 "bytes": 38,
             },
             None,
