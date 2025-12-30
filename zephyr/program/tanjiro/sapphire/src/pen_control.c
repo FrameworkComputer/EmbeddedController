0a71e2c1fbc260551cfdee4193f2d8dff8639561
mike5 <mike5@huaqin.corp-partner.google.com>
2025-12-30 09:28:13 +0800

    sapphire: enable pchg hall detect function
    Use the Hall sensor to detect whether the pen is present. At the
    same time, ensure compatibility with the previous board by configuring
    the Hall sensor to remain in an always-triggered state.
    
    BUG=b:468889062
    TEST=stylus can be charged on all boards.
    LOW_COVERAGE_REASON=b:468889062
    
    Change-Id: Ibf4ff32f73d119fb094e495a982b6ca0154b421f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7317426
    Reviewed-by: Knox Chiou <knoxchiou@chromium.org>
    Reviewed-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Commit-Queue: Knox Chiou <knoxchiou@chromium.org>

--- zephyr/program/tanjiro/sapphire/src/pen_control.c
+++ zephyr/program/tanjiro/sapphire/src/pen_control.c
@@ -4,6 +4,8 @@
  */
 
 #include "chipset.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
 #include "gpio/gpio_int.h"
 #include "gpio_signal.h"
 #include "hooks.h"
@@ -11,8 +13,11 @@
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/init.h>
+#include <zephyr/logging/log.h>
 
 #include <ap_power/ap_power.h>
+
+LOG_MODULE_REGISTER(pen_control, LOG_LEVEL_ERR);
 
 __override void board_pchg_power_on(int port, bool on)
 {
@@ -21,3 +26,22 @@ __override void board_pchg_power_on(int port, bool on)
 
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pp5000_wlc_en), on);
 }
+
+static void stylus_int_init(void)
+{
+	uint32_t board_version;
+
+	if (cbi_get_board_version(&board_version) != EC_SUCCESS) {
+		LOG_ERR("Failed to get board version.");
+		/* force enable stylus hall sensor detect function */
+		board_version = 2;
+	}
+
+	if (board_version <= 1) {
+		/* for EVT board, set interrupt always enable */
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_pen_pres),
+				      GPIO_OUTPUT);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_pen_pres), false);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, stylus_int_init, HOOK_PRIO_PRE_DEFAULT);
