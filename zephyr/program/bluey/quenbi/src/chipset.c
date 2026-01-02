a8e417282fd688da5f70bea9345eddbff0f7b3b7
Jayvik Desai <jayvik@google.com>
2026-01-02 15:44:26 +0530

    quenbi: Enable ec_batt_i2c_en_odl control for bluey
    Enable control for gpio_ec_batt_i2c_en_odl to allow ADSP to take control
    of the i2c bus
    
    EC_BATT_I2C_EN -> on when ADSP is the i2c bus controller.
    EC_BATT_I2C_EN -> off when EC is the i2c bus controller.
    
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    BUG=none
    TEST=zmake build %bluey
    
    Change-Id: I37fbea7a4fe899be34a71ceb0073e0638508e381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7365200
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Jayvik Desai <jayvik@google.com>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Jayvik Desai <jayvik@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>

--- zephyr/program/bluey/quenbi/src/chipset.c
+++ zephyr/program/bluey/quenbi/src/chipset.c
@@ -45,6 +45,7 @@ void board_chipset_startup_quenbi(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_batt_i2c_en_odl), 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup_quenbi,
 	     HOOK_PRIO_DEFAULT);
@@ -62,6 +63,7 @@ void board_chipset_shutdown_quenbi(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_hdmi_pwr), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 0);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_batt_i2c_en_odl), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown_quenbi,
 	     HOOK_PRIO_DEFAULT);
