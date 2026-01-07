ab9e087d4c798ede52f2ca4e43b5d22d13988e85
Tristan Honscheid <honscheid@google.com>
2026-01-07 13:34:09 -0700

    ocelotrvp: Use CSE early recovery GPIO driver
    Remove the board code that asserts the CSE early recovery GPIO and add
    the driver instead.
    
    BUG=b:470380019
    TEST=zmake build ocelotrvp*
    TEST=Run on RVP
    
    Change-Id: Ibe28eeadf12f3d7d7a4db0fb62161a5041a69db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7458501
    Tested-by: Tristan Honscheid <honscheid@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>
    Reviewed-by: Tom, Poornima <poornima.tom@intel.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/program/ocelot/src/rvp_board_power.c
+++ zephyr/program/ocelot/src/rvp_board_power.c
@@ -57,15 +57,6 @@ static int board_ap_power_action_g3_run(void *data)
 	if (ap_pwrseq_sm_is_event_set(data, AP_PWRSEQ_EVENT_POWER_STARTUP)) {
 		power_signal_set(PWR_EN_PP3300_A, 1);
 
-		/* Indication to soc on recovery boot */
-		if (system_is_manual_recovery()) {
-			gpio_pin_set_dt(
-				GPIO_DT_FROM_NODELABEL(cse_early_rec_sw), 1);
-		} else {
-			gpio_pin_set_dt(
-				GPIO_DT_FROM_NODELABEL(cse_early_rec_sw), 0);
-		}
-
 		update_ap_boot_time(ARAIL);
 	}
 
