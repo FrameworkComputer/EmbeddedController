aa4e3cb14fa0ef2c5839c54ce5f680720c425b0f
Tristan Honscheid <honscheid@google.com>
2026-01-07 13:25:00 -0700

    zephyr: Add shared driver for CSE early recovery GPIO
    PTL, WCL boards have a signal from EC to SoC that is asserted during
    recovery mode boot to provide an early warning to the CSE code in AP FW
    before the normal recovery mode host event is emitted by the EC.
    
    Because this is needed on many boards, furnish a simple driver for
    controlling this GPIO based on AP power callbacks. This eliminates the
    need to copy and paste this functionality into board code many times.
    
    Tests are included in subsequent CL.
    
    BUG=b:470380019
    TEST=zmake build ocelotrvp*
    TEST=./twister -T zephyr/test/ap_power (see next CL)
    
    Change-Id: Iee361cfae5b3731ac84f88ccb1f5873e5a28786f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7458499
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Tested-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Tristan Honscheid <honscheid@google.com>

---
+++ zephyr/drivers/cse_early_rec/cse_early_rec.c
@@ -0,0 +1,109 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define DT_DRV_COMPAT cros_ec_cse_early_rec
+
+#include "system.h"
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#include <ap_power/ap_pwrseq.h>
+
+LOG_MODULE_REGISTER(cse_early_rec, LOG_LEVEL_INF);
+
+BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 1,
+	     "only one 'cros-ec,cse-early-rec' compatible node may be present");
+
+struct cse_early_rec_config {
+	struct gpio_dt_spec cse_early_rec_gpio;
+} cse_early_rec_cfg = {
+	.cse_early_rec_gpio =
+		GPIO_DT_SPEC_GET(DT_DRV_INST(0), cse_early_rec_gpios),
+};
+
+struct cse_early_rec_data {
+	struct ap_pwrseq_state_callback ap_power_g3_exit_cb;
+} cse_early_rec_data;
+
+static void ap_power_g3_exit_cb(const struct device *dev,
+				const enum ap_pwrseq_state entry,
+				const enum ap_pwrseq_state exit)
+{
+	/* System is powering up from G3. Check the flags to see if a
+	 * boot to recovery mode is happening and set the CSE early
+	 * recovery GPIO appropriately.
+	 *
+	 * Note: this is NOT the authoritative mechanism for signalling
+	 * recovery mode boot to the AP. That is performed by the EC
+	 * sending a host event. This GPIO is read by the AP FW's CSE
+	 * blob, which needs to be aware of recovery boots before the
+	 * host event is sent.
+	 */
+	if (system_is_manual_recovery()) {
+		LOG_INF("CSE early recovery signal asserted");
+		gpio_pin_set_dt(&cse_early_rec_cfg.cse_early_rec_gpio, 1);
+	} else {
+		LOG_INF("CSE early recovery signal de-asserted");
+		gpio_pin_set_dt(&cse_early_rec_cfg.cse_early_rec_gpio, 0);
+	}
+}
+
+/**
+ * @brief Driver initialization function
+ *
+ *        Initialize the provided GPIO pin as an output and do not
+ * assert it at this time.
+ */
+static int cse_early_rec_driver_init(const struct device *dev)
+{
+	int ret;
+	struct cse_early_rec_data *data = dev->data;
+	const struct cse_early_rec_config *cfg = dev->config;
+	const struct device *ap_pwrseq_dev = ap_pwrseq_get_instance();
+
+	if (!gpio_is_ready_dt(&cfg->cse_early_rec_gpio)) {
+		/* LCOV_EXCL_START */
+		LOG_ERR("CSE early recovery GPIO is not ready");
+		return -ENODEV;
+		/* LCOV_EXCL_STOP */
+	}
+
+	ret = gpio_pin_configure_dt(&cfg->cse_early_rec_gpio,
+				    GPIO_OUTPUT_INACTIVE);
+	if (ret) {
+		/* LCOV_EXCL_START */
+		LOG_ERR("CSE early recovery pin config failed: %d", ret);
+		return ret;
+		/* LCOV_EXCL_STOP */
+	}
+
+	data->ap_power_g3_exit_cb = (struct ap_pwrseq_state_callback){
+		.cb = ap_power_g3_exit_cb,
+		.states_bit_mask = BIT(AP_POWER_STATE_G3),
+	};
+
+	ret = ap_pwrseq_register_state_exit_callback(
+		ap_pwrseq_dev, &data->ap_power_g3_exit_cb);
+
+	if (ret) {
+		/* LCOV_EXCL_START */
+		LOG_ERR("Cannot register AP power callback: %d", ret);
+		return ret;
+		/* LCOV_EXCL_STOP */
+	}
+
+	return 0;
+}
+
+/* This driver depends on GPIO drivers being ready. */
+BUILD_ASSERT(CONFIG_CSE_EARLY_RECOVERY_GPIO_DRIVER_INIT_PRIORITY >
+	     CONFIG_GPIO_INIT_PRIORITY);
+
+DEVICE_DT_INST_DEFINE(0, cse_early_rec_driver_init, NULL, &cse_early_rec_data,
+		      &cse_early_rec_cfg, POST_KERNEL,
+		      CONFIG_CSE_EARLY_RECOVERY_GPIO_DRIVER_INIT_PRIORITY,
+		      NULL);
