86030ccd40c8c16d54f717c816270c80ec96018b
Caveh Jalali <caveh@chromium.org>
2026-01-08 15:40:43 -0800

    battery: Print Manufacture Info string
    Add support for printing the Manufacture Info string in the EC console
    "battery" command.
    
    BUG=b:448252576
    TEST=checked fatcat-npcx with battery
    LOW_COVERAGE_REASON=b:448252576 determined delta coverage looks wrong
    
    Change-Id: I1c1e6c7474c87c56adbf231ce3dc2160b600c336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7415053
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- common/battery.c
+++ common/battery.c
@@ -134,6 +134,13 @@ static void print_battery_strings(void)
 	if (check_print_error(battery_manufacturer_name(text, sizeof(text))))
 		ccprintf("%s\n", text);
 
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_BATTERY_MANUF_INFO)) {
+		print_item_name("ManufInfo:");
+		if (check_print_error(
+			    battery_manufacture_info(text, sizeof(text))))
+			ccprintf("%s\n", text);
+	}
+
 	print_item_name("Device:");
 	if (check_print_error(battery_device_name(text, sizeof(text))))
 		ccprintf("%s\n", text);
@@ -744,6 +751,13 @@ test_mockable int battery_manufacturer_name(char *dest, int size)
 {
 	return get_battery_manufacturer_name(dest, size);
 }
+
+#ifdef CONFIG_PLATFORM_EC_BATTERY_MANUF_INFO
+test_mockable int battery_manufacture_info(char *dest, int size)
+{
+	return get_battery_manufacture_info(dest, size);
+}
+#endif /* CONFIG_PLATFORM_EC_BATTERY_MANUF_INFO */
 
 __overridable enum battery_disconnect_state battery_get_disconnect_state(void)
 {
--- include/battery.h
+++ include/battery.h
@@ -381,6 +381,15 @@ int get_battery_manufacturer_name(char *dest, int size);
 /**
  * Read manufacture info string.
  *
+ * @param dest		Destination buffer.
+ * @param size		Length of destination buffer in chars.
+ * @return non-zero if error.
+ */
+int battery_manufacture_info(char *dest, int size);
+
+/**
+ * Read manufacture info string.
+ *
  * This can be overridden to return a chip or board custom string.
  *
  * @param dest		Destination buffer.
