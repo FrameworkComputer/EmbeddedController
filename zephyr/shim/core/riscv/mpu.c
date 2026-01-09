04df15be94e281461c9f553fc2c0d9c99478c469
Firas Sammoura <fsammoura@google.com>
2026-01-09 10:47:56 -0800

    zephyr: riscv: Look up rollback region index dynamically
    The `mpu_lock_rollback` function previously assumed the rollback memory
    region was configured as Physical Memory Protection (PMP) region 0.
    This is unsafe, as the region's index in the memory attribute list
    is not guaranteed to be constant and may change depending on
    configuration.
    
    Replace the hardcoded index 0 with a dynamic lookup using the Zephyr
    API `mem_attr_get_region_index_by_name`. The function now searches for
    the region index associated with the `rollback` devicetree node label
    at runtime. This ensures the correct PMP region is locked or unlocked.
    
    A build assertion has been added to verify that the `rollback` node
    label is defined in the devicetree.
    
    BUG=b:406946889
    TEST=./test/run_device_tests.py -b sanok rollback_region0 --zephyr;
         flash on ET171 board; ztest run-testsuite rollback_region0;
         Repeat for rollback_region1
    LOW_COVERAGE_REASON=b:406946889, test is done on hardware
    
    Change-Id: Ibaa14e81e3cb00d7f442bf9ea52a5651f8145ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7424854
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>

--- zephyr/shim/core/riscv/mpu.c
+++ zephyr/shim/core/riscv/mpu.c
@@ -5,7 +5,14 @@
 #include "hooks.h"
 #include "system.h"
 
+#include <errno.h>
+
 #include <zephyr/arch/riscv/pmp.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/mem_mgmt/mem_attr.h>
+
+LOG_MODULE_REGISTER(riscv_mpu, LOG_LEVEL_ERR);
 
 static void prepare_for_sysjump_to_ec(void)
 {
@@ -21,14 +28,28 @@ static void prepare_for_sysjump_to_ec(void)
 DECLARE_HOOK(HOOK_SYSJUMP, prepare_for_sysjump_to_ec, HOOK_PRIO_LAST);
 
 #if defined(CONFIG_PLATFORM_EC_ROLLBACK_MPU_PROTECT)
+
+#define ROLLBACK_NODE DT_NODELABEL(rollback)
+
+BUILD_ASSERT(DT_NODE_EXISTS(ROLLBACK_NODE),
+	     "The 'rollback' node label is not defined in the Devicetree.");
+
 int mpu_lock_rollback(int lock)
 {
-	const int region_idx = 0;
-	if (lock) {
-		z_riscv_pmp_change_permissions(region_idx, 0);
-	} else {
-		z_riscv_pmp_change_permissions(region_idx, PMP_R | PMP_W);
+	const char *rollback_node_name = DT_NODE_FULL_NAME(ROLLBACK_NODE);
+	int rollback_region_idx =
+		mem_attr_get_region_index_by_name(rollback_node_name);
+
+	if (rollback_region_idx < 0) {
+		LOG_ERR("Rollback region index for '%s' not found.",
+			rollback_node_name);
+		return -ENOENT;
 	}
-	return 0;
+
+	if (lock) {
+		return z_riscv_pmp_change_permissions(rollback_region_idx, 0);
+	}
+	return z_riscv_pmp_change_permissions(rollback_region_idx,
+					      PMP_R | PMP_W);
 }
 #endif
