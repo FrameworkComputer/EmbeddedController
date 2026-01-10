f37932cff06ffd7edf059b3daa5a2cc78021d5c4
Tom Hughes <tomhughes@chromium.org>
2026-01-09 17:17:29 -0800

    zephyr: program: corsola: Fix enable_base_by_lid signature
    This interrupt handler takes an argument.
    
    BUG=b:474679693
    TEST=zmake compare-builds -b wyrdeer
    TEST=zmake compare-builds -b starmie
    TEST=CQ
    
    Change-Id: I1e7658cd5cc3ba958f2ab2f2b083fa05a378bb20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7426092
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Firas Sammoura <fsammoura@google.com>

--- zephyr/program/corsola/wyrdeer/src/base_detect.c
+++ zephyr/program/corsola/wyrdeer/src/base_detect.c
@@ -161,7 +161,7 @@ void base_force_state(enum ec_set_base_state_cmd state)
 	k_mutex_unlock(&modify_base_detection_mutex);
 }
 
-void enable_base_by_lid(void)
+void enable_base_by_lid(enum gpio_signal signal)
 {
 	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(lid_open))) {
 		base_detect_enable(true);
