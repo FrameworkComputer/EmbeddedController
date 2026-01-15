da5a0ed398ee2f9e3d5423f0e64264378777c801
Firas Sammoura <fsammoura@google.com>
2026-01-15 03:22:25 +0000

    cortex-m: Document mpu_lock_rollback parameters and returns
    Add Doxygen comments to the `mpu_lock_rollback()` function declaration
    in `core/cortex-m/include/mpu.h`.
    
    This change clarifies the behavior of the `lock` parameter, specifying
    that a non-zero value enables protection (locks) and zero disables
    it (unlocks). It also documents the return values: 0 indicates
    success, and a negative integer signifies an error. These
    enhancements improve code clarity and make the function easier to
    use correctly.
    
    BUG=b:475939514
    TEST=CQ
    
    Change-Id: Icd4bba12a6ae9c80744482cf11aad907c6874b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7480142
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- core/cortex-m/include/mpu.h
+++ core/cortex-m/include/mpu.h
@@ -130,7 +130,12 @@ int mpu_lock_ro_flash(void);
 int mpu_lock_rw_flash(void);
 
 /**
- * Protect/unprotect rollback region readback.
+ * @brief Protects or unprotects the rollback region readback.
+ *
+ * @param lock Non-zero to lock (protect), 0 to unlock (unprotect).
+ *
+ * @retval 0 Success.
+ * @retval <0 Error occurred.
  */
 int mpu_lock_rollback(int lock);
 
