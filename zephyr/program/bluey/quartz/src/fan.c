b15c4102d61faa1e9e3ad95b6220bd9d1c5a7ea8
Wen Zhang <zhangwen6@huaqin.corp-partner.google.com>
2026-01-16 15:50:17 +0800

    quartz: Enable custom fan control
    Set a fixed fan speed until we have a tuned fan table.
    
    BUG=b:442969499
    TEST=faninfo shows the target fan speed
    LOW_COVERAGE_REASON=Tests will be added in b:453895244
    
    Change-Id: I94efafe507e4f588cfe786526530ff9cf3c2fd81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7487617
    Reviewed-by: Ricky Chang <rickytlchang@chromium.org>
    Commit-Queue: Ricky Chang <rickytlchang@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Reviewed-by: Jayvik Desai <jayvik@google.com>

--- zephyr/program/bluey/quartz/src/fan.c
+++ zephyr/program/bluey/quartz/src/fan.c
@@ -5,9 +5,13 @@
 
 /* Quartz Fan configuration */
 
+#include "chipset.h"
 #include "common.h"
 #include "fan.h"
 #include "power/qcom.h"
+
+#define FAN_1_CH 0
+#define FAN_2_CH 1
 
 /**
  * Override default fan control.
@@ -24,4 +28,20 @@ enum fan_status board_override_fan_control_duty(int ch)
 	}
 
 	return fan_smart_control(ch);
+}
+
+void board_override_fan_control(int fan, int *temp)
+{
+	/*
+	 * In common/fan.c pwm_fan_stop() will turn off fan
+	 * when chipset suspend or shutdown.
+	 *
+	 * This is temporary workaround before the Fan table is ready.
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ON)) {
+		fan_set_rpm_mode(FAN_1_CH, true);
+		fan_set_rpm_target(FAN_1_CH, 6000);
+		fan_set_rpm_mode(FAN_2_CH, true);
+		fan_set_rpm_target(FAN_2_CH, 5000);
+	}
 }
