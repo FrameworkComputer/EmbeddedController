71706bacb4669b5e794f3396f448e1fe044cc54b
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2026-01-19 14:49:52 +0800

    lapis: Enable touchpad to follow lid behavior
    Disable the touchpad when the lid is closed to avoid abnormal
    wake-ups of the DUT caused by the touchpad.
    
    BUG=b:474251008
    TEST=1.zmake build lapis
         2.DUT will not wake when lid is closed.
    LOW_COVERAGE_REASON=b:451868360 track all lapis ztest code
    
    Change-Id: I08d18a62d1fc3c036634893b060282b7c6175f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7486534
    Reviewed-by: Ricky Chen <rickychen@google.com>
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Tested-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Commit-Queue: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/fatcat/lapis/src/board.c
+++ zephyr/program/fatcat/lapis/src/board.c
@@ -4,10 +4,22 @@
  */
 
 #include "charger.h"
+#include "gpio.h"
 #include "hooks.h"
+#include "lid_switch.h"
 
 static void set_chg_reg_custom(void)
 {
 	charger_set_frequency(808);
 }
 DECLARE_HOOK(HOOK_INIT, set_chg_reg_custom, HOOK_PRIO_POST_BATTERY_INIT + 1);
+
+static void tp_enable(void)
+{
+	if (lid_is_open()) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_disable), true);
+	} else {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_disable), false);
+	}
+}
+DECLARE_HOOK(HOOK_LID_CHANGE, tp_enable, HOOK_PRIO_DEFAULT);
