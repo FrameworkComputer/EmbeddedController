04222992836601c74adff50510a83a62362f4606
Firas Sammoura <fsammoura@google.com>
2026-01-23 13:26:57 -0800

    zephyr: test: add flash_config flash physical test for helipilot
    BUG=b:448407366
    TEST=./run_device_tests.py -b helipilot -f servo_micro -t flash_physical
         --zephyr
    
    Change-Id: Ie9d381e61bf989b26470d1732e428c26985f3fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7514283
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- zephyr/test/hwtest/src/flash_physical.c
+++ zephyr/test/hwtest/src/flash_physical.c
@@ -9,11 +9,39 @@
 
 ZTEST_SUITE(flash_physical, NULL, NULL, NULL, NULL, NULL);
 
-#ifdef CONFIG_SOC_FAMILY_STM32
+struct flash_info {
+	int num_flash_banks;
+	int write_protect_bank_offset;
+	int write_protect_bank_count;
+};
+
+#if defined(CONFIG_SOC_FAMILY_STM32)
 /* Covered by Zephyr tests. "flash_stm32" suite for STM32 chips */
 ZTEST(flash_physical, test_flash_physical)
 {
 	zassert_equal(1, 1);
+}
+#elif defined(CONFIG_SOC_FAMILY_NPCX)
+
+struct flash_info flash_info = {
+	.num_flash_banks = 16,
+	.write_protect_bank_offset = 0,
+	/* Helipilot's CONFIG_RO_SIZE is 128 KB and write protect size is 64 KB,
+	 * so there should be 2 banks
+	 */
+	.write_protect_bank_count = 2,
+};
+
+ZTEST(flash_physical, test_flash_config)
+{
+	zassert_equal(crec_flash_total_banks(), flash_info.num_flash_banks,
+		      "Flash total banks mismatch");
+
+	zassert_equal(WP_BANK_OFFSET, flash_info.write_protect_bank_offset,
+		      "WP bank offset mismatch");
+
+	zassert_equal(WP_BANK_COUNT, flash_info.write_protect_bank_count,
+		      "WP bank count mismatch");
 }
 #else
 #error "Flash tests not defined for this chip. Please add it."
