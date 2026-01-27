8c295912be0f6adce7e246324b9bde4bad6060d9
Firas Sammoura <fsammoura@google.com>
2026-01-27 14:49:58 -0800

    zephyr: test: add flash_config flash physical test for bloonchipper
    BUG=b:448407366
    TEST=./run_device_tests.py -b bloonchipper -f servo_micro -t
         flash_physical --zephyr
    
    Change-Id: I09a4d51ff93f980429a0d9654a684b8efe210cf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7523847
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>

--- zephyr/test/hwtest/src/flash_physical.c
+++ zephyr/test/hwtest/src/flash_physical.c
@@ -15,14 +15,21 @@ struct flash_info {
 	int write_protect_bank_count;
 };
 
+/*
+ * flash_config coverage only; see Zephyr "flash_stm32" for upstream STM32 flash
+ * tests.
+ */
 #if defined(CONFIG_SOC_FAMILY_STM32)
-/* Covered by Zephyr tests. "flash_stm32" suite for STM32 chips */
-ZTEST(flash_physical, test_flash_physical)
-{
-	zassert_equal(1, 1);
-}
+struct flash_info flash_info = {
+	/*
+	 * 12 sectors listed in STM32F412CG (Bloonchipper) layout:
+	 * https://chromium.googlesource.com/chromiumos/platform/ec/+/HEAD/docs/fingerprint/fingerprint-ram-and-flash.md#stm32f412cg-bloonchipper
+	 */
+	.num_flash_banks = 12,
+	.write_protect_bank_offset = 0,
+	.write_protect_bank_count = 5,
+};
 #elif defined(CONFIG_SOC_FAMILY_NPCX)
-
 struct flash_info flash_info = {
 	.num_flash_banks = 16,
 	.write_protect_bank_offset = 0,
@@ -31,6 +38,9 @@ struct flash_info flash_info = {
 	 */
 	.write_protect_bank_count = 2,
 };
+#else
+#error "Flash config tests not defined for this chip. Please add it."
+#endif
 
 ZTEST(flash_physical, test_flash_config)
 {
@@ -43,6 +53,3 @@ ZTEST(flash_physical, test_flash_config)
 	zassert_equal(WP_BANK_COUNT, flash_info.write_protect_bank_count,
 		      "WP bank count mismatch");
 }
-#else
-#error "Flash tests not defined for this chip. Please add it."
-#endif
