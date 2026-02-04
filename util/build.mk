c6d2d1a6eab3450fef226b6094f2c497a6418186
Jeremy Bettis <jbettis@chromium.org>
2026-02-03 16:36:48 -0800

    Delete other unused files
    These are legacy files not used in zephyr
    
    BUG=b:456553702
    TEST=CQ
    
    Change-Id: I99d86c979d73c0f559cb680c41cd9d1ba9071b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7540413
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Commit-Queue: Jon Murphy <jpmurphy@google.com>

--- util/build.mk
+++ util/build.mk
@@ -79,7 +79,7 @@ genvif-pd-objs=$(genvif-pd-srcs:%.c=$(out)/util/%.o)
 genvif-pd-objs += $(out)/common/usb_common.o $(out)/common/usb_pd_pdo.o
 deps-$(CONFIG_USB_POWER_DELIVERY) += $(genvif-pd-objs:%.o=%.o.d)
 
-$(out)/util/genvif: $(genvif-pd-objs) util/genvif.h board/$(BOARD)/board.h \
+$(out)/util/genvif: $(genvif-pd-objs) board/$(BOARD)/board.h \
 			include/usb_pd.h include/usb_pd_tcpm.h
 $(out)/util/genvif: BUILD_LDFLAGS+=$(genvif-pd-objs) -flto
 
