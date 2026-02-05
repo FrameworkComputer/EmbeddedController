8049e1637a820eb906cacc11feaa50207d36e01e
Abe Levkoy <alevkoy@chromium.org>
2026-02-05 16:46:54 -0700

    atria: Add initial Realtek RVP board
    Add minimal configuration for atria RVP with Realtek EC. This is a
    somewhat minimized copy from ocelot RVP configurations and ojal.
    Specific component selection has not been validated and will generally
    need to be updated.
    
    BUG=b:482448230
    TEST=zmake build atriarvp-rtk
    
    Change-Id: Ife9436a0189150730873b3beb99fbccdcae2d40c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7547505
    Commit-Queue: Abe Levkoy <alevkoy@chromium.org>
    Tested-by: Abe Levkoy <alevkoy@chromium.org>
    Auto-Submit: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

---
+++ zephyr/program/atria/BUILD.py
@@ -0,0 +1,41 @@
+# Copyright 2026 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for atria."""
+
+
+def register_rtk59_project(
+    project_name,
+    extra_kconfig_base_files=(),
+    extra_kconfig_proj_files=(),
+):
+    """Register a Realtek-based variant of atria."""
+    register_rtk_project(
+        project_name=project_name,
+        zephyr_board="realtek/rts5912",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Customization to apply before project-specific config.
+            *extra_kconfig_base_files,
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_proj_files,
+        ],
+    )
+
+
+# Realtek RVP SKU
+register_rtk59_project(
+    project_name="atriarvp-rtk",
+)
+
+
+# Note for reviews, do not let anyone edit these assertions, the addresses
+# must not change after the first RO release.
+assert_rw_fwid_DO_NOT_EDIT(project_name="atriarvp-rtk", addr=0x80404)
