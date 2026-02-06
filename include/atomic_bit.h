a3c3899e11c18b21810bf81b81e61c3510ac4abd
Patryk Duda <patrykd@google.com>
2026-02-06 17:53:33 +0000

    atomic: Add atomic_set() for EC
    The equivalent of the Zephyr atomic_set() function [1] in EC is
    atomic_exchange().
    
    Add atomic_set() to the EC code so that we can use it in code shared
    between EC and Zephyr.
    
    [1] https://docs.zephyrproject.org/4.3.0/doxygen/html/group__atomic__apis.html#ga5f0555245d8932c2e7f7e94575e1a095
    
    BUG=b:471160577
    TEST=None
    
    Change-Id: Ie52399917dcd8fe8de4a5189e9171b6a251dc964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7551952
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Patryk Duda <patrykd@google.com>
    Commit-Queue: Patryk Duda <patrykd@google.com>

--- include/atomic_bit.h
+++ include/atomic_bit.h
@@ -16,6 +16,11 @@ static inline atomic_val_t atomic_get(const atomic_t *target)
 {
 	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
 }
+
+static inline atomic_val_t atomic_set(atomic_t *target, atomic_val_t value)
+{
+	return __atomic_exchange_n(target, value, __ATOMIC_SEQ_CST);
+}
 #ifdef __cplusplus
 }
 #endif
