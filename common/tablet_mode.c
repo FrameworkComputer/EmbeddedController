12dbf3a45790d34b85240cc9b202e2049aa15f0c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2026-02-10 16:11:31 +0800

    tablet-mode: Fix tablet mode switch failure after reboot
    Fix an issue where tablet mode switching could malfunction
    after a reboot following a 360_L hall sensor trigger.
    
    BUG=b:482983270
    TEST=Verified tablet mode switching behaves normally
    LOW_COVERAGE_REASON=Tests will be added in b:482983270
    
    Change-Id: I34e182e0f75f91a324f420deb6eb323d688fa7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7561951
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Commit-Queue: Edward Dai <edwarddai@chromium.org>
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- common/tablet_mode.c
+++ common/tablet_mode.c
@@ -133,7 +133,7 @@ void tablet_set_mode(int mode, uint32_t trigger)
 		return;
 	}
 
-	if (IS_ENABLED(GMR_TABLET_MODE) &&
+	if (IS_ENABLED(GMR_TABLET_MODE) && !(trigger & TABLET_TRIGGER_BASE) &&
 	    ((gmr_sensor_at_360 && !mode) || (gmr_sensor_at_0 && mode))) {
 		/*
 		 * If tablet mode is being forced by the user, then this logging
