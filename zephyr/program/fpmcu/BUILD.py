f5ff17c0a98760b9cf5f9b22cbb98e6f400b4902
Firas Sammoura <fsammoura@google.com>
2026-02-25 18:22:53 +0000

    fpmcu: Remove deprecated gwendolin project
    The gwendolin platform is deprecated and no longer supported.
    
    This change removes the gwendolin variant registration from BUILD.py
    and deletes its associated files, including: Device Tree, Development
    key, and RW signature DTSi.
    
    BUG=b:487652426
    TEST=CQ
    
    Change-Id: I20762ed9076cb3380ebd340749ac67905a98e8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7609364
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- zephyr/program/fpmcu/BUILD.py
+++ zephyr/program/fpmcu/BUILD.py
@@ -108,28 +108,6 @@ helipilot = register_fpmcu_variant(
 # good reason to change it.
 assert_rw_fwid_DO_NOT_EDIT(project_name="helipilot", addr=0x40144)
 
-gwendolin = register_fpmcu_variant(
-    project_name="gwendolin",
-    zephyr_board="google_quincy",
-    register_func=register_npcx_project,
-    variant_modules=["cmsis", "cmsis_6"],
-    variant_optional_modules=["egis"],
-    variant_dts_overlays=[
-        here / "helipilot" / "gwendolin.dts",
-    ],
-    variant_kconfig_files=[
-        here / "helipilot" / "prj.conf",
-        here / "helipilot" / "ec_quirks.conf",
-    ],
-    signer=signers.RwsigSigner(  # pylint: disable=undefined-variable
-        here / "helipilot" / "gwendolin" / "dev_key.pem",
-    ),
-)
-
-# The address of RW_FWID is hardcoded in RO. You need to have REALLY
-# good reason to change it.
-assert_rw_fwid_DO_NOT_EDIT(project_name="gwendolin", addr=0x40144)
-
 
 def register_et171_project(
     project_name,
