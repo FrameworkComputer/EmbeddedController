111ea0bdd498c61dad7cd61ceaf2c753a4198e7f
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2026-02-26 09:09:53 +0800

    lapis:Update charger limit policy
    Implement charger limit policy of b/471527262#comment#6.
    
    BUG=b:471527262
    TEST=thermal engineer verified the policy pass
    LOW_COVERAGE_REASON=b:471527262 track all lapis ztest code
    
    Change-Id: I9420ea821af5f91a9e45941fafd994af18891bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7608882
    Tested-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Reviewed-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Commit-Queue: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>

---
+++ zephyr/program/fatcat/lapis/src/thermal_policy.c
@@ -0,0 +1,123 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_state.h"
+#include "extpower.h"
+#include "hooks.h"
+#include "power.h"
+#include "temp_sensor/temp_sensor.h"
+#include "usb_pd.h"
+
+#define POLL_COUNT 5
+#define CHARGER_LIMIT_LEVELS 4
+#define TEMP_MAX 120
+#define TEMP_MIN 0
+
+static int current_limit;
+
+static uint8_t charger_limit_level = 0;
+static uint8_t charger_trigger_cnt = 0;
+static uint8_t charger_release_cnt = 0;
+
+typedef enum {
+	LIMIT_NONE = 99999,
+	LIMIT_2300 = 2300,
+	LIMIT_1800 = 1800,
+	LIMIT_1000 = 1000
+} charge_limit_t;
+
+typedef struct {
+	uint8_t trigger_temp;
+	uint8_t release_temp;
+} temp_limit_t;
+
+static const charge_limit_t charger_limit_table[CHARGER_LIMIT_LEVELS] = {
+	LIMIT_NONE, LIMIT_2300, LIMIT_1800, LIMIT_1000
+};
+
+static const temp_limit_t charge_temp_limits[CHARGER_LIMIT_LEVELS - 1] = {
+	{ 51, 49 },
+	{ 60, 58 },
+	{ 62, 60 }
+};
+
+static bool temp_is_valid(int temp)
+{
+	return temp > TEMP_MIN && temp < TEMP_MAX;
+}
+
+static void update_charge_limit(void)
+{
+	int charger_temp_k, charger_temp;
+
+	temp_sensor_read(TEMP_SENSOR_ID(DT_NODELABEL(temp_charger)),
+			 &charger_temp_k);
+
+	charger_temp = K_TO_C(charger_temp_k);
+
+	if (!temp_is_valid(charger_temp)) {
+		charger_limit_level = 0;
+		return;
+	}
+
+	if (charger_limit_level < CHARGER_LIMIT_LEVELS - 1) {
+		temp_limit_t chg = charge_temp_limits[charger_limit_level];
+
+		if (charger_temp >= chg.trigger_temp) {
+			if (++charger_trigger_cnt >= POLL_COUNT) {
+				charger_limit_level++;
+				charger_trigger_cnt = 0;
+				charger_release_cnt = 0;
+			}
+		} else {
+			charger_trigger_cnt = 0;
+		}
+	}
+
+	if (charger_limit_level > 0) {
+		temp_limit_t chg = charge_temp_limits[charger_limit_level - 1];
+
+		if (charger_temp < chg.release_temp) {
+			if (++charger_release_cnt >= POLL_COUNT) {
+				charger_limit_level--;
+				charger_trigger_cnt = 0;
+				charger_release_cnt = 0;
+			}
+		} else {
+			charger_release_cnt = 0;
+		}
+	}
+}
+
+static void update_current_limit(void)
+{
+	if (extpower_is_present() && chipset_in_state(CHIPSET_STATE_ON)) {
+		update_charge_limit();
+	} else {
+		charger_limit_level = 0;
+	}
+
+	current_limit = charger_limit_table[charger_limit_level];
+}
+DECLARE_HOOK(HOOK_SECOND, update_current_limit, HOOK_PRIO_TEMP_SENSOR_DONE);
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	curr->requested_current = min(curr->requested_current, current_limit);
+
+	return EC_SUCCESS;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
