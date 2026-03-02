253afa76cb0c715060e5349d20d68d830833fc2d
Tony Chang <tonytcc@google.com>
2026-03-02 02:47:16 +0000

    cme: add more PDC and redriver chips
    add PDC and redriver chips:
    - RTS5453P-VB + TUSB546
    
    BUG=none
    TEST=none
    
    Change-Id: Ia088dc77e0b9730a98768feeb64a8b376479f08c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7622672
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Tested-by: Tony Chang <tonytcc@google.com>
    Commit-Queue: Tony Chang <tonytcc@google.com>

--- zephyr/scripts/cme_additional_id.py
+++ zephyr/scripts/cme_additional_id.py
@@ -349,5 +349,23 @@ ADDITIONAL_DICTIONARY = {
                 "bytes": 38,
             },
         ),
+        AdditionalInfo(
+            "ti,tusb546",
+            "mux",
+            None,
+            {
+                # the combined command string is 0x3A 0x3 0x0 0x0 0x26
+                "reg": "0x3A",
+                "write_data": "0x03000026",
+                "bytes": 0,
+            },
+            {
+                "reg": "0x80",
+                # the ascii characters "GOOG0X" on the 27-32 bytes
+                "multi_byte_mask": "0x000000000000000000000000000000000000000000000000000000ffffffffffff0000000000",
+                "multi_byte_value": "0x000000000000000000000000000000000000000000000000000000474f4f4730580000000000",
+                "bytes": 38,
+            },
+        ),
     ],
 }
