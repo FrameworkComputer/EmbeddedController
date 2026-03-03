a6ae947bffd6765ed9f7a5238af13ed78272b02e
Tom Hughes <tomhughes@chromium.org>
2026-03-02 16:17:51 -0800

    test: hwtest: panic_data: Add RISC-V support
    Update the panic_data test to check the return address (ra) register
    stored in the panic data and verifying the expected panic flags for
    RISC-V.
    
    BUG=b:488459074
    TEST=./test/run_device_tests.py --board sanok \
            --zephyr -t panic_data
          Manually flash resulting ec.bin to sanok
          ztest run-all
    Change-Id: Id6a7b8223a3e7cccc98eb41e961d3cab006bfd1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7618717
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Dawid Niedźwiecki <dawidn@google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- zephyr/test/hwtest/src/panic_data.c
+++ zephyr/test/hwtest/src/panic_data.c
@@ -21,6 +21,13 @@ static const uint32_t crash_addr = (uint32_t)test_crash;
 static const uint32_t crash_addr = (uint32_t)crash_system;
 #endif
 
+/*
+ * This is the index of RA in the panic_data.riscv.regs array, not the
+ * architectural register number. The layout is defined in
+ * https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/ec/zephyr/shim/src/panic.c;l=79;drc=f66d5f641b3a9aa8a715fb29bd0ce6b2bb96ac8a
+ */
+static const uint32_t RISCV_PANIC_REG_RA = 29;
+
 /* Estimated end of the crash function. */
 static const uint32_t crash_end = crash_addr + 0x20;
 
@@ -38,20 +45,35 @@ static void crash_system(void)
 static void check_panic_data(void)
 {
 	struct panic_data *pdata = panic_get_data();
-#ifdef CONFIG_ARM
-	uint32_t lr = pdata->cm.frame[CORTEX_PANIC_FRAME_REGISTER_LR];
+	uint32_t lr = 0;
+	uint32_t expected_flags = PANIC_DATA_FLAG_OLD_HOSTEVENT;
+
+	if (IS_ENABLED(SECTION_IS_RW)) {
+		expected_flags |= PANIC_DATA_FLAG_RW_IMAGE;
+	} else {
+		expected_flags |= PANIC_DATA_FLAG_RO_IMAGE;
+	}
+
+	if (IS_ENABLED(CONFIG_ARM)) {
+		lr = pdata->cm.frame[CORTEX_PANIC_FRAME_REGISTER_LR];
+		/*
+		 * This flag is for ARM only.
+		 * https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/ec/zephyr/shim/src/panic.c;l=152-154;drc=f66d5f641b3a9aa8a715fb29bd0ce6b2bb96ac8a
+		 * https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/ec/include/panic_defs.h;l=169-170;drc=ed1fa38550a14fd0aa35f1d7c79bf0aa3069acf6
+		 */
+		expected_flags |= PANIC_DATA_FLAG_FRAME_VALID;
+	} else if (IS_ENABLED(CONFIG_RISCV)) {
+		lr = pdata->riscv.regs[RISCV_PANIC_REG_RA];
+	} else {
+		zassert_true(false, "Unsupported architecture");
+	}
 
 	/* Make sure Link Register is stored correctly and points at the
 	 * crash_system function. */
 	zassert_true((crash_addr <= lr) && (crash_end >= lr));
 
 	/* Check panic flags. */
-	zassert_equal(pdata->flags, PANIC_DATA_FLAG_FRAME_VALID |
-					    PANIC_DATA_FLAG_OLD_HOSTEVENT |
-					    (IS_ENABLED(SECTION_IS_RW) ?
-						     PANIC_DATA_FLAG_RW_IMAGE :
-						     PANIC_DATA_FLAG_RO_IMAGE));
-#endif
+	zassert_equal(pdata->flags, expected_flags);
 }
 
 static void test_crash(void)
