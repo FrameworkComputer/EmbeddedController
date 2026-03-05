b24038fcd60ae3a60c752233c2079bd2635a8fc4
Dawid Niedzwiecki <dawidn@google.com>
2026-03-05 13:25:09 +0100

    zephyr: fpmcu: add stobnica project
    Add a new FPMCU, based on ET171 chip, with EC660 sensor,
    connected via USB. It uses additional GPIOs comparing to srebrna.
    
    BUG=b:489024826
    TEST=zmake build stobnica
    
    Change-Id: I593dda2978b523f835dc42c71d6ac0036a0ec85f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7637629
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Reviewed-by: Wenbin Bao <wenbin.bao@egistec.corp-partner.google.com>
    Reviewed-by: Jason Huang <jason.huang@egistec.corp-partner.google.com>
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>

--- zephyr/program/fpmcu/BUILD.py
+++ zephyr/program/fpmcu/BUILD.py
@@ -138,6 +138,9 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="sanok", addr=0x42104)
 srebrna = register_et171_project("srebrna")
 assert_rw_fwid_DO_NOT_EDIT(project_name="srebrna", addr=0x42104)
 
+stobnica = register_et171_project("stobnica")
+assert_rw_fwid_DO_NOT_EDIT(project_name="stobnica", addr=0x42104)
+
 niedzica = register_fpmcu_variant(
     project_name="niedzica",
     zephyr_board="32f967_dv",
