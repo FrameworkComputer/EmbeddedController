a889867e4176a9c324137a478234b194faf22083
Keith Short <keithshort@chromium.org>
2026-03-06 10:11:00 -0700

    Create rtk_flame stub project
    First step in having ec-devutils build and install the Realtek monitor
    code required for flashing.
    
    Create a stub project that triggers building the Realtek monitor code
    "rtk_flame" and generates the rts5912_flash_upload.bin executable.
    
    BUG=b:490412247
    TEST=zmake build rtk_flame. Verify flash_ec works with the generated
      rts5912_flash_upload.bin file.
    
    Change-Id: Id42da7ab8bd02b16dc05404e5f75034b808c3e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7643478
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Tested-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>

---
+++ zephyr/program/rtk_flame/BUILD.py
@@ -0,0 +1,26 @@
+# Copyright 2026 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for rtk_flame."""
+
+# This is a stub project, based on the Realtek EC, that triggers a build of the
+# the rts5915_flash_upload.bin binary.  See zephyr/chip/rtk_flame for details
+# on how this binary is generated.
+#
+# After running "zmake build rtk_flame" the Realtek monitor binary is
+# created at:
+#     build/zephyr/rtk_flame/build-singleimage/rts5912_flash_upload.bin
+#
+# Use the above artifact with the "rktupdate" utility when using the
+# "--method=frame" option.
+#
+# The Zephyr binaries listed below should *not* be used.
+#    build/zephyr/rtk_flame/build-singleimage/zephyr/zephyr.bin
+#    build/zephyr/rtk_flame/output/ec.bin
+
+register_raw_project(
+    project_name="rtk_flame",
+    zephyr_board="realtek/rts5912",
+    modules=["ec", "cmsis_6"],
+)
