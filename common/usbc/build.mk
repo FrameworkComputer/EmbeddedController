00891fcd7acb9dd90a80a408a11821026f661fd7
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2026-03-12 11:57:47 +0530

    TCPMV2: Move Discover Identity response handling to common TCPMv2 code
    BUG=b:498101464
    TEST=pass TEST.PD.VDM.SRC.1
    
    Change-Id: I59d6302b4f59efea3dffd973d7287973f875155f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7660084
    Tested-by: Debasmitha Ghosh <debasmitha.ghosh@7rays.corp-partner.google.com>
    Commit-Queue: Debasmitha Ghosh <debasmitha.ghosh@7rays.corp-partner.google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

--- common/usbc/build.mk
+++ common/usbc/build.mk
@@ -46,3 +46,6 @@ endif # CONFIG_USB_PD_TCPMV2
 common-usbc-$(CONFIG_TEST_USB_PD_TIMER) += usb_pd_timer.o
 common-usbc-$(CONFIG_TEST_USB_PE_SM) += usbc_pd_policy.o usb_pe_drp_sm.o
 common-usbc-$(CONFIG_TEST_SM) += usb_sm.o
+
+# SVDM DFP-only response
+common-usbc-$(CONFIG_SVDM_RSP_DFP_ONLY) += svdm_rsp_dfp_only.o
--- include/config.h
+++ include/config.h
@@ -1891,6 +1891,9 @@
  */
 #undef CONFIG_PANIC_LOG_DEBUG
 
+/* Enable generic SVDM DFP identity response */
+#undef CONFIG_SVDM_RSP_DFP_ONLY
+
 /*
  * noinit_end_of_ram is a memory section placed at the very end
  * of used ram, where it is least likely to overlap with RO ram.
