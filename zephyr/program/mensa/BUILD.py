e61b1e18ded0a7b725cb9aabd472e1d65c0c57ae
Jayvik Desai <jayvik@google.com>
2026-03-30 11:41:32 +0530

    mensa: add initial nuvoton board
    Add minimal config for bluey board based on nuvoton NPCX9 EC
    
    BUG=b:496650089
    TEST=zmake build mensa
    
    Change-Id: I3937ea8015d02fa2db054ffb2582b5309269bfa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7707043
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Jayvik Desai <jayvik@google.com>
    Auto-Submit: Jayvik Desai <jayvik@google.com>
    Tested-by: Jayvik Desai <jayvik@google.com>

---
+++ zephyr/program/mensa/BUILD.py
@@ -0,0 +1,36 @@
+# Copyright 2026 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for mensa."""
+
+
+def register_npcx9_project(
+    project_name,
+    extra_kconfig_files=(),
+):
+    """Register an npcx9 based variant of mensa."""
+    register_npcx_project(
+        project_name=project_name,
+        zephyr_board="npcx9/npcx9m7fb",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
+
+
+register_npcx9_project(
+    project_name="mensa",
+)
+
+# Note for reviews, do not let anyone edit these assertions, the addresses
+# must not change after the first RO release.
+assert_rw_fwid_DO_NOT_EDIT(project_name="mensa", addr=0x40144)
