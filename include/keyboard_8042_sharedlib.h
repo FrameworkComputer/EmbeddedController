ee4da32c030cb549d735df8e672fe92c2e771803
akshaim <akshaim@google.com>
2026-04-13 12:50:18 +0000

    keyboard_8042_sharedlib: define SCANCODE_CONTEXTUAL_INSERT
    Add support for KEY_CONTEXTUAL_INSERT.
    
    Picking 0xe01a, which maps to e02c in code 1 and scancode 0xac in the
    kernel, which is next available value from Coreboot
    src/acpi/acpigen_ps2_keybd.c action_keymaps[].
    
    BUG=b:500210160
    TEST=tested on Google/Fatcat
    
    Change-Id: I54f05eeb25e92eff9a8a8e0babd4f93e2aa41f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7755379
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Tested-by: Akshai Murari <akshaim@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Akshai Murari <akshaim@google.com>

--- include/keyboard_8042_sharedlib.h
+++ include/keyboard_8042_sharedlib.h
@@ -174,6 +174,7 @@ enum scancode_values {
 	SCANCODE_HOME = 0xe012, /* e02a in codeset 1 */
 	SCANCODE_INSERT = 0xe070, /* e052 in codeset 1 */
 	SCANCODE_SCREENLOCK = 0xe05d, /* e02b in codeset 1 */
+	SCANCODE_CONTEXTUAL_INSERT = 0xe01a, /* e02c in codeset 1 */
 
 	SCANCODE_UP = 0xe075,
 	SCANCODE_DOWN = 0xe072,
