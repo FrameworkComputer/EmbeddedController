ced65d5232f506d62a8d10885e0285b97dee3e9f
Boris Mittelberg <bmbm@google.com>
2026-04-20 17:54:11 -0700

    gpios: remove unused GPIO toggle
    remove unused 5V GPIO on/off toggling
    
    BUG=none
    TEST=build; manual test on moonstone
    
    Change-Id: I5f13f09de62067c1b82ec0f7d51f39b4d896da5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7780431
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Tested-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/bluey/mica/src/chipset.c
+++ zephyr/program/bluey/mica/src/chipset.c
@@ -17,7 +17,6 @@ void board_chipset_startup_mica(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_batt_i2c_en_odl), 1);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x), 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup_mica,
 	     HOOK_PRIO_DEFAULT);
@@ -30,7 +29,6 @@ void board_chipset_shutdown_mica(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_oled), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_batt_i2c_en_odl), 0);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown_mica,
 	     HOOK_PRIO_DEFAULT);
--- zephyr/program/fatcat/moonstone/src/suspend_resume.c
+++ zephyr/program/fatcat/moonstone/src/suspend_resume.c
@@ -20,13 +20,6 @@ static void moonstone_power_event_handler(struct ap_power_ev_callback *callback,
 	case AP_POWER_STARTUP:
 		/* Deassert AMP_MUTE_ODL when AP is on. */
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_amp_mute_odl), 1);
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x),
-				1);
-		break;
-	case AP_POWER_SHUTDOWN:
-		/* Deassert AMP_MUTE_ODL when AP is on. */
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x),
-				0);
 		break;
 	case AP_POWER_HARD_OFF:
 		/* Assert AMP_MUTE_ODL when powered off. */
