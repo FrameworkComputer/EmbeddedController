37712681cf73ae898dcc5537f5507bb7fbc2d047
Jan Dabros <dabros@google.com>
2026-05-07 09:16:17 +0000

    em32f967: Enable HW WP
    Change default state which previously configured WP to false. By the
    means of wp_external driver it can be disabled only in developer mode.
    
    BUG=b:446109143
    TEST=Check HW WP after reboot
    TEST=Execute fpdev_tool and verify if WP is disabled
    
    Change-Id: Ibc5a89c0e3e2b4aea0592f0b0a6e98469c09bfac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7826555
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Tested-by: Jan Dąbroś <dabros@google.com>
    Commit-Queue: Jan Dąbroś <dabros@google.com>

--- zephyr/program/fpmcu/em32f967/src/platform.c
+++ zephyr/program/fpmcu/em32f967/src/platform.c
@@ -6,15 +6,6 @@
 #include "system.h"
 #include "write_protect.h"
 
-/* TODO(b/446109143): Change based on the final solution of WP state. */
-static int disable_write_protect(void)
-{
-	disable_write_protect_external();
-
-	return 0;
-}
-SYS_INIT(disable_write_protect, POST_KERNEL, 0);
-
 /* Actual implementation is placed in the private repository. */
 __weak void chip_enter_bootloader(uint8_t mode)
 {
